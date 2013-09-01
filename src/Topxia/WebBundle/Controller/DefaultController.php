dd29b8c407d5848dd1f1adb371f29fa837f1c976
Wellming Li <wellming.li@howzhi.com>
2013-09-01 21:41:30 +0800

    导航埋点

--- src/Topxia/WebBundle/Controller/DefaultController.php
+++ src/Topxia/WebBundle/Controller/DefaultController.php
@@ -10,13 +10,22 @@ class DefaultController extends BaseController
         return $this->redirect($this->generateUrl('course_explore'));
     }
 
-    public function navigationAction()
+    public function topNavigationAction()
     {
     	$navigations = $this->getNavigationService()->findNavigationsByType('top', 0, 100);
 
-    	return $this->render('TopxiaWebBundle:Default:navigation.html.twig', array(
+    	return $this->render('TopxiaWebBundle:Default:top-navigation.html.twig', array(
     		'navigations' => $navigations,
 		));
+    }
+
+    public function footNavigationAction()
+    {
+        $navigations = $this->getNavigationService()->findNavigationsByType('foot', 0, 100);
+
+        return $this->render('TopxiaWebBundle:Default:foot-navigation.html.twig', array(
+            'navigations' => $navigations,
+        ));
     }
 
     protected function getNavigationService()
