5cea4339735f204c585ce720e8203ee26d3dfcd8
Wellming Li <wellming.li@howzhi.com>
2013-09-09 09:37:31 +0800

    fix bug

--- src/Topxia/WebBundle/Resources/public/js/controller/user/user.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/user/user.js
@@ -1,48 +1,25 @@
 define(function(require, exports, module) {
 
-  exports.run = function() {
+    exports.run = function() {
 
-      $('.nickname').find('.follow-btn').on('click', function(){
-          var $this = $(this);
-          $.post($this.data('url'), function(){
-            $this.hide();
-            $('.nickname').find('.unfollow-btn').show();
-          });
+        $('.follow-btn').on('click', function() {
+            var $this = $(this);
+            $.post($this.data('url'), function() {
+                $this.hide();
+                $('.unfollow-btn').show();
+            });
         });
 
 
-      $('.nickname').find('.unfollow-btn').on('click', function(){
-        var $this = $(this);
-        $.post($this.data('url'), function(){
-          $this.hide();
-          $('.nickname').find('.follow-btn').show();
+        $('.unfollow-btn').on('click', function() {
+            var $this = $(this);
+            $.post($this.data('url'), function() {
+                $this.hide();
+                $('.follow-btn').show();
+            });
         });
-      });
 
-      $textAbout = $('.personal-about').next('p');
-      var originText = $textAbout.html();
-      var originHeight=$textAbout.height();
-      var heightAfterCut;
 
-      if(originText.length > 250){
-        $textAbout.html(originText.substring(0,250));
-        heightAfterCut=$textAbout.height();
-        $textAbout.parents('.panel')
-          .append("<h5> <a class='action-personal-about pull-right' href='javascript:;'> 更多</a> </h5>");
-      }
-
-      $('.action-personal-about').on('click', function(){
-        var currentHeight=$textAbout.height();
-        if($textAbout.html().length > 250){
-          $textAbout.height(heightAfterCut).html(originText.substring(0,250));
-          $(this).html("更多");
-        } else {
-          $textAbout.height(originHeight).html(originText);
-          $(this).html("合起");
-        }        
-      });
-
-      
-  }
+    }
 
 });
