1e48e90381c765ffa6c905ad1b99604ebb2d5b97
Wellming Li <wellming.li@howzhi.com>
2013-10-11 17:49:15 +0800

    fix for mysql strict mode

--- src/Topxia/Service/Course/Impl/CourseServiceImpl.php
+++ src/Topxia/Service/Course/Impl/CourseServiceImpl.php
@@ -513,6 +513,23 @@ class CourseServiceImpl extends BaseService implements CourseService
 
 	public function createLesson($lesson)
 	{
+		$lesson = ArrayToolkit::filter($lesson, array(
+			'courseId' => 0,
+			'chapterId' => 0,
+			'free' => 0,
+			'title' => '',
+			'summary' => '',
+			'tags' => array(),
+			'type' => 'text',
+			'content' => '',
+			'media' => array(),
+			'length' => 0,
+		));
+
+		if (!ArrayToolkit::requireds($lesson, array('courseId', 'title', 'type'))) {
+			throw $this->createServiceException('参数缺失，创建课时失败！');
+		}
+
 		if (empty($lesson['courseId'])) {
 			throw $this->createServiceException('添加课时失败，课程ID为空。');
 		}
@@ -522,20 +539,16 @@ class CourseServiceImpl extends BaseService implements CourseService
 			throw $this->createServiceException('添加课时失败，课程不存在。');
 		}
 
-		//课程内容的过滤
+		//课程内容的过滤 @todo
 		// if(isset($lesson['content'])){
 		// 	$lesson['content'] = $this->purifyHtml($lesson['content']);
 		// }
 
 		// 课程处于发布状态时，新增课时，课时默认的状态为“未发布"
 		$lesson['status'] = $course['status'] == 'published' ? 'unpublished' : 'published';
-
-		$lesson['title'] = empty($lesson['title']) ? '' : $lesson['title'];
-		$lesson['summary'] = empty($lesson['summary']) ? null : $lesson['summary'];
 		$lesson['free'] = empty($lesson['free']) ? 0 : 1;
 		$lesson['number'] = $this->getNextLessonNumber($lesson['courseId']);
 		$lesson['seq'] = $this->getNextCourseItemSeq($lesson['courseId']);
-		$lesson['content'] = empty($lesson['content']) ? null : $lesson['content'];
 		$lesson['userId'] = $this->getCurrentUser()->id;
 		$lesson['createdTime'] = time();
 
@@ -562,8 +575,16 @@ class CourseServiceImpl extends BaseService implements CourseService
 			throw $this->createServiceException("课时(#{$lessonId})不存在！");
 		}
 
-		$fields = ArrayToolkit::parts($fields, array('title', 'summary', 'content', 'media', 'free', 'length'));
-		// if (isset($fields['content'])) {
+		$fields = ArrayToolkit::filter($fields, array(
+			'title' => '',
+			'summary' => '',
+			'content' => '',
+			'media' => array(),
+			'free' => 0,
+			'length' => 0,
+		));
+
+		// if (isset($fields['content'])) { @todo
 		// 	$fields['content'] = $this->purifyHtml($fields['content']);
 		// }
 
@@ -572,12 +593,12 @@ class CourseServiceImpl extends BaseService implements CourseService
 				throw $this->createServiceException("参数不正确，缺少media参数。");
 			}
 		} else {
-			$fields['media'] = null;
+			$fields['media'] = array();
 		}
 
-		$fields = LessonSerialize::serialize($fields);
-
-		return $this->getLessonDao()->updateLesson($lessonId, $fields);
+		return LessonSerialize::unserialize(
+			$this->getLessonDao()->updateLesson($lessonId, LessonSerialize::serialize($fields))
+		);
 	}
 
 	public function deleteLesson($courseId, $lessonId)
