f2ce7f8f7aae827fe56fc73f76eaf35aa89f843c
Wellming Li <wellming.li@howzhi.com>
2013-10-14 20:02:29 +0800

    修复settings不存在时，每次都更新cache的性能问题。

--- src/Topxia/Service/System/Impl/SettingServiceImpl.php
+++ src/Topxia/Service/System/Impl/SettingServiceImpl.php
@@ -24,7 +24,7 @@ class SettingServiceImpl extends BaseService implements SettingService
     public function get($name, $default = NULL)
     {
         if (is_null($this->cached)) {
-            $this->cached = $this->getCacheService()->get(self::CACHE_NAME);
+            $this->cached = $this->getCacheService()->get(self::CACHE_NAME) ? : array();
             if (is_null($this->cached)) {
                 $settings = $this->getSettingDao()->findAllSettings();
                 foreach ($settings as $setting) {
