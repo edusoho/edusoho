814fdd01c8cae6e8e1f0c4abeb507d45aa317f11
chenqingjin <chenqingjin@teksource.cn>
2013-10-15 18:58:18 +0800

    change user logo

--- src/Topxia/WebBundle/Resources/public/js/controller/homepage.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/homepage.js
@@ -8,13 +8,15 @@ define(function(require, exports, module) {
 
     	$("#photo .list-item").mouseenter(function(){
 			$(this).find(".tip").addClass("i-active");
-			$(this).siblings().css("opacity", "0.6");
+			//$(this).siblings().css("opacity", "0.6");
 			$(this).find(".todos-thumb-span").fadeIn("fast");
+			$(this).find(".todos-thumb-span").css("opacity", "0.6");
 		});
 		$("#photo .list-item").mouseleave(function(){
 			$(this).find(".tip").removeClass("i-active");
-			$(this).siblings().css("opacity", "1");
+			//$(this).siblings().css("opacity", "1");
 			$(this).find(".todos-thumb-span").fadeOut("slow");
+			$(this).find(".todos-thumb-span").css("opacity", "1");
 		});
 
 
