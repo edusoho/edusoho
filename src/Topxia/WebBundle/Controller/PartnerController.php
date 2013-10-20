8029d6d97e18a867303b44faa9667fa846d0d198
Wellming Li <wellming.li@howzhi.com>
2013-10-20 11:21:11 +0800

    phpwind

--- src/Topxia/WebBundle/Controller/PartnerController.php
+++ src/Topxia/WebBundle/Controller/PartnerController.php
@@ -2,6 +2,7 @@
 namespace Topxia\WebBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @todo 需要加个sign，来防止页面能直接打开
@@ -67,6 +68,61 @@ class PartnerController extends BaseController
 
 	}
 
+    public function windidAction(Request $request)
+    {
+        if (!defined('WEKIT_TIMESTAMP')) {
+            define('WEKIT_TIMESTAMP', time());
+        }
+
+        require_once __DIR__ . '/../../../../web/windid_client/src/windid/WindidApi.php';//引入windid接口类
+        require_once __DIR__ . '/../../../../web/windid_client/src/windid/service/base/WindidUtility.php'; //引入windid工具库
+
+        $windidKey = $request->query->get('windidkey');
+        $queryTime = $request->query->get('time');
+        $clientId = $request->query->get('clientid');
+        $operation = $request->query->get('operation');
+
+        $currentTimestamp = \Pw::getTime();
+
+        if (\WindidUtility::appKey(WINDID_CLIENT_ID, $queryTime, WINDID_CLIENT_KEY, $_GET, $_POST) != $windidKey) {
+            return $this->createWindidResponse('sign error.');
+        }
+
+        if ($currentTimestamp -> $queryTime >120) {
+            return $this->createWindidResponse('timeout.');
+        }
+
+        list($method, $args) = $this->getWindidMethod($operation);
+
+        if (!$method) {
+            return $this->createWindidResponse('fail');
+        }
+
+
+        $notify = new WindidNotify();  //定义一个通知处理类 在这时定义为下一步所示的notify
+        if(!method_exists($notify, $method)) {
+            return $this->createWindidResponse('success');
+        }
+
+        $result = call_user_func_array(array($notify, $method), $request->request->all());
+
+
+        if ($result == true) {
+            return $this->createWindidResponse('success');
+        }
+
+        return $this->createWindidResponse('fail');
+    }
+
+    private function getWindidMethod($operation)
+    {
+        $config = include  __DIR__ . '/../../../../web/windid_client/src/windid/service/base/WindidNotifyConf.php';
+        $method = isset($config[$operation]['method']) ? $config[$operation]['method'] : '';
+        $args = isset($config[$operation]['args']) ? $config[$operation]['args'] : array();
+        return array($method, $args);
+    }
+
+
     private function createWindidApi($name)
     {
         if (!defined('WEKIT_TIMESTAMP')) {
@@ -76,4 +132,39 @@ class PartnerController extends BaseController
         return \WindidApi::api($name);
     }
 
+    private function createWindidResponse($content = 'success')
+    {
+        return new Response($content);
+    }
+
+}
+
+class WindidNotify
+{
+    public function test($uid) {
+        return $uid ? true : false;
+    }
+            
+    public function addUser($uid) {
+        $api = WindidApi::api('user');
+        $user = $api->getUser($uid);
+            //客户端系统处理添加新用户
+         return true;
+    }
+            
+    public function editUser($uid) {
+        global $cfg_ml,$dsql;
+        $api = WindidApi::api('user');
+        $user = $api->getUser($uid);
+          //客户端系统处理修改用户信息
+        return true;
+    }
+        
+    public function synLogin($uid) {
+        return true;
+    }
+            
+    public function synLogout($uid) {
+        return true;
+    }
 }
