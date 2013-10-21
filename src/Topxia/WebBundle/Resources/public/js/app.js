02c36f12a7de32ed8df628a6b67914f105913c45
Wellming Li <wellming.li@howzhi.com>
2013-10-21 12:30:49 +0800

    fix text

--- src/Topxia/WebBundle/Resources/public/js/app.js
+++ src/Topxia/WebBundle/Resources/public/js/app.js
@@ -36,7 +36,7 @@ define(function(require, exports, module) {
 
 	if ($('html').hasClass('lt-ie8')) {
 		var message = '<div class="alert alert-warning" style="margin-bottom:0;text-align:center;">';
-		message += '您的浏览器版本太低，请使用';
+		message += '您的浏览器版本太低，不能正常使用本站，请使用';
 		message += '<a href="http://windows.microsoft.com/zh-CN/internet-explorer/downloads/ie" target="_blank">IE8浏览器</a>、';
 		message += '<a href="http://www.baidu.com/s?wd=%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8" target="_blank">谷歌浏览器</a><strong>(推荐)</strong>、';
 		message += '<a href="http://firefox.com.cn/download/" target="_blank">Firefox浏览器</a>，访问本站。';
