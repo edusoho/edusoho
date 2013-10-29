607bed90d750a89521854e909852fcb067a2c20b
chenqingjin <chenqingjin@teksource.cn>
2013-10-29 17:20:53 +0800

    modify experts

--- src/Topxia/WebBundle/Resources/public/js/controller/activity/show.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/activity/show.js
@@ -52,16 +52,7 @@ define(function(require, exports, module) {
             $('#qustiontext').focus();
         });
 
-        $('#teacher-carousel').carousel({interval: 0});
-        $('#teacher-carousel').on('slide.bs.carousel', function (e) {
-            var teacherId = $(e.relatedTarget).data('id');
-
-            $('#teacher-detail').find('.teacher-item').removeClass('teacher-item-active');
-            $('#teacher-detail').find('.teacher-item-' + teacherId).addClass('teacher-item-active');
-        });
-
-
-        
+               
         $(".reply").bind("click",createReply);
         //提问
         var validator = new Validator({
