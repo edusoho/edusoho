0ed567aec29363eb8e68a6ac8d76a5ef053ac656
Wellming Li <wellming.li@howzhi.com>
2013-10-29 23:06:54 +0800

    fix bug #1999, #1968

--- src/Topxia/Service/Content/Impl/NavigationServiceImpl.php
+++ src/Topxia/Service/Content/Impl/NavigationServiceImpl.php
@@ -44,7 +44,7 @@ class NavigationServiceImpl extends BaseService implements NavigationService
         $fields['createdTime'] = $fields['updateTime'] = time();
         $result = $this->getNavigationDao()->addNavigation($fields);
 
-        $this->getLogService()->info('info', 'navigation_create', "创建导航{$fields['name']}", $result);
+        $this->getLogService()->info('info', 'navigation_create', "创建导航{$fields['name']}");
 
         return $result;
     }
