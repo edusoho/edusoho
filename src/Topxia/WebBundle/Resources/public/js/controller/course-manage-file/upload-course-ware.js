437f27fd0c4697613f3a07b73a4a3e2cefb32035
shaojiatong <shaojiatong@howzhi.com>
2013-10-30 09:39:14 +0800

    update

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage-file/upload-course-ware.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage-file/upload-course-ware.js
@@ -1,10 +1,49 @@
 define(function(require, exports, module) {
 
-	require('jquery.plupload-full');
-	require('jquery.jquery-plupload-queue');
-
     exports.run = function() {
-        console.log("shaojiatong");
+    	
+
+    	$("#uploader").pluploadQueue({
+
+			runtimes : 'html5,flash,silverlight,html4',
+			url : '../upload.php',
+			chunk_size : '1mb',
+			unique_names : true,
+			
+			filters : {
+				max_file_size : '10mb',
+				mime_types: [
+					{title : "Audio Files", extensions : "mp3,wma,mp2,wav,aiff,aif,m4a,ra,dss"},
+		            {title : "Video files", extensions : "mov,mp4,wmv,rm"},
+					{title : "Image files", extensions : "jpg,gif,png"},
+					{title : "Zip files", extensions : "zip"}
+				]
+			},
+
+			resize : {width : 320, height : 240, quality : 90},
+
+			flash_swf_url : '../../js/Moxie.swf',
+			silverlight_xap_url : '../../js/Moxie.xap'
+		});
+
+		$('#form').submit(function(e) {
+		// Files in queue upload them first
+			if ($('#uploader').plupload('getFiles').length > 0) {
+
+				// When all files are uploaded submit form
+				$('#uploader').on('complete', function() {
+					$('#form')[0].submit();
+				});
+
+				$('#uploader').plupload('start');
+			} else {
+				alert("You must have at least one file in the queue.");
+			}
+			return false; // Keep the form from submitting
+
+		});
+
+
     };
 
 });
