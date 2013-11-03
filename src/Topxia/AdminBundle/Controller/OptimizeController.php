0d751abeaf5f82698028bea56f306a0cb42b04ce
Kent Wei <kent@howzhi.com>
2013-11-03 22:18:18 +0800

    add optimize structure

---
+++ src/Topxia/AdminBundle/Controller/OptimizeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Topxia\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Topxia\Common\ArrayToolkit;
+use Topxia\Common\FileToolkit;
+use Topxia\Common\Paginator;
+
+class SettingController extends BaseController
+{
+    public function siteAction(Request $request)
+    {
+
+    }
+
+}
