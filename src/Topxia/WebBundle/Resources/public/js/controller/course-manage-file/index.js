3cdd8c27b39a1a439fab8fab5fb763b56c9ce9d5
shaojiatong <shaojiatong@howzhi.com>
2013-11-05 20:25:36 +0800

    update file manage

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage-file/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage-file/index.js
@@ -2,8 +2,8 @@ define(function(require, exports, module) {
 
     exports.run = function() {
 
-        var $container = $('#course-ware-table-container');
-	    var $table = $("#course-ware-table");
+        var $container = $('#course-lesson-table-container');
+	    var $table = $("#course-lesson-table");
 	    require('../../util/batch-select')($container);
 	    require('../../util/batch-delete')($container);
 
