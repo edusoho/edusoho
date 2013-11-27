d300afa70447d94a812348bf2607789f8de0b731
Wellming Li <wellming.li@howzhi.com>
2013-11-27 15:20:50 +0800

    partner client config

---
+++ app/config/uc_client_config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'dz3');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`dz3`.pre_ucenter_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', '595exnobXzZAqF3Q0bToiIZlbT2Y7JvVPaefTvg');
+define('UC_API', 'http://www.dz3.com/uc_server');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '3');
+define('UC_PPP', '20');
