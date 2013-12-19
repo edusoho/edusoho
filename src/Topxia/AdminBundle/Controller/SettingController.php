d466b694886fcd0ba98f8347f73bcea20321ece4
Xxvholic Wu <xxvholic@howzhi.com>
2013-12-20 00:08:53 +0800

    私信自定义功能

--- src/Topxia/AdminBundle/Controller/SettingController.php
+++ src/Topxia/AdminBundle/Controller/SettingController.php
@@ -373,6 +373,7 @@ class SettingController extends BaseController
 
         $default = array(
             'welcome_message_enabled' => '0',
+            'welcome_body' => '{{nickname}},欢迎加入课程{{course}}'
         );
 
         $courseSetting = array_merge($default, $courseSetting);
