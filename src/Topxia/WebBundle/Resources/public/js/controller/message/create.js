8bd19fa7ad13baebfa624e2a6e2b5bd761f60691
shaojiatong <shaojiatong@howzhi.com>
2013-12-19 17:29:24 +0800

    fix bug

--- src/Topxia/WebBundle/Resources/public/js/controller/message/create.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/message/create.js
@@ -27,7 +27,12 @@ define(function(require, exports, module) {
 
         $('#message-create-btn').on('click','',function(){
             var $self = $(this);
+            
             $self.addClass('disabled');
+            setTimeout(function() {
+                $self.removeClass('disabled');
+            }, 1000);
+            
         });
 
         var autocomplete = new AutoComplete({
