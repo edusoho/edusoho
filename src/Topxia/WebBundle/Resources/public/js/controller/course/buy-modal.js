2a7b780e191bf4d7818151a996ab4cdebc0bd140
Xxvholic Wu <xxvholic@howzhi.com>
2013-12-25 16:53:24 +0800

    学员购买课程时填写个人资料功能

--- src/Topxia/WebBundle/Resources/public/js/controller/course/buy-modal.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/buy-modal.js
@@ -1,15 +1,25 @@
 define(function(require, exports, module) {
 
 	var Validator = require('bootstrap.validator');
+    require('common/validator-rules').inject(Validator);
 
     exports.run = function() {
         var validator = new Validator({
             element: '#course-buy-form',
-            autoSubmit: false
+            autoSubmit: true
         });
         if ($('#course-buy-form').find('input[name="mobile"]')){
             validator.addItem({
                 element: '[name="mobile"]',
+                rule: 'phone',
+                required: true
+            });
+        }
+
+        if ($('#course-buy-form').find('input[name="truename"]')){
+            validator.addItem({
+                element: '[name="truename"]',
+                rule: 'chinese byte_minlength{min:4} byte_maxlength{max:10}',
                 required: true
             });
         }
