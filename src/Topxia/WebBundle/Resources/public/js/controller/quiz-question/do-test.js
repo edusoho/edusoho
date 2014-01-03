dceb15db3a9f3a50cd24b43946a943136bc5ef73
Xxvholic Wu <xxvholic@howzhi.com>
2014-01-03 12:08:27 +0800

    d

--- src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/do-test.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/do-test.js
@@ -7,9 +7,10 @@ define(function(require, exports, module) {
         var changeAnswers = {};
 
         $('*[data-type]').each(function(index){
+            var name = $(this).attr('name');
 
             $(this).on('change', function(){
-                var name = $(this).attr('name');
+                // var name = $(this).attr('name');
 
                 var values = [];
                 //choice
@@ -32,20 +33,41 @@ define(function(require, exports, module) {
                 if ($(this).data('type') == 'fill') {
 
                     $('input[name='+name+']').each(function(){
-                        values.push($(this).val());
+                        if ($(this).val() != "") {
+                            values.push($(this).val());
+                        }
                     });
 
                 }
                 //essay
                 if ($(this).data('type') == 'essay') {
-                    values.push($(this).val());     
+                    if ($(this).val() != "") {
+                        values.push($(this).val());
+                    }     
 
                 }
                 changeAnswers[name] = values.join(',');
 
-                console.log(changeAnswers);
-            })
 
+                if (values.length > 0) {
+                    $('a[href="#question' + name + '"]').css('background-color', '#f5f5f5');
+                } else {
+                    $('a[href="#question' + name + '"]').css('background-color', 'inherit');
+                }
+
+
+            });
+
+        });
+
+        $('.panel-heading').on('click', 'a.btn', function(){
+            id = $(this).parents('.panel').attr('id');
+            btn = $('.answerCard .panel-body [href="#'+id+'"]');
+            if (btn.css('border-left-width') == '1px') {
+                btn.css('border', '2px solid #428bca');
+            } else {
+                btn.css('border', '1px solid #CCCCCC');
+            }
         });
 
         $('body').on('click', '#postPaper', function(){
