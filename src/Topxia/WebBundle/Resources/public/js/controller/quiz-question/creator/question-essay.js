8fe0663242297ed4ec3d15f077f648e9ce83ef31
Wellming Li <wellming.li@howzhi.com>
2014-01-10 15:22:02 +0800

    question ui

--- src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/creator/question-essay.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/creator/question-essay.js
@@ -1,6 +1,8 @@
 define(function(require, exports, module) {
 
     var BaseQuestion = require('./question-base');
+    var EditorFactory = require('common/kindeditor-factory');
+    var Uploader = require('upload');
 
     var EssayQuestion = BaseQuestion.extend({
         setup: function() {
@@ -13,6 +15,29 @@ define(function(require, exports, module) {
                 element: '#question-answer-field',
                 required: true
             });
+
+            var editor = EditorFactory.create('#question-answer-field', 'simple_noimage');
+            this.get('validator').on('formValidate', function(elemetn, event) {
+                editor.sync();
+            });
+
+            var $trigger = this.$('[data-role=answer-uploader]');
+            var uploader = new Uploader({
+                trigger: $trigger,
+                name: 'file',
+                action: this.element.data('uploadUrl'),
+                accept: 'image/*',
+                error: function(file) {
+                    Notify.danger('上传失败，请重试！')
+                },
+                success: function(response) {
+                    var result = '[image]' + response.hashId + '[/image]'
+                    editor.insertHtml(result);
+                }
+            });
+
+
+
         },
     });
 
