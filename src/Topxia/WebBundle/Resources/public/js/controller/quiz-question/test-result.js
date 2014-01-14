95dcec7eba51ca27730357371b2e4f5c3a4749cb
Xxvholic Wu <xxvholic@howzhi.com>
2014-01-14 19:02:21 +0800

    d

--- src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/test-result.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/test-result.js
@@ -19,40 +19,64 @@ define(function(require, exports, module) {
             alls.push($(this).attr('href'));
         });
 
+
         $('.answerCard').on('click', '#showWrong', function(){
-            
             $.each(alls, function(index, val){
                 if ($.inArray(val, wrongs) < 0) {
-                    $(val).hide();
-                } else {
-                    $(val).show();
+                    $(val).toggle();
                 }
             });
-            $(this).parent().find('.btn').removeClass('btn-info');
-            $(this).addClass('btn-info');
-        });
 
-        $('.answerCard').on('click', '#showNotRight', function(){
-            
-            $.each(alls, function(index, val){
-                if ($.inArray(val, rights) < 0) {
-                    $(val).hide();
-                } else {
-                    $(val).show();
+            $('.material').each(function(){
+                var isHidden = true;
+                $(this).find('div.panel').each(function(){
+                    id = $(this).attr('id');
+                    if (!$.inArray(id, wrongs)) {
+                        isHidden = false;
+                    }
+                });
+
+                if (isHidden){
+                    $(this).toggle();
                 }
             });
-            $(this).parent().find('.btn').removeClass('btn-info');
-            $(this).addClass('btn-info');
+
         });
 
-        $('.answerCard').on('click', '#showAll', function(){
+        // $('.answerCard').on('click', '#showWrong', function(){
             
-            $.each(alls, function(index, val){
-                $(val).show();
-            });
-            $(this).parent().find('.btn').removeClass('btn-info');
-            $(this).addClass('btn-info');
-        });
+            // $.each(alls, function(index, val){
+            //     if ($.inArray(val, wrongs) < 0) {
+            //         $(val).hide();
+            //     } else {
+            //         $(val).show();
+            //     }
+            // });
+            // $(this).parent().find('.btn').removeClass('btn-info');
+            // $(this).addClass('btn-info');
+        // });
+
+        // $('.answerCard').on('click', '#showNotRight', function(){
+            
+        //     $.each(alls, function(index, val){
+        //         if ($.inArray(val, rights) < 0) {
+        //             $(val).hide();
+        //         } else {
+        //             $(val).show();
+        //         }
+        //     });
+        //     $(this).parent().find('.btn').removeClass('btn-info');
+        //     $(this).addClass('btn-info');
+        // });
+
+        // $('.answerCard').on('click', '#showAll', function(){
+            
+        //     $.each(alls, function(index, val){
+        //         $(val).show();
+        //     });
+        //     $(this).parent().find('.btn').removeClass('btn-info');
+        //     $(this).addClass('btn-info');
+        // });
 
         $.each(alls, function(index, val){
             $(val).on('click', '.panel-footer a.btn', function(){
@@ -60,7 +84,7 @@ define(function(require, exports, module) {
             })
         });
 
-        $('input').attr('disabled', true);
+        $('.row input').attr('disabled', true);
         $('textarea').attr('disabled', true);
 
         $('body').on('click', '.favorite-btn', function(){
