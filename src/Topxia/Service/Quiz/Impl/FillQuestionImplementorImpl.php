0ecf61c64cbb856704e06014904354793d58ce9a
Xxvholic Wu <xxvholic@howzhi.com>
2014-01-15 14:15:14 +0800

    填空题编辑时候，修改-显示正则bug

--- src/Topxia/Service/Quiz/Impl/FillQuestionImplementorImpl.php
+++ src/Topxia/Service/Quiz/Impl/FillQuestionImplementorImpl.php
@@ -17,8 +17,9 @@ class FillQuestionImplementorImpl extends BaseService implements QuestionImpleme
 	public function getQuestion($question)
     {
         $question = QuestionSerialize::unserialize($question);
-    	$a = array_fill(0,count($question['answer']['0']), SUBJECT2);
-        $question['stem'] = preg_replace($a, $question['answer']['0'], $question['stem'], 1);
+        foreach ($question['answer'] as $key => $value) {
+            $question['stem'] = preg_replace(SUBJECT2, "[[".$value."]]", $question['stem'], 1);
+        }
         return $question;
     }
 
@@ -44,7 +45,7 @@ class FillQuestionImplementorImpl extends BaseService implements QuestionImpleme
         if(count($answer['1']) == 0){
             throw $this->createServiceException('该问题没有答案或答案格式不正确！');
         }
-        $field['answer'] = $answer;
+        $field['answer'] = $answer[1];
         return  QuestionSerialize::unserialize(
             $this->getQuizQuestionDao()->updateQuestion($id, QuestionSerialize::serialize($field))
         );
