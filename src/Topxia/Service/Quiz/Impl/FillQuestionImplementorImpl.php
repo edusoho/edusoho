888cac696b8e34f8e3a605ef151bb3ef93a179f8
Wellming Li <wellming.li@howzhi.com>
2014-01-21 01:51:08 +0800

    question refactor

--- src/Topxia/Service/Quiz/Impl/FillQuestionImplementorImpl.php
+++ src/Topxia/Service/Quiz/Impl/FillQuestionImplementorImpl.php
@@ -5,26 +5,39 @@ use Topxia\Service\Quiz\QuestionImplementor;
 use Topxia\Service\Quiz\Impl\QuestionSerialize;
 use Topxia\Common\ArrayToolkit;
 
-const PATTERN = "/\[\[(.+?)\]\]/";
-const SUBJECT1 = "(____)";
-const SUBJECT2 = "/\(____\)/";
-    
 class FillQuestionImplementorImpl extends BaseQuestionImplementor implements QuestionImplementor
 {
 	public function getQuestion($question)
     {
-        $question = QuestionSerialize::unserialize($question);
-        foreach ($question['answer'] as $key => $value) {
-            $value = implode('|', $value);
-            $question['stem'] = preg_replace(SUBJECT2, "[[".$value."]]", $question['stem'], 1);
-        }
-        return $question;
+        return QuestionSerialize::unserialize($question);
     }
 
 	public function createQuestion($question) {
         $question = $this->filterQuestionFields($question);
 
-        preg_match_all(PATTERN, $question['stem'], $answer, PREG_PATTERN_ORDER);
+        return QuestionSerialize::unserialize(
+            $this->getQuizQuestionDao()->addQuestion(QuestionSerialize::serialize($question))
+        );
+	}
+
+    public function updateQuestion($id, $question){
+        $question = $this->filterQuestionFields($question);
+
+        return  QuestionSerialize::unserialize(
+            $this->getQuizQuestionDao()->updateQuestion($id, QuestionSerialize::serialize($question))
+        );
+    }
+
+    private function getQuizQuestionDao()
+    {
+        return $this->createDao('Quiz.QuizQuestionDao');
+    }
+
+    protected function filterQuestionFields($question)
+    {
+        $question = parent::filterQuestionFields($question);
+        
+        preg_match_all("/\[\[(.+?)\]\]/", $question['stem'], $answer, PREG_PATTERN_ORDER);
         if (empty($answer[1])){
             throw $this->createServiceException('该问题没有答案或答案格式不正确！');
         }
@@ -38,34 +51,7 @@ class FillQuestionImplementorImpl extends BaseQuestionImplementor implements Que
             $question['answer'][] = $value;
         }
 
-        return QuestionSerialize::unserialize(
-            $this->getQuizQuestionDao()->addQuestion(QuestionSerialize::serialize($question))
-        );
-
-	}
-
-    public function updateQuestion($id, $question, $field){
-    	preg_match_all(PATTERN, $field['stem'], $answer);
-        $field['stem']  = preg_replace(PATTERN, SUBJECT1, $field['stem']);
-        if(count($answer['1']) == 0){
-            throw $this->createServiceException('该问题没有答案或答案格式不正确！');
-        }
-
-        foreach ($answer['1'] as $key => $value) {
-            $answer['1'][$key] = array_map(function($v){
-                return trim($v);
-            }, explode('|', $value));
-        }
-
-        $field['answer'] = $answer[1];
-        return  QuestionSerialize::unserialize(
-            $this->getQuizQuestionDao()->updateQuestion($id, QuestionSerialize::serialize($field))
-        );
-    }
-
-    private function getQuizQuestionDao()
-    {
-        return $this->createDao('Quiz.QuizQuestionDao');
+        return $question;
     }
 
 }
