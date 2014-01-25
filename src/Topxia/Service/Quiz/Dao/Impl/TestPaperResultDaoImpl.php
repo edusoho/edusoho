211f32744bead2a74ff14897dfa6c5a48dbf59c7
Xxvholic <xxvholic@howzhi.com>
2014-01-25 18:07:35 +0800

    d

--- src/Topxia/Service/Quiz/Dao/Impl/TestPaperResultDaoImpl.php
+++ src/Topxia/Service/Quiz/Dao/Impl/TestPaperResultDaoImpl.php
@@ -114,6 +114,14 @@ class TestPaperResultDaoImpl extends BaseDao implements TestPaperResultDao
         return $this->getConnection()->fetchAssoc($sql, array($testId, $userId)) ? : null;
     }
 
+    public function findTestPaperResultByTestIdAndStatusAndUserId($testId, $userId, array $status)
+    {
+        $marks = str_repeat('?,', count($status) - 1) . '?';
+        array_push($status, $testId, $userId);
+        $sql = "SELECT * FROM {$this->table} WHERE `status` IN ({$marks}) AND `testId` = ? AND `userId` = ? LIMIT 1";
+        return $this->getConnection()->fetchAssoc($sql, $status) ? : null;
+    }
+
     public function findTestPaperResultsByStatusAndTestIds ($ids, $status, $start, $limit)
     {
         if(empty($ids)){ 
