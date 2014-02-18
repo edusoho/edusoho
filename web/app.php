f6e75820dffa926d26a5fbee46925b59e44d48a1
Xxvholic <xxvholic@howzhi.com>
2014-02-18 17:58:21 +0800

    功能 #2186 安装脚本检测magic_quotes_gpc

--- web/app.php
+++ web/app.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Topxia\Service\Common\ServiceKernel;
 use Topxia\Service\User\CurrentUser;
 
+fix_gpc_magic();
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
@@ -57,3 +59,33 @@ try {
 
 $response->send();
 $kernel->terminate($request, $response);
+
+function _fix_gpc_magic(&$item) {
+  if (is_array($item)) {
+    array_walk($item, '_fix_gpc_magic');
+  }
+  else {
+    $item = stripslashes($item);
+  }
+}
+
+function _fix_gpc_magic_files(&$item, $key) {
+  if ($key != 'tmp_name') {
+    if (is_array($item)) {
+      array_walk($item, '_fix_gpc_magic_files');
+    }
+    else {
+      $item = stripslashes($item);
+    }
+  }
+}
+
+function fix_gpc_magic() {
+  if (ini_get('magic_quotes_gpc')) {
+    array_walk($_GET, '_fix_gpc_magic');
+    array_walk($_POST, '_fix_gpc_magic');
+    array_walk($_COOKIE, '_fix_gpc_magic');
+    array_walk($_REQUEST, '_fix_gpc_magic');
+    array_walk($_FILES, '_fix_gpc_magic_files');
+  }
+}
