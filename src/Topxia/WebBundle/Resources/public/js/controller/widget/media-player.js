dbcbb49f458b55aeca4fe6be17ab0c1027708a19
Wellming Li <wellming.li@howzhi.com>
2014-03-13 15:00:16 +0800

    fix media player cdn address

--- src/Topxia/WebBundle/Resources/public/js/controller/widget/media-player.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/widget/media-player.js
@@ -59,8 +59,7 @@ define(function(require, exports, module) {
             };
 
             if (this.get('src').indexOf('.m3u8') > 0 || this.get('src').indexOf('HLSQualitiyList') > 0) {
-                flashvars.plugin_hls = "http://cdn.staticfile.org/GrindPlayerCN/1.0.0/HLSProviderOSMF-0.5.0.swf";
-                flashvars.plugin_hls = "http://hlstest.qiniudn.com/HLSProviderOSMF.swf";
+                flashvars.plugin_hls = "http://cdn.staticfile.org/GrindPlayerCN/1.0.1/HLSProviderOSMF-0.5.0.swf";
             }
 
             var params = {
@@ -75,8 +74,7 @@ define(function(require, exports, module) {
             };
 
             swfobject.embedSWF(
-                // "http://cdn.staticfile.org/GrindPlayerCN/1.0.0/GrindPlayerCN.swf",
-                "http://hlstest.qiniudn.com/GrindPlayer-4.swf",
+                "http://cdn.staticfile.org/GrindPlayerCN/1.0.1/GrindPlayerCN.swf",
                 this.get('playerId'),
                 this.get('width'),  this.get('height') , "10.2", null, flashvars, params, attrs
             );
