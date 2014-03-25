8e7319e4ca758a32c910a7c5531ce3bec2f14f33
chenqingjin <chenqingjin@teksource.cn>
2014-03-25 12:03:27 +0800

    mysale

--- src/Topxia/WebBundle/Resources/public/js/controller/mysale/web-link-list.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/mysale/web-link-list.js
@@ -8,36 +8,28 @@ define(function(require, exports, module) {
 
         $(document).ready(function() {
 
-            var copyButton = 'copy';
+            var copyButton = 'copy-button';
 
-            var textInput = 'becopied';
 
-            $('.copy-button').each(function() { //给每个a标签加上copy样式
+            $('.copy-button').each(function() {
 
                 var thisId = this.id.replace(copyButton, '');
 
                 $(this).zclip({
 
-                    path: 'js/ZeroClipboard.swf',
+                    path: '/assets/libs/jquery-plugin/zclip/1.1.1/ZeroClipboard.swf',
+                    copy: function() {
 
-                    copy: $('p#becopied' + thisId).text()
+                        var hpid='p#becopied' + thisId;
+                       
+                        return $(hpid).text();
+                    },
+                    afterCopy:function(){
+                        $('div#copy'+ thisId).html('链接地址复制成功!');
+                        $('div#copy'+ thisId).css('color', 'green');
+                    }
+                });
 
-                })
-
-            })
-
-
-
-            $('#copy-button').zclip({
-                path: '/assets/libs/jquery-plugin/zclip/1.1.1/ZeroClipboard.swf',
-                copy: function() {
-                    return $('#content').val();
-                },
-                afterCopy:function(){
-                    $('#copy').html('链接地址复制成功!');
-                    $('#copy').css('color','green');
-
-                }
             });
 
         });
