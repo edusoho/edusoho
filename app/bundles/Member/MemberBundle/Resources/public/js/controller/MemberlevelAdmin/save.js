b2023a6cada51935654a75c7e795714c91668628
ChengBin Zheng <zhengchengbin@howzhi.com>
2014-03-31 17:42:08 +0800

    fix member level

--- app/bundles/Member/MemberBundle/Resources/public/js/controller/MemberlevelAdmin/save.js
+++ app/bundles/Member/MemberBundle/Resources/public/js/controller/MemberlevelAdmin/save.js
@@ -14,21 +14,37 @@ define(function(require, exports, module) {
             element: $form
         });
 
+        $form.find('[name="monthType"]:checked').trigger('change');
+
+        $form.find('[name="yearType"]:checked').trigger('change');
+
+        $form.on('change', '[name="monthType"]', function(e) {
+            if ($(this).is(':checked')) {
+                validator.addItem({
+                    element: '[name="monthPrice"]',
+                    required: true,
+                    rule:'currency arithmetic_number'            
+                });
+            } else {
+                validator.removeItem('[name="monthPrice"]');
+            };
+        });
+
+        $form.on('change', '[name="yearType"]', function(e) {
+            if ($(this).is(':checked')) {
+                validator.addItem({
+                    element: '[name="yearPrice"]',
+                    required: true,
+                    rule:'currency arithmetic_number'            
+                });
+            } else {
+                validator.removeItem('[name="yearPrice"]');
+            };
+        });
+
         validator.addItem({
             element: '[name="name"]',
             required: true
-        });
-
-        validator.addItem({
-            element: '[name="yearPrice"]',
-            required: true,
-            rule:'currency'            
-        });
-
-        validator.addItem({
-            element: '[name="monthPrice"]',
-            required: true,
-            rule:'currency'            
         });
 
         var editor = EditorFactory.create('#memberlevel-content-field', 'standard', {extraFileUploadParams:{}, height: '100px'});
