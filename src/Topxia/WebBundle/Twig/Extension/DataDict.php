4bc5d669afdbd6e572a2ffea5cb8095f5b29bdac
Wellming Li <wellming.li@howzhi.com>
2014-04-01 20:11:33 +0800

    fix

--- src/Topxia/WebBundle/Twig/Extension/DataDict.php
+++ src/Topxia/WebBundle/Twig/Extension/DataDict.php
@@ -117,10 +117,10 @@ class DataDict
 			'year' => '年'
 		),
 		'boughtType' => array(
-			'new' => '首次购买',
+			'new' => '购买',
 			'renew' => '续费',
 			'upgrade' => '升级',
-			'edit' => '管理员设置',
+			'edit' => '管理员编辑',
 			'cancel' => '取消会员'
 		),
 		'userKeyWordType' => array(
