9491a8528d7c18835c0c2645fe0f9d47213c7d35
ChengBin Zheng <zhengchengbin@howzhi.com>
2014-05-06 23:34:47 +0800

    #2919

--- src/Topxia/WebBundle/Controller/LiveCourseController.php
+++ src/Topxia/WebBundle/Controller/LiveCourseController.php
@@ -16,9 +16,72 @@ class LiveCourseController extends BaseController
 
 	}
 
-    public function listAction(Request $request)
+    public function listAction(Request $request, $category)
     {
-        
+        $now = time();
+
+        $today = date("Y-m-d");
+        $today = strtotime("$today");
+
+        $tomorrow = strtotime("$today tomorrow");
+
+        $nextweek = strtotime("$today next week");
+
+        if (!empty($category)) {
+            if (ctype_digit((string) $category)) {
+                $category = $this->getCategoryService()->getCategory($category);
+            } else {
+                $category = $this->getCategoryService()->getCategoryByCode($category);
+            }
+
+            if (empty($category)) {
+                throw $this->createNotFoundException();
+            }
+        } else {
+            $category = array('id' => null);
+        }
+
+        $group = $this->getCategoryService()->getGroupByCode('course');
+        if (empty($group)) {
+            $categories = array();
+        } else {
+            $categories = $this->getCategoryService()->getCategoryTree($group['id']);
+        }
+
+        $date = $request->query->get('date', 'today');
+
+        $conditions = array(
+            'status' => 'published',
+            'categoryId' => $category['id'],
+            'isLive' => '1'
+        );
+
+        $paginator = new Paginator(
+            $this->get('request'),
+            $this->getCourseService()->searchCourseCount($conditions)
+            , 10
+        );
+
+        $courses = $this->getCourseService()->searchCourses(
+            $conditions, 'lastest',
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
+        $popularCourses = $this->getCourseService()->searchCourses( array( 'status' => 'published', 'isLive' => '1' ), 'studentNum',0,10 );
+
+        return $this->render('TopxiaWebBundle:LiveCourse:list.html.twig',array(
+            'now' => $now,
+            'today' => $today,
+            'tomorrow' => $tomorrow,
+            'nextweek' => $nextweek,
+            'date' => $date,
+            'category' => $category,
+            'categories' => $categories,
+            'paginator' => $paginator,
+            'courses' => $courses,
+            'popularCourses' => $popularCourses
+        ));
     }
 
   	public function createAction(Request $request)
---
+++ src/Topxia/WebBundle/Resources/public/js/controller/live-course/list.js
@@ -0,0 +1,10 @@
+define(function(require, exports, module) {
+
+	exports.run = function() {
+        $('#live-course_categoryId').change(function(){
+            this.element.find.$(':checked');
+
+        });
+	};
+
+});
