65d912500f53b7321cade04664932c2f553d1f75
Wellming Li <wellming.li@howzhi.com>
2014-05-07 09:40:39 +0800

    fix bug

--- src/Topxia/WebBundle/Resources/public/js/controller/course/dashboard.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/dashboard.js
@@ -2,6 +2,18 @@ define(function(require, exports, module) {
 
     exports.run = function() {
         require('./common').run();
+
+
+        $('.announcement-list').on('click', '[data-role=delete]', function(){
+            if (confirm('真的要删除该公告吗？')) {
+                $.post($(this).data('url'), function(){
+                    window.location.reload();
+                });
+            }
+            return false;
+        });
+
+
     };
 
 });
