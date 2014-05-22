442d4c7a697843b5ea7e852aae46f54614fc448e
Wellming Li <wellming.li@howzhi.com>
2014-05-23 03:10:25 +0800

    mapi refactor

--- src/Topxia/MobileBundle/Controller/CourseController.php
+++ src/Topxia/MobileBundle/Controller/CourseController.php
@@ -8,19 +8,29 @@ use Topxia\Common\ArrayToolkit;
 use Topxia\WebBundle\Form\ReviewType;
 use Topxia\Service\Util\CloudClientFactory;
 
-class CustomSort
-{
-    static function sortChapters($a, $b)
-    {
-        return $a['number'] > $b['number'];
-    }
-}
-
 class CourseController extends MobileController
 {
     public function __construct()
     {
         $this->setResultStatus();
+    }
+
+    public function coursesAction(Request $request)
+    {
+        $conditions = $request->query->all();
+        $conditions['status'] = 'published';
+        
+        $result = array();
+        $result['total'] = $this->getCourseService()->searchCourseCount($conditions);
+        $result['start'] = (int) $request->query->get('start', 0);
+        $result['limit'] = (int) $request->query->get('limit', 10);
+        
+        $sort = $request->query->get('sort', 'latest');
+        $courses = $this->getCourseService()->searchCourses($conditions, $sort, $result['start'], $result['limit']);
+
+        $result['data'] = $courses = $this->filterCourses($courses);
+
+        return $this->createJson($request, $result);
     }
 
     public function courseAction(Request $request, $courseId)
@@ -140,35 +150,58 @@ class CourseController extends MobileController
         return $this->createJson($request, $json);
     }
 
-    public function getfavoriteCourseAction(Request $request)
+    public function favoriteAction(Request $request, $courseId)
     {
-        $token = $this->getUserToken($request);
-        if ($token) {
-            $page = $this->getParam($request, 'page', 0);
-            $favoriteCourses = $this->getCourseService()->findUserFavoritedCourses($token['userId'], $page, self::$defLimit);
-            $users = $this->getUserService()->findUsersByIds(ArrayToolkit::column($favoriteCourses, 'userId'));
-            $this->setResultStatus("success");
-            $this->result['users'] = $users;
-            $this->result['favoriteCourses'] = $this->changeCoursePicture($favoriteCourses, true);
-            $count = $this->getCourseService()->findUserFavoritedCourseCount($token['userId']);
-            $this->result = $this->setPage($this->result, $page, $count);
+        $this->getUserToken($request);
+        $user = $this->getCurrentUser();
+
+        if (!$user->isLogin()) {
+            return $this->createErrorResponse('not_login', "您尚未登录，不能收藏课程！");
         }
-        return $this->createJson($request, $this->result);
+
+        if (!$this->getCourseService()->hasFavoritedCourse($courseId)) {
+            $this->getCourseService()->favoriteCourse($courseId);
+        }
+
+        return $this->createJson($request, true);
     }
 
-    public function favoriteAction(Request $request)
+    public function unfavoriteAction(Request $request, $courseId)
     {
-        $token = $this->getUserToken($request);
-        if ($token) {
-            $course_id = $this->getParam($request, 'course_id');
-            if ($this->getCourseService()->hasFavoritedCourse($course_id)) {
-                $this->result['message'] = "课程已收藏";
-            } else if ($this->getCourseService()->favoriteCourse($course_id)) {
-                $this->setResultStatus("success");
-            }
+        $this->getUserToken($request);
+        $user = $this->getCurrentUser();
+
+        if (!$user->isLogin()) {
+            return $this->createErrorResponse('not_login', "您尚未登录，不能收藏课程！");
         }
-    
-        return $this->createJson($request, $this->result);
+
+        if (!$this->getCourseService()->hasFavoritedCourse($courseId)) {
+            return $this->createErrorResponse('runtime_error', "您尚未收藏课程，不能取消收藏！");
+        }
+
+        $this->getCourseService()->unfavoriteCourse($courseId);
+
+        return $this->createJson($request, true);
+    }
+
+    public function meFavoritesAction(Request $request)
+    {
+        $this->getUserToken($request);
+        $user = $this->getCurrentUser();
+
+        if (!$user->isLogin()) {
+            return $this->createErrorResponse('not_login', "您尚未登录，不能收藏课程！");
+        }
+
+        $result = array();
+        $result['start'] = (int) $request->query->get('start', 0);
+        $result['limit'] = (int) $request->query->get('limit', 10);
+        $result['total'] = $this->getCourseService()->findUserFavoritedCourseCount($user['id']);
+
+        $courses = $this->getCourseService()->findUserFavoritedCourses($user['id'], $result['start'], $result['limit']);
+        $result['data'] = $this->filterCourses($courses);
+
+        return $this->createJson($request, $result);
     }
 
     public function learnStatusAction(Request $request, $courseId, $lessonId)
@@ -226,21 +259,6 @@ class CourseController extends MobileController
         return $this->createJson($request, $this->result);
     }
 
-    public function unFavoriteAction(Request $request)
-    {
-        $token = $this->getUserToken($request);
-        if ($token) {
-            $course_id = $this->getParam($request, 'course_id', 0);
-            if ($this->getCourseService()->hasFavoritedCourse($course_id)) {
-                if ($this->getCourseService()->unFavoriteCourse($course_id)) {
-                   $this->setResultStatus("success");
-                }  
-            }
-        }
-        
-        return $this->createJson($request, $this->result);
-    }
-
     public function getLearnCourseAction(Request $request)
     {   
         $token = $this->getUserToken($request);
@@ -280,129 +298,6 @@ class CourseController extends MobileController
         return $learnCourses;
     }
 
-    public function getCourseLessonAction(Request $request, $course_id)
-    {
-        $courseLesson = $this->getCourseService()->getCourseItems($course_id);
-        $rootChapters = array(
-            "id"=>"0",
-            "number"=>0,
-            "createdTime"=>"",
-            "seq"=>"0",
-            "courseId"=>$course_id,
-            "title"=>"课时列表"
-        );
-        $chapters = $this->getCourseService()->getCourseChapters($course_id);
-        array_unshift($chapters, $rootChapters);
-
-        $sort = new CustomSort();
-        usort($chapters, array($sort,'sortChapters'));
-
-        for ($i=0; $i<count($chapters); $i++) {
-            $chapters[$i]['course_lesson_list'] = $this->_findLessonByChapterId($courseLesson, $chapters[$i]['id']);
-        }
-        
-        $hls = array();
-        foreach ($courseLesson as $lesson) {
-            if (! isset($lessoon["type"]) || ! isset($lesson['mediaSource'])) {
-                continue;
-            }
-            if ($lesson['type'] == 'video' and $lesson['mediaSource'] == 'self') {
-                $file = $this->getUploadFileService()->getFile($lesson['mediaId']);
-                if (!empty($file['metas2']) && !empty($file['metas2']['hd']['key'])) {
-                    $factory = new CloudClientFactory();
-                    $client = $factory->createClient();
-                    $hls[$lesson["id"]] = $client->generateHLSQualitiyListUrl($file['metas2'], 3600);
-                }
-            }
-        }
-        
-        $result = array(
-            "chapters"=>$chapters,
-            "hls"=>$hls
-        );
-        return $this->createJson($request, $result);
-    }
-
-    protected function _findLessonByChapterId($courseLesson, $chapterId)
-    {
-        $temp_lessons = array();
-        foreach($courseLesson as $lesson) {
-            if (isset($lesson['chapterId']) && $lesson['chapterId'] == $chapterId) {
-                array_push($temp_lessons, $lesson);
-            }
-        }
-        return $temp_lessons;
-    }
-
-    /**
-    * sort => latest, popular, recommended, Rating, hitNum, studentNum, createdTime
-    */
-    public function getCourseListAction(Request $request)
-    {
-        $conditions = array('status' => 'published');
-        $search = $request->query->get('search');
-        if ($search) {
-            $conditions['title'] = $search;
-        }
-
-        $sort = $request->query->get('sort');
-        if (empty($sort)) {
-            $sort = "latest";
-        }
-
-        $page = $request->query->get('page');
-        if (empty($page)) {
-            $page = 0;
-        }
-
-        $count = $this->getCourseService()->searchCourseCount($conditions);
-        $courses = $this->getCourseService()->searchCourses($conditions, $sort, $page * self::$defLimit, self::$defLimit);
-        $courses = $this->changeCoursePicture($courses, true);
-
-        $result = $this->coursesBlock($courses);
-        $result = $this->setPage($result, $page, $count);
-
-        return $this->createJson($request, $result);
-    }
-
-    protected function coursesBlock($courses, $mode = 'default')
-    {
-        $userIds = array();
-        foreach ($courses as $course) {
-            $userIds = array_merge($userIds, $course['teacherIds']);
-        }
-        $users = $this->getUserService()->findUsersByIds($userIds);
-        $users = $this->changeUserPicture($users, true);
-
-        return array(
-            'courses' => $courses,
-            'users' => $users,
-            'mode' => $mode,
-        );
-    }
-
-    private function getEnabledPayments()
-    {
-        $enableds = array();
-
-        $setting = $this->setting('payment', array());
-
-        if (empty($setting['enabled'])) {
-            return $enableds;
-        }
-
-        $payNames = array('alipay');
-        foreach ($payNames as $payName) {
-            if (!empty($setting[$payName . '_enabled'])) {
-                $enableds[$payName] = array(
-                    'type' => empty($setting[$payName . '_type']) ? '' : $setting[$payName . '_type'],
-                );
-            }
-        }
-
-        return $enableds;
-    }
-
     protected function filterCourse($course)
     {
         if (empty($course)) {
@@ -420,12 +315,10 @@ class CourseController extends MobileController
             return array();
         }
 
-        $teacherIdss = ArrayToolkit::column($courses, 'teacherIds');
         $teacherIds = array();
-        foreach ($teacherIdss as $ids) {
-            $teacherIds = array_merge($teacherIds, $ids);
+        foreach ($courses as $course) {
+            $teacherIds = array_merge($teacherIds, $course['teacherIds']);
         }
-
         $teachers = $this->getUserService()->findUsersByIds($teacherIds);
         $teachers = $this->simplifyUsers($teachers);
 
@@ -457,14 +350,6 @@ class CourseController extends MobileController
             return $item;
         }, $items);
 
-    }
-
-    private function canShowCourse($course, $user)
-    {
-        return ($course['status'] == 'published') or 
-            $user->isAdmin() or 
-            $this->getCourseService()->isCourseTeacher($course['id'],$user['id']) or
-            $this->getCourseService()->isCourseStudent($course['id'],$user['id']);
     }
 
     protected function getCourseService()
