c2ec5d7342ad5eb1f2aa18e07a27233d003b9483
Su ju <suju@howzhi.com>
2014-05-27 14:04:46 +0800

    fix搜索课程bug

--- src/Topxia/MobileBundle/Controller/CourseController.php
+++ src/Topxia/MobileBundle/Controller/CourseController.php
@@ -14,14 +14,15 @@ class CourseController extends MobileController
         $conditions = $request->query->all();
         $conditions['status'] = 'published';
         
-        $result = array();
-        $result['total'] = $this->getCourseService()->searchCourseCount($conditions);
-        $result['start'] = (int) $request->query->get('start', 0);
-        $result['limit'] = (int) $request->query->get('limit', 10);
         $search = $request->query->get('search', '');
         if ($search != '') {
             $conditions['title'] = $search;
         }
+
+        $result = array();
+        $result['total'] = $this->getCourseService()->searchCourseCount($conditions);
+        $result['start'] = (int) $request->query->get('start', 0);
+        $result['limit'] = (int) $request->query->get('limit', 10);
         
         $sort = $request->query->get('sort', 'latest');
         $courses = $this->getCourseService()->searchCourses($conditions, $sort, $result['start'], $result['limit']);
