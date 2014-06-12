d91d806688b5029beef0a7d6193b0c998fcb032c
Wellming Li <wellming.li@howzhi.com>
2014-06-12 20:36:43 +0800

    clean code

--- src/Topxia/MobileBundle/Alipay/MobileAlipayConfig.php
+++ src/Topxia/MobileBundle/Alipay/MobileAlipayConfig.php
@@ -44,11 +44,11 @@ class MobileAlipayConfig
 
 		//商户的私钥（后缀是.pen）文件相对路径
 		//如果签名方式设置为“0001”时，请设置该参数
-		$alipay_config['private_key_path'] = 'key/rsa_private_key.pem';
+		// $alipay_config['private_key_path'] = 'key/rsa_private_key.pem';
 
-		//支付宝公钥（后缀是.pen）文件相对路径
-		//如果签名方式设置为“0001”时，请设置该参数
-		$alipay_config['ali_public_key_path'] = 'key/alipay_public_key.pem';
+		// //支付宝公钥（后缀是.pen）文件相对路径
+		// //如果签名方式设置为“0001”时，请设置该参数
+		// $alipay_config['ali_public_key_path'] = 'key/alipay_public_key.pem';
 
 		//签名方式 不需修改
 		$alipay_config['sign_type'] = 'MD5';
