fda4dc4df6e2b8d46459fdecd9b78eb84d532560
Jiajun Xu <xujiajun@howzhi.com>
2014-06-17 12:01:01 +0800

    magrate

--- src/Topxia/Service/Course/Impl/CourseServiceImpl.php
+++ src/Topxia/Service/Course/Impl/CourseServiceImpl.php
@@ -328,7 +328,8 @@ class CourseServiceImpl extends BaseService implements CourseService
 			'endTime'  => 0,
 			'locationId' => 0,
 			'address' => '',
-			'maxStudentNum' => 0
+			'maxStudentNum' => 0,
+			'homeworkCompleteLimit' => 'no'
 		));
 
 		if (!empty($fields['about'])) {
