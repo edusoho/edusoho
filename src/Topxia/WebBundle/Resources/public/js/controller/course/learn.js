1536d0075a0abdb3805ada686f68a14373636eae
Wellming Li <wellming.li@howzhi.com>
2014-06-24 10:38:48 +0800

    ppt

--- src/Topxia/WebBundle/Resources/public/js/controller/course/learn.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/learn.js
@@ -372,21 +372,17 @@ define(function(require, exports, module) {
                     }
 
                     else if (lesson.type == 'ppt') {
-                        // $.get(that.get('courseUri') + '/lesson/' + id + '/ppt', function(response) {
+                        $.get(that.get('courseUri') + '/lesson/' + id + '/ppt', function(response) {
 
-                        var response = ['http://localhost/ppt/img-1.jpg', 'http://localhost/ppt/img-2.jpg', 'http://localhost/ppt/img-3.jpg', 'http://localhost/ppt/img-4.jpg']
+                            var html = '<div class="slide-player"><div class="slide-player-body loading-background"></div><div class="slide-notice"><div class="header">已经到最后一张图片了哦<button type="button" class="close">×</button></div></div><div class="slide-player-control clearfix"><a href="javascript:" class="goto-first"><span class="glyphicon glyphicon-step-backward"></span></a><a href="javascript:" class="goto-prev"><span class="glyphicon glyphicon-chevron-left"></span></a><a href="javascript:" class="goto-next"><span class="glyphicon glyphicon-chevron-right"></span></span></a><a href="javascript:" class="goto-last"><span class="glyphicon glyphicon-step-forward"></span></a><a href="javascript:" class="fullscreen"><span class="glyphicon glyphicon-fullscreen"></span></a><div class="goto-index-input"><input type="text" class="goto-index form-control input-sm" value="1">&nbsp;/&nbsp;<span class="total"></span></div></div></div>';
+                            $("#lesson-ppt-content").html(html).show();
 
-                        var html = '<div class="slide-player"><div class="slide-player-body loading-background"></div><div class="slide-notice"><div class="header">已经到最后一张图片了哦<button type="button" class="close">×</button></div></div><div class="slide-player-control clearfix"><a href="javascript:" class="goto-first"><span class="glyphicon glyphicon-step-backward"></span></a><a href="javascript:" class="goto-prev"><span class="glyphicon glyphicon-chevron-left"></span></a><a href="javascript:" class="goto-next"><span class="glyphicon glyphicon-chevron-right"></span></span></a><a href="javascript:" class="goto-last"><span class="glyphicon glyphicon-step-forward"></span></a><a href="javascript:" class="fullscreen"><span class="glyphicon glyphicon-fullscreen"></span></a><div class="goto-index-input"><input type="text" class="goto-index form-control input-sm" value="1">&nbsp;/&nbsp;<span class="total"></span></div></div></div>';
-                        $("#lesson-ppt-content").html(html).show();
+                            var player = new SlidePlayer({
+                                element: '.slide-player',
+                                slides: response
+                            });
 
-                        var player = new SlidePlayer({
-                            element: '.slide-player',
-                            slides: response
-                        });
-
-                            console.log(response);
-
-                        // }, 'json');
+                        }, 'json');
                     }
                 }
 
