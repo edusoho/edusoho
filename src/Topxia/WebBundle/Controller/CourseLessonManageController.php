81061af071b589d6541d593b054899d1a5d318c3
Wellming Li <wellming.li@howzhi.com>
2014-06-24 09:49:05 +0800

    ppt

--- src/Topxia/WebBundle/Controller/CourseLessonManageController.php
+++ src/Topxia/WebBundle/Controller/CourseLessonManageController.php
@@ -81,7 +81,7 @@ class CourseLessonManageController extends BaseController
 
     	$setting = $this->setting('storage');
     	if ($setting['upload_mode'] == 'local') {
-    		$videoUploadToken = $audioUploadToken = array(
+    		$videoUploadToken = $audioUploadToken = $pptUploadToken = array(
 	    		'token' => $this->getUserService()->makeToken('fileupload', $user['id'], strtotime('+ 2 hours')),
 	    		'url' => $this->generateUrl('uploadfile_upload', array('targetType' => $targetType, 'targetId' => $targetId)),
 			);
@@ -202,7 +202,7 @@ class CourseLessonManageController extends BaseController
 
     	$setting = $this->setting('storage');
     	if ($setting['upload_mode'] == 'local') {
-            $videoUploadToken = $audioUploadToken = array(
+            $videoUploadToken = $audioUploadToken = $pptUploadToken = array(
                 'token' => $this->getUserService()->makeToken('fileupload', $user['id'], strtotime('+ 2 hours')),
                 'url' => $this->generateUrl('uploadfile_upload', array('targetType' => $targetType, 'targetId' => $targetId)),
             );
@@ -226,6 +226,16 @@ class CourseLessonManageController extends BaseController
 	    		if (!empty($audioUploadToken['error'])) {
 	    			return $this->createMessageModalResponse('error', $audioUploadToken['error']['message']);
 	    		}
+
+                $commands = array_keys($client->getPPTConvertCommands());
+                $pptUploadToken = $client->generateUploadToken($client->getBucket(), array(
+                    'convertCommands' => implode(';', $commands),
+                    'convertNotifyUrl' => $this->generateUrl('uploadfile_cloud_convert_callback', array('key' => $convertKey), true),
+                ));
+                if (!empty($pptUploadToken['error'])) {
+                    return $this->createMessageModalResponse('error', $pptUploadToken['error']['message']);
+                }
+
     		}
     		 catch (\Exception $e) {
     			return $this->createMessageModalResponse('error', $e->getMessage());
@@ -238,6 +248,7 @@ class CourseLessonManageController extends BaseController
             'targetId' => $targetId,
 			'videoUploadToken' => $videoUploadToken,
 			'audioUploadToken' => $audioUploadToken,
+            'pptUploadToken' => $pptUploadToken,
 			'filePath' => $filePath,
 			'fileKey' => $fileKey,
 			'convertKey' => $convertKey,
