49fe0bd309cfd1c7a92ef4b738572036293097a7
xxvholic <xxvholic@howzhi.com>
2014-07-09 23:32:44 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/theme-edit/left-edit-modal.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/theme-edit/left-edit-modal.js
@@ -11,7 +11,7 @@ define(function(require, exports, module) {
         $("#save-btn").on('click', function(){
             var config = formSerialize($($(this).data('form')));
 
-            var code = $currentItem.attr('id').split('-').pop();
+            var code = $currentItem.data('code').split('-').pop();
 
             config.code = code;
 
