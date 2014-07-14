5d0835db751069f99c8e0a121a36771ff2f5bdd4
iron <iron@howzhi.com>
2014-07-14 18:26:14 +0800

    fix

--- src/Topxia/DataTag/VipLevelsDataTag.php
+++ src/Topxia/DataTag/VipLevelsDataTag.php
@@ -10,7 +10,7 @@ class VipLevelsDataTag extends CourseBaseDataTag implements DataTag
      * 获取一个VIP等级
      *
      * @param  array $arguments 参数
-     * @return array 分类
+     * @return array vip等级
      */
     
     public function getData(array $arguments)
