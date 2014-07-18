cff033fb55e5f8a219f730be96ec58a639d3a8c2
Wellming Li <wellming.li@howzhi.com>
2014-07-18 14:35:28 +0800

    cloud file refactor

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage-file/upload-course-files.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage-file/upload-course-files.js
@@ -8,49 +8,7 @@ define(function(require, exports, module) {
 	// 这里require是有顺序要求的
 	require('jquery.plupload-queue-zh-cn');
 
-	var VideoQualitySwitcher = Widget.extend({
-		attrs: {
-			'videoQuality': 'low',
-			'audioQuality': 'low'
-		},
-		events: {
-			'click .edit-btn': 'onClickEditBtn',
-			'click .cancel-btn': 'onClickCancelBtn',
-			'click .confrim-btn': 'onClickConfrimBtn'
-		},
-
-		setup: function() {
-			this.makeQualitySwitcherName();
-		},
-
-		onClickEditBtn: function() {
-			this.$('.edit-btn').hide();
-			this.$('.quality-switcher-control').show();
-		},
-
-		onClickCancelBtn: function() {
-			this.$('.quality-switcher-control').hide();
-			this.$('.edit-btn').show();
-		},
-
-		onClickConfrimBtn: function() {
-			this.makeQualitySwitcherName();
-			this.$('.quality-switcher-control').hide();
-			this.$('.edit-btn').show();
-		},
-
-		makeQualitySwitcherName: function() {
-			this.set('videoQuality', this.$('[name=video_quality]:checked').val());
-			this.set('audioQuality', this.$('[name=video_audio_quality]:checked').val());
-
-			var qualities = {'low': '流畅', 'normal': '标准', 'high': '精细'};
-
-			var name = qualities[this.get('videoQuality')] + '画质 / ' + qualities[this.get('audioQuality')] + '音质';
-
-			this.$('.quality-switcher-name').html( '(' + name + ')');
-		}
-	});
-
+	var VideoQualitySwitcher = require('../widget/video-quality-switcher');
 
 	exports.run = function() {
 		var $container = $("#file-uploader-container"),
