66effc70646cc1a8314e035ababa2764cb52c875
Wellming Li <wellming.li@howzhi.com>
2014-07-21 07:27:16 +0800

    fix bug

--- src/Topxia/WebBundle/Resources/public/js/controller/widget/media-chooser/audio-chooser.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/widget/media-chooser/audio-chooser.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
 
-    var BaseChooser = require('./base-chooser-2');
+    var BaseChooser = require('./base-chooser-3');
     require('jquery.perfect-scrollbar');
     var Notify = require('common/bootstrap-notify');
 
