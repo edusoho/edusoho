f0925b2c539026eb5513f644110790735a4fec9d
Wellming Li <wellming.li@howzhi.com>
2014-07-29 23:41:15 +0800

    fix text

--- src/Topxia/WebBundle/Controller/CourseLessonController.php
+++ src/Topxia/WebBundle/Controller/CourseLessonController.php
@@ -121,11 +121,11 @@ class CourseLessonController extends BaseController
             } else {
                 $json['mediaUri'] = '';
                 if ($lesson['type'] == 'video') {
-                    $json['mediaError'] = '抱歉，视频文件不存在，暂时无法观看。';
-                } else if ($lesson['type'] = 'audio') {
-                    $json['mediaError'] = '抱歉，音频文件不存在，暂时无法观看。';
-                } else if ($lesson['type'] = 'ppt') {
-                    $json['mediaError'] = '抱歉，PPT文件不存在，暂时无法观看。';
+                    $json['mediaError'] = '抱歉，视频文件不存在，此课时不能学习。';
+                } else if ($lesson['type'] == 'audio') {
+                    $json['mediaError'] = '抱歉，音频文件不存在，此课时不能学习。';
+                } else if ($lesson['type'] == 'ppt') {
+                    $json['mediaError'] = '抱歉，PPT文件不存在，此课时不能学习。';
                 }
             }
         } else {
