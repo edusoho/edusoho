6dbf70fde0db4a19dcac1ac5c6646a349b43af9a
cc <fucongcong@howzhi.com>
2014-08-05 10:03:11 +0800

    js refactor

--- src/Topxia/AdminBundle/Resources/public/js/controller/analysis/finished-lesson.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/analysis/finished-lesson.js
@@ -2,10 +2,11 @@ define(function(require, exports, module) {
     var Morris=require("morris");
     require("jquery.bootstrap-datetimepicker");
     var Validator = require('bootstrap.validator');
+    var forwordByType=require('./onChangeType.js');
     require('common/validator-rules').inject(Validator);
     var now = new Date();
     exports.run = function() {
-
+        forwordByType.forwordByType();
         if($('#data').length > 0){
                     var data = eval ("(" + $('#data').attr("value") + ")");
 
@@ -51,42 +52,6 @@ define(function(require, exports, module) {
             element: '[name=endTime]',
             required: true,
             rule:'date_check'
-        });
-
-        $("[name=analysisDateType]").on("change",function(){
-console.log(1);
-                switch($("[name=analysisDateType]").val())
-                {
-                    case "register":
-                    window.location.href=$("[name=register]").attr("value");break;
-                    case "login":
-                    window.location.href=$("[name=login]").attr("value");break;
-                    case "course":
-                    window.location.href=$("[name=course]").attr("value");break;
-                    case "lesson":
-                    window.location.href=$("[name=lesson]").attr("value");break;
-                    case "joinLesson":
-                    window.location.href=$("[name=lesson_join]").attr("value");break;
-                    case "paidLesson":
-                    window.location.href=$("[name=lesson_paid]").attr("value");break;
-                    case "finishedLesson":
-                    window.location.href=$("[name=lesson_finished]").attr("value");break;
-                    case "videoViewed":
-                    window.location.href=$("[name=video_viewed]").attr("value");break;
-                    case "cloudVideoViewed":
-                    window.location.href=$("[name=video_cloud_viewed]").attr("value");break;
-                    case "localVideoViewed":
-                    window.location.href=$("[name=video_local_viewed]").attr("value");break;
-                    case "netVideoViewed":
-                    window.location.href=$("[name=video_net_viewed]").attr("value");break;
-                    case "income":
-                    window.location.href=$("[name=income]").attr("value");break;
-                    case "courseIncome":
-                    window.location.href=$("[name=course_income]").attr("value");break;
-                    case "exitLesson":
-                    window.location.href=$("[name=lesson_exit]").attr("value");break;
-                }
-
         });
     };
 
