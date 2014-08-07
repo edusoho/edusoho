7a44bfc0bdd577a1fc92bd328054a25cc60ceb38
Wellming Li <wellming.li@howzhi.com>
2014-08-08 05:41:42 +0800

    fix bug

--- src/Topxia/WebBundle/Controller/CourseFileManageController.php
+++ src/Topxia/WebBundle/Controller/CourseFileManageController.php
@@ -39,6 +39,7 @@ class CourseFileManageController extends BaseController
 
         foreach ($files as $key => $file) {
             $files[$key]['metas2'] = json_decode($file['metas2']) ? : array();
+            $files[$key]['convertParams'] = json_decode($file['convertParams']) ? : array();
         }
 
         $users = $this->getUserService()->findUsersByIds(ArrayToolkit::column($files, 'updatedUserId'));
