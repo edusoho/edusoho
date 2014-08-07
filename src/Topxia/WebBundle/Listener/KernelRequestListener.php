3a516688215485a4ded6ac245389e0c5a532593b
Su ju <suju@howzhi.com>
2014-08-07 17:03:40 +0800

    更新mapi_v1 post

--- src/Topxia/WebBundle/Listener/KernelRequestListener.php
+++ src/Topxia/WebBundle/Listener/KernelRequestListener.php
@@ -18,6 +18,9 @@ class KernelRequestListener
     {
     	$request = $event->getRequest();
     	if (($event->getRequestType() == HttpKernelInterface::MASTER_REQUEST) && ($request->getMethod() == 'POST')) {
+            if (stripos($request->getPathInfo(), '/mapi_v1/') == 0) {
+                return;
+            }
             $whiteList = array('/course/order/pay/alipay/notify', '/vip/pay_notify/alipay', '/uploadfile/upload', '/uploadfile/cloud_convertcallback', '/disk/upload', '/file/upload', '/kindeditor/upload', '/disk/convert/callback', '/partner/phpwind/api/notify', '/partner/discuz/api/notify', '/live/auth');
             if (in_array($request->getPathInfo(), $whiteList)) {
                 return ;
