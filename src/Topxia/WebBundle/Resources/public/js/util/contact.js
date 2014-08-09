ee6b2f51d9c79c26bee2c9b20efe7ce011aba823
ruanwenqin <452253895@qq.com>
2014-08-10 00:59:01 +0800

    contact float UI

--- src/Topxia/WebBundle/Resources/public/js/util/contact.js
+++ src/Topxia/WebBundle/Resources/public/js/util/contact.js
@@ -11,8 +11,8 @@ define(function(require, exports, module) {
          
          o.each(function() {
              var n = $(this), h= n.find("h2 a") ,hover = n.find("#onlineSort4"), r = n.find(".close_btn"), i = n.find(".show_btn"), s = n.find(".side_content"), o = n.find(".side_list").width(), u = n.find(".side_list"), a = n.offset().top;
-           n.css(t.float, 0), t.minStatue && ($(".show_btn").css("float", t.float), s.css("width", 0), i.css("width", 32)), t.skin && n.addClass("side_" + t.skin), r.bind("click", function() {
-                 s.animate({width: "0"}, "fast"), i.stop(!0, !0).delay(300).animate({width: "33px"}, "fast").css("float", "right")
+           n.css(t.float, 0), t.minStatue && ($(".show_btn").css("float", t.float), s.css("width", 0), i.css("width", 48)), t.skin && n.addClass("side_" + t.skin), r.bind("click", function() {
+                 s.animate({width: "0"}, "fast"), i.stop(!0, !0).delay(300).animate({width: "48px"}, "fast").css("float", "right")
              }), i.click(function() {
                  $(this).animate({width: "0px"}, "fast"), n.width(o), s.stop(!0, !0).delay(200).animate({width: "167px"}, "fast")
              }), h.bind('click',function(){
