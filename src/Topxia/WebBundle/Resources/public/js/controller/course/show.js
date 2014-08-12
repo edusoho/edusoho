a3fcbd8d27a4426ee7046f25594c955f0f2a52e0
ruanwenqin <452253895@qq.com>
2014-08-12 21:52:17 +0800

    fix bug

--- src/Topxia/WebBundle/Resources/public/js/controller/course/show.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/show.js
@@ -1,15 +1,17 @@
 define(function(require, exports, module) {
+
     require('countdown');
 
     exports.run = function() {
 
-        var startTime = $('.countdown').data('starttime');
-        var endTime = $('.countdown').data('endtime');
-        var now = new Date();
-        var targetTime = '';
-        if(now < new Date(startTime)){
-            targetTime = startTime;
-        }else{
+        if($('.countdown').length){
+            var startTime = $('.countdown').data('starttime');
+            var endTime = $('.countdown').data('endtime');
+            var now = new Date();
+            var targetTime = '';
+            if(now < new Date(startTime)){
+                targetTime = startTime;
+            }else{
             targetTime = endTime;
         }
         
@@ -17,6 +19,8 @@ define(function(require, exports, module) {
             date: targetTime,
             offset: 8
             });
+        }
+
         // 
         setInterval(function(){
             $('.countdown-div').css("display","block");
