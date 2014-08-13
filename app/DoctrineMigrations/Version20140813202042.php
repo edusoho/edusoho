6d2dd70ba019066564a3a344e4153d69df5fce11
Wellming Li <wellming.li@howzhi.com>
2014-08-13 21:52:05 +0800

    fix

--- app/DoctrineMigrations/Version20140813202042.php
+++ app/DoctrineMigrations/Version20140813202042.php
@@ -13,7 +13,7 @@ class Version20140813202042 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("ALTER TABLE  `testpaper` ADD  `passScore` FLOAT( 10, 1 ) UNSIGNED NOT NULL DEFAULT  '0' COMMENT  '通过考试的分数线' AFTER  `score`");
+        $this->addSql("ALTER TABLE  `testpaper` ADD  `passedScore` FLOAT( 10, 1 ) UNSIGNED NOT NULL DEFAULT  '0' COMMENT  '通过考试的分数线' AFTER  `score`");
         $this->addSql("ALTER TABLE  `testpaper_result` ADD  `passedStatus` ENUM(  'none',  'passed',  'unpassed' ) NOT NULL DEFAULT  'none' COMMENT  '考试通过状态，none表示该考试没有' AFTER  `rightItemCount`");
     }
 
