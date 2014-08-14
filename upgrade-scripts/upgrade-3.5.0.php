19daf7bf9620abe2cd3966fb271801ed3a843690
Wellming Li <wellming.li@howzhi.com>
2014-08-14 08:53:26 +0800

    update 3.5.0 upgrade script

--- upgrade-scripts/upgrade-3.5.0.php
+++ upgrade-scripts/upgrade-3.5.0.php
@@ -43,6 +43,26 @@ use Symfony\Component\Filesystem\Filesystem;
         if (!$this->isFieldExist('upload_files', 'length')) {
             $connection->exec("ALTER TABLE  `upload_files` ADD  `length` INT UNSIGNED NOT NULL DEFAULT  '0' COMMENT  '长度（音视频则为时长，PPT/文档为页数）' AFTER  `etag`");
         }
+
+        if (!$this->isFieldExist('testpaper', 'passedScore')) {
+            $connection->exec("ALTER TABLE  `testpaper` ADD  `passedScore` FLOAT( 10, 1 ) UNSIGNED NOT NULL DEFAULT  '0' COMMENT  '通过考试的分数线' AFTER  `score`");
+        }
+
+        if (!$this->isFieldExist('testpaper_result', 'passedStatus')) {
+            $connection->exec("ALTER TABLE  `testpaper_result` ADD  `passedStatus` ENUM(  'none',  'passed',  'unpassed' ) NOT NULL DEFAULT  'none' COMMENT  '考试通过状态，none表示该考试没有' AFTER  `rightItemCount`");
+        }
+
+        $connection->exec("
+            CREATE TABLE IF NOT EXISTS `mobile_device` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '设备ID',
+              `imei` varchar(255) NOT NULL COMMENT '串号',
+              `platform` varchar(255) NOT NULL COMMENT '平台',
+              `version` varchar(255) NOT NULL COMMENT '版本',
+              `screenresolution` varchar(100) NOT NULL COMMENT '分辨率',
+              `kernel` varchar(255) NOT NULL COMMENT '内核',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
         
      }
 
