908bdfa9761f34053b700da02b8a8913b5cdb9fd
ruanwenqin <452253895@qq.com>
2014-08-15 11:32:48 +0800

    css fix and bug fix

--- src/Topxia/WebBundle/Resources/public/js/controller/live-course/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/live-course/index.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
 	require('jquery.cycle2');
 
 	exports.run = function() {
+		require('../course/timeleft').run();
         $('.homepage-feature').cycle({
 	        fx:"scrollHorz",
 	        slides: "> a, > img",
