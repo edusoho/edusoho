d804fb4e17ace052b0284b081ba583f732db38c7
lvliujie <lvliujie@howzhi.com>
2014-09-01 20:42:50 +0800

    change code place

--- src/Topxia/WebBundle/Controller/K12DefaultController.php
+++ src/Topxia/WebBundle/Controller/K12DefaultController.php
@@ -32,8 +32,32 @@ class K12DefaultController extends BaseController
         
     }
 
+    public function passwordAction(Request $request){
+        $user = $this->getCurrentUser();
+        if (empty($user) || !$user->isLogin()) {
+            throw $this->createAccessDeniedException();
+        }
+        if ($request->getMethod() == 'POST') {
+            $passwords = $request->request->all();
+            $this->getAuthService()->changePassword($user['id'], null, $passwords['newPassword']);
+            $this->setFlashMessage('success', '密码修改成功。');
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+        if($user['firstLogin']!=1){
+             throw $this->createAccessDeniedException("不是首登陆,请去账号设置处修改密码");
+        }
+        $this->getUserService()->changeFirstLogin($user['id']);
+        return $this->render('TopxiaWebBundle:K12Default:change-password.html.twig',array(
+            'user'=>$user
+        ));
+    }
+
     protected function getClassesService()
     {
         return $this->getServiceKernel()->createService('Classes.ClassesService');
+    }
+    protected function getAuthService()
+    {
+        return $this->getServiceKernel()->createService('User.AuthService');
     }
 }
