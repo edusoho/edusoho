0db73a8e8a4fde73c735f89c6ee7315ceffaa846
cc <fucongcong@howzhi.com>
2014-09-03 10:21:38 +0800

    flash

--- src/Topxia/WebBundle/Controller/CourseLessonController.php
+++ src/Topxia/WebBundle/Controller/CourseLessonController.php
@@ -407,7 +407,7 @@ class CourseLessonController extends BaseController
         return $response;
     }
 
-    public static function isMobile() {
+    private function isMobile() {
         // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
         if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
             return true;
