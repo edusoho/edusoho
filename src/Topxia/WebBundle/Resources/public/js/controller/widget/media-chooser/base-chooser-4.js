502a3cf2a5b8101e490c5b2bd5f1f0750eaef6a3
nifeng <nifeng@howzhi.com>
2014-09-04 10:50:20 +0800

    upload panel

--- src/Topxia/WebBundle/Resources/public/js/controller/widget/media-chooser/base-chooser-4.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/widget/media-chooser/base-chooser-4.js
@@ -4,6 +4,8 @@ define(function(require, exports, module) {
     var Widget = require('widget');
     var FileBrowser = require('../file/file-browser');
     var ChunkUpload = require('./chunk-upload');
+    var UploadProgressBar = require('./upload-progress-bar');
+    var UploadPanel = require('./upload-panel');
     var Notify = require('common/bootstrap-notify');
 
     var BaseChooser = Widget.extend({
@@ -11,7 +13,8 @@ define(function(require, exports, module) {
             choosed: null,
             uploader: null,
             uploaderSettings: {},
-            preUpload: null
+            preUpload: null,
+            uploadPanel: null
         },
 
         events: {
@@ -105,161 +108,21 @@ define(function(require, exports, module) {
             }).show();
 
             browser.on('select', function(file) {
-                self.trigger('change', self._convertFileToMedia(file));
+                self.trigger('change', self.get("uploadPanel")._convertFileToMedia(file));
             });
         },
 
-        _convertFileToMedia: function(file) {
-            var media = {};
-            media.id = file.id ? file.id : 0;
-            media.status = file.convertStatus ? file.convertStatus : 'none';
-            media.type = file.type;
-            media.source = 'self';
-            media.name = file.filename;
-            media.length = file.length;
-            return media;
-        },
-
-        _initUploadPane: function() {
-            var $btn = this.$('[data-role=uploader-btn]');
-            var progressbar = new UploadProgressBar($btn.data('progressbar'));
-            this.set('uploaderProgressbar', progressbar);
-            this.set('uploader', this._createUpload($btn, progressbar));
-        },
-
-        _createUpload: function($btn, progressbar) {
-            var self = this;
-
-            var settings = $.extend({}, {
-                file_types : "*.*",
-                file_size_limit : "10 MB",
-                file_upload_limit : 1,
-                file_queue_limit: 1,
-                file_post_name: 'file',
-
-                button_placeholder_id : $btn.attr('id'),
-                button_width: "75",
-                button_height: "35",
-                button_text: "<span class=\"btnText\">上传</span>",
-                button_text_style : ".btnText { color: #333; font-size:16px;}",
-                button_text_left_padding : 18,
-                button_text_top_padding : 5,
-                button_image_url: $btn.data('buttonImage'),
-                button_window_mode: 'transparent',
-
-                file_dialog_complete_handler: function(numFilesSelected, numFilesQueued) {
-                    if (numFilesSelected == 0) {
-                        return;
-                    }
-                    if (numFilesSelected > 1) {
-                        Notify.danger('一次只能上传一个文件，请重新选择。');
-                        return ;
-                    }
-
-                    if (numFilesQueued == 0) {
-                        Notify.info('文件正在上传中，请等待本次上传完毕后，再上传。');
-                        return ;
-                    }
-                    this.startUpload();
-                },
-
-                upload_start_handler: function(file) {
-                    if (self.get('preUpload')) {
-                        self.get('preUpload').call(self, this, file);
-                    }
-                    progressbar.reset().show();
-                },
-
-                upload_progress_handler: function(file, bytesLoaded, bytesTotal) {
-                    var percentage = Math.ceil((bytesLoaded / bytesTotal) * 100);
-                    progressbar.setProgress(percentage);
-                },
-
-                upload_error_handler: function(file, errorCode, message) {
-                    Notify.danger('文件上传失败，请重试！');
-                },
-
-                upload_success_handler: function(file, serverData) {
-                    progressbar.setComplete().hide();
-                    serverData = $.parseJSON(serverData);
-
-                    if ($btn.data('callback')) {
-                        $.post($btn.data('callback'), serverData, function(response) {
-                            var media = self._convertFileToMedia(response);
-                            self.trigger('change',  media);
-                            Notify.success('文件上传成功！');
-                        }, 'json');
-                    } else {
-                        var media = self._convertFileToMedia(serverData);
-                        self.trigger('change',  media);
-                        Notify.success('文件上传成功！');
-                    }
-
-
-                }
-            }, this.get('uploaderSettings'));
-
-            if ($btn.data('filetypes')) {
-                settings.file_types = $btn.data('filetypes');
-            }
-            if(this._supportChunkUpload() && $btn.data('storageType')=="cloud"){
-                settings.element=this.element;
-                settings.progressbar = progressbar;
-                return new ChunkUpload(settings);
-            }else{
-                return new SWFUpload(settings);
-            }
-        },
-        _supportChunkUpload: function(){
-            if(this.get("uploaderSettings").file_types.indexOf("ppt")>-1 || typeof(FileReader)=="undefined" || typeof(XMLHttpRequest)=="undefined"){
-                return false;
-            }
-            return true;
-        },
-
-        destroy: function(){
-            if(this._supportChunkUpload() && this.$('[data-role=uploader-btn]').data('storageType')=="cloud"){
-                var uploader = this.get("uploader");
-                uploader.destroy();
-            }
+        _initUploadPane: function(){
+            var uploadPanel = new UploadPanel({
+                element: this.element,
+                uploaderSettings: this.get("uploaderSettings"),
+                preUpload: this.get("preUpload")
+            });
+            this.set("uploadPanel", uploadPanel);
+            this.set("uploaderProgressbar", uploadPanel.get("uploaderProgressbar"));
         }
 
     });
-
-    function UploadProgressBar(element) {
-        this.element = $(element);
-        this.percentage = 0;
-    }
-
-    UploadProgressBar.prototype.show = function () {
-        this.element.show();
-        return this;
-    }
-
-    UploadProgressBar.prototype.hide = function () {
-        this.element.hide();
-        return this;
-    }
-
-    UploadProgressBar.prototype.setProgress = function (percentage) {
-        this.percentage = percentage;
-        this.element.find('.progress-bar').css('width', percentage + '%');
-        return this;
-    }
-
-    UploadProgressBar.prototype.setComplete = function () {
-        this.setProgress(100);
-        return this;
-    }
-
-    UploadProgressBar.prototype.reset = function () {
-        this.setProgress(0);
-        return this;
-    }
-
-    UploadProgressBar.prototype.isProgressing = function () {
-        return this.percentage > 0;
-    }
 
     module.exports = BaseChooser;
 });
