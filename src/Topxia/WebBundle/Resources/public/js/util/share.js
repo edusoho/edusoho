356c1f425c4f054df48aec434b51d85ecd471b44
cc <fucongcong@howzhi.com>
2014-09-09 14:37:03 +0800

    sgare

--- src/Topxia/WebBundle/Resources/public/js/util/share.js
+++ src/Topxia/WebBundle/Resources/public/js/util/share.js
@@ -57,10 +57,12 @@ define(function(require, exports, module) {
                    "bdMini":"2",       
                    "bdMiniList":false,       
                    "bdPic":"",       
-                   "bdStyle":"0",       
-                   "bdSize":"24"       
+                   "bdStyle":"0"      
+                          
                     },       
-            "share":{},
+            "share":{
+                    "bdSize":"24"
+            },
         };       
     with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)]; 
     
