4600ec39934f0d2ffd585dbf91b70069591ae793
nifeng <nifeng@howzhi.com>
2014-09-10 11:16:31 +0800

    head leader

--- src/Topxia/WebBundle/Controller/UploadFileController.php
+++ src/Topxia/WebBundle/Controller/UploadFileController.php
@@ -214,23 +214,25 @@ class UploadFileController extends BaseController
 
         $result = $request->getContent();
 
-        $result = preg_replace_callback(
-          "(\\\\x([0-9a-f]{2}))i",
-          function($a) {return chr(hexdec($a[1]));},
-          $result
-        );
+        $headLeader = $this->getSettingService()->set('headLeader', $result);
 
-        $this->getLogService()->info('headLeader', 'cloud_convert_head_leader_callback', "片头处理回调", array('result' => $result));
+        // $result = preg_replace_callback(
+        //   "(\\\\x([0-9a-f]{2}))i",
+        //   function($a) {return chr(hexdec($a[1]));},
+        //   $result
+        // );
 
-        $result = json_decode($result, true);
-        $result = array_merge($request->query->all(), $result);
+        // $this->getLogService()->info('headLeader', 'cloud_convert_head_leader_callback', "片头处理回调", array('result' => $result));
 
-        $headLeader = $this->getSettingService()->get('headLeader', array());
-        $headLeader = json_decode($headLeader, true);
-        $headLeader['meta'] = $result;
-        $headLeader = json_encode($headLeader);
+        // $result = json_decode($result, true);
+        // $result = array_merge($request->query->all(), $result);
 
-        $this->getSettingService()->set('headLeader', $headLeader);
+        // $headLeader = $this->getSettingService()->get('headLeader', array());
+        // $headLeader = json_decode($headLeader, true);
+        // $headLeader['meta'] = $result;
+        // $headLeader = json_encode($headLeader);
+
+        // $this->getSettingService()->set('headLeader', $headLeader);
 
         return $this->createJsonResponse($result);
     } 
