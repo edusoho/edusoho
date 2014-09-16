446aafaf40e149c3fa06c7441f56a1b1b9ecd055
ruanwenqin <452253895@qq.com>
2014-09-16 18:10:19 +0800

    move k12 routing to k12 routing file

---
+++ src/Topxia/WebBundle/Controller/SignController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Topxia\WebBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Topxia\Common\ArrayToolkit;
+
+class SignController extends BaseController
+{
+	public function signAction(Request $request, $classId, $userId)
+	{
+		$this->getSignService()->classMemberSign($userId, $classId);
+		return $this->createJsonResponse('success');
+	}
+
+	public function getSignService()
+	{
+		return $this->getServiceKernel()->createService('Classes.SignService');
+	}
+}
