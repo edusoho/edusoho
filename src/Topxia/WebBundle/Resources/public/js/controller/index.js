02daa7e3feaa622ad62893f21936381a366e6ed1
Jiajun Xu <xujiajun@howzhi.com>
2014-09-22 15:05:27 +0800

    fix bug for share under ie8

--- src/Topxia/WebBundle/Resources/public/js/controller/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/index.js
@@ -1,21 +1,20 @@
 define(function(require, exports, module) {
+
     var Share = require('../util/share.js');
     require('jquery.cycle2');
 
     exports.run = function() {
-
-    	$('.homepage-feature').cycle({
-	        fx:"scrollHorz",
-	        slides: "> a, > img",
-	        log: "false",
-	        pauseOnHover: "true"
-    	});
-
-    Share.create({
-        selector: '.share',
-        icons: 'itemsAll',
-        display: ''
-    });
+        $('.homepage-feature').cycle({
+            fx:"scrollHorz",
+            slides: "> a, > img",
+            log: "false",
+            pauseOnHover: "true"
+        });
+        Share.create({
+            selector: '.share',
+            icons: 'itemsAll',
+            display: ''
+        });
     };
 
 });
