ee1e600a7e38e31a7d4832a186699ef1092ecf24
Wellming Li <wellming.li@howzhi.com>
2014-09-24 08:02:09 +0800

    update edusoho sql

--- web/install/edusoho.sql
+++ web/install/edusoho.sql
@@ -481,45 +481,46 @@ CREATE TABLE IF NOT EXISTS `groups` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `groups_member`;
-CREATE TABLE IF NOT EXISTS `groups_member` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '成员id主键',
-              `groupId` int(10) unsigned NOT NULL COMMENT '小组id',
-              `userId` int(10) unsigned NOT NULL COMMENT '用户id',
-              `role` varchar(100) NOT NULL DEFAULT 'member',
-              `postNum` int(10) unsigned NOT NULL DEFAULT '0',
-              `threadNum` int(10) unsigned NOT NULL DEFAULT '0',
-              `createdTime` int(11) unsigned NOT NULL COMMENT '加入时间',
-              PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `groups_member` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '成员id主键',
+  `groupId` int(10) unsigned NOT NULL COMMENT '小组id',
+  `userId` int(10) unsigned NOT NULL COMMENT '用户id',
+  `role` varchar(100) NOT NULL DEFAULT 'member',
+  `postNum` int(10) unsigned NOT NULL DEFAULT '0',
+  `threadNum` int(10) unsigned NOT NULL DEFAULT '0',
+  `createdTime` int(11) unsigned NOT NULL COMMENT '加入时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `groups_thread`;
-CREATE TABLE IF NOT EXISTS `groups_thread` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '话题id',
-              `title` varchar(1024) NOT NULL COMMENT '话题标题',
-              `content` text COMMENT '话题内容',
-              `isElite` int(11) unsigned NOT NULL DEFAULT '0',
-              `isStick` int(11) unsigned NOT NULL DEFAULT '0',
-              `lastPostMemberId` int(10) unsigned NOT NULL,
-              `lastPostTime` int(10) unsigned NOT NULL,
-              `groupId` int(10) unsigned NOT NULL,
-              `userId` int(10) unsigned NOT NULL,
-              `createdTime` int(10) unsigned NOT NULL COMMENT '添加时间',
-              `postNum` int(10) unsigned NOT NULL DEFAULT '0',
-              `status` enum('open','close') NOT NULL DEFAULT 'open',
-              `hitNum` int(10) unsigned NOT NULL,
-              PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `groups_thread` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '话题id',
+  `title` varchar(1024) NOT NULL COMMENT '话题标题',
+  `content` text COMMENT '话题内容',
+  `isElite` int(11) unsigned NOT NULL DEFAULT '0',
+  `isStick` int(11) unsigned NOT NULL DEFAULT '0',
+  `lastPostMemberId` int(10) unsigned NOT NULL DEFAULT '0',
+  `lastPostTime` int(10) unsigned NOT NULL DEFAULT '0',
+  `groupId` int(10) unsigned NOT NULL,
+  `userId` int(10) unsigned NOT NULL,
+  `createdTime` int(10) unsigned NOT NULL COMMENT '添加时间',
+  `postNum` int(10) unsigned NOT NULL DEFAULT '0',
+  `status` enum('open','close') NOT NULL DEFAULT 'open',
+  `hitNum` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `groups_thread_post`;
-CREATE TABLE IF NOT EXISTS `groups_thread_post` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id主键',
-              `threadId` int(11) unsigned NOT NULL COMMENT '话题id',
-              `content` text NOT NULL COMMENT '回复内容',
-              `userId` int(10) unsigned NOT NULL COMMENT '回复人id',
-              `postId` int(10) unsigned DEFAULT '0',
-              `createdTime` int(10) unsigned NOT NULL COMMENT '回复时间',
-              PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `groups_thread_post` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id主键',
+  `threadId` int(11) unsigned NOT NULL COMMENT '话题id',
+  `content` text NOT NULL COMMENT '回复内容',
+  `userId` int(10) unsigned NOT NULL COMMENT '回复人id',
+  `fromUserId` int(10) unsigned NOT NULL DEFAULT '0',
+  `postId` int(10) unsigned DEFAULT '0',
+  `createdTime` int(10) unsigned NOT NULL COMMENT '回复时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `location`;
 CREATE TABLE `location` (
