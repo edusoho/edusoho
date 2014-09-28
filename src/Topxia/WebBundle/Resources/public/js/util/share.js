3a041c3587b76954f658cf06ca001cc9c650b34e
Jiajun Xu <xujiajun@howzhi.com>
2014-09-28 16:56:02 +0800

    优化share css

--- src/Topxia/WebBundle/Resources/public/js/util/share.js
+++ src/Topxia/WebBundle/Resources/public/js/util/share.js
@@ -31,30 +31,30 @@ define(function(require, exports, module) {
         config.itemsByqq=itemsByqq;
 
         if(type=="dropdown"){
-            var html='<div class="dropdown pull-right" style="padding:1px"><a class="dropdown-toggle text-muted "  href="#" id="dropdownMenu1" data-toggle="dropdown" >分享到<span class="caret"></span></a><ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1"><div class="bdsharebuttonbox" style="padding:1px;">';
+            var html='<div class="dropdown pull-right"><a class="dropdown-toggle text-muted "  href="#" id="dropdownMenu1" data-toggle="dropdown" >分享到<span class="caret"></span></a><ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1"><div class="bdsharebuttonbox" style="padding:1px;min-width:170px">';
         }
         else{
             var html='<ul style="padding:1px;"><div class="bdsharebuttonbox" >';
         }
 
         if(contains(config[name],'qq')){
-             html+='<a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a>';
+             html+='<a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间" style="margin:3px 1px 2px 6px;"></a>';
         }
 
         if(contains(config[name],'tsina')){
-             html+='<a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a>';
+             html+='<a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博" style="margin:3px 2px 2px 6px;"></a>';
         }
 
         if(contains(config[name],'weixin')){
-             html+='<a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a>';
+             html+='<a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信" style="margin:3px 2px 2px 6px;"></a>';
         }
 
         if(contains(config[name],'renren')){
-             html+='<a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a>';
+             html+='<a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网" style="margin:3px 2px 2px 6px;"></a>';
         }
 
         if(contains(config[name],'more')){
-             html+='<a href="#" class="bds_more" data-cmd="more"></a>';
+             html+='<a href="#" class="bds_more" data-cmd="more" style="margin:3px 2px 2px 6px;"></a>';
         }
 
         var bdText=$(select).attr("data-bdText");
