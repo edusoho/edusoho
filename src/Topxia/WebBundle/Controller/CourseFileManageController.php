ca011f673b0258966acbe0de29b15bc148bd7922
cc <fucongcong@howzhi.com>
2014-09-28 18:07:48 +0800

    #3860

--- src/Topxia/WebBundle/Controller/CourseFileManageController.php
+++ src/Topxia/WebBundle/Controller/CourseFileManageController.php
@@ -38,7 +38,9 @@ class CourseFileManageController extends BaseController
         );
 
         foreach ($files as $key => $file) {
-            $files[$key]['metas2'] = json_decode($file['metas2']) ? : array();
+         
+            $files[$key]['metas2'] = json_decode($file['metas2'],true) ? : array();
+
             $files[$key]['convertParams'] = json_decode($file['convertParams']) ? : array();
         }
 
