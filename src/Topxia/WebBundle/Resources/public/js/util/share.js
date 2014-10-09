5ead26f0f70f6e340db1fadd8dccdb4d27f8a63e
Jiajun Xu <xujiajun@howzhi.com>
2014-10-09 18:26:35 +0800

    change sharebuttin min-width

--- src/Topxia/WebBundle/Resources/public/js/util/share.js
+++ src/Topxia/WebBundle/Resources/public/js/util/share.js
@@ -31,7 +31,7 @@ define(function(require, exports, module) {
         config.itemsByqq=itemsByqq;
 
         if(type=="dropdown"){
-            var html='<div class="dropdown pull-right"><a class="dropdown-toggle text-muted "  href="#" id="dropdownMenu1" data-toggle="dropdown" >分享到<span class="caret"></span></a><ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1"><div class="bdsharebuttonbox" style="padding:1px;min-width:170px">';
+            var html='<div class="dropdown pull-right"><a class="dropdown-toggle text-muted "  href="#" id="dropdownMenu1" data-toggle="dropdown" >分享到<span class="caret"></span></a><ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1"><div class="bdsharebuttonbox" style="padding:1px;">';
         }
         else if(type=="dropdownWithIcon"){
             var html='<div class="dropdown pull-right" style="padding:1px;"><a class="dropdown-toggle btn btn-link"  href="javascript:" id="dropdownMenu1" data-toggle="dropdown" ><span class="glyphicon glyphicon-share"></span> 分享到</a><ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1"><div class="bdsharebuttonbox" style="padding:1px;">';
