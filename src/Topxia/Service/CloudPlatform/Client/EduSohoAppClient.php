282ec4304151a0b44764ab998c6bd2a8312f087d
ruanwenqin <452253895@qq.com>
2014-10-13 21:51:47 +0800

    K12 open

--- src/Topxia/Service/CloudPlatform/Client/EduSohoAppClient.php
+++ src/Topxia/Service/CloudPlatform/Client/EduSohoAppClient.php
@@ -92,7 +92,7 @@ class EduSohoAppClient implements AppClient
     {
         $url = "{$this->apiUrl}?action={$action}";
         $edusoho = array(
-            'edition' => 'opensource', 
+            'edition' => 'k12', 
             'host' => $_SERVER['HTTP_HOST'],
             'version' => System::VERSION, 
             'debug' => $this->debug ? '1' : '0',
