6fbf89e9200197f344b89702fa43305a2ef63634
nifeng <nifeng@howzhi.com>
2014-10-23 18:14:44 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage/lesson-modal.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage/lesson-modal.js
@@ -6,114 +6,88 @@ define(function(require, exports, module) {
     var AudioChooser = require('../widget/media-chooser/audio-chooser7');
     var PPTChooser = require('../widget/media-chooser/ppt-chooser6');
     var Notify = require('common/bootstrap-notify');
-        require('jquery.sortable');
+    require('jquery.sortable');
 
-            var tmpContents = {};
-            var editor;
-            var LocalContent = {};
-            function getTmpContents(){
-                var date = new Date(); //日期对象
-                var now = "";
-                // now = date.getFullYear()+"年"; //读英文就行了
-                // now = now + (date.getMonth()+1)+"月"; //取月的时候取的是当前月-1如果想取当前月+1就可以了
-                // now = now + date.getDate()+"日";
-                now = now + date.getHours()+"时";
-                now = now + date.getMinutes()+"分";
-                now = now + date.getSeconds()+"秒";
-                tmpContents["title"] = $("#lesson-title-field").val();
-                tmpContents["summary"] = $("#lesson-summary-field").val();
-                tmpContents["courseId"]  = $("#course-lesson-form").data("courseId");
-                tmpContents["lessonId"]  = $("#course-lesson-form").data("lessonId");
-                var lessonId = tmpContents["lessonId"];
-                editor.sync();
-                var z = editor.html();
-                var x = editor.html().match(/<embed[\s\S]*?\/>/g);
-                if (x) {
-                        for (var i = x.length - 1; i >= 0; i--) {
-                           var y = x[i].replace(/\/>/g,"wmode='Opaque' \/>");
-                           var z =  z.replace(x[i],y);
-                        };
-                }
-                tmpContents["content"] = z;
-                tmpContents["createdTime"] = now;
-                function compare(tmp, local){
-                    if($.isEmptyObject(tmp)){
-                        return false;
-                    }
-                    for(var key in tmp){
-                        if(key!="courseId" && key!="createdTime" && tmp[key] != "" && tmp[key] != local[key]){
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-                function objClone(jsonObj){
-                     var txt=JSON.stringify(jsonObj);
-                     return JSON.parse(txt);
-                }
-                if(compare(tmpContents, LocalContent)){
-                    if(lessonId == undefined){
-                        $.post('/course/textdraft/create', tmpContents, function(data){
-                            LocalContent = objClone(tmpContents);
-                            $(".modal-title").text('添加课时(草稿已于' + tmpContents['createdTime'] + '保存)');
-                        });
-                      } else {
-                         $.post('/course/editdraft/'+lessonId+'/create', tmpContents, function(data){
-                            LocalContent = objClone(tmpContents);
-                            $(".modal-title").text('编辑课时(草稿已于' + tmpContents['createdTime'] + '保存)');
-                        });
-                     }
+    var tmpContents = {};
+    var editor;
+    var LocalContent = {};
+    function getTmpContents(){
+        var date = new Date(); //日期对象
+        var now = "";
+        // now = date.getFullYear()+"年"; //读英文就行了
+        // now = now + (date.getMonth()+1)+"月"; //取月的时候取的是当前月-1如果想取当前月+1就可以了
+        // now = now + date.getDate()+"日";
+        now = now + date.getHours()+"时";
+        now = now + date.getMinutes()+"分";
+        now = now + date.getSeconds()+"秒";
+        tmpContents["title"] = $("#lesson-title-field").val();
+        tmpContents["summary"] = $("#lesson-summary-field").val();
+        tmpContents["courseId"]  = $("#course-lesson-form").data("courseId");
+        tmpContents["lessonId"]  = $("#course-lesson-form").data("lessonId");
+        var lessonId = tmpContents["lessonId"];
+        editor.sync();
+        var z = editor.html();
+        var x = editor.html().match(/<embed[\s\S]*?\/>/g);
+        if (x) {
+                for (var i = x.length - 1; i >= 0; i--) {
+                   var y = x[i].replace(/\/>/g,"wmode='Opaque' \/>");
+                   var z =  z.replace(x[i],y);
+                };
+        }
+        tmpContents["content"] = z;
+        tmpContents["createdTime"] = now;
+        function compare(tmp, local){
+            if($.isEmptyObject(tmp)){
+                return false;
+            }
+            for(var key in tmp){
+                if(key!="courseId" && key!="createdTime" && tmp[key] != "" && tmp[key] != local[key]){
+                    return true;
                 }
             }
-             $("#see-draft-btn").on('click',function(e) {
-                tmpContents["courseId"]  = $("#course-lesson-form").data("courseId");
-                var courseId = tmpContents["courseId"];
-                tmpContents["lessonId"]  = $("#course-lesson-form").data("lessonId");
-                var lessonId = tmpContents["lessonId"];
-                if(lessonId == undefined) 
-                    {
-                        $.get('/course/textdraft/'+courseId+'/view',function(response){  
-                            $("#lesson-title-field").val(response.title); 
-                            $("#lesson-summary-field").val(response.summary); 
-                            editor.sync();
-                            var z = editor.html(response.content);
-                            $("#lesson-content-field").val(z);        
-                        }); 
-                        $("#see-draft-btn").hide();
-                    } else {
-                            $.get('/course/'+courseId+'/editdraft/'+lessonId+'/see',function(response){  
-                            $("#lesson-title-field").val(response.title); 
-                            $("#lesson-summary-field").val(response.summary); 
-                            editor.sync();
-                            var z = editor.html(response.content);
-                            $("#lesson-content-field").val(z);        
-                        }); 
-                        $("#see-draft-btn").hide();
-                    }
-            });
-
-            var sortList = function($list) {
-            var data = $list.sortable("serialize").get();
-            $.post($list.data('sortUrl'), {ids:data}, function(response){
-                var lessonNum = chapterNum = unitNum = 0;
-
-                $list.find('.item-lesson, .item-chapter').each(function() {
-                    var $item = $(this);
-                    if ($item.hasClass('item-lesson')) {
-                        lessonNum ++;
-                        $item.find('.number').text(lessonNum);
-                    } else if ($item.hasClass('item-chapter-unit')) {
-                        unitNum ++;
-                        $item.find('.number').text(unitNum);
-                    } else if ($item.hasClass('item-chapter')) {
-                        chapterNum ++;
-                        unitNum = 0;
-                        $item.find('.number').text(chapterNum);
-                    }
-
+            return false;
+        }
+        function objClone(jsonObj){
+             var txt=JSON.stringify(jsonObj);
+             return JSON.parse(txt);
+        }
+        if(compare(tmpContents, LocalContent)){
+            if(lessonId == undefined){
+                $.post('/course/textdraft/create', tmpContents, function(data){
+                    LocalContent = objClone(tmpContents);
+                    $(".modal-title").text('添加课时(草稿已于' + tmpContents['createdTime'] + '保存)');
                 });
+              } else {
+                 $.post('/course/editdraft/'+lessonId+'/create', tmpContents, function(data){
+                    LocalContent = objClone(tmpContents);
+                    $(".modal-title").text('编辑课时(草稿已于' + tmpContents['createdTime'] + '保存)');
+                });
+             }
+        }
+    }
+
+    var sortList = function($list) {
+        var data = $list.sortable("serialize").get();
+        $.post($list.data('sortUrl'), {ids:data}, function(response){
+            var lessonNum = chapterNum = unitNum = 0;
+
+            $list.find('.item-lesson, .item-chapter').each(function() {
+                var $item = $(this);
+                if ($item.hasClass('item-lesson')) {
+                    lessonNum ++;
+                    $item.find('.number').text(lessonNum);
+                } else if ($item.hasClass('item-chapter-unit')) {
+                    unitNum ++;
+                    $item.find('.number').text(unitNum);
+                } else if ($item.hasClass('item-chapter')) {
+                    chapterNum ++;
+                    unitNum = 0;
+                    $item.find('.number').text(chapterNum);
+                }
+
             });
-        };
+        });
+    };
 
     function createValidator ($form, $choosers) {
 
@@ -276,7 +250,7 @@ define(function(require, exports, module) {
     }
 
     exports.run = function() {
-         var Timer;
+        var Timer;
         var updateDuration = function (length) {
             length = parseInt(length);
             if (isNaN(length) || length == 0) {
@@ -342,16 +316,9 @@ define(function(require, exports, module) {
 
             $form.removeClass('lesson-form-video').removeClass("lesson-form-audio").removeClass("lesson-form-text").removeClass("lesson-form-ppt")
             $form.addClass("lesson-form-" + type);
+            
             if (type == 'text'){
-                 Timer = setInterval(getTmpContents,5000);//1000为1秒钟
-
-                 $(".close").on('click',function(e){
-                        getTmpContents();
-                 });
-
-                 $("#cancel-btn").on('click',function(e){
-                        getTmpContents();
-                 });
+                Timer = setInterval(getTmpContents,5000);//1000为1秒钟
             }
 
             if (type == 'video') {
@@ -372,8 +339,9 @@ define(function(require, exports, module) {
             }
 
             $(".modal").on('hidden.bs.modal', function (){
-            clearInterval(Timer);
-       });
+                clearInterval(Timer);
+            });
+
             switchValidator(validator, type);
         });
 
@@ -394,6 +362,39 @@ define(function(require, exports, module) {
                 };
             }
             $content.val(z);
+        });
+
+        $(".close,#cancel-btn").on('click',function(e){
+            if($form.find('[name="type"]:checked').val()=='text'){
+                getTmpContents();
+            }
+        });
+
+        $("#see-draft-btn").on('click',function(e) {
+            tmpContents["courseId"]  = $("#course-lesson-form").data("courseId");
+            var courseId = tmpContents["courseId"];
+            tmpContents["lessonId"]  = $("#course-lesson-form").data("lessonId");
+            var lessonId = tmpContents["lessonId"];
+            if(lessonId == undefined) 
+            {
+                $.get('/course/textdraft/'+courseId+'/view',function(response){  
+                    $("#lesson-title-field").val(response.title); 
+                    $("#lesson-summary-field").val(response.summary); 
+                    editor.sync();
+                    var z = editor.html(response.content);
+                    $("#lesson-content-field").val(z);        
+                }); 
+                $("#see-draft-btn").hide();
+            } else {
+                    $.get('/course/'+courseId+'/editdraft/'+lessonId+'/see',function(response){  
+                    $("#lesson-title-field").val(response.title); 
+                    $("#lesson-summary-field").val(response.summary); 
+                    editor.sync();
+                    var z = editor.html(response.content);
+                    $("#lesson-content-field").val(z);        
+                }); 
+                $("#see-draft-btn").hide();
+            }
         });
         
     };
