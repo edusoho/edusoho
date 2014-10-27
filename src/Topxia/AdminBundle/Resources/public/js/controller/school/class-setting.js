dbf108cbc8ea06c8eb33620e31079e7b0e1607bf
lvliujie <lvliujie@howzhi.com>
2014-10-27 16:54:03 +0800

    class page change

--- src/Topxia/AdminBundle/Resources/public/js/controller/school/class-setting.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/school/class-setting.js
@@ -1,4 +1,15 @@
 define(function(require, exports, module) {
-	exports.run = function() {
-	};
+    exports.run = function() {
+        var classList=$(".class-list");
+        classList.find('.class-grid').each(function(){
+            $(this).hover(
+                function(){
+                    $(this).find('.class-option').show();
+                },
+                function(){
+                    $(this).find('.class-option').hide();
+                }
+            );
+        });       
+    };
 });
