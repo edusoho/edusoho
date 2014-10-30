93002e7447d43ccd4b29d7b149bded697751f7ae
Jiajun Xu <xujiajun@howzhi.com>
2014-10-30 20:17:13 +0800

    clean

--- src/Topxia/Service/Course/Dao/HomeworkDao.php
+++ src/Topxia/Service/Course/Dao/HomeworkDao.php
@@ -4,15 +4,15 @@ namespace Topxia\Service\Course\Dao;
 
 interface HomeworkDao
 {
-	public function getHomework($id);
+    public function getHomework($id);
 
     public function findHomeworkByCourseIdAndLessonIds($courseId, $lessonIds);
 
     public function findHomeworksByCreatedUserId($userId);
     
-    public function findHomeworksByCourseIdAndLessonId($courseId, $lessonId);
+    public function findHomeworksByLessonId($lessonId);
 
-	public function addHomework($fields);
+    public function addHomework($fields);
 
     public function updateHomework($id,$fields);
 
