ddf2b69e3b4f901de395a23bcdaad0dc6b7c307d
lvliujie <lvliujie@howzhi.com>
2014-11-06 14:19:57 +0800

    class remove bug fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/school/class-setting.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/school/class-setting.js
@@ -1,4 +1,5 @@
 define(function(require, exports, module) {
+    var Notify = require('common/bootstrap-notify');
     exports.run = function() {
         var classList=$(".class-list");
         classList.find('.class-grid').each(function(){
@@ -10,6 +11,18 @@ define(function(require, exports, module) {
                     $(this).find('.class-option').hide();
                 }
             );
-        });       
+        });
+
+        classList.on('click','.class-remove',function(){
+            if($(this).data('stunum')>0){
+                Notify.danger('尚存在学生,请先移除班级学生');
+            }else{
+                $.get($(this).data('url'),function(){
+                    Notify.success('成功移除班级');
+                    window.location.reload();
+                });
+            }
+        });
+
     };
 });
