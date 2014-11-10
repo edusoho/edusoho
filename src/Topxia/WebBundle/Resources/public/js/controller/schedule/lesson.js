010e5247b23e6d8ab5a438192c03683be6b705f4
lvliujie <lvliujie@howzhi.com>
2014-11-10 17:21:36 +0800

    js file path bug fix

--- src/Topxia/WebBundle/Resources/public/js/controller/schedule/lesson.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/schedule/lesson.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
     require('jquery.sortable');
-    var Schedule = require('/bundles/topxiaweb/js/controller/widget/schedule.js');
+    var Schedule = require('../widget/schedule.js');
     exports.run = function() {
         var schedule = new Schedule({
             element: 'body'
