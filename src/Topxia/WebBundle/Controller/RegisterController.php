bb5c82d40c0b5028d727b75158f2908887a568d2
wangchao <wangchao@howzhi.com>
2014-11-11 14:51:45 +0800

    验证码php controller部分修改

--- src/Topxia/WebBundle/Controller/RegisterController.php
+++ src/Topxia/WebBundle/Controller/RegisterController.php
@@ -3,7 +3,9 @@ namespace Topxia\WebBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Topxia\WebBundle\Form\RegisterType;
+
 
 class RegisterController extends BaseController
 {
@@ -20,7 +22,17 @@ class RegisterController extends BaseController
         if ($request->getMethod() == 'POST') {
     
             $registration = $request->request->all();
-   
+            $captchaCodePostedByUser = $registration['captcha_num'];
+
+            error_log("post captcha\n",3,'/var/tmp/mylogs.log');
+            error_log("captchaCode posted by user:".$captchaCodePostedByUser."\n",3,'/var/tmp/mylogs.log');
+
+            $session = new Session();
+            $captchaCode = $session->get('captcha_code');   
+            error_log("captchaCode in Session:".$captchaCode."\n",3,'/var/tmp/mylogs.log');
+            if ($captchaCode != $captchaCodePostedByUser){   
+                throw new \RuntimeException('验证码错误。');
+            }
             $registration['createdIp'] = $request->getClientIp();
 
             $user = $this->getAuthService()->register($registration);
@@ -234,12 +246,62 @@ class RegisterController extends BaseController
 
     public function captchaAction(Request $request)
     {
-        $image = "1222px.png";
-        $file = readfile("/home/zjsxwc/1px.png");
+        $captchaCode = ""; 
+        for ($i = 0; $i < 4; $i++) { 
+            $captchaCode .= rand(0, 9); 
+        } 
+
+        $session = new Session();
+        $session->set('captcha_code',$captchaCode);
+
+
+        $h=20;$w=60;$num = 4;
+        $im = imagecreate($w, $h); 
+        $black = imagecolorallocate($im, 0, 0, 0); 
+        $gray = imagecolorallocate($im, 200, 200, 200); 
+        $bgcolor = imagecolorallocate($im, 255, 255, 255); 
+        //填充背景 
+        imagefill($im, 0, 0, $gray); 
+     
+        //画边框 
+        imagerectangle($im, 0, 0, $w-1, $h-1, $black); 
+     
+        //随机绘制两条虚线，起干扰作用 
+        $style = array ($black,$black,$black,$black,$black, 
+            $gray,$gray,$gray,$gray,$gray 
+        ); 
+        imagesetstyle($im, $style); 
+        $y1 = rand(0, $h); 
+        $y2 = rand(0, $h); 
+        $y3 = rand(0, $h); 
+        $y4 = rand(0, $h); 
+        imageline($im, 0, $y1, $w, $y3, IMG_COLOR_STYLED); 
+        imageline($im, 0, $y2, $w, $y4, IMG_COLOR_STYLED); 
+
+        //在画布上随机生成大量黑点，起干扰作用; 
+        for ($i = 0; $i < 80; $i++) { 
+            imagesetpixel($im, rand(0, $w), rand(0, $h), $black); 
+        } 
+        //将数字随机显示在画布上,字符的水平间距和位置都按一定波动范围随机生成 
+        $strx = rand(3, 8); 
+        for ($i = 0; $i < $num; $i++) { 
+            $strpos = rand(1, 6); 
+            imagestring($im, 5, $strx, $strpos, substr($captchaCode, $i, 1), $black); 
+            $strx += rand(8, 12); 
+        } 
+
+        ob_start();
+        imagepng($im);
+        $str = ob_get_clean();
+
+        imagedestroy($im);//释放图片所占内存
+        $im = null;
+        
         $headers = array(
             'Content-Type' => 'image/png',
-            'Content-Disposition' => 'inline; filename="'.$image.'"');
-        return new Response($file, 200, $headers);
+            'Content-Disposition' => 'inline; filename="'."reg_captcha.png".'"');
+
+        return new Response($str, 200, $headers);
     }
 
     protected function getSettingService()
