42c8ace4794ff834ba60063fd5f6e92a3e8a1cd1
shenjian <shenjian@howzhi.com>
2014-11-12 19:05:54 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/tag2/manage-modal.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/tag2/manage-modal.js
@@ -7,15 +7,62 @@ define(function(require, exports, module) {
     var $table = $('#tag-table');
 
     exports.run = function() {
-        $('#tag-create-btn').on('click',function(){
-            var $btn = $(this);
-            $btn.addClass('disabled');
-            $.post($btn.data('url'), $('#tag-create-form').serialize(),function(html) {
-                $('#tag').val('');
-                $table.find('tbody').prepend(html);
-                Notify.success('标签添加成功！');
-                $btn.removeClass('disabled');
-            });
+        var validator = new Validator({
+            element: $('#tag-create-form'),
+            autoSubmit: false,
+            onFormValidated: function(error, results, $form) {
+                if (error) {
+                    return ;
+                }
+                $.post($('#tag-create-btn').data('url'), $('#tag-create-form').serialize(),function(html) {
+                    $('#tag').val('');
+                    $table.find('tbody').prepend(html);
+                    Notify.success('标签添加成功！');
+                });
+
+            }
+        });
+        validator.addItem({
+            element: '#tag',
+            required: true,
+            rule: 'remote',
+            display: '标签名'
+        });
+
+        var updataValidator = new Validator({
+            element: $table,
+            autoSubmit: false,
+            onFormValidated: function(error, results, $form) {
+                if (error) {
+                    return ;
+                }
+
+                $table.on('click','[data-role=update-save]',function(){
+                    var $btn = $(this);
+                    var $form = $btn.parent().prev('td').children('form');
+                    console.log($form.find('input'));
+                    $btn.addClass('disabled');
+                    $.post($btn.data('url'), $form.serialize(),function(html) {
+                        $btn.parent().parent().prev('tr').remove();
+                        $btn.parent().parent('tr').remove();
+                        $table.find('tbody').prepend(html);
+                        Notify.success('标签更新成功！');
+                    }).error(function() {
+                        Notify.danger("标签更新失败，请重试！");
+                    });
+
+                });
+            }
+                    // updataValidator.addItem({
+                    //     element: $form.find('input'),
+                    //     required: true,
+                    //     rule: 'remote'
+                    // });
+        });
+
+        updataValidator.addItem({
+            element: '.tag-input',
+            required: true
         });
 
         $table.on('click','[data-role=update]',function(){
@@ -28,7 +75,7 @@ define(function(require, exports, module) {
         $table.on('click','[data-role=delete]',function(){
             var $btn = $(this);
             $btn.addClass('disabled');
-            if (!confirm('确定要删除标签吗?')) return ;
+            if (!confirm('确定要删除标签吗?')) {return ;}
             $.post($btn.data('url'),function(response) {
                 $btn.parent().parent().prev('tr').remove();
                 $btn.parent().parent('tr').remove();
@@ -38,23 +85,11 @@ define(function(require, exports, module) {
             });
         });
 
-        $table.on('click','[data-role=update-save]',function(){
-            var $form = $btn.parent().prev('td').children('form');
-            $btn.addClass('disabled');
-            $.post($btn.data('url'), $form.serialize(),function(html) {
-                $btn.parent().parent().prev('tr').remove();
-                $btn.parent().parent('tr').remove();
-                $table.find('tbody').prepend(html);
-                Notify.success('标签更新成功！');
-            }).error(function() {
-                Notify.danger("标签更新失败，请重试！");
-            });
-        });
-
         $table.on('click','[data-role=update-cancle]',function(){
             var self = $(this);
             var selfTr = self.parent().parent();
             selfTr.prev('tr').show();
+            selfTr.find('input').val(selfTr.prev('tr').find('td').eq(0).text());
             selfTr.hide();
         });
     }
