b75e3ea255f9e0d0fab5b88c61c7f853a441265d
wangchao <wangchao@howzhi.com>
2014-11-12 13:19:39 +0800

    去掉2个逗号

--- src/Topxia/WebBundle/Resources/public/js/controller/course/show.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/show.js
@@ -38,7 +38,7 @@ define(function(require, exports, module) {
 
         $body.scrollspy({
             target: '.course-nav-tabs',
-            offset: 120,
+            offset: 120
         });
 
         $(window).on('load', function () {
@@ -48,7 +48,7 @@ define(function(require, exports, module) {
         $('#course-nav-tabs').affix({
             offset: {
                 top: 300
-            },
+            }
         });
 
         $(window).bind("scroll",function(){ 
