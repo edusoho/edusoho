500988b546ee7fe16948acc6e2659a00709a7387
wangchao <wangchao@howzhi.com>
2014-11-13 09:36:41 +0800

    给 登录 增加 验证码

--- src/Topxia/WebBundle/Resources/public/js/controller/auth/login.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/auth/login.js
@@ -1,6 +1,8 @@
 define(function(require, exports, module) {
     var Validator = require('bootstrap.validator');
-
+    require('common/validator-rules').inject(Validator);
+    require("jquery.bootstrap-datetimepicker");
+    
     exports.run = function() {
         var validator = new Validator({
             element: '#login-form'
@@ -14,6 +16,15 @@ define(function(require, exports, module) {
         validator.addItem({
             element: '[name="_password"]',
             required: true
+        });
+
+        $(function(){
+            //数字验证 
+            if ($("#getcode_num").length > 0){
+                $("#getcode_num").click(function(){ 
+                    $(this).attr("src",$("#getcode_num").data("url")+ "?" + Math.random()); 
+                }); 
+            }
         });
         if ($("#getcode_num").length > 0){
             validator.addItem({
