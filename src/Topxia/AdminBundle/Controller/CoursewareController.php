2c9024733bea015c97a9a7322a005f1c799479fd
shenjian <shenjian@howzhi.com>
2014-11-17 15:13:25 +0800

    fix

--- src/Topxia/AdminBundle/Controller/CoursewareController.php
+++ src/Topxia/AdminBundle/Controller/CoursewareController.php
@@ -7,7 +7,18 @@ class CoursewareController extends BaseController
 {
     public function manageAction(Request $request, $categoryId)
     {
-    	   $category = $this->getCategoryService()->getCategory($categoryId);
+        $fields = $request->query->all();
+        if (empty($fields)){
+            $conditions = array(
+                'status' => '',
+                'title' => '',
+                'ownerName' =>'',
+                'category' => $categoryId
+            );
+        }
+
+        $category = $this->getCategoryService()->getCategory($categoryId);
+
         return $this->render('TopxiaAdminBundle:Courseware:manage.html.twig',array(
             'category' => $category
         ));
