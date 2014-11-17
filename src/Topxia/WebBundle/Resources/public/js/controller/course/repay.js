fa667f6120103e04225581a07ea108e6e9d1e216
wulianming <wulianming@howzhi.com>
2014-11-17 17:02:43 +0800

    order

---
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/repay.js
@@ -0,0 +1,20 @@
+define(function(require, exports, module) {
+
+    var Notify = require('common/bootstrap-notify');
+
+    exports.run = function() {
+
+        $(".cancel").on('click', function(){
+            if (!confirm('真的要取消订单吗？')) {
+                return false;
+            }
+
+            $.post($(this).data('url'), function(data) {
+                Notify.success('订单已取消成功！');
+                window.location.reload();
+            });
+        });
+
+    };
+
+});
--- src/Topxia/WebBundle/Resources/public/js/controller/my/orders.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/my/orders.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
 	var Notify = require('common/bootstrap-notify');
 
     exports.run = function() {
+
     	$("#orders-table").on('click', '.cancel-refund', function(){
     		if (!confirm('真的要取消退款吗？')) {
     			return false;
@@ -13,6 +14,12 @@ define(function(require, exports, module) {
     			window.location.reload();
     		});
     	});
+
+            $("#orders-table").on('click', '.pay', function(){
+                    $.post($(this).data('url'), {orderId: $(this).data('orderId')} ,function(html) {
+                            $("body").html(html);
+                    });
+            });
 
         $("#orders-table").on('click', '.cancel', function(){
             if (!confirm('真的要取消订单吗？')) {
