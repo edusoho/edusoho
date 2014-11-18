e0be1e9e4f6aa6a7dfbc61158d1607266e802034
jimmylu <jimmylu@howzhi.com>
2014-11-18 15:52:21 +0800

    fix

--- src/Topxia/WebBundle/Controller/MaterialLibController.php
+++ src/Topxia/WebBundle/Controller/MaterialLibController.php
@@ -3,6 +3,7 @@
 namespace Topxia\WebBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Topxia\Common\ArrayToolkit;
 use Topxia\Common\Paginator;
 use Topxia\Service\Util\CloudClientFactory;
