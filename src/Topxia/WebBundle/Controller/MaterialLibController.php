de70197503c5e692a2595d4fa4d31e2115aef462
jimmylu <jimmylu@howzhi.com>
2014-11-18 15:43:33 +0800

    fix

--- src/Topxia/WebBundle/Controller/MaterialLibController.php
+++ src/Topxia/WebBundle/Controller/MaterialLibController.php
@@ -222,6 +222,33 @@ class MaterialLibController extends BaseController {
 		return $this->createJsonResponse($result);
 	}
 
+	private function getHeadLeaderInfo()
+	{
+		$storage = $this->getSettingService()->get("storage");
+		if(!empty($storage) && array_key_exists("video_header", $storage) && $storage["video_header"]){
+	
+			$headLeader = $this->getUploadFileService()->getFileByTargetType('headLeader');
+			$headLeaderArray = json_decode($headLeader['metas2'],true);
+			$headLeaders = array();
+			foreach ($headLeaderArray as $key => $value) {
+				$headLeaders[$key] = $value['key'];
+			}
+			$headLeaderHlsKeyUrl = $this->generateUrl('uploadfile_cloud_get_head_leader_hlskey', array(), true);
+	
+			return array(
+					'headLeaders' => $headLeaders,
+					'headLeaderHlsKeyUrl' => $headLeaderHlsKeyUrl,
+					'headLength' => $headLeader['length']
+			);
+		} else {
+			return array(
+					'headLeaders' => '',
+					'headLeaderHlsKeyUrl' => '',
+					'headLength' => 0
+			);
+		}
+	}
+	
 	protected function getSettingService() {
 		return $this->getServiceKernel ()->createService ( 'System.SettingService' );
 	}
