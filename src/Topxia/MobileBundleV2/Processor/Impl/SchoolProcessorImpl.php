981c63d86fb77d0083f69a590a211da9d1d4f864
Su ju <suju@howzhi.com>
2014-11-18 19:52:14 +0800

    wx test

--- src/Topxia/MobileBundleV2/Processor/Impl/SchoolProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/SchoolProcessorImpl.php
@@ -13,13 +13,7 @@ class SchoolProcessorImpl extends BaseProcessor implements SchoolProcessor {
     public function getWXToken()
     {
         $code = $this->request->query->get('code', 1);
-        $data = $this->sendRequest(
-            "GET", 
-            "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe2d699a880ac55d5&secret=de737f49ba8b4bc349b5409cc25f6432 &code=" . $code . "&grant_type=authorization_code",
-            array(),
-            true
-            );
-        return $data;
+        return $this->controller->redirect("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe2d699a880ac55d5&secret=de737f49ba8b4bc349b5409cc25f6432 &code=" . $code . "&grant_type=authorization_code");
     }
 
     public function getPlugins()
