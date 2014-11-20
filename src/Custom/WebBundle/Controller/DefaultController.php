a2e1cb0c9d1bc71450393ef015ee8d726580523d
yangshuai <yangshuai@howzhi.com>
2014-11-20 14:58:52 +0800

    tag avatar

--- src/Custom/WebBundle/Controller/DefaultController.php
+++ src/Custom/WebBundle/Controller/DefaultController.php
@@ -1,13 +1,33 @@
 <?php
 namespace Custom\WebBundle\Controller;
 
-use Topxia\WebBundle\Controller\BaseController;
+use Topxia\WebBundle\Controller\DefaultController as TopXiaDefaultController;
 
 
-class DefaultController extends BaseController
+class DefaultController extends TopXiaDefaultController
 {
-	public function indexAction()
-	{
-         return $this->render('TopxiaWebBundle:Default:test.html.twig');
-	}
+     public function indexAction ()
+    {
+        $conditions = array('status' => 'published', 'type' => 'normal');
+        $courses = $this->getCourseService()->searchCourses($conditions, 'latest', 0, 12);
+
+        $courseSetting = $this->getSettingService()->get('course', array());
+
+        if (!empty($courseSetting['live_course_enabled']) && $courseSetting['live_course_enabled']) {
+            $recentLiveCourses = $this->getRecentLiveCourses();
+        } else {
+            $recentLiveCourses = array();
+        }
+
+        $categories = $this->getCategoryService()->findGroupRootCategories('course');
+
+        $blocks = $this->getBlockService()->getContentsByCodes(array('home_top_banner'));
+        return $this->render('CustomWebBundle:Default:index.html.twig', array(
+            'courses' => $courses,
+            'categories' => $categories,
+            'blocks' => $blocks,
+            'recentLiveCourses' => $recentLiveCourses,
+            'consultDisplay' => true
+        ));
+    }
 }
---
+++ src/Topxia/DataTag/CourseTagDataTag.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Topxia\DataTag;
+
+use Topxia\DataTag\DataTag;
+use Topxia\Service\Common\ServiceKernel;
+
+class CourseTagDataTag extends CourseBaseDataTag implements DataTag  
+{
+
+   
+    /**
+    * 获取课程的标签名称
+    *
+    */
+    public function getData(array $arguments)
+    {   
+
+        if(empty($arguments['tagId'])){
+               return  array('name' => '');
+        }
+
+        return  $this->getTagService()->getTag($arguments['tagId']);
+
+       
+
+        // return $this->getCourseTeachersAndCategories($courses);
+    }
+
+     protected function getTagService()
+    {
+        return $this->getServiceKernel()->createService('Taxonomy.TagService');
+    }
+
+
+}
