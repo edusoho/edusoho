f26f0b21bc0902c07293f926dc64b24e6255745a
yangshuai <yangshuai@howzhi.com>
2014-11-20 10:55:51 +0800

    tag avatar

--- src/Custom/AdminBundle/Controller/TagController.php
+++ src/Custom/AdminBundle/Controller/TagController.php
@@ -5,8 +5,13 @@ use Topxia\AdminBundle\Controller\BaseController as BaseController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Topxia\Common\Paginator;
+use Topxia\Common\FileToolkit;
 use Topxia\Service\Common\ServiceException;
 
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+use Imagine\Image\Point;
+use Imagine\Image\ImageInterface;
 class TagController extends BaseController
 {
 
@@ -52,7 +57,99 @@ class TagController extends BaseController
 		));
 	}
 
+ public function avatarAction(Request $request, $id)
+    {
+       $tag = $this->getTagService()->getTag($id);
+        $user = $this->getUserService()->getUser($id);
 
+        $form = $this->createFormBuilder()
+            ->add('avatar', 'file')
+            ->getForm();
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $file = $data['avatar'];
+
+                if (!FileToolkit::isImageFile($file)) {
+                    return $this->createMessageResponse('error', '上传图片格式错误，请上传jpg, gif, png格式的文件。');
+                }
+
+                $filenamePrefix = "user_{$user['id']}_";
+                $hash = substr(md5($filenamePrefix . time()), -8);
+                $ext = $file->getClientOriginalExtension();
+                $filename = $filenamePrefix . $hash . '.' . $ext;
+
+                $directory = $this->container->getParameter('topxia.upload.public_directory') . '/tmp';
+                $file = $file->move($directory, $filename);
+
+                $fileName = str_replace('.', '!', $file->getFilename());
+
+                $avatarData = $this->avatar_2($id, $fileName);
+                return $this->render('CustomAdminBundle:Tag:tag-avatar-crop-modal.html.twig', array(
+                    'user' => $user,
+                    'filename' => $fileName,
+                    'pictureUrl' => $avatarData['pictureUrl'],
+                    'naturalSize' => $avatarData['naturalSize'],
+                    'scaledSize' => $avatarData['scaledSize'],
+                    'tag'=>$tag
+                ));
+            }
+        }
+
+
+        return $this->render('CustomAdminBundle:Tag:tag-avatar-modal.html.twig', array(
+            'form' => $form->createView(),
+            'user' => $this->getUserService()->getUser($user['id']),
+            'tag'=>$tag
+        ));
+    }
+    public function avatarCropAction(Request $request, $id)
+    {
+        if($request->getMethod() == 'POST') {
+            $options = $request->request->all();
+            $filename = $request->query->get('filename');
+            $filename = str_replace('!', '.', $filename);
+            $filename = str_replace(array('..' , '/', '\\'), '', $filename);
+            $pictureFilePath = $this->container->getParameter('topxia.upload.public_directory') . '/tmp/' . $filename;
+            $this->getCustomTagService()->changeTagAvatar($id, realpath($pictureFilePath), $options);
+            return $this->createJsonResponse(true);
+        }
+
+        
+    }
+
+
+    private function avatar_2 ($id, $filename)
+    {
+        if (false === $this->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $currentUser = $this->getCurrentUser();
+
+        $filename = str_replace('!', '.', $filename);
+        $filename = str_replace(array('..' , '/', '\\'), '', $filename);
+        $pictureFilePath = $this->container->getParameter('topxia.upload.public_directory') . '/tmp/' . $filename;
+
+        try {
+            $imagine = new Imagine();
+            $image = $imagine->open($pictureFilePath);
+        } catch (\Exception $e) {
+            @unlink($pictureFilePath);
+            return $this->createMessageResponse('error', '该文件为非图片格式文件，请重新上传。');
+        }
+
+        $naturalSize = $image->getSize();
+        $scaledSize = $naturalSize->widen(270)->heighten(270);
+        $pictureUrl = 'tmp/' . $filename;
+
+        return array(
+            'naturalSize' => $naturalSize,
+            'scaledSize' => $scaledSize,
+            'pictureUrl' => $pictureUrl
+        );
+    }
 	private function getTagService()
 	{
         		return $this->getServiceKernel()->createService('Taxonomy.TagService');
--- src/Custom/Service/Taxonomy/Impl/TagServiceImpl.php
+++ src/Custom/Service/Taxonomy/Impl/TagServiceImpl.php
@@ -1,11 +1,18 @@
 <?php
 namespace Custom\Service\Taxonomy\Impl;
 use Topxia\Service\Taxonomy\Impl\TagServiceImpl as BaseTagServiceImpl ;
-use Topxia\Service\Taxonomy\TagService;
-
+use Custom\Service\Taxonomy\CustomTagService;
 use Topxia\Common\ArrayToolkit;
 
-class TagServiceImpl extends BaseTagServiceImpl implements TagService
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\File;
+
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+use Imagine\Image\Point;
+use Imagine\Image\ImageInterface;
+
+class TagServiceImpl extends BaseTagServiceImpl implements CustomTagService
 {
 
     public function addTag(array $tag)
@@ -53,6 +60,57 @@ class TagServiceImpl extends BaseTagServiceImpl implements TagService
         return $tag;
     }
 
+    public function changeTagAvatar($tagId, $filePath, array $options)
+    {
+        $tag = $this->getTag($tagId);
+        if (empty($tag)) {
+            throw $this->createServiceException('标签不存在，图标更新失败！');
+        }
+
+
+        $pathinfo = pathinfo($filePath);
+
+        $imagine = new Imagine();
+        $rawImage = $imagine->open($filePath);
+
+        $largeImage = $rawImage->copy();
+        $largeImage->crop(new Point($options['x'], $options['y']), new Box($options['width'], $options['height']));
+        $largeImage->resize(new Box(200, 200));
+        $largeFilePath = "{$pathinfo['dirname']}/{$pathinfo['filename']}_large.{$pathinfo['extension']}";
+        $largeImage->save($largeFilePath, array('quality' => 90));
+        $largeFileRecord = $this->getFileService()->uploadFile('course', new File($largeFilePath));
+
+        $largeImage->resize(new Box(120, 120));
+        $mediumFilePath = "{$pathinfo['dirname']}/{$pathinfo['filename']}_medium.{$pathinfo['extension']}";
+        $largeImage->save($mediumFilePath, array('quality' => 90));
+        $mediumFileRecord = $this->getFileService()->uploadFile('course', new File($mediumFilePath));
+
+        $largeImage->resize(new Box(48, 48));
+        $smallFilePath = "{$pathinfo['dirname']}/{$pathinfo['filename']}_small.{$pathinfo['extension']}";
+        $largeImage->save($smallFilePath, array('quality' => 90));
+        $smallFileRecord = $this->getFileService()->uploadFile('course', new File($smallFilePath));
+        @unlink($filePath);
+
+        $oldAvatars = array(
+            'smallAvatar' => $tag['smallAvatar'] ? $this->getKernel()->getParameter('topxia.upload.public_directory') . '/' . str_replace('public://', '', $tag['smallAvatar']) : null,
+            'mediumAvatar' => $tag['mediumAvatar'] ? $this->getKernel()->getParameter('topxia.upload.public_directory') . '/' . str_replace('public://', '', $tag['mediumAvatar']) : null,
+            'largeAvatar' => $tag['largeAvatar'] ? $this->getKernel()->getParameter('topxia.upload.public_directory') . '/' . str_replace('public://', '', $tag['largeAvatar']) : null
+        );
+
+        array_map(function($oldAvatar){
+            if (!empty($oldAvatar)) {
+                @unlink($oldAvatar);
+            }
+        }, $oldAvatars);
+
+        return  $this->getTagDao()->updateTag($tagId, array(
+            'smallAvatar' => $smallFileRecord['uri'],
+            'mediumAvatar' => $mediumFileRecord['uri'],
+            'largeAvatar' => $largeFileRecord['uri'],
+        ));
+    }
+
+
         private function getTagDao()
     {
         return $this->createDao('Taxonomy.TagDao');
@@ -62,5 +120,10 @@ class TagServiceImpl extends BaseTagServiceImpl implements TagService
     {
         return $this->createService('System.LogService');
     }
+      private function getFileService()
+    {
+        return $this->createService('Content.FileService');
+    }
+
 
 }  
