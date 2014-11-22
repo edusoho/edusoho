cbb12b682041e7933907bb85dc5c44bb6f66c8de
yellowbig <dahwang@126.com>
2014-11-22 23:33:32 +0800

    初步完成setup页面

--- plugins/Fomalhaut/AcctSetupBundle/Form/Type/SetupType.php
+++ plugins/Fomalhaut/AcctSetupBundle/Form/Type/SetupType.php
@@ -16,3 +16,87 @@ class SetupType extends AbstractType
                     'label' => 'form.setup.name',
                 )
             )
+            ->add(
+                'account',
+                'text',
+                array(
+                    'label' => 'form.setup.account',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'level',
+                'choice',
+                array(
+                    'label' => 'form.setup.level',
+                    'required' => false,
+                    'choices' => array(
+                        'level_ordinsub' => 'form.setup.level.ordinary_subscription',
+                        'level_authsub_ordinserv' => 'form.setup.level.auth_sub_or_ordin_serv',
+                        'level_authserv' => 'form.setup.level.auth_sub_or_ordin_serv',
+                    ),
+                )
+            )
+            ->add(
+                'key',
+                'text',
+                array(
+                    'label' => 'form.setup.key',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'secret',
+                'text',
+                array(
+                    'label' => 'form.setup.secret',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'interfaceaddr',
+                'text',
+                array(
+                    'label' => 'form.setup.interface_address',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'token',
+                'text',
+                array(
+                    'label' => 'form.setup.token',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'encodingaeskey',
+                'text',
+                array(
+                    'label' => 'form.setup.encodingaeskey',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'qrcode',
+                'file',
+                array(
+                    'label' => 'form.setup.qrcode',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'headimg',
+                'file',
+                array(
+                    'label' => 'form.setup.headimg',
+                    'required' => false,
+                )
+            );
+    }
+
+    public function getName()
+    {
+        return 'wechat_acctsetup_setup';
+    }
+}
--- plugins/Fomalhaut/AcctSetupBundle/Process/Step/SetupStep.php
+++ plugins/Fomalhaut/AcctSetupBundle/Process/Step/SetupStep.php
@@ -3,13 +3,13 @@ namespace Fomalhaut\AcctSetupBundle\Process\Step;
 
 use Sylius\Bundle\FlowBundle\Process\Context\ProcessContextInterface;
 
-class OneclickStep extends AbstractStep
+class SetupStep extends AbstractStep
 {
     public function displayAction(ProcessContextInterface $context)
     {
         return $this->render(
             'AcctSetupBundle:Prosess/Step:SetupStep.html.twig',
-            array('form' => $this->createForm('wechat_acctsetup_Setup')->createView())
+            array('form' => $this->createForm('wechat_acctsetup_setup')->createView())
         );
     }
 
