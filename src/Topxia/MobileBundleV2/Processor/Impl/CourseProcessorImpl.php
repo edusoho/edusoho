016e13916d16232485be9b64bb36fb1469c120eb
HuangBoyi <huangboyi@howzhi.com>
2014-11-27 23:23:16 +0800

    no message

--- src/Topxia/MobileBundleV2/Processor/Impl/CourseProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/CourseProcessorImpl.php
@@ -315,7 +315,6 @@ class CourseProcessorImpl extends BaseProcessor implements CourseProcessor
 
 	        	$courseMembers = $this->controller->getCourseService()->searchMember($conditions,$start,$limit);
 	        	var_dump(ArrayToolkit::column($courseMembers, 'courseId'));
-	        	$courses = $this->getCourseService()->findCoursesByIds(ArrayToolkit::column($courseMembers, 'courseId'));
 	        	$noteInfos = array();
 	        	for ($i=0; $i < count($courseMembers); $i++) { 
 	        		$courseMember = $courseMembers[$i];
@@ -324,6 +323,7 @@ class CourseProcessorImpl extends BaseProcessor implements CourseProcessor
 	        		if (empty($noteNum)) {
 	        			continue;
 	        		}
+	        		var_dump($course);
 	        		//$noteListByOneCourse = $this->controller->getNoteService()->findUserCourseNotes($user['id'],ArrayToolkit::column($courseMembers, 'courseId'));
 	        		//var_dump($noteListByOneCourse);
 	        		$noteInfos[] = array(
