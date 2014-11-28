6586297eeac23a72ed746cc141fa6acab320fa13
nifeng <nifeng@howzhi.com>
2014-11-28 14:00:14 +0800

    fix

--- web/app_dev.php
+++ web/app_dev.php
@@ -59,7 +59,7 @@ $serviceKernel->setCurrentUser($currentUser);
 // NOTICE: 防止请求捕捉失败而做异常处理 
 // 包括：数据库连接失败等
 try {
-  $response = $kernel->handle($request);
+    $response = $kernel->handle($request);
 } catch (\RuntimeException $e) {
     echo "Error!  ". $e->getMessage();
     die();
