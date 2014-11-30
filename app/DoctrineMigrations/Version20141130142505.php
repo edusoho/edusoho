90a4cec8b3e1de0bdad8889d04d97faaf4e646f2
ruanwenqin <452253895@qq.com>
2014-11-30 17:02:31 +0800

    fix migrations file

--- app/DoctrineMigrations/Version20141130142505.php
+++ app/DoctrineMigrations/Version20141130142505.php
@@ -15,7 +15,7 @@ class Version20141130142505 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
     	$this->addSql("ALTER TABLE `question` ADD `mainKnowledgeId` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '主知识点id' AFTER `categoryId`;");
     	$this->addSql("ALTER TABLE `question` ADD `relatedKnowledgeIds` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '关联知识点ID' AFTER `mainKnowledgeId`;");
-    	$this->addSql("AALTER TABLE `question` ADD `tagIds` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'tagsIds' AFTER `relatedKnowledgeIds`;");
+    	$this->addSql("ALTER TABLE `question` ADD `tagIds` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'tagsIds' AFTER `relatedKnowledgeIds`;");
     }
 
     public function down(Schema $schema)
