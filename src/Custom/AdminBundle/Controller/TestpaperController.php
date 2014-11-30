0bf68492d1d2a88fc102b1459ae597a999c054fb
Wellming Li <wellming.li@howzhi.com>
2014-11-30 17:39:48 +0800

    testpaper

---
+++ src/Custom/AdminBundle/Controller/TestpaperController.php
@@ -0,0 +1,364 @@
+<?php
+namespace Custom\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Topxia\AdminBundle\Controller\BaseController;
+use Topxia\Common\ArrayToolkit;
+use Topxia\Common\Paginator;
+use Topxia\Service\Question\Type\QuestionTypeFactory;
+
+class TestpaperController extends BaseController
+{
+    public function indexAction(Request $request)
+    {
+        $category = $this->getCategoryService()->getCategory($request->query->get('categoryId'));
+
+        $conditions = array();
+        $conditions['target'] = "category-{$category['id']}";
+        $paginator = new Paginator(
+            $this->get('request'),
+            $this->getTestpaperService()->searchTestpapersCount($conditions),
+            10
+        );
+
+        $testpapers = $this->getTestpaperService()->searchTestpapers(
+            $conditions,
+            array('createdTime' ,'DESC'),
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
+        $users = $this->getUserService()->findUsersByIds(ArrayToolkit::column($testpapers, 'updatedUserId')); 
+        
+        return $this->render('CustomAdminBundle:Testpaper:index.html.twig', array(
+            'category' => $category,
+            'testpapers' => $testpapers,
+            'users' => $users,
+            'paginator' => $paginator,
+        ));
+    }
+
+    public function createAction(Request $request)
+    {
+        $category = $this->getCategoryService()->getCategory($request->query->get('categoryId'));
+
+        if ($request->getMethod() == 'POST') {
+            $fields = $request->request->all();
+            $fields['ranges'] = empty($fields['ranges']) ? array() : explode(',', $fields['ranges']);
+            $fields['target'] = "category-{$category['id']}";
+            $fields['pattern'] = 'QuestionType';
+            list($testpaper, $items) = $this->getTestpaperService()->createTestpaper($fields);
+            return $this->redirect($this->generateUrl('admin_testpaper_items',array('id' => $testpaper['id'])));
+        }
+
+        $typeNames = $this->get('topxia.twig.web_extension')->getDict('questionType');
+        $types = array();
+        foreach ($typeNames as $type => $name) {
+            $typeObj = QuestionTypeFactory::create($type);
+            $types[] = array(
+                'key' => $type,
+                'name' => $name,
+                'hasMissScore' => $typeObj->hasMissScore(),
+            );
+        }
+
+        return $this->render('CustomAdminBundle:Testpaper:create.html.twig', array(
+            'category'    => $category,
+            'types' => $types,
+        ));
+    }
+
+    public function buildCheckAction(Request $request)
+    {
+        $category = $this->getCategoryService()->getCategory($request->query->get('categoryId'));
+
+        $data = $request->request->all();
+        $data['target'] = "category-{$category['id']}";
+        $data['ranges'] = empty($data['ranges']) ? array() : explode(',', $data['ranges']);
+        $result = $this->getTestpaperService()->canBuildTestpaper('QuestionType', $data);
+        return $this->createJsonResponse($result);
+    }
+
+    public function updateAction(Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->getTestpaper($id);
+        if (empty($testpaper)) {
+            throw $this->createNotFoundException('试卷不存在');
+        }
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        if ($request->getMethod() == 'POST') {
+            $data = $request->request->all();
+            $testpaper = $this->getTestpaperService()->updateTestpaper($id, $data);
+            $this->setFlashMessage('success', '试卷信息保存成功！');
+            return $this->redirect($this->generateUrl('admin_testpaper', array('categoryId' => $category['id'])));
+        }
+
+        return $this->render('CustomAdminBundle:Testpaper:update.html.twig', array(
+            'category'    => $category,
+            'testpaper' => $testpaper,
+        ));
+    }
+
+    public function deleteAction(Request $request, $id)
+    {
+        $this->getTestpaperService()->deleteTestpaper($id);
+
+        return $this->createJsonResponse(true);
+    }
+
+    public function deletesAction(Request $request)
+    {   
+        $ids = $request->request->get('ids');
+
+        foreach (is_array($ids) ? $ids : array() as $id) {
+            $this->getTestpaperService()->deleteTestpaper($id);
+        }
+
+        return $this->createJsonResponse(true);
+    }
+
+    public function publishAction (Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->publishTestpaper($id);
+
+        $user = $this->getUserService()->getUser($testpaper['updatedUserId']);
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        return $this->render('CustomAdminBundle:Testpaper:testpaper-tr.html.twig', array(
+            'testpaper' => $testpaper,
+            'user' => $user,
+            'category' => $category,
+        ));
+    }
+
+    public function closeAction (Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->closeTestpaper($id);
+
+        $user = $this->getUserService()->getUser($testpaper['updatedUserId']);
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        return $this->render('CustomAdminBundle:Testpaper:testpaper-tr.html.twig', array(
+            'testpaper' => $testpaper,
+            'user' => $user,
+            'category' => $category,
+        ));
+    }
+
+    private function getTestpaperWithException($course, $testpaperId)
+    {
+        $testpaper = $this->getTestpaperService()->getTestpaper($testpaperId);
+        if (empty($testpaper)) {
+            throw $this->createNotFoundException();
+        }
+
+        if ($testpaper['target'] != "course-{$course['id']}") {
+            throw $this->createAccessDeniedException();
+        }
+        return $testpaper;
+    }
+
+    private function getCategoryByTarget($target)
+    {
+        $id = intval(str_replace('category-', '', $target));
+        return $this->getCategoryService()->getCategory($id);
+    }
+
+    public function itemsAction(Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->getTestpaper($id);
+        if(empty($testpaper)){
+            throw $this->createNotFoundException('试卷不存在');
+        }
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        if ($request->getMethod() == 'POST') {
+            $data = $request->request->all();
+            if (empty($data['questionId']) or empty($data['scores'])) {
+                return $this->createMessageResponse('error', '试卷题目不能为空！');
+            }
+            if (count($data['questionId']) != count($data['scores'])) {
+                return $this->createMessageResponse('error', '试卷题目数据不正确');
+            }
+
+            $data['questionId'] = array_values($data['questionId']);
+            $data['scores'] = array_values($data['scores']);
+
+            $items = array();
+            foreach ($data['questionId'] as $index => $questionId) {
+                $items[] = array('questionId' => $questionId, 'score' => $data['scores'][$index]);
+            }
+
+            $this->getTestpaperService()->updateTestpaperItems($testpaper['id'], $items);
+
+            $this->setFlashMessage('success', '试卷题目保存成功！');
+            return $this->redirect($this->generateUrl('admin_testpaper',array( 'categoryId' => $category['id'])));
+        }
+
+        $items = $this->getTestpaperService()->getTestpaperItems($testpaper['id']);
+
+        $questions = $this->getQuestionService()->findQuestionsByIds(ArrayToolkit::column($items, 'questionId'));
+
+        $subItems = array();
+        foreach ($items as $key => $item) {
+            if ($item['parentId'] > 0) {
+                $subItems[$item['parentId']][] = $item;
+                unset($items[$key]);
+            }
+        }
+
+        return $this->render('CustomAdminBundle:Testpaper:items.html.twig', array(
+            'category' => $category,
+            'testpaper' => $testpaper,
+            'items' => ArrayToolkit::group($items, 'questionType'),
+            'subItems' => $subItems,
+            'questions' => $questions,
+        ));
+    }
+
+    public function itemsResetAction(Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->getTestpaper($id);
+        if(empty($testpaper)){
+            throw $this->createNotFoundException('试卷不存在');
+        }
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        if ($request->getMethod() == 'POST') {
+            $data = $request->request->all();
+            $data['target'] = "category-{$category['id']}";
+            $this->getTestpaperService()->buildTestpaper($testpaper['id'], $data);
+            return $this->redirect($this->generateUrl('admin_testpaper_items', array('id' => $id)));
+        }
+
+        $typeNames = $this->get('topxia.twig.web_extension')->getDict('questionType');
+        $types = array();
+        foreach ($typeNames as $type => $name) {
+            $typeObj = QuestionTypeFactory::create($type);
+            $types[] = array(
+                'key' => $type,
+                'name' => $name,
+                'hasMissScore' => $typeObj->hasMissScore(),
+            );
+        }
+
+
+        return $this->render('CustomAdminBundle:Testpaper:items-reset.html.twig', array(
+            'category'    => $category,
+            'testpaper' => $testpaper,
+            'types' => $types,
+        ));
+    }
+
+    public function itemPickerAction(Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->getTestpaper($id);
+        if (empty($testpaper)) {
+            throw $this->createNotFoundException();
+        }
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        $conditions = $request->query->all();
+
+        if (empty($conditions['target'])) {
+            $conditions['targetPrefix'] = "category-{$category['id']}";
+        }
+
+        $conditions['parentId'] = 0;
+        $conditions['excludeIds'] = empty($conditions['excludeIds']) ? array() : explode(',', $conditions['excludeIds']);
+
+        if (!empty($conditions['keyword'])) {
+            $conditions['stem'] = $conditions['keyword'];
+        }
+
+
+        $replace = empty($conditions['replace']) ? '' : $conditions['replace'];
+        
+        $paginator = new Paginator(
+            $request,
+            $this->getQuestionService()->searchQuestionsCount($conditions),
+            7
+        );
+
+        $questions = $this->getQuestionService()->searchQuestions(
+                $conditions, 
+                array('createdTime' ,'DESC'), 
+                $paginator->getOffsetCount(),
+                $paginator->getPerPageCount()
+        );
+
+        return $this->render('CustomAdminBundle:Testpaper:item-picker-modal.html.twig', array(
+            'category' => $category,
+            'testpaper' => $testpaper,
+            'questions' => $questions,
+            'replace' => $replace,
+            'paginator' => $paginator,
+            'conditions' => $conditions,
+        ));
+        
+    }
+
+    public function itemPickedAction(Request $request, $id)
+    {
+        $testpaper = $this->getTestpaperService()->getTestpaper($id);
+        if (empty($testpaper)) {
+            throw $this->createNotFoundException();
+        }
+
+        $category = $this->getCategoryByTarget($testpaper['target']);
+
+        $question = $this->getQuestionService()->getQuestion($request->query->get('questionId'));
+        if (empty($question)) {
+            throw $this->createNotFoundException();
+        }
+
+        if ($question['subCount'] > 0) {
+            $subQuestions = $this->getQuestionService()->findQuestionsByParentId($question['id']);
+        } else {
+            $subQuestions = array();
+        }
+
+        return $this->render('CustomAdminBundle:Testpaper:item-picked.html.twig', array(
+            'category'    => $category,
+            'testpaper' => $testpaper,
+            'question' => $question,
+            'subQuestions' => $subQuestions,
+            'type' => $question['type']
+        ));
+
+    }
+
+
+
+
+
+    private function getCourseService()
+    {
+        return $this->getServiceKernel()->createService('Course.CourseService');
+    }
+
+    private function getTestpaperService()
+    {
+        return $this->getServiceKernel()->createService('Testpaper.TestpaperService');
+    }
+
+    private function getQuestionService()
+    {
+        return $this->getServiceKernel()->createService('Question.QuestionService');
+    }
+
+    private function getCategoryService()
+    {
+        return $this->getServiceKernel()->createService('Taxonomy.CategoryService');
+    }
+
+}
---
+++ src/Custom/AdminBundle/Resources/public/js/controller/testpaper/testpaper-form.js
@@ -0,0 +1,341 @@
+define(function(require, exports, module) {
+
+    var Widget     = require('widget');
+    var Notify = require('common/bootstrap-notify');
+    var EditorFactory = require('common/kindeditor-factory');
+    var Validator = require('bootstrap.validator');
+    require('common/validator-rules').inject(Validator);
+    require('jquery.nouislider');
+    require('jquery.nouislider-css');
+    require('jquery.sortable');
+
+    var TestpaperForm = Widget.extend({
+
+        attrs: {
+            validator: null
+        },
+
+        events: {
+            'click [name=mode]': 'onClickModeField',
+        },
+
+        setup:function() {
+            this.createValidator();
+            if (this.get('haveBaseFields') === true) {
+                this.initBaseFields();
+            }
+            if (this.get('haveBuildFields') === true) {
+                this.initBuildFields();
+            }
+        },
+
+        createValidator: function() {
+            this.set('validator', new Validator({
+                element: this.element,
+            }));
+        },
+
+        initBaseFields: function() {
+            var validator = this.get('validator');
+            var editor = EditorFactory.create('#testpaper-description-field', 'simple_noimage');
+            validator.on('formValidate', function(elemetn, event) {
+                editor.sync();
+            });
+
+            validator.addItem({
+                element: '#testpaper-name-field',
+                required: true
+            });
+
+            validator.addItem({
+                element: '#testpaper-description-field',
+                required: true,
+                rule: 'maxlength{max:500}'
+            });
+
+            validator.addItem({
+                element: '#testpaper-limitedTime-field',
+                required: true,
+                rule: 'integer,max{max:10000}'
+            });
+        },
+
+        initBuildFields: function() {
+            this.initDifficultyPercentageSlider();
+            //@todo, refact it, wellming.
+            this.initRangeField();
+            this.initQuestionTypeSortable();
+
+            var validator = this.get('validator'),
+                self = this;
+
+            validator.on('formValidated', function(error, msg, $form) {
+
+                if (error) {
+                    return ;
+                }
+
+                if(!self.checkBuildCountAndScoreInputs()){
+                    return ;
+                }
+
+                if (!self.canBuild()) {
+                    return ;
+                }
+                $('#testpaper-create-btn').button('submiting').addClass('disabled');
+
+                $form[0].submit();
+            });
+        },
+
+        onClickModeField: function(e) {
+           if ($(e.currentTarget).val() == 'difficulty') {
+                this.$('.difficulty-form-group').removeClass('hidden');
+                this.$('.difficulty-percentage-slider').change();
+            } else {
+                this.$('.difficulty-form-group').addClass('hidden');
+            }
+        },
+
+        canBuild: function() {
+            var $form = $("#testpaper-form"),
+                isOk = true;
+
+            $.ajax($form.data('buildCheckUrl'), {
+                type: 'POST',
+                async: false,
+                data: $form.serialize(),
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status != 'yes') {
+                        var missingTexts = [];
+                        var types = {
+                            single_choice: '单选题',
+                            uncertain_choice: '不定项选择题',
+                            choice: '多选题',
+                            fill: '填空题',
+                            determine: '判断题',
+                            essay: '问答题',
+                            material: '材料题'
+                        }
+                        $.each(response.missing, function(type, count) {
+                            missingTexts.push(types[type] + '缺<strong>' + count + '</strong>道');
+                        });
+                        Notify.danger('课程题库题目数量不足，无法生成试卷：<br>' + missingTexts.join('，'), 5);
+                        isOk = false;
+                    }
+                }
+            });
+
+            return isOk;
+        },
+
+        checkBuildCountAndScoreInputs: function() {
+            var $form = this.element;
+            var totalNumber = 0,
+                isOk = true;
+
+            $form.find('.item-number').each(function() {
+                var number = $(this).val();
+                if (!/^[0-9]*$/.test(number)) {
+                    Notify.danger('题目数量只能填写数字');
+                    $(this).focus();
+                    isOk = false;
+                    return false;
+                }
+                totalNumber += parseInt(number);
+            });
+
+            if (isOk) {
+                if (totalNumber == 0) {
+                    isOk = false;
+                    Notify.danger('试卷题目总数量不能为0。');
+                    return isOk;
+                }
+
+                if (totalNumber > 1000) {
+                    isOk = false;
+                    Notify.danger('试卷题目总数不能超过1000道。');
+                    return isOk;
+                }
+            }
+
+            $form.find('.item-score').each(function() {
+                var score = $(this).val();
+
+                if (score == '0') {
+                    Notify.danger('题目分值不能为0。');
+                    isOk = false;
+                    return false;
+                }
+
+                if (!/^(([1-9]{1}\d*)|([0]{1}))(\.(\d){1})?$/.test(score)) {
+                    Notify.danger('题目分值只能填写数字，且最多一位小数。');
+                    $(this).focus();
+                    isOk = false;
+                    return false;
+                }
+            });
+
+            $form.find('.item-miss-score').each(function() {
+                var missScore = $(this).val();
+
+                if (!/^(([1-9]{1}\d*)|([0]{1}))(\.(\d){1})?$/.test(missScore)) {
+                    Notify.danger('题目漏选分值只能填写数字，且最多一位小数。');
+                    $(this).focus();
+                    isOk = false;
+                    return false;
+                }
+
+                var score=$(this).parent().find('.item-score').val();
+
+                if (Number(missScore) > Number(score)) {
+                    Notify.danger('题目漏选分值不能大于题目分值。');
+                    isOk = false;
+                    $(this).focus();
+                    return false;
+                }
+
+            });
+
+
+            return isOk;
+        },
+
+        initQuestionTypeSortable: function() {
+            var $list = $('#testpaper-question-options').sortable({
+                itemSelector: '.testpaper-question-option-item',
+                handle: '.testpaper-question-option-item-sort-handler',
+                serialize: function(parent, children, isContainer) {
+                    return isContainer ? children : parent.attr('id');
+                }
+            });
+        },
+
+        initDifficultyPercentageSlider: function() {
+            var self = this;
+            return self.$('.difficulty-percentage-slider').noUiSlider({
+                range: [0, 100],
+                start: [30, 70],
+                step: 5,
+                serialization: {
+                    resolution: 1
+                },
+                slide: function() {
+                    this.trigger('change');
+                }
+            }).change(function() {
+                var values = $(this).val();
+
+                var simplePercentage = values[0],
+                    normalPercentage = values[1] - values[0],
+                    difficultyPercentage = 100 - values[1];
+
+                self.$('.simple-percentage-text').html('简单' + simplePercentage + '%');
+                self.$('.normal-percentage-text').html('一般' + normalPercentage + '%');
+                self.$('.difficulty-percentage-text').html('困难' + difficultyPercentage + '%');
+
+                self.$('input[name="percentages[simple]"]').val(simplePercentage);
+                self.$('input[name="percentages[normal]"]').val(normalPercentage);
+                self.$('input[name="percentages[difficulty]"]').val(difficultyPercentage);
+
+            });
+        },
+
+        initRangeField: function() {
+            var self = this;
+            $('input[name=range]').on('click', function() {
+                if ($(this).val() == 'lesson') {
+                    $("#testpaper-range-selects").show();
+                } else {
+                    $("#testpaper-range-selects").hide();
+                }
+
+                self._refreshRangesValue();
+            });
+
+            $("#testpaper-range-start").change(function() {
+                var startIndex = self._getRangeStartIndex();
+
+                self._resetRangeEndOptions(startIndex);
+
+                self._refreshRangesValue();
+            });
+
+            $("#testpaper-range-end").change(function() {
+                self._refreshRangesValue();
+            });
+
+        },
+
+        _resetRangeEndOptions: function(startIndex) {
+            if (startIndex > 0) {
+                startIndex--;
+                var $options = $("#testpaper-range-start option:gt(" + startIndex + ")");
+            } else {
+                var $options = $("#testpaper-range-start option");
+            }
+
+            var selected = $("#testpaper-range-end option:selected").val();
+
+            $("#testpaper-range-end option").remove();
+            $("#testpaper-range-end").html($options.clone());
+            $("#testpaper-range-end option").each(function() {
+                if ($(this).val() == selected) {
+                    $("#testpaper-range-end").val(selected);
+                }
+            });
+        },
+
+        _refreshRangesValue: function() {
+            var $ranges = $('input[name=ranges]');
+            if ($('input[name=range]:checked').val() != 'lesson') {
+                $ranges.val('');
+                return;
+            }
+
+            var startIndex = this._getRangeStartIndex();
+            var endIndex = this._getRangeEndIndex();
+
+            if (startIndex < 0 || endIndex < 0) {
+                $ranges.val('');
+                return;
+            }
+
+            var values = [];
+            for (var i = startIndex; i <= endIndex; i++) {
+                values.push($("#testpaper-range-start option:eq(" + i + ")").val());
+            }
+
+            $ranges.val(values.join(','));
+        },
+
+        _getRangeStartIndex: function() {
+            var $startOption = $("#testpaper-range-start option:selected");
+            return parseInt($("#testpaper-range-start option").index($startOption));
+        },
+
+        _getRangeEndIndex: function() {
+            var selected = $("#testpaper-range-end option:selected").val();
+            if (selected == '') {
+                return -1;
+            }
+
+            var index = -1;
+            $("#testpaper-range-start option").each(function(i, item) {
+                if ($(this).val() == selected) {
+                    index = i;
+                }
+            });
+
+            return index;
+        },
+    });
+
+    exports.run = function() {
+        new TestpaperForm({
+            element: '#testpaper-form'
+        });
+    }
+
+});
