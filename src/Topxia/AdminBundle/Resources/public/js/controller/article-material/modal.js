2eb894247cc2df1891791eff2c127b4868c324fe
shenjian <shenjian@howzhi.com>
2014-11-30 22:50:38 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/article-material/modal.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/article-material/modal.js
@@ -3,7 +3,110 @@ define(function(require, exports, module){
     require('common/validator-rules').inject(Validator);
     var Notify = require('common/bootstrap-notify');
     var EditorFactory = require('common/kindeditor-factory');
+    var TagChooser = require('tag-chooser');
+    var TagTreeChooser = require('tag-tree-chooser');
+
+    var tagIds = [];
+    var relatedKnowledgeIds = [];
+    var mainKnowledgeId = [];
+    var queryUrl = "";
+
     exports.run = function(){
+        var $tagIds = [];
+        var $mainKnowledgeId = [];
+        var $relatedKnowledgeIds = [];
+
+        _initTagChooer();
+        _initMainknowledgeTagChooer();
+        _initRelatedknowledgeTagChooer();
+
+        function _initTagChooer()
+        {
+            if ($('[data-role=tag-ids]').length > 0) {
+                $tagIds = $('[data-role=tag-ids]').val();
+                $tagIds = [$tagIds];
+            }; 
+
+            var chooser = new TagChooser({
+              element: '#tag-chooser',
+              sourceUrl: '/admin/tagset/get',
+              queryUrl: '/admin/tags/Choosered',
+              matchUrl: '/admin/tagset/match?q={{query}}',
+              maxTagNum: 15,
+              choosedTags: $tagIds
+            });
+
+            chooser.on('change', function(tags) {
+              var tagIdsTemp = [];
+              $.each(tags,function(i,item){
+                  tagIdsTemp.push(item.id)
+              })
+              tagIds = tagIdsTemp;
+            });
+
+            chooser.on('existed', function(existTag){
+            });
+        }
+
+        function _initMainknowledgeTagChooer()
+        {
+            if ($('[data-role=main-knowledge-ids]').length > 0) {
+                $mainKnowledgeId = $('[data-role=main-knowledge-ids]').val();
+                $mainKnowledgeId = [$mainKnowledgeId];
+            };
+            $categoryId = $('[data-role=categoryId]').val();
+
+            var chooserTreeForMainKnowlege = new TagTreeChooser({
+              element: '#tag-main-knowlege-tree-chooser',
+              sourceUrl: "/admin/knowledge/getTreeList?categoryId="+$categoryId,
+              queryUrl: '/admin/knowledge/choosered',
+              matchUrl: '/admin/tagset/match?q={{query}}',
+              maxTagNum: 1,
+              choosedTags: $mainKnowledgeId
+            });
+
+            chooserTreeForMainKnowlege.on('change', function(tags) {
+
+              $.each(tags,function(i,item){
+                mainKnowledgeId = item.id;
+              });
+
+            });
+
+            chooserTreeForMainKnowlege.on('existed', function(existTag){
+            });
+        }
+         
+        function _initRelatedknowledgeTagChooer()
+        {
+            if ($('[data-role=related-knowledge-ids]').length > 0) {
+                $relatedKnowledgeIds = $('[data-role=related-knowledge-ids]').val();
+                $relatedKnowledgeIds = [$relatedKnowledgeIds];
+            };
+
+            var chooserTreeForRelatedKnowlege = new TagTreeChooser({
+                element: '#tag-releated-knowlege-tree-chooser',
+                sourceUrl: "/admin/knowledge/getTreeList?categoryId="+$categoryId,
+                queryUrl: '/admin/knowledge/choosered',
+                matchUrl: '/admin/tagset/match?q={{query}}',
+                maxTagNum: 3,
+                choosedTags: $relatedKnowledgeIds
+            });
+
+            chooserTreeForRelatedKnowlege.on('change', function(tags) {
+
+              var relatedKnowledgeIdsTemp = [];
+              $.each(tags,function(i,item){
+                  relatedKnowledgeIdsTemp.push(item.id)
+              })
+              relatedKnowledgeIds = relatedKnowledgeIdsTemp;
+
+            });
+
+            chooserTreeForRelatedKnowlege.on('existed', function(existTag){
+            });
+        }
+
         var $form = $("#article-material-form");
         $modal = $form.parents('.modal');
         var validator = _initValidator($form, $modal);
@@ -54,6 +157,21 @@ define(function(require, exports, module){
 
             return validator;
         }
+
+        var tagModalChooser = new TagChooser({
+            element: '#tag-chooser',
+            sourceUrl: 'xxxx',
+            multi: true,
+            items: []
+        });
+
+        tagModalChooser.on('choosed', function(items) {
+            var tagIds = [];
+            for (var i = items.length - 1; i >= 0; i--) {
+                tagIds[i] = items[i]['id'];
+            };
+            $('#article-material-tag-field').val(tagIds);
+        });
 
     }
 });
