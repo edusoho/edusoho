4371c8255def43cd3b4dbb10122c996e76da7475
wangchao <wangchao@howzhi.com>
2014-12-03 18:02:21 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/setting/auth.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/setting/auth.js
@@ -3,12 +3,39 @@ define(function(require, exports, module) {
     var EditorFactory = require('common/kindeditor-factory');
     var Validator = require('bootstrap.validator');
     require('jquery.sortable');
+    require('common/validator-rules').inject(Validator);
 
     exports.run = function() {
 
         var editor = EditorFactory.create('#user_terms_body', 'simple');
 
             editor.sync();
+
+        var validator = new Validator({
+            element: '#auth-form'
+        });
+        
+        validator.addItem({
+            element: '[name=temporary_lock_allowed_times]',
+            rule: 'integer'
+        });
+
+        validator.addItem({
+            element: '[name=temporary_lock_hours]',
+            rule: 'integer'
+        });
+
+        var hideOrShowTimeAndHours = function (){
+          if ( $('[name=temporary_lock_enabled]').filter(':checked').attr("value") == 1 ){
+            $('#times_and_hours').show();
+          }else if ( $('[name=temporary_lock_enabled]').filter(':checked').attr("value") == 0 ){
+            $('#times_and_hours').hide();
+          };
+        };
+        hideOrShowTimeAndHours();
+        $('[name=temporary_lock_enabled]').change(function (){
+           hideOrShowTimeAndHours();
+        });  
 
     	$(".register-list").sortable({
 			'distance':20
