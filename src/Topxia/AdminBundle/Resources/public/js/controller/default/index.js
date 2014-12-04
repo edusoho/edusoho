c339a25ca767f5f886e79201770f5896d7613957
Wellming Li <wellming.li@howzhi.com>
2014-12-04 15:29:49 +0800

    fix 后台登录慢的问题，是因为ajax同时提交，造成session竞争，卡住了

--- src/Topxia/AdminBundle/Resources/public/js/controller/default/index.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/default/index.js
@@ -12,43 +12,44 @@ define(function(require, exports, module) {
             });
         });
 
+        step1();
+
+    };
+
+    function step1() {
         $("#popular-courses-type").on('change', function() {
             $.get($(this).data('url'), {dateType: this.value}, function(html) {
                 $('#popular-courses-table').html(html);
+                step2();
             });
         }).trigger('change');
+    }
 
-
-/*        var $alert = $("#app-upgrade-alert");
-        $.post($alert.data('url'), function(result) {
-            var count = parseInt(result);
-            if (count == 0) {
-                return ;
-            }
-            var html = "<a href='" + $alert.data('upgradeUrl') + "'>亲爱的用户，系统现在有 <span class='badge'>" + count + " </span> 个更新,请及时去应用中心检查查看，体验最新的功能和改进。</a>";
-            $alert.append(html);
-            $alert.removeClass('hide');
-        });*/
-
+    function step2() {
         $.post($('#operation-analysis-title').data('url'),function(html){
             $('#operation-analysis-table').html(html);
+            step3();
         });
+    }
 
-/*        $.post($('#open-message-title').data('url'),function(html){
-            $('#edusoho-open-message').html(html);
-        });*/
-
+    function step3() {
         $.post($('#system-status-title').data('url'),function(html){
             $('#system-status').html(html);
+            step4();
         });
+    }
 
+    function step4() {
         $.post($('#onlineNum').data('url'),function(res){
             $('#onlineNum').html("当前在线："+res.onlineCount+"人");
+            step5();
         });
+    }
 
+    function step5() {
         $.post($('#loginNum').data('url'),function(res){
             $('#loginNum').html("登录人数："+res.loginCount+"人");
         });
-    };
+    }
 
 });
