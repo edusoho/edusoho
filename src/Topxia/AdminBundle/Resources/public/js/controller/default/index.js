e0af2c5fe837b21fde97327b54966c4a1f48c905
nifeng <nifeng@howzhi.com>
2014-12-04 21:00:29 +0800

    revert file

--- src/Topxia/AdminBundle/Resources/public/js/controller/default/index.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/default/index.js
@@ -11,68 +11,44 @@ define(function(require, exports, module) {
                 Notify.success('提醒教师的通知，发送成功！');
             });
         });
-        $("#popular-courses-type").on('change', step1);
 
-        step1();
-        step2();
-        step3();
-        step4();
-        step5();
-    };
-
-    function step1() {
-        $.ajax({
-            url: $(this).data('url'), 
-            data: {dateType: this.value}, 
-            async : false, 
-            success: function(html) {
+        $("#popular-courses-type").on('change', function() {
+            $.get($(this).data('url'), {dateType: this.value}, function(html) {
                 $('#popular-courses-table').html(html);
-            }
-        });
-    }
+            });
+        }).trigger('change');
 
-    function step2() {
-        $.ajax({
-            url: $('#operation-analysis-title').data('url'),
-            async : false,
-            type : "post",
-            success: function(html){
-                $('#operation-analysis-table').html(html);
-            }
-        });
-    }
 
-    function step3() {
-        $.ajax({
-            url: $('#system-status-title').data('url'),
-            type : "post",
-            async : false,
-            success: function(html){
-                $('#system-status').html(html);
+/*        var $alert = $("#app-upgrade-alert");
+        $.post($alert.data('url'), function(result) {
+            var count = parseInt(result);
+            if (count == 0) {
+                return ;
             }
-        });
-    }
+            var html = "<a href='" + $alert.data('upgradeUrl') + "'>亲爱的用户，系统现在有 <span class='badge'>" + count + " </span> 个更新,请及时去应用中心检查查看，体验最新的功能和改进。</a>";
+            $alert.append(html);
+            $alert.removeClass('hide');
+        });*/
 
-    function step4() {
-        $.ajax({
-            url: $('#onlineNum').data('url'),
-            type : "post",
-            async : false,
-            success: function(res){
-                $('#onlineNum').html("当前在线："+res.onlineCount+"人");
-            }
+        $.post($('#operation-analysis-title').data('url'),function(html){
+            $('#operation-analysis-table').html(html);
         });
-    }
 
-    function step5() {
-        $.ajax({
-            url: $('#loginNum').data('url'),
-            type : "post",
-            async : false,
-            success: function(res){
-                $('#loginNum').html("登录人数："+res.loginCount+"人");
-            }
+/*        $.post($('#open-message-title').data('url'),function(html){
+            $('#edusoho-open-message').html(html);
+        });*/
+
+        $.post($('#system-status-title').data('url'),function(html){
+            $('#system-status').html(html);
         });
-    }
+
+        $.post($('#onlineNum').data('url'),function(res){
+            $('#onlineNum').html("当前在线："+res.onlineCount+"人");
+        });
+
+        $.post($('#loginNum').data('url'),function(res){
+            $('#loginNum').html("登录人数："+res.loginCount+"人");
+        });
+    };
 
 });
