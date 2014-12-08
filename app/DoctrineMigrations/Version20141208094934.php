462c0ecb481861b0d2fd7961b2c3f921f019e423
wangchao <wangchao@howzhi.com>
2014-12-08 16:29:00 +0800

    start add questions

--- app/DoctrineMigrations/Version20141208094934.php
+++ app/DoctrineMigrations/Version20141208094934.php
@@ -15,12 +15,50 @@ class Version20141208094934 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
     	$this->addSql("	
 			ALTER table `user` 
-			Add column `payPassword` varchar(64) NOT NULL AFTER `password`;
+			Add column `payPassword` varchar(64) NOT NULL AFTER `salt`;
     	");     	
         $this->addSql(" 
             ALTER table `user` 
-            Add column `payPasswordSalt` varchar(64) NOT NULL AFTER `password`;
-        ");         
+            Add column `payPasswordSalt` varchar(64) NOT NULL AFTER `salt`;
+        ");  
+
+        
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `question1` varchar(64) NOT NULL AFTER `salt`;
+        ");  
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `answer1` varchar(64) NOT NULL AFTER `salt`;
+        ");   
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `answerSalt1` varchar(64) NOT NULL AFTER `salt`;
+        ");     
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `question2` varchar(64) NOT NULL AFTER `salt`;
+        ");  
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `answer2` varchar(64) NOT NULL AFTER `salt`;
+        ");   
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `answerSalt2` varchar(64) NOT NULL AFTER `salt`;
+        ");  
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `question3` varchar(64) NOT NULL AFTER `salt`;
+        ");  
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `answer3` varchar(64) NOT NULL AFTER `salt`;
+        ");   
+        $this->addSql(" 
+            ALTER table `user` 
+            Add column `answerSalt3` varchar(64) NOT NULL AFTER `salt`;
+        ");                                                
     }
 
     public function down(Schema $schema)
