7cc8aea3da6a3783d6d63e6373feba44d9259460
Jiajun Xu <xujiajun@howzhi.com>
2014-12-09 10:03:34 +0800

    fix bug

--- src/Custom/WebBundle/Controller/CourseLessonManageController.php
+++ src/Custom/WebBundle/Controller/CourseLessonManageController.php
@@ -12,9 +12,8 @@ class CourseLessonManageController extends BaseController
         $course = $this->getCourseService()->tryManageCourse($id);
 
         $categoryId = $course['subjectIds'];
-        //test
-        $categoryId = 4;
-        $category = $this->getCategoryService()->getCategory($categoryId);
+
+        $category = $this->getCategoryService()->getCategory($categoryId[0]);
 
         if ($request->getMethod() == "POST") {
             $formData = $request->request->all();
@@ -31,9 +30,27 @@ class CourseLessonManageController extends BaseController
         ));
     }
 
-    public function FunctionName($value='')
+    public function editAction(Request $Request,$courseId,$lessonId)
     {
-        # code...
+        $course = $this->getCourseService()->tryManageCourse($courseId);
+        $lesson = $this->getCourseService()->getCourseLesson($course['id'], $lessonId);
+        if (empty($lesson)) {
+            throw $this->createNotFoundException("课时(#{$lessonId})不存在！");
+        }
+
+        if($request->getMethod() == 'POST'){
+
+            return $this->render('TopxiaWebBundle:CourseLessonManage:list-item.html.twig', array(
+                'course' => $course,
+                'lesson' => $lesson
+            ));
+        }
+
+        return $this->render('CustomWebBundle:CourseLessonManage:lesson-modal.html.twig', array(
+            'course' => $course,
+            'category' => $category,
+            'lesson' => $lesson
+        ));
     }
 
     private function getCategoryService()
