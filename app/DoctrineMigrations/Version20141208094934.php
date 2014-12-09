4797e7b04395b487cf85f8483924013d6539bf75
wangchao <wangchao@howzhi.com>
2014-12-09 10:12:50 +0800

    create user_secure_question

--- app/DoctrineMigrations/Version20141208094934.php
+++ app/DoctrineMigrations/Version20141208094934.php
@@ -43,44 +43,6 @@ class Version20141208094934 extends AbstractMigration
     }
 
 
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityQuestion1` varchar(64) NOT NULL AFTER `salt`;
-        ");  
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityAnswer1` varchar(64) NOT NULL AFTER `salt`;
-        ");   
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityAnswerSalt1` varchar(64) NOT NULL AFTER `salt`;
-        ");     
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityQuestion2` varchar(64) NOT NULL AFTER `salt`;
-        ");  
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityAnswer2` varchar(64) NOT NULL AFTER `salt`;
-        ");   
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityAnswerSalt2` varchar(64) NOT NULL AFTER `salt`;
-        ");  
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityQuestion3` varchar(64) NOT NULL AFTER `salt`;
-        ");  
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityAnswer3` varchar(64) NOT NULL AFTER `salt`;
-        ");   
-        $this->addSql(" 
-            ALTER table `user` 
-            Add column `securityAnswerSalt3` varchar(64) NOT NULL AFTER `salt`;
-        ");                                                
-    }
-
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
