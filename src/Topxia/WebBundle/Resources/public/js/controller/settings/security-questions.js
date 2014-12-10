5edb1764f612c568134ec341e7b935fecb23aa70
wangchao <wangchao@howzhi.com>
2014-12-10 15:02:26 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/settings/security-questions.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/settings/security-questions.js
@@ -31,7 +31,23 @@ define(function(require, exports, module) {
             rule: 'maxlength{max:20}'
         });
 
+
+
+        $('option[value=parents]').css('display', 'none');
+        $('option[value=teacher]').css('display', 'none');
+        $('option[value=lover]').css('display', 'none');
+
+
         var q1 = $('[name=question-1]'), q2 = $('[name=question-2]'), q3 = $('[name=question-3]');
+        var reflesh_option_display = function(){
+            var questions = new Array('parents' , 'teacher' , 'lover' , 'schoolName' , 'firstTeacher' , 'hobby' , 'notSelected' ), questionId;
+            for (questionId in questions){
+                if (questions[questionId] !== q1.val() && questions[questionId] !== q2.val() && questions[questionId] !== q3.val()){
+                    $('option[value='+questions[questionId]+']').css('display', 'block');
+                }
+            }
+        }
+
         q1.change(function(){ 
             if (q1.val() === q2.val() || q3.val() === q2.val() || q1.val() === q3.val()){
                 alert('问题类型不能重复')
@@ -41,7 +57,7 @@ define(function(require, exports, module) {
             } else {
                 $('option[value='+q1.val()+']').css('display', 'none');
             }
-
+            reflesh_option_display();
          });
 
         q2.change(function(){ 
@@ -53,6 +69,7 @@ define(function(require, exports, module) {
             } else {       
                 $('option[value='+q2.val()+']').css('display', 'none');
             }
+            reflesh_option_display();
          });
 
         q3.change(function(){ 
@@ -64,6 +81,7 @@ define(function(require, exports, module) {
             } else {            
                 $('option[value='+q3.val()+']').css('display', 'none');
             }
+            reflesh_option_display();
          });
     };
 
