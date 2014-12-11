9e50ba07950a4b1aaca4503fbb7126f2b05ca122
nifeng <nifeng@howzhi.com>
2014-12-11 21:15:58 +0800

    fix

--- src/Topxia/Service/Course/Impl/CourseCopyServiceImpl.php
+++ src/Topxia/Service/Course/Impl/CourseCopyServiceImpl.php
@@ -152,7 +152,11 @@ class CourseCopyServiceImpl extends BaseService implements CourseCopyService
 
             $fields['createdTime'] = time();
 
-            $map[$lesson['id']] = $this->getLessonDao()->addLesson($fields);
+            $copiedLesson = $this->getLessonDao()->addLesson($fields);
+            $map[$lesson['id']] = $copiedLesson;
+            if(array_key_exists("mediaId", $copiedLesson) && $copiedLesson["mediaId"]>0 && in_array($copiedLesson["type"], array('video', 'audio','ppt'))) {
+                $this->getUploadFileDao()->updateFileUsedCount(array($copiedLesson["mediaId"]), 1);
+            }
         }
 
         return $map;
@@ -224,7 +228,12 @@ class CourseCopyServiceImpl extends BaseService implements CourseCopyService
 
             $fields['createdTime'] = time();
 
-            $map[$material['id']] = $this->getMaterialDao()->addMaterial($fields);
+            $copiedMaterial = $this->getMaterialDao()->addMaterial($fields);
+            $map[$material['id']] = $copiedMaterial;
+
+            if(array_key_exists("fileId", $copiedMaterial) && $copiedMaterial["fileId"]>0 ) {
+                $this->getUploadFileDao()->updateFileUsedCount(array($copiedMaterial["fileId"]), 1);
+            }
         }
 
         return $map;
@@ -295,6 +304,11 @@ class CourseCopyServiceImpl extends BaseService implements CourseCopyService
         return $map;
     }
 
+    public function getUploadFileDao()
+    {
+        return $this->createDao('File.UploadFileDao');
+    }
+
     public function getCourseMemberDao()
     {
         return $this->createDao('Course.CourseMemberDao');
@@ -313,11 +327,6 @@ class CourseCopyServiceImpl extends BaseService implements CourseCopyService
     public function getQuestionDao()
     {
         return $this->createDao('Question.QuestionDao');
-    }
-
-    public function getUploadFileDao()
-    {
-        return $this->createDao('File.UploadFileDao');
     }
 
     public function getCourseChapterDao()
