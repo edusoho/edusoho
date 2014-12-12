e6d3731f27f615c1f266d5ab90063132e5578edd
ruanwenqin <452253895@qq.com>
2014-12-12 16:49:56 +0800

    disabled enter submit

--- src/Custom/WebBundle/Resources/public/js/controller/coursewareexercise/add-modal.js
+++ src/Custom/WebBundle/Resources/public/js/controller/coursewareexercise/add-modal.js
@@ -37,6 +37,13 @@ define(function(require, exports, module) {
             $("#search-form").find('input[name=tagIds]').val(tagIds.join(','));
         });
 
+        $('#search-form').on("keyup keypress", function(e) {
+          var code = e.keyCode || e.which; 
+          if (code  == 13) {               
+            e.preventDefault();
+            return false;
+          }
+        });
     };
 
 });
--- src/Custom/WebBundle/Resources/public/js/controller/subcoursemanage/add-modal.js
+++ src/Custom/WebBundle/Resources/public/js/controller/subcoursemanage/add-modal.js
@@ -32,6 +32,13 @@ define(function(require, exports, module) {
 
         });
 
+        $('#search-form').on("keyup keypress", function(e) {
+          var code = e.keyCode || e.which; 
+          if (code  == 13) {               
+            e.preventDefault();
+            return false;
+          }
+        });
     };
 
 });
