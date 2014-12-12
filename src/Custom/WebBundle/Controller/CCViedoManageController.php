53b2c687d8cc29c2d30a373ac82a123e571e0c3d
shenjian <shenjian@howzhi.com>
2014-12-12 13:13:45 +0800

    fix

--- src/Custom/WebBundle/Controller/CCViedoManageController.php
+++ src/Custom/WebBundle/Controller/CCViedoManageController.php
@@ -21,6 +21,67 @@ class CCViedoManageController extends BaseController
         ));
     }
 
+    public function questionPreviewAction(Request $request, $questionId)
+    {
+        $question = $this->getQuestionService()->getQuestion($questionId);
+
+        if (empty($question)) {
+            throw $this->createNotFoundException('题目不存在！');
+        }
+
+        $item = array(
+            'questionId' => $question['id'],
+            'questionType' => $question['type'],
+            'question' => $question
+        );
+
+        if ($question['subCount'] > 0) {
+            $questions = $this->getQuestionService()->findQuestionsByParentId($id);
+
+            foreach ($questions as $value) {
+                $items[] = array(
+                    'questionId' => $value['id'],
+                    'questionType' => $value['type'],
+                    'question' => $value
+                );
+            }
+
+            $item['items'] = $items;
+        }
+
+        $type = in_array($question['type'], array('single_choice', 'uncertain_choice')) ? 'choice' : $question['type'];
+
+        $mainKnowledge = $this->getKnowledgeService()->getKnowledge($question['mainKnowledgeId']);
+        $tags = $this->getTagService()->findTagsByIds($question['tagIds']);
+        $relatedKnowledges = $this->getKnowledgeService()->findKnowledgeByIds($question['relatedKnowledgeIds']);
+
+        return $this->render('CustomWebBundle:CCViedoManage:question-preview-modal.html.twig', array(
+            'item' => $item,
+            'type' => $type,
+            'mainKnowledge' => $mainKnowledge,
+            'relatedKnowledges' => $relatedKnowledges,
+            'tags' => $tags,
+        ));
+    }
+
+    public function answerCheckAction(Request $request, $questionId)
+    {
+        $answer = $request->request->all();
+        if(empty($answer)){
+            $result = array('answer' => 'false', 'message'=>'没有答案');
+        }
+        $question = $this->getQuestionService()->getQuestion($questionId);
+        $trueAnswer =json_decode($question['answer']);
+
+        if ($answer == $trueAnswer){
+            $result = array('answer' => 'true', 'message'=>'回答正确');
+        } else {
+            $result = array('answer' => 'false', 'message'=>'回答错误');
+        }
+
+        return $this->createJsonResponse($result);
+    }
+
     private function getCoursewareService()
     {
         return $this->getServiceKernel()->createService('Courseware.CoursewareService');
