ec43dad630cfb60091caeef5f02122280b162e99
lvliujie <lvliujie@howzhi.com>
2014-12-16 17:14:42 +0800

    lesson preview

--- src/Custom/WebBundle/Controller/CourseController.php
+++ src/Custom/WebBundle/Controller/CourseController.php
@@ -192,6 +192,135 @@ class CourseController extends BaseController
 		));
 	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public function previewAction(Request $request,$id)
+	{
+		$course = $this->getCourseService()->getCourse($id);
+        $user = $this->getCurrentUser();
+		$lessons=$this->getCourseService()->getCourseLessons($id);
+		$lesson=array();
+		foreach ($lessons as $le) {
+			if ($le['type']=='video' && empty($lesson)) {
+				$lesson=$le;
+			}
+		}
+
+        if (empty($lesson)) {
+            throw $this->createNotFoundException();
+        }
+
+        if (!empty($course['status']) && $course['status'] == 'closed') {
+            return $this->render('TopxiaWebBundle:CourseLesson:preview-notice-modal.html.twig',array('course' => $course));
+        }
+
+        if (empty($lesson['free'])) {
+            if (!$user->isLogin()) {
+                throw $this->createAccessDeniedException();
+            }
+            return $this->forward('TopxiaWebBundle:CourseOrder:buy', array('id' => $id), array('preview' => true));
+        }else{
+            $allowAnonymousPreview = $this->setting('course.allowAnonymousPreview', 1);
+            if (empty($allowAnonymousPreview) && !$user->isLogin()) {
+                throw $this->createAccessDeniedException();
+            }
+        }
+
+        if ($lesson['type'] == 'video' and $lesson['mediaSource'] == 'self') {
+            $file = $this->getUploadFileService()->getFile($lesson['mediaId']);
+            if (!empty($file['metas2']) && !empty($file['metas2']['sd']['key'])) {
+                $factory = new CloudClientFactory();
+                $client = $factory->createClient();
+                $hls = $client->generateHLSQualitiyListUrl($file['metas2'], 3600);
+
+                if (isset($file['convertParams']['convertor']) && ($file['convertParams']['convertor'] == 'HLSEncryptedVideo')) {
+                    $token = $this->getTokenService()->makeToken('hlsvideo.view', array('data' => $lessonId, 'times' => 1, 'duration' => 3600));
+                    $hlsKeyUrl = $this->generateUrl('course_lesson_hlskeyurl', array('courseId' => $lesson['courseId'], 'lessonId' => $lesson['id'], 'token' => $token['token']), true);
+                    $headLeaderInfo = $this->getHeadLeaderInfo();
+                    $hls = $client->generateHLSEncryptedListUrl($file['convertParams'], $file['metas2'], $hlsKeyUrl, $headLeaderInfo['headLeaders'], $headLeaderInfo['headLeaderHlsKeyUrl'], 3600);
+                } else {
+                    $hls = $client->generateHLSQualitiyListUrl($file['metas2'], 3600);
+                }
+
+            }
+
+        } else if ($lesson['mediaSource'] == 'youku') {
+            $matched = preg_match('/\/sid\/(.*?)\/v\.swf/s', $lesson['mediaUri'], $matches);
+            if ($matched) {
+                $lesson['mediaUri'] = "http://player.youku.com/embed/{$matches[1]}";
+                $lesson['mediaSource'] = 'iframe';
+            } else {
+                $lesson['mediaUri'] = $lesson['mediaUri'];
+            }
+        } else if ($lesson['mediaSource'] == 'tudou'){
+            $matched = preg_match('/\/v\/(.*?)\/v\.swf/s', $lesson['mediaUri'], $matches);
+            if ($matched) {
+                $lesson['mediaUri'] = "http://www.tudou.com/programs/view/html5embed.action?code={$matches[1]}";
+                $lesson['mediaSource'] = 'iframe';
+            } else {
+                $lesson['mediaUri'] = $lesson['mediaUri'];
+            }
+        }
+        return $this->render('TopxiaWebBundle:Course:lesson-preview.html.twig', array(
+            'user' => $user,
+            'course' => $course,
+            'lesson' => $lesson,
+            'hlsUrl' => (isset($hls) and is_array($hls) and !empty($hls['url'])) ? $hls['url'] : '',
+        ));
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public function headerAction(Request $request,$id)
 	{
 		$course=$this->getCourseService()->getCourse($id);
@@ -218,7 +347,7 @@ class CourseController extends BaseController
 			'member' => $member,
 			'hasFavorited' => $hasFavorited,
 			'courseMemberLevel' => $courseMemberLevel,
-			'checkMemberLevelResult' => $checkMemberLevelResult,
+			'checkMemberLevelResult' => $checkMemberLevelResult
 		));
 	}
 
