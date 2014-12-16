0a67807ce3b06abec281741ca13212ef07d221c4
ruanwenqin <452253895@qq.com>
2014-12-16 11:17:41 +0800

    hidden node and expand node

--- src/Topxia/AdminBundle/Resources/public/js/controller/knowledge/main.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/knowledge/main.js
@@ -35,12 +35,21 @@ define(function(require, exports, module) {
 				}
 			},
 			callback: {
-				//beforeRemove: beforeRemove,
-				//beforeEditName: beforeEditName,
+				onAsyncSuccess: onAsyncSuccess,
 				onDrop: onDrop
 			}
 		};
 
+		function onAsyncSuccess(event, treeId, treeNode, msg) {
+			if(typeof(treeNode) == "undefined" ) {
+				var zTree = $.fn.zTree.getZTreeObj(treeId);
+				var nodes = zTree.getNodes();
+				for(var i=0; i< nodes.length;i++) {
+					zTree.expandNode(nodes[i],true, false, false);
+				}
+			}
+		}
+		
 		function addDiyDom(treeId, treeNode) {
 		    var html = '<div class="actions ">';
 		    html += '<button class="btn btn-link btn-sm" id="addBtn_'+treeNode.tId+'"><span class="glyphicon glyphicon-plus"></span> 添加子节点</button>';
@@ -95,57 +104,10 @@ define(function(require, exports, module) {
 					}
 		  		});
 	  		}
-			$('#knowledge-tree_1_switch').click();
-			$('#knowledge-tree_2_switch').click();
-/*	  		$('#'+ treeId).bind('hover', function(){
-	  			$(this).addClass('show').removeClass('hidden');
-	  		},function(){
-	  			$(this).addClass('hidden').removeClass('show');
-	  		});*/
 	  		
 		};
 
-		/*function filter(treeId, parentNode, childNodes) {
-			if (!childNodes) return null;
-			for (var i=0, l=childNodes.length; i<l; i++) {
-				childNodes[i].name = childNodes[i].name.replace(/\.n/g, '.');
-			}
-			return childNodes;
-		}
-		function beforeRemove(treeId, treeNode) {
-			var zTree = $.fn.zTree.getZTreeObj("knowledge-tree");
-			zTree.selectNode(treeNode);
-			if(confirm("确认删除 节点 -- " + treeNode.name + " 吗？")) {
-				var flag = false;
-				$.ajax({ 
-					type: 'POST', 
-					url: $tree.data('durl'), 
-					data: {id:treeNode.id}, 
-					success: function(result){
-                    	Notify.success('删除知识点成功！');
-                    	flag = true;
-					}, 
-					error:function() {
-                    	Notify.danger("删除知识点失败！");
-                    	flag = false;
-                	},
-					async:false 
-				});
-				return flag;
-			} else {
-				return false;
-			}
-		}		
-		function beforeEditName(treeId, treeNode, newName) {
-			var pid = treeNode.parentId,
-				cid = treeNode.categoryId,
-				id = treeNode.id,
-				newUrl = $('#edit-knowledge').data('turl')+'?id='+id+'&pid='+pid+'&cid='+cid+'&tid='+treeNode.tId;
-
-			$('#edit-knowledge').data('url', newUrl);
-			$('#edit-knowledge').click();
-			return false;
-		}*/
+	
 
 		function onDrop(event, treeId, treeNodes, targetNode, moveType, isCopy) {
 			var pid = treeNodes[0].pId,
