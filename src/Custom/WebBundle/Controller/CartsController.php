244861f64e7cf9fc04b34b4d2198c08a73e724e7
shenjian <shenjian@howzhi.com>
2014-12-17 15:16:50 +0800

    carts fnction

---
+++ src/Custom/WebBundle/Controller/CartsController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Custom\WebBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Topxia\WebBundle\Controller\BaseController;
+use Topxia\Common\ArrayToolkit;
+use Topxia\Common\Paginator;
+
+class CartsController extends BaseController
+{
+    public function showAction(Request $request)
+    {
+        # code...
+    }
+}
