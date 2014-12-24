788792634371c0588ab833d50ca52cb76fdbe418
wangchao <wangchao@howzhi.com>
2014-12-24 13:20:37 +0800

    fix

--- app/DoctrineMigrations/Version20141224130707.php
+++ app/DoctrineMigrations/Version20141224130707.php
@@ -13,6 +13,13 @@ class Version20141224130707 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+        INSERT INTO `block` (`userId`, `title`, `mode`, `content`, `code`) 
+        VALUES ('1', '我的账户Bar', 'html', 
+        '<br>\n<div class=\"col-md-12\">\n  
+        <a href=\"#\"><img src=\"http://open.edusoho.com/assets/img/edusoho-demo/vip-banner-1.jpg\" /></a>
+        <br>\n<br>\n</div>', 'bill_bar');
+        ");
 
     }
 
