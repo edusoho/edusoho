69c87c69b4df2989a1aab98ef7868464e84d943c
yangshuai <yangshuai@howzhi.com>
2014-12-24 09:27:42 +0800

    create table

--- src/Custom/sql/column_course_vote.sql
+++ src/Custom/sql/column_course_vote.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS `column_course_vote` (
   `specialColumnId` int(10) NOT NULL,
   `isShow` enum('active','none') DEFAULT NULL,
   `courseAName` varchar(1024) NOT NULL,
-  `courseACount` int(10) NOT NULL,
+  `courseACount` int(10) NOT NULL DEFAULT 0,
   `courseBName` varchar(1024) NOT NULL,
-  `courseBCount` int(10) NOT NULL,
-  `courseVoteCount` int(10) NOT NULL,
+  `courseBCount` int(10) NOT NULL DEFAULT 0,
+  `courseVoteCount` int(10) NOT NULL DEFAULT 0,
   `voteStartTime` int(10) unsigned NOT NULL,
   `voteEndTime` int(10) unsigned NOT NULL,
   `createdTime` int(10) unsigned NOT NULL
