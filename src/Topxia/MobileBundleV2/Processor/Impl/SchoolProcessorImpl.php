97dd18904061af42047523e73699d59298dedf7d
Su ju <suju@howzhi.com>
2014-12-25 09:38:29 +0800

    fix

--- src/Topxia/MobileBundleV2/Processor/Impl/SchoolProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/SchoolProcessorImpl.php
@@ -65,6 +65,9 @@ class SchoolProcessorImpl extends BaseProcessor implements SchoolProcessor {
     {
         $code = $this->request->get("code", "edusoho");
         $client = $this->request->get("client", "android");
+        return $this->controller->render('TopxiaMobileBundleV2:Content:download.html.twig', array(
+            'client' => $client
+        ));
         return $this->controller->redirect("http://www.edusoho.com/download/mobile?client={$client}&code=" . $code);
     }
 
