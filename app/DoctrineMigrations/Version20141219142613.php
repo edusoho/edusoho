06b74f211d8bb4f5b570ae797688d220d21aaca7
nifeng <nifeng@howzhi.com>
2014-12-27 22:32:37 +0800

    fix

--- app/DoctrineMigrations/Version20141219142613.php
+++ app/DoctrineMigrations/Version20141219142613.php
@@ -13,7 +13,7 @@ class Version20141219142613 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-    	$this->addSql("ALTER TABLE `cash_flow` ADD `cashType` ENUM('RMB','Coin') NOT NULL AFTER `amount`;");
+    	$this->addSql("ALTER TABLE `cash_flow` ADD `cashType` ENUM('RMB','Coin') NOT NULL DEFAULT 'Coin' AFTER `amount`;");
     }
 
     public function down(Schema $schema)
