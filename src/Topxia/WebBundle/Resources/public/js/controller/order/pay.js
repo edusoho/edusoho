99b28381f373f3e566e8cf41a433688de30e5dcb
nifeng <nifeng@howzhi.com>
2014-12-27 16:59:17 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/order/pay.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/order/pay.js
@@ -112,8 +112,16 @@ define(function(require, exports, module) {
 
 		$('[role="coinNum"]').blur(function(e){
 			var coinNum = $(this).val();
-			if(isNaN(coinNum)){
+			if(isNaN(coinNum) || coinNum<=0){
 				$(this).val(0);
+				coinPriceZero();
+			} else {
+				$(".pay-password div[role='password-input']").show();
+				validator.addItem({
+					element: '[name="payPassword"]',
+					required: true,
+        			rule: 'remote'
+				});
 			}
 			conculatePrice();
 
