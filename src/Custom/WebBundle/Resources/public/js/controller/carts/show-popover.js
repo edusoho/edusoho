b8661cae496e8d207b5aa960d376551f78d2a09f
shenjian <shenjian@howzhi.com>
2015-01-06 17:50:01 +0800

    fix

--- src/Custom/WebBundle/Resources/public/js/controller/carts/show-popover.js
+++ src/Custom/WebBundle/Resources/public/js/controller/carts/show-popover.js
@@ -2,16 +2,20 @@ define(function(require, exports, module) {
     var Notify = require('common/bootstrap-notify');
 
     exports.run = function() {
+                    console.log(Number($('#count').html()));
         $('.carts-popover-list').on('click','.carts-item-delete',function(){
             $btn = $(this);
             $.get($btn.data('url'),function(result){
                 if(result.status == 'success') {
-                    $btn.parents('li').remove();
-                    var count = Number($('#float-carts #carts-badge').html());
+                    var count = Number($('#count').html());
                     count == 1 && $('.btn-carts .btn').popover('hide');
-                    count > 0 && $('#float-carts #carts-badge').html(--count);
+                    count > 0 && $('#count').html(--count);
+
+                    var price = Number($('#price').html());
+                    price = $('#price').html(price - Number($btn.prev('div').find('.course-price').html()));
+
+                    $btn.parents('li').remove();
                 }
-         
             });
         });
     }
