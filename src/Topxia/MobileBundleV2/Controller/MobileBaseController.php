e077af43be2ffa038727694005b92939e7489bbd
Su ju <suju@howzhi.com>
2015-01-12 10:38:59 +0800

    fix teacher info

--- src/Topxia/MobileBundleV2/Controller/MobileBaseController.php
+++ src/Topxia/MobileBundleV2/Controller/MobileBaseController.php
@@ -294,7 +294,7 @@ class MobileBaseController extends BaseController
             }
             $userProfile = $controller->getUserService()->getUserProfile($user['id']);
             $user['signature'] = $userProfile['signature'];
-            $user['about'] = $userProfile['about'];
+            $user['about'] = $this->convertAbsoluteUrl($userProfile['about']);
             
             unset($user['password']);
             unset($user['salt']);
