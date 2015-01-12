cdb6c51e7c667080ba11fd6731700ef67179b9eb
cc <fucongcong@howzhi.com>
2015-01-12 10:12:12 +0800

    add tests

--- src/Topxia/DataTag/Test/DataTagTests.php
+++ src/Topxia/DataTag/Test/DataTagTests.php
@@ -1,6 +1,53 @@
 <?php
-use PHPUnit_Framework_TestCase;
 use Topxia\DataTag\HotGroupDataTag;
+use Topxia\DataTag\CategoriesDataTag;
+use Topxia\DataTag\CategoryAnnouncementDataTag;
+use Topxia\DataTag\CourseDataTag;
+use Topxia\DataTag\CategoryDataTag;
+use Topxia\DataTag\CourseLessonsDataTag;
+use Topxia\DataTag\CourseRankByHitDataTag;
+use Topxia\DataTag\CourseRankByRatingDataTag;
+use Topxia\DataTag\CourseRankByStudentDataTag;
+use Topxia\DataTag\CourseRelatedArticlesDataTag;
+use Topxia\DataTag\CourseReviewDataTag;
+use Topxia\DataTag\CourseThreadDataTag;
+use Topxia\DataTag\CourseThreadsByTypeDataTag;
+use Topxia\DataTag\CoursesCountDataTag;
+use Topxia\DataTag\EliteCourseThreadsByTypeDataTag;
+use Topxia\DataTag\ElitedCourseQuestionsDataTag;
+use Topxia\DataTag\ElitedCourseThreadsDataTag;
+use Topxia\DataTag\FreeCoursesDataTag;
+use Topxia\DataTag\FreeLessonsDataTag;
+use Topxia\DataTag\HotThreadsDataTag;
+use Topxia\DataTag\LatestArticlesDataTag;
+use Topxia\DataTag\LatestCourseMembers2DataTag;
+use Topxia\DataTag\LatestCourseMembersDataTag;
+use Topxia\DataTag\LatestCourseQuestionsDataTag;
+use Topxia\DataTag\LatestCourseReviewsDataTag;
+use Topxia\DataTag\LatestCourseThreadsByTypeDataTag;
+use Topxia\DataTag\LatestCourseThreadsDataTag;
+use Topxia\DataTag\LatestCoursesDataTag;
+use Topxia\DataTag\LatestFinishedLearnsDataTag;
+use Topxia\DataTag\LatestLoginUsersDataTag;
+use Topxia\DataTag\LatestTeachersDataTag;
+use Topxia\DataTag\LatestUsersDataTag;
+use Topxia\DataTag\NavigationDataTag;
+use Topxia\DataTag\PersonDynamicDataTag;
+use Topxia\DataTag\PopularCoursesByCategoryDataTag;
+use Topxia\DataTag\PopularCoursesDataTag;
+use Topxia\DataTag\PromotedTeacherDataTag;
+use Topxia\DataTag\RecentLiveCoursesDataTag;
+use Topxia\DataTag\RecommendCoursesDataTag;
+use Topxia\DataTag\RecommendTeachersDataTag;
+use Topxia\DataTag\TagsCoursesDataTag;
+use Topxia\DataTag\TagsDataTag;
+use Topxia\DataTag\TeacherCoursesDataTag;
+use Topxia\DataTag\TopRatingCourseReviewsDataTag;
+use Topxia\DataTag\UserDataTag;
+use Topxia\DataTag\UserLatestLearnCoursesDataTag;
+use Topxia\DataTag\UserandProfilesDataTag;
+use Topxia\DataTag\VipLevelsDataTag;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 use Topxia\Service\Common\ServiceKernel;
@@ -8,15 +55,114 @@ use Topxia\Service\User\CurrentUser;
 
 class DataTagTests extends PHPUnit_Framework_TestCase
 {
-    public function testHotGroupDataTag()
+    public function testCategoriesDataTag()
     {
-        $Group = new HotGroupDataTag(1);
+        $Categories = new CategoriesDataTag();
 
-        $groups = $Group->getData(array('count'=>5));
-    
-        $this->assertEquals(5, count($groups));
+        $categories = $Categories->getData(array('group'=>'course','parentId'=>"test"));
+
+     /*   print_r($categories);*/
     }
 
+    public function testCategoryAnnouncementDataTag()
+    {
+        $Announcement = new CategoryAnnouncementDataTag();
+
+        $announcements = $Announcement->getData(array('count'=>'5','categoryId'=>"1"));
+
+    /*        print_r($announcements);*/
+    }
+
+    public function testCategoryDataTag()
+    {
+        $Category = new CategoryDataTag();
+
+        $data = $Category->getData(array('categoryId'=>"1"));
+
+    /*         print_r($data);*/
+    }
+
+    public function testCourseDataTag()
+    {
+        $Course = new CourseDataTag();
+
+        $data = $Course->getData(array('courseId'=>"1"));
+
+/*             print_r($data);*/
+    }
+
+    public function testCourseLessonsDataTag()
+    {
+        $CourseLessons = new CourseLessonsDataTag();
+
+        $data = $CourseLessons->getData(array('courseId'=>"1",'count'=>5));
+
+/*             print_r($data);*/
+    }
+
+    public function testCourseRankByHitDataTag()
+    {
+        $CourseRank = new CourseRankByHitDataTag();
+
+        $data = $CourseRank->getData(array('count'=>5));
+
+/*             print_r($data);*/
+    }
+
+    public function testCourseRankByRatingDataTag()
+    {
+        $CourseRank = new CourseRankByRatingDataTag();
+
+        $data = $CourseRank->getData(array('count'=>5));
+
+/*             print_r($data);*/
+    }
+
+    public function testCourseRankByStudentDataTag()
+    {
+        $CourseRank = new CourseRankByStudentDataTag();
+
+        $data = $CourseRank->getData(array('count'=>5));
+
+/*             print_r($data);*/
+    }
+
+    public function testCourseRelatedArticlesDataTag()
+    {
+        $Article = new CourseRelatedArticlesDataTag();
+
+        $data = $Article->getData(array('count'=>5,'courseId'=>1));
+
+/*             print_r($data);*/
+    }
+
+    public function testCourseReviewDataTag()
+    {
+        $CourseReview = new CourseReviewDataTag();
+
+        $data = $CourseReview->getData(array('count'=>5,'reviewId'=>1));
+/*
+             print_r($data);*/
+    }
+
+    public function testCourseThreadDataTag()
+    {
+        $CourseThread = new CourseThreadDataTag();
+
+        $data = $CourseThread->getData(array('count'=>5,'reviewId'=>1));
+/*
+             print_r($data);*/
+    }
+
+    public function testHotGroupDataTag()
+    {
+        $Group = new HotGroupDataTag();
+
+        $groups = $Group->getData(array('count'=>5));
+
+    /*        print_r($groups);*/
+
+    }
 
     public function __construct()
     {
