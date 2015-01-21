580a56695f0ba55953b54ab53bac812af3b2485f
Jiajun Xu <xujiajun@howzhi.com>
2015-01-21 16:52:43 +0800

    fix bug && refactor

--- src/Custom/WebBundle/Controller/OrderController.php
+++ src/Custom/WebBundle/Controller/OrderController.php
@@ -13,12 +13,12 @@ class OrderController extends BaseController
     {
         $user = $this->getCurrentUser();
 
-
         if (!$user->isLogin()) {
             return $this->createMessageResponse('info', '你好像忘了登录哦？', null, 3000, $this->generateUrl('login'));
         }
 
         $data = $request->query->all();
+
         $profile = $this->getUserService()->getUserProfile($user['id']);
         $invoices = $this->getUserInvoiceService()->findUserInvoicesByUserId($user['id']);
 
@@ -26,12 +26,16 @@ class OrderController extends BaseController
         $userId = $this->getCurrentUser()->id;
         list($groupCarts, $itemResult) = $this->getCartsService()->findCurrentUserCarts();
 
+        $cartIds = $request->request->get('cartIds',array());
+        if (!empty($cartIds)) {
+            $groupCarts = $this->filterGroupCarts($groupCarts,$cartIds);
+        }
+
         $type = '';
         $targetId = '';
         $referer = !empty($data['referer']) ? $data['referer'] : 'cartsAction';
 
-        if (!empty($data['referer']) && $referer == 'showAction') {
-
+        if ($referer == 'showAction') {
             $groupCarts = array();
             $targetType = !empty($data['targetType']) ? $data['targetType'] : 'course';
             $targetId = !empty($data['targetId']) ? $data['targetId'] : '';
@@ -42,20 +46,7 @@ class OrderController extends BaseController
 
             $type = 'course';
 
-            $course = $this->getCourseService()->getCourse($targetId);
-            $itemResult = array();
-            $itemResult['course']['items'][$targetId] = $course;
-
-            $teacherIds = array_values(array_unique($course['teacherIds']));
-            $teachers = $this->getUserService()->findUsersByIds($teacherIds);
-            $itemResult['course']['extra']['users'] = $teachers;
-
-            $subcourses = $this->getCourseService()->findSubcoursesByCourseId($targetId);
-            $subcourses = $subcourses[1];
-            $itemResult[$targetId]['subcourses'] = $subcourses;
-
-            $totalPrice = $this->getCourseTotalPrice($subcourses);
-            $itemResult['course']['items'][$targetId]['costPrice'] = $totalPrice;
+            $itemResult = $this->getItemResultBytargetId($targetId);
         }
 
         return $this->render('CustomWebBundle:Order:order-verify.html.twig',array(
@@ -67,7 +58,8 @@ class OrderController extends BaseController
             'role' => 'verify',
             'referer' => $referer,
             'type' => $type,
-            'targetId' => $targetId
+            'targetId' => $targetId,
+            'cartIds' => $cartIds
         ));
     }
 
@@ -82,15 +74,58 @@ class OrderController extends BaseController
         ));
     }
     
-    private function getCourseTotalPrice($courses)
+    private function getItemResultBytargetId($targetId)
+    {
+        $course = $this->getCourseService()->getCourse($targetId);
+        $itemResult = array();
+        $itemResult['course']['items'][$targetId] = $course;
+
+        $subcourses = $this->getCourseService()->findSubcoursesByCourseId($targetId);
+        $subcourses = $subcourses[1];
+        $itemResult[$targetId]['subcourses'] = $subcourses;
+
+        list($totalPrice,$teacherIds) = $this->getCourseTotalPriceAndUserIds($course,$subcourses);
+        $teacherIds = array_values(array_unique($teacherIds));
+        $teachers = $this->getUserService()->findUsersByIds($teacherIds);
+        $itemResult['course']['extra']['users'] = $teachers;
+        $itemResult['course']['items'][$targetId]['costPrice'] = $totalPrice;
+
+        return $itemResult;
+    }
+
+    private function filterGroupCarts($groupCarts,$cartIds)
+    {
+        $groupCarts = ArrayToolkit::index($groupCarts['course'],'id');
+        $groupCartIds = ArrayToolkit::column($groupCarts,'id');
+        $cartIds = explode(',', $cartIds);
+
+        $newGroupCarts = array();
+        foreach ($groupCartIds as $key => $groupCartId) {
+            if (!in_array($groupCartId, $cartIds)) {
+                unset($groupCarts[$groupCartId]);
+            }
+        }
+        $newGroupCarts['course'] = $groupCarts;
+
+        return $newGroupCarts;
+    }
+
+    private function getCourseTotalPriceAndUserIds($course,$subcourses)
     {
         $totalPrice = 0;
+        $teacherIds = array();
 
-        foreach ($courses as $key => $course) {
-            $totalPrice += floatval($course['price']);
+        foreach ($subcourses as $key => $subcourse) {
+            $totalPrice += floatval($subcourse['price']);
+            $teacherIds = array_merge($teacherIds,$subcourse['teacherIds']);
         }
 
-        return $totalPrice;
+        $teacherIds = array_merge($teacherIds,$course['teacherIds']);
+
+        return array(
+            $totalPrice,
+            $teacherIds
+        );
     }
 
     private function getCartsService()
