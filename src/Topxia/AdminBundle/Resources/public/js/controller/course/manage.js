9fed0d4f8b5c22caac27f59a7291f1a8f206be1d
wulianming <wulianming@howzhi.com>
2015-01-21 14:55:09 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/course/manage.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/course/manage.js
@@ -13,7 +13,8 @@ define(function(require, exports, module) {
 		});
 
 		$table.on('click', '.close-course', function(){
-			if (!confirm('您确认要关闭此课程吗？课程关闭后，仍然还在有效期内的学员，将可以继续学习。')) return false;
+			var user_name = $(this).data('user') ;
+			if (!confirm('您确认要关闭此课程吗？课程关闭后，仍然还在有效期内的'+user_name+'，将可以继续学习。')) return false;
 			$.post($(this).data('url'), function(html){
 				var $tr = $(html);
 				$table.find('#' + $tr.attr('id')).replaceWith(html);
@@ -33,7 +34,8 @@ define(function(require, exports, module) {
 		$table.on('click', '.delete-course', function() {
 			var chapter_name = $(this).data('charpter') ;
 			var part_name = $(this).data('part') ;
-			if (!confirm('删除课程，将删除课程的'+chapter_name+''+part_name+'、课时、学员信息。真的要删除该课程吗？')) {
+			var user_name = $(this).data('user') ;
+			if (!confirm('删除课程，将删除课程的'+chapter_name+''+part_name+'、课时、'+user_name+'信息。真的要删除该课程吗？')) {
 				return ;
 			}
 
