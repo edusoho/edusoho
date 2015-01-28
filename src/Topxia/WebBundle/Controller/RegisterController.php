dd721eea4f3d01855483f4499ddf600b627b37f3
wangchao <wangchao@howzhi.com>
2015-01-28 13:03:48 +0800

    fix

--- src/Topxia/WebBundle/Controller/RegisterController.php
+++ src/Topxia/WebBundle/Controller/RegisterController.php
@@ -234,9 +234,6 @@ class RegisterController extends BaseController
             return $this->createJsonResponse(true);
         }
 
-        $this->getUserService()->setupAccount($user['id']);
-        $this->authenticateUser($user);
-
         return $this->render('TopxiaWebBundle:Register:email-verify-success.html.twig', array(
             'token' => $token,
         ));
