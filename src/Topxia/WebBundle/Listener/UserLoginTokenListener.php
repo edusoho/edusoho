3a12d1e10df0aa10151964b83f0faaf80ef71577
wangchao <wangchao@howzhi.com>
2015-01-28 20:24:57 +0800

    fix

--- src/Topxia/WebBundle/Listener/UserLoginTokenListener.php
+++ src/Topxia/WebBundle/Listener/UserLoginTokenListener.php
@@ -46,7 +46,7 @@ class UserLoginTokenListener
         	&& $user['type'] == 'default'
         	&& $auth['email_enabled'] == 'opened'
             && $request->get('_route') != 'register_email_verify')
-        {
+        {var_dump($request->get('_route'));
                 $request->getSession()->invalidate();
                 $this->container->get("security.context")->setToken(null);
 
