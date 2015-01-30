2b7cd56514891b3d8c8d2209bba0b61f98dcd6c7
Su ju <suju@howzhi.com>
2015-01-30 12:48:19 +0800

    fix user notification

--- src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
@@ -216,7 +216,7 @@ class UserProcessorImpl extends BaseProcessor implements UserProcessor
             $content = $matches[2];
             $className = $matches[1];
             if ($className == "notification-footer") {
-                return "<p><font color=#CFCFCF>" . $content . "</font></p>";
+                return "<br><font color=#CFCFCF>" . $content . "</font>";
             }
             
         }, $message);
