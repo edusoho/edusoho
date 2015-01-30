1656ee8d02e3bce981dd12f0104708a9ef51a81b
Su ju <suju@howzhi.com>
2015-01-30 10:55:20 +0800

    fix user notification

--- src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
@@ -212,8 +212,12 @@ class UserProcessorImpl extends BaseProcessor implements UserProcessor
             "notification"=>$notification
             ))->getContent();
 
-        $message = preg_replace_callback('/<[\\/]?li[^>]*>/', function($matches) {
-            return "";
+        $message = preg_replace_callback('/<[\\/]?([\\w]+) class=\\"([\\w-]+)\\"[^>]*>/', function($matches) {
+            //var_dump($matches);
+            $className = $matches[2];
+            if ($className == "notification-footer") {
+                return "<div class=\"notification-footer\" style=''color:red;'>";
+            }
         }, $message);
 
         $message = str_replace("\n", "", $message);
