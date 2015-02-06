8da3b74b6b84800771156fcc26981066581ac984
wangchao <wangchao@howzhi.com>
2015-02-06 10:11:31 +0800

    fix

--- src/Topxia/AdminBundle/Controller/EduCloudController.php
+++ src/Topxia/AdminBundle/Controller/EduCloudController.php
@@ -13,6 +13,9 @@ use Topxia\Service\Util\CloudClientFactory;
 
 class EduCloudController extends BaseController
 {
+    private $cloudOptions = null;
+    private $cloudApi = null;
+
     public function indexAction(Request $request)
     {
         //8888888888
@@ -23,63 +26,15 @@ class EduCloudController extends BaseController
     public function smsAction(Request $request)
     {
         //8888888888
-        // depricated
-        // $factory = new CloudClientFactory();
-        // $client = $factory->createClient();
-        // var_dump($client->getBucket());
-        // var_dump($client->getBills($client->getBucket()));
-        // exit;
-
-    // return $this->render('TopxiaAdminBundle:Cloud:bill.html.twig', array(
-    //             'money' => $result['money'],
-    //             'bills' => $result['bills'],
-    //             'token' => $loginToken["token"]
-    //         ));
-
-      // <div class="well well-sm">
-      //   账户余额：<strong class="text-success">{{ money }}元</strong>&nbsp;&nbsp;&nbsp;&nbsp;
-      //   <a href="http://open.edusoho.com/token_login?token={{token}}&goto=order_recharge" target="_blank" class="btn btn-primary">充值</a>
-      //   <a href="http://open.edusoho.com/article/1#recharge" style="float:right;" target="_blank" class="btn btn-link">充值帮助</a>
-      // </div>
-
-
-        // $result = $client->getBills($client->getBucket());
-
-
-        $settings = $this->getServiceKernel()->createService('System.SettingService')->get('storage', array());
-        $options = array(
-            'accessKey' => empty($settings['cloud_access_key']) ? '' : $settings['cloud_access_key'],
-            'secretKey' => empty($settings['cloud_secret_key']) ? '' : $settings['cloud_secret_key'],
-            'apiUrl' => empty($settings['cloud_api_server']) ? '' : $settings['cloud_api_server'],
-        );
-
-        $api = $this->createAPIClient();
-
-        // $result = $api->get('/me');
-
-        //get bills
-        // $result = $api->get(sprintf('/accounts'));
-        $result = $api->get('/accounts');
-
-        //verify accesss key
-        // $result = $api->post(sprintf('/keys/%s/verification', $options['accessKey']));
-
-        //apply for sms service
-        // $result = $api->post(
-        //     sprintf('/sms/%s/apply', $options['accessKey']), 
-        //     $params=array('name'=>'Eschol8' ) );
-
-        //look for apply status
-        // $result = $api->post(
-        //     sprintf('/sms/%s/applyResult', $options['accessKey']) );
-
-        //send a sms
-        // $result = $api->post(
-        //     sprintf('/sms/%s/sendVerify', $options['accessKey']),
-        //     $params=array('mobile'=>'13758129341', 'verify'=>'D35H72', 'category' => '不必须') );
+        $result = $this->sendSms('13758129341','3577');
         var_dump($result);
         exit;
         return $this->render('TopxiaAdminBundle:System:sms.html.twig', array());
+    }
+
+    public function smsUsageAction(Request $request)
+    {
+        //8888888888
     }
 
     public function applyForSmsAction(Request $request)
@@ -92,9 +47,71 @@ class EduCloudController extends BaseController
         //8888888888
     }
 
-    public function smsUsageAction(Request $request)
+
+    public function smsCaptchaAction(Request $request)
     {
         //8888888888
+    }    
+
+    private function getCloudOptions()
+    {        
+        if (empty($this->cloudOptions)) {
+            $settings = $this->getServiceKernel()->createService('System.SettingService')->get('storage', array());
+            $this->cloudOptions = array(
+                'accessKey' => empty($settings['cloud_access_key']) ? '' : $settings['cloud_access_key'],
+                'secretKey' => empty($settings['cloud_secret_key']) ? '' : $settings['cloud_secret_key'],
+                'apiUrl' => empty($settings['cloud_api_server']) ? '' : $settings['cloud_api_server'],
+            );
+        }        
+        return $this->cloudOptions;
     }
 
+    private function getCloudApi()
+    {        
+        if (empty($this->cloudApi)) {
+            $this->cloudApi = $this->createAPIClient();
+        }        
+        return $this->cloudApi;
+    }
+
+    private function getAccounts()
+    {
+        $api = $this->getCloudApi();
+        $options = $this->getCloudOptions();
+        return $api->get('/accounts');
+    }
+
+    private function applyForSms($name = 'smsHead')
+    {
+        $api = $this->getCloudApi();
+        $options = $this->getCloudOptions();
+        
+        $result = $api->post(
+            sprintf('/sms/%s/apply', $options['accessKey']), 
+            $params = array('name' => $name)
+        );
+
+        return $result;
+    }    
+
+    private function lookForStatus()
+    {
+        $api = $this->getCloudApi();
+        $options = $this->getCloudOptions();
+        $result = $api->post(
+            sprintf('/sms/%s/applyResult', $options['accessKey'])
+        );
+        return $result;
+    }
+
+    private function sendSms($to, $captcha, $category = 'captcha')
+    {
+        $api = $this->getCloudApi();
+        $options = $this->getCloudOptions();
+        $result = $api->post(
+            sprintf('/sms/%s/sendVerify', $options['accessKey']),
+            $params=array('mobile' => $to, 'verify'=> $captcha, 'category' => $category)
+        );
+        return $result;
+    }    
 }
