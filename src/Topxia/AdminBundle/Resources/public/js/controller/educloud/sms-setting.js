d7c3a2aabe516e1447337b3018bec0b284333c3e
wangchao <wangchao@howzhi.com>
2015-02-09 17:33:31 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/educloud/sms-setting.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/educloud/sms-setting.js
@@ -3,34 +3,15 @@ define(function(require, exports, module) {
 	require('common/validator-rules').inject(Validator);
 
 	if ($('#sms-form').length>0){	
-		// var validator = new Validator({
-	 //        element: '#sms-form',
-	 //        autoSubmit: false,
-	 //        onFormValidated: function(error, results, $form) {
-	 //            if (error) {
-	 //                return false;
-	 //            }
-	 //        }
 
-	 //    });
-
-	    // validator.addItem({
-	    //     element: '[name="sms-school-name"]',
-	    //     required: true,
-	    //     rule: 'chinese_alphanumeric byte_minlength{min:2} byte_maxlength{max:8}'
-	    // });
-
-		// $('.js-alter-school-name').click(function(){
-		// 	$('.js-school-name').removeAttr("readonly");
-		// });
-
-		// $('.js-school-name').blur(function(){
-		// 	$('.js-school-name').attr("readonly","readonly");
-		// });
-
-		// $('.js-school-name').keyup(function(){
-		// 	$('#js-sms-school-name').html($('.js-school-name').val());
-		// });
+		$('[name="sms_enabled"]').click(function(){
+			var status = $('[name="sms_enabled"]:checked').val();
+			if (status == 0){
+				$('.js-usage').hide();
+			}else{
+				$('.js-usage').show();
+			}
+		});
 		
 	}
 });
