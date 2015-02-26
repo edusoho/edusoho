2ae28ab4ad7b07348b2ff091b494fc4140c5ca67
nifeng <nifeng@howzhi.com>
2015-02-26 17:42:24 +0800

    fix

--- src/Topxia/Service/Order/OrderProcessor/ClassroomOrderProcessor.php
+++ src/Topxia/Service/Order/OrderProcessor/ClassroomOrderProcessor.php
@@ -123,7 +123,7 @@ class ClassroomOrderProcessor extends BaseProcessor implements OrderProcessor
     private function calculateUserLearnProgress($course, $member)
     {
         if ($course['lessonNum'] == 0) {
-            return array('percent' => '0%', 'number' => 0, 'total' => 0);
+            return 0;
         }
 
         $percent = intval($member['learnedNum'] / $course['lessonNum'] * 100) . '%';
