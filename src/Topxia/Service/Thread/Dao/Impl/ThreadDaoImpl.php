8fad2b6580cd328740f2b8eebd835e76e03b7bf9
Wellming Li <wellming.li@howzhi.com>
2015-03-05 09:27:48 +0800

    fix

--- src/Topxia/Service/Thread/Dao/Impl/ThreadDaoImpl.php
+++ src/Topxia/Service/Thread/Dao/Impl/ThreadDaoImpl.php
@@ -20,7 +20,7 @@ class ThreadDaoImpl extends BaseDao implements ThreadDao
         return $this->fetchCached('id', $id, function($id) use ($that) {
             $sql = "SELECT * FROM {$that->getTable()} WHERE id = ? LIMIT 1";
             $thread = $that->getConnection()->fetchAssoc($sql, array($id));
-            return $thread ? $that->createSerializer()->unserialize($thread, $that->serializeFields) : null;
+            return $thread ? $that->createSerializer()->unserialize($thread, $that->getSerializeFields()) : null;
         });
 	}
 
@@ -129,6 +129,11 @@ class ThreadDaoImpl extends BaseDao implements ThreadDao
     public function getTable()
     {
         return $this->table;
+    }
+
+    public function getSerializeFields()
+    {
+        return $this->serializeFields;
     }
 
 }
