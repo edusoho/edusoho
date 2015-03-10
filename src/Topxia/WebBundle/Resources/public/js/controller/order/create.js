82ae3d0c546858dbdd94307f3f7c3ce16b07d12a
wangchao <wangchao@howzhi.com>
2015-03-10 17:13:08 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/order/create.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/order/create.js
@@ -254,7 +254,10 @@ define(function(require, exports, module) {
  		$('#order-create-btn').click(function(){
  			var coinToPay = $('#coinPayAmount').val();
  			if ((coinToPay.length > 0)&&(!isNaN(coinToPay))&&(coinToPay > 0)){
- 				$("#js-order-create-sms-btn").trigger("click");
+ 				$("#payPassword").trigger("change");
+ 				if ($('span[class="text-danger"]').length == 0) {
+ 					$("#js-order-create-sms-btn").trigger("click");
+ 				}
  				return false;
  			}
  			return true;
