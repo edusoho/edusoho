7f90abb6a2ed22abe1643d2ae92c12371bc17ea8
Wellming Li <wellming.li@howzhi.com>
2015-03-15 23:16:16 +0800

    fix unit text

--- app/DoctrineMigrations/Version20150313161449.php
+++ app/DoctrineMigrations/Version20150313161449.php
@@ -13,7 +13,7 @@ class Version20150313161449 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-    	$this->addSql("delete from classroom_courses where classroomId not in (select id from classroom);");
+    	// $this->addSql("delete from classroom_courses where classroomId not in (select id from classroom);");
     }
 
     public function down(Schema $schema)
