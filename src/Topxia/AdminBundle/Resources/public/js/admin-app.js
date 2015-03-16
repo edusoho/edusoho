1b8d787f4d05c22a0c9abd8bfe732136e3c87f11
cc <fucongcong@howzhi.com>
2015-03-16 17:03:50 +0800

    add title

--- src/Topxia/AdminBundle/Resources/public/js/admin-app.js
+++ src/Topxia/AdminBundle/Resources/public/js/admin-app.js
@@ -6,7 +6,7 @@ define(function(require, exports, module) {
 	require('bootstrap');
 	require('common/bootstrap-modal-hack2');
 
- 	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="tooltip"]').tooltip();
 	exports.load = function(name) {
 		if (window.app.jsPaths[name.split('/', 1)[0]] == undefined) {
 			name = window.app.basePath + '/bundles/topxiaadmin/js/controller/' + name;
@@ -19,14 +19,20 @@ define(function(require, exports, module) {
 		});
 
 	};
-    
+
+	$('.collect').on('click', function() {
+
+		var title = $(document).attr("title");
+		alert(title);
+	});
+
 	window.app.load = exports.load;
 
 	if (app.controller) {
 		exports.load(app.controller);
 	}
 
-	$( document ).ajaxSend(function(a, b, c) {
+	$(document).ajaxSend(function(a, b, c) {
 		if (c.type == 'POST') {
 			b.setRequestHeader('X-CSRF-Token', $('meta[name=csrf-token]').attr('content'));
 		}
