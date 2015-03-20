4a734b7a763d35baaabc1e991b08922dad8385fa
Su ju <suju@howzhi.com>
2015-03-20 09:49:59 +0800

    fix wx sign

--- src/Topxia/MobileBundleV2/Processor/Impl/SchoolProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/SchoolProcessorImpl.php
@@ -42,7 +42,44 @@ class SchoolProcessorImpl extends BaseProcessor implements SchoolProcessor {
             "post"=>$_POST
         ));
 
-        return "hello test";
+        return $this->responseMsg();
+    }
+
+    private function responseMsg()
+    {
+      //get post data, May be due to the different environments
+      $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+          //extract post data
+      if (!empty($postStr)){
+                  /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+                     the best way is to check the validity of xml by yourself */
+                  libxml_disable_entity_loader(true);
+                  $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                  $fromUsername = $postObj->FromUserName;
+                  $toUsername = $postObj->ToUserName;
+                  $keyword = trim($postObj->Content);
+                  $time = time();
+                  $textTpl = "<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[%s]]></MsgType>
+                <Content><![CDATA[%s]]></Content>
+                <FuncFlag>0</FuncFlag>
+                </xml>";             
+          if(!empty( $keyword ))
+                  {
+                    $msgType = "text";
+                    $contentStr = "Welcome to wechat world!";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    return $resultStr;
+                  }else{
+                    return "Input something...";
+                  }
+
+          }else {
+            return "";
+          }
     }
 
     public function loginSchoolWithSite()
