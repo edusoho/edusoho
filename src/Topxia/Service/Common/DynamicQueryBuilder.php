d40b9e49765f5b28d070439a145e02450f780290
nifeng <nifeng@howzhi.com>
2015-03-24 19:07:39 +0800

    fix

--- src/Topxia/Service/Common/DynamicQueryBuilder.php
+++ src/Topxia/Service/Common/DynamicQueryBuilder.php
@@ -27,12 +27,41 @@ class DynamicQueryBuilder extends QueryBuilder
     	if (!$this->isWhereInConditions($where)) {
     		return $this;
     	}
-        return parent::andWhere($where);
+
+        if($this->isInCondition($where)) {
+            $conditionName = $this->getConditionName($where);
+            return $this->addWhereIn($where, $conditionName, $this->conditions[$conditionName]);
+        } else {
+            return parent::andWhere($where);
+        }
+
     }
 
     public function andStaticWhere($where)
     {
     	return parent::andWhere($where);
+    }
+
+    private function addWhereIn($where, $conditionName, $params)
+    {
+        if(count($params) == 0) {
+            return $this;
+        }
+        $sqlWhere = "";
+        foreach ($params as $index => $param) {
+            if(count($params) == ($index+1)) {
+                $sqlWhere .= ":{$conditionName}_".$index;
+            } else {
+                $sqlWhere .= ":{$conditionName}_".$index.",";
+            }
+
+            $this->conditions[$conditionName."_".$index] = $param;
+        }
+        $sqlWhere .= "";
+        
+        $where = str_replace(":".$conditionName, $sqlWhere, $where);
+
+        return parent::andWhere($where);
     }
 
     public function execute()
@@ -43,12 +72,31 @@ class DynamicQueryBuilder extends QueryBuilder
     	return parent::execute();
     }
 
+    private function isInCondition($where)
+    {
+        $matched = preg_match('/ (IN) /', $where, $matches);
+        if (empty($matched)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private function getConditionName($where) {
+        $matched = preg_match('/:([a-zA-z0-9_]+)/', $where, $matches);
+        if (empty($matched)) {
+            return false;
+        }
+        return $matches[1];
+    }
+
     private function isWhereInConditions($where)
     {
-    	$matched = preg_match('/:([a-zA-z0-9_]+)/', $where, $matches);
-    	if (empty($matched)) {
+        $conditionName = $this->getConditionName($where);
+    	if (!$conditionName) {
     		return false;
     	}
-        return array_key_exists($matches[1], $this->conditions) && !is_null($this->conditions[$matches[1]]);
+
+        return array_key_exists($conditionName, $this->conditions) && !is_null($this->conditions[$conditionName]);
     }
 }
