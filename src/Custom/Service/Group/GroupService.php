348be962c4ccb648ddc623ca119275fdcd321acc
IlhamTahir <yantaq@bilig.biz>
2015-03-26 07:31:59 +0000

    Customize GroupService has been created

---
+++ src/Custom/Service/Group/GroupService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Custom\Service\Group;
+
+interface GroupService
+{
+   
+}
---
+++ src/Custom/Service/Group/Impl/GroupServiceImpl.php
@@ -0,0 +1,11 @@
+<?php
+namespace Custom\Service\Course\Impl;
+
+use Topxia\Service\User\Impl\GroupServiceImpl as BaseService;
+use Custom\Service\Course\CourseService;
+
+class GroupServiceImpl extends BaseService implements GroupService
+{
+
+	
+}
