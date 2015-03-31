00cca74f4a2e80d1be39cfdb486311f9722b1cff
IlhamTahir <yantaq@bilig.biz>
2015-03-31 04:31:48 +0000

    bug

---
+++ src/Custom/WebBundle/Resources/public/js/controller/teacher-tag/list.js
@@ -0,0 +1,18 @@
+define(function(require, exports, module) {
+	var Notify = require('common/bootstrap-notify');
+	exports.run = function() {
+		$('body').on('click', 'button.delete-btn', function() {
+			if (!confirm('确认要删除标签吗？')) return false;
+			var $btn = $(this);
+			$.post($btn.data('url'), function(response) {
+				if (response.status == 'ok') {
+					$('#' + $btn.data('target')).remove();
+					Notify.success('删除成功!');
+				} else {
+					alert('服务器错误!');
+				}
+			}, 'json');
+		});
+	};
+
+});
