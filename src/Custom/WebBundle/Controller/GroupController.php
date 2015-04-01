f372b9b9a6d78386dda5f3f24474f82af5dcaa40
IlhamTahir <yantaq@bilig.biz>
2015-04-01 07:03:12 +0000

    bug fixed

--- src/Custom/WebBundle/Controller/GroupController.php
+++ src/Custom/WebBundle/Controller/GroupController.php
@@ -22,7 +22,9 @@ class GroupController extends BaseController
 
         $activeGroup = $this->getGroupService()->searchGroups(array('status'=>'open',),  array('memberNum', 'DESC'),0, 12);
 		$recommendList = $this->getGroupService()->getRecommendList(12);
+		
 		$recommendGroup=$this->getGroupService()->getGroupsByIds(ArrayToolkit::column($recommendList, 'id'));
+		var_dump($recommendGroup);die;
         $recentlyThread = $this->getThreadService()->searchThreads(
             array(
                 'createdTime'=>time()-30*24*60*60,
