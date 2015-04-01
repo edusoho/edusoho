7d3fec2e50e446a4945b1a7978c6cc94994bedd2
wulianming <wulianming@howzhi.com>
2015-04-01 11:58:13 +0800

    fix

---
+++ src/Topxia/AdminBundle/Resources/public/js/controller/app/center.js
@@ -0,0 +1,11 @@
+define(function(require, exports, module) {
+        
+    exports.run = function() {
+        var $element = $('#app-table-container');
+
+        require('../../util/short-long-text')($element);
+
+    };
+
+  });
+
