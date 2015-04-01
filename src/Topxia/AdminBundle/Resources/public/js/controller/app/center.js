40d0289c31664b80ea143f9af7cabccc250f078a
wulianming <wulianming@howzhi.com>
2015-04-01 17:33:21 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/app/center.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/app/center.js
@@ -1,21 +1,20 @@
 define(function(require, exports, module) {
-        
+
     exports.run = function() {
         var $element = $('#app-table-container');
 
         require('../../util/short-long-text')($element);
 
-        $('input:checkbox[name="appTypeChoices"]').on("change", function () {
+        $('input:checkbox[name="appTypeChoices"]').on("change", function() {
             var element = $(this);
-            if(element.attr("id") == "installedApps" && element.prop('checked')){
+            var hidden = $("#type").attr("value");
+            if (element.attr("id") == "installedApps" && element.prop('checked')) {
                 window.location.href = $(this).data('url');
-            }else{
-            	 var hidden =  $("#type").attr("value") ;
-               window.location.href = '/admin/app/center/'+hidden+'/unhidden';
+            } else {
+                window.location.href = '/admin/app/center/' + hidden + '/unhidden';
             }
         });
 
     };
 
-  });
-
+});
