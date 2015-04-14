fec5d8b1f1c595859bd5b5850108c9a1893fe732
ruanwenqin <452253895@qq.com>
2015-04-14 14:14:17 +0800

    fix

--- app/DoctrineMigrations/Version20150412211039.php
+++ app/DoctrineMigrations/Version20150412211039.php
@@ -13,7 +13,7 @@ class Version20150412211039 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("ALTER TABLE `course` ADD `parentId` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '课程的父Id' AFTER `discountActivityId`;");
+        $this->addSql("ALTER TABLE `course` ADD `parentId` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '课程的父Id' AFTER `locationId`;");
         $this->addSql("ALTER TABLE `thread` CHANGE `maxUsers` `maxUsers` INT(10) NOT NULL DEFAULT '0' COMMENT '最大人数';");
     }
 
