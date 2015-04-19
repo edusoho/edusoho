59910fa9c35757b6cef066b8f4939edb8230e376
Wellming Li <wellming.li@howzhi.com>
2015-04-20 01:45:41 +0800

    menu refactor

--- src/Topxia/WebBundle/Twig/Extension/MenuExtension.php
+++ src/Topxia/WebBundle/Twig/Extension/MenuExtension.php
@@ -2,11 +2,22 @@
 namespace Topxia\WebBundle\Twig\Extension;
 
 use Topxia\Service\Common\ServiceKernel;
+use Topxia\Common\MenuBuilder;
 use Topxia\WebBundle\Util\Permission;
 
 class MenuExtension extends \Twig_Extension
 {
+    protected $container;
+
     protected $menuUtil = null;
+
+    protected $builders = array();
+
+
+    public function __construct ($container)
+    {
+        $this->container = $container;
+    }
 
     public function getFilters ()
     {
@@ -20,7 +31,44 @@ class MenuExtension extends \Twig_Extension
             'menus' => new \Twig_Function_Method($this, 'menus'),
             'html_title' => new \Twig_Function_Method($this, 'htmlTitle'),
             'page_title' => new \Twig_Function_Method($this, 'pageTitle'),
+            'menu_children' => new \Twig_Function_Method($this, 'getMenuChildren'),
+            'menu_breadcrumb' => new \Twig_Function_Method($this, 'getMenuBreadcrumb'),
+            'menu_path' => new \Twig_Function_Method($this, 'getMenuPath', array('needs_context' => true)),
+
         );
+    }
+
+    public function getMenuPath($context, $menu)
+    {
+        $route = empty($menu['router_name']) ? $menu['code'] : $menu['router_name'];
+        $params = empty($menu['router_params']) ? array() : $menu['router_params'];
+
+        if (!empty($menu['router_params_context'])) {
+            foreach ($params as $key => $value) {
+                $value = split('.', $value, 2);
+                $params[$key] = $context[$value[0]][$value[1]];
+            }
+        }
+
+        return $this->container->get('router')->generate($route, $params);
+    }
+
+    public function getMenuChildren($position, $code, $group = null)
+    {
+        return $this->createMenuBuilder($position)->getMenuChildren($code, $group);
+    }
+
+    public function getMenuBreadcrumb($position, $code)
+    {
+        return $this->createMenuBuilder($position)->getMenuBreadcrumb($code);
+    }
+
+    private function createMenuBuilder($position)
+    {
+        if (!isset($this->builders[$position])) {
+            $this->builders[$position] = new MenuBuilder($position);
+        }
+        return $this->builders[$position];
     }
 
     public function htmlTitle($position, $code)
