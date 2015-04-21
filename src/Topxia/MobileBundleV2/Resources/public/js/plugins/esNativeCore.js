9c26057948cb01cfd332f02e6280ea4417962669
Su ju <suju@howzhi.com>
2015-04-21 10:08:44 +0800

    fix esNativeCore.js

--- src/Topxia/MobileBundleV2/Resources/public/js/plugins/esNativeCore.js
+++ src/Topxia/MobileBundleV2/Resources/public/js/plugins/esNativeCore.js
@@ -2,13 +2,13 @@ cordova.define("com.edusoho.kuozhi.webview.cordova.ESNativeCore", function(requi
     var exec = require('cordova/exec');
     module.exports = {
         showImages : function(index, images) {
-            exec(null, null, "esNativeCore", "showImages", [index, images]);
+            exec(null, null, "ESNativeCore", "showImages", [index, images]);
         },
         createMenu : function(menuJson) {
-            exec(null, null, "esNativeCore", "createMenu", [menuJson]);
+            exec(null, null, "ESNativeCore", "createMenu", [menuJson]);
         },
         version : function(version) {
-            exec(null, null, "esNativeCore", "version", [version]);
+            exec(null, null, "ESNativeCore", "version", [version]);
         }
     };
 });
