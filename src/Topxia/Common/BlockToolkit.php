32a726e184f4759377740f499b1a3144e0e5e61b
ruanwenqin <452253895@qq.com>
2015-05-06 09:31:48 +0800

    add a replace link function for blocktoolkit

--- src/Topxia/Common/BlockToolkit.php
+++ src/Topxia/Common/BlockToolkit.php
@@ -77,4 +77,38 @@ class BlockToolkit {
         ));
     } 
 
+    public static function updateLinks($code)
+    {
+        $blockService = ServiceKernel::instance()->createService('Content.BlockService');
+        $block = $blockService->getBlockByCode($code);
+        $data = $block['data'];
+        $content = $block['content'];
+        preg_match_all('/<dt>(.?*)</dt>/i', $content, $textMatchs);
+        preg_match_all('/< *img[^>]*alt *= *["\']?([^"\']*)/i', $content, $altMatchs);
+        preg_match_all('/< *a[^>]*href *= *["\']?([^"\']*)/i', $content, $linkMatchs);
+        preg_match_all('/< *a[^>]*target *= *["\']?([^"\']*)/i', $content, $targetMatchs);
+        foreach ($data['carousel']['items'] as $key => $imglink) {
+            if (!empty($imgMatchs[1][$key])) {
+                $imglink['src'] = $imgMatchs[1][$key];
+            }
+
+            if (!empty($altMatchs[1][$key])) {
+                $imglink['alt'] = $altMatchs[1][$key];
+            }
+
+            if (!empty($linkMatchs[1][$key])) {
+                $imglink['href'] = $linkMatchs[1][$key];
+            }
+
+            if (!empty($targetMatchs[1][$key])) {
+                $imglink['target'] = $targetMatchs[1][$key];
+            }
+
+            $data['carousel']['items'][$key] = $imglink;
+        }
+
+        $blockService->updateBlock($block['id'], array(
+            'data' => $data,
+        ));
+    } 
 }
