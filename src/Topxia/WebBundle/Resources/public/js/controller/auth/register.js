f2394a5a1f7a1827ed24a4c3c730a148e54028c9
nifeng <nifeng@howzhi.com>
2015-05-07 20:19:04 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/auth/register.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/auth/register.js
@@ -1,7 +1,7 @@
 define(function(require, exports, module) {
     var Validator = require('bootstrap.validator');
     require('common/validator-rules').inject(Validator);
-    require("common/jquery.placeholder.js")
+    require("placeholder");
     require("jquery.bootstrap-datetimepicker");
     var SmsSender = require('../widget/sms-sender');
     exports.run = function() {
