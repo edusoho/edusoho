e5af2982f5b2689adb9f42fb07ec66b3fa8fb880
Su ju <suju@howzhi.com>
2015-05-12 15:46:26 +0800

    add regist no nickname

--- src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
@@ -286,7 +286,7 @@ class UserProcessorImpl extends BaseProcessor implements UserProcessor
             return $this->createErrorResponse('email_invalid', '邮箱地址格式不正确');
         }
 
-        if (!SimpleValidator::nickname($nickname)) {
+        if ($nickname && !SimpleValidator::nickname($nickname)) {
             return $this->createErrorResponse('nickname_invalid', '昵称格式不正确');
         }
 
@@ -298,10 +298,17 @@ class UserProcessorImpl extends BaseProcessor implements UserProcessor
             return $this->createErrorResponse('email_exist', '该邮箱已被注册');
         }
 
-        if (!$this->controller->getUserService()->isNicknameAvaliable($nickname)) {
-            return $this->createErrorResponse('nickname_exist', '该昵称已被注册');
+        if (! $nickname) {
+            $nickname = "ES" . time();
+            while (!$this->controller->getUserService()->isNicknameAvaliable($nickname)) {
+                $nickname = "ES". time();
+            }
+        } else {
+            if (!$this->controller->getUserService()->isNicknameAvaliable($nickname)) {
+                return $this->createErrorResponse('nickname_exist', '该昵称已被注册');
+            }
         }
-
+        
         $user = $this->controller->getAuthService()->register(array(
             'email' => $email,
             'nickname' => $nickname,
