a6792c3b400cadea68b57e8f9c3417903f1060cb
danyue <wangdanyue@howzhi.com>
2015-05-13 18:17:13 +0800

    feature/5546-lesson-add-suggest-period-sql

---
+++ app/DoctrineMigrations/Version20150513181253.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150513181253 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("ALTER  TABLE course_lesson CHANGE  `suggestPeriod` `suggestHours` float(10,1) unsigned NOT NULL DEFAULT '0.0' COMMENT '建议学习时长'");
+        $this->addSql("ALTER TABLE homework_result ADD COLUMN `passedStatus` enum('none','excellent','good','passed','unpassed') NOT NULL DEFAULT 'none' COMMENT '作业通过状态，none表示还未审批'");
+        $this->addSql("ALTER TABLE homework ADD COLUMN `correctPercent` VARCHAR(255) DEFAULT NULL COMMENT '通过率百分比设置'");    
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
