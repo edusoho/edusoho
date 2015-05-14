e3c6a9583b8003128a584c34ceb73841ad0e221c
danyue <wangdanyue@howzhi.com>
2015-05-14 17:34:38 +0800

    feature/5546-lesson-add-suggest-period-sql-fix3

--- app/DoctrineMigrations/Version20150513181253.php
+++ app/DoctrineMigrations/Version20150513181253.php
@@ -16,8 +16,10 @@ class Version20150513181253 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("ALTER TABLE course_lesson ADD COLUMN `suggestHours` float(10,1) unsigned NOT NULL DEFAULT '0.0' COMMENT '建议学习时长'");
-            
+        $this->addSql("ALTER TABLE `course_lesson` ADD COLUMN `suggestHours` float(10,1) unsigned NOT NULL DEFAULT '0.0' COMMENT '建议学习时长'");
+        $this->addSql("UPDATE `course_lesson` SET `suggestHours` = CEIL(length/3600) WHERE type IN('video','audio') AND length is not Null'");
+        $this->addSql("UPDATE `course_lesson` SET `suggestHours` = 1 WHERE type IN('video','audio')  AND  length is Null");
+        $this->addSql("UPDATE course_lesson SET suggestHours=2  WHERE type NOT IN('video','audio') AND  length is Null");
     }
 
     /**
