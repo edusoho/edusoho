9612e16bcf0b5fe00ba37ef1ca7427f8f7f4ee78
nifeng <nifeng@howzhi.com>
2015-05-21 16:45:40 +0800

    fix

--- src/Topxia/WebBundle/Controller/AnnouncementController.php
+++ src/Topxia/WebBundle/Controller/AnnouncementController.php
@@ -140,14 +140,15 @@ class AnnouncementController extends BaseController
 	{
 		$conditions = array(
 			'targetType' => $targetType,
-			'targetId' => $targetObject['id']
+			'targetId' => $targetObject['id'],
+			'endTime' => time()
 		);
 
 		$processor = $this->getAnnouncementProcessor($targetType);
 		$canManage = $processor->checkManage($targetObject['id']);
 		$canTake = $processor->checkTake($targetObject['id']);
 
-		$announcements = $this->getAnnouncementService()->searchAnnouncements($conditions, array('createdTime','DESC'), 0, 1);
+		$announcements = $this->getAnnouncementService()->searchAnnouncements($conditions, array('createdTime','DESC'), 0, 10);
 
 		return $this->render('TopxiaWebBundle:Announcement:announcement-block.html.twig',array(
 			'targetObject' => $targetObject,
