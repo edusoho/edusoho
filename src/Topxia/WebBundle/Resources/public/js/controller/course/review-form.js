75c91307ac6a8571a37c6b20a2ab78dc92c8e095
Wellming Li <wellming.li@howzhi.com>
2015-05-23 14:30:58 +0800

    course ui

--- src/Topxia/WebBundle/Resources/public/js/controller/course/review-form.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/review-form.js
@@ -6,25 +6,26 @@ define(function(require, exports, module) {
 
     exports.run = function() {
 
-        $('#my-course-rate').raty({
-            path: $('#my-course-rate').data('imgPath'),
+        var $form = $('#review-form');
+
+        $form.find('.rating-btn').raty({
+            path: $form.find('.rating-btn').data('imgPath'),
             hints: ['很差', '较差', '还行', '推荐', '力荐'],
             score: function() {
                 return $(this).attr('data-rating');
             },
             click: function(score, event) {
-                $('#review_rating').val($("#my-course-rate > input[name=score]").val());
-
+                $form.find('[name=rating]').val(score);
             }
         });
 
         var validator = new Validator({
-            element: '#review-form',
+            element: $form,
             autoSubmit: false
         });
 
         validator.addItem({
-            element: '[name="review[rating]"]',
+            element: '[name=rating]',
             required: true,
             errormessageRequired: '请打分'
         });
@@ -34,23 +35,28 @@ define(function(require, exports, module) {
                 return;
             }
 
+            $form.find('[type=submit]').button('loading');
+
             $.post($form.attr('action'), $form.serialize(), function(json) {
-                window.location.reload();
+                $form.find('.text-success').fadeIn('fast', function(){
+                    window.location.reload();
+                });
             }, 'json');
 
         });
 
 
-        $('#modal').on('click', '#list_reviews', function(){
-            $.get($('#list_reviews').data('url'), function(html) {
-                $('#modal').html(html);
-            });
+        $('.js-hide-review-form').on('click', function(){
+            $(this).hide();
+            $('.js-show-review-form').show();
+            $form.hide();
         });
 
-        $('#modal').on('click', '#back_to_create', function(){
-            $('#modal').load($('#back_to_create').data('url'));
+        $('.js-show-review-form').on('click', function(){
+            $(this).hide();
+            $('.js-hide-review-form').show();
+            $form.show();
         });
-
 
     };
 
