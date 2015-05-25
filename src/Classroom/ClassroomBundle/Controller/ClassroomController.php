2a550afdefee4b5c26c067ec4851c75dc52299c4
danyue <wangdanyue@howzhi.com>
2015-05-25 17:09:42 +0800

    classroom-test-wdy-fix3

--- src/Classroom/ClassroomBundle/Controller/ClassroomController.php
+++ src/Classroom/ClassroomBundle/Controller/ClassroomController.php
@@ -134,7 +134,8 @@ class ClassroomController extends BaseController
 
         $canFreeJoin = $this->canFreeJoin($courses, $user);
         $canManage = $this->getClassroomService()->canManageClassroom($classroomId);
-if ($classroom['price'] > 0 && ( !$canFreeJoin or $checkMemberLevelResult == 'level_low' or in_array($checkMemberLevelResult,array('not_member', 'member_expired'))) {
+if ($classroom['price'] > 0 && ( !$canFreeJoin or $checkMemberLevelResult == 'level_low' or in_array($checkMemberLevelResult,array('not_member', 'member_expired'))) )
+{
     var_dump(1);
 } else {
     var_dump(0);
