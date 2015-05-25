2c1ea3c3432a9d60ba9990bdddd6d713c07719d5
danyue <wangdanyue@howzhi.com>
2015-05-25 17:14:11 +0800

    classroom-test-wdy-fix5

--- src/Classroom/ClassroomBundle/Controller/ClassroomController.php
+++ src/Classroom/ClassroomBundle/Controller/ClassroomController.php
@@ -134,12 +134,9 @@ class ClassroomController extends BaseController
 
         $canFreeJoin = $this->canFreeJoin($courses, $user);
         $canManage = $this->getClassroomService()->canManageClassroom($classroomId);
-if ($classroom['price'] > 0 && !$canFreeJoin)
-{
-    var_dump(1);
-} else {
-    var_dump(0);
-}
+
+    var_dump($canFreeJoin);
+
         if ($member && !$member["locked"]) {
             return $this->render("ClassroomBundle:Classroom:classroom-join-header.html.twig", array(
                 'classroom' => $classroom,
