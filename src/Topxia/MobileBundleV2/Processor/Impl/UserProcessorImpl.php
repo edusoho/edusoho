7813168d092fa9d3ea6fccaa7026ca7fc19664a4
Huang Boyi <huangboyi@howzhi.com>
2015-05-26 21:03:01 +0800

    fix

--- src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/UserProcessorImpl.php
@@ -433,7 +433,7 @@ class UserProcessorImpl extends BaseProcessor implements UserProcessor
                 &&($this->getEduCloudService()->getCloudSmsKey('sms_registration') == 'on')) {
                 //$requestInfo = array('sms_code' => $smsCode, 'mobile' => $phoneNumber);
                 //$this->request['']
-                var_dump($request->getSession()->get('sms_registration'));
+                var_dump($this->request->getSession()->get('sms_registration'));
             exit();
                 list($result, $sessionField, $requestField) = SmsToolkit::smsCheck($this->request, $scenario = 'sms_registration');
                 if ($result) {
