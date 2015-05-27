9aee3d2ac4f58663f0a5d0b164c8fcfedff4b1e4
danyue <wangdanyue@howzhi.com>
2015-05-27 14:46:37 +0800

    favorite-wdy-fix

--- src/Topxia/WebBundle/Resources/public/js/controller/course/common.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/common.js
@@ -23,8 +23,7 @@ define(function(require, exports, module) {
 
         $("#favorite-btn").on('click', function() {
             var $btn = $(this);
-            alert('url='+$btn.data('url'));
-            $.post($btn.data('url'), {'time':new Date().getTime()}, function() {
+            $.post($btn.data('url'), function() {
                 $btn.hide();
                 $("#unfavorite-btn").show();
             });
@@ -32,8 +31,7 @@ define(function(require, exports, module) {
 
         $("#unfavorite-btn").on('click', function() {
             var $btn = $(this);
-            alert('url='+$btn.data('url'));
-            $.post($btn.data('url'), {'time':new Date().getTime()}, function() {
+            $.post($btn.data('url'), function() {
                 $btn.hide();
                 $("#favorite-btn").show();
             });
