ce8e669834283c25ed570ff7c622d679ee6f6cdf
Su ju <suju@howzhi.com>
2015-06-05 16:11:20 +0800

    merge AuthServiceImpl.php

--- src/Topxia/Service/User/Impl/AuthServiceImpl.php
+++ src/Topxia/Service/User/Impl/AuthServiceImpl.php
@@ -4,6 +4,7 @@ namespace Topxia\Service\User\Impl;
 use Topxia\Common\ArrayToolkit;
 use Topxia\Service\Common\BaseService;
 use Topxia\Service\User\AuthService;
+use Topxia\Common\SimpleValidator;
 
 class AuthServiceImpl extends BaseService implements AuthService
 {
@@ -11,6 +12,8 @@ class AuthServiceImpl extends BaseService implements AuthService
 
     public function register($registration, $type = 'default')
     {
+
+        $registration = $this->refillFormData($registration);
         $authUser = $this->getAuthProvider()->register($registration);
 
         if ($type == 'default') {
@@ -31,6 +34,18 @@ class AuthServiceImpl extends BaseService implements AuthService
         return $newUser;
     }
 
+    private function refillFormData($registration){
+        $registration = $this->getUserService()->parseEmailOrMobile($registration);
+        if(!isset($registration['nickname']) || empty($registration['nickname'])){
+            $registration['nickname'] = $this->getUserService()->generateNickname($registration);
+        }
+
+        if($this->getUserService()->isMobileRegisterMode() && !isset($registration['email'])){
+           $registration['email'] = $this->getUserService()->generateEmail($registration);   
+        }
+        return $registration;
+    }
+    
     public function syncLogin($userId)
     {
         $providerName = $this->getAuthProvider()->getProviderName();
@@ -108,21 +123,23 @@ class AuthServiceImpl extends BaseService implements AuthService
 
 
 
-    public function checkUsername($username)
-    {   
-        try {
-            $result = $this->getAuthProvider()->checkUsername($username);
-        } catch (\Exception $e) {
-            return array('error_db', '暂时无法注册，管理员正在努力修复中。（Ucenter配置或连接问题）');
-        }
+    public function checkUsername($username, $randomName='')
+    {     
+        //如果一步注册则$randomName为空，正常校验discus和系统校验，如果两步注册，则判断是否使用默认生成的，如果是，跳过discus和系统校验
+        if(empty($randomName)|| $username != $randomName){
+            try {
+                $result = $this->getAuthProvider()->checkUsername($username);
+            } catch (\Exception $e) {
+                return array('error_db', '暂时无法注册，管理员正在努力修复中。（Ucenter配置或连接问题）');
+            }
 
-        if ($result[0] != 'success') {
-            return $result;
-        }
-
-        $avaliable = $this->getUserService()->isNicknameAvaliable($username);
-        if (!$avaliable) {
-            return array('error_duplicate', '名称已存在!');
+            if ($result[0] != 'success') {
+                return $result;
+            }
+            $avaliable = $this->getUserService()->isNicknameAvaliable($username);
+            if (!$avaliable) {
+                return array('error_duplicate', '名称已存在!');
+            }
         }
 
         return array('success', '');
@@ -144,6 +161,33 @@ class AuthServiceImpl extends BaseService implements AuthService
         }
         
         return array('success', '');
+    }
+
+    public function checkMobile($mobile){
+        try {
+            $result = $this->getAuthProvider()->checkMobile($mobile);
+        } catch (\Exception $e) {
+            return array('error_db', '暂时无法注册，管理员正在努力修复中。（Ucenter配置或连接问题）');
+        }
+        if ($result[0] != 'success') {
+            return $result;
+        }
+        $avaliable = $this->getUserService()->isMobileAvaliable($mobile);
+        if (!$avaliable) {
+            return array('error_duplicate', '手机号码已存在!');
+        }
+        
+        return array('success', '');
+    }
+
+    public function checkEmailOrMobile($emailOrMobile){
+        if(SimpleValidator::email($emailOrMobile)){
+           return $this->checkEmail($emailOrMobile);
+        }else if(SimpleValidator::mobile($emailOrMobile)){
+           return $this->checkMobile ($emailOrMobile);
+        }else {
+           return array('error_dateInput', '电子邮箱或者手机号码格式不正确!');
+        }
     }
 
     public function checkPassword($userId, $password)
@@ -208,7 +252,7 @@ class AuthServiceImpl extends BaseService implements AuthService
     {
         $auth = $this->getSettingService()->get('auth');
         if($auth && array_key_exists('register_mode',$auth)){
-            return ($auth['register_mode'] == 'opened');
+            return (in_array($auth['register_mode'], array('email', 'email_or_mobile')));
         }
         return true;
     }
@@ -231,7 +275,6 @@ class AuthServiceImpl extends BaseService implements AuthService
 
             $this->partner = new $class();
         }
-
         return $this->partner;
     }
 
