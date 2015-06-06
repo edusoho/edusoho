6bdfb2228b4c21476b1a01f30c1eb5112bd8ee7e
ketuzhong <zhongjie@howzhi.com>
2015-06-06 16:22:45 +0800

    修改手机端侧边栏

--- web/themes/jianmo/js/global-script.js
+++ web/themes/jianmo/js/global-script.js
@@ -1,38 +1,37 @@
 define(function(require, exports, module) {
 
-  exports.run = function() {
- 
-    var resizeTimer;
+    exports.run = function() {
 
-    var removeNavMobile = function(){
-        $(".nav-mobile").removeClass("active");
-        $(".html-mask").hide();
-        $("body").removeClass("nav-active")
-        $("html").removeClass("html-nav-active");
-    }
-
-    $(".js-navbar-more").click(function(e){
-        var $nav = $(".nav-mobile");
-        var $mask = $("<div class='html-mask'></div>");
-        var $maskItem = $("." + $mask.attr("class"))
-        if($nav.hasClass("active")){
-            removeNavMobile()
-        }else{
-            $nav.addClass("active");
-            if($maskItem.length == 0){
-                $("body").append($mask)
-            }else{
-                $maskItem.show()
-            }
-             $("body").addClass("nav-active")
-            $("html").addClass("html-nav-active");
+        var removeNavMobile = function(){
+            $(".nav-mobile").removeClass("active");
+            $(".html-mask").hide();
+            $("body").removeClass("nav-active")
+            $("html").removeClass("html-nav-active");
         }
-    })
 
-    $("body").on("click",".html-mask",function(e){
-        removeNavMobile();
-    });
+        $(".js-navbar-more").click(function(e){
+            var $nav = $(".nav-mobile");
+            var $mask = $("<div class='html-mask'></div>");
+            var $maskItem = $("." + $mask.attr("class"))
+            if($nav.hasClass("active")){
+                removeNavMobile()
+            }else{
+                $nav.addClass("active");
+                if($maskItem.length == 0){
+                    $("body").append($mask)
+                }else{
+                    $maskItem.show()
+                }
+                 $("body").addClass("nav-active")
+                $("html").addClass("html-nav-active");
+            }
+        })
 
-  }
+        $("body").on("click",".html-mask",function(e){
+            removeNavMobile();
+        });
+        
+
+    }
 
 });
