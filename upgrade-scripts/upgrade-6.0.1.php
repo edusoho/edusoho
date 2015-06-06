b20eb861a846e6023e0087d4d35842bc65395c6f
nifeng <nifeng@howzhi.com>
2015-06-07 02:28:33 +0800

    fix

--- upgrade-scripts/upgrade-6.0.1.php
+++ upgrade-scripts/upgrade-6.0.1.php
@@ -119,11 +119,11 @@ class EduSohoUpgrade extends AbstractUpdater
         $connection = $this->getConnection();
 
         ///各个默认主题的编辑区
-        $connection->exec("UPDATE `block` SET `code` = 'default-b:home_top_banner' WHERE `code` = 'home_top_banner';");
-        if(!$this->isBlockExist("default-c:home_top_banner")){
-            $connection->exec("INSERT INTO `block`( `userId`, `title`, `mode`, `template`, `templateName`, `templateData`, `content`, `code`, `meta`, `data`, `tips`, `createdTime`, `updateTime`, `category`) select `userId`, `title`, `mode`, `template`, `templateName`, `templateData`, `content`, 'default-c:home_top_banner', `meta`, `data`, `tips`, `createdTime`, `updateTime`, `category` from `block` where `code`='default-b:home_top_banner';");
+        $connection->exec("UPDATE `block` SET `code` = 'default:home_top_banner' WHERE `code` = 'home_top_banner';");
+        if(!$this->isBlockExist("default-b:home_top_banner")){
+            $connection->exec("INSERT INTO `block`( `userId`, `title`, `mode`, `template`, `templateName`, `templateData`, `content`, `code`, `meta`, `data`, `tips`, `createdTime`, `updateTime`, `category`) select `userId`, `title`, `mode`, `template`, `templateName`, `templateData`, `content`, 'default-b:home_top_banner', `meta`, `data`, `tips`, `createdTime`, `updateTime`, `category` from `block` where `code`='default:home_top_banner';");
         }
-        $connection->exec("UPDATE `block` SET templateName='@theme/default-c/block/home_top_banner.template.html.twig' where code='default-c:home_top_banner';");
+        $connection->exec("UPDATE `block` SET templateName='@theme/default/block/home_top_banner.template.html.twig' where code='default:home_top_banner';");
         $connection->exec("UPDATE `block` SET templateName='@theme/default-b/block/home_top_banner.template.html.twig' where code='default-b:home_top_banner';");
 
         ///TODO：老课程复制的内容
