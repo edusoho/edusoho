3d47c4e192da0f9e06cda80f0ff7f66990316a1b
ketuzhong <zhongjie@howzhi.com>
2015-06-09 10:38:18 +0800

    修复手机端导航

--- web/themes/jianmo/js/global-script.js
+++ web/themes/jianmo/js/global-script.js
@@ -2,19 +2,34 @@ define(function(require, exports, module) {
 
     exports.run = function() {
 
+        var removeNavMobile = function(){
+            $(".nav-mobile").removeClass("active");
+            $(".html-mask").hide();
+            $(".es-wrap").removeClass("nav-active")
+            $("html").removeClass("html-nav-active");
+        }
+
         $(".js-navbar-more").click(function(e){
             var $nav = $(".nav-mobile");
-            
+            var $mask = $("<div class='html-mask'></div>");
+            var $maskItem = $("." + $mask.attr("class"));
             if($nav.hasClass("active")){
-                $nav.removeClass("active");
-                $(".es-wrap").removeClass("nav-active")
-                $("html").removeClass("html-nav-active");
+                removeNavMobile();
 
             }else {
                 $nav.addClass("active");
+                if($maskItem.length == 0){
+                $(".es-wrap").append($mask);
+                }else{
+                    $maskItem.show();
+                }
                 $(".es-wrap").addClass("nav-active");
                 $("html").addClass("html-nav-active");
             }
+        });
+
+        $("body").on("click",".html-mask",function(e){
+            removeNavMobile();
         });
     }
 
