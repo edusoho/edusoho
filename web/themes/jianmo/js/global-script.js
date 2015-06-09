08f9dc123fed453ec66554183bed42d7940d7fcf
ketuzhong <zhongjie@howzhi.com>
2015-06-09 11:16:58 +0800

    fix

--- web/themes/jianmo/js/global-script.js
+++ web/themes/jianmo/js/global-script.js
@@ -4,31 +4,33 @@ define(function(require, exports, module) {
 
         var removeNavMobile = function(){
             $(".nav-mobile").removeClass("active");
-            $(".html-mask").hide();
+            // $(".html-mask").hide();
+            $(".html-mask").removeClass("active");
             $(".es-wrap").removeClass("nav-active")
             $("html").removeClass("html-nav-active");
         }
 
         $(".js-navbar-more").click(function(e){
             var $nav = $(".nav-mobile");
-            var $mask = $("<div class='html-mask'></div>");
-            var $maskItem = $("." + $mask.attr("class"));
+            // var $mask = $("<div class='html-mask'></div>");
+            // var $maskItem = $("." + $mask.attr("class"));
             if($nav.hasClass("active")){
                 removeNavMobile();
 
             }else {
                 $nav.addClass("active");
-                if($maskItem.length == 0){
-                $(".es-wrap").append($mask);
-                }else{
-                    $maskItem.show();
-                }
+                // if($maskItem.length == 0){
+                // $(".es-wrap").append($mask);
+                // }else{
+                //     $maskItem.show();
+                // }
+                $(".html-mask").addClass("active");
                 $(".es-wrap").addClass("nav-active");
                 $("html").addClass("html-nav-active");
             }
         });
 
-        $("body").on("click",".html-mask",function(e){
+        $("body").on("click",".html-mask.active",function(e){
             removeNavMobile();
         });
     }
