a0f03bc74f4b718dbcf5f5f668519bf8f8559879
nifeng <nifeng@howzhi.com>
2015-06-11 20:49:02 +0800

    fix

--- src/Topxia/Service/Course/Dao/Impl/CourseMemberDaoImpl.php
+++ src/Topxia/Service/Course/Dao/Impl/CourseMemberDaoImpl.php
@@ -126,6 +126,7 @@ class CourseMemberDaoImpl extends BaseDao implements CourseMemberDao
     {
         $this->filterStartLimit($start, $limit);
         $sql = "SELECT * FROM {$this->table} WHERE courseId = ? AND role = ? ORDER BY createdTime, seq DESC LIMIT {$start}, {$limit}";
+        var_dump($sql);
         return $this->getConnection()->fetchAll($sql, array($courseId, $role));
     }
 
