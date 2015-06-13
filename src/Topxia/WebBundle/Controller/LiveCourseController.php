ae199518de98acd5995ad5b37726d00e34474074
nifeng <nifeng@howzhi.com>
2015-06-13 21:21:21 +0800

    fix

--- src/Topxia/WebBundle/Controller/LiveCourseController.php
+++ src/Topxia/WebBundle/Controller/LiveCourseController.php
@@ -185,6 +185,7 @@ class LiveCourseController extends BaseController
             return $this->render("TopxiaWebBundle:LiveCourse:classroom.html.twig", array(
                 'lesson' => $lesson,
                 'url' => $result['url'],
+                'param' => isset($result['param']) ? $result['param']:null
             ));
 
         }
