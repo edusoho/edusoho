9616cb52fea0844782b0ef1ab0afe41a5e086c1c
nifeng <nifeng@howzhi.com>
2015-06-18 20:46:02 +0800

    fix

--- src/Classroom/ClassroomBundle/Controller/ClassroomManageController.php
+++ src/Classroom/ClassroomBundle/Controller/ClassroomManageController.php
@@ -16,7 +16,7 @@ class ClassroomManageController extends BaseController
         $classroom = $this->getClassroomService()->getClassroom($id);
 
         $courses = $this->getClassroomService()->findActiveCoursesByClassroomId($classroom['id']);
-        $courseIds = ArrayToolkit::column($courses, 'courseId');
+        $courseIds = ArrayToolkit::column($courses, 'id');
 
         $currentTime = time();
         $todayTimeStart = strtotime(date("Y-m-d", $currentTime));
