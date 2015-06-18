1ec7053dcaca55e176ded0ab6e0a59d14a831fc9
Huang Boyi <huangboyi@howzhi.com>
2015-06-18 09:44:46 +0800

    add openWebView plugin

--- src/Topxia/MobileBundleV2/Resources/public/esmobile/js/plugins/esNativeCore.js
+++ src/Topxia/MobileBundleV2/Resources/public/esmobile/js/plugins/esNativeCore.js
@@ -11,7 +11,10 @@ cordova.define("com.edusoho.kuozhi.v3.plugin.MenuClickPlugin", function(require,
             exec(null, null, "ESNativeCore", "version", [version]);
         },
         openDrawer : function(action){
-            exec(null, null, "ESNativeCore", "open", [action]);
+            exec(null, null, "ESNativeCore", "openDrawer", [action]);
+        },
+        openWebView : function(data){
+            exec(null, null, "ESNativeCore", "openWebView", [data]);
         },
         openCourseChat : function(data){
             exec(null, null, "ESNativeCore", "Course", [data]);
