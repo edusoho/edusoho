870b3d4b6f5c917fb5281f69ec8822426bd78cae
nifeng <nifeng@howzhi.com>
2015-06-23 12:54:55 +0800

    fix 课程详情页面的相关课程bug

--- src/Topxia/Service/Course/Dao/Impl/CourseDaoImpl.php
+++ src/Topxia/Service/Course/Dao/Impl/CourseDaoImpl.php
@@ -75,7 +75,7 @@ class CourseDaoImpl extends BaseDao implements CourseDao
             return array();
         }
 
-        $sql ="SELECT * FROM {$this->getTablename()} WHERE status = ? AND ";
+        $sql ="SELECT * FROM {$this->getTablename()} WHERE parentId = 0 AND status = ? AND ";
 
         foreach ($tagIds as $key => $tagId) {
             if ($key > 0 ) {
