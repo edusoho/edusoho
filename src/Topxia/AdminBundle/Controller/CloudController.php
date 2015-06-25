a7c7e9884b3390ba88c7ea07273ce99ee14df76c
lvliujie <lvliujie@howzhi.com>
2015-06-25 15:41:37 +0800

    tpl link

--- src/Topxia/AdminBundle/Controller/CloudController.php
+++ src/Topxia/AdminBundle/Controller/CloudController.php
@@ -57,10 +57,10 @@ class CloudController extends BaseController
         return $this->redirect('http://115.29.78.158:99/token_login?token='.$loginToken["token"].'&goto=edu_cloud_buy&param='.urldecode(json_encode($param)));
     }
 
-    public function tplAction(Request $request)
+    public function tlpAction(Request $request)
     {
         $loginToken = $this->getAppService()->getLoginToken();
-        $param = array( 'type' => 'tpl' );
+        $param = array( 'type' => 'tlp' );
         return $this->redirect('http://115.29.78.158:99/token_login?token='.$loginToken["token"].'&goto=edu_cloud_show&param='.urldecode(json_encode($param)));
     }
 
