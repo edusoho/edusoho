1808f9edd693dacc02f2a788caa57955f4108aaf
zhanggaosheng <245085167@howzhi.com>
2015-06-30 09:44:38 +0800

    fix

--- src/Topxia/WebBundle/Listener/KernelRequestListener.php
+++ src/Topxia/WebBundle/Listener/KernelRequestListener.php
@@ -27,6 +27,10 @@ class KernelRequestListener
         $_target_path = $request->getPathInfo();
         if (strpos($user_agent,'MicroMessenger') && !$currentUser->isLogin() && $setting['enabled'] && $setting['weixinmob_enabled'] && !strpos($_target_path,'login/bind')) {
             $route = 'login_bind';
+            $whiteList = array('/login/bind/weixinmob','/login/bind/weixinmob/callback','/login/bind/weixinmob/new','/login/bind/weixinmob/newset','/login/bind/weixinmob/existbind')
+            if (in_array($request->getPathInfo(), $whiteList)) {
+                return ;
+            }
             $url = $this->container->get('router')->generate($route,array('type' => 'weixinmob'));
             $response = new RedirectResponse($url);
             $event->setResponse($response);
