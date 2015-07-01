f9cce60737eb3c08115e9eefd30a96659f431d35
sunminghui <sunminghui@howzhi.com>
2015-07-01 11:50:22 +0800

    format file

--- src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/testpaper-result.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/quiz-question/testpaper-result.js
@@ -1,43 +1,18 @@
 define(function(require, exports, module) {
-
-    var Notify = require('common/bootstrap-notify');
-    require('jquery.form');
-
     exports.run = function() {
-        	 if (window.history && window.history.pushState) {
-                $(window).on('popstate', function () {
+        if (window.history && window.history.pushState) {
+            $(window).on('popstate', function () {
                 var hashLocation = location.hash;
                 var hashSplit = hashLocation.split("#!/");
                 var hashName = hashSplit[1];
-            if (hashName !== '') {
-                var hash = window.location.hash;
-              if (hash === '') {
-                  // alert("您已经提交试卷！不能再次进入考试页面");
-                  location.reload();
-            }
-          }
-        });
+                if (hashName !== '') {
+                    var hash = window.location.hash;
+                    if (hash === '') {
+                        location.reload();
+                    }
+                }
+            });
         window.history.pushState('forward', null, './result');
-      }
+        }
     };
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
