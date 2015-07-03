c18b8a8b46c6c031146f8cc51a604fb357c43f25
lvliujie <lvliujie@howzhi.com>
2015-07-03 14:21:51 +0800

    token type变动

--- api/src/users.php
+++ api/src/users.php
@@ -152,7 +152,7 @@ $api->post('/login', function (Request $request) {
         throw new \Exception('password error');
     }
 
-    $token = ServiceKernel::instance()->createService('User.UserService')->makeToken('api_login',$user['id']);
+    $token = ServiceKernel::instance()->createService('User.UserService')->makeToken('mobile_login',$user['id']);
     setCurrentUser($token);
     return array(
         'user' => filter($user, 'user'),
