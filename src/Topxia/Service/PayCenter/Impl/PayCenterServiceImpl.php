ccc69a8f90e0c1ef6aa614d29252affe6df7bf91
zhanggaosheng <245085167@howzhi.com>
2015-07-05 14:22:11 +0800

    fix

--- src/Topxia/Service/PayCenter/Impl/PayCenterServiceImpl.php
+++ src/Topxia/Service/PayCenter/Impl/PayCenterServiceImpl.php
@@ -23,7 +23,7 @@ class PayCenterServiceImpl extends BaseService implements PayCenterService
 		$myfile = fopen("mytestfile1","w");
         $txt = $payData['status'].$payData['amount'].$payData['targetType'];
         fwrite($myfile, $txt);
-        $txt = 'test2!!@@!!@@'
+        $txt = 'test2!!@@!!@@';
        	fwrite($myfile, $txt);
         fclose($myfile);
 		if ($payData['status'] != 'success') {
