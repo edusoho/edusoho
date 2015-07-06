409362f9bb97ea8fa2cb2f89bcc99eb5ac62c694
lvliujie <lvliujie@howzhi.com>
2015-07-06 11:39:26 +0800

    私信和私信会话添加类型字段，私信页面添加显示模板文件

---
+++ app/DoctrineMigrations/Version20150706112110.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150706112110 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+        $this->addSql("ALTER TABLE `message` ADD `type` varchar(32) NOT NULL DEFAULT 'text' COMMENT '私信类型' AFTER `id`;");
+        $this->addSql("ALTER TABLE `message_conversation` ADD `type` varchar(32) NOT NULL DEFAULT 'text' COMMENT '最后一条私信类型' AFTER `latestMessageContent`;");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
