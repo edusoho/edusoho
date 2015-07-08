0d5b4007d1c3a39aa42ee3067ca2421ef6186805
lvliujie <lvliujie@howzhi.com>
2015-07-08 15:57:05 +0800

    云视频续费按钮显示

--- src/Topxia/AdminBundle/Controller/CloudController.php
+++ src/Topxia/AdminBundle/Controller/CloudController.php
@@ -75,10 +75,16 @@ class CloudController extends BaseController
         return $this->redirectUrl('edu_cloud_video_upgrade');
     }
 
+    public function videoRenewAction(Request $request)
+    {
+        return $this->redirectUrl('edu_cloud_video_renew');
+    }
+
     protected function redirectUrl($routingName, $params = array())
     {
         $loginToken = $this->getAppService()->getLoginToken();
-        $url = 'http://open.edusoho.com/token_login?token='.$loginToken["token"].'&goto='.$routingName;
+        $developer = $this->getSettingService()->get('developer', array());
+        $url = $developer['app_api_url'].'/token_login?token='.$loginToken["token"].'&goto='.$routingName;
         if(!empty($params)){
             $url .= '&param='.urldecode(json_encode($params));
         }
@@ -88,5 +94,10 @@ class CloudController extends BaseController
     protected function getAppService()
     {
         return $this->getServiceKernel()->createService('CloudPlatform.AppService');
+    }
+
+    protected function getSettingService()
+    {
+        return $this->getServiceKernel()->createService('System.SettingService');
     }
 }
