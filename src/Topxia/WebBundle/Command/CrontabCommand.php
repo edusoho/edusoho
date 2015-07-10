98b96e805d7b2435a6526a537bdb6dbb2b9e28f2
lvliujie <lvliujie@howzhi.com>
2015-07-10 17:02:56 +0800

    定时任务类

---
+++ src/Topxia/WebBundle/Command/CrontabCommand.php
@@ -0,0 +1,31 @@
+<?php
+namespace Topxia\WebBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Topxia\Service\User\CurrentUser;
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Topxia\Service\Common\ServiceKernel;
+use Topxia\Common\BlockToolkit;
+
+class CrontabCommand extends BaseCommand
+{
+
+	protected function configure()
+	{
+		$this->setName ( 'topxia:crontab' );
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$output->writeln('<info>开始初始化系统</info>');
+		
+		$output->writeln('<info>初始化系统完毕</info>');
+	}
+
+	
+
+}
