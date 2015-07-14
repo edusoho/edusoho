1b5e48a0abac233360e026354ec82e3fb778a98a
lcj <lincanjun@howzhi.com>
2015-07-14 11:54:28 +0800

    修复搜索之后时间未限制

--- src/Topxia/AdminBundle/Resources/public/js/controller/login-record/list.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/login-record/list.js
@@ -11,10 +11,14 @@ define(function(require, exports, module) {
             $("#endDate").datetimepicker('setStartDate',$("#startDate").val().substring(0,16));
         });
 
+        $("#startDate").datetimepicker('setEndDate',$("#endDate").val().substring(0,16));
+
         $("#endDate").datetimepicker().on('changeDate',function(){
 
             $("#startDate").datetimepicker('setEndDate',$("#endDate").val().substring(0,16));
         });
+
+        $("#endDate").datetimepicker('setStartDate',$("#startDate").val().substring(0,16));
 
     };
 
