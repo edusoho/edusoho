ffef76dbc6c63141852be0226834897ad4d23705
zhanggaosheng <245085167@howzhi.com>
2015-07-16 19:58:57 +0800

    src/Topxia/WebBundle/Controller/LoginBindController.php

--- src/Topxia/WebBundle/Controller/LoginBindController.php
+++ src/Topxia/WebBundle/Controller/LoginBindController.php
@@ -114,7 +114,9 @@ class LoginBindController extends BaseController
 
         $this->authenticateUser($user);
         $response = array('success' => true, '_target_path' => $request->getSession()->get('_target_path', $this->generateUrl('homepage')));
-
+        if ($response) {
+            # code...
+        }
         response:
         return $response;
     }
@@ -160,7 +162,9 @@ class LoginBindController extends BaseController
         $this->authenticateUser($user);
 
         $response = array('success' => true, '_target_path' => $request->getSession()->get('_target_path', $this->generateUrl('homepage')));
-
+        if (!$response['_target_path']) {
+            $response['_target_path'] = $this->generateUrl('homepage');
+        }
         response:
         return $this->createJsonResponse($response);
     }
