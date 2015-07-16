a5e8864fc89c7d6cffba16f37a1354dda7ecf1b9
xb0123 <xiangbin@howzhi.com>
2015-07-16 15:39:08 +0800

    test2

--- src/Topxia/Service/Thread/Impl/ThreadServiceImpl.php
+++ src/Topxia/Service/Thread/Impl/ThreadServiceImpl.php
@@ -532,12 +532,8 @@ class ThreadServiceImpl extends BaseService implements ThreadService
 
     public function cancelPostAdopted($postId)
     {
-<<<<<<< Updated upstream
         $post = $this->getThreadPostDao()->getPost($postId);
 
-=======
-        $post = $this->getThreadPostDao()->getPost($postId);;
->>>>>>> Stashed changes
         if (empty($post)) {
             throw $this->createServiceException(sprintf('话题回复(ID: %s)不存在。', $post['id']));
         }
