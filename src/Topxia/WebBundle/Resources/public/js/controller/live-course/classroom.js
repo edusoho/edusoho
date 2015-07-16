7eeb14781344310a0bc0028babb66dc0a2ca94b8
nifeng <nifeng@howzhi.com>
2015-07-16 20:15:44 +0800

    fix 文案

--- src/Topxia/WebBundle/Resources/public/js/controller/live-course/classroom.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/live-course/classroom.js
@@ -14,7 +14,7 @@ define(function(require, exports, module) {
 				$("body").html(html);
 			},
 			error: function(){
-				var html = "进入直播教室错误，请联系管理员，或<a href='javascript:document.location.reload()'>重试</a>，或<a href='javascript:window.close();'>关闭</a>"
+				var html = "进入直播教室错误，请联系管理员，<a href='javascript:document.location.reload()'>重试</a>或<a href='javascript:window.close();'>关闭</a>"
 				$("#classroom-url").html(html);
 			}
 		})
