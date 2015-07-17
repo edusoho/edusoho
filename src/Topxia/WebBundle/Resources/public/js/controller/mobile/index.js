d1e9f40d3f7e3c4c94b15cb6bdf79964d5868090
ketuzhong <zhongjie@howzhi.com>
2015-07-17 10:11:29 +0800

    app下载页更新

--- src/Topxia/WebBundle/Resources/public/js/controller/mobile/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/mobile/index.js
@@ -1,9 +1,19 @@
 define(function(require, exports, module) {
 
+    require("jquery.waypoints");
+
     exports.run = function() {
 
-        $('#iphone-download-btn').tooltip()
+        // $('#iphone-download-btn').tooltip()
+        $(".js-mobile-item").waypoint(function(){
+            $(this).addClass('active');
+        },{offset:300});
 
+        $(".es-mobile .btn-mobile").click(function(){
+            $('html,body').animate({
+                scrollTop: $($(this).attr('data-url')).css('top')
+            },1000);
+        });
 
     };
 
