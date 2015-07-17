d47959f5e2514191fea8f6f4312a71b01af93eb6
ketuzhong <zhongjie@howzhi.com>
2015-07-17 11:42:31 +0800

    添加抖动效果

--- src/Topxia/WebBundle/Resources/public/js/controller/mobile/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/mobile/index.js
@@ -4,15 +4,14 @@ define(function(require, exports, module) {
 
     exports.run = function() {
 
-        // $('#iphone-download-btn').tooltip()
         $(".js-mobile-item").waypoint(function(){
             $(this).addClass('active');
         },{offset:300});
 
         $(".es-mobile .btn-mobile").click(function(){
             $('html,body').animate({
-                scrollTop: $($(this).attr('data-url')).css('top')
-            },1000);
+                scrollTop: $($(this).attr('data-url')).offset().top + 60
+            },300);
         });
 
     };
