46db617431ae09f0f2d469ffee57428c50f68691
nifeng <nifeng@howzhi.com>
2015-07-17 11:15:44 +0800

    fix delete lesson bug

--- src/Topxia/Service/User/Job/DeleteSessionJob.php
+++ src/Topxia/Service/User/Job/DeleteSessionJob.php
@@ -9,7 +9,7 @@ class DeleteSessionJob implements Job
     public function execute($params)
     {
       $retentionTime = time()-7200;
-      $limit = 100;
+      $limit = 500;
       $number = $this->getSessionService()->deleteInvalidSession($retentionTime, $limit);
     }
 
