f94856c546a1c6fb767b6f458d6fa2e9dc2e5632
nifeng <nifeng@howzhi.com>
2015-07-21 01:36:53 +0800

    install

--- web/install/start-install.php
+++ web/install/start-install.php
@@ -176,11 +176,11 @@ function install_step3()
         $init->initNavigations();
         $init->initBlocks();
         $init->initThemes();
-        $init->initLockFile();
         $init->initRefundSetting();
         $init->initArticleSetting();
         $init->initDefaultSetting();
         $init->initCrontabJob();
+        $init->initLockFile();
 
         header("Location: start-install.php?step=4");
         exit();
@@ -287,11 +287,43 @@ function _create_database($config, $replace)
             return "创建数据库表结构失败，请删除数据库后重试！";
         }
 
+        if(!empty($config["database_replace"]) && $config["database_replace"]) {
+            init_data($pdo,$config);
+        }
+
         return null;
 
     } catch (\PDOException $e) {
         return '数据库连接不上，请检查数据库服务器、用户名、密码是否正确!';
     }
+}
+
+function init_data($pdo, $config)
+{
+    $sql = file_get_contents('./edusoho_init.sql');
+    $result = $pdo->exec($sql);
+
+    $sql = "show tables";
+    $results = $pdo->query($sql)->fetchAll();
+    foreach ($results as $result) {
+        $table = $result["0"];
+        $countSql = "select count(*) from {$table}";
+        $sqlPdo = $pdo->query($countSql);
+        if(!empty($sqlPdo)) {
+            $count = $pdo->query($countSql)->fetchColumn(0);
+            if($count>0) {
+                $pdo->exec("alter table {$table} AUTO_INCREMENT={$count};");
+            }
+        }
+    }
+
+    _create_config($config);
+
+    $init = new SystemInit();
+    $init->initLockFile();
+
+    header("Location: start-install.php?step=4");
+    exit();
 }
 
 function _create_config($config)
