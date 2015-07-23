65ac4cc7c9f75afa3980e0e7b7a3ef707e6ea6d8
ketuzhong <zhongjie@howzhi.com>
2015-07-23 20:56:36 +0800

    js移位置

--- src/Topxia/WebBundle/Resources/public/js/app.js
+++ src/Topxia/WebBundle/Resources/public/js/app.js
@@ -162,23 +162,4 @@ define(function(require, exports, module) {
         $(this).prop("placeholder", "搜索").removeClass("active");
     });
 
-    $(".es-qrcode").click(function(){
-        var $this = $(this); 
-        if($this.hasClass('open')) {
-            $this.removeClass('open');
-        }else {
-            $.ajax({
-                type: "post",
-                url: $this.data("url"),
-                dataType: "json",
-                success:function(data){
-                    $this.find(".qrcode-popover img").attr("src",data.img);
-                }
-            });
-            
-            $(this).addClass('open');
-        }
-    })
-
-
 });
