45d2af4ecbbb1671aa98ff410d5fbe06390a65a6
sam <shen.yf@gmail.com>
2015-07-27 11:45:06 +0800

    实现周期开课显示页面

--- src/Custom/WebBundle/Controller/CourseController.php
+++ src/Custom/WebBundle/Controller/CourseController.php
@@ -11,23 +11,22 @@ class CourseController extends BaseCourseController
 {
     public function showAction(Request $request, $id)
     {
-		list ($course, $member) = $this->buildCourseLayoutData($request, $id);
-		if(empty($member)) {
-			$user = $this->getCurrentUser();
-			$member = $this->getCourseService()->becomeStudentByClassroomJoined($id, $user->id);
-			if(isset($member["id"])) {
-				$course['studentNum'] ++ ;
-			}
+	list ($course, $member) = $this->buildCourseLayoutData($request, $id);
+	if(empty($member)) {
+		$user = $this->getCurrentUser();
+		$member = $this->getCourseService()->becomeStudentByClassroomJoined($id, $user->id);
+		if(isset($member["id"])) {
+			$course['studentNum'] ++ ;
 		}
+	}
 
-		$this->getCourseService()->hitCourse($id);
-
+	$this->getCourseService()->hitCourse($id);
         $items = $this->getCourseService()->getCourseItems($course['id']);
 
-		return $this->render("CustomWebBundle:Course:{$course['type']}-show.html.twig", array(
-			'course' => $course,
-			'member' => $member,
-			'items' => $items,
-		));
+	return $this->render(($course['type']=='periodic') ? "CustomWebBundle:Course:{$course['type']}-show.html.twig" : "TopxiaWebBundle:Course:{$course['type']}-show.html.twig", array(
+		'course' => $course,
+		'member' => $member,
+		'items' => $items,
+	));
     }
 }
