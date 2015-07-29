a85453a5fc5aa45f5367cbf5ff4b4d168b01ad54
wuli <wuli@howzhi.com>
2015-07-29 16:07:14 +0800

    css

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,15 +13,15 @@ fix_gpc_magic();
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-) {
-  if (!file_exists(__DIR__ . '/../app/data/dev.lock')) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-  }
-}
+// if (isset($_SERVER['HTTP_CLIENT_IP'])
+//     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+// ) {
+//   if (!file_exists(__DIR__ . '/../app/data/dev.lock')) {
+//     header('HTTP/1.0 403 Forbidden');
+//     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//   }
+// }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
