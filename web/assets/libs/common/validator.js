78085cf14ba96bcd59bdc44b2c844be0bf38310e
wuli <wuli@howzhi.com>
2015-07-31 15:00:16 +0800

    css

--- web/assets/libs/common/validator.js
+++ web/assets/libs/common/validator.js
@@ -11,8 +11,13 @@ define(function(require, exports, module) {
             inputClass: "input-with-feedback",
             textareaClass: "input-with-feedback",
             showMessage: function(message, element) {
+                 this.element.css("display","block");
                 message = '<span class="text-danger">' + message + '</span>';
+                console.log(message);
                 this.getExplain(element).html(message).show();
+                if(message=="") {
+                    console.log("sdfsdf");
+                }
                 this.getItem(element).addClass(this.get("itemErrorClass"));
             }
         },
@@ -20,10 +25,13 @@ define(function(require, exports, module) {
             var item = this.getItem(ele);
             ele = $(ele);
             var explain = ele.parents('.controls').find("." + this.get("explainClass"));
-
+           
             if (explain.length == 0) {
                 var explain = $('<div class="' + this.get("explainClass") + '" style="display:none;"></div>').appendTo(ele.parents('.controls'));
             }
+
+            $(explain).css("display", "none");
+
             return explain;
         }
     });
