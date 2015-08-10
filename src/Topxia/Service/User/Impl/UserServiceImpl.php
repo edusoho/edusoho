ffb3754e84adf01aed0fa87cbaabd26200d3b438
danyue <wangdanyue@howzhi.com>
2015-08-10 17:16:59 +0800

    update-approval-status-fix

--- src/Topxia/Service/User/Impl/UserServiceImpl.php
+++ src/Topxia/Service/User/Impl/UserServiceImpl.php
@@ -1114,7 +1114,7 @@ class UserServiceImpl extends BaseService implements UserService
             array(
             'userId'=> $user['id'],
             'note'=> $note,
-            'status' => 'approved',
+            //'status' => 'approved',
             'operatorId' => $currentUser['id'])
         );
 
