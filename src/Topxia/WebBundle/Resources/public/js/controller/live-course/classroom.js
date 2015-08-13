67907d1ab1feb1230441af37c58cf9716eadb7cc
nifeng <fengni@fengdeMBP.lan>
2015-08-13 19:47:47 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/live-course/classroom.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/live-course/classroom.js
@@ -15,6 +15,13 @@ define(function(require, exports, module) {
 			$.ajax({
 				url: $("#classroom-url").data("url"),
 				success: function(data){
+					if(data.error){
+						clearInterval(intervalId);
+						var html = data.errorMsg+"，<a href='javascript:document.location.reload()'>重试</a>或<a href='javascript:window.close();'>关闭</a>"
+						$("#classroom-url").html(html);
+						return;
+					}
+
 					if(data.url) {
 						var url = data.url;
 						if(data.param) {
