c488e9d76013e1289a170f4ec82ff11d7fdef8d4
xb <xiangbin@howzhi.com>
2015-08-19 17:19:04 +0800

    人人网接入帮助地址修改

--- src/Topxia/Component/OAuthClient/OAuthClientFactory.php
+++ src/Topxia/Component/OAuthClient/OAuthClientFactory.php
@@ -66,7 +66,7 @@ class OAuthClientFactory
                 'large_icon_img' => 'assets/img/social/renren.gif',
                 'key_setting_label' => 'App Key',
                 'secret_setting_label' => 'App Secret',
-                'apply_url' => 'http://wiki.dev.renren.com/wiki/WEB%E7%BD%91%E7%AB%99%E5%B8%90%E5%8F%B7%E7%99%BB%E5%BD%95%E5%85%A5%E9%97%A8%E6%95%99%E7%A8%8B'
+                'apply_url' => 'http://dev.renren.com/website'
             ),
             'weixinweb' => array(
                 'name' => '微信网页登录接口',
