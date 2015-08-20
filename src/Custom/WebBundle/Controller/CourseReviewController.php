ffcd7afd4270ca377bc4ab2f5f18d45688f7f8cc
morana <morana@moranadeMacBook-Pro.local>
2015-08-20 17:29:23 +0800

    点评结果

--- src/Custom/WebBundle/Controller/CourseReviewController.php
+++ src/Custom/WebBundle/Controller/CourseReviewController.php
@@ -7,12 +7,9 @@ use Topxia\Common\Paginator;
 use Topxia\WebBundle\Form\ReviewType;
 use Topxia\WebBundle\Controller\CourseReviewController as CourseReviewBaseController;
 
-<<<<<<< HEAD
 /**
  * 作业评价控制器.
 **/
-=======
->>>>>>> course_homepage_fix
 class CourseReviewController extends CourseReviewBaseController
 {
 
