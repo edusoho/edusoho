2764580979e97fdd4f254162754cdb3d8f21bc12
Stanley Sun <sunminghui@howzhi.com>
2015-08-20 20:11:43 +0800

    add upgrade 6.5.0

---
+++ upgrade-scripts/upgrade-6.5.0.php
@@ -0,0 +1,120 @@
+<?php
+
+use Symfony\Component\Filesystem\Filesystem;
+use Topxia\Service\Common\ServiceKernel;
+use Topxia\Common\BlockToolkit;
+
+ class EduSohoUpgrade extends AbstractUpdater
+ {
+     public function update()
+     {
+         $this->getConnection()->beginTransaction();
+         try {
+             $this->updateConfig();
+             $this->updateScheme();
+             $this->getConnection()->commit();
+         } catch (\Exception $e) {
+             $this->getConnection()->rollback();
+             throw $e;
+         }
+
+         try {
+             $dir = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."../web/install");
+             $filesystem = new Filesystem();
+
+             if (!empty($dir)) {
+                 $filesystem->remove($dir);
+             }
+         } catch (\Exception $e) {
+         }
+
+         $developerSetting = $this->getSettingService()->get('developer', array());
+         $developerSetting['debug'] = 0;
+
+         ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
+         ServiceKernel::instance()->createService('Crontab.CrontabService')->setNextExecutedTime(time());
+    }
+
+   
+    private function updateConfig()
+    {
+        $filePath = $this->kernel->getParameter('kernel.root_dir').'/config/parameters.yml';
+        $fileContent = file_get_contents($filePath);
+        $yaml = new Yaml();
+        $config = $yaml->parse($fileContent);
+
+        if(!isset($config['parameters']['database_port']) || !is_numeric($config['parameters']['database_port'])){
+            $config['parameters']['database_port'] = 3306;
+            $content = $yaml->dump($config);
+            $fh = fopen($filePath,"w");
+            fwrite($fh,$content);
+            fclose($fh);
+        }
+            
+    }
+    private function updateScheme()
+    {
+            
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    
+    
+    protected function isCrontabJobExist($code)
+    {
+        $sql = "select * from crontab_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+    
+    
+
+     private function getSettingService()
+     {
+         return ServiceKernel::instance()->createService('System.SettingService');
+     }
+
+
+ }
+
+ abstract class AbstractUpdater
+ {
+    protected $kernel;
+     public function __construct($kernel)
+     {
+         $this->kernel = $kernel;
+     }
+
+     public function getConnection()
+     {
+         return $this->kernel->getConnection();
+     }
+
+     protected function createService($name)
+     {
+         return $this->kernel->createService($name);
+     }
+
+     protected function createDao($name)
+     {
+         return $this->kernel->createDao($name);
+     }
+
+   
+
+     abstract public function update();
+ }
