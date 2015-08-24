aa59aa0ec3c3a822c285b8408146be678b7a14bd
danyue <wangdanyue@howzhi.com>
2015-08-24 16:31:06 +0800

    CourseServiceImpl-fix

--- src/Topxia/Service/Course/Impl/CourseServiceImpl.php
+++ src/Topxia/Service/Course/Impl/CourseServiceImpl.php
@@ -1473,7 +1473,7 @@ class CourseServiceImpl extends BaseService implements CourseService
 
 		$this->dispatchEvent(
 			'task.finished', 
-			new ServiceEvent($lesson, array('type'=>'studyplan', 'userId'=>$member['userId']))
+			new ServiceEvent($lesson, array('taskType'=>'studyPlan', 'userId'=>$member['userId']))
 		);
 	}
 
