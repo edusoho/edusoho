b6c9d180666b93433954e3c8950108ad69e41aad
laisancun <laisancun@howzhi.com>
2015-08-26 15:55:17 +0800

    删除移除班级课程移除操作

--- src/Topxia/Service/Course/Impl/CourseDeleteServiceImpl.php
+++ src/Topxia/Service/Course/Impl/CourseDeleteServiceImpl.php
@@ -10,42 +10,16 @@ class CourseDeleteServiceImpl extends BaseService implements CourseDeleteService
 {
 	public function delete($courseId)
     {
-    	$course = $this->getCourseService()->getCourse($courseId);
-    	$this->getClassroomDao()->getConnection()->beginTransaction();
-    	try{
-    		$this->removeClassroomCourse($course['id']);
-    		$this->getClassroomDao()->getConnection()->commit();
-    	} catch (\Exception $e) {
-    		$this->getClassroomDao()->getConnection()->rollback();
-            throw $e;
-    	}
-    	
+    	    	
     	$this->getCourseDao()->getConnection()->beginTransaction();
     	try{
-    		
+    		$course = $this->getCourseService()->getCourse($courseId);
     	} catch (\Exception $e) {
 
     		$this->getCourseDao()->getConnection()->rollback();
             
             throw $e;
     	}
-    }
-
-    protected function removeClassroomCourse($courseId)
-    {
-    	$classroomIds = $this->getClassroomService()->findClassroomIdsByCourseId($courseId);
-    	$classroom_courses = $this->getClassroomService()->findCoursesByClassroomId($classroomIds[0]);
-    	$classroom_coursesIds = ArrayToolkit::column($classroom_courses,"id");
-    	if (in_array($courseId, $classroom_coursesIds)) {
- 			$key = array_search($courseId, $classroom_coursesIds);
- 			unset($classroom_coursesIds[$key]);
-    	}
-    	$this->getClassroomService()->updateClassroomCourses($classroomIds[0],$classroom_coursesIds);
-    }
-
-    protected function getClassroomService()
-    {
-        return $this->createService('Classroom:Classroom.ClassroomService');
     }
 
     protected function getCourseService()
