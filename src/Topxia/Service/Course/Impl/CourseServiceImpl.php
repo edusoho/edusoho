805a7c799fbed5caa4cc0bf4c4b8841db32d886c
zhanggaosheng <245085167@howzhi.com>
2015-08-31 11:25:18 +0800

    fix

--- src/Topxia/Service/Course/Impl/CourseServiceImpl.php
+++ src/Topxia/Service/Course/Impl/CourseServiceImpl.php
@@ -2534,7 +2534,7 @@ class CourseServiceImpl extends BaseService implements CourseService
 		$classroom = $this->getClassroomService()->findClassroomByCourseId($course['id']);
 		if ($classroom['classroomId']) {
 			$member = $this->getClassroomService()->getClassroomMember($classroom['classroomId'], $userId);
-			if (!empty($member) && $member['role'] != 'auditor') {
+			if (!empty($member) && !in_array('auditor', $member['role'])) {
 				return true;
 			}
 		}
