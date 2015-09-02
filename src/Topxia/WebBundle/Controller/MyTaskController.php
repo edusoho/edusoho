ac6fb4f5178f303ae7cca20a3fea4d49ee71454e
wengzheqing <wengzheqing@howzhi.com>
2015-09-02 10:45:45 +0800

    keep in

---
+++ src/Topxia/WebBundle/Controller/MyTaskController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Topxia\WebBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Topxia\Common\Paginator;
+use Topxia\Common\ArrayToolkit;
+
+class MyTaskController extends BaseController
+{
+	public function indexAction(Request $request){
+		$user = $this->getCurrentUser();
+		if($user->isTeacher())
+		return $this->render('TopxiaWebBundle:MyTask:index.html.twig', array(
+            'user'=>$user,
+            ));
+		
+	}
+	protected function getTaskService()
+	{
+		return $this->getServiceKernel()->createService('Task.TaskService');
+	}
+}
