684a02aa903144e9b601fc1b448aeac55bd383d3
wengzheqing <wengzheqing@howzhi.com>
2015-09-08 15:05:30 +0800

    over4

--- src/Classroom/ClassroomBundle/Resources/public/js/controller/classroom-admin/classroom-create.js
+++ src/Classroom/ClassroomBundle/Resources/public/js/controller/classroom-admin/classroom-create.js
@@ -1,7 +1,20 @@
 define(function(require, exports, module) {
 
     exports.run = function() {
-        var showable = document.getElementsByName("showable");
+        if($("#create-classroom").val() != ''){
+            if($("#showable-open").data('showable')==1){
+                $("#showable-open").attr('checked','checked');
+            }
+            else{
+                $("#showable-close").attr('checked','checked');
+            }
+            if($("#buyable-open").data('buyable')==1){
+                $("#buyable-open").attr('checked','checked');
+            }
+            else{
+                $("#buyable-close").attr('checked','checked');
+            }
+        }
         $("#showable-close").click(function(){
             $("#buyable-open").attr('disabled','disabled');
             $("#buyable-close").attr('disabled','disabled');
@@ -10,6 +23,5 @@ define(function(require, exports, module) {
             $("#buyable-open").removeAttr('disabled');
             $("#buyable-close").removeAttr('disabled');
         })
-        
 	}
 });
