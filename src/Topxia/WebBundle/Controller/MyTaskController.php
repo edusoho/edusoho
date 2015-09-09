3aeb662f508fa5be705d54de664b61785c4778cc
wengzheqing <wengzheqing@howzhi.com>
2015-09-09 13:59:56 +0800

    keepin

--- src/Topxia/WebBundle/Controller/MyTaskController.php
+++ src/Topxia/WebBundle/Controller/MyTaskController.php
@@ -9,14 +9,21 @@ class MyTaskController extends BaseController
 {
 	public function indexAction(Request $request){
 		$user = $this->getCurrentUser();
-		$condition=array(
+		$conditions=array(
 			'userId' => $user['id']
 			);
-		$tasks = $this->getTaskService()->serchTasks($condition,,0,9999)
-		if($user->isTeacher())
+		$tasks = $this->getTaskService()->searchTasks($conditions,array('taskStartTime','DESC'),0,9999);
+		foreach($task as $key =>$tasks){
+			$task['createTime']=$task['createTime'].date(”Y-m-d H:i:s”,time());
+		}
+		$jsontasks=json_encode($tasks);
+		//var_dump($jsontasks);
+		//exit();
+		if($user->isTeacher()){
 		return $this->render('TopxiaWebBundle:MyTask:index.html.twig', array(
             'user'=>$user,
             ));
+		}
 		
 	}
 	protected function getTaskService()
