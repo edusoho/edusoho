12edcf71b6ba881eae8710e5f4e06e97ca65c063
lvliujie <lvliujie@howzhi.com>
2015-09-10 10:06:19 +0800

    ＡＰＩ紧急修复

---
+++ api/src/Topxia/Api/Filter/AnnouncementFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Topxia\Api\Filter;
+
+class AnnouncementFilter implements Filter
+{
+	//输出前的字段控制
+    //查看权限,附带内容可以写在这里
+    public function filter(array &$data)
+    {
+        $data['createdTime'] = date('c', $data['createdTime']);
+        $data['updatedTime'] = date('c', $data['updatedTime']);
+        $data['startTime'] = date('c', $data['startTime']);
+        $data['endTime'] = date('c', $data['endTime']);
+
+        return $data;
+    }
+
+    public function filters(array &$datas)
+    {
+        $num = 0;
+        $results = array();
+        foreach ($datas as $data) {
+            $results[$num] = $this->filter($data);
+            $num++;
+        }
+        return $results;
+    }
+
+}
+
