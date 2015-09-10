89b2a2d3b026d86fccce2f7f7b40989a32b92e54
wengzheqing <wengzheqing@howzhi.com>
2015-09-10 16:37:25 +0800

    keepin

--- src/Topxia/WebBundle/Controller/MyTaskController.php
+++ src/Topxia/WebBundle/Controller/MyTaskController.php
@@ -10,18 +10,41 @@ class MyTaskController extends BaseController
 	public function indexAction(Request $request){
 		$user = $this->getCurrentUser();
 		$conditions=array(
-			'userId' => $user['id']
+			'userId' => $user['id'],
+			'status' => 'active'
 			);
 		$tasks = $this->getTaskService()->searchTasks($conditions,array('taskStartTime','DESC'),0,9999);
-		foreach($task as $key =>$tasks){
-			$task['createTime']=$task['createTime'].date(”Y-m-d H:i:s”,time());
+		$tasksevents=array(array(
+			));
+		if($tasks){
+			foreach($tasks as $key => $task){
+				$tasksevents[$key]['title'] = $task['title'];
+				$tasksevents[$key]['start'] = date("Y-m-d H:i:s",$task['taskStartTime']);
+				$tasksevents[$key]['end'] = date("Y-m-d H:i:s",$task['taskEndTime']);
+				switch($task['targetType']){
+					case  'testpaper':
+					$tasksevents[$key]['url'] = $this->generateUrl('course_manage_do_test',array(
+						'testId'=>$task['targetId']));
+					break;
+					case  'text':
+					$tasksevents[$key]['url'] = $this->generateUrl('course_manage_do_test',array(
+						'testId'=>$task['targetId']));
+					break;
+				}
+
+
+				
+				if($task['targetType'] = 'testpaper'){
+				$tasksevents[$key]['url'] = $this->generateUrl('course_manage_do_test',array(
+					'testId'=>$task['targetId']));
+				}
+			}
 		}
-		$jsontasks=json_encode($tasks);
-		//var_dump($jsontasks);
-		//exit();
+		$jsontasks = json_encode($tasksevents);
 		if($user->isTeacher()){
 		return $this->render('TopxiaWebBundle:MyTask:index.html.twig', array(
-            'user'=>$user,
+            'user' => $user,
+            'taskjson' => $jsontasks,
             ));
 		}
 		
