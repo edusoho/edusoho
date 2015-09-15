ae06e82188354bca26a4daeafb6dcd2dcbc5778e
wengzheqing <wengzheqing@howzhi.com>
2015-09-15 16:48:51 +0800

    mytask

---
+++ src/Topxia/WebBundle/Resources/public/js/controller/mytask/fullcalendar.js
@@ -0,0 +1,32 @@
+define(function(require, exports, module) {
+	require('fullcalendar');
+	require('momentmin');
+	//require('json');
+	exports.run = function() {
+		var datajson=$('#datajson').data('fullcalendarevents');
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month'
+			},
+			buttonText: {    
+                today: '今天',
+                month: '月',
+            },
+			lang:'zh-cn',
+			defaultDate: '2015-09-10',
+			businessHours: true, // display business hours
+			editable: false,
+			eventLimit: true,
+			theme: false,
+			events: datajson,
+			eventClick: function(event,element) {
+        		if (event.url) {
+	    			window.open(event.url);
+	    			return false;
+        		}
+        	},
+		});
+	};
+});
