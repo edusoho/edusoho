0fb36d7b44c62f5614e8f1f995e1f8c564bb046c
zhanggaosheng <245085167@howzhi.com>
2015-09-16 11:59:48 +0800

    fix

--- src/Topxia/Component/Payment/Wxpay/WxpayResponse.php
+++ src/Topxia/Component/Payment/Wxpay/WxpayResponse.php
@@ -15,7 +15,7 @@ class WxpayResponse extends Response
         $data['payment'] = 'wxpay';
         $data['sn'] = $params['out_trade_no'];
         $result = $this->confirmSellerSendGoods($data['sn']);
-        $returnArray = $this->fromXml($returnXml);
+        $returnArray = $this->fromXml($result);
         if ($returnArray['return_code'] != 'SUCCESS' || $returnArray['result_code'] != 'SUCCESS' || $returnArray['trade_state'] != 'SUCCESS') {
             throw new \RuntimeException('微信支付失败');
         }
