01b4236984919b6b329486fba2dd0289e6826a93
wengzheqing <wengzheqing@howzhi.com>
2015-09-18 15:37:35 +0800

    修改为Arale框架

--- src/Topxia/WebBundle/Resources/public/js/controller/mytask/fullcalendar.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/mytask/fullcalendar.js
@@ -1,34 +1,54 @@
 define(function(require, exports, module) {
 	require('fullcalendar');
 	require('momentmin');
-	//require('json');
+	var Widget = require('widget');
 	exports.run = function() {
 		var datajson=$('#datajson').data('fullcalendarevents');
 		var today=$('#datajson').data('today');
-		$('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month'
+		var $calendar = $('#calendar');
+		var Cal  = Widget.extend({
+			attr:{
 			},
-			buttonText: {    
-                today: '今天',
-                month: '月',
-            },
-			lang:'zh-cn',
-			allDayDefault:true,
-			defaultDate: today,
-			businessHours: true, // display business hours
-			editable: false,
-			eventLimit: true,
-			theme: false,
-			events: datajson,
-			eventClick: function(event,element) {
-        		if (event.url) {
-	    			window.open(event.url);
-	    			return false;
-        		}
-        	},
+			setup:function(){
+				var calendar = this.element.fullCalendar({
+					header: {
+						left: 'prev,next today',
+						center: 'title',
+						right: 'month'
+					},
+					buttonText: {    
+		                today: '今天',
+		                month: '月',
+		            },
+					lang:'zh-cn',
+					allDayDefault:true,
+					defaultDate: today,
+					businessHours: true, // display business hours
+					editable: false,
+					eventLimit: true,
+					theme: false,
+					events: datajson,
+					displayEventEnd: true,
+					resizable:true,
+					eventClick: function(event,element) {
+		        		if (event.url) {
+			    			window.open(event.url);
+			    			return false;
+		        		}
+		        	},
+		        	eventMouseover: function( event, jsEvent, view ) {
+		        	 	$(this).attr({'title': event.title,"data-toggle":"tooltip"});
+		        	},
+				});	
+			},
+			/*render:function(){
+				var cal = new Cal({
+            		element: $calendar
+        		});
+			}*/
 		});
+		var cal = new Cal({
+            element: $calendar
+        }).render();
 	};
 });
