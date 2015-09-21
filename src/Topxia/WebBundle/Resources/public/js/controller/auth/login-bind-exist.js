2247089a20eb2f81118edda03c2bf64d96002c54
xb <xiangbin@howzhi.com>
2015-09-21 09:59:45 +0800

    手机或邮箱JS验证规则

--- src/Topxia/WebBundle/Resources/public/js/controller/auth/login-bind-exist.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/auth/login-bind-exist.js
@@ -39,7 +39,7 @@ define(function(require, exports, module) {
         validator.addItem({
             element: '#bind-email-field',
             required: true,
-            rule: 'email'
+            rule: 'email_or_mobile'
         });
 
 
