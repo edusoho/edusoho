e4ea171965aaea682af80eb873cbe297a6b1f2ab
nifeng <fengni@fengdeMBP.lan>
2015-09-22 15:01:26 +0800

    fix

--- src/Topxia/WebBundle/Controller/PlayerController.php
+++ src/Topxia/WebBundle/Controller/PlayerController.php
@@ -3,21 +3,25 @@ namespace Topxia\WebBundle\Controller;
 
 use Topxia\Service\Util\CloudClientFactory;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlayerController extends BaseController
 {
-	public function showAction($id, $url)
+	public function showAction(Request $request, $id, $url)
 	{
-		$blacklistPath = $this->getServiceKernel()->getParameter('kernel.root_dir') . '/config/play_video_agent_blacklist.yml';
-		$blacklist = Yaml::parse(file_get_contents($blacklistPath));
-		var_dump($blacklist);
-
 		$file = $this->getUploadFileService()->getFile($id);
 		if(empty($file)){
 			throw $this->createNotFoundException();
 		}
 
 		if($file["storage"] == 'cloud' && $file["type"] == 'video') {
+			$blacklistPath = $this->getServiceKernel()->getParameter('kernel.root_dir') . '/config/play_video_agent_blacklist.yml';
+			$blacklist = Yaml::parse(file_get_contents($blacklistPath));
+			foreach ($blacklist["blacklist"] as $value) {
+				if(strpos($request->headers->get("user-agent"), $value)) {
+					return $this->render('TopxiaWebBundle:Player:show.html.twig', array());
+				}
+			}
 			if($this->setting("developer.balloon_player", 0)){
 		        $player = "balloon-cloud-video-player";
 			} else {
