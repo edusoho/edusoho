c99049dd8c73c99608c70d7c81ba0a01cc7cae76
zhanggaosheng <245085167@howzhi.com>
2015-09-22 11:57:47 +0800

    fix

--- src/Topxia/WebBundle/Controller/EduCloudController.php
+++ src/Topxia/WebBundle/Controller/EduCloudController.php
@@ -171,6 +171,7 @@ class EduCloudController extends BaseController
         $url .= '?index='.$index.'&token='.$originToken.'&smsType='.$smsType;
         $api = CloudAPIFactory::create('leaf');
         $sign = $this->getPasswordEncoder()->encodePassword($url, $api->getAccessKey());
+        file_put_contents('test.txt', $sign.'===='.$originSign, FILE_APPEND);
         if ($originSign != $sign) {
             throw new \RuntimeException('sign不正确');
         }
