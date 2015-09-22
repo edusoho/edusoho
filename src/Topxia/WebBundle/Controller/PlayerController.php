4d62c7cd8e8b4b75d987f182402e024459066d58
nifeng <fengni@fengdeMBP.lan>
2015-09-22 14:42:22 +0800

    fix

--- src/Topxia/WebBundle/Controller/PlayerController.php
+++ src/Topxia/WebBundle/Controller/PlayerController.php
@@ -2,11 +2,16 @@
 namespace Topxia\WebBundle\Controller;
 
 use Topxia\Service\Util\CloudClientFactory;
+use Symfony\Component\Yaml\Yaml;
 
 class PlayerController extends BaseController
 {
 	public function showAction($id, $url)
 	{
+		$blacklistPath = $this->getServiceKernel()->getParameter('kernel.root_dir') . '/config/play_video_agent_blacklist.yml';
+		$blacklist = Yaml::parse(file_get_contents($blacklistPath));
+		var_dump($blacklist);
+
 		$file = $this->getUploadFileService()->getFile($id);
 		if(empty($file)){
 			throw $this->createNotFoundException();
