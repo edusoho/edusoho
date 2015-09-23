578b5075a426443a0427befc9fc3f6d2043105bc
ketuzhong <zhongjie@howzhi.com>
2015-09-23 11:45:47 +0800

    修复#8096 首页幻灯片

--- src/Topxia/WebBundle/Resources/public/js/controller/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/index.js
@@ -5,18 +5,20 @@ define(function(require, exports, module) {
     var Swiper = require('swiper');
 
     exports.run = function() {
-        var swiper = new Swiper('.es-poster.swiper-container', {
-            pagination: '.swiper-pager',
-            paginationClickable: true,
-            autoplay: 5000,
-            autoplayDisableOnInteraction: false,
-            loop: true,
-            calculateHeight: true,
-            roundLengths: true,
-            onInit: function(swiper) {
-               $(".swiper-slide").removeClass('swiper-hidden'); 
-            }
-        });
+        if ($(".es-poster .swiper-slide").length > 1) {
+            var swiper = new Swiper('.es-poster.swiper-container', {
+                pagination: '.swiper-pager',
+                paginationClickable: true,
+                autoplay: 5000,
+                autoplayDisableOnInteraction: false,
+                loop: true,
+                calculateHeight: true,
+                roundLengths: true,
+                onInit: function(swiper) {
+                   $(".swiper-slide").removeClass('swiper-hidden'); 
+                }
+            });
+        }
         Lazyload.init();
 
         $("#course-list").on('click','.js-course-filter',function(){
