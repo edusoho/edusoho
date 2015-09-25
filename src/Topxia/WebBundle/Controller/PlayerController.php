ea3fca42b7dde29ba0911da8c54b05669490449d
nifeng <fengni@www.nifeng.com>
2015-09-25 10:17:33 +0800

    fix

--- src/Topxia/WebBundle/Controller/PlayerController.php
+++ src/Topxia/WebBundle/Controller/PlayerController.php
@@ -23,14 +23,6 @@ class PlayerController extends BaseController
                 $file['videoWatermarkEmbedded'] = 1;
             }
 			
-			$blacklistPath = $this->getServiceKernel()->getParameter('kernel.root_dir') . '/config/play_video_agent_blacklist.yml';
-			$blacklist = Yaml::parse(file_get_contents($blacklistPath));
-			foreach ($blacklist["blacklist"] as $value) {
-				if(strpos($request->headers->get("user-agent"), $value)) {
-					return $this->render('TopxiaWebBundle:Player:show.html.twig', array());
-				}
-			}
-
 			if($this->setting("developer.balloon_player", 0)){
 		        $player = "balloon-cloud-video-player";
 			} else {
