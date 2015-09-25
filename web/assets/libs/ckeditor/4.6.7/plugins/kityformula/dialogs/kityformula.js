73409cd2a7ec37f8a8755a1112d7d4648a1f3b8c
lvliujie <lvliujie@howzhi.com>
2015-09-25 11:48:34 +0800

    使用iframe方式规避kityformular的实例冲突问题

--- web/assets/libs/ckeditor/4.6.7/plugins/kityformula/dialogs/kityformula.js
+++ web/assets/libs/ckeditor/4.6.7/plugins/kityformula/dialogs/kityformula.js
@@ -1,139 +1,71 @@
-﻿(function() {
-	CKEDITOR.dialog.add("kityformula",function(editor) {
-		var html='<div id="kfEditorContainer" class="kf-editor" style="width: 750px; height: 502px;"></div>';
-		var isIE=!-[1,];
-		if (isIE) {
-			html='<div class="formulaOld" style="width:750px;height:350px;"><div class="tips" style="color:red;line-height:40px;">请升级您的浏览器到IE9版本及以上版本，或使用chrome浏览器使用可视化公式编辑器</div><textarea id="oldFormula" style="width:100%;height:240px;border:1px solid #ccc;"></textarea></div>';
-		}
-		return {
-			title:'公式编辑器',
-			contents: [{
-				id: 'kityformula',
-				label: '公式编辑器',
-				title: '公式编辑器',
-				expand: true,
-				padding: 0,
-				elements: [{
-					type: "html",
-					html:html
-				}]
-			}],
-			onHide: function(){
-				$("#cke_"+editor.name).data("source","");
-			},
-			onShow: function() {
-				var source=$("#cke_"+editor.name).data("source");
-				if(isIE){
-					$("#oldFormula").val(source);
-					return false;
-				}
-				if(!source){
-					source='\\placeholder';
-				}
-				if(window.kfe){
-					window.kfe.execCommand( "render", source);
-				}
-			},
-			onOk: function() {
-				var source;
-				if(isIE){
-					source=$("#oldFormula").val();
-				}else{
-					source=kfe.execCommand( "get.source" );
-					if ($.trim(source) == "\\placeholder"){
-						return;
-					}
-				}
-				source = kfe.replaceSpecialCharacter(source);
-				var $imgUrl = 'http://formula.edusoho.net/cgi-bin/mimetex.cgi?'+source;
-				$.post($('#'+editor.name).data('imageDownloadUrl')+'&url='+$imgUrl, function(result){
-					var insertHtml='<img kityformula="true" src="'+result+'" alt="'+source+'">';
-					editor.insertHtml(insertHtml);
-				});
-			},
-			onLoad:function(){
-				if(!isIE){
-					var ready=function(){}
-					ready.prototype={
-						js:editor.kityFormuaPath+"libs/js/kityformula.js",
-						css:editor.kityFormuaPath + 'libs/css/base.css',
-						loadCSS:function(url,fn){
-							var link = document.createElement('link');
-							link.type = 'text/css';
-							link.rel = 'stylesheet';
-							var isLoad = false;
-							link.onload = function() {
-								if(!isLoad){
-									isLoad = true;
-									fn&&fn();
-								}
-							};
-							setTimeout(function(){
-								if(!isLoad){
-									isLoad = true;
-									fn&&fn();
-								}
-							},1000);
+﻿(function () {
+    function KityformulaDialog(editor) {
+        var isIE=!-[1,];
+        if (isIE) {
+            html='<div class="formulaOld" style="width:750px;height:350px;"><div class="tips" style="color:red;line-height:40px;">请升级您的浏览器到IE9版本及以上版本，或使用chrome浏览器使用可视化公式编辑器</div><textarea id="oldFormula" style="width:100%;height:240px;border:1px solid #ccc;"></textarea></div>';
+        }
+        html = '<iframe scrolling="no" id="editorContainer_'+editor.name+'" src="/assets/libs/ckeditor/4.6.7/plugins/kityformula/kityformula/index.html" width="780" height="500"></iframe>';
+        
+        return {
+            title: '公式编辑器',
+            minWidth: 300,
+            minHeight: 80,
+            buttons: [
+            CKEDITOR.dialog.okButton,
+            CKEDITOR.dialog.cancelButton],
+            contents: [{
+                id: 'kityformula',
+                label: '公式编辑器',
+                title: '公式编辑器',
+                expand: true,
+                padding: 0,
+                elements: [{
+                    type: "html",
+                    html:html
+                }]
+            }],
+            onLoad: function () {
+                // alert('onLoad');
+            },
+            onShow: function () {
+                if(isIE){
+                    $("#oldFormula").val(source);
+                    return false;
+                }
+                var kfe = $("#editorContainer_"+editor.name)[0].contentWindow.kfe;
+                if(kfe){
+                    kfe.execCommand( "render", '\\placeholder');
+                }
+            },
+            onHide: function () {
+                // alert('onHide');
+            },
+            onOk: function () {
 
-							link.href = url;
-							document.getElementsByTagName('head')[0].appendChild(link);
-						},
-						loadJS:function(url,fn){
-							var script = document.createElement('script');
-							script.type = 'text/javascript';
-							script.charset = 'UTF-8';
-
-							if (script.readyState){  //IE
-								script.onreadystatechange = function(){
-									if (script.readyState == "loaded" || script.readyState == "complete"){
-										script.onreadystatechange = null;
-										fn&&fn();
-									}
-								};
-							} else {  //Others
-								script.onload = function(){
-									fn&&fn();
-								};
-							}
-
-							script.src = url;
-							document.getElementsByTagName('head')[0].appendChild(script);
-						},
-						initKityformula:function(){
-							var factory = kf.EditorFactory.create($("#kfEditorContainer")[0], {
-								render: {
-									fontsize: 40
-								},
-								resource: {
-									path: editor.kityFormuaPath+"libs/fonts/"
-								},
-								toolbarPath:editor.kityFormuaPath+'libs/images/toolbar/'
-							});
-							factory.ready( function ( KFEditor ) {
-								var source=$("#cke_"+editor.name).data("source");
-								if(!source){
-									source='\\placeholder';
-								}
-								// this指向KFEditor
-								KFEditor.execCommand( "render",source);
-								KFEditor.execCommand( "focus" );
-								window.kfe = KFEditor;
-							});
-						},
-						init:function(){
-							var self=this;
-							self.loadCSS(self.css,function(){
-								self.loadJS(self.js,function(){
-									self.initKityformula();
-								});
-							});
-						}
-					}
-
-					var Ready = new ready();
-					Ready.init();
-				}
-			}
-		};
-	})
+                if(isIE){
+                    source = $("#oldFormula").val();
+                }else{
+                    var kfe = $("#editorContainer_"+editor.name)[0].contentWindow.kfe;
+                    source = kfe.execCommand( "get.source" );
+                    source = kfe.replaceSpecialCharacter(source);
+                    if ($.trim(source) == "\\placeholder"){
+                        return;
+                    }
+                    var $imgUrl = 'http://formula.edusoho.net/cgi-bin/mimetex.cgi?'+source;
+                    $.post($('#'+editor.name).data('imageDownloadUrl')+'&url='+$imgUrl, function(result){
+                        var insertHtml='<img kityformula="true" src="'+result+'" alt="'+source+'">';
+                        editor.insertHtml(insertHtml);
+                    });
+                }
+            },
+            onCancel: function () {
+                // alert('onCancel');
+            },
+            resizable: CKEDITOR.DIALOG_RESIZE_HEIGHT
+        };
+    }
+ 
+    CKEDITOR.dialog.add('kityformula', function (editor) {
+        return KityformulaDialog(editor);
+    });
 })();
--- web/assets/libs/ckeditor/4.6.7/plugins/kityformula/plugin.js
+++ web/assets/libs/ckeditor/4.6.7/plugins/kityformula/plugin.js
@@ -1,25 +1,13 @@
-﻿(function() {
-	CKEDITOR.plugins.add('kityformula', {
-		requires: ['dialog'],
-		init: function(editor) {
-			editor.kityFormuaPath=this.path;
-			editor.addCommand("kityformula", new CKEDITOR.dialogCommand("kityformula"));
-			editor.ui.addButton("kityformula", {
-				label: "公式编辑器",
-				command: "kityformula",
-				icon: this.path + "icons/kityformula.png"
-			});
-			CKEDITOR.dialog.add("kityformula", this.path + "dialogs/kityformula.js");
-			editor.on('doubleclick', function( evt ) {
-				var element = evt.data.element;
-				var kityformula=$(element).attr("kityformula");
-				var $editorEle=$("#cke_"+editor.name);
-				var source = $(element).attr("alt");
-				if ( kityformula=="true" && element.is( 'img' ) && !element.data( 'cke-realelement' ) && !element.isReadOnly() ){
-					$editorEle.data("source", source);
-					evt.data.dialog = 'kityformula';
-				}
-			});
-		}
-	});
-})();
+﻿CKEDITOR.plugins.add('kityformula', {
+    init: function (editor) {
+        var pluginName = 'kityformula';
+        CKEDITOR.dialog.add(pluginName, this.path + 'dialogs/kityformula.js');
+        editor.addCommand(pluginName, new CKEDITOR.dialogCommand(pluginName));
+        editor.ui.addButton(pluginName,
+        {
+            label: '公式编辑器',
+            command: pluginName,
+            icon: this.path + 'icons/kityformula.png'
+        });
+    }
+});
