0f4c3b18383a54c94311304df4b3dd44e573c7dd
lcj <lincanjun@howzhi.com>
2015-10-08 16:14:27 +0800

    修复bug已结束周期课程可以加入

--- web/themes/mooc/js/course-lesson/base.js
+++ web/themes/mooc/js/course-lesson/base.js
@@ -5,6 +5,10 @@ define(function(require, exports, module) {
         $('a.course-before-open').click(function(){
             Notify.danger("未到开课时间，等待开课。");
         })
+
+        $('a.course-ended').click(function(){
+            Notify.danger("本期课程已结束，请等待下次开课。");
+        })
     };
 
 });
