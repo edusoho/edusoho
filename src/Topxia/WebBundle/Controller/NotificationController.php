9e0c824b18958dbb0a149fb6f1e09308bea2b6d8
wengzheqing <wengzheqing@howzhi.com>
2015-10-13 17:05:02 +0800

    全站通知链接跳转

--- src/Topxia/WebBundle/Controller/NotificationController.php
+++ src/Topxia/WebBundle/Controller/NotificationController.php
@@ -36,6 +36,17 @@ class NotificationController extends BaseController
         ));
     }
 
+    public function showAction(Request $request,$id)
+    {
+        $batchnotification = $this->getBatchNotificationService()->getBatchNotificationById($id);
+        if (empty($batchnotification)) {
+            throw $this->createNotFoundException('通知已被管理员删除！');
+        }
+        return $this->render('TopxiaWebBundle:Notification:batch-notification-show.html.twig',array(
+            'batchnotification' => $batchnotification
+        ));
+    }
+
     protected function getCourseService()
     {
         return $this->getServiceKernel()->createService('Course.CourseService');
@@ -50,4 +61,8 @@ class NotificationController extends BaseController
     {
         return $this->getServiceKernel()->createService('User.NotificationService');
     }
+    protected function getBatchNotificationService()
+    {
+        return $this->getServiceKernel()->createService('User.BatchNotificationService');
+    }   
 }
