ca234f97282c99eb0b954267232363a2b4836a41
wengzheqing <wengzheqing@howzhi.com>
2015-10-20 16:51:50 +0800

    js逻辑修改

--- src/Topxia/AdminBundle/Resources/public/js/controller/batchnotification/notification-modal.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/batchnotification/notification-modal.js
@@ -11,51 +11,43 @@ define(function(require, exports, module) {
         $("#notification-operate-publish").click(function(){
             $('#hidden').val('publish');
         });
-        var $form = $("#notification-form");
-        $modal = $form.parents('.modal');
-        var validator = _initValidator($form, $modal);
-        var $editor = _initEditorFields($form, validator);
-    };
-    function _initEditorFields($form, validator) {
-
         // group: 'default'
-        CKEDITOR.replace('richeditor-body-field', {
+        var ckeditor=CKEDITOR.replace('richeditor-body-field', {
             toolbar: 'Full',
             filebrowserImageUploadUrl: $('#richeditor-body-field').data('imageUploadUrl'),
             filebrowserFlashUploadUrl: $('#richeditor-body-field').data('flashUploadUrl'),
             height: 300
         });
-    }
-    function _initValidator($form, $modal) {
         var validator = new Validator({
             element: '#notification-form',
-            failSilently: true,
-            triggerType: 'change',
-            onFormValidated: function(error, results, $form) {
-                if (error) {
-                    return false;
-                }
-                CKupdate();
-                if($('#hidden').val() == 'publish'){
-                    $('#notification-operate-publish').button('loading').addClass('disabled');
-                    Notify.success('发布成功！');
-                }
-                else{
-                    $('#notification-operate-save').button('loading').addClass('disabled');
-                    Notify.success('保存成功！');
-                }
-            }
         });
-
+        
+        validator.addItem({
+            element: '[name=content]',
+            required: true
+        });
         validator.addItem({
             element: '[name=title]',
             rule: 'maxlength{max:50}',
             required: true
         });
-
-        return validator;
-    }
-
+        validator.on('formValidate', function(elemetn, event) {
+            ckeditor.updateElement();
+        });
+        validator.on('formValidated', function(error, results, $form) {
+            if (error) {
+                return false;
+            }
+            if($('#hidden').val() == 'publish'){
+                $('#notification-operate-publish').button('loading').addClass('disabled');
+                Notify.success('发布成功！');
+            }
+            else{
+                $('#notification-operate-save').button('loading').addClass('disabled');
+                Notify.success('保存成功！');
+            }
+        });
+     };
     function CKupdate() {
     for ( instance in CKEDITOR.instances ) {
         CKEDITOR.instances[instance].updateElement(); 
