861d7150ce63c8195f9acbc2ad74fa120e69c3b7
danyue <wangdanyue@howzhi.com>
2015-10-21 11:41:49 +0800

    fix

--- app/DoctrineMigrations/Version20150513181253.php
+++ app/DoctrineMigrations/Version20150513181253.php
@@ -16,11 +16,14 @@ class Version20150513181253 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("ALTER TABLE `course_lesson` ADD COLUMN `suggestHours` float(10,1) unsigned NOT NULL DEFAULT '0.0' COMMENT '建议学习时长'");
-        $this->addSql("UPDATE `course_lesson` SET `suggestHours` = CEIL(length/3600) WHERE type IN('video','audio') AND length is not Null");
-        $this->addSql("UPDATE `course_lesson` SET `suggestHours` = 1 WHERE type IN('video','audio')  AND  length is Null");
-        $this->addSql("UPDATE `course_lesson` SET `suggestHours` = 2 WHERE type NOT IN('video','audio') AND  length is Null");
-        $this->addSql("UPDATE `course_lesson` SET `suggestHours` = CEIL(length/60) WHERE type IN('live') AND length is not Null");
+        if (!$this->isFieldExist('course_lesson', 'suggestHours')) {
+            $this->addSql("ALTER TABLE `course_lesson` ADD COLUMN `suggestHours` float(10,1) unsigned NOT NULL DEFAULT '0.0' COMMENT '建议学习时长'");
+            $this->addSql("UPDATE `course_lesson` SET `suggestHours` = CEIL(length/3600) WHERE type IN('video','audio') AND length is not Null");
+            $this->addSql("UPDATE `course_lesson` SET `suggestHours` = 1 WHERE type IN('video','audio')  AND  length is Null");
+            $this->addSql("UPDATE `course_lesson` SET `suggestHours` = 2 WHERE type NOT IN('video','audio') AND  length is Null");
+            $this->addSql("UPDATE `course_lesson` SET `suggestHours` = CEIL(length/60) WHERE type IN('live') AND length is not Null");
+        }
+        
     }
 
     /**
@@ -30,5 +33,13 @@ class Version20150513181253 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
 
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->connection->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
     }
 }
