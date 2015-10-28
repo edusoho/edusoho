08b4a6f4b6dc7d6c05b01c7fe9847a2826f9d365
bellorchid <zhongyunchang@yeah.net>
2015-10-28 09:10:26 +0800

    保存进度

--- src/Topxia/WebBundle/Controller/CardController.php
+++ src/Topxia/WebBundle/Controller/CardController.php
@@ -35,6 +35,41 @@ class CardController extends BaseController
         
     }
 
+    public function showUsableCardsAction($targetType,$targetId,$totalPrice,$priceType)
+    {
+        $user = $this->getCurrentUser();
+        if (!$targetType == 'coupon'){
+            return $this->createMessageResponse('error','您选择的卡的类型不正确，请重新选择');
+        }
+        $cardLists = $this->getCardService()->findCardsByUserIdAndCardType($user['id'],'coupon');
+        $cardIds = ArrayToolkit::column($cardLists,'cardId');
+        $cardsDetail = $this->getCardService->findCardsByCardTypeAndCardIds($cardIds,'coupon');
+        $usableCards = array();
+        foreach ($cardsDetail as $key => $item) {
+            if ($item['targetType'] == $targetType && $item['status'] = 'unused') {
+                if ($item['targetId'] == 0 || $item['targetId'] == $targetId ){
+                    $usableCards = array_merge($usableCards,array($key => $item));
+                }
+            }
+        }
+        $nowTime = time();
+        $diff = array();
+        foreach ($usableCards as $key => $item) {
+            $diff = abs($item['deadline'] - $nowTime);
+        }
+
+
+
+        return $this->render('TopxiaWebBundle:Order:order-item-coupon.html.twig',array(
+            'targetType' => $targetType,
+            'targetId' => $targetId,
+            'totalPrice' => $totalPrice,
+            'priceType' => $priceType
+            ));
+    }
+
+
+
     protected function getCardService() {
     	return $this->getServiceKernel()->createService('Card.CardService');
     }
