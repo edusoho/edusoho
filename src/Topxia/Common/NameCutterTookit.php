d8a24ebc451e8f47e4f341ece309dc4b74641397
zhanggaosheng <245085167@howzhi.com>
2015-10-29 09:46:07 +0800

    fix

--- src/Topxia/Common/NameCutterTookit.php
+++ src/Topxia/Common/NameCutterTookit.php
@@ -9,14 +9,7 @@ class NameCutterTookit
         $l=mb_strlen($name,'UTF-8');
         if ($l > $length) {
             $afterCutName = mb_substr($name, 0, $startNum, 'utf-8').'…';
-            for($i=0;$i<$l;$i++){
-                $array[]=mb_substr($name,$i,1,'UTF-8');
-            }
-            //反转字符串
-            krsort($array);
-            //拼接字符串
-            $string=implode($array);
-            $afterCutName .= mb_substr($string, 0, $endNum, 'utf-8');
+            $afterCutName .= mb_substr($name, $l-$endNum, $l, 'utf-8');
         }
       	return $afterCutName;
     }
