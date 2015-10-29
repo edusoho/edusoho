d906c3c411a7bec6f61f428f4624a4e9f09abffd
bellorchid <zhongyunchang@yeah.net>
2015-10-29 14:10:48 +0800

    修改错误

--- src/Topxia/WebBundle/Controller/BaseController.php
+++ src/Topxia/WebBundle/Controller/BaseController.php
@@ -104,7 +104,7 @@ abstract class BaseController extends Controller
 
     protected function isPluginInstalled($name)
     {
-        return $this->get('topxia.twig.web_extension')->isPluginInstaled($name);
+        return $this->get('topxia.twig.web_extension')->isPluginInstalled($name);
     }
 
     protected function createNamedFormBuilder($name, $data = null, array $options = array())
