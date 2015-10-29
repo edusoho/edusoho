3eea1c476bf03019796e08d2b08a388a1e0e5d5f
lizhenxiong <lizhenxiong@howzhi.com>
2015-10-29 11:47:30 +0800

    修改单元测试类NumberToolkitTest

--- src/Topxia/Common/Tests/NumberToolkitTest.php
+++ src/Topxia/Common/Tests/NumberToolkitTest.php
@@ -3,51 +3,27 @@
 namespace Topxia\Common\Tests;
 
 use Topxia\Service\Common\BaseTestCase;
+use Topxia\Common\NumberToolkit;
 
 class NumberToolkitTest extends BaseTestCase
 {
 
         public function testroundUp(){
 
-              $testNumArray=array(123.456,783463.437843,12.01,34.00,45.5,8934.00001,100.101,34.10,33.30,40);
-              $testPrecision=2;
-              $testString="";
-              for($i=0;$i<8;$i++){
-              	  $testTem=0;
-                       $testAmt = explode(".",$testNumArray[$i]);
-                       if(count($testAmt)==0){
-                       	return 0;
-                       }
+              $testNumArray=array(123.456,5784975.4328278,437.30,89.11,345.030);
+              
+              $testNum1 = NumberToolkit::roundUp($testNumArray[0]);
+              $testNum2 = NumberToolkit::roundUp($testNumArray[1]);
+              $testNum3 = NumberToolkit::roundUp($testNumArray[2]);
+              $testNum4 = NumberToolkit::roundUp($testNumArray[3]);
+              $testNum5 = NumberToolkit::roundUp($testNumArray[4]);
+              
+              $this->assertEquals(123.46,$testNum1);
+              $this->assertEquals(5784975.44,$testNum2);
+              $this->assertEquals(437.30,$testNum3);
+              $this->assertEquals(89.11,$testNum4);
+              $this->assertEquals(345.03,$testNum5);
 
-                       if(count($testAmt)==1){
-                       	return $testAmt[0];
-                       }
-
-                       if(strlen($testAmt[1]) > $testPrecision){
-                       	$testFloatStr = substr($testAmt[1],$testPrecision);
-                       	$testAmt[1] = (float)(".".substr($testAmt[1],0,$testPrecision));
-
-                       	if((int)substr($testFloatStr,0,$testPrecision+2)>0){
-                       		$testNext = (int)$testFloatStr;
-                       		if($testNext != 0){
-                                                $testrUp="";
-                                          for($x=1;$x<$testPrecision;$x++){
-                                          	$testrUp .= "0";
-                                          }
-                                          $testAmt[1] = $testAmt[1] + (float)(".".$testrUp."1");
-                       		}
-                       	}
-                       }
-                       else{
-                       	$testAmt[1] = (float)(".".$testAmt[1]);
-                       }
-                   $testTem = $testAmt[0]+$testAmt[1];
-                   $testString="  ".$testTem;
-              }
-
-              preg_match_all('^\d+(\.\d{2})?$',$testString,$matchs);
-
-              $this->assertEquals(10,count($matchs));
 
         }
 
