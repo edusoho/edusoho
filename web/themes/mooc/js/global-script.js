769604fb1d9b3942618a7ba891c11a61334dd4b9
wangxibin <wangxibin@howzhi.com>
2015-10-30 13:41:31 +0800

    mooc themes

---
+++ web/themes/mooc/js/global-script.js
@@ -0,0 +1,51 @@
+define(function(require, exports, module) {
+
+    exports.run = function() {
+
+        var removeNavMobile = function(){
+            $(".nav-mobile").removeClass("active");
+            $(".html-mask").removeClass("active");
+            $(".es-wrap").removeClass("nav-active")
+            $("html").removeClass("html-nav-active").css("height",'auto');
+        }
+
+        $(".js-navbar-more").click(function(e){
+            var $nav = $(".nav-mobile");
+            
+            if($nav.hasClass("active")){
+                removeNavMobile();
+
+            }else {
+                var height = $(window).height();
+                $("html").css("height",height);
+                $nav.addClass("active").css("height",height);
+                
+                $(".html-mask").addClass("active");
+                $(".es-wrap").addClass("nav-active");
+                $("html").addClass("html-nav-active");
+            }
+        });
+
+        $("body").on("click",'.nav-mobile.active',function(e){
+            e.stopPropagation(); 
+        });
+
+        $("body").on("click",'.html-mask.active',function(e){
+            removeNavMobile();
+        });
+
+
+        $("#home-course-list").on('click','.js-course-filter',function(){
+             var $btn = $(this);
+             $.get($btn.data('url'),function(html){
+               $('#course-list').html(html);
+               Lazyload.init();
+               $(".tooltip").remove();
+               $('[data-toggle="tooltip"]').tooltip({container: 'body'});
+            })
+        })
+       
+        
+    }
+
+});
