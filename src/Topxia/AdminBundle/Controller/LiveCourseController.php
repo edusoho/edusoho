c6a9d1e9a8d4b0e363a0745215281a47456d2681
xb <xiangbin@howzhi.com>
2015-11-03 17:11:56 +0800

    直播课时增加搜索条件

--- src/Topxia/AdminBundle/Controller/LiveCourseController.php
+++ src/Topxia/AdminBundle/Controller/LiveCourseController.php
@@ -4,13 +4,18 @@ namespace Topxia\AdminBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Topxia\Common\Paginator;
 use Topxia\Common\ArrayToolkit;
+use Topxia\Common\StringToolkit;
 
 class LiveCourseController extends BaseController
 {
 
     public function indexAction (Request $request, $status)
-    {   
-
+    {
+        $conditions = $request->query->all();
+        if(empty($conditions['titleLike'])){
+            unset($conditions['titleLike']);
+        }
+            
         $courses = $this->getCourseService()->searchCourses(array('type' => 'live','status' => 'published'), $sort = 'latest', 0, 1000);
 
         $courseIds = ArrayToolkit::column($courses, 'id');
@@ -18,7 +23,8 @@ class LiveCourseController extends BaseController
         $courses = ArrayToolkit::index($courses, 'id');
 
         $conditions['type']="live";
-        
+
+                
         switch ($status) {
             case 'coming':
                 $conditions['startTimeGreaterThan'] = time();
@@ -31,6 +37,12 @@ class LiveCourseController extends BaseController
                 $conditions['endTimeGreaterThan'] = time();
                 break;
         }
+        if(!empty($conditions['startDateTime']) && !empty($conditions['endDateTime'])){
+            if($status == 'end'){ unset($conditions['endTimeLessThan']);}
+            if($status == 'underway'){ unset($conditions['endTimeGreaterThan']);}
+            $conditions['startTimeGreaterThan'] = strtotime($conditions['startDateTime']);
+            $conditions['startTimeLessThan'] = strtotime($conditions['endDateTime']);
+        }
 
         $conditions['courseIds'] = $courseIds;
         $conditions['status'] ='published';
@@ -40,10 +52,9 @@ class LiveCourseController extends BaseController
             $this->getCourseService()->searchLessonCount($conditions),
             20
         );
-
         if ($status == 'end') {
-            $lessons = $this->getCourseService()->searchLessons($conditions, 
-                array('startTime', 'DESC'), 
+            $lessons = $this->getCourseService()->searchLessons($conditions,
+                array('startTime', 'DESC'),
                 $paginator->getOffsetCount(),
                 $paginator->getPerPageCount()
             );
@@ -54,7 +65,8 @@ class LiveCourseController extends BaseController
                 $paginator->getPerPageCount()
             );
         }
-         $default = $this->getSettingService()->get('default', array());
+        $default = $this->getSettingService()->get('default', array());
+
         return $this->render('TopxiaAdminBundle:LiveCourse:index.html.twig', array(
             'status' => $status,
             'lessons' => $lessons,
