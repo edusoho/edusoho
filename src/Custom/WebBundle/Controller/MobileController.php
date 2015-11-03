ded0f7a714756d2e27e68899a2710d6998a22c6e
retamia <liumengte@howzhi.com>
2015-11-03 09:36:40 +0800

    移动端和题目

---
+++ src/Custom/WebBundle/Controller/MobileController.php
@@ -0,0 +1,34 @@
+<?php
+namespace Custom\WebBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Topxia\Service\CloudPlatform\CloudAPIFactory;
+use Topxia\WebBundle\Controller\MobileController as BaseMobileController;
+
+class MobileController extends BaseController
+{    
+    
+    public function indexAction(Request $request)
+    {
+        $mobile = $this->setting('mobile', array());
+
+        if (empty($mobile['enabled'])) {
+            return $this->createMessageResponse('info', '客户端尚未开启！');
+        }
+
+        $result = CloudAPIFactory::create('leaf')->get('/me');
+
+        if(array_key_exists('ver',$mobile) && $mobile['ver']){
+            $mobileCode = ( (array_key_exists("mobileCode", $result) && !empty($result["mobileCode"])) ? $result["mobileCode"] : "edusoho-mooc");
+        }else{
+            $mobileCode = ( (array_key_exists("mobileCode", $result) && !empty($result["mobileCode"])) ? $result["mobileCode"] : "edusoho-mooc");  
+        }
+
+        return $this->render('CustomWebBundle:Mobile:index.html.twig', array(
+            'host' => $request->getHttpHost(),
+            'mobileCode' => $mobileCode,
+            'mobile' => $mobile
+        ));
+    }
+
+}
