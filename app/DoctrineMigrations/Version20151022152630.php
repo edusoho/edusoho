497c7562c0157086548f3d40f0a420cd9b9da02c
laisancun <laisancun@howzhi.com>
2015-11-04 16:26:35 +0800

    ＂修改升级脚本＂

--- app/DoctrineMigrations/Version20151022152630.php
+++ app/DoctrineMigrations/Version20151022152630.php
@@ -20,7 +20,7 @@ class Version20151022152630 extends AbstractMigration
         $this->addSql("ALTER TABLE `cash_flow` CHANGE `payment` `payment` ENUM('alipay','wxpay','heepay') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL");
         $this->addSql("ALTER TABLE `cash_orders` CHANGE `payment` `payment` ENUM('none','alipay','wxpay','heepay') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL");
         if (!$this->isFieldExist('orders', 'token')) {
-            $this->addSql("ALTER TABLE `orders` ADD `token` VARCHAR(50) NOT NULL COMMENT '令牌' AFTER `discount`");
+            $this->addSql("ALTER TABLE `orders` ADD `token` VARCHAR(50) NULL DEFAULT NULL COMMENT '令牌'");
         }
     }
 
