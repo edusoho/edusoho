dfd7fe366e07409cb632e2d001a138a3dd805487
wangxibin <wangxibin@howzhi.com>
2015-11-05 19:53:13 +0800

    合并课程评分的需求

---
+++ src/Custom/Service/Course/CourseScoreService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Custom\Service\Course;
+
+interface CourseScoreService {
+	public function getUserScoreByUserIdAndCourseId($userId, $courseId);
+
+	public function updateUserCourseScore($id, $score);
+
+	public function addUserCourseScore($score);
+
+	public function searchMemberScoreCount($conditions);
+
+	public function searchMemberScore($conditions, $orderBy, $start, $limit);
+
+    public function findUsersScoreBySqlJoinUser($fields);
+
+	public function findAllMemberScore($courseId);
+
+	public function findUserScoreByIdsAndCourseId($userIds, $courseId);
+
+	public function getCoursePassStudentCount($courseId);
+
+	/**
+	 * CourseScoreSetting
+	 */
+	public function addScoreSetting($scoreSetting);
+
+	public function updateScoreSetting($courseId, $fields);
+
+	public function getScoreSettingByCourseId($courseId);
+
+    public function findScoreSettingsByCourseIds($courseIds);
+
+}
---
+++ src/Custom/Service/Course/module_config.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'event_subscriber' => array(
+		'Custom\\Service\\Course\\Event\\CourseScoreEventSubscriber',
+		'Custom\\Service\\Course\\Event\\CourseEventSubscriber'
+	),
+);
