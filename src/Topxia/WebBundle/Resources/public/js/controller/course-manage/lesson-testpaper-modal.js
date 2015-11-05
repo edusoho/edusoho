694b07f9c9eefc0d8e298996bf69c19413505217
retamia <liumengte@howzhi.com>
2015-11-05 17:05:02 +0800

    删除注释

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage/lesson-testpaper-modal.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage/lesson-testpaper-modal.js
@@ -176,7 +176,6 @@ define(function (require, exports, module) {
             };
 
             this._addTimePicker = function(){
-                console.log('addTimePicker');
                 that._$testStartTime.datetimepicker({
                     language:'zh-CN',
                     autoclose: true,
