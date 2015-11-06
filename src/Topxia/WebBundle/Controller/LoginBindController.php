1c3d18292c793f584de89376987b86b0e0433e29
wengzheqing <wengzheqing@howzhi.com>
2015-11-06 13:39:20 +0800

    zancun

--- src/Topxia/WebBundle/Controller/LoginBindController.php
+++ src/Topxia/WebBundle/Controller/LoginBindController.php
@@ -40,11 +40,11 @@ class LoginBindController extends BaseController
                 return $this->redirect($goto);
             }
         } else {
-            /*if ($type == 'weixinmob') {
+            if ($type == 'weixinmob') {
                 $response = $this->autobind($request,$type);
                 $_target_path = $response['_target_path'];
                 return $this->redirect($_target_path);
-            }*/
+            }
             return $this->redirect($this->generateUrl('login_bind_choose', array('type' => $type)));
         }
 
