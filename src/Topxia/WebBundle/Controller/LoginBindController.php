ec044e7c291d559b906fd861171cf2330b2510d3
wengzheqing <wengzheqing@howzhi.com>
2015-11-09 15:22:18 +0800

    zancun

--- src/Topxia/WebBundle/Controller/LoginBindController.php
+++ src/Topxia/WebBundle/Controller/LoginBindController.php
@@ -15,8 +15,6 @@ class LoginBindController extends BaseController
         }
         $client = $this->createOAuthClient($type);
         $callbackUrl = $this->generateUrl('login_bind_callback', array('type' => $type), true);
-        var_dump($callbackUrl);
-        exit();
         $url = $client->getAuthorizeUrl($callbackUrl);
         return $this->redirect($url);
     }
@@ -36,7 +34,8 @@ class LoginBindController extends BaseController
             }
             $this->authenticateUser($user);
             if ($this->getAuthService()->hasPartnerAuth()) {
-                return $this->redirect($this->generateUrl('partner_login', array('goto'=>$request->getSession()->get('_target_path', ''))));
+                return $this->redirect($this->generateUrl('login_bind_choose', array('type' => $type)));
+                //return $this->redirect($this->generateUrl('partner_login', array('goto'=>$request->getSession()->get('_target_path', ''))));
             } else {
                 $goto = $request->getSession()->get('_target_path', '') ? : $this->generateUrl('homepage');
                 return $this->redirect($goto);
