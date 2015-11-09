82d43e32c5eff5decdcdc899a61e390ca01d52dc
wengzheqing <wengzheqing@howzhi.com>
2015-11-09 14:28:33 +0800

    zancun

--- src/Topxia/WebBundle/Controller/LoginBindController.php
+++ src/Topxia/WebBundle/Controller/LoginBindController.php
@@ -15,8 +15,6 @@ class LoginBindController extends BaseController
         }
         $client = $this->createOAuthClient($type);
         $callbackUrl = $this->generateUrl('login_bind_callback', array('type' => $type), true);
-        var_dump($callbackUrl);
-        exit();
         $url = $client->getAuthorizeUrl($callbackUrl);
         return $this->redirect($url);
     }
