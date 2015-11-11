4d57f06d79e27f2f4097aaf8f494d42c8e0e15dd
ketuzhong <zhongjie@howzhi.com>
2015-11-11 20:40:39 +0800

    搜索初始化

--- src/Topxia/WebBundle/Resources/public/js/controller/search/search.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/search/search.js
@@ -1,10 +1,17 @@
 define(function(require, exports, module) {
 
     var Lazyload = require('echo.js');
+    require('topxiawebbundle/util/follow-btn');
+    require('jquery.lavaTab');
 
     exports.run = function() {
 
         Lazyload.init();
+        
+        $('.nav-tabs').lavaTab({
+        	fx: "backout",
+        	speed: 700
+        });
 
     };
 
