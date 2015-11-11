9c06b143995609c604935b03ec9607ba8f8fd2db
wengzheqing <wengzheqing@howzhi.com>
2015-11-11 17:42:34 +0800

    fix

--- src/Topxia/WebBundle/Controller/LoginBindController.php
+++ src/Topxia/WebBundle/Controller/LoginBindController.php
@@ -114,7 +114,7 @@ class LoginBindController extends BaseController
             $this->getUserService()->setupAccount($user['id']);
         }
         $this->authenticateUser($user);
-        $response = array('success' => true, '_target_path' => $request->getSession()->get('_target_path', $request->getSession()->get('_target_path', $this->generateUrl('homepage')));
+        $response = array('success' => true, '_target_path' => $request->getSession()->get('_target_path',  $this->generateUrl('homepage')));
         if (!$response['_target_path']) {
             $response['_target_path'] = $this->generateUrl('homepage');
         }
