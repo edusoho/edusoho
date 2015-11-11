cc9f57682c949bd0375551c3948fe1e3dc4497f5
wengzheqing <wengzheqing@howzhi.com>
2015-11-11 10:05:43 +0800

    zancun

--- src/Topxia/WebBundle/Controller/LoginBindController.php
+++ src/Topxia/WebBundle/Controller/LoginBindController.php
@@ -41,10 +41,10 @@ class LoginBindController extends BaseController
                 return $this->redirect($goto);
             }
         } else {
-            /*if ($type == 'weixinmob') {
+            if ($type == 'weixinmob') {
                 /*$response = $this->autobind($request,$type);
                 $_target_path = $response['_target_path'];
-                return $this->redirect($_target_path);
+                return $this->redirect($_target_path);*/
                 $client = $this->createOAuthClient($type);
                 try {
                     $oauthUser = $client->getUserInfo($token);
@@ -63,7 +63,7 @@ class LoginBindController extends BaseController
                     'type' => $type,
                     'hasPartnerAuth' => $this->getAuthService()->hasPartnerAuth(),
                 ));
-            }*/
+            }
             return $this->redirect($this->generateUrl('login_bind_choose', array('type' => $type)));
         }
 
@@ -192,7 +192,7 @@ class LoginBindController extends BaseController
         
         $this->authenticateUser($user);
 
-        $response = array('success' => true, '_target_path' =>   $this->generateUrl('homepage'));
+        $response = array('success' => true, '_target_path' =>  $request->getSession()->get('_target_path', $this->generateUrl('homepage')));
 
         response:
         return $this->createJsonResponse($response);
