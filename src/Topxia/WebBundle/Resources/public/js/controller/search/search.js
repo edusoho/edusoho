d507aea70dc31b32465ccde83af6134cc8f054b9
ketuzhong <zhongjie@howzhi.com>
2015-11-12 11:20:36 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/search/search.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/search/search.js
@@ -7,11 +7,24 @@ define(function(require, exports, module) {
     exports.run = function() {
 
         Lazyload.init();
-        
+
         $('.nav-tabs').lavaTab({
         	fx: "backout",
         	speed: 700
         });
+
+        $(".js-btn-clear").on("click",function(){
+        	var $this = $(this);
+        	
+        })
+
+        $('.nav-tabs').on('click','li:not(".active")>a',function(){
+        	var $this = $(this);
+        	var url = $this.data(url);
+        	$.get(url,function(html){
+    			$(".search-result").html(html);
+        	})
+        })
 
     };
 
