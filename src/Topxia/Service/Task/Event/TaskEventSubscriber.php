0daef4a58fdf02cceccfe10058de3d36549a2295
danyue <wangdanyue@howzhi.com>
2015-11-13 16:05:54 +0800

    phpfmt-fix

--- src/Topxia/Service/Task/Event/TaskEventSubscriber.php
+++ src/Topxia/Service/Task/Event/TaskEventSubscriber.php
@@ -1,24 +1,22 @@
 <?php
 namespace Topxia\Service\Task\Event;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Topxia\Common\StringToolkit;
 use Topxia\Service\Common\ServiceEvent;
 use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class TaskEventSubscriber implements EventSubscriberInterface
 {
-
     public static function getSubscribedEvents()
     {
         return array(
             //'task.finished' => 'onFinished',
 
             'course.lesson_finish' => 'onLessonFinished',
-            'homework.finish' => 'onHomeworkFinished',
-            'homework.check' => 'onHomeworkCheck',
-            'testpaper.reviewed' => 'onTestPaperFinished',
-            'testpaper.finish' => 'onTestPaperFinished',
+            'homework.finish'      => 'onHomeworkFinished',
+            'homework.check'       => 'onHomeworkCheck',
+            'testpaper.reviewed'   => 'onTestPaperFinished',
+            'testpaper.finish'     => 'onTestPaperFinished'
         );
     }
 
@@ -32,16 +30,16 @@ class TaskEventSubscriber implements EventSubscriberInterface
     public function onFinished(ServiceEvent $event)
     {
         $targetObject = $event->getSubject();
-        $taskType = $event->getArgument('taskType');
+        $taskType     = $event->getArgument('taskType');
 
         $this->_finishTask($taskType, $targetObject);
     }
 
     public function onHomeworkFinished(ServiceEvent $event)
     {
-        $homework = $event->getSubject();
+        $homework       = $event->getSubject();
         $homeworkResult = $event->getArgument('homeworkResult');
-        $targetObject = array('id'=>$homework['id'], 'type'=>'homework','passedStatus'=>$homeworkResult['passedStatus'],'userId'=>$homeworkResult['userId']);
+        $targetObject   = array('id' => $homework['id'], 'type' => 'homework', 'passedStatus' => $homeworkResult['passedStatus'], 'userId' => $homeworkResult['userId']);
 
         $this->_finishTask('studyplan', $targetObject);
     }
@@ -49,24 +47,23 @@ class TaskEventSubscriber implements EventSubscriberInterface
     public function onHomeworkCheck(ServiceEvent $event)
     {
         $homeworkResult = $event->getSubject();
-        $targetObject = array('id'=>$homeworkResult['homeworkId'], 'type'=>'homework','passedStatus'=>$homeworkResult['passedStatus'],'userId'=>$homeworkResult['userId']);
+        $targetObject   = array('id' => $homeworkResult['homeworkId'], 'type' => 'homework', 'passedStatus' => $homeworkResult['passedStatus'], 'userId' => $homeworkResult['userId']);
 
         $this->_finishTask('studyplan', $targetObject);
     }
 
     public function onTestPaperFinished(ServiceEvent $event)
     {
-        $testpaper = $event->getSubject();
+        $testpaper       = $event->getSubject();
         $testpaperResult = $event->getArgument('testpaperResult');
-        $target = explode('-', $testpaperResult['target']);
-        
+        $target          = explode('-', $testpaperResult['target']);
+
         if (isset($target[2])) {
             $lesson = $this->getCourseService()->getLesson($target[2]);
 
-            $targetObject = array('id'=>$lesson['id'], 'type'=>'testpaper', 'passedStatus'=>$testpaperResult['passedStatus'],'userId'=>$testpaperResult['userId']);
-            $this->_finishTask('studyplan',$targetObject);
+            $targetObject = array('id' => $lesson['id'], 'type' => 'testpaper', 'passedStatus' => $testpaperResult['passedStatus'], 'userId' => $testpaperResult['userId']);
+            $this->_finishTask('studyplan', $targetObject);
         }
-
     }
 
     private function _finishTask($taskType, $targetObject)
@@ -83,5 +80,4 @@ class TaskEventSubscriber implements EventSubscriberInterface
     {
         return ServiceKernel::instance()->createService('Course.CourseService');
     }
-
 }
