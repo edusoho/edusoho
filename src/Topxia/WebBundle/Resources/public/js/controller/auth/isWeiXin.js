989aed4f0f91324bc1af6bf8e26670dd403ba903
wengzheqing <wengzheqing@howzhi.com>
2015-11-16 17:17:28 +0800

    调试

--- src/Topxia/WebBundle/Resources/public/js/controller/auth/isWeiXin.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/auth/isWeiXin.js
@@ -2,7 +2,7 @@ define(function(require, exports, module) {
 	exports.run = function() {
 		var ua = window.navigator.userAgent.toLowerCase(); 
 		if(ua.match(/MicroMessenger/i) == 'micromessenger'){ 
-			window.location.href = ('/');
+			window.location.href = ('/login');
 		} else { 
 			return false; 
 		}
