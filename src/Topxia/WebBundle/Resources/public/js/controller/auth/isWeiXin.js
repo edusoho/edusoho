bb32a2f1aef54539ff006a6760da8e88003d8772
wengzheqing <wengzheqing@howzhi.com>
2015-11-16 17:12:58 +0800

    tiaoshi

--- src/Topxia/WebBundle/Resources/public/js/controller/auth/isWeiXin.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/auth/isWeiXin.js
@@ -2,7 +2,7 @@ define(function(require, exports, module) {
 	exports.run = function() {
 		var ua = window.navigator.userAgent.toLowerCase(); 
 		if(ua.match(/MicroMessenger/i) == 'micromessenger'){ 
-			window.herf('homepage');
+			window.location.href = ('/');
 		} else { 
 			return false; 
 		}
