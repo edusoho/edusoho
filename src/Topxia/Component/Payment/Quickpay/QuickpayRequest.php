671f251ec75bd0e9233af9d8311d1088866eb890
laisancun <laisancun@howzhi.com>
2015-11-16 16:43:32 +0800

    测试

--- src/Topxia/Component/Payment/Quickpay/QuickpayRequest.php
+++ src/Topxia/Component/Payment/Quickpay/QuickpayRequest.php
@@ -3,79 +3,89 @@ namespace Topxia\Component\Payment\Quickpay;
 
 use Topxia\Component\Payment\Request;
 
-class QuickpayRequest extends Request {
-
-    protected $url = 'Https://Pay.Heepay.com/ShortPay/SubmitOrder.aspx';
+class QuickpayRequest extends Request
+{
+    protected $url       = 'Https://Pay.Heepay.com/ShortPay/SubmitOrder.aspx';
     protected $submitUrl = '';
 
     public function form()
     {
-        $form = array();
+        $form           = array();
         $form['method'] = 'get';
         $form['params'] = $this->convertParams($this->params);
         $form['action'] = $this->submitUrl;
         return $form;
     }
- 
-    public function signParams($params) {
 
-        $params['key']=$this->options['secret'];
-        $params['agent_id']=$this->options['key'];
+    public function signParams($params)
+    {
+        $params['key']      = $this->options['secret'];
+        $params['agent_id'] = $this->options['key'];
         ksort($params);
         $sign = '';
+
         foreach ($params as $key => $value) {
-            $sign .= $key . '=' . $value . '&';
+            $sign .= $key.'='.$value.'&';
         }
-        $sign=trim($sign,"&");
+
+        $sign = trim($sign, "&");
         return md5(strtolower($sign));
     }
 
     protected function convertParams($params)
     {
-        $converted = array();
-        $converted['version']=1;
-        $converted['user_identity']=$this->options['key']."_".$params['userId'];
-        $converted['hy_auth_uid']='';
-        if(isset($params['authBank']['bankAuth'])){
-            $converted['hy_auth_uid']=$params['authBank']['bankAuth'];
-        }
-        $converted['mobile']=$params['mobile'];
-        $converted['device_type']=1;
-        $converted['device_id']='';
-        $converted['custom_page']=0;
-        $converted['display']=1;
-        if (!empty($params['returnUrl'])) {
-            $converted['return_url']=$params['returnUrl'];
-        }
-        if (!empty($params['notifyUrl'])) {
-            $converted['notify_url']=$params['notifyUrl'];
-        }
-        $converted['agent_bill_id']=$this->generateOrderToken();
-        $converted['agent_bill_time']=date("YmdHis",time());
-        $converted['pay_amt']=$params['amount'];
-        $converted['goods_name']=mb_substr($this->filterText($params['title']), 0, 50, 'utf-8');
-        $converted['goods_note']='';
-        $converted['goods_num']=1;
-        $converted['user_ip']=$this->get_client_ip();
-        $converted['ext_param1']='';
-        $converted['ext_param2']='';
-        $converted['auth_card_type']=0;
-        $converted['timestamp']=time()*1000;
-        $sign = $this->signParams($converted);
-        $encrypt_data = urlencode(base64_encode($this->Encrypt(http_build_query($converted),$this->options['aes'])));        
-        $url = $this->url."?agent_id=".$this->options['key']."&encrypt_data=".$encrypt_data."&sign=".$sign;
-        $result = $this->curlRequest($url);
-        $xml = simplexml_load_string($result);
-        $redir=(string)$xml->encrypt_data;
-        $redirurl=$this->Decrypt($redir,$this->options['aes']);
-        parse_str($redirurl,$tip);
+        $converted                  = array();
+        $converted['version']       = 1;
+        $converted['user_identity'] = $this->options['key']."_".$params['userId'];
+        $converted['hy_auth_uid']   = '';
 
-        $this->submitUrl=$tip['redirect_url'];
-        unset($tip['ret_code'],$tip['ret_msg'],$tip['redirect_url']);
-        $converted = array();
-        $converted['agent_id']=$this->options['key'];
+        if (isset($params['authBank']['bankAuth'])) {
+            $converted['hy_auth_uid'] = $params['authBank']['bankAuth'];
+        }
+
+        $converted['mobile']      = $params['mobile'];
+        $converted['device_type'] = 1;
+        $converted['device_id']   = '';
+        $converted['custom_page'] = 0;
+        $converted['display']     = 1;
+
+        if (!empty($params['returnUrl'])) {
+            $converted['return_url'] = $params['returnUrl'];
+        }
+
+        if (!empty($params['notifyUrl'])) {
+            $converted['notify_url'] = $params['notifyUrl'];
+        }
+
+        $converted['agent_bill_id']   = $this->generateOrderToken();
+        $converted['agent_bill_time'] = date("YmdHis", time());
+        $converted['pay_amt']         = $params['amount'];
+        $converted['goods_name']      = mb_substr($this->filterText($params['title']), 0, 50, 'utf-8');
+        $converted['goods_note']      = '';
+        $converted['goods_num']       = 1;
+        $converted['user_ip']         = $this->get_client_ip();
+        $converted['ext_param1']      = '';
+        $converted['ext_param2']      = '';
+        $converted['auth_card_type']  = 0;
+        $converted['timestamp']       = time() * 1000;
+        $sign                         = $this->signParams($converted);
+        $encrypt_data                 = urlencode(base64_encode($this->Encrypt(http_build_query($converted), $this->options['aes'])));
+        $url                          = $this->url."?agent_id=".$this->options['key']."&encrypt_data=".$encrypt_data."&sign=".$sign;
+        $result                       = $this->curlRequest($url);
+        $xml                          = simplexml_load_string($result);
+        $redir                        = (string) $xml->encrypt_data;
+        $redirurl                     = $this->Decrypt($redir, $this->options['aes']);
+        var_dump($redirect_url);
+        exit();
+        parse_str($redirurl, $tip);
+
+        $this->submitUrl = $tip['redirect_url'];
+        unset($tip['ret_code'], $tip['ret_msg'], $tip['redirect_url']);
+        $converted             = array();
+        $converted['agent_id'] = $this->options['key'];
+
         foreach ($tip as $key => $value) {
-            $converted[$key]=$value;
+            $converted[$key] = $value;
         }
 
         return $converted;
@@ -83,15 +93,14 @@ class QuickpayRequest extends Request {
 
     private function generateOrderToken()
     {
-        return  'heepay_' . date('YmdHis', time()) . mt_rand(10000,99999);
+        return 'heepay_'.date('YmdHis', time()).mt_rand(10000, 99999);
     }
-
 
     private function curlRequest($url)
     {
         $curl = curl_init();
 
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_USERAGENT, 'Topxia Payment Client 1.0');
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($curl, CURLOPT_TIMEOUT, 10);
@@ -99,8 +108,8 @@ class QuickpayRequest extends Request {
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLINFO_HEADER_OUT, TRUE );
-        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
 
         $response = curl_exec($curl);
 
@@ -125,24 +134,25 @@ class QuickpayRequest extends Request {
         } else {
             $cip = "unknown";
         }
+
         return $cip;
     }
 
-    private function Encrypt($data,$key){
+    private function Encrypt($data, $key)
+    {
         $decodeKey = base64_decode($key);
-        $iv     = substr($decodeKey,0,16);
-        $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $decodeKey, $data, MCRYPT_MODE_CBC, $iv);  
+        $iv        = substr($decodeKey, 0, 16);
+        $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $decodeKey, $data, MCRYPT_MODE_CBC, $iv);
         return $encrypted;
     }
 
-    private function Decrypt($data,$key){
-
+    private function Decrypt($data, $key)
+    {
         $decodeKey = base64_decode($key);
-        $data = base64_decode($data);
-        $iv = substr($decodeKey,0,16);
-        $encrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $decodeKey, $data, MCRYPT_MODE_CBC, $iv); 
+        $data      = base64_decode($data);
+        $iv        = substr($decodeKey, 0, 16);
+        $encrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $decodeKey, $data, MCRYPT_MODE_CBC, $iv);
 
         return $encrypted;
     }
-
 }
