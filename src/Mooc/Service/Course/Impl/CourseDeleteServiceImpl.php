45d044298c5389830d2346e78b283c255ecf0f2d
retamia <liumengte@howzhi.com>
2015-11-17 14:11:26 +0800

    课程删除时删除课程证书设置

--- src/Mooc/Service/Course/Impl/CourseDeleteServiceImpl.php
+++ src/Mooc/Service/Course/Impl/CourseDeleteServiceImpl.php
@@ -11,19 +11,45 @@ class CourseDeleteServiceImpl extends BaseCourseDeleteService
     {
         parent::deleteCourse($course);
         if ('periodic' == $course['type']) {
-            $this->deleteCourseScore($course);
-            $this->getCourseDao()->subPeriodsByRootId($course['rootId'], $course['periods']);
+            $this->_deletePeriodicCourse($course);
         }
         return 0;
     }
 
-    protected function deleteCourseScore($course)
+    private function _deletePeriodicCourse($course)
+    {
+        if($course['type'] != 'periodic'){
+            throw $this->createServiceException("{$course['title']}不是周期课程");
+        }
+
+        $this->_deleteCourseScore($course);
+        $this->_deleteCourseCertificate($course);
+        $this->getCourseDao()->subPeriodsByRootId($course['rootId'], $course['periods']);
+    }
+
+    private function _deleteCourseScore($course)
     {
         $this->getCourseScoreService()->deleteCourseScoreByCourseId($course['id']);
+    }
+
+    private function _deleteCourseCertificate($course)
+    {
+        $certificate = $this->getAppService()->findInstallApp('Certificate');
+
+        if(empty($certificate)){
+            return;
+        }
+
+        $this->getCertificateService()->deleteCourseCertificateByCourseId($course['id']);
     }
 
     protected function getCourseScoreService()
     {
         return $this->createService('Mooc:Course.CourseScoreService');
+    }
+
+    protected function getCertificateService()
+    {
+        return $this->createService('Certificate:CertificateService');
     }
 }
