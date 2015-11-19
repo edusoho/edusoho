d8f8fe7f30755a723d7c27a2a814951b25786847
ketuzhong <zhongjie@howzhi.com>
2015-11-19 13:21:49 +0800

    求是主题新首页

---
+++ web/themes/qiushi/js/index.js
@@ -0,0 +1,20 @@
+define(function(require, exports, module) {
+    var Swiper = require('swiper');
+    
+    exports.run = function() {
+        if ($(".es-poster .swiper-slide").length > 1) {
+            var swiper = new Swiper('.es-poster.swiper-container', {
+                pagination: '.swiper-pager',
+                paginationClickable: true,
+                autoplay: 5000,
+                autoplayDisableOnInteraction: false,
+                loop: true,
+                calculateHeight: true,
+                roundLengths: true,
+                onInit: function(swiper) {
+                   $(".swiper-slide").removeClass('swiper-hidden'); 
+                }
+            });
+        }
+    }
+});
