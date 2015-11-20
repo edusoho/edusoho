440de98cfb4efc85039562b89d49b91fa79a1c94
drools <baolianjing@howzhi.com>
2015-11-20 13:58:17 +0800

    search setting

--- src/Topxia/WebBundle/Controller/SearchController.php
+++ src/Topxia/WebBundle/Controller/SearchController.php
@@ -1,9 +1,9 @@
 <?php
 namespace Topxia\WebBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Topxia\Common\Paginator;
 use Topxia\Common\ArrayToolkit;
+use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends BaseController
 {
@@ -14,49 +14,58 @@ class SearchController extends BaseController
         $currentUser = $this->getCurrentUser();
 
         $keywords = $request->query->get('q');
-        $keywords=trim($keywords);
-        
+        $keywords = trim($keywords);
+
+        $pattern = $this->setting('magic.cloud_search');
+
+        if ($pattern) {
+            return $this->forward('TopxiaWebBundle:Search:cloudSearch', array(
+                'q' => $keywords
+            ));
+        }
+
         $vip = $this->getAppService()->findInstallApp('Vip');
 
         $isShowVipSearch = $vip && version_compare($vip['version'], "1.0.7", ">=");
-        
+
         $currentUserVipLevel = "";
-        $vipLevelIds = "";
-        if($isShowVipSearch){
-            $currentUserVip = $this->getVipService()->getMemberByUserId($currentUser['id']);
+        $vipLevelIds         = "";
+
+        if ($isShowVipSearch) {
+            $currentUserVip      = $this->getVipService()->getMemberByUserId($currentUser['id']);
             $currentUserVipLevel = $this->getLevelService()->getLevel($currentUserVip['levelId']);
-            $vipLevels = $this->getLevelService()->findAllLevelsLessThanSeq($currentUserVipLevel['seq']);
-            $vipLevelIds = ArrayToolkit::column($vipLevels, "id");
+            $vipLevels           = $this->getLevelService()->findAllLevelsLessThanSeq($currentUserVipLevel['seq']);
+            $vipLevelIds         = ArrayToolkit::column($vipLevels, "id");
         }
 
-        $parentId = 0;
+        $parentId   = 0;
         $categories = $this->getCategoryService()->findAllCategoriesByParentId($parentId);
-        
-        $categoryIds=array();
+
+        $categoryIds = array();
+
         foreach ($categories as $key => $category) {
             $categoryIds[$key] = $category['name'];
         }
 
         $categoryId = $request->query->get('categoryIds');
-        $fliter = $request->query->get('fliter');       
-
+        $fliter     = $request->query->get('fliter');
 
         $conditions = array(
-            'status' => 'published',
-            'title' => $keywords,
+            'status'     => 'published',
+            'title'      => $keywords,
             'categoryId' => $categoryId,
-            'parentId' => 0
+            'parentId'   => 0
         );
 
         if ($fliter == 'vip') {
             $conditions['vipLevelIds'] = $vipLevelIds;
-        } else if ($fliter == 'live') {
+        } elseif ($fliter == 'live') {
             $conditions['type'] = 'live';
-        } else if ($fliter == 'free'){
+        } elseif ($fliter == 'free') {
             $conditions['price'] = '0.00';
         }
 
-        $count = $this->getCourseService()->searchCourseCount($conditions);
+        $count     = $this->getCourseService()->searchCourseCount($conditions);
         $paginator = new Paginator(
             $this->get('request'),
             $count
@@ -69,16 +78,90 @@ class SearchController extends BaseController
             $paginator->getPerPageCount()
         );
 
-
         return $this->render('TopxiaWebBundle:Search:index.html.twig', array(
-            'courses' => $courses,
-            'paginator' => $paginator,
-            'keywords' => $keywords,
-            'isShowVipSearch' => $isShowVipSearch,
+            'courses'             => $courses,
+            'paginator'           => $paginator,
+            'keywords'            => $keywords,
+            'isShowVipSearch'     => $isShowVipSearch,
             'currentUserVipLevel' => $currentUserVipLevel,
-            'categoryIds' => $categoryIds,
-            'fliter' => $fliter,
-            'count' => $count,
+            'categoryIds'         => $categoryIds,
+            'fliter'              => $fliter,
+            'count'               => $count
+        ));
+    }
+
+    public function cloudSearchAction(Request $request)
+    {
+        $courses = $paginator = null;
+
+        $currentUser = $this->getCurrentUser();
+
+        $keywords = $request->query->get('q');
+        $keywords = trim($keywords);
+
+        $vip = $this->getAppService()->findInstallApp('Vip');
+
+        $isShowVipSearch = $vip && version_compare($vip['version'], "1.0.7", ">=");
+
+        $currentUserVipLevel = "";
+        $vipLevelIds         = "";
+
+        if ($isShowVipSearch) {
+            $currentUserVip      = $this->getVipService()->getMemberByUserId($currentUser['id']);
+            $currentUserVipLevel = $this->getLevelService()->getLevel($currentUserVip['levelId']);
+            $vipLevels           = $this->getLevelService()->findAllLevelsLessThanSeq($currentUserVipLevel['seq']);
+            $vipLevelIds         = ArrayToolkit::column($vipLevels, "id");
+        }
+
+        $parentId   = 0;
+        $categories = $this->getCategoryService()->findAllCategoriesByParentId($parentId);
+
+        $categoryIds = array();
+
+        foreach ($categories as $key => $category) {
+            $categoryIds[$key] = $category['name'];
+        }
+
+        $categoryId = $request->query->get('categoryIds');
+        $fliter     = $request->query->get('fliter');
+
+        $conditions = array(
+            'status'     => 'published',
+            'title'      => $keywords,
+            'categoryId' => $categoryId,
+            'parentId'   => 0
+        );
+
+        if ($fliter == 'vip') {
+            $conditions['vipLevelIds'] = $vipLevelIds;
+        } elseif ($fliter == 'live') {
+            $conditions['type'] = 'live';
+        } elseif ($fliter == 'free') {
+            $conditions['price'] = '0.00';
+        }
+
+        $count     = $this->getCourseService()->searchCourseCount($conditions);
+        $paginator = new Paginator(
+            $this->get('request'),
+            $count
+            , 12
+        );
+        $courses = $this->getCourseService()->searchCourses(
+            $conditions,
+            'latest',
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
+        return $this->render('TopxiaWebBundle:Search:cloud-search.html.twig', array(
+            'courses'             => $courses,
+            'paginator'           => $paginator,
+            'keywords'            => $keywords,
+            'isShowVipSearch'     => $isShowVipSearch,
+            'currentUserVipLevel' => $currentUserVipLevel,
+            'categoryIds'         => $categoryIds,
+            'fliter'              => $fliter,
+            'count'               => $count
         ));
     }
 
@@ -97,19 +180,18 @@ class SearchController extends BaseController
         return $this->getServiceKernel()->createService('CloudPlatform.AppService');
     }
 
-   protected function getLevelService()
+    protected function getLevelService()
     {
         return $this->getServiceKernel()->createService('Vip:Vip.LevelService');
     }
 
-     protected function getVipService()
+    protected function getVipService()
     {
         return $this->getServiceKernel()->createService('Vip:Vip.VipService');
-    }    
+    }
 
     protected function getCategoryService()
     {
         return $this->getServiceKernel()->createService('Taxonomy.CategoryService');
     }
-
 }
