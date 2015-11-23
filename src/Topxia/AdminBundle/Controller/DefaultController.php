399730e3fad6a94dd3b95c0c183960cdbbb16508
laisancun <laisancun@howzhi.com>
2015-11-23 10:54:13 +0800

    调试

--- src/Topxia/AdminBundle/Controller/DefaultController.php
+++ src/Topxia/AdminBundle/Controller/DefaultController.php
@@ -97,8 +97,9 @@ class DefaultController extends BaseController
         $site  = $this->getSettingService()->get('site');
         $user  = $this->getCurrentUser();
         $token = trim($this->postRequest("http://www.edusoho.com/question/get/token"), '"');
-        $site  = array('name' => $site['name'], 'url' => $site['url'], 'token' => $token, 'username' => $user->nickname);
-        $site  = urlencode(http_build_query($site));
+        var_dump($token);
+        $site = array('name' => $site['name'], 'url' => $site['url'], 'token' => $token, 'username' => $user->nickname);
+        $site = urlencode(http_build_query($site));
         return $this->redirect("http://www.edusoho.com/question?site=".$site."");
     }
 
