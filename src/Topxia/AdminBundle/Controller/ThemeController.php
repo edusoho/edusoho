ef114b4953ad37ca9b5421a1d574254b82cd27d3
wengzheqing <wengzheqing@howzhi.com>
2015-11-25 14:50:44 +0800

    颜色样式修改

--- src/Topxia/AdminBundle/Controller/ThemeController.php
+++ src/Topxia/AdminBundle/Controller/ThemeController.php
@@ -97,6 +97,21 @@ class ThemeController extends BaseController
         return $this->$code($config);
     }
 
+    public function getCurrentConfigColorAction($isEditColor = false)
+    {
+        $config = $this->getThemeService()->getCurrentThemeConfig();
+
+        if (!$isEditColor) {
+            $color = $config['confirmConfig']['color'];
+        } else {
+            $color = $config['config']['color'];
+        }
+
+        return $this->render("TopxiaWebBundle:Default:color.html.twig", array(
+            'color' => $color
+        ));
+    }
+
     protected function fiterCode($code)
     {
         $codes = explode('-', $code);
