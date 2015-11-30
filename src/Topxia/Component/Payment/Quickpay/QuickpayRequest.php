0d94ba646d4ad97112d0d321b91ab3a837a833ea
laisancun <laisancun@howzhi.com>
2015-11-30 19:03:28 +0800

    添加测试方式

--- src/Topxia/Component/Payment/Quickpay/QuickpayRequest.php
+++ src/Topxia/Component/Payment/Quickpay/QuickpayRequest.php
@@ -35,10 +35,7 @@ class QuickpayRequest extends Request
 
     protected function convertParams($params)
     {
-        var_dump($_SERVER['HTTP_USER_AGENT']);
-        exit();
         $isMobile = $this->isMobile();
-
         $mobileType = $this->mobileType();
 
         $converted                  = array();
@@ -134,62 +131,9 @@ class QuickpayRequest extends Request
 
     public function isMobile()
     {
-        if (isset($_SERVER['HTTP_VIA'])) {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') || strpos($_SERVER['HTTP_USER_AGENT'], 'iPad') || strpos($_SERVER['HTTP_USER_AGENT'], 'Android') {
             return true;
-        }
-
-        if (isset($_SERVER['HTTP_X_NOKIA_CONNECTION_MODE'])) {
-            return true;
-        }
-
-        if (isset($_SERVER['HTTP_X_UP_CALLING_LINE_ID'])) {
-            return true;
-        }
-
-        if (strpos(strtoupper($_SERVER['HTTP_ACCEPT']), "VND.WAP.WML") > 0) {
-            // Check whether the browser/gateway says it accepts WML.
-            $br = "WML";
-        } else {
-            $browser = isset($_SERVER['HTTP_USER_AGENT']) ? trim($_SERVER['HTTP_USER_AGENT']) : '';
-
-            if (empty($browser)) {
-                return true;
-            }
-
-            $browser = substr($browser, 0, 4);
-
-            if ($browser == "Noki" || // Nokia phones and emulators
-                $browser == "Eric" || // Ericsson WAP phones and emulators
-                $browser == "WapI" || // Ericsson WapIDE 2.0
-                $browser == "MC21" || // Ericsson MC218
-                $browser == "AUR" || // Ericsson R320
-                $browser == "R380" || // Ericsson R380
-                $browser == "UP.B" || // UP.Browser
-                $browser == "WinW" || // WinWAP browser
-                $browser == "UPG1" || // UP.SDK 4.0
-                $browser == "upsi" || // another kind of UP.Browser ??
-                $browser == "QWAP" || // unknown QWAPPER browser
-                $browser == "Jigs" || // unknown JigSaw browser
-                $browser == "Java" || // unknown Java based browser
-                $browser == "Alca" || // unknown Alcatel-BE3 browser (UP based?)
-                $browser == "MITS" || // unknown Mitsubishi browser
-                $browser == "MOT-" || // unknown browser (UP based?)
-                $browser == "My S" || // unknown Ericsson devkit browser ?
-                $browser == "WAPJ" || // Virtual WAPJAG www.wapjag.de
-                $browser == "fetc" || // fetchpage.cgi Perl script from www.wapcab.de
-                $browser == "ALAV" || // yet another unknown UP based browser ?
-                $browser == "Wapa" || // another unknown browser (Web based "Wapalyzer"?)
-                $browser == "Oper") // Opera
-            {
-                $br = "WML";
-            } else {
-                $br = "HTML";
-            }
-        }
-
-        if ($br == "WML") {
-            return true;
-        } else {
+        }else{
             return false;
         }
     }
