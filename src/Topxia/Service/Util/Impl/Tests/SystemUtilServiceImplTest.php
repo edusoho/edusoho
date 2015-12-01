afc31e9d2c9e1d411e1f908437fda0dd511c73c8
lvjie <lujie@howzhi.com>
2015-12-01 16:35:17 +0800

    修改SystemUtilServiceImplTest.php单元测试

--- src/Topxia/Service/Util/Impl/Tests/SystemUtilServiceImplTest.php
+++ src/Topxia/Service/Util/Impl/Tests/SystemUtilServiceImplTest.php
@@ -1,44 +1,39 @@
 <?php
 namespace Topxia\Service\Util\Impl\Tests;
 
-use Topxia\Service\Util\SystemUtilService;
 use Topxia\Service\Common\BaseTestCase;
 
 class SystemUtilServiceImplTest extends BaseTestCase
 {
+    public function testRemoveUnusedUploadFiles()
+    {
+        $file = array(
+            'hashId'     => 'courselesson/1/20151030034018-4rnonr.mp4',
+            'targetId'   => 1,
+            'targetType' => 'courselesson',
+            'filename'   => '1.mp4',
+            'ext'        => 'mp4',
+            'size'       => 0,
+            'storage'    => 'local',
+            'usedCount'  => 0
+        );
 
+        $this->getUploadFileDao()->addFile($file);
 
-	public function testRemoveUnusedUploadFiles()
-	{	
-		$conn = mysql_connect("localhost","root","root");
-		if($conn){
-			echo '链接';
-		}else{
-			echo '失败';
-		}
-		
-		 mysql_select_db("edusoho-test");
+        $test = $this->getSystemUtilService()->removeUnusedUploadFiles();
 
-		$sql = "INSERT INTO `upload_files` (`hashId`,`targetId`, `targetType`,`filename`,`ext`,`size`,`storage`,`usedCount`)
-		 VALUES ('courselesson/1/20151030034018-4rnonr.mp4','1', 'courselesson','1.mp4','mp4','0','local','0')";
-		
-		$test_query = mysql_query($sql , $conn);
-		if($test_query){
-			echo '插入成功';
-		};
+        $this->assertEquals(1, $test);
 
-		$test = $this->getSystemUtilService()->removeUnusedUploadFiles();
+    }
 
-		 $this->assertEquals(1,$test);
-		
-	}
+    protected function getUploadFileDao()
+    {
+        return $this->getServiceKernel()->createDao('File.UploadFileDao');
+    }
 
-
-	protected function getSystemUtilService()
-	{
-	return $this->getServiceKernel()->createService('Util.SystemUtilService');
-	}
-
-  	
+    protected function getSystemUtilService()
+    {
+        return $this->getServiceKernel()->createService('Util.SystemUtilService');
+    }
 
 }
