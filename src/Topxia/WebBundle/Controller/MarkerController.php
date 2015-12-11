25ad45f09475bf5bf4f8d749fc990ca1d71e51e9
drools <baolianjing@howzhi.com>
2015-12-11 10:11:41 +0800

    marker show

--- src/Topxia/WebBundle/Controller/MarkerController.php
+++ src/Topxia/WebBundle/Controller/MarkerController.php
@@ -1,12 +1,22 @@
 <?php
 namespace Topxia\WebBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class MarkerController extends BaseController
 {
-    public function manageAction()
+    public function manageAction(Request $request, $id)
     {
-        $user = $this->getCurrentUser();
-        return $this->render('TopxiaWebBundle:Marker:index.html.twig');
+        $user   = $this->getCurrentUser();
+        $lesson = $this->getCourseService()->getLesson($id);
+        return $this->render('TopxiaWebBundle:Marker:index.html.twig', array(
+            'lesson' => $lesson
+        ));
+    }
+
+    protected function getCourseService()
+    {
+        return $this->getServiceKernel()->createService('Course.CourseService');
     }
 
 }
--- src/Topxia/WebBundle/Resources/public/js/controller/marker/index.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/marker/index.js
@@ -2,7 +2,11 @@ define(function(require, exports, module) {
 
     exports.run = function() {
 
-        var playerUrl = '/course/1/lesson/9/player';
+        var videoHtml = $('#lesson-dashboard');
+        var courseId = videoHtml.data("course-id");
+        var lessonId = videoHtml.data("lesson-id");
+
+        var playerUrl = '/course/'+courseId+'/lesson/'+lessonId+'/player';
         var html = '<iframe src=\''+playerUrl+'\' name=\'viewerIframe\' id=\'viewerIframe\' width=\'100%\'allowfullscreen webkitallowfullscreen height=\'100%\' style=\'border:0px\'></iframe>';
         $("#lesson-video-content").html(html);
 
