25b43c1e502b5a49fb76c2a6a6a8abb7d47e0354
nifeng <fengni@fengdeMBP.lan>
2015-12-14 21:41:52 +0800

    add updatedTime column

--- src/Topxia/Service/Course/Dao/Impl/CourseDaoImpl.php
+++ src/Topxia/Service/Course/Dao/Impl/CourseDaoImpl.php
@@ -118,6 +118,7 @@ class CourseDaoImpl extends BaseDao implements CourseDao
 
     public function updateCourse($id, $fields)
     {
+        $fields['updateTime'] = time();
         $this->getConnection()->update(self::TABLENAME, $fields, array('id' => $id));
         return $this->getCourse($id);
     }
@@ -135,14 +136,17 @@ class CourseDaoImpl extends BaseDao implements CourseDao
             throw \InvalidArgumentException(sprintf("%s字段不允许增减，只有%s才被允许增减", $field, implode(',', $fields)));
         }
 
-        $sql = "UPDATE {$this->getTablename()} SET {$field} = {$field} + ? WHERE id = ? LIMIT 1";
+        $currentTime = time();
+
+        $sql = "UPDATE {$this->getTablename()} SET {$field} = {$field} + ?, updatedTime = '{$currentTime}' WHERE id = ? LIMIT 1";
 
         return $this->getConnection()->executeQuery($sql, array($diff, $id));
     }
 
     public function clearCourseDiscountPrice($discountId)
     {
-        $sql = "UPDATE course SET price = originPrice, coinPrice = originCoinPrice, discountId = 0, discount = 10 WHERE discountId = ?";
+        $currentTime = time();
+        $sql         = "UPDATE course SET updatedTime = '{$currentTime}', price = originPrice, coinPrice = originCoinPrice, discountId = 0, discount = 10 WHERE discountId = ?";
         return $this->getConnection()->executeQuery($sql, array($discountId));
     }
 
