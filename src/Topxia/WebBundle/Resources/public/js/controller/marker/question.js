e6f97265b278727dd4ac962a52249602f33a0a12
laisancun <laisancun@howzhi.com>
2015-12-16 18:37:58 +0800

    修改分页bug

--- src/Topxia/WebBundle/Resources/public/js/controller/marker/question.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/marker/question.js
@@ -14,7 +14,7 @@ define(function(require, exports, module) {
 	                return ;
 	           	}
 	          	$.post($form.attr('action'), $form.serialize(), function(response){
-	             	$('.question-tr').html(response);
+	             	$('.question').html(response);
 	          	});	
     
         	}
