549be324312fdca6105a8ec5ec5bdc2622c9d13b
lvliujie <lvliujie@howzhi.com>
2015-12-19 17:41:48 +0800

    暂时回复/{id}/friendship接口

--- api/src/users.php
+++ api/src/users.php
@@ -358,4 +358,73 @@ $api->post('/{id}/followers', function (Request $request, $id) {
 }
 
 );
+
+
+//获得用户的好友关系
+/*
+## 获得用户的好友关系
+GET /users/{id}/friendship
+
+ddddd
+| 名称  | 类型  | 必需   | 说明 |
+| ---- | ----- | ----- | ---- |
+| toIds | int | 否 | 被选方的的用户id,未传则默认为当前登录用户,多id格式为id-1,id-2,id-3|
+
+ ** 响应 **
+
+```
+[
+no-user,
+none,
+following,
+follower,
+friend,
+...
+]
+```
+返回数组，排序与传入id对应,好友关系的值有：
+
+no-user : toId用户不存在
+none : 双方无关系
+following : id用户关注了toId用户
+follower : toId用户关注了id用户
+friend : 互相关注
+ */
+$api->get('/{id}/friendship', function (Request $request, $id) {
+    $user = convert($id, 'user');
+    $currentUser = getCurrentUser();
+    $toIds = $request->query->get('toIds');
+
+    if (!empty($toIds)) {
+        $toIds = explode(',', $toIds);
+    } else {
+        $toIds = array($currentUser['id']);
+    }
+
+    foreach ($toIds as $toId) {
+        $toUser = ServiceKernel::instance()->createService('User.UserService')->getUser($toId);
+
+        if (empty($toUser)) {
+            $result[] = 'no-user';
+            continue;
+        }
+
+        //关注id的人
+        $follwers = ServiceKernel::instance()->createService('User.UserService')->findAllUserFollower($user['id']);
+        //id关注的人
+        $follwings = ServiceKernel::instance()->createService('User.UserService')->findAllUserFollowing($user['id']);
+
+        $toId = $toUser['id'];
+
+        if (!empty($follwers[$toId])) {
+            $result[] = !empty($follwings[$toId]) ? 'friend' : 'follower';
+        } else {
+            $result[] = !empty($follwings[$toId]) ? 'following' : 'none';
+        }
+    }
+
+    return $result;
+}
+
+);
 return $api;
