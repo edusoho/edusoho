69a7a7c39757e3ad3b596034a2161e1edebf3845
wengzheqing <wengzheqing@howzhi.com>
2015-12-23 16:29:15 +0800

    选择题判断

--- src/Topxia/WebBundle/Resources/public/js/controller/marker/do-test.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/marker/do-test.js
@@ -1,14 +1,18 @@
 define(function(require, exports, module) {
 	exports.run = function() {
     $btn = $('#submit');
+    var markerId = $('#data').data('markerid');
+    var questionId = $('#data').data('questionid');
     $btn.on('click',function(){
-    	var markerId = $('#data').data('markerid');
-    	var questionId = $('#data').data('questionid');
 	    $.get($('#data').data('url'),{"markerId":markerId,"questionId":questionId},function(data){
-	    	//console.log(data);
 	    	var player = window.BalloonPlayer;
 	    	player.trigger('doNextQuestionMarker',data);
 		  });
 	  });
+	  $("input[name='answer["+questionId+"]']").on('click',function(){
+      if($(this).is(':checked')) {
+      	$(this).attr("checked","checked").parent().siblings().find("input").removeAttr("checked"); 
+      }
+    });
 	}
 });
