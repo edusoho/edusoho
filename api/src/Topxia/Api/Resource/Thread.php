5ca5a2b253f5511817145698f1a90098ae150de4
suju <suju@howzhi.com>
2015-12-24 14:53:08 +0600

    Thread create api add params

--- api/src/Topxia/Api/Resource/Thread.php
+++ api/src/Topxia/Api/Resource/Thread.php
@@ -7,19 +7,40 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Thread extends BaseResource
 {
-	public function create(Application $app, Request $request, $id)
+	public function create(Application $app, Request $request)
     {
     	try{
             $data = $request->request->all();
-            $data['targetType'] = $target['type'];
-            $data['targetId'] = $target['id'];
+            if (!isset($data["type"])) {
+                $data['type'] = "question";
+            }
+
+            $targetType = $data['targetType'];
+            $targetId = $data['targetId'];
+
+            if (empty($targetId) || empty($targetType)) {
+                return $this->error('500', "创建问答失败，缺失数据");
+            }
+            if ("lesson" == $targetType) {
+                $lesson = $this->getCourseService()->getLesson($targetId);
+                if (empty($lesson)) {
+                    return $this->error('500', "课时不存在");
+                }
+                $data['courseId'] = $lesson['courseId'];
+                $data['lessonId'] = $targetId;
+            } else {
+                $data['courseId'] = $targetId;
+            }
+
+            unset($data['targetId']);
+            unset($data['targetType']);
 
             $thread = $this->getThreadService()->createThread($data);
             if (!empty($thread)) {
             	return array("threadId" => $thread['id']);
             }
         } catch (\Exception $e){
-            return $this->error('500', '发帖错误');
+            return $this->error('500', "发帖错误");
         }
 
         return $this->error('500', '发帖错误');
@@ -32,6 +53,6 @@ class Thread extends BaseResource
 
     protected function getThreadService()
     {
-        return $this->getServiceKernel()->createService('Thread.ThreadService');
+        return $this->getServiceKernel()->createService('Course.ThreadService');
     }
 }
