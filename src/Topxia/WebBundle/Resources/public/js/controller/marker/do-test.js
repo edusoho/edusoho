a5bef6e4d377b6954e6492124b26f05316b5184f
wengzheqing <wengzheqing@howzhi.com>
2015-12-28 10:26:37 +0800

    调试

--- src/Topxia/WebBundle/Resources/public/js/controller/marker/do-test.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/marker/do-test.js
@@ -1,15 +1,18 @@
 define(function(require, exports, module) {
     exports.run = function() {
-        var $close = $(".modal-header .close");
-        var $btn =$('#submitQuestion');
-        var $data = $("#data");
-        var markerId = $data.data('markerid');
-        var questionId = $data.data('questionid');
-        $btn.on('click', function() {
-            $.get($data.data('url'), {
+        var close = $(".modal-header .close");
+        var btn =$('#submitQuestion');
+        var data = $("#data");
+        var markerId = data.data('markerid');
+        var questionId = data.data('questionid');
+        console.log(markerId);
+        btn.on('click', function() {
+        	console.log(markerId);
+            $.get(data.data('url'), {
                 "markerId": markerId,
                 "questionId": questionId
             }, function(data) {
+            	console.log(markerId);
                 var player = window.frames["viewerIframe"].window.BalloonPlayer;
                 player.trigger('doNextQuestionMarker', data);
             });
@@ -19,7 +22,7 @@ define(function(require, exports, module) {
                 $(this).attr("checked", "checked").parent().siblings().find("input").removeAttr("checked");
             }
         });
-        $close.on('click',function() {
+        close.on('click',function() {
             $(".modal").html("").hide();
         });
     }
