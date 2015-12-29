3e57836c27f151287299a94bdc4c69ac193d27ea
wangtao <392521774@qq.com>
2015-12-29 17:00:13 +0800

    fix

--- src/Mooc/Service/Course/Impl/CourseScoreServiceImpl.php
+++ src/Mooc/Service/Course/Impl/CourseScoreServiceImpl.php
@@ -134,8 +134,11 @@ class CourseScoreServiceImpl extends BaseService implements CourseScoreService
             throw $this->createServiceException('成绩发布预告时间，无法更新设置课程评分！');
         }
 
-        $fields['expectPublishTime'] = strtotime($fields['expectPublishTime']);
-        $scoreSetting                = $this->getCourseScoreSettingDao()->updateScoreSetting($courseId, $fields);
+        if (!empty($fields['expectPublishTime'])) {
+            $fields['expectPublishTime'] = strtotime($fields['expectPublishTime']);
+        }
+
+        $scoreSetting = $this->getCourseScoreSettingDao()->updateScoreSetting($courseId, $fields);
         $this->dispatchEvent("scoreSetting.update", $scoreSetting);
 
         return $scoreSetting;
