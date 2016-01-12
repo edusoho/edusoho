051467590ca9e38767cab4fa4048dc13c6e0f897
wengzheqing <wengzheqing@howzhi.com>
2016-01-12 15:40:44 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/marker/answer.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/marker/answer.js
@@ -9,5 +9,9 @@ define(function(require, exports, module) {
             var player = window.frames["viewerIframe"].window.BalloonPlayer;
             player.trigger('onMarkerReached', markerId);
         });
+        $(".marker-modal .close").on('click', function() {
+            console.log($(this).closest('#modal'));
+            $(this).closest('#modal').hide();
+        });
     }
 });
