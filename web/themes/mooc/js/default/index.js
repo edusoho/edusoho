0b3bb54b0f7a89bbd6cf02d565fd3e8ce79c7e45
mowanping <mowanping@howzhi.com>
2016-01-14 10:59:19 +0800

    优化动画效果

--- web/themes/mooc/js/default/index.js
+++ web/themes/mooc/js/default/index.js
@@ -1,9 +1,8 @@
 define(function(require, exports, module) {
-    require("jquery.nivo.slider");
+    require("jquery.waypoints");
     var Lazyload = require('echo.js');
 
     var Swiper = require('swiper');
-
     exports.run = function() {
     var swiper = new Swiper('.es-poster.swiper-container', {
             pagination: '.es-poster .swiper-pager',
@@ -25,8 +24,6 @@ define(function(require, exports, module) {
             e.preventDefault()
             swiper.swipeNext()
         })
-      
-
         var swiper2 = new Swiper('.set-right.swiper-container', {
             autoplay: 6000,
             autoplayDisableOnInteraction: false,
@@ -45,8 +42,6 @@ define(function(require, exports, module) {
             e.preventDefault()
             swiper2.swipeNext()
         })
-
-
         var swiper3 = new Swiper('.class-list.swiper-container', {
             calculateHeight: true,
             roundLengths: true,
@@ -63,7 +58,6 @@ define(function(require, exports, module) {
             e.preventDefault()
             swiper3.swipeNext()
         })
-     
         var swiper4 = new Swiper('.teach-list.swiper-container', {
             pagination: '.teach-list .swiper-pager',
             paginationClickable: true,
@@ -87,22 +81,14 @@ define(function(require, exports, module) {
               }
             }
           }();
-         $(window).scroll(function() {   
-            var scroll = $(window).scrollTop();
-
-            if($(".mooc-learn").length){
-              var mottotop = $(".mooc-learn").position().top-$(window).height();
-              if (scroll >= mottotop) {
-                  $(".mooc-learn .img").addClass("animate");
-              }else if($(".mooc-feature").length){
-                var mottotop = $(".mooc-feature").position().top-$(window).height();
-                if (scroll >= mottotop) {
-                    $(".mooc-feature .row").addClass("animate");
-                }
-              }       
-            }
-
-          });
+          var indexAnimate = function () {
+              $(".js-animate-item").waypoint(function(){
+                $(this).addClass('active');
+              },{offset:500});
+          }();
+        
+       
+         
 
          var swiperLength = function (obj,parentsObj) {
            if($(obj).length<=4){
