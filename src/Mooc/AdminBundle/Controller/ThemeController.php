a4604ed43f7ff19aaf6e792081b5aa63a940e601
lcj <lincanjun@howzhi.com>
2016-01-15 17:10:33 +0800

    主题信息修改

---
+++ src/Mooc/AdminBundle/Controller/ThemeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mooc\AdminBundle\Controller;
+
+use Topxia\AdminBundle\Controller\ThemeController as BaseController;
+
+class ThemeController extends BaseController
+{
+    protected function getTheme($uri)
+    {
+        if (empty($uri)) {
+            return;
+        }
+
+        $dir = $this->container->getParameter('kernel.root_dir').'/../web/themes';
+
+        $metaPath = $dir.'/'.$uri.'/theme.json';
+
+        if (!file_exists($metaPath)) {
+            return;
+        }
+
+        $theme = json_decode(file_get_contents($metaPath), true);
+
+        if (empty($theme)) {
+            return;
+        }
+
+        if (empty($theme['themeType']) || $theme['themeType'] != 'mooc') {
+            return;
+        }
+
+        $theme['uri'] = $uri;
+
+        return $theme;
+    }
+}
