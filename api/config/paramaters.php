bd92afaff233389dc2d8766edee3afa50352c952
Wellming Li <wellming.li@howzhi.com>
2016-01-20 15:09:49 +0800

    fix

--- api/config/paramaters.php
+++ api/config/paramaters.php
@@ -2,7 +2,6 @@
 
 use Symfony\Component\Yaml\Yaml;
 
-$environment = 'dev';
 $parameters  = Yaml::parse(file_get_contents(__DIR__.'/../../app/config/parameters.yml'));
 
 if (file_exists(__DIR__.'/../app/config/parameters_service.yml')) {
@@ -12,6 +11,6 @@ if (file_exists(__DIR__.'/../app/config/parameters_service.yml')) {
 
 $parameters['parameters']['topxia.upload.public_directory'] = __DIR__.'/../../web/files';
 $parameters['parameters']['kernel.logs_dir']                = __DIR__.'/../../app/logs';
-$parameters['parameters']['environment']                    = $environment;
-$parameters['parameters']['kernel.cache_dir']               = __DIR__.'/../../app/cache/'.$environment;
+$parameters['parameters']['environment']                    = API_ENV;
+$parameters['parameters']['kernel.cache_dir']               = __DIR__.'/../../app/cache/' . API_ENV;
 return $parameters['parameters'];
