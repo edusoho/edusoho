04a8cb821300dc7b20f89dbc43f9dafb06242d88
zhanggaosheng <245085167@howzhi.com>
2016-01-22 15:05:19 +0800

    ix

--- api/src/Topxia/Api/Resource/MyCourses.php
+++ api/src/Topxia/Api/Resource/MyCourses.php
@@ -27,7 +27,7 @@ class MyCourses extends BaseResource
 
     public function filter(&$res)
     {
-        return $this->multicallFilter('Course', $res);
+        return $this->multicallFilter('MyCourse', $res);
     }
 
     protected function multicallFilter($name, &$res)
