2a69a32eb0d03555835c360451c69135cab58cf8
lvjie <lujie@howzhi.com>
2016-01-29 10:14:51 +0800

    推荐优先级高于普通，有推荐班级，先按推荐班级排序，无推荐班级或推荐不足，先按推荐再按普通班级创建时间倒序

--- src/Topxia/WebBundle/Extensions/DataTag/RecommendClassroomsDataTag.php
+++ src/Topxia/WebBundle/Extensions/DataTag/RecommendClassroomsDataTag.php
@@ -3,48 +3,65 @@
 namespace Topxia\WebBundle\Extensions\DataTag;
 
 use Topxia\WebBundle\Extensions\DataTag\DataTag;
-use Topxia\Common\ArrayToolkit;
 
-class RecommendClassroomsDataTag extends CourseBaseDataTag implements DataTag  
+class RecommendClassroomsDataTag extends CourseBaseDataTag implements DataTag
 {
     /**
      * 获取推荐班级列表
      *
      * 可传入的参数：
      *   count    必需 班级数量，取值不能超过100
-     * 
-     * @param  array $arguments 参数
+     *
+     * @param  array $arguments           参数
      * @return array 班级推荐列表
      */
     public function getData(array $arguments)
-    {	
+    {
         $this->checkCount($arguments);
-        
+
         $conditions = array(
-            'status' => 'published',
+            'status'   => 'published',
             'showable' => 1
         );
 
         $classrooms = $this->getClassroomService()->searchClassrooms(
-                $conditions,
-                array('recommendedSeq','ASC'),
-                0,
-                $arguments['count']
+            $conditions,
+            array('recommendedSeq', 'ASC'),
+            0,
+            $arguments['count']
         );
 
-        $users = array();
+        $recommendedClassrooms = array();
+        $normalClassromms      = array();
+
+        foreach ($classrooms as $classroom) {
+            if ($classroom['recommended'] == 1) {
+                $recommendedClassrooms[] = $classroom;
+            } else {
+                $normalClassromms[] = $classroom;
+            }
+        }
+
+        foreach ($normalClassromms as $key => $value) {
+            $createdTime[$key] = $value['createdTime'];
+        }
+
+        array_multisort($createdTime, SORT_DESC, $normalClassromms);
+
+        $classrooms = array_merge($recommendedClassrooms, $normalClassromms);
+        $users      = array();
 
         foreach ($classrooms as &$classroom) {
             if (empty($classroom['teacherIds'])) {
-                $classroomTeacherIds=array();
-            }else{
-                $classroomTeacherIds=$classroom['teacherIds'];
+                $classroomTeacherIds = array();
+            } else {
+                $classroomTeacherIds = $classroom['teacherIds'];
             }
 
-            $users = $this->getUserService()->findUsersByIds($classroomTeacherIds);
+            $users              = $this->getUserService()->findUsersByIds($classroomTeacherIds);
             $classroom['users'] = $users;
         }
-        
+
         return $classrooms;
     }
 
@@ -62,5 +79,4 @@ class RecommendClassroomsDataTag extends CourseBaseDataTag implements DataTag
     {
         return $this->getServiceKernel()->createService('Course.CourseService');
     }
-
 }
