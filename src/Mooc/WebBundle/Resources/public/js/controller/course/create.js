1cdabfa14f475b5b6c3292f4e3bce0815769c05e
wangtao <392521774@qq.com>
2016-02-01 16:39:21 +0800

    fix

--- src/Mooc/WebBundle/Resources/public/js/controller/course/create.js
+++ src/Mooc/WebBundle/Resources/public/js/controller/course/create.js
@@ -28,27 +28,6 @@ define(function(require, exports, module) {
         }
 
 
-        // var uploader = new WebUploader({
-        //     element: '#upload-picture-btn'
-        // });
-
-        // uploader.on('uploadSuccess', function(file, response ) {
-        //     var url = $("#upload-picture-btn").data("gotoUrl");
-        //     Notify.success('上传成功！', 1);
-        //     document.location.href = url;
-        // });
-
-        // $('.use-partner-avatar').on('click', function(){
-        //     var $this = $(this);
-        //     var goto = $this.data('goto');
-
-        //     $.post($this.data('url'), {imgUrl:$this.data('imgUrl')},function(){
-        //         window.location.href = goto;
-        //     });
-        // });
-
-
-
         var editor = CKEDITOR.replace('profile_about', {
             toolbar: 'Simple',
             filebrowserImageUploadUrl: $('#profile_about').data('imageUploadUrl')
