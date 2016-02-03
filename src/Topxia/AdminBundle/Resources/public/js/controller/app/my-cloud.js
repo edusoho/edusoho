b3adec7075857102d92c7fbb169d7ab8a1603fc6
wengzheqing <wengzheqing@howzhi.com>
2016-02-03 14:45:21 +0800

    概览

--- src/Topxia/AdminBundle/Resources/public/js/controller/app/my-cloud.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/app/my-cloud.js
@@ -13,30 +13,35 @@ define(function(require, exports, module) {
         var videoData = eval ("(" + $('#videoUsedInfo').attr("value") + ")");
         var smsData = eval ("(" + $('#smsUsedInfo').attr("value") + ")");
         var liveData = eval ("(" + $('#liveUsedInfo').attr("value") + ")");
-        Morris.Bar({
-          element: 'storage-data-tab',
-          data: videoData,
-          xkey: 'date',
-          ykeys: ['count'],
-          labels: ['使用量']
-        });
-
-        Morris.Bar({
-          element: 'live-data-tab',
-          data: liveData,
-          xkey: 'date',
-          ykeys: ['count'],
-          labels: ['使用量']
-        });
-
-        Morris.Bar({
-          element: 'sms-data-tab',
-          data: smsData,
-          xkey: 'date',
-          ykeys: ['count'],
-          labels: ['使用量']
-        });
-
+        console.log(liveData);
+        //console.log(videoData==""?[{"date":"2015-03","count":5},{"date":"2015-04","count":9},{"date":"2015-05","count":77},{"date":"2015-06","count":10},{"date":"2015-07","count":40},{"date":"2015-08","count":30},{"date":"2015-09","count":20}]:"");
+        if(videoData!=""){
+          Morris.Bar({
+            element: 'storage-data-tab',
+            data: videoData==""?[{"date":"2015-03","count":0},{"date":"2015-04","count":0},{"date":"2015-05","count":0},{"date":"2015-06","count":0},{"date":"2015-07","count":0},{"date":"2015-08","count":0},{"date":"2015-09","count":0}]:"",
+            xkey: 'date',
+            ykeys: ['count'],
+            labels: ['使用量']
+          });
+        }
+        if(liveData!=""){
+          Morris.Bar({
+            element: 'live-data-tab',
+            data: liveData,
+            xkey: 'date',
+            ykeys: ['count'],
+            labels: ['使用量']
+          });
+        }
+        if(smsData!=""){
+          Morris.Bar({
+            element: 'sms-data-tab',
+            data: smsData,
+            xkey: 'date',
+            ykeys: ['count'],
+            labels: ['使用量']
+          });
+        }
         Morris.Bar({
           element: 'email-data-tab',
           data: [{"date":"2015-03","count":5},{"date":"2015-04","count":9},{"date":"2015-05","count":77},{"date":"2015-06","count":10},{"date":"2015-07","count":40},{"date":"2015-08","count":30},{"date":"2015-09","count":20}],
