292863fc1285830d50b47cdff43cf1469fbd7aef
zhanggaosheng <245085167@howzhi.com>
2016-02-03 18:41:37 +0800

    fix

--- src/Topxia/Component/MediaParser/ParserProxy.php
+++ src/Topxia/Component/MediaParser/ParserProxy.php
@@ -1,11 +1,35 @@
 <?php
 namespace Topxia\Component\MediaParser;
 
+use Topxia\Service\Common\ServiceKernel;
+
 class ParserProxy
 {
     public function parseItem($url)
     {
-        $parsers = array('YoukuVideo', 'QQVideo', 'NeteaseOpenCourse', 'TudouVideo', 'LetvYun');
+        $parsers = array('YoukuVideo', 'QQVideo', 'NeteaseOpenCourse', 'TudouVideo');
+
+        $kernel = ServiceKernel::instance();
+        $extras = array();
+
+        if ($kernel->hasParameter('media_parser')) {
+            $extras = $kernel->getParameter('media_parser');
+        }
+
+        if ($extras['item']) {
+            $extrasParsers = $extras['item'];
+
+            foreach ($extrasParsers as $extrasParser) {
+                $class  = $extrasParser['class'];
+                $parser = new $class();
+
+                if (!$parser->detect($url)) {
+                    continue;
+                }
+
+                return $parser->parse($url);
+            }
+        }
 
         foreach ($parsers as $parserName) {
             $class  = __NAMESPACE__."\\ItemParser\\{$parserName}ItemParser";
@@ -24,6 +48,25 @@ class ParserProxy
     public function parseAlbum($url)
     {
         $parsers = array('YoukuVideo', 'QQVideo', 'NeteaseOpenCourse', 'SinaOpenCourse');
+
+        if ($kernel->hasParameter('MediaParser')) {
+            $extras = $kernel->getParameter('MediaParser');
+        }
+
+        if ($extras['Album']) {
+            $extrasParsers = $extras['Album'];
+
+            foreach ($extrasParsers as $extrasParser) {
+                $class  = $extrasParser['class'];
+                $parser = new $class();
+
+                if (!$parser->detect($url)) {
+                    continue;
+                }
+
+                return $parser->parse($url);
+            }
+        }
 
         foreach ($parsers as $parserName) {
             $class  = __NAMESPACE__."\\AlbumParser\\{$parserName}AlbumParser";
