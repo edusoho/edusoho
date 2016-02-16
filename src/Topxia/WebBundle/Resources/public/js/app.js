40e1fd84bbaa25804c325df874ba5ccc3472e819
钟杰 <ketu@ketuzhong-mbp.lan>
2016-02-16 22:03:21 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/app.js
+++ src/Topxia/WebBundle/Resources/public/js/app.js
@@ -127,15 +127,7 @@ define(function(require, exports, module) {
 	    });
 	    
 	} else {
-		// $("body").on("click","li.nav-hover",function(){
-		// 	$(this).toggleClass("open");
-		// })
-// console.log(1);
         $("li.nav-hover >a").attr("data-toggle","dropdown");
-
-		// if ($(".nav-mobile li.nav-hover").is(":has(ul)")) {
-		// 	$(".nav-mobile li.nav-hover>a").attr("href","javascript:;");
-		// }
 	}
 	
 
