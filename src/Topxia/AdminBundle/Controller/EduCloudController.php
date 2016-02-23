0b6e5303abd6165f0d610bfec4b6f8b43889e6ee
wengzheqing <wengzheqing@howzhi.com>
2016-02-23 14:15:42 +0800

    fix

--- src/Topxia/AdminBundle/Controller/EduCloudController.php
+++ src/Topxia/AdminBundle/Controller/EduCloudController.php
@@ -312,6 +312,7 @@ class EduCloudController extends BaseController
             return $this->render('TopxiaAdminBundle:EduCloud:sms.html.twig', array(
                 'locked'    => isset($info['locked']) ? $info['locked'] : 0,
                 'enabled'   => isset($info['enabled']) ? $info['enabled'] : 1,
+                'level'     => isset($info['level']) ? $info['level'] : 'error',
                 'smsStatus' => $smsStatus
             ));
         } catch (\RuntimeException $e) {
