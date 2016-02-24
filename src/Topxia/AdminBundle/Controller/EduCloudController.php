6930d6b990af0dd87f8a2006765b3b28fb164420
wengzheqing <wengzheqing@howzhi.com>
2016-02-24 20:01:43 +0800

    fix

--- src/Topxia/AdminBundle/Controller/EduCloudController.php
+++ src/Topxia/AdminBundle/Controller/EduCloudController.php
@@ -435,7 +435,6 @@ class EduCloudController extends BaseController
             $api = CloudAPIFactory::create('root');
             $api->setApiUrl('http://124.160.104.74:8098/');
             $info = $api->get('/me');
-            var_dump($info);
         } catch (\RuntimeException $e) {
             $info['error'] = 'error';
         }
