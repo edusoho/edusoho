979a4a7179acc7e34eacbd06d68dd0446e1b7235
dongjianbin <dongjianbin@howzhi.com>
2016-02-25 10:42:17 +0800

    fix

---
+++ src/Topxia/Service/MoneyCard/Tests/MoneyCardServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Topxia\Service\MoneyCard\Tests;
+
+use Topxia\Service\Common\BaseTestCase;
+
+class MoneyCardServiceTest extends BaseTestCase
+{
+	public function testCreateMoneyCard()
+	{
+		$currentUser = new CurrentUser();
+
+		$MoneyCardData = array(
+			'cardId' => 1,
+			'password' => 12345,
+			'deadline' => 
+			);
+	}
+}
