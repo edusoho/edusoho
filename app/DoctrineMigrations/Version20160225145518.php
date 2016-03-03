0fe079e6e105f8528dde9ecf05ef293b7f8661f9
dongjianbin <dongjianbin@howzhi.com>
2016-03-03 10:49:26 +0800

    fix

--- app/DoctrineMigrations/Version20160225145518.php
+++ app/DoctrineMigrations/Version20160225145518.php
@@ -24,7 +24,7 @@ class Version20160225145518 extends AbstractMigration
                          `orderType` varchar(32) NOT NULL,
                          `showCount` int(10) NOT NULL,
                          `createdTime` int(10) unsigned NOT NULL,
-                         `updateTime` int(10) unsigned NOT NULL,
+                         `updateTime` int(10) unsigned NOT NULL DEFAULT '0',
                          PRIMARY KEY (`id`)
                         ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
                         ");
