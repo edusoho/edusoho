06dbe9ca97315d334f7d66e308c95ee5406b22d2
nifeng <fengni@fengdeMacBook-Pro.local>
2016-03-03 14:09:36 +0800

    refactor code

--- src/Topxia/AdminBundle/Controller/DiscoveryColumnController.php
+++ src/Topxia/AdminBundle/Controller/DiscoveryColumnController.php
@@ -2,43 +2,42 @@
 
 namespace Topxia\AdminBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Topxia\Common\ArrayToolkit;
-use Topxia\Common\FileToolkit;
-use Topxia\Component\OAuthClient\OAuthClientFactory;
-use Topxia\Service\Util\CloudClientFactory;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DiscoveryColumnController extends BaseController
 {
-	public function deleteAction(Request $request, $id)
+    public function deleteAction(Request $request, $id)
     {
         $this->getDiscoveryColumnService()->deleteDiscoveryColumn($id);
-        return $this->redirect($this->generateUrl('admin_operation_mobile_find'));
+        return $this->redirect($this->generateUrl('admin_discovery_column_index'));
     }
 
-    public function findAction(Request $request)
+    public function indexAction(Request $request)
     {
         $discoveryColumns = array();
         $discoveryColumns = $this->getDiscoveryColumnService()->getAllDiscoveryColumns();
 
-        return $this->render('TopxiaAdminBundle:System:discovery-column.html.twig',array('discoveryColumns' => $discoveryColumns));
+        return $this->render('TopxiaAdminBundle:DiscoveryColumn:discovery-column.html.twig', array('discoveryColumns' => $discoveryColumns));
     }
 
     public function createAction(Request $request)
     {
         $categoryId = array();
+
         if ($request->getMethod() == 'POST') {
-            $conditions = $request->request->all();
+            $conditions                = $request->request->all();
             $conditions['createdTime'] = time();
-            
+
             $discoveryColumn = $this->getDiscoveryColumnService()->findDiscoveryColumnByTitle($conditions['title']);
-            if (empty($discoveryColumn)&&$conditions['title']) {
+
+            if (empty($discoveryColumn) && $conditions['title']) {
                 $discoveryColumn = $this->getDiscoveryColumnService()->addDiscoveryColumn($conditions);
             }
-            return $this->redirect($this->generateUrl('admin_operation_mobile_find'));
+
+            return $this->redirect($this->generateUrl('admin_discovery_column_index'));
         }
+
         if (empty($categoryId)) {
             $categoryId = 0;
         }
@@ -46,28 +45,30 @@ class DiscoveryColumnController extends BaseController
         if (empty($discoveryColumn)) {
             $discoveryColumn = array();
         }
-        return $this->render('TopxiaAdminBundle:System:discovery-column-modal.html.twig',array(
-                'discoveryColumn' => $discoveryColumn,
-                'categoryId' => $categoryId
-            ));
+
+        return $this->render('TopxiaAdminBundle:DiscoveryColumn:discovery-column-modal.html.twig', array(
+            'discoveryColumn' => $discoveryColumn,
+            'categoryId'      => $categoryId
+        ));
     }
 
     public function editAction(Request $request, $id)
     {
         $discoveryColumn = $this->getDiscoveryColumnService()->getDiscoveryColumn($id);
+
         if (empty($discoveryColumn)) {
             throw $this->createNotFoundException();
         }
 
         if ($request->getMethod() == 'POST') {
-            $conditions = $request->request->all();
+            $conditions      = $request->request->all();
             $discoveryColumn = $this->getDiscoveryColumnService()->updateDiscoveryColumn($id, $conditions);
-            return $this->redirect($this->generateUrl('admin_operation_mobile_find'));
+            return $this->redirect($this->generateUrl('admin_discovery_column_index'));
         }
 
-        return $this->render('TopxiaAdminBundle:System:discovery-column-modal.html.twig', array(
+        return $this->render('TopxiaAdminBundle:DiscoveryColumn:discovery-column-modal.html.twig', array(
             'discoveryColumn' => $discoveryColumn,
-            'categoryId' => $discoveryColumn['categoryId']
+            'categoryId'      => $discoveryColumn['categoryId']
         ));
     }
 
