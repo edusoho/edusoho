d6519d87c68836e6c7fe0ba329b2fb4f34e2a7e6
Huang Boyi <huangboyi@howzhi.com>
2016-03-04 14:47:04 +0800

    fix classroom datas

--- api/src/Topxia/Api/Resource/Classrooms.php
+++ api/src/Topxia/Api/Resource/Classrooms.php
@@ -7,42 +7,47 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Classrooms extends BaseResource
 {
-	public function discoveryColumn(Application $app, Request $request)
+    public function discoveryColumn(Application $app, Request $request)
     {
-    	$result = $request->query->all();
+        $result = $request->query->all();
 
-        $childrenIds = $this->getCategoryService()->findCategoryChildrenIds($result['categoryId']);
+        $childrenIds               = $this->getCategoryService()->findCategoryChildrenIds($result['categoryId']);
         $conditions['categoryIds'] = array_merge(array($result['categoryId']), $childrenIds);
         unset($conditions['categoryId']);
 
         if ($result['orderType'] == 'hot') {
-        	$orderBy = 'studentNum';
-        }
-        elseif ($result['orderType'] == 'new') {
-        	$orderBy = 'createdTime';
-        }
-        else {
-        	$orderBy = 'recommendedSeq';
+            $orderBy = 'studentNum';
+        } elseif ($result['orderType'] == 'new') {
+            $orderBy = 'createdTime';
+        } else {
+            $orderBy = 'recommendedSeq';
         }
 
-        $classrooms = $this->getClassroomService()->searchClassrooms($conditions,array($orderBy, 'desc'),0,$result['showCount']);
+        $classrooms = $this->getClassroomService()->searchClassrooms($conditions, array($orderBy, 'desc'), 0, $result['showCount']);
 
-        return $classrooms;
+        return $this->wrap($this->filter($classrooms), $total);
     }
 
     public function get(Application $app, Request $request)
     {
-    	
     }
 
     public function post(Application $app, Request $request)
     {
-    	
     }
 
     public function filter(&$res)
     {
-        
+        return $this->multicallFilter('Classroom', $res);
+    }
+
+    protected function multicallFilter($name, &$res)
+    {
+        foreach ($res as &$one) {
+            $this->callFilter($name, $one);
+        }
+
+        return $res;
     }
 
     protected function getCategoryService()
@@ -54,5 +59,4 @@ class Classrooms extends BaseResource
     {
         return $this->getServiceKernel()->createService('Classroom:Classroom.ClassroomService');
     }
-
 }
