54b902c581372ca9e00b96c8c9cead4589f19838
杨佳龙 <yangjlong315@gmail.com>
2016-03-04 14:57:18 +0800

    sign单元测试

--- src/Topxia/Service/Sign/Tests/SignServiceTest.php
+++ src/Topxia/Service/Sign/Tests/SignServiceTest.php
@@ -7,7 +7,7 @@ class SignServiceTest extends BaseTestCase
 {
     public function testUserSign()
     {
-        $user = $this->createUser();
+        $user = $this->createUser('user');
         $sign = $this->getSignService()->userSign($user['id'], 'classroom_sign', 1);
         $this->assertNotEmpty($sign);
         $this->assertEquals($user['id'], $sign['userId']);
@@ -17,28 +17,36 @@ class SignServiceTest extends BaseTestCase
 
     public function testIsSignedToday()
     {
-        $user1 = $this->createUser();
-        $user2 = $this->createUser();
+        $user1 = $this->createUser('user1');
+        $user2 = $this->createUser('user2');
         $this->getSignService()->userSign($user2['id'], 'classroom_sign', 1);
         $signToday1 = $this->getSignService()->isSignedToday($user1['id'], 'classroom_sign', 1);
         $signToday2 = $this->getSignService()->isSignedToday($user2['id'], 'classroom_sign', 1);
-        $this->assertTrue($signToday1);
-        $this->assertFalse($signToday2);
+        $this->assertFalse($signToday1);
+        $this->assertTrue($signToday2);
     }
 
-    // public function testIsYestodaySigned()
-    // {
-    //     $user         = $this->createUser();
-    //     $signYestoday = $this->getSignService()->isYestodaySigned($user['id'], 'classroom_sign', 1);
-    //     $this->assertTrue($signYestoday);
-    // }
+    public function testIsYestodaySigned()
+    {
+        $user         = $this->createUser('user');
+        $signYestoday = $this->getSignService()->isYestodaySigned($user['id'], 'classroom_sign', 1);
+        $this->assertFalse($signYestoday);
+    }
 
-    // public function testGetSignRecordsByPeriod()
-    // {
-    //     $user1    = array('id' => 1, 'nickname' => 'user', 'password' => 'user');
-    //     $user2    = array('id' => 1, 'nickname' => 'user', 'password' => 'user');
-    //     $getSign1 = $this->getSignService()->getSignRecordsByPeriod();
-    // }
+    public function testGetSignRecordsByPeriod()
+    {
+        $today    = date("Y-m-d");
+        $yestoday = date("Y-m-d", strtotime("-1 day"));
+        $user1    = $this->createUser('user1');
+        $user2    = $this->createUser('user2');
+        $this->getSignService()->userSign($user2['id'], 'classroom_sign', 1);
+        $getSign1 = $this->getSignService()->getSignRecordsByPeriod($user1['id'], 'classroom_sign', 1, $yestoday, $today);
+        $getSign2 = $this->getSignService()->getSignRecordsByPeriod($user2['id'], 'classroom_sign', 1, $yestoday, $today);
+        $this->assertNull($getSign1);
+        $this->assertEquals($user2['id'], $getSign2[0]['userId']);
+        $this->assertEquals('classroom_sign', $getSign2[0]['targetType']);
+        $this->assertEquals('1', $getSign2[0]['targetId']);
+    }
 
     // public function testgetSignUserStatistics()
     // {
@@ -48,25 +56,25 @@ class SignServiceTest extends BaseTestCase
     // {
     // }
 
-    // public function testgetTodayRank()
-    // {
-    //     $user1 = array('id' => 1, 'nickname' => 'user', 'password' => 'user');
-    //     $user2 = array('id' => 1, 'nickname' => 'user', 'password' => 'user');
-    //     $rank1=$this->getSignService()->getTodayRank($user1['id'],'classroom_sign',1);
-    //     $this->getSignService()->userSign($user2['id'], 'classroom_sign', 1);
-    //     $rank2=$this->getSignService()->getTodayRank($user2['id'],'classroom_sign',1)
-    // }
-
-    private function createUser()
+    public function testgetTodayRank()
     {
-        $user              = array();
-        $user['id']        = 1;
-        $user['email']     = "user@user.com";
-        $user['nickname']  = "user";
-        $user['password']  = "user";
-        $user['currentIp'] = '127.0.0.1';
-        $user['roles']     = array('ROLE_USER', 'ROLE_SUPER_ADMIN', 'ROLE_TEACHER');
-        return $user;
+        $user1 = $this->createUser('user1');
+        $user2 = $this->createUser('user2');
+        $rank1 = $this->getSignService()->getTodayRank($user1['id'], 'classroom_sign', 1);
+        $this->getSignService()->userSign($user2['id'], 'classroom_sign', 1);
+        $rank2 = $this->getSignService()->getTodayRank($user2['id'], 'classroom_sign', 1);
+        $this->assertEquals('-1', $rank1);
+        $this->assertEquals('1', $rank2);
+    }
+
+    protected function createUser($user)
+    {
+        $userInfo             = array();
+        $userInfo['email']    = "{$user}@{$user}.com";
+        $userInfo['nickname'] = "{$user}";
+        $userInfo['password'] = "{$user}";
+        $userInfo['loginIp']  = '127.0.0.1';
+        return $this->getUserService()->register($userInfo);
     }
 
     protected function getUserService()
