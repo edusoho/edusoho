01cd1317e81abb9b3cbd44efd3b7fb577e10d535
nifeng <fengni@fengdeMacBook-Pro.local>
2016-03-07 17:09:44 +0800

    global file player

--- src/MaterialLib/MaterialLibBundle/Controller/GlobalFilePlayerController.php
+++ src/MaterialLib/MaterialLibBundle/Controller/GlobalFilePlayerController.php
@@ -34,6 +34,49 @@ class GlobalFilePlayerController extends BaseController
         ));
     }
 
+    protected function getPlayUrl($globalId, $context)
+    {
+        $file = $this->getMaterialLibService()->get($globalId);
+
+        if (empty($file)) {
+            throw $this->createNotFoundException();
+        }
+
+        if (!in_array($file["type"], array("audio", "video"))) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $factory = new CloudClientFactory();
+        $client  = $factory->createClient();
+
+        if (!empty($file['metas']) && !empty($file['metas']['levels']['sd']['key'])) {
+            // if (isset($file['convertParams']['convertor']) && ($file['convertParams']['convertor'] == 'HLSEncryptedVideo')) {
+            $token = $this->makeToken('hls.playlist', $file['id'], $context);
+
+            $params = array(
+                'id'    => $file['id'],
+                'token' => $token['token']
+            );
+
+            return $this->generateUrl('hls_playlist', $params, true);
+            // } else {
+            //     $result = $client->generateHLSQualitiyListUrl($file['metas'], 3600);
+            // }
+        } else {
+            if (!empty($file['metas']) && !empty($file['metas']['hd']['key'])) {
+                $key = $file['metas']['hd']['key'];
+            } else {
+                $key = $file['reskey'];
+            }
+
+            if ($key) {
+                $result = $client->generateFileUrl($client->getBucket(), $key, 3600);
+            }
+        }
+
+        return $result['url'];
+    }
+
     protected function getMaterialLibService()
     {
         return $this->createService('MaterialLib:MaterialLib.MaterialLibService');
