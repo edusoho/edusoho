02cee043cb17a729bb6935a7002a5c8047c180ee
wangtao <392521774@qq.com>
2016-03-07 16:42:25 +0800

    fix

--- src/Topxia/WebBundle/Controller/EditorController.php
+++ src/Topxia/WebBundle/Controller/EditorController.php
@@ -58,54 +58,6 @@ class EditorController extends BaseController
         }
     }
 
-    public function fileUploadAction(Request $request)
-    {
-        try {
-            $token = $request->query->get('token');
-
-            $maker = new UploadToken();
-            $token = $maker->parse($token);
-
-            if (empty($token)) {
-                throw new \RuntimeException("上传授权码已过期，请刷新页面后重试！");
-            }
-
-            $originalFile = $request->files->get('file');
-            $fileName     = $originalFile->getClientOriginalName();
-
-            if ($token['type'] == 'file') {
-                $errors = FileToolkit::validateFileExtension($originalFile);
-
-                if (!empty($errors)) {
-                    throw new \RuntimeException("您上传的文件类型不支持，请重新上传。");
-                }
-            } else {
-                throw new \RuntimeException("上传类型不正确！");
-            }
-
-            $settings = $this->getSettingService()->get('storage', array());
-
-            $file        = $this->getUploadFileService()->addFile('attachment', 0, array('isPublic' => 0), 'local', $originalFile);
-            $downloadUrl = $this->generateUrl('editor_file_download', array('fileId' => $file['id']));
-
-            $response = "附件：<a color=\"red\" href=\"{$downloadUrl}\">".$fileName."</a>";
-            return new Response($response);
-        } catch (\Exception $e) {
-            $message  = $e->getMessage();
-            $response = $message;
-            return new Response($response);
-        }
-    }
-
-    public function fileDownloadAction(Request $request, $fileId, $isDownload = true)
-    {
-        return $this->forward("TopxiaWebBundle:UploadFile:download", array(
-            'request'    => $request,
-            'fileId'     => $fileId,
-            'isDownload' => $isDownload
-        ));
-    }
-
     public function downloadAction(Request $request)
     {
         $token = $request->query->get('token');
