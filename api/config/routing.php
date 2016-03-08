2a7d21452854b5b7f4f812ea3a8c74605b19ac80
dongjianbin <dongjianbin@howzhi.com>
2016-03-08 16:21:55 +0800

    fix

--- api/config/routing.php
+++ api/config/routing.php
@@ -21,7 +21,7 @@ $app->mount(_u('/files'), include dirname(__DIR__).'/src/files.php');
 /**
  * 新的路由配置方式
  */
-$app->get(_u('/users/{id}'), 'res.User:get');
+$app->get(_u('/user/{id}'), 'res.User:get');
 $app->get(_u('/users'), 'res.Users:get');
 $app->post(_u('/users'), 'res.Users:post');
 $app->get(_u('/courses'), 'res.Courses:get');
