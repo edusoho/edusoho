8cb1b5ee0f27c5f85d43d6f97d493851c7cc770f
nifeng <fengni@fengdeMacBook-Pro.local>
2016-03-08 23:39:59 +0800

    fix

--- src/Topxia/Service/CloudPlatform/Client/EventCloudAPI.php
+++ src/Topxia/Service/CloudPlatform/Client/EventCloudAPI.php
@@ -14,7 +14,95 @@ class EventCloudAPI extends AbstractCloudAPI
         $event['user']      = $this->accessKey;
         $event["signature"] = $this->makeSignature($event);
 
-        return parent::post('/events', $event);
+        return $this->_request('POST', '/events', $event);
+    }
+
+    protected function _request($method, $uri, $params, $headers = array())
+    {
+        $requestId = substr(md5(uniqid('', true)), -16);
+
+        $url = $this->apiUrl.'/'.$uri;
+        $this->debug && $this->logger && $this->logger->debug("[{$requestId}] {$method} {$url}", array('params' => $params, 'headers' => $headers));
+
+        $headers[] = 'Content-type: application/json';
+
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_USERAGENT, $this->userAgent);
+
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $this->connectTimeout);
+        curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+
+        if ($method == 'POST') {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+        } elseif ($method == 'PUT') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+        } elseif ($method == 'DELETE') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+        } elseif ($method == 'PATCH') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+        } else {
+            if (!empty($params)) {
+                $url = $url.(strpos($url, '?') ? '&' : '?').http_build_query($params);
+            }
+        }
+
+        $headers[] = 'API-REQUEST-ID: '.$requestId;
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        $response = curl_exec($curl);
+        $curlinfo = curl_getinfo($curl);
+
+        $header = substr($response, 0, $curlinfo['header_size']);
+        $body   = substr($response, $curlinfo['header_size']);
+
+        $this->debug && $this->logger && $this->logger->debug("[{$requestId}] CURL_INFO", $curlinfo);
+        $this->debug && $this->logger && $this->logger->debug("[{$requestId}] RESPONSE_HEADER {$header}");
+        $this->debug && $this->logger && $this->logger->debug("[{$requestId}] RESPONSE_BODY {$body}");
+
+        curl_close($curl);
+
+        $context = array(
+            'CURLINFO' => $curlinfo,
+            'HEADER'   => $header,
+            'BODY'     => $body
+        );
+
+        if (empty($curlinfo['namelookup_time'])) {
+            $this->logger && $this->logger->error("[{$requestId}] NAME_LOOK_UP_TIMEOUT", $context);
+        }
+
+        if (empty($curlinfo['connect_time'])) {
+            $this->logger && $this->logger->error("[{$requestId}] API_CONNECT_TIMEOUT", $context);
+            throw new CloudAPIIOException("Connect api server timeout (url: {$url}).");
+        }
+
+        if (empty($curlinfo['starttransfer_time'])) {
+            $this->logger && $this->logger->error("[{$requestId}] API_TIMEOUT", $context);
+            throw new CloudAPIIOException("Request api server timeout (url:{$url}).");
+        }
+
+        if ($curlinfo['http_code'] >= 500) {
+            $this->logger && $this->logger->error("[{$requestId}] API_RESOPNSE_ERROR", $context);
+            throw new CloudAPIIOException("Api server internal error (url:{$url}).");
+        }
+
+        $result = json_decode($body, true);
+
+        if (empty($result)) {
+            $this->logger && $this->logger->error("[{$requestId}] RESPONSE_JSON_DECODE_ERROR", $context);
+            throw new CloudAPIIOException("Api result json decode error: (url:{$url}).");
+        }
+
+        return $result;
     }
 
     public function makeSignature($event)
