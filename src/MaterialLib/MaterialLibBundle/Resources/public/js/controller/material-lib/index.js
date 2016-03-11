da9e6ffbfbea790e5dd7942c400f494cfbf982a8
wengzheqing <wengzheqing@howzhi.com>
2016-03-11 15:57:04 +0800

    fix

--- src/MaterialLib/MaterialLibBundle/Resources/public/js/controller/material-lib/index.js
+++ src/MaterialLib/MaterialLibBundle/Resources/public/js/controller/material-lib/index.js
@@ -5,7 +5,6 @@ define(function(require, exports, module) {
     require('jquery.select2-css');
     require('jquery.select2');
     require('jquery.colorbox');
-    var DetailWidget = require('materiallibbundle/controller/web/detail');
 
     exports.run = function() {
         var MaterialWidget = Widget.extend({
@@ -18,9 +17,8 @@ define(function(require, exports, module) {
                 'click .pagination li': 'onClickPagination',
                 'click .js-detail-btn': 'onClickDetailBtn',
                 'click .js-delete-btn': 'onClickDeleteBtn',
-                'click .js-download-btn': 'onClickDownloadBtn',
                 'click .js-reconvert-btn': 'onClickReconvertBtn',
-                'click .op-li div.op-btn': 'onClickOperationBtn'
+                'click .op-li div.op-btn': 'onClickOperationBtn',
             },
             setup: function() {
                 this.set('renderUrl', $('#material-item-list').data('url'));
@@ -46,54 +44,42 @@ define(function(require, exports, module) {
             },
             onClickDetailBtn: function(event)
             {
-                if (!this.DetailBtnActive) {
-                    var self = this;
-                    var $target = $(event.currentTarget);
-                    this.DetailBtnActive = true;
-                    $.ajax({
-                        type:'GET',
-                        url:$target.data('url'),
-                    }).done(function(resp){
-                        self.element.hide();
-                        self.element.prev().hide();
-                        self.element.parent().append(resp);
-                        new DetailWidget({
-                            element:'#material-detail',
-                            callback: function() {
-                                var $form = $('#material-search-form');
-                                $form.show();
-                                $form.prev().show();
-                                window.materialWidget.renderTable();
-                            }
-                        });
-                    }).fail(function(){
-                        Notify.danger('Opps,出错了!');
-                    }).always(function() {
-                        self.DetailBtnActive = false;
+                var self = this;
+                var $target = $(event.currentTarget);
+                $.ajax({
+                    type:'GET',
+                    url:$target.data('url'),
+                }).done(function(resp){
+                    self.element.hide();
+                    self.element.prev().hide();
+                    self.element.parent().append(resp);
+                    new DetailWidget({
+                        element:'#material-detail',
+                        callback: function() {
+                            var $form = $('#materials-form');
+                            $form.show();
+                            $form.prev().show();
+                            window.materialWidget.renderTable();
+                        }
                     });
-                } 
-                
+                }).fail(function(){
+                    Notify.danger('Opps,出错了!');
+                });
             },
             onClickDeleteBtn: function(event)
             {
-                if (confirm('真的要删除该资源吗？')) {
-                    var self = this;
-                    var $target = $(event.currentTarget);
-                    this._loading();
-                    $.ajax({
-                        type:'POST',
-                        url:$target.data('url'),
-                    }).done(function(){
-                        Notify.success('删除成功!');
-                        self.renderTable();
-                    }).fail(function(){
-                        Notify.danger('删除失败!');
-                    });
-                }
-            },
-            onClickDownloadBtn: function(event) {
+                var self = this;
                 var $target = $(event.currentTarget);
-                window.open($target.data('url'));
+                this._loading();
+                $.ajax({
+                    type:'POST',
+                    url:$target.data('url'),
+                }).done(function(){
+                    Notify.success('删除成功!');
+                    self.renderTable();
+                }).fail(function(){
+                    Notify.danger('删除失败!');
+                });
             },
             onClickReconvertBtn: function(event)
             {
@@ -120,8 +106,11 @@ define(function(require, exports, module) {
                 $.get($target.data('url'),function(data){
                     if(data){
                         $target.find('i').addClass("material-collection");
+                        Notify.success('收藏成功');
                     } else {
                         $target.find('i').removeClass("material-collection");
+                        Notify.success('取消收藏成功');
+
                     }
                 });
             },
@@ -350,6 +339,145 @@ define(function(require, exports, module) {
         window.materialWidget = new MaterialWidget({
             element: '#material-search-form'
         });
+        // $('[name="search-items"]').on('click',function(){
+        //     console.log($(this).data('url'));
+        //     $.post($(this).data('url'), function(date){
+        //         $("[name='item-material-views']").html(date);
+        //     });
+        // });
+        // var $panel = $('#material-lib-items-panel');
+        // require('../../../../topxiaweb/js/util/batch-select')($panel);
+
+        // $(".tip").tooltip();
+
+        //var $list = $("#material-item-list");
+
+        // $list.on('mouseover', '.item-material', function(e) {
+        //     $(".file-name-container", this).hide();
+        //     $(".action-buttons-container", this).show();
+        // });
+
+        // $list.on('mouseout', '.item-material', function(e) {
+        //     $(".file-name-container", this).show();
+        //     $(".action-buttons-container", this).hide();
+        // });
+
+        // $list.on('click', '.delete-material-btn', function(e) {
+        //     var warning = '您真的要删除该文件吗？';
+
+        //     if ($(this).data('link-count') > 0) {
+        //         warning = "该文件目前正被 " + $(this).data('link-count') + " 个地方使用。 删除文件将导致这些地方不可用。\n\n" + warning;
+        //     }
+
+        //     if (!confirm(warning)) {
+        //         return;
+        //     }
+
+        //     var $btn = $(e.currentTarget);
+
+        //     $.post($(this).data('url'), function(response) {
+        //         $btn.parents('.item-material').remove();
+        //         Notify.success('文件已删除！');
+        //         window.location.reload();
+        //     }, 'json');
+        // });
+
+        // Batch delete (there is a special logic for this page, so we're not using the common batch-delete.js)
+        // $panel.on('click', '[data-role=batch-delete]', function() {
+        //     var $btn = $(this);
+        //     var name = $btn.data('name');
+
+        //     var ids = [];
+        //     var warning = "";
+
+        //     $panel.find('[data-role=batch-item]:checked').each(function() {
+        //         ids.push(this.value);
+
+        //         if ($(this).data('link-count') > 0) {
+        //             warning = "\t\"" + $(this).data('file-name') + "\"目前被 " + $(this).data('link-count') + " 个地方使用。 \n" + warning;
+        //         }
+        //     });
+
+        //     if (warning.length > 0) {
+        //         warning = "下列文件目前正被其它地方使用： \n\n" + warning + "\n删除文件将导致这些地方不可用。\n\n";
+        //     }
+
+        //     if (ids.length == 0) {
+        //         Notify.danger('未选中任何' + name);
+        //         return;
+        //     }
+
+        //     if (!confirm(warning + '确定要删除选中的' + ids.length + '条' + name + '吗？')) {
+        //         return;
+        //     }
+
+        //     $panel.find('.btn').addClass('disabled');
+
+        //     Notify.info('正在删除' + name + '，请稍等。', 60);
+
+        //     $.post($btn.data('url'), {
+        //         ids: ids
+        //     }, function(response) {
+        //         window.location.reload();
+        //     });
+
+        // });
+
+
+        // $list.on('click', '.convert-file-btn', function() {
+        //     $.post($(this).data('url'), function(response) {
+        //         if (response.status == 'error') {
+        //             alert(response.message);
+        //         } else {
+        //             window.location.reload();
+        //         }
+        //     }, 'json').fail(function() {
+        //         alert('文件转换提交失败，请重试！');
+        //     });
+        // });
+
+        // $('.tip').tooltip();
+
+        // $("#modal").modal({
+        //     backdrop: 'static',
+        //     keyboard: false,
+        //     show: false
+        // });
+
+        // $('.image-preview').colorbox({innerWidth:'70%',innerHeight:'70%',rel:'group1', photo:true, current:'{current} / {total}', title:function() {
+        //     return $(this).data('fileName');
+        // }});
+
+        // asyncLoadFiles();
     }
+
+    // function asyncLoadFiles()
+    // {
+    //   var fileIds = new Array();
+    //     $('#material-item-list [type=checkbox]').each(function(){
+    //         if(!isNaN($(this).val())){
+    //             fileIds.push($(this).val());
+    //         }
+    //     });
+
+    //     if(fileIds.length==0){
+    //       return ;
+    //     }
+
+    //     $.get("/course/manage/file/status?ids="+fileIds.join(","),'',function(data){
+    //         if(!data||data.length==0){
+    //             return ;
+    //         }
+            
+    //         for(var i=0;i<data.length;i++){
+    //           var file=data[i];
+    //           if(file.convertStatus=='waiting'||file.convertStatus=='doing'){
+    //             $(".convertInfo"+file.id).append("<br><span class='text-warning text-sm'>正在文件格式转换</span>");
+    //           }else if(file.convertStatus=='error'){
+    //             $(".convertInfo"+file.id).append("<br><span class='text-danger text-sm'>文件格式转换失败</span>");
+    //           }
+    //         }
+    //     });
+    // }
 
 });
