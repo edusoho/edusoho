434201cfca7d40be4ca019d17e81b52270eef145
wangdanyue <wangdanyue@howzhi.com>
2016-03-15 10:26:52 +0800

    live-course-list-fix2

--- src/Topxia/WebBundle/Controller/LiveCourseController.php
+++ src/Topxia/WebBundle/Controller/LiveCourseController.php
@@ -513,6 +513,7 @@ class LiveCourseController extends BaseController
                 $lessons = $this->getCourseService()->findRecentLiveLessons(array($val['courseId']), 0, 1);
 
                 $liveCourses[$val['courseId']]['liveStartTime'] = $lessons[0]['startTime'];
+                $liveCourses[$val['courseId']]['liveEndTime']   = $lessons[0]['endTime'];
                 $liveCourses[$val['courseId']]['lessonId']      = $lessons[0]['id'];
             }
         }
