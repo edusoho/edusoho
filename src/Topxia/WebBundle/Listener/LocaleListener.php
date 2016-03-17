3bb05d08a2a9f768b217bd87f55dd09c35e9e429
Simon <liuyangyang@howzhi.com>
2016-03-17 11:42:12 +0800

    用户切换国际化语言

---
+++ src/Topxia/WebBundle/Listener/LocaleListener.php
@@ -0,0 +1,40 @@
+<?php
+// src/AppBundle/EventListener/LocaleListener.php
+namespace Topxia\WebBundle\Listener;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleListener implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered after the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 15))
+        );
+    }
+}
