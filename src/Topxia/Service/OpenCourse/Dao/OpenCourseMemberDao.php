5df34fc835b063cb28724a3c49440f84f77f9852
wangdanyue <wangdanyue@howzhi.com>
2016-03-25 13:40:12 +0800

    open-course-table-build

---
+++ src/Topxia/Service/OpenCourse/Dao/OpenCourseMemberDao.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Topxia\Service\OpenCourse\Dao;
+
+interface OpenCourseMemberDao
+{
+    public function getMember($id);
+
+    public function getMemberByCourseIdAndUserId($courseId, $userId);
+
+    public function findMembersByCourseIds($courseIds);
+
+    public function searchMemberCount($conditions);
+
+    public function searchMembers($conditions, $orderBy, $start, $limit);
+
+    public function addMember($member);
+
+    public function updateMember($id, $member);
+
+    public function deleteMember($id);
+
+}
