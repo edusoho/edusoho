8e17b646e4776aa932cbfacc344281c2e26812fe
suobitong <suobitong@howzhi.com>
2016-03-27 12:21:46 +0800

    取消验证学号 增加对用户名多验证

--- src/Mooc/WebBundle/Resources/public/js/controller/auth/register.js
+++ src/Mooc/WebBundle/Resources/public/js/controller/auth/register.js
@@ -123,14 +123,14 @@ define(function(require, exports, module) {
         validator.addItem({
             element: '[name="nickname"]',
             required: true,
-            rule: 'chinese_alphanumeric byte_minlength{min:4} byte_maxlength{max:18} remote'
+            rule: 'chinese_alphanumeric not_all_digital byte_minlength{min:4} byte_maxlength{max:18} remote'
         });
 
-        validator.addItem({
-            element: '[name="staffNo"]',
-            required: true,
-            rule: 'staff_no minlength{min:5} maxlength{max:12} remote',
-        });
+        // validator.addItem({
+        //     element: '[name="staffNo"]',
+        //     required: true,
+        //     rule: 'staff_no minlength{min:5} maxlength{max:12} remote',
+        // });
 
         validator.addItem({
             element: '[name="password"]',
