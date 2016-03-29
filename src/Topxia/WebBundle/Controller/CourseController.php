b1113861fc9aac30c2f4b39ead584f5ec7515940
钟杰 <ketu@ketuzhong-mbp.lan>
2016-03-29 17:56:49 +0800

    修复课程详情页description乱码的问题

--- src/Topxia/WebBundle/Controller/CourseController.php
+++ src/Topxia/WebBundle/Controller/CourseController.php
@@ -364,7 +364,7 @@ class CourseController extends CourseBaseController
         $courseAbout = strip_tags($courseAbout, '');
 
         $courseAbout = preg_replace("/ /", "", $courseAbout);
-        $courseAbout = substr($courseAbout, 0, 100);
+        
         return $this->render("TopxiaWebBundle:Course:{$course['type']}-show.html.twig", array(
             'course'      => $course,
             'member'      => $member,
