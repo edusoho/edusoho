024de1863d6a599e7a824473ba288f3b8748d6e9
Simon <wq@zlzkj.com>
2016-04-01 18:13:51 +0800

    add api/classrooms/{classroomId}/members/{userId}

--- api/src/Topxia/Api/Resource/ClassroomMember.php
+++ api/src/Topxia/Api/Resource/ClassroomMember.php
@@ -9,13 +9,25 @@ class ClassroomMember extends BaseResource
 {
     public function get(Application $app, Request $request, $classroomId, $memberId)
     {
+        $classroomMember = $this->getClassroomService()->getClassroomMember($classroomId, $memberId);
+        if (empty($classroomMember)) {
+            return array();
+        }
+        return $this->filter($classroomMember);
     }
 
     public function filter($res)
     {
-        unset($res['userId']);
-        $res['user']        = $this->callSimplify('User', $res['user']);
+        // unset($res['userId']);
+        if (!empty($res['user'])) {
+            $res['user'] = $this->callSimplify('User', $res['user']);
+        }
         $res['createdTime'] = date('c', $res['createdTime']);
         return $res;
+    }
+
+    protected function getClassroomService()
+    {
+        return $this->getServiceKernel()->createService('Classroom:Classroom.ClassroomService');
     }
 }
