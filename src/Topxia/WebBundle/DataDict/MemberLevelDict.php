9e541c6c2e22425d8560a0890f8dab6fb9cdc89c
dingyangyang <dingyangyang@howzhi.com>
2016-04-07 10:12:10 +0800

    WebBundle下DataDict Extensions php翻译

--- src/Topxia/WebBundle/DataDict/MemberLevelDict.php
+++ src/Topxia/WebBundle/DataDict/MemberLevelDict.php
@@ -7,8 +7,8 @@ class MemberLevelDisct  implements DataDictInterface{
 	public function getDict()
 	{
 		return array(
-			'level_p'=>'普通会员',
-			'level_g'=>'金牌会员',
+			'level_p'=>$this->getServiceKernel()->trans('普通会员'),
+			'level_g'=>$this->getServiceKernel()->trans('金牌会员'),
 		);
 	}
 
