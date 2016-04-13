eb47f4b66eb4b21a9ee19a18e23a5b917f4c909f
nifeng <fengni@fengdeMacBook-Pro.local>
2016-04-13 22:04:07 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage/material-modal.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage/material-modal.js
@@ -3,8 +3,26 @@ define(function(require, exports, module) {
     var Notify = require('common/bootstrap-notify');
     var Validator = require('bootstrap.validator');
     var FileChooser = require('../widget/file/file-chooser3');
+    var BatchUploader = require('./batch-uploader');
 
     exports.run = function() {
+
+        var $el = $('#batch-uploader');
+        var esuploader = new BatchUploader({
+            element: $el,
+            initUrl: $el.data('initUrl'),
+            finishUrl: $el.data('finishUrl'),
+            uploadAuthUrl: $el.data('uploadAuthUrl')
+        });
+
+        esuploader.on('preupload', function(file){
+            var quality = {
+                videoQuality: $('.video-quality-switcher').find('input[name=video_quality]:checked').val(), 
+                audioQuality: $('.video-quality-switcher').find('input[name=video_audio_quality]:checked').val()
+            };
+            esuploader.set('process', quality);
+        });
+
 
         var $form = $("#course-material-form");
 
