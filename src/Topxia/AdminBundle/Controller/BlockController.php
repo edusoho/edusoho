6b8518fd0bdce3394ea7cf72fb8ad799d3dc561b
杨佳龙 <yangjlong315@gmail.com>
2016-04-14 10:37:47 +0800

    修复查看json配置bug

--- src/Topxia/AdminBundle/Controller/BlockController.php
+++ src/Topxia/AdminBundle/Controller/BlockController.php
@@ -192,8 +192,11 @@ class BlockController extends BaseController
         foreach ($block['meta']['items'] as $key => &$item) {
             $item['default'] = $block['data'][$key];
         }
-       
-        return new Response('<pre>' . StringToolkit::jsonPettry(json_encode($block['meta'], JSON_UNESCAPED_UNICODE)) . '</pre>');
+        if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
+            return new Response('<pre>' . StringToolkit::jsonPettry(json_encode($block['meta'], JSON_UNESCAPED_UNICODE)) . '</pre>');
+        }else{
+            return new Response('<pre>' . StringToolkit::jsonPettry(json_encode(iconv("GB2312","UTF-8//IGNORE",$block['meta']))) . '</pre>');
+        }
     }
 
     public function visualHistoryAction(Request $request, $blockId)
