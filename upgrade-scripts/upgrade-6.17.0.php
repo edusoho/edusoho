201abc7f477c41f8326d5f782ff9d9c9789f8146
nifeng <fengni@fengdeMacBook-Pro.local>
2016-04-15 10:03:11 +0800

    fix global file

---
+++ upgrade-scripts/upgrade-6.17.0.php
@@ -0,0 +1,198 @@
+<?php
+
+use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\Filesystem\Filesystem;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+
+        try {
+            $result = $this->batchUpdate($index);
+            $this->getConnection()->commit();
+
+            $this->updateCrontabSetting();
+            return $result;
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            throw $e;
+        }
+
+        try {
+            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../web/install");
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $developerSetting          = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
+        ServiceKernel::instance()->createService('Crontab.CrontabService')->setNextExcutedTime(time());
+    }
+
+    private function updateScheme()
+    {
+        $this->getConnection()->exec("ALTER TABLE `upload_files` ADD `globalId` VARCHAR(32) NOT NULL DEFAULT '0' COMMENT '云文件ID' AFTER `id`;");
+
+        $this->getConnection()->exec("ALTER TABLE `upload_files` ADD `status` ENUM('uploading','ok') NOT NULL DEFAULT 'ok' COMMENT '文件上传状态' AFTER `length`;");
+
+        $this->getConnection()->exec("ALTER TABLE `upload_files` CHANGE `size` `fileSize` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '文件大小';");
+
+        $this->getConnection()->exec("CREATE TABLE `upload_files_collection` (
+          `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `fileId` int(10) unsigned NOT NULL COMMENT '文件Id',
+          `userId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '收藏者',
+          `createdTime` int(10) unsigned NOT NULL,
+          `updatedTime` INT(10) unsigned NULL DEFAULT '0'  COMMENT '更新时间',
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='文件收藏表';");
+
+        $this->getConnection()->exec("CREATE TABLE `upload_files_tag` (
+            `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '系统ID',
+            `fileId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '文件ID',
+            `tagId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '标签ID',
+            PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文件与标签的关联表'");
+
+        $this->getConnection()->exec("CREATE TABLE `upload_files_share_history` (
+             `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '系统ID',
+             `sourceUserId` int(10) NOT NULL COMMENT '分享用户的ID',
+             `targetUserId` int(10) NOT NULL COMMENT '被分享的用户的ID',
+             `isActive` tinyint(4) NOT NULL DEFAULT '0' COMMENT '',
+             `createdTime` int(10) DEFAULT '0' COMMENT '创建时间',
+             PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+
+        $this->getConnection()->exec("CREATE TABLE `upload_file_inits` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+              `globalId` varchar(32) NOT NULL DEFAULT '0' COMMENT '云文件ID',
+              `status` ENUM('uploading','ok') NOT NULL DEFAULT 'ok' COMMENT '文件上传状态',
+              `hashId` varchar(128) NOT NULL DEFAULT '' COMMENT '文件的HashID',
+              `targetId` int(11) NOT NULL COMMENT '所存目标id',
+              `targetType` varchar(64) NOT NULL DEFAULT '' COMMENT '目标类型',
+              `filename` varchar(1024) NOT NULL DEFAULT '',
+              `ext` varchar(12) NOT NULL DEFAULT '' COMMENT '后缀',
+              `fileSize` bigint(20) NOT NULL DEFAULT '0',
+              `etag` VARCHAR( 256 ) NOT NULL DEFAULT  '',
+              `length` INT UNSIGNED NOT NULL DEFAULT  '0',
+              `convertHash` varchar(256) NOT NULL DEFAULT '' COMMENT '文件转换时的查询转换进度用的Hash值',
+              `convertStatus` enum('none','waiting','doing','success','error') NOT NULL DEFAULT 'none',
+              `metas` text,
+              `metas2` TEXT NULL DEFAULT NULL,
+              `type` ENUM(  'document',  'video',  'audio',  'image',  'ppt',  'flash', 'other' ) NOT NULL DEFAULT 'other',
+              `storage` enum('local','cloud') NOT NULL,
+              `convertParams` TEXT NULL COMMENT  '文件转换参数',
+              `updatedUserId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新用户名',
+              `updatedTime` int(10) unsigned DEFAULT '0',
+              `createdUserId` int(10) unsigned NOT NULL,
+              `createdTime` int(10) unsigned NOT NULL,
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `hashId` (`hashId`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;");
+    }
+
+    private function batchUpdate($index)
+    {
+        if ($index === 0) {
+            $this->updateScheme();
+            return array(
+                'index'    => 1,
+                'message'  => '正在升级数据，目前是 0% 进度',
+                'progress' => 4.4
+            );
+        }
+
+        $conditions = array(
+            'storage'       => 'cloud',
+            'convertStatus' => 'success',
+            'globalId'      => '0'
+        );
+        $total = $this->getUploadFileService()->searchFileCount($conditions);
+
+        $maxPage = ceil($total / 100) ? ceil($total / 100) : 1;
+
+        $this->getCloudFileService()->synData($conditions);
+
+        if ($index == $maxPage) {
+            return array(
+                'index'    => $index,
+                'message'  => '正在升级数据，目前是 '.intval(($index - 1) / $maxPage * 100).'% 进度',
+                'progress' => 4.4
+            );
+        } else {
+            return array();
+        }
+    }
+
+    private function updateCrontabSetting()
+    {
+        $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../app/data/crontab_config.yml");
+        $filesystem = new Filesystem();
+
+        if (!empty($dir)) {
+            $filesystem->remove($dir);
+        }
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql    = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql    = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    private function getSettingService()
+    {
+        return ServiceKernel::instance()->createService('System.SettingService');
+    }
+
+    private function getCloudFileService()
+    {
+        return ServiceKernel::instance()->createService('CloudFile.CloudFileService');
+    }
+
+    private function getUploadFileService()
+    {
+        return ServiceKernel::instance()->createService('File.UploadFileService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $kernel;
+    public function __construct($kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function getConnection()
+    {
+        return $this->kernel->getConnection();
+    }
+
+    protected function createService($name)
+    {
+        return $this->kernel->createService($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->kernel->createDao($name);
+    }
+
+    abstract public function update();
+}
