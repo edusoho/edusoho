fe8269901849310c2d29a6435fadf53052a09b7c
dongjianbin <dongjianbin@howzhi.com>
2016-04-18 14:47:08 +0800

    增加个人主页QQ，微信，微博是否公开

---
+++ app/DoctrineMigrations/Version20160418144352.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160418144352 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("ALTER TABLE `user_profile` ADD `isQQPublic` INT NOT NULL DEFAULT '1' AFTER `weixin`, ADD `isWeixinPublic` INT NOT NULL DEFAULT '1' AFTER `isQQPublic`, ADD `isWeiboPublic` INT NOT NULL DEFAULT '1' AFTER `isWeixinPublic`;");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
