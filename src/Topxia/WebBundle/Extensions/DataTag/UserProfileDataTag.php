d3d929256a2673f5464a4f90540b6c48f531d7e1
wangdanyue <wangdanyue@howzhi.com>
2016-04-19 11:21:18 +0800

    graceful-theme-open-course-module

---
+++ src/Topxia/WebBundle/Extensions/DataTag/UserProfileDataTag.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Topxia\WebBundle\Extensions\DataTag;
+
+use Topxia\WebBundle\Extensions\DataTag\DataTag;
+
+class UserProfileDataTag extends CourseBaseDataTag implements DataTag  
+{
+    
+    /**
+     * 获取一个用户的个人详细信息
+     *
+     * 可传入的参数：
+     *   userId 必需 用户ID
+     * 
+     * @param  array $arguments 参数
+     * @return array 用户个人信息
+     */
+    
+    public function getData(array $arguments)
+    {
+        $this->checkUserId($arguments);
+
+    	$userProfile = $this->getUserService()->getUserProfile($arguments['userId']);
+        
+        return $userProfile;
+    }
+
+}
