73bfb959e513bffd65f245f2c265d813a14b5c5f
dongjianbin <dongjianbin@howzhi.com>
2016-04-22 16:49:18 +0800

    fix

--- src/Topxia/Service/Dictionary/DictionaryService.php
+++ src/Topxia/Service/Dictionary/DictionaryService.php
@@ -6,4 +6,18 @@ interface DictionaryService
 {
 	public function findAllDictionaries();
 
+	public function getDictionaryItem($id);
+
+	public function deleteDictionaryItem($id);
+
+	public function updateDictionaryItem($id, $fields);
+
+	public function addDictionaryItem($fields);
+
+	public function findAllDictionaryItemsOrderByWeight();
+
+	public function findDictionaryItemByName($name);
+
+	public function findDictionaryItemByType($type);
+
 }
