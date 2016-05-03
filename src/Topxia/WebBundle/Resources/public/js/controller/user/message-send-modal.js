4b0e7724475d6ca23287fcb6884fdc21bc7ed067
dongjianbin <dongjianbin@howzhi.com>
2016-05-03 17:43:29 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/user/message-send-modal.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/user/message-send-modal.js
@@ -6,9 +6,6 @@ define(function(require, exports, module) {
     exports.run = function() {
         var $modal = $("#message-create-form").parents('.modal');
 
-        $.ajax({ 
-          async:false 
-        });
         var validator = new Validator({
             element: '#message-create-form',
             autoSubmit: false,
