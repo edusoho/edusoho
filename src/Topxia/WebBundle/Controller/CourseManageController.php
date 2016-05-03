f1009e31d116b17615891a8b7921ff0ea9ba1a24
dongjianbin <dongjianbin@howzhi.com>
2016-05-03 17:00:15 +0800

    fix

--- src/Topxia/WebBundle/Controller/CourseManageController.php
+++ src/Topxia/WebBundle/Controller/CourseManageController.php
@@ -340,15 +340,19 @@ class CourseManageController extends BaseController
             $column .= $orders['title'].",";
             $column .= "《".$course['title']."》".",";
             $column .= $course['price'].",";
-            $column .= $orders['discount'].",";
+            if ($orders['discount'] > $result['price']) {
+                $column .= $result['price'].",";
+            } else {
+                $column .= $orders['discount'].",";
+            }
             $column .= $orders['totalPrice'].",";
             $column .= $orders['couponDiscount'].",";
             $column .= $users[$orders['userId']]['nickname'].",";
             $column .= $profiles[$orders['userId']]['truename'] ? $profiles[$orders['userId']]['truename']."," : "-".",";
             $column .= $orders['amount'].",";
             $column .= $payment[$orders['payment']].",";
-            if (!empty($order['coupon'])) {
-                $column .= $order['coupon'].",";
+            if (!empty($orders['coupon'])) {
+                $column .= $orders['coupon'].",";
             } else {
                 $column .= "无".",";
             }
