bdadeee7d6920b5682089e8dc5e8579e5502f02f
wangdanyue <wangdanyue@howzhi.com>
2016-05-12 20:14:09 +0800

    upgrade-6.17.4

---
+++ upgrade-scripts/upgrade-6.17.4.php
@@ -0,0 +1,110 @@
+<?php
+
+use Topxia\Common\BlockToolkit;
+use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\Filesystem\Filesystem;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $result = $this->updateScheme($index);
+            $this->getConnection()->commit();
+
+            $this->updateCrontabSetting();
+
+            if (!empty($result)) {
+                return $result;
+            }
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            throw $e;
+        }
+
+        try {
+            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../web/install");
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $developerSetting          = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
+        ServiceKernel::instance()->createService('Crontab.CrontabService')->setNextExcutedTime(time());
+    }
+
+    private function updateScheme()
+    {
+        if ($this->isTableExist('upload_file_inits')) {
+            $sql    = "select max(id) as maxId from upload_files;";
+            $result = $this->getConnection()->fetchAssoc($sql);
+
+            $start = $result['maxId'] + 1000;
+            $this->getConnection()->exec("alter table upload_file_inits AUTO_INCREMENT = {$start};");
+        }
+    }
+
+
+    private function updateCrontabSetting()
+    {
+        $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../app/data/crontab_config.yml");
+        $filesystem = new Filesystem();
+
+        if (!empty($dir)) {
+            $filesystem->remove($dir);
+        }
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql    = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql    = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    private function getSettingService()
+    {
+        return ServiceKernel::instance()->createService('System.SettingService');
+    }
+
+}
+
+abstract class AbstractUpdater
+{
+    protected $kernel;
+    public function __construct($kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function getConnection()
+    {
+        return $this->kernel->getConnection();
+    }
+
+    protected function createService($name)
+    {
+        return $this->kernel->createService($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->kernel->createDao($name);
+    }
+
+    abstract public function update();
+}
