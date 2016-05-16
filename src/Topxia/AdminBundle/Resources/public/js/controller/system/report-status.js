8b5f9c81b0bafad90fc3d1ce55c29db4a532af47
Simon <liuyangyang@howzhi.com>
2016-05-16 15:26:50 +0800

    use post instand ajax

--- src/Topxia/AdminBundle/Resources/public/js/controller/system/report-status.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/system/report-status.js
@@ -16,19 +16,15 @@ define(function(require, exports, module) {
 
         });
 
-        $.ajax({
-                url: $('.js-email-send-check').data('url'),
-                timeout: 3000
-            }).done(function(resp) {
-                if (resp.status) {
-                    $('.js-email-send-check').closest('td').html('<span class="text-success">√ ' + resp.message + '</span>');
-                } else {
-                    $('.js-email-send-check').closest('td').html('<span class="text-danger">X ' + resp.message + '</span>');
-                }
-            })
-            .fail(function(resp) {
-                $('.js-email-send-check').closest('td').html('<span class="text-danger">X 邮件发送异常 </span>');
-            });
+        $.post($('.js-email-send-check').data('url'), function(resp) {
+            var $td = $('.js-email-send-check').closest('td');
+            if (resp.status) {
+                $td.html('<span class="text-success">√ ' + resp.message + '</span>');
+            } else {
+                $td.html('<span class="text-danger">X ' + resp.message + '</span>');
+            }
+
+        });
     };
 
 });
