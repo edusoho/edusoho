bcaac45117641cafda173a9975185a8e8ba97175
Simon <liuyangyang@howzhi.com>
2016-05-25 16:09:05 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/manage/list.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/manage/list.js
@@ -23,10 +23,8 @@ define(function(require, exports, module) {
         $("#endDate").datetimepicker('setStartDate', $("#startDate").val().substring(0, 16));
 
 
-        $(".btn-export-csv").on('click', function() {
+        $(".btn-export-csv").on('click', function(event) {
             var $btn = $(this);
-            var url = $btn.data('url');
-            var checkUrl = $btn.data('checkUrl');
             var exportCount = $btn.data('exportCount');
             var exportCountFormat = $btn.data('exportCountFormat');
             var exportAllowCount = $btn.data('exportAllowCount');
@@ -34,16 +32,15 @@ define(function(require, exports, module) {
 
             if (exportCount > exportAllowCount) {
                 Notify.warning("您的导出结果数量（" + exportCountFormat + "条）已超出最大值（" + exportAllowCountFormat + "条），请调整筛选范围后分批导出");
+                event.preventDefault();
                 return false;
             }
             if (exportCount == 0) {
                 Notify.warning("没有可导出的数据");
+                event.preventDefault();
                 return false;
             }
-            $btn.addClass('disabled');
             Notify.warning("正在导出数据，请稍候...");
-            window.location.href = url;
-            $btn.removeClass('disabled');
         })
 
     };
