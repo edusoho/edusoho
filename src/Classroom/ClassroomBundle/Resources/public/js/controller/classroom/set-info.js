c7406bc29a55b6028552ad65e782a0913a691e7b
Simon <liuyangyang@howzhi.com>
2016-05-31 21:04:52 +0800

    班级管理 优化ztree的api调

--- src/Classroom/ClassroomBundle/Resources/public/js/controller/classroom/set-info.js
+++ src/Classroom/ClassroomBundle/Resources/public/js/controller/classroom/set-info.js
@@ -1,17 +1,10 @@
 define(function(require, exports, module) {
 
     var Validator = require('bootstrap.validator');
-    var SelectTree = require('edusoho.selecttree');
     var Notify = require('common/bootstrap-notify');
     require('common/validator-rules').inject(Validator);
     require('es-ckeditor');
     exports.run = function() {
-        if ($("#orgSelectTree").val()) {
-            var selectTree = new SelectTree({
-                element: "#orgSelectTree",
-                name: 'orgCode'
-            });
-        }
 
         var editor_classroom_about = CKEDITOR.replace('about', {
             allowedContent: true,
