27e871064bdda3637c6269d1c368a670a5320ed9
wangdanyue <wangdanyue@howzhi.com>
2016-06-01 09:54:01 +0800

    course-lesson-mediaId-add-course-material

---
+++ app/DoctrineMigrations/Version20160531203720.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Topxia\Service\Common\ServiceKernel;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160531203720 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $sql     = "select * from course_lesson where type not in('live','testpaper') and mediaId != 0";
+        $lessons = $this->connection->fetchAll($sql, array());
+
+        if ($lessons) {
+            foreach ($lessons as $key => $lesson) {
+                $courseId = $lesson['courseId'];
+                $lessonId = $lesson['id'];
+                $fileId   = $lesson['mediaId'];
+                $copyId   = 0;
+                $time     = time();
+
+                $this->addSql("insert into course_material (courseId,lessonId,title,fileId,source,copyId,createdTime) values({$courseId},{$lessonId},'',{$fileId},'courselesson',{$copyId},UNIX_TIMESTAMP());");
+            }
+        }
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
