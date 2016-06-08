52d71fcbe319cdb5bb03d7377c353dd6b232d74f
=tianshengjie <tianshengjie@howzhi.com>
2016-06-08 14:23:02 +0800

    fix

--- src/Topxia/Service/System/Impl/SettingServiceImpl.php
+++ src/Topxia/Service/System/Impl/SettingServiceImpl.php
@@ -22,8 +22,10 @@ class SettingServiceImpl extends BaseService implements SettingService
         $this->clearCache();
     }
 
+// TODO 获得当前组织结构ID
     public function get($name, $default = null)
     {
+        $user = $this->getCurrentUser()->toArray();
 
         if (is_null($this->cached)) {
             $this->cached = $this->getCacheService()->get(self::CACHE_NAME);
@@ -31,14 +33,14 @@ class SettingServiceImpl extends BaseService implements SettingService
             if (is_null($this->cached)) {
                 $settings = $this->getSettingDao()->findAllSettings();
                 foreach ($settings as $setting) {
-                    $settingName = $this->getSettingName($setting['name']);
-                    $this->cached[$settingName] = $setting['value'];
+                    $this->cached[$setting['namespace'].'-'.$setting['name']] = $setting['value'];
                 }
                 $this->getCacheService()->set(self::CACHE_NAME, $this->cached);
             }
         }
-        $settingName = $this->getSettingName($name);
-        return isset($this->cached[$settingName]) ? unserialize($this->cached[$settingName]) : $default;
+        $defaultSet= isset($this->cached['default-'.$name]) ? unserialize($this->cached['default-'.$name]) : $default ;
+        $orgSet = isset($this->cached['org-20-'.$name]) ? unserialize($this->cached['org-20-'.$name]) : array();
+        return empty( $defaultSet ) ? $defaultSet : array_merge( $defaultSet , $orgSet );
     }
 
     public function delete($name)
@@ -64,11 +66,6 @@ class SettingServiceImpl extends BaseService implements SettingService
     {
         $this->getSettingDao()->deleteByNamespaceAndName($namespace,$name);
         $this->clearCache();
-    }
-
-    // TODO: org
-    protected function getSettingName($name){
-        return  $this->getNameSpace() . $name ;
     }
 
     protected function clearCache()
