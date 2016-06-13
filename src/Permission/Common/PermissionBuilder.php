275191dc2f9c2fe8a6a0a5efb8df38c2142a56a5
nifeng <fengni@fengdeMacBook-Pro.local>
2016-06-13 21:20:04 +0800

    fix

--- src/Permission/Common/PermissionBuilder.php
+++ src/Permission/Common/PermissionBuilder.php
@@ -114,6 +114,7 @@ class PermissionBuilder
         $configPaths[] = "{$rootDir}/src/MaterialLib/MaterialLibBundle/Resources/config/menus_{$position}.yml";
         $configPaths[] = "{$rootDir}/src/SensitiveWord/SensitiveWordBundle/Resources/config/menus_{$position}.yml";
         $configPaths[] = "{$rootDir}/src/Permission/PermissionBundle/Resources/config/menus_{$position}.yml";
+        $configPaths[] = "{$rootDir}/src/Org/OrgBundle/Resources/config/menus_{$position}.yml";
 
         $count         = $this->getAppService()->findAppCount();
         $apps          = $this->getAppService()->findApps(0, $count);
