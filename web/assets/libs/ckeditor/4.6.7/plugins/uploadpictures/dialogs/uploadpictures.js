a652717a38457492921f0949f1ffc3d1dae4b8df
Simon <wq@zlzkj.com>
2016-06-14 14:30:10 +0800

    fix

--- web/assets/libs/ckeditor/4.6.7/plugins/uploadpictures/dialogs/uploadpictures.js
+++ web/assets/libs/ckeditor/4.6.7/plugins/uploadpictures/dialogs/uploadpictures.js
@@ -14,6 +14,7 @@ CKEDITOR.dialog.add('uploadpictures', function(editor) {
             pick: '.' + editor.id + ' .ckeditor-uploadpictures-pick-btn',
             resize: false,
             fileNumLimit: 10,
+            threads: 1,
             fileSingleSizeLimit: 10*1024*1024,
             accept: {
                 title: 'Images',
