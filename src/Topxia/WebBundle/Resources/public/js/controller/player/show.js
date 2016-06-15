b7a312668fc84046b9709a0f8b7f9a886071b93f
Simon <wq@zlzkj.com>
2016-06-15 18:19:25 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/player/show.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/player/show.js
@@ -6,22 +6,22 @@ define(function(require, exports, module) {
 
         var videoHtml = $('#lesson-video-content');
 
-        var userId = videoHtml.data("userId");
-        var fileId = videoHtml.data("fileId");
+        // var userId = videoHtml.data("userId");
+        // var fileId = videoHtml.data("fileId");
 
-        var courseId = videoHtml.data("courseId");
-        var lessonId = videoHtml.data("lessonId");
+        // var courseId = videoHtml.data("courseId");
+        // var lessonId = videoHtml.data("lessonId");
         var timelimit = videoHtml.data('timelimit');
 
-        var playerType = videoHtml.data('player');
-        var fileType = videoHtml.data('fileType');
+        // var playerType = videoHtml.data('player');
+        // var fileType = videoHtml.data('fileType');
         var url = videoHtml.data('url');
         var watermark = videoHtml.data('watermark');
         var fingerprint = videoHtml.data('fingerprint');
-        var fingerprintSrc = videoHtml.data('fingerprintSrc');
-        var balloonVideoPlayer = videoHtml.data('balloonVideoPlayer');
+        // var fingerprintSrc = videoHtml.data('fingerprintSrc');
+        // var balloonVideoPlayer = videoHtml.data('balloonVideoPlayer');
         var markerUrl = videoHtml.data('markerurl');
-        var starttime = videoHtml.data('starttime');
+        // var starttime = videoHtml.data('starttime');
         var agentInWhiteList = videoHtml.data('agentInWhiteList');
 
         // var PlayerFactory = require('./player-factory');
@@ -56,46 +56,45 @@ define(function(require, exports, module) {
             type: 'child'
         });
 
-        // player.on('timeupdate', function(data) {
-        //     console.log('timeupdate', data);
-        // });
-
-        // player.on('anwsered', function(data) {
-        //     console.log('anwsered', data);
-        // });
-
-        player.on("timechange", function(e){
-
+        player.on('timeupdate', function(data) {
+            console.log('timeupdate', data);
         });
 
-        player.on("firstplay", function(){
+        player.on('anwsered', function(data) {
+            console.log('anwsered', data);
+        });
+
+        // player.on("timechange", function(e){
+
+        // });
+
+        // player.on("firstplay", function(){
  
-        });
+        // });
         
-        player.on("ready", function(){
-            messenger.sendToParent("ready", {pause: true});
-            player.play();
-        });
-        player.on("onMarkerReached",function(markerId,questionId){
-            // $('.vjs-break-overlay-text').html("");
-            messenger.sendToParent("onMarkerReached", {pause: true,markerId:markerId,questionId:questionId});
-        });
+        // player.on("ready", function(){
+        //     messenger.sendToParent("ready", {pause: true});
+        //     player.play();
+        // });
+        // player.on("onMarkerReached",function(markerId,questionId){
+        //     messenger.sendToParent("onMarkerReached", {pause: true,markerId:markerId,questionId:questionId});
+        // });
 
-        player.on("timechange", function(){
-            messenger.sendToParent("timechange", {pause: true});
-        });
+        // player.on("timechange", function(){
+        //     messenger.sendToParent("timechange", {pause: true});
+        // });
 
-        player.on("paused", function(){
-            messenger.sendToParent("paused", {pause: true});
-        });
+        // player.on("paused", function(){
+        //     messenger.sendToParent("paused", {pause: true});
+        // });
 
-        player.on("playing", function(){
-            messenger.sendToParent("playing", {pause: false});
-        });
+        // player.on("playing", function(){
+        //     messenger.sendToParent("playing", {pause: false});
+        // });
 
-        player.on("ended", function(){
-            messenger.sendToParent("ended", {stop: true});
-        });
+        // player.on("ended", function(){
+        //     messenger.sendToParent("ended", {stop: true});
+        // });
 
     };
 
