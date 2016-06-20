2f59b70d782672b40e97aef10dc19135aee4e2c3
Simon <liuyangyang@howzhi.com>
2016-06-20 16:14:56 +0800

    refererlog 增加 add get api

---
+++ src/Topxia/Service/RefererLog/Impl/RefererLogServiceImpl.php
@@ -0,0 +1,16 @@
+<?php
+namespace Topxia\Service\RefererLog\Impl;
+
+use Topxia\Service\Common\BaseService;
+use Topxia\Service\RefererLog\ReferLogService;
+
+class RefererLogServiceImpl extends BaseService implements ReferLogService
+{
+    public function addRefererLog($targertId, $targertType, $refererUrl)
+    {
+    }
+
+    public function findRefererLogById($id)
+    {
+    }
+}
