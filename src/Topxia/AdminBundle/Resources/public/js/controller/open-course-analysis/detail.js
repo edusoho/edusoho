a490f8cb0388944d9d7b4bac59127818e07002d5
Simon <liuyangyang@howzhi.com>
2016-06-24 09:35:47 +0800

    优化逻辑

--- src/Topxia/AdminBundle/Resources/public/js/controller/open-course-analysis/detail.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/open-course-analysis/detail.js
@@ -1,64 +1,7 @@
 define(function(require, exports, module) {
 	require('echarts-debug');
-
+	var EchartsConfig = require('./echartsConfig');
 	exports.run = function() {
-
-		function EchartsConfig() {
-		}
-		EchartsConfig.prototype = {
-			legendData: function() {
-				return eval("(" + $("#dataName").val() + ")");
-			},
-			seriesData: function() {
-				var datas = eval("(" + $("#data").val() + ")");
-				var optionDatas = [];
-				datas.forEach(function(element, index, array) {
-					optionDatas.push({
-						name: element.refererHost,
-						value: element.count
-					})
-				}, optionDatas);
-				return optionDatas;
-			},
-			option: function() {
-				return  {
-					tooltip: {
-						trigger: 'item',
-						formatter: "{a} <br/>{b}: {c} ({d}%)"
-					},
-					legend: {
-						orient: 'vertical',
-						x: 'left',
-						data: this.legendData()
-					},
-					series: [{
-						name: '访问来源',
-						type: 'pie',
-						radius: '55%',
-						center: ['50%', '60%'],
-						avoidLabelOverlap: false,
-						label: {
-							normal: {
-								show: false,
-							},
-							emphasis: {
-								show: false,
-								textStyle: {
-									fontSize: '30',
-									fontWeight: 'bold'
-								}
-							}
-						},
-						labelLine: {
-							normal: {
-								show: false
-							}
-						},
-						data: this.seriesData()
-					}]
-				};
-			}
-		}
 
 		var myChart = echarts.init(document.getElementById('echats-pie'));
 		var config = new EchartsConfig();
---
+++ src/Topxia/AdminBundle/Resources/public/js/controller/open-course-analysis/echartsConfig.js
@@ -0,0 +1,67 @@
+define(function(require, exports, module) {
+
+
+	function EchartsConfig(options) {
+		var defaultOption = {
+			name: '访问来源',
+			formatter: "{a} <br/>{b}: {c} ({d}%)"
+		}
+		this.config = $.extend(defaultOption, options);
+	}
+	EchartsConfig.prototype = {
+		legendData: function() {
+			return JSON.parse($("#dataName").val());
+		},
+		seriesData: function() {
+			var datas = JSON.parse($("#data").val());
+			var optionDatas = [];
+			datas.forEach(function(element, index, array) {
+				optionDatas.push({
+					name: element.refererHost,
+					value: element.count
+				})
+			}, optionDatas);
+			return optionDatas;
+		},
+		option: function() {
+			return {
+				tooltip: {
+					trigger: 'item',
+					formatter: this.config.formatter
+				},
+				legend: {
+					orient: 'vertical',
+					x: 'left',
+					data: this.legendData()
+				},
+				series: [{
+					name: this.config.name,
+					type: 'pie',
+					radius: '55%',
+					center: ['50%', '60%'],
+					avoidLabelOverlap: false,
+					label: {
+						normal: {
+							show: false,
+						},
+						emphasis: {
+							show: false,
+							textStyle: {
+								fontSize: '30',
+								fontWeight: 'bold'
+							}
+						}
+					},
+					labelLine: {
+						normal: {
+							show: false
+						}
+					},
+					data: this.seriesData()
+				}]
+			};
+		}
+	}
+
+	module.exports = EchartsConfig;
+});
