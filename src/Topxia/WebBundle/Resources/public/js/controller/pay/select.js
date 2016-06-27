e4e82426d1c4ade7382f5d2607837725c394a585
Simon <liuyangyang@howzhi.com>
2016-06-27 21:38:25 +0800

    fix bug

--- src/Topxia/WebBundle/Resources/public/js/controller/pay/select.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/pay/select.js
@@ -59,7 +59,7 @@ define(function(require, exports, module){
 
         $("input[name='payment']").val($('div .active').attr("id"));
 
-        $("#copy").click(function(event){
+        $("#copy").on('mouseenter',function(event){
             var textarea = document.createElement("textarea");
             textarea.style.position = 'fixed';
             textarea.style.top = 0;
