4649efadf0dba25569f57f04983fe94ecff8ada0
IlhamTahir <yantaq@bilig.biz>
2016-07-13 16:51:40 +0800

    注册页面js报错修复

--- src/Topxia/WebBundle/Resources/public/js/controller/auth/register.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/auth/register.js
@@ -28,7 +28,7 @@ define(function(require, exports, module) {
             }
             return  result;  
         },
-          Translator.trans('%display%格式错误',{display:display})
+          Translator.trans('%display%格式错误',{display:'{{display}}'})
     );
 
     Validator.addRule(
@@ -44,10 +44,10 @@ define(function(require, exports, module) {
                 }
                 return result;
             },
-                Translator.trans('%display%不允许以1开头的11位纯数字',{display:display})
+                Translator.trans('%display%不允许以1开头的11位纯数字',{display:'{{display}}'})
         );
 
-    exports.run = function() 
+    exports.run = function() {
         $(".date").datetimepicker({
             language: 'zh-CN',
             autoclose: true,
