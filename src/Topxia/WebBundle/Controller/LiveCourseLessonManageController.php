22368eb223e1bff966bca614f3f8fb78bb3e9bbc
nifeng <fengni@fengdeMacBook-Pro.local>
2016-07-18 21:52:55 +0800

    fix live course lesson manage

--- src/Topxia/WebBundle/Controller/LiveCourseLessonManageController.php
+++ src/Topxia/WebBundle/Controller/LiveCourseLessonManageController.php
@@ -14,7 +14,7 @@ class LiveCourseLessonManageController extends BaseController
         if ($request->getMethod() == 'POST') {
             $liveLesson = $request->request->all();
 
-            //直播课时名称只支持中文、英文大小写以及数字，暂不支持<、>、"、&、‘、’、”、“字符
+            // 直播课时名称只支持中文、英文大小写以及数字，暂不支持<、>、"、&、‘、’、”、“字符
             if (!(bool) preg_match('/^[^(<|>|\'|"|&|‘|’|”|“)]*$/', $liveLesson['title'])) {
                 throw $this->createAccessDeniedException("illegal title");
             }
