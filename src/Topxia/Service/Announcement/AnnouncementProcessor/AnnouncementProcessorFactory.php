535ee0f43d621470054df15ca13279b2c3a99221
IlhamTahir <yantaq@bilig.biz>
2016-07-18 18:04:18 +0800

    AnnouncementService模块修复

--- src/Topxia/Service/Announcement/AnnouncementProcessor/AnnouncementProcessorFactory.php
+++ src/Topxia/Service/Announcement/AnnouncementProcessor/AnnouncementProcessorFactory.php
@@ -9,7 +9,7 @@ class AnnouncementProcessorFactory
 	public static function create($target)
     {
     	if(empty($target) || !in_array($target,array('course','classroom'))) {
-    		throw new Exception($this->getKernel()->trans('公告类型不存在'));
+    		throw new Exception(self::getKernel()->trans('公告类型不存在'));
     	}
 
     	$class = __NAMESPACE__ . '\\' . ucfirst($target). 'AnnouncementProcessor';
