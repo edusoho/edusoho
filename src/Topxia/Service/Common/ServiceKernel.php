e85dcb8c4554173c2f62e7689e05ac19df0ce23c
IlhamTahir <yantaq@bilig.biz>
2016-07-19 16:40:20 +0800

    语言串生成

--- src/Topxia/Service/Common/ServiceKernel.php
+++ src/Topxia/Service/Common/ServiceKernel.php
@@ -61,7 +61,7 @@ class ServiceKernel
     public static function instance()
     {
         if (empty(self::$_instance)) {
-            throw new \RuntimeException(self::trans('ServiceKernel未实例化'));
+            throw new \RuntimeException('ServiceKernel未实例化');
         }
 
         self::$_instance->boot();
