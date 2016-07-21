a0e9e1d61a749a003cb5595c808009d65a3da92e
wangdanyue <wangdanyue@howzhi.com>
2016-07-21 21:10:19 +0800

    admin-open-course-publish-notice

--- src/Topxia/AdminBundle/Resources/public/js/controller/course/manage.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/course/manage.js
@@ -24,10 +24,14 @@ define(function(require, exports, module) {
 
 		$table.on('click', '.publish-course', function() {
 			if (!confirm('您确认要发布此课程吗？')) return false;
-			$.post($(this).data('url'), function(html) {
-				var $tr = $(html);
-				$table.find('#' + $tr.attr('id')).replaceWith(html);
-				Notify.success('课程发布成功！');
+			$.post($(this).data('url'), function(response) {
+				if (response['message']) {
+					Notify.danger(response['message']);
+				} else {
+					var $tr = $(response);
+					$table.find('#' + $tr.attr('id')).replaceWith(response);
+					Notify.success('课程发布成功！');
+				}
 			});
 		});
 
