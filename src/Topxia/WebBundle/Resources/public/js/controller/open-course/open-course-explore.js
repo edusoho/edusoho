581deac4b8007160d79ae511e1c794e32340fa5f
liumengte <liumengte@howzhi.com>
2016-07-23 14:45:38 +0800

    fix 分页

--- src/Topxia/WebBundle/Resources/public/js/controller/open-course/open-course-explore.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/open-course/open-course-explore.js
@@ -2,22 +2,7 @@ define(function(require, exports, module) {
     "use strict";
     var Lazyload = require('echo.js');
 
-    var getPageList = function (url){
-        $.post(url, function(response){
-            $(".section-more-btn").remove();
-            $('.open-course-list').append(response);
-            Lazyload.init();
-        })
-    };
-
     exports.run = function() {
-        var $list = $('.open-course-list');
-        $list.on('click','.section-more-btn a',function(){
-        	var url = $(this).attr('data-url');
-	      	getPageList(url);
-        });
-
-        getPageList($list.data('url'));
+        Lazyload.init();
     };
-
 });
