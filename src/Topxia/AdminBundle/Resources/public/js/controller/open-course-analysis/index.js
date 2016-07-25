463764d0e03ec69ee62c66b1258f540ae3cc945f
liumengte <liumengte@howzhi.com>
2016-07-25 15:58:45 +0800

    fix

--- src/Topxia/AdminBundle/Resources/public/js/controller/open-course-analysis/index.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/open-course-analysis/index.js
@@ -4,27 +4,42 @@ define(function (require, exports, module) {
     require('jquery.bootstrap-datetimepicker');
     var Validator = require('bootstrap.validator');
     require('common/validator-rules').inject(Validator);
-
+    var moment = require('moment');
     var date2Str = function (date) {
         if (!date instanceof Date) {
             return null;
         } else {
-            return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay();
+            var dateWrapper = moment(date);
+            return dateWrapper.format("YYYY-MM-DD");
         }
     };
 
+    var prevYear = function (date_str) {
+        var date = new Date(date_str);
+        date.setFullYear(date.getFullYear() - 1);
+        date.setDate(date.getDate() + 1);
+        return date2Str(date);
+    };
+    
+    var nextYear = function (date_str) {
+        var date = new Date(date_str.val());
+        date.setFullYear(date.getFullYear() + 1);
+        date.setDate(date.getDate() - 1);
+        return date2Str(date);
+    };
+    
     var addDateRule = function () {
         Validator.addRule(
             'date_range_with_year',
-            function(options, commit) {
+            function (options, commit) {
                 var date = Date.parse(options.element.val());
                 var otherDate = Date.parse(options.element.siblings('.datetimepicker-input').val());
                 var startTime = otherDate > date ? date : otherDate;
                 var endTime = otherDate > date ? otherDate : date;
                 var yearTime = 3600 * 24 * 365 * 1000;
-                if (endTime - startTime <= yearTime){
+                if (endTime - startTime <= yearTime) {
                     return true;
-                }else {
+                } else {
                     Notify.danger('日期跨度不可超过一年', 2);
                     return false;
                 }
@@ -43,10 +58,8 @@ define(function (require, exports, module) {
             format: 'yyyy-mm-dd',
             minView: 2
         }).on('changeDate', function () {
-            var date = new Date($startTime.val());
-            date.setFullYear(date.getFullYear() + 1);
             $endTime.datetimepicker('setStartDate', $startTime.val());
-            $endTime.datetimepicker('setEndDate', date2Str(date));
+            $endTime.datetimepicker('setEndDate', nextYear($startTime.val()));
         });
 
         $startTime.datetimepicker('setEndDate', $endTime.val());
@@ -58,16 +71,14 @@ define(function (require, exports, module) {
             minView: 2
         }).on('changeDate', function () {
             $startTime.datetimepicker('setEndDate', $endTime.val());
-            var date = new Date($endTime.val());
-            date.setFullYear(date.getFullYear() - 1);
-            $startTime.datetimepicker('setEndDate', date2Str(date));
+            $startTime.datetimepicker('setStartDate', prevYear($endTime.val()));
         });
 
         $endTime.datetimepicker('setStartDate', $startTime.val());
         var $form = $('#refererlog-search-form');
         var validator = new Validator({
             element: $form,
-            onFormValidated: function(error, results, $form) {
+            onFormValidated: function (error, results, $form) {
                 if (error) {
                     return false;
                 }
