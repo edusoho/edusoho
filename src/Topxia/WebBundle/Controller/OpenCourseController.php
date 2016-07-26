386bfcb291830a44ecb4dacb6d5cb86c06f492a1
liumengte <liumengte@howzhi.com>
2016-07-26 11:18:42 +0800

    fix 去掉无用的公开课二维码路由

--- src/Topxia/WebBundle/Controller/OpenCourseController.php
+++ src/Topxia/WebBundle/Controller/OpenCourseController.php
@@ -232,27 +232,6 @@ class OpenCourseController extends BaseController
         return $this->createJsonResponse(array('result' => true, 'number' => $course['likeNum']));
     }
 
-    public function qrcodeAction(Request $request, $id)
-    {
-        $user  = $this->getUserService()->getCurrentUser();
-        $host  = $request->getSchemeAndHttpHost();
-        $token = $this->getTokenService()->makeToken('qrcode', array(
-            'userId'   => $user['id'],
-            'data'     => array(
-                'url'    => $this->generateUrl('open_course_show', array('courseId' => $id), true),
-                'appUrl' => ""
-            ),
-            'times'    => 0,
-            'duration' => 3600
-        ));
-        $url = $this->generateUrl('common_parse_qrcode', array('token' => $token['token']), true);
-
-        $response = array(
-            'img' => $this->generateUrl('common_qrcode', array('text' => $url), true)
-        );
-        return $this->createJsonResponse($response);
-    }
-
     protected function getWxPreviewQrCodeUrl($id)
     {
         $user  = $this->getUserService()->getCurrentUser();
