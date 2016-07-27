80e6654e8e93481af91012a653074c865ddb93c4
Simon.liu <liuyangyang@howzhi.com>
2016-07-27 18:48:09 +0800

    增加QQ,QQ空间的分享

--- web/assets/libs/common/weixin-share.js
+++ web/assets/libs/common/weixin-share.js
@@ -3,33 +3,21 @@ define(function(require, exports, module) {
 	exports.run = function() {
 		$jweixin = $(".jweixin-data");
 		wx.config($jweixin.data('config'));
-
-		/*{
-			debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-			appId: 'wxe2d699a880ac55d5', // 必填，公众号的唯一标识
-			timestamp: 1469604128, // 必填，生成签名的时间戳
-			nonceStr: 'edusohononcestr', // 必填，生成签名的随机串
-			signature: 'c580879dcbad50bb35aec23362021d6789ab8d7e', // 必填，签名，见附录1
-			jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-		}*/
 		wx.ready(function() {
-
-			// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+			//分享到朋友圈
 			wx.onMenuShareTimeline({
 				title: $jweixin.data('title'), // 分享标题
 				link: $jweixin.data('link'), // 分享链接
 				imgUrl: $jweixin.data('imgUrl'), // 分享图标
 				success: function() {
 					// 用户确认分享后执行的回调函数
-					//alert('onMenuShareTimeline:  success')
 				},
 				cancel: function() {
 					// 用户取消分享后执行的回调函数
-					//alert('onMenuShareTimeline:  cancel')
 				}
 			});
 
-
+			//分享给朋友
 			wx.onMenuShareAppMessage({
 				title: $jweixin.data('title'), // 分享标题
 				desc: $jweixin.data('desc'), // 分享描述
@@ -39,11 +27,37 @@ define(function(require, exports, module) {
 				dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
 				success: function() {
 					// 用户确认分享后执行的回调函数
-					//alert('onMenuShareAppMessage:  success')
 				},
 				cancel: function() {
 					// 用户取消分享后执行的回调函数
-					//alert('onMenuShareAppMessage:  cancel')
+				}
+			});
+
+			//分享到QQ
+			wx.onMenuShareQQ({
+				title: $jweixin.data('title'), // 分享标题
+				desc: $jweixin.data('desc'), // 分享描述
+				link: $jweixin.data('link'), // 分享链接
+				imgUrl: $jweixin.data('imgUrl'), // 分享图标
+				success: function() {
+					// 用户确认分享后执行的回调函数
+				},
+				cancel: function() {
+					// 用户取消分享后执行的回调函数
+				}
+			});
+
+			//分享到QQ空间
+			wx.onMenuShareQZone({
+				title: $jweixin.data('title'), // 分享标题
+				desc: $jweixin.data('desc'), // 分享描述
+				link: $jweixin.data('link'), // 分享链接
+				imgUrl: $jweixin.data('imgUrl'), // 分享图标
+				success: function() {
+					// 用户确认分享后执行的回调函数
+				},
+				cancel: function() {
+					// 用户取消分享后执行的回调函数
 				}
 			});
 		});
