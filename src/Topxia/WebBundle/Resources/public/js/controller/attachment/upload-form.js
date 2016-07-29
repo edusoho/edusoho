44a570fc0421c2fa48bc41cb1b77fc128915fe6c
Simon.liu <liuyangyang@howzhi.com>
2016-07-29 09:46:21 +0800

    增加删除附件时的错误提示

--- src/Topxia/WebBundle/Resources/public/js/controller/attachment/upload-form.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/attachment/upload-form.js
@@ -5,9 +5,15 @@ define(function(require, exports, module){
             var attachment_remove =confirm("确定要删除附件吗?")
             if(attachment_remove){
                 $.post($(this).data('url'), function (result) {
+
+                }).done(function(data){
                     if(result.msg == 'ok'){
                         Notify.success('附件已删除');
+                    }else{
+                        Notify.danger('附件删除失败,请稍后再试');
                     }
+                }).fail(function(ajaxFailed){
+                    Notify.danger('附件删除失败,请稍稍后再试');
                 })
             }
         })
