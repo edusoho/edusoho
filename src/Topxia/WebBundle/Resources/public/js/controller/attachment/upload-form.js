8ce73efb44543a6c0e0c9a7bd159f243e57d4750
Simon.liu <liuyangyang@howzhi.com>
2016-08-04 20:59:10 +0800

    删除附件后显示附件

--- src/Topxia/WebBundle/Resources/public/js/controller/attachment/upload-form.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/attachment/upload-form.js
@@ -1,20 +1,21 @@
 define(function(require, exports, module) {
     var Notify = require('common/bootstrap-notify');
-    exports.run = function () {
-        $('.js-attachment-delete').on('click', function () {
+    exports.run = function() {
+        $('.js-attachment-delete').on('click', function() {
             var $this = $(this);
-            var attachment_remove =confirm("确定要删除附件吗?")
-            if(attachment_remove){
-                $.post($this.data('url'), function (result) {
+            var attachment_remove = confirm("确定要删除附件吗?")
+            if (attachment_remove) {
+                $.post($this.data('url'), function(result) {
 
-                }).done(function(result){
-                    if(result.msg == 'ok'){
+                }).done(function(result) {
+                    if (result.msg == 'ok') {
                         Notify.success('附件已删除');
                         $this.parent().remove();
-                    }else{
+                        $('.js-upload-file').removeClass('hidden');
+                    } else {
                         Notify.danger('附件删除失败,请稍后再试');
                     }
-                }).fail(function(ajaxFailed){
+                }).fail(function(ajaxFailed) {
                     Notify.danger('附件删除失败,请稍稍后再试');
                 })
             }
