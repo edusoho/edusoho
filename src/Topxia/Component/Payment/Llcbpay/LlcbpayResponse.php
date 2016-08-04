3d030845489ca49d0030e1c242786f27a2436156
laisancun <laisancun@howzhi.com>
2016-08-04 14:28:57 +0800

    添加网银测试

--- src/Topxia/Component/Payment/Llcbpay/LlcbpayResponse.php
+++ src/Topxia/Component/Payment/Llcbpay/LlcbpayResponse.php
@@ -11,6 +11,8 @@ class LlcbpayResponse extends Response
     public function getPayData()
     {
         $params = $this->params;
+        var_dump($params);
+        exit();
         $error  = $this->hasError($paymentrams);
 
         if ($error) {
