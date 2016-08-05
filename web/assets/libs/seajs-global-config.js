b568f88e7a8d9f9a5ec204571edb8de9f3a3ec53
Simon <wq@zlzkj.com>
2016-08-05 11:13:42 +0800

    fix messenger

--- web/assets/libs/seajs-global-config.js
+++ web/assets/libs/seajs-global-config.js
@@ -40,7 +40,7 @@ seajs.config({
         'mask': 'arale/overlay/1.1.4/mask',
         'sticky': 'arale/sticky/1.3.1/sticky',
         'cookie': 'arale/cookie/1.0.2/cookie',
-        'messenger': 'arale/messenger/2.0.0/messenger',
+        'messenger': 'arale/messenger/2.0.1/messenger',
         "templatable": "arale/templatable/0.9.1/templatable",
         'placeholder': 'arale/placeholder/1.1.0/placeholder',
         'json': 'gallery/json/1.0.3/json',
