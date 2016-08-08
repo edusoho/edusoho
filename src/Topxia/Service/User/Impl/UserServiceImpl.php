e807598bde28440bfa2210ea9187ef448728ffc0
nifeng <fengni@fengdeMacBook-Pro.local>
2016-08-08 13:42:30 +0800

    fix

--- src/Topxia/Service/User/Impl/UserServiceImpl.php
+++ src/Topxia/Service/User/Impl/UserServiceImpl.php
@@ -1326,7 +1326,9 @@ class UserServiceImpl extends BaseService implements UserService
 
         $permissions = $this->loadPermissions($user);
 
-        if (in_array('admin', $permissions)) {
+        if (in_array('ROLE_SUPER_ADMIN', $user->getRoles())
+            || in_array('ROLE_ADMIN', $user->getRoles())
+            || in_array('admin', $permissions)) {
             return true;
         }
 
