e2ee716235a8ed28cfe86a1cb7b9a5a32091a3e1
wangdanyue <wangdanyue@howzhi.com>
2016-08-09 16:16:03 +0800

    classroom-auditor-vip-buy

--- src/Classroom/ClassroomBundle/Resources/public/js/controller/classroom/classroom-join.js
+++ src/Classroom/ClassroomBundle/Resources/public/js/controller/classroom/classroom-join.js
@@ -43,6 +43,29 @@ define(function(require, exports, module) {
             });
         }
 
+        if ($('.icon-vip').length > 0) {
+           $(".icon-vip").popover({
+                trigger: 'manual',
+                placement: 'auto top',
+                html: 'true',
+                container: 'body',
+                animation: false
+            }).on("mouseenter", function () {
+                var _this = $(this);
+                _this.popover("show");
+                $(".popover").on("mouseleave", function () {
+                    $(_this).popover('hide');
+                });
+            }).on("mouseleave", function () {
+                var _this = $(this);
+                setTimeout(function () {
+                    if (!$(".popover:hover").length) {
+                        _this.popover("hide")
+                    }
+                }, 100);
+            }); 
+        }
+
     };
 
 });
