fe6401b28ea0ad74e5abffe54e2bc80a95409a74
IlhamTahir <yantaq@bilig.biz>
2016-08-10 14:49:10 +0800

    优化生成函数

--- src/Topxia/WebBundle/Command/TransGenerateCommand.php
+++ src/Topxia/WebBundle/Command/TransGenerateCommand.php
@@ -287,7 +287,7 @@ class TransGenerateCommand extends BaseCommand
         foreach ($finder as $file) {
             $content = file_get_contents($file->getRealpath());
 
-            $matched = preg_match_all('/set^?.*\=\s*\'([^,\{\}]+)\'\s*\|\s*?trans/', $content, $matches);
+            $matched = preg_match_all('/set[^\?.]*\=\s*\'([^,\{\}]+)\'\s*\|\s*?trans/', $content, $matches);
 
             if ($matched) {
                 $output->write("{$file->getRealpath()}");
