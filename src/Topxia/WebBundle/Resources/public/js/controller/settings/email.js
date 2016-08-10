4beafc55cee285704fe6970e1ba0cf84287f5e60
IlhamTahir <yantaq@bilig.biz>
2016-08-10 17:34:27 +0800

    添加js rules

--- src/Topxia/WebBundle/Resources/public/js/controller/settings/email.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/settings/email.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
 
     var Validator = require('bootstrap.validator');
+    require('common/validator-rules').inject(Validator);
 
     exports.run = function() {
 
