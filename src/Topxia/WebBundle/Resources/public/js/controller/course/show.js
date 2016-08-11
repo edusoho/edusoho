ac0665b72121cb74ade6f119b9697a364093d5a1
wangdanyue <wangdanyue@howzhi.com>
2016-08-11 16:23:52 +0800

    course-refund-fix

--- src/Topxia/WebBundle/Resources/public/js/controller/course/show.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course/show.js
@@ -94,17 +94,6 @@ define(function(require, exports, module) {
             }); 
         }
 
-
-        $(".cancel-refund").on('click', function() {
-            if (!confirm('真的要取消退款吗？')) {
-                return false;
-            }
-
-            $.post($(this).data('url'), function() {
-                window.location.reload();
-            });
-        });
-
         $('#vip-join-course').on('click', function() {
             $.post($(this).data('url'), function(result) {
                 if (result == true) {
