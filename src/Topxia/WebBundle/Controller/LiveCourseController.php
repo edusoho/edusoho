62349aa9a9a5fefca62285b791ce341de314304f
wangdanyue <wangdanyue@howzhi.com>
2016-08-16 10:05:34 +0800

    error-push

--- src/Topxia/WebBundle/Controller/LiveCourseController.php
+++ src/Topxia/WebBundle/Controller/LiveCourseController.php
@@ -444,18 +444,6 @@ class LiveCourseController extends BaseController
         ));
     }
 
-    public function uploadModalAction(Request $request, $courseId, $lessonId)
-    {
-        $course = $this->getCourseService()->getCourse($courseId);
-        $lesson = $this->getCourseService()->getCourseLesson($courseId, $lessonId);
-
-        return $this->render('TopxiaWebBundle:LiveCourseReplayManage:upload-modal.html.twig', array(
-            'course'     => $course,
-            'lesson'     => $lesson,
-            'targetType' => 'courselesson'
-        ));
-    }
-
     public function entryReplayAction(Request $request, $courseId, $lessonId, $courseLessonReplayId)
     {
         $course = $this->getCourseService()->tryTakeCourse($courseId);
