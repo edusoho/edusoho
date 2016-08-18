cbe9ceec57cc5ffee426ed34c7db761e75802201
Simon Lau <liuyangyang@howzhi.com>
2016-08-18 20:05:25 +0800

    资源管理区分文件和附件

--- src/Topxia/AdminBundle/Controller/CloudAttachmentController.php
+++ src/Topxia/AdminBundle/Controller/CloudAttachmentController.php
@@ -1,8 +1,6 @@
 <?php
 namespace Topxia\AdminBundle\Controller;
 
-use Topxia\Common\Paginator;
-use Topxia\Common\ArrayToolkit;
 use Symfony\Component\HttpFoundation\Request;
 use Topxia\Service\CloudPlatform\CloudAPIFactory;
 
@@ -26,44 +24,6 @@ class CloudAttachmentController extends BaseController
         return $this->render('TopxiaAdminBundle:CloudAttachment:error.html.twig', array());
     }
 
-    public function renderAction(Request $request)
-    {
-        $conditions = $request->query->all();
-
-        if (!isset($conditions['processStatus'])) {
-            $conditions['processStatus'] = '';
-        }
-
-        $count     = $this->getCloudAttachmentService()->searchFileCount($conditions);
-        $paginator = new Paginator($this->get('request'), $count, 20);
-
-        $materials = $this->getCloudAttachmentService()->searchFiles(
-            $conditions,
-            $orderBy = array('createdTime', 'DESC'),
-            $paginator->getOffsetCount(),
-            $paginator->getPerPageCount()
-        );
-
-        $materials = array_map(function ($material) {
-            if (!array_key_exists('createdUserId', $material)) {
-                $material['createdUserId'] = $material['userId'];
-            }
-            return $material;
-        }, $materials);
-
-        $createdUserIds = ArrayToolkit::column($materials, 'createdUserId');
-        $createdUsers   = $this->getUserService()->findUsersByIds($createdUserIds);
-
-        return $this->render('TopxiaAdminBundle:CloudFile:tbody.html.twig', array(
-            'pageType'     => 'attachment',
-            'type'         => empty($conditions['type']) ? 'all' : $conditions['type'],
-            'targetType'   => empty($conditions['targetType']) ? 'all' : $conditions['targetType'],
-            'materials'    => $materials,
-            'createdUsers' => $createdUsers,
-            'paginator'    => $paginator
-        ));
-    }
-
     protected function getSettingService()
     {
         return $this->createService('System.SettingService');
@@ -77,10 +37,5 @@ class CloudAttachmentController extends BaseController
     protected function getUserService()
     {
         return $this->createService('User.UserService');
-    }
-
-    protected function getCloudAttachmentService()
-    {
-        return $this->createService('CloudAttachment.CloudAttachmentService');
     }
 }
