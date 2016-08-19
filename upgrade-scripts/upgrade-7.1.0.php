98044cf583508cd394082ce0f1f23f20f8f9e7d1
Simon Lau <liuyangyang@howzhi.com>
2016-08-19 11:24:08 +0800

    update

--- upgrade-scripts/upgrade-7.1.0.php
+++ upgrade-scripts/upgrade-7.1.0.php
@@ -12,13 +12,15 @@ class EduSohoUpgrade extends AbstractUpdater
         try {
             $this->updateScheme();
             $this->getConnection()->commit();
+
+            $this->updateCrontabSetting();
         } catch (\Exception $e) {
             $this->getConnection()->rollback();
             throw $e;
         }
 
         try {
-            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."../web/install");
+            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../web/install");
             $filesystem = new Filesystem();
 
             if (!empty($dir)) {
@@ -31,11 +33,13 @@ class EduSohoUpgrade extends AbstractUpdater
         $developerSetting['debug'] = 0;
 
         ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
-        ServiceKernel::instance()->createService('System.SettingService')->set("crontab_next_executed_time", time());
+        ServiceKernel::instance()->createService('Crontab.CrontabService')->setNextExcutedTime(time());
     }
 
     private function updateScheme()
     {
+        $connection = $this->getConnection();
+
         $api  = CloudAPIFactory::create('root');
         $info = $api->get('/me');
 
@@ -46,6 +50,45 @@ class EduSohoUpgrade extends AbstractUpdater
             $copyright['thirdCopyright'] = $info['thirdCopyright'];
             $copyright['licenseDomains'] = $info['licenseDomains'];
             $this->getSettingService()->set('copyright', $copyright);
+        }
+
+        if (!$this->isTableExist('file_used')) {
+            $connection->exec("
+                CREATE TABLE IF NOT EXISTS  `file_used` (
+                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                  `type` varchar(32) NOT NULL,
+                  `fileId` int(11) NOT NULL COMMENT 'upload_files id',
+                  `targetType` varchar(32) NOT NULL,
+                  `targetId` int(11) NOT NULL,
+                  `createdTime` int(11) NOT NULL,
+                  PRIMARY KEY (`id`),
+                  KEY `file_used_type_targetType_targetId_index` (`type`,`targetType`,`targetId`),
+                  KEY `file_used_type_targetType_targetId_fileId_index` (`type`,`targetType`,`targetId`,`fileId`),
+                  KEY `file_used_fileId_index` (`fileId`)
+              ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+        }
+
+        if (!$this->isFieldExist('upload_files', 'useType')) {
+            $connection->exec("ALTER TABLE upload_files  ADD `useType` varchar(64) DEFAULT NULL COMMENT '文件使用的模块类型'  AFTER  `targetType`;");
+        }
+
+        if ($this->isFieldExist('cash_orders', 'payment')) {
+            $connection->exec("ALTER TABLE `cash_orders` CHANGE `payment` `payment` VARCHAR(32) NOT NULL DEFAULT 'none';");
+        }
+
+        if ($this->isFieldExist('cash_flow', 'payment')) {
+            $connection->exec("ALTER TABLE `cash_flow` CHANGE `payment` `payment` VARCHAR(32) NULL DEFAULT ''");
+        }
+    }
+
+    private function updateCrontabSetting()
+    {
+        $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../app/data/crontab_config.yml");
+        $filesystem = new Filesystem();
+
+        if (!empty($dir)) {
+            $filesystem->remove($dir);
         }
     }
 
@@ -61,29 +104,6 @@ class EduSohoUpgrade extends AbstractUpdater
         $sql    = "SHOW TABLES LIKE '{$table}'";
         $result = $this->getConnection()->fetchAssoc($sql);
         return empty($result) ? false : true;
-    }
-
-    protected function isIndexExist($table, $filedName, $indexName)
-    {
-        $sql    = "show index from `{$table}` where column_name = '{$filedName}' and Key_name = '{$indexName}';";
-        $result = $this->getConnection()->fetchAssoc($sql);
-        return empty($result) ? false : true;
-    }
-
-    protected function isCrontabJobExist($code)
-    {
-        $sql    = "select * from crontab_job where name='{$code}'";
-        $result = $this->getConnection()->fetchAssoc($sql);
-
-        return empty($result) ? false : true;
-    }
-
-    protected function isblockTemplateEmpty()
-    {
-        $sql    = "select * from block_template";
-        $result = $this->getConnection()->fetchAssoc($sql);
-
-        return empty($result) ? true : false;
     }
 
     private function getSettingService()
