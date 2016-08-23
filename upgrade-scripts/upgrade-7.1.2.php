edb9aee7f49c5d16a7f2267f9716c64fc24f99e3
retamia <1600686809@qq.com>
2016-08-23 16:18:19 +0800

    upgrade 7.1.2

---
+++ upgrade-scripts/upgrade-7.1.2.php
@@ -0,0 +1,149 @@
+<?php
+
+use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\Filesystem\Filesystem;
+use Topxia\Service\CloudPlatform\CloudAPIFactory;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function update()
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $this->updateScheme();
+            $this->getConnection()->commit();
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            throw $e;
+        }
+
+        try {
+            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir') . "../web/install");
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $developerSetting          = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
+        ServiceKernel::instance()->createService('System.SettingService')->set("crontab_next_executed_time", time());
+    }
+
+    private function updateScheme()
+    {
+        $this->registerOpenCourseSearch(); // 注册公开课云搜索
+    }
+
+    protected function registerOpenCourseSearch()
+    {
+        $search = $this->getSettingService()->get('cloud_search');
+
+        if(!isset($search['search_enabled']) || !(bool)$search['search_enabled'] ){
+            return;
+        }
+
+        $site = $this->getSettingService()->get('site');
+
+        if (empty($site)) {
+            throw new \RuntimeException('没有站点信息');
+        }
+
+        $host = $site['url'];
+        if (strpos($host, 'http://') !== 0) {
+            $host = 'http://' . $host;
+        }
+        $host = rtrim(rtrim($host), '/');
+
+        $openCourseUrl = array(
+            'url'      => $host . '/api/open_courses?cursor=0&start=0&limit=100',
+            'category' => 'openCourse'
+        );
+        $openLessonUrl = array(
+            'url'      => $host . '/api/open_course_lessons?cursor=0&start=0&limit=100',
+            'category' => 'openLesson'
+        );
+
+        $urls = array(
+            $openCourseUrl,
+            $openLessonUrl
+        );
+
+        $remote = CloudAPIFactory::create();
+        $ret    = $remote->post('/search/accounts/me', array(
+            'urls' => urlencode(json_encode($urls)),
+        ));
+
+        if (!(bool)$ret['success']) {
+            throw new \RuntimeException('注册公开课云搜索失败');
+        }
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql    = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql    = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isIndexExist($table, $filedName, $indexName)
+    {
+        $sql    = "show index from `{$table}` where column_name = '{$filedName}' and Key_name = '{$indexName}';";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isCrontabJobExist($code)
+    {
+        $sql    = "select * from crontab_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    /**
+     * @return \Topxia\Service\System\Impl\SettingServiceImpl
+     */
+    private function getSettingService()
+    {
+        return ServiceKernel::instance()->createService('System.SettingService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $kernel;
+
+    public function __construct($kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function getConnection()
+    {
+        return $this->kernel->getConnection();
+    }
+
+    protected function createService($name)
+    {
+        return $this->kernel->createService($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->kernel->createDao($name);
+    }
+
+    abstract public function update();
+}
