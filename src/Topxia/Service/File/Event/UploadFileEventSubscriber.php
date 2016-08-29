918a8c12d27ab5daaddad2c11bbc8775270348f9
Simon Lau <liuyangyang@howzhi.com>
2016-08-29 16:13:34 +0800

    帖子删除

--- src/Topxia/Service/File/Event/UploadFileEventSubscriber.php
+++ src/Topxia/Service/File/Event/UploadFileEventSubscriber.php
@@ -10,22 +10,108 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'course.delete'          => 'onCourseDelete',
+            'course.delete'             => 'onCourseDelete',
             //'course.lesson.create' => 'onCourseLessonCreate',
-            'course.lesson.delete'   => 'onCourseLessonDelete',
-            'course.material.create' => 'onMaterialCreate',
-            'course.material.update' => 'onMaterialUpdate',
-            'course.material.delete' => 'onMaterialDelete',
+            'course.lesson.delete'      => 'onCourseLessonDelete',
+            'course.material.create'    => 'onMaterialCreate',
+            'course.material.update'    => 'onMaterialUpdate',
+            'course.material.delete'    => 'onMaterialDelete',
 
             'open.course.lesson.delete' => 'onOpenCourseLessonDelete',
-            'open.course.delete'        => 'onOpenCourseDelete'
+            'open.course.delete'        => 'onOpenCourseDelete',
+
+            'article.delete'            => 'onArticleDelete',
+            'question.delete'           => 'onQuestionDelete',
+            'group.thread.post.delete'  => 'onGroupThreadPostDelete',
+            'group.thread.delete'       => 'onGroupThreadDelete',
+            'course.thread.delete'      => 'onCourseThreadDelete',
+            'course.thread.post.delete' => 'onCourseThreadPostDelete',
+            'thread.delete'             => 'onThreadDelete',
+            'thread.post.delete'        => 'onThreadPostDelete'
         );
+    }
+
+    public function onArticleDelete(ServiceEvent $event)
+    {
+        $article = $event->getSubject();
+        $this->deleteAttachment('article', $article['id']);
+    }
+
+    public function onQuestionDelete(ServiceEvent $event)
+    {
+        $question = $event->getSubject();
+        $this->deleteAttachment('question.stem,question.analysis', $question['id']);
+    }
+
+    public function onGroupThreadPostDelete(ServiceEvent $event)
+    {
+        $threadPost = $event->getSubject();
+        $this->deleteAttachment('group.thread.post', $threadPost['id']);
+    }
+
+    public function onGroupThreadDelete(ServiceEvent $event)
+    {
+        $thread = $event->getSubject();
+        $this->deleteAttachment('group.thread', $thread['id']);
+    }
+
+    /**
+     * [onCourseThreadPostDelete description]
+     * @param  ServiceEvent $event          [description]
+     * @return [type]       [description]
+     */
+    public function onCourseThreadDelete(ServiceEvent $event)
+    {
+        $thread = $event->getSubject();
+        $this->deleteAttachment('course.thread', $thread['id']);
+    }
+
+    public function onCourseThreadPostDelete(ServiceEvent $event)
+    {
+        $threadPost = $event->getSubject();
+        $this->deleteAttachment('course.thread.post', $threadPost['id']);
+    }
+
+    /**
+     * [onThreadPostDelete description]
+     * @param  ServiceEvent $event          [description]
+     * @return [type]       [description]
+     */
+    public function onThreadDelete(ServiceEvent $event)
+    {
+        $thread = $event->getSubject();
+        if (!empty($thread['targetType'])) {
+            $this->deleteAttachment($thread['targetType'].'.thread', $thread['id']);
+        }
+    }
+
+    public function onThreadPostDelete(ServiceEvent $event)
+    {
+        $threadPost = $event->getSubject();
+        if (!empty($threadPost['targetType'])) {
+            $this->deleteAttachment($threadPost['targetType'].'.thread.post', $threadPost['id']);
+        }
+    }
+
+    private function deleteAttachment($targetType, $targetId)
+    {
+        $conditions = array('targetId' => $targetId, 'type' => 'attachment');
+        if (strpos($targetType, ',') === false) {
+            $conditions['targetType'] = $targetType;
+        } else {
+            $conditions['targetTypes'] = explode(',', $targetType);
+        }
+
+        $attachments = $this->getUploadFileService()->searchUseFiles($conditions);
+        foreach ($attachments as $key => $attachment) {
+            $this->getUploadFileService()->deleteUseFile($attachment['id']);
+        }
     }
 
     public function onCourseDelete(ServiceEvent $event)
     {
         $course = $event->getSubject();
-        
+
         $lessons = $this->getCourseService()->getCourseLessons($course['id']);
 
         if (!empty($lessons)) {
@@ -52,8 +138,8 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
 
     public function onCourseLessonDelete(ServiceEvent $event)
     {
-        $context  = $event->getSubject();
-        $lesson   = $context['lesson'];
+        $context = $event->getSubject();
+        $lesson  = $context['lesson'];
 
         if (!empty($lesson['mediaId'])) {
             $this->getUploadFileService()->waveUploadFile($lesson['mediaId'], 'usedCount', -1);
@@ -65,8 +151,8 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
         $context  = $event->getSubject();
         $material = $context['material'];
 
-        if(!empty($material['fileId'])){
-            $this->getUploadFileService()->waveUploadFile($material['fileId'],'usedCount',1);
+        if (!empty($material['fileId'])) {
+            $this->getUploadFileService()->waveUploadFile($material['fileId'], 'usedCount', 1);
         }
     }
 
@@ -77,15 +163,14 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
         $material = $context['material'];
 
         if ($material['fileId'] != $argument['fileId'] && $argument['fileId']) {
-            $this->getUploadFileService()->waveUploadFile($material['fileId'],'usedCount', 1);
-            $this->getUploadFileService()->waveUploadFile($argument['fileId'],'usedCount',-1);
+            $this->getUploadFileService()->waveUploadFile($material['fileId'], 'usedCount', 1);
+            $this->getUploadFileService()->waveUploadFile($argument['fileId'], 'usedCount', -1);
         }
-
     }
 
     public function onMaterialDelete(ServiceEvent $event)
     {
-        $material  = $event->getSubject();
+        $material = $event->getSubject();
 
         $file = $this->getUploadFileService()->getFile($material['fileId']);
 
@@ -93,12 +178,12 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
             return false;
         }
 
-        $this->getUploadFileService()->waveUploadFile($file['id'],'usedCount',-1);
+        $this->getUploadFileService()->waveUploadFile($file['id'], 'usedCount', -1);
 
         if (!$this->getUploadFileService()->canManageFile($file['id'])) {
             return false;
         }
-        
+
         if ($file['targetId'] == $material['courseId']) {
             $this->getUploadFileService()->update($material['fileId'], array('targetId' => 0));
         }
@@ -106,8 +191,8 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
 
     public function onOpenCourseLessonDelete(ServiceEvent $event)
     {
-        $context  = $event->getSubject();
-        $lesson   = $context['lesson'];
+        $context = $event->getSubject();
+        $lesson  = $context['lesson'];
 
         if (!empty($lesson['mediaId'])) {
             $this->getUploadFileService()->waveUploadFile($lesson['mediaId'], 'usedCount', -1);
@@ -117,7 +202,7 @@ class UploadFileEventSubscriber implements EventSubscriberInterface
     public function onOpenCourseDelete(ServiceEvent $event)
     {
         $course = $event->getSubject();
-        
+
         $lessons = $this->getOpenCourseService()->findLessonsByCourseId($course['id']);
 
         if (!empty($lessons)) {
