941f641d491584c2de294948e06e5d59800e8aef
liumengte <liumengte@howzhi.com>
2016-09-01 19:44:32 +0800

    fix smscontroller

--- src/Topxia/WebBundle/Controller/SmsController.php
+++ src/Topxia/WebBundle/Controller/SmsController.php
@@ -14,7 +14,7 @@ class SmsController extends BaseController
         $item                  = array();
         $verifiedMobileUserNum = 0;
         $url                   = '';
-        $smsType               = 'sms_'.$targetType.'_publish';
+        $smsType               = 'sms_' . $targetType . '_publish';
 
         if ($targetType == 'classroom') {
             $item                  = $this->getClassroomService()->getClassroom($id);
@@ -48,7 +48,7 @@ class SmsController extends BaseController
 
     public function sendAction(Request $request, $targetType, $id)
     {
-        $smsType     = 'sms_'.$targetType.'_publish';
+        $smsType     = 'sms_' . $targetType . '_publish';
         $index       = $request->query->get('index');
         $onceSendNum = 100;
         $url         = $request->query->get('url');
@@ -60,14 +60,16 @@ class SmsController extends BaseController
             $classroomSetting              = $this->getSettingService()->get("classroom");
             $classroomName                 = isset($classroomSetting['name']) ? $classroomSetting['name'] : '班级';
             $classroom['title']            = StringToolkit::cutter($classroom['title'], 20, 15, 4);
-            $parameters['classroom_title'] = $classroomName.'：《'.$classroom['title'].'》';
-            $description                   = $parameters['classroom_title'].'发布';
-            $students                      = $this->getUserService()->searchUsers(array(), array('createdTime', 'DESC'), $index, $onceSendNum);
+            $parameters['classroom_title'] = $classroomName . '：《' . $classroom['title'] . '》';
+            $description                   = $parameters['classroom_title'] . '发布';
+            $profiles                      = $this->getUserService()->searchUserProfiles(array('mobile' => '1'), array('id', 'DESC'), 0, PHP_INT_MAX);
+            $userIds                       = ArrayToolkit::column($profiles, 'id');
+            $students                      = $this->getUserService()->searchUsers(array('locked' => 0, 'ids' => $userIds), array('createdTime', 'DESC'), $index, $onceSendNum);
         } elseif ($targetType == 'course') {
             $course                     = $this->getCourseService()->getCourse($id);
             $course['title']            = StringToolkit::cutter($course['title'], 20, 15, 4);
-            $parameters['course_title'] = '课程：《'.$course['title'].'》';
-            $description                = $parameters['course_title'].'发布';
+            $parameters['course_title'] = '课程：《' . $course['title'] . '》';
+            $description                = $parameters['course_title'] . '发布';
 
             if ($course['parentId']) {
                 $classroom = $this->getClassroomService()->findClassroomByCourseId($course['id']);
@@ -77,7 +79,9 @@ class SmsController extends BaseController
                     $students = $this->getClassroomService()->searchMembers(array('classroomId' => $classroom['classroomId']), array('createdTime', 'Desc'), $index, $onceSendNum);
                 }
             } else {
-                $students = $this->getUserService()->searchUsers(array(), array('createdTime', 'DESC'), $index, $onceSendNum);
+                $profiles = $this->getUserService()->searchUserProfiles(array('mobile' => '1'), array('id', 'DESC'), 0, PHP_INT_MAX);
+                $userIds  = ArrayToolkit::column($profiles, 'id');
+                $students = $this->getUserService()->searchUsers(array('locked' => 0, 'ids' => $userIds), array('createdTime', 'DESC'), $index, $onceSendNum);
             }
         }
 
@@ -85,7 +89,7 @@ class SmsController extends BaseController
             throw new \RuntimeException("请先开启相关设置!");
         }
 
-        $parameters['url'] = $url.' ';
+        $parameters['url'] = $url . ' ';
 
         if (!empty($students)) {
             if ($targetType == 'course' && $course['parentId']) {
@@ -110,9 +114,9 @@ class SmsController extends BaseController
         $url .= $request->query->get('url');
 
         $shortUrl = SmsToolkit::getShortLink($url);
-        $url      = empty($shortUrl) ? 'http://'.$url : $shortUrl;
+        $url      = empty($shortUrl) ? 'http://' . $url : $shortUrl;
 
-        return $this->createJsonResponse(array('url' => $url.' '));
+        return $this->createJsonResponse(array('url' => $url . ' '));
     }
 
     protected function getSettingService()
