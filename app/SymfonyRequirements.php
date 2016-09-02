150e97b79bcdced84cd5b50448fae760f6202705
ruanwenqin <ruanwenqin@howzhi.com>
2016-09-02 16:02:32 +0800

    remove

--- app/SymfonyRequirements.php
+++ app/SymfonyRequirements.php
@@ -403,7 +403,7 @@ class SymfonyRequirements extends RequirementCollection
         );
 
         $this->addRequirement(
-            is_dir(__DIR__.'/../vendor/composer'),
+            is_dir(__DIR__.'/../vendor2/composer'),
             'Vendor libraries must be installed',
             'Vendor libraries are missing. Install composer following instructions from <a href="http://getcomposer.org/">http://getcomposer.org/</a>. '.
                 'Then run "<strong>php composer.phar install</strong>" to install them.'
