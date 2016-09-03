2df1b9557d7c007cdffa776f9cf51d1a2b26dc48
zhaoyao <zhaoyao@howzhi.com>
2016-09-03 14:09:22 +0800

    fix

--- src/Permission/Service/Role/Impl/RoleServiceImpl.php
+++ src/Permission/Service/Role/Impl/RoleServiceImpl.php
@@ -80,29 +80,42 @@ class RoleServiceImpl extends BaseService implements RoleService
 
     public function initRoles()
     {
-        $role = PermissionBuilder::instance()->getOriginPermissionTree();
-        $role = ArrayToolkit::column($role, 'code');
+        $getSuperAdminRole = PermissionBuilder::instance()->getOriginPermissionTree();
+        $getSuperAdminRole = ArrayToolkit::column($getSuperAdminRole, 'code');
+        
+        $getAdminRole = PermissionBuilder::instance()->findpersmissionsbycode('web');
+        $getAdminRole = ArrayToolkit::column($getAdminRole, 'code');
+        
+        $getTeacherRole = PermissionBuilder::instance()->findpersmissionsbycode('web');
+        $getTeacherRole = ArrayToolkit::column($getTeacherRole, 'code');
 
         $superAdminRole = $this->getRoleDao()->getRoleByCode('ROLE_SUPER_ADMIN');
         $adminRole      = $this->getRoleDao()->getRoleByCode('ROLE_ADMIN');
         $teacherRole    = $this->getRoleDao()->getRoleByCode('ROLE_TEACHER');
         $userRole       = $this->getRoleDao()->getRoleByCode('ROLE_USER');
         if (empty($superAdminRole)) {
-            $superAdminRole = $this->makeRole('ROLE_SUPER_ADMIN', $role);
-            $superAdminRole = $this->createRole($superAdminRole);
+            $superAdminRole = $this->makeRole('ROLE_SUPER_ADMIN', $getSuperAdminRole);
+            $superAdminRole = $this->initCreateRole($superAdminRole);
+        } else {
+            $superAdminRole = $this->getRoleDao()->updateRole($superAdminRole['id'], $getSuperAdminRole);
         }
         if (empty($adminRole)) {
-            $superAdminRole = $this->makeRole('ROLE_ADMIN', $role);
-            $superAdminRole = $this->createRole($superAdminRole);
+            $adminRole = $this->makeRole('ROLE_ADMIN', $getAdminRole);
+            $adminRole = $this->initCreateRole($adminRole);
+        } else {
+            $adminRole = $this->getRoleDao()->updateRole($adminRole['id'], $getAdminRole);
         }
         if (empty($teacherRole)) {
-            $superAdminRole = $this->makeRole('ROLE_TEACHER', $role);
-            $superAdminRole = $this->createRole($superAdminRole);
+            $teacherRole = $this->makeRole('ROLE_TEACHER', $getTeacherRole);
+            $teacherRole = $this->initCreateRole($teacherRole);
+        } else {
+            $teacherRole = $this->getRoleDao()->updateRole($teacherRole['id'], $getTeacherRole);
         }
         if (empty($userRole)) {
-            $superAdminRole = $this->makeRole('ROLE_USER', $role);
-            $superAdminRole = $this->createRole($superAdminRole);
+            $userRole = $this->makeRole('ROLE_USER', array());
+            $userRole = $this->initCreateRole($userRole);
         }
+        return array($superAdminRole, $adminRole, $teacherRole, $userRole);
     }
 
     protected function makeRole($code, $role)
@@ -116,8 +129,6 @@ class RoleServiceImpl extends BaseService implements RoleService
             return $condition;
         }
         if ($code == 'ROLE_ADMIN') {
-            $removeRole = array("admin","admin_user","admin_user_show","admin_user_manage","admin_user_avatar","admin_user_change_password","admin_app","admin_my_cloud","admin_my_cloud_overview","admin_cloud_bill","admin_cloud_video_setting","admin_setting_cloud_video","admin_edu_cloud_sms","admin_edu_cloud_sms_setting","admin_edu_cloud_search_setting","admin_edu_cloud_search","admin_setting_cloud_attachment","admin_cloud_attachment","admin_setting_cloud","admin_setting_my_cloud","admin_system","admin_org_manage","admin_org","admin_roles","admin_role_manage","admin_role_create","admin_role_edit","admin_role_delete","admin_setting","admin_setting_message","admin_setting_theme","admin_setting_mailer","admin_top_navigation","admin_foot_navigation","admin_friendlyLink_navigation","admin_setting_consult_setting","admin_setting_es_bar","admin_setting_share","admin_setting_user","admin_user_auth","admin_setting_login_bind","admin_setting_user_center","admin_setting_user_fields","admin_setting_avatar","admin_setting_course_setting","admin_classroom_setting","admin_setting_course","admin_setting_live_course","admin_setting_questions_setting","admin_setting_course_avatar","admin_setting_operation","admin_article_setting","admin_group_set","admin_invite_set","admin_setting_finance","admin_payment","admin_coin_settings","admin_setting_refund","admin_setting_mobile","admin_setting_mobile_settings","admin_optimize","admin_optimize_settings","admin_jobs","admin_jobs_manage","admin_setting_ip_blacklist","admin_setting_ip_blacklist_manage","admin_setting_post_num_rules","admin_setting_post_num_rules_settings","admin_report_status","admin_report_status_list","admin_logs","admin_logs_query","admin_logs_prod");
-            $role = array_diff($role, $removeRole);
             $condition = array(
                 'name' => '管理员',
                 'code' => 'ROLE_ADMIN',
@@ -126,7 +137,6 @@ class RoleServiceImpl extends BaseService implements RoleService
             return $condition;
         }
         if ($code == 'ROLE_TEACHER') {
-            $role = array("web","course_manage","course_manage_info","course_manage_base","course_manage_detail","course_manage_picture","course_manage_lesson","course_manage_lesson_create","course_manage_lesson_edit","course_manage_lesson_delete","course_manage_lesson_preview","course_manage_lesson_create_testpaper","course_manage_chapter_create","course_manage_chapter_edit","course_manage_chapter_delete","course_manage_lesson_create_homework","course_manage_lesson_delete_homework","course_manage_lesson_create_exercise","course_manage_lesson_delete_exercise","course_manage_lesson_create_material","course_manage_lesson_publish","course_manage_lesson_unpublish","live_course_manage_replay","course_manage_files","course_manage_setting","course_manage_price","course_manage_teachers","course_manage_students","course_manage_student_create","course_manage_questions","course_manage_question","course_manage_testpaper","course_manange_operate","course_manage_data","course_manage_order","classroom_manage","classroom_manage_settings","classroom_manage_set_info","classroom_manage_set_price","classroom_manage_set_picture","classroom_manage_service","classroom_manage_headteacher","classroom_manage_teachers","classroom_manage_assistants","classroom_manage_content","classroom_manage_courses","classroom_manage_students","classroom_manage_testpaper");
             $condition = array(
                 'name' => '教师',
                 'code' => 'ROLE_TEACHER',
@@ -138,10 +148,18 @@ class RoleServiceImpl extends BaseService implements RoleService
             $condition = array(
                 'name' => '学生',
                 'code' => 'ROLE_USER',
-                'data' => array()
+                'data' => $role
             );
             return $condition;
         }
+    }
+
+    protected function initCreateRole($role)
+    {
+        $role['createdTime']   = time();
+        $role['createdUserId'] = 1;
+        $this->getLogService()->info('role', 'init_create_role', '初始化四个角色"'.$role['name'].'"', $role);
+        return $this->getRoleDao()->createRole($role);        
     }
 
     protected function prepareSearchConditions($conditions)
@@ -204,5 +222,10 @@ class RoleServiceImpl extends BaseService implements RoleService
     protected function getLogService()
     {
         return $this->createService('System.LogService');
+    }
+
+    protected function getUserService()
+    {
+        return $this->createService('User.UserService');
     }
 }
