70febe4491d5a0952592a14a565c540f871f5495
wangdanyue <wangdanyue@howzhi.com>
2016-09-05 09:58:00 +0800

    edusoho-structure

--- src/Topxia/WebBundle/Command/DumpInitDataCommand.php
+++ src/Topxia/WebBundle/Command/DumpInitDataCommand.php
@@ -43,6 +43,10 @@ class DumpInitDataCommand extends BaseCommand
         $output->writeln("<info>{$command}</info>");
         exec($command);
 
+        $command = "ssh -l root {$domain} \"mysqldump -u{$user} -p{$password} -d {$database} --compact --add-drop-table | sed 's/ AUTO_INCREMENT=[0-9]*//g' > edusoho_structure.{$time}.sql\"";
+        $output->writeln("<info>{$command}</info>");
+        exec($command);
+
         $rootPath   = __DIR__.'/../../../..';
         $filesystem = new Filesystem();
 
@@ -54,7 +58,15 @@ class DumpInitDataCommand extends BaseCommand
         $output->writeln("<info>{$command}</info>");
         exec($command);
 
+        $command = "scp root@{$domain}:~/edusoho_structure.{$time}.sql {$rootPath}/installFiles/edusoho_structure.sql";
+        $output->writeln("<info>{$command}</info>");
+        exec($command);
+
         $command = "cp {$rootPath}/installFiles/edusoho_init.sql {$rootPath}/web/install/edusoho_init.sql";
+        $output->writeln("<info>{$command}</info>");
+        exec($command);
+
+        $command = "cp {$rootPath}/installFiles/edusoho_structure.sql {$rootPath}/web/install/edusoho.sql";
         $output->writeln("<info>{$command}</info>");
         exec($command);
 
