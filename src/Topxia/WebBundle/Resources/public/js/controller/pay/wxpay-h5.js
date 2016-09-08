d717746f98d5f8f6035de3c5746b640905d504a0
Simon Lau <liuyangyang@howzhi.com>
2016-09-08 16:11:04 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/pay/wxpay-h5.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/pay/wxpay-h5.js
@@ -1,18 +1,5 @@
 define(function (require, exports, module) {
-
     exports.run = function () {
-        /*   var $img = $('.img-js');
-         setInterval(wxpay_roll, 2000);
-
-         function wxpay_roll() {
-         $.get($img.data('url'), function (response) {
-         if (response) {
-         window.location.href = $img.data('goto');
-         }
-         ;
-         });
-         }*/
-
         $('.section-wxpay').on('click', '.js-wxpay', function () {
             if (typeof WeixinJSBridge == "undefined") {
                 if (document.addEventListener) {
@@ -26,19 +13,21 @@ define(function (require, exports, module) {
             }
         });
 
-        var jsApiParameters = $("#jsApiParameters").data('value');
         //调用微信JS api 支付
         function jsApiCall() {
             WeixinJSBridge.invoke(
                 'getBrandWCPayRequest',
-                jsApiParameters,
+                $("#jsApiParameters").data('value'),
                 function (res) {
-                    var msg = res.err_msg.split(':');
-                    if (msg.pop() == 'ok') {
-                        window.location.href = jsApiParameters.data('goto');
+                    alert($("#jsApiParameters").data('goto'));
+                    alert(res.err_msg);
+                    if (res.err_msg == 'get_brand_wcpay_request:ok') {
+                        window.location.href = $("#jsApiParameters").data('goto');
+                    } else {
+                        alert(res.err_msg);
+                        window.location.href = $("#jsApiParameters").data('goto');
                     }
-                    WeixinJSBridge.log(res.err_msg);
-                    alert(res.err_code + res.err_desc + res.err_msg);
+
                 }
             );
         }
