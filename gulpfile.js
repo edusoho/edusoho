0a342cd0c28d5f73d9701240eb8d3a93df575d38
yujiabin <yujiabin@howzhi.com>
2016-09-09 10:14:11 +0800

    css

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+gulp.task('less', function(){
+    gulp.src(['web/bundles/customweb/less/main.less'])
+        .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(less())
+        .pipe(gulp.dest('web/bundles/customweb/css/'));
+});
+
+
+gulp.task('default', function(){
+    gulp.watch("web/bundles/customweb/less/**/*.less",['less']);
+});
