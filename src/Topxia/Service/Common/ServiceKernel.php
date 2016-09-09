61683674b491a67ff89c517dc142a32804c882f0
Simon <wq@zlzkj.com>
2016-09-09 17:36:33 +0800

    fix

--- src/Topxia/Service/Common/ServiceKernel.php
+++ src/Topxia/Service/Common/ServiceKernel.php
@@ -64,7 +64,7 @@ class ServiceKernel
     public static function instance()
     {
         if (empty(self::$_instance)) {
-            throw new \RuntimeException('ServiceKernel未实例化');
+            throw new \RuntimeException('The instance of ServiceKernel is not created!');
         }
 
         self::$_instance->boot();
@@ -133,7 +133,7 @@ class ServiceKernel
     public function getParameter($name)
     {
         if (is_null($this->parameterBag)) {
-            throw new \RuntimeException($this->trans('尚未初始化ParameterBag'));
+            throw new \RuntimeException('The `ParameterBag` of ServiceKernel is not setted!');
         }
 
         return $this->parameterBag->get($name);
@@ -147,7 +147,7 @@ class ServiceKernel
     public function getTranslator()
     {
         if (is_null($this->translator)) {
-            throw new \RuntimeException($this->trans('尚未初始化Translator'));
+            throw new \RuntimeException('The `Translator` of ServiceKernel is not setted!');
         }
 
         return $this->translator;
@@ -156,7 +156,7 @@ class ServiceKernel
     public function hasParameter($name)
     {
         if (is_null($this->parameterBag)) {
-            throw new \RuntimeException($this->trans('尚未初始化ParameterBag'));
+            throw new \RuntimeException('The `ParameterBag` of ServiceKernel is not setted!');
         }
 
         return $this->parameterBag->has($name);
@@ -171,7 +171,7 @@ class ServiceKernel
     public function getCurrentUser()
     {
         if (is_null($this->currentUser)) {
-            throw new \RuntimeException($this->trans('尚未初始化CurrentUser'));
+            throw new \RuntimeException('The `CurrentUser` of ServiceKernel is not setted!');
         }
 
         return $this->currentUser;
@@ -199,7 +199,7 @@ class ServiceKernel
     public function getConnection()
     {
         if (is_null($this->connection)) {
-            throw new \RuntimeException($this->trans('尚未初始化数据库连接'));
+            throw new \RuntimeException('The database connection of ServiceKernel is not setted!');
         }
 
         return $this->connection;
@@ -265,14 +265,6 @@ class ServiceKernel
         return $this->_moduleConfig[$key];
     }
 
-    /**
-     * 翻译数组文本
-     * @param  [type] $message        要翻译的数组文本
-     * @param  array  $arguments      占位符
-     * @param  [type] $domain         [description]
-     * @param  [type] $locale         [description]
-     * @return [type] [description]
-     */
     public function transArray($messages, $arguments = array(), $domain = null, $locale = null)
     {
         foreach ($messages as &$message) {
@@ -281,14 +273,6 @@ class ServiceKernel
         return $messages;
     }
 
-    /**
-     * 翻译文本
-     * @param  [type] $message        要翻译的文本
-     * @param  array  $arguments      占位符
-     * @param  [type] $domain         [description]
-     * @param  [type] $locale         [description]
-     * @return [type] [description]
-     */
     public function trans($message, $arguments = array(), $domain = null, $locale = null)
     {
         return $this->getTranslator()->trans($message, $arguments, $domain, $locale);
