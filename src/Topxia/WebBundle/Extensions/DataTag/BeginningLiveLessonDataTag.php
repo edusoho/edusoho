433694dab2544aa2f68e425802e5c1008e7e90a4
nifeng <fengni@fengdeMacBook-Pro.local>
2016-09-16 17:58:08 +0800

    fix

--- src/Topxia/WebBundle/Extensions/DataTag/BeginningLiveLessonDataTag.php
+++ src/Topxia/WebBundle/Extensions/DataTag/BeginningLiveLessonDataTag.php
@@ -7,7 +7,7 @@ use Topxia\WebBundle\Extensions\DataTag\DataTag;
 class BeginningLiveLessonDataTag extends BaseDataTag implements DataTag
 {
     /**
-     * 获取一个即将直播的课时
+     * 根据当前用户获取一个即将直播的课时
      *
      * 可传入的参数：
      *   afterSecond 必需 即将在多少秒后直播
