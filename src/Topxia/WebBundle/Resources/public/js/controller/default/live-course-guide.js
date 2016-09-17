432abcd0b028ccd9ef3bf2fd3c8487ea6cbc4670
nifeng <fengni@fengdeMacBook-Pro.local>
2016-09-17 11:53:40 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/default/live-course-guide.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/default/live-course-guide.js
@@ -7,22 +7,22 @@ define(function(require, exports, module) {
         if($('#liveCourseGuide').data('time')>0){
             var data = $('#liveCourseGuide').data();
             var time = parseInt(data.time/5);
-            var courseId = data.courseId;
-            var times = Store.get('live-course-guide-'+courseId);
+            var lessonId = data.lessonId;
+            var times = Store.get('live-course-guide-'+lessonId);
 
             if(!times || $.inArray(time, times)<0){
                 $('#liveCourseGuide').show();
                 $('#liveCourseGuideModal').modal();
                 $('#liveCourseGuideModal').on('hidden.bs.modal', function(e){
-                    var courseId = data.courseId;
-                    var times = Store.get('live-course-guide-'+courseId);
+                    var lessonId = data.lessonId;
+                    var times = Store.get('live-course-guide-'+lessonId);
                     if(times){
                         if($.inArray(time, times)<0){
                             times[times.length] = time;
-                            Store.set('live-course-guide-'+courseId, times);
+                            Store.set('live-course-guide-'+lessonId, times);
                         }
                     } else {
-                        Store.set('live-course-guide-'+courseId, [time]);
+                        Store.set('live-course-guide-'+lessonId, [time]);
                     }
                 })
             }
