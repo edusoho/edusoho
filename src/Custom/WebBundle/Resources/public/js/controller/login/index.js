aa05e6a0c7110923cadc52ba6fcecbbca241f9e0
lizhenxiong <lizhenxiong@howzhi.com>
2016-09-21 12:42:19 +0800

    edit

--- src/Custom/WebBundle/Resources/public/js/controller/login/index.js
+++ src/Custom/WebBundle/Resources/public/js/controller/login/index.js
@@ -3,11 +3,9 @@ define(function(require, exports, module) {
         $('.remember-me').on('click',function(){
             if( $(this).hasClass('glyphicon-ok') ){
                 $(this).removeClass('glyphicon-ok');
-                $('#remember').removeAttr('checked');
             }
             else {
                 $(this).addClass('glyphicon-ok');
-                $('#remember').attr('checked', 'checked');
             }
         });
     };
