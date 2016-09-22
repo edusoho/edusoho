df74250e6f8df5f4cff1639b72f2b13787815f43
suju <suju@howzhi.com>
2016-09-22 14:31:51 +0800

    add增加logout

---
+++ api/src/Topxia/Api/Resource/IM/MeLogout.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Topxia\Api\Resource\IM;
+
+use Silex\Application;
+use Topxia\Api\Resource\BaseResource;
+use Symfony\Component\HttpFoundation\Request;
+use Topxia\Service\CloudPlatform\CloudAPIFactory;
+
+class MeLogout extends BaseResource
+{
+    public function get(Application $app, Request $request)
+    {
+        $user = $this->getCurrentUser();
+        $clientId = $user['id'];
+        if ($clientId <= 0) {
+            return $this->error(500, "clientid no empty");
+        }
+        $message = array(
+            'mute' => 1
+        );
+        
+        //@todo leaf
+        return CloudAPIFactory::create('root')->post('/im/me/clients/' + $clientId, $message);
+    }
+
+    public function filter($res)
+    {
+        return $res;
+    }
+}
