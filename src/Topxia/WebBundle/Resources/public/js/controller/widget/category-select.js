8d950ad79354e173e2a2466d4a7378415e9e992d
yexuhui <yexuhui@howzhi.com>
2016-09-26 19:41:54 +0800

    workaround to fix org drop down auto width too short issue

--- src/Topxia/WebBundle/Resources/public/js/controller/widget/category-select.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/widget/category-select.js
@@ -10,7 +10,28 @@ define(function(require, exports, module) {
             treeviewInitState: 'collapsed'
             // treeviewInitState: 'expanded'
         });
-
+        
+        /*workaround to fix org drop down auto width too short issue*/
+    	var updateWidth = function(){
+    		var dropDown = $('.select2-drop-auto-width.select2-drop-active').last();
+    		var width = 0;
+	        $(dropDown).find('ul li').each(function() {
+	           var thisWidth = 5 + $(this).attr('data-indent-count') * 15 + $(this).find('.select2-result-text').text().length*30;
+	             width = width > thisWidth ? width : thisWidth;
+	        });
+	        $(dropDown).css("width",width); 
+        };
+        
+        $('[data-role="tree-select"]').on('select2-open', function(){
+            updateWidth();
+            $( window ).scroll(function() {
+        		updateWidth();
+            })
+        });
+        
+        $('[data-role="tree-select"]').on('select2-close', function(){
+        	$(window).unbind('scroll');
+        });
     };
 
 });
