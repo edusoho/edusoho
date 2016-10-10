7caeb7b824f630884f963bbe75a3e5f0ebddcd29
zhongjie <zhongjie@howzhi.com>
2016-10-10 16:19:57 +0800

    添加后台图表

--- src/Topxia/AdminBundle/Resources/public/js/controller/default/index.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/default/index.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
     var Notify = require('common/bootstrap-notify');
     var Validator = require('bootstrap.validator');
     require('common/validator-rules').inject(Validator);
+    require('echarts');
 
     exports.run = function() {
 
@@ -13,31 +14,53 @@ define(function(require, exports, module) {
         });
         //noticeModal();
         getData();
+        popularCoursesChange();
+        searchCount();
+        usersStatistic($("#user-statistic"));
+        studyCountStatistic($("#study-count-statistic"));
+        payOrderStatistic($("#pay-order-statistic"));
+        studyLessonCountStatistic($("#study-lesson-count-statistic"))
 
     };
 
+    var searchCount = function() {
+        var totalWidth = $(".js-search-count").parent().width();
+        var $countDom = $(".js-search-count");
+        var totalCount = 0;
+
+        $countDom.each(function(){
+            totalCount += $(this).data('count');
+        })
+
+        $countDom.each(function(){
+            var width = ($(this).data('count')/totalCount * totalWidth * 3 + 2).toFixed(2);
+            $(this).width(width);
+        })
+    }
+
 
     var getData = function() {
-        initData()
+        // systemStatusData()
+        // .then(popularCoursesData)
+        popularCoursesData()
         .then(operationData)
-        .then(systemStatusData)
         .then(onlineNum)
         .then(loginNum)
     }
 
-
-    var initData = function() {
-        var $popularCoursesType = $("#popular-courses-type");
-        return $.get($popularCoursesType.data('url'), {dateType: $popularCoursesType.val()}, function(html) {
+    var popularCoursesData = function() {
+        var $this = $("#popular-courses-type");
+        return $.get($this.data('url'), {dateType: $this.val()}, function(html) {
             $('#popular-courses-table').html(html);
         });
     }
 
-    var popularCoursesData = function () {
+    var popularCoursesChange = function () {
         $("#popular-courses-type").on('change', function() {
-            $.get($(this).data('url'), {dateType: this.value}, function(html) {
-                $('#popular-courses-table').html(html);
-            });
+            // $.get($(this).data('url'), {dateType: this.value}, function(html) {
+            //     $('#popular-courses-table').html(html);
+            // });
+            popularCoursesData();
         });
     }
 
@@ -80,6 +103,200 @@ define(function(require, exports, module) {
         return $.post($('#loginNum').data('url'),function(res){
             $('#loginNum').html(Translator.trans('登录人数：%res%人',{res:res.loginCount}));
         });
+    }
+
+    var usersStatistic =  function($el) {
+        this.element = $el;
+        var chart = echarts.init(this.element.get(0));
+
+        var option = {
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data:['新增注册','活跃用户','流失用户']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['16/06/12','16/06/13','16/06/14','16/06/15','16/06/16','16/06/17','16/06/18']
+            },
+            yAxis: {
+                type: 'value',
+            },
+            series: [
+                {
+                    name:'新增注册',
+                    type:'line',
+                    stack: '总量',
+                    data:[120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name:'活跃用户',
+                    type:'line',
+                    stack: '总量',
+                    data:[220, 182, 191, 234, 290, 330, 310]
+                },
+                {
+                    name:'流失用户',
+                    type:'line',
+                    stack: '总量',
+                    data:[150, 232, 201, 154, 190, 330, 410]
+                }
+            ]
+        };
+        chart.setOption(option);
+    }
+
+    var studyCountStatistic = function($el) {
+        this.element = $el;
+        var chart = echarts.init(this.element.get(0));
+
+        var option = {
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data:['新增人次','付费人次','学习人次']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['16/06/12','16/06/13','16/06/14','16/06/15','16/06/16','16/06/17','16/06/18']
+            },
+            yAxis: {
+                type: 'value',
+            },
+            series: [
+                {
+                    name:'新增人次',
+                    type:'line',
+                    stack: '总量',
+                    data:[120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name:'付费人次',
+                    type:'line',
+                    stack: '总量',
+                    data:[220, 182, 191, 234, 290, 330, 310]
+                },
+                {
+                    name:'学习人次',
+                    type:'line',
+                    stack: '总量',
+                    data:[150, 232, 201, 154, 190, 330, 410]
+                }
+            ]
+        };
+        chart.setOption(option);
+    }
+
+    var payOrderStatistic = function($el) {
+        this.element = $el;
+        var chart = echarts.init(this.element.get(0));
+
+        var option = {
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                right: 'right',
+                top: 'center',
+                data:['课程订单','班级订单','会员订单']
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            series: [
+                {
+                    name:'订单量',
+                    type:'pie',
+                    radus: '50%',
+                    center:['40%','60%'],
+                    data:[
+                        {value: 100,name: '课程订单'},
+                        {value: 200,name: '班级订单'},
+                        {value: 300,name: '会员订单'}
+                    ]
+                }
+            ]
+        };
+        chart.setOption(option);
+
+    }
+
+    var studyLessonCountStatistic = function($el) {
+        this.element = $el;
+        var chart = echarts.init(this.element.get(0));
+        var option = {
+            tooltip : {
+                trigger: 'axis',
+                axisPointer : {            
+                    type : 'shadow'
+                }
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis : [
+                {
+                    type : 'category',
+                    data : ['16/06/12', '16/06/13', '16/06/14', '16/06/15', '16/06/16', '16/06/17', '16/06/18'],
+                    axisTick: {
+                        alignWithLabel: true
+                    }
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+
+            series : [
+                {
+                    name:'课时学习数',
+                    type:'bar',
+                    barWidth: '60%',
+                    data:[10, 52, 200, 334, 390, 330, 220]
+                }
+            ]
+        };
+        chart.setOption(option);
     }
 
 
