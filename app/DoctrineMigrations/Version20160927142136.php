60abd3dc8f0fdc5bbad5a83e50cd5769b4f64fc2
杨佳龙 <yangjlong315@gmail.com>
2016-10-14 09:15:23 +0800

    add upgrade

--- app/DoctrineMigrations/Version20160927142136.php
+++ app/DoctrineMigrations/Version20160927142136.php
@@ -22,7 +22,7 @@ class Version20160927142136 extends AbstractMigration
 
         $this->addSql(" ALTER TABLE `classroom_member` ADD `learnedNum` INT(10) COMMENT '已学课时数'; ");
 
-        $this->addSql(" UPDATE `course_member` SET `lastLearnTime` = (SELECT max(startTime) FROM `course_lesson_learn` WHERE course_member.courseId = course_lesson_learn.courseId);");
+        $this->addSql(" UPDATE `course_member` SET `lastLearnTime` = (SELECT max(startTime) FROM `course_lesson_learn` WHERE course_member.courseId = course_lesson_learn.courseId AND course_member.userId = course_lesson_learn.userId);");
 
         $this->addSql(" UPDATE `classroom_member` SET `lastLearnTime` = (SELECT max(lastLearnTime) FROM `course_member` WHERE classroom_member.classroomId = course_member.classroomId AND classroom_member.userId = course_member.userId AND course_member.joinedType = 'classroom');");
 
