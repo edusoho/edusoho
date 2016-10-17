991d1f118ab09fee9dbd22d29dcfbf52c1d03b09
Simon <wq@zlzkj.com>
2016-10-17 15:13:12 +0800

    判断event push时isWithoutNetwork

--- src/Topxia/Service/CloudPlatform/Client/AbstractCloudAPI.php
+++ src/Topxia/Service/CloudPlatform/Client/AbstractCloudAPI.php
@@ -84,19 +84,15 @@ class AbstractCloudAPI
 
     protected function _request($method, $uri, $params, $headers)
     {
-        if ($this->isWithoutNetwork()) {
-            $this->debug && $this->logger && $this->logger->debug("AbstractCloudAPI Network is turned off, you can not request: [{$requestId}] {$method} {$url}", array('params' => $params, 'headers' => $headers));
-            return true;
-        }
-
         $requestId = substr(md5(uniqid('', true)), -16);
 
         $url = $this->apiUrl.'/'.self::VERSION.$uri;
         if ($this->isWithoutNetwork()) {
-            $this->logger && $this->logger->debug("NetWork Off, So Block:[{$requestId}] {$method} {$url}", array('params' => $params, 'headers' => $headers));
+            if ($this->debug && $this->logger) {
+                $this->logger->debug("NetWork Off, So Block:[{$requestId}] {$method} {$url}", array('params' => $params, 'headers' => $headers));
+            }
             return array('network' => 'off');
         }
-        $this->debug && $this->logger && $this->logger->debug("[{$requestId}] {$method} {$url}", array('params' => $params, 'headers' => $headers));
 
         $headers[] = 'Content-type: application/json';
 
@@ -175,6 +171,10 @@ class AbstractCloudAPI
         if (empty($result)) {
             $this->logger && $this->logger->error("[{$requestId}] RESPONSE_JSON_DECODE_ERROR", $context);
             throw new CloudAPIIOException("Api result json decode error: (url:{$url}).");
+        }
+
+        if ($this->debug && $this->logger) {
+            $this->logger->debug("[{$requestId}] {$method} {$url}", array('params' => $params, 'headers' => $headers));
         }
 
         return $result;
