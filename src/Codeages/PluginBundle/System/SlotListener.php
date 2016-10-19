a4aacb87cdd5753a839262b82eebafa793810cbf
Wellming Li <wellming.li@howzhi.com>
2016-10-19 11:52:30 +0800

    add slot

---
+++ src/Codeages/PluginBundle/System/SlotListener.php
@@ -0,0 +1,20 @@
+<?php
+namespace Codeages\PluginBundle\System;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class SlotListener
+{
+    public function __construct($controller)
+    {
+        $this->controller = $controller;
+    }
+
+    public function onExtend($event)
+    {
+        $content = $this->controller->extend($args);
+
+        $event->addContent($content);
+    }
+
+}
