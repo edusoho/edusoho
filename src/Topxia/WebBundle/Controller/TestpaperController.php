67f922a78595e43dd650bf1687ddf8282cd752d9
lufangyu1217 <584141397@qq.com>
2016-10-19 14:12:03 +0800

    fix

--- src/Topxia/WebBundle/Controller/TestpaperController.php
+++ src/Topxia/WebBundle/Controller/TestpaperController.php
@@ -583,22 +583,20 @@ class TestpaperController extends BaseController
         $testpapers   = $this->getTestpaperService()->findAllTestpapersByTargets($courseIds);
         $testpaperIds = ArrayToolkit::column($testpapers, 'id');
 
+        $conditions = array(
+            'testIds'        => $testpaperIds,
+            'status'         => 'finished',
+            'checkTeacherId' => $user['id']
+        );
+
         $paginator = new Paginator(
             $request,
-            $this->getTestpaperService()->searchTestpaperResultsCount(array(
-                'testIds'        => $testpaperIds,
-                'status'         => 'finished',
-                'checkTeacherId' => $user['id']
-            )),
+            $this->getTestpaperService()->searchTestpaperResultsCount($conditions),
             10
         );
 
         $paperResults = $this->getTestpaperService()->searchTestpaperResults(
-            array(
-                'testIds'        => $testpaperIds,
-                'status'         => 'finished',
-                'checkTeacherId' => $user['id']
-            ),
+            $conditions,
             array(
                 'checkedTime',
                 'DESC'
