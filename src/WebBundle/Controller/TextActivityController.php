65f5937846ef14149a7060c1e50c63d99ba15dd4
nifeng <fengni@fengdeMacBook-Pro.local>
2016-10-24 15:23:21 +0800

    rename textactivity

--- src/WebBundle/Controller/TextActivityController.php
+++ src/WebBundle/Controller/TextActivityController.php
@@ -21,14 +21,14 @@ class TextActivityController extends BaseController implements ActivityActionInt
     public function editAction(Request $request, $id)
     {
         $activity = $this->getActivityService()->getActivity($id);
-        return $this->render('WebBundle:ActivityManage:text.html.twig', array(
+        return $this->render('WebBundle:TextActivity:modal.html.twig', array(
             'activity'    => $activity
         ));
     }
 
     public function createAction(Request $request)
     {
-        return $this->render('WebBundle:ActivityManage:text.html.twig');
+        return $this->render('WebBundle:TextActivity:modal.html.twig');
     }
 
     /**
