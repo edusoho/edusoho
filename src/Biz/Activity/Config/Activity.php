32421851afb8bcc3ef07e0812f81fb01ed2b3124
liumengte <liumengte@howzhi.com>
2016-10-24 14:41:22 +0800

    add getActionMap comment line

--- src/Biz/Activity/Config/Activity.php
+++ src/Biz/Activity/Config/Activity.php
@@ -10,7 +10,6 @@ namespace Biz\Activity\Config;
 use Biz\Activity\Listener\Listener;
 use Codeages\Biz\Framework\Context\Biz;
 use Topxia\Common\Exception\UnexpectedValueException;
-use WebBundle\Controller\ActivityActionInterface;
 
 abstract class Activity
 {
@@ -24,25 +23,29 @@ abstract class Activity
      * @inheritdoc
      */
     public function create($fields)
-    {}
+    {
+    }
 
     /**
      * @inheritdoc
      */
     public function update($targetId, $fields)
-    {}
+    {
+    }
 
     /**
      * @inheritdoc
      */
     public function delete($targetId)
-    {}
+    {
+    }
 
     /**
      * @inheritdoc
      */
     public function get($targetId)
-    {}
+    {
+    }
 
     public final function __construct(Biz $biz)
     {
@@ -50,7 +53,17 @@ abstract class Activity
     }
 
     /**
-     * @return array<String, ActivityActionInterface.class.name>
+     * Activity 创建、编辑、进行时指定的Controller配置
+     *
+     * 如：
+     * ExampleActivity
+     * array(
+     *    'create' => 'ExampleBundle:Example:create',
+     *    'edit' => 'ExampleBundle:Example:edit',
+     *    'create' => 'ExampleBundle:Example:create'
+     * )
+     *
+     * @return array<String, String>
      */
     public abstract function getActionMap();
 
@@ -97,13 +110,13 @@ abstract class Activity
     public final function getListener($eventName)
     {
         $map = $this->getEventMap();
-        if(empty($map) || !isset($map[$eventName])){
+        if (empty($map) || !isset($map[$eventName])) {
             return null;
         }
         $reflection = new \ReflectionClass($map[$eventName]);
-        $listener = $reflection->newInstanceArgs(array($this->getBiz()));
+        $listener   = $reflection->newInstanceArgs(array($this->getBiz()));
 
-        if(!$listener instanceof Listener){
+        if (!$listener instanceof Listener) {
             throw new UnexpectedValueException("listener class must be Listener Derived Class");
         }
 
