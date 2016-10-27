024d27120ee40f320fcdb37d61edfdce7bd72994
lufangyu1217 <584141397@qq.com>
2016-10-27 10:34:17 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/settings/profile.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/settings/profile.js
@@ -40,7 +40,7 @@
 
         validator.addItem({
             element: '[name="profile[truename]"]',
-            rule: 'chinese_alphanumeric minlength{min:2} maxlength{max:36}'
+            rule: 'chinese_alphanumeric byte_minlength{min:2} byte_maxlength{max:36}'
         });
 
         validator.addItem({
