778b7916ce70d3c3f8485aa15af6348c0485d4a1
Simon Lau <liuyangyang@howzhi.com>
2016-10-27 14:30:29 +0800

    init vidoe activity

---
+++ src/WebBundle/Controller/VideoActivityController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: Edusoho V8
+ * Date: 26/10/2016
+ * Time: 19:25
+ */
+
+namespace WebBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class VideoActivityController extends BaseController implements ActivityActionInterface
+{
+    public function showAction(Request $request, $id)
+    {
+        // TODO: Implement showAction() method.
+    }
+
+    public function editAction(Request $request, $id)
+    {
+        // TODO: Implement editAction() method.
+    }
+
+    public function createAction(Request $request)
+    {
+        return $this->render('WebBundle:VideoActivity:modal.html.twig');
+    }
+
+
+}
