285c5cf4bd6066bfadfaad9436b4c3dbab87763c
wuli <wuli@howzhi.com>
2016-10-28 18:12:18 -0700

    iframe-heigth

--- src/WebBundle/Resources/assets/js/activity-manage/video-activity-fields.js
+++ src/WebBundle/Resources/assets/js/activity-manage/video-activity-fields.js
@@ -1,32 +1,25 @@
-
-
-//设置上传控件的高度
-$('#upload-iframe').height($('#upload-iframe').contents().find('body').height());
 //设置任务高度；
+var $parentiframe = $(window.parent.document).find('#task-manage-content-iframe');
 
-
-
-
-
-
-
-// setTimeout(() => {
+setTimeout(() => {
     
-//     let $uploader = $('#uploader-container');
+    let $uploader = $('#uploader-container');
 
-//     let uploaderSdk = new UploaderSDK({
-//         id: $uploader.attr('id'),
-//         initUrl: $uploader.data('initUrl'),
-//         accept: $uploader.data('accept'),
-//         process: $uploader.data('process')
-//     });
+    let uploaderSdk = new UploaderSDK({
+        id: $uploader.attr('id'),
+        initUrl: $uploader.data('initUrl'),
+        accept: $uploader.data('accept'),
+        process: $uploader.data('process')
+    });
 
+    console.log($("iframe").contents().find('body').height());
+    $("#iframe").height($("iframe").contents().find('body').height());
+    $parentiframe.height($parentiframe.contents().find('body').height());
 
-
-//     $("#material a").click(function (e) {
-//         e.preventDefault()
-//         $(this).tab('show')
-//     });
-// }, 1000);
+    $("#material a").click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
+}, 1000);
 
 
