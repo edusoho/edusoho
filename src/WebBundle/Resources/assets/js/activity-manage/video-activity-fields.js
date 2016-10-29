33d298201818445bd547995c6d14b165c6f734b7
wuli <wuli@howzhi.com>
2016-10-28 17:24:48 -0700

    Css

--- src/WebBundle/Resources/assets/js/activity-manage/video-activity-fields.js
+++ src/WebBundle/Resources/assets/js/activity-manage/video-activity-fields.js
@@ -1,20 +1,30 @@
 $('#upload-iframe').height($('#upload-iframe').contents().find('body').height());
 
-setTimeout(() => {
+
+
+
+
+
+
+
+
+// setTimeout(() => {
     
-    let $uploader = $('#uploader-container');
+//     let $uploader = $('#uploader-container');
 
-    let uploaderSdk = new UploaderSDK({
-        id: $uploader.attr('id'),
-        initUrl: $uploader.data('initUrl'),
-        accept: $uploader.data('accept'),
-        process: $uploader.data('process')
-    });
+//     let uploaderSdk = new UploaderSDK({
+//         id: $uploader.attr('id'),
+//         initUrl: $uploader.data('initUrl'),
+//         accept: $uploader.data('accept'),
+//         process: $uploader.data('process')
+//     });
 
-    $("#material a").click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    });
-}, 1000);
+
+
+//     $("#material a").click(function (e) {
+//         e.preventDefault()
+//         $(this).tab('show')
+//     });
+// }, 1000);
 
 
