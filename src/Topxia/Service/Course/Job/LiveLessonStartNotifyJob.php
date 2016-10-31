e3e69430320d35fd57da81a85148ed0d2a7f4522
hamster <malianbo@163.com>
2016-10-31 11:05:36 +0800

    直播推送API，草稿，方案可能调整，代码先缓存

--- src/Topxia/Service/Course/Job/LiveLessonStartNotifyJob.php
+++ src/Topxia/Service/Course/Job/LiveLessonStartNotifyJob.php
@@ -8,7 +8,25 @@ class LiveLessonStartNotifyJob implements Job
 {
     public function execute($params)
     {
+        $targetType = $params['targetType'];
+        $targetId   = $params['targetId'];
+        if ($targetType == 'live_lesson') {
+            $lesson = $this->getCourseService()->getLesson($targetId);
+            $course = $this->getCourseService()->getCourse($lesson['courseId']);
 
+            $lesson['course'] = $course;
+            //获取课程的学员列表，发送通知
+            //FXIME
+            //1. 下面的分页查询应确保查询出所有学员
+            //2. 数据量可能很大，应进行过滤，只传递所需信息，但同时也要保证一定的通用性 @苏菊
+            $lession['students'] = $this->getCourseService()->findCourseStudents($lesson['courseId'], 0, 1000);
+            $this->pushCloud('lesson.live_notify', $lesson);
+        }
+    }
+
+    protected function pushCloud($eventName, array $data, $level = 'normal')
+    {
+        return $this->getCloudDataService()->push('school.'.$eventName, $data, time(), $level);
     }
 
     private function getCourseService()
@@ -20,5 +38,4 @@ class LiveLessonStartNotifyJob implements Job
     {
         return ServiceKernel::instance();
     }
-
 }
