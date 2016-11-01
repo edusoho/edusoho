3d1d1850d237eae669c63e3545f420540e83d070
nifeng <fengni@fengdeMacBook-Pro.local>
2016-11-01 16:18:34 +0800

    fix connection factory

--- src/Topxia/Common/AppConnectionFactory.php
+++ src/Topxia/Common/AppConnectionFactory.php
@@ -2,11 +2,12 @@
 namespace Topxia\Common;
 
 use Topxia\Service\Common\ConnectionFactory;
+use Topxia\Service\Common\ServiceKernel;
 
 class AppConnectionFactory implements ConnectionFactory
 {
-
     protected $container;
+    protected $connection;
 
     public function __construct($container)
     {
@@ -15,9 +16,12 @@ class AppConnectionFactory implements ConnectionFactory
 
     public function getConnection()
     {
-        $connection = $this->container->get('database_connection');
-        $connection->exec('SET NAMES UTF8');
-        return $connection;
+        if(empty($this->connection)){
+            $connection = $this->container->get('database_connection');
+            $connection->exec('SET NAMES UTF8');
+            $this->connection = $connection;
+        }
+        return $this->connection;
     }
 
 }
