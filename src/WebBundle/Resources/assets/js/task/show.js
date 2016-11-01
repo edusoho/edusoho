34308883f8788afc39182a8dfdf24029fbf2346d
wuli <wuli@howzhi.com>
2016-10-31 17:44:24 -0700

    Css

--- src/WebBundle/Resources/assets/js/task/show.js
+++ src/WebBundle/Resources/assets/js/task/show.js
@@ -5,14 +5,6 @@ class TaskShow {
 
     initPlugin() {
         $('[data-toggle="tooltip"]').tooltip();
-        // var container = document.getElementById('task-group');
-        // var Ps = require('perfect-scrollbar.js');
-        
-        // Ps.initialize(container, {
-        //   wheelSpeed: 2,
-        //   wheelPropagation: true,
-        //   minScrollbarLength: 20
-        // });
 
         $('#task-group').perfectScrollbar({wheelSpeed:50});
         $("#task-group").scrollTop(30);
