c1d2b0a94ba4b24632616a5f4c1ba1580ce78f3f
liumengte <liumengte@howzhi.com>
2016-11-02 13:15:18 +0800

    local upload support cors

--- src/WebBundle/Resources/assets/js/component/chooser/file-upload.js
+++ src/WebBundle/Resources/assets/js/component/chooser/file-upload.js
@@ -1,16 +1,18 @@
-/**
- * Created by Simon on 31/10/2016.
- */
 let $uploader = $('#uploader-container');
 
 let uploaderSdk = new UploaderSDK({
-    id: $uploader.attr('id'),
-    initUrl: $uploader.data('initUrl'),
-    finishUrl: $uploader.data('finishUrl'),
-    accept: $uploader.data('accept'),
-    process: $uploader.data('process')
+  id: $uploader.attr('id'),
+  initUrl: $uploader.data('initUrl'),
+  finishUrl: $uploader.data('finishUrl'),
+  accept: $uploader.data('accept'),
+  process: $uploader.data('process')
 });
-uploaderSdk.process = {
-    "videoQuality": "high",
-    "audioQuality": "high"
-};
+
+
+
+uploaderSdk.on('file.finish', (file) => {
+  file.source = 'self';
+  $('[name="media"]').val(JSON.stringify(file));
+});
+
+
