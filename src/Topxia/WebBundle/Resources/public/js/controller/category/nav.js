29d91dda3285076b181e9287155a0bd54305e674
yujiabin <yujiabin@howzhi.com>
2016-11-04 17:33:38 +0800

    add 添加展开判断

--- src/Topxia/WebBundle/Resources/public/js/controller/category/nav.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/category/nav.js
@@ -6,6 +6,18 @@ define(function(require, exports, module) {
                 $(this).find('li.all').addClass('active');
             }
         });
+        $(window).on('resize', function(){
+            tabsToggleInit();
+        });
+        function tabsToggleInit() {
+            $('.tabs-group').each(function(index,item){
+                var cheight = $(this).find('.content').height();
+                if( 50 != cheight){
+                    $(this).find('.js-tabs-toggle').removeClass('hidden');
+                }
+            });
+        }
+        tabsToggleInit();
         $('.js-tabs-toggle').on('click', function(){
             var $parent = $(this).parents('.tabs-group');
             if( $(this).hasClass('tabs-hide') ) {
