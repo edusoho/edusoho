85594e9e06dfd6e0e8dc7e4dbf333dd490992362
yujiabin <yujiabin@howzhi.com>
2016-11-04 17:00:57 +0800

    css

--- src/Topxia/WebBundle/Resources/public/js/controller/category/nav.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/category/nav.js
@@ -6,7 +6,26 @@ define(function(require, exports, module) {
                 $(this).find('li.all').addClass('active');
             }
         });
+        $('.js-tabs-toggle').on('click', function(){
+            var $parent = $(this).parents('.tabs-group');
+            if( $(this).hasClass('tabs-hide') ) {
+                var height = $parent.find('.content').height();
+                $(this).removeClass('tabs-hide');
+                $(this).text('收齐');
+                $parent.animate({
+                    height: height
 
+                },100);
+            }
+            else {
+                $(this).addClass('tabs-hide');
+                $(this).text('展开');
+                $parent.animate({
+                    height: '50px'
+                }, 100);
+            }
+            
+        });
     };
 
     
