93177d104e6c110849f954e93be8b2c9641a44b6
hamster <malianbo@163.com>
2016-11-07 21:28:51 +0800

    refact

--- api/src/Topxia/Api/Resource/IM/MemberSync.php
+++ api/src/Topxia/Api/Resource/IM/MemberSync.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class MemberSync extends BaseResource
 {
+    /*每次请求允许创建的最大会话数量*/
+    const MAX_CREATION_PER_TIME = 20;
     public function post(Application $app, Request $request)
     {
         $setting = $this->getSettingservice()->get('app_im', array());
@@ -51,9 +53,8 @@ class MemberSync extends BaseResource
 
     protected function syncCourseConversations($user)
     {
-        $count   = $this->getCourseService()->findUserLearnCourseCount($user['id']);
-        $courses = $this->getCourseService()->findUserLearnCourses($user['id'], 0, $count);
-
+        $count     = $this->getCourseService()->findUserLearnCourseCount($user['id']);
+        $courses   = $this->getCourseService()->findUserLearnCourses($user['id'], 0, $count);
         $courseIds = ArrayToolkit::column($courses, 'id');
 
         $this->syncTargetConversations($user, $courses, 'course');
@@ -62,11 +63,11 @@ class MemberSync extends BaseResource
 
     protected function syncClassroomConversations($user)
     {
-        $classroomIds = $this->getClassroomService()->findUserJoinedClassroomIds($user['id']);
-        $classrooms   = $this->getClassroomService()->findClassroomsByIds($classroomIds);
+        // $classroomIds = $this->getClassroomService()->findUserJoinedClassroomIds($user['id']);
+        // $classrooms   = $this->getClassroomService()->findClassroomsByIds($classroomIds);
 
-        $this->syncTargetConversations($user, $classrooms, 'classroom');
-        $this->syncClassroomConversationMembers($user, $classroomIds);
+        // $this->syncTargetConversations($user, $classrooms, 'classroom');
+        // $this->syncClassroomConversationMembers($user, $classroomIds);
     }
 
     protected function syncCourseConversationMembers($user, $courseIds)
@@ -99,38 +100,6 @@ class MemberSync extends BaseResource
         }
     }
 
-    protected function joinConversations($targetIds, $targets)
-    {
-        $targetMap = ArrayToolkit::index($targets, 'id');
-        $params    = array(
-            'targetIds'   => $targetIds,
-            'targetTypes' => array($targetType.'-push')
-        );
-        $count       = $this->getConversationService()->searchConversationCount($params);
-        $targetConvs = $this->getConversationService()->searchConversations($params, array('createdTime', 'asc'), 0, $count);
-
-        $targetConvsMap = ArrayToolkit::index($targetConvs, 'targetId');
-        $targetConvIds  = ArrayToolkit::column($targetConvs, 'targetId');
-
-        $toCreate = array_diff($targetIds, $targetConvIds);
-
-        foreach ($targetIds as $id) {
-            if (array_key_exists($toCreate, $id)) {
-                $this->getConversationService()->createConversation('推送：'.$targetMap[$id]['title'], $targetType.'-push', $id, array($user));
-            } else {
-                $this->getConversationService()->joinConversation($targetConvsMap[$id]['no'], $user['id']);
-            }
-        }
-    }
-
-    protected function quitConversations($targetIds, $userConvs)
-    {
-        $userConvsMap = ArrayToolkit::index($userConvs, 'targetId');
-        foreach ($targetIds as $id) {
-            $this->getConversationService()->quitConversation($userConvsMap[$id]['convNo'], $user['id']);
-        }
-    }
-
     protected function syncTargetConversations($user, $targets, $targetType)
     {
         $userConvs = $this->getConversationService()->findMembersByUserIdAndTargetType($user['id'], $targetType.'-push');
@@ -138,8 +107,44 @@ class MemberSync extends BaseResource
         $targetIds   = ArrayToolkit::column($targets, 'id');
         $userConvIds = ArrayToolkit::column($userConvs, 'targetId');
 
-        $this->joinConversations(array_diff($targetIds, $userConvIds), $targets);
-        $this->quitConversations(array_diff($userConvIds, $targetIds), $userConvs);
+        $this->joinConversations(array_diff($targetIds, $userConvIds), $targets, $targetType, $user);
+        $this->quitConversations(array_diff($userConvIds, $targetIds), $userConvs, $user);
+    }
+
+    protected function joinConversations($targetIds, $targets, $targetType, $user)
+    {
+        $targetMap = ArrayToolkit::index($targets, 'id');
+        $params    = array(
+            'targetIds'   => $targetIds,
+            'targetTypes' => array($targetType.'-push')
+        );
+        $count       = $this->getConversationService()->searchConversationCount($params);
+        $targetConvs = $this->getConversationService()->searchConversations($params, array('createdTime', 'desc'), 0, $count);
+
+        $targetConvsMap = ArrayToolkit::index($targetConvs, 'targetId');
+        $targetConvIds  = ArrayToolkit::column($targetConvs, 'targetId');
+
+        $toCreate = array_diff($targetIds, $targetConvIds);
+
+        $cnt = 0;
+        foreach ($targetIds as $id) {
+            if (array_key_exists($id, $toCreate)) {
+                if (++$cnt > MAX_CREATION_PER_TIME) {
+                    break; // 防止请求过于频繁造成服务器压力过大
+                }
+                $this->getConversationService()->createConversation('推送：'.$targetMap[$id]['title'], $targetType.'-push', $id, array($user));
+            } else {
+                $this->getConversationService()->joinConversation($targetConvsMap[$id]['no'], $user['id']);
+            }
+        }
+    }
+
+    protected function quitConversations($targetIds, $userConvs, $user)
+    {
+        $userConvsMap = ArrayToolkit::index($userConvs, 'targetId');
+        foreach ($targetIds as $id) {
+            $this->getConversationService()->quitConversation($userConvsMap[$id]['convNo'], $user['id']);
+        }
     }
 
     protected function getConversationService()
