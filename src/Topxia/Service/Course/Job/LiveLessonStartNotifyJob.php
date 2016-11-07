83169724fcc3912c8310727348b1a3221bcbe4bd
hamster <malianbo@163.com>
2016-11-07 11:26:17 +0800

    调整直播消息推送逻辑

--- src/Topxia/Service/Course/Job/LiveLessonStartNotifyJob.php
+++ src/Topxia/Service/Course/Job/LiveLessonStartNotifyJob.php
@@ -15,18 +15,78 @@ class LiveLessonStartNotifyJob implements Job
             $course = $this->getCourseService()->getCourse($lesson['courseId']);
 
             $lesson['course'] = $course;
-            //获取课程的学员列表，发送通知
-            //FXIME
-            //1. 下面的分页查询应确保查询出所有学员
-            //2. 数据量可能很大，应进行过滤，只传递所需信息，但同时也要保证一定的通用性 @苏菊
-            $lession['students'] = $this->getCourseService()->findCourseStudents($lesson['courseId'], 0, 1000);
-            $this->pushCloud('lesson.live_notify', $lesson);
+            $message = "您报名的课程$lesson['title']，即将于".date('h:i', $lesson['startTime'])."开始直播，马上前往直播教室准备学习吧!";
+            $convNo = $this->getConversationService()->getConversationByTarget($lesson['courseId'], 'course-push');
+
+            $from = array(
+                'courseId' => $lesson['courseId'],
+                'lessonId' => $targetId
+            );
+            $to = array(
+               'type'   => 'user',
+               'convNo' => $convNo 
+            );
+            $body = array(
+                'message'  => $message,
+                'courseId' => $lesson['courseId'],
+                'lessonId' => $targetId
+            );
+            
+            return $this->pushIM($form, $to, $body);
+        }   
+    }
+
+    //FIXME 该方法和PushMessageEventSubscriber的一样，最好抽提出来
+    protected function pushIM($from, $to, $body)
+    {
+        $setting = $this->getSettingService()->get('app_im', array());
+        if (empty($setting['enabled'])) {
+            return;
+        }
+
+        $params = array(
+            'fromId'   => 0,
+            'fromName' => '系统消息',
+            'toName'   => '全部',
+            'body'     => array(
+                'v' => 1,
+                't' => 'push',
+                'b' => $body,
+                's' => $from,
+                'd' => $to
+            ),
+            'convNo'   => empty($to['convNo']) ? '' : $to['convNo']
+        );
+
+        if ($to['type'] == 'user') {
+            $params['toId'] = $to['id'];
+        }
+
+        if (empty($params['convNo'])) {
+            return;
+        }
+
+        try {
+            $api    = IMAPIFactory::create();
+            $result = $api->post('/push', $params);
+
+            $setting = $this->getSettingService()->get('developer', array());
+            if (!empty($setting['debug'])) {
+                IMAPIFactory::getLogger()->debug('API RESULT', !is_array($result) ? array() : $result);
+            }
+        } catch (\Exception $e) {
+            IMAPIFactory::getLogger()->warning('API REQUEST ERROR:'.$e->getMessage());
         }
     }
 
     protected function pushCloud($eventName, array $data, $level = 'normal')
     {
         return $this->getCloudDataService()->push('school.'.$eventName, $data, time(), $level);
+    }
+
+    protected function getConversationService()
+    {
+        return $this->getServiceKernel()->createService('IM.ConversationService');
     }
 
     private function getCourseService()
