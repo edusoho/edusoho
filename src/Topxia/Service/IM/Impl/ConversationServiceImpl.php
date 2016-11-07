0af52cfed5c97b416d19893a671dde24a673f403
hamster <malianbo@163.com>
2016-11-07 09:39:58 +0800

    实现push相关的用户会话同步

--- src/Topxia/Service/IM/Impl/ConversationServiceImpl.php
+++ src/Topxia/Service/IM/Impl/ConversationServiceImpl.php
@@ -143,14 +143,8 @@ class ConversationServiceImpl extends BaseService implements ConversationService
         $this->deleteMembersByTargetIdAndTargetType($conv['targetId'], $conv['targetType']);
         $this->deleteConversationByTargetIdAndTargetType($conv['targetId'], $conv['targetType']);
 
-        //TODO 如何删除会话？
-        // $res = $this->createImApi()->post("/conversations/{$convNo}/members", array('clients' => $clients));
-
-        // if (isset($res['success'])) {
-        //     return true;
-        // }
-
-        return false;
+        //不需要删除云端会话
+        return true;
     }
 
     public function getMember($id)
