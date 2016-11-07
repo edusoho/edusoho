c7fe1be7ef6aa299450522ad9e74967f0c72cf0d
yangshuai <yangshuai@howzhi.com>
2016-11-07 21:22:22 +0800

    set orders update when order log is empty

--- app/DoctrineMigrations/Version20160926154110.php
+++ app/DoctrineMigrations/Version20160926154110.php
@@ -16,8 +16,9 @@ class Version20160926154110 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("ALTER TABLE  `orders` ADD  `updatedTime` INT(10) NOT NULL AFTER  `createdTime`");
-        $this->addSql("UPDATE `orders` SET `updatedTime` = (select if(max(createdTime),max(createdTime),0) from `order_log` where order_log.orderId = orders.id)");
+        $this->addSql("ALTER TABLE  `orders` ADD  `updatedTime` INT(10) NOT NULL AFTER  `createdTime`;");
+        $this->addSql("UPDATE `orders` SET `updatedTime` = (select if(max(createdTime),max(createdTime),0) from `order_log` where order_log.orderId = orders.id);");
+        $this->addSql("UPDATE `orders` set `updatedTime`= createdTime WHERE updatedTime = 0 ;");
     }
 
     /**
