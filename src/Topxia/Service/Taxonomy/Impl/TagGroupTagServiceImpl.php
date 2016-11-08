db0110b7de5dc653cd3995121e7171419bcfcc28
lufangyu1217 <584141397@qq.com>
2016-11-08 11:14:48 +0800

    fix

--- src/Topxia/Service/Taxonomy/Impl/TagGroupTagServiceImpl.php
+++ src/Topxia/Service/Taxonomy/Impl/TagGroupTagServiceImpl.php
@@ -7,18 +7,9 @@ use Topxia\Service\Taxonomy\TagGroupTagService;
 
 class TagGroupTagServiceImpl extends BaseService implements TagGroupTagService
 {
-    private $allowConditions = array(
-        'name', 'scope', 'tagNum', 'createdTime', 'updatedTime'
-    );
-
     public function findTagsByGroupId($groupId)
     {
         return $this->getTagGroupTagDao()->findTagsByGroupId($groupId);
-    }
-
-    protected function fieterConditions($conditions)
-    {
-        return ArrayToolkit::parts($conditions, $this->allowConditions);
     }
 
     protected function getTagGroupTagDao()
