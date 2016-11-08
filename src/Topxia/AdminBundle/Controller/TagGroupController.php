754b0f92c5f51a201a5c1b731774b97412eea324
lufangyu1217 <584141397@qq.com>
2016-11-08 17:23:34 +0800

    列表展示部分新增和编辑功能

--- src/Topxia/AdminBundle/Controller/TagGroupController.php
+++ src/Topxia/AdminBundle/Controller/TagGroupController.php
@@ -10,11 +10,12 @@ class TagGroupController extends BaseController
     public function indexAction(Request $request)
     {   
         $tagGroups = array(
+            array(
             'id'          => '1',
             'name'        => '测试标签组',
             'scope'       => '班级筛选',
             'tagNum'      => '1',
-            'createdTime' => '11年11月11日'
+            'createdTime' => '11年11月11日')
         );
 
         return $this->render('TopxiaAdminBundle:TagGroup:index.html.twig',array(
@@ -25,12 +26,19 @@ class TagGroupController extends BaseController
 
     public function createAction(Request $request)
     {
+        if ($request->getMethod() == 'POST') {
+            var_dump($request->request->all());exit();
+        }
 
+        return $this->render('TopxiaAdminBundle:TagGroup:tag-group-modal.html.twig');
     }
 
     public function updateAction(Request $request, $groupId)
     {
-
+        return $this->render('TopxiaAdminBundle:TagGroup:tag-group-modal.html.twig', array(
+            'tagGroup' => $tagGroup,
+            'groupId'  => $groupId
+        ));
     }
 
     protected function getTagService()
