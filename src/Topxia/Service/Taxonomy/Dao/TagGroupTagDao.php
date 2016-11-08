479d91a0195480d0056140895dde213c0df44bc8
lufangyu1217 <584141397@qq.com>
2016-11-08 11:12:25 +0800

    fix

--- src/Topxia/Service/Taxonomy/Dao/TagGroupTagDao.php
+++ src/Topxia/Service/Taxonomy/Dao/TagGroupTagDao.php
@@ -4,6 +4,4 @@ namespace Topxia\Service\Taxonomy\Dao;
 interface TagGroupTagDao
 {
     public function findTagsByGroupId($groupId);
-
-    public function search($conditions, $order, $start, $limit);
 }
