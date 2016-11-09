cff9a1ec383c9a8b8986489a68c5588581860756
Simon <wq@zlzkj.com>
2016-11-09 13:59:47 +0800

    add table

--- app/DoctrineMigrations/Version20161108141257.php
+++ app/DoctrineMigrations/Version20161108141257.php
@@ -18,6 +18,16 @@ class Version20161108141257 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql("ALTER TABLE `upload_files` CHANGE COLUMN `type` `type` enum('document','video','audio','image','ppt','other','flash','subtitle') NOT NULL DEFAULT 'other' COMMENT '文件类型';");
         $this->addSql("ALTER TABLE `upload_file_inits` CHANGE COLUMN `type` `type` enum('document','video','audio','image','ppt','other','flash','subtitle') NOT NULL DEFAULT 'other' COMMENT '文件类型';");
+        $this->addSql("CREATE TABLE `subtitle` (
+                `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `name` varchar(255) NOT NULL COMMENT '字幕名称',
+                `subtitleId` int(10) UNSIGNED NOT NULL COMMENT 'subtitle的uploadFileId',
+                `mediaId` int(10) UNSIGNED NOT NULL COMMENT 'video/audio的uploadFileId',
+                `ext` varchar(12) NOT NULL DEFAULT '' COMMENT '后缀',
+                `createdTime` int(10) UNSIGNED NOT NULL DEFAULT 0,
+                PRIMARY KEY (`id`)
+            ) COMMENT='字幕关联表';"
+        );
     }
 
     /**
