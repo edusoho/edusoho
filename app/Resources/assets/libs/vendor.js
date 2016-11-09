188a6eb8019c533f0df4654f8874b2bed50f6cb2
wuli <wuli@howzhi.com>
2016-11-08 17:02:50 -0800

    modal

--- app/Resources/assets/libs/vendor.js
+++ app/Resources/assets/libs/vendor.js
@@ -4,7 +4,7 @@ import 'jquery';
 import 'bootstrap';
 import 'bootstrap-notify';
 
-import 'common/bootstrap-modal-hack';
+// import 'common/bootstrap-modal-hack';
 
 
 
@@ -13,6 +13,21 @@ $(document).ajaxSend(function(a, b, c) {
     b.setRequestHeader('X-CSRF-Token', $('meta[name=csrf-token]').attr('content'));
   }
 });
+
+$('#modal').on('show.bs.modal', function (e) {
+  let url = $(e.relatedTarget).data('url');
+  let $this = $(this);
+
+  if(!$this.html()) {
+    localStorage.removeItem("modalUrl");
+  }
+  
+  if(localStorage.getItem("modalUrl") != url ) {
+    $this.empty().load(url);
+    localStorage.setItem("modalUrl",url);
+  }
+
+})
 
 // $('#modal').on('show.bs.modal', function (e) {
 //   $(this).load($(e.relatedTarget).data('url'));
