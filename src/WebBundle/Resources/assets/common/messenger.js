d4ada3eaeac09daf035171923a0c14a24763899f
Simon Lau <liuyangyang@howzhi.com>
2016-11-10 13:07:36 +0800

    messenger

---
+++ src/WebBundle/Resources/assets/common/messenger.js
@@ -0,0 +1,63 @@
+import Messenger from 'messengerjs';
+import Emitter from 'es6-event-emitter';
+
+class EsMessager extends Emitter {
+    constructor(name, project, children, type) {
+        super();
+        this.name = name | '';
+        this.project = project | '';
+        this.children = children | [];
+        this.type = type | '';
+        this.messenger = ''
+
+        this.setup();
+    }
+
+    setMessenger(messenger) {
+        this.messenger = messenger
+    }
+
+    getMessenger() {
+        return this.messenger;
+    }
+
+    setup() {
+        let self = this;
+        var messenger = new Messenger(this.name, this.project);
+        if (this.type == "child") {
+            messenger.addTarget(window.parent, 'parent');
+        } else if (this.type == "parent") {
+            var children = this.children;
+            for (var i = children.length - 1; i >= 0; i--) {
+                messenger.addTarget(children[i].contentWindow, children[i].id);
+            }
+        }
+        messenger.listen(function (msg) {
+            msg = JSON.parse(msg);
+            self.trigger(msg.eventName, msg.args);
+        })
+        this.setMessenger(messenger);
+    }
+
+    sendToParent(eventName, args) {
+        this.getMessenger().targets['parent'].send(
+            this.convertToString(eventName, args)
+        );
+    }
+
+
+    sendToChild(child, eventName, args) {
+        this.getMessenger().targets[child.id].send(
+            this.convertToString(eventName, args)
+        );
+    }
+
+    convertToString(eventName, args) {
+        var msg = {"eventName": eventName, "args": args};
+        msg = JSON.stringify(msg);
+        return msg;
+    }
+
+}
+
+module.exports = EsMessager;
--- src/WebBundle/Resources/assets/js/task/video-show.js
+++ src/WebBundle/Resources/assets/js/task/video-show.js
@@ -1,13 +1,15 @@
 /**
  * Created by Simon on 08/11/2016.
  */
-import  swfobject from 'swfobject';
-
+import  swfobject from 'es-swfobject';
+import  EsMessager from '../../common/messenger';
 class VideoPlay {
     constructor(elment) {
         this.dom = $(elment);
         this.data = this.dom.data();
+        this.player = {};
     }
+
 
     play() {
         console.log(this.data.media);
@@ -18,59 +20,63 @@ class VideoPlay {
         }
     }
 
-    _playVideo() {
-        let lessonVideoDiv = this.dom;
+    _playerSwf() {
+        console.log(this.dom, this.data);
+        this.dom.html('<div id="lesson-swf-player"></div>');
+        swfobject.embedSWF(this.data.media.mediaUri,
+            'lesson-swf-player', '100%', '100%', "9.0.0", null, null, {
+                wmode: 'opaque',
+                allowFullScreen: 'true'
+            });
+    }
 
+    _playVideo() {
         // if ((lesson.mediaConvertStatus == 'waiting') || (lesson.mediaConvertStatus == 'doing')) {
         //     Notify.warning('视频文件正在转换中，稍后完成后即可查看');
         //     return;
         // }
         let startTime = this.data.startTime | 0;
-        var playerUrl = `/course/${this.data.courseId}/task/${this.data.taskId}/player`;// '../../course/' + lesson.courseId + '/lesson/' + lesson.id + '/player';
+        let playerUrl = `/course/${this.data.courseId}/task/${this.data.taskId}/player`;// '../../course/' + lesson.courseId + '/lesson/' + lesson.id + '/player';
         if (startTime) {
             playerUrl += "?starttime=" + startTime;
         }
-        var html = `<iframe src='${playerUrl}' name='viewerIframe' id='viewerIframe' width='100%' allowfullscreen webkitallowfullscreen height='100%' style='border:0px'></iframe>`;
+        const html = `<iframe src='${playerUrl}' name='viewerIframe' id='viewerIframe' width='100%' allowfullscreen webkitallowfullscreen height='100%' style='border:0px'></iframe>`;
+        let self = this;
         this.dom.show();
         this.dom.html(html);
 
-        // var messenger = new Messenger({
-        //     name: 'parent',
-        //     project: 'PlayerProject',
-        //     children: [document.getElementById('viewerIframe')],
-        //     type: 'parent'
-        // });
-        //
-        // messenger.on("ended", function () {
-        //     var player = self.get("player");
-        //     player.playing = false;
-        //     self.set("player", player);
-        //     self._onFinishLearnLesson();
-        // });
-        //
-        // messenger.on("playing", function () {
-        //     var player = self.get("player");
-        //     player.playing = true;
-        //     self.set("player", player);
-        // });
-        //
-        // messenger.on("paused", function () {
-        //     var player = self.get("player");
-        //     player.playing = false;
-        //     self.set("player", player);
-        // });
+        var messenger = new EsMessager({
+            name: 'parent',
+            project: 'PlayerProject',
+            children: [document.getElementById('viewerIframe')],
+            type: 'parent'
+        });
 
-        // self.set("player", {});
+        messenger.on("ended", function () {
+            console.log('messenger------------', 'ended')
+            var player = self.player;
+            player.playing = false;
+            self.player = player;
+            self._onFinishLearnLesson();
+        });
+
+        messenger.on("playing", function () {
+            console.log('messenger------------', 'playing')
+            var player = self.player;
+            player.playing = true;
+            self.player = player;
+        });
+
+        messenger.on("paused", function () {
+            console.log('messenger------------', 'paused')
+            var player = self.player;
+            player.playing = false;
+            self.player = player;
+        });
     }
 
-    _playerSwf() {
-        console.log(this.dom, this.data);
-        this.dom.html('<div id="lesson-swf-player"></div>');
-        swfobject.embedSWF(this.data.ext.mediaUri,
-            'lesson-swf-player', '100%', '100%', "9.0.0", null, null, {
-                wmode: 'opaque',
-                allowFullScreen: 'true'
-            });
+    _onFinishLearnLesson() {
+        console.log('messenger------------', '_onFinishLearnLesson')
     }
 
 
