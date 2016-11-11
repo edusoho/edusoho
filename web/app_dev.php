b3969aec055d0cb908fc955db62d7e136fa87792
hamster <malianbo@163.com>
2016-11-11 14:02:15 +0800

    还原代码

--- web/app_dev.php
+++ web/app_dev.php
@@ -36,7 +36,7 @@ Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
