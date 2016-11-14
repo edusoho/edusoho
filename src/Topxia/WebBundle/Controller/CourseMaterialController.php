0aa26afef00200d90405072bea80ce1198467a72
nifeng <fengni@fengdeMacBook-Pro.local>
2016-11-14 21:55:17 +0800

    资料下载逻辑的权限

--- src/Topxia/WebBundle/Controller/CourseMaterialController.php
+++ src/Topxia/WebBundle/Controller/CourseMaterialController.php
@@ -56,7 +56,7 @@ class CourseMaterialController extends CourseBaseController
         }
 
         if ($member && $member['levelId'] > 0) {
-            if ($this->getVipService()->checkUserInMemberLevel($member['userId'], $course['vipLevelId']) != 'ok') {
+            if ($this->getVipService()->checkUserInMemberLevel($member['userId'], $member['levelId']) != 'ok') {
                 return $this->redirect($this->generateUrl('course_show', array('id' => $courseId)));
             }
         }
