34ca2b22e66a4e9bb3a7b5ec94ed9b16d1544045
ruanwenqin <ruanwenqin@howzhi.com>
2016-11-14 16:22:54 +0800

    显示批量创建课时的btn

--- src/Topxia/WebBundle/Resources/public/js/controller/course-manage/lesson.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/course-manage/lesson.js
@@ -221,6 +221,7 @@ define(function(require, exports, module) {
     $('.js-lesson-batch-btn-popover').popover({
         html: true,
         trigger: 'hover',
+        delay: { "show": 200, "hide": 1000 },
         placement: 'top',
         template: '<div class="popover tata-popover tata-popover-lg" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>',
         content: function() {
