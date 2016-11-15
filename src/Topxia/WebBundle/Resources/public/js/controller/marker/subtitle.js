3e815aa9ab12d9e953a7ba0ceeef57261c04497a
chenhaijiao <chenhaijiao@howzhi.com>
2016-11-15 17:30:08 +0800

    添加时间线滚动

--- src/Topxia/WebBundle/Resources/public/js/controller/marker/subtitle.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/marker/subtitle.js
@@ -16,6 +16,61 @@ define(function(require,exports,module){
         $textTrackDisplay.height(height - tabHeight - textTrackTitleHeight - selectorHeight - 140).show();
     })();
 
+
+    var timeBox = $('#editbox');
+    var partnum = 6;
+    $.ajax({
+        type: "get",
+        url: $('.js-marker-manage-content').data('marker-metas-url'),
+        cache: false,
+        async: false,
+        success: function (data) {
+            initMarkerArry = data.markersMeta;
+            mediaLength = data.videoTime;
+            console.log(mediaLength);
+            var parttime = mediaLength/partnum;
+            for (var i = 0; i <= partnum; i++) {
+                var $new_scale_default = $('[data-role="scale-default"]').clone().css('left', getleft(parttime * i,mediaLength)).removeClass('hidden').removeAttr('data-role');
+                $new_scale_default.find('[data-role="scale-time"]').text(convertTime(Math.round(parttime * i)));
+                $('[data-role="scale-default"]').before($new_scale_default);
+            }
+            messenger.on("timechange", function (data) {
+                $('.scale-white').css('left', getleft(data.currentTime, mediaLength));
+            });
+        }
+    });
+    function getleft (time, videoLength) {
+        var _width = $('#editbox-lesson-list').width();
+        var _totaltime = parseInt(videoLength);
+        var _left = time * _width / _totaltime;
+        return _left + 20;
+    }
+    function convertTime (num) {
+        var time = "";
+        var h = parseInt((num % 86400) / 3600);
+        var s = parseInt((num % 3600) / 60);
+        var m = num % 60;
+        if (h > 0) {
+            time += h + ':';
+        }
+        if (s.toString().length < 2) {
+            time += '0' + s + ':';
+        } else {
+            time += s + ':';
+
+        }
+        if (m.toString().length < 2) {
+            time += '0' + m;
+        } else {
+            time += m;
+        }
+        return time;
+    }
+    
+    
+    
+    
+
     //选择框组件实例
     var select = Object.create(Select);
     select.init('track-select');
