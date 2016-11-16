f6075ae85b6ccc581cc14882cb5efb52e7bb5b57
ruanwenqin <ruanwenqin@howzhi.com>
2016-11-16 10:58:00 +0800

    media play url

---
+++ src/Topxia/WebBundle/Controller/Media/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Topxia\WebBundle\Controller\Media;
+
+use Symfony\Component\HttpFoundation\Request;
+use Topxia\Common\Exception\InvalidArgumentException;
+use Topxia\Common\Exception\ResourceNotFoundException;
+use Topxia\WebBundle\Controller\BaseController;
+
+class IndexController extends BaseController
+{
+    public function playAction($mediaId)
+    {
+        if (!$this->getUploadFileService()->canManageFile($mediaId)) {
+            throw $this->createAccessDeniedException($this->trans('没有权限管理资源'));
+        }
+
+        $media   = $this->getUploadFileService()->getFile($mediaId);
+
+        return $this->forward('MaterialLibBundle:GlobalFilePlayer:player', array('globalId' => $media['globalId']));
+    }
+
+    protected function getUploadFileService()
+    {
+        return $this->createService('File.UploadFileService');
+    }
+}
