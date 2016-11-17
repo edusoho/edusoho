6e571fa12a5867ee0ac62f4ab1276b79e460d560
ruanwenqin <ruanwenqin@howzhi.com>
2016-11-17 17:09:30 +0800

    更改升级脚本

--- upgrade-scripts/upgrade-7.3.0.php
+++ upgrade-scripts/upgrade-7.3.0.php
@@ -38,32 +38,6 @@ class EduSohoUpgrade extends AbstractUpdater
     {
         $connection = $this->getConnection();
 
-        if (!$this->isFieldExist('course_member', 'finishedTime')) {
-            $connection->exec("ALTER TABLE `course_member` ADD `finishedTime` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '完成课程时间' AFTER `isLearned`");
-        }
-
-        $now = time();
-        $connection->exec("UPDATE `course_member` SET finishedTime = {$now} WHERE isLearned = 1");
-        if (!$this->isTableExist('subtitle')) {
-            $connection->exec("CREATE TABLE `subtitle` (
-                `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-                `name` varchar(255) NOT NULL COMMENT '字幕名称',
-                `subtitleId` int(10) UNSIGNED NOT NULL COMMENT 'subtitle的uploadFileId',
-                `mediaId` int(10) UNSIGNED NOT NULL COMMENT 'video/audio的uploadFileId',
-                `ext` varchar(12) NOT NULL DEFAULT '' COMMENT '后缀',
-                `createdTime` int(10) UNSIGNED NOT NULL DEFAULT 0,
-                PRIMARY KEY (`id`)
-            ) COMMENT='字幕关联表';");
-        }
-
-        if ($this->isFieldExist('upload_files', 'type')) {
-            $connection->exec("ALTER TABLE `upload_files` CHANGE COLUMN `type` `type` enum('document','video','audio','image','ppt','other','flash','subtitle') NOT NULL DEFAULT 'other' COMMENT '文件类型';");
-        }
-
-        if ($this->isFieldExist('upload_file_inits', 'type')) {
-            $connection->exec("ALTER TABLE `upload_file_inits` CHANGE COLUMN `type` `type` enum('document','video','audio','image','ppt','other','flash','subtitle') NOT NULL DEFAULT 'other' COMMENT '文件类型';");
-        }
-
         $connection->exec("update course_member cm1 LEFT JOIN classroom_member cm2 ON cm1.classroomId = cm2.classroomId and cm1.userId=cm2.userId and cm1.joinedType='classroom' set cm1.levelId=cm2.levelId where cm1.joinedType='classroom'");
 
         $setting = $this->getSettingService()->get('user_partner');
