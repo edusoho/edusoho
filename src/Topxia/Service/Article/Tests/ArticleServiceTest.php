aa9404f0ddaa55805e7b04f6501fdc235e19b745
lufangyu1217 <584141397@qq.com>
2016-11-17 19:15:43 +0800

    fix

--- src/Topxia/Service/Article/Tests/ArticleServiceTest.php
+++ src/Topxia/Service/Article/Tests/ArticleServiceTest.php
@@ -342,10 +342,10 @@ class ArticleServiceTest extends BaseTestCase
         $article3 = $this->getArticleService()->createArticle($article3);
 
         $relativeArticles = $this->getArticleService()->findRelativeArticles($article1['id'], 3);
-        $this->assertEquals(count($relativeArticles), 1);
+        $this->assertEquals(count($relativeArticles), 2);
 
         $relativeArticles = $this->getArticleService()->findRelativeArticles($article3['id'], 3);
-        $this->assertEquals(count($relativeArticles), 1);
+        $this->assertEquals(count($relativeArticles), 2);
     }
 
     protected function createArticle()
