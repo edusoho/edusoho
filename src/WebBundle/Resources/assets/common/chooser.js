1f1ed80d0b5849105de5991d053d3899463c1eac
wuli <wuli@howzhi.com>
2016-11-17 19:46:36 -0800

    upload

--- src/WebBundle/Resources/assets/common/chooser.js
+++ src/WebBundle/Resources/assets/common/chooser.js
@@ -11,12 +11,10 @@ class Chooser extends Emitter {
     _open() {
         $('.file-chooser-bar').addClass('hidden');
         $('.file-chooser-main').removeClass('hidden');
-        this.$parentiframe.height(this.$parentiframe.contents().find('body').height());
     }
     _close() {
         $('.file-chooser-main').addClass('hidden');
         $('.file-chooser-bar').removeClass('hidden');
-        this.$parentiframe.height(this.$parentiframe.contents().find('body').height());
     }
 
 }
