8a3100212d763f2dd476be5306ac07dd25de4f9b
dongjianbin <dongjianbin@howzhi.com>
2016-11-21 10:10:53 +0800

    账务中心路由

---
+++ src/Topxia/AdminBundle/Controller/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Topxia\AdminBundle\Controller;
+
+use Topxia\Common\Paginator;
+use Topxia\Common\ArrayToolkit;
+use Symfony\Component\HttpFoundation\Request;
+
+class AccountController extends BaseController
+{
+    public function IndexAction(Request $request)
+    {
+        
+    }
+}
