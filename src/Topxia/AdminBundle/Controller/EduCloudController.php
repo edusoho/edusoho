4b4beb44fa3462fd58df6aa2fb21b687ca0a15d9
zhaoyao <zhaoyao@howzhi.com>
2016-11-21 11:55:23 +0800

    为购买云直播服务页面显示

--- src/Topxia/AdminBundle/Controller/EduCloudController.php
+++ src/Topxia/AdminBundle/Controller/EduCloudController.php
@@ -7,6 +7,7 @@ use Imagine\Gd\Imagine;
 use Topxia\Common\Paginator;
 use Topxia\Common\FileToolkit;
 use Topxia\Common\ArrayToolkit;
+use Topxia\Service\Util\EdusohoLiveClient;
 use Topxia\Service\CloudPlatform\KeyApplier;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -1310,6 +1311,11 @@ class EduCloudController extends BaseController
         try {
             $api         = CloudAPIFactory::create('root');
             $overview    = $api->get("/me/live/overview");
+            if (isset($overview['isBuy']) && $overview['isBuy'] == false) {
+                return $this->render('TopxiaAdminBundle:EduCloud/Live:without-enable.html.twig', array(
+                    'overview'  => $overview
+                ));                
+            }
             foreach ($overview['items'] as $key => $value) {
                 $items['date'][] = $value['date'];
                 $items['count'][] = $value['count'];
@@ -1326,6 +1332,43 @@ class EduCloudController extends BaseController
 
     public function liveSettingAction(Request $request)
     {
+        $courseSetting     = $this->getSettingService()->get('course', array());
+        $liveCourseSetting = $this->getSettingService()->get('live-course', array());
+        // var_dump($courseSetting);
+        // var_dump($liveCourseSetting);
+        $client            = new EdusohoLiveClient();
+        $capacity          = $client->getCapacity();
+        // var_dump($capacity);
+        $default = array(
+            'live_course_enabled' => '0'
+        );
+
+        $this->getSettingService()->set('course', $courseSetting);
+        $this->getSettingService()->set('live-course', $liveCourseSetting);
+        $setting = array_merge($default, $liveCourseSetting);
+
+        if ($request->getMethod() == 'POST') {
+            $liveCourseSetting                          = $request->request->all();
+            $liveCourseSetting['live_student_capacity'] = empty($capacity['capacity']) ? 0 : $capacity['capacity'];
+            $setting                                    = array_merge($courseSetting, $liveCourseSetting);
+            $this->getSettingService()->set('live-course', $liveCourseSetting);
+            $this->getSettingService()->set('course', $setting);
+
+            $hiddenMenus = $this->getSettingService()->get('menu_hiddens', array());
+            // var_dump($hiddenMenus);exit();
+            if ($liveCourseSetting['live_course_enabled']) {
+                unset($hiddenMenus['admin_live_course_add']);
+                unset($hiddenMenus['admin_live_course']);
+            } else {
+                $hiddenMenus['admin_live_course_add'] = true;
+                $hiddenMenus['admin_live_course']     = true;
+            }
+
+            $this->getSettingService()->set('menu_hiddens', $hiddenMenus);
+
+            $this->getLogService()->info('system', 'update_settings', '更新课程设置', $setting);
+        }
+
         return $this->render('TopxiaAdminBundle:EduCloud/Live:setting.html.twig');
     }
     // 添加重建索引模态框
