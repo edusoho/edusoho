eaae0899cf5717adce8b094f0c4d76310ff2f41c
hamster <malianbo@163.com>
2016-11-22 16:39:27 +0800

    fix course & courseset

--- migrations/20161121114926_c2_course.php
+++ migrations/20161121114926_c2_course.php
@@ -12,32 +12,28 @@ class C2Course extends Migration
         $biz = $this->getContainer();
         $db  = $biz['db'];
         $db->exec("CREATE TABLE `c2_course` (
-            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-            `courseSetId` int(11) NOT NULL,
-            `copyCourseId` int(11) DEFAULT NULL,
-            `title` varchar(1024) DEFAULT NULL,
-            `learnMode` varchar(32) DEFAULT NULL COMMENT 'byOrder, freeOrder',
-            `expiryMode` varchar(32) DEFAULT NULL,
-            `expiryDays` int(11) DEFAULT NULL,
-            `expiryStartDate` int(11) DEFAULT NULL,
-            `expiryEndDate` int(11) DEFAULT NULL,
-            `summary` text,
-            `goals` text,
-            `audiences` text,
-            `isDefault` tinyint(1) DEFAULT '0',
-            `maxStudentNum` int(11) DEFAULT '0',
-            `smallPicture` varchar(255) DEFAULT NULL,
-            `middlePicture` varchar(255) DEFAULT NULL,
-            `largePicture` varchar(255) DEFAULT NULL,
-            `status` varchar(32) DEFAULT NULL COMMENT 'draft, published, closed',
-            `auditStatus` varchar(32) DEFAULT NULL COMMENT 'draft, committed, rejected, accepted',
-            `auditRemark` text,
-            `creator` int(11) DEFAULT NULL,
-            `created` int(11) DEFAULT NULL,
-            `updated` int(11) DEFAULT NULL,
-            `deleted` int(11) NOT NULL DEFAULT '0',
-            PRIMARY KEY (`id`)
-          ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `courseSetId` int(11) NOT NULL,
+              `title` varchar(1024) DEFAULT NULL,
+              `learnMode` varchar(32) DEFAULT NULL COMMENT 'byOrder, freeOrder',
+              `expiryMode` varchar(32) DEFAULT NULL,
+              `expiryDays` int(11) DEFAULT NULL,
+              `expiryStartDate` int(11) DEFAULT NULL,
+              `expiryEndDate` int(11) DEFAULT NULL,
+              `summary` text,
+              `goals` text,
+              `audiences` text,
+              `isDefault` tinyint(1) DEFAULT '0',
+              `maxStudentNum` int(11) DEFAULT '0',
+              `status` varchar(32) DEFAULT NULL COMMENT 'draft, published, closed',
+              `auditStatus` varchar(32) DEFAULT NULL COMMENT 'draft, committed, rejected, accepted',
+              `auditRemark` text,
+              `creator` int(11) DEFAULT NULL,
+              `created` int(11) DEFAULT NULL,
+              `updated` int(11) DEFAULT NULL,
+              `deleted` int(11) NOT NULL DEFAULT '0',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
         ");
     }
 
--- migrations/20161121114931_c2_course_set.php
+++ migrations/20161121114931_c2_course_set.php
@@ -11,21 +11,23 @@ class C2CourseSet extends Migration
     {
         $biz = $this->getContainer();
         $db  = $biz['db'];
-        $db->exec("
-            CREATE TABLE `c2_course_set` (
-              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-              `type` varchar(32) NOT NULL DEFAULT '',
-              `title` varchar(1024) DEFAULT '',
-              `subtitle` varchar(1024) DEFAULT '',
-              `tags` text,
-              `categories` text,
-              `status` varchar(32) DEFAULT '0' COMMENT 'draft, published, closed',
-              `creator` int(11) DEFAULT '0',
-              `created` int(11) DEFAULT '0',
-              `updated` int(11) DEFAULT '0',
-              `deleted` int(11) NOT NULL DEFAULT '0',
-              PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        $db->exec("CREATE TABLE `c2_course_set` (
+            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+            `type` varchar(32) NOT NULL DEFAULT '',
+            `title` varchar(1024) DEFAULT '',
+            `subtitle` varchar(1024) DEFAULT '',
+            `tags` text,
+            `categories` text,
+            `smallPicture` varchar(255) DEFAULT NULL,
+            `middlePicture` varchar(255) DEFAULT NULL,
+            `largePicture` varchar(255) DEFAULT NULL,
+            `status` varchar(32) DEFAULT '0' COMMENT 'draft, published, closed',
+            `creator` int(11) DEFAULT '0',
+            `created` int(11) DEFAULT '0',
+            `updated` int(11) DEFAULT '0',
+            `deleted` int(11) NOT NULL DEFAULT '0',
+            PRIMARY KEY (`id`)
+          ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
         ");
     }
 
