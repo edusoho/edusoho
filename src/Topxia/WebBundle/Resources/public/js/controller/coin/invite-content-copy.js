be4c82e8f4f1b6cda071ff9f1dc8626f93861602
ruanwenqin <ruanwenqin@howzhi.com>
2016-11-24 19:44:25 +0800

    退滚

---
+++ src/Topxia/WebBundle/Resources/public/js/controller/coin/invite-content-copy.js
@@ -0,0 +1,10 @@
+define(function(require, exports, module) {
+    var Notify = require('common/bootstrap-notify');
+    exports.run = function (){
+        $('#copy').click(function(){
+            $("#content").select();
+            document.execCommand("Copy"); 
+            Notify.success(Translator.trans('链接复制成功'));
+        });
+    }
+});
