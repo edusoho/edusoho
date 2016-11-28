3b73e39b211a36c300cffa5a3092bd1840bd5ee6
hamster <malianbo@163.com>
2016-11-28 19:54:10 +0800

    fxi

--- src/WebBundle/Controller/CourseManageController.php
+++ src/WebBundle/Controller/CourseManageController.php
@@ -22,8 +22,8 @@ class CourseManageController extends BaseController
 
     public function copyAction(Request $request, $courseSetId, $courseId)
     {
-        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
         $course    = $this->getCourseService()->tryManageCourse($courseId, $courseSetId);
+        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
         return $this->render('WebBundle:CourseManage:create-modal.html.twig', array(
             'courseSet' => $courseSet,
             'course'    => $course
@@ -42,8 +42,8 @@ class CourseManageController extends BaseController
 
     public function tasksAction(Request $request, $courseSetId, $courseId)
     {
-        $courseSet   = $this->getCourseSetService()->getCourseSet($courseSetId);
         $course      = $this->getCourseService()->tryManageCourse($courseId, $courseSetId);
+        $courseSet   = $this->getCourseSetService()->getCourseSet($courseSetId);
         $tasks       = $this->getTaskService()->findUserTasksFetchActivityAndResultByCourseId($courseId);
         $courseItems = $this->getCourseService()->getCourseItems($courseId);
 
@@ -61,8 +61,8 @@ class CourseManageController extends BaseController
             $data = $request->request->all();
             $this->getCourseService()->updateCourse($data['id'], $data);
         }
-        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
         $course    = $this->getCourseService()->tryManageCourse($courseId, $courseSetId);
+        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
         return $this->render('WebBundle:CourseManage:info.html.twig', array(
             'courseSet' => $courseSet,
             'course'    => $this->formatCourseDate($course)
@@ -71,8 +71,8 @@ class CourseManageController extends BaseController
 
     public function marketingAction(Request $request, $courseSetId, $courseId)
     {
-        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
         $course    = $this->getCourseService()->tryManageCourse($courseId, $courseSetId);
+        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
         return $this->render('WebBundle:CourseManage:marketing.html.twig', array(
             'courseSet' => $courseSet,
             'course'    => $course
