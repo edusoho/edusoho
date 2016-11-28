edca2c489caf4c8b0afac12e6c4ed2051b368244
retamia <retamia@gmail.com>
2016-11-28 14:30:38 +0800

    fix trans code

--- src/WebBundle/Resources/assets/js/file-chooser/file-choose.js
+++ src/WebBundle/Resources/assets/js/file-chooser/file-choose.js
@@ -31,7 +31,7 @@ class FileChooser extends Emitter {
     const materialLibChoose = new MaterialLibChoose($('#chooser-material-panel'));
     const courseFileChoose = new CourseFileChoose($('#chooser-course-panel'));
     const videoImport = new VideoImport($('#import-video-panel'));
-    const uploader = new UploadChooser();
+    const uploader = new UploadChooser($('#chooser-upload-panel'));
     materialLibChoose.on('select', this.fileSelect.bind(this));
     courseFileChoose.on('select', this.fileSelect.bind(this));
     videoImport.on('file.select', this.fileSelect.bind(this));
