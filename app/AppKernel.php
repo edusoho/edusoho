c4d402cd3178df8ee336ee4c31e5af7e65e7d315
retamia <retamia@gmail.com>
2016-11-29 10:27:02 +0800

    fix exception

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ use Topxia\Service\User\CurrentUser;
 use Symfony\Component\HttpFoundation\Request;
 use Codeages\PluginBundle\System\PluginConfigurationManager;
 use Codeages\PluginBundle\System\PluginableHttpKernelInterface;
+use Codeages\Biz\Framework\Provider\DoctrineServiceProvider;
 
 class AppKernel extends Kernel implements PluginableHttpKernelInterface
 {
@@ -155,7 +156,7 @@ class AppKernel extends Kernel implements PluginableHttpKernelInterface
     {
         $biz = $this->getContainer()->get('biz');
         $biz['migration.directories'][] = dirname(__DIR__) . '/migrations';
-        $biz->register(new \Codeages\Biz\Framework\Provider\DoctrineServiceProvider());
+        $biz->register(new DoctrineServiceProvider());
         $biz->boot();
     }
 
