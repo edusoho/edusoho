3ca2eb8fdb5dee2ae9557d756d1cfef1e67fda87
lufangyu1217 <584141397@qq.com>
2016-12-05 10:02:32 +0800

    fix

--- api/src/Topxia/Api/Resource/ImgCodes.php
+++ api/src/Topxia/Api/Resource/ImgCodes.php
@@ -11,6 +11,27 @@ class ImgCodes extends BaseResource
 {
     public function post(Application $app, Request $request)
     {
+        $mobile = $request->request->get('mobile');
+
+        if (empty($mobile)) {
+            return $this->error('500', '手机号为空');
+        }
+
+        $user = $this->getUserService()->getUserByVerifiedMobile($mobile);
+        if (!$user) {
+            return $this->error('500', '该手机号未绑定用户');
+        }
+
+        $imgToken = $this->getTokenService()->makeToken('sms_registration', array(
+            'times'    => 5,
+            'duration' => 60 * 2,
+            'userId'   => $user['id'],
+            'data'     => array(
+                'img_code' => $result['captcha_code'],
+                'mobile'   => $mobile
+            )
+        ));
+
         $imgBuilder = new CaptchaBuilder;
         $imgBuilder->build($width = 150, $height = 32, $font = null);
 
@@ -23,13 +44,16 @@ class ImgCodes extends BaseResource
             'Content-Disposition' => 'inline; filename="'."img_captcha.jpg".'"',
         );
 
-
-        $response = new Response($str, 200, $headers);
-        $response->send();
+        return array('' => $str, '' => $token);
     }
 
     public function filter($res)
     {
         return $res;
+    }
+
+    protected function getUserService()
+    {
+        return $this->getServiceKernel()->createService('User.UserService');
     }
 }
--- api/src/Topxia/Api/Resource/SmsCodes.php
+++ api/src/Topxia/Api/Resource/SmsCodes.php
@@ -6,6 +6,7 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Gregwar\Captcha\CaptchaBuilder;
 use Symfony\Component\HttpFoundation\Response;
+use Topxia\Api\Util\ImgCodeUtil;
 
 class SmsCodes extends BaseResource
 {
@@ -13,40 +14,45 @@ class SmsCodes extends BaseResource
     {
         $biz = $this->getServiceKernel()->getBiz();
         $factory = $biz['ratelimiter.factory'];
-        $limiter = $factory('ip', 10, 600);
+        $limiter = $factory('ip', 10, 86400);
 
         $remain = $limiter->check($request->getClientIp());
         if ($remain == 0) {
-            // $imgBuilder = new CaptchaBuilder;
-            // $imgBuilder->build($width = 150, $height = 32, $font = null);
-
-            // ob_start();
-            // $imgBuilder->output();
-            // $str = ob_get_clean();
-            // $imgBuilder = null;
-            // $headers = array(
-            //     'Content-type'        => 'image/jpeg',
-            //     'Content-Disposition' => 'inline; filename="'."img_captcha.jpg".'"',
-            // );
-
-
-            // $response = new Response($str, 200, $headers);
-            // $response->send();
-            $imgCaptcha = $request->request->get('img_captcha_code');
+            $imgCode  = $request->request->get('img_code');
+            $imgToken = $request->request->get('img_token');
 
             if (empty($imgCaptcha)) {
                 return $this->error('500', '图形验证码为空');
+            }
+
+            if (empty($imgToken)) {
+                return $this->error('500', 'Token为空');
+            }            
+
+            $imgCodeUtil = new ImgCodeUtil();
+            try {
+                $smsUtil->verifyImgCode('img_verify', $imgCode, $imgToken);
+            } catch(Expection $e) {
+                return array('500', $e->getMessage());
             }
         }
 
         $type   = $request->request->get('type');
         $mobile = $request->request->get('mobile');
 
-        if (!in_array($type, array('sms_change_password', 'sms_verify_mobile'))) {
+        if (!in_array($type, array('sms_change_password', 'sms_verify_mobile', 'sms_registration'))) {
             return $this->error('500', '短信服务不支持该业务');
         }
         if (empty($mobile)) {
             return $this->error('500', '手机号为空');
+        }
+
+        if ($type == 'sms_registration') {
+            try {
+                $result = $this->getSmsService()->sendVerifySms('sms_registration', $mobile);
+            } catch(Exception $e) {
+                return $this->error('500', $e->getMessage());
+            }
         }
 
         if ($type == 'sms_change_password') {
@@ -64,7 +70,7 @@ class SmsCodes extends BaseResource
                 return $this->error('500', $e->getMessage());
             }
         }
-            
+        
         $user = $this->getCurrentUser();
         $smsToken = $this->getTokenService()->makeToken($type, array(
             'times'    => 5,
