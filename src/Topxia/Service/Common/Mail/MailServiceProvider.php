a5e020e16e63d4891c888f2a51f32ced03e509b8
yangshuai <yangshuai@howzhi.com>
2016-12-06 16:45:38 +0800

    base

--- src/Topxia/Service/Common/Mail/MailServiceProvider.php
+++ src/Topxia/Service/Common/Mail/MailServiceProvider.php
@@ -2,14 +2,26 @@
 
 namespace Topxia\Service\Common\Mail;
 
+use Pimple\ServiceProviderInterface;
+use Topxia\Service\Common\Mail\MailFactory;
+
+
 /**
 * 
 */
-class MailServiceProvider impl  ServiceProvider
+class MailServiceProvider implements  ServiceProviderInterface
 {
-	public function register($biz){
-		$biz['mail.factory'] = $biz->factory({
+	// public function register($biz){
+	// 	$biz['mail.factory'] = $biz->factory({
+	// 		return new 
+	// 	}
+	// }
+
+	public function register(Container $app)
+    {
+		$app['mail.factory'] = function ($app) {
 			return new 
-		}
-	}
+		};
+
+    }
 }
