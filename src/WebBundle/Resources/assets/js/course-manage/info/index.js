f64a9751a29cb3d5173a653e21ebe7b5e1d78a5a
wuli <wuli@howzhi.com>
2016-12-08 17:48:50 -0800

    react

--- src/WebBundle/Resources/assets/js/course-manage/info/index.js
+++ src/WebBundle/Resources/assets/js/course-manage/info/index.js
@@ -5,82 +5,21 @@ import MultiGroup from '../../../common/widget/multi-group';
 import sortList from 'common/sortable';
 
 
-import Select, { Option } from 'rc-select';
-
-//测试数据
-const Search = React.createClass({
-  getInitialState() {
-    return {
-      disabled: false,
-      data: [],
-      value: undefined,
-    };
-  },
-
-  onChange(value) {
-    console.log('select ', value);
-    this.setState({
-      value,
-    });
-  },
-
-  fetchData(value) {
-    if (value) {
-      fetch(value, (data) => {
-        this.setState({
-          data,
-        });
-      });
-    } else {
-      this.setState({
-        data: [],
-      });
+let items = {
+  "success": 1,
+  "result": {
+       "token": "12312312312",
+       "userId": 123
     }
-  },
-
-  toggleDisabled() {
-    this.setState({
-      disabled: !this.state.disabled,
-    });
-  },
-
-  render() {
-    const data = this.state.data;
-    let options;
-    options = data.map((d) => {
-      return <Option key={d.value}><i>{d.text}</i></Option>;
-    });
-    return (<div>
-      <h2>force suggest</h2>
-      <p>
-        <button onClick={this.toggleDisabled}>toggle disabled</button>
-      </p>
-      <div>
-        <Select
-          labelInValue
-          onSearch={this.fetchData}
-          disabled={this.state.disabled}
-          value={this.state.value}
-          optionLabelProp="children"
-          placeholder="placeholder"
-          defaultActiveFirstOption
-          style={{ width: 500 }}
-          onChange={this.onChange}
-          filterOption={false}
-        >
-          {options}
-        </Select>
-      </div>
-    </div>);
-  },
-});
-// ReactDOM.render(<Search />, document.getElementById('test'));
+}
 
 function renderMultiGroupComponent(elementId){
   ReactDOM.render( <MultiGroup items = {$("#"+elementId).data("init-value")} fieldName={$("#"+elementId).data('field-name')} sortable={true}/>,
     document.getElementById(elementId)
   );
 }
+
+console.log($("#course-goals").data("init-value"));
 
 renderMultiGroupComponent('course-goals');
 renderMultiGroupComponent('intended-students', $('#intended-students').data('init-value'));
