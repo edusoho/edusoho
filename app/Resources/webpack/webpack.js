2d1e76ce1505980f2028c77f7bb16e49c2da2189
zhongjie <zhongjie@howzhi.com>
2016-12-14 10:42:28 +0800

    调整webpack配置文件

--- app/Resources/webpack/webpack.js
+++ app/Resources/webpack/webpack.js
@@ -1,5 +1,8 @@
 import esWebpackEngine from 'es-webpack-engine/dist/build';
 
-import options from './options';
+import config from './webpack.config';
+import settings from './settings';
+
+const options = Object.assign({}, config, settings);
 
 export default esWebpackEngine(options);
