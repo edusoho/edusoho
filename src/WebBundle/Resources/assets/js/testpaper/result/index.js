19aa42964444dac4731e00cabadaa0c427da2c20
wangdanyue <wangdanyue@howzhi.com>
2016-12-14 09:48:14 +0800

    testpaper-check-list

---
+++ src/WebBundle/Resources/assets/js/testpaper/result/index.js
@@ -0,0 +1,10 @@
+import DoTestBase from '../../../common/testpaper/do-test-base';
+
+
+class ShowResult extends DoTestBase {
+  constructor($container) {
+    super($container);
+  }
+}
+
+new ShowResult($('.testpaper-activity-show'));
