091460c9d5623a422b44c7b9a08e3953085260c9
retamia <retamia@gmail.com>
2016-12-14 16:41:39 +0800

    重新回滚 教育云改版 commit hash

--- src/Topxia/AdminBundle/Resources/public/js/controller/setting/cloud.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/setting/cloud.js
@@ -1,4 +1,5 @@
 define(function(require, exports, module) {
+    require('echarts-debug');
 
     var Notify = require('common/bootstrap-notify');
     var Uploader = require('upload');
@@ -7,6 +8,8 @@ define(function(require, exports, module) {
     require('common/validator-rules').inject(Validator);
 
     exports.run = function() {
+
+        $("[data-toggle='popover']").popover();
 
         var $form = $("#cloud-setting-form");
         var uploader = new Uploader({
@@ -57,7 +60,6 @@ define(function(require, exports, module) {
                 Notify.success(Translator.trans('上传云视频内嵌水印成功！'));
             }
         });
-
         $("#cloud-video-embed-watermark-remove").on('click', function(){
             if (!confirm(Translator.trans('确认要删除云视频水印吗？'))) return false;
             var $btn = $(this);
@@ -152,7 +154,8 @@ define(function(require, exports, module) {
                 validator.removeItem('[name="video_fingerprint_time"]');
             }
         })
-        
+
+
     }
 
 })
