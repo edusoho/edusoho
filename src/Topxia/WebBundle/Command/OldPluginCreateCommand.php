1c56e269920b9de55e6faaed0ed070d73668d661
Simon Lau <liuyangyang@howzhi.com>
2016-12-18 14:08:05 +0800

    fix symfony 2.8 generate bundle error

--- src/Topxia/WebBundle/Command/OldPluginCreateCommand.php
+++ src/Topxia/WebBundle/Command/OldPluginCreateCommand.php
@@ -3,6 +3,7 @@ namespace Topxia\WebBundle\Command;
 
 use Sensio\Bundle\GeneratorBundle\Command\GeneratorCommand;
 use Sensio\Bundle\GeneratorBundle\Generator\BundleGenerator;
+use Sensio\Bundle\GeneratorBundle\Model\Bundle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,13 +20,36 @@ class OldPluginCreateCommand extends GeneratorCommand
                 '插件名称?'
             )
             ->setName('old-plugin:create')
-            ->setDescription('创建插件模板')
-        ;
+            ->setDescription('创建插件模板');
     }
 
+
+    /**
+     * @param $name
+     * @return Bundle
+     */
+    protected function createBundleObject($name)
+    {
+        $bundle    = $name."Bundle";
+        $namespace = $name;
+        $namespace = $namespace."\\".$name."Bundle";
+
+
+        $dir    = $this->getContainer()->getParameter('kernel.root_dir')."/..";
+        $dir    = $dir."/plugins";
+        $format = "yml";
+        return new Bundle(
+            $namespace,
+            $bundle,
+            $dir,
+            $format,
+            $shared = false
+        );
+    }
+
+
     protected function execute(InputInterface $input, OutputInterface $output)
-    {   
-        $dialog = $this->getDialogHelper();
+    {
         $name = $input->getArgument('bundlename');
 
         if (!$name) {
@@ -35,31 +59,24 @@ class OldPluginCreateCommand extends GeneratorCommand
         if (!preg_match('/^[a-zA-Z\s]+$/', $name)) {
             throw new \RuntimeException("插件名称只能为英文！");
         }
-        $name=ucfirst($name);
-
-        $bundle=$name."Bundle";
-        $namespace=$name;
-        $pluginName=$namespace;
-        $namespace=$namespace."\\".$name."Bundle";
-       
-        
-        $dir=$this->getContainer()->getParameter('kernel.root_dir')."/..";
-        $dir=$dir."/plugins";
-        $format="yml";
-        $structure="yes";
+        $name      = ucfirst($name);
         $generator = $this->getGenerator();
-        $generator->generate($namespace, $bundle, $dir, $format, $structure);
+
+        $bundleObject = $this->createBundleObject($name);
+        $generator->generateBundle($bundleObject);
 
         $output->writeln('创建插件包: <info>OK</info>');
 
         $errors = array();
-        $runner = $dialog->getRunner($output, $errors);
+        $this->getQuestionHelper()->getRunner($output, $errors);
+        $pluginName = $name;
+        $dir        = $bundleObject->getTargetDirectory();
 
         //write jspn
-        $filename=$dir."/".$pluginName."/plugin.json";
-        
-        $data = 
-        '{
+        $filename = $dir."/plugin.json";
+
+        $data
+            = '{
             "code": "'.$pluginName.'",
             "name": "'.$pluginName.'",
             "description": "",
@@ -68,41 +85,41 @@ class OldPluginCreateCommand extends GeneratorCommand
             "support_version": "1.0.0"
         }';
 
-        file_put_contents ($filename, $data);
+        file_put_contents($filename, $data);
 
         //mkdir script
         $this->filesystem = new Filesystem();
 
-        $this->filesystem ->mkdir($dir."/".$pluginName."/Scripts");
-        $this->filesystem ->mkdir($dir."/".$pluginName."/Service");
+        $this->filesystem->mkdir($dir."/Scripts");
+        $this->filesystem->mkdir($dir."/Service");
 
-        $this->filesystem ->mkdir($dir."/".$pluginName."/Service/".$pluginName."");
+        $this->filesystem->mkdir($dir."/Service/".$pluginName."");
 
-        $this->filesystem ->mkdir($dir."/".$pluginName."/Service/".$pluginName."/Impl");
-        $this->filesystem ->mkdir($dir."/".$pluginName."/Service/".$pluginName."/Dao");
-        $this->filesystem ->mkdir($dir."/".$pluginName."/Service/".$pluginName."/Dao/Impl");
+        $this->filesystem->mkdir($dir."/Service/".$pluginName."/Impl");
+        $this->filesystem->mkdir($dir."/Service/".$pluginName."/Dao");
+        $this->filesystem->mkdir($dir."/Service/".$pluginName."/Dao/Impl");
 
-        $data=$this->getBaseInstallScript();
-        file_put_contents ($dir."/".$pluginName."/Scripts/BaseInstallScript.php", $data);
+        $data = $this->getBaseInstallScript();
+        file_put_contents($dir."/Scripts/BaseInstallScript.php", $data);
 
-        $data=$this->getInstallScript();
-        file_put_contents ($dir."/".$pluginName."/Scripts/InstallScript.php", $data);
+        $data = $this->getInstallScript();
+        file_put_contents($dir."/Scripts/InstallScript.php", $data);
 
-        $data=$this->getService($pluginName);
-        file_put_contents ($dir."/".$pluginName."/Service/".$pluginName."/".$pluginName."Service.php", $data);
+        $data = $this->getService($pluginName);
+        file_put_contents($dir."/Service/".$pluginName."/".$pluginName."Service.php", $data);
 
-        $data=$this->getServiceImpl($pluginName);
-        file_put_contents ($dir."/".$pluginName."/Service/".$pluginName."/Impl/".$pluginName."ServiceImpl.php", $data);
+        $data = $this->getServiceImpl($pluginName);
+        file_put_contents($dir."/Service/".$pluginName."/Impl/".$pluginName."ServiceImpl.php", $data);
 
-        $data=$this->getDao($pluginName);
-        file_put_contents ($dir."/".$pluginName."/Service/".$pluginName."/Dao/".$pluginName."Dao.php", $data);
+        $data = $this->getDao($pluginName);
+        file_put_contents($dir."/Service/".$pluginName."/Dao/".$pluginName."Dao.php", $data);
 
-        $data=$this->getDaoImpl($pluginName);
-        file_put_contents ($dir."/".$pluginName."/Service/".$pluginName."/Dao/Impl/".$pluginName."DaoImpl.php", $data);
-        $dialog->writeGeneratorSummary($output, $errors);
+        $data = $this->getDaoImpl($pluginName);
+        file_put_contents($dir."/Service/".$pluginName."/Dao/Impl/".$pluginName."DaoImpl.php", $data);
+        $this->getQuestionHelper()->writeGeneratorSummary($output, $errors);
     }
 
-    private function getData($data,$pluginName)
+    private function getData($data, $pluginName)
     {
         return str_replace("{{name}}", $pluginName, $data);
     }
@@ -112,45 +129,46 @@ class OldPluginCreateCommand extends GeneratorCommand
         return new BundleGenerator($this->getContainer()->get('filesystem'));
     }
 
-    public function getDaoImpl($pluginName)
-    {   
-        $data=file_get_contents(__DIR__."/plugins-tpl/DaoImpl.twig");
 
-        return $this->getData($data,$pluginName);
+    public function getDaoImpl($pluginName)
+    {
+        $data = file_get_contents(__DIR__."/plugins-tpl/DaoImpl.twig");
+
+        return $this->getData($data, $pluginName);
     }
 
     public function getDao($pluginName)
     {
-        $data=file_get_contents(__DIR__."/plugins-tpl/Dao.twig");
+        $data = file_get_contents(__DIR__."/plugins-tpl/Dao.twig");
 
-        return $this->getData($data,$pluginName);
-    }   
+        return $this->getData($data, $pluginName);
+    }
 
     public function getServiceImpl($pluginName)
     {
-        $data=file_get_contents(__DIR__."/plugins-tpl/ServiceImpl.twig");
+        $data = file_get_contents(__DIR__."/plugins-tpl/ServiceImpl.twig");
 
-        return $this->getData($data,$pluginName);
+        return $this->getData($data, $pluginName);
     }
 
     public function getService($pluginName)
     {
-        $data=file_get_contents(__DIR__."/plugins-tpl/Service.twig");
+        $data = file_get_contents(__DIR__."/plugins-tpl/Service.twig");
 
-        return $this->getData($data,$pluginName);
+        return $this->getData($data, $pluginName);
     }
 
     private function getInstallScript()
     {
-        $data=file_get_contents(__DIR__."/plugins-tpl/InstallScript.twig");
+        $data = file_get_contents(__DIR__."/plugins-tpl/InstallScript.twig");
 
-        return $this->getData($data,"");
+        return $this->getData($data, "");
     }
 
     private function getBaseInstallScript()
     {
-        $data=file_get_contents(__DIR__."/plugins-tpl/BaseInstallScript.twig");
+        $data = file_get_contents(__DIR__."/plugins-tpl/BaseInstallScript.twig");
 
-        return $this->getData($data,"");
+        return $this->getData($data, "");
     }
 }
