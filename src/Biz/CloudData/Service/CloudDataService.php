d6cd7a5084379c205b3dcb8b7ce1ddd7d4e234f4
retamia <retamia@gmail.com>
2016-12-19 18:03:37 +0800

    migrate cloud data

--- src/Biz/CloudData/Service/CloudDataService.php
+++ src/Biz/CloudData/Service/CloudDataService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Topxia\Service\CloudData;
+namespace Biz\CloudData\Service;
 
 interface CloudDataService
 {
--- src/Biz/CloudData/Service/Impl/CloudDataServiceImpl.php
+++ src/Biz/CloudData/Service/Impl/CloudDataServiceImpl.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace Topxia\Service\CloudData\Impl;
+namespace Biz\CloudData\Service\Impl;
 
-use Topxia\Service\Common\BaseService;
-use Topxia\Service\CloudData\CloudDataService;
+
+
+use Biz\BaseService;
+use Biz\CloudData\Dao\CloudDataDao;
+use Biz\CloudData\Service\CloudDataService;
+use Biz\System\Service\LogService;
 use Topxia\Service\CloudPlatform\CloudAPIFactory;
-use Topxia\Service\Common\ServiceKernel;
 
 class CloudDataServiceImpl extends BaseService implements CloudDataService
 {
-
     public function push($name, array $body = array(), $timestamp = 0, $level = 'normal')
     {
         try {
@@ -25,7 +27,7 @@ class CloudDataServiceImpl extends BaseService implements CloudDataService
                     'timestamp'     => $timestamp,
                     'createdUserId' => $user['id']
                 );
-                $this->getCloudDataDao()->add($fields);
+                $this->getCloudDataDao()->create($fields);
             }
             return false;
         }
@@ -33,26 +35,32 @@ class CloudDataServiceImpl extends BaseService implements CloudDataService
 
     public function searchCloudDataCount($conditions)
     {
-        return $this->getCloudDataDao()->searchCloudDataCount($conditions);
+        return $this->getCloudDataDao()->count($conditions);
     }
 
     public function searchCloudDatas($conditions, $orderBy, $start, $limit)
     {
-        return $this->getCloudDataDao()->searchCloudDatas($conditions, $orderBy, $start, $limit);
+        return $this->getCloudDataDao()->search($conditions, $orderBy, $start, $limit);
     }
 
     public function deleteCloudData($id)
     {
-        return $this->getCloudDataDao()->deleteCloudData($id);
+        return $this->getCloudDataDao()->delete($id);
     }
 
+    /**
+     * @return CloudDataDao
+     */
     protected function getCloudDataDao()
     {
-        return $this->createDao('CloudData.CloudDataDao');
+        return $this->createDao('CloudData:CloudDataDao');
     }
 
+    /**
+     * @return LogService
+     */
     protected function getLogService()
     {
-        return ServiceKernel::instance()->getBiz()->service('System:LogService');
+        return $this->createService('System:LogService');
     }
 }
