94a433067713d66045c4d595c8497c0d96da4d20
xingjingang <xingjingang@howzhi.com>
2016-12-19 12:01:44 +0800

    edit

--- src/Topxia/AdminBundle/Controller/EduCloudController.php
+++ src/Topxia/AdminBundle/Controller/EduCloudController.php
@@ -481,7 +481,7 @@ class EduCloudController extends BaseController
                 $smsSignUrl = $this->generateUrl('admin_cloud_sms_sign');
                 $this->setFlashMessage('danger', $this->getServiceKernel()->trans("尚未设置短信签名,不能发送短信, <a href='{$smsSignUrl}' class='plm' target='_blank'>去设置</a>"));
             }
-            if (empty($smsInfo['name']) && $smsInfo['usedSmsSign']['status'] == 'checking') {
+            if (empty($smsInfo['name']) && !empty($smsInfo['usedSmsSign']['status']) == 'checking') {
                 $this->setFlashMessage('danger', $this->getServiceKernel()->trans("短信签名正在审核中,不能发送短信。"));
             }
         }
