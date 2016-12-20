8e94eb794a2cf04ca4247fc499c6baed2b489325
nifeng <fengni@fengdeMacBook-Pro.local>
2016-12-20 13:07:09 +0800

    rename role module

---
+++ src/Biz/Role/Dao/Impl/RoleDaoImpl.php
@@ -0,0 +1,47 @@
+<?php
+namespace Biz\Role\Dao\Impl;
+
+use Codeages\Biz\Framework\Dao\GeneralDaoImpl;
+use Biz\Role\Dao\RoleDao;
+
+class RoleDaoImpl extends GeneralDaoImpl implements RoleDao
+{
+    protected $table         = 'role';
+
+    public function declares()
+    {
+
+        $declares['conditions'] = array(
+            'name = :name',
+            'code = :code',
+            'name like :nameLike',
+            'createdUserId = :createdUserId',
+        );
+
+        $declares['serializes'] = array(
+            'data' => 'json',
+        );
+
+        $declares['orderbys'] = array(
+            'createdTime',
+            'updatedTime'
+        );
+
+        return $declares;
+    }
+
+    public function getByCode($code)
+    {
+        return $this->getByFields(array('code' => $code));
+    }
+
+    public function findByCodes($codes)
+    {
+        return $this->findInField('code', $codes);
+    }
+
+    public function getByName($name)
+    {
+        return $this->getByFields(array('name' => $name));
+    }
+}
