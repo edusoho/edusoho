4982cb3111e79cedcba0446b8f3dbafffceef91f
retamia <retamia@gmail.com>
2016-12-21 15:59:01 +0800

    fix role testcase

--- src/Biz/Role/Service/Impl/RoleServiceImpl.php
+++ src/Biz/Role/Service/Impl/RoleServiceImpl.php
@@ -101,17 +101,7 @@ class RoleServiceImpl extends BaseService implements RoleService
         $tree = Tree::buildWithArray($permissions, null, 'code', 'parent');
 
         $getSuperAdminRoles = $tree->column('code');
-        $adminForbidRoles   = array(
-            'admin_user_avatar', 
-            'admin_user_change_password', 
-            'admin_my_cloud', 
-            'admin_cloud_video_setting', 
-            'admin_edu_cloud_sms', 
-            'admin_edu_cloud_search_setting', 
-            'admin_setting_cloud_attachment', 
-            'admin_setting_cloud', 
-            'admin_system'
-        );
+        $adminForbidRoles   = self::ADMIN_FORBIDDEN_PERMISSIONS;
 
         $getAdminForbidRoles = array();
         foreach ($adminForbidRoles as $adminForbidRole) {
--- src/Biz/Role/Service/RoleService.php
+++ src/Biz/Role/Service/RoleService.php
@@ -3,6 +3,19 @@ namespace Biz\Role\Service;
 
 interface RoleService
 {
+
+    const ADMIN_FORBIDDEN_PERMISSIONS = array(
+        'admin_user_avatar',
+        'admin_user_change_password',
+        'admin_my_cloud',
+        'admin_cloud_video_setting',
+        'admin_edu_cloud_sms',
+        'admin_edu_cloud_search_setting',
+        'admin_setting_cloud_attachment',
+        'admin_setting_cloud',
+        'admin_system'
+    );
+
     public function getRole($id);
 
     public function getRoleByCode($code);
