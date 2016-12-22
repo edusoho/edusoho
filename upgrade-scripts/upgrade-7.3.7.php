963e1ff688a99d458d1187cae6cce319fe467059
zhaoyao <zhaoyao@howzhi.com>
2016-12-22 19:00:04 +0800

    7.3.7

---
+++ upgrade-scripts/upgrade-7.3.7.php
@@ -0,0 +1,70 @@
+<?php
+
+use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\Filesystem\Filesystem;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $this->batchUpdate($index);
+            $this->getConnection()->commit();
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            throw $e;
+        }
+
+        try {
+            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."../web/install");
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $developerSetting          = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
+        ServiceKernel::instance()->createService('System.SettingService')->set("crontab_next_executed_time", time());
+    }
+
+    protected function batchUpdate($index)
+    {
+        $connection = $this->getConnection();
+        
+        $connection->exec("UPDATE `category_group` set `depth`= 3 WHERE code = 'course' or code = 'classroom' ;");
+    }
+
+    /**
+     * @return \Topxia\Service\System\Impl\SettingServiceImpl
+     */
+    private function getSettingService()
+    {
+        return ServiceKernel::instance()->createService('System.SettingService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $kernel;
+
+    public function __construct($kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @return \Topxia\Service\Common\Connection
+     */
+    public function getConnection()
+    {
+        return $this->kernel->getConnection();
+    }
+
+    abstract public function update();
+}
