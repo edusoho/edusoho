d976c4e9b63054e6e2c84a1b07437a09a9d5a67f
wangdanyue <wangdanyue@howzhi.com>
2016-12-24 10:56:21 +0800

    review-fix

--- src/AppBundle/Controller/Course/ReviewController.php
+++ src/AppBundle/Controller/Course/ReviewController.php
@@ -3,9 +3,27 @@
 namespace AppBundle\Controller\Course;
 
 use Biz\BaseController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReviewController extends BaseController
 {
+    public function listAction(Request $request, $courseSetId)
+    {
+        $conditions = array(
+            'courseSetId' => $courseSetId,
+            'parentId'    => 0
+        );
+
+        $courseId = $request->query->get('courseId');
+
+        $reviews = $this->getReviewService()->searchReviews(
+            $conditions,
+            array('createdTime' => 'DESC'),
+            0,
+            10
+        );
+    }
+
     protected function getCourseService()
     {
         return $this->createService('Course:CourseService');
