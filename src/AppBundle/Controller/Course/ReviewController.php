bda8b98525e3638b78c506ae5dc6ebe963556e4e
wangdanyue <wangdanyue@howzhi.com>
2016-12-26 09:40:39 +0800

    classroom-fix

--- src/AppBundle/Controller/Course/ReviewController.php
+++ src/AppBundle/Controller/Course/ReviewController.php
@@ -24,6 +24,33 @@ class ReviewController extends BaseController
         );
     }
 
+    public function postAction(Request $request, $courseId, $reviewId)
+    {
+        $course = $this->getCourseService()->tryManageCourse($id);
+
+        $postNum = $this->getReviewService()->searchReviewsCount(array('parentId' => $reviewId));
+
+        if ($postNum >= 5) {
+            return $this->createJsonResponse(array('error' => $this->trans('回复数量已达5条上限，不能再回复')));
+        }
+
+        $user = $this->getCurrentUser();
+
+        $fields             = $request->request->all();
+        $fields['userId']   = $user['id'];
+        $fields['courseId'] = $course['id'];
+        $fields['rating']   = 1;
+        $fields['parentId'] = $reviewId;
+
+        $post = $this->getReviewService()->saveReview($fields);
+
+        return $this->render('review/widget/subpost-item.html.twig', array(
+            'post'      => $post,
+            'author'    => $this->getCurrentUser(),
+            'canAccess' => true
+        ));
+    }
+
     protected function getCourseService()
     {
         return $this->createService('Course:CourseService');
--- src/AppBundle/Controller/CourseSetController.php
+++ src/AppBundle/Controller/CourseSetController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use Topxia\Common\Paginator;
+use Topxia\Common\ArrayToolkit;
 use Biz\Course\Service\CourseService;
 use Biz\Course\Service\CourseSetService;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,7 +21,50 @@ class CourseSetController extends BaseController
 
     public function noteListAction(Request $request, $id)
     {
+    }
 
+    public function reviewListAction(Request $request, $id)
+    {
+        list($courseSet, $course) = $this->getCourseSetAndCourse($request, $id);
+        list($course, $member)    = $this->getCourseService()->tryTakeCourse($course['id']);
+
+        $courseId = $request->query->get('courseId', 0);
+
+        $conditions = array(
+            'courseSetId' => $courseSet['id'],
+            'parentId'    => 0
+        );
+
+        if ($courseId > 0) {
+            $conditions['courseId'] = $courseId;
+        }
+
+        $paginator = new Paginator(
+            $this->get('request'),
+            $this->getReviewService()->searchReviewsCount($conditions),
+            20
+        );
+
+        $reviews = $this->getReviewService()->searchReviews(
+            $conditions,
+            array('createdTime' => 'DESC'),
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
+        $user       = $this->getCurrentUser();
+        $userReview = $this->getReviewService()->getUserCourseReview($user['id'], $course['id']);
+
+        $users = $this->getUserService()->findUsersByIds(ArrayToolkit::column($reviews, 'userId'));
+
+        return $this->render('courseset/review/list.html.twig', array(
+            'courseSet'  => $courseSet,
+            'course'     => $course,
+            'reviews'    => $reviews,
+            'userReview' => $userReview,
+            'users'      => $users,
+            'member'     => $member
+        ));
     }
 
     protected function getCourseSetAndCourse(Request $request, $id)
@@ -51,5 +96,18 @@ class CourseSetController extends BaseController
     protected function getCourseSetService()
     {
         return $this->createService('Course:CourseSetService');
+    }
+
+    /**
+     * @return ReviewService
+     */
+    protected function getReviewService()
+    {
+        return $this->createService('Course:ReviewService');
+    }
+
+    protected function getUserService()
+    {
+        return $this->createService('User:UserService');
     }
 }
