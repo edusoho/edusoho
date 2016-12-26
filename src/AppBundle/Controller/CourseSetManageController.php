f21e1fdbfcdf56782f34c7daeaa4c9da53e3285c
Simon Lau <liuyangyang@howzhi.com>
2016-12-26 14:24:54 +0800

    fix page not found when create courseset

--- src/AppBundle/Controller/CourseSetManageController.php
+++ src/AppBundle/Controller/CourseSetManageController.php
@@ -23,7 +23,7 @@ class CourseSetManageController extends BaseController
         $user        = $this->getUser();
         $userProfile = $this->getUserService()->getUserProfile($user->getId());
         $user        = $this->getUserService()->getUser($user->getId());
-        return $this->render('courseset/create.html.twig', array(
+        return $this->render('course-set/create.html.twig', array(
             'user'        => $user,
             'userProfile' => $userProfile
         ));
