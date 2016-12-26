365b208381e1444c3fd20ec1c98e6a052f46dd19
hamster <malianbo@163.com>
2016-12-26 12:02:21 +0800

    fix : seajs仍使用旧版本的jquery-1.11，独立于webpack

--- src/Topxia/WebBundle/Resources/public/js/app-compatible.js
+++ src/Topxia/WebBundle/Resources/public/js/app-compatible.js
@@ -1,4 +1,6 @@
 define(function(require, exports, module) {
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
     require('common/bootstrap-modal-hack2');
     require("placeholder");
     require('./util/card');
