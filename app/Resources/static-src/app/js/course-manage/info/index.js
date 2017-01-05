89326419bf2af4f9f0eed9e73db98b8f6de3baa9
Simon Lau <liuyangyang@howzhi.com>
2017-01-05 16:48:07 +0800

    fix

--- app/Resources/static-src/app/js/course-manage/info/index.js
+++ app/Resources/static-src/app/js/course-manage/info/index.js
@@ -1,18 +1,21 @@
-import { TabChange } from '../help';
+import {TabChange} from '../help';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import MultiInput from '../../../common/component/multi-input';
 import sortList from 'common/sortable';
 
-function renderMultiGroupComponent(elementId,name){
-  let datas = $('#'+elementId).data('init-value');
-  ReactDOM.render( <MultiInput dataSource= {datas} outputDataElement={name} />,
+function renderMultiGroupComponent(elementId, name) {
+  let datas = $('#' + elementId).data('init-value');
+  ReactDOM.render( < MultiInput
+  dataSource = {datas}
+  outputDataElement = {name} / >,
     document.getElementById(elementId)
-  );
+)
+  ;
 }
 
-renderMultiGroupComponent('course-goals','goals');
-renderMultiGroupComponent('intended-students','audiences');
+renderMultiGroupComponent('course-goals', 'goals');
+renderMultiGroupComponent('intended-students', 'audiences');
 
 
 _initDatePicker('#expiryStartDate');
@@ -26,15 +29,20 @@ CKEDITOR.replace('summary', {
   filebrowserImageUploadUrl: $('#summary').data('imageUploadUrl')
 });
 
-$('input[name="expiryMode"]').on('change', function(event){
-  if($('input[name="expiryMode"]:checked').val() == 'date'){
+$('input[name="expiryMode"]').on('change', function (event) {
+  if ($('input[name="expiryMode"]:checked').val() == 'date') {
     $('#expiry-days').removeClass('hidden').addClass('hidden');
     $('#expiry-date').removeClass('hidden');
-  }else{
+  } else {
     $('#expiry-date').removeClass('hidden').addClass('hidden');
     $('#expiry-days').removeClass('hidden');
   }
-}); 
+});
+
+jQuery.validator.addMethod("max_year", function (value, element) {
+  return this.optional(element) || value < 100000;
+}, "有效期最大值不能超过99,999天");
+
 
 let $form = $('#course-info-form');
 let validator = $form.validate({
@@ -49,7 +57,8 @@ let validator = $form.validate({
     },
     expiryDays: {
       required: '#expiryByDays:checked',
-      digits: true
+      digits: true,
+      max_year: true
     },
     expiryStartDate: {
       required: '#expiryByDate:checked',
@@ -64,7 +73,9 @@ let validator = $form.validate({
   },
   messages: {
     title: Translator.trans('请输入教学计划课程标题'),
-    expiryDays: Translator.trans('请输入学习有效期'),
+    expiryDays: {
+      required: Translator.trans('请输入学习有效期'),
+    },
     expiryStartDate: {
       required: Translator.trans('请输入开始日期'),
       before: Translator.trans('开始日期应早于结束日期')
@@ -78,7 +89,7 @@ let validator = $form.validate({
 
 $.validator.addMethod(
   "before",
-  function(value, element, params) {
+  function (value, element, params) {
     console.log($('input[name="expiryMode"]:checked').val());
     if ($('input[name="expiryMode"]:checked').val() !== 'date') {
       return true;
@@ -90,7 +101,7 @@ $.validator.addMethod(
 
 $.validator.addMethod(
   "after",
-  function(value, element, params) {
+  function (value, element, params) {
     console.log($('input[name="expiryMode"]:checked').val());
     if ($('input[name="expiryMode"]:checked').val() !== 'date') {
       return true;
@@ -100,7 +111,7 @@ $.validator.addMethod(
   Translator.trans('结束日期应晚于开始日期')
 );
 
-$('#course-submit').click(function(evt) {
+$('#course-submit').click(function (evt) {
   if (validator.form()) {
     $form.submit();
   }
