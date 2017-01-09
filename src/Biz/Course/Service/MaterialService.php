c7b4ab46dbc33e688492e5d71683a1dfc9216ced
hamster <malianbo@163.com>
2017-01-09 17:26:01 +0800

    fix: 课程文件的js未加载及删除逻辑有问题

--- src/Biz/Course/Service/MaterialService.php
+++ src/Biz/Course/Service/MaterialService.php
@@ -9,7 +9,7 @@ interface MaterialService
 
     public function updateMaterial($id, $fields, $argument);
 
-    public function deleteMaterial($courseId, $materialId);
+    public function deleteMaterial($courseSetId, $materialId);
 
     public function deleteMaterialsByLessonId($lessonId, $courseType = 'course');
 
@@ -17,7 +17,7 @@ interface MaterialService
 
     public function deleteMaterialsByCourseSetId($courseSetId, $courseType = 'course');
 
-    public function deleteMaterials($courseId, $fileIds, $courseType = 'course');
+    public function deleteMaterials($courseSetId, $fileIds, $courseType = 'course');
 
     public function deleteMaterialsByFileId($fileId);
 
