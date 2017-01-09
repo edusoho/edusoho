0972e56d9db5c1961b36b49f99ee9e8daf3707cc
wuli <wuli@howzhi.com>
2017-01-09 16:05:28 +0800

    change option

--- app/Resources/static-src/app/common/component/question-options/option.js
+++ app/Resources/static-src/app/common/component/question-options/option.js
@@ -1,30 +1,25 @@
 import React,{ Component } from 'react';
 
-
 export default class Options extends Component {
   constructor(props) {
     super(props);
-    let datas = this.props.datas;
     this.state = {
-      datas: datas,
+      datas: this.props.datas,
     }
     this.editor = null;
-    //value不需传递给付组件的state,因为value是属于自身组件的行为不应该去出发别的组件的Render；
+    //value不需传递给付组件的state,因为value是属于自身组件的行为不应该去触发别的组件的Render；
   }
  
   componentDidMount() {
-    console.log('componentDidMount');
     this.initCkeditor();
   }
 
-  
   deleteOption(event) {
-    this.props.deleteOption(event.currentTarget.id);
+    this.props.deleteOption(event.currentTarget.attributes["data-option-id"].value);
   }
 
   onChangeChecked(event) {
-    console.log('event',event.currentTarget.checked,event.currentTarget.id);
-    this.props.changeOptionChecked(event.currentTarget.id);
+    this.props.changeOptionChecked(event.currentTarget.attributes["data-option-id"].value);
   }
 
   initCkeditor(dataSourceUi) {
@@ -71,13 +66,13 @@ export default class Options extends Component {
           <textarea className="form-control datas-input col-md-8" id={this.state.datas.optionId}  defaultValue={this.state.datas.inputValue} name='choices[]'></textarea>
           <div className="mtm">
             <label>
-              <input type={type} name='answer[]' id={this.state.datas.optionId} value={this.props.index}  checked={this.state.datas.checked} className="answer-checkbox" onChange = {(event)=>this.onChangeChecked(event)}/>正确答案 
+              <input type={type} name='answer[]' data-option-id={this.state.datas.optionId} value={this.props.index}  checked={this.state.datas.checked} className="answer-checkbox" onChange = {(event)=>this.onChangeChecked(event)}/>正确答案 
             </label>
           </div>
           <p className={showDanger ? 'color-danger' : 'hidden'}>请输入选项内容</p>
         </div>
         <div className="col-sm-2">
-          <a className="btn btn-default btn-sm"  href="javascript:;" id={this.state.datas.optionId} onClick={(event)=>this.deleteOption(event)}><i className="glyphicon glyphicon-trash"></i></a>
+          <a className="btn btn-default btn-sm" data-option-id={ this.state.datas.optionId }  onClick={(event)=>this.deleteOption(event)} href="javascript:;"><i className="glyphicon glyphicon-trash"></i></a>
         </div>
       </div>
     )
