db78d1b0c6526ee5973c41dc90bb246307eefa6a
retamia <retamia@gmail.com>
2017-01-13 14:15:13 +0800

    fix: 我的教学--在教课程

--- src/AppBundle/Controller/My/CourseSetController.php
+++ src/AppBundle/Controller/My/CourseSetController.php
@@ -4,12 +4,11 @@
 namespace AppBundle\Controller\My;
 
 
-use AppBundle\Controller\BaseController;
-use Biz\Course\Service\CourseSetService;
+use AppBundle\Controller\Course\CourseBaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Topxia\Common\Paginator;
 
-class CourseSetController extends BaseController
+class CourseSetController extends CourseBaseController
 {
     public function favoriteAction(Request $request)
     {
@@ -34,11 +33,46 @@ class CourseSetController extends BaseController
         ));
     }
 
-    /**
-     * @return CourseSetService
-     */
-    protected function getCourseSetService()
+    public function teachingAction(Request $request, $filter = 'normal')
     {
-        return $this->createService('Course:CourseSetService');
+        $user = $this->getCurrentUser();
+
+        if (!$user->isTeacher()) {
+            return $this->createMessageResponse('error', '您不是老师，不能查看此页面！');
+        }
+
+        $conditions = array(
+            'type' => 'normal'
+        );
+
+        if ($filter == 'live') {
+            $conditions['type'] = 'live';
+        }
+
+        $paginator = new Paginator(
+            $this->get('request'),
+            $this->getCourseSetService()->countUserTeachingCourseSets($user['id'], $conditions),
+            20
+        );
+
+        $sets = $this->getCourseSetService()->searchUserTeachingCourseSets(
+            $user['id'],
+            $conditions,
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
+        $service = $this->getCourseService();
+        $sets    = array_map(function ($set) use ($user, $service) {
+            $set['canManage'] = $set['creator'] == $user['id'];
+            $set['courses']   = $service->findUserTeachingCoursesByCourseSetId($set['id'], false);
+            return $set;
+        }, $sets);
+
+        return $this->render('my/teaching/course-sets.html.twig', array(
+            'courseSets' => $sets,
+            'paginator'  => $paginator,
+            'filter'     => $filter
+        ));
     }
 }
