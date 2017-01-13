15c0f7b4835da3790197a6c8af1e259030b1eca0
Wellming Li <wellming.li@howzhi.com>
2017-01-13 17:11:53 +0800

    add hls json support

--- src/Topxia/WebBundle/Controller/HLSController.php
+++ src/Topxia/WebBundle/Controller/HLSController.php
@@ -10,6 +10,7 @@ class HLSController extends BaseController
     public function playlistAction(Request $request, $id, $token)
     {
         $line       = $request->query->get('line', null);
+        $format     = $request->query->get('format', '');
         $levelParam = $request->query->get('level', '');
 
         $token    = $this->getTokenService()->verifyToken('hls.playlist', $token);
@@ -85,6 +86,14 @@ class HLSController extends BaseController
         );
 
         $api = CloudAPIFactory::create('leaf');
+
+        //新版api需要返回json形式的m3u8
+        if (strtolower($format) == 'json') {
+            $playlist = $api->get('/hls/playlist/json', array('streams' => $streams, 'qualities' => $qualities));
+            return $this->createJsonResponse($playlist);
+        }
+
+
         $playlist = $api->get('/hls/playlist', array(
             'streams'   => $streams,
             'qualities' => $qualities,
