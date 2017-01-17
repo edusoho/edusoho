1901315bb413df4d444e105ec54785b52dc2bcea
xingjingang <xingjingang@howzhi.com>
2017-01-17 14:41:56 +0800

    edit

--- src/Topxia/WebBundle/Controller/OpenCourseManageController.php
+++ src/Topxia/WebBundle/Controller/OpenCourseManageController.php
@@ -292,11 +292,8 @@ class OpenCourseManageController extends BaseController
     public function searchAction(Request $request, $id, $filter)
     {
         $course = $this->getOpenCourseService()->tryManageOpenCourse($id);
-
         $conditions = array("title" => $request->request->get('key'));
-
         list($paginator, $courses) = $this->_getPickCourseData($request, $id, $conditions);
-
         $users = $this->_getTeacherUsers($courses);
 
         return $this->render('TopxiaWebBundle:Course:course-select-list.html.twig', array(
