e7debd14d99a17b9b897ae5c7a55c57c8a75c685
=tianshengjie <tianshengjie@howzhi.com>
2017-01-18 14:59:53 +0800

    update

--- src/Biz/Task/Service/TaskService.php
+++ src/Biz/Task/Service/TaskService.php
@@ -112,5 +112,8 @@ interface TaskService
 
     public function getTaskByCourseIdAndActivityId($courseId, $activityId);
 
+    /**
+    * 获得课程的总学习时间
+    */ 
     public function getLearnTimeByCourseSetId($courseSetId);
 }
