56cd705871c38be5147f3ff09b7bb0321a7613a2
杨佳龙 <yangjlong315@gmail.com>
2017-01-19 14:54:11 +0800

    fix

--- upgrade-scripts/upgrade-7.4.1.php
+++ upgrade-scripts/upgrade-7.4.1.php
@@ -1,10 +1,29 @@
 <?php
 
+use Topxia\Service\Common\ServiceKernel;
+use Symfony\Component\Filesystem\Filesystem;
+
 class EduSohoUpgrade extends AbstractUpdater
 {
     public function update()
     {
         $this->updateScheme();
+
+        try {
+            $dir        = realpath(ServiceKernel::instance()->getParameter('kernel.root_dir')."/../web/install");
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $developerSetting          = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        ServiceKernel::instance()->createService('System.SettingService')->set('developer', $developerSetting);
+        ServiceKernel::instance()->createService('System.SettingService')->set("crontab_next_executed_time", time());
     }
 
     private function updateScheme()
@@ -20,6 +39,11 @@ class EduSohoUpgrade extends AbstractUpdater
         $sql = "DESCRIBE `{$table}` `{$filedName}`;";
         $result = $this->getConnection()->fetchAssoc($sql);
         return empty($result) ? false : true;
+    }
+
+    private function getSettingService()
+    {
+        return ServiceKernel::instance()->createService('System.SettingService');
     }
 }
 
