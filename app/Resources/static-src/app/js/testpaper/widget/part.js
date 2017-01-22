8de3fb2e4b8be925f005b3d8e7cdb495109667c9
wuli <wuli@howzhi.com>
2017-01-22 15:34:08 +0800

    learn

--- app/Resources/static-src/app/js/testpaper/widget/part.js
+++ app/Resources/static-src/app/js/testpaper/widget/part.js
@@ -5,7 +5,7 @@ export const initScrollbar = ()=> {
 	let $taskTestpaperBodyIframe = $('.js-task-testpaper-body-iframe');
 	let $paneCard = $('.js-panel-card');
 	
-	if (Browser.isIE  || Browser.isFirefox || Browser.isIE11 ) {
+	if (Browser.isIE  || Browser.isFirefox || Browser.isMozilla ) {
 		$taskTestpaperBodyIframe.css('overflow-y','auto')
 	} else {
 		$taskTestpaperBodyIframe.perfectScrollbar();
--- app/Resources/static-src/common/utils.js
+++ app/Resources/static-src/common/utils.js
@@ -4,7 +4,6 @@ Browser.isMozilla = (typeof document.implementation != 'undefined')
   && (typeof document.implementation.createDocument != 'undefined')
   && (typeof HTMLDocument != 'undefined');
 Browser.isIE = window.ActiveXObject ? true : false;
-Browser.isIE11 = (navigator.userAgent.toLowerCase().indexOf("Mozilla") != -1);
 Browser.isFirefox = (navigator.userAgent.toLowerCase().indexOf("firefox") != -1);
 Browser.isSafari = (navigator.userAgent.toLowerCase().indexOf("safari") != -1);
 Browser.isOpera = (navigator.userAgent.toLowerCase().indexOf("opera") != -1);
