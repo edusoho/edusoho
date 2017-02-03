c11ffd04e6b3fabb983bb95938558fe293f546c0
hamster <malianbo@163.com>
2017-02-03 11:47:57 +0800

    fix: liveActivityService unittest

--- src/Biz/Activity/Service/Impl/LiveActivityServiceImpl.php
+++ src/Biz/Activity/Service/Impl/LiveActivityServiceImpl.php
@@ -3,12 +3,12 @@
 namespace Biz\Activity\Service\Impl;
 
 use Biz\BaseService;
-use Biz\Course\Service\LiveReplayService;
 use Biz\Util\EdusohoLiveClient;
+use Topxia\Common\ArrayToolkit;
 use Biz\User\Service\UserService;
 use Biz\System\Service\SettingService;
-use Topxia\Common\ArrayToolkit;
 use Topxia\Service\Common\ServiceKernel;
+use Biz\Course\Service\LiveReplayService;
 use Biz\Activity\Service\LiveActivityService;
 
 class LiveActivityServiceImpl extends BaseService implements LiveActivityService
@@ -65,11 +65,11 @@ class LiveActivityServiceImpl extends BaseService implements LiveActivityService
             $liveParams = array(
                 'liveId'  => $fields['liveId'],
                 'summary' => empty($fields['remark']) ? '' : $fields['remark'],
-                'title'   => $fields['title'],
+                'title'   => $fields['title']
             );
 
             $liveParams['startTime'] = $activity['startTime'];
-            $liveParams['endTime']   = (string)($activity['startTime'] + $fields['length'] * 60);
+            $liveParams['endTime']   = (string) ($activity['startTime'] + $fields['length'] * 60);
 
             //直播开始后不更新开始时间和直播时长
             if (empty($activity['startTime']) || $activity['startTime'] <= time()) {
@@ -79,13 +79,15 @@ class LiveActivityServiceImpl extends BaseService implements LiveActivityService
 
             $this->getEdusohoLiveClient()->updateLive($liveParams);
         }
-        
+
         $liveActivity = ArrayToolkit::parts($fields, array('replayStatus', 'fileId'));
 
         if (!empty($liveActivity['fileId'])) {
             $liveActivity['mediaId']      = $liveActivity['fileId'];
             $liveActivity['replayStatus'] = LiveReplayService::REPLAY_VIDEO_GENERATE_STATUS;
             unset($liveActivity['fileId']);
+        } else {
+            return $this->getLiveActivityDao()->get($id);
         }
 
         $liveActivity = $this->getLiveActivityDao()->update($id, $liveActivity);
