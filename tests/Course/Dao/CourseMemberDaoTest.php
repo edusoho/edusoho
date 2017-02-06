74d8c04bea19f4b3484b89f483760d7103d601f8
hamster <malianbo@163.com>
2017-02-06 14:13:02 +0800

    fix: 修复CourseMemberDao的unittest

--- tests/Course/Dao/CourseMemberDaoTest.php
+++ tests/Course/Dao/CourseMemberDaoTest.php
@@ -397,7 +397,6 @@ class CourseMemberDaoTest extends BaseDaoTestCase
             'maxStudentNum'              => 10,
             'status'                     => 'published',
             'creator'                    => 1,
-            'copyCourseId'               => 1,
             'isFree'                     => 1,
             'price'                      => 1,
             'vipLevelId'                 => 1,
