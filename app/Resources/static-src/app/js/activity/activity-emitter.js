16c9df0b1af20fc686de153a51e1139eea354259
zhongjie <zhongjie@howzhi.com>
2017-02-08 17:07:00 +0800

    添加ie8兼容方案

--- app/Resources/static-src/app/js/activity/activity-emitter.js
+++ app/Resources/static-src/app/js/activity/activity-emitter.js
@@ -1,6 +1,7 @@
 import postal from 'postal';
-import 'postal.federation';
-import 'postal.xframe';
+import 'postal-federation';
+import 'postal-xframe';
+
 
 export default class ActivityEmitter {
 
