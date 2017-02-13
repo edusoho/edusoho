f89d738a6294de5b47fdc1d634cc997e0586d3b8
zhongjie <zhongjie@howzhi.com>
2017-02-13 15:24:09 +0800

    调整首页js

--- app/Resources/static-src/app/js/default/index.js
+++ app/Resources/static-src/app/js/default/index.js
@@ -17,6 +17,21 @@ if ($(".es-poster .swiper-slide").length > 1) {
 
 echo.init();
 
+$("body").on('click','.js-course-filter',function(){
+    var $btn = $(this);
+    var courseType = $btn.data('type');
+    var text = $('.course-filter .visible-xs .active a').text();
+    $.get($btn.data('url'),function(html){
+        $('#'+courseType+'-list-section').after(html).remove();
+        var parent = $btn.parent();
+        if(!parent.hasClass('course-sort')){
+            text = $btn.find("a").text();   
+        }
+    $('.course-filter .visible-xs .btn').html(text+" "+'<span class="caret"></span>');
+        echo.init();
+    })
+})
+
 $('.recommend-teacher').on('click', '.teacher-item .follow-btn', function(){
   var $btn = $(this);
   var loggedin = $btn.data('loggedin');
