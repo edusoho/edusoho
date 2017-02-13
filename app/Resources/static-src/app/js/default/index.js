9c42fd1208549952e287cc70acd8662f0f569878
zhongjie <zhongjie@howzhi.com>
2017-02-13 15:19:05 +0800

    移动首页js

---
+++ app/Resources/static-src/app/js/default/index.js
@@ -0,0 +1,50 @@
+import Swiper from 'swiper';
+
+if ($(".es-poster .swiper-slide").length > 1) {
+  var swiper = new Swiper('.es-poster.swiper-container', {
+    pagination: '.swiper-pager',
+    paginationClickable: true,
+    autoplay: 5000,
+    autoplayDisableOnInteraction: false,
+    loop: true,
+    calculateHeight: true,
+    roundLengths: true,
+    onInit: function(swiper) {
+      $(".swiper-slide").removeClass('swiper-hidden'); 
+    }
+  });
+}
+
+echo.init();
+
+$('.recommend-teacher').on('click', '.teacher-item .follow-btn', function(){
+  var $btn = $(this);
+  var loggedin = $btn.data('loggedin');
+  if(loggedin == "1"){
+      showUnfollowBtn($btn);
+  }
+  $.post($btn.data('url'));
+}).on('click', '.teacher-item .unfollow-btn', function(){
+  var $btn = $(this);
+  showFollowBtn($btn);
+  $.post($btn.data('url'));
+})
+
+
+function showFollowBtn($btn)
+{
+  $btn.hide();
+  $btn.siblings('.follow-btn').show();
+  $actualCard = $('#user-card-'+ $btn.closest('.js-card-content').data('userId'));
+  $actualCard.find('.unfollow-btn').hide();
+  $actualCard.find('.follow-btn').show();
+}
+
+function showUnfollowBtn($btn)
+{
+  $btn.hide();
+  $btn.siblings('.unfollow-btn').show();
+  $actualCard = $('#user-card-'+ $btn.closest('.js-card-content').data('userId'));
+  $actualCard.find('.follow-btn').hide();
+  $actualCard.find('.unfollow-btn').show();
+}
