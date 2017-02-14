98cae6184e9660d551b38ea901b82ffe22623250
zhongjie <zhongjie@howzhi.com>
2017-02-14 11:20:21 +0800

    迁移资讯js

--- app/Resources/static-src/libs/js/jquery-validation.js
+++ app/Resources/static-src/libs/js/jquery-validation.js
@@ -85,7 +85,7 @@ $.extend($.validator.prototype, {
         }
       }
 
-      name = $(element).attr("name");
+      name = $(element).data('display') || $(element).attr("name");
       message = message.replace(displayregex, labeltext || name)
     }
 
