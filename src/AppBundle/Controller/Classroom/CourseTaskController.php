79963cc03f53774855a18ec97e102122fdfb3cf0
hamster <malianbo@163.com>
2017-02-14 11:09:40 +0800

    删除无用代码

--- src/AppBundle/Controller/Classroom/CourseTaskController.php
+++ src/AppBundle/Controller/Classroom/CourseTaskController.php
@@ -5,36 +5,9 @@ use Biz\Task\Service\TaskService;
 use Biz\Course\Service\CourseService;
 use AppBundle\Controller\BaseController;
 use Biz\Classroom\Service\ClassroomService;
-use Symfony\Component\HttpFoundation\Request;
 
 class CourseTaskController extends BaseController
 {
-    public function previewAction(Request $request, $classroomId, $courseId)
-    {
-        $taskId    = $request->query->get('taskId', 0);
-        $course    = $this->getCourseService()->getCourse($courseId);
-        $task      = $this->getTaskService()->getTask($taskId);
-        $classroom = $this->getClassroomService()->getClassroom($classroomId);
-        $user      = $this->getCurrentUser();
-        $member    = $user['id'] ? $this->getClassroomService()->getClassroomMember($classroom['id'], $user['id']) : null;
-
-        if (!$user->isLogin()) {
-            return $this->forward('AppBundle:CourseTask:preview', array(
-                'courseId' => $courseId,
-                'lessonId' => $taskId
-            ));
-        }
-
-        if ($task['free'] || $course['tryLookable'] || ($member && !$member['locked'])) {
-            return $this->forward('AppBundle:CourseTask:preview', array(
-                'courseId' => $courseId,
-                'lessonId' => $taskId
-            ));
-        }
-
-        return $this->redirect($this->generateUrl('classroom_buy_hint', array('courseId' => $course["id"])));
-    }
-
     public function buyHintAction($courseId)
     {
         $classroom = $this->getClassroomService()->getClassroomByCourseId($courseId);
