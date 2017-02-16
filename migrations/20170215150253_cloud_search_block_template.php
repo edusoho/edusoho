c8b1c4dc743409ec8923efceb85e52266bc7783d
lufangyu1217 <584141397@qq.com>
2017-02-16 09:44:51 +0800

    fix

--- migrations/20170215150253_cloud_search_block_template.php
+++ migrations/20170215150253_cloud_search_block_template.php
@@ -11,18 +11,19 @@ class CloudSearchBlockTemplate extends Migration
     {
         $biz = $this->getContainer();
         $db  = $biz['db'];
-        $db->exec("INSERT INTO `block_template` (`title`, `mode`, `template`, `templateName`, `templateData`, `content`, `data`, `code`, `meta`, `tips`, `category`, `createdTime`, `updateTime`) 
-        VALUES(
-            '云搜索背景图',
-            'template',
-            NULL, 
-            'TopxiaWebBundle:Block:cloud_search_banner.template.html.twig',
-            NULL,
-            '', 
-            '{\"posters\":[{\"src\":\"\\/assets\\/img\\/placeholder\\/banner_search.jpg\",\"alt\":\"背景图\",\"layout\":\"tile\",\"background\":\"blue\",\"href\":\"\",\"html\":\"\",\"status\":\"1\",\"mode\":\"img\"}]}',
-            'cloud_search_banner', 
-            '{\"title\":\"背景图\",\"category\":\"system\",\"templateName\":\"TopxiaWebBundle:Block:cloud_search_banner.template.html.twig\",\"items\":{\"posters\":{\"title\":\"背景图\",\"type\":\"poster\",\"desc\":\"建议图片大小为1440*200，最多可设置1张图片。\",\"count\":1,\"default\":{\"src\":\"\\/assets\\/img\\/placeholder\\/banner_search.jpg\",\"alt\":\"背景图\",\"layout\":\"tile\",\"background\":\"blue\",\"href\":\"\",\"html\":\"\",\"status\":\"1\",\"mode\":\"img\"}}}}', 
-            NULL, 'system', 1486538595, 1486543476);
+        $db->exec("
+            INSERT INTO `block_template` (`title`, `mode`, `template`, `templateName`, `templateData`, `content`, `data`, `code`, `meta`, `tips`, `category`, `createdTime`, `updateTime`) 
+            VALUES(
+                '云搜索背景图',
+                'template',
+                NULL, 
+                'TopxiaWebBundle:Block:cloud_search_banner.template.html.twig',
+                NULL,
+                '', 
+                '{\"posters\":[{\"src\":\"\\/assets\\/img\\/placeholder\\/banner_search.jpg\",\"alt\":\"背景图\",\"layout\":\"tile\",\"background\":\"blue\",\"href\":\"\",\"html\":\"\",\"status\":\"1\",\"mode\":\"img\"}]}',
+                'cloud_search_banner', 
+                '{\"title\":\"背景图\",\"category\":\"system\",\"templateName\":\"TopxiaWebBundle:Block:cloud_search_banner.template.html.twig\",\"items\":{\"posters\":{\"title\":\"背景图\",\"type\":\"poster\",\"desc\":\"建议图片大小为1440*200，最多可设置1张图片。\",\"count\":1,\"default\":{\"src\":\"\\/assets\\/img\\/placeholder\\/banner_search.jpg\",\"alt\":\"背景图\",\"layout\":\"tile\",\"background\":\"blue\",\"href\":\"\",\"html\":\"\",\"status\":\"1\",\"mode\":\"img\"}}}}', 
+                NULL, 'system', 1486538595, 1486543476);
         ");
     }
 
@@ -31,6 +32,5 @@ class CloudSearchBlockTemplate extends Migration
      */
     public function down()
     {
-        
     }
 }
