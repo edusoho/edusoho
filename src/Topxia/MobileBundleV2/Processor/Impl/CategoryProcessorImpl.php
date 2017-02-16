53b8a0092f884bf0d3ed04c96a6b1b832ad2c86f
lufangyu1217 <584141397@qq.com>
2017-02-16 19:06:30 +0800

    分类接口的修改,数据格式做了调整

--- src/Topxia/MobileBundleV2/Processor/Impl/CategoryProcessorImpl.php
+++ src/Topxia/MobileBundleV2/Processor/Impl/CategoryProcessorImpl.php
@@ -3,6 +3,7 @@ namespace Topxia\MobileBundleV2\Processor\Impl;
 
 use Topxia\MobileBundleV2\Processor\BaseProcessor;
 use Topxia\MobileBundleV2\Processor\CategoryProcessor;
+use Topxia\Common\ArrayToolkit;
 
 class CategoryProcessorImpl extends BaseProcessor implements CategoryProcessor
 {
@@ -37,87 +38,32 @@ class CategoryProcessorImpl extends BaseProcessor implements CategoryProcessor
         return $categories;
     }
 
-    private function coverCategoryChilds($categories)
-    {
-        $realityDepth = 0;
-        $categorieStack = array();
-
-        foreach ($categories as $key => $categorie) {
-            if (empty($categorieStack)) {
-                array_push($categorieStack, $categorie);
-                continue;
-            }
-
-            $popCategory = &$categorieStack[count($categorieStack) - 1];
-            $popDepth = $popCategory["depth"];
-            $depth = $categorie["depth"];
-            if ($depth > 0 && $depth > $popDepth) {
-                if (! isset($popCategory["childs"])) {
-                    $popCategory["childs"] = array();
-                }
-
-                array_push($categorieStack, $categorie);
-                $count = count($categorieStack);
-                if ($realityDepth < $count) {
-                    $realityDepth ++;
-                }
-                $popCategory["childs"][] = &$categorieStack[$count - 1];
-            }  else {
-                //最后的节点出栈
-                $popChildCategory = end($categorieStack);
-                $popChildDepth = $popChildCategory["depth"];
-                while ($depth <= $popChildDepth) {
-                    //如果最后节点depth仍然比要加入的节点的depth大，继续弹出
-                    array_pop($categorieStack);
-                    $popChildCategory = end($categorieStack);
-                    $popChildDepth = $popChildCategory["depth"];
-                }
-
-                //获取当前出栈的节点的父节点，并添加子节点
-                $popCategory = &$categorieStack[count($categorieStack) - 1];
-                array_push($categorieStack, $categorie);
-                $popCategory["childs"][] = &$categorieStack[count($categorieStack) - 1];
-            }
-
-        }
-
-        if (count($categorieStack) > 1) {
-            array_pop($categorieStack);
-        }
-
-        return array($categorieStack, $realityDepth);
-    }
-
     public function getCategorieTree()
     {
         $type = $this->getParam('type', 'course');
 
         $group = $this->controller->getCategoryService()->getGroupByCode($type);
+
         if (empty($group)) {
             return array();
-        } 
+        }
 
-        $categories = $this->controller->getCategoryService()->getCategoryTree($group['id']);
+        $categories = $this->controller->getCategoryService()->getCategoryStructureTree($group['id']);
 
-        array_unshift($categories, array(
-            "id"=>"0",
-            "code"=>"root",
-            "name"=>"默认分类",
-            "icon"=>"",
-            "path"=>"0",
-            "weight"=>"0",
-            "groupId"=>"0",
-            "description"=>"默认分类",
-            "depth"=>"0"
-            ));
-
-        list($coverCategorys, $realityDepth) = $this->coverCategoryChilds($categories);
         return array(
-            "realityDepth"=>$realityDepth,
-            "depth"=>$group["depth"],
-            "data"=>$coverCategorys
+            "realityDepth" => $this->getRealityDepthByGroupId($group['id']),
+            "depth"        => $group["depth"],
+            "data"         => $categories
             );
-        return $coverCategorys;
+    }
+
+    protected function getRealityDepthByGroupId($groupId)
+    {
+        $categories = $this->controller->getCategoryService()->getCategoryTree($groupId);
+
+        $depths = ArrayToolkit::column($categories, 'depth');
+
+        return max($depths);
     }
 
     public function getAllCategories()
@@ -127,19 +73,7 @@ class CategoryProcessorImpl extends BaseProcessor implements CategoryProcessor
             return array();
         } 
 
-        $categories = $this->controller->getCategoryService()->getCategoryTree($group['id']);
-
-        array_unshift($categories, array(
-            "id"=>"0",
-            "code"=>"root",
-            "name"=>"默认分类",
-            "icon"=>"",
-            "path"=>"0",
-            "weight"=>"0",
-            "groupId"=>"0",
-            "description"=>"默认分类",
-            "depth"=>"0"
-            ));
+        $categories = $this->controller->getCategoryService()->getCategoryStructureTree($group['id']);
 
         return $categories;
     }
