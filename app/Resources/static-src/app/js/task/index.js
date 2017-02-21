8a99db7ce7e6ee89f4be38390e829421c2e80815
wuli <wuli@howzhi.com>
2017-02-21 10:02:53 +0800

    learn

--- app/Resources/static-src/app/js/task/index.js
+++ app/Resources/static-src/app/js/task/index.js
@@ -80,6 +80,7 @@ class TaskShow extends Emitter {
     // response.result.status == 'finish'
     //     &&
     if ( $('input[name="task-result-status"]', $('#js-hidden-data')).val() != 'finish') {
+        // 盘点是任务式学习还是自由式学习
         $.get($(".js-learned-prompt").data('url'), html => {
         $(".js-learned-prompt").attr('data-content', html);
         this.ui.learnedWeakPrompt();
