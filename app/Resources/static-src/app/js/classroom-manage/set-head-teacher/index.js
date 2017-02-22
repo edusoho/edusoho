584679f5620d7368ce7ab6e5c35ae856db8e1bdb
wuli <wuli@howzhi.com>
2017-02-22 19:30:24 +0800

    classromm-teacher

--- app/Resources/static-src/app/js/classroom-manage/set-head-teacher/index.js
+++ app/Resources/static-src/app/js/classroom-manage/set-head-teacher/index.js
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import PersonaMultiInput from 'app/common/component/persona-multi-input';
 
-ReactDOM.render( <PersonaMultiInput replaceItem={true} sortable={false} addable={true} showCheckbox={false} dataSource= {$('#classroom-head-teacher').data('teacher')} outputDataElement='teachers' searchable={{enable:true,url:$('#classroom-head-teacher').data('url') + "?q="}} />,
+ReactDOM.render( <PersonaMultiInput replaceItem={true} sortable={false} addable={true} showCheckbox={false} dataSource= {$('#classroom-head-teacher').data('teacher')} outputDataElement='teachers' searchable={{enable:true,url:$('#classroom-head-teacher').data('url') + "?q="}} showDeleteBtn={false} />,
   document.getElementById('classroom-head-teacher')
 );
