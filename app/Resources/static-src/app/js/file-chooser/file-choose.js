52fbb51df4a4445d1fad1c8e48ccb6da9475e31e
=tianshengjie <tianshengjie@howzhi.com>
2017-02-22 17:38:24 +0800

    自动填充的时候，判断是否为空，为空的时候，才自动填充标题

--- app/Resources/static-src/app/js/file-chooser/file-choose.js
+++ app/Resources/static-src/app/js/file-chooser/file-choose.js
@@ -43,7 +43,7 @@ class FileChooser extends Emitter{
 
   _fillTitle(file){
     let $title = $("#title");
-    if ($title.length > 0) {
+    if ($title.length > 0 && $title.val()=='') {
       let title = file.name.substring(0,file.name.indexOf('.'));
       $title.val(title);
     }
