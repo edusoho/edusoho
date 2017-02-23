397b5bf28bee3f979a668454fabbed6620ad1eaf
tangyang <tangyang@howzhi.com>
2017-02-23 10:27:16 +0800

    .

--- src/AppBundle/Extensions/DataTag/FreeCourseSetsDataTag.php
+++ src/AppBundle/Extensions/DataTag/FreeCourseSetsDataTag.php
@@ -10,15 +10,15 @@ class FreeCourseSetsDataTag extends CourseBaseDataTag implements DataTag
      * 获取最新课程列表
      *
      * 可传入的参数：
-     *   categoryId 可选 分类ID
-     *   notFree 可选 1：代表不包括免费课程 0：代表包括 默认包括
+     *   categoryId/categoryCode 可选 分类ID/分类编码
+     *   orderby 可选 课程排序方式
      *   count    必需 课程数量，取值不能超过100
      * 
      * @param  array $arguments 参数
      * @return array 课程列表
      */
     public function getData(array $arguments)
-    {   
+    {
         $this->checkCount($arguments);
 
         $conditions = array();
@@ -33,9 +33,15 @@ class FreeCourseSetsDataTag extends CourseBaseDataTag implements DataTag
             $conditions['categoryId'] = empty($category) ? -1 : $category['id'];
         }
 
+        if (!empty($arguments['orderby'])) {
+            $orderby = $arguments['orderby'];
+        } else {
+            $orderby = 'latest';
+        }
+
         $courseSets = $this->getCourseSetService()->searchCourseSets(
             $conditions,
-            'latest',
+            $orderby,
             0,
             $arguments['count']
         );
