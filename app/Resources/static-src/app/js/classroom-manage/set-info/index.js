b9b32000ee4de703ab106a86d9763a37fde55e50
wuli <wuli@howzhi.com>
2017-02-23 12:55:07 +0800

    change

--- app/Resources/static-src/app/js/classroom-manage/set-info/index.js
+++ app/Resources/static-src/app/js/classroom-manage/set-info/index.js
@@ -1,6 +1,7 @@
 import notify from 'common/notify';
 import 'common/select2';
-import 'app/js/classroom-manage/category-select';
+// import 'common/select2/3.4.1/select2.css';
+// import 'app/js/classroom-manage/category-select';
 import 'app/js/classroom-manage/classroom-create';
 
 let editor_classroom_about = CKEDITOR.replace('about', {
@@ -9,6 +10,14 @@ let editor_classroom_about = CKEDITOR.replace('about', {
     filebrowserImageUploadUrl: $('#about').data('imageUploadUrl'),
     filebrowserFlashUploadUrl: $('#about').data('flashUploadUrl')
 });
+
+// $('[data-role="tree-select"], [name="categoryId"]').select2({
+//     treeview: true,
+//     dropdownAutoWidth: true,
+//     treeviewInitState: 'collapsed',
+//     placeholderOption: 'first'
+//     // treeviewInitState: 'expanded'
+//   });
 
 let validator = $('#classroom-set-form').validate({
   onkeyup: false,
