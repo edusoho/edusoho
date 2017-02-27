936328e1495050e43a1da9a7b6c50c01f8a65e69
zhongjie <zhongjie@howzhi.com>
2017-02-27 15:41:17 +0800

    修改script加载的问题

--- app/Resources/static-src/common/script.js
+++ app/Resources/static-src/common/script.js
@@ -6,17 +6,15 @@ const script = (scripts, fn, target) => {
   if (!scripts.length) return;
   target = !target ? document.getElementsByTagName('body')[0] : target;
   (function callback(s = scripts.shift()) {
-    loadJs(s, callback, target);
+    !scripts.length ? loadJs(s, fn, target) : loadJs(s, callback, target);
   }());
-};
-
+}
 const loadJs = (path, fn, target) => {
   let elem = document.createElement('script'), loaded;
   elem.onload = elem.onerror = elem.onreadystatechange = () => {
     if ((elem.readyState && !(/^c|loade/.test(elem.readyState))) || loaded) {
       return;
     }
-
     elem.onload = elem.onreadystatechange = null;
     loaded = 1;
     fn();
