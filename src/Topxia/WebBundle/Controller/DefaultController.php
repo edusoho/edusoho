4f1779958a747f10e38a2a78e6a1fe09de9bf5cc
杨佳龙 <yangjlong315@gmail.com>
2017-03-01 19:22:39 +0800

    fix

--- src/Topxia/WebBundle/Controller/DefaultController.php
+++ src/Topxia/WebBundle/Controller/DefaultController.php
@@ -34,7 +34,7 @@ class DefaultController extends BaseController
             return $this->createMessageResponse('warning', '非法请求');
         }
 
-        $mobileCode = (!empty($result["mobileCode"]) ? $result["mobileCode"] : "edusohov3");
+        $mobileCode = (empty($result["mobileCode"]) ? 'edusohov3' : $result["mobileCode"]);
 
         if ($this->getWebExtension()->isMicroMessenger()) {
             $url ="http://a.app.qq.com/o/simple.jsp?pkgname=com.edusoho.kuozhi";
