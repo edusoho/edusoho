14387421b9371a217b7a46511e1acf849793ae13
retamia <retamia@gmail.com>
2017-03-01 15:07:49 +0800

    feature: 作业批阅短信通知

--- src/Biz/Sms/Event/TestPaperEventSubscriber.php
+++ src/Biz/Sms/Event/TestPaperEventSubscriber.php
@@ -22,26 +22,64 @@ class TestPaperEventSubscriber extends EventSubscriber implements EventSubscribe
     public static function getSubscribedEvents()
     {
         return array(
-            'testpaper.reviewed' => 'onTestpaperReviewed'
+            'exam.reviewed' => 'onExamReviewed',
         );
     }
 
-    public function onTestpaperReviewed(Event $event)
+    public function onExamReviewed(Event $event)
     {
-        $parameters = array();
-        $smsType    = 'sms_testpaper_check';
+        $paperResult = $event->getSubject();
+
+        if ($paperResult['type'] === 'homework') {
+            $this->notifyHomeworkResult($paperResult);
+        } else if ($paperResult['type'] === 'testpaper') {
+            $this->notifyTestpaperResult($paperResult);
+        }
+    }
+
+    protected function notifyTestpaperResult($result)
+    {
+        $smsType = 'sms_testpaper_check';
 
         if ($this->getSmsService()->isOpen($smsType)) {
-            $paperResult = $event->getSubject();
-            $courseSet   = $this->getCourseSetService()->getCourseSet($paperResult['courseSetId']);
+            $parameters = array();
+
+            $courseSet = $this->getCourseSetService()->getCourseSet($result['courseSetId']);
 
             if (!empty($courseSet)) {
                 $courseSet['title']         = StringToolkit::cutter($courseSet['title'], 20, 15, 4);
-                $task                       = $this->getTaskService()->getTaskByCourseIdAndActivityId($paperResult['courseId'], $paperResult['lessonId']);
-                $parameters['lesson_title'] = '《' . $task['title'] . '》' . '(试卷)';
-                $parameters['course_title'] = '《' . $courseSet['title'] . '》';
-                $description                = $parameters['course_title'] . ' ' . $parameters['lesson_title'] . '试卷批阅提醒';
-                $userId                     = $paperResult['userId'];
+                $task                       = $this->getTaskService()->getTaskByCourseIdAndActivityId(
+                    $result['courseId'],
+                    $result['lessonId']
+                );
+                $parameters['lesson_title'] = '《'.$task['title'].'》的试卷';
+                $parameters['course_title'] = '《'.$courseSet['title'].'》';
+                $description                = $parameters['course_title'].' '.$parameters['lesson_title'].'批阅提醒';
+                $userId                     = $result['userId'];
+                $this->getSmsService()->smsSend($smsType, array($userId), $description, $parameters);
+            }
+        }
+    }
+
+    protected function notifyHomeworkResult($result)
+    {
+        $smsType = 'sms_homework_check';
+
+        if ($this->getSmsService()->isOpen($smsType)) {
+            $parameters = array();
+
+            $courseSet = $this->getCourseSetService()->getCourseSet($result['courseSetId']);
+
+            if (!empty($courseSet)) {
+                $courseSet['title']         = StringToolkit::cutter($courseSet['title'], 20, 15, 4);
+                $task                       = $this->getTaskService()->getTaskByCourseIdAndActivityId(
+                    $result['courseId'],
+                    $result['lessonId']
+                );
+                $parameters['lesson_title'] = '《'.$task['title'].'》的作业';
+                $parameters['course_title'] = '《'.$courseSet['title'].'》';
+                $description                = $parameters['course_title'].' '.$parameters['lesson_title'].'批阅提醒';
+                $userId                     = $result['userId'];
                 $this->getSmsService()->smsSend($smsType, array($userId), $description, $parameters);
             }
         }
