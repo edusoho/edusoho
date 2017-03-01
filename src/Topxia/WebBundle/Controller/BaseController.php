f914d96d7b899f0e0694f2e7574e748605abdfe8
杨佳龙 <yangjlong315@gmail.com>
2017-03-01 17:09:18 +0800

    fix check self host

--- src/Topxia/WebBundle/Controller/BaseController.php
+++ src/Topxia/WebBundle/Controller/BaseController.php
@@ -4,6 +4,7 @@ namespace Topxia\WebBundle\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Codeages\Biz\Framework\Service\Exception\AccessDeniedException;
 use Topxia\Common\ArrayToolkit;
+use Topxia\Component\Payment\Request;
 use Topxia\Service\User\CurrentUser;
 use Topxia\Service\Common\ServiceKernel;
 use Topxia\Service\Common\ServiceEvent;
@@ -128,11 +129,11 @@ abstract class BaseController extends Controller
         return new JsonResponse($data);
     }
 
-    private function isSelfHost($request,$url)
+    private function isSelfHost($request, $url)
     {
         $host = $request->getHost();
-        preg_match("/^(http[s]:\/\/)?([^\/]+)/i",$url, $matches);
-        $ulrHost = $matches[2];
+        preg_match("/^(http[s]:\/\/)?([^\/]+)/i", $url, $matches);
+        $ulrHost = empty($matches[2]) ? '' : $matches[2];
         return $host == $ulrHost;
     }
 
@@ -144,8 +145,10 @@ abstract class BaseController extends Controller
             $targetPath = $request->getSession()->get('_target_path');
         } else {
             $targetPath = $request->headers->get('Referer');
-            if($this->isSelfHost($request,$targetPath) === false){
-                $targetPath = '';
+            if (!empty($targetPath)) {
+                if ($this->isSelfHost($request, $targetPath) === false) {
+                    $targetPath = '';
+                }
             }
         }
 
