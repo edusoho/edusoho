a097dd9f524941fbb59e56727ecf3a9be6cc60bc
Allen Lau <liuyangyang@howzhi.com>
2017-03-01 14:52:37 +0800

    udpate

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -81,7 +81,8 @@ class TaskController extends BaseController
         // 1. 有时间限制设置
         // 2. 课时为视频课时
         // 3. 视频课时非优酷等外链视频时提示购买
-        if (empty($task['isFree']) && !(!empty($course['tryLookable']) && $task['type'] == 'video' && $task['mediaSource'] == 'self')) {
+        $taskCanTryLook = $course['tryLookable'] && $task['type'] == 'video' && $task['mediaSource'] == 'self';
+        if (empty($task['isFree']) && !$taskCanTryLook) {
             if (!$user->isLogin()) {
                 throw $this->createAccessDeniedException();
             }
