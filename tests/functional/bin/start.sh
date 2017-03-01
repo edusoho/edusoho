fd0ac4fe3909e581817dc7145fb2d8c5b992efb4
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-01 23:54:20 +0800

    test

--- tests/functional/bin/start.sh
+++ tests/functional/bin/start.sh
@@ -4,6 +4,7 @@ wget http://ojc8jepus.bkt.clouddn.com/functional-nginx-conf;
 mv functional-nginx-conf /etc/nginx/sites-enabled/;
 nginx -t;
 service nginx start 2>&1;
+
 ps aux|grep nginx;
 echo 'nginx started...';
 service php5-fpm start;
