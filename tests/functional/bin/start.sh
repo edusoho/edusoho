a48487a13ee3787f1d3b3b6f51e8e2a532f9a768
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-01 21:06:34 +0800

    update start.sh

--- tests/functional/bin/start.sh
+++ tests/functional/bin/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+wget http://ojc8jepus.bkt.clouddn.com/functional-nginx-conf;
+mv functional-nginx-conf /etc/nginx/sites-enabled/;
 service nginx start;
 ps aux|grep nginx;
-wget http://ojc8jepus.bkt.clouddn.com/es-latest.conf;
+echo 'nginx started...';
+service php5-fpm start;
+echo 'php5-fpm started...';
