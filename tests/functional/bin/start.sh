64bbb8b5a39dcd5704751de99dc4dc9972f73569
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-02 11:23:25 +0800

    test

--- tests/functional/bin/start.sh
+++ tests/functional/bin/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget http://ojc8jepus.bkt.clouddn.com/functional-nginx-conf;
-mv functional-nginx-conf /etc/nginx/sites-enabled/;
-nginx -t;
-service nginx start 2>&1;
+# wget http://ojc8jepus.bkt.clouddn.com/functional-nginx-conf;
+# mv functional-nginx-conf /etc/nginx/sites-enabled/;
+# nginx -t;
+# service nginx start 2>&1;
 
-ps aux|grep nginx;
-echo 'nginx started...';
-service php5-fpm start;
-echo 'php5-fpm started...';
+# ps aux|grep nginx;
+# echo 'nginx started...';
+# service php5-fpm start;
+# echo 'php5-fpm started...';
