aa8554b7220cb716e68b00fdfc3944d5e3af755c
=tianshengjie <tianshengjie@howzhi.com>
2017-03-06 14:00:09 +0800

    好友

--- api/src/Topxia/Api/Resource/MeFriends.php
+++ api/src/Topxia/Api/Resource/MeFriends.php
@@ -9,13 +9,18 @@ class MeFriends extends BaseResource
 {
     public function get(Application $app, Request $request)
     {
+
         $user = getCurrentUser();
         $start = $request->query->get('start', 0);
         $limit = $request->query->get('limit', 10);
+
         $friends = $this->getUserService()->findFriends($user['id'], $start, $limit);
+
         $count = $this->getUserService()->findFriendCount($user['id']);
+        $friends = $this->multicallFilter('User', $friends);
+
         return array(
-            'data'  => filters($friends, 'user'),
+            'data'  => $friends,
             'total' => $count
         );
     }
