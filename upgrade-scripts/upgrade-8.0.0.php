3dec6f4fd7a3cb797a1b3ff749b18f139f04e23b
wangdanyue <wangdanyue@howzhi.com>
2017-03-07 16:27:38 +0800

    question-upgrade-add

--- upgrade-scripts/upgrade-8.0.0.php
+++ upgrade-scripts/upgrade-8.0.0.php
@@ -39,6 +39,7 @@ class EduSohoUpgrade extends AbstractUpdater
         // $this->c2courseMigrate();
         // $this->c2CourseLessonMigrate();
         // $this->c2testpaperMigrate();
+        $this->c2QuestionMigrate();
         $this->migrate();
     }
 
@@ -1611,93 +1612,129 @@ class EduSohoUpgrade extends AbstractUpdater
          ");
     }
 
+    protected function c2QuestionMigrate()
+    {
+        if (!$this->isFieldExist('question', 'courseId')) {
+            $this->exec("
+                ALTER TABLE question add courseId INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `target`
+            ");
+        }
+
+        if (!$this->isFieldExist('question', 'lessonId')) {
+            $this->exec("
+                ALTER TABLE question add lessonId INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `courseId`
+            ");
+        }
+
+        $sql = "SELECT * FROM question";
+        $questions = $this->getConnection()->fetchAll($sql);
+
+        foreach ($questions as $question) {
+            $targetArr = explode('/', $question['target']);
+            $courseArr = explode('-', $targetArr[0]);
+            $lessonId = 0;
+            if (!empty($targetArr[1])) {
+                $lessonArr = explode('-', $targetArr[1]);
+                $lessonId = $lessonArr[1];
+            }
+
+            $sql = "UPDATE question set courseId = {$courseArr[1]},lessonId={$lessonId} WHERE id = {$question['id']}";
+            $this->exec($sql);
+        }
+
+        if (!$this->isFieldExist('question_favorite', 'targetType')) {
+            $this->exec("
+                ALTER TABLE question_favorite ADD targetType VARCHAR(50) NOT NULL DEFAULT '' AFTER `questionId`
+            ");
+        }
+
+        if (!$this->isFieldExist('question_favorite', 'targetId')) {
+            $this->exec("
+                ALTER TABLE question_favorite ADD targetId INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `targetType`
+            ");
+        }
+
+        $sql = "SELECT * FROM question_favorite";
+        $favorites = $this->getConnection()->fetchAll($sql);
+
+        foreach ($favorites as $favorite) {
+            $targetArr = explode('/', $favorite['target']);
+
+            $sql = "UPDATE question_favorite set targetId = {$targetArr[1]},targetType={$targetArr[0]} WHERE id = {$favorite['id']}";
+            $this->exec($sql);
+        }
+    }
+
     protected function migrate()
     {
-      if($this->isFieldExist('course_note', 'lessonId'))
-      {
-        $this->exec("ALTER TABLE `course_note` CHANGE `lessonId` `taskId` INT(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID';");
-      } 
+        if ($this->isFieldExist('course_note', 'lessonId')) {
+            $this->exec("ALTER TABLE `course_note` CHANGE `lessonId` `taskId` INT(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID';");
+        }
 
-      if(!$this->isFieldExist('course_note', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE `course_note` ADD COLUMN `courseSetId` INT(10) UNSIGNED NOT NULL;");
-      }
-      $this->exec("UPDATE course_note SET courseSetId = courseId");
+        if (!$this->isFieldExist('course_note', 'courseSetId')) {
+            $this->exec("ALTER TABLE `course_note` ADD COLUMN `courseSetId` INT(10) UNSIGNED NOT NULL;");
+        }
+        $this->exec("UPDATE course_note SET courseSetId = courseId");
 
+        if (!$this->isFieldExist('course_review', 'courseSetId')) {
+            $this->exec("ALTER TABLE `course_review` add COLUMN `courseSetId` int(10) UNSIGNED NOT NULL DEFAULT '0';");
+        }
+        $this->exec("UPDATE course_review SET courseSetId = courseId");
 
-      if(!$this->isFieldExist('course_review', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE `course_review` add COLUMN `courseSetId` int(10) UNSIGNED NOT NULL DEFAULT '0';");
-      }
-      $this->exec("UPDATE course_review SET courseSetId = courseId");
+        if (!$this->isFieldExist('course_thread', 'courseSetId')) {
+            $this->exec("ALTER TABLE `course_thread` ADD courseSetId INT(10) UNSIGNED NOT NULL;");
+        }
+        $this->exec("UPDATE course_thread SET courseSetId = courseId");
 
+        if ($this->isFieldExist('course_thread', 'lessonId')) {
+            $this->exec("ALTER TABLE `course_thread` CHANGE `lessonId` `taskId` INT(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID';");
+        }
 
-      if(!$this->isFieldExist('course_thread', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE `course_thread` ADD courseSetId INT(10) UNSIGNED NOT NULL;");
-      }
-      $this->exec("UPDATE course_thread SET courseSetId = courseId");
+        if ($this->isFieldExist('course_thread_post', 'lessonId')) {
+            $this->exec("ALTER TABLE `course_thread_post` CHANGE `lessonId` `taskId` INT(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID';");
+        }
 
-      if($this->isFieldExist('course_thread', 'lessonId'))
-      {
-        $this->exec("ALTER TABLE `course_thread` CHANGE `lessonId` `taskId` INT(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID';");
-      }
+        if (!$this->isFieldExist('course_favorite', 'courseSetId')) {
+            $this->exec('ALTER TABLE course_favorite ADD courseSetId INT(10) NOT NULL DEFAULT 0 COMMENT "课程ID";');
+        }
+        $this->exec("UPDATE course_favorite SET courseSetId = courseId");
 
-      if($this->isFieldExist('course_thread_post', 'lessonId'))
-      {
-        $this->exec("ALTER TABLE `course_thread_post` CHANGE `lessonId` `taskId` INT(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID';");
-      }
+        if ($this->isFieldExist('course_favorite', 'courseId')) {
+            $this->exec("ALTER TABLE course_favorite MODIFY courseId INT(10) unsigned NOT NULL COMMENT '教学计划ID';");
+        }
 
-      if(!$this->isFieldExist('course_favorite', 'courseSetId'))
-      {
-        $this->exec('ALTER TABLE course_favorite ADD courseSetId INT(10) NOT NULL DEFAULT 0 COMMENT "课程ID";');
-      }
-      $this->exec("UPDATE course_favorite SET courseSetId = courseId");
+        if ($this->isFieldExist('course_favorite', 'courseSetId')) {
+            $this->exec("ALTER TABLE `course_favorite` CHANGE `courseSetId` `courseSetId` INT(10) NOT NULL DEFAULT '0' COMMENT '课程ID';");
+        }
 
-      if($this->isFieldExist('course_favorite', 'courseId'))
-      {
-        $this->exec("ALTER TABLE course_favorite MODIFY courseId INT(10) unsigned NOT NULL COMMENT '教学计划ID';");
-      }
+        if (!$this->isFieldExist('course_material', 'courseSetId')) {
+            $this->exec("ALTER TABLE course_material ADD COLUMN courseSetId int(10) default 0 COMMENT '课程ID';");
+        }
+        $this->exec("UPDATE course_material SET courseSetId = courseId");
 
-      if($this->isFieldExist('course_favorite', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE `course_favorite` CHANGE `courseSetId` `courseSetId` INT(10) NOT NULL DEFAULT '0' COMMENT '课程ID';");
-      }
+        if (!$this->isFieldExist('course_member', 'courseSetId')) {
+            $this->exec("ALTER TABLE `course_member` ADD COLUMN  `courseSetId` int(10) unsigned NOT NULL COMMENT '课程ID';");
+        }
+        $this->exec("UPDATE course_member SET courseSetId = courseId");
 
-      if(!$this->isFieldExist('course_material', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE course_material ADD COLUMN courseSetId int(10) default 0 COMMENT '课程ID';");
-      }
-      $this->exec("UPDATE course_material SET courseSetId = courseId");
+        if ($this->isFieldExist('course_member', 'courseId')) {
+            $this->exec("ALTER TABLE `course_member` MODIFY courseId INT(10) unsigned NOT NULL COMMENT '教学计划ID';");
+        }
 
-      if(!$this->isFieldExist('course_member', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE `course_member` ADD COLUMN  `courseSetId` int(10) unsigned NOT NULL COMMENT '课程ID';");
-      }
-      $this->exec("UPDATE course_member SET courseSetId = courseId");
+        if (!$this->isFieldExist('classroom_courses', 'courseSetId')) {
+            $this->exec("ALTER TABLE `classroom_courses` ADD COLUMN `courseSetId` INT(10) NOT NULL DEFAULT '0' COMMENT '课程ID';");
+        }
+        $this->exec("UPDATE classroom_courses SET courseSetId = courseId");
 
-      if($this->isFieldExist('course_member', 'courseId'))
-      {
-        $this->exec("ALTER TABLE `course_member` MODIFY courseId INT(10) unsigned NOT NULL COMMENT '教学计划ID';");
-      }
+        $this->exec('UPDATE course_member AS cm INNER JOIN c2_course c ON c.id = cm.courseId SET cm.courseSetId=c.courseSetId;');
+        $this->exec("UPDATE block_template SET templateName = 'block/live-top-banner.template.html.twig' WHERE code = 'live_top_banner';");
+        $this->exec("UPDATE block_template SET templateName = 'block/open-course-top-banner.template.html.twig' WHERE code = 'open_course_top_banner';");
 
-      if(!$this->isFieldExist('classroom_courses', 'courseSetId'))
-      {
-        $this->exec("ALTER TABLE `classroom_courses` ADD COLUMN `courseSetId` INT(10) NOT NULL DEFAULT '0' COMMENT '课程ID';");
-      }
-      $this->exec("UPDATE classroom_courses SET courseSetId = courseId");
-
-
-      $this->exec('UPDATE course_member AS cm INNER JOIN c2_course c ON c.id = cm.courseId SET cm.courseSetId=c.courseSetId;');
-      $this->exec("UPDATE block_template SET templateName = 'block/live-top-banner.template.html.twig' WHERE code = 'live_top_banner';");
-      $this->exec("UPDATE block_template SET templateName = 'block/open-course-top-banner.template.html.twig' WHERE code = 'open_course_top_banner';");
-
-      if($this->isTableExist('live_activity')) 
-      {
-        $this->exec("UPDATE `live_activity` SET roomCreated = 1 WHERE liveId > 0;");
-      }
-      $this->exec("UPDATE crontab_job SET targetType = 'task' WHERE targetType = 'lesson' AND name = 'SmsSendOneDayJob';");
-      $this->exec("UPDATE crontab_job SET targetType = 'task' WHERE targetType = 'lesson' AND name = 'SmsSendOneHourJob';");
+        if ($this->isTableExist('live_activity')) {
+            $this->exec("UPDATE `live_activity` SET roomCreated = 1 WHERE liveId > 0;");
+        }
+        $this->exec("UPDATE crontab_job SET targetType = 'task' WHERE targetType = 'lesson' AND name = 'SmsSendOneDayJob';");
+        $this->exec("UPDATE crontab_job SET targetType = 'task' WHERE targetType = 'lesson' AND name = 'SmsSendOneHourJob';");
     }
 
     /**
