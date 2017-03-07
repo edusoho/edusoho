fc0bfe0b3785c4e6080027f34d22e8c5dca6aee2
hamster <malianbo@163.com>
2017-03-07 17:12:07 +0800

    fix：免费任务不需要显示免费试看等标签

--- src/Biz/Task/Service/Impl/TaskServiceImpl.php
+++ src/Biz/Task/Service/Impl/TaskServiceImpl.php
@@ -239,10 +239,12 @@ class TaskServiceImpl extends BaseService implements TaskService
         $activities = $this->getActivityService()->findActivities($activityIds, true);
         $activities = ArrayToolkit::index($activities, 'id');
 
-        array_walk($tasks, function (&$task) use ($activities) {
-            $activity = $activities[$task['activityId']];
-            $task['activity'] = $activity;
-        }
+        array_walk(
+            $tasks,
+            function (&$task) use ($activities) {
+                $activity = $activities[$task['activityId']];
+                $task['activity'] = $activity;
+            }
         );
 
         return $tasks;
@@ -265,7 +267,7 @@ class TaskServiceImpl extends BaseService implements TaskService
             }
             $task = $this->setTaskLockStatus($tasks, $task);
             //设置第一个发布的任务为解锁的
-            if ($task['status'] == 'published' && empty($isLock)) {
+            if ($task['status'] == 'published' && !$isLock) {
                 $task['lock'] = false;
                 $isLock = true;
             }
