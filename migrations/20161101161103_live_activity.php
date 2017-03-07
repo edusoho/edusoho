bede3a886d674a857abc1e571b71df8365ed8ff4
nifeng <fengni@fengdeMacBook-Pro.local>
2017-03-07 11:57:27 +0800

    merge

--- migrations/20161101161103_live_activity.php
+++ migrations/20161101161103_live_activity.php
@@ -15,6 +15,9 @@ class LiveActivity extends Migration
           `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
           `liveId` int(11) NOT NULL COMMENT '直播间ID',
           `liveProvider` int(11) NOT NULL COMMENT '直播供应商',
+          `replayStatus` enum('ungenerated','generating','generated','videoGenerated') NOT NULL DEFAULT 'ungenerated' COMMENT '回放状态',
+          `mediaId` INT(11) UNSIGNED DEFAULT 0 COMMENT '视频文件ID',
+          `roomCreated` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '直播教室是否已创建',
           PRIMARY KEY (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
     }
--- migrations/20170118161613_course_add_publishedTaskNum.php
+++ migrations/20170118161613_course_add_publishedTaskNum.php
@@ -12,7 +12,6 @@ class CourseAddPublishedTaskNum extends Migration
         $biz = $this->getContainer();
         $db = $biz['db'];
         $db->exec("
-            ALTER TABLE `c2_course` ADD COLUMN `publishedTaskNum` INT(10) DEFAULT '0' COMMENT '已发布的任务数' AFTER `taskNum`;
             CREATE VIEW view_course_task AS
             SELECT count(id) as num, courseid FROM course_task WHERE  STATUS = 'published' GROUP BY courseId
             UPDATE `c2_course` ce, view_course_task vk SET ce.`publishedTaskNum` = vk.`num` WHERE vk.`courseid` = ce.id;
@@ -24,10 +23,5 @@ class CourseAddPublishedTaskNum extends Migration
      */
     public function down()
     {
-        $biz = $this->getContainer();
-        $db = $biz['db'];
-        $db->exec('
-           ALTER TABLE `c2_course` DROP COLUMN `publishedTaskNum`;
-        ');
     }
 }
