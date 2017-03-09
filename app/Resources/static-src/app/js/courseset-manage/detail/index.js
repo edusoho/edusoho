c55f6c6cb4f67460004ebe2f1c330c19afee0c0b
wuli <wuli@howzhi.com>
2017-03-09 15:25:41 +0800

    change detail js

--- app/Resources/static-src/app/js/courseset-manage/detail/index.js
+++ app/Resources/static-src/app/js/courseset-manage/detail/index.js
@@ -3,37 +3,52 @@ import React from 'react';
 import MultiInput from 'app/common/component/multi-input';
 import postal from 'postal';
 
-CKEDITOR.replace('summary', {
-  allowedContent: true,
-  toolbar: 'Detail',
-  filebrowserImageUploadUrl: $('#courseset-summary-field').data('imageUploadUrl')
-});
+class detail {
+  constructor() {
+    this.init();
+  }
 
-renderMultiGroupComponent('course-goals', 'goals');
-renderMultiGroupComponent('intended-students', 'audiences');
+  init() {
+    this.initCkeditor();
+    this.renderMultiGroupComponent('course-goals', 'goals');
+    this.renderMultiGroupComponent('intended-students', 'audiences');
+    this.submitEvent();
+  }
 
-$('#courseset-submit').click(function (evt) {
-  publishAddMessage();
-  $(evt.currentTarget).button('loading');
-  $('#courseset-detail-form').submit();
-});
+  initCkeditor() {
+    CKEDITOR.replace('summary', {
+      allowedContent: true,
+      toolbar: 'Detail',
+      filebrowserImageUploadUrl: $('#courseset-summary-field').data('imageUploadUrl')
+    });
+  }
 
-function renderMultiGroupComponent(elementId, name) {
-  let datas = $('#' + elementId).data('init-value');
-  ReactDOM.render(<MultiInput
-    blurIsAdd={true}
-    sortable={true}
-    dataSource={datas}
-    inputName={name + "[]"}
-    outputDataElement={name}
-  />,
-    document.getElementById(elementId)
-  );
+  renderMultiGroupComponent(elementId, name) {
+    let datas = $('#' + elementId).data('init-value');
+    ReactDOM.render(<MultiInput
+      blurIsAdd={true}
+      sortable={true}
+      dataSource={datas}
+      inputName={name + "[]"}
+      outputDataElement={name} />,
+      document.getElementById(elementId)
+    );
+  }
+
+  submitEvent() {
+    $('#courseset-submit').click(function (evt) {
+      publishAddMessage();
+      $(evt.currentTarget).button('loading');
+      $('#courseset-detail-form').submit();
+    });
+  }
+
+  publishAddMessage() {
+    postal.publish({
+      channel: "courseInfoMultiInput",
+      topic: "addMultiInput",
+    });
+  }
 }
 
-function publishAddMessage() {
-  postal.publish({
-    channel: "courseInfoMultiInput",
-    topic: "addMultiInput",
-  });
-}
+new detail();
