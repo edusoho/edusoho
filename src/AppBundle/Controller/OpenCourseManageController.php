bf0e011b244d0a244ced7546d28b9942187dae8c
retamia <retamia@gmail.com>
2017-03-11 18:46:20 +0800

    fix: 公开课-推荐设置-添加课程报错

--- src/AppBundle/Controller/OpenCourseManageController.php
+++ src/AppBundle/Controller/OpenCourseManageController.php
@@ -372,15 +372,15 @@ class OpenCourseManageController extends BaseController
         $recommendedCourses[$recommend['id']] = $this->getTypeCourseService($recommend['type'])->getCourse($recommend['recommendCourseId']);
         }*/
         $courseSetIds = ArrayToolkit::column($recommends, 'recommendCourseId');
-        $recommendedCourses = $this->getCourseSetService()->findCourseSetsByIds($courseSetIds);
+        $commendedCourseSets = $this->getCourseSetService()->findCourseSetsByIds($courseSetIds);
 
         $coursesPrice = $this->_findCoursesPriceInterval($courseSetIds);
-        $users = $this->_getTeacherUsers($recommendedCourses);
+        $users = $this->_getTeacherUsers($commendedCourseSets);
 
         return $this->render(
             'open-course-manage/open-course-marketing.html.twig',
             array(
-                'courses' => $recommendedCourses,
+                'courseSets' => $commendedCourseSets,
                 'users' => $users,
                 'course' => $course,
                 'coursesPrice' => $coursesPrice,
