1185392c4490d9938f260c736cbd53914ad35faa
Allen Lau <liuyangyang@howzhi.com>
2017-03-14 17:54:25 +0800

    fix

--- src/Topxia/WebBundle/Resources/public/js/controller/settings/avatar-crop.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/settings/avatar-crop.js
@@ -1,43 +1,44 @@
 define(function (require, exports, module) {
-    var Notify = require('common/bootstrap-notify');
-    var ImageCrop = require('edusoho.imagecrop');
-    var store = require('store');
-    exports.run = function () {
-        var imageCrop = new ImageCrop({
-            element: "#avatar-crop",
-            group: "user",
-            cropedWidth: 200,
-            cropedHeight: 200
-        });
+  var Notify = require('common/bootstrap-notify');
+  var ImageCrop = require('edusoho.imagecrop');
+  var store = require('store');
+  exports.run = function () {
+    var imageCrop = new ImageCrop({
+      element: "#avatar-crop",
+      group: "user",
+      cropedWidth: 200,
+      cropedHeight: 200
+    });
 
-        imageCrop.on("afterCrop", function (response) {
-            var url = $("#upload-avatar-btn").data("url");
-            $.post(url, {images: response}, function () {
-                var courseGuestUrl = store.get('course-guest-page-url');
-                if (courseGuestUrl) {
-                    document.location.href = courseGuestUrl;
-                } else {
-                    document.location.href = $("#upload-avatar-btn").data("gotoUrl");
-                }
-            });
-        });
+    imageCrop.on("afterCrop", function (response) {
+      var url = $("#upload-avatar-btn").data("url");
+      $.post(url, {images: response}, function () {
+        var courseGuestUrl = store.get('COURSE-GUEST-PAGE-URL');
+        if (courseGuestUrl) {
+          store.remove('COURSE-GUEST-PAGE-URL');
+          document.location.href = courseGuestUrl;
+        } else {
+          document.location.href = $("#upload-avatar-btn").data("gotoUrl");
+        }
+      });
+    });
 
-        $("#upload-avatar-btn").click(function (e) {
-            e.stopPropagation();
+    $("#upload-avatar-btn").click(function (e) {
+      e.stopPropagation();
 
-            imageCrop.crop({
-                imgs: {
-                    large: [200, 200],
-                    medium: [120, 120],
-                    small: [48, 48]
-                }
-            });
+      imageCrop.crop({
+        imgs: {
+          large: [200, 200],
+          medium: [120, 120],
+          small: [48, 48]
+        }
+      });
 
-        })
+    })
 
-        $('.go-back').click(function () {
-            history.go(-1);
-        });
-    };
+    $('.go-back').click(function () {
+      history.go(-1);
+    });
+  };
 
 });
