d810c3491996f47b5f0d3603d8323c9ac9e54090
wuli <wuli@howzhi.com>
2017-03-15 22:00:53 +0800

    merge

--- app/Resources/static-src/app/js/testpaper/widget/part.js
+++ app/Resources/static-src/app/js/testpaper/widget/part.js
@@ -2,15 +2,7 @@ import 'app/common/watermark';
 import { Browser } from 'common/utils';
 
 export const initScrollbar = ()=> {
-	// let $taskTestpaperBodyIframe = $('.js-task-testpaper-body-iframe');
 	let $paneCard = $('.js-panel-card');
-  // $taskTestpaperBodyIframe.css('overflow-y','auto');
-	// if (Browser.ie  || Browser.firefox) {
-	// 	$taskTestpaperBodyIframe.css('overflow-y','auto')
-	// } else {
-	// 	$taskTestpaperBodyIframe.perfectScrollbar();
-	// 	$taskTestpaperBodyIframe.perfectScrollbar('update'); 
-	// }
   $paneCard.perfectScrollbar();
 	$paneCard.perfectScrollbar('update');
 }
@@ -38,17 +30,7 @@ export const testpaperCardLocation = () => {
 			$btn.addClass('doing').siblings('.doing').removeClass('doing');
 		}
     let $current = $($btn.data('anchor'));
-		// let $testpaperBodyIframe = $('.js-task-testpaper-body-iframe');
-		//iframe中的滚动事件
     $("body").scrollTop($current.offset().top);
-
-		// if($testpaperBodyIframe.length>0) {
-		// 	$(".js-task-testpaper-body-iframe").scrollTop($current.offset().top);
-		// 	$(".js-task-testpaper-body-iframe").perfectScrollbar('update');
-		// }
-		// else {
-		// 	$("body").scrollTop($current.offset().top);
-		// }
 	})
 }
 
