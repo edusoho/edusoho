7f4248b098d37892214e1a0cf9155c83877dad02
wuli <wuli@howzhi.com>
2017-03-15 18:22:25 +0800

    merge

---
+++ src/Topxia/WebBundle/Resources/public/js/controller/attachment/sdk-upload-modal.js
@@ -0,0 +1,42 @@
+define(function (require, exports, module) {
+
+  require('new-uploader');
+  const $uploader = $('#uploader-container');
+
+  let uploader = new UploaderSDK({
+    id: $uploader.attr('id'),
+    initUrl: $uploader.data('initUrl'),
+    finishUrl: $uploader.data('finishUrl'),
+    accept: $uploader.data('accept'),
+    process: $uploader.data('process'),
+    fileSingleSizeLimit: $uploader.data('fileSingleSizeLimit'),
+    ui: 'single'
+  });
+
+  uploader.on('file.finish', (file) => {
+    if (file.length && file.length > 0) {
+      let minute = parseInt(file.length / 60);
+      let second = Math.round(file.length % 60);
+      $("#minute").val(minute);
+      $("#second").val(second);
+      $("#length").val(minute * 60 + second);
+    }
+
+    const $metas = $('[data-role="metas"]');
+    const $ids = $('.' + $metas.data('idsClass'));
+    const $list = $('.' + $metas.data('listClass'));
+
+    $.get('/attachment/file/' + file.id + '/show', function (html) {
+      $list.append(html);
+      $ids.val(file.id);
+      $('#attachment-modal').modal('hide');
+      $list.siblings('.js-upload-file').hide();
+    })
+  });
+
+  //只执行一次
+  $('#attachment-modal').one('hide.bs.modal', (event) => {
+    uploader.destroy();
+    uploader = null;
+  });
+});
