7dea79683e4603f634d87a63e92080c783a4c5cd
retamia <retamia@gmail.com>
2017-03-16 19:03:18 +0800

    feature: 图文活动的草稿

--- src/AppBundle/Controller/Activity/TextController.php
+++ src/AppBundle/Controller/Activity/TextController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Activity;
 
 use AppBundle\Controller\BaseController;
 use Biz\Activity\Service\ActivityService;
+use Biz\Course\Service\CourseDraftService;
 use Symfony\Component\HttpFoundation\Request;
 
 class TextController extends BaseController implements ActivityActionInterface
@@ -48,18 +49,59 @@ class TextController extends BaseController implements ActivityActionInterface
 
     public function editAction(Request $request, $id, $courseId)
     {
+        $user = $this->getCurrentUser();
         $activity = $this->getActivityService()->getActivity($id);
         $text = $this->getActivityService()->getActivityConfig('text')->get($activity['mediaId']);
-
+        $draft = $this->getCourseDraftService()->getCourseDraftByCourseIdAndActivityIdAndUserId($courseId, $activity['id'], $user->id);
         return $this->render('activity/text/modal.html.twig', array(
             'activity' => $activity,
             'text' => $text,
+            'courseId' => $courseId,
+            'draft' => $draft
         ));
+    }
+
+    public function autoSaveAction(Request $request, $courseId, $activityId = 0)
+    {
+        $user = $this->getCurrentUser();
+        if (!$user->isLogin()) {
+            throw $this->createAccessDeniedException('user not login');
+        }
+
+        $content = $request->request->get('content', '');
+
+        if (empty($content)) {
+            return $this->createJsonResponse(true);
+        }
+
+        $draft = $this->getCourseDraftService()->getCourseDraftByCourseIdAndActivityIdAndUserId($courseId, $activityId,
+            $user->getId());
+
+        if (empty($draft)) {
+            $draft = array(
+                'activityId' => $activityId,
+                'title' => '',
+                'content' => $content,
+                'courseId' => $courseId,
+            );
+
+            $this->getCourseDraftService()->createCourseDraft($draft);
+        } else {
+            $draft['content'] = $content;
+            $this->getCourseDraftService()->updateCourseDraft($draft['id'], $draft);
+        }
+
+        return $this->createJsonResponse(true);
     }
 
     public function createAction(Request $request, $courseId)
     {
-        return $this->render('activity/text/modal.html.twig');
+        $user = $this->getCurrentUser();
+        $draft = $this->getCourseDraftService()->getCourseDraftByCourseIdAndActivityIdAndUserId($courseId, 0, $user->id);
+        return $this->render('activity/text/modal.html.twig', array(
+            'courseId' => $courseId,
+            'draft' => $draft
+        ));
     }
 
     public function finishConditionAction($activity)
@@ -69,6 +111,14 @@ class TextController extends BaseController implements ActivityActionInterface
         return $this->render('activity/text/finish-condition.html.twig', array(
             'media' => $media,
         ));
+    }
+
+    /**
+     * @return CourseDraftService
+     */
+    public function getCourseDraftService()
+    {
+        return $this->createService('Course:CourseDraftService');
     }
 
     /**
