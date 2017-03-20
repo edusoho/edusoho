03cd32c828b81e3246ec4d23cafff69c0ebfc925
retamia <retamia@gmail.com>
2017-03-20 11:35:55 +0800

    fix: 图文任务自动完成

--- src/Biz/Activity/Type/Text.php
+++ src/Biz/Activity/Type/Text.php
@@ -65,9 +65,7 @@ class Text extends Activity
         $activity = $this->getActivityService()->getActivity($activityId);
         $textActivity = $this->getTextActivityDao()->get($activity['mediaId']);
 
-        return !empty($result)
-            && $textActivity['finishType'] == 'time'
-            && $result >= $textActivity['finishDetail'];
+        return !empty($result) && $result >= $textActivity['finishDetail'];
     }
 
     public function delete($targetId)
