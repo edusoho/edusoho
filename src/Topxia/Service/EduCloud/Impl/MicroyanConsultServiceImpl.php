64637c5d1add2085256c2b1a35e72e10c27dea66
xingjingang <xingjingang@howzhi.com>
2017-03-23 18:44:34 +0800

    edit

--- src/Topxia/Service/EduCloud/Impl/MicroyanConsultServiceImpl.php
+++ src/Topxia/Service/EduCloud/Impl/MicroyanConsultServiceImpl.php
@@ -34,6 +34,7 @@ class MicroyanConsultServiceImpl extends BaseService implements ConsultService
             'cloud_consult_login_url' => '',
             'cloud_consult_js' => ''
         );
+        var_dump($account);
 
         if ((isset($account['code']) && $account['code']== '10000') || (isset($jsResource['code']) && $jsResource['code']== '10000')) {
             $cloudConsult['cloud_consult_is_buy'] = 0;
@@ -41,6 +42,7 @@ class MicroyanConsultServiceImpl extends BaseService implements ConsultService
             $cloudConsult['cloud_consult_is_buy'] = 0;
             $cloudConsult['error'] = '账号已过期,请联系客服人员:4008041114！';
         } else if(isset($account['error']) || isset($jsResource['error'])) {
+            var_dump($account);
             $cloudConsult['cloud_consult_is_buy'] = 0;
         } else {
             $cloudConsult['cloud_consult_is_buy'] = 1;
