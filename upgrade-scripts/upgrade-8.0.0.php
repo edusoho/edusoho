1413ff31836a320dec3f445d0a704be0c85a0ade
nifeng <fengni@fengdeMacBook-Pro.local>
2017-03-25 14:28:09 +0800

    fix

--- upgrade-scripts/upgrade-8.0.0.php
+++ upgrade-scripts/upgrade-8.0.0.php
@@ -53,6 +53,7 @@ class EduSohoUpgrade extends AbstractUpdater
         'c2LiveActivity',
         'c2CourseTaskView',
         'c2CourseTaskResult',
+        'c2ActivityLearnLog',
         'c2Exercise',
         'c2Homework',
         'c2CourseMaterial',
@@ -1253,6 +1254,47 @@ class EduSohoUpgrade extends AbstractUpdater
             UPDATE `course_task_result` cl,  `course_task` ck SET cl.`activityId`= ck.`activityId` WHERE cl.`courseTaskId` = ck.`id`;
         "
         );
+    }
+
+    protected function c2ActivityLearnLog()
+    {
+        if (!$this->isTableExist('activity_learn_log')) {
+            $this->exec(
+                "
+                CREATE TABLE `activity_learn_log` (
+                  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+                  `activityId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '教学活动id',          
+                  `userId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用户id',
+                  `event` varchar(255) NOT NULL DEFAULT '' COMMENT '',
+                  `data` text COMMENT '',
+                  `watchTime` int(10) unsigned NOT NULL DEFAULT '0',
+                  `courseTaskId` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '教学活动id',
+                  `learnedTime` int(11) DEFAULT 0,
+                  `createdTime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+                PRIMARY KEY (`id`)
+              ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+            "
+            );
+        }
+
+        // $this->exec("
+        //     insert into `activity_learn_log` (
+        //       id
+        //       ,activityId
+        //       ,userId
+        //       ,event
+        //       ,data
+        //       ,watchTime
+        //       ,courseTaskId
+        //       ,learnedTime
+        //       ,createdTime
+        //     ) values select 
+        //       id
+        //       ,lessonId
+        //       ,userId
+        //       ,learnedTime
+        //     from course_lesson_learn where id not in (select id from activity_learn_log)
+        // ");
     }
 
     /**
