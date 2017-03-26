15e5185f4fb60aa4b7691fe277c5374e836e2453
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-26 12:48:28 +0800

    add course api

---
+++ tests/Unit/ApiBundle/Api/Resource/CourseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit\ApiBundle\Api\Resource;
+
+use ApiBundle\Api\Exception\ResourceNotFoundException;
+use ApiBundle\Api\PathParser;
+use ApiBundle\Api\Resource\Course\Course;
+use ApiBundle\Api\Resource\ResourceManager;
+use ApiBundle\Api\ResourceKernel;
+use ApiBundle\ApiTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class CourseTest extends ApiTestCase
+{
+    /**
+     * @expectedException ApiBundle\Api\Exception\ResourceNotFoundException
+     */
+    public function testGetWithError()
+    {
+        $courseRes = new Course($this->getBiz());
+        $courseRes->get(Request::create(''), 100000);
+    }
+
+    public function testWithSuccess()
+    {
+
+        $fakeCourse = array(
+            'id' => 1,
+            'title' => 'hello bike',
+            'createdTime' => time()
+        );
+        $this->mockBiz('Course:CourseService',array(
+            array('functionName' => 'getCourse', 'runTimes' => 1, 'returnValue' => $fakeCourse)
+        ));
+        $courseRes = new Course($this->getBiz());
+        $resp = $courseRes->get(Request::create(''), 100000);
+
+        $this->assertNotNull($resp);
+        $this->assertEquals($fakeCourse, $resp);
+    }
+}
