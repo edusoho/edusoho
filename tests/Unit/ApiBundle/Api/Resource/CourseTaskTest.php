574820dafe8a5ad9990cc34153d019b3b7006ded
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-26 14:55:03 +0800

    add course task api

---
+++ tests/Unit/ApiBundle/Api/Resource/CourseTaskTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit\ApiBundle\Api\Resource;
+
+use ApiBundle\Api\Resource\Course\Course;
+use ApiBundle\Api\Resource\Course\CourseTask;
+use ApiBundle\ApiTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class CourseTaskTest extends ApiTestCase
+{
+    /**
+     * @expectedException ApiBundle\Api\Exception\ResourceNotFoundException
+     */
+    public function testGetWithError()
+    {
+        $res = new CourseTask($this->getBiz());
+        $res->search(Request::create(''), 100000);
+    }
+
+    public function testWithSuccess()
+    {
+
+        $tasks = array(
+            array('id' => 1, 'title' => '123'),
+            array('id' => 2, 'title' => '456'),
+            array('id' => 3, 'title' => '789'),
+        );
+        $this->mockBiz('Course:CourseService',array(
+            array('functionName' => 'getCourse', 'runTimes' => 1, 'returnValue' => 1)
+        ));
+
+        $this->mockBiz('Task:TaskService', array(
+            array('functionName' => 'findTasksByCourseId', 'runTimes' => 1, 'returnValue' => $tasks)
+        ));
+        $res = new CourseTask($this->getBiz());
+        $resp = $res->search(Request::create(''), 100000);
+
+        $this->assertEquals($tasks, $resp);
+    }
+}
