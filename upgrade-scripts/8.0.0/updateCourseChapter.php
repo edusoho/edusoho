0f535104218ca17281c4201da2898e482ff4d8e1
Allen Lau <liuyangyang@howzhi.com>
2017-03-28 16:29:10 +0800

    update lessonId to migrateLessonId

--- upgrade-scripts/8.0.0/updateCourseChapter.php
+++ upgrade-scripts/8.0.0/updateCourseChapter.php
@@ -7,7 +7,7 @@ class updateCourseChapter extends AbstractMigrate
         $this->getConnection()->exec(
           '
           UPDATE `course_task` c1 , `course_task` c2 SET c1.`categoryId` = c2.`categoryId`
-          WHERE c1.mode <> 'lesson'  AND c2.mode = 'lesson' AND  c1.`lessonId` = c2.`lessonId`  AND c1.`categoryId` <> c2.`categoryId`;
+          WHERE c1.mode <> 'lesson'  AND c2.mode = 'lesson' AND  c1.`migrateLessonId` = c2.`migrateLessonId`  AND c1.`categoryId` <> c2.`categoryId`;
           '
         );
     }
