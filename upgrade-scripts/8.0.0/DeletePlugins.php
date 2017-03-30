9c3ed88a5979029ef412033fbc9ebb62bbae2d93
nifeng <fengni@fengdeMacBook-Pro.local>
2017-03-30 16:03:29 +0800

    add delete plugins

--- upgrade-scripts/8.0.0/DeletePlugins.php
+++ upgrade-scripts/8.0.0/DeletePlugins.php
@@ -4,6 +4,9 @@ class DeletePlugins extends AbstractMigrate
 {
     public function update($page)
     {
-        $this->exec("delete from cloud_app where code <> 'MAIN';");
+        $apps = $this->getConnection()->fetchAll("select * from cloud_app where code <> 'MAIN';");
+        foreach ($apps as $key => $app) {
+            $this->getConnection()->exec("delete from cloud_app where id = {$app['id']};");
+        }
     }
 }
