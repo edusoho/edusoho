4e8c2f6d55b562835825d03f74a6098406e11699
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-30 21:07:59 +0800

    订单信息

--- src/ApiBundle/Api/Resource/OrderInfo/OrderInfoFilter.php
+++ src/ApiBundle/Api/Resource/OrderInfo/OrderInfoFilter.php
@@ -8,10 +8,19 @@ class OrderInfoFilter extends Filter
 {
     protected function customFilter(&$data)
     {
-        $data['title'] = $data[$data['targetType']]['title'];
-        unset($data[$data['targetType']]);
-        unset($data['users']);
+        $orderInfo = array(
+            'targetId' => $data['targetId'],
+            'targetType' => $data['targetType'],
+            'title' => $data[$data['targetType']]['title'],
+            'account' => empty($data['account']) ? array() : $data['account'],
+            'hasPayPassword' => empty($data['hasPayPassword']) ? 0 : $data['hasPayPassword'],
+            'verifiedMobile' => empty($data['verifiedMobile']) ? '' : $data['verifiedMobile'],
+            'cashRate' => empty($data['cashRate']) ? 0 : $data['cashRate'],
+            'priceType' => empty($data['priceType']) ? 'RMB' : $data['priceType'],
+            'coinPayAmount' => empty($data['coinPayAmount']) ? 0 : $data['coinPayAmount'],
+            'maxCoin' => empty($data['maxCoin']) ? 0: $data['maxCoin']
+        );
 
-
+        $data = $orderInfo;
     }
 }
