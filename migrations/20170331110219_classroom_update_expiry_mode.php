79c1ab6b6e0b332f10fbe8982eb750d5184faae0
hamster <malianbo@163.com>
2017-03-31 14:15:24 +0800

    format code

--- migrations/20170331110219_classroom_update_expiry_mode.php
+++ migrations/20170331110219_classroom_update_expiry_mode.php
@@ -5,7 +5,7 @@ use Phpmig\Migration\Migration;
 class ClassroomUpdateExpiryMode extends Migration
 {
     /**
-     * Do the migration
+     * Do the migration.
      */
     public function up()
     {
@@ -18,7 +18,7 @@ class ClassroomUpdateExpiryMode extends Migration
     }
 
     /**
-     * Undo the migration
+     * Undo the migration.
      */
     public function down()
     {
