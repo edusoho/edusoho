70b63dfbed919944c19f378f73b51c4f58b940af
ruanwenqin <ruanwenqin@howzhi.com>
2017-03-31 15:12:16 +0800

    评价过滤字段

--- src/ApiBundle/Api/Resource/CourseSet/CourseSetReviewFilter.php
+++ src/ApiBundle/Api/Resource/CourseSet/CourseSetReviewFilter.php
@@ -4,13 +4,21 @@ namespace ApiBundle\Api\Resource\CourseSet;
 
 use ApiBundle\Api\Resource\Filter;
 use ApiBundle\Api\Resource\User\UserFilter;
+use AppBundle\Common\ArrayToolkit;
 
 class CourseSetReviewFilter extends Filter
 {
+    protected $publicFields = array(
+        'id', 'title', 'content', 'rating', 'private', 'createdTime', 'parentId',
+        'updatedTime', 'courseSetId', 'user', 'course'
+    );
+
     protected function customFilter(&$data)
     {
         //评价的用户
         $userFilter = new UserFilter();
         $userFilter->filter($data['user']);
+
+        $data['course'] = ArrayToolkit::parts($data['course'], array('title'));
     }
 }
