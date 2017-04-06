a06ea61cdcb3f370e4e5b7303b1d5e01402c41d5
retamia <retamia@gmail.com>
2017-04-06 17:32:18 +0800

    fix: 附件预览access denied

--- src/Topxia/WebBundle/Resources/public/js/controller/material-lib/player/document-player.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/material-lib/player/document-player.js
@@ -1,19 +1,19 @@
-define(function(require, exports, module) {
+define(function (require, exports, module) {
 
-    var DocumentPlayer = require('../../../../../topxiaweb/js/controller/widget/document-player');
+  var DocumentPlayer = require('../../../../../topxiaweb/js/controller/widget/document-player');
 
-    exports.run = function() {
-    	var player = $("#document-player");
+  exports.run = function () {
+    var player = $("#document-player");
 
-        var html = '<iframe id=\'viewerIframe\' width=\'100%\'allowfullscreen webkitallowfullscreen height=\'100%\'></iframe>';
-        player.html(html).show();
+    var html = '<iframe id=\'viewerIframe\' width=\'100%\'allowfullscreen webkitallowfullscreen height=\'100%\'></iframe>';
+    player.html(html).show();
 
-		$.get(player.data('url'), function(response) {
-            var player = new DocumentPlayer({
-                element: '#document-player',
-                swfFileUrl: response.swf,
-                pdfFileUrl: response.pdf
-            });
-        }, 'json');
-	}
+    $.get(player.data('url'), function (response) {
+      var player = new DocumentPlayer({
+        element: '#document-player',
+        swfFileUrl: response.swf,
+        pdfFileUrl: response.pdf
+      });
+    }, 'json');
+  }
 });
