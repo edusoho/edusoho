b673554cf854e87c802dd3e8f5028d193c76e4a8
zhongjie <zhongjie@howzhi.com>
2017-04-06 11:42:28 +0800

    修复个人名片报错问题

--- app/Resources/static-src/common/card.js
+++ app/Resources/static-src/common/card.js
@@ -1,21 +1,19 @@
 import { isMobileDevice } from 'common/utils';
 
-const Card = () => {
-  if (!navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i)) {
-    bindCardEvent('.js-card-content');
-    $(".js-user-card").on("mouseenter", function () {
+if (!navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i)) {
+  bindCardEvent('.js-card-content');
+  $(".js-user-card").on("mouseenter", function () {
 
+    var _this = $(this);
+    var userId = _this.data('userId');
+    var loadingHtml = '<div class="card-body"><div class="card-loader"><span class="loader-inner"><span></span><span></span><span></span></span>' + Translator.trans('名片加载中') + '</div>';
 
-      var _this = $(this);
-      var userId = _this.data('userId');
-      var loadingHtml = '<div class="card-body"><div class="card-loader"><span class="loader-inner"><span></span><span></span><span></span></span>' + Translator.trans('名片加载中') + '</div>';
+    var timer = setTimeout(function () {
 
-      var timer = setTimeout(function () {
+      function callback(html) {
+        _this.popover('destroy');
 
-        function callback(html) {
-
-          _this.popover('destroy');
-
+        setTimeout(function () {
           if ($('#user-card-' + userId).length == 0) {
             if ($('body').find('#user-card-store').length > 0) {
               $('#user-card-store').append(html);
@@ -36,77 +34,74 @@ const Card = () => {
             container: 'body',
             animation: true
           });
-
           _this.popover("show");
 
           _this.data('popover', true);
 
           $(".popover").on("mouseleave", function () {
-            $(_this).popover('hide');
+            _this.popover('hide');
           });
+        }, 200);
+      }
+
+      if ($('#user-card-' + userId).length == 0 || !_this.data('popover')) {
+
+        function beforeSend() {
+
+          _this.popover({
+            trigger: 'manual',
+            placement: 'auto top',
+            html: 'true',
+            content: function () {
+              return loadingHtml;
+            },
+            template: '<div class="popover es-card"><div class="arrow"></div><div class="popover-content"></div></div>',
+            container: 'body',
+            animation: true
+          });
+
+          // _this.popover("show");
 
         };
 
-        if ($('#user-card-' + userId).length == 0 || !_this.data('popover')) {
+        $.ajax({
+          type: "GET",
+          url: _this.data('cardUrl'),
+          dataType: "html",
+          beforeSend: beforeSend,
+          success: callback
+        });
+      } else {
+        var html = $('#user-card-' + userId).clone();
+        callback(html);
+        // _this.popover("show");
+      }
 
-          function beforeSend() {
-
-            _this.popover({
-              trigger: 'manual',
-              placement: 'auto top',
-              html: 'true',
-              content: function () {
-                return loadingHtml;
-              },
-              template: '<div class="popover es-card"><div class="arrow"></div><div class="popover-content"></div></div>',
-              container: 'body',
-              animation: true
-            });
-
-            // _this.popover("show");
-
-          };
-
-          $.ajax({
-            type: "GET",
-            url: _this.data('cardUrl'),
-            dataType: "html",
-            beforeSend: beforeSend,
-            success: callback
-          });
-
-        } else {
-          var html = $('#user-card-' + userId).clone();
-          callback(html);
-          // _this.popover("show");
-        }
-
-        bindMsgBtn($('.es-card'), _this);
+      bindMsgBtn($('.es-card'), _this);
 
 
-      }, 300);
+    }, 100);
 
-      _this.data('timerId', timer);
+    _this.data('timerId', timer);
 
-    }).on("mouseleave", function () {
+  }).on("mouseleave", function () {
 
-      var _this = $(this);
+    var _this = $(this);
 
-      setTimeout(function () {
+    setTimeout(function () {
 
-        if (!$(".popover:hover").length) {
+      if (!$(".popover:hover").length) {
 
-          _this.popover("hide");
+        _this.popover("hide");
 
-        }
+      }
 
-      }, 100);
+    }, 100);
 
-      clearTimeout(_this.data('timerId'));
+    clearTimeout(_this.data('timerId'));
 
-    });
-  }
-};
+  });
+}
 
 function bindCardEvent(selector) {
   $('body').on('click', '.js-card-content .follow-btn', function () {
@@ -132,7 +127,7 @@ function bindMsgBtn($card, self) {
 function showFollowBtn($btn) {
   $btn.hide();
   $btn.siblings('.follow-btn').show();
-  $actualCard = $('#user-card-' + $btn.closest('.js-card-content').data('userId'));
+  let $actualCard = $('#user-card-' + $btn.closest('.js-card-content').data('userId'));
   $actualCard.find('.unfollow-btn').hide();
   $actualCard.find('.follow-btn').show();
 }
@@ -140,9 +135,8 @@ function showFollowBtn($btn) {
 function showUnfollowBtn($btn) {
   $btn.hide();
   $btn.siblings('.unfollow-btn').show();
-  $actualCard = $('#user-card-' + $btn.closest('.js-card-content').data('userId'));
+  let $actualCard = $('#user-card-' + $btn.closest('.js-card-content').data('userId'));
   $actualCard.find('.follow-btn').hide();
   $actualCard.find('.unfollow-btn').show();
 }
 
-export default Card();
