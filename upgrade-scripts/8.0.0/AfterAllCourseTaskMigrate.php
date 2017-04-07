8a213fb4949d863b1fcdc0a40a8551cd8a218414
nifeng <fengni@fengdeMacBook-Pro.local>
2017-04-07 23:00:41 +0800

    fix

--- upgrade-scripts/8.0.0/AfterAllCourseTaskMigrate.php
+++ upgrade-scripts/8.0.0/AfterAllCourseTaskMigrate.php
@@ -9,5 +9,12 @@ class AfterAllCourseTaskMigrate extends AbstractMigrate
 
         $sql = "UPDATE `course_task` c set `fromCourseSetId` = `courseId`";
         $result = $this->getConnection()->exec($sql);
+
+
+        $sql = "UPDATE `course_v8` c, (select courseId, count(*) as c from course_task group by courseId) co set `taskNum` = co.c where c.id = co.courseId";
+        $result = $this->getConnection()->exec($sql);
+
+        $sql = "UPDATE `course_v8` c, (select courseId, count(*) as c from course_task where status='published' group by courseId) co set `taskNum` = co.c where c.id = co.courseId";
+        $result = $this->getConnection()->exec($sql);
     }
 }
