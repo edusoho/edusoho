e451a6ebca4b7363740e338843dfb73a15c77e94
nifeng <fengni@fengdeMacBook-Pro.local>
2017-04-07 23:24:00 +0800

    fix

--- upgrade-scripts/8.0.0/UpdateCourseChapter.php
+++ upgrade-scripts/8.0.0/UpdateCourseChapter.php
@@ -6,7 +6,7 @@ class UpdateCourseChapter extends AbstractMigrate
     {
         $this->getConnection()->exec(
           "
-          UPDATE `course_task` c1 , `course_task` c2 SET c1.`categoryId` = c2.`categoryId`
+          UPDATE `course_task` c1 , `course_task` c2 SET c1.`categoryId` = c2.`categoryId`, c1.`status`= c2.`status`
           WHERE c1.`mode` <> 'lesson'  AND c2.`mode` = 'lesson' AND  c1.`migrateLessonId` = c2.`migrateLessonId`  AND c1.`categoryId` <> c2.`categoryId`;
           "
         );
