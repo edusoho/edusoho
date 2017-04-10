b07496ad6ce846f70bf1cd6e73a407486eb36418
hamster <malianbo@163.com>
2017-04-10 14:23:00 +0800

    refact

--- upgrade-scripts/8.0.0/UpdateCourseChapter.php
+++ upgrade-scripts/8.0.0/UpdateCourseChapter.php
@@ -12,23 +12,23 @@ class UpdateCourseChapter extends AbstractMigrate
         );
 
 
-        if (!$this->isFieldExist('course_chapter', 'copyCourseId')) {
-            $this->exec("ALTER TABLE `course_chapter` ADD COLUMN copyCourseId int(10) DEFAULT 0;");
+        if (!$this->isFieldExist('course_chapter', 'migrateCopyCourseId')) {
+            $this->exec("ALTER TABLE `course_chapter` ADD COLUMN migrateCopyCourseId int(10) DEFAULT 0;");
         }
 
-        if (!$this->isFieldExist('course_chapter', 'refTaskId')) {
-            $this->exec("ALTER TABLE `course_chapter` ADD COLUMN refTaskId int(10) DEFAULT 0;");
+        if (!$this->isFieldExist('course_chapter', 'migrateRefTaskId')) {
+            $this->exec("ALTER TABLE `course_chapter` ADD COLUMN migrateRefTaskId int(10) DEFAULT 0;");
         }
 
-        if (!$this->isFieldExist('course_chapter', 'copyTaskId')) {
-            $this->exec("ALTER TABLE `course_chapter` ADD COLUMN copyTaskId int(10) DEFAULT 0;");
+        if (!$this->isFieldExist('course_chapter', 'mgrateCopyTaskId')) {
+            $this->exec("ALTER TABLE `course_chapter` ADD COLUMN mgrateCopyTaskId int(10) DEFAULT 0;");
         }
 
         $this->getConnection()->exec("
-            UPDATE `course_chapter`  LEFT JOIN `course_v8` c ON course_chapter.courseId = c.id  SET copyCourseId = c.parentId WHERE c.parentId > 0;
-            UPDATE `course_chapter` LEFT JOIN `course_task` t ON course_chapter.id = t.categoryId SET refTaskId = t.id , copyTaskId = t.copyId WHERE t.copyId > 0 AND t.mode = 'lesson';
+            UPDATE `course_chapter`  LEFT JOIN `course_v8` c ON course_chapter.courseId = c.id  SET migrateCopyCourseId = c.parentId WHERE c.parentId > 0;
+            UPDATE `course_chapter` LEFT JOIN `course_task` t ON course_chapter.id = t.categoryId SET migrateRefTaskId = t.id , mgrateCopyTaskId = t.copyId WHERE t.copyId > 0 AND t.mode = 'lesson';
 
-            UPDATE `course_chapter` LEFT JOIN `course_task` t ON course_chapter.copyTaskId = t.id SET course_chapter.copyId = t.categoryId WHERE t.mode = 'lesson';
+            UPDATE `course_chapter` LEFT JOIN `course_task` t ON course_chapter.mgrateCopyTaskId = t.id SET course_chapter.copyId = t.categoryId WHERE t.mode = 'lesson';
         ");
     }
 }
