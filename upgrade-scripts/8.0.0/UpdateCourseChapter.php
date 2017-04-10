54f8439ee248a59dea7bcaf36055b161b81b9114
hamster <malianbo@163.com>
2017-04-10 11:31:58 +0800

    更新chapter中班级课程的copyId

--- upgrade-scripts/8.0.0/UpdateCourseChapter.php
+++ upgrade-scripts/8.0.0/UpdateCourseChapter.php
@@ -8,6 +8,19 @@ class UpdateCourseChapter extends AbstractMigrate
           "
           UPDATE `course_task` c1 , `course_task` c2 SET c1.`categoryId` = c2.`categoryId`, c1.`status`= c2.`status`
           WHERE c1.`mode` <> 'lesson'  AND c2.`mode` = 'lesson' AND  c1.`migrateLessonId` = c2.`migrateLessonId`  AND c1.`categoryId` <> c2.`categoryId`;
+
+          ALTER TABLE `course_chapter` ADD COLUMN copyCourseId int(10) DEFAULT 0;
+          update `course_chapter`  left join `course_v8` c on course_chapter.courseId = c.id  set copyCourseId = c.parentId where c.parentId > 0;
+
+          ALTER TABLE `course_chapter` ADD COLUMN refTaskId int(10) DEFAULT 0;
+          ALTER TABLE `course_chapter` ADD COLUMN copyTaskId int(10) DEFAULT 0;
+          update `course_chapter` left join `course_task` t on course_chapter.id = t.categoryId set refTaskId = t.id , copyTaskId = t.copyId where t.copyId > 0 and t.mode = 'lesson';
+
+          update `course_chapter` left join `course_task` t on course_chapter.copyTaskId = t.id set course_chapter.copyId = t.categoryId where t.mode = 'lesson';
+
+          ALTER TABLE `course_chapter` DROP COLUMN copyCourseId;
+          ALTER TABLE `course_chapter` DROP COLUMN refTaskId;
+          ALTER TABLE `course_chapter` DROP COLUMN copyTaskId;
           "
         );
     }
