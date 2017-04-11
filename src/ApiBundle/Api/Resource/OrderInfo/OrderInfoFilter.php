12f7416c9c5173989e8e04582c036d4a79ea815e
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-11 16:29:52 +0800

    订单增加一个是否允许虚拟币全额支付的字段

--- src/ApiBundle/Api/Resource/OrderInfo/OrderInfoFilter.php
+++ src/ApiBundle/Api/Resource/OrderInfo/OrderInfoFilter.php
@@ -11,21 +11,39 @@ class OrderInfoFilter extends Filter
     {
         $couponFilter = new CouponFilter();
         $couponFilter->filters($data['availableCoupons']);
-
         $orderInfo = array(
             'targetId' => $data['targetId'],
             'targetType' => $data['targetType'],
+            'totalPrice' => $data['totalPrice'],
             'title' => $data[$data['targetType']]['title'],
             'account' => empty($data['account']) ? array() : $data['account'],
-            'hasPayPassword' => empty($data['hasPayPassword']) ? 0 : $data['hasPayPassword'],
+            'hasPayPassword' => empty($data['hasPayPassword']) ? 0 : 1,
             'verifiedMobile' => empty($data['verifiedMobile']) ? '' : $data['verifiedMobile'],
             'cashRate' => empty($data['cashRate']) ? 0 : $data['cashRate'],
             'priceType' => empty($data['priceType']) ? 'RMB' : $data['priceType'],
             'coinPayAmount' => empty($data['coinPayAmount']) ? 0 : $data['coinPayAmount'],
-            'maxCoin' => empty($data['maxCoin']) ? 0: $data['maxCoin'],
-            'availableCoupons' => $data['availableCoupons']
+            'maxCoin' => empty($data['maxCoin']) ? 0 : $data['maxCoin'],
+            'availableCoupons' => $data['availableCoupons'],
+            'fullCoinPayable' => $this->fullCoinPayable($data)
         );
 
         $data = $orderInfo;
+    }
+
+    private function fullCoinPayable($data)
+    {
+        if ($data['priceType'] == 'Coin') {
+            return 1;
+        }
+
+        if ($data['cashRate'] == 0) {
+            return 0;
+        }
+
+        if ($data[$data['targetType']]['maxRate'] == 100) {
+            return 1;
+        }
+
+        return 0;
     }
 }
