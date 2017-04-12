dfd4a832b16266b4a6bdc8b1907d6869be9d3a7e
retamia <retamia@gmail.com>
2017-04-12 15:15:52 +0800

    fix: 课程笔记分页、排序和按任务查询

--- src/Biz/Task/Dao/Impl/TaskDaoImpl.php
+++ src/Biz/Task/Dao/Impl/TaskDaoImpl.php
@@ -31,6 +31,13 @@ class TaskDaoImpl extends GeneralDaoImpl implements TaskDao
         return $this->findInField('activityId', $activityIds);
     }
 
+    public function findByCourseSetId($courseSetId)
+    {
+        return $this->findByFields(array(
+            'fromCourseSetId' => $courseSetId,
+        ));
+    }
+
     public function findByIds($ids)
     {
         return $this->findInField('id', $ids);
