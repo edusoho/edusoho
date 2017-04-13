6d9337aea1e13e938274bc21d888270d5069222e
retamia <retamia@gmail.com>
2017-04-13 20:16:34 +0800

    fix: Cannot redeclare class Symfony\Component\Debug\ErrorHandler

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -4,16 +4,12 @@ date_default_timezone_set('UTC');
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\Debug\ErrorHandler;
-use Symfony\Component\Debug\ExceptionHandler;
 use Symfony\Component\HttpFoundation\Request;
 
 define('RUNTIME_ENV', 'API');
 define('ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR . '/../app');
 
 if (API_ENV == 'prod') {
-    ErrorHandler::register(0);
-    ExceptionHandler::register(false);
     $kernel = new AppKernel('prod', false);
 }else{
     $kernel = new AppKernel('dev', true);
