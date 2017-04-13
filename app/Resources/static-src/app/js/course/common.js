a2fdfb9c7f9a0b13169475f53af641f015d16d36
wuli <wuli@howzhi.com>
2017-04-13 19:48:40 +0800

    打折活动

--- app/Resources/static-src/app/js/course/common.js
+++ app/Resources/static-src/app/js/course/common.js
@@ -1,23 +1,23 @@
 //     require('../../util/qrcode').run();
 //     require('jquery.countdown');
-var remainTime = parseInt($('#discount-endtime-countdown').data('remaintime'));
-if (remainTime >= 0) {
-  var endtime = new Date(new Date().valueOf() + remainTime * 1000);
-  $('#discount-endtime-countdown').countdown(endtime, function (event) {
-    var $this = $(this).html(event.strftime(Translator.trans('剩余 ')
-      + '<span>%D</span>' + Translator.trans('天 ')
-      + '<span>%H</span>' + Translator.trans('时 ')
-      + '<span>%M</span>' + Translator.trans('分 ')
-      + '<span>%S</span> ' + Translator.trans('秒')));
-  }).on('finish.countdown', function () {
-    $(this).html(Translator.trans('活动时间到，正在刷新网页，请稍等...'));
-    setTimeout(function () {
-      $.post(app.crontab, function () {
-        window.location.reload();
-      });
-    }, 2000);
-  });
-}
+// var remainTime = parseInt($('#discount-endtime-countdown').data('remaintime'));
+// if (remainTime >= 0) {
+//   var endtime = new Date(new Date().valueOf() + remainTime * 1000);
+//   $('#discount-endtime-countdown').countdown(endtime, function (event) {
+//     var $this = $(this).html(event.strftime(Translator.trans('剩余 ')
+//       + '<span>%D</span>' + Translator.trans('天 ')
+//       + '<span>%H</span>' + Translator.trans('时 ')
+//       + '<span>%M</span>' + Translator.trans('分 ')
+//       + '<span>%S</span> ' + Translator.trans('秒')));
+//   }).on('finish.countdown', function () {
+//     $(this).html(Translator.trans('活动时间到，正在刷新网页，请稍等...'));
+//     setTimeout(function () {
+//       $.post(app.crontab, function () {
+//         window.location.reload();
+//       });
+//     }, 2000);
+//   });
+// }
 
 // $('.course-exit-btn').on('click', function () {
 //   var $btn = $(this);
