0606132ad7df6badb1cdafebb2cbb921158505b5
retamia <retamia@gmail.com>
2017-04-14 18:02:19 +0800

    fix: 8.0升级文件下载

--- upgrade-scripts/8.0.0/DownloadUpgradeFile.php
+++ upgrade-scripts/8.0.0/DownloadUpgradeFile.php
@@ -11,10 +11,15 @@ class DownloadUpgradeFile extends AbstractMigrate
 
         if($page == $this->end + 1){
             $this->extractUpgradeFiles();
-            return null;
+            return $page + 1;
         }
 
-        $filepath = 'http://ojc8jepus.bkt.clouddn.com/x8-package/v7-'.$page.'.zip';
+        if($page == $this->end + 2){
+            $this->copyNoneSideEffectFiles();
+            return 0;
+        }
+
+        $filepath = 'http://ojc8jepus.bkt.clouddn.com/x8-package/v8-'.$page.'.zip';
 
         $dir = $this->kernel->getParameter('kernel.root_dir').'/data/upgrade';
 
@@ -28,6 +33,39 @@ class DownloadUpgradeFile extends AbstractMigrate
         touch($targetPath);
         file_put_contents($targetPath, file_get_contents($filepath));
         return $page + 1;
+    }
+
+    private function copyNoneSideEffectFiles()
+    {
+        $sourceDir = $this->kernel->getParameter('kernel.root_dir').'/data/upgrade/es-8.0/source';
+        $edusohoDir = $this->kernel->getParameter('kernel.root_dir') . '/../';
+
+        $filesystem = new \Symfony\Component\Filesystem\Filesystem();
+
+        // copy or overwrite AppBundle Dir
+        $filesystem->mirror($sourceDir.'/src/AppBundle', $edusohoDir.'/src/AppBundle', null, array(
+            'override' => true,
+            'copy_on_windows' => true,
+        ));
+
+        // copy or overwrite Biz Dir
+        $filesystem->mirror($sourceDir.'/src/Biz', $edusohoDir.'/src/Biz', null, array(
+            'override' => true,
+            'copy_on_windows' => true,
+        ));
+
+        foreach (array('app', 'autumntheme', 'defaultbtheme', 'jianmotheme', 'defaulttheme', 'libs') as $dir){
+            $filesystem->mirror($sourceDir.'/web/static-dist/'.$dir, $edusohoDir.'/web/static-dist/'.$dir, null, array(
+                'override' => true,
+                'delete' => true,
+                'copy_on_windows' => true,
+            ));
+        }
+
+        $filesystem->mirror($sourceDir.'/app/Resources/static-src', $edusohoDir.'/app/Resources/static-src', null, array(
+            'override' => true,
+            'copy_on_windows' => true,
+        ));
     }
 
     private function extractUpgradeFiles()
--- upgrade-scripts/8.0.0/FileMigrate.php
+++ upgrade-scripts/8.0.0/FileMigrate.php
@@ -7,7 +7,7 @@ class FileMigrate extends AbstractMigrate
     public function update($page)
     {
 
-        if($page == 2){
+
             $this->rebuildCloudSearchIndex();
             $this->upgradeEduSohoApp();
             $this->copyAndOverwriteUpgradeFiles();
@@ -18,43 +18,7 @@ class FileMigrate extends AbstractMigrate
             @unlink($lockFile);
             echo json_encode(array('status' => 'ok'));
             exit(0);
-        }
 
-        $this->copyNoneSideEffectFiles();
-        return $page + 1;
-    }
-
-    private function copyNoneSideEffectFiles()
-    {
-        $sourceDir = $this->kernel->getParameter('kernel.root_dir').'/data/upgrade/es-8.0/source';
-        $edusohoDir = $this->kernel->getParameter('kernel.root_dir') . '/../';
-
-        $filesystem = new \Symfony\Component\Filesystem\Filesystem();
-
-        // copy or overwrite AppBundle Dir
-        $filesystem->mirror($sourceDir.'/src/AppBundle', $edusohoDir.'/src/AppBundle', null, array(
-            'override' => true,
-            'copy_on_windows' => true,
-        ));
-
-        // copy or overwrite Biz Dir
-        $filesystem->mirror($sourceDir.'/src/Biz', $edusohoDir.'/src/Biz', null, array(
-            'override' => true,
-            'copy_on_windows' => true,
-        ));
-
-        foreach (array('app', 'autumntheme', 'defaultbtheme', 'jianmotheme', 'defaulttheme', 'libs') as $dir){
-            $filesystem->mirror($sourceDir.'/web/static-dist/'.$dir, $edusohoDir.'/web/static-dist/'.$dir, null, array(
-                'override' => true,
-                'delete' => true,
-                'copy_on_windows' => true,
-            ));
-        }
-
-        $filesystem->mirror($sourceDir.'/app/Resources/static-src', $edusohoDir.'/app/Resources/static-src', null, array(
-            'override' => true,
-            'copy_on_windows' => true,
-        ));
     }
 
     private function copyAndOverwriteUpgradeFiles()
