c2c80892105c018958e2dc6fd5f74dc6459d6b6f
zhaoyao <zhaoyao@howzhi.com>
2017-04-17 17:03:35 +0800

    回滚

--- src/Biz/Course/Event/CourseMemberEventSubscriber.php
+++ src/Biz/Course/Event/CourseMemberEventSubscriber.php
@@ -17,8 +17,6 @@ use Biz\User\Service\UserService;
 use Codeages\Biz\Framework\Event\Event;
 use Codeages\PluginBundle\Event\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Topxia\Service\Common\ServiceKernel;
-use Biz\CloudPlatform\CloudAPIFactory;
 
 class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscriberInterface
 {
@@ -26,22 +24,14 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
     {
         return array(
             'course.join' => 'onCourseJoin',
-            'course.join' => 'onLiveCourseJoin',
             'course.quit' => 'onMemberDelete',
-            'course.quit' => 'onLiveMemberDelete',
             'course.view' => 'onCourseView',
 
             'classroom.course.join' => 'onClassroomCourseJoin',
-            'classroom.course.join' => 'onClassroomLiveCourseJoin',
             'classroom.course.copy' => 'onClassroomCourseCopy',
 
             'course.task.delete' => 'onTaskDelete',
             'course.task.finish' => 'onTaskFinish',
-
-            'course.teachers.create' =>'onCourseTeachersCreate',
-            'course.teachers.delete' =>'onCourseTeachersDelete',
-
-            'course.member.import' =>'onCourseMemberImport',
         );
     }
 
@@ -64,175 +54,9 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
         $this->publishStatus($event, 'become_student');
     }
 
-    public function onCourseTeachersCreate(Event $event)
-    {
-        $course = $event->getArgument('course');
-
-        $teachers = $this->getCourseMemberService()->findCourseTeachers($course['id']);
-        $teachers = ArrayToolkit::index($teachers, 'userId');
-
-        $teacherIds = $event->getSubJect();
-        $teacherIds = array_values($teacherIds);
-        $users = $this->getUserService()->findUsersByIds($teacherIds);
-        $users = ArrayToolkit::index($users, 'id');
-
-        list($activities, $liveActivities) = $this->findActivitiesAndLiveActivities($course);
-
-        foreach ($activities as $mediaId => $activity) {
-            $isPush = $this->canPushLiveMessage($activity);
-            if (!$isPush) {
-                continue;
-            }
-
-            foreach ($teacherIds as $userId) {
-                $result[] = $this->buildLiveMemberData($users[$userId], $teachers[$userId]);
-            }
-            $this->pushJoinLiveCourseMember($result, $liveActivities[$mediaId]['liveId']);
-        }
-    }
-
-    public function onCourseTeachersDelete(Event $event)
-    {
-        $course = $event->getArgument('course');
-
-        $teacherIds = $event->getSubJect();
-        list($activities, $liveActivities) = $this->findActivitiesAndLiveActivities($course);
-        foreach ($activities as $mediaId => $activity) {
-            $isPush = $this->canPushLiveMessage($activity);
-            if (!$isPush) {
-                continue;
-            }
-
-            foreach ($teacherIds as $teacherId) {
-                $this->pushDeleteLiveCourseMember($teacherId, $liveActivities[$mediaId]['liveId']);
-            }
-        }
-    }
-
-    public function onLiveCourseJoin(Event $event)
-    {
-        $course = $event->getSubject();
-        $userId = $event->getArgument('userId');
-        $user = $this->getUserService()->getUser($userId);
-        $member = $event->getArgument('member');
-
-        list($activities, $liveActivities) = $this->findActivitiesAndLiveActivities($course);
-        foreach ($activities as $mediaId => $activity) {
-            $isPush = $this->canPushLiveMessage($activity);
-            if (!$isPush) {
-                continue;
-            }
-            $result[] = $this->buildLiveMemberData($user, $member);
-            $this->pushJoinLiveCourseMember($result, $liveActivities[$mediaId]['liveId']);
-        }
-    }
-
-    public function onClassroomLiveCourseJoin(Event $event)
-    {
-        $course = $event->getSubject();
-        $member = $event->getArgument('member');
-        $user = $this->getUserService()->getUser($member['userId']);
-
-        if ($course['locked']) {
-            $course = $this->getCourseService()->getCourse($course['parentId']);
-        }
-
-        list($activities, $liveActivities) = $this->findActivitiesAndLiveActivities($course);
-        foreach ($activities as $mediaId => $activity) {
-            $isPush = $this->canPushLiveMessage($activity);
-            if (!$isPush) {
-                continue;
-            }
-            $result[] = $this->buildLiveMemberData($user, $member);
-            $this->pushJoinLiveCourseMember($result, $liveActivities[$mediaId]['liveId']);
-        }
-    }
-
-    protected function getFileUrl($path, $default = '')
-    {
-        if (empty($path)) {
-            if (empty($default)) {
-                return '';
-            }
-            $path = $this->getHttpHost().'/assets/img/default/'.$default;
-            return $path;
-        };
-        
-        if (strpos($path, $this->getHttpHost().'://') !== false) {
-            return $path;
-        }
-        $path = str_replace('public://', '', $path);
-        $path = str_replace('files/', '', $path);
-        $path = $this->getHttpHost().'/files/'.ltrim($path, '/');
-
-        return $path;
-    }
-
-    protected function getHttpHost()
-    {
-        $schema = (!empty($_SERVER['HTTPS']) && 'off' !== strtolower($_SERVER['HTTPS'])) ? 'https' : 'http';
-
-        return $schema."://{$_SERVER['HTTP_HOST']}";
-    }
-
-    protected function buildLiveMemberData($user, $member)
-    {
-        $result['clientName'] = $user['nickname'];
-        $result['clientId'] = $user['id'];
-        $result['avatar'] = $this->getFileUrl($user['smallAvatar'], 'avatar.png');
-        $result['role'] = $member['role'];
-        
-        return $result;
-    }
-
-    protected function pushJoinLiveCourseMember($result, $liveId)
-    {
-        try {
-            $api = CloudAPIFactory::create('root');
-            $result = $api->post("/lives/{$liveId}/room_members", array('members' => $result));
-        } catch (\RuntimeException $e) {
-            throw new \RuntimeException(ServiceKernel::instance()->trans('发送失败！'));
-        }
-    }
-
-    public function pushDeleteLiveCourseMember($userId, $liveId)
-    {
-        try {
-            $api = CloudAPIFactory::create('root');
-            $result = $api->delete("/lives/{$liveId}/room_members", array('clientId' => $userId));
-        } catch (\RuntimeException $e) {
-            throw new \RuntimeException(ServiceKernel::instance()->trans('发送失败！'));
-        }
-    }
-
     public function onClassroomCourseJoin(Event $event)
     {
         $this->publishStatus($event, 'become_student');
-    }
-
-    public function onCourseMemberImport(Event $event)
-    {
-        $members = $event->getArgument('members');
-        $members = ArrayToolkit::index($members, 'userId');
-        $userIds = ArrayToolkit::column($members, 'userId');
-
-        $users = $this->getUserService()->findUsersByIds($userIds);
-        $users = ArrayToolkit::index($users, 'id');
-
-        $course = $event->getSubject();
-        list($activities, $liveActivities) = $this->findActivitiesAndLiveActivities($course);
-
-        foreach ($activities as $mediaId => $activity) {
-            $isPush = $this->canPushLiveMessage($activity);
-            if (!$isPush) {
-                continue;
-            }
-
-            foreach ($members as $userId => $member) {
-                $result[] = $this->buildLiveMemberData($users[$userId], $member);
-            }
-            $this->pushJoinLiveCourseMember($result, $liveActivities[$mediaId]['liveId']);
-        }
     }
 
     public function onClassroomCourseCopy(Event $event)
@@ -326,35 +150,6 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
         }
     }
 
-    public function onLiveMemberDelete(Event $event)
-    {
-        $course = $event->getSubject();
-        $userId = $event->getArgument('userId');
-
-        if ($course['locked']) {
-            $course = $this->getCourseService()->getCourse($course['parentId']);
-        }
-        list($activities, $liveActivities) = $this->findActivitiesAndLiveActivities($course);
-        foreach ($activities as $mediaId => $activity) {
-            $isPush = $this->canPushLiveMessage($activity);
-            if (!$isPush) {
-                continue;
-            }
-            $this->pushDeleteLiveCourseMember($userId, $liveActivities[$mediaId]['liveId']);
-        }
-    }
-
-    protected function canPushLiveMessage($activity)
-    {
-        if ($activity['mediaType'] != 'live') {
-            return false;
-        }
-        if (time() > $activity['endTime']) {
-            return false;
-        }
-        return true;
-    }
-
     public function onTaskFinish(Event $event)
     {
         $taskResult = $event->getSubject();
@@ -377,16 +172,6 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
         $welcomeMessageBody = str_replace($valuesToBeReplace, $valuesToReplace, $setting['welcome_message_body']);
 
         return $welcomeMessageBody;
-    }
-
-    protected function findActivitiesAndLiveActivities($course)
-    {
-        $activities = $this->getActivityService()->findActivitiesByCourseIdAndType($course['id'], 'live');
-        $activities = ArrayToolkit::index($activities, 'mediaId');
-        $liveActivities = $this->getLiveActivityService()->findLiveActivitiesByIds(ArrayToolkit::column($activities, 'mediaId'));
-        $liveActivities = ArrayToolkit::index($liveActivities, 'id');
-
-        return array($activities, $liveActivities);
     }
 
     protected function simplifyCourse($course)
@@ -519,15 +304,5 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
     protected function getCourseDao()
     {
         return $this->getBiz()->dao('Course:CourseDao');
-    }
-
-    protected function getActivityService()
-    {
-        return $this->getBiz()->service('Activity:ActivityService');
-    }
-
-   protected function getLiveActivityService()
-   {
-        return $this->getBiz()->service('Activity:LiveActivityService');
     }
 }
