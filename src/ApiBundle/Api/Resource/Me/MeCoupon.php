72d6477de2b49c1be1c061e7fc9b3432ea62f6d1
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-17 17:36:00 +0800

    获取我地优惠码

--- src/ApiBundle/Api/Resource/Me/MeCoupon.php
+++ src/ApiBundle/Api/Resource/Me/MeCoupon.php
@@ -4,21 +4,42 @@ namespace ApiBundle\Api\Resource\Me;
 
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Card\Service\CardService;
+use Biz\Coupon\Service\CouponService;
 
-class MeCashAccount extends AbstractResource
+class MeCoupon extends AbstractResource
 {
     public function search(ApiRequest $request)
     {
         $conditions = array(
             'userId' => $this->getCurrentUser()->getId(),
-            'status' => $request->query->get('status', '')
+            'status' => 'receive',
+            'cardType' => 'coupon'
         );
 
-        $this->getCouponService()->searchCoupons();
+        $myCards = $this->getCardService()->searchCards(
+            $conditions,
+            array('createdTime' => 'DESC'),
+            0,
+            PHP_INT_MAX
+        );
+
+        return array_values($this->getCouponService()->findCouponsByIds(array_column($myCards, 'cardId')));
     }
 
+    /**
+     * @return CouponService
+     */
     private function getCouponService()
     {
         return $this->service('Coupon:CouponService');
+    }
+
+    /**
+     * @return CardService
+     */
+    private function getCardService()
+    {
+        return $this->service('Card:CardService');
     }
 }
