2089d72ee1d5570e130845c2e71f43dd1a842ee6
Allen Lau <liuyangyang@howzhi.com>
2017-04-18 14:16:29 +0800

    fix

--- migrations/20170417201930_course_set_default_course_id.php
+++ migrations/20170417201930_course_set_default_course_id.php
@@ -10,7 +10,7 @@ class CourseSetDefaultCourseId extends Migration
     public function up()
     {
         $biz = $this->getContainer();
-        $biz['db']->exec("ALTER TABLE `course_set_v8` ADD COLUMN `defaultCourseId` int(11) unsigned DEFAULT NULL COMMENT '默认的计划ID';");
+        $biz['db']->exec("ALTER TABLE `course_set_v8` ADD COLUMN `defaultCourseId` int(11) unsigned DEFAULT 0 COMMENT '默认的计划ID';");
         $biz['db']->exec('update `course_set_v8` cs , `course_v8` c set  cs.`defaultCourseId` = c.id where   c.`courseSetid` = cs.id ');
     }
 
