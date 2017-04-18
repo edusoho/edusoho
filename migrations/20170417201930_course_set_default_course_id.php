97baf8b907ca88ea8bf88157eb9ac63d414812b6
Allen Lau <liuyangyang@howzhi.com>
2017-04-18 14:14:45 +0800

    update sql

--- migrations/20170417201930_course_set_default_course_id.php
+++ migrations/20170417201930_course_set_default_course_id.php
@@ -11,6 +11,7 @@ class CourseSetDefaultCourseId extends Migration
     {
         $biz = $this->getContainer();
         $biz['db']->exec("ALTER TABLE `course_set_v8` ADD COLUMN `defaultCourseId` int(11) unsigned DEFAULT NULL COMMENT '默认的计划ID';");
+        $biz['db']->exec("update `course_set_v8` cs , `course_v8` c set  cs.`defaultCourseId` = c.id where   c.`courseSetid` = cs.id ");
     }
 
     /**
