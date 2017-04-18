1d61704080e1f643c099881423a31ffe8506fa4c
Allen Lau <liuyangyang@howzhi.com>
2017-04-18 14:14:48 +0800

    fix code style  fixed

--- migrations/20170417201930_course_set_default_course_id.php
+++ migrations/20170417201930_course_set_default_course_id.php
@@ -5,21 +5,21 @@ use Phpmig\Migration\Migration;
 class CourseSetDefaultCourseId extends Migration
 {
     /**
-     * Do the migration
+     * Do the migration.
      */
     public function up()
     {
         $biz = $this->getContainer();
         $biz['db']->exec("ALTER TABLE `course_set_v8` ADD COLUMN `defaultCourseId` int(11) unsigned DEFAULT NULL COMMENT '默认的计划ID';");
-        $biz['db']->exec("update `course_set_v8` cs , `course_v8` c set  cs.`defaultCourseId` = c.id where   c.`courseSetid` = cs.id ");
+        $biz['db']->exec('update `course_set_v8` cs , `course_v8` c set  cs.`defaultCourseId` = c.id where   c.`courseSetid` = cs.id ');
     }
 
     /**
-     * Undo the migration
+     * Undo the migration.
      */
     public function down()
     {
         $biz = $this->getContainer();
-        $biz['db']->exec("ALTER TABLE `course_set_v8` DROP COLUMN `defaultCourseId`;");
+        $biz['db']->exec('ALTER TABLE `course_set_v8` DROP COLUMN `defaultCourseId`;');
     }
 }
