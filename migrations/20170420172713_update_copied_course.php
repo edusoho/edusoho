4d574cbd91dddf5e41f6c3fe65f43540c2b66bb1
hamster <malianbo@163.com>
2017-04-20 17:37:35 +0800

    format code

--- migrations/20170420172713_update_copied_course.php
+++ migrations/20170420172713_update_copied_course.php
@@ -5,21 +5,20 @@ use Phpmig\Migration\Migration;
 class UpdateCopiedCourse extends Migration
 {
     /**
-     * Do the migration
+     * Do the migration.
      */
     public function up()
     {
         $biz = $this->getContainer();
-        $biz['db']->exec("
+        $biz['db']->exec('
             UPDATE `course_v8`, `course_v8` c2 SET course_v8.parentId=0 WHERE course_v8.parentId = c2.id AND course_v8.parentId > 0 AND course_v8.courseSetId=c2.courseSetId;
-        ");
+        ');
     }
 
     /**
-     * Undo the migration
+     * Undo the migration.
      */
     public function down()
     {
-
     }
 }
