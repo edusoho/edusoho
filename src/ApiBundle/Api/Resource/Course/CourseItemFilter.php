1c9c9ef97d890d41355fe4a97b8d59c9c57de823
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-20 17:05:23 +0800

    精简item 字段

--- src/ApiBundle/Api/Resource/Course/CourseItemFilter.php
+++ src/ApiBundle/Api/Resource/Course/CourseItemFilter.php
@@ -8,4 +8,15 @@ use AppBundle\Common\ServiceToolkit;
 
 class CourseItemFilter extends Filter
 {
+    protected $publicFields = array(
+        'id', 'type', 'number', 'seq', 'title', 'tasks', 'itemType'
+    );
+
+    protected function publicFields(&$data)
+    {
+        if (!empty($data['tasks'])) {
+            $tasKFilter = new CourseTaskFilter();
+            $tasKFilter->filters($data['tasks']);
+        }
+    }
 }
---
+++ src/ApiBundle/Api/Resource/Course/CourseTaskFilter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ApiBundle\Api\Resource\Course;
+
+use ApiBundle\Api\Resource\Filter;
+
+class CourseTaskFilter extends Filter
+{
+    protected $simpleFields = array(
+        'id', 'title', 'isFree', 'startTime', 'endTime', 'status', 'length', 'mode'
+    );
+}
