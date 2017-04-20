4969c4b73e0d9923b40cc6c6a80622885a36fbae
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-20 17:19:50 +0800

    增加type字段

--- src/ApiBundle/Api/Resource/Course/CourseTaskFilter.php
+++ src/ApiBundle/Api/Resource/Course/CourseTaskFilter.php
@@ -7,6 +7,6 @@ use ApiBundle\Api\Resource\Filter;
 class CourseTaskFilter extends Filter
 {
     protected $simpleFields = array(
-        'id', 'title', 'isFree', 'startTime', 'endTime', 'status', 'length', 'mode'
+        'id', 'title', 'isFree', 'startTime', 'endTime', 'status', 'length', 'mode', 'type'
     );
 }
