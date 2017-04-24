7cf8a07db07d24e4cba070628f95c5a7ffb5500d
wuli <wuli@howzhi.com>
2017-04-24 16:40:04 +0800

    merge

---
+++ app/Resources/static-src/app/js/open-course-manage/base/base.js
@@ -0,0 +1,97 @@
+import 'common/select2';
+export default class Base {
+  constructor() {
+    this.initSelect2();
+    this.initCkeditor();
+    this.initValidator();
+  }
+
+  initSelect2() {
+    $('#course_tags').select2({
+      ajax: {
+        url: app.arguments.tagMatchUrl + '#',
+        dataType: 'json',
+        quietMillis: 100,
+        data: function (term, page) {
+          return {
+            q: term,
+            page_limit: 10
+          };
+        },
+        results: function (data) {
+          var results = [];
+          $.each(data, function (index, item) {
+
+            results.push({
+              id: item.name,
+              name: item.name
+            });
+          });
+
+          return {
+            results: results
+          };
+
+        }
+      },
+      initSelection: function (element, callback) {
+        var data = [];
+        $(element.val().split(",")).each(function () {
+          data.push({
+            id: this,
+            name: this
+          });
+        });
+        callback(data);
+      },
+      formatSelection: function (item) {
+        return item.name;
+      },
+      formatResult: function (item) {
+        return item.name;
+      },
+      width: 'off',
+      multiple: true,
+      maximumSelectionSize: 20,
+      placeholder: Translator.trans('请输入标签'),
+      width: 'off',
+      multiple: true,
+      createSearchChoice: function () {
+        return null;
+      },
+      maximumSelectionSize: 20
+    });
+  }
+  
+  initValidator() {
+    let $form = $('#course-form');
+    let validator = $form.validate({
+      rules: {
+        title: {
+          required: true
+        },
+        subtitle: {
+          required: true,
+          maxlength: 70,
+        },
+      }
+    })
+
+    $('#course-create-btn').click(()=> {
+      if(validator.form()) {
+        $from.submit();
+      }
+    })
+  }
+
+  initCkeditor() {
+    if ($('#course-about-field').length > 0) {
+      CKEDITOR.replace('course-about-field', {
+        allowedContent: true,
+        toolbar: 'Detail',
+        filebrowserImageUploadUrl: $('#course-about-field').data('imageUploadUrl')
+      });
+    }
+  }
+
+}
