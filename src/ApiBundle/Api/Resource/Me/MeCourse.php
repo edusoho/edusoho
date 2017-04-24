dd065bf126f3600aed17bd05e8a516caf356a660
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-24 15:33:00 +0800

    我的课程

---
+++ src/ApiBundle/Api/Resource/Me/MeCourse.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace ApiBundle\Api\Resource\Me;
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Course\Service\CourseService;
+use Biz\Course\Service\MemberService;
+
+class MeCourse extends AbstractResource
+{
+    public function search(ApiRequest $request)
+    {
+        $conditions = $request->query->all();
+        $conditions['status'] = 'published';
+        $conditions['classroomId'] = 0;
+        $conditions['userId'] = $this->getCurrentUser()->getId();
+
+        list($offset, $limit) = $this->getOffsetAndLimit($request);
+        $members = $this->getCourseMemberService()->searchMembers(
+            $conditions,
+            array('lastViewTime' => 'DESC'),
+            $offset,
+            $limit
+        );
+
+        $courses = $this->getCourseService()->findCoursesByIds(array_column($members, 'courseId'));
+
+        $this->appendLearnedNum($courses, $members);
+
+        $total = $this->getCourseMemberService()->countMembers($conditions);
+
+        $this->getOCUtil()->multiple($courses, array('courseSetId'), 'courseSet');
+
+        return $this->makePagingObject(array_values($courses), $total, $offset, $limit);
+    }
+
+    private function appendLearnedNum(&$courses, $members)
+    {
+        foreach ($members as $member) {
+            if (!empty($courses[$member['courseId']])) {
+                $courses[$member['courseId']]['learnedNum'] = $member['learnedNum'];
+            }
+        }
+    }
+
+    /**
+     * @return MemberService
+     */
+    private function getCourseMemberService()
+    {
+        return $this->service('Course:MemberService');
+    }
+
+    /**
+     * @return CourseService
+     */
+    private function getCourseService()
+    {
+        return $this->service('Course:CourseService');
+    }
+}
