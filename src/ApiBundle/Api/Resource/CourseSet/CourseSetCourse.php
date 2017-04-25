b07470a849c266aa74558c8cfb1878ddbd869e8f
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-25 17:39:56 +0800

    增加access

--- src/ApiBundle/Api/Resource/CourseSet/CourseSetCourse.php
+++ src/ApiBundle/Api/Resource/CourseSet/CourseSetCourse.php
@@ -6,6 +6,8 @@ use ApiBundle\Api\Annotation\ApiConf;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Exception\ResourceNotFoundException;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Course\Service\CourseService;
+use Biz\Course\Service\CourseSetService;
 
 class CourseSetCourse extends AbstractResource
 {
@@ -14,16 +16,40 @@ class CourseSetCourse extends AbstractResource
      */
     public function search(ApiRequest $request, $courseSetId)
     {
-        $courseSet = $this->service('Course:CourseSetService')->getCourseSet($courseSetId);
+        $courseSet = $this->getCourseSetService()->getCourseSet($courseSetId);
 
         if (!$courseSet) {
             throw new ResourceNotFoundException('课程不存在');
         }
 
-        $courses = $this->service('Course:CourseService')->findPublishedCoursesByCourseSetId($courseSetId);
+        $courses = $this->getCourseService()->findPublishedCoursesByCourseSetId($courseSetId);
         $this->getOCUtil()->multiple($courses, array('creator', 'teacherIds'));
         $this->getOCUtil()->multiple($courses, array('courseSetId'), 'courseSet');
 
+        $this->addAccessAttr($courses);
+
         return $courses;
+    }
+
+    private function addAccessAttr(&$courses)
+    {
+        foreach ($courses as &$course) {
+            $course['access'] = $this->getCourseService()->canJoinCourse($course['id']);
+        }
+    }
+
+    /**
+     * @return CourseSetService
+     */
+    private function getCourseSetService()
+    {
+        return $this->service('Course:CourseSetService');
+    }
+    /**
+     * @return CourseService
+     */
+    private function getCourseService()
+    {
+        return $this->service('Course:CourseService');
     }
 }
