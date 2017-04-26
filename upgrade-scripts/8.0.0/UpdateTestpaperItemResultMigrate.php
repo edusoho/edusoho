78bb18e1f80801ac851662687edaf53cd6cefa1a
wangdanyue <wangdanyue@howzhi.com>
2017-04-26 17:36:28 +0800

    testpaper migrate fix

--- upgrade-scripts/8.0.0/UpdateTestpaperItemResultMigrate.php
+++ upgrade-scripts/8.0.0/UpdateTestpaperItemResultMigrate.php
@@ -7,7 +7,7 @@ class UpdateTestpaperItemResultMigrate extends AbstractMigrate
         $sql = "SELECT count(id) FROM `testpaper_item_result_v8` WHERE type = 'testpaper';";
         $count = $this->getConnection()->fetchColumn($sql);
         if ($count > 300000) {
-            $nextPage = $this->bigDataUpdate($page,$count);
+            $nextPage = $this->bigDataUpdate($page);
             if (!empty($nextPage)) {
                 return $nextPage;
             }
@@ -20,28 +20,28 @@ class UpdateTestpaperItemResultMigrate extends AbstractMigrate
         }
     }
 
-    private function bigDataUpdate($page,$count)
+    private function bigDataUpdate($page)
     {
         $start = $this->getStart($page);
 
         $sql = "SELECT id FROM `testpaper_item_result_v8` WHERE type = 'testpaper' order by id asc limit 1 offset {$start}";
         $startId = $this->getConnection()->fetchColumn($sql);
 
+        if (empty($startId)) {
+            return ;
+        }
+
         $end = $start + $this->perPageCount;
         $sql = "SELECT id FROM `testpaper_item_result_v8` WHERE type = 'testpaper' order by id asc limit 1 offset {$end}";
         $endId = $this->getConnection()->fetchColumn($sql);
-        $endId = empty($endId) ? $count : $endId;
+        $endWhere = empty($endId) ? '' : " and ir.id < {$endId} ";
 
-        if (!empty($startId)) {
-            $sql = "UPDATE testpaper_item_result_v8 AS ir, testpaper_v8 as t SET ir.testId = t.id WHERE t.migrateTestId = ir.testId and ir.type='testpaper' and ir.id >= {$startId} and ir.id < {$endId};";
-            $this->getConnection()->exec($sql);
+        $sql = "UPDATE testpaper_item_result_v8 AS ir, testpaper_v8 as t SET ir.testId = t.id WHERE t.migrateTestId = ir.testId and ir.type='testpaper' and ir.id >= {$startId} {$endWhere};";
+        $this->getConnection()->exec($sql);
 
-            $sql = "UPDATE testpaper_item_result_v8 AS ir, testpaper_result_v8 AS tr SET ir.resultId = tr.id WHERE tr.migrateResultId = ir.resultId and ir.type='testpaper' and ir.id >= {$startId} and ir.id < {$endId};";
-            $this->getConnection()->exec($sql);
+        $sql = "UPDATE testpaper_item_result_v8 AS ir, testpaper_result_v8 AS tr SET ir.resultId = tr.id WHERE tr.migrateResultId = ir.resultId and ir.type='testpaper' and ir.id >= {$startId} {$endWhere};";
+        $this->getConnection()->exec($sql);
 
-            return $page + 1;
-        }
-
-        return ;
+        return $page + 1;
     }
 }
