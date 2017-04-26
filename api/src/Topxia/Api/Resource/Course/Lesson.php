2417deaa7a3e5ca65b59b64b0d25b0728e5ba6e4
ruanwenqin <ruanwenqin@howzhi.com>
2017-04-26 18:16:24 +0800

    使用task 的courseid

--- api/src/Topxia/Api/Resource/Course/Lesson.php
+++ api/src/Topxia/Api/Resource/Course/Lesson.php
@@ -38,7 +38,7 @@ class Lesson extends BaseResource
                 return $this->error('not_login', '您尚未登录，不能查看该课时');
             }
         } else {
-            if (!$this->getCourseMemberService()->isCourseMember($lesson['courseId'], $currentUser['id'])) {
+            if (!$this->getCourseMemberService()->isCourseMember($task['courseId'], $currentUser['id'])) {
                 if (!$lesson['free']) {
                     return $this->error('not_student', '你不是该课程学员，请加入学习');
                 }
