c0aaf693f86d65cd348a60b5429ef2f969e48a0b
wuli <wuli@howzhi.com>
2017-04-27 18:11:20 +0800

    redmin

--- app/Resources/static-src/app/js/task/plugin/note/index.js
+++ app/Resources/static-src/app/js/task/plugin/note/index.js
@@ -1,29 +1,38 @@
 import { saveRedmineLoading, saveRedmineSuccess } from '../save-redmine';
-
+let heigth = ($('.js-sidebar-pane').height() - 175);
 let $content = $('#note-content-field');
-
 let editor = CKEDITOR.replace('note-content-field', {
   toolbar: 'Simple',
   filebrowserImageUploadUrl: $content.data('imageUploadUrl'),
   allowedContent: true,
-  height: 300
+  height: heigth < 300 ? 200 : heigth,
 });
 
 editor.on('change', () => {
   $content.val(editor.getData());
 });
 
-
 $('#note-save-btn').click(function (event) {
   let $btn = $(this);
   $btn.attr('disabled', 'disabled');
   event.preventDefault();
+  saveNote();
+});
+
+setInterval(saveNote,10000);
+
+function saveNote($btn = null) {
+  if(!$content.val()) {
+    return;
+  }
   let $form = $('#task-note-plugin-form');
   let data = $form.serializeArray();
   saveRedmineLoading();
   $.post($form.attr('action'), data)
     .then((response) => {
       saveRedmineSuccess();
-      $btn.removeAttr('disabled');
+      if($btn) {
+        $btn.removeAttr('disabled');
+      }
     });
-});
+}
