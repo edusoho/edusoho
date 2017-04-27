32aeef272447bbdc45d278a070c0f675c42320c7
wangdanyue <wangdanyue@howzhi.com>
2017-04-27 16:10:06 +0800

    testpaper-result-update-fix

---
+++ upgrade-scripts/upgrade-8.0.4.php
@@ -0,0 +1,113 @@
+<?php
+
+use Symfony\Component\Filesystem\Filesystem;
+use Topxia\Service\Common\Servicebiz;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function update()
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $this->updateScheme();
+            $this->getConnection()->commit();
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            throw $e;
+        }
+
+        try {
+            $dir = realpath($this->biz['kernel.root_dir']. "/../web/install");
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $developerSetting = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        $this->getSettingService()->set('developer', $developerSetting);
+        $this->getSettingService()->set("crontab_next_executed_time", time());
+    }
+
+    private function updateScheme()
+    {
+        $countSql = "SELECT count(id) from testpaper_result_v8 where migrateResultId > 0 and courseId = 0";
+        $count = $this->getConnection()->fetchColumn($countSql);
+
+        if (!empty($count)) {
+            $sql = "UPDATE testpaper_result_v8 as t, course_lesson as cl set 
+                t.courseId = cl.courseId,
+                t.courseSetId = cl.courseId, 
+                t.lessonId = cl.id 
+                where cl.type = 'testpaper' and t.type = 'testpaper' and t.testId = cl.mediaId and t.courseId = 0 and migrateResultId > 0";
+            $this->getConnection()->exec($sql);
+        }
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function isIndexExist($table, $filedName, $indexName)
+    {
+        $sql = "show index from `{$table}` where column_name = '{$filedName}' and Key_name = '{$indexName}';";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+
+    protected function isCrontabJobExist($code)
+    {
+        $sql = "select * from crontab_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+
+    private function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+}
