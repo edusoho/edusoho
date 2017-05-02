3ee71737a25ad1a728a0745e2579ffa20c3962e2
yangshuai <yangshuai@howzhi.com>
2017-05-02 21:16:18 +0800

    fix code style  fixed

--- src/AppBundle/Command/TranslationAddNewKeyCommand.php
+++ src/AppBundle/Command/TranslationAddNewKeyCommand.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Command;
 
-use AppBundle\Command\BaseCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,81 +18,84 @@ class TranslationAddNewKeyCommand extends BaseCommand
             ->setDescription('add translation into en file');
     }
 
-
-    private function isYamlKeyVal($line){
+    private function isYamlKeyVal($line)
+    {
         $pattern = '/^([a-zA-Z]+[._]?)+[a-zA-Z]+:/';
-        $flag = preg_match($pattern,$line);
+        $flag = preg_match($pattern, $line);
+
         return $flag;
     }
 
-    private function isAnnotationLine($line){
+    private function isAnnotationLine($line)
+    {
         $pattern = '/^#.+/';
-        $flag = preg_match($pattern,$line);
+        $flag = preg_match($pattern, $line);
+
         return $flag;
     }
 
-    private function isEnglish($val){
+    private function isEnglish($val)
+    {
         return !preg_match("/([\x81-\xfe][\x40-\xfe])/", $val);
     }
 
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->rootDir       = dirname($this->getContainer()->getParameter('kernel.root_dir'));
+        $this->rootDir = dirname($this->getContainer()->getParameter('kernel.root_dir'));
         $translationFilePath = $input->getArgument('translationFilePath');
 
-        $translationFile = $this->rootDir."/".$translationFilePath;
+        $translationFile = $this->rootDir.'/'.$translationFilePath;
         if (!file_exists($translationFile)) {
             $output->writeln('<error>File Not Exist</error>');
             exit;
         }
 
         $transArray = array();
-        $file_handle = fopen($translationFile, "r");
+        $file_handle = fopen($translationFile, 'r');
         $lastKey;
         $lastVal;
         while (!feof($file_handle)) {
             $line = fgets($file_handle);
             $line = trim($line);
-            if(!$line){
+            if (!$line) {
                 continue;
             }
-            if($this->isAnnotationLine($line)){
+            if ($this->isAnnotationLine($line)) {
                 continue;
             }
             // var_dump($line);
             $flag = $this->isYamlKeyVal($line);
             // var_dump($flag);
-            if($flag){
-                $yamlArray = explode(':',$line,2);
+            if ($flag) {
+                $yamlArray = explode(':', $line, 2);
                 $yamlKey = $yamlArray[0];
-                $yamlKey = str_replace(" ", "",$yamlKey); 
+                $yamlKey = str_replace(' ', '', $yamlKey);
                 $yamlVal = $yamlArray[1];
                 $yamlVal = trim($yamlVal);
                 $transArray[$yamlKey] = $yamlVal;
                 $lastKey = $yamlKey;
                 $lastVal = $yamlVal;
-            }else{
+            } else {
                 $lastVal = $lastVal.$line;
                 $transArray[$lastKey] = $lastVal;
             }
         }
 
         // var_dump($transArray);exit();
-        $enFile         = $this->getEnFile();
+        $enFile = $this->getEnFile();
         $enTranslations = Yaml::parse($enFile);
         $newEnTrans = array();
 
         foreach ($transArray as $key => $translation) {
-            if (!empty($enTranslations[$key]) && $this->isEnglish($translation) ) {
+            if (!empty($enTranslations[$key]) && $this->isEnglish($translation)) {
                 $enTranslations[$key] = $translation;
-            }else{
+            } else {
                 $newEnTrans[$key] = $translation;
             }
         }
 
         file_put_contents("{$this->rootDir}/app/Resources/translations/messages.en.yml", Yaml::dump($enTranslations));
-         $output->writeln('<info>新的翻译，添加完成</info>');
+        $output->writeln('<info>新的翻译，添加完成</info>');
 
         file_put_contents("{$this->rootDir}/app/Resources/translations/messages.en_newKey.yml", Yaml::dump($newEnTrans));
         $output->writeln('<info>提交的新的key，生成文件完成</info>');
@@ -103,5 +105,4 @@ class TranslationAddNewKeyCommand extends BaseCommand
     {
         return $this->rootDir.'/app/Resources/translations/messages.en.yml';
     }
-
 }
