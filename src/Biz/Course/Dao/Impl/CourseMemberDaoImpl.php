fee19b024be20aac73743a36173ee8f4ba71bb8e
tianshengjiie <tianshengjie@howzhi.com>
2017-05-02 10:32:49 +0800

    coursememberdao，查询条件增加updatedtime

--- src/Biz/Course/Dao/Impl/CourseMemberDaoImpl.php
+++ src/Biz/Course/Dao/Impl/CourseMemberDaoImpl.php
@@ -426,6 +426,7 @@ class CourseMemberDaoImpl extends GeneralDaoImpl implements CourseMemberDao
                 'deadline >= :deadlineGreaterThan',
                 'lastViewTime >= lastViewTime_GE',
                 'lastLearnTime >= :lastLearnTimeGreaterThan',
+                'updatedTime >= :updatedTime_GE'
             ),
         );
     }
