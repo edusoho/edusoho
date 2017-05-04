62e881613c232efee5a4095945fea86c41f05321
Allen Lau <liuyangyang@howzhi.com>
2017-05-04 14:29:55 +0800

    add

--- app/Resources/static-src/app/common/doc-player.js
+++ app/Resources/static-src/app/common/doc-player.js
@@ -58,7 +58,7 @@ class DocPlayer {
   initPDFJSViewer() {
     $("html").attr('dir', 'ltr');
 
-    let src = 'http://opencdn.edusoho.net/pdf.js/v3/viewer.html#' + this.pdfUrl;
+    let src = '//opencdn.edusoho.net/pdf.js/v3/viewer.html#' + this.pdfUrl;
 
     if (!this.canCopy) {
       src += '#false';
--- app/Resources/static-src/app/js/activity/doc/index.js
+++ app/Resources/static-src/app/js/activity/doc/index.js
@@ -2,7 +2,6 @@ import DocPlayer from '../../../common/doc-player';
 
 let $element = $('#document-content');
 let watermarkUrl = $element.data('watermark-url');
-console.log(watermarkUrl);
 
 if(watermarkUrl) {
   console.log('watermarkUrl');
