21bb020571a70de6813a76f1ef6da6f06c95f4de
bellorchid <zhongyunchang@yeah.net>
2017-05-09 10:51:05 +0800

    fix

--- app/Resources/static-src/app/js/activity/doc/index.js
+++ app/Resources/static-src/app/js/activity/doc/index.js
@@ -13,15 +13,22 @@ if(watermarkUrl) {
 }
 
 function initDocPlayer(contents) {
-  let doc = new DocPlayer({
-    element: $element,
-    swfUrl: $element.data('swf'),
-    pdfUrl: $element.data('pdf'),
-    watermarkOptions: {
-      contents,
-      xPosition: 'center',
-      yPosition: 'center',
-      rotate: 45,
-    }
-  });
+  // let doc = new DocPlayer({
+  //   element: $element,
+  //   swfUrl: $element.data('swf'),
+  //   pdfUrl: $element.data('pdf'),
+  //   watermarkOptions: {
+  //     contents,
+  //     xPosition: 'center',
+  //     yPosition: 'center',
+  //     rotate: 45,
+  //   }
+  // });
+  let doc = new DocPlayerSDK({
+      id: 'document-content',
+      src: 'http://7xqnui.com1.z0.glb.clouddn.com/v7aes-Scalability/12eb6eca36dd6ce2_html',
+      key: 'fkjwofojdjjiweio',
+      iv: '4321876521096543'
+      // iv: '1234567890123456'
+    })
 }
