39bc175cee825cd15a7db955b89e09c03d0ff887
sunyanyan <sunyanyan@hwozhi.com>
2017-05-12 14:08:02 +0800

    modify bug

--- app/Resources/static-src/app/js/task-manage/create/editor.js
+++ app/Resources/static-src/app/js/task-manage/create/editor.js
@@ -19,6 +19,7 @@ class Editor {
     this.step = 1;
     this.loaded = false;
     this.contentUrl = '';
+    this.needAppend = true;
     this._init();
     this._initEvent();
   }
@@ -85,15 +86,26 @@ class Editor {
 
     $.post(this.$task_manage_type.data('saveUrl'), postData)
       .done((response) => {
-        const needAppend = response.append;
+        //alert(1);
+        this.needAppend = response.append;
         const html = response.html;
+        console.log(needAppend);
+        console.log(html);
+        this.$task_manage_type.data('saveUrl');
+        console.log(this.$task_manage_type.data('saveUrl'));
         this.$element.modal('hide');
         if(!$('.js-task-empty').hasClass('hidden') ){
             $('.js-task-empty').addClass('hidden');
         }
         if (needAppend === false) {
+
+          let editIds = this.$task_manage_type.data('saveUrl').split('/');
+          let $item = $(`#chapter-${editIds[editIds.length-2]}`);
+          console.log(editIds[editIds.length-2]);
+
           // @TODO这里也需要返回html,进行替换   
           document.location.reload();
+          this.showDefaultSetting($item);
         }
 
         let chapterId = postData.find(function (input) {
