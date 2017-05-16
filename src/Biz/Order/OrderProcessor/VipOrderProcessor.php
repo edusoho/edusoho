6eb3a50318466c7177c6cb6ae463134b84efef21
Allen Lau <liuyangyang@howzhi.com>
2017-05-16 21:51:25 +0800

    fix code style  fixed

--- src/Biz/Order/OrderProcessor/VipOrderProcessor.php
+++ src/Biz/Order/OrderProcessor/VipOrderProcessor.php
@@ -70,7 +70,7 @@ class VipOrderProcessor extends BaseProcessor implements OrderProcessor
             if (!ArrayToolkit::requireds($fields, array('unit', 'duration'))) {
                 throw new Exception('参数不正确');
             }
-            if((int)$fields['duration'] > static::MAX_DURATION){
+            if ((int) $fields['duration'] > static::MAX_DURATION) {
                 throw new Exception('参数错误!');
             }
 
