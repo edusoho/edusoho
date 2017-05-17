fa78e50f01b153a4199c5009db65b9ab39a5c7f0
Allen Lau <liuyangyang@howzhi.com>
2017-05-17 16:13:23 +0800

    fix code style  fixed

--- src/Biz/Classroom/Service/Impl/ClassroomServiceImpl.php
+++ src/Biz/Classroom/Service/Impl/ClassroomServiceImpl.php
@@ -732,7 +732,7 @@ class ClassroomServiceImpl extends BaseService implements ClassroomService
             throw $this->createServiceException('学员不存在，备注失败!');
         }
 
-        $fields = array('remark' => empty($remark) ? '' : (string)$remark);
+        $fields = array('remark' => empty($remark) ? '' : (string) $remark);
 
         return $this->getClassroomMemberDao()->update($member['id'], $fields);
     }
@@ -1666,7 +1666,7 @@ class ClassroomServiceImpl extends BaseService implements ClassroomService
             $id,
             array(
                 'recommended' => 1,
-                'recommendedSeq' => (int)$number,
+                'recommendedSeq' => (int) $number,
                 'recommendedTime' => time(),
             )
         );
