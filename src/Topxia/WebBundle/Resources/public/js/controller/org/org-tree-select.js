96c35436f3e793c4c01fd4a82a8688124d19c2f5
yangshuai <yangshuai@howzhi.com>
2017-05-23 15:19:01 +0800

    fix confilct

--- src/Topxia/WebBundle/Resources/public/js/controller/org/org-tree-select.js
+++ src/Topxia/WebBundle/Resources/public/js/controller/org/org-tree-select.js
@@ -1,24 +1,4 @@
 define(function(require, exports, module) {
-<<<<<<< HEAD
-    var SelectTree = require('edusoho.selecttree');
-    exports.run = function() {
-        
-        var initOrgOptions = function() {
-            var option = {};
-            if ($("#modalOrgSelectTree").length > 0) {
-                option.element = "#modalOrgSelectTree";
-                option.modal = true;
-            } else if ($("#orgSelectTree").length > 0) {
-                option.element = "#orgSelectTree";
-            }
-            return option;
-        }
-        var option = initOrgOptions();
-        if (option.element) {
-            var selectTree = new SelectTree(option);
-        }
-    };
-=======
   var SelectTree = require('edusoho.selecttree');
   exports.run = function() {
       
@@ -39,5 +19,4 @@ define(function(require, exports, module) {
     }
   };
 
->>>>>>> develop
 });
