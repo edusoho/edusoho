bd1923e8f01504eb5672b59afe9a4702cd9c0737
ruanwenqin <ruanwenqin@howzhi.com>
2017-05-26 12:42:06 +0800

    重置回去

--- src/ApiBundle/DependencyInjection/ApiExtension.php
+++ src/ApiBundle/DependencyInjection/ApiExtension.php
@@ -3,7 +3,6 @@
 namespace ApiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -15,24 +14,5 @@ class ApiExtension extends Extension
         // load services
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $this->loadListeners($container);
     }
-
-    private function loadListeners(ContainerBuilder $container)
-    {
-        $container->getDefinition('api_firewall')
-            ->replaceArgument(0, array(
-                new Reference('api_oauth2_authentication_listener'),
-                new Reference('api_basic_authentication_listener'),
-                new Reference('api_token_header_listener'),
-                new Reference('api_anonymous_listener'),
-            ));
-
-        $container->getDefinition('api_authentication_manager')
-            ->replaceArgument(0, array(
-               new Reference('api_default_authentication')
-            ));
-    }
-
 }
