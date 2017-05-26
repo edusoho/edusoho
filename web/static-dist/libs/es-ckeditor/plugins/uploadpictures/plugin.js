5f906021f5f2f4dd1eb62898ca39678d95a6bf1e
Allen Lau <liuyangyang@howzhi.com>
2017-05-26 21:59:06 +0800

    add static file

--- web/static-dist/libs/es-ckeditor/plugins/uploadpictures/plugin.js
+++ web/static-dist/libs/es-ckeditor/plugins/uploadpictures/plugin.js
@@ -1,10 +1,11 @@
 CKEDITOR.plugins.add('uploadpictures', {
+    lang: 'en,zh,ug,zh-cn',
     icons: 'uploadpictures',
     init: function(editor) {
         CKEDITOR.dialog.add('uploadpictures', this.path + 'dialogs/uploadpictures.js');
         editor.addCommand('uploadpictures', new CKEDITOR.dialogCommand('uploadpictures'));
         editor.ui.addButton('uploadpictures', {
-            label: '批量图片上传',
+            label: editor.lang.uploadpictures.toolbar,
             command: 'uploadpictures'
         });
         CKEDITOR.scriptLoader.load(CKEDITOR.getUrl('plugins/uploadpictures/webuploader/webuploader.js'));
