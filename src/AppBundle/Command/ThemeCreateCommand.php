05c277243cc521f1ce3816e807edf91d5092543e
tianshengjiie <tianshengjie@howzhi.com>
2017-06-07 17:28:32 +0800

    fix

--- src/AppBundle/Command/ThemeCreateCommand.php
+++ src/AppBundle/Command/ThemeCreateCommand.php
@@ -48,7 +48,10 @@ class ThemeCreateCommand extends BaseCommand
         $this->createOtherDirectories($name);
         $this->createInstallScript();
         $this->createImg($dir, $name);
-
+        $this->createJs($name);
+        $this->createLess($name);
+        $this->createIndexView();
+        $this->createCssView($name);
         $output->writeln('创建主题包: <info>OK</info>');
     }
 
@@ -61,8 +64,9 @@ class ThemeCreateCommand extends BaseCommand
         $this->filesystem->mkdir($themeDir.'static-src/js');
         $this->filesystem->mkdir($themeDir.'static-src/font');
         $this->filesystem->mkdir($themeDir.'static-src/img');
-        $this->filesystem->mkdir($themeDir.'static-dist/'.$name.'theme');
-        $this->filesystem->mkdir($themeDir.'views');
+        $this->filesystem->mkdir($themeDir.'static-dist/'.$name.'theme/css');
+        $this->filesystem->mkdir($themeDir.'static-dist/'.$name.'theme/js');
+        $this->filesystem->mkdir($themeDir.'views/default');
         $this->filesystem->mkdir($themeDir.'Scripts');
         $this->output->writeln('创建目录: <info>OK</info>');
     }
@@ -102,7 +106,40 @@ class ThemeCreateCommand extends BaseCommand
         $green = ImageColorAllocate($myImage, 70, 195, 123);
         $white = ImageColorAllocate($myImage, 255, 255, 255);
 
-        imagettftext($myImage, 100, 0, 80, 200, $white, __DIR__.'/theme-tpl/OBLIVIOUSFONT.TTF',  "demo");
+        imagettftext($myImage, 100, 0, 70, 200, $white, __DIR__.'/theme-tpl/OBLIVIOUSFONT.TTF',  "demo");
         ImagePng($myImage, $this->themeDir.'theme.jpg');
+        $this->output->writeln('创建 图片: <info>OK</info>');
      }
+
+    private function createJs($name)
+    {
+        $data = file_get_contents(__DIR__.'/theme-tpl/mainjs.twig');
+        $data1 = file_get_contents(__DIR__.'/theme-tpl/main.js');
+        file_put_contents($this->themeDir.'static-src/js/main.js', $data);
+        file_put_contents($this->themeDir."static-dist/{$name}theme/js/main.js", $data1);
+        $this->output->writeln('创建js: <info>OK</info>');
+    }
+
+    private function createLess($name)
+    {
+        $data = file_get_contents(__DIR__.'/theme-tpl/themeless.twig');
+        file_put_contents($this->themeDir.'static-src/less/theme.less', $data);
+        file_put_contents($this->themeDir."static-dist/{$name}theme/css/theme.css", $data);
+        $this->output->writeln('创建less: <info>OK</info>');
+    }
+
+    private function createIndexView()
+    {
+        $data = file_get_contents(__DIR__.'/theme-tpl/index.twig');
+        file_put_contents($this->themeDir.'views/default/index.html.twig', $data);
+        $this->output->writeln('创建主题首页模板: <info>OK</info>');
+    }
+
+    private function createCssView($name)
+    {
+        $data = file_get_contents(__DIR__.'/theme-tpl/webpackcss.twig');
+        $data = str_replace('{{name}}', $name, $data);
+        file_put_contents($this->themeDir.'views/default/stylesheet-webpack.html.twig', $data);
+        $this->output->writeln('创建主题首页模板: <info>OK</info>');
+    }
 }
