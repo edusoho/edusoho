e16f922536f9abfd815225db77c47d20fd7d7052
zhongjie <zhongjie@howzhi.com>
2017-06-08 16:52:05 +0800

    迁移班级头部的js

--- app/Resources/static-src/app/js/classroom/index.js
+++ app/Resources/static-src/app/js/classroom/index.js
@@ -1,35 +1,6 @@
 import Sign from 'app/common/widget/sign';
 import 'app/common/widget/qrcode';
-
-let buy_btn = false;
-
-$('.buy-btn').click(function () {
-  if (!buy_btn) {
-    $('.buy-btn').addClass('disabled');
-    buy_btn = true;
-  }
-  return true;
-});
-
-$(".cancel-refund").on('click', function () {
-  if (!confirm(Translator.trans('真的要取消退款吗？'))) {
-    return false;
-  }
-
-  $.post($(this).data('url'), function () {
-    window.location.reload();
-  });
-});
-
-$("#quit").on('click', function () {
-  if (!confirm(Translator.trans('确定退出班级吗？'))) {
-    return false;
-  }
-
-  $.post($(this).data('url'), function () {
-    window.location.reload();
-  });
-});
+import 'app/common/widget/cancel-refund';
 
 
 if ($('#classroom-sign').length > 0) {
