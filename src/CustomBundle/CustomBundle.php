855a5252a10ec2b3a1d5ec28e0d6b77b6b73a5c2
Allen Lau <liuyangyang@howzhi.com>
2017-06-12 20:59:32 +0800

    fix

--- src/CustomBundle/CustomBundle.php
+++ src/CustomBundle/CustomBundle.php
@@ -2,9 +2,7 @@
 
 namespace CustomBundle;
 
-use AppBundle\DependencyInjection\Compiler\ExtensionPass;
 use Codeages\PluginBundle\System\PluginBase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CustomBundle extends PluginBase
 {
@@ -13,10 +11,6 @@ class CustomBundle extends PluginBase
         return 'AppBundle';
     }
 
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new ExtensionPass());
-    }
     public function boot()
     {
         parent::boot();
