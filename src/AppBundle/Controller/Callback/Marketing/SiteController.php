2707e1b9d83fbfa5bb7829f4f6ebc354af854115
tangyue <tangyue@howzhi.com>
2017-06-12 21:01:01 +0800

    网站信息获取

---
+++ src/AppBundle/Controller/Callback/Marketing/SiteController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller\Callback\Marketing;
+
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Controller\BaseController;
+use AppBundle\Common\ArrayToolkit;
+
+class SiteController extends BaseController
+{
+    public function SiteInfoAction(Request $request)
+    {
+        $site = $this->getSettingService()->get('site', array());
+        $consult = $this->getSettingService()->get('consult', array());
+
+        $siteInfo = [
+            'name' => $site['name'],
+            'logo' => empty($site['logo']) ? '' : $this->getWebExtension()->getFurl($site['logo']),
+            'about' => $site['slogan'],
+            'wechat' => empty($consult['webchatURI']) ? '' : $this->getWebExtension()->getFurl($consult['webchatURI']),
+            'qq' => empty($consult['qq']) ? '' : $consult[0]['number'],
+            'telephone' => empty($consult['phone']) ? '' : $consult[0]['number']
+        ];
+
+        return $this->createJsonResponse($siteInfo);
+    }
+
+    protected function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+    protected function getWebExtension()
+    {
+        return $this->container->get('web.twig.extension');
+    }
+}
