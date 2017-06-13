adf3088f3ea539390359067f5956d9fe546d5e30
zhangyao <zhangyao@howzhi.com>
2017-06-13 10:15:40 +0800

    前台我的积分兑换记录

--- src/AppBundle/Controller/My/RewardPointController.php
+++ src/AppBundle/Controller/My/RewardPointController.php
@@ -38,8 +38,40 @@ class RewardPointController extends BaseController
         ));
     }
 
+    public function exchangeAction(Request $request)
+    {
+        $user = $this->getCurrentUser();
+        $conditions['userId'] = $user['id'];
+        $productOrderCount = $this->getProductOrderService()->countProductOrders($conditions);
+        $paginator = new Paginator(
+            $request,
+            $productOrderCount,
+            10
+        );
+
+        $productOrders = $this->getProductOrderService()->searchProductOrders(
+            $conditions,
+            array('createdTime' => 'DESC'),
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
+        return $this->render(
+            'reward-point/exchange.html.twig',
+            array(
+                'productOrders' => $productOrders,
+                'paginator' => $paginator,
+            )
+        );
+    }
+
     protected function getAccountFlowService()
     {
         return $this->createService('RewardPoint:AccountFlowService');
+    }
+
+    protected function getProductOrderService()
+    {
+        return $this->createService('RewardPoint:ProductOrderService');
     }
 }
