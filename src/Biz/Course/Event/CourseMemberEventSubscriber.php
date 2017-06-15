9a904e5aa7ac478c5b4d023590533bd2cd9cbb9d
ruanwenqin <ruanwenqin@howzhi.com>
2017-06-15 20:55:07 +0800

    learned time 统计

--- src/Biz/Course/Event/CourseMemberEventSubscriber.php
+++ src/Biz/Course/Event/CourseMemberEventSubscriber.php
@@ -30,7 +30,6 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
             'classroom.course.join' => 'onClassroomCourseJoin',
             'classroom.course.copy' => 'onClassroomCourseCopy',
 
-            'course.task.delete' => 'onTaskDelete',
             'course.task.finish' => 'onTaskFinish',
         );
     }
@@ -128,7 +127,7 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
         }
     }
 
-    private function publishStatus($event, $type)
+    private function publishStatus(Event $event, $type)
     {
         $course = $event->getSubject();
         $member = $event->getArgument('member');
@@ -162,15 +161,11 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
     public function onTaskFinish(Event $event)
     {
         $taskResult = $event->getSubject();
-        $user = $event->getArgument('user');
-        $this->updateMemberLearnData($taskResult['courseId'], $user['id']);
-    }
-
-    public function onTaskDelete(Event $event)
-    {
-        $task = $event->getSubject();
-        $user = $event->getArgument('user');
-        $this->updateMemberLearnedNum($task['courseId'], $user['id']);
+        $this->getCourseService()->recountLearningData($taskResult['courseId'], $taskResult['userId']);
+        $this->getCourseMemberService()->updateMembers(
+            array('courseId' => $taskResult['courseId'], 'userId' => $taskResult['userId']),
+            array('lastLearnTime' => time())
+        );
     }
 
     protected function getWelcomeMessageBody($user, $course)
@@ -273,39 +268,6 @@ class CourseMemberEventSubscriber extends EventSubscriber implements EventSubscr
     protected function getCourseMemberService()
     {
         return $this->getBiz()->service('Course:MemberService');
-    }
-
-    protected function updateMemberLearnedNum($courseId, $userId)
-    {
-        $member = $this->getCourseMemberService()->getCourseMember($courseId, $userId);
-
-        $conditions = array(
-            'status' => 'finish',
-            'courseId' => $courseId,
-            'userId' => $userId,
-        );
-        $learnedNum = $this->getTaskResultService()->countTaskResults($conditions);
-
-        $this->getCourseMemberService()->updateMember($member['id'], array('learnedNum' => $learnedNum));
-    }
-
-    private function updateMemberLearnData($courseId, $userId)
-    {
-        $member = $this->getCourseMemberService()->getCourseMember($courseId, $userId);
-
-        $conditions = array(
-            'status' => 'finish',
-            'courseId' => $courseId,
-            'userId' => $userId,
-        );
-        $learnedNum = $this->getTaskResultService()->countTaskResults($conditions);
-
-        $learnData = array(
-            'learnedNum' => $learnedNum,
-            'lastLearnTime' => time(),
-        );
-
-        $this->getCourseMemberService()->updateMember($member['id'], $learnData);
     }
 
     /**
