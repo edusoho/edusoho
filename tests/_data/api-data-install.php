2190d6123ec45f5854b0726b23ac9c361c81b15d
ruanwenqin <ruanwenqin@howzhi.com>
2017-06-19 15:10:46 +0800

    打印错误信息

--- tests/_data/api-data-install.php
+++ tests/_data/api-data-install.php
@@ -8,6 +8,7 @@ $config = array(
 );
 
 $pdo = new PDO("mysql:host={$config['database_host']};", "{$config['database_user']}", "{$config['database_password']}");
+
 $pdo->exec('SET NAMES utf8');
 
 $pdo->exec("drop database if exists `{$config['database_name']}`;");
@@ -22,4 +23,5 @@ $result = $pdo->exec($sql);
 
 if ($result === false) {
     echo '创建数据库表结构失败，请删除数据库后重试！';
+    print_r($pdo->errorInfo());
 }
