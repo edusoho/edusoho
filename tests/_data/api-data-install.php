89806afd8a5a30310238964f13610f58b0e09624
ruanwenqin <ruanwenqin@howzhi.com>
2017-06-19 14:51:20 +0800

    使用 php 安装数据

---
+++ tests/_data/api-data-install.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = array(
+    'database_host' => 'localhost',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_name' => 'edusoho_test'
+);
+
+
+$pdo = new PDO("mysql:host={$config['database_host']};", "{$config['database_user']}", "{$config['database_password']}");
+$pdo->exec('SET NAMES utf8');
+
+$pdo->exec("drop database if exists `{$config['database_name']}`;");
+
+$pdo->exec("create database `{$config['database_name']}`;");
+
+$pdo->exec("USE `{$config['database_name']}`;");
+
+$sql = file_get_contents(__DIR__.'/api-test-data.sql');
+
+$result = $pdo->exec($sql);
+
+if ($result === false) {
+    echo '创建数据库表结构失败，请删除数据库后重试！';
+}
