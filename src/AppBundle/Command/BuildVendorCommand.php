add1e6b20355828a643b9e15e4a7994ee2fd05ef
Allen Lau <liuyangyang@howzhi.com>
2017-06-27 00:01:07 +0800

    refoctor

--- src/AppBundle/Command/BuildVendorCommand.php
+++ src/AppBundle/Command/BuildVendorCommand.php
@@ -10,39 +10,6 @@ use Symfony\Component\Finder\Finder;
 
 class BuildVendorCommand extends ContainerAwareCommand
 {
-    /**
-     * project root dir.
-     *
-     * @var string
-     */
-    public $rootDir;
-
-    /**
-     * build dir.
-     *
-     * @var string
-     */
-    public $buildDir;
-
-    public $unneededFiles = array(
-        'appveyor.yml',
-        'CONTRIBUTING.md',
-        'CONTRIBUTORS.md',
-        'phpunit',
-        '.gitignore',
-        '.travis.yml',
-        'CHANGELOG.md',
-        'composer.json',
-        'phpunit.xml.dist',
-        'Gemfile',
-        'README.md',
-        'UPGRADE.md',
-        'VERSION',
-        'CHANGES',
-        '.gitattributes',
-        '.DS_Store',
-    );
-
     protected function configure()
     {
         $this
@@ -60,79 +27,111 @@ class BuildVendorCommand extends ContainerAwareCommand
         $rootDir = $biz['kernel.root_dir'].'/../';
         $originDir =  $rootDir. 'vendor/';
         $buildVendorDir = $rootDir.'build/edusoho/vendor/';
-
-        if ($fileSystem->exists($buildVendorDir)) {
-            $fileSystem->remove($buildVendorDir);
-        }else{
-            $fileSystem->mkdir($buildVendorDir);
-        }
+        $fileSystem->mkdir($buildVendorDir);
 
         $output->writeln('remove develop bundle using command: composer install --no-dev');
-        chdir($rootDir);
-        $fileSystem->remove($rootDir.'vendor/codeages/plugin-bundle/Command/PluginCreateCommand.php');
-        $fileSystem->remove(rootDir. 'src/AppBundle/Command/OldPluginCreateCommand.php');
-        exec('composer install --no-dev');
+        $this->ignoreDevelopVendor($rootDir, $fileSystem);
 
+        $this->copyVendor($output, $finder, $originDir, $fileSystem, $buildVendorDir);
 
-        $finder->depth('== 1')->in($originDir);
-
-        foreach ($finder as $folder){
-            $fileName = $folder->getFilename();
-            if( $folder->isFile()){
-                $paths = explode('vendor',  $folder->getRealPath());
-                $fileSystem->copy($folder->getRealPath(), $buildVendorDir.$paths[1]);
-                $output->writeln('build vendor/'. $buildVendorDir.$paths[1]);
-            }else if( !in_array($folder->getRelativePath(), $this->ignoreDeveloperFolders())){
-                $path = $folder->getRelativePath(). '/'. $fileName;
-                if(in_array($path, $this->ignoreDeveloperFolders())){
-                    $output->writeln('ignore vendor/'. $path);
-                }else{
-                    $fileSystem->mirror($folder->getRealPath(), $buildVendorDir.$path);
-                    $output->writeln('build vendor/'. $path);
-                }
-            }
-        }
-        $fileSystem->copy($originDir.'autoload.php', $buildVendorDir.'autoload.php');
         $output->writeln('recovery bundle using command: composer install');
-        exec('composer install');
-        exec('git checkout -- vendor');
+        $this->recoveryDevelopVendor($rootDir);
 
+        $finder->in($buildVendorDir)->depth('<= 3')->ignoreUnreadableDirs(true);
+
+        $this->cleanDevlopVendorFiles($output, $finder, $fileSystem);
+        exec('git checkout -- vendor');
     }
 
-    private function ignoreDeveloperFolders(){
-        return array(
-            'codeception/codeception',
-            'behat/gherkin',
-            'facebook/webdriver',
-            'guzzlehttp/guzzle',
-            'guzzlehttp/promises',
-            'guzzlehttp/psr7',
-            'psr/http-message',
-            'phpunit/phpunit',
-            'phpspec/prophecy',
-            'phpdocumentor/reflection-docblock',
-            'phpdocumentor/type-resolver',
-            'phpdocumentor/reflection-common',
-            'webmozart/assert',
-            'phpunit/php-code-coverage',
-            'phpunit/php-token-stream',
-            'phpunit/php-file-iterator',
-            'phpunit/php-timer',
-            'sebastian/environment',
-            'sebastian/global-state',
-            'sebastian/version',
-            'phpunit/phpunit-mock-objects',
-            'phpunit/php-text-template',
-            'doctrine/instantiator',
-            'sebastian/comparator',
-            'sebastian/exporter',
-            'sebastian/recursion-context',
-            'sebastian/diff',
-            'stecman/symfony-console-completion',
-            'mockery/mockery',
-            'hamcrest/hamcrest-php',
-            'symfony/phpunit-bridge',
-            'sensio/generator-bundle',
+    /**
+     * we ignore require-dev vendor vae composer.json  by composer install --no-dev
+     * and remove file that dependance with SensioGeneratorBundle
+     * @param $rootDir
+     * @param $fileSystem
+     */
+    protected function ignoreDevelopVendor($rootDir, $fileSystem)
+    {
+        chdir($rootDir);
+        $fileSystem->remove($rootDir . 'vendor/codeages/plugin-bundle/Command/PluginCreateCommand.php');
+        $fileSystem->remove($rootDir . 'src/AppBundle/Command/OldPluginCreateCommand.php');
+        exec('composer install --no-dev');
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param $finder
+     * @param $originDir
+     * @param $fileSystem
+     * @param $buildVendorDir
+     */
+    protected function copyVendor(OutputInterface $output, Finder $finder, $originDir, Filesystem $fileSystem, $buildVendorDir)
+    {
+        $finder->depth('== 1')->in($originDir);
+        foreach ($finder as $folder) {
+            $fileName = $folder->getFilename();
+            if ($folder->isFile()) {
+                $paths = explode('vendor', $folder->getRealPath());
+                $fileSystem->copy($folder->getRealPath(), $buildVendorDir . $paths[1]);
+                $output->writeln('build vendor' . $paths[1]);
+            } else {
+                $targetDir = $buildVendorDir. $folder->getRelativePath() . '/' . $fileName;
+                $fileSystem->mirror($folder->getRealPath(), $targetDir);
+                $output->writeln('build vendor/' . $folder->getRelativePath() . '/' . $fileName);
+            }
+        }
+        $fileSystem->copy($originDir . 'autoload.php', $buildVendorDir . 'autoload.php');
+    }
+
+    protected function recoveryDevelopVendor($rootDir)
+    {
+        chdir($rootDir);
+        exec('composer install');
+        exec('git checkout -- vendor');
+        exec('git checkout --  src/AppBundle/Command/OldPluginCreateCommand.php');
+    }
+
+    protected function ignoreVendorFiles(){
+        return  array(
+            'appveyor.yml',
+            'CONTRIBUTING.md',
+            'CONTRIBUTORS.md',
+            'phpunit',
+            '.gitignore',
+            '.travis.yml',
+            'CHANGELOG.md',
+            'composer.json',
+            'phpunit.xml.dist',
+            'Gemfile',
+            'README.md',
+            'UPGRADE.md',
+            'VERSION',
+            'CHANGES',
+            '.gitattributes',
+            '.DS_Store',
+            'CHANGELOG',
+            'changelog.txt',
+            'README',
+            'README_zh_CN.md'
         );
+    }
+
+    /**
+     * remvove test file and document
+     * @param OutputInterface $output
+     * @param $finder
+     * @param $fileSystem
+     */
+    protected function cleanDevlopVendorFiles(OutputInterface $output, Finder $finder, Filesystem $fileSystem)
+    {
+        foreach ($finder as $folder) {
+            if (in_array($folder->getFilename(), array('tests', 'Tests', 'test'))) {
+                $output->writeln('remove  Test folder : ' . $folder->getRelativePath() . '/' . $folder->getFilename());
+                $fileSystem->remove($folder->getRealPath());
+            }
+            if ($folder->isFile() && in_array($folder->getFilename(), $this->ignoreVendorFiles())) {
+                $output->writeln('remove  File : ' . $folder->getRelativePath() . '/' . $folder->getFilename());
+                $fileSystem->remove($folder->getRealPath());
+            }
+        }
     }
 }
