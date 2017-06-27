9536bd3936c7bccd0f937e737f1fcb606033dfa9
ruanwenqin <ruanwenqin@howzhi.com>
2017-06-27 11:50:34 +0800

    增加单元测试

---
+++ tests/api/Me/MeLiveCourseSetCest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use ApiTester;
+
+class MeLiveCourseSetCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    public function _after(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function search(ApiTester $I)
+    {
+        $I->wantTo('Search app channel via API');
+        $I->amHttpAuthenticated('ruanwenqin', 'ruan1314');
+        $I->haveHttpHeader('Accept', 'application/vnd.edusoho.v2+json');
+        $I->sendGET('/api/me/live_course_sets');
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+        $I->seeResponseIsJson();
+        $I->seeResponseJsonMatchesJsonPath('$[0].id');
+    }
+}
