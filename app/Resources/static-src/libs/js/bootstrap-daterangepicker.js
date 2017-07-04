891833b7695f0d1136f3c7465ca1d0f3e7459885
bellorchid <zhongyunchang@yeah.net>
2017-07-04 17:30:16 +0800

    fix

---
+++ app/Resources/static-src/libs/js/bootstrap-daterangepicker.js
@@ -0,0 +1,40 @@
+import 'bootstrap-daterangepicker';
+import '!style-loader?insertAt=top!css-loader!nodeModulesDir/bootstrap-daterangepicker/daterangepicker.css';
+
+let locale = {
+    'format': 'yyyy-mm-dd',
+        'separator': '-',
+        'applyLabel': '确定',
+        'cancelLabel': '取消',
+        'fromLabel': '起始时间',
+        'toLabel': '结束时间',
+        'customRangeLabel': '自定义',
+        'weekLabel': 'W',
+        "daysOfWeek": [
+        "日",
+        "一",
+        "二",
+        "三",
+        "四",
+        "五",
+        "六"
+    ],
+        "monthNames": [
+        "一月",
+        "二月",
+        "三月",
+        "四月",
+        "五月",
+        "六月",
+        "七月",
+        "八月",
+        "九月",
+        "十月",
+        "十一月",
+        "十二月"
+    ],
+        'firstDay': 1
+
+
+};
+// $.fn.daterangepicker['locale'] = locale;
