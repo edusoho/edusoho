e65ff077261cb88a001f6340fe931b4012a9eaab
tianshengjiie <tianshengjie@howzhi.com>
2017-07-06 15:44:31 +0800

    js改造

--- app/Resources/static-src/app/js/course-manage/overview/index.js
+++ app/Resources/static-src/app/js/course-manage/overview/index.js
@@ -1,115 +1,49 @@
-let taskChart = echarts.init(document.getElementById('task-data-chart'));
-let option = {
-    tooltip : {
-        trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-        }
-    },
-    legend: {
-        data: ['已完成', '学习中','未开始']
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-    },
-    xAxis:  {
-        type: 'value',
-        show: false,
-    },
-    yAxis: {
-        data: ['任务1','任务2','任务3','任务4','任务5','任务6','任务7'],
-        axisLine: {
-            show: false
-        },
-        axisTick:{
-            show: false
-        }
-    },
-    series: [
-        {
-            name: '已完成',
-            type: 'bar',
-            stack: '总量',
-            label: {
-                normal: {
-                    show: true,
-                    position: 'insideRight'
-                }
-            },
-            data: [2, 4, 5, 5, 1, 6, 1],
-            itemStyle: {
-                normal: {
-                    color: '#92D178'
-                }
-            },
-        },
-        {
-            name: '学习中',
-            type: 'bar',
-            stack: '总量',
-            label: {
-                normal: {
-                    show: true,
-                    position: 'insideRight'
-                }
-            },
-            data:[0, 4, 5, 1, 1, 6, 3],
-            itemStyle: {
-                normal: {
-                    color: '#FECF7D'
-                }
-            },
-        },
-        {
-            name: '未开始',
-            type: 'bar',
-            stack: '总量',
-            label: {
-                normal: {
-                    show: true,
-                    position: 'insideRight'
-                }
-            },
-            data: [2, 4, 2, 6, 10, 0, 7],
-            itemStyle: {
-                normal: {
-                    color: '#D3D3D3'
-                }
-            },
-        },
-    ]
-};
+import TaskDetail from './task-detail';
 
+class CourseDashboard{
+    constructor() {
+        this.init();
+        this.timeSelectEvent();
+        this.tabToggle();
+        this.charts();
+    }
 
-taskChart.setOption(option);
-let url = $('.js-date-change-url').data('url');
-let $timeSlectBtn = $('.is-date-change');
-// let $tabChangBtn = $('.js-tab-change');
-let ajax = false;
+    init(){
+        this.$timeSlectBtn = $('.is-date-change');
+    }
 
-$timeSlectBtn.on('click', function() {
-  let type = $(this).data('type');
-  let time = $(this).data('time');
-  $.post(url, {
-    type: type,
-    time: time
-  }).done(() => {
-    console.log('success');
-  }).fail(() => {
-    console.log('error');
-  })
-});
+    timeSelectEvent(){
+        this.$timeSlectBtn.on('click', function() {
+            let type = $(this).data('type');
+            let time = $(this).data('time');
+            $.post(url, {
+                type: type,
+                time: time
+            }).done(() => {
+                console.log('success');
+            }).fail(() => {
+                console.log('error');
+            })
+        });
+    }
 
-// $tabChangBtn.on('click', function() {
-//   $(this).parents('.course-statictics-content').find('.js-chart-change').toggle();
-//   if (ajax == false) {
-//     $.post(url).done(() => {
-//       ajax = true;
-//     }).fail(() => {
-//       console.log('fail');
-//     })
-//   }
-// })
+    tabToggle(){
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            e.target // 激活的标签页
+            let $target = $(e.target);
+            let $content = $($target.attr('href'));
+            $content.trigger('init');
+        })
+    }
+
+    charts(){
+        let self = this;
+        $('#task-data-detail').on('init', function(){
+            if (self.taskDetail) return;
+            self.taskDetail = new TaskDetail();
+            self.taskDetail.show();
+        })
+    }
+}
+
+let courseDashboard = new CourseDashboard()
---
+++ app/Resources/static-src/app/js/course-manage/overview/task-detail.js
@@ -0,0 +1,114 @@
+export default class taskDetail{
+    constructor($dom) {
+        this.init();
+        this.$dom = $dom;
+    }
+
+    init(){
+        console.log('init');
+    }
+
+    show(){
+        console.log('show');
+        let taskChart = echarts.init(document.getElementById('task-data-chart'));
+        let option = {
+            tooltip : {
+                trigger: 'axis',
+                axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                    type : 'none',        // 默认为直线，可选为：'line' | 'shadow'
+                    shadowStyle: {
+                        shadowColor: 'rgba(0, 0, 0, 0)',
+                        shadowBlur: 0
+                    },
+                }
+            },
+            legend: {
+                // 图表标题
+                data: ['已完成', '学习中','未开始'],
+                left: 100,
+            },
+            grid: {
+                left: '0',
+                right: '50px',
+                bottom: '20px',
+                containLabel: true,
+            },
+            xAxis:  {
+                type: 'value',
+                show: false,
+                boundaryGap: false,
+            },
+            yAxis: {
+                data: ['任务7','任务6','任务5','任务4','任务3','任务2','任务1'],
+                axisLine: {
+                    show: false
+                },
+                axisTick:{
+                    show: false
+                },
+                boundaryGap: false,
+            },
+            series: [
+                {
+                    name: '已完成',
+                    type: 'bar',
+                    stack: '总量',
+                    barWidth: 16,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'insideRight'
+                        }
+                    },
+                    data: [2, 4, 5, 5, 1, 6, 1],
+                    itemStyle : {
+                        normal: {
+                            color: '#92D178',
+                            label : {
+                                show: true,
+                                position: 'insideRight'
+                            }
+                        }
+                    },
+                },
+                {
+                    name: '学习中',
+                    type: 'bar',
+                    stack: '总量',
+                    barWidth: 16,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'insideRight'
+                        }
+                    },
+                    data:[0, 4, 5, 1, 1, 6, 3],
+                    itemStyle: {
+                        normal: {
+                            color: '#FECF7D'
+                        }
+                    },
+                },
+                {
+                    name: '未开始',
+                    type: 'bar',
+                    stack: '总量',
+                    barWidth: 16,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'insideRight'
+                        }
+                    },
+                    data: [2, 4, 2, 6, 10, 0, 7],
+                    itemStyle: {
+                        normal: {
+                            color: '#D3D3D3'
+                        }
+                    },
+                },
+            ]
+        };
+        taskChart.setOption(option);
+    }
+}
