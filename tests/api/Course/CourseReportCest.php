f9f8b6201b826d8f9f371296e3b84677bb314c81
ruanwenqin <ruanwenqin@howzhi.com>
2017-07-07 10:32:06 +0800

    增加api测试

--- tests/api/Course/CourseReportCest.php
+++ tests/api/Course/CourseReportCest.php
@@ -16,10 +16,10 @@ class CourseReportCest
 
     public function getCourse(ApiTester $I)
     {
-        $I->wantTo('Get a course info');
+        $I->wantTo('Get a course completion rate trend');
         $I->haveHttpHeader('Accept', 'application/vnd.edusoho.v2+json');
-        $I->amHttpAuthenticated('st001', 'edusoho504');
-        $I->sendGET('/api/courses/3');
+        $I->amHttpAuthenticated('测试管理员', 'testedusoho!@#');
+        $I->sendGET('/api/courses/3/reports/completion_rate_trend');
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
         $I->seeResponseIsJson();
     }
