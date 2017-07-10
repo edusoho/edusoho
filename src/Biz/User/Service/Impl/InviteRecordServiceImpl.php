84e3ec72d553c467ca1a867876c92a73096f6696
Allen Lau <liuyangyang@howzhi.com>
2017-07-10 08:49:18 +0800

    fix code style  fixed

--- src/Biz/User/Service/Impl/InviteRecordServiceImpl.php
+++ src/Biz/User/Service/Impl/InviteRecordServiceImpl.php
@@ -84,6 +84,7 @@ class InviteRecordServiceImpl extends BaseService implements InviteRecordService
         if (!empty($conditions['endDate'])) {
             $conditions['endDateTime'] = strtotime($conditions['endDate']);
         }
+
         return $conditions;
     }
 
