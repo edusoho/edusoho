97c0115e378927325628930f945a4d562ed2c4ed
Allen Lau <liuyangyang@howzhi.com>
2017-07-11 12:11:32 +0800

    fix

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -24,7 +24,7 @@ class AppExtension extends \Twig_Extension
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
-        $this->biz = $container->get('Biz');
+        $this->biz = $container->get('biz');
     }
 
     public function getFilters()
