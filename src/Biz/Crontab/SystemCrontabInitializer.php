f8425a31bdb3ac920a062c7a9c4e52f016f6e4b5
ruanwenqin <ruanwenqin@howzhi.com>
2017-07-11 14:40:00 +0800

    初始化crontab

--- src/Biz/Crontab/SystemCrontabInitializer.php
+++ src/Biz/Crontab/SystemCrontabInitializer.php
@@ -2,14 +2,52 @@
 
 namespace Biz\Crontab;
 
+use AppBundle\System;
 use Codeages\Biz\Framework\Scheduler\Service\SchedulerService;
+use TiBeN\CrontabManager\CrontabAdapter;
+use TiBeN\CrontabManager\CrontabJob;
+use TiBeN\CrontabManager\CrontabRepository;
 use Topxia\Service\Common\ServiceKernel;
 
 class SystemCrontabInitializer
 {
     const SOURCE_SYSTEM = 'MAIN';
 
+    const MAX_CRONTAB_NUM = 10;
+
     public static function init()
+    {
+        self::registerDefaultCrontab();
+        self::registerDefaultJobs();
+    }
+
+    private static function registerDefaultCrontab()
+    {
+        if (System::getOS() === System::OS_LINUX || System::getOS() === System::OS_OSX) {
+            $crontabRepository = new CrontabRepository(new CrontabAdapter());
+
+            $crontabJobs = $crontabRepository->findJobByRegex('/app\/console util\:scheduler/');
+            if (count($crontabJobs) < self::MAX_CRONTAB_NUM) {
+
+                $rootDir = ServiceKernel::instance()->getParameter('kernel.root_dir');
+                $commandPath = $rootDir.'/console util:scheduler';
+                $logPath = $rootDir.'/logs/crontab.log';
+                $command = "*/1 * * * * {$commandPath} >> {$logPath} 2>&1";
+
+                for ($i = 0; $i < self::MAX_CRONTAB_NUM - count($crontabJobs);$i++) {
+                    $crontabJob = CrontabJob::createFromCrontabLine($command);
+                    $crontabJob->comments = 'Job '. $i;
+                    $crontabRepository->addJob(
+                        $crontabJob
+                    );
+                }
+
+                $crontabRepository->persist();
+            }
+        }
+    }
+
+    private static function registerDefaultJobs()
     {
         $count = self::getSchedulerService()->countJobs(array('name' => 'CancelOrderJob', 'source' => self::SOURCE_SYSTEM));
         if ($count == 0) {
