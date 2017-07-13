9a25b3b438038c1032939bb7cc4272cf39f6836a
bellorchid <zhongyunchang@yeah.net>
2017-07-13 18:01:59 +0800

    fix

---
+++ src/Biz/Course/Copy/Chain/CourseCopy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Biz\Course\Copy\Chain;
+
+use Biz\Course\Copy\AbstractEntityCopy;
+
+class CourseCopy extends AbstractEntityCopy
+{
+    /**
+     * 复制链说明: 复制courseSet，复制多个教学计划
+     */
+
+    public function copyEntity($source, $config = array())
+    {
+        // TODO: Implement copyEntity() method.
+    }
+
+    protected function getFields()
+    {
+        // TODO: Implement getFields() method.
+    }
+}
