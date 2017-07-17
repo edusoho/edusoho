ad63a756a473363cb39d2551b10e94b4d2534038
bellorchid <zhongyunchang@yeah.net>
2017-07-17 09:39:55 +0800

    fix

---
+++ src/Biz/Course/Component/Clones/Chain/CourseClone.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Biz\Course\Component\Clones\Chain;
+
+use Biz\Course\Component\Clones\AbstractClone;
+use Codeages\Biz\Framework\Context\Biz;
+
+class CourseClone extends AbstractClone
+{
+    public function __construct(Biz $biz, array $processNodes = array(), $auto = false)
+    {
+        $processNodes = array(
+            'course-member' => array(
+                'class' => 'Biz\Course\Component\Clones\Chain\CourseMemberClone',
+                'priority' => 100
+            ),
+            'course-question' => array(
+                
+            )
+        );
+        parent::__construct($biz, $processNodes, $auto);
+    }
+
+    protected function cloneEntity($source, $options)
+    {
+        return $this->doCourseCloneProcess($options,$options);
+    }
+
+    private function doCourseCloneProcess($options,$options)
+    {
+
+
+    }
+
+    protected function getFields()
+    {
+        // TODO: Implement getFields() method.
+    }
+}
---
+++ src/Biz/Course/Component/Clones/Chain/CourseMemberClone.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Biz\Course\Component\Clones\Chain;
+
+use Biz\Course\Component\Clones\AbstractClone;
+use Codeages\Biz\Framework\Context\Biz;
+
+class CourseMemberClone extends AbstractClone
+{
+    protected function cloneEntity($source, $options)
+    {
+    }
+
+    protected function getFields()
+    {
+        // TODO: Implement getFields() method.
+    }
+}
