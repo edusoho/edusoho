c3a0c76d7ffc5ff1836cb7aeb02352769d9b3cf3
tianshengjiie <tianshengjie@howzhi.com>
2017-07-17 10:01:32 +0800

    fix

--- src/Biz/Wrap/CourseSetWrap.php
+++ src/Biz/Wrap/CourseSetWrap.php
@@ -18,23 +18,30 @@ class CourseSetWrap extends BaseWrap
         $coinSetting = $this->getSettingService()->get('coin',array(
             'coin_enabled' => 0,
             'cash_model' => 'none',
+            'cash_rate' => 1,
         ));
 
         if (0 == $courseSet['maxCoursePrice']) {
-            $courseSet['showPrice'] = $this->container->get('translator')->trans('course.block_grid.price_free');
+            $courseSet['priceText'] = $this->container->get('translator')->trans('course.block_grid.price_free');
             return $courseSet;
         }
 
         $unit = $this->container->get('translator')->trans('admin.account_center.RMB');
         $price = round($courseSet['minCoursePrice'], 2);
+        $courseSet['priceCurrency'] = 'RMB';
+
         if ($coinSetting['coin_enabled'] && $coinSetting['cash_model'] == 'currency') {
+            $courseSet['priceCurrency'] = 'coin';
+
             $unit = isset($coinSetting['coin_name']) ? $coinSetting['coin_name'] : $this->container->get('translator')->trans('finance.coin');
+            $price = $courseSet['minCoursePriceShow'] =  round($courseSet['minCoursePrice'] * $coinSetting['cash_rate'], 2);
+            $courseSet['maxCoursePriceShow'] = round($courseSet['maxCoursePrice'] * $coinSetting['cash_rate'], 2);
         }
 
-        if ($courseSet['minCoursePrice']== $courseSet['maxCoursePrice']) {
-            $courseSet['showPrice'] = $price.$unit;
+        if ($courseSet['minCoursePrice'] == $courseSet['maxCoursePrice']) {
+            $courseSet['priceText'] = $price.$unit;
         } else {
-            $courseSet['showPrice'] =  $this->container->get('translator')->trans('course.minimum_price.unit', array('%price%'=>$price, '%unit%'=> $unit));
+            $courseSet['priceText'] =  $this->container->get('translator')->trans('course.minimum_price.unit', array('%price%'=>$price, '%unit%'=> $unit));
         }
 
         return $courseSet;
