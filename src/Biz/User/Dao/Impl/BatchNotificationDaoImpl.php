06ef96bcc332db90e0759ba75189d8f37fe0caac
tianshengjiie <tianshengjie@howzhi.com>
2017-07-17 16:33:01 +0800

    fix

--- src/Biz/User/Dao/Impl/BatchNotificationDaoImpl.php
+++ src/Biz/User/Dao/Impl/BatchNotificationDaoImpl.php
@@ -49,7 +49,7 @@ class BatchNotificationDaoImpl extends GeneralDaoImpl implements BatchNotificati
                 'published = :published',
                 'createdTime = :createdTime',
                 'sendedTime <= :sendedTime_LE',
-                'sendedTime >= :sendedTime_GE'
+                'sendedTime >= :sendedTime_GE',
             ),
             'orderbys' => array('createdTime'),
         );
