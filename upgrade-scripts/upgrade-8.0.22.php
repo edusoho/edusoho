e37318564baf13a83257cd25a29622890a40ca52
ruanwenqin <ruanwenqin@howzhi.com>
2017-07-20 20:02:39 +0800

    更新升级脚本

--- upgrade-scripts/upgrade-8.0.22.php
+++ upgrade-scripts/upgrade-8.0.22.php
@@ -114,6 +114,120 @@ class EduSohoUpgrade extends AbstractUpdater
     {
         $this->logger(self::VERSION, 'info', '开始：初始化 crontab ');
 
+        if (!$this->isTableExist('job_pool')) {
+            $this->getConnection()->exec("
+              CREATE TABLE IF NOT EXISTS `job_pool` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+              `name` varchar(128) NOT NULL DEFAULT 'default' COMMENT '组名',
+              `max_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最大数',
+              `num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '已使用的数量',
+              `timeout` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '执行超时时间',
+              `updated_time` int(10) unsigned NOT NULL COMMENT '更新时间',
+              `created_time` int(10) unsigned NOT NULL COMMENT '创建时间',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            ");
+        }
+
+        if (!$this->isTableExist('job')) {
+            $this->getConnection()->exec("
+              CREATE TABLE IF NOT EXISTS `job` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
+              `name` varchar(128) NOT NULL COMMENT '任务名称',
+              `pool` varchar(64) NOT NULL DEFAULT 'default' COMMENT '所属组',
+              `source` varchar(64) NOT NULL DEFAULT 'MAIN' COMMENT '来源',
+              `expression` varchar(128) NOT NULL DEFAULT '' COMMENT '任务触发的表达式',
+              `class` varchar(128) NOT NULL COMMENT '任务的Class名称',
+              `args` text COMMENT '任务参数',
+              `priority` int(10) unsigned NOT NULL DEFAULT 50 COMMENT '优先级',
+              `pre_fire_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '任务下次执行的时间',
+              `next_fire_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '任务下次执行的时间',
+              `misfire_threshold` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '触发过期的阈值(秒)',
+              `misfire_policy` varchar(32) NOT NULL COMMENT '触发过期策略: missed, executing',
+              `enabled` tinyint(1) DEFAULT 1 COMMENT '是否启用',
+              `deleted` tinyint(1) DEFAULT 0 COMMENT '是否启用',
+              `creator_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '任务创建人',
+              `deleted_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '删除时间',
+              `updated_time` int(10) unsigned NOT NULL COMMENT '修改时间',
+              `created_time` int(10) unsigned NOT NULL COMMENT '任务创建时间',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            ");
+        }
+
+        if (!$this->isTableExist('job_fired')) {
+            $this->getConnection()->exec("
+              CREATE TABLE IF NOT EXISTS `job_fired` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
+              `job_id` int(10) NOT NULL COMMENT 'jobId',
+              `fired_time` int(10) unsigned NOT NULL COMMENT '触发时间',
+              `priority` int(10) unsigned NOT NULL DEFAULT 50 COMMENT '优先级',
+              `status` varchar(32) NOT NULL DEFAULT 'acquired' COMMENT '状态：acquired, executing, success, missed, ignore, failure',
+              `failure_msg` text,
+              `updated_time` int(10) unsigned NOT NULL COMMENT '修改时间',
+              `created_time` int(10) unsigned NOT NULL COMMENT '任务创建时间',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            ");
+        }
+
+        if (!$this->isTableExist('job_log')) {
+            $this->getConnection()->exec("
+              CREATE TABLE IF NOT EXISTS `job_log` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
+              `job_id` int(10) unsigned NOT NULL COMMENT '任务编号',
+              `job_fired_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '激活的任务编号',
+              `hostname` varchar(128) NOT NULL DEFAULT '' COMMENT '执行的主机',
+              `name` varchar(128) NOT NULL COMMENT '任务名称',
+              `pool` varchar(64) NOT NULL DEFAULT 'default' COMMENT '所属组',
+              `source` varchar(64) NOT NULL COMMENT '来源',
+              `class` varchar(128) NOT NULL COMMENT '任务的Class名称',
+              `args` text COMMENT '任务参数',
+              `priority` int(10) unsigned NOT NULL DEFAULT 50 COMMENT '优先级',
+              `status` varchar(32) NOT NULL DEFAULT 'waiting' COMMENT '任务执行状态',
+              `created_time` int(10) unsigned NOT NULL COMMENT '任务创建时间',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            ");
+        }
+
+        if (!$this->isTableExist('target_log')) {
+            $this->getConnection()->exec("
+              CREATE TABLE `target_log` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+              `target_type` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '日志对象类型',
+              `target_id` varchar(48) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '日志对象ID',
+              `action` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '日志行为',
+              `level` smallint(6) NOT NULL DEFAULT '0' COMMENT '日志等级',
+              `message` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '日志信息',
+              `context` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '日志上下文',
+              `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '操作人ID',
+              `ip` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '操作人IP',
+              `created_time` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+            ");
+        }
+
+        if (!$this->isTableExist('biz_token')) {
+            $this->getConnection()->exec("
+              CREATE TABLE `biz_token` (
+              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+              `place` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '使用场景',
+              `_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT 'KEY',
+              `data` text COLLATE utf8_unicode_ci NOT NULL COMMENT '数据',
+              `expired_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '过期时间',
+              `times` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最多可被校验的次数',
+              `remaining_times` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '剩余可被校验的次数',
+              `created_time` int(10) unsigned NOT NULL COMMENT '创建时间',
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `_key` (`_key`),
+              KEY `expired_time` (`expired_time`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+            ");
+        }
+
+
         \Biz\Crontab\SystemCrontabInitializer::init();
 
         $this->logger(self::VERSION, 'info', '结束：初始化 crontab - 成功');
