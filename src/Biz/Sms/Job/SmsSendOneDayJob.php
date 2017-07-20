3ae1e8b34e2da9e75c7fbf37058a66236bd197e4
ruanwenqin <ruanwenqin@howzhi.com>
2017-07-20 17:21:00 +0800

    直播短信

--- src/Biz/Sms/Job/SmsSendOneDayJob.php
+++ src/Biz/Sms/Job/SmsSendOneDayJob.php
@@ -2,8 +2,11 @@
 
 namespace Biz\Sms\Job;
 
+use AppBundle\Common\ExceptionPrintingToolkit;
+use Biz\Common\Logger;
+use Biz\Sms\Service\SmsService;
+use Biz\System\Service\LogService;
 use Codeages\Biz\Framework\Scheduler\AbstractJob;
-use Topxia\Service\Common\ServiceKernel;
 use Biz\CloudPlatform\CloudAPIFactory;
 use Biz\Sms\SmsProcessor\SmsProcessorFactory;
 
@@ -15,28 +18,36 @@ class SmsSendOneDayJob extends AbstractJob
         $dayIsOpen = $this->getSmsService()->isOpen($smsType);
 
         if ($dayIsOpen) {
-            $targetType = $this->args['targetType'];
-            $targetId = $this->args['targetId'];
-            $processor = SmsProcessorFactory::create($targetType);
-            $return = $processor->getUrls($targetId, $smsType);
-            $callbackUrls = $return['urls'];
-            $count = ceil($return['count'] / 1000);
             try {
+                $targetType = $this->args['targetType'];
+                $targetId = $this->args['targetId'];
+                $processor = SmsProcessorFactory::create($targetType);
+                $return = $processor->getUrls($targetId, $smsType);
+                $callbackUrls = $return['urls'];
+                $count = ceil($return['count'] / 1000);
+
                 $api = CloudAPIFactory::create('leaf');
                 $result = $api->post('/sms/sendBatch', array('total' => $count, 'callbackUrls' => $callbackUrls));
-            } catch (\RuntimeException $e) {
-                throw new \RuntimeException('发送失败！');
+            } catch (\Exception $e) {
+                $this->getLogService()->error(Logger::SMS, 'sms_live_play_one_day', "发送短信通知失败:targetType:{$targetType}, targetId:{$targetId}", ExceptionPrintingToolkit::printTraceAsArray($e));
             }
         }
     }
 
-    protected function getSmsService()
+    /**
+     * @return LogService
+     */
+    private function getLogService()
     {
-        return $this->getKernel()->createService('Sms:SmsService');
+        return $this->biz->service('System:LogService');
     }
 
-    protected function getKernel()
+    /**
+     * @return SmsService
+     */
+    protected function getSmsService()
     {
-        return ServiceKernel::instance();
+        return $this->biz->service('Sms:SmsService');
     }
+
 }
--- src/Biz/Sms/Job/SmsSendOneHourJob.php
+++ src/Biz/Sms/Job/SmsSendOneHourJob.php
@@ -2,6 +2,8 @@
 
 namespace Biz\Sms\Job;
 
+use AppBundle\Common\ExceptionPrintingToolkit;
+use Biz\Common\Logger;
 use Codeages\Biz\Framework\Scheduler\AbstractJob;
 use Topxia\Service\Common\ServiceKernel;
 use Biz\CloudPlatform\CloudAPIFactory;
@@ -15,19 +17,20 @@ class SmsSendOneHourJob extends AbstractJob
         $dayIsOpen = $this->getSmsService()->isOpen($smsType);
 
         if ($dayIsOpen) {
-            $targetType = $this->args['targetType'];
-            $targetId = $this->args['targetId'];
-            $processor = SmsProcessorFactory::create($targetType);
-            $return = $processor->getUrls($targetId, $smsType);
-            $callbackUrls = $return['urls'];
-            $count = ceil($return['count'] / 1000);
             try {
+                $targetType = $this->args['targetType'];
+                $targetId = $this->args['targetId'];
+                $processor = SmsProcessorFactory::create($targetType);
+                $return = $processor->getUrls($targetId, $smsType);
+                $callbackUrls = $return['urls'];
+                $count = ceil($return['count'] / 1000);
+
                 $api = CloudAPIFactory::create('leaf');
                 $result = $api->post('/sms/sendBatch', array('total' => $count, 'callbackUrls' => $callbackUrls));
                 $this->getLogService()->info('sms', 'sms-sendbatch', 'callbackUrls', $callbackUrls);
                 $this->getLogService()->info('sms', 'sms-sendbatch', 'result', empty($result) ? array() : $result);
-            } catch (\RuntimeException $e) {
-                throw new \RuntimeException('发送失败！');
+            } catch (\Exception $e) {
+                $this->getLogService()->error(Logger::SMS, 'sms_live_play_one_hour', "发送短信通知失败:targetType:{$targetType}, targetId:{$targetId}", ExceptionPrintingToolkit::printTraceAsArray($e));
             }
         }
     }
