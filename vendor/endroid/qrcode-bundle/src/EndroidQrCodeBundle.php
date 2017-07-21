3e4710386270d4398ed1eaac2938d855d4f4cdcc
Allen Lau <liuyangyang@howzhi.com>
2017-07-21 18:41:17 +0800

    revert qrcode-bundle

---
+++ vendor/endroid/qrcode-bundle/src/EndroidQrCodeBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Endroid\Bundle\QrCodeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EndroidQrCodeBundle extends Bundle
+{
+}
