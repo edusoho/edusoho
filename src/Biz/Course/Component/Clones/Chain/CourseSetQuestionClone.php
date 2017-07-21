2481d7be413e78c1ae552b0bf7108deeebf20d9a
bellorchid <zhongyunchang@yeah.net>
2017-07-21 11:26:09 +0800

    fix

--- src/Biz/Course/Component/Clones/Chain/CourseSetQuestionClone.php
+++ src/Biz/Course/Component/Clones/Chain/CourseSetQuestionClone.php
@@ -5,6 +5,7 @@ namespace Biz\Course\Component\Clones\Chain;
 use AppBundle\Common\ArrayToolkit;
 use Biz\Course\Component\Clones\AbstractClone;
 use Biz\Question\Dao\QuestionDao;
+use Codeages\Biz\Framework\Dao\BatchUpdateHelper;
 
 class CourseSetQuestionClone extends AbstractClone
 {
@@ -23,6 +24,8 @@ class CourseSetQuestionClone extends AbstractClone
             'analysis',
             'metas',
             'categoryId',
+            'courseId',//先保存courseId LessonId 后面再更新
+            'lessonId',
             'difficulty',
             'subCount',
         );
@@ -39,7 +42,7 @@ class CourseSetQuestionClone extends AbstractClone
     {
         $conditions = array(
             'parentId' => 0,
-            'courseId' => 0,
+//            'courseId' => 0,
             'courseSetId' => $originalCourseSet['id'],
         );
         $parentQuestions = $this->getQuestionDao()->search($conditions, array(), 0, PHP_INT_MAX);
@@ -50,9 +53,9 @@ class CourseSetQuestionClone extends AbstractClone
         $newQuestions = array();
         foreach ($parentQuestions as $question) {
             $newQuestion = $this->filterFields($question);
-            $newQuestion['courseId'] = 0;
+//            $newQuestion['courseId'] = 0;
             $newQuestion['courseSetId'] = $newCourseSet['id'];
-            $newQuestion['lessonId'] = 0;
+//            $newQuestion['lessonId'] = 0;
             $newQuestion['copyId'] = $question['id']; //暂时存储copyId，当把childrenQuestion 填写之后，再Update将copyId归零
             $newQuestion['createdUserId'] = $this->biz['user']['id'];
             $newQuestion['updatedUserId'] = $this->biz['user']['id'];
@@ -74,7 +77,7 @@ class CourseSetQuestionClone extends AbstractClone
         $conditions = array(
             'parentIdGT' => 0,
             'courseSetId' => $originalCourseSet['id'],
-            'courseId' => 0,
+//            'courseId' => 0,
         );
         $childrenQuestions = $this->getQuestionDao()->search($conditions, array(), 0, PHP_INT_MAX);
         if (empty($childrenQuestions)) {
@@ -83,11 +86,12 @@ class CourseSetQuestionClone extends AbstractClone
         $newChildQuestions = array();
         foreach ($childrenQuestions as $question) {
             $newQuestion = $this->filterFields($question);
-            $newQuestion['courseId'] = 0;
+//            $newQuestion['courseId'] = 0;
             $newQuestion['courseSetId'] = $newCourseSet['id'];
-            $newQuestion['lessonId'] = 0;
+//            $newQuestion['lessonId'] = 0;
             $newQuestion['createdUserId'] = $this->biz['user']['id'];
             $newQuestion['updatedUserId'] = $this->biz['user']['id'];
+            $newQuestion['copyId'] = $question['id'];
             $parentQuestion = $newQuestions[$question['parentId']];
             $newQuestion['parentId'] = $parentQuestion['id'];
 
@@ -95,7 +99,23 @@ class CourseSetQuestionClone extends AbstractClone
         }
 
         $this->getQuestionDao()->batchCreate($newChildQuestions);
-//        $this->getQuestionDao()->batchUpdate($newQuestionIds,array('copyId' => 0));
+//        copyId的刷新不在这里做，在课程复制完之后然后再操作
+//        foreach ($newQuestionIds as $newQuestionId) {
+//            $this->getQuestionBatchUpdateHelper()->add('id',$newQuestionId,array('copyId' => 0));
+//        }
+//        $this->getQuestionBatchUpdateHelper()->flush();
+    }
+
+    /**
+     * @return BatchUpdateHelper
+     */
+    protected function getQuestionBatchUpdateHelper()
+    {
+        $questionDao = $this->getQuestionDao();
+
+        $questionUpdateHelper = new BatchUpdateHelper($questionDao);
+
+        return $questionUpdateHelper;
     }
 
     /**
