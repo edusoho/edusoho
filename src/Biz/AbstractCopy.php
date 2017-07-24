1aab155f1af5e8102b864e442633cf10ee3da808
bellorchid <zhongyunchang@yeah.net>
2017-07-24 20:04:59 +0800

    fix

---
+++ src/Biz/AbstractCopy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Biz;
+
+use Codeages\Biz\Framework\Context\Biz;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+abstract class AbstractCopy
+{
+    private $logger;
+
+    protected $copyChain;
+
+    private $currentNode;
+
+    protected $biz;
+
+    private $preCopyResult;
+
+    private $doCopyResult;
+
+    public function __construct(Biz $biz, $copyChain)
+    {
+        $this->biz = $biz;
+        $this->copyChain = $copyChain;
+    }
+
+    protected function getLogger($name)
+    {
+        if ($this->logger) {
+            return $this->logger;
+        }
+
+        $this->logger = new Logger($name);
+        $this->logger->pushHandler(new StreamHandler($this->biz['log_directory'].'/service.log', Logger::DEBUG));
+
+        return $this->logger;
+    }
+
+    final public function copy($source,$options)
+    {
+        $this->preCopyResult = $this->preCopy($source, $options);
+        $this->doCopyResult = $this->doCopy($source, $options);
+        $this->afterCopy($source,$options);
+
+    }
+
+    abstract public function preCopy($source,$options);
+
+    abstract public function doCopy($source,$options);
+
+    abstract public function afterCopy($source, $options);
+
+    protected function getCurrentNode()
+    {
+        return $this->currentNode;
+    }
+    
+
+}
