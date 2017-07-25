d80555a1cfc16df4ca1cf2759237fecf7971f901
bellorchid <zhongyunchang@yeah.net>
2017-07-25 08:47:45 +0800

    fix code style  fixed

--- src/Biz/AbstractCopy.php
+++ src/Biz/AbstractCopy.php
@@ -38,17 +38,16 @@ abstract class AbstractCopy
         return $this->logger;
     }
 
-    final public function copy($source,$options)
+    final public function copy($source, $options)
     {
         $this->preCopyResult = $this->preCopy($source, $options);
         $this->doCopyResult = $this->doCopy($source, $options);
-        $this->afterCopy($source,$options);
-
+        $this->afterCopy($source, $options);
     }
 
-    abstract public function preCopy($source,$options);
+    abstract public function preCopy($source, $options);
 
-    abstract public function doCopy($source,$options);
+    abstract public function doCopy($source, $options);
 
     abstract public function afterCopy($source, $options);
 
@@ -59,33 +58,30 @@ abstract class AbstractCopy
 
     protected function getChildrenNodes($currentNode, $chains)
     {
-        if(empty($chains)) {
+        if (empty($chains)) {
             return array();
         }
 
-        foreach($chains as $name => $chain) {
-            if ($name == $currentNode)
-            {
-                if(!empty($chain['children'])){
+        foreach ($chains as $name => $chain) {
+            if ($name == $currentNode) {
+                if (!empty($chain['children'])) {
                     return $chain['children'];
                 }
-            }elseif (!empty($chain['children'])) {
+            } elseif (!empty($chain['children'])) {
                 return $this->getChildrenNodes($currentNode, $chain['children']);
             }
         }
+
         return array();
     }
 
     protected function getCopyChain()
     {
         return $this->copyChain;
-
     }
 
     private function setCopyChain($copyChain)
     {
         $this->copyChain = $copyChain;
     }
-    
-
 }
