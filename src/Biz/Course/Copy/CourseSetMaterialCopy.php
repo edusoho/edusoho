b910048fad4d9280e5d829de3dbf092cd8a51c22
bellorchid <zhongyunchang@yeah.net>
2017-07-25 15:45:02 +0800

    fix

---
+++ src/Biz/Course/Copy/CourseSetMaterialCopy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Biz\Course\Copy;
+
+use Biz\AbstractCopy;
+
+class CourseSetMaterialCopy extends AbstractCopy
+{
+    public function preCopy($source, $options)
+    {
+        return ;
+    }
+
+    public function doCopy($source, $options)
+    {
+        // TODO: Implement doCopy() method.
+    }
+
+    protected function getFields()
+    {
+        return array(
+            
+        );
+    }
+}
