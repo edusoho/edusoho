6ae3f4a314c4abab2dc909c011d922a43b601873
bellorchid <zhongyunchang@yeah.net>
2017-07-25 15:35:34 +0800

    fix

--- src/AppBundle/Extension/CourseCopyExtension.php
+++ src/AppBundle/Extension/CourseCopyExtension.php
@@ -97,37 +97,38 @@ class CourseCopyExtension extends Extension implements ServiceProviderInterface
     protected function generateCourseSetCoursesCopy()
     {
         return array(
-//            'course-set' => array(
-//                'class' => 'Biz\Course\Copy\CourseSetCopy',
-//                'priority' => 100,
-//                'children' => array(
-//                    'tag-owner' => array(
-//                        'class' => 'Biz\Taxonomy\Copy\TagOwnerCopy',
-//                        'priority' => 100,
-//                    ),
-//                    'courseset-material' => array(
-//                        'class' => 'Biz\Course\Copy\CourseSetMaterialCopy',
-//                        'priority' => 90,
-//                    ),
-//                    'courseset-courses' => array(
-//                        'class' => 'Biz\Course\Copy\CourseSetCoursesCopy',
-//                        'priority' => 80,
-//                        'children' => array(
-//                            'course-member' => array(
-//                                'class' => 'Biz\Course\Copy\CourseMemberCopy',
-//                                'priority' => 100,
-//                            ),
-//                            'course-task' => array(
-//                                'class' => 'Biz\Task\Copy\CourseTaskCopy',
-//                                'priority' => 90,
-//                            ),
-//                        ),
-//                    ),
-//                ),
             'course-set' => array(
-                'class' => 'Biz\Course\Copy\CourseSet\CourseSetCopy',
-                'isCopy' => 0,
+                'class' => 'Biz\Course\Copy\CourseSetCopy',
+                'priority' => 100,
+                'children' => array(
+                    'tag-owner' => array(
+                        'class' => 'Biz\Taxonomy\Copy\TagOwnerCopy',
+                        'priority' => 100,
+                    ),
+                    'courseset-material' => array(
+                        'class' => 'Biz\Course\Copy\CourseSetMaterialCopy',
+                        'priority' => 90,
+                    ),
+                    'courseset-courses' => array(
+                        'class' => 'Biz\Course\Copy\CourseSetCoursesCopy',
+                        'priority' => 80,
+                        'children' => array(
+                            'course-member' => array(
+                                'class' => 'Biz\Course\Copy\CourseMemberCopy',
+                                'priority' => 100,
+                            ),
+                            'course-task' => array(
+                                'class' => 'Biz\Task\Copy\CourseTaskCopy',
+                                'priority' => 90,
+                            ),
+                        ),
+                    ),
+                ),
             ),
+//            'course-set' => array(
+//                'class' => 'Biz\Course\Copy\CourseSet\CourseSetCopy',
+//                'isCopy' => 0,
+//            ),
         );
     }
 }
