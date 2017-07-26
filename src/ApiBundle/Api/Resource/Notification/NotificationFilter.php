c0f41d3d7a496438035ec224dbfba368008c80d6
wangdanyue <wangdanyue@howzhi.com>
2017-07-26 20:44:53 +0800

    user follow notification add avatar

--- src/ApiBundle/Api/Resource/Notification/NotificationFilter.php
+++ src/ApiBundle/Api/Resource/Notification/NotificationFilter.php
@@ -4,6 +4,7 @@ namespace ApiBundle\Api\Resource\Notification;
 
 use ApiBundle\Api\Resource\Filter;
 use ApiBundle\Api\Util\AssetHelper;
+use Topxia\Service\Common\ServiceKernel;
 
 class NotificationFilter extends Filter
 {
@@ -20,5 +21,16 @@ class NotificationFilter extends Filter
         }
         
         $data['content'] = trim(AssetHelper::renderView("ApiBundle:notification:{$data['type']}.tpl.html.twig", array('notification' => $data)));
+
+        $data['userAvatar'] = null;
+        if ($data['type'] == 'user-follow') {
+            $user = $this->getUserService()->getUser($data['data']['userId']);
+            $data['userAvatar'] = AssetHelper::getFurl($user['smallAvatar']);
+        }
+    }
+
+    protected function getUserService()
+    {
+        return ServiceKernel::instance()->createService('User:UserService');
     }
 }
