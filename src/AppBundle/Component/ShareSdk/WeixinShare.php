e37059d51eba3ef751c94808f24be52737824ccc
bellorchid <zhongyunchang@yeah.net>
2017-08-02 07:42:37 +0800

    fix

--- src/AppBundle/Component/ShareSdk/WeixinShare.php
+++ src/AppBundle/Component/ShareSdk/WeixinShare.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Component\ShareSdk;
 
+use Psr\Log\LoggerInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 class WeixinShare
 {
     protected $config;
+
+    protected $logger = null;
 
     protected $userAgent = 'Topxia OAuth Client 1.0';
 
@@ -18,6 +24,16 @@ class WeixinShare
     public function __construct($config)
     {
         $this->config = $config;
+        $this->setLogger();
+    }
+
+    public function setLogger()
+    {
+        $logger = new Logger('WeixinShareSDK');
+        $logger->pushHandler(new StreamHandler(ServiceKernel::instance()->getParameter('kernel.logs_dir').'/share-login.log', Logger::DEBUG));
+        $this->logger = $logger;
+
+        return $this;
     }
 
     public function getAccessToken()
@@ -28,9 +44,14 @@ class WeixinShare
             'grant_type' => 'client_credential',
         );
         $result = $this->getRequest(self::ACCESS_TOKEN_URL, $params);
+
         $rawToken = array();
         $rawToken = json_decode($result, true);
 
+        if (empty($rawToken)) {
+            $this->logger && $this->logger->error('WEIXIN_ACCESS_TOKEN_ERROR','rawToken can not be null ,null return');
+            return array();
+        }
         return array(
             'expires_in' => $rawToken['expires_in'],
             'access_token' => $rawToken['access_token'],
@@ -41,13 +62,23 @@ class WeixinShare
     {
         $token = $this->getAccessToken();
 
+        if (empty($token)) {
+            return array();
+        }
+
         $params = array(
             'type' => 'jsapi',
             'access_token' => $token['access_token'],
         );
         $result = $this->getRequest(self::JSAPI_TICKET_URL, $params);
+
         $rawToken = array();
         $rawToken = json_decode($result, true);
+
+        if (empty($rawToken)) {
+            $this->logger && $this->logger->error('WEIXIN_JS_API_TICKET_ERROR','result can not be null ,null return');
+            return array();
+        }
 
         return array(
             'ticket' => $rawToken['ticket'],
