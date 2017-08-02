d8ec9f8e1fef6463ec172ccbfde1759441d222bc
nifeng <nifeng@howzhi.com>
2017-08-02 15:17:55 +0800

    fix

--- src/Biz/System/Service/Impl/StatisticsServiceImpl.php
+++ src/Biz/System/Service/Impl/StatisticsServiceImpl.php
@@ -4,16 +4,27 @@ namespace Biz\System\Service\Impl;
 
 use Biz\BaseService;
 use Biz\System\Service\StatisticsService;
+use Topxia\Service\Common\ServiceKernel;
 
 class StatisticsServiceImpl extends BaseService implements StatisticsService
 {
     public function countOnline($retentionTime)
     {
+        $currentTime = time();
+        if ($this->getRedis()) {
+            return $this->getRedis()->zCount("es3_sess:online", $retentionTime, $currentTime);
+        }
+
         return $this->getSessionDao()->countOnline($retentionTime);
     }
 
     public function countLogin($retentionTime)
     {
+        $currentTime = time();
+
+        if ($this->getRedis()) {
+            return $this->getRedis()->zCount("es3_sess:logined", $retentionTime, $currentTime);
+        }
         return $this->getSessionDao()->countLogin($retentionTime);
     }
 
@@ -25,5 +36,10 @@ class StatisticsServiceImpl extends BaseService implements StatisticsService
     protected function getSettingService()
     {
         return $this->biz->service('System:SettingService');
+    }
+
+    protected function getRedis()
+    {
+        return ServiceKernel::instance()->getRedis();
     }
 }
