fea805e5df30b1184da634dfe037a6e15b6b1c01
bellorchid <zhongyunchang@yeah.net>
2017-08-03 17:50:23 +0800

    fix

--- src/Biz/Course/Job/CloneCourseSetJob.php
+++ src/Biz/Course/Job/CloneCourseSetJob.php
@@ -32,8 +32,8 @@ class CloneCourseSetJob extends AbstractJob
         );
 
         try {
-            $message['status'] = 'success';
             $this->getCourseSetService()->cloneCourseSet($courseSetId, $params);
+            $message['status'] = 'success';
             $this->getNotificationService()->notify($userId, 'course-copy', $message);
         } catch (\Exception $e) {
             $message['status'] = 'failure';
