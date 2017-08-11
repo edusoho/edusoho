bc2274e1f29fa9c3fa29bf51756e61c7d832a562
bellorchid <zhongyunchang@yeah.net>
2017-08-11 08:35:45 +0800

    fix code style  fixed

--- migrations/20170811061121_add_refund_end_time_for_order.php
+++ migrations/20170811061121_add_refund_end_time_for_order.php
@@ -14,7 +14,6 @@ class AddRefundEndTimeForOrder extends Migration
         $db = $biz['db'];
 
         $db->exec("ALTER TABLE `orders` ADD COLUMN `refundEndTime`  int(10) NOT NULL DEFAULT '0' COMMENT '退款截止时间' AFTER `data`");
-
     }
 
     /**
