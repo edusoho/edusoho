b81851dc886dd13add5e120c827f4b7b604024e9
zhongjie <zhongjie@howzhi.com>
2017-08-18 15:59:30 +0800

    添加表格的筛选和排序js

--- app/Resources/static-src/common/codeages-design/js/table.js
+++ app/Resources/static-src/common/codeages-design/js/table.js
@@ -1,24 +1,67 @@
 import notify from 'common/notify';
 
 (function($) {
-  $(document).on('click.cd.table.filter', '[data-toggle="table-filter"]', function() {
-    let $this = $(this);
-    if ($this.closest('li').hasClass('active')) {
-      return;
-    }
-
-    let url = $this.data('url');
-    let $target = $($this.data('target'));
-
-    if (!$target) {
-      return;
-    }
-
+  function tabelAjax($target, url) {
     $.get(url).done(function(html) {
       $target.html(html);
     }).fail(function() {
       notify('danger', Translator.trans('site.response_error'))
     })
+  }
+
+  $(document).on('click.cd.table.filter', '[data-toggle="table-filter"]', function() {
+    let $this = $(this);
+    if ($this.closest('li').hasClass('active')) {
+      return;
+    }
     
+    let $target = $($this.data('target'));
+    let url = $target.data('url');
+
+    let filterStr = $this.data('filter');
+    $target.data('filter', filterStr);
+
+    let sortStr = $target.data('sort');
+
+    if (sortStr) {
+      url = `${url}?${sortStr}`;
+
+      if (filterStr) {
+        url = `${url}&${filterStr}`;
+      }
+    } else {
+      if (filterStr) {
+        url = `${url}?${filterStr}`;
+      }
+    }
+
+    tabelAjax($target, url);
+  });
+
+  $(document).on('click.cd.table.sort', '[data-toggle="table-sort"]', function() {
+    let $this = $(this);
+
+    let $target = $($this.data('target')); 
+    let url = $target.data('url');
+
+    let sortKey = $this.data('sort-key');
+    let sortValue = 'desc';
+
+    let $sortIcon = $this.find('.active');
+    if ($sortIcon.length) {
+      sortValue = $sortIcon.siblings().data('sort-value');
+    }
+    
+    let sortStr = `${sortKey}=${sortValue}`;
+
+    let filterStr = $target.data('filter');
+
+    if (filterStr) {
+      url = `${url}?${sortStr}&${filterStr}`;
+    } else {
+      url = `${url}?${sortStr}`;
+    }
+
+    tabelAjax($target, url);
   });
 })(jQuery);
