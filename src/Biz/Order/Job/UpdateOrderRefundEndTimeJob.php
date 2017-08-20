43ea3e32e2e8d6c35f14346e5d30ce14583378eb
bellorchid <zhongyunchang@yeah.net>
2017-08-20 15:46:59 +0800

    fix code style  fixed

--- src/Biz/Order/Job/UpdateOrderRefundEndTimeJob.php
+++ src/Biz/Order/Job/UpdateOrderRefundEndTimeJob.php
@@ -10,7 +10,7 @@ class UpdateOrderRefundEndTimeJob extends AbstractJob
 {
     public function execute()
     {
-        try{
+        try {
             $refundSetting = $this->getSettingService()->get('refund', array());
             $currentMaxRefundDays = empty($refundSetting['maxRefundDays']) ? 0 : $refundSetting['maxRefundDays'];
             $currentMaxRefundTimes = $currentMaxRefundDays * 86400;
@@ -18,9 +18,9 @@ class UpdateOrderRefundEndTimeJob extends AbstractJob
             $sql = "UPDATE `orders` SET refundEndTime = (paidTime + {$currentMaxRefundTimes}) WHERE status = 'paid' AND refundEndTime = 0";
             $this->biz['db']->exec($sql);
             $this->getLogService()->error('order', 'update_refund_end_time', 'order.update_refund_end_time.success', array());
-        }catch(\Exception $e) {
+        } catch (\Exception $e) {
             $this->getLogService()->error('order', 'update_refund_end_time', 'order.update_refund_end_time.error', array(
-                'errorMsg' => ExceptionPrintingToolkit::printTraceAsArray($e)
+                'errorMsg' => ExceptionPrintingToolkit::printTraceAsArray($e),
             ));
         }
     }
@@ -32,5 +32,4 @@ class UpdateOrderRefundEndTimeJob extends AbstractJob
     {
         return $this->biz->service('System:LogService');
     }
-
 }
