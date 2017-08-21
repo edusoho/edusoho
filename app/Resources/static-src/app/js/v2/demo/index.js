019122b4e24d792ff29dd746ff32ca8cb0cf6a6f
zhongjie <zhongjie@howzhi.com>
2017-08-21 16:51:16 +0800

    优化popover样式细节

--- app/Resources/static-src/app/js/v2/demo/index.js
+++ app/Resources/static-src/app/js/v2/demo/index.js
@@ -1,5 +1,5 @@
 import notify from 'common/notify';
 
 $(document).on('click', '[data-toggle="notify"]', function() {
-  notify('success', '这是警告消息<a href="http://baidu.com" class="notify-action">操作</a>');
+  notify('danger', '这是警告消息<a href="http://demo.edusoho.com" class="notify-action">操作</a>');
 })
