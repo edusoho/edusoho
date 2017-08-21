882c6897ad177a6504658130065c5c6d9db76e0c
bellorchid <zhongyunchang@yeah.net>
2017-08-21 10:39:29 +0800

    修改job

--- src/Biz/Order/Job/UpdateOrderRefundEndTimeJob.php
+++ src/Biz/Order/Job/UpdateOrderRefundEndTimeJob.php
@@ -2,34 +2,30 @@
 
 namespace Biz\Order\Job;
 
-use AppBundle\Common\ExceptionPrintingToolkit;
-use Biz\System\Service\LogService;
+use Biz\System\Service\SettingService;
 use Codeages\Biz\Framework\Scheduler\AbstractJob;
 
 class UpdateOrderRefundEndTimeJob extends AbstractJob
 {
     public function execute()
     {
-        try {
-            $refundSetting = $this->getSettingService()->get('refund', array());
-            $currentMaxRefundDays = empty($refundSetting['maxRefundDays']) ? 0 : $refundSetting['maxRefundDays'];
-            $currentMaxRefundTimes = $currentMaxRefundDays * 86400;
+        $refundSetting = $this->getSettingService()->get('refund', array());
+        $currentMaxRefundDays = empty($refundSetting['maxRefundDays']) ? 0 : $refundSetting['maxRefundDays'];
+        $currentMaxRefundTimes = $currentMaxRefundDays * 86400;
 
-            $sql = "UPDATE `orders` SET refundEndTime = (paidTime + {$currentMaxRefundTimes}) WHERE status = 'paid' AND refundEndTime = 0";
-            $this->biz['db']->exec($sql);
-            $this->getLogService()->error('order', 'update_refund_end_time', 'order.update_refund_end_time.success', array());
-        } catch (\Exception $e) {
-            $this->getLogService()->error('order', 'update_refund_end_time', 'order.update_refund_end_time.error', array(
-                'errorMsg' => ExceptionPrintingToolkit::printTraceAsArray($e),
-            ));
-        }
+        $sql = "UPDATE `orders` SET refundEndTime = (paidTime + {$currentMaxRefundTimes}) WHERE status = 'paid' AND refundEndTime = 0";
+        $this->biz['db']->exec($sql);
     }
 
     /**
-     * @return LogService
+     * @return SettingService
      */
-    private function getLogService()
+    protected function getSettingService()
     {
-        return $this->biz->service('System:LogService');
+        return $this->biz->service('System:SettingService');
     }
+
+
+
+
 }
