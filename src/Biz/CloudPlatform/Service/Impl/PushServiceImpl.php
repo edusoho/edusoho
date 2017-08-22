a597e467f8dfad5c8aed5163e51607bf41e7abee
bellorchid <zhongyunchang@yeah.net>
2017-08-22 18:49:23 +0800

    save service

---
+++ src/Biz/CloudPlatform/Service/Impl/PushServiceImpl.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Biz\CloudPlatform\Service\Impl;
+
+use Biz\BaseService;
+use Biz\CloudPlatform\Service\PushService;
+
+class PushServiceImpl extends BaseService implements PushService
+{
+    public function push()
+    {
+        // TODO: Implement push() method.
+    }
+
+    public function pushArticleCreate($event)
+    {
+
+    }
+}
---
+++ src/Biz/CloudPlatform/Service/SearchService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Biz\CloudPlatform\Service;
+
+interface SearchService
+{
+    public function notifyUpdate();
+
+    public function notifyDelete();
+
+}
