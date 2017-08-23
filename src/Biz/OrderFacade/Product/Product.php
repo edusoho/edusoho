f67c7d75893662e656376c828b96e71e32fa722d
ruanwenqin <ruanwenqin@howzhi.com>
2017-08-23 11:15:46 +0800

    style

--- src/Biz/OrderFacade/Product/Product.php
+++ src/Biz/OrderFacade/Product/Product.php
@@ -8,30 +8,35 @@ abstract class Product extends BizAware
 {
     /**
      * 商品ID
+     *
      * @var int
      */
     public $id;
 
     /**
      * 商品类型
+     *
      * @var string
      */
     public $type;
 
     /**
      * 商品名称
+     *
      * @var string
      */
     public $title;
 
     /**
      * 商品价格
+     *
      * @var float
      */
     public $price;
 
     /**
      * 商品营销属性
+     *
      * @var array
      */
     public $marketing = array();
