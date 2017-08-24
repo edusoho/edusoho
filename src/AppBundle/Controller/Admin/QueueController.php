69331671910de7075d7df5034433532603e8e6c0
bellorchid <zhongyunchang@yeah.net>
2017-08-25 06:46:38 +0800

    add failed jobs show

---
+++ src/AppBundle/Controller/Admin/QueueController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Common\Paginator;
+use Codeages\Biz\Framework\Queue\Service\QueueService;
+use Symfony\Component\HttpFoundation\Request;
+
+class QueueController extends BaseController
+{
+    public function failedJobsAction(Request $request)
+    {
+
+        $count = $this->getQueueService()->countFailedJobs(array());
+
+        $paginator = new Paginator(
+            $request,
+            $count,
+            20
+        );
+
+        $jobs = $this->getQueueService()->searchFailedJobs(
+            array(),
+            array('failed_time' => 'desc'),
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+        return $this->render(
+            'admin/queue/failed-logs.html.twig',
+            array(
+                'jobs' => $jobs,
+                'paginator' => $paginator
+            )
+        );
+
+    }
+
+    /**
+     * @return QueueService
+     */
+    protected function getQueueService()
+    {
+        return $this->createService('Queue:QueueService');
+    }
+}
