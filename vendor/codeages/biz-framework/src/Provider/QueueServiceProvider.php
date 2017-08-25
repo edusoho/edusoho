42e59776dfa40f0ec320d683e0d2ef651911a6f4
bellorchid <zhongyunchang@yeah.net>
2017-08-25 17:54:05 +0800

    修改vendor

--- vendor/codeages/biz-framework/src/Provider/QueueServiceProvider.php
+++ vendor/codeages/biz-framework/src/Provider/QueueServiceProvider.php
@@ -15,7 +15,7 @@ class QueueServiceProvider implements ServiceProviderInterface
     public function register(Container $biz)
     {
         $biz['migration.directories'][] = dirname(dirname(__DIR__)).'/migrations/Queue';
-        $biz['autoload.aliases']['Queue'] = 'Codeages\Biz\Framework\Queue';
+        $biz['autoload.aliases']['Queue'] = 'Codeages\Biz\Framework\queue';
         $biz['console.commands'][] = function () use ($biz) {
             return new WorkerCommand($biz);
         };
