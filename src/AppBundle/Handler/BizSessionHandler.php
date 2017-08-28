4328d7b296eb7e365990400cebff443a0e3ef227
nifeng <nifeng@howzhi.com>
2017-08-28 23:18:35 +0800

    fix code style  fixed

--- src/AppBundle/Handler/BizSessionHandler.php
+++ src/AppBundle/Handler/BizSessionHandler.php
@@ -12,7 +12,7 @@ class BizSessionHandler implements \SessionHandlerInterface
 
     const MAX_LIFE_TIME = 86400;
 
-    function __construct($container, TokenStorage $storage)
+    public function __construct($container, TokenStorage $storage)
     {
         $this->biz = $container->get('biz');
         $this->storage = $storage;
@@ -20,11 +20,14 @@ class BizSessionHandler implements \SessionHandlerInterface
 
     /**
      * Close the session
-     * @link http://php.net/manual/en/sessionhandlerinterface.close.php
+     *
+     * @see http://php.net/manual/en/sessionhandlerinterface.close.php
+     *
      * @return bool <p>
-     * The return value (usually TRUE on success, FALSE on failure).
-     * Note this value is returned internally to PHP for processing.
-     * </p>
+     *              The return value (usually TRUE on success, FALSE on failure).
+     *              Note this value is returned internally to PHP for processing.
+     *              </p>
+     *
      * @since 5.4.0
      */
     public function close()
@@ -35,31 +38,40 @@ class BizSessionHandler implements \SessionHandlerInterface
 
     /**
      * Destroy a session
-     * @link http://php.net/manual/en/sessionhandlerinterface.destroy.php
-     * @param string $session_id The session ID being destroyed.
+     *
+     * @see http://php.net/manual/en/sessionhandlerinterface.destroy.php
+     *
+     * @param string $session_id the session ID being destroyed
+     *
      * @return bool <p>
-     * The return value (usually TRUE on success, FALSE on failure).
-     * Note this value is returned internally to PHP for processing.
-     * </p>
+     *              The return value (usually TRUE on success, FALSE on failure).
+     *              Note this value is returned internally to PHP for processing.
+     *              </p>
+     *
      * @since 5.4.0
      */
     public function destroy($session_id)
     {
         $this->getSessionService()->deleteSessionBySessId($session_id);
+
         return true;
     }
 
     /**
      * Cleanup old sessions
-     * @link http://php.net/manual/en/sessionhandlerinterface.gc.php
+     *
+     * @see http://php.net/manual/en/sessionhandlerinterface.gc.php
+     *
      * @param int $maxlifetime <p>
-     * Sessions that have not updated for
-     * the last maxlifetime seconds will be removed.
-     * </p>
+     *                         Sessions that have not updated for
+     *                         the last maxlifetime seconds will be removed.
+     *                         </p>
+     *
      * @return bool <p>
-     * The return value (usually TRUE on success, FALSE on failure).
-     * Note this value is returned internally to PHP for processing.
-     * </p>
+     *              The return value (usually TRUE on success, FALSE on failure).
+     *              Note this value is returned internally to PHP for processing.
+     *              </p>
+     *
      * @since 5.4.0
      */
     public function gc($maxlifetime)
@@ -69,54 +81,68 @@ class BizSessionHandler implements \SessionHandlerInterface
 
     /**
      * Initialize session
-     * @link http://php.net/manual/en/sessionhandlerinterface.open.php
-     * @param string $save_path The path where to store/retrieve the session.
-     * @param string $name The session name.
+     *
+     * @see http://php.net/manual/en/sessionhandlerinterface.open.php
+     *
+     * @param string $save_path the path where to store/retrieve the session
+     * @param string $name      the session name
+     *
      * @return bool <p>
-     * The return value (usually TRUE on success, FALSE on failure).
-     * Note this value is returned internally to PHP for processing.
-     * </p>
+     *              The return value (usually TRUE on success, FALSE on failure).
+     *              Note this value is returned internally to PHP for processing.
+     *              </p>
+     *
      * @since 5.4.0
      */
     public function open($save_path, $name)
     {
         $maxlifetime = self::MAX_LIFE_TIME;
         $this->gc($maxlifetime);
+
         return true;
     }
 
     /**
      * Read session data
-     * @link http://php.net/manual/en/sessionhandlerinterface.read.php
-     * @param string $session_id The session id to read data for.
+     *
+     * @see http://php.net/manual/en/sessionhandlerinterface.read.php
+     *
+     * @param string $session_id the session id to read data for
+     *
      * @return string <p>
-     * Returns an encoded string of the read data.
-     * If nothing was read, it must return an empty string.
-     * Note this value is returned internally to PHP for processing.
-     * </p>
+     *                Returns an encoded string of the read data.
+     *                If nothing was read, it must return an empty string.
+     *                Note this value is returned internally to PHP for processing.
+     *                </p>
+     *
      * @since 5.4.0
      */
     public function read($session_id)
     {
         $session = $this->getSessionService()->getSessionBySessId($session_id);
+
         return $session['sess_data'];
     }
 
     /**
      * Write session data
-     * @link http://php.net/manual/en/sessionhandlerinterface.write.php
-     * @param string $session_id The session id.
+     *
+     * @see http://php.net/manual/en/sessionhandlerinterface.write.php
+     *
+     * @param string $session_id   the session id
      * @param string $session_data <p>
-     * The encoded session data. This data is the
-     * result of the PHP internally encoding
-     * the $_SESSION superglobal to a serialized
-     * string and passing it as this parameter.
-     * Please note sessions use an alternative serialization method.
-     * </p>
+     *                             The encoded session data. This data is the
+     *                             result of the PHP internally encoding
+     *                             the $_SESSION superglobal to a serialized
+     *                             string and passing it as this parameter.
+     *                             Please note sessions use an alternative serialization method.
+     *                             </p>
+     *
      * @return bool <p>
-     * The return value (usually TRUE on success, FALSE on failure).
-     * Note this value is returned internally to PHP for processing.
-     * </p>
+     *              The return value (usually TRUE on success, FALSE on failure).
+     *              Note this value is returned internally to PHP for processing.
+     *              </p>
+     *
      * @since 5.4.0
      */
     public function write($session_id, $session_data)
@@ -136,13 +162,14 @@ class BizSessionHandler implements \SessionHandlerInterface
             'sess_time' => time(),
             'sess_lifetime' => self::MAX_LIFE_TIME,
             'sess_user_id' => $userId,
-            'source' => 'web'
+            'source' => 'web',
         );
         if (empty($session)) {
             $this->getSessionService()->createSession($unsavedSession);
         } else {
             $this->getSessionService()->updateSessionBySessId($session_id, $unsavedSession);
         }
+
         return true;
     }
 
