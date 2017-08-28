90fe30340920c3bbc96154f7c0252b1efc2a30df
bellorchid <zhongyunchang@yeah.net>
2017-08-28 17:16:48 +0800

    fix error

--- src/Biz/Notification/Event/PushMessageEventSubscriber.php
+++ src/Biz/Notification/Event/PushMessageEventSubscriber.php
@@ -1509,18 +1509,20 @@ class PushMessageEventSubscriber extends EventSubscriber
 
     protected function createJob($lesson)
     {
-        if ($lesson['startTime'] >= (time() + 60 * 60)) {
-            $startJob = array(
-                'name' => 'PushNotificationOneHourJob_lesson_'.$lesson['id'],
-                'expression' => $lesson['startTime'] - 60 * 60,
-                'class' => 'Biz\Notification\Job\PushNotificationOneHourJob',
-                'args' => array(
-                    'targetType' => 'lesson',
-                    'targetId' => $lesson['id'],
-                ),
-            );
-            $this->getSchedulerService()->register($startJob);
-        }
+//        if ($lesson['startTime'] >= (time() + 60 * 60)) {
+//            $startJob = array(
+//                'name' => 'PushNotificationOneHourJob_lesson_'.$lesson['id'],
+//                'expression' => $lesson['startTime'] - 60 * 60,
+//                'class' => 'Biz\Notification\Job\PushNotificationOneHourJob',
+//                'args' => array(
+//                    'targetType' => 'lesson',
+//                    'targetId' => $lesson['id'],
+//                ),
+//            );
+//            $this->getSchedulerService()->register($startJob);
+//        }
+
+        //@todo 屏蔽的原因是，客户端暂时不支持
 
         if ($lesson['type'] == 'live') {
             $startJob = array(
