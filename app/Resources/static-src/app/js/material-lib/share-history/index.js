d30b7d3ecc769b9115a7cdf65780fa8c1ab87bbe
bellorchid <zhongyunchang@yeah.net>
2017-08-28 19:39:50 +0800

    fix

--- app/Resources/static-src/app/js/material-lib/share-history/index.js
+++ app/Resources/static-src/app/js/material-lib/share-history/index.js
@@ -1,7 +1,6 @@
 // import '../share-form';
 import notify from 'common/notify';
 const $table = $('#share-history-table');
-const $container = $('#share-history-table-container');
 
 
 // tab 切换
@@ -15,7 +14,7 @@ $('.js-share-tab').on('click', function() {
   $.get($this.data('url'), function(html) {
     $table.html(html);
   });
-  
+
   $this.parent().addClass('active')
     .siblings().removeClass('active');
 });
@@ -26,21 +25,22 @@ $table.on('click', '.cancel-share-btn', function(e) {
   let $this = $(this);
 
   $.post($this.data('url'), {
-    targetUserId: $this.attr('targetUserId') 
+    targetUserId: $this.attr('targetUserId')
     }, function(response) {
     $btn.closest('.share-history-record').remove();
     notify('success', Translator.trans('material.cancel_share.tips'));
   }, 'json');
-  
+
 });
 
 
 $('.modal').off('click.modal-pagination');
 $table.on('click', '.pagination li', function() {
   let $this = $(this);
-  let url = $this.data('url');
-  
-  $.get(url, function(html) {
-    $container.html(html);
+  let page = $this.data('page');
+  let url = $this.closest(".pagination").data('url');
+
+  $.get(url, { 'page': page }, function(html) {
+    $table.html(html);
   });
 });
