3fe92343711c7c2821ea7efd55c8810b7cbf881a
zhongjie <zhongjie@howzhi.com>
2017-08-29 12:50:30 +0800

    中台改版-用户名修改

--- app/Resources/static-src/common/codeages-design/js/form.js
+++ app/Resources/static-src/common/codeages-design/js/form.js
@@ -28,4 +28,20 @@
     fr.readAsDataURL(this.files[0]);
   });
 
+  $(document).on('click.cd.form.editable', '[data-toggle="form-editable"]', function() {
+    let $this = $(this);
+    let parent = $this.closest('[data-target="form-static-text"]');
+    let $formGroup = $this.closest('.cd-form-group');
+    parent.hide();
+    $formGroup.find('[data-target="form-editable"]').show();
+  });
+
+  $(document).on('click.cd.form.editable.close', '[data-dismiss="form-editable-close"]', function() {
+    let $this = $(this);
+    let parent = $this.closest('[data-target="form-editable"]');
+    let $formGroup = $this.closest('.cd-form-group');
+    parent.hide();
+    $formGroup.find('[data-target="form-static-text"]').show();
+  });
+
 })(jQuery);
