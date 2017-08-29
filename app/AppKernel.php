f6d48894ecb0f1020d53245494780896477dbba6
wangdanyue <wangdanyue@howzhi.com>
2017-08-29 16:42:34 +0800

    pay callback

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -171,7 +171,6 @@ class AppKernel extends Kernel implements PluginableHttpKernelInterface
 
         $biz->register(new \Biz\Accessor\AccessorServiceProvider());
         $biz->register(new \Biz\OrderFacade\OrderFacadeServiceProvider());
-        $biz->register(new \Biz\Callback\CallbackProvider());
 
         $biz->boot();
 
