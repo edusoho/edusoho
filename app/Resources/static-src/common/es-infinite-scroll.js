c725c212bca46e1aa9a970a7957a9db1432311ac
maofangfei <maofangfei@howzhi.com>
2017-08-30 13:00:04 +0800

    减少类

--- app/Resources/static-src/common/es-infinite-scroll.js
+++ app/Resources/static-src/common/es-infinite-scroll.js
@@ -12,7 +12,6 @@ export default class ESInfiniteScroll extends Emitter {
     this.options = options;
 
     this.initDownInfinite();
-    this.initUpInfinite();
   }
 
   initDownInfinite() {
@@ -23,50 +22,5 @@ export default class ESInfiniteScroll extends Emitter {
     defaultDownOptions = Object.assign(defaultDownOptions, this.options);
 
     this.downInfinite = new Waypoint.Infinite(defaultDownOptions);
-  }
-
-  initUpInfinite() {
-    let instance = this;
-    if ($('#up-more-link').length > 0) {
-      let defaultUpOptions = {
-        element: document.getElementById(this.UP_MORE_LINK_ID),
-        handler: function(direction) {
-          if (direction === 'up') {
-            instance.handleUpAction();
-          }
-        }
-      };
-
-      defaultUpOptions = Object.assign(defaultUpOptions, this.options);
-
-      this.upInfinite = new Waypoint(defaultUpOptions);
-    }
-  }
-
-  handleUpAction() {
-    let upInfinite = this.upInfinite,
-        upId = this.UP_MORE_LINK_ID,
-        downInfinite = this.downInfinite,
-        self = this;
-
-    upInfinite.disable();
-    downInfinite.$container.addClass('infinite-loading-top');
-    $.get($(upInfinite.element).data('url'), function (html) {
-
-      $(html).find(downInfinite.options.items).prependTo(downInfinite.$container);
-      let $upLink = $(html).find('#'+upId);
-      if ($upLink.length > 0) {
-        $(upInfinite.element).data('url', $upLink.data('url'));
-        upInfinite.enable();
-      } else {
-        upInfinite.element.remove();
-        upInfinite.destroy();
-      }
-
-      self.emit('up-infinite.loaded');
-
-      downInfinite.$container.removeClass('infinite-loading-top');
-
-    });
   }
 }
