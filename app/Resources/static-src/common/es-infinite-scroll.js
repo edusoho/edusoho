76d47090ef5df1c9c8bdf126db36784045a1bffa
ruanwenqin <ruanwenqin@howzhi.com>
2017-09-05 17:33:09 +0800

    点击加载更多改成同步请求

--- app/Resources/static-src/common/es-infinite-scroll.js
+++ app/Resources/static-src/common/es-infinite-scroll.js
@@ -16,15 +16,21 @@ export default class ESInfiniteScroll extends Emitter {
   initUpLoading() {
     $('.js-up-more-link').on('click', event => {
       let $target = $(event.currentTarget);
-      $.get($target.data('url'), html => {
-        $(html).find('.infinite-item').prependTo($('.infinite-container'));
-        let $upLink = $(html).find('.js-up-more-link');
-        if ($upLink.length > 0) {
-          $target.data('url', $upLink.data('url'));
-        } else {
-          $target.remove();
+      $.ajax({
+        method: 'GET',
+        url: $target.data('url'),
+        async: false,
+        success: html => {
+          $(html).find('.infinite-item').prependTo($('.infinite-container'));
+          let $upLink = $(html).find('.js-up-more-link');
+          if ($upLink.length > 0) {
+            $target.data('url', $upLink.data('url'));
+          } else {
+            $target.remove();
+          }
         }
-      })
+      });
+      
     })
   }
 
