411d2e20056ddf21c4094cdae201f90578e46908
tianshengjie <tianshengjie@howzhi.com>
2017-09-07 20:45:45 +0800

    fix

--- app/Resources/static-src/app/js/settings/security/index.js
+++ app/Resources/static-src/app/js/settings/security/index.js
@@ -2,9 +2,9 @@ import notify from 'common/notify';
 
 $('#send-verify-email').click(function() {
   let $btn = $(this);
-  $btn.button('loading');
   $.post($btn.data('url')).done(function(data) {
     $('#modal').html(data).modal('show');
+    $btn.button('reset');
   }).fail(function(data) {
     $btn.button('reset');
     notify('danger',  Translator.trans(data.responseJSON.message));
