2495561b6cf7ca9264b21ff6495d46c01d96deca
Allen Lau <liuyangyang@howzhi.com>
2017-09-13 11:59:45 +0800

    add variable for bin/biz

--- biz.php
+++ biz.php
@@ -1,17 +1,18 @@
 <?php
 
-require_once __DIR__.'/app/autoload.php';
-require_once __DIR__.'/app/bootstrap.php.cache';
-require_once __DIR__.'/app/AppKernel.php';
+require_once __DIR__ . '/app/autoload.php';
+require_once __DIR__ . '/app/bootstrap.php.cache';
+require_once __DIR__ . '/app/AppKernel.php';
 
 
 $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$kernel = new AppKernel('dev', true);
+
+$appEnv = getAppEvn();
+$kernel = new AppKernel($appEnv, true);
 $kernel->setRequest($request);
 $kernel->boot();
 
 $options = $kernel->getContainer()->getParameter('biz_config');
-
 
 $biz = new Codeages\Biz\Framework\Context\Biz($options);
 $biz->register(new \Codeages\Biz\Framework\Provider\DoctrineServiceProvider());
@@ -20,7 +21,7 @@ $biz->register(new \Codeages\Biz\Framework\Provider\TokenServiceProvider());
 $biz->register(new \Codeages\Biz\Framework\Provider\SchedulerServiceProvider());
 $biz->register(new \Codeages\Biz\Framework\Provider\SettingServiceProvider());
 $biz->register(new \Codeages\Biz\Framework\Provider\TargetlogServiceProvider());
-$biz->register(new \Codeages\Biz\Framework\Provider\MonologServiceProvider(), array(  'monolog.logfile' => $biz['log_directory'].'/biz.log',));
+$biz->register(new \Codeages\Biz\Framework\Provider\MonologServiceProvider(), array('monolog.logfile' => $biz['log_directory'] . '/biz.log',));
 $biz->boot();
 
 return $biz;
@@ -29,19 +30,31 @@ return $biz;
  * @param $arguments
  * @return mixed|string
  */
-function getAppEvn($arguments)
+function getAppEvn()
 {
-    //check if set variable environment
-    $variables = array_filter($arguments, function ($arg) {
-        return strpos($arg, '-e=') === 0 || strpos($arg, '--env=') === 0;
-    });
-
-    if (empty($variables)) {
-        return $env = 'dev';
+    $file = '/tmp/evn';
+    if (!file_exists($file)) {
+        throw new \InvalidArgumentException("Environment file is not exist.");
     }
-    //get first environment
-    $env = array_shift($variables);
-    // get environment value
-    $variables = explode("=", $env);
-    return $env = array_pop($variables);
+
+    $env = require $file;
+
+    return isset($env['evn']) ? $env['evn'] : 'dev';
+//    if (!is_array($env)) {
+//        throw new \RuntimeException("Environment file must return array.");
+//    }
+//
+//    //check if set variable environment
+//    $variables = array_filter($arguments, function ($arg) {
+//        return strpos($arg, '-e=') === 0 || strpos($arg, '--env=') === 0;
+//    });
+//
+//    if (empty($variables)) {
+//        return $env = 'dev';
+//    }
+//    //get first environment
+//    $env = array_shift($variables);
+//    // get environment value
+//    $variables = explode("=", $env);
+//    return $env = array_pop($variables);
 }
