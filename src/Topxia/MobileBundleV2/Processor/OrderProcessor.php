01f13160a90d5ebfb519cca7e79dacc5341341c1
ruanwenqin <ruanwenqin@howzhi.com>
2017-09-14 16:43:51 +0800

    移除老接口和增加trans接口

--- src/Topxia/MobileBundleV2/Processor/OrderProcessor.php
+++ src/Topxia/MobileBundleV2/Processor/OrderProcessor.php
@@ -6,21 +6,23 @@ interface OrderProcessor
 {
     public function validateIAPReceipt();
 
-    public function payCourse();
+    //public function payCourse();
 
     /*
     * (float) amount 充值金额
     * (String)  payType  iap, alipay
     */
-    public function buyCoin();
+   // public function buyCoin();
+
+   // public function payVip();
+
+    //public function payClassRoom();
+
+    //public function checkCoupon();
+
+    public function getPaymentMode();
 
     public function getPayOrder();
 
-    public function payVip();
-
-    public function payClassRoom();
-
-    public function checkCoupon();
-
-    public function getPaymentMode();
+    public function createOrder();
 }
