0c1e048469e59e8f963f44f173c28c99fddec769
bellorchid <zhongyunchang@yeah.net>
2017-09-15 11:10:46 +0800

    修改导出的BUG

--- src/AppBundle/Component/Export/Order/OrderExporter.php
+++ src/AppBundle/Component/Export/Order/OrderExporter.php
@@ -4,6 +4,8 @@ namespace AppBundle\Component\Export\Order;
 
 use AppBundle\Common\ArrayToolkit;
 use AppBundle\Component\Export\Exporter;
+use Biz\Course\Service\CourseService;
+use Biz\User\Service\UserService;
 use Codeages\Biz\Framework\Order\Service\OrderService;
 
 class OrderExporter extends Exporter
@@ -22,102 +24,15 @@ class OrderExporter extends Exporter
 
     public function getTitles()
     {
-        return array('订单号', '订单状态', '订单名称', '订单价格', '优惠码', '优惠金额', '虚拟币支付', '实付价格', '支付方式', '购买者', '姓名', '操作', '创建时间', '付款时间');
+        return array();
     }
 
     public function getContent($start, $limit)
     {
-        $orders = $this->getOrderService()->searchOrders($this->conditions, array('created_time' => 'DESC'), $start, $limit);
-        $userIds = ArrayToolkit::column($orders, 'user_id');
-
-        $users = $this->getUserService()->findUsersByIds($userIds);
-        $profiles = $this->getUserService()->findUserProfilesByIds($userIds);
-
-        $ordersContent = $this->handlerOrder($orders, $users, $profiles);
-
-        return $ordersContent;
     }
 
     public function buildCondition($conditions)
     {
-        $conditions = ArrayToolkit::parts($conditions,
-            array(
-                'startDateTime',
-                'endDateTime',
-                'status',
-                'payment',
-                'keywordType',
-                'keyword',
-            )
-        );
-        if (!empty($conditions['startDateTime']) && !empty($conditions['startDateTime'])) {
-            $conditions['start_time'] = strtotime($conditions['startDateTime']);
-            $conditions['end_time'] = strtotime($conditions['startDateTime']);
-        }
-
-        return $conditions;
-    }
-
-    protected function handlerOrder($orders, $users, $profiles)
-    {
-        $ordersContent = array();
-
-        $payment = $this->container->get('codeages_plugin.dict_twig_extension')->getDict('payment');
-        $status = array(
-            'created' => '未付款',
-            'paid' => '已付款',
-            'refunding' => '退款中',
-            'refunded' => '已退款',
-            'cancelled' => '已关闭',
-        );
-
-        foreach ($orders as $key => $order) {
-            $member = array();
-            $member[] = $order['sn'];
-            $member[] = $status[$order['status']];
-
-            $member[] = $order['title'];
-
-            $member[] = $order['price_amount'];
-
-            if (!empty($order['coupon'])) {
-                $member[] = $order['coupon'];
-            } else {
-                $member[] = '无';
-            }
-
-            $member[] = empty($order['couponDiscount']) ? '' : $order['couponDiscount'];
-            $member[] = !empty($order['coinRate']) ? ($order['coinAmount'] / $order['coinRate']) : '0';
-            $member[] = $order['pay_amount'];
-
-            $orderPayment = empty($order['payment']) ? 'none' : $order['payment'];
-            if (!empty($payment[$orderPayment])) {
-                $member[] = $payment[$orderPayment];
-            } else {
-                $member[] = $payment['none'];
-            }
-
-            $member[] = $users[$order['user_id']]['nickname'];
-            $member[] = $profiles[$order['user_id']]['truename'] ? $profiles[$order['user_id']]['truename'] : '-';
-
-            if (preg_match('/管理员添加/', $order['title'])) {
-                $member[] = '管理员添加';
-            } else {
-                $member[] = '-';
-            }
-
-            $member[] = date('Y-n-d H:i:s', $order['created_time']);
-
-            if ($order['pay_time'] != 0) {
-                $member[] = date('Y-n-d H:i:s', $order['pay_time']);
-            } else {
-                $member[] = '-';
-            }
-
-            $ordersContent[] = $member;
-        }
-
-        return $ordersContent;
     }
 
     /**
@@ -126,5 +41,21 @@ class OrderExporter extends Exporter
     protected function getOrderService()
     {
         return $this->getBiz()->service('Order:OrderService');
+    }
+
+    /**
+     * @return UserService
+     */
+    protected function getUserService()
+    {
+        return parent::getUserService();
+    }
+
+    /**
+     * @return CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->getBiz()->service('Course:CourseService');
     }
 }
