f781b36c7a05d27b96f8aa4c2dac5f5df625564d
nifeng <nifeng@howzhi.com>
2017-09-19 10:38:19 +0800

    fix lianlianpay in mobile

--- src/AppBundle/Controller/Cashier/LianlianpayController.php
+++ src/AppBundle/Controller/Cashier/LianlianpayController.php
@@ -11,7 +11,7 @@ class LianlianpayController extends BaseController
     public function payAction($trade)
     {
         $user = $this->getUser();
-        $trade['platform_type'] = 'Web';
+        $trade['platform_type'] = $this->isMobileClient()? 'Wap' : 'Web';
         $trade['attach']['user_created_time'] = $user['createdTime'];
         $trade['attach']['identify_user_id'] = $this->getIdentify().'_'.$user['id'];
         $trade['notify_url'] = $this->generateUrl('cashier_pay_notify', array('payment' => 'lianlianpay'), true);
@@ -50,6 +50,49 @@ class LianlianpayController extends BaseController
         }
 
         return $identify;
+    }
+
+    protected function isMobileClient()
+    {
+        // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+        if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
+            return true;
+        }
+
+        //如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+        if (isset($_SERVER['HTTP_VIA'])) {
+            //找不到为flase,否则为true
+            return stristr($_SERVER['HTTP_VIA'], 'wap') ? true : false;
+        }
+
+        //判断手机发送的客户端标志,兼容性有待提高
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            $clientkeywords = array(
+                'nokia', 'sony', 'ericsson', 'mot', 'samsung', 'htc', 'sgh', 'lg', 'sharp',
+                'sie-', 'philips', 'panasonic', 'alcatel', 'lenovo', 'iphone', 'ipod', 'blackberry', 'meizu',
+                'android', 'netfront', 'symbian', 'ucweb', 'windowsce', 'palm', 'operamini', 'operamobi',
+                'openwave', 'nexusone', 'cldc', 'midp', 'wap', 'mobile',
+            );
+
+            // 从HTTP_USER_AGENT中查找手机浏览器的关键字
+            if (preg_match('/('.implode('|', $clientkeywords).')/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+                return true;
+            }
+        }
+
+        //协议法，因为有可能不准确，放到最后判断
+        if (isset($_SERVER['HTTP_ACCEPT'])) {
+            // 如果只支持wml并且不支持html那一定是移动设备
+            // 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false)
+                && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false
+                    || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html'))
+                )) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
