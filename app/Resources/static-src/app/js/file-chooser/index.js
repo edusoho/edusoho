1a6b4815b4214bc7b1cb53220040aa26d9807f4f
bellorchid <zhongyunchang@yeah.net>
2017-09-20 17:12:22 +0800

    删除无用JS

--- app/Resources/static-src/app/js/file-chooser/index.js
+++
@@ -1,9 +0,0 @@
-function initUploadedFile() {
-  let existFile = $('#selected-file').data();
-  console.log(existFile);
-  if (existFile) {
-    $('[name="media"]').val(JSON.stringify(existFile));
-  }
-}
-
-initUploadedFile();
