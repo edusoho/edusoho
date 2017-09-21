31f5a7adab3a0362f5e690627fba461536f68cea
nifeng <nifeng@howzhi.com>
2017-09-21 14:33:31 +0800

    add refund_deadline

---
+++ migrations/20170921063253_order_add_refund_deadline.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class OrderAddRefundDeadline extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        if (!$this->isFieldExist('biz_order', 'refund_deadline')) {
+            $db->exec("ALTER TABLE `biz_order` ADD COLUMN `refund_deadline` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '申请退款截止日期';");
+        }
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        $db->exec("ALTER TABLE `biz_order` DROP COLUMN `refund_deadline`;");
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $db->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+}
