00131be3fdb1cec9aca81bc7e08d938d45787abb
nifeng <nifeng@howzhi.com>
2017-09-25 07:34:03 +0800

    update job class

---
+++ migrations/20170925072759_add_clear_session_job.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddClearSessionJob extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $connection = $biz['db'];
+        $connection->exec("update biz_job set class='Codeages\Biz\Framework\Session\Job\DeleteSessionJob' where name='DeleteSessionJob';");
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
