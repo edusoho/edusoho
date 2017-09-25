16269749055651c159d87a3f39bc31061f1cc887
nifeng <nifeng@howzhi.com>
2017-09-25 19:20:46 +0800

    add fields

---
+++ migrations/20170925111954_biz_online_add_fields.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class BizOnlineAddFields extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $connection = $biz['db'];
+
+        $connection->exec("ALTER TABLE `biz_online` ADD COLUMN `device` varchar(1024) COMMENT '设备';");
+        $connection->exec("ALTER TABLE `biz_online` ADD COLUMN `os` varchar(1024) COMMENT '操作系统';");
+        $connection->exec("ALTER TABLE `biz_online` ADD COLUMN `client` varchar(1024) COMMENT '客户端信息';");
+        $connection->exec("ALTER TABLE `biz_online` ADD COLUMN `device_brand` varchar(1024) COMMENT '品牌名称';");
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $biz = $this->getContainer();
+        $connection = $biz['db'];
+
+        $connection->exec("ALTER TABLE `biz_online` DROP COLUMN `device`;");
+        $connection->exec("ALTER TABLE `biz_online` DROP COLUMN `os`;");
+        $connection->exec("ALTER TABLE `biz_online` DROP COLUMN `client`;");
+        $connection->exec("ALTER TABLE `biz_online` DROP COLUMN `device_brand`;");
+
+    }
+}
