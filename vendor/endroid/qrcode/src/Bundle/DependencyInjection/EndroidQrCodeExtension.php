747fef08adc34e59b1d5a0ced8019f80bf15a226
ruanwenqin <ruanwenqin@howzhi.com>
2017-09-27 10:24:48 +0800

    dwonggrade

--- vendor/endroid/qrcode/src/Bundle/DependencyInjection/EndroidQrCodeExtension.php
+++ vendor/endroid/qrcode/src/Bundle/DependencyInjection/EndroidQrCodeExtension.php
@@ -6,7 +6,6 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-
 namespace Endroid\QrCode\Bundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
@@ -14,20 +13,17 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-
-class EndroidQrCodeExtension extends Extension
+class EndroidQrCodeExtension extends \Symfony\Component\HttpKernel\DependencyInjection\Extension
 {
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, \Symfony\Component\DependencyInjection\ContainerBuilder $container)
     {
-        $processor = new Processor();
-        $config = $processor->processConfiguration(new Configuration(), $configs);
-
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $processor = new \Symfony\Component\Config\Definition\Processor();
+        $config = $processor->processConfiguration(new \Endroid\QrCode\Bundle\DependencyInjection\Configuration(), $configs);
+        $loader = new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader($container, new \Symfony\Component\Config\FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
-
         $factoryDefinition = $container->getDefinition('endroid.qrcode.factory');
         $factoryDefinition->setArguments(array($config));
     }
