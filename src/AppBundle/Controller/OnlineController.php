cc1c7f093d2a8e4177713c7d909cf5ddaabb45af
nifeng <nifeng@howzhi.com>
2017-09-28 15:15:22 +0800

    migrate

--- src/AppBundle/Controller/OnlineController.php
+++ src/AppBundle/Controller/OnlineController.php
@@ -14,7 +14,7 @@ class OnlineController extends BaseController
     {
         $sessionId = $request->getSession()->getId();
         $lastFlushTime = $request->getSession()->get('online_flush_time', 0);
-        if (!empty($sessionId) && (time() - $lastFlushTime) > 1 * 10) {
+        if (!empty($sessionId) && (time() - $lastFlushTime) > 5 * 60) {
             $online = array(
                 'sess_id' => $sessionId,
                 'ip' => $request->getClientIp(),
