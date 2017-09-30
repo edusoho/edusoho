ef15815ecb2116985a7ea18971e6ed1c843efe2a
nifeng <nifeng@howzhi.com>
2017-09-30 14:40:27 +0800

    fix lianlian mobile

--- src/AppBundle/Controller/Cashier/LianlianpayController.php
+++ src/AppBundle/Controller/Cashier/LianlianpayController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Cashier;
 
+use AppBundle\Common\DeviceToolkit;
 use Symfony\Component\HttpFoundation\Request;
 
 class LianlianpayController extends PaymentController
@@ -55,9 +56,23 @@ class LianlianpayController extends PaymentController
         return $this->createJsonResponse($result);
     }
 
-    public function returnAction(Request $request, $payment)
+    public function mobileReturnAction(Request $request, $payment)
     {
         $data = $request->request->all();
+        $data = json_decode($data['res_data'], true);
+        $this->getPayService()->notifyPaid($payment, $data);
+
+        return $this->redirect($this->generateUrl('cashier_pay_success', array('trade_sn' => $data['no_order']), true));
+    }
+
+    public function returnAction(Request $request, $payment)
+    {
+        if (DeviceToolkit::isMobileClient()) {
+            return $this->forward('AppBundle:Cashier/Lianlianpay:mobileReturn', $request, $payment);
+        }
+
+        $data = $request->request->all();
+
         $this->getPayService()->notifyPaid($payment, $data);
 
         return $this->redirect($this->generateUrl('cashier_pay_success', array('trade_sn' => $data['no_order']), true));
