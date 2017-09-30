ec10b3ebf6c3aeafe09d2683a3243aaea6037501
nifeng <nifeng@howzhi.com>
2017-09-30 14:26:16 +0800

    fix lianlian notify

--- src/AppBundle/Controller/Cashier/LianlianpayController.php
+++ src/AppBundle/Controller/Cashier/LianlianpayController.php
@@ -58,7 +58,6 @@ class LianlianpayController extends PaymentController
     public function returnAction(Request $request, $payment)
     {
         $data = $request->request->all();
-        $data = json_decode($data['res_data'], true);
         $this->getPayService()->notifyPaid($payment, $data);
 
         return $this->redirect($this->generateUrl('cashier_pay_success', array('trade_sn' => $data['no_order']), true));
