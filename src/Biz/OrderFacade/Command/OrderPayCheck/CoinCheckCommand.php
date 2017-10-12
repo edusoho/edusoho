517af839c22f22f9318bb533d6e910b6fbde726e
bellorchid <zhongyunchang@yeah.net>
2017-10-12 16:07:01 +0800

    fix code style  fixed

--- src/Biz/OrderFacade/Command/OrderPayCheck/CoinCheckCommand.php
+++ src/Biz/OrderFacade/Command/OrderPayCheck/CoinCheckCommand.php
@@ -11,7 +11,7 @@ class CoinCheckCommand extends OrderPayCheckCommand
 {
     public function execute($order, $params)
     {
-        if ((float)$params['coinAmount'] <= 0) {
+        if ((float) $params['coinAmount'] <= 0) {
             return;
         }
 
