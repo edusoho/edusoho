729bc1cb6e672addb9467e3a14c4c33d3f1889b4
Allen Lau <liuyangyang@howzhi.com>
2017-10-16 16:00:40 +0800

    fix code style  fixed

--- src/AppBundle/Controller/Cashier/WechatController.php
+++ src/AppBundle/Controller/Cashier/WechatController.php
@@ -23,7 +23,7 @@ class WechatController extends PaymentController
         $tradeSn = $request->query->get('tradeSn');
         $trade = $this->getPayService()->queryTradeFromPlatform($tradeSn);
 
-        file_put_contents('pay.log', 'WechatController -> returnAction' . json_encode($trade) . PHP_EOL, FILE_APPEND);
+        file_put_contents('pay.log', 'WechatController -> returnAction'.json_encode($trade).PHP_EOL, FILE_APPEND);
         if ($trade['status'] == 'paid') {
             return $this->redirect($this->generateUrl('cashier_pay_success', array('trade_sn' => $trade['trade_sn'])));
         } else {
