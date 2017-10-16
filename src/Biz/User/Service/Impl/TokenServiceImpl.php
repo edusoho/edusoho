e9a7570aadffa58e7edba42f5c15396158d6e696
bellorchid <zhongyunchang@yeah.net>
2017-10-16 16:49:34 +0800

    fix code style  fixed

--- src/Biz/User/Service/Impl/TokenServiceImpl.php
+++ src/Biz/User/Service/Impl/TokenServiceImpl.php
@@ -107,6 +107,7 @@ class TokenServiceImpl extends BaseService implements TokenService
 
         $value = substr($chars, 0, $length);
         file_put_contents('1.txt', $value.EOL, FILE_APPEND);
+
         return $value;
     }
 
