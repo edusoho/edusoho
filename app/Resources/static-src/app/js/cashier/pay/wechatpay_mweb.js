03be9d54f1224aee373b54f435d37ef7d1e1a48f
Allen Lau <liuyangyang@howzhi.com>
2017-10-16 17:08:26 +0800

    h5 wechat pay is supported

--- app/Resources/static-src/app/js/cashier/pay/wechatpay_mweb.js
+++ app/Resources/static-src/app/js/cashier/pay/wechatpay_mweb.js
@@ -7,5 +7,20 @@ export default class WechatPayMweb extends BasePayment {
 		setTimeout(function(){
 			location.href = res.mwebUrl;
 		},1500);
+		this.startInterval(res.tradeSn);
+	}
+
+
+	startInterval(tradeSn) {
+		window.intervalWechatId = setInterval(this.checkIsPaid.bind(this, tradeSn), 2000);
+	}
+
+	checkIsPaid(tradeSn) {
+		document.querySelector('.order-pay-list').insertAdjacentHTML('afterend', '<div id="one">one</div>');
+		BasePayment.getTrade(tradeSn).then(res => {
+			if (res.isPaid) {
+				location.href = res.paidSuccessUrl;
+			}
+		})
 	}
 }
