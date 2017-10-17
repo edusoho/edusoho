524905ba022bf760715d4f2f06f4dc4cbb41af1e
nifeng <nifeng@howzhi.com>
2017-10-17 16:21:03 +0800

    fix code style  fixed

--- src/AppBundle/Command/CheckOrderPayUpgradeCommand.php
+++ src/AppBundle/Command/CheckOrderPayUpgradeCommand.php
@@ -5,7 +5,6 @@ namespace AppBundle\Command;
 use AppBundle\Common\ArrayToolkit;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 
 class CheckOrderPayUpgradeCommand extends BaseCommand
 {
@@ -23,10 +22,9 @@ class CheckOrderPayUpgradeCommand extends BaseCommand
         $this->checkBizOrder($output, $biz, $connection);
     }
 
-
     protected function checkBizOrder($output, $biz, $connection)
     {
-        $output->writeln("<info>biz_order：</info>");
+        $output->writeln('<info>biz_order：</info>');
 
         $countOrders = $connection->fetchColumn('select count(*) from orders');
         $countBizOrder = $connection->fetchColumn('select count(*) from biz_order');
@@ -37,7 +35,7 @@ class CheckOrderPayUpgradeCommand extends BaseCommand
         }
 
         $status = $connection->fetchAll('select status from biz_order group by status');
-        $output->writeln("<info>biz_order的status字段：".json_encode(ArrayToolkit::column($status, 'status')));
+        $output->writeln('<info>biz_order的status字段：'.json_encode(ArrayToolkit::column($status, 'status')));
 
         $count = $connection->fetchColumn("select count(*) from biz_order where status='closed' and close_time=0");
         $output->writeln("<info>biz_order的closed状态下close_time=0的数量：{$count}");
@@ -45,18 +43,18 @@ class CheckOrderPayUpgradeCommand extends BaseCommand
         $count = $connection->fetchColumn("select count(*) from biz_order where status='closed' and close_user_id=0");
         $output->writeln("<info>biz_order的closed状态下close_user_id=0的数量：{$count}");
 
-        $count = $connection->fetchColumn("select count(*) from biz_order where price_amount < pay_amount");
+        $count = $connection->fetchColumn('select count(*) from biz_order where price_amount < pay_amount');
         $output->writeln("<info>biz_order的price_amount < pay_amount数量：{$count}");
 
-        $payments = $connection->fetchAll("select payment from biz_order group by payment");
-        $output->writeln("<info>biz_order的payment：".json_encode(ArrayToolkit::column($payments, 'payment')));
+        $payments = $connection->fetchAll('select payment from biz_order group by payment');
+        $output->writeln('<info>biz_order的payment：'.json_encode(ArrayToolkit::column($payments, 'payment')));
 
         $payments = $connection->fetchAll("select payment from biz_order where status = 'success' group by payment");
-        $output->writeln("<info>biz_order的success状态下的payment：".json_encode(ArrayToolkit::column($payments, 'payment')));
+        $output->writeln('<info>biz_order的success状态下的payment：'.json_encode(ArrayToolkit::column($payments, 'payment')));
 
         $payments = $connection->fetchAll("select payment from biz_order where status = 'success' and pay_amount>0 group by payment");
         $output->writeln("<info>biz_order的status = 'success' and pay_amount>0下的payment：".json_encode(ArrayToolkit::column($payments, 'payment')));
 
-        $output->writeln("");
+        $output->writeln('');
     }
 }
