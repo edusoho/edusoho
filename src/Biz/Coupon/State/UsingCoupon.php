424bd92d8d9d7b4a89d6e2e6604b6586480ceded
tianshengjie <tianshengjie@howzhi.com>
2017-10-20 13:41:18 +0800

    修复优惠码被使用了，但是被使用数量未增加

--- src/Biz/Coupon/State/UsingCoupon.php
+++ src/Biz/Coupon/State/UsingCoupon.php
@@ -25,6 +25,7 @@ class UsingCoupon extends Coupon implements CouponInterface
                 'orderId' => $params['orderId'],
             )
         );
+        $this->updateBachCouponUsedNum($coupon['batchId']);
 
         $card = $this->getCardService()->getCardByCardIdAndCardType($coupon['id'], 'coupon');
 
@@ -50,6 +51,22 @@ class UsingCoupon extends Coupon implements CouponInterface
         $this->getCouponService()->updateCoupon($this->coupon['id'], array(
             'status' => 'receive',
         ));
+        $this->updateBachCouponUsedNum($this->coupon['batchId']);
+    }
+
+    private function updateBachCouponUsedNum($batchId)
+    {
+        if (0 == $batchId) {
+            return;
+        }
+        $couponApp = $this->getAppService()->getAppByCode('Coupon');
+        if (!empty($couponApp)) {
+            $usedCouponsCount = $this->getCouponService()->searchCouponsCount(array(
+                'status' => 'used',
+                'batchId' => $batchId,
+            ));
+            $usedCouponCount = $this->getCouponBatchService()->updateBatch($batchId, array('usedNum' => $usedCouponsCount));
+        }
     }
 
     /**
@@ -66,5 +83,21 @@ class UsingCoupon extends Coupon implements CouponInterface
     private function getUserService()
     {
         return $this->biz->service('User:UserService');
+    }
+
+    /**
+     * @return AppService
+     */
+    protected function getAppService()
+    {
+        return $this->biz->service('CloudPlatform:AppService');
+    }
+
+    /**
+     * @return CouponBatchService
+     */
+    private function getCouponBatchService()
+    {
+        return $this->biz->service('CouponPlugin:Coupon:CouponBatchService');
     }
 }
