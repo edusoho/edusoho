78db91ebb7b120b996ec654925c6bd1158305054
zhongjie <zhongjie@howzhi.com>
2017-10-20 19:38:39 +0800

    修改table组件以及它的调用方式

--- app/Resources/static-src/common/codeages-design/js/codeages-design.js
+++ app/Resources/static-src/common/codeages-design/js/codeages-design.js
@@ -2,14 +2,14 @@ import radio from './radio';
 import confirm from './confirm';
 import loading from './loading';
 import upload from './upload';
-
-import './table';
+import table from './table';
 
 let codeAgesDisign = {
   radio,
   confirm,
   loading,
-  upload
+  upload,
+  table,
 };
 
 window.cd = codeAgesDisign;
