ca4f03e4246040b9e7aac5c3a4272aba970dc1c3
bellorchid <zhongyunchang@yeah.net>
2017-10-24 12:10:38 +0800

    修改班级的lock判断

--- src/Biz/Classroom/Service/Impl/ClassroomServiceImpl.php
+++ src/Biz/Classroom/Service/Impl/ClassroomServiceImpl.php
@@ -1684,7 +1684,11 @@ class ClassroomServiceImpl extends BaseService implements ClassroomService
 
         $member = $this->getClassroomMember($classroomId, $userId);
 
-        if (empty($member) || !in_array('student', $member['role'])) {
+        if (empty($member)) {
+            return ;
+        }
+
+        if (!in_array('student', $member['role'])) {
             throw $this->createServiceException("用户(#{$classroomId})不是班级(#{$classroomId})的学员，封锁学员失败。");
         }
 
@@ -1705,7 +1709,11 @@ class ClassroomServiceImpl extends BaseService implements ClassroomService
 
         $member = $this->getClassroomMember($classroomId, $userId);
 
-        if (empty($member) || !in_array('student', $member['role'])) {
+        if (empty($member)) {
+            return ;
+        }
+
+        if (!in_array('student', $member['role'])) {
             throw $this->createServiceException("用户(#{$userId})不是该班级(#{$classroomId})的学员，解封学员失败。");
         }
 
