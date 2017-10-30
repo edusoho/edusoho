81158c163af53e2a757fb600c1753389c897058c
ruanwenqin <ruanwenqin@howzhi.com>
2017-10-30 19:16:30 +0800

    重命名

--- app/Resources/static-src/app/js/user/fill-info/index.js
+++ app/Resources/static-src/app/js/user/fill-info/index.js
@@ -1,4 +1,4 @@
-import UserInfoFieldsItemValidate from '../userinfo-fields-common2';
+import UserInfoFieldsItemValidate from '../userinfo-fields-common-no-ajax';
 
 new UserInfoFieldsItemValidate({
   element: '#fill-userinfo-form'
