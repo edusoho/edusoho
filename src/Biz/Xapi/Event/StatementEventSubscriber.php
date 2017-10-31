15fabef762dfc5584a399d9c040cc1d99a5a8fe7
bellorchid <zhongyunchang@yeah.net>
2017-10-31 16:45:19 +0800

    修改错误

--- src/Biz/Xapi/Event/StatementEventSubscriber.php
+++ src/Biz/Xapi/Event/StatementEventSubscriber.php
@@ -355,7 +355,7 @@ class StatementEventSubscriber extends EventSubscriber implements EventSubscribe
         $siteName = empty($siteSettings['name']) ? '' : $siteSettings['name'];
         $accessKey = empty($settings['cloud_access_key']) ? '' : $settings['cloud_access_key'];
         $secretKey = empty($settings['cloud_secret_key']) ? '' : $settings['cloud_secret_key'];
-        $auth = new Auth($accessKey, $secretKey);
+        $auth = new Auth('9DdikSDLhmObBhE0t3mhN9UUl8FW2Zdh', 'jNqSV44Fx5kxBFc4VI840pLk8D6QeO86');
 
         return new \QiQiuYun\SDK\Service\XAPIService($auth, array(
             'base_uri' => 'http://192.168.4.214:8769/v1/xapi/', //推送的URL需要配置
