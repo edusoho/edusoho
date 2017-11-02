c0e93bedbcf047b5b3845c723d549982878afa98
bellorchid <zhongyunchang@yeah.net>
2017-11-02 14:42:12 +0800

    修改XAPI的数据挂载

---
+++ src/Biz/Xapi/XapiServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Biz\Xapi;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class XapiServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $biz)
+    {
+        // TODO: Implement register() method.
+    }
+
+
+}
