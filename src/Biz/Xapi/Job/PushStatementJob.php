14f58905d35ecdc2dfd4931de23d4a31bc488a11
bellorchid <zhongyunchang@yeah.net>
2017-11-02 14:55:54 +0800

    修改任务参数

---
+++ src/Biz/Xapi/Job/PushStatementJob.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Biz\Framework\Xapi\Job;
+
+use Biz\Xapi\Service\XapiService;
+use Codeages\Biz\Framework\Scheduler\AbstractJob;
+use Codeages\Biz\Framework\Util\ArrayToolkit;
+use Guzzle\Http\Client;
+
+class PushStatementsJob extends AbstractJob
+{
+
+    public function execute()
+    {
+        $condition = array(
+            'status' => 'created'
+        );
+        $statements = $this->getXapiService()->searchStatements($condition, array('created_time' => 'ASC'), 0, 100);
+        $statementIds = ArrayToolkit::column($statements, 'id');
+
+        $this->getXapiService()->updateStatementsPushingByStatementIds($statementIds);
+        $result = $this->pushStatements($statements);
+        if ($result) {
+            $this->getXapiService()->updateStatementsPushedByStatementIds($statementIds);
+        }
+    }
+
+    protected function pushStatements($statements)
+    {
+        $pushdStatements = array();
+        foreach ($statements as $statement) {
+            $pushStatement = ArrayToolkit::parts($statement['data'], array('actor', 'verb', 'object', 'result', 'context'));
+            $pushStatement['timestamp'] = time();
+            $pushStatement['id'] = $statement['uuid'];
+            $pushdStatements[] = $pushStatement;
+        }
+
+        $client = new Client();
+        $request = $client->post($this->biz['xapi.options']['getway'], array(
+            'Content-type' => 'application/json; charset=utf-8',
+        ), json_encode($pushdStatements));
+
+        $response = $request->send();
+        if ($response->getStatusCode() == 200) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return XapiService
+     */
+    protected function getXapiService()
+    {
+        return $this->biz->service('Xapi:XapiService');
+    }
+}
