131b6479a2eef1df93e3c3271d96d895bfaaee46
maofangfei <maofangfei@howzhi.com>
2017-11-02 20:12:15 +0800

    三个格式化

--- src/Topxia/AdminBundle/Resources/public/js/controller/uploader/local-strategy.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/uploader/local-strategy.js
@@ -1,42 +1,42 @@
 define(function(require, exports, module) {
 
-	var Class = require('class');
+  var Class = require('class');
 
-    var LocalStrategy = Class.extend({
-    	initialize: function(file, response) {
-            this.file = file;
-            file.uploaderWidget.uploader.option('server', response.url);
-            file.uploaderWidget.uploader.option('formData', response.postParams);
-        },
-        
-        uploadBeforeSend: function(object, data, headers){
-            $.each(data, function(i, n){
-                if(i!='file') {
-                    delete data[i];
-                }
-            })
-        },
+  var LocalStrategy = Class.extend({
+    initialize: function(file, response) {
+      this.file = file;
+      file.uploaderWidget.uploader.option('server', response.url);
+      file.uploaderWidget.uploader.option('formData', response.postParams);
+    },
 
-        uploadAccept: function(object, ret){
-            this.file.fileId = ret.id;
-        },
-
-        finishUpload: function(deferred) {
-            return {
-                id: this.file.fileId,
-                status: 'success'
-            };
-        },
-        /**
-         *
-         * @param lastPercentage    上一次进度
-         * @param currentPercentage 当前进度
-         * @returns {boolean}
-         */
-        needDisplayPercent: function (lastPercentage, currentPercentage) {
-            return true;
+    uploadBeforeSend: function(object, data, headers) {
+      $.each(data, function(i, n) {
+        if (i != 'file') {
+          delete data[i];
         }
-    });
+      })
+    },
 
-    module.exports = LocalStrategy;
+    uploadAccept: function(object, ret) {
+      this.file.fileId = ret.id;
+    },
+
+    finishUpload: function(deferred) {
+      return {
+        id: this.file.fileId,
+        status: 'success'
+      };
+    },
+    /**
+     *
+     * @param lastPercentage    上一次进度
+     * @param currentPercentage 当前进度
+     * @returns {boolean}
+     */
+    needDisplayPercent: function(lastPercentage, currentPercentage) {
+      return true;
+    }
+  });
+
+  module.exports = LocalStrategy;
 });
