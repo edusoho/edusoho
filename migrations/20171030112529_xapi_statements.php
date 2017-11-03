db52d916cffd27f06f112d976c837b346836c7ce
bellorchid <zhongyunchang@yeah.net>
2017-11-03 11:04:23 +0800

    修改错误

--- migrations/20171030112529_xapi_statements.php
+++ migrations/20171030112529_xapi_statements.php
@@ -12,19 +12,21 @@ class XapiStatements extends Migration
         $biz = $this->getContainer();
         $connection = $biz['db'];
         $connection->exec("
-            CREATE TABLE `xapi_statement` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `uuid` varchar(64)  NOT NULL ,
-              `version` varchar(32) NOT NULL DEFAULT '' COMMENT '版本号',
-              `data` text NOT NULL COMMENT '数据',
-              `push_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '上报时间',
-              `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '所属用户',
-              `status` varchar(16) NOT NULL DEFAULT 'created' COMMENT '状态: created, pushing, pushed',
-              `created_time` int(10) unsigned NOT NULL COMMENT '创建时间',
-              PRIMARY KEY (`id`),
-              UNIQUE KEY `uuid` (`uuid`),
-              KEY `status` (`status`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+          CREATE TABLE `xapi_statement` (
+          `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `uuid` varchar(64) NOT NULL,
+          `version` varchar(32) NOT NULL DEFAULT '' COMMENT '版本号',
+          `push_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '上报时间',
+          `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '所属用户',
+          `verb` varchar(32) NOT NULL DEFAULT '' COMMENT '用户行为',
+          `target_id` int(10) DEFAULT NULL COMMENT '目标Id',
+          `target_type` varchar(32) NOT NULL COMMENT '目标类型',
+          `status` varchar(16) NOT NULL DEFAULT 'created' COMMENT '状态: created, pushing, pushed',
+          `created_time` int(10) unsigned NOT NULL COMMENT '创建时间',
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `uuid` (`uuid`),
+          KEY `status` (`status`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
         ");
     }
 
