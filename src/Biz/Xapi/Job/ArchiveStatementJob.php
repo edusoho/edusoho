1bfb0ac4e2f44bd4ee23a5e58446ae6335d4fc1b
bellorchid <zhongyunchang@yeah.net>
2017-11-15 08:42:13 +0800

    修改错误

---
+++ src/Biz/Xapi/Job/ArchiveStatementJob.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Biz\Xapi\Job;
+
+use Biz\System\Service\SettingService;
+use Biz\Xapi\Dao\StatementDao;
+use Biz\Xapi\Service\XapiService;
+use Codeages\Biz\Framework\Scheduler\AbstractJob;
+use AppBundle\Common\ArrayToolkit;
+use Guzzle\Http\Client;
+use QiQiuYun\SDK\Auth;
+
+class PushStatementJob extends AbstractJob
+{
+    public function execute()
+    {
+        $statements = $this->getXapiService()->searchStatements(
+            array(
+                'status' => 'pushed',
+            ),
+            array('push_time' => 'ASC'),
+            0,
+            1000
+        );
+
+        $this->getStatementDao()->batchCreate($statements);
+        foreach ($statements as $statement) {
+            $this->getStatementDao()->delete($statement['id']);
+        }
+    }
+
+    /**
+     * @return SettingService
+     */
+    protected function getSettingService()
+    {
+        return $this->biz->service('System:SettingService');
+    }
+
+    /**
+     * @return XapiService
+     */
+    protected function getXapiService()
+    {
+        return $this->biz->service('Xapi:XapiService');
+    }
+
+    /**
+     * @return StatementDao
+     */
+    protected function getStatementDao()
+    {
+        return $this->biz->dao('Xapi:XapiDao');
+    }
+}
