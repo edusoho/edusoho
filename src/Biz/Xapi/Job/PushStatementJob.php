8c3800b735605e40aebec1bca5f060b963e3442c
bellorchid <zhongyunchang@yeah.net>
2017-11-15 15:07:47 +0800

    修改气球云XAPI默认地址

--- src/Biz/Xapi/Job/PushStatementJob.php
+++ src/Biz/Xapi/Job/PushStatementJob.php
@@ -48,14 +48,16 @@ class PushStatementJob extends AbstractJob
         $siteSettings = $this->getSettingService()->get('site', array());
         $xapiSetting = $this->getSettingService()->get('xapi', array());
 
+        $pushUrl = !empty($xapiSetting['push_url']) ? $xapiSetting['push_url'] : 'http://xapi.qiqiuyu.net/vi/';
+
         $siteName = empty($siteSettings['name']) ? '' : $siteSettings['name'];
         $siteUrl = empty($siteSettings['url']) ? '' : $siteSettings['url'];
         $accessKey = empty($settings['cloud_access_key']) ? '' : $settings['cloud_access_key'];
         $secretKey = empty($settings['cloud_secret_key']) ? '' : $settings['cloud_secret_key'];
-        $auth = new Auth('9DdikSDLhmObBhE0t3mhN9UUl8FW2Zdh', 'jNqSV44Fx5kxBFc4VI840pLk8D6QeO86');
+        $auth = new Auth($accessKey, $secretKey);
 
         return new \QiQiuYun\SDK\Service\XAPIService($auth, array(
-            'base_uri' => 'http://192.168.4.214:8769/v1/xapi/', //推送的URL需要配置
+            'base_uri' => $pushUrl,
             'school' => array(
                 'accessKey' => $accessKey,
                 'url' => $siteUrl,
