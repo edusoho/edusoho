f2276f8576557a29c133dcb9f65e2e23fdab6c27
chenwei <chenwei@howzhi.com>
2017-11-15 09:07:27 +0800

    新增，PushMessageEventSubscriber的单元测试

---
+++ tests/Unit/Notification/PushMessageEventSubscriberTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Unit\Notification;
+
+use Biz\BaseTestCase;
+use Codeages\Biz\Framework\Event\Event;
+use Biz\Notification\Event\PushMessageEventSubscriber;
+use Tests\Unit\Notification\Tool\MockedQueueServiceImpl;
+
+class PushMessageEventSubscriberTest extends BaseTestCase
+{
+    public function testOnUserFollow()
+    {
+        $this->mockBiz(
+            'System:SettingService',
+            array(
+                array(
+                    'functionName' => 'get',
+                    'returnValue' => array('enabled' => true),
+                    'withParams' => array('app_im', array())
+                ),
+            )
+        );
+
+        $this->mockBiz(
+            'User:UserService',
+            array(
+                array(
+                    'functionName' => 'getUser',
+                    'returnValue' => array('id' => 1, 'nickname' => '用户1'),
+                    'withParams' => array(1)
+                ),
+                array(
+                    'functionName' => 'getUser',
+                    'returnValue' => array('id' => 2, 'nickname' => '用户2'),
+                    'withParams' => array(2)
+                ),
+            )
+        );
+
+        $biz = $this->getBiz();
+        $biz['@Queue:QueueService'] = new MockedQueueServiceImpl();
+
+        $friendsInfo = array(
+            'fromId' => 1,
+            'toId' => 2
+        );
+        $event = new Event($friendsInfo);
+        $subscriber = new PushMessageEventSubscriber($this->biz);
+        $subscriber->onUserFollow($event);
+
+        $this->assertEquals(
+            array(
+                'from' => array(
+                    'id' => 1,
+                    'type' => 'user',
+                ),
+                'to' => array(
+                    'type' => 'user',
+                    'id' => 2,
+                    'convNo' => '',
+                ),
+                'body' => array(
+                    'type' => 'user.follow',
+                    'fromId' => 1,
+                    'toId' => 2,
+                    'title' => '收到一个用户关注',
+                    'message' => '用户1已经关注了你！',
+                )
+            ),
+            $this->getQueueService()->getJob()->getBody()
+        );
+    }
+
+    protected function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+    protected function getUserService()
+    {
+        return $this->createService('User:UserService');
+    }
+
+    protected function getQueueService()
+    {
+        return $this->createService('Queue:QueueService');
+    }
+}
