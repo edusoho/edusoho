1862d9b7271377cd9a34b27e2e31f967dffc91d7
maofangfei <maofangfei@howzhi.com>
2017-11-16 21:02:32 +0800

    适配多种登录

--- app/Resources/static-src/app/js/oauth2/login/index.js
+++ app/Resources/static-src/app/js/oauth2/login/index.js
@@ -2,17 +2,7 @@ import axis from 'common/axis';
 
 const $form = $('#third-party-login-form');
 const $btn = $('.js-submit-btn');
-let validator = $form.validate({
-  rules: {
-    'way': {
-      required: true,
-      email_or_mobile_check: true,
-    },
-  },
-  messages: {
-    required: Translator.trans('validate.phone_and_email_input.message')
-  },
-});
+
 
 $form.keypress(function (e) {
   if (e.which == 13) {
@@ -21,6 +11,36 @@ $form.keypress(function (e) {
   }
 });
 
+const validateLogin = (validateName, rule, message) => {
+  const $item = $('input[name='+ validateName +']');
+  if (!$item.length) {
+    return;
+  }
+  let validator = $form.validate({
+    rules: {
+      validateName: {
+        required: true,
+        rule: true,
+      },
+    },
+    messages: {
+      required: Translator.trans(message)
+    },
+  });
+  let isBind = bindCheck($item);
+  let isValidated = validator.form();
+
+  let type;
+
+  if (isValidated && isBind) {
+    type = 'bind';
+  } else if (isValidated && !isBind) {
+    type = 'create';
+  } else {
+    type = 'fail';
+  }
+  return type;
+}
 
 const bindCheck = ($dom) => {
   let url = $dom.data('url');
@@ -46,13 +66,21 @@ const bindCheck = ($dom) => {
 }
 
 
-const IsBind = bindCheck($('input[name="way"]'));
-
 $btn.click((event) => {
-  if (validator.form() && !IsBind) {
-    $form.submit();
-    window.location.href = $btn.data('url');
-  } else if (validator.form() && IsBind) {
-    window.location.href = $btn.data('create-url');
-  }
-});
+  let isMobileOrEmail = validateLogin('mobileOrEmail', 'email_or_mobile_check', 'validate.phone_and_email_input.message');
+
+  let isMobile = validateLogin('mobile', 'mobile', 'validate.phone.message');
+
+  let isEmail = validateLogin('email', 'email', 'validate.valid_email_input.message');
+
+  console.log(isEmail);
+
+})
+// $btn.click((event) => {
+//   if (validator.form() && !IsBind) {
+//     $form.submit();
+//     window.location.href = $btn.data('url');
+//   } else if (validator.form() && IsBind) {
+//     window.location.href = $btn.data('create-url');
+//   }
+// });
