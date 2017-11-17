0d7882700af0a19ec664baf4a2cb01103648155b
maofangfei <maofangfei@howzhi.com>
2017-11-17 11:53:46 +0800

    绑定

--- app/Resources/static-src/app/js/oauth2/bind/index.js
+++ app/Resources/static-src/app/js/oauth2/bind/index.js
@@ -2,22 +2,11 @@ const $form = $('#third-party-bind-form');
 const $btn = $('.js-submit-btn');
 
 let validator = $form.validate({
-  currentDom: $btn,
-  ajax: true,
   rules: {
     password: {
       required: true,
-      es_remote: {
-        type: 'post'
-      },
     },
   },
-  submitSuccess(data) {
-    notify('success', Translator.trans(data.message));
-  },
-  submitError(data) {
-    notify('danger',  Translator.trans(data.responseJSON.message));
-  }
 });
 
 
@@ -31,6 +20,8 @@ $form.keypress(function (e) {
 
 $btn.click((event) => {
   if (validator.form()) {
-    window.location.href = $btn.data('url');
+    $.post($form.attr('url'), (response) => {
+      console.log(response);
+    })
   }
 });
