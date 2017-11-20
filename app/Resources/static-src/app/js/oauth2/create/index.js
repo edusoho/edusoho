ab184ea101457acf30f3da4692abd348e182479b
maofangfei <maofangfei@howzhi.com>
2017-11-20 15:34:59 +0800

    添加图形验证码规则

--- app/Resources/static-src/app/js/oauth2/create/index.js
+++ app/Resources/static-src/app/js/oauth2/create/index.js
@@ -1,8 +1,10 @@
 import SmsSender from 'app/common/widget/sms-sender';
 import { enterSubmit } from 'common/utils';
+import Api from 'common/api';
 
 const $form = $('#third-party-create-account-form');
 const $btn = $('.js-submit-btn');
+let captchaToken = null;
 
 let validator = $form.validate({
   rules: {
@@ -28,11 +30,11 @@ let validator = $form.validate({
     sms_code: {
       required: true,
       unsigned_integer: true,
-      es_remote: true,
     },
     captcha_code: {
       required: true,
       alphanumeric: true,
+      captcha_checkout: true,
     }
   },
   messages: {
@@ -41,6 +43,44 @@ let validator = $form.validate({
     }
   }
 });
+
+$.validator.addMethod('captcha_checkout', function(value, element, param) {
+  let $element = $(element);
+  let data = param.data ? param.data : { phrase: value };
+  let isSuccess = 0;
+  let params = {
+    captchaToken: captchaToken
+  }
+  Api.captcha.validate({ data: data, params: params, async: false, promise: false }).done(res => {
+    isSuccess = res.status;
+    if (!isSuccess) {
+      $.validator.messages.captcha_checkout = Translator.trans('图形验证码错误');
+      initCaptchaCode();
+    }
+  }).error(res => {
+    console.log(res);
+  });
+  return this.optional(element) || isSuccess;
+}, Translator.trans('validate.captcha_checkout.message'));
+
+
+const initCaptchaCode = () => {
+  Api.captcha.get({ async: false, promise: false }).done(res => {
+    $('#getcode_num').attr('src', res.image);
+    captchaToken = res.captchaToken;
+  }).error(res => {
+    console.log('catch', res.responseJSON.error.message);
+  });
+}
+
+window.onload = () => {
+ initCaptchaCode();
+}
+
+$('#getcode_num').click((event) => {
+  initCaptchaCode();
+})
+
 
 enterSubmit($form, $btn);
 
