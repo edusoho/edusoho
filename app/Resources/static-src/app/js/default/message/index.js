c17af46db0d4ea85164914ac748d836fb0e58ec6
maofangfei <maofangfei@howzhi.com>
2017-11-22 10:01:52 +0800

    修改引用js文件

--- app/Resources/static-src/app/js/default/message/index.js
+++ app/Resources/static-src/app/js/default/message/index.js
@@ -1,21 +1,9 @@
 let $message = $("#page-message-container");
 let gotoUrl = $message.data('goto');
 let duration = $message.data('duration');
-let os = $message.data('os');
-let token = $message.data('token');
-
-if ('iOS' === os) {
-  window.webkit.messageHandlers.login.postMessage(token);
-} else if ('Android' === os) {
-  window.android.login(token);
-} else {
-  if (duration > 0 && gotoUrl) {
+if (duration > 0 && gotoUrl) {
     setTimeout(function () {
       window.location.href = gotoUrl;
     }, duration);
   }
 }
-
-
-
-
