c2f6dddf38c7280659cac436e46a4d6dadcb7706
chenwei <chenwei@howzhi.com>
2017-11-24 20:39:35 +0800

    新增，单元测试

--- tests/Unit/Notification/PushMessageEventSubscriberTest.php
+++ tests/Unit/Notification/PushMessageEventSubscriberTest.php
@@ -9,19 +9,58 @@ use Tests\Unit\Notification\Tool\MockedQueueServiceImpl;
 
 class PushMessageEventSubscriberTest extends BaseTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->enableIm();
+    }
+
+    public function testOnArticleCreateWithCloudSearchOn()
+    {
+        list($subscriber, $event) = $this->createArticleTestData(true);
+        $subscriber->onArticleCreate($event);
+
+        $this->assertArrayEquals(
+            array(
+                'type' => 'update',
+                'args' => array(
+                    'category' => 'article'
+                )
+            ),
+            $this->getQueueService()->getJob()->getBody()
+        );
+    }
+
+    public function testOnArticleCreateWithCloudSearchOff()
+    {
+        list($subscriber, $event) = $this->createArticleTestData(false);
+        $subscriber->onArticleCreate($event);
+
+        $this->assertArrayEquals(
+            array(
+                'from' => array(
+                    'id' => 2,
+                    'type' => 'news',
+                ),
+                'to' => array(
+                    'type' => 'global',
+                    'convNo' => '',
+                ),
+                'body' => array(
+                    'type' => 'news.create',
+                    'id' => 123,
+                    'title' => 'article title',
+                    'image' => 'http://test.com/files/thumb.png',
+                    'content' => "article title",
+                    'message' => "article title",
+                ),
+            ),
+            $this->getQueueService()->getJob()->getBody()
+        );
+    }
+
     public function testOnUserFollow()
     {
-        $this->mockBiz(
-            'System:SettingService',
-            array(
-                array(
-                    'functionName' => 'get',
-                    'returnValue' => array('enabled' => true),
-                    'withParams' => array('app_im', array()),
-                ),
-            )
-        );
-
         $this->mockBiz(
             'User:UserService',
             array(
@@ -38,15 +77,13 @@ class PushMessageEventSubscriberTest extends BaseTestCase
             )
         );
 
-        $biz = $this->getBiz();
-        $biz['@Queue:QueueService'] = new MockedQueueServiceImpl();
+        $subscriber = $this->getEventSubscriberWithMockedQueue();
 
         $friendsInfo = array(
             'fromId' => 1,
             'toId' => 2,
         );
         $event = new Event($friendsInfo);
-        $subscriber = new PushMessageEventSubscriber($this->biz);
         $subscriber->onUserFollow($event);
 
         $this->assertEquals(
@@ -85,5 +122,68 @@ class PushMessageEventSubscriberTest extends BaseTestCase
     protected function getQueueService()
     {
         return $this->createService('Queue:QueueService');
+    }
+
+    private function createArticleTestData($isCloudSearchOn)
+    {
+        $this->mockBiz(
+            'System:SettingService',
+            array(
+                array(
+                    'functionName' => 'get',
+                    'returnValue' => array('about' => 'aboutSchool', 'logo' => 'logo.png'),
+                    'withParams' => array('mobile'),
+                ),
+                array(
+                    'functionName' => 'get',
+                    'returnValue' => array('slogan' => 'slogan'),
+                    'withParams' => array('site'),
+                ),
+                array(
+                    'functionName' => 'get',
+                    'returnValue' => array('enabled' => true),
+                    'withParams' => array('app_im', array()),
+                ),
+                array(
+                    'functionName' => 'get',
+                    'returnValue' => array('search_enabled' => $isCloudSearchOn),
+                    'withParams' => array('cloud_search', array()),
+                ),
+            )
+        );
+
+        $subscriber = $this->getEventSubscriberWithMockedQueue();
+
+        $article = array(
+            'thumb' => 'thumb.png',
+            'originalThumb' => 'originalThumb.png',
+            'picture' => 'picture.png',
+            'title' => 'article title',
+            'id' => 123,
+        );
+        $event = new Event($article);
+        
+        return array($subscriber, $event);
+    }
+
+    private function enableIm()
+    {
+        $this->mockBiz(
+            'System:SettingService',
+            array(
+                array(
+                    'functionName' => 'get',
+                    'returnValue' => array('enabled' => true),
+                    'withParams' => array('app_im', array()),
+                ),
+            )
+        );
+    }
+
+    private function getEventSubscriberWithMockedQueue()
+    {
+        $biz = $this->getBiz();
+        $biz['@Queue:QueueService'] = new MockedQueueServiceImpl();
+        return new PushMessageEventSubscriber($this->biz);
     }
 }
