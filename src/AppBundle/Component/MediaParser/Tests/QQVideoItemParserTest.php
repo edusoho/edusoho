26a6301f21b388c76b718fbaddca4054e74fa78c
chenwei <chenwei@howzhi.com>
2017-11-25 21:28:35 +0800

    修复，qq视频单元测试偶现报错的问题

--- src/AppBundle/Component/MediaParser/Tests/QQVideoItemParserTest.php
+++ src/AppBundle/Component/MediaParser/Tests/QQVideoItemParserTest.php
@@ -4,46 +4,48 @@ namespace AppBundle\Component\MediaParser\Tests;
 
 use Biz\BaseTestCase;
 use AppBundle\Component\MediaParser\ItemParser\QQVideoItemParser;
+use AppBundle\Component\MediaParser\ParseException;
 
 class QQVideoItemParserTest extends BaseTestCase
 {
     public function testParse()
     {
-        $video = $this->createParser()->parse('http://v.qq.com/cover/0/0i17jjqo3piy5h8.html?vid=l0011p22hob');
-        $this->assertEquals('video', $video['type']);
-        $this->assertEquals('qqvideo', $video['source']);
+        $video = $this->parseQqItem('http://v.qq.com/cover/0/0i17jjqo3piy5h8.html?vid=l0011p22hob');
         $this->assertEquals('qqvideo:l0011p22hob', $video['uuid']);
-        $this->assertArrayHasKey('name', $video);
-        $this->assertArrayHasKey('page', $video);
-        $this->assertArrayHasKey('files', $video);
 
-        $video = $this->createParser()->parse('http://v.qq.com/x/page/h0150zpvfq5.html');
-        $this->assertEquals('video', $video['type']);
-        $this->assertEquals('qqvideo', $video['source']);
+        $video = $this->parseQqItem('http://v.qq.com/x/page/h0150zpvfq5.html');
         $this->assertEquals('qqvideo:h0150zpvfq5', $video['uuid']);
-        $this->assertArrayHasKey('name', $video);
-        $this->assertArrayHasKey('page', $video);
-        $this->assertArrayHasKey('files', $video);
 
-        $video = $this->createParser()->parse('http://v.qq.com/x/page/w0168yk7k1e.html');
-        $this->assertEquals('video', $video['type']);
-        $this->assertEquals('qqvideo', $video['source']);
+        $video = $this->parseQqItem('http://v.qq.com/x/page/w0168yk7k1e.html');
         $this->assertEquals('qqvideo:w0168yk7k1e', $video['uuid']);
-        $this->assertArrayHasKey('name', $video);
-        $this->assertArrayHasKey('page', $video);
-        $this->assertArrayHasKey('files', $video);
 
-        $video = $this->createParser()->parse('http://v.qq.com/x/cover/32kshc0e1wcifxj/n0329caqnyf.html');
-        $this->assertEquals('video', $video['type']);
-        $this->assertEquals('qqvideo', $video['source']);
+        $video = $this->parseQqItem('http://v.qq.com/x/cover/32kshc0e1wcifxj/n0329caqnyf.html');
         $this->assertEquals('qqvideo:n0329caqnyf', $video['uuid']);
-        $this->assertArrayHasKey('name', $video);
-        $this->assertArrayHasKey('page', $video);
-        $this->assertArrayHasKey('files', $video);
     }
 
     private function createParser()
     {
         return new QQVideoItemParser();
+    }
+
+    private function parseQqItem($url)
+    {
+        $video = $this->parseAgainIfFailed($url);
+        $this->assertEquals('video', $video['type']);
+        $this->assertEquals('qqvideo', $video['source']);
+        $this->assertArrayHasKey('name', $video);
+        $this->assertArrayHasKey('page', $video);
+        $this->assertArrayHasKey('files', $video);
+
+        return $video;
+    }
+
+    private function parseAgainIfFailed($url)
+    {
+        try {
+            return $this->createParser()->parse($url);
+        } catch (ParseException $e) {
+            return $this->createParser()->parse($url);
+        }
     }
 }
