64543e824d15382e7b29790a22e91052b98175f7
tianshengjie <tianshengjie@howzhi.com>
2017-11-26 22:14:30 +0800

    增加 cashflowdao 接口”

--- vendor/codeages/biz-order-pay/src/Pay/Dao/Impl/CashflowDaoImpl.php
+++ vendor/codeages/biz-order-pay/src/Pay/Dao/Impl/CashflowDaoImpl.php
@@ -38,6 +38,15 @@ class CashflowDaoImpl extends GeneralDaoImpl implements CashflowDao
         return $builder->execute()->fetchColumn(0);
     }
 
+    public function sumAmountGroupByUserId($conditions)
+    {
+        $builder = $this->createQueryBuilder($conditions)
+            ->select("sum(amount) as amount, user_id")
+            ->groupBy('user_id');
+
+        return $builder->execute()->fetchAll();
+    }
+
     private function sumColumnWhiteList()
     {
         return array('amount');
@@ -67,6 +76,7 @@ class CashflowDaoImpl extends GeneralDaoImpl implements CashflowDao
                 'user_id != :except_user_id',
                 'user_id = :user_id',
                 'buyer_id = :buyer_id',
+                'user_id IN (:user_ids)',
                 'type = :type',
                 'title LIKE :title_like',
                 'amount > :amount_GT',
