cb630e83856fb99687f716ca48170a11841c55c4
zhongyunchang <zhongyunchang@yeah.net>
2017-11-30 16:54:28 +0800

    修改Exercise业务错误

---
+++ migrations/20171130104921_testpaper_result_v8_add_metas.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class TestpaperResultV8AddMetas extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        $db->exec("ALTER TABLE `testpaper_result_v8` ADD COLUMN `metas` text COMMENT '练习的题型排序等附属信息' AFTER `updateTime`; ");
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        $db->exec("ALTER TABLE `testpaper_result_v8` DROP COLUMN `metas`;");
+    }
+}
