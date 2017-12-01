f7ad5b9eac16bbf47bfc6e443c2f243ee0082a9f
zhongyunchang <zhongyunchang@yeah.net>
2017-12-01 09:15:05 +0800

    修改函数错误

--- tests/Unit/Course/Copy/CourseMemberCopyTest.php
+++ tests/Unit/Course/Copy/CourseMemberCopyTest.php
@@ -3,12 +3,53 @@
 namespace Tests\Unit\Course\Copy;
 
 use Biz\BaseTestCase;
+use Biz\Course\Copy\CourseMemberCopy;
 
 class CourseMemberCopyTest extends BaseTestCase
 {
     public function testPreCopy()
     {
+        $copy = new CourseMemberCopy($this->biz, array(
+            'class' => 'Biz\Course\Copy\CourseMemberCopy',
+            'priority' => 100,
+        ),false);
 
+        $this->assertNull($copy->preCopy($this->mockCourse(), array()));
+    }
+
+    public function testDoCopy()
+    {
+        $courseMembers = array(
+            array(
+                'courseId' => 1,
+                'courseSetId' => 1,
+                'userId' => 1,
+                'role' => 'teacher',
+                'seq' => 1,
+                'isVisible' => 1,
+                'createdTime' => time(),
+            )
+        );
+        $this->mockBiz('Course:CourseMemberDao', array(
+            array('functionName' => 'findByCourseIdAndRole', 'returnValue' => $courseMembers)
+        ));
+
+        $this->
+
+        $copy = new CourseMemberCopy($this->biz, array(
+            'class' => 'Biz\Course\Copy\CourseMemberCopy',
+            'priority' => 100,
+        ),false);
+    }
+
+    public function testAfterCopy()
+    {
+        $copy = new CourseMemberCopy($this->biz, array(
+            'class' => 'Biz\Course\Copy\CourseMemberCopy',
+            'priority' => 100,
+        ),false);
+
+        $this->assertNull($copy->afterCopy($this->mockCourse(), array()));
     }
 
     protected function mockCopyCourses()
@@ -16,11 +57,11 @@ class CourseMemberCopyTest extends BaseTestCase
     
     }
 
-    protected function mockCourse($title, $courseSet)
+    protected function mockCourse($title = '测试课程', $courseSet = array())
     {
         return array(
             'title' => $title,
-            'courseSetId' => $courseSet['id'],
+            'courseSetId' => empty($courseSet) ? 1 : $courseSet['id'],
             'expiryMode' => 'forever',
             'learnMode' => 'freeMode',
             'courseType' => 'normal',
