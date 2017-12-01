76ea0efbdaac24708d6e544cc464c57eb1522496
ruanwenqin <ruanwenqin@howzhi.com>
2017-12-01 13:52:30 +0800

    邮件模版单元测试

---
+++ tests/Unit/Email/EmailSystemSelfTestTemplateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\CloudFile;
+
+use Biz\BaseTestCase;
+use Biz\Mail\Template\EmailResetPasswordTemplate;
+use Biz\Mail\Template\EmailSystemSelfTestTemplate;
+
+class EmailSystemSelfTestTemplateTest extends BaseTestCase
+{
+    public function testParse()
+    {
+        $template = new EmailSystemSelfTestTemplate();
+        $template->setBiz($this->getBiz());
+        $result = $template->parse(array(
+            'params' => array('nickname' => 'test')
+        ));
+
+        $this->assertArrayHasKey('title', $result);
+        $this->assertArrayHasKey('body', $result);
+    }
+}
