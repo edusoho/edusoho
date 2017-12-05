6fff514338ddf846fbdbdedc36b8105e07f611bd
wangdanyue <wangdanyue@howzhi.com>
2017-12-05 20:21:44 +0800

    add CouponDeduct unit test

---
+++ tests/Unit/OrderFacade/CouponCheckCommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit\OrderFacade;
+
+use Biz\BaseTestCase;
+use Biz\OrderFacade\Command\OrderPayCheck\CouponCheckCommand;
+
+class CouponCheckCommandTest extends BaseTestCase
+{
+    public function testExecute()
+    {
+        $command = new CouponCheckCommand();
+        $command->setBiz($this->getBiz());
+
+        $this->mockBiz('Order:OrderService', array(
+            array(
+                'functionName' => 'findOrderItemDeductsByItemId',
+                'returnValue' => array(array('id'=>1,'deduct_type' => 'coupon','deduct_id' => 2))
+            )
+        ));
+
+        $this->mockBiz('Coupon:CouponService', array(
+            array(
+                'functionName' => 'getCoupon',
+                'returnValue' => array('status' => 'using')
+            )
+        ));
+
+        $result = $command->execute(array('id' => 1), array());
+
+        $this->assertNull($result);
+    }
+
+    /**
+     * @expectedException Biz\OrderFacade\Exception\OrderPayCheckException
+     * @expectedExceptionMessage order.pay_check_msg.coupon_had_been_used
+     */
+    public function testExecuteCoinAmountNegative()
+    {
+        $command = new CouponCheckCommand();
+        $command->setBiz($this->getBiz());
+
+        $this->mockBiz('Order:OrderService', array(
+            array(
+                'functionName' => 'findOrderItemDeductsByItemId',
+                'returnValue' => array(array('id'=>1,'deduct_type' => 'coupon','deduct_id' => 2))
+            )
+        ));
+
+        $this->mockBiz('Coupon:CouponService', array(
+            array(
+                'functionName' => 'getCoupon',
+                'returnValue' => array('status' => 'unuse')
+            )
+        ));
+
+        $result = $command->execute(array('id' => 1), array());
+    }
+}
---
+++ tests/Unit/OrderFacade/Deduct/CouponDeductTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit\OrderFacade\Deduct;
+
+use Biz\BaseTestCase;
+use Biz\OrderFacade\Deduct\CouponDeduct;
+use Codeages\Biz\Order\Status\OrderStatusCallback;
+
+class CouponDeductTest extends BaseTestCase
+{
+    public function testOnCreated()
+    {
+        $coupon = $this->getCouponDao()->create(array(
+            'code' => 'Test123456',
+            'type' => 'minus',
+            'status' => 'receive',
+            'rate' => 10,
+            'deadline' => time(),
+        ));
+
+        $deduct = new CouponDeduct();
+        $deduct->setBiz($this->getBiz());
+
+        $deduct->onCreated(array('deduct_id' => $coupon['id']));
+
+        $result = $this->getCouponService()->getCoupon($coupon['id']);
+
+        $this->assertEquals('using', $result['status']);
+    }
+
+    public function testOnClosed()
+    {
+        $coupon = $this->_createCoupon();
+
+        $deduct = new CouponDeduct();
+        $deduct->setBiz($this->getBiz());
+
+        $deduct->onClosed(array('deduct_id' => $coupon['id']));
+        $result = $this->getCouponService()->getCoupon($coupon['id']);
+
+        $this->assertEquals('receive', $result['status']);
+    }
+
+    public function testOnPaid()
+    {
+        $coupon = $this->_createCoupon();
+
+        $deduct = new CouponDeduct();
+        $deduct->setBiz($this->getBiz());
+
+        $user = $this->getCurrentUser();
+
+        $result = $deduct->onPaid(array('deduct_id' => $coupon['id'],'user_id' => $user['id'],'order_id' => 1,'item' => array('target_type'=>'course','target_id' => 1)));
+
+        $this->assertEquals(OrderStatusCallback::SUCCESS, $result);
+    }
+
+    private function _createCoupon()
+    {
+        return $this->getCouponDao()->create(array(
+            'code' => 'Test123456',
+            'type' => 'minus',
+            'status' => 'using',
+            'rate' => 10,
+            'deadline' => time(),
+        ));
+    }
+
+    protected function getCouponService()
+    {
+        return $this->biz->service('Coupon:CouponService');
+    }
+
+    private function getCouponDao()
+    {
+        return $this->createDao('Coupon:CouponDao');
+    }
+}
