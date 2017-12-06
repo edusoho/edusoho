c13c0f4f9a7f1bdec7b8820a0c8041674e0ac9c4
chenwei <chenwei@howzhi.com>
2017-12-06 17:46:48 +0800

    修复编译失败的问题

--- migrations/20171205150037_update_lian_lian_pay_time_and_refund_deadline.php
+++ migrations/20171205150037_update_lian_lian_pay_time_and_refund_deadline.php
@@ -11,7 +11,7 @@ class UpdateLianLianPayTimeAndRefundDeadline extends Migration
     {
         $container = $this->getContainer();
         $db = $container['db'];
-        // 以前的连连支付的 pay_time = 8位日期， 如 20171102, refund_deadline = pay_time + expired_refund_days * 86400 
+        // 以前的连连支付的 pay_time = 8位日期， 如 20171102, refund_deadline = pay_time + expired_refund_days * 86400
         $db->exec("update biz_order set refund_deadline = expired_refund_days * 86400 + updated_time, pay_time = updated_time where payment = 'lianlianpay' and length(pay_time) = 8;");
     }
 }
