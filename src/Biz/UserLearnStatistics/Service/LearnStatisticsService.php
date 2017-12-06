642fa27bebbb567504785e04ed4787b2da2e218c
tianshengjie <tianshengjie@howzhi.com>
2017-12-06 11:53:49 +0800

    修改LearnStatisticsService

--- src/Biz/UserLearnStatistics/Service/LearnStatisticsService.php
+++ src/Biz/UserLearnStatistics/Service/LearnStatisticsService.php
@@ -29,4 +29,6 @@ interface LearnStatisticsService
     public function storageDailyStatistics($limit = 1000);
 
     public function getStatisticsSetting();
+
+    public function setStatisticsSetting();
 }
