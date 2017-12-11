d639c84e15c47af63ee71a23509a8e3ea15c5303
bellorchid <zhongyunchang@yeah.net>
2017-12-11 08:02:42 +0800

    修改单元测试

--- tests/Unit/Activity/Type/DiscussTest.php
+++ tests/Unit/Activity/Type/DiscussTest.php
@@ -2,7 +2,17 @@
 
 namespace Tests\Unit\Activity\Type;
 
+use AppBundle\Common\ReflectionUtils;
+
 class DiscussTest extends BaseTypeTestCase
 {
+    const TYPE = 'discuss';
 
+    public function testRegisterListeners()
+    {
+        $type = $this->getActivityConfig(self::TYPE);
+        $return = ReflectionUtils::invokeMethod($type, 'registerListeners');
+
+        $this->assertEquals(array(), $return);
+    }
 }
