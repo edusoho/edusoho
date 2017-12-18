769485b8f77594424be898378cf98d9fae8628cb
yangshuai <yangshuai@howzhi.com>
2017-12-18 17:19:54 +0800

    login entry

--- src/AppBundle/Controller/Admin/MarketingController.php
+++ src/AppBundle/Controller/Admin/MarketingController.php
@@ -16,6 +16,7 @@ class MarketingController extends BaseController
         $client = $this->createMarketingClient();
 
         $siteInfo = $this->getSiteInfo();
+        $entry =  $request->query->get('entry');
 
         try {
             $login = $client->post('/login', array(
@@ -24,8 +25,8 @@ class MarketingController extends BaseController
                 'user_id' => $user['id'],
                 'user_name' => $user['nickname'],
                 'user_avatar' => $this->getWebExtension()->getFurl($user['largeAvatar'], 'avatar.png'),
+                'entry' => $entry
             ));
-
             return  $this->redirect($login['url']);
         } catch (\Exception $e) {
             return $this->createMessageResponse('error', $e->getMessage());
