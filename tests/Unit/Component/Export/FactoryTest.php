8bc17a6ce5ed53d4deb3926e04fe3a21b23fd7f8
tianshengjie <tianshengjie@howzhi.com>
2017-12-18 11:57:55 +0800

    add tests/Unit/Component/Export/FactoryTest.php

--- tests/Unit/Component/Export/FactoryTest.php
+++ tests/Unit/Component/Export/FactoryTest.php
@@ -8,23 +8,33 @@ use AppBundle\Common\ReflectionUtils;
 
 class FactoryTest extends BaseTestCase
 {
+    private  $map = array(
+        'invite-records',
+        'user-invite-records',
+        'order',
+        'course-overview-student-list',
+        'course-overview-task-list',
+        'course-overview-normal-task-detail',
+        'course-overview-testpaper-task-detail',
+        'bill-cash-flow',
+        'bill-coin-flow',
+        'user-learn-statistics',
+    );
+
     public function testCreate()
     {
         $container = self::$appKernel->getContainer();
         $factory = new Factory($container);
+    
+        $this->assertNotEmpty($factory->create('invite-records'));
+    }
 
-        $map = array(
-            'invite-records',
-            'user-invite-records',
-            'order',
-            'course-overview-student-list',
-            'course-overview-task-list',
-            'course-overview-normal-task-detail',
-            'course-overview-testpaper-task-detail',
-            'bill-cash-flow',
-            'bill-coin-flow',
-            'user-learn-statistics',
-        );
+    public function testFactoryMap()
+    {
+        $container = self::$appKernel->getContainer();
+        $factory = new Factory($container);
+
+        $map = $this->map;
 
         foreach ($map as $value) {
             $result = ReflectionUtils::invokeMethod($factory, 'exportMap', array($value));
@@ -48,6 +58,8 @@ class FactoryTest extends BaseTestCase
         $container->set('faq_export_map', new ExportMap());
         ReflectionUtils::invokeMethod($factory, 'exportMap', array($faqPluginExport));
     }
+
+    
 }
 
 class ExportMap
