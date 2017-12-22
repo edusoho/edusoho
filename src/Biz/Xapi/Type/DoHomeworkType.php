2c4e1e73f662242609f81d8c6c3e687a96b0a58a
zhongyunchang <zhongyunchang@yeah.net>
2017-12-22 15:50:25 +0800

    修改试卷和作业

--- src/Biz/Xapi/Type/DoHomeworkType.php
+++ src/Biz/Xapi/Type/DoHomeworkType.php
@@ -2,6 +2,8 @@
 
 namespace Biz\Xapi\Type;
 
+use AppBundle\Common\ArrayToolkit;
+
 class DoHomeworkType extends Type
 {
     const TYPE = 'completed_homework';
@@ -32,6 +34,50 @@ class DoHomeworkType extends Type
 
     public function packages($statements)
     {
-        // TODO: Implement packages() method.
+        $homeworkResultIds = ArrayToolkit::column($statements, 'target_id');
+        $homeworkResults = $this->getTestpaperService()->findTestpaperResultsByIds($homeworkResultIds);
+        $homeworkResults = ArrayToolkit::index($homeworkResults, 'id');
+
+        $courseIds = ArrayToolkit::column($homeworkResults, 'courseId');
+        $courses = $this->getCourseService()->findCoursesByIds($courseIds);
+        $courses = ArrayToolkit::index($courses, 'id');
+
+        $courseSetIds = ArrayToolkit::column($homeworkResults, 'courseSetId');
+        $courseSets = $this->getCourseSetService()->findCourseSetsByIds($courseSetIds);
+        $courseSets = ArrayToolkit::index($courseSets, 'id');
+
+        foreach ($courses as &$course) {
+            $course['description'] = empty($courseSet['subtitle']) ? '' : $courseSet['subtitle'];
+            $course['title'] = $courseSet['title'].'-'.$course['title'];
+            if (!empty($courseSets[$course['courseSetId']])) {
+                $courseSet = $courseSets[$course['courseSetId']];
+                $course['description'] = empty($courseSet['subtitle']) ? '' : $courseSet['subtitle'];
+                $course['title'] = $courseSet['title'].'-'.$course['title'];
+            }
+        }
+
+        $sdk = $this->createXAPIService();
+        $pushStatements = array();
+        foreach ($statements as $statement) {
+            $homeworkResult = $homeworkResults[$statement['target_id']];
+            $course = $courses[$homeworkResult['courseId']];
+            $object = array(
+                'id' => $homeworkResult['id'],
+                'name' => $homeworkResult['paperName'],
+                'course' => $course,
+            );
+
+            $actor = $this->getActor($statement['user_id']);
+            $result = array();
+            if ('none' != $homeworkResult['passedStatus']) {
+                $result['success'] = ('passed' == $homeworkResult['passedStatus']) ? true : false;
+            }
+
+            $pushStatements[] = $sdk->finishHomework($actor, $object, $result, $statement['uuid'], $statement['occur_time'], false);
+        }
+
+        return $pushStatements;
     }
+
+
 }
