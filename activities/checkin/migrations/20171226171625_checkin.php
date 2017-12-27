0941d92170a2bea2c7c0422cb7e8954d74e2fba7
ruanwenqin <ruanwenqin@howzhi.com>
2017-12-27 09:37:17 +0800

    change

---
+++ activities/checkin/migrations/20171226171625_checkin.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class Checkin extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
