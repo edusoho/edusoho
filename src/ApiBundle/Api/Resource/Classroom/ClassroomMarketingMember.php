ab1949061ea08700732b1a2a256d84674d338aae
bellorchid <zhongyunchang@yeah.net>
2018-01-04 15:46:53 +0800

    修改授权

--- src/ApiBundle/Api/Resource/Classroom/ClassroomMarketingMember.php
+++ src/ApiBundle/Api/Resource/Classroom/ClassroomMarketingMember.php
@@ -2,12 +2,20 @@
 
 namespace ApiBundle\Api\Resource\Classroom;
 
+use ApiBundle\Api\Annotation\Access;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use Biz\Marketing\Service\MarketingService;
 
 class ClassroomMarketingMember extends AbstractResource
 {
+    /**
+     * @param ApiRequest $request
+     * @param $classroomId
+     *
+     * @return array
+     * @Access(roles="ROLE_ADMIN,ROLE_SUPER_ADMIN")
+     */
     public function add(ApiRequest $request, $classroomId)
     {
         $biz = $this->getBiz();
@@ -19,6 +27,7 @@ class ClassroomMarketingMember extends AbstractResource
             return $this->getMarketingService()->addUserToClassroom($postData);
         } catch (\Exception $e) {
             $logger->error($e);
+
             return array('code' => 'error', 'msg' => 'ES处理微营销订单失败,'.$e->getTraceAsString());
         }
     }
