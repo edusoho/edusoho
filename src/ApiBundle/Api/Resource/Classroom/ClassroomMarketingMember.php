c3efdf897033f9bd51da9839ed26635b3c3f1cfb
bellorchid <zhongyunchang@yeah.net>
2018-01-04 16:04:13 +0800

    修改权限

--- src/ApiBundle/Api/Resource/Classroom/ClassroomMarketingMember.php
+++ src/ApiBundle/Api/Resource/Classroom/ClassroomMarketingMember.php
@@ -3,9 +3,12 @@
 namespace ApiBundle\Api\Resource\Classroom;
 
 use ApiBundle\Api\Annotation\Access;
+use ApiBundle\Api\Exception\ErrorCode;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Classroom\Service\ClassroomService;
 use Biz\Marketing\Service\MarketingService;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ClassroomMarketingMember extends AbstractResource
 {
@@ -18,6 +21,10 @@ class ClassroomMarketingMember extends AbstractResource
      */
     public function add(ApiRequest $request, $classroomId)
     {
+        $classroom = $this->getClassroomService()->getClassroom($classroomId);
+        if (empty($classroom)) {
+            throw new NotFoundHttpException('班级不存在', null, ErrorCode::RESOURCE_NOT_FOUND);
+        }
         $biz = $this->getBiz();
         $logger = $biz['logger'];
         $logger->debug('微营销通知处理订单');
@@ -38,5 +45,13 @@ class ClassroomMarketingMember extends AbstractResource
     protected function getMarketingService()
     {
         return $this->service('Marketing:MarketingService');
+    }
+
+    /**
+     * @return ClassroomService
+     */
+    protected function getClassroomService()
+    {
+        return $this->service('Classroom:ClassroomService');
     }
 }
