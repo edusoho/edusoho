64227bec3cb18a32795a19f449922abdb6aae60b
zhongyunchang <zhongyunchang@yeah.net>
2018-01-09 13:50:08 +0800

    修改函数错误

---
+++ tests/Unit/Testpaper/Dao/TestpaperItemResultDaoTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Testpapeer\Dao;
+
+use Tests\Unit\Base\BaseDaoTestCase;
+
+class TestpaperItemResultDaoTest extends BaseDaoTestCase
+{
+
+    public function getDefaultMockFields()
+    {
+        return array(
+
+            ''
+        );
+    }
+}
