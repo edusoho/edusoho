77ca1bdf26c0d796181fbce55e14fcf6de32ba15
bellorchid <zhongyunchang@yeah.net>
2018-01-10 21:04:09 +0800

    修改函数

--- app/Resources/static-src/app/js/pay/wxpay-app-result/index.js
+++ app/Resources/static-src/app/js/pay/wxpay-app-result/index.js
@@ -3,7 +3,6 @@ import UAParser from 'ua-parser-js';
 let $uapraser = new UAParser(navigator.userAgent);
 let browser = $uapraser.getBrowser();
 let os = $uapraser.getOS();
-alert("kuozhi://" + $('#jsWebViewPayResult').data('goto'));
 if (os.name === 'iOS' && browser.name === '[Mobile] Safari') {
-  window.location="kuozhi://" + $('#jsWebViewPayResult').data('goto');
+  $("#openApp").trigger("click");
 }
