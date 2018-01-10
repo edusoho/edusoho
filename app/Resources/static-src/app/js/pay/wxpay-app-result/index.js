5c0468020e4f70684130ec6d7425c2aaa8486181
bellorchid <zhongyunchang@yeah.net>
2018-01-10 21:59:04 +0800

    修改错误

--- app/Resources/static-src/app/js/pay/wxpay-app-result/index.js
+++ app/Resources/static-src/app/js/pay/wxpay-app-result/index.js
@@ -4,8 +4,8 @@ let $uapraser = new UAParser(navigator.userAgent);
 let browser = $uapraser.getBrowser();
 let os = $uapraser.getOS();
 
-alert(os.name + browser.name);
-
+$('#openApp').html(os.name + browser.name);
 if (os.name === 'iOS' && browser.name === '[Mobile] Safari') {
+  $('#openApp').html(os.name + browser.name);
   document.getElementById("openApp").click();
 }
