f57f6ac416945ac416ec92e45c2ac5ac129c0c82
bellorchid <zhongyunchang@yeah.net>
2018-01-10 18:24:15 +0800

    修改redirect

--- src/AppBundle/Controller/Cashier/WechatController.php
+++ src/AppBundle/Controller/Cashier/WechatController.php
@@ -76,15 +76,21 @@ class WechatController extends PaymentController
     {
         $tradeSn = $request->query->get('tradeSn');
         $trade = $this->getPayService()->getTradeByTradeSn($tradeSn);
-        if ($trade['status'] == 'paid') {
-            return $this->createJsonResponse(array('result' => true, 'message' => '订单已支付！'));
-        }
-        $platformCreatedResult = $this->getPayService()->getCreateTradeResultByTradeSnFromPlatform($tradeSn);
 
-        return $this->render(
-            'cashier/wechat/redirect.html.twig',
+        if ($trade['status'] == 'created') {
+            $platformCreatedResult = $this->getPayService()->getCreateTradeResultByTradeSnFromPlatform($tradeSn);
+
+            return $this->render(
+                'cashier/wechat/app-redirect.html.twig',
+                array(
+                    'mwebUrl' => $platformCreatedResult['mweb_url'],
+                    'trade' => $trade,
+                )
+            );
+        }
+
+        return $this->render('cashier/wechat/app-result.html.twig',
             array(
-                'mwebUrl' => $platformCreatedResult['mweb_url'],
                 'trade' => $trade,
             )
         );
