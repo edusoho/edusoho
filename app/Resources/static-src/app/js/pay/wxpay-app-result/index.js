0a0e61f44b9073d676841dcb84ef9e0b8e0fd397
bellorchid <zhongyunchang@yeah.net>
2018-01-10 21:40:02 +0800

    修改函数

--- app/Resources/static-src/app/js/pay/wxpay-app-result/index.js
+++ app/Resources/static-src/app/js/pay/wxpay-app-result/index.js
@@ -4,6 +4,8 @@ let $uapraser = new UAParser(navigator.userAgent);
 let browser = $uapraser.getBrowser();
 let os = $uapraser.getOS();
 
+alert(os.name + browser.name);
+
 if (os.name === 'iOS' && browser.name === '[Mobile] Safari') {
   document.getElementById("openApp").click();
 }
