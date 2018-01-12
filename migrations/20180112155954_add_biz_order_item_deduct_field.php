121f1aba5c9dab89fae1ef411d03c6e678606b93
shenchupeng <shenchupeng@howzhi.com>
2018-01-12 16:12:26 +0800

    修改biz_order_item_deduct表

---
+++ migrations/20180112155954_add_biz_order_item_deduct_field.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddBizOrderItemDeductField extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $container = $this->getContainer();
+        $db = $container['db'];
+
+        if (!$this->isFieldExist('biz_order_item_deduct', 'deduct_type_name')) {
+            $db->exec("ALTER TABLE `biz_order_item_deduct` ADD COLUMN `deduct_type_name` varchar(255)  NOT NULL DEFAULT '' COMMENT '促销类型名称' AFTER `deduct_type`");
+        }
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $container = $this->getContainer();
+        $db = $container['db'];
+
+        if ($this->isFieldExist('biz_order_item_deduct', 'deduct_type_name')) {
+            $db->exec('ALTER TABLE `biz_order_item_deduct` DROP `deduct_type_name`;');
+        }
+    }
+
+    protected function isFieldExist($table, $fieldName)
+    {
+        $container = $this->getContainer();
+
+        $sql = "DESCRIBE `{$table}` `$fieldName`";
+        $result = $container['db']->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+}
