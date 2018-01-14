4d627520e2d595e9abf183da97af3641da37f066
chenwei <chenwei@howzhi.com>
2018-01-14 21:23:59 +0800

    新增，单元测试

--- tests/Unit/AppBundle/Component/RateLimit/SmsRateLimiterTest.php
+++ tests/Unit/AppBundle/Component/RateLimit/SmsRateLimiterTest.php
@@ -5,10 +5,11 @@ namespace Tests\Unit\AppBundle\Component\RateLimit;
 use Biz\BaseTestCase;
 use AppBundle\Component\RateLimit\SmsRateLimiter;
 use Biz\Common\BizCaptcha;
+use AppBundle\Common\ReflectionUtils;
 
 class SmsRateLimiterTest extends BaseTestCase
 {
-    public function testHandleWithMiddleSecurity()
+    public function testHandle()
     {
         $limiter = new SmsRateLimiter($this->biz);
         $request = $this->mockRequest(
@@ -39,5 +40,33 @@ class SmsRateLimiterTest extends BaseTestCase
         $captcha->shouldHaveReceived('check')->times(1);
         $request->shouldHaveReceived('getClientIp')->times(1);
         $this->assertNull($result);
+    }
+
+    public function testCreateMaxRequestOccurException()
+    {
+        $limiter = new SmsRateLimiter($this->biz);
+        $exception = ReflectionUtils::invokeMethod($limiter, 'createMaxRequestOccurException');
+        $this->assertEquals(
+            'Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException',
+            get_class($exception)
+        );
+    }
+
+    public function testCreateCaptchaOccurException()
+    {
+        $limiter = new SmsRateLimiter($this->biz);
+        $exception = ReflectionUtils::invokeMethod($limiter, 'createCaptchaOccurException');
+        $this->assertEquals(
+            'Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException',
+            get_class($exception)
+        );
+    }
+
+    public function testSetBiz()
+    {
+        $limiter = new SmsRateLimiter($this->biz);
+        $limiter->setBiz($this->biz);
+        $result = ReflectionUtils::getProperty($limiter, 'biz');
+        $this->assertEquals('Codeages\Biz\Framework\Context\Biz', get_class($result));
     }
 }
