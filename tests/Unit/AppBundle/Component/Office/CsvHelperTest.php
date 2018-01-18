29050ef7048149d0b9d99453ad9b432db9f798b8
chenwei <chenwei@howzhi.com>
2018-01-18 15:30:13 +0800

    新增，单元测试

---
+++ tests/Unit/AppBundle/Component/Office/CsvHelperTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\AppBundle\Component\Office;
+
+use Biz\BaseTestCase;
+use AppBundle\Component\Office\CsvHelper;
+
+class CsvHelperTest extends BaseTestCase
+{
+    public function testRead()
+    {
+        $helper = new CsvHelper();
+    }
+}
