05f04f92b136e7576321dc6d456f0d5781f53de6
lvliujie <lvliujie@howzhi.com>
2018-01-18 09:12:23 +0800

    数据统计bug修改前准备

--- tests/Unit/AppBundle/Component/Export/UserLearnStatistics/UserLearnStatisticsExporterTest.php
+++ tests/Unit/AppBundle/Component/Export/UserLearnStatistics/UserLearnStatisticsExporterTest.php
@@ -13,7 +13,7 @@ class UserLearnStatisticsExporterTest extends BaseTestCase
         $expoter = new UserLearnStatisticsExporter(self::$appKernel->getContainer(), array(
         ));
 
-        $result = array('user.learn.statistics.nickname', 'user.learn.statistics.join.classroom.num', 'user.learn.statistics.exit.classroom.num', 'user.learn.statistics.join.course.num', 'user.learn.statistics.exit.course.num', 'user.learn.statistics.finished.task.num', 'user.learn.statistics.learned.econds', 'user.learn.statistics.actual.amount');
+        $result = array('user.learn.statistics.nickname', 'user.learn.statistics.join.classroom.num', 'user.learn.statistics.exit.classroom.num', 'user.learn.statistics.join.course.num', 'user.learn.statistics.exit.course.num', 'user.learn.statistics.finished.task.num', 'user.learn.statistics.learned.seconds', 'user.learn.statistics.actual.amount');
 
         $this->assertArrayEquals($expoter->getTitles(), $result);
     }
