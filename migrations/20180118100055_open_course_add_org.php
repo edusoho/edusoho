261f3fe841868d763a1234296c1b9e201860038b
raopinbin <raopinbin@howzhi.com>
2018-01-18 10:57:43 +0800

    公开课添加组织机构

---
+++ migrations/20180118100055_open_course_add_org.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class OpenCourseAddOrg extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        if (!$this->isFieldExist('open_course', 'orgId')) {
+            $db->exec(
+                "ALTER TABLE `open_course` int(10) unsigned NOT NULL DEFAULT '1' COMMENT '组织机构ID';
+            ");
+        }
+
+        if (!$this->isFieldExist('open_course', 'orgCode')) {
+            $db->exec(
+                "ALTER TABLE `open_course` varchar(255) NOT NULL DEFAULT '1.' COMMENT '组织机构内部编码';
+            ");
+        }
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $db->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+}
