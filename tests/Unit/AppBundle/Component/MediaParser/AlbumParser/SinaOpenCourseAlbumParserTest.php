0f973a38faa7b74c486201d96f0451568faf8e49
chenwei <chenwei@howzhi.com>
2018-01-19 16:15:40 +0800

    新增，单元测试

---
+++ tests/Unit/AppBundle/Component/MediaParser/AlbumParser/SinaOpenCourseAlbumParserTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit\AppBundle\Component\MediaParser\AlbumParser;
+
+use Biz\BaseTestCase;
+use AppBundle\Component\MediaParser\AlbumParser\SinaOpenCourseAlbumParser;
+use AppBundle\Common\ReflectionUtils;
+
+class SinaOpenCourseAlbumParserTest extends BaseTestCase
+{
+    /**
+     * @expectedException \AppBundle\Component\MediaParser\ParseException
+     */
+    public function testParseWithError()
+    {
+        $parser = new SinaOpenCourseAlbumParser();
+        $mockedSender = $this->mockBiz(
+            'Mocked:MockedSender',
+            array(
+                array(
+                    'functionName' => 'fetchUrl',
+                    'withParams' => array('http://v.163.com/special/M941471K5_M9414FGNS.html'),
+                    'returnValue' => array(),
+                ),
+            )
+        );
+        $parser = ReflectionUtils::setProperty($parser, 'mockedSender', $mockedSender);
+
+        $video = $parser->parse('http://v.163.com/special/M941471K5_M9414FGNS.html');
+    }
+
+    public function testParse()
+    {
+        $parser = new SinaOpenCourseAlbumParser();
+
+        $mockedSender = $this->mockBiz(
+            'Mocked:MockedSender',
+            array(
+                array(
+                    'functionName' => 'fetchUrl',
+                    'withParams' => array('http://open.sina.com.cn/course/id_1122'),
+                    'returnValue' => array(
+                        'code' => 200,
+                        'content' => '<h2 class="fblue">blue(3)</h2><p class="txt">summary show</p>'.
+                                     '<div class="container2">'.
+                                       '<ul>'.
+                                         '<li><img alt="<a href=\"hypelink\"" src="hyperlinkSrc"</li>'.
+                                       '</ul>'.
+                                     '</div>',
+                    ),
+                ),
+            )
+        );
+        $parser = ReflectionUtils::setProperty($parser, 'mockedSender', $mockedSender);
+        $video = $parser->parse('http://open.sina.com.cn/course/id_1122');
+
+        $mockedSender->shouldHaveReceived('fetchUrl')->times(1);
+
+        $this->assertArrayEquals(
+            array(
+                'id' => 'course_1122',
+                'uuid' => 'SinaOpenCourseAlbum:course_1122',
+                'title' => 'blue',
+                'summary' => 'summary show',
+                'items' => array(
+                    array(
+                        'url' => 'http://open.sina.com.cn/course/id_1122',
+                        'title' => '<a href=\\',
+                        'picture' => 'hyperlinkSrc',
+                    ),
+                ),
+            ),
+            $video
+        );
+    }
+}
