e3afe5824934213b4a3f31e6f9175bf8daaf10b3
zhongyunchang <zhongyunchang@yeah.net>
2018-01-22 20:43:35 +0800

    修改单元测试

---
+++ tests/Unit/Xapi/Type/FinishActivityTypeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Xapi\Type;
+
+use Biz\BaseTestCase;
+
+class FinishActivityTypeTest extends BaseTestCase
+{
+    public function testPackage()
+    {
+    }
+
+    public function testPackages()
+    {
+    }
+}
---
+++ tests/Unit/Xapi/Type/LiveWatchTypeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Xapi\Type;
+
+use Biz\BaseTestCase;
+
+class LiveWatchTypeTest extends BaseTestCase
+{
+    public function testPackage()
+    {
+    }
+
+    public function testPackages()
+    {
+    }
+}
