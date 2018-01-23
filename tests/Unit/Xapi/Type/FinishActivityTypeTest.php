e3419dee0eef5226edceabd93f55987e79d3233f
zhongyunchang <zhongyunchang@yeah.net>
2018-01-23 09:00:58 +0800

    修改单元测试

--- tests/Unit/Xapi/Type/FinishActivityTypeTest.php
+++ tests/Unit/Xapi/Type/FinishActivityTypeTest.php
@@ -3,14 +3,260 @@
 namespace Tests\Unit\Xapi\Type;
 
 use Biz\BaseTestCase;
+use Biz\Xapi\Type\FinishActivityType;
 
 class FinishActivityTypeTest extends BaseTestCase
 {
     public function testPackage()
     {
+        $taskResultService = $this->mockBiz('Task:TaskResultService',
+            array(
+                array(
+                    'functionName' => 'getTaskResult',
+                    'withParams' => array(1),
+                    'returnValue' => array(
+                        'courseId' => 1,
+                        'activityId' => 2,
+                        'courseTaskId' => 3,
+                    ),
+                ),
+            )
+        );
+
+        $taskService = $this->mockBiz(
+            'Task:TaskService',
+            array(
+                array(
+                    'functionName' => 'getTask',
+                    'withParams' => array(3),
+                    'returnValue' => array(
+                        'id' => 3,
+                        'title' => 'test task',
+                        'activityId' => 2,
+                        'type' => 'video',
+                    ),
+                ),
+            )
+        );
+
+        $courseService = $this->mockBiz(
+            'Course:CourseService',
+            array(
+                array(
+                    'functionName' => 'getCourse',
+                    'withParams' => array(1),
+                    'returnValue' => array(
+                        'id' => 1,
+                        'title' => 'course title',
+                        'courseSetId' => 5,
+                    ),
+                ),
+            )
+        );
+
+        $courseSetService = $this->mockBiz(
+            'Course:CourseSetService',
+            array(
+                array(
+                    'functionName' => 'getCourseSet',
+                    'withParams' => array(5),
+                    'returnValue' => array(
+                        'id' => 5,
+                        'title' => 'course set title',
+                        'subtitle' => 'course set subtitle',
+                    ),
+                ),
+            )
+        );
+
+        $activityService = $this->mockBiz(
+            'Activity:ActivityService',
+            array(
+                array(
+                    'functionName' => 'getActivity',
+                    'withParams' => array(2, true),
+                    'returnValue' => array(
+                        'id' => 2,
+                        'mediaType' => 'video',
+                        'ext' => array(
+                            'mediaId' => 333333,
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $uploadFileService = $this->mockBiz(
+            'File:UploadFileService',
+            array(
+                array(
+                    'functionName' => 'getFile',
+                    'withParams' => array(333333),
+                    'returnValue' => array(
+                        'fileId' => 444456,
+                    ),
+                ),
+            )
+        );
+
+        $type = new FinishActivityType();
+        $type->setBiz($this->biz);
+        $packageInfo = $type->package(array(
+            'target_id' => 1,
+            'user_id' => 12121,
+            'uuid' => '123123123dse',
+            'occur_time' => time(),
+        ));
+
+        $taskResultService->shouldHaveReceived('getTaskResult');
+        $taskService->shouldHaveReceived('getTask');
+        $courseService->shouldHaveReceived('getCourse');
+        $courseSetService->shouldHaveReceived('getCourseSet');
+        $activityService->shouldHaveReceived('getActivity');
+
+        $this->assertEquals('123123123dse', $packageInfo['id']);
+        $this->assertEquals('http://adlnet.gov/expapi/verbs/completed', $packageInfo['verb']['id']);
+        $this->assertEquals(3, $packageInfo['object']['id']);
+        $this->assertArrayEquals(
+            array('title' => 'course set title-course title', 'description' => 'course set subtitle'),
+            $packageInfo['object']['definition']['extensions']['http://xapi.edusoho.com/extensions/course']
+        );
     }
 
     public function testPackages()
     {
+        $type = new FinishActivityType();
+        $type->setBiz($this->biz);
+        $packageInfo = $type->packages(array());
+
+        $this->assertEmpty($packageInfo);
+        $taskResultService = $this->mockBiz('Task:TaskResultService',
+            array(
+                array(
+                    'functionName' => 'searchTaskResults',
+                    'returnValue' => array(
+                        0 => array(
+                            'id' => 1,
+                            'courseId' => 1,
+                            'activityId' => 2,
+                            'courseTaskId' => 3,
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $taskService = $this->mockBiz(
+            'Task:TaskService',
+            array(
+                array(
+                    'functionName' => 'findTasksByIds',
+                    'withParams' => array(array(3)),
+                    'returnValue' => array(
+                        0 => array(
+                            'id' => 3,
+                            'title' => 'test task',
+                            'activityId' => 2,
+                            'type' => 'video',
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $courseService = $this->mockBiz(
+            'Course:CourseService',
+            array(
+                array(
+                    'functionName' => 'findCoursesByIds',
+                    'withParams' => array(array(1)),
+                    'returnValue' => array(
+                        0 => array(
+                            'id' => 1,
+                            'title' => 'course title',
+                            'courseSetId' => 5,
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $courseSetService = $this->mockBiz(
+            'Course:CourseSetService',
+            array(
+                array(
+                    'functionName' => 'findCourseSetsByIds',
+                    'withParams' => array(array(5)),
+                    'returnValue' => array(
+                        0 => array(
+                            'id' => 5,
+                            'title' => 'course set title',
+                            'subtitle' => 'course set subtitle',
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $activityService = $this->mockBiz(
+            'Activity:ActivityService',
+            array(
+                array(
+                    'functionName' => 'findActivities',
+                    'withParams' => array(array(2), true),
+                    'returnValue' => array(
+                        0 => array(
+                            'id' => 2,
+                            'mediaType' => 'video',
+                            'ext' => array(
+                                'mediaId' => 333333,
+                            ),
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $uploadFileService = $this->mockBiz(
+            'File:UploadFileService',
+            array(
+                array(
+                    'functionName' => 'findFilesByIds',
+                    'withParams' => array(array(333333)),
+                    'returnValue' => array(
+                        0 => array(
+                            'fileId' => 444456,
+                        ),
+                    ),
+                ),
+            )
+        );
+
+        $type = new FinishActivityType();
+        $type->setBiz($this->biz);
+        $packageInfo = $type->packages(array(
+            array(
+                'target_id' => 1,
+                'user_id' => 12121,
+                'uuid' => '123123123dse',
+                'occur_time' => time(),
+            ),
+        ));
+
+        $taskResultService->shouldHaveReceived('searchTaskResults');
+        $taskService->shouldHaveReceived('findTasksByIds');
+        $courseService->shouldHaveReceived('findCoursesByIds');
+        $courseSetService->shouldHaveReceived('findCourseSetsByIds');
+        $activityService->shouldHaveReceived('findActivities');
+
+        $packageInfo = reset($packageInfo);
+
+        $this->assertEquals('123123123dse', $packageInfo['id']);
+        $this->assertEquals('http://adlnet.gov/expapi/verbs/completed', $packageInfo['verb']['id']);
+        $this->assertEquals(3, $packageInfo['object']['id']);
+        $this->assertArrayEquals(
+            array('title' => 'course set title-course title', 'description' => 'course set subtitle'),
+            $packageInfo['object']['definition']['extensions']['http://xapi.edusoho.com/extensions/course']
+        );
     }
 }
