04f8e08dd235aab7b106883301d03361def5d45c
maofangfei <maofangfei@howzhi.com>
2018-01-30 11:17:10 +0800

    兼容ie9

--- app/Resources/static-src/common/utils.js
+++ app/Resources/static-src/common/utils.js
@@ -8,7 +8,8 @@ let s;
   (s = userAgent.match(/opera.([\d.]+)/)) ? Browser.opera = s[1] :
   (s = userAgent.match(/version\/([\d.]+).*safari/)) ? Browser.safari = s[1] : 0;
 
-Browser.ie10 = /MSIE\s+10.0/i.test(navigator.userAgent)
+Browser.ie9 = /MSIE\s+9.0/i.test(navigator.userAgent);
+Browser.ie10 = /MSIE\s+10.0/i.test(navigator.userAgent);
 Browser.ie11 = (/Trident\/7\./).test(navigator.userAgent);
 Browser.edge = /Edge\/13./i.test(navigator.userAgent);
 
