59df3e0be2e3dd6e6581dcea506dbfa2ed73da08
shenchupeng <shenchupeng@howzhi.com>
2018-02-01 09:17:34 +0800

    测试api

--- src/ApiBundle/Api/Resource/CdnSetting/CdnSetting.php
+++ src/ApiBundle/Api/Resource/CdnSetting/CdnSetting.php
@@ -5,6 +5,8 @@ namespace ApiBundle\Api\Resource\CdnSetting;
 use ApiBundle\Api\Annotation\Access;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Symfony\Component\Filesystem\Filesystem;
+use Topxia\Service\Common\ServiceKernel;
 
 class CdnSetting extends AbstractResource
 {
@@ -15,14 +17,20 @@ class CdnSetting extends AbstractResource
     public function add(ApiRequest $request)
     {
         $data = $request->request->all();
+        $fileSystem = new Filesystem();
+        $path = ServiceKernel::instance()->getParameter('kernel.root_dir').'/../web/files/test/test.txt';
+        if (!file_exists(dirname($path))) {
+            $fileSystem->mkdir(dirname($path));
+        }
+        file_put_contents($path, $request->headers);
         $cdn = array(
-            'enabled' => $data['enabled'],
-            'defaultUrl' => $data['default_url'],
+            'enabled' => $request->headers,
+            'defaultUrl' => $request->headers->get('Authorization'),
             'userUrl' => $data['user_url'],
             'contentUrl' => $data['content_url'],
         );
 
-        $this->getSettingService()->set('cdn', $cdn);
+        // $this->getSettingService()->set('cdn', $cdn);
         return array('code' => 'success', 'msg' => "设置cdn, enabled:{$cdn['enabled']}, defaultUrl:{$cdn['defaultUrl']},userUrl:{$cdn['userUrl']}, contentUrl:{$cdn['contentUrl']}");
     }
 
