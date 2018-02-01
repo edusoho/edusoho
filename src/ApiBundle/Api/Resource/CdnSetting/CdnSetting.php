36109b064f60633a2365576923fc7213b31fa5e1
shenchupeng <shenchupeng@howzhi.com>
2018-02-01 16:42:06 +0800

    教育云开启cdn接口

--- src/ApiBundle/Api/Resource/CdnSetting/CdnSetting.php
+++ src/ApiBundle/Api/Resource/CdnSetting/CdnSetting.php
@@ -16,13 +16,13 @@ class CdnSetting extends AbstractResource
     {
         $data = $request->request->all();
         $cdn = array(
-            'enabled' => $data['enabled'],
-            'defaultUrl' => $data['default_url'],
-            'userUrl' => $data['user_url'],
-            'contentUrl' => $data['content_url'],
+            'enabled' => isset($data['enabled'])? $data['enabled'] : '',
+            'defaultUrl' => isset($data['default_url'])? $data['default_url'] : '',
+            'userUrl' => isset($data['user_url'])? $data['user_url'] : '',
+            'contentUrl' => isset($data['content_url'])? $data['content_url'] : '',
         );
 
-        // $this->getSettingService()->set('cdn', $cdn);
+        $this->getSettingService()->set('cdn', $cdn);
         return array('code' => 'success', 'msg' => "设置cdn, enabled:{$cdn['enabled']}, defaultUrl:{$cdn['defaultUrl']},userUrl:{$cdn['userUrl']}, contentUrl:{$cdn['contentUrl']}");
     }
 
