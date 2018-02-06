e2aadd2b2e6919dca9076d235a5ca2210967ca10
yangshuai <yangshuai@howzhi.com>
2018-02-06 16:03:53 +0800

    remove not user method

--- src/Biz/BaseService.php
+++ src/Biz/BaseService.php
@@ -163,27 +163,4 @@ class BaseService extends \Codeages\Biz\Framework\Service\BaseService
     {
         return ServiceKernel::instance()->trans($message, $arguments, $domain, $locale);
     }
-
-    protected function getQiQiuYunSDK()
-    {
-        $developerSetting = $this->getSettingService()->get('developer', array());
-        $settings = $this->getSettingService()->get('storage', array());
-        $sdk = new \QiQiuYun\SDK\QiQiuYunSDK(
-            array(
-            'access_key' => $settings['cloud_access_key'],
-            'secret_key' => $settings['cloud_secret_key'],
-            'service' => array(
-                'drp' => array(
-                    'host' => $developerSetting['distributor_server'],
-                ),
-            ), )
-        );
-
-        return $sdk;
-    }
-
-    private function getSettingService()
-    {
-        return $this->biz->service('System:SettingService');
-    }
 }
