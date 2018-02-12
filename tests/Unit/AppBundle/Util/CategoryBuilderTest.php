5bba17e1ad8484b8f64b9b0badc16023bb3fde0a
lvliujie <lvliujie@howzhi.com>
2018-02-12 14:58:51 +0800

    categorybuildertest单元测试

--- tests/Unit/AppBundle/Util/CategoryBuilderTest.php
+++ tests/Unit/AppBundle/Util/CategoryBuilderTest.php
@@ -9,11 +9,63 @@ class CategoryBuilderTest extends BaseTestCase
 {
     public function testBuildChoices()
     {
+        $this->mockBiz(
+            'Taxonomy:CategoryService',
+            array(
+                array(
+                    'functionName' => 'getGroupByCode',
+                    'withParams' => array('xxx'),
+                    'returnValue' => null
+                ),
+                array(
+                    'functionName' => 'getGroupByCode',
+                    'withParams' => array('course'),
+                    'returnValue' => array(
+                        'id' => 1,
+                        'code' => 'course',
+                        'name' => '课程分类',
+                        'depth' => 3
+                    )
+                ),
+                array(
+                    'functionName' => 'getCategoryTree',
+                    'returnValue' => json_decode('[{
+                        "id": "1",
+                        "code": "default",
+                        "name": "默认分类",
+                        "icon": "",
+                        "path": "",
+                        "weight": "0",
+                        "groupId": "1",
+                        "parentId": "0",
+                        "orgId": "1",
+                        "orgCode": "1.",
+                        "description": null,
+                        "depth": 1
+                    }, {
+                        "id": "3",
+                        "code": "zifenlei",
+                        "name": "子分类",
+                        "icon": "",
+                        "path": "",
+                        "weight": "0",
+                        "groupId": "1",
+                        "parentId": "1",
+                        "orgId": "1",
+                        "orgCode": "1.",
+                        "description": "",
+                        "depth": 2
+                    }]', true)
+                ),
+            )
+        );
         $builder = new CategoryBuilder();
         $result = $builder->buildChoices('xxx');
         $this->assertEmpty($result);
 
+
         $result = $builder->buildChoices('course');
-        $this->assertNotNull($result);
+        $this->assertEquals('默认分类', $result[1]);
+        $this->assertEquals('　子分类', $result[3]);
     }
 }
