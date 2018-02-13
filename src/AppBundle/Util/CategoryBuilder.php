dfa861824b6d7b5f00a7e27143c845935bf7651e
lvliujie <lvliujie@howzhi.com>
2018-02-13 15:10:20 +0800

    去除测试代码

--- src/AppBundle/Util/CategoryBuilder.php
+++ src/AppBundle/Util/CategoryBuilder.php
@@ -9,14 +9,12 @@ class CategoryBuilder
     public function buildChoices($groupCode, $indent = '　')
     {
         $group = $this->getCategoryService()->getGroupByCode($groupCode);
-        var_dump($group);
         if (empty($group)) {
             return array();
         }
 
         $choices = array();
         $categories = $this->getCategoryService()->getCategoryTree($group['id']);
-        var_dump($categories);
 
         foreach ($categories as $category) {
             $choices[$category['id']] = str_repeat(is_null($indent) ? '' : $indent, ($category['depth'] - 1)).$category['name'];
