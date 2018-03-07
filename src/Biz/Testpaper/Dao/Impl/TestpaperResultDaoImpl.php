2790910654edd83e2a8b8fb1c75d503bf7f33d99
qichen <qichen@howzhi.com>
2018-03-07 16:24:50 +0800

    app/Resources/static-src/app/js/testpaper/result/index.js

--- src/Biz/Testpaper/Dao/Impl/TestpaperResultDaoImpl.php
+++ src/Biz/Testpaper/Dao/Impl/TestpaperResultDaoImpl.php
@@ -23,11 +23,11 @@ class TestpaperResultDaoImpl extends GeneralDaoImpl implements TestpaperResultDa
         return $this->db()->fetchAssoc($sql, array($testId, $courseId, $activityId, $type, $userId)) ?: null;
     }
 
-    public function getUserLatelyResultByTestId($userId, $testId, $courseId, $activityId, $type)
+    public function getUserLatelyResultByTestId($userId, $testId, $courseId, $lessonId, $type)
     {
         $sql = "SELECT * FROM {$this->table} WHERE userId = ? AND testId = ? AND courseId = ? AND lessonId = ? AND type = ? ORDER BY id DESC ";
 
-        return $this->db()->fetchAssoc($sql, array($userId, $testId, $courseId, $activityId, $type)) ?: null;
+        return $this->db()->fetchAssoc($sql, array($userId, $testId, $courseId, $lessonId, $type)) ?: null;
     }
 
     public function findPaperResultsStatusNumGroupByStatus($testId, $activityId)
