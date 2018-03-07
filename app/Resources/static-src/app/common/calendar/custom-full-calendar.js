bb8937b2d3e38bb07200dbab1d32681779c48c7d
tianshengjie <tianshengjie@howzhi.com>
2018-03-07 13:47:42 +0800

    格式化

--- app/Resources/static-src/app/common/calendar/custom-full-calendar.js
+++ app/Resources/static-src/app/common/calendar/custom-full-calendar.js
@@ -52,203 +52,203 @@
  */
 let current;
 export default class CustomFullCalendar {
-	constructor(options = {}) {
-		current = this;
-		this.options = options;
+  constructor(options = {}) {
+    current = this;
+    this.options = options;
 
-		if (this._verifyNeccessaryFields()) {
-			this._fillDefaultFields();
-			this._init();
-		}
-	}
+    if (this._verifyNeccessaryFields()) {
+      this._fillDefaultFields();
+      this._init();
+    }
+  }
 
-	_init() {
-		let calendarOptions = {
-			header: {
-				left: '',
-				center: 'title',
-				right: 'prev,today,next'
-			},
-			defaultDate: this.options['currentTime'],
-			eventLimit: true, // allow "more" link when too many events
-			locale: this.options['locale'],
-			defaultView: this.options['defaultView'],
-			allDaySlot: false,
-			scrollTime: '08:00:00', //默认移动到　８点位置
-		};
+  _init() {
+    let calendarOptions = {
+      header: {
+        left: '',
+        center: 'title',
+        right: 'prev,today,next'
+      },
+      defaultDate: this.options['currentTime'],
+      eventLimit: true, // allow "more" link when too many events
+      locale: this.options['locale'],
+      defaultView: this.options['defaultView'],
+      allDaySlot: false,
+      scrollTime: '08:00:00', //默认移动到　８点位置
+    };
 
-		if (calendarOptions['defaultView'] == 'agendaWeek') {
-			calendarOptions['columnFormat'] = 'ddd DD';
-		}
+    if (calendarOptions['defaultView'] == 'agendaWeek') {
+      calendarOptions['columnFormat'] = 'ddd DD';
+    }
 
-		if (typeof this.options['switcher'] != 'undefined') {
-			calendarOptions['headers']['left'] = this.options['switchers'];
-		}
+    if (typeof this.options['switcher'] != 'undefined') {
+      calendarOptions['headers']['left'] = this.options['switchers'];
+    }
 
-		if (typeof this.options['data'] != 'undefined') {
-			let events = this.options['data'];
-			calendarOptions['events'] = this._generateEventOtherAttrs(events, events);
-		}
+    if (typeof this.options['data'] != 'undefined') {
+      let events = this.options['data'];
+      calendarOptions['events'] = this._generateEventOtherAttrs(events, events);
+    }
 
-		if (this.options['locale'] != 'en') {
-			require('libs/fullcalendar/locale/' + this.options['locale']);
-		}
+    if (this.options['locale'] != 'en') {
+      require('libs/fullcalendar/locale/' + this.options['locale']);
+    }
 
-		if (typeof this.options['dataApi'] != 'undefined') {
-			calendarOptions['lazyFetching'] = true;
-			calendarOptions['events'] = this._ajaxLoading;
-			if (calendarOptions['defaultView'] === 'month') {
-				calendarOptions['viewRender'] = this._formatMonthFirstDay;
-			}
-		} else if (typeof this.options['data'] != 'undefined') {
-			calendarOptions['events'] = this.options['data'];
-		}
+    if (typeof this.options['dataApi'] != 'undefined') {
+      calendarOptions['lazyFetching'] = true;
+      calendarOptions['events'] = this._ajaxLoading;
+      if (calendarOptions['defaultView'] === 'month') {
+        calendarOptions['viewRender'] = this._formatMonthFirstDay;
+      }
+    } else if (typeof this.options['data'] != 'undefined') {
+      calendarOptions['events'] = this.options['data'];
+    }
 
-		calendarOptions = this._registerCompActions(calendarOptions);
-		this.calendarOptions = calendarOptions;
+    calendarOptions = this._registerCompActions(calendarOptions);
+    this.calendarOptions = calendarOptions;
 
-		$(this.options['calendarContainer']).fullCalendar(calendarOptions);
-	}
+    $(this.options['calendarContainer']).fullCalendar(calendarOptions);
+  }
 
-	_ajaxLoading(start, end, timezone, callback) {
-		$('.fc-day-header span').hide();
-		let startTimeAttr = current.options['dateParams']['start'];
-		let endTimeAttr = current.options['dateParams']['end'];
-		let params = {};
-		params[startTimeAttr] = current._getDateStartUnixTime(start);
-		params[endTimeAttr] = current._getDateStartUnixTime(end);
-		params['limit'] = 1000;
-		current.options['dataApi']({
-			data: params
-		}).then((result) => {
-			let calEvents = [];
-			for (let i = 0; i < result['data'].length; i++) {
-				calEvents.push(current._generateEventInitValues(result['data'][i]));
-			}
-			calEvents = current._generateEventOtherAttrs(calEvents, result['data']);
-			callback(calEvents);
-		}).catch((res) => {
-			console.log('error callback');
-		});
-	}
+  _ajaxLoading(start, end, timezone, callback) {
+    $('.fc-day-header span').hide();
+    let startTimeAttr = current.options['dateParams']['start'];
+    let endTimeAttr = current.options['dateParams']['end'];
+    let params = {};
+    params[startTimeAttr] = current._getDateStartUnixTime(start);
+    params[endTimeAttr] = current._getDateStartUnixTime(end);
+    params['limit'] = 1000;
+    current.options['dataApi']({
+      data: params
+    }).then((result) => {
+      let calEvents = [];
+      for (let i = 0; i < result['data'].length; i++) {
+        calEvents.push(current._generateEventInitValues(result['data'][i]));
+      }
+      calEvents = current._generateEventOtherAttrs(calEvents, result['data']);
+      callback(calEvents);
+    }).catch((res) => {
+      console.log('error callback');
+    });
+  }
 
-	_formatMonthFirstDay(view) {
-		$('.fc-day-top').each(function() {
-			const $this = $(this);
-			const isMonthFirstDay = $this.data('date').substr(-3, 3) === '-01';
-			const isOtherMonth = $this.hasClass('fc-other-month');
-			const currentMonthFirstDay = isMonthFirstDay && !isOtherMonth;
-			const nextMonthFirstDay = isMonthFirstDay && isOtherMonth;
-			const $day = $this.find('.fc-day-number');
-			if (currentMonthFirstDay) {
-				$day.html(view.intervalStart.format('LL'));
-			} else if (nextMonthFirstDay) {
-				$day.html(view.intervalEnd.format('LL'));
-			}
-		});
-	}
+  _formatMonthFirstDay(view) {
+    $('.fc-day-top').each(function() {
+      const $this = $(this);
+      const isMonthFirstDay = $this.data('date').substr(-3, 3) === '-01';
+      const isOtherMonth = $this.hasClass('fc-other-month');
+      const currentMonthFirstDay = isMonthFirstDay && !isOtherMonth;
+      const nextMonthFirstDay = isMonthFirstDay && isOtherMonth;
+      const $day = $this.find('.fc-day-number');
+      if (currentMonthFirstDay) {
+        $day.html(view.intervalStart.format('LL'));
+      } else if (nextMonthFirstDay) {
+        $day.html(view.intervalEnd.format('LL'));
+      }
+    });
+  }
 
-	_generateEventOtherAttrs(events, data) {
-		for (let i = 0; i < events.length; i++) {
-			$.extend(events[i], this._generateEventCompValues(data[i]));
-			events[i] = this._addDateClassToEvent(events[i]);
-		}
+  _generateEventOtherAttrs(events, data) {
+    for (let i = 0; i < events.length; i++) {
+      $.extend(events[i], this._generateEventCompValues(data[i]));
+      events[i] = this._addDateClassToEvent(events[i]);
+    }
 
-		return events;
-	}
+    return events;
+  }
 
-	_addDateClassToEvent(event) {
-		let startUnixTime = this._getDateStartUnixTime(moment(event['start']));
-		let currentUnixTime = this._getDateStartUnixTime(moment(this.options['currentTime']));
-		let endUnixTime = this._getDateStartUnixTime(moment(event['end']));
+  _addDateClassToEvent(event) {
+    let startUnixTime = this._getDateStartUnixTime(moment(event['start']));
+    let currentUnixTime = this._getDateStartUnixTime(moment(this.options['currentTime']));
+    let endUnixTime = this._getDateStartUnixTime(moment(event['end']));
 
-		if (endUnixTime < currentUnixTime) {
-			event['className'].push('calendar-before');
-		} else if (currentUnixTime < startUnixTime) {
-			event['className'].push('calendar-future');
-		} else  {
-			event['className'].push('calendar-today');
-		}
-		return event;
-	}
+    if (endUnixTime < currentUnixTime) {
+      event['className'].push('calendar-before');
+    } else if (currentUnixTime < startUnixTime) {
+      event['className'].push('calendar-future');
+    } else  {
+      event['className'].push('calendar-today');
+    }
+    return event;
+  }
 
-	_verifyNeccessaryFields() {
-		if (typeof this.options['data'] == 'undefined' && typeof this.options['dataApi'] == 'undefined') {
-			console.log('custom-full-calendar: no "data" or "dataApi" in options');
-			return false;
-		}
+  _verifyNeccessaryFields() {
+    if (typeof this.options['data'] == 'undefined' && typeof this.options['dataApi'] == 'undefined') {
+      console.log('custom-full-calendar: no "data" or "dataApi" in options');
+      return false;
+    }
 
-		if (typeof this.options['dataApi'] != 'undefined' && typeof this.options['attrs'] == 'undefined') {
-			console.log('custom-full-calendar: no "attrs" in options');
-			return false;
-		}
+    if (typeof this.options['dataApi'] != 'undefined' && typeof this.options['attrs'] == 'undefined') {
+      console.log('custom-full-calendar: no "attrs" in options');
+      return false;
+    }
 
-		if (typeof this.options['calendarContainer'] == 'undefined') {
-			console.log('custom-full-calendar: no "calendarContainer" in options');
-			return false;
-		}
+    if (typeof this.options['calendarContainer'] == 'undefined') {
+      console.log('custom-full-calendar: no "calendarContainer" in options');
+      return false;
+    }
 
-		if (typeof this.options['currentTime'] == 'undefined') {
-			console.log('custom-full-calendar: no "currentTime" in options');
-			return false;
-		}
+    if (typeof this.options['currentTime'] == 'undefined') {
+      console.log('custom-full-calendar: no "currentTime" in options');
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	_fillDefaultFields() {
-		this._fillIfEmpty({
-			'defaultView': 'month',
-			'locale': 'zh-cn',
-			'dateParams': {
-				'start': 'createdTime_GE',
-				'end': 'createdTime_LT'
-			},
-			'components': []
-		});
-	}
+  _fillDefaultFields() {
+    this._fillIfEmpty({
+      'defaultView': 'month',
+      'locale': 'zh-cn',
+      'dateParams': {
+        'start': 'createdTime_GE',
+        'end': 'createdTime_LT'
+      },
+      'components': []
+    });
+  }
 
-	_fillIfEmpty(params) {
-		for (let key in params) {
-			if (typeof this.options[key] == 'undefined' || this.options[key] == null) {
-				this.options[key] = params[key];
-			}
-		}
-	}
+  _fillIfEmpty(params) {
+    for (let key in params) {
+      if (typeof this.options[key] == 'undefined' || this.options[key] == null) {
+        this.options[key] = params[key];
+      }
+    }
+  }
 
-	/*
+  /*
    * 获取所给时间的0小时0分0秒的时间戳 
    * @param momentObj 
    */
-	_getDateStartUnixTime(momentObj) {
-		let dateStr = momentObj.format('YYYY-MM-DD HH:mm:ss');
-		return moment(dateStr).unix();
-	}
+  _getDateStartUnixTime(momentObj) {
+    let dateStr = momentObj.format('YYYY-MM-DD HH:mm:ss');
+    return moment(dateStr).unix();
+  }
 
-	_registerCompActions(calendarOptions) {
-		for (let i = 0; i < this.options['components'].length; i++) {
-			calendarOptions = this.options['components'][i].registerAction(calendarOptions);
-		}
-		return calendarOptions;
-	}
+  _registerCompActions(calendarOptions) {
+    for (let i = 0; i < this.options['components'].length; i++) {
+      calendarOptions = this.options['components'][i].registerAction(calendarOptions);
+    }
+    return calendarOptions;
+  }
 
-	_generateEventInitValues(singleResult) {
-		let copiedFields = ['title', 'start', 'end'];
-		let singleEvent = {};
-		for (let i = 0; i < copiedFields.length; i++) {
-			let fieldName = copiedFields[i];
-			singleEvent[fieldName] = singleResult[this.options['attrs'][fieldName]];
-		}
-		singleEvent['className'] = [];
-		return singleEvent;
-	}
+  _generateEventInitValues(singleResult) {
+    let copiedFields = ['title', 'start', 'end'];
+    let singleEvent = {};
+    for (let i = 0; i < copiedFields.length; i++) {
+      let fieldName = copiedFields[i];
+      singleEvent[fieldName] = singleResult[this.options['attrs'][fieldName]];
+    }
+    singleEvent['className'] = [];
+    return singleEvent;
+  }
 
-	_generateEventCompValues(singleResult) {
-		let singleEvent = {};
-		for (let i = 0; i < this.options['components'].length; i++) {
-			$.extend(singleEvent, this.options['components'][i].generateEventValues(singleResult));
-		}
-		return singleEvent;
-	}
+  _generateEventCompValues(singleResult) {
+    let singleEvent = {};
+    for (let i = 0; i < this.options['components'].length; i++) {
+      $.extend(singleEvent, this.options['components'][i].generateEventValues(singleResult));
+    }
+    return singleEvent;
+  }
 
 }
--- app/Resources/static-src/common/autocomplete/index.js
+++ app/Resources/static-src/common/autocomplete/index.js
@@ -2,27 +2,27 @@ import './jquery.autocomplete';
 import '!style-loader?insertAt=top!css-loader!./jquery.autocomplete.css';
 
 let autocomplete = ({ element, valueKey, url }) => {
-	let $element = $(element);
-	$element.autocomplete({
-		appendMethod: 'replace',
-		valueKey: valueKey || 'nickname',
-		source: [
-			function( q, add ) {
-				if (!q) return;
-				let source = [];
-				let getUrl = url  || $element.data('auto-url');
+  let $element = $(element);
+  $element.autocomplete({
+    appendMethod: 'replace',
+    valueKey: valueKey || 'nickname',
+    source: [
+      function( q, add ) {
+        if (!q) return;
+        let source = [];
+        let getUrl = url  || $element.data('auto-url');
         
-				$.get(`${getUrl}?q=${q}`, (data) => {
-					if (data) {
-						data.map((item) => {
-							source.push(item[this.valueKey]);
-						});
-					}
-					add(source);
-				});
-			},
-		],
-	});
+        $.get(`${getUrl}?q=${q}`, (data) => {
+          if (data) {
+            data.map((item) => {
+              source.push(item[this.valueKey]);
+            });
+          }
+          add(source);
+        });
+      },
+    ],
+  });
 };
 
 export default autocomplete;
