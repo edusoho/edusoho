801a9a3e866a55258f2ecebb7ab94a2e3cb6ef66
LianJion <1995lian1002@gmail.com>
2018-03-08 10:11:56 +0800

    普通课程

--- app/Resources/static-src/app/js/my/teaching-courseset/index.js
+++ app/Resources/static-src/app/js/my/teaching-courseset/index.js
@@ -3,4 +3,11 @@ cd.select({
   type: 'single'
 }).on('change', (value, text) => {
   console.log('single', value, text);
+  // if (value) {
+  //   $('.js-course-set-item').not('.js-course-set-type-' + value).hide();
+  //   $('.js-course-set-type-' + value).show();
+  // } else {
+  //   $('.js-course-set-item').show();
+  // }
+
 });
