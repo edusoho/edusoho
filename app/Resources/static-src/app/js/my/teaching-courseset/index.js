511aa45f5a312273170de67e41c999d3d9505628
LianJion <1995lian1002@gmail.com>
2018-03-12 13:35:03 +0800

    添加js类

--- app/Resources/static-src/app/js/my/teaching-courseset/index.js
+++ app/Resources/static-src/app/js/my/teaching-courseset/index.js
@@ -2,7 +2,5 @@ cd.select({
   el: '#select-single',
   type: 'single'
 }).on('change', (value, text) => {
-  if ($('.select-options li').hasClass('checked')) {
-    window.location.href = $('.select-options li.checked').data('url');
-  }
+  window.location.href = $('.js-select-options li.checked').data('url');
 });
