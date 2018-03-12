65655c03cbe626cadfe94bd8591260463dd9b182
LianJion <1995lian1002@gmail.com>
2018-03-12 12:01:31 +0800

    js跳转路由

--- app/Resources/static-src/app/js/my/teaching-courseset/index.js
+++ app/Resources/static-src/app/js/my/teaching-courseset/index.js
@@ -2,12 +2,7 @@ cd.select({
   el: '#select-single',
   type: 'single'
 }).on('change', (value, text) => {
-  console.log('single', value, text);
-  // if (value) {
-  //   $('.js-course-set-item').not('.js-course-set-type-' + value).hide();
-  //   $('.js-course-set-type-' + value).show();
-  // } else {
-  //   $('.js-course-set-item').show();
-  // }
-
+  if ($('.select-options li').hasClass('checked')) {
+    window.location.href = $('.select-options li.checked').data('url');
+  }
 });
