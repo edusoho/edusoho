73d3803a868d0cce9ec8aea8c2c5b271d53262f3
tianshengjie <tianshengjie@howzhi.com>
2018-03-16 10:50:41 +0800

    update

--- tests/Js/common/test.unit.js
+++ tests/Js/common/test.unit.js
@@ -1,8 +1,8 @@
 import { trim, dateFormat, numberConvertLetter } from '../../../app/Resources/static-src/app/common/unit.js';
-console.log(trim);
+
 var assert = require('chai').assert;
-describe('common:unit when is_global is true', function() {
-  it('function:trim', function() {
+describe('common:unit', function() {
+  it('function:trim when is_global is true', function() {
     assert.equal(trim(' a asdf '), 'aasdf');
   });
   it('function:trim when is_global is false', function() {
