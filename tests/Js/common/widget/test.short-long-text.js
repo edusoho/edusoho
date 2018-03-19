53e03dd2e3285a03934bb6aa765bf761d4083b7b
tianshengjie <tianshengjie@howzhi.com>
2018-03-19 09:18:53 +0800

    add js tes

---
+++ tests/Js/common/widget/test.short-long-text.js
@@ -0,0 +1,10 @@
+import { shortLongText } from '../../../../app/Resources/static-src/app/common/widget/short-long-text.js';
+var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
+
+// shortLongText($('body'));
+// var assert = require('chai').assert;
+// describe('common:unit', function() {
+//   it('function:trim when is_global is true', function() {
+//     console.log($);
+//   });
+// });
