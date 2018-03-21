e42b85a4f4e78cbe700be90c1bef344b2bad2892
zhongyunchang <zhongyunchang@yeah.net>
2018-03-21 16:23:00 +0800

    修改错误

--- app/Resources/static-src/app/js/search/index.js
+++ app/Resources/static-src/app/js/search/index.js
@@ -2,8 +2,6 @@ import notify from 'common/notify';
 echo.init();
 
 let $errorType = $('.with-error').data('type');
-console.log(12111);
 if ($errorType === 'cloudSearchError') {
-  console.log(12144411);
-  notify('danger', Translator.trans('网络不给力，为您切换极简搜索模式'),{delay:2000});
+  notify('danger', Translator.trans('cloud_search.network.error_message'),{delay:2000});
 }
