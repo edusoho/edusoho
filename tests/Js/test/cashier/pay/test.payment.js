7a2a1c32027f00fd95281653957e29b7629a3628
tianshengjie <tianshengjie@howzhi.com>
2018-03-21 10:01:08 +0800

    add sinon

--- tests/Js/test/cashier/pay/test.payment.js
+++ tests/Js/test/cashier/pay/test.payment.js
@@ -2,6 +2,7 @@
 import { getRootPath, init } from '../../../util/init.js';
 import decache from 'decache';
 const assert = require('chai').assert;
+const sinon = require('sinon');
 
 init('');
 let BasePament = require(getRootPath() + '/app/Resources/static-src/app/js/cashier/pay/payment.js').default;
@@ -16,15 +17,29 @@ describe('pay:payment', function() {
     decache('../../../util/init.js');
   });
 
-  it('function:getTrade', function(done) {
-    // BasePament.getTrade('').then(res => {
-    //   assert.equal(res.isPaid, false);
-    // });
-    // console.log(BasePament.getTrade('baidu.com'));
-    $.post('/', function(e){
-      console.log(e);
-      done();
-    })
+  it('function:getTrade when trade is empty', function() {
+    BasePament.getTrade('').then(res => {
+      assert.equal(res.isPaid, false);
+    });
+  });
+
+  it('function:getTrade when trade not empty', function() {
+    var ajax = sinon.stub($, 'ajax');
+
+    var expectedParams = {
+      async: true,
+      dataType: "json",
+      params: { tradeSn: "20180202" },
+      promise: true,
+      type: "GET",
+      url: "/api/trades/20180202"
+    };
+
+    BasePament.getTrade('20180202').then(res => {
+    });
+
+    ajax.restore();
+    sinon.assert.calledWith(ajax, expectedParams);
   });
 
   it('function:startInterval', function() {
