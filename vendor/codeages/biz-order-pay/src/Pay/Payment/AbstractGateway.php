368064e4bdcb2207dca1b71794b93f8648129b46
zhongyunchang <zhongyunchang@yeah.net>
2018-03-22 14:41:12 +0800

    增加vendor

--- vendor/codeages/biz-order-pay/src/Pay/Payment/AbstractGateway.php
+++ vendor/codeages/biz-order-pay/src/Pay/Payment/AbstractGateway.php
@@ -2,8 +2,8 @@
 
 namespace Codeages\Biz\Pay\Payment;
 
-
 use Codeages\Biz\Framework\Service\Exception\AccessDeniedException;
+use Codeages\Biz\Pay\Message\CloseTradeResponse;
 
 abstract class AbstractGateway
 {
@@ -16,7 +16,7 @@ abstract class AbstractGateway
 
     public function closeTrade($trade)
     {
-        throw new AccessDeniedException('can not close trade.');
+        return new CloseTradeResponse(true);
     }
 
     abstract public function createTrade($data);
@@ -27,16 +27,17 @@ abstract class AbstractGateway
 
     /**
      * @param $data 第三方支付平台的通知信息
+     *
      * @return array
-     *     支付成功的返回值：
-     *              status          success
-     *              cash_flow       第三方支付平台的支付流水号
-     *              paid_time       支付时间
-     *              pay_amount      支付金额，整数，单位为分。
-     *              cash_type       支付币种
-     *              trade_no        订单号
-     *              attach          附件字段
-     *              notify_data     第三方通知的元信息
+     *               支付成功的返回值：
+     *               status          success
+     *               cash_flow       第三方支付平台的支付流水号
+     *               paid_time       支付时间
+     *               pay_amount      支付金额，整数，单位为分。
+     *               cash_type       支付币种
+     *               trade_no        订单号
+     *               attach          附件字段
+     *               notify_data     第三方通知的元信息
      *
      *      支付失败的返回值：
      *              status:         failture
@@ -45,5 +46,4 @@ abstract class AbstractGateway
     abstract public function converterNotify($data);
 
     abstract public function converterRefundNotify($data);
-
 }
