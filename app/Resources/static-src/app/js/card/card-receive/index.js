b40a0719e0224ebcbde4488b69605f3d4142d204
tianshengjie <tianshengjie@howzhi.com>
2018-04-02 16:32:05 +0800

    fix: #35755 修改了虚拟币名称，领取时，提示文案不正确

--- app/Resources/static-src/app/js/card/card-receive/index.js
+++ app/Resources/static-src/app/js/card/card-receive/index.js
@@ -1,13 +1,15 @@
 import notify from 'common/notify';
 
 if ($('a').hasClass('money-card-use')) {
-  var url = $('.money-card-use').data('url');
-  var target_url = $('.money-card-use').data('target-url');
+  let $moneyCard = $('.money-card-use');
+  var url = $moneyCard.data('url');
+  var coinName = $moneyCard.data('coinName');
+  var targetUrl = $moneyCard.data('targetUrl');
   var coin = $('.card-coin-val').val();
 
   $.post(url, function (response) {
-    notify('success',Translator.trans('card.card_receive_success_hint', {coin:coin}));
-    setTimeout('window.location.href = \'' + target_url + '\'', 2000);
+    notify('success',Translator.trans('card.card_receive_success_hint', {coin:coin, coinName: iconName}));
+    setTimeout('window.location.href = \'' + targetUrl + '\'', 2000);
   }).error(function () {
     notify('danger',Translator.trans('card.card_receive_failed_hint'));
   });
