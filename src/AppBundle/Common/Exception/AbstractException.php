05a3fc5a5e3765fff122621ea67d7dbb9d2932a2
tianshengjie <tianshengjie@howzhi.com>
2018-04-13 17:39:42 +0800

    refactor: 重构异常处理

--- src/AppBundle/Common/Exception/AbstractException.php
+++ src/AppBundle/Common/Exception/AbstractException.php
@@ -6,4 +6,15 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 abstract class AbstractException extends HttpException
 {
+    public $moduleCode;
+
+    public $infos;
+
+    public function __construct($code)
+    {
+        $info = $this->infos[$code];
+        $code = $info['statusCode'].$this->moduleCode.$code;
+
+        parent::__construct($statusCode, $info['message'], null, array(), $code);
+    }
 }
--- src/Biz/User/UserException.php
+++ src/Biz/User/UserException.php
@@ -2,9 +2,20 @@
 
 namespace AppBundle\Common\Exception;
 
-use AppBundle\Common\Exception\AbstractException;
-
-class NewException extends AbstractException
+class UserException 
 {
-    
+    public $moduleCode = 01;
+
+    const UN_LOGIN = 01;
+
+    public $messages = array(
+        01 => array(
+            'statusCode' => 403,
+            'message' => '用户未登录'
+        ),
+        02 => array(
+            'statusCode' => 404,
+            'message' => '限制登录'
+        ), 
+    );
 }
