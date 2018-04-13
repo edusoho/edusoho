838296375e3d400624403cb3019c5cbb28e1373f
chenwei <chenwei@howzhi.com>
2018-04-13 18:06:24 +0800

    test: add unit test

--- src/AppBundle/Common/RandMachine.php
+++ src/AppBundle/Common/RandMachine.php
@@ -11,6 +11,11 @@ class RandMachine
         return empty(self::$mockedRand) ? rand() : self::$mockedRand;
     }
 
+    public static function uniqidWithMtRand()
+    {
+        return empty(self::$mockedRand) ? uniqid(mt_rand()) : self::$mockedRand;
+    }
+
     public static function setMockedRand($rand)
     {
         self::$mockedRand = $rand;
