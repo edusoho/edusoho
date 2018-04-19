eebd9aadbb8cd6fcdcb139a080659570d06c28dd
qichen <qichen@howzhi.com>
2018-04-19 21:22:19 +0800

    feat: #38321 refactor-course-distributor

--- src/Biz/Distributor/Common/DistributorCookieToolkit.php
+++ src/Biz/Distributor/Common/DistributorCookieToolkit.php
@@ -7,6 +7,15 @@ use AppBundle\Common\TimeMachine;
 
 class DistributorCookieToolkit
 {
+    const USER = 'user';
+
+    const COURSE = 'course';
+
+    const TYPE = array(
+        self::USER,
+        self::COURSE,
+    );
+
     public static function setTokenToCookie($response, $token, $cookieName, $liveTime = 604800)
     {
         if ($liveTime) {
@@ -30,8 +39,13 @@ class DistributorCookieToolkit
         return $fields;
     }
 
-    public static function clearCookieToken($request, $response, $cookieName)
+    public static function clearCookieToken($request, $response, $cookieName = null)
     {
+        if (empty($cookieName)) {
+            foreach (self::TYPE as $type) {
+                self::clearCookieToken($request, $response, $type);
+            }
+        }
         $distributorTokenCookie = $request->cookies->get("distributor-{$cookieName}-token");
         if (!empty($distributorTokenCookie)) {
             $response->headers->setCookie(new Cookie("distributor-{$cookieName}-token", ''));
--- src/Biz/Distributor/Util/DistributorUtil.php
+++ src/Biz/Distributor/Util/DistributorUtil.php
@@ -2,6 +2,10 @@
 
 namespace Biz\Distributor\Util;
 
+use Biz\Distributor\Common\DistributorCookieToolkit;
+use AppBundle\Common\Exception\InvalidArgumentException;
+use Topxia\Service\Common\ServiceKernel;
+
 class DistributorUtil
 {
     public static function getType($token)
@@ -9,5 +13,48 @@ class DistributorUtil
         $splitedStr = explode(':', $token);
 
         return $splitedStr[1] ? $splitedStr[1] : 'course';
+    }
+
+    public static function generateTokenByType($type, $params)
+    {
+        if (!in_array($type, DistributorCookieToolkit::TYPE)) {
+            return false;
+        }
+
+        switch ($type) {
+            case DistributorCookieToolkit::USER:
+                $data = array(
+                    'merchant_id' => '123',
+                    'agency_id' => '22221',
+                    'coupon_price' => $params['couponPrice'],
+                    'coupon_expiry_day' => $params['couponExpiryDay'],
+                );
+                $tokenExpireDateNum = strtotime($params['tokenExpireDateStr']);
+                break;
+            case DistributorCookieToolkit::COURSE:
+                $data = array(
+                    'org_id' => $params['orgId'],
+                    'type' => $params['type'],
+                    'course_id' => $params['courseId'],
+                    'merchant_id' => '123',
+                );
+                $tokenExpireDateNum = null;
+                break;
+            default:
+                throw new InvalidArgumentException('invalid type!');
+                break;
+        }
+
+        return self::getDistributorUserService()->encodeToken($data, $tokenExpireDateNum);
+    }
+
+    protected static function getDistributorUserService()
+    {
+        return self::getServiceKernel()->getBiz()->service('Distributor:DistributorUserService');
+    }
+
+    protected static function getServiceKernel()
+    {
+        return ServiceKernel::instance();
     }
 }
