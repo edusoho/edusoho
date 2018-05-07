fd78601dc73eb3bc13b056014394980b1fff0867
wangdanyue <wangdanyue@howzhi.com>
2018-05-07 19:33:34 +0800

    feat: #32220 add phpunit test

---
+++ tests/Unit/AppBundle/Extensions/DataTag/OrgDataTagTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit\AppBundle\Extensions\DataTag;
+
+use Biz\BaseTestCase;
+use AppBundle\Extensions\DataTag\OrgDataTag;
+
+class OrgDataTagTest extends BaseTestCase
+{
+    public function testGetDataById()
+    {
+        $mockData = array('id' => 1);
+        $this->mockBiz('Org:OrgService', array(
+            array(
+                'functionName' => 'getOrg',
+                'returnValue' => $mockData
+            )
+        ));
+        $dataTag = new OrgDataTag();
+        $result = $dataTag->getData(array('id' => 1));
+
+        $this->assertArrayEquals($mockData, $result);
+    }
+
+    public function testGetDataByOrgCode()
+    {
+        $mockData = array('id' => 2, 'orgCode' => 'codename');
+        $this->mockBiz('Org:OrgService', array(
+            array(
+                'functionName' => 'getOrgByOrgCode',
+                'returnValue' => $mockData
+            )
+        ));
+        $dataTag = new OrgDataTag();
+        $result = $dataTag->getData(array('orgCode' => 'codename'));
+
+        $this->assertArrayEquals($mockData, $result);
+    }
+
+    public function testGetDataEmptyArgument()
+    {
+        $dataTag = new OrgDataTag();
+        $result = $dataTag->getData(array());
+
+        $this->assertNull($result);
+    }
+}
