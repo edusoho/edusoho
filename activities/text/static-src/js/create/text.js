08fc5541995d347fd52fcd94a063ab4626cd742f
zhongyunchang <zhongyunchang@yeah.net>
2018-05-08 20:08:31 +0800

    fix: #37912 check confilct

--- activities/text/static-src/js/create/text.js
+++ activities/text/static-src/js/create/text.js
@@ -1,5 +1,5 @@
 // import { initEditor } from "app/js/activity-manage/editor";
-import { initEditor } from '../editor';
+// import { initEditor } from '../editor';
 import 'store';
 export default class Text {
   constructor(props) {
