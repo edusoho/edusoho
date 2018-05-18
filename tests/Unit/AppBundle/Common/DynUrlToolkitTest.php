dc339887e04becffbd3d2817d87493537b7fee31
wangdanyue <wangdanyue@howzhi.com>
2018-05-18 11:44:13 +0800

    feat: #39139 fix DynUrlToolkitTest

--- tests/Unit/AppBundle/Common/DynUrlToolkitTest.php
+++ tests/Unit/AppBundle/Common/DynUrlToolkitTest.php
@@ -9,21 +9,21 @@ class DynUrlToolkitTest extends BaseTestCase
 {
     public function testGetUrlWithSupportedType()
     {
-        $this->biz['course-manage/create-modal'] = array(
-            'one-to-one' => 'plugins/course-manage/create-modal/one-to-one.html.twig',
+        $this->biz['template_extension.live'] = array(
+            'course-manage/create-modal' => 'course-manage/create-modal/one-to-one.html.twig',
         );
 
-        $result = DynUrlToolkit::getUrl($this->biz, 'course-manage/create-modal.html.twig', array('type' => 'one-to-one'));
-        $this->assertEquals('plugins/course-manage/create-modal/one-to-one.html.twig', $result);
+        $result = DynUrlToolkit::getUrl($this->biz, 'course-manage/create-modal.html.twig', array('type' => 'live'));
+        $this->assertEquals('course-manage/create-modal/one-to-one.html.twig', $result);
     }
 
     public function testGetUrlWithUnsupportedType()
     {
-        $this->biz['course-manage/create-modal'] = array(
-            'one-to-one' => 'plugins/course-manage/create-modal/one-to-one.html.twig',
+        $this->biz['template_extension.reservation'] = array(
+            'course-manage/create-modal' => 'plugin/course-manage/create-modal/one-to-one.html.twig',
         );
 
-        $result = DynUrlToolkit::getUrl($this->biz, 'course-manage/create-modal.html.twig', array('type' => 'one'));
+        $result = DynUrlToolkit::getUrl($this->biz, 'course-manage/create-modal.html.twig', array('type' => 'normal'));
         $this->assertEquals('course-manage/create-modal.html.twig', $result);
     }
 }
