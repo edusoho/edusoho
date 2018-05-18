2032bc7c17c2e9a850932b0ea557c9638fc17579
LianJion <1995lian1002@gmail.com>
2018-05-18 10:32:44 +0800

    feat: #38436 重置教学课表样式

--- app/Resources/static-src/app/js/my/teaching-calendar/live-calendar.js
+++ app/Resources/static-src/app/js/my/teaching-calendar/live-calendar.js
@@ -1,30 +1,23 @@
 import CustomFullCalendar from 'app/common/calendar/custom-full-calendar';
 import LiveTooltipComp from 'app/common/calendar/comp/tooltip/live-tooltip-comp';
 import ClickComp from 'app/common/calendar/comp/click-comp';
-import SelectComp from 'app/common/calendar/comp/select-comp';
-import RightClickComp from 'app/common/calendar/comp/right-click-comp';
 import Api from 'common/api';
 
-
 new CustomFullCalendar({
-  'selectable': true,
   'calendarContainer': '#calendar',
-  'data': [],
-  // 'dataApi': Api.teacherLiveCourse.search, //需要使用 common/api/index.js 指定的路由
+  'dataApi': Api.teacherLiveCourse.search, //需要使用 common/api/index.js 指定的路由
   'attrs': {
     'title': 'title',
     'start': 'startTime',
     'end': 'endTime'
   },
-  'dateParams': { 'start': 'startTime_GE', 'end': 'endTime_LT' },
+  'dateParams': {'start': 'startTime_GE', 'end': 'endTime_LT'},
   'currentTime': $('#todayDateStr').html(),
   'components': [
-    new SelectComp(),
-    // new RightClickComp(),
-    // new LiveTooltipComp(),
-    // new ClickComp('{url}') //routing course_show
+    new LiveTooltipComp(),
+    new ClickComp('{url}') //routing course_show
   ],
-  'defaultView': 'agendaWeek', // 'agendaWeek'
+  'defaultView': 'agendaWeek' // 'agendaWeek'
 });
 
 
