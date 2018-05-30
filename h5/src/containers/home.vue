d7b21a46897c0eb5a2d86741985405437085d586
GrowthCoder <hdu_gaoting@126.com>
2018-05-30 16:49:29 +0800

    vw

--- h5/src/containers/home.vue
+++ h5/src/containers/home.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>
-    home
+  <div class="home">
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="shop">标签</van-tabbar-item>
+      <van-tabbar-item icon="chat" dot>标签</van-tabbar-item>
+      <van-tabbar-item icon="records" info="5">标签</van-tabbar-item>
+      <van-tabbar-item icon="gold-coin" info="20">标签</van-tabbar-item>
+    </van-tabbar>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .home {
+
+  }
+</style>
+
