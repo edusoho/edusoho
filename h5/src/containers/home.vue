5e4f487cad9e08a05749fd2963941a9d89adf48d
liushihao <liushihao@howzhi.com>
2018-05-31 11:45:58 +0800

    tabbar暂存

--- h5/src/containers/home.vue
+++ h5/src/containers/home.vue
@@ -2,5 +2,21 @@
   <div>
     home
     <router-view></router-view>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="shop">发现</van-tabbar-item>
+      <van-tabbar-item icon="chat" dot>学习</van-tabbar-item>
+      <van-tabbar-item icon="records" info="5">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        active: 0
+      };
+    },
+  }
+
+</script>
