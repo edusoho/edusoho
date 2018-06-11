ebdbf52d82669216a552e50070e084df164470bf
liushihao <liushihao@howzhi.com>
2018-06-11 15:28:45 +0800

    组件完成

--- h5/src/containers/components/e-promotion/e-promotion.vue
+++ h5/src/containers/components/e-promotion/e-promotion.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="e-promotion">
-
+    <img class="e-promotion__img" v-bind:src="promotion.image" @click="jumpTo(link.id)">
   </div>
 </template>
 
 <script>
   export default {
+    props: {
+      promotion: {
+        type: Object,
+        default: {}
+      }
+    },
     data () {
       return {
+        link: this.promotion.link
       };
     },
+    methods: {
+      jumpTo(courseId) {
+        this.$router.push({
+          path: `/course/${courseId}`
+        });
+      }
+    }
   }
 </script>
