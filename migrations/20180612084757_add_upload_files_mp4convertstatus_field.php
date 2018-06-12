78bfff0fd39767d47379e5d3cba516eb2ac68542
lvliujie <lvliujie@howzhi.com>
2018-06-12 11:19:17 +0800

    feat: #39265 upload_files新增mp4转码状态

---
+++ migrations/20180612084757_add_upload_files_mp4convertstatus_field.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddUploadFilesMp4convertstatusField extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $container = $this->getContainer();
+        $db = $container['db'];
+
+        if (!$this->isFieldExist('upload_files', 'mp4ConvertStatus')) {
+            $db->exec("ALTER TABLE `upload_files` ADD `mp4ConvertStatus` enum('none','waiting','doing','success','error') NOT NULL DEFAULT 'none' COMMENT '视频转mp4的状态';");
+        }
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $container = $this->getContainer();
+        $db = $container['db'];
+
+        if ($this->isFieldExist('upload_files', 'mp4ConvertStatus')) {
+            $db->exec('ALTER TABLE `upload_files` DROP `mp4ConvertStatus`;');
+        }
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $biz = $this->getContainer();
+
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $biz['db']->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+}
