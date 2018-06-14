61cc07f5fff3ba79ac0d1c7d15ba0c508bcc4d68
GrowthCoder <hdu_gaoting@126.com>
2018-06-14 13:45:43 +0800

    登录缺省页

--- h5/src/containers/learning/index.vue
+++ h5/src/containers/learning/index.vue
@@ -7,6 +7,7 @@
     beforeRouteEnter(to, from, next) {
       // 判断是否登录
       const isLogin = !!store.state.token;
+
       !isLogin ? next({name: 'prelogin',query: { redirect: to.name }}) : next();
     }
   }
