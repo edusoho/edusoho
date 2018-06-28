f1935163b8991d1dd3205307ef41b8a083cc0ac8
chenwei <chenwei@howzhi.com>
2018-06-28 20:39:32 +0800

    refactor: develop分支报错的问题

--- src/Biz/WeChat/Service/WeChatAppService.php
+++ src/Biz/WeChat/Service/WeChatAppService.php
@@ -5,11 +5,13 @@ namespace Biz\WeChat\Service;
 interface WeChatAppService
 {
     /**
-     * @return 小程序助手插件的状态
-     *                                        array(
-     *                                        'purchased' => 是否已购买,
-     *                                        'installed' => 是否已安装
-     *                                        )
+     * 获取小程序插件状态
+     *
+     * @return
+     * array(
+     *  'purchased' => 是否已购买,
+     *  'installed' => 是否已安装
+     * )
      */
     public function getWeChatAppStatus();
 }
