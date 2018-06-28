e451c278c2ae3cff46d67fef518c41d2dceb5713
LianJion <1995lian1002@gmail.com>
2018-06-28 17:34:50 +0800

    fix: #40941 修改ipad作业，试卷，练习滚动和modal弹出

--- app/Resources/static-src/app/js/activity-manage/homework/picker/index.js
+++ app/Resources/static-src/app/js/activity-manage/homework/picker/index.js
@@ -3,7 +3,8 @@ import BatchSelect from 'app/common/widget/batch-select';
 import SelectLinkage from 'app/js/question-manage/widget/select-linkage.js';
 
 let $questionPickerBody = $('#question-picker-body',window.parent.document);
-new QuestionPicker($questionPickerBody , $('#step2-form'));
+const $step2Form = $('#task-create-content-iframe').contents().find('#step2-form');
+new QuestionPicker($questionPickerBody , $step2Form);
 new BatchSelect($questionPickerBody);
 
 new SelectLinkage($('[name="courseId"]',window.parent.document),$('[name="lessonId"]',window.parent.document));
