f5ea75a5567e4492fb627edb26d80039efe0d9ba
liushihao <liushihao@howzhi.com>
2018-06-28 17:29:52 +0800

    学习页大致结构，等待数据再调整

---
+++ h5/src/containers/learning/emptyCourse/emptyCourse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="empty-course">
+    <img class="empty-course__img" src="/static/images/courseEmpty.png" alt="">
+    <p class="empty-course__text">暂无课程</p>
+    <div class="empty-course__btn">+ 更多好课等您加入</div>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+
+      };
+    },
+  }
+
+</script>
--- h5/src/containers/learning/index.vue
+++ h5/src/containers/learning/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>learning</div>
+  <div class="e-learn">
+    <emptyCourse></emptyCourse>
+    <lazyLoading></lazyLoading>
+  </div>
 </template>
 <script>
   import store from '@/store';
+  import emptyCourse from './emptyCourse/emptyCourse.vue';
+  import lazyLoading from '../components/e-lazy-loading/e-lazy-loading.vue';
   export default {
     beforeRouteEnter(to, from, next) {
       // 判断是否登录
       const isLogin = !!store.state.token;
 
       !isLogin ? next({name: 'prelogin',query: { redirect: to.name }}) : next();
+    },
+    components: {
+      emptyCourse,
+      lazyLoading,
     }
   }
 </script>
