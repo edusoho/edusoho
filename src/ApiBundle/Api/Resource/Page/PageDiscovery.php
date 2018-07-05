8937fa96d12f157b999a7273b951c96dbda98c0f
shenchupeng <shenchupeng@howzhi.com>
2018-07-05 09:13:02 +0800

    feat: #41356 homepage api

---
+++ src/ApiBundle/Api/Resource/Page/PageDiscovery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ApiBundle\Api\Resource\Page;
+
+use ApiBundle\Api\Annotation\ApiConf;
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Exception\ErrorCode;
+use ApiBundle\Api\Resource\AbstractResource;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class PageDiscovery extends AbstractResource
+{
+    /**
+     * @ApiConf(isRequiredAuth=false)
+     */
+    public function search(ApiRequest $request, $portal)
+    {
+        if (!in_array($portal, array('h5', 'miniprogram'))) {
+            throw new BadRequestHttpException('Portal is error', null, ErrorCode::INVALID_ARGUMENT);
+        }
+        
+        $setting = array('hot_course_list', 'recommended_course_list');
+        $conditions = array('parentId' => 0, 'status' => 'published');
+        $result = array();
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseSetService
+     */
+    protected function getCourseSetService()
+    {
+        return $this->service('Course:CourseSetService');
+    }
+}
