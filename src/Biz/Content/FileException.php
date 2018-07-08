baf2db2875e7adfefa6ed71a3701b8a42a995f78
lvliujie <lvliujie@howzhi.com>
2018-07-08 14:45:34 +0800

    feat: #41467 文件格式

--- src/Biz/Content/FileException.php
+++ src/Biz/Content/FileException.php
@@ -13,12 +13,10 @@ class FileException extends AbstractException
     const FILE_NOT_UPLOAD = 5001003;
     const FILE_HANDLE_ERROR = 5001004;
 
-
-
     public $messages = array(
         4041001 => 'exception.file.not_found',
         4031002 => 'exception.file.group_invalid',
         5001003 => 'exception.file.not_upload',
-        5001004 => 'exception.file.handle_error'
+        5001004 => 'exception.file.handle_error',
     );
 }
