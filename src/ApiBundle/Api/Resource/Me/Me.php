23b664f13b0290d4be8fdf8837f1740dd0dde2ea
lvliujie <lvliujie@howzhi.com>
2018-07-09 11:39:59 +0800

    feat: #41467 头像修改切换到service中去

--- src/ApiBundle/Api/Resource/Me/Me.php
+++ src/ApiBundle/Api/Resource/Me/Me.php
@@ -6,18 +6,14 @@ use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use Biz\User\Service\UserService;
 use VipPlugin\Biz\Vip\Service\VipService;
-use AppBundle\Common\FileToolkit;
-use Symfony\Component\HttpFoundation\File\File;
-use Biz\Content\FileException;
 
 class Me extends AbstractResource
 {
     public function get(ApiRequest $request)
     {
         $user = $this->getUserService()->getUser($this->getCurrentUser()->getId());
-        $profile = $this->getUserService()->getUserProfile($user['id']);
-        $user = array_merge($profile, $user);
         $this->appendUser($user);
+
         return $user;
     }
 
@@ -26,74 +22,12 @@ class Me extends AbstractResource
         $user = $this->getUserService()->getUser($this->getCurrentUser()->getId());
         $fields = $request->request->all();
         if (isset($fields['avatarId'])) {
-            $this->updateAvatar($user, $fields['avatarId']);
+            $user = $this->getUserService()->changeAvatarByFileId($user['id'], $fields['avatarId']);
         }
-        $profile = $this->getUserService()->updateUserProfile($user['id'], $fields, false);
-        $user = array_merge($profile, $user);
+        $this->getUserService()->updateUserProfile($user['id'], $fields, false);
         $this->appendUser($user);
 
         return $user;
-    }
-
-    protected function updateAvatar($user, $fileId)
-    {
-        if (empty($fileId)) {
-            throw FileException::FILE_NOT_FOUND();
-        }
-        list($pictureUrl, $naturalSize, $scaledSize) = $this->getFileService()->getImgFileMetaInfo($fileId, 270, 270);
-
-        $options = $this->createImgCropOptions($naturalSize, $scaledSize);
-        $record = $this->getFileService()->getFile($fileId);
-        if (empty($record)) {
-            throw FileException::FILE_NOT_FOUND();
-        }
-        $parsed = $this->getFileService()->parseFileUri($record['uri']);
-
-        $filePaths = FileToolKit::cropImages($parsed['fullpath'], $options);
-
-        $fields = array();
-        foreach ($filePaths as $key => $filePath) {
-            $file = $this->getFileService()->uploadFile('user', new File($filePath));
-            $fields[] = array(
-                'type' => $key,
-                'id' => $file['id'],
-            );
-        }
-
-        if (isset($options['deleteOriginFile']) && 0 == $options['deleteOriginFile']) {
-            $fields[] = array(
-                'type' => 'origin',
-                'id' => $record['id'],
-            );
-        } else {
-            $this->getFileService()->deleteFileByUri($record['uri']);
-        }
-
-        if (empty($fields)) {
-            throw FileException::FILE_HANDLE_ERROR();
-        }
-        $this->getUserService()->changeAvatar($user['id'], $fields);
-    }
-
-    private function createImgCropOptions($naturalSize, $scaledSize)
-    {
-        $options = array();
-
-        $options['x'] = 0;
-        $options['y'] = 0;
-        $options['x2'] = $scaledSize->getWidth();
-        $options['y2'] = $scaledSize->getHeight();
-        $options['w'] = $naturalSize->getWidth();
-        $options['h'] = $naturalSize->getHeight();
-
-        $options['imgs'] = array();
-        $options['imgs']['large'] = array(200, 200);
-        $options['imgs']['medium'] = array(120, 120);
-        $options['imgs']['small'] = array(48, 48);
-        $options['width'] = $naturalSize->getWidth();
-        $options['height'] = $naturalSize->getHeight();
-
-        return $options;
     }
 
     protected function appendUser(&$user)
@@ -122,10 +56,5 @@ class Me extends AbstractResource
     protected function getUserService()
     {
         return $this->service('User:UserService');
-    }
-
-    protected function getFileService()
-    {
-        return $this->service('Content:FileService');
     }
 }
--- src/ApiBundle/Api/Resource/Me/MeNickname.php
+++ src/ApiBundle/Api/Resource/Me/MeNickname.php
@@ -5,7 +5,6 @@ namespace ApiBundle\Api\Resource\Me;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use Biz\User\Service\UserService;
-use VipPlugin\Biz\Vip\Service\VipService;
 use ApiBundle\Api\Annotation\ResponseFilter;
 use Biz\System\SettingException;
 use Biz\Sensitive\SensitiveException;
@@ -20,12 +19,8 @@ class MeNickname extends AbstractResource
     {
         $this->checkNickname($nickname);
         $this->getAuthService()->changeNickname($this->getCurrentUser()->getId(), $nickname);
-        $user = $this->getUserService()->getUser($this->getCurrentUser()->getId());
-        $profile = $this->getUserService()->getUserProfile($user['id']);
-        $user = array_merge($profile, $user);
-        $this->appendUser($user);
 
-        return $user;
+        return $this->getUserService()->getUser($this->getCurrentUser()->getId());
     }
 
     protected function checkNickname($nickname)
@@ -57,29 +52,6 @@ class MeNickname extends AbstractResource
                     break;
             }
         }
-    }
-
-    protected function appendUser(&$user)
-    {
-        $profile = $this->getUserService()->getUserProfile($user['id']);
-        $user = array_merge($profile, $user);
-
-        if ($this->isPluginInstalled('vip')) {
-            $vip = $this->service('VipPlugin:Vip:VipService')->getMemberByUserId($user['id']);
-            $level = $this->service('VipPlugin:Vip:LevelService')->getLevel($vip['levelId']);
-            if ($vip) {
-                $user['vip'] = array(
-                    'levelId' => $vip['levelId'],
-                    'vipName' => $level['name'],
-                    'deadline' => date('c', $vip['deadline']),
-                    'seq' => $level['seq'],
-                );
-            } else {
-                $user['vip'] = null;
-            }
-        }
-
-        return $user;
     }
 
     protected function getUserService()
