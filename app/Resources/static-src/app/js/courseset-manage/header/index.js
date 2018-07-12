8f80a4ed33f114d4a6139aedfd474044d3672029
LianJion <1995lian1002@gmail.com>
2018-07-12 15:09:34 +0800

    fix: #41032 删除多余的发布代码

--- app/Resources/static-src/app/js/courseset-manage/header/index.js
+++ app/Resources/static-src/app/js/courseset-manage/header/index.js
@@ -6,21 +6,6 @@ const info = {
   success: 'course_set.manage.publish_success_hint',
   fail: 'course_set.manage.publish_fail_hint'
 };
-export const publishCourseSet = () => {
-  $('body').on('click', '.course-publish-btn', function(evt) {
-    if (!confirm(Translator.trans('course_set.manage.publish_hint'))) {
-      return;
-    }
-    $.post($(evt.target).data('url'), function(data) {
-      if (data.success) {
-        notify('success', Translator.trans('course_set.manage.publish_success_hint'));
-        location.reload();
-      } else {
-        notify('danger', Translator.trans('course_set.manage.publish_fail_hint') + ':' + Translator.trans(data.message), { delay: 5000 });
-      }
-    });
-  });
-};
 
 const $originHeader = $('.js-origin-header');
 const $originHeaderContent = $('.js-origin-header-content');
