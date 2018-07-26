407ab576207d0a54d1b333cb2f9a6a59e0dd673c
LianJion <1995lian1002@gmail.com>
2018-07-26 13:56:39 +0800

    fix: #42380 普通课程的notify

--- app/Resources/static-src/app/js/course-manage/live-replay/index.js
+++ app/Resources/static-src/app/js/course-manage/live-replay/index.js
@@ -1,18 +1,16 @@
-import notify from 'common/notify';
-
 $('.js-generate-replay').on('click', (event) => {
   const $this = $(event.currentTarget);
   const url = $this.data('url');
   if (!url) return;
   Promise.resolve($.post(url))
     .then(success => {
-      notify('success', Translator.trans('course.manage.live_replay_generate_success'));
+      cd.message({ type: 'success', message: Translator.trans('course.manage.live_replay_generate_success')});
       window.location.reload();
     })
     .catch(response => {
       const error = JSON.parse(response.responseText);
       const code = error.code;
       const message = error.error;
-      notify('danger', Translator.trans('course.manage.live_replay_generate_error'));
+      cd.message({ type: 'danger', message: Translator.trans('course.manage.live_replay_generate_error')});
     });
 });
