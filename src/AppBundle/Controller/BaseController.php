f086bb763ca11e0a977b26abbbd101382d9179e8
tianshengjie <tianshengjie@howzhi.com>
2018-08-01 16:55:09 +0800

    feat: #42660 baseController 重写rednerview的方法

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -309,7 +309,7 @@ class BaseController extends Controller
 
         throw new \Exception();
     }
-    
+
     /**
      * 安全的重定向.
      *
@@ -335,6 +335,16 @@ class BaseController extends Controller
         }
 
         return parent::render($view, $parameters, $response);
+    }
+
+    public function renderView($view, array $parameters = array())
+    {
+        $biz = $this->getBiz();
+        foreach ($biz['render_view_resolvers'] as $resolver) {
+            $view = $resolver->generateRenderView($view, $parameters);
+        }
+
+        return parent::renderView($view, $parameters);
     }
 
     /**
