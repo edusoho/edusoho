421c8580d391c9565c8be134cd426458c7bad6f2
lvliujie <lvliujie@howzhi.com>
2018-08-02 12:01:14 +0800

    feat: #42209 h5路由转换基础结构

---
+++ src/AppBundle/Listener/KernelH5RequestListener.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use ApiBundle\ApiBundle;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Codeages\Biz\Framework\Service\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class KernelH5RequestListener
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if ($event->getRequestType() != HttpKernelInterface::MASTER_REQUEST) {
+            return;
+        }
+
+        if ($request->getMethod() !== 'GET') {
+            return;
+        }
+
+        $pathInfo = $request->getPathInfo();
+        $route = $this->container
+            ->get('router')
+            ->getMatcher()
+            ->match($pathInfo);
+
+        $h5 = empty($route['_h5']) ? array() : $route['_h5'];
+        if (empty($h5)) {
+            return ;
+        }
+
+        $wapSetting = $this->getSettingService()->get('wap');
+        if (empty($wapSetting['enabled'])) {
+            return ;
+        }
+        $params = $request->query->all();
+        $query = http_build_query($params);
+        $url = empty($query) ? '/h5/index.html#'.$pathInfo : '/h5/index.html?'.$query.'#'.$pathInfo;
+        $response = new RedirectResponse($url);
+        $event->setResponse($response);
+    }
+
+    
+
+
+
+
+    protected function getSettingService()
+    {
+        return $this->getBiz()->service('System:SettingService');
+    }
+
+    protected function trans($id, array $parameters = array())
+    {
+        return $this->container->get('translator')->trans($id, $parameters);
+    }
+
+    protected function getBiz()
+    {
+        return $this->container->get('biz');
+    }
+}
