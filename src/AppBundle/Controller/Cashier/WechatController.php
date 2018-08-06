e1419f75ccfbe7bb5a199301ed35fce02cedaad2
lvliujie <lvliujie@howzhi.com>
2018-08-06 17:25:31 +0800

    feat: #41354 response对象返回

--- src/AppBundle/Controller/Cashier/WechatController.php
+++ src/AppBundle/Controller/Cashier/WechatController.php
@@ -101,7 +101,7 @@ class WechatController extends PaymentController
         $params['openid'] = $openid;
         $params = http_build_query($params);
 
-        return $this->createRedirectResponse('/h5/#/weixin_pay?'.$params);
+        return $this->redirect('/h5/#/weixin_pay?'.$params);
     }
 
     public function wechatAppMwebTradeAction(Request $request)
