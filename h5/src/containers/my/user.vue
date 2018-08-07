0f570105905108a7ff06bf8d18656596e59bd3fb
gaoyong <gaoyong@howzhi.com>
2018-08-07 21:53:10 +0800

    feat: 头像点击可以进入设置

--- h5/src/containers/my/user.vue
+++ h5/src/containers/my/user.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="user">
     <div class="user-section">
-      <img class='user-img' :src="user.avatar.large" />
+      <router-link to="/my/setting">
+        <img class='user-img' :src="user.avatar.large" />
+      </router-link>
       <span class='user-name'>{{ user.nickname }}</span>
       <router-link to="/my/setting">
         <img class='user-setting' src='static/images/setting.png'>
