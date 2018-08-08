0f2d794f2c1aafb2e04d1cf594710b468e5eb337
gaoyong <gaoyong@howzhi.com>
2018-08-08 14:35:07 +0800

    课程详情标题

--- h5/src/store/mutation-types.js
+++ h5/src/store/mutation-types.js
@@ -4,6 +4,7 @@ export const USER_LOGIN = 'USER_LOGIN';
 export const USER_LOGOUT = 'USER_LOGOUT';
 export const USER_INFO = 'USER_INFO';
 export const GET_SETTINGS = 'GET_SETTINGS';
+export const SET_NAVBAR_TITLE = 'SET_NAVBAR_TITLE';
 
 // course detail
 export const SELECTED_PLAN_ID = 'selectedPlanId';
