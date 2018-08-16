f84d093d2eff0719d9fa7e4e17b8a2d18b5d98e6
gaoyong <gaoyong@howzhi.com>
2018-08-16 17:02:33 +0800

    打包方案: vendor 提取方案修改，保证hash不变

--- h5/build/webpack.prod.conf.js
+++ h5/build/webpack.prod.conf.js
@@ -84,17 +84,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         new webpack.optimize.ModuleConcatenationPlugin(),
         // split vendor js into its own file
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks (module) {
-                // any required modules inside node_modules are extracted to vendor
-                return (
-                    module.resource &&
-                    /\.js$/.test(module.resource) &&
-                    module.resource.indexOf(
-                        path.join(__dirname, '../node_modules')
-                    ) === 0
-                )
-            }
+            names: ['vendor']
         }),
         // extract webpack runtime and module manifest to its own file in order to
         // prevent vendor hash from being updated whenever app bundle is updated
