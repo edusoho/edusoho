1e9504f7ff7bcf8de5a9a2f28c46fadabce3fa5f
lijingwen <lijingwen@howzhi.com>
2018-08-22 16:40:44 +0800

    delete course

--- h5/admin/store/index.js
+++ h5/admin/store/index.js
@@ -4,7 +4,6 @@ import Vue from 'vue';
 import * as getters from './getters';
 import * as actions from './actions';
 import mutations from './mutations';
-import course from './modules/course';
 
 Vue.use(Vuex);
 
@@ -22,8 +21,5 @@ export default new Vuex.Store({
   state,
   getters,
   actions,
-  mutations,
-  modules: {
-    course
-  }
+  mutations
 });
