3a326c1ea52b615e330207a2db56238e71ad192d
lijingwen <lijingwen@howzhi.com>
2018-08-27 21:52:35 +0800

    添加组件选择静态

--- h5/admin/containers/setting/index.vue
+++ h5/admin/containers/setting/index.vue
@@ -10,6 +10,16 @@
       </module-template>
     </div>
 
+    <div class="find-section clearfix">
+      <div class="section-title">点击添加组件</div>
+      <el-button class="find-section-item" type="" size="medium"
+        v-for="item in moduleItems"
+        @click="addModuleItem"
+        :key="item.type">
+        {{ item.name }}
+      </el-button>
+    </div>
+
     <div class="find-footer">
       <div class="find-footer-item" v-for="item in items"
           :class="{ active: item.name === 'find' }"
@@ -37,6 +47,11 @@ export default {
       modules: [],
       currentModule: 'slide-1',
       items,
+      moduleItems: [
+        { name: '轮播图' },
+        { name: '课程列表' },
+        { name: '图片广告' }
+      ]
     }
   },
   computed: {
@@ -76,6 +91,8 @@ export default {
     },
     activeModule({ moduleId }) {
       this.currentModule = moduleId;
+    },
+    addModuleItem() {
     }
   }
 }
