6e26e4ca65812b7188813905123eba23f5f93704
lijingwen <lijingwen@howzhi.com>
2018-08-28 13:43:27 +0800

    h5增加广告模块

---
+++ h5/src/containers/poster/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="e-poster">
+    <a v-bind:href="posters.link.url">
+      <img class="poster-image" v-bind:src="posters.image.uri">
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      posters: {
+        type: Array,
+        default: []
+      }
+    },
+  }
+</script>
