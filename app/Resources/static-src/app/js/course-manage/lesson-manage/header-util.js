f4157f4953c74d33576f35a35c9e7536a9e2e9b9
chenwei <chenwei@howzhi.com>
2018-08-29 20:00:43 +0800

    feat: #43003 未发布课时隐藏功能

--- app/Resources/static-src/app/js/course-manage/lesson-manage/header-util.js
+++ app/Resources/static-src/app/js/course-manage/lesson-manage/header-util.js
@@ -6,13 +6,20 @@ export const hiddenUnpublishTask = () => {
     const url = $ele.data('url');
     const status = $ele.parent().hasClass('checked') ? 0 : 1;
 
-    $.post(url, { status: status })
-      .success((response) => {
-        cd.message({ type: 'success', message: Translator.trans('site.save_success_hint') });
-      })
-      .error((response) => {
-        cd.message({ type: 'danger', message: response.responseJSON.error.message });
-      });
+    cd.confirm({
+      title: Translator.trans('site.data.delete_title_hint', { 'name': name }),
+      content: Translator.trans('site.data.delete_name_hint', { 'name': name }),
+      okText: Translator.trans('site.confirm'),
+      cancelText: Translator.trans('site.close'),
+    }).on('ok', () => {
+      $.post(url, { status: status })
+        .success((response) => {
+          cd.message({ type: 'success', message: Translator.trans('site.save_success_hint') });
+        })
+        .error((response) => {
+          cd.message({ type: 'danger', message: response.responseJSON.error.message });
+        });
+    });
   });
 };
 
@@ -23,7 +30,7 @@ export const addLesson = () => {
       .success((response) => {
         $('#modal').html('');
         $('#modal').append(response.html);
-        $('#modal').modal({'backdrop':'static','show':true});
+        $('#modal').modal({ 'backdrop': 'static', 'show': true });
       })
       .error((response) => {
         cd.message({ type: 'danger', message: Translator.trans(response.responseJSON.error.message) });
