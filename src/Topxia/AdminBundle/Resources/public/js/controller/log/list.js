51f94820df2ea6da73bef1ee984e51d04f433dc8
liuhaobin <liuhaobin>
2018-08-29 16:44:19 +0800

    fix: #43790 js调整

--- src/Topxia/AdminBundle/Resources/public/js/controller/log/list.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/log/list.js
@@ -17,9 +17,10 @@ define(function(require, exports, module) {
 		});
 
 		$("#switch-operation").on('click', function() {
-			var hasSystemOperation = $('#hasSystemOperation').val();
-			var hasSystemOperation_value = hasSystemOperation == 1 ? 0 : 1;
-			$('#hasSystemOperation').val(hasSystemOperation_value);
+			var hasSystemOperation = $('#hasSystemOperation');
+			var hasSystemOperationValue = hasSystemOperation.val();
+			var hasSystemOperationNewValue = hasSystemOperationValue == 1 ? 0 : 1;
+			hasSystemOperation.val(hasSystemOperationNewValue);
 			var $form = $('#search-form');
 			$form.submit();
 		});
