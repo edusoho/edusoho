41532fbcea294669a38e16a02f4d4fc4f9c7b6d8
tianshengjie <tianshengjie@howzhi.com>
2018-09-10 14:15:52 +0800

    fix: #44190 修复视频任务站外视频保存报错

--- web/activities/video/static-src/js/create/video.js
+++ web/activities/video/static-src/js/create/video.js
@@ -97,8 +97,8 @@ export default class Video {
           file.minute = minute;
           file.second = second;
         }
+        file.id = !file.id ? 0 : file.id;
         $('[name="media"]').val(JSON.stringify(file));
-
       };
       placeMediaAttr(file);
 
