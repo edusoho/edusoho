9ff5a03711d2d352bcaf12e16fb5d1837fa8441d
zhanggaosheng <245085167@howzhi.com>
2018-09-16 22:34:20 +0800

    face-sdk

--- vendor/codeages/qiqiuyun-php-sdk/tests/Service/DrpServiceTest.php
+++ vendor/codeages/qiqiuyun-php-sdk/tests/Service/DrpServiceTest.php
@@ -14,21 +14,21 @@ class DrpServiceTest extends BaseTestCase
     public function testParseRegisterToken_normal()
     {
         $data = array(
-            'agency_id' => '120',
-            'merchant_id' => '110',
-            'coupon_price' => '100',
-            'coupon_expiry_day' => '5', );
+            'agency_id' => '120', 
+            'merchant_id' => '110', 
+            'coupon_price' => '100', 
+            'coupon_expiry_day' => '5');
         $nonce = 'abcedf';
         $time = time();
         ksort($data);
         $dataStr = json_encode($data);
         $signingText = implode("\n", array($nonce, $time, $dataStr));
-        $sign = $this->auth->makeSignature($signingText);
+        $sign =  $this->auth->makeSignature($signingText);
 
         $token = "{$data['merchant_id']}:{$data['agency_id']}:{$data['coupon_price']}:{$data['coupon_expiry_day']}:{$time}:{$nonce}:{$sign}";
         $actualData = $this->getDrpService()->parseRegisterToken($token);
 
-        $this->assertEquals($data['coupon_price'], $actualData['coupon_price']);
+        $this->assertEquals($data['coupon_price'],$actualData['coupon_price']);
         $this->assertEquals($data['coupon_expiry_day'], $actualData['coupon_expiry_day']);
         $this->assertEquals($time, $actualData['time']);
         $this->assertEquals($nonce, $actualData['nonce']);
@@ -38,15 +38,15 @@ class DrpServiceTest extends BaseTestCase
     {
         $data = array(
             'distribution_type' => 'courseOrder',
-            'agency_id' => '120',
-            'merchant_id' => '110',
-            'course_id' => '100', );
+            'agency_id' => '120', 
+            'merchant_id' => '110', 
+            'course_id' => '100');
         $nonce = 'abcedf';
         $time = time();
         ksort($data);
         $dataStr = json_encode($data);
         $signingText = implode("\n", array($nonce, $time, $dataStr));
-        $sign = $this->auth->makeSignature($signingText);
+        $sign =  $this->auth->makeSignature($signingText);
 
         $token = "courseOrder:{$data['course_id']}:{$data['merchant_id']}:{$data['agency_id']}:{$time}:{$nonce}:{$sign}";
         $actualData = $this->getDrpService()->parseToken($token);
@@ -57,8 +57,7 @@ class DrpServiceTest extends BaseTestCase
         $this->assertEquals($nonce, $actualData['nonce']);
     }
 
-    private function getDrpService()
-    {
+    private function getDrpService(){
         return new DrpService($this->auth);
     }
 }
