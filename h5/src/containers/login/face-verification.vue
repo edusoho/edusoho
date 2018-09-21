f8c652e066e0df53d2585e2c02c83c16f7dedefa
lijingwen <lijingwen@howzhi.com>
2018-09-21 17:35:52 +0800

    人脸采样样式及流程完善

--- h5/src/containers/login/face-verification.vue
+++ h5/src/containers/login/face-verification.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="login-face-verification">
-    <div class="verification-tips">
+    <div v-show="tipShow" class="verification-tips">
       <div>即将进行人脸识别认证</div>
       <div class="mt5">请将面部正对摄像头</div>
     </div>
-    <label for="cameraItem" class="btn-open-camera">立即开启摄像头</label>
-    <input id="cameraItem" class="hide" type="file" accept="image/*" @change="openCamera" capture="user">
+    <div v-show="imgShow">
+      <img id="imgContent" alt="">
+      <div>认证中，请稍后...</div>
+    </div>
+    <div v-show="failText">人脸识别多次认证不通过<div class="mt5">请改用其它方式认证或联系管理员</div></div>
+    <div v-show="btnShow">
+      <label for="cameraItem" class="btn-open-camera">{{ btnText }}</label>
+      <input id="cameraItem" class="hide" type="file" accept="image/*" @change="openCamera" capture="user">
+    </div>
   </div>
 </template>
 <script>
@@ -17,7 +24,11 @@ import Api from '@/api';
 export default {
   data() {
     return {
-      isShow: true,
+      tipShow: true,
+      btnShow: true,
+      imgShow: false,
+      failText: false,
+      btnText: '立即开启摄像头',
       uploadParams: {},
     }
   },
@@ -59,32 +70,58 @@ export default {
             message: '人脸识别成功'
           });
           const redirect = decodeURIComponent(this.$route.query.redirect || 'find');
-          var jumpAction = () => {
+          const jumpAction = () => {
             this.$router.push({name: redirect});
           }
-          setTimeout(jumpAction, 2000);
+          setTimeout(jumpAction, 3000);
         } else {
           if (res.lastFailed === 1) {
             Toast.fail({
               duration: 2000,
               message: '人脸识别认证失败，多次不通过'
             });
+            this.failText = true;
+            this.btnShow = false;
+            this.imgShow = false;
+            const toLogin = () => {
+              this.$router.push({
+                name: 'login',
+                query: {
+                  redirect: this.$route.query.redirect || ''
+                }
+              });
+            }
+            setTimeout(toLogin, 2000);
           } else {
             Toast.fail({
               duration: 2000,
               message: '人脸识别认证失败'
             });
+            this.btnShow = true;
+            this.tipShow = true;
+            this.btnText = '重新认证';
+            this.imgShow = false;
           }
         }
       })
     },
     openCamera(e) {
+      this.imgShow = true;
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend= (e) => {
+        document.getElementById('imgContent').src = e.target.result;
+        this.tipShow = false;
+        this.btnShow = false;
+      };
+
       const url = this.uploadParams.uploadUrl;
       const config = {
         headers: { 'Content-Type':'multipart/form-data'},
         interceptor: 'end',
       };
-      const file = e.target.files[0];
+
       const formData = new FormData();
       formData.append('file', file, file.name);
       formData.append('token', this.uploadParams.uploadToken);
@@ -101,10 +138,12 @@ export default {
         };
         Api.finishUploadResult(data).then(res => {
           if (res.success) {
-            Toast.success({
-              duration: 2000,
-              message: '认证中，请稍候...'
-            });
+            // Toast.success({
+            //   duration: 2000,
+            //   message: '认证中，请稍候...'
+            // });
+            this.imgShow = true;
+            this.btnShow = false;
             this.polling();
           } else {
             console.log(res.error.message);
