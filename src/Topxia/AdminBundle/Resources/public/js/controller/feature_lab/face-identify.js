29d766096235e7a4889b991dd9ad0e8398e993e8
LianJion <1995lian1002@gmail.com>
2018-09-29 09:32:33 +0800

    fix: #44912 后台设置

--- src/Topxia/AdminBundle/Resources/public/js/controller/feature_lab/face-identify.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/feature_lab/face-identify.js
@@ -1,22 +1,41 @@
 define(function(require, exports, module) {
-	exports.run = function() {
-		$('#face_identify input[type=checkbox]').click(function() {
-			var data={
-				enabled: 0,
-				app_enabled: 0,
-				wechat_enabled: 0,
-				pc_enabled: 0
-			};
-			var formData = $('#face_identify').serializeArray();
-			if (formData) {
-				for (var i = formData.length - 1; i >= 0; i--) {
-					data[formData[i].name] = formData[i].value;
-				}
-			}
+  exports.run = function() {
 
-			$.post(document.location.href, data, function(data){
-				//document.location.reload();
-			})
-		})
-	}
-})
+    $('input[name=enabled]').click(function() {
+      var $this = $(this);
+      var $parent = $this.parent();
+      var faceIdentify = $this.val();
+      var reverseFaceIdentify = faceIdentify == 1 ? 0 : 1;
+      $.post(document.location.href, { enabled: reverseFaceIdentify }, function(data) { 
+        if ($parent.hasClass('checked')) {
+          
+          $parent.removeClass('checked');
+        } else {
+          $parent.addClass('checked');
+        }
+        $this.val(reverseFaceIdentify);
+        document.location.reload();
+      });
+    });
+
+
+    $('#face_identify input[type=checkbox]').click(function() {
+      var data={
+        app_enabled: 0,
+        wechat_enabled: 0,
+        pc_enabled: 0
+      };
+      var formData = $('#face_identify').serializeArray();
+      console.log(formData);
+      if (formData) {
+        for (var i = formData.length - 1; i >= 0; i--) {
+          data[formData[i].name] = formData[i].value;
+        }
+      }
+
+      $.post(document.location.href, data, function(data){
+        //document.location.reload();
+      });
+    });
+  };
+});
