7eae85df7199bd4f876c953e82ec504685405cf4
LianJion <1995lian1002@gmail.com>
2018-09-30 12:14:05 +0800

    fix: #44912 js参考face-identify

--- src/Topxia/AdminBundle/Resources/public/js/controller/feature_lab/face-identify.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/feature_lab/face-identify.js
@@ -1,5 +1,33 @@
 define(function(require, exports, module) {
   exports.run = function() {
+    $('input[name=enabled]').click(function() {
+      var $this = $(this);
+      var $parent = $this.parent();
+      var faceIdentify = $this.val();
+      var reverseFaceIdentify = faceIdentify == 1 ? 0 : 1;
+      var data = {};
+      $('.js-sts-checkbox').each(function() {
+        if ($(this).is(':checked')) {
+          data[$(this).attr('name')] = $(this).val();
+        }
+      });
+      data.enabled = reverseFaceIdentify;
+      $.post(document.location.href, data, function(data) {
+        if ($parent.hasClass('checked')) {
+          $parent.removeClass('checked');
+          $('.js-sts-checkbox').prop('disabled', true);
+          $('.js-sts-label').addClass('text-muted');
+        } else {
+          $parent.addClass('checked');
+          $('.js-sts-label').removeClass('text-muted');
+          $('.js-sts-checkbox').prop('disabled', false);
+        }
+
+        $this.val(reverseFaceIdentify);
+      });
+    });
+
+
     $('.js-sts-switch input[type=checkbox]').click(function() {
       var data={
         app_enabled: 0,
@@ -9,11 +37,10 @@ define(function(require, exports, module) {
       var formData = $('#face_identify').serializeArray();
       if (formData) {
         for (var i = formData.length - 1; i >= 0; i--) {
-          if(formData[i].name != 'enabled') {
-            data[formData[i].name] = formData[i].value;
-          }
+          data[formData[i].name] = formData[i].value;
         }
       }
+      data.enabled = $('.js-switch-input').val();
       $.post(document.location.href, data, function(data){
         //document.location.reload();
       });
