0641b7f376bcac4700f492408f9036dfaae55e4c
yanrui <yanrui@howzhi.com>
2018-10-08 17:46:52 +0800

    fix: #44985 教学计划重新排序之后-查看教学计划课时任务500

--- src/Biz/Course/Service/Impl/CourseSetServiceImpl.php
+++ src/Biz/Course/Service/Impl/CourseSetServiceImpl.php
@@ -825,11 +825,9 @@ class CourseSetServiceImpl extends BaseService implements CourseSetService
 
     public function updateCourseSetDefaultCourseId($id)
     {
-        $course = $this->getCourseService()->getSeqMinPublishedCourseByCourseSetId($id);
-        //如果计划都尚未发布，则获取第一个创建的
-        if (empty($course)) {
-            $course = $this->getCourseService()->getFirstCourseByCourseSetId($id);
-        }
+        //获取第一个创建的
+        $course = $this->getCourseService()->getFirstCourseByCourseSetId($id);
+
         if (empty($course)) {
             throw $this->createNotFoundException('No Avaliable Course in CourseSet#{$id}');
         }
