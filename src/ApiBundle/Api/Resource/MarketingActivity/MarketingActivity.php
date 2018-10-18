9f2f16cc0c0ca4938d92a65a026e2344e9e50010
lvliujie <lvliujie@howzhi.com>
2018-10-18 15:17:15 +0800

    feat: #100201 活动列表调用远程接口逻辑

--- src/ApiBundle/Api/Resource/MarketingActivity/MarketingActivity.php
+++ src/ApiBundle/Api/Resource/MarketingActivity/MarketingActivity.php
@@ -5,9 +5,7 @@ namespace ApiBundle\Api\Resource\MarketingActivity;
 use ApiBundle\Api\Annotation\Access;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
-use AppBundle\Common\ArrayToolkit;
-use Codeages\RestApiClient\RestApiClient;
-use Codeages\RestApiClient\Specification\JsonHmacSpecification2;
+use Biz\Marketing\MarketingAPIFactory;
 
 class MarketingActivity extends AbstractResource
 {
@@ -19,48 +17,24 @@ class MarketingActivity extends AbstractResource
      */
     public function search(ApiRequest $request)
     {
-        list($offset, $limit) = $this->getOffsetAndLimit($request);
-        //搜索条件：学校id,type,name,status,itemType
+        $conditions = $this->fillParams($request);
         $user = $this->getCurrentUser();
+        $client = MarketingAPIFactory::create();
 
-        $client = $this->createMarketingClient();
-        $entry = $request->query->get('entry');
-        $activities = $client->get('/activities?merchant_user_id='.$user['id'], array());
-
-        exit();
-
-        $activityGroups = ArrayToolkit::group($activities, 'itemType');
-        $activities = array();
-        foreach ($activityGroups as $key => &$groups) {
-            $this->getOCUtil()->multiple($groups, array('itemId'), $key);
-            $activities = array_merge($activities, $groups);
-        }
-
-        $total = 33;
-
-        return $this->makePagingObject($activities, $total, $offset, $limit);
-    }
-
-    private function createMarketingClient()
-    {
-        $storage = $this->getSettingService()->get('storage', array());
-        $developerSetting = $this->getSettingService()->get('developer', array());
-
-        $marketingDomain = !empty($developerSetting['marketing_domain']) ? $developerSetting['marketing_domain'] : 'http://wyx.edusoho.cn';
-
-        $config = array(
-            'accessKey' => $storage['cloud_access_key'],
-            'secretKey' => $storage['cloud_secret_key'],
-            'endpoint' => $marketingDomain.'/merchant',
+        return $client->get(
+            '/activities',
+            $conditions,
+            array('MERCHANT-USER-ID: '.$user['id'])
         );
-        $spec = new JsonHmacSpecification2('sha1');
-        $client = new RestApiClient($config, $spec);
-
-        return $client;
     }
 
-    private function getSettingService()
+    public function fillParams($request)
     {
-        return $this->service('System:SettingService');
+        $conditions = $request->query->all();
+        list($offset, $limit) = $this->getOffsetAndLimit($request);
+        $conditions['page'] = ceil(($offset + 1) / $limit);
+        $conditions['limit'] = $limit;
+
+        return $conditions;
     }
 }
