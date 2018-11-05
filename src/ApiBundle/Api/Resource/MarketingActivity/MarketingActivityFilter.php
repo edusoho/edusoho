5bc5f828cdbd0d81b0f44643e0058652d2610ad9
lvliujie <lvliujie@howzhi.com>
2018-11-05 17:25:41 +0800

    feat: #8311 增加ios跳转链接

--- src/ApiBundle/Api/Resource/MarketingActivity/MarketingActivityFilter.php
+++ src/ApiBundle/Api/Resource/MarketingActivity/MarketingActivityFilter.php
@@ -4,6 +4,7 @@ namespace ApiBundle\Api\Resource\MarketingActivity;
 
 use ApiBundle\Api\Resource\Filter;
 use ApiBundle\Api\Util\Converter;
+use Biz\Marketing\Util\MarketingUtils;
 
 class MarketingActivityFilter extends Filter
 {
@@ -13,14 +14,14 @@ class MarketingActivityFilter extends Filter
 
     protected function publicFields(&$data)
     {
-        $data['originPrice'] = $data['item_origin_price']/100;
+        $data['originPrice'] = $data['item_origin_price'] / 100;
         unset($data['item_origin_price']);
         if (isset($data['owner_price'])) {
-            $data['ownerPrice'] = $data['owner_price']/100;
+            $data['ownerPrice'] = $data['owner_price'] / 100;
             unset($data['owner_price']);
         }
         if (isset($data['member_price'])) {
-            $data['memberPrice'] = $data['member_price']/100;
+            $data['memberPrice'] = $data['member_price'] / 100;
             unset($data['member_price']);
         }
         $data['cover'] = $data['item_cover'];
@@ -34,5 +35,7 @@ class MarketingActivityFilter extends Filter
         Converter::timestampToDate($data['end_time']);
         $data['endTime'] = $data['end_time'];
         unset($data['end_time']);
+        $marketingDomain = MarketingUtils::getMarketingDomain();
+        $data['url'] = $marketingDomain.'/h5/a/groupon/show/'.$data['id'];
     }
 }
