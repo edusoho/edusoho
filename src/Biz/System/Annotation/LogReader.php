65043acd0f3c666b4e695b6cf7bb32f33e292905
kuozhi <kuozhi@kuozhideMBP-2.lan>
2018-11-07 10:51:56 +0800

    fix: #45919 修改logReader

--- src/Biz/System/Annotation/LogReader.php
+++ src/Biz/System/Annotation/LogReader.php
@@ -10,10 +10,7 @@ class LogReader
     public function __construct($cacheDirectory = null)
     {
         $this->cacheDirectory = $cacheDirectory;
-        if ($cacheDirectory) {
-            $fs = new Filesystem();
-            $fs->mkdir($cacheDirectory);
-        }
+        $this->initCacheDir();
     }
 
     public function read($service)
@@ -87,6 +84,7 @@ class LogReader
         if (!$this->cacheDirectory) {
             return;
         }
+        $this->initCacheDir();
 
         $metadata['cached_time'] = time();
 
@@ -94,6 +92,15 @@ class LogReader
         $content = "<?php \n return ".var_export($interceptorData, true).';';
 
         file_put_contents($filePath, $content);
+    }
+    
+    protected function initCacheDir()
+    {
+        $cacheDirectory = $this->cacheDirectory;
+        if ($cacheDirectory && is_dir($cacheDirectory)) {
+            $fs = new Filesystem();
+            $fs->mkdir($cacheDirectory);
+        }
     }
 
     protected function getCacheFilePath($cacheDirectory, $service)
