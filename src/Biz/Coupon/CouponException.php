e25d9891f2715b48a11a7d5d73cea9d5e58f47e6
chelingfeng <chelingfeng@edusoho.com>
2018-11-20 16:36:07 +0800

    bin/fix

--- src/Biz/Coupon/CouponException.php
+++ src/Biz/Coupon/CouponException.php
@@ -7,7 +7,7 @@ use AppBundle\Common\Exception\AbstractException;
 class CouponException extends AbstractException
 {
     const EXCEPTION_COUPON_RECEIVE_FAILED = 5005101;
-    
+
     public $messages = array(
         5005101 => 'exception.coupon.receive.failed',
     );
