65e732997a819929b11b1dbfbf4f1b3bd3748c1d
zhongyunchang <zhongyunchang@yeah.net>
2018-11-21 15:01:33 +0800

    fix: #45482 jwt apply

--- src/AppBundle/Controller/Callback/ESLive/ESLiveBase.php
+++ src/AppBundle/Controller/Callback/ESLive/ESLiveBase.php
@@ -7,14 +7,9 @@ use AppBundle\Controller\BaseController;
 
 class ESLiveBase extends BaseController
 {
-    /**
-     * @return JWTAuth
-     *
-     * @throws \Exception
-     */
     protected function getJWTAuth()
     {
-        $setting = $this->getSettingService()->get('storage', array());
+        $setting = $this->setting('storage', array());
         $accessKey = !empty($setting['cloud_access_key']) ? $setting['cloud_access_key'] : '';
         $secretKey = !empty($setting['cloud_secret_key']) ? $setting['cloud_secret_key'] : '';
 
