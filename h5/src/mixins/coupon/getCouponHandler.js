f7fdafb2f45b46bee508c2b9e1647c94a061bd3e
gaoyong <gaoyong@howzhi.com>
2018-12-10 14:30:22 +0800

    fix: 优惠券去使用逻辑中 课程路由参数id 不正确修复

--- h5/src/mixins/coupon/getCouponHandler.js
+++ h5/src/mixins/coupon/getCouponHandler.js
@@ -1,6 +1,13 @@
 import Api from '@/api';
 import { Toast } from 'vant';
 
+const ALL_TYPE = {
+  classroom: 'classroom',
+  course: 'course',
+  vip: 'vip',
+  all: 'all'
+};
+
 export default {
   methods: {
     couponHandle(coupon) {
@@ -8,7 +15,6 @@ export default {
 
       // 未领券
       if (!coupon.currentUserCoupon) {
-        coupon.currentUserCoupon = true;
         Api.receiveCoupon({
           query: { token }
         }).then(() => {
@@ -21,26 +27,52 @@ export default {
       }
 
       // 已领券
-      const couponType = coupon.targetType;
+      const targetType = coupon.targetType === ALL_TYPE.all ?
+        ALL_TYPE.course : coupon.targetType; // 全站优惠券跳转课程列表页
+      const allType = Object.values(ALL_TYPE);
+
+      if (!allType.includes(targetType)) {
+        Toast.fail(`暂不支持查看：${targetType}类型商品`);
+        return;
+      }
+
       if (coupon.target) {
-        const id = coupon.target.id;
-        this.$router.push({
-          path: `${couponType}/${id}`
+        const targetId = coupon.target.id;
+        this.getPathParams('course', targetId).then(({ id }) => {
+          if (!id) return;
+          this.$router.push({
+            path: `${targetType}/${id}` // course/{id} | classroom/{id}
+          });
         });
         return;
       }
-      if (couponType === 'vip') {
-        Toast.warning('你可以在电脑端或App上购买会员');
+
+      if (targetType === ALL_TYPE.vip) {
+        Toast.fail('你可以在电脑端或App上购买会员');
         return;
       }
-      if (couponType === 'classroom') {
-        this.$router.push({
-          path: 'classroom/explore'
-        });
-        return;
-      }
+
       this.$router.push({
-        path: 'course/explore'
+        path: `${targetType}/explore` // course/explore | classroom/explore
+      });
+    },
+    // 课程的id 需要转换成计划id 跳转到对应计划详情页
+    getPathParams(type, id) {
+      if (type !== ALL_TYPE.course) {
+        return Promise.resolve({ type, id });
+      }
+
+      return Api.getCourseByCourseSet({
+        query: { id }
+      }).then(res => {
+        if (res.length && res[0]) {
+          return { id: res[0].id };
+        }
+        return Promise.reject({
+          error: { message: '当前课程不存在了' }
+        });
+      }).catch(err => {
+        Toast.fail(err.error.message);
       });
     }
   }
