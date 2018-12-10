01b0dffdf8302e2c4f6172417fc8f4ba4dab4e66
lvliujie <lvliujie@howzhi.com>
2018-12-10 16:01:41 +0800

    feat: #8317 修改领取优惠券到主程序

--- src/ApiBundle/Api/Resource/Me/MeCoupon.php
+++ src/ApiBundle/Api/Resource/Me/MeCoupon.php
@@ -6,6 +6,9 @@ use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use Biz\Card\Service\CardService;
 use Biz\Coupon\Service\CouponService;
+use ApiBundle\Api\Annotation\ResponseFilter;
+use Biz\Coupon\CouponException;
+use Biz\Common\CommonException;
 
 class MeCoupon extends AbstractResource
 {
@@ -28,16 +31,38 @@ class MeCoupon extends AbstractResource
     }
 
     /**
-     * @return CouponService
+     * @ResponseFilter(class="ApiBundle\Api\Resource\Coupon\CouponFilter", mode="public")
      */
+    public function add(ApiRequest $request)
+    {
+        $user = $this->getCurrentUser();
+        $token = $request->request->get('token');
+        if (empty($token)) {
+            throw CommonException::ERROR_PARAMETER_MISSING();
+        }
+
+        if (!$this->isPluginInstalled('coupon')) {
+            throw CouponException::EXCEPTION_COUPON_PLUGIN_NOT_INSTALLED();
+        }
+        $result = $this->getCouponBatchService()->receiveCoupon($token, $user['id']);
+
+        if ('success' != $result['code']) {
+            throw CouponException::EXCEPTION_COUPON_RECEIVE_FAILED();
+        }
+
+        return $this->getCouponService()->getCoupon($result['id']);
+    }
+
+    private function getCouponBatchService()
+    {
+        return $this->service('CouponPlugin:Coupon:CouponBatchService');
+    }
+
     private function getCouponService()
     {
         return $this->service('Coupon:CouponService');
     }
 
-    /**
-     * @return CardService
-     */
     private function getCardService()
     {
         return $this->service('Card:CardService');
--- src/Biz/Coupon/CouponException.php
+++ src/Biz/Coupon/CouponException.php
@@ -8,7 +8,10 @@ class CouponException extends AbstractException
 {
     const EXCEPTION_COUPON_RECEIVE_FAILED = 5005101;
 
+    const EXCEPTION_COUPON_PLUGIN_NOT_INSTALLED = 5005102;
+
     public $messages = array(
         5005101 => 'exception.coupon.receive.failed',
+        5005102 => 'exception.coupon.plugin.not.installed',
     );
 }
