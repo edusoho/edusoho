b3b3de96c91760a4bc8194a247aaa360b8934239
LianJion <1995lian1002@gmail.com>
2018-12-11 17:06:30 +0800

    fix: #46203 表单提交的值过滤空格

--- app/Resources/static-src/app/js/settings/approval/index.js
+++ app/Resources/static-src/app/js/settings/approval/index.js
@@ -18,6 +18,18 @@ $('#approval-form').validate({
     backImg: {
       required: Translator.trans('user.fields.idcard_back_placeholder')
     }
+  },
+  submitHandler: function(form) {
+    const $form = $(form);
+    const $btn = $(form).find('[type="submit"]');
+    $('.js-input-val').each(function() {
+      const $this = $(this);
+      const spaceVal = $this.val();
+      const finalVal = $.trim(spaceVal);
+      $this.val(finalVal);
+    })
+    $btn.button('loading');
+    form.submit();
   }
 });
 
