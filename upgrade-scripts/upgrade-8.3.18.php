55aee67e32f66bca5fbbb3e5f2aa0a05493025dd
zhongyunchang <zhongyunchang@yeah.net>
2018-12-13 20:50:10 +0800

    fix: #83180 修改错误

--- upgrade-scripts/upgrade-8.3.18.php
+++ upgrade-scripts/upgrade-8.3.18.php
@@ -1,55 +1,52 @@
 <?php
 
 use Symfony\Component\Filesystem\Filesystem;
-use Biz\CloudPlatform\Service\AppService;
+use Codeages\Biz\Framework\Dao\BatchUpdateHelper;
 
 class EduSohoUpgrade extends AbstractUpdater
 {
+
     public function __construct($biz)
     {
         parent::__construct($biz);
+
     }
 
     public function update($index = 0)
     {
         $this->getConnection()->beginTransaction();
         try {
-            $this->updateScheme($index);
+            $result = $this->updateScheme($index);
+
             $this->getConnection()->commit();
+
+            if (!empty($result)) {
+                return $result;
+            } else {
+                $this->logger('info', '执行升级脚本结束');
+            }
         } catch (\Exception $e) {
-            $this->logger($e->getMessage(),'error');
             $this->getConnection()->rollback();
+            $this->logger('error', $e->getTraceAsString());
             throw $e;
         }
 
         try {
-            $dir = realpath($this->biz['kernel.root_dir']. "/../web/install");
+            $dir = realpath($this->biz['kernel.root_dir'] . '/../web/install');
             $filesystem = new Filesystem();
 
             if (!empty($dir)) {
                 $filesystem->remove($dir);
             }
         } catch (\Exception $e) {
-            $this->logger($e->getMessage(),'error');
+            $this->logger('error', $e->getTraceAsString());
         }
 
         $developerSetting = $this->getSettingService()->get('developer', array());
         $developerSetting['debug'] = 0;
 
         $this->getSettingService()->set('developer', $developerSetting);
-        $this->getSettingService()->set("crontab_next_executed_time", time());
-    }
-
-    protected function deleteCache()
-    {
-        $cachePath = $this->biz['cache_directory'];
-        $filesystem = new Filesystem();
-        $filesystem->remove($cachePath);
-
-        clearstatcache(true);
-
-        $this->logger( 'info', '删除缓存');
-        return 1;
+        $this->getSettingService()->set('crontab_next_executed_time', time());
     }
 
     private function updateScheme($index)
@@ -64,30 +61,30 @@ class EduSohoUpgrade extends AbstractUpdater
             $funcNames[$key + 1] = $funcName;
         }
 
-        if (0 == $index) {
+        if ($index == 0) {
             $this->logger('info', '开始执行升级脚本');
             $this->deleteCache();
 
             return array(
                 'index' => $this->generateIndex(1, 1),
                 'message' => '升级数据...',
-                'progress' => 0,
+                'progress' => 0
             );
         }
-
+        
         list($step, $page) = $this->getStepAndPage($index);
         $method = $funcNames[$step];
         $page = $this->$method($page);
 
-        if (1 == $page) {
-            ++$step;
+        if ($page == 1) {
+            $step++;
         }
 
         if ($step <= count($funcNames)) {
             return array(
                 'index' => $this->generateIndex($step, $page),
                 'message' => '升级数据...',
-                'progress' => 0,
+                'progress' => 0
             );
         }
     }
@@ -181,6 +178,27 @@ class EduSohoUpgrade extends AbstractUpdater
         return $pluginList[$page - 1];
     }
 
+    protected function isJobExist($code)
+    {
+        $sql = "select * from biz_scheduler_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function deleteCache()
+    {
+        $cachePath = $this->biz['cache_directory'];
+        $filesystem = new Filesystem();
+        $filesystem->remove($cachePath);
+
+        clearstatcache(true);
+
+        $this->logger('info', '删除缓存');
+
+        return 1;
+    }
+
     protected function generateIndex($step, $page)
     {
         return $step * 1000000 + $page;
@@ -190,36 +208,8 @@ class EduSohoUpgrade extends AbstractUpdater
     {
         $step = intval($index / 1000000);
         $page = $index % 1000000;
+
         return array($step, $page);
-    }
-
-    protected function isFieldExist($table, $filedName)
-    {
-        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
-        $result = $this->getConnection()->fetchAssoc($sql);
-        return empty($result) ? false : true;
-    }
-
-    protected function isTableExist($table)
-    {
-        $sql = "SHOW TABLES LIKE '{$table}'";
-        $result = $this->getConnection()->fetchAssoc($sql);
-        return empty($result) ? false : true;
-    }
-
-    protected function isIndexExist($table, $filedName, $indexName)
-    {
-        $sql    = "show index from `{$table}` where column_name = '{$filedName}' and Key_name = '{$indexName}';";
-        $result = $this->getConnection()->fetchAssoc($sql);
-        return empty($result) ? false : true;
-    }
-
-    protected function isCrontabJobExist($code)
-    {
-        $sql = "select * from crontab_job where name='{$code}'";
-        $result = $this->getConnection()->fetchAssoc($sql);
-
-        return empty($result) ? false : true;
     }
 
     private function getSettingService()
@@ -227,18 +217,64 @@ class EduSohoUpgrade extends AbstractUpdater
         return $this->createService('System:SettingService');
     }
 
+    protected function getUserDao()
+    {
+        return $this->createDao('User:UserDao');
+    }
+
     /**
-     * @return AppService
+     * @return \Codeages\Biz\Framework\Scheduler\Service\SchedulerService
      */
-    private function getAppService()
+    protected function getSchedulerService()
+    {
+        return $this->createService('Scheduler:SchedulerService');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Dao\JobDao
+     */
+    protected function getJobDao()
+    {
+        return $this->createDao('Scheduler:JobDao');
+    }
+
+    /**
+     * @return \Biz\CloudPlatform\Service\AppService
+     */
+    protected function getAppService()
     {
         return $this->createService('CloudPlatform:AppService');
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->createService('Course:CourseService');
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseSetService
+     */
+    protected function getCourseSetService()
+    {
+        return $this->createService('Course:CourseSetService');
+    }
+
+    /**
+     * @return \Biz\Course\Dao\CourseDao
+     */
+    protected function getCourseDao()
+    {
+        return $this->createDao('Course:CourseDao');
     }
 }
 
 abstract class AbstractUpdater
 {
     protected $biz;
+
     public function __construct($biz)
     {
         $this->biz = $biz;
@@ -259,10 +295,12 @@ abstract class AbstractUpdater
         return $this->biz->dao($name);
     }
 
-    protected function logger($message, $level='info')
+    abstract public function update();
+
+    protected function logger($level, $message)
     {
         $version = \AppBundle\System::VERSION;
-        $data = date('Y-m-d H:i:s')." [{$level}] {$version} ".$message.PHP_EOL;
+        $data = date('Y-m-d H:i:s') . " [{$level}] {$version} " . $message . PHP_EOL;
         if (!file_exists($this->getLoggerFile())) {
             touch($this->getLoggerFile());
         }
@@ -271,8 +309,6 @@ abstract class AbstractUpdater
 
     private function getLoggerFile()
     {
-        return $this->biz['kernel.root_dir'].'/../app/logs/upgrade.log';
+        return $this->biz['kernel.root_dir'] . '/../app/logs/upgrade.log';
     }
-
-    abstract public function update();
 }
