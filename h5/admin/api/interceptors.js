33e103a79b2aed0452e33bdd9a2fdc1e3f5ccf4d
Josie <1529867213@qq.com>
2018-12-13 11:48:22 +0800

    token

--- h5/admin/api/interceptors.js
+++ h5/admin/api/interceptors.js
@@ -14,7 +14,7 @@ axios.interceptors.request.use(config => {
   const env = process.env.NODE_ENV;
 
   if (env !== 'production') {
-    config.headers['X-Auth-Token'] = '5ddiygxm04sowwwksg8swoc0cgswks8';
+    config.headers['X-Auth-Token'] = 'e6cfpzdgvpws80sk4c8ckcckkc8sco';
   } else {
     config.headers['X-Requested-With'] = 'XMLHttpRequest';
     config.headers['X-CSRF-Token'] = store.state.csrfToken;
