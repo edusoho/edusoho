68e8dedbaa378f1e960908c948c61d4fc8eaf6a6
gaoyong <gaoyong@howzhi.com>
2018-12-21 16:47:36 +0800

    fix: 抽离优惠券 popup 组件

---
+++ h5/src/components/popup.vue
@@ -0,0 +1,42 @@
+<template>
+  <van-popup class="e-popup" v-model="popupShow" position="bottom">
+    <div class="e-popup__title">
+      <span class="name">{{ title }}</span>
+      <i class="icon h5-icon h5-icon-guanbi" @click="toggleClick"></i>
+    </div>
+    <div class="e-popup__content">
+      <slot></slot>
+    </div>
+  </van-popup>
+</template>
+
+<script>
+export default {
+  name: 'e-popup',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    popupShow: {
+      get() {
+        return this.show;
+      },
+      set(value) {
+        this.$emit('update:show', value);
+      }
+    }
+  },
+  methods: {
+    toggleClick() {
+      this.popupShow = false;
+    },
+  }
+}
+</script>
