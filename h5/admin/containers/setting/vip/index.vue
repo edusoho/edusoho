5b087f4c43e4b124f499684ec4ed4d0b89bb08ab
Josie <1529867213@qq.com>
2018-12-27 19:49:20 +0800

    小程序后台版本判断文案放在modal里面

--- h5/admin/containers/setting/vip/index.vue
+++ h5/admin/containers/setting/vip/index.vue
@@ -7,6 +7,8 @@
     <div slot="setting">
       <header class="title">
         会员专区
+        <div class="text-12 color-gray" v-if="portal === 'miniprogram'">
+        使用会员专区配置功能，小程序版本需要升级到1.3.4及以上</div>
       </header>
       <div class="default-allocate__content clearfix">
         <!-- 标题栏 -->
@@ -34,7 +36,7 @@ import Api from '@admin/api';
 import moduleFrame from '../module-frame';
 import settingCell from '../module-frame/setting-cell';
 import vipList from '@/containers/components/e-vip-list/e-vip-list';
-
+import pathName2Portal from '@admin/config/api-portal-config';
 
 export default {
   name: 'vip',
@@ -45,7 +47,7 @@ export default {
   },
   data () {
     return {
-
+      pathName: this.$route.name
     }
   },
   props: {
@@ -103,6 +105,9 @@ export default {
       set(value) {
         this.copyModuleData.titleShow = value;
       },
+    },
+    portal() {
+      return pathName2Portal[this.pathName];
     }
   },
   watch: {
