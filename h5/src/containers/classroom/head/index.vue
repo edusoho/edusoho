54b36007ac1b00f258ae3e5c7bff4e0c7ba81f83
Josie <1529867213@qq.com>
2019-01-18 11:29:28 +0800

    班级倒计时样式优化

--- h5/src/containers/classroom/head/index.vue
+++ h5/src/containers/classroom/head/index.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <div class="course-detail__head">
-      <div class="course-detail__head--img">
-        <img :src="cover" alt="">
-      </div>
+  <div class="course-detail__head">
+    <div class="course-detail__head--img">
+      <img :src="cover" alt="">
     </div>
     <countDown v-if="seckillActivities" :activity="seckillActivities"></countDown>
   </div>
