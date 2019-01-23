a2c8e00b142cf580f149d8a5543663abc763cd09
LianJion <1995lian1002@gmail.com>
2019-01-23 15:22:20 +0800

    fix: #47597 调整js格式

--- app/Resources/static-src/app/js/open-course-manage/recommend-course/index.js
+++ app/Resources/static-src/app/js/open-course-manage/recommend-course/index.js
@@ -1,10 +1,9 @@
 import sortList from 'common/sortable';
 
-$('.course-list-group').on('click', '.close', function () {
-  let recommendId = $(this).data('recommendId');
-  let courseId = $(this).data('id');
-  $.post($(this).data('cancelUrl')).done(function () {
-
+$('.js-course-list-group').on('click', '.js-delete-btn',  (event) => {
+  const $target = $(event.target);
+  const courseId = $target.data('id');
+  $.post($target.data('cancelUrl')).done(() => {
     $('.item-' + courseId).remove();
   });
 });
