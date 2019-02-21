55d6b758a37639c1eea1d16dba9fc9b19f4343ef
yanrui <yanrui@howzhi.com>
2019-02-21 19:09:56 +0800

    feat: #8322 编译

---
+++ web/static-dist/libs/es-ckeditor/plugins/pasteimage/lang/zh.js
@@ -0,0 +1,8 @@
+CKEDITOR.plugins.setLang( 'pasteimage', 'zh', {
+    title: '批量圖片上傳',
+    file_type_tip: '文件類型只支持：',
+    single_file_max_size_tip: '單個文件大小不能超過：',
+    waiting_upload_text: '待上傳',
+    delete_text: '移除',
+    uploaded_text: '已上傳'
+} );
