e85286450b5eb38b7ae3a9bda59bdee5ad451e4f
zhongyunchang <zhongyunchang@yeah.net>
2019-03-11 17:45:56 +0800

    fix: #190311 修改班级list报错

---
+++ upgrade-scripts/upgrade-8.3.24.php
@@ -0,0 +1,261 @@
+<?php
+
+use Codeages\Biz\Framework\Dao\BatchUpdateHelper;
+use Symfony\Component\Filesystem\Filesystem;
+use AppBundle\Common\ArrayToolkit;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    private $classroomUpdateHelper = null;
+
+    public function __construct($biz)
+    {
+        parent::__construct($biz);
+        $this->classroomUpdateHelper = new BatchUpdateHelper($this->getClassroomDao());
+    }
+
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $result = $this->updateScheme($index);
+
+            $this->getConnection()->commit();
+
+            if (!empty($result)) {
+                return $result;
+            } else {
+                $this->logger('info', '执行升级脚本结束');
+            }
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            $this->logger('error', $e->getTraceAsString());
+            throw $e;
+        }
+
+        try {
+            $dir = realpath($this->biz['kernel.root_dir'] . '/../web/install');
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+            $this->logger('error', $e->getTraceAsString());
+        }
+
+        $developerSetting = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        $this->getSettingService()->set('developer', $developerSetting);
+        $this->getSettingService()->set('crontab_next_executed_time', time());
+    }
+
+    private function updateScheme($index)
+    {
+        $definedFuncNames = array(
+            'dealClassroomTeachers',
+        );
+
+        $funcNames = array();
+        foreach ($definedFuncNames as $key => $funcName) {
+            $funcNames[$key + 1] = $funcName;
+        }
+
+        if ($index == 0) {
+            $this->logger('info', '开始执行升级脚本');
+            $this->deleteCache();
+
+            return array(
+                'index' => $this->generateIndex(1, 1),
+                'message' => '升级数据...',
+                'progress' => 0
+            );
+        }
+
+        list($step, $page) = $this->getStepAndPage($index);
+        $method = $funcNames[$step];
+        $page = $this->$method($page);
+
+        if ($page == 1) {
+            $step++;
+        }
+
+        if ($step <= count($funcNames)) {
+            return array(
+                'index' => $this->generateIndex($step, $page),
+                'message' => '升级数据...',
+                'progress' => 0
+            );
+        }
+    }
+
+    protected function dealClassroomTeachers()
+    {
+        $sql = "SELECT id, teacherIds from classroom;";
+        $classrooms = $this->getConnection()->fetchAll($sql);
+        if (empty($classrooms)) {
+            return 1;
+        }
+
+        foreach ($classrooms as $classroom) {
+            $this->classroomUpdateHelper->add('id', $classroom['id'], array('teacherIds' => array_values($classroom['teacherIds'])));
+        }
+
+        $this->classroomUpdateHelper->flush();
+
+        $this->logger('info', '更新classroom.teacherIds字段，共有'.count($classrooms).'条');
+        return 1;
+
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isJobExist($code)
+    {
+        $sql = "select * from biz_scheduler_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function deleteCache()
+    {
+        $cachePath = $this->biz['cache_directory'];
+        $filesystem = new Filesystem();
+        $filesystem->remove($cachePath);
+
+        clearstatcache(true);
+
+        $this->logger('info', '删除缓存');
+
+        return 1;
+    }
+
+    protected function generateIndex($step, $page)
+    {
+        return $step * 1000000 + $page;
+    }
+
+    protected function getStepAndPage($index)
+    {
+        $step = intval($index / 1000000);
+        $page = $index % 1000000;
+
+        return array($step, $page);
+    }
+
+    private function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+    protected function getUserDao()
+    {
+        return $this->createDao('User:UserDao');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Service\SchedulerService
+     */
+    protected function getSchedulerService()
+    {
+        return $this->createService('Scheduler:SchedulerService');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Dao\JobDao
+     */
+    protected function getJobDao()
+    {
+        return $this->createDao('Scheduler:JobDao');
+    }
+
+    /**
+     * @return \Biz\CloudPlatform\Service\AppService
+     */
+    protected function getAppService()
+    {
+        return $this->createService('CloudPlatform:AppService');
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->createService('Course:CourseService');
+    }
+
+    /**
+     * @return \Biz\Classroom\Service\ClassroomService
+     */
+    protected function getClassroomService()
+    {
+        return $this->createService('Classroom:ClassroomService');
+    }
+
+    /**
+     * @return \Biz\Classroom\Dao\ClassroomDao
+     */
+    protected function getClassroomDao()
+    {
+        return $this->createDao('Classroom:ClassroomDao');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+
+    protected function logger($level, $message)
+    {
+        $version = \AppBundle\System::VERSION;
+        $data = date('Y-m-d H:i:s') . " [{$level}] {$version} " . $message . PHP_EOL;
+        if (!file_exists($this->getLoggerFile())) {
+            touch($this->getLoggerFile());
+        }
+        file_put_contents($this->getLoggerFile(), $data, FILE_APPEND);
+    }
+
+    private function getLoggerFile()
+    {
+        return $this->biz['kernel.root_dir'] . '/../app/logs/upgrade.log';
+    }
+}
