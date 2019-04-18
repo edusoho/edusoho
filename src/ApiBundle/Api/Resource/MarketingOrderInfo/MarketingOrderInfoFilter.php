4f222baa935863594836af2a9d8e943434c37726
zhongyunchang <zhongyunchang@yeah.net>
2019-04-18 09:14:15 +0800

    fix: #80326 修改冲突

--- src/ApiBundle/Api/Resource/MarketingOrderInfo/MarketingOrderInfoFilter.php
+++ src/ApiBundle/Api/Resource/MarketingOrderInfo/MarketingOrderInfoFilter.php
@@ -7,6 +7,11 @@ use ApiBundle\Api\Resource\Filter;
 class MarketingOrderInfoFilter extends Filter
 {
     protected $publicFields = array(
-        'targetId', 'targetType', 'cover', 'title', 'mobile', 'payAmount', 'merchantName',
+        'id', 'status', 'targetId', 'targetType', 'cover', 'title', 'mobile', 'payAmount', 'payApiUrl', 'merchantName', 'createdTime',
     );
+
+    protected function publicFields(&$data)
+    {
+        $data['payAmount'] = sprintf('%.2f', $data['payAmount'] / 100);
+    }
 }
--- src/Biz/Xapi/Service/Impl/XapiServiceImpl.php
+++ src/Biz/Xapi/Service/Impl/XapiServiceImpl.php
@@ -3,7 +3,6 @@
 namespace Biz\Xapi\Service\Impl;
 
 use AppBundle\Common\ArrayToolkit;
-use AppBundle\Common\DateToolkit;
 use Biz\BaseService;
 use Biz\Task\Service\TaskService;
 use Biz\User\UserException;
@@ -54,7 +53,6 @@ class XapiServiceImpl extends BaseService implements XapiService
 
     protected function generateUUID()
     {
-        mt_srand(DateToolkit::getMicroSecond());
         $charid = strtoupper(md5(uniqid(rand(), true)));
         $hyphen = chr(45);
         $uuid = ''.substr($charid, 0, 8).$hyphen.substr($charid, 8, 4).$hyphen.substr($charid, 12, 4).$hyphen.substr($charid, 16, 4).$hyphen.substr($charid, 20, 12);
