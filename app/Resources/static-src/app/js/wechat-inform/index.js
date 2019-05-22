fd9f40daea11de3411ed71ab58951b6290e2a62d
LianJion <1995lian1002@gmail.com>
2019-05-22 17:07:10 +0800

    fix: #50053 适配响应式

--- app/Resources/static-src/app/js/wechat-inform/index.js
+++ app/Resources/static-src/app/js/wechat-inform/index.js
@@ -1,9 +1,12 @@
+import { isMobileDevice } from 'common/utils';
+
 export default class wechatInform {
 	constructor() {
 		this.$section = $('.js-wechat-inform');
 		this.$pendant = $('.js-wechat-pendant');
 		this.$qrcode = $('.js-wechat-qrcode');
 		this.$entrance = $('.js-inform-wechat-entrance');
+		this.$mask = $('.js-wechat-mask');
 		this.init();
 	}
 	
@@ -16,17 +19,30 @@ export default class wechatInform {
     this.$section.on('click', '.js-wechat-pendant', (event) => this.showQrcode(event));
 	}
 	closeWechatInform(event) {
+		const self = this;
 		const $target = $(event.currentTarget);
 		event.stopPropagation();
 		$target.parent().hide();
-		this.$entrance.fadeIn('normal', () => {
-			setTimeout(() => {
-				this.$entrance.fadeOut('normal');
-			}, 3000);
-		});
+
+		if (isMobileDevice()) {
+			this.$mask.hide();
+			$('body').removeClass('wechat-inform-body');
+		} else {
+			$('html, body').animate({scrollTop: '0px'}, 800, () => {
+				self.$entrance.fadeIn('normal', () => {
+					setTimeout(() => {
+						self.$entrance.fadeOut('normal');
+					}, 3000);
+				});
+			});
+		}
 	}
 	showQrcode(event) {
 		const $target = $(event.currentTarget);
+		if (isMobileDevice()) {
+			$('body').addClass('wechat-inform-body');
+			this.$mask.show();
+		}
 		$target.addClass('hidden');
 		this.$qrcode.removeClass('hidden');
 	}
