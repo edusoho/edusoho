f09a867a4b1aae5fccc073a6986cefcd909d08cf
zhongyunchang <zhongyunchang@yeah.net>
2019-05-22 12:25:37 +0800

    fix: #50087 新增通知状态接口，用户微信用户状态接口

--- src/Biz/WeChat/WeChatException.php
+++ src/Biz/WeChat/WeChatException.php
@@ -8,7 +8,10 @@ class WeChatException extends AbstractException
 {
     const EXCEPTION_MODULE = 66;
 
+    const NOTIFY_SETTING_NOT_ENABLED = '4036602';
+
     public $messages = array(
         4046601 => 'exception.wechat.',
+        4036602 => 'exception.wechat.notify_setting_not_enabled',
     );
 }
