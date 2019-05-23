c1a0398bf2783cc45128bcf1b25d78e0a51afef0
shenchupeng <shenchupeng@howzhi.com>
2019-05-23 19:30:52 +0800

    feat: #50053 fix

--- src/AppBundle/Extensions/DataTag/WeChatUserDataTag.php
+++ src/AppBundle/Extensions/DataTag/WeChatUserDataTag.php
@@ -6,16 +6,6 @@ use Topxia\Service\Common\ServiceKernel;
 
 class WeChatUserDataTag extends BaseDataTag implements DataTag
 {
-    /**
-     * 获取一个用户的关注/粉丝的数量.
-     *
-     * 可传入的参数：
-     *   userId 必需 用户ID
-     *
-     * @param array $arguments 参数
-     *
-     * @return array 一个用户的关注/粉丝的数量
-     */
     public function getData(array $arguments)
     {
         $userId = $arguments['userId'];
