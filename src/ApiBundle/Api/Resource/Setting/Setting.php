e82230364f9e258b1b7bf72456497f2f49068b4d
chelingfeng <chelingfeng@edusoho.com>
2019-05-27 11:14:06 +0800

    feat: #48504 获取班级设置项

--- src/ApiBundle/Api/Resource/Setting/Setting.php
+++ src/ApiBundle/Api/Resource/Setting/Setting.php
@@ -15,7 +15,7 @@ class Setting extends AbstractResource
      */
     public function get(ApiRequest $request, $type)
     {
-        if (!in_array($type, array('site', 'wap', 'register', 'payment', 'vip', 'magic', 'cdn', 'course', 'weixinConfig', 'login', 'face', 'miniprogram', 'hasPluginInstalled'))) {
+        if (!in_array($type, array('site', 'wap', 'register', 'payment', 'vip', 'magic', 'cdn', 'course', 'weixinConfig', 'login', 'face', 'miniprogram', 'hasPluginInstalled', 'classroom'))) {
             throw CommonException::ERROR_PARAMETER();
         }
 
@@ -215,6 +215,15 @@ class Setting extends AbstractResource
         $clients = OAuthClientFactory::clients();
 
         return $this->getLoginConnect($clients);
+    }
+
+    public function getClassRoom()
+    {
+        $classroomSetting = $this->getSettingService()->get('classroom', array());
+
+        return array(
+            'show_student_num_enabled' => !isset($classroomSetting['show_student_num_enabled']) ? '1' : $classroomSetting['show_student_num_enabled'],
+        );
     }
 
     private function getLoginConnect($clients)
