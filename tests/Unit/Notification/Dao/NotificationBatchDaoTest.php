d2b49894a22f4ff327de940847a871ccdd5035ad
shenchupeng <shenchupeng@howzhi.com>
2019-06-10 10:02:10 +0800

    feat: #49810 fix

--- tests/Unit/Notification/Dao/NotificationBatchDaoTest.php
+++ tests/Unit/Notification/Dao/NotificationBatchDaoTest.php
@@ -6,6 +6,24 @@ use Tests\Unit\Base\BaseDaoTestCase;
 
 class NotificationBatchDaoTest extends BaseDaoTestCase
 {
+    public function testDeclares()
+    {
+        $result = $this->createDao('Notification:NotificationBatchDao')->declares();
+        $declare = array(
+            'timestamps' => array('createdTime', 'updatedTime'),
+            'orderbys' => array('id', 'createdTime', 'updatedTime'),
+            'conditions' => array(
+                'id = :id',
+                'eventId = :eventId',
+                'sn = :sn',
+                'status = :status',
+                'strategyId = :strategyId',
+            ),
+        );
+
+        $this->assertArrayEquals($declare, $result);
+    }
+
     protected function getDefaultMockFields()
     {
         return array(
