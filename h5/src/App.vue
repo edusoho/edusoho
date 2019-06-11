dcc6feb0ffcf38559fd675812146176a230c26c6
chenjinhong <chenjinhong@edusoho.com>
2019-06-11 18:30:30 +0800

    修复了分享回退空白

--- h5/src/App.vue
+++ h5/src/App.vue
@@ -5,21 +5,23 @@
       :class="[{hide: isQrcode}, 'nav-bar']"
       :title="title"
       :left-arrow="showLeftArrow"
-      @click-left="backFn()"/>
+      @click-left="backFn()"
+    />
     <router-view></router-view>
   </div>
 </template>
 <script>
-import Api from '@/api'
-import * as types from '@/store/mutation-types';
-import { mapMutations, mapState } from 'vuex';
+import Api from "@/api";
+import * as types from "@/store/mutation-types";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   data() {
-    return  {
+    return {
       showLeftArrow: false,
-      isQrcode: false
-    }
+      isQrcode: false,
+      isShare:false
+    };
   },
   methods: {
     ...mapMutations({
@@ -28,37 +30,49 @@ export default {
     backFn() {
       const query = this.$route.query;
       if (query.backUrl) {
-        this.$router.push({ path: query.backUrl })
+        this.$router.push({ path: query.backUrl });
         return;
       }
-      this.$router.go(-1);
+      if(this.isShare){ //如果是从分享页进来
+         this.$router.push({ path: '/'});
+         return
+      }
+       this.$router.go(-1)
     }
   },
   computed: {
-    ...mapState(['title']),
+    ...mapState(["title"]),
     routerKeepAlive() {
       return this.$route.meta.keepAlive;
-    },
+    }
   },
   watch: {
-    '$route': {
-      handler(to) {
-        const redirect = to.query.redirect || '';
+    $route: {
+      handler(to,from) {
+        this.isShare=from.fullPath==='/'?true:false //需要返回首页标记
+
+        const redirect = to.query.redirect || "";
 
         this.isQrcode = !!to.query.loginToken;
 
-        this.showLeftArrow =
-          !['my','find','learning','prelogin','preview','coupon_receive','share_redirect']
-          .includes(to.name);
+        this.showLeftArrow = ![
+          "my",
+          "find",
+          "learning",
+          "prelogin",
+          "preview",
+          "coupon_receive",
+          "share_redirect"
+        ].includes(to.name);
 
-        if(redirect === 'learning') {
-          this.setNavbarTitle('我的学习')
+        if (redirect === "learning") {
+          this.setNavbarTitle("我的学习");
           return;
         }
 
-        this.setNavbarTitle(to.meta.title)
+        this.setNavbarTitle(to.meta.title);
       }
     }
   }
-}
+};
 </script>
