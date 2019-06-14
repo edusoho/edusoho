603df5aa1369777c36089b8e319bccd12afa9f07
zhongyunchang <zhongyunchang@yeah.net>
2019-06-14 14:40:09 +0800

    fix: #190614 修改索引

---
+++ upgrade-scripts/upgrade-8.3.34.php
@@ -0,0 +1,402 @@
+<?php
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function __construct($biz)
+    {
+        parent::__construct($biz);
+    }
+
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $result = $this->updateScheme($index);
+
+            $this->getConnection()->commit();
+
+            if (!empty($result)) {
+                return $result;
+            } else {
+                $this->logger('info', '执行升级脚本结束');
+            }
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            $this->logger('error', $e->getTraceAsString());
+            throw $e;
+        }
+
+        try {
+            $dir = realpath($this->biz['kernel.root_dir'] . '/../web/install');
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+            $this->logger('error', $e->getTraceAsString());
+        }
+
+        $developerSetting = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        $this->getSettingService()->set('developer', $developerSetting);
+        $this->getSettingService()->set('crontab_next_executed_time', time());
+    }
+
+    private function updateScheme($index)
+    {
+        $definedFuncNames = array(
+            'createNotificationTable',
+        );
+
+        $funcNames = array();
+        foreach ($definedFuncNames as $key => $funcName) {
+            $funcNames[$key + 1] = $funcName;
+        }
+
+        if ($index == 0) {
+            $this->logger('info', '开始执行升级脚本');
+            $this->deleteCache();
+
+            return array(
+                'index' => $this->generateIndex(1, 1),
+                'message' => '升级数据...',
+                'progress' => 0
+            );
+        }
+
+        list($step, $page) = $this->getStepAndPage($index);
+        $method = $funcNames[$step];
+        $page = $this->$method($page);
+
+        if ($page == 1) {
+            $step++;
+        }
+
+        if ($step <= count($funcNames)) {
+            return array(
+                'index' => $this->generateIndex($step, $page),
+                'message' => '升级数据...',
+                'progress' => 0
+            );
+        }
+    }
+
+    protected function createNotificationTable()
+    {
+        if (!$this->isTableExist('user_wechat')) {
+            $this->getConnection()->exec("
+            CREATE TABLE `user_wechat` (
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `appId` varchar(64) NOT NULL DEFAULT '' COMMENT '服务标识',
+              `type` varchar(32) NOT NULL DEFAULT '' COMMENT 'official-公众号 open_app-开放平台应用',
+              `userId` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'ES 用户ID',
+              `openId` varchar(64) NOT NULL DEFAULT '' COMMENT '微信openid',
+              `unionId` varchar(64) DEFAULT NULL COMMENT '微信unionid',
+              `isSubscribe` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否订阅服务号',
+              `data` text COMMENT '接口信息',
+              `lastRefreshTime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '上一次数据更新时间',
+              `createdTime` int(10) unsigned NOT NULL DEFAULT '0',
+              `updatedTime` int(10) unsigned NOT NULL DEFAULT '0',
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `openId_type` (`openId`,`type`),
+              KEY `unionId_type` (`unionId`,`type`),
+              KEY `userId_type` (`userId`,`type`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
+        }
+
+        if (!$this->isTableExist('notification_event')) {
+            $this->getConnection()->exec("
+            CREATE TABLE `notification_event` (
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `title` varchar(64) NOT NULL DEFAULT '' COMMENT '通知标题',
+              `content` text NOT NULL COMMENT '通知主体',
+              `totalCount` int(10) unsigned NOT NULL COMMENT '通知数量',
+              `succeedCount` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '通知发送成功数量',
+              `status` varchar(32) NOT NULL DEFAULT 'created' COMMENT 'sending,fail,success',
+              `reason` varchar(128) NOT NULL DEFAULT '' COMMENT '失败原因',
+              `createdTime` int(10) unsigned NOT NULL,
+              `updatedTime` int(10) unsigned NOT NULL DEFAULT '0',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
+        }
+
+        if (!$this->isTableExist('notification_strategy')) {
+            $this->getConnection()->exec("
+            CREATE TABLE `notification_strategy` (
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `eventId` int(10) unsigned NOT NULL,
+              `type` varchar(32) NOT NULL DEFAULT '' COMMENT 'wechat,email,short_message',
+              `seq` int(11) unsigned NOT NULL,
+              `createdTime` int(10) unsigned NOT NULL,
+              `updatedTime` int(10) unsigned NOT NULL DEFAULT '0',
+              PRIMARY KEY (`id`),
+              KEY `eventId` (`eventId`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
+        }
+
+        if (!$this->isTableExist('notification_batch')) {
+            $this->getConnection()->exec("
+            CREATE TABLE `notification_batch` (
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `eventId` int(11) unsigned NOT NULL COMMENT 'eventId',
+              `strategyId` int(11) unsigned NOT NULL COMMENT 'strategyId',
+              `sn` varchar(64) NOT NULL DEFAULT '' COMMENT '第三方批次SN',
+              `status` varchar(32) NOT NULL DEFAULT 'created' COMMENT 'created,finished',
+              `extra` int(11) DEFAULT NULL COMMENT '单批次',
+              `createdTime` int(11) NOT NULL,
+              `updatedTime` int(11) NOT NULL DEFAULT '0',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
+        }
+        return 1;
+    }
+
+    protected function downloadPlugin($page)
+    {
+        $plugin = $this->getUpdatePluginInfo($page);
+        if (empty($plugin)) {
+            return 1;
+        }
+
+        $pluginCode = $plugin[0];
+        $pluginPackageId = $plugin[1];
+
+        $this->logger('warning', '检测是否安装'.$pluginCode);
+        $pluginApp = $this->getAppService()->getAppByCode($pluginCode);
+        if (empty($pluginApp)) {
+            $this->logger('warning', '网校未安装'.$pluginCode);
+
+            return $page + 1;
+        }
+        try {
+            $package = $this->getAppService()->getCenterPackageInfo($pluginPackageId);
+            if (isset($package['error'])) {
+                $this->logger('warning', $package['error']);
+                return $page + 1;
+            }
+            $error1 = $this->getAppService()->checkDownloadPackageForUpdate($pluginPackageId);
+            $error2 = $this->getAppService()->downloadPackageForUpdate($pluginPackageId);
+            $errors = array_merge($error1, $error2);
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    $this->logger('warning', $error);
+                }
+            };
+        } catch (\Exception $e) {
+            $this->logger('warning', $e->getMessage());
+        }
+        $this->logger('info', '检测完毕');
+        return $page + 1;
+    }
+
+    protected function updatePlugin($page)
+    {
+        $plugin = $this->getUpdatePluginInfo($page);
+        if (empty($plugin)) {
+            return 1;
+        }
+
+        $pluginCode = $plugin[0];
+        $pluginPackageId = $plugin[1];
+
+        $this->logger('warning', '升级' . $pluginCode);
+        $pluginApp = $this->getAppService()->getAppByCode($pluginCode);
+        if (empty($pluginApp)) {
+            $this->logger('warning', '网校未安装' . $pluginCode);
+
+            return $page + 1;
+        }
+
+        try {
+            $package = $this->getAppService()->getCenterPackageInfo($pluginPackageId);
+            if (isset($package['error'])) {
+                $this->logger('warning', $package['error']);
+                return $page + 1;
+            }
+            $errors = $this->getAppService()->beginPackageUpdate($pluginPackageId, 'install', 0);
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    $this->logger('warning', $error);
+                }
+            }
+        } catch (\Exception $e) {
+            $this->logger('warning', $e->getMessage());
+        }
+        $this->logger('info', '升级完毕');
+        return $page + 1;
+    }
+
+    private function getUpdatePluginInfo($page)
+    {
+        $pluginList = array(
+            array(
+                'Coupon',
+                1522
+            ),
+        );
+
+        if (empty($pluginList[$page - 1])) {
+            return;
+        }
+
+        return $pluginList[$page - 1];
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isJobExist($code)
+    {
+        $sql = "select * from biz_scheduler_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function deleteCache()
+    {
+        $cachePath = $this->biz['cache_directory'];
+        $filesystem = new Filesystem();
+        $filesystem->remove($cachePath);
+
+        clearstatcache(true);
+
+        $this->logger('info', '删除缓存');
+
+        return 1;
+    }
+
+    protected function generateIndex($step, $page)
+    {
+        return $step * 1000000 + $page;
+    }
+
+    protected function getStepAndPage($index)
+    {
+        $step = intval($index / 1000000);
+        $page = $index % 1000000;
+
+        return array($step, $page);
+    }
+
+    private function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+    protected function getUserDao()
+    {
+        return $this->createDao('User:UserDao');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Service\SchedulerService
+     */
+    protected function getSchedulerService()
+    {
+        return $this->createService('Scheduler:SchedulerService');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Dao\JobDao
+     */
+    protected function getJobDao()
+    {
+        return $this->createDao('Scheduler:JobDao');
+    }
+
+    /**
+     * @return \Biz\CloudPlatform\Service\AppService
+     */
+    protected function getAppService()
+    {
+        return $this->createService('CloudPlatform:AppService');
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->createService('Course:CourseService');
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseSetService
+     */
+    protected function getCourseSetService()
+    {
+        return $this->createService('Course:CourseSetService');
+    }
+
+    /**
+     * @return \Biz\Course\Dao\CourseDao
+     */
+    protected function getCourseDao()
+    {
+        return $this->createDao('Course:CourseDao');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+
+    protected function logger($level, $message)
+    {
+        $version = \AppBundle\System::VERSION;
+        $data = date('Y-m-d H:i:s') . " [{$level}] {$version} " . $message . PHP_EOL;
+        if (!file_exists($this->getLoggerFile())) {
+            touch($this->getLoggerFile());
+        }
+        file_put_contents($this->getLoggerFile(), $data, FILE_APPEND);
+    }
+
+    private function getLoggerFile()
+    {
+        return $this->biz['kernel.root_dir'] . '/../app/logs/upgrade.log';
+    }
+}
