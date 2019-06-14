22f118723f40db73f6b60c531e58b9cac3f99bee
zhongyunchang <zhongyunchang@yeah.net>
2019-06-14 16:51:40 +0800

    fix: #190614 changelog version

--- web/bundles/translations/zh_CN.js
+++ web/bundles/translations/zh_CN.js
@@ -1089,6 +1089,7 @@ t.add("admin.system.payment.wxpay_key_input.message", "\u8bf7\u8f93\u5165\u5546\
 t.add("admin.system.payment.llpay_key_input.message", "\u8bf7\u8f93\u5165\u5546\u6237\u5e10\u53f7", "js", "zh_CN");
 t.add("admin.system.payment.llpay_accessKey_input.message", "\u8bf7\u8f93\u5165\u516c\u94a5", "js", "zh_CN");
 t.add("admin.system.payment.llpay_secretKey_input.message", "\u8bf7\u8f93\u5165\u5bc6\u94a5", "js", "zh_CN");
+t.add("admin.system.wechat.notification_open", "\u8bf7\u5148\u5f00\u542f\u201c\u7f51\u9875\u626b\u7801\u767b\u5f55\u201d\u548c\u201c\u5fae\u4fe1\u5185\u6388\u6743\u767b\u5f55\u201d\u3002", "js", "zh_CN");
 t.add("admin.system.user_fields.truename", "\u771f\u5b9e\u59d3\u540d", "js", "zh_CN");
 t.add("admin.system.user_fields.mobile", "\u624b\u673a\u53f7\u7801", "js", "zh_CN");
 t.add("admin.system.user_fields.company", "\u6240\u5728\u516c\u53f8", "js", "zh_CN");
@@ -1176,4 +1177,8 @@ t.add("material.common_table.not_support_error_tip", "\u4e0a\u4f20\u7684\u6587\u
 t.add("material.common_table.solution_way", "\u89e3\u51b3\u65b9\u6848", "js", "zh_CN");
 t.add("course.stick.success", "\u7f6e\u9876\u6210\u529f\uff01", "js", "zh_CN");
 t.add("course.cancel.stick.success", "\u53d6\u6d88\u7f6e\u9876\u6210\u529f\uff01", "js", "zh_CN");
+t.add("wechat.notification.wechat_bind_hover", "<div class=\"wechat-intro-title cd-text-warning\"><i class=\"es-icon es-icon-xinxi cd-mr8\"><\/i>\u63d0\u793a<\/div>\n  <div class=\"wechat-intro-content\">\n    <div>\u4e3a\u4eab\u53d7\u66f4\u597d\u7684\u670d\u52a1\uff0c\u5efa\u8bae\u60a8\u5f00\u542f\u5fae\u4fe1\u8bfe\u7a0b\u901a\u77e5\u3002<\/div>\n    <div>\u70b9\u8fd9\u91cc\u7ed1\u5b9a\u5fae\u4fe1\uff0c\u5e76\u5173\u6ce8\u670d\u52a1\u53f7\u5373\u53ef\u5f00\u542f\u8bfe\u7a0b\u901a\u77e5\u3002<\/div>\n  <\/div>\n", "js", "zh_CN");
+t.add("wechat.notification.homepage.open_tip", "<span class=\"%messageClass%\">\u201c\u5fae\u4fe1\u901a\u77e5\u201d\u53ef\u5728\u201c\u4e2a\u4eba\u8bbe\u7f6e-\u7b2c\u4e09\u65b9\u767b\u5f55\u201d\u5f00\u542f\u3002<\/span>", "js", "zh_CN");
+t.add("wechat.notification.homepage.open_tip_title", "<span class=\"%messageClass%\">\u524d\u5f80\u5f00\u542f<\/span>", "js", "zh_CN");
+t.add("wechat.notification.cloud_open_tip", "\u8bf7\u5148\u5f00\u542f\u6559\u80b2\u4e91", "js", "zh_CN");
 })(Translator);
