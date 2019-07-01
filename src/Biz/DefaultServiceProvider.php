eb6b00241101ae33b8b51599ebe864a2e83d2f0c
shenchupeng <shenchupeng@howzhi.com>
2019-07-01 18:22:32 +0800

    feat: #51030 fix

--- src/Biz/DefaultServiceProvider.php
+++ src/Biz/DefaultServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Biz;
 
 use AppBundle\Component\Notification\WeChatTemplateMessage\Client;
-use AppBundle\Component\Notification\WeChatTemplateMessage\CloudNotificationClient;
 use AppBundle\Component\RateLimit\EmailRateLimiter;
 use Biz\Common\BizCaptcha;
 use Biz\Common\BizSms;
