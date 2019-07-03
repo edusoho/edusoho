0deb579c80d6ad371b0cd86c50ab448bdabbebe6
shenchupeng <shenchupeng@howzhi.com>
2019-07-03 10:12:29 +0800

    feat: #51067 修改sonar的bugs

--- src/AppBundle/Common/ClassroomToolkit.php
+++ src/AppBundle/Common/ClassroomToolkit.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Common;
 
-use AppBundle\Common\Exception\RuntimeException;
+use AppBundle\Common\Exception\UnexpectedValueException;
 
 class ClassroomToolkit
 {
@@ -16,7 +16,7 @@ class ClassroomToolkit
 
         if ('date' == $expiryDate['expiryMode']) {
             if ($deadline < time()) {
-                throw new RuntimeException('有效期的设置时间小于当前时间！');
+                throw new UnexpectedValueException('有效期的设置时间小于当前时间！');
             }
         }
 
