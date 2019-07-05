d1f6618cab5a6c1d08debc7c7b415186920fce0a
zhongyunchang <zhongyunchang@yeah.net>
2019-07-05 18:17:37 +0800

    feat: #51079 修改遮罩提示

--- app/Resources/static-src/app/js/testpaper-manage/index.js
+++ app/Resources/static-src/app/js/testpaper-manage/index.js
@@ -1,6 +1,8 @@
 import notify from 'common/notify';
 import BatchSelect from '../../common/widget/batch-select';
 import DeleteAction from '../../common/widget/delete-action';
+import 'store';
+const TESTPAPER_IMPORT_INTRO = 'TESTPAPER_IMPORT_INTRO';
 
 class TestpaperManage
 {
@@ -9,6 +11,11 @@ class TestpaperManage
     this.$container = $container;
     this._initEvent();
     this._init();
+    if (!store.get(TESTPAPER_IMPORT_INTRO)) {
+      store.set(TESTPAPER_IMPORT_INTRO, true);
+      this.importIntro();
+    }
+    
   }
 
   _initEvent() {
@@ -34,6 +41,25 @@ class TestpaperManage
     }).error(function(){
       notify('danger', Translator.trans('testpaper_manage.save_error_hint'));
     });
+  }
+
+  importIntro() {
+    const doneLabel = Translator.trans('document.import.skip_btn');
+    const customClass = 'import-intro';
+    introJs().setOptions({
+      steps: [{
+        element: '.js-import-btn',
+        intro: Translator.trans('document.import.intro_hover'),
+      }],
+      skipLabel: doneLabel,
+      doneLabel: doneLabel,
+      showBullets: false,
+      tooltipPosition: 'down',
+      showStepNumbers: false,
+      exitOnEsc: false,
+      exitOnOverlayClick: false,
+      tooltipClass: customClass
+    }).start();
   }
   
 }
