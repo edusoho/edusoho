1a62e82e5a46b17cf0b3696a6570271ce14f16ed
shenchupeng <shenchupeng@howzhi.com>
2019-07-11 13:58:00 +0800

    feat: #51180 设置题目难度

--- app/Resources/static-src/app/js/subject/index.js
+++ app/Resources/static-src/app/js/subject/index.js
@@ -1,3 +1,5 @@
+import notify from 'common/notify';
+
 export default class sbList {
   constructor() {
     this.$element = $('.js-subject-list');
@@ -6,18 +8,27 @@ export default class sbList {
     this.$sbCheckbox = $('.js-show-checkbox');
     this.$finishBtn = $('.js-finish-btn');
     this.$allBtn = $('.js-batch-select');
+    this.$modal = $('#question-difficulty-modal');
     this.init();
   }
 
   init() {
+    this.confirmFresh();
     this.sbListFixed();
     this.initEvent();
+  }
+
+  confirmFresh() {
+    // $(window).on('beforeunload',function(){
+    //   return Translator.trans('admin.block.not_saved_data_hint');
+    // });
   }
 
   initEvent() {
     this.$element.on('click','.js-batch-select', event => this.batchSelect(event));
     this.$element.on('click','.js-batch-btn', event =>this.batchBtnClick(event));
-    this.$element.on('click','.js-finish-btn',event => this.finishBtnClick(event));
+    this.$element.on('click','.js-finish-btn', event => this.finishBtnClick(event));
+    this.$element.on('click','.js-difficult-setting', event => this.showModal(event, this.$modal));
   }
 
   sbListFixed() {
@@ -61,6 +72,43 @@ export default class sbList {
   toggleClass() {
     this.$batchWrap.toggleClass('hidden');
     this.$sbCheckbox.toggleClass('hidden');
+  }
+
+  showModal(event, modal) {
+    let stats = this.statChosedQuestion();
+    var keys = Object.keys(stats);
+    if (keys.length == 0) {
+      notify('danger', Translator.trans('请选择题目'));
+      return;
+    }
+    let html = '';
+    $.each(stats, function(index, statsItem){
+      let tr = statsItem.count + statsItem.name + '，';
+      html += tr;
+    });
+    html = html.substring(0, html.length - 1) + '。';
+
+    modal.find('.js-select').html(html);
+
+    modal.modal('show');
+  }
+
+  statChosedQuestion() {
+    let stats = {};
+    let self = this;
+
+    self.$element.find('.js-show-checkbox.checked').each(function(){
+      let type = $(this).data('type'),
+          name = $(this).data('name');
+
+      if (typeof stats[type] == 'undefined') {
+        stats[type] = {name:name, count:1};;
+      } else {
+        stats[type]['count']++;
+      }
+    });
+
+    return stats;
   }
 }
 
