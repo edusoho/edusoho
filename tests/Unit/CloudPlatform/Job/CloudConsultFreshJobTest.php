73d8a5a4ee796421b01368d2f4bd83701857885d
yanrui <yanrui@howzhi.com>
2019-07-16 17:59:25 +0800

    feat: #51371 单元测试

---
+++ tests/Unit/CloudPlatform/Job/CloudConsultFreshJobTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Course;
+
+use Biz\BaseTestCase;
+use Biz\CloudPlatform\Job\CloudConsultFreshJob;
+use Biz\System\Service\SettingService;
+
+class CloudConsultFreshJobTest extends BaseTestCase
+{
+    public function testExcuteWhitEmptyCloudConsult()
+    {
+        $job = new CloudConsultFreshJob(array(), $this->biz);
+        $result = $job->execute();
+        $this->assertNull($result);
+
+//        $memberService->shouldHaveReceived('findMemberUserIdsByCourseId')->times(2);
+//        $analysisDao->shouldHaveReceived('batchRefreshUserLearningData')->times(2);
+    }
+
+    public function testExcute()
+    {
+        $this->getSettingService()->set('cloud_consult', array('cloud_consult_setting_enabled' => 1));
+        $job = new CloudConsultFreshJob(array(), $this->biz);
+        $job->execute();
+        $result = $this->getSettingService()->get('cloud_consult', array());
+
+        $this->assertEquals($result['cloud_consult_setting_enabled'], 1);
+        $this->assertEquals($result['cloud_consult_code'], 0);
+    }
+
+    /**
+     * @return SettingService
+     */
+    protected function getSettingService()
+    {
+        return $this->biz->service('System:SettingService');
+    }
+}
