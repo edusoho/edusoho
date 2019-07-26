154bd66d6e92cdeeb9072d2a3ce85f6dd5ed38d6
zhangyao <zhangyao@howzhi.com>
2019-07-26 09:46:08 +0800

    fix: #51265 班级上传图片后，多次点击保存图片按钮报服务器错误

--- app/Resources/static-src/app/js/classroom-manage/set-picture/index.js
+++ app/Resources/static-src/app/js/classroom-manage/set-picture/index.js
@@ -9,6 +9,7 @@ class Cover {
     new EsWebUploader({
       element: '#upload-picture-btn',
       onUploadSuccess: function(file, response) {
+        $('#upload-picture-btn').button('loading');
         let url = $('#upload-picture-btn').data('gotoUrl');
         cd.message({type: 'success', message: Translator.trans('site.upload_success_hint')});
         document.location.href = url;
