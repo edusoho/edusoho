e5fc7ee56483a8aebdaeeca8e22ea639e0dd58bf
zhongyunchang <zhongyunchang@yeah.net>
2019-07-30 13:42:29 +0800

    fix: #51123 试题编辑之后换行的问题

--- src/AppBundle/Controller/Testpaper/ManageController.php
+++ src/AppBundle/Controller/Testpaper/ManageController.php
@@ -639,7 +639,11 @@ class ManageController extends BaseController
 
         foreach ($questions as $question) {
             ++$questionAnalysis[$question['type']];
-            if ('material' != $question['type']) {
+            if ('material' == $question['type']) {
+                foreach ($question['subQuestions'] as $subQuestion) {
+                    $totalScore += $subQuestion['score'];
+                }
+            } else {
                 $totalScore += $question['score'];
             }
         }
