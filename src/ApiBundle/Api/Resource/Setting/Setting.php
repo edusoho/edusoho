b2f686592eef1dc99bbe2332c3e390d56d6b1635
wurui <wurui@edusoho.com>
2019-07-31 14:10:24 +0800

    feat: #51588 『获取用户设置』接口

--- src/ApiBundle/Api/Resource/Setting/Setting.php
+++ src/ApiBundle/Api/Resource/Setting/Setting.php
@@ -7,6 +7,7 @@ use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use Biz\Common\CommonException;
 use AppBundle\Component\OAuthClient\OAuthClientFactory;
+use Biz\System\SettingException;
 
 class Setting extends AbstractResource
 {
@@ -15,13 +16,36 @@ class Setting extends AbstractResource
      */
     public function get(ApiRequest $request, $type)
     {
-        if (!in_array($type, array('site', 'wap', 'register', 'payment', 'vip', 'magic', 'cdn', 'course', 'weixinConfig', 'login', 'face', 'miniprogram', 'hasPluginInstalled', 'classroom', 'wechat', 'cloud'))) {
+        if (!in_array($type, array('site', 'wap', 'register', 'payment', 'vip', 'magic', 'cdn', 'course', 'weixinConfig', 'login', 'face', 'miniprogram', 'hasPluginInstalled', 'classroom', 'wechat', 'cloud', 'user'))) {
             throw CommonException::ERROR_PARAMETER();
         }
 
         $method = "get${type}";
 
         return $this->$method($request);
+    }
+
+    public function getUser($request = null)
+    {
+        $authSetting = $this->getSettingService()->get('auth');
+        $loginSetting = $this->getSettingService()->get('login_bind');
+
+        if (empty($loginSetting)) {
+            SettingException::NOTFOUND_THIRD_PARTY_AUTH_CONFIG();
+        }
+
+        $result = array(
+            'register_mode' => $authSetting['register_mode'],
+            'oauth_enabled' => (int) $loginSetting['enabled'] ?: 0,
+            'weibo_enabled' => (int) $loginSetting['weibo_enabled'] ?: 0,
+            'qq_enabled' => (int) $loginSetting['qq_enabled'] ?: 0,
+            'weixinweb_enabled' => (int) $loginSetting['weixinweb_enabled'] ?: 0,
+            'weixinmob_enabled' => (int) $loginSetting['weixinmob_enabled'] ?: 0,
+            'userTerms_enabled' => $authSetting['user_terms'] == 'opened' ? 1 : 0,
+            'privacyPolicy_enabled' => $authSetting['privacy_policy'] == 'opened' ? 1 : 0,
+        );
+
+        return $result;
     }
 
     public function getCloud($request = null)
