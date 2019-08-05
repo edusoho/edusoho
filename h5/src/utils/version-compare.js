c516a690ecbf393f69d482f5fd52d320a4a79389
chenjinhong <chenjinhong@edusoho.com>
2019-08-05 18:43:30 +0800

    版本号判断兼容处理

--- h5/src/utils/version-compare.js
+++ h5/src/utils/version-compare.js
@@ -1,15 +1,26 @@
 // 版本号比较工具 1.0.1  1.0.0
-const needUpgrade = (supportVersion, currentVersion) => {
-  const supportVArray = supportVersion.split('.');
-  const currentVArray = currentVersion.split('.');
-  const supportVLength = supportVArray.length;
-  const currentVLength = currentVArray.length;
-  const length = Math.min(supportVLength, currentVLength);
-  for (let i = 0; i < length; i += 1) {
-    if (supportVArray[i] > currentVArray[i]) return true;
-    if (supportVArray[i] < currentVArray[i]) return false;
+function toNum(a) {
+  let c = a.toString().split('.');
+  let num_place = ["", "0", "00", "000", "0000"];
+  let r = num_place.reverse();
+  for (let i = 0; i < c.length; i++) {
+    let len = c[i].length;
+    c[i] = r[len] + c[i];
   }
-  return currentVLength < supportVLength;
-};
+  let res = c.join('');
+  return res;
+}
+
+const needUpgrade = function needUpgrade(supportVersion, currentVersion) {
+  let a = toNum(supportVersion);
+  let b = toNum(currentVersion);
+  if (a >= b) {
+    //支持
+    return true;
+  } else {
+    //不支持
+    return false;
+  }
+}
 
 export default needUpgrade;
