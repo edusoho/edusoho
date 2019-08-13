b2b307d93bc48182cd9bfe85988c82847edf9745
chenjinhong <chenjinhong@edusoho.com>
2019-08-13 14:49:45 +0800

    路由冲突

--- h5/src/router/index.js
+++ h5/src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
       title: ''
     },
     component: () => import(/* webpackChunkName: "fill" */ '@/containers/course/lessonTask/testPaper.vue')
-  }, 
+  },
   {
     path: '/',
     meta: {
