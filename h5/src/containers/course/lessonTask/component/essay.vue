f96c4c0c327a9d31c9321aa64522545db78b1ec2
chenjinhong <chenjinhong@edusoho.com>
2019-08-13 10:41:09 +0800

    填空题和问题题组件完成

---
+++ h5/src/containers/course/lessonTask/component/essay.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="essay">
+       <div v-html="essaydata.stem"></div>
+        <van-field
+                v-model="essaydata.answers"
+                class="essay-input"
+                label-width="0px"
+                type="textarea"
+                placeholder="请填写你的答案......"
+                :autosize="{ maxHeight: 200,minHeight: 200 }"
+        />
+  </div>
+</template>
+
+<script>
+export default {
+    name:'essay-type',
+    props:{
+        essaydata:{
+            type:Object,
+            default:()=>{}
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ h5/src/containers/course/lessonTask/component/fill.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="fill" v-if="filldata">
+        <div v-html="filldata.stem"></div>
+        <div v-for="(list,index) in filldata.answers" :key="index">
+            <div class="fill-subject">填空题（{{index+1}}）</div>
+            <van-field
+                v-model="filldata.answers[index]"
+                class="fill-input"
+                label-width="0px"
+                type="textarea"
+                placeholder="请填写答案"
+                rows="1"
+                autosize
+            />
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'fill-type',
+    props:{
+        filldata:{
+            type:Object,
+            default:()=>{}
+        }
+    },
+    data(){
+        return{
+            // filldata:{
+            //   id: "6",
+            //   type: "fill",
+            //   stem:
+            //     "<p>但是我奋斗的是[[我其实对方认为|v 奋斗色个人]]dwqfdwqer[[dewqad2w|dfwqe ]]</p>\r\n",
+            //   score: "2.0",
+            //   metas: [],
+            //   categoryId: "0",
+            //   difficulty: "normal",
+            //   target: "course-1",
+            //   courseId: "0",
+            //   lessonId: "0",
+            //   parentId: "0",
+            //   subCount: "0",
+            //   finishedTimes: "0",
+            //   passedTimes: "0",
+            //   createdUserId: "2",
+            //   updatedUserId: "2",
+            //   courseSetId: "1",
+            //   seq: "6",
+            //   missScore: "0.0",
+            //   answers:[]
+            // },
+            index:0
+        }
+    },
+    created(){
+
+    },
+    mounted(){
+        //this.replaceString();
+    },
+    methods:{
+         replaceString(){
+            const reg=/\[\[.+?\]\]/;
+            if(!reg.test(this.filldata.stem)){
+                return false
+            }else{
+                this.filldata.answers.push('');
+                this.$set(this.filldata,'answers',this.filldata.answers)
+                this.filldata.stem=this.filldata.stem.replace(reg,() => {
+                    return `<span class="fill-bank">（${++this.index}）</span>`
+                });
+                this.replaceString(this.filldata.stem)
+            }
+        }
+    }
+}
+</script>
