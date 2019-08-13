ad3dff5bdd8e282069bfaea1aabf9c8fab96b32a
LianJion <1995lian1002@gmail.com>
2019-08-13 13:27:52 +0800

    fix: #52047 新增题型

---
+++ h5/src/containers/course/lessonTask/component/determine.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="subject">
+		<div class="subject-stem">
+			<span class="subject-stem__order">{{ determine.id }}、</span>
+			<div class="subject-stem__content" v-html="determine.stem"></div>
+		</div>
+		<van-radio-group v-model="radio">
+			<van-radio class="subject-option subject-option--determine" name="1">
+				<div class="subject-option__content">对</div>
+				<i
+				slot="icon"
+				slot-scope="props"
+				class="iconfont icon-yes subject-option__order"
+				></i>
+			</van-radio>
+			<van-radio class="subject-option subject-option--determine" name="2">
+				<div class="subject-option__content">错</div>
+				<i
+				slot="icon"
+				slot-scope="props"
+				class="iconfont icon-no subject-option__order"
+				></i>
+			</van-radio>
+		</van-radio-group>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'single_choice',
+    data() {
+    return {
+      checked: true,
+      radio: '1',
+      result: [],
+      determine: {
+        "id": "4",
+        "type": "single_choice",
+        "stem": "<p>测试单选卡夫卡的飞开口道福克斯宽度发发多少发的（）</p>\r\n",
+        "score": "2.0",
+        "metas": {
+            "choices": [
+                "<p>选择铁马冰河入梦来选择铁马冰河入梦来选择铁马冰河入梦来选择铁马冰河入梦来</p>\r\n",
+                "<p>选择家祭无忘告乃翁</p>\r\n",
+                "<p>选项日照香炉生紫烟</p>\r\n",
+                "<p>选项葡萄美酒夜光杯</p>\r\n",
+            ]
+        },
+        "categoryId": "0",
+        "difficulty": "normal",
+        "target": "course-20",
+        "courseId": "0",
+        "lessonId": "0",
+        "parentId": "0",
+        "subCount": "0",
+        "finishedTimes": "0",
+        "passedTimes": "0",
+        "createdUserId": "2",
+        "updatedUserId": "2",
+        "courseSetId": "20",
+        "seq": "1",
+      },
+    }
+  },
+  methods: {
+  }
+}
+</script>
---
+++ h5/src/containers/course/lessonTask/component/single-choice.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="subject">
+    <div class="subject-stem">
+      <span class="subject-stem__order">{{ singleChoice.id }}、</span>
+      <div class="subject-stem__content" v-html="singleChoice.stem"></div>
+    </div>
+    <van-radio-group v-model="radio">
+      <van-radio class="subject-option" name="1"
+         v-for="(item, index) in singleChoice.metas.choices"
+        :key="item"
+        :name="item"
+      >
+        <div class="subject-option__content" v-html="item"></div>
+        <span
+          slot="icon"
+          slot-scope="props"
+          class="subject-option__order"
+        >{{ index|filterOrder }}</span>
+      </van-radio>
+    </van-radio-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'single_choice',
+    data() {
+    return {
+      checked: true,
+      radio: '1',
+      singleChoice: {
+        "id": "4",
+        "type": "single_choice",
+        "stem": "<p>测试单选卡夫卡的飞开口道福克斯宽度发发多少发的（）</p>\r\n",
+        "score": "2.0",
+        "metas": {
+            "choices": [
+                "<p>选择铁马冰河入梦来选择铁马冰河入梦来选择铁马冰河入梦来选择铁马冰河入梦来</p>\r\n",
+                "<p>选择家祭无忘告乃翁</p>\r\n",
+                "<p>选项日照香炉生紫烟</p>\r\n",
+                "<p>选项葡萄美酒夜光杯</p>\r\n",
+            ]
+        },
+        "categoryId": "0",
+        "difficulty": "normal",
+        "target": "course-20",
+        "courseId": "0",
+        "lessonId": "0",
+        "parentId": "0",
+        "subCount": "0",
+        "finishedTimes": "0",
+        "passedTimes": "0",
+        "createdUserId": "2",
+        "updatedUserId": "2",
+        "courseSetId": "20",
+        "seq": "1",
+        "missScore": "0.0"
+      },
+      result: [],
+    }
+  },
+  methods: {
+  },
+  filters: {
+    filterOrder(index) {
+      const arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+      return arr[index];
+    },
+  },
+
+}
+</script>
