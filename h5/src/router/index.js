cc73ae2c269f12f549aee2dffa5b356e94b2e884
LianJion <1995lian1002@gmail.com>
2019-08-16 08:58:34 +0800

    迁移目录

--- h5/src/router/index.js
+++ h5/src/router/index.js
@@ -16,14 +16,14 @@ const routes = [
     meta: {
       title: '考试说明'
     },
-    component: () => import(/* webpackChunkName: "intro" */ '@/containers/course/lessonTask/testpaperIntro.vue')
+    component: () => import(/* webpackChunkName: "intro" */ '@/containers/course/lessonTask/exam/testpaperIntro.vue')
   }, {
     path: '/testpaperDo',
     name: 'testpaperDo',
     meta: {
       title: '模拟考试开始'
     },
-    component: () => import(/* webpackChunkName: "do" */ '@/containers/course/lessonTask/testpaperDo.vue')
+    component: () => import(/* webpackChunkName: "do" */ '@/containers/course/lessonTask/exam/testpaperDo.vue')
   }, {
     path: '/result',
     name: 'testpaperResult',
