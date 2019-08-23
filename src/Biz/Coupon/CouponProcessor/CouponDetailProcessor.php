4a8f331630d633c51ddb4cc5a89e8dcd678f9299
zhangyao <zhangyao@howzhi.com>
2019-08-23 17:47:03 +0800

    feat: #52379 后台优惠券代码迁移从插件迁移到主程序

--- src/Biz/Coupon/CouponProcessor/CouponDetailProcessor.php
+++ src/Biz/Coupon/CouponProcessor/CouponDetailProcessor.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace Coupon\Biz\Coupon\CouponProcessor;
+namespace Biz\Coupon\CouponProcessor;
 
+use Biz\Card\DetailProcessor\DetailProcessor;
+use Biz\Coupon\Service\CouponService;
 use Topxia\Service\Common\ServiceKernel;
-use Biz\Card\DetailProcessor;
 
 class CouponDetailProcessor implements DetailProcessor
 {
@@ -17,8 +18,11 @@ class CouponDetailProcessor implements DetailProcessor
         return $this->getCouponService()->getCouponsByIds($ids);
     }
 
+    /**
+     * @return CouponService
+     */
     protected function getCouponService()
     {
-        return ServiceKernel::instance()->createService('Coupon:CouponService');
+        return ServiceKernel::instance()->getBiz()->service('Coupon:CouponService');
     }
 }
