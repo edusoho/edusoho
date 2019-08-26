b3c0583d05a247a25f4db5792d0c17a35c1f3e43
shenchupeng <shenchupeng@howzhi.com>
2019-08-26 18:04:53 +0800

    feat: #52321 短信发送增加图形验证

--- app/Resources/static-src/app/js/mobile-bind/mobile-bind.js
+++ app/Resources/static-src/app/js/mobile-bind/mobile-bind.js
@@ -1,21 +1,35 @@
 import SmsSender from 'app/common/widget/sms-sender';
 import Cookies from 'js-cookie';
 import notify from 'common/notify';
+import Drag from 'app/common/drag';
 
 export default class MobileBind {
   constructor() {
     this.$form = $('#mobile-bind-form');
     this.$smsCode = this.$form.find('.js-sms-send');
+    this.drag = $('#drag-btn').length ? new Drag($('#drag-btn'), $('.js-jigsaw'), {
+      limitType: 'web_register'
+    }) : null;
+    this.dragEvent();
     this.initCheckCookie();
     this.initValidator();
     this.initMobileCodeSendBtn();
+  }
+
+  dragEvent() {
+    let self = this;
+    if (this.drag) {
+      this.drag.on('success', function(token){
+        self.$smsCode.removeClass('disabled').attr('disabled', false);
+      });
+    }
   }
 
   initCheckCookie() {
     let key = this.$form.data('userId') + '-last-login-in';
 
     if (!Cookies.get(key) || Cookies.get(key) != new Date().getDate()) {
-      $('#mobile-bind-modal').modal();
+      $('#mobile-bind-modal').modal('show');
       Cookies.set(key, new Date().getDate());
     }
   }
@@ -74,10 +88,32 @@ export default class MobileBind {
     let self = this;
 
     this.$smsCode.on('click', function () {
+      self.$smsCode.attr('disabled', true);
       new SmsSender({
         element: '.js-sms-send',
         url: self.$smsCode.data('url'),
         smsType: 'sms_bind',
+        captcha: true,
+        captchaValidated: true,
+        captchaNum: 'dragCaptchaToken',
+        preSmsSend: function() {
+          return true;
+        },
+        error: function(error) {
+          self.drag.initDragCaptcha();
+        },
+        additionalAction: function(ackResponse) {
+          console.log(ackResponse);
+          if (ackResponse == 'captchaRequired') {
+            self.$smsCode.attr('disabled', true);
+            $('.js-drag-jigsaw').removeClass('hidden');
+            if(self.drag) {
+              self.drag.initDragCaptcha();
+            }
+            return true;
+          }
+          return false;
+        }
       });
     });
   }
--- app/Resources/static-src/app/js/settings/find_pay_password_by_sms/index.js
+++ app/Resources/static-src/app/js/settings/find_pay_password_by_sms/index.js
@@ -1,8 +1,12 @@
 import SmsSender from 'app/common/widget/sms-sender';
+import Drag from 'app/common/drag';
 
 let smsSend = '.js-sms-send';
 let $smsCode = $(smsSend);
 let $form = $('#settings-find-pay-password-form');
+let drag = $('#drag-btn').length ? new Drag($('#drag-btn'), $('.js-jigsaw'), {
+  limitType: 'web_register'
+}) : null;
 let validator = $form.validate({
   rules: {
     sms_code: {
@@ -24,13 +28,36 @@ $('#submit-btn').click(() => {
   }
 });
 
+drag.on('success', function(token){
+  $smsCode.removeClass('disabled').attr('disabled', false);
+});
+
 $smsCode.on('click', () => {
+  $smsCode.attr('disabled', true);
   new SmsSender({
     element: smsSend,
     url: $smsCode.data('smsUrl'),
     smsType: $smsCode.data('smsType'),
+    captcha: true,
+    captchaValidated: true,
+    captchaNum: 'dragCaptchaToken',
     preSmsSend: () => {
       return true;
+    },
+    error: function(error) {
+      drag.initDragCaptcha();
+    },
+    additionalAction: function(ackResponse) {
+      if (ackResponse == 'captchaRequired') {
+        $smsCode.attr('disabled', true);
+        $('.js-drag-jigsaw').removeClass('hidden');
+        if(drag) {
+          drag.initDragCaptcha();
+        }
+        return true;
+      }
+      return false;
     }
   });
 });
+
