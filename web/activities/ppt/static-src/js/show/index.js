2d22f6d17b40aa44ff42a52d24396d2616483953
LianJion <1995lian1002@gmail.com>
2019-08-27 17:37:49 +0800

    fix: #52456 切换代码

--- web/activities/ppt/static-src/js/show/index.js
+++ web/activities/ppt/static-src/js/show/index.js
@@ -12,11 +12,71 @@ let url = $('.js-cloud-url').data('url');
 
 let $element = $('#activity-ppt-content');
 
+let currentPPTPlayer = $element.data('type') || 'slide';
+
 initPptPlayer();
 
+
+
+$('.js-change-ppt-btn').on('click', (event) => {
+  console.log('点击数时间');
+  currentPPTPlayer = currentPPTPlayer === 'img' ? 'slide' : 'img';
+  initPptPlayer();
+})
+
+
 function initPptPlayer() {
+  if ($element.data('imgType') === 'onlyImg') {
+    initPPTNormalPlayer();
+  } else {
+    if (currentPPTPlayer ===  'img') {
+      initPPTImgPlayer();
+    } else if (currentPPTPlayer === 'slide') {
+      initPPTNormalPlayer();
+    }
+  }
+}
+
+
+function initPPTImgPlayer() {
+  let images = $element.data('imageInfo');
+  const imgPlayer = new QiQiuYun.Player({
+    id: 'activity-ppt-content',
+    // 环境配置
+    // playServer: app.cloudPlayServer,
+    // sdkBaseUri: app.cloudSdkBaseUri,
+    // disableDataUpload: app.cloudDisableLogReport,
+    // disableSentry: app.cloudDisableLogReport,
+    // resNo: $element.data('resNo'),
+    // token: $element.data('token'),
+    // user: {
+    //   id: $element.data('userId'),
+    //   name: $element.data('userName')
+    // },
+    source: {
+      type: 'ppt',
+      args: {
+        player: "ppt",
+        images,
+        type: "img",
+      }
+    },
+  });
+
+  imgPlayer.on('img.poschanged', (obj) => {
+    const page = Number(obj.pageNum);
+  });
+
+  imgPlayer.on('img.ready', () => {
+    imgPlayer.setCurrentPage(1);
+  });
+}
+
+// 包括旧版的img播放器和ppt播放器
+function initPPTNormalPlayer() {
   new QiQiuYun.Player({
     id: 'activity-ppt-content',
+    // 环境配置
     playServer: app.cloudPlayServer,
     sdkBaseUri: app.cloudSdkBaseUri,
     disableDataUpload: app.cloudDisableLogReport,
@@ -29,4 +89,3 @@ function initPptPlayer() {
     }
   });
 }
-
