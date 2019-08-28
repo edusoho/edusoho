af413452dad5bbeace2b97e418d6733aa221faf9
shenchupeng <shenchupeng@howzhi.com>
2019-08-28 09:30:13 +0800

    fix: #49350 修改学员详情加载

--- src/Biz/Activity/Dao/Impl/ActivityLearnLogDaoImpl.php
+++ src/Biz/Activity/Dao/Impl/ActivityLearnLogDaoImpl.php
@@ -57,6 +57,18 @@ class ActivityLearnLogDaoImpl extends GeneralDaoImpl implements ActivityLearnLog
         return $this->db()->fetchAll($sql, array($activityId, $userId)) ?: array();
     }
 
+    /**
+     * @deprecated
+     *
+     * @todo
+     * 对activity_learn_log分表后无法使用该方式统计用户对某一课程的累计学习天数，应使用临时表的方式解决：
+     * 即：定时将activity_learn_log原始数据按照业务进行精简汇总保存到某临时表中，从临时表查询所需数据
+     *
+     * @param $activityIds
+     * @param $userId
+     *
+     * @return int
+     */
     public function countLearnedDaysByActivityIdsAndUserId($activityIds, $userId)
     {
         if (empty($activityIds)) {
