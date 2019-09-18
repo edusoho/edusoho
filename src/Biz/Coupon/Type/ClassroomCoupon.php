09b5c3f2f907004ba080c0a287b26940f417926d
mixiaotian <mixiaotian@edusoho.com>
2019-09-18 17:56:44 +0800

    feat: #52446 移动端

--- src/Biz/Coupon/Type/ClassroomCoupon.php
+++ src/Biz/Coupon/Type/ClassroomCoupon.php
@@ -2,6 +2,8 @@
 
 namespace Biz\Coupon\Type;
 
+use Biz\Coupon\Service\CouponBatchResourceService;
+
 class ClassroomCoupon extends BaseCoupon
 {
     /**
@@ -9,6 +11,14 @@ class ClassroomCoupon extends BaseCoupon
      */
     public function canUseable($coupon, $target)
     {
-        return isset($target['id']) && $coupon['targetId'] === $target['id'];
+        return isset($target['id']) && $this->getCouponBatchResourceService()->isCouponTarget($coupon['batchId'], $target['id']);
+    }
+
+    /**
+     * @return CouponBatchResourceService
+     */
+    protected function getCouponBatchResourceService()
+    {
+        return $this->biz->service('Coupon:CouponBatchResourceService');
     }
 }
