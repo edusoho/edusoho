083cde752d0991a2d4b5090c73519e9bafc7ffea
wangcaijuan <wangcaijuan@edusoho.com>
2019-10-12 15:13:13 +0800

    feat: #191008 191008迭代升级脚本

---
+++ upgrade-scripts/upgrade-191008.php
@@ -0,0 +1,304 @@
+<?php
+
+use AppBundle\Component\Notification\WeChatTemplateMessage\TemplateUtil;
+use Symfony\Component\Filesystem\Filesystem;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function __construct($biz)
+    {
+        parent::__construct($biz);
+    }
+
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $result = $this->updateScheme($index);
+
+            $this->getConnection()->commit();
+
+            if (!empty($result)) {
+                return $result;
+            } else {
+                $this->logger('info', '执行升级脚本结束');
+            }
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            $this->logger('error', $e->getTraceAsString());
+            throw $e;
+        }
+
+        try {
+            $dir = realpath($this->biz['kernel.root_dir'] . '/../web/install');
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+            $this->logger('error', $e->getTraceAsString());
+        }
+
+        $developerSetting = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        $this->getSettingService()->set('developer', $developerSetting);
+        $this->getSettingService()->set('crontab_next_executed_time', time());
+    }
+
+    private function updateScheme($index)
+    {
+        $definedFuncNames = array(
+            'updateWechatNotificationSetting',
+            'registerUpdateTestpaperJob',
+        );
+
+        $funcNames = array();
+        foreach ($definedFuncNames as $key => $funcName) {
+            $funcNames[$key + 1] = $funcName;
+        }
+
+        if (0 == $index) {
+            $this->logger('info', '开始执行升级脚本');
+            $this->deleteCache();
+
+            return array(
+                'index' => $this->generateIndex(1, 1),
+                'message' => '升级数据...',
+                'progress' => 0,
+            );
+        }
+
+        list($step, $page) = $this->getStepAndPage($index);
+        $method = $funcNames[$step];
+        $page = $this->$method($page);
+
+        if (1 == $page) {
+            ++$step;
+        }
+
+        if ($step <= count($funcNames)) {
+            return array(
+                'index' => $this->generateIndex($step, $page),
+                'message' => '升级数据...',
+                'progress' => 0,
+            );
+        }
+    }
+
+    public function updateWechatNotificationSetting()
+    {
+        $wechatSetting = $this->getSettingService()->get('wechat');
+
+        if (!empty($wechatSetting['templates']['liveOpen']['templateId'])) {
+            $client = $this->biz['wechat.template_message_client'];
+            if (empty($client)) {
+                throw new \RuntimeException('获取微信信息错误');
+            }
+
+            if (!empty($wechatSetting['is_authorization'])) {
+                $sdk = $this->biz['qiQiuYunSdk.wechat'];
+                $data = $sdk->createNotificationTemplate(TemplateUtil::TEMPLATE_LIVE_OPEN_CODE);
+            } else {
+                $data = $client->addTemplate(TemplateUtil::TEMPLATE_LIVE_OPEN_CODE);
+            }
+
+            if (empty($data)) {
+                throw new \RuntimeException('模板打开失败');
+            }
+
+            $wechatSetting['templates']['liveOpen']['templateId'] = $data['template_id'];
+
+            $this->getSettingService()->set('wechat', $wechatSetting);
+        }
+
+        return 1;
+    }
+
+    public function registerUpdateTestpaperJob()
+    {
+        if ($this->isTableExist('biz_scheduler_job')) {
+            $currentTime = time();
+            $time = strtotime(date('Y-m-d' . ' 01:00:00', strtotime("+1 day")));
+            $this->getConnection()->exec("INSERT INTO `biz_scheduler_job` (
+                `name`,
+                `expression`,
+                `class`,
+                `args`,
+                `priority`,
+                `pre_fire_time`,
+                `next_fire_time`,
+                `misfire_threshold`,
+                `misfire_policy`,
+                `enabled`,
+                `creator_id`,
+                `updated_time`,
+                `created_time`
+            ) VALUES (
+              'UpdateTestpaperTotalScoresJob',
+              '',
+              'Biz\\\\Testpaper\\\\\Job\\\\UpdateTestpaperTotalScoresJob',
+              '',
+              '200',
+              '0',
+              '{$time}',
+              '300',
+              'executing',
+              '1',
+              '0',
+              '{$currentTime}',
+              '{$currentTime}'
+        )");
+            $this->logger('info', '试卷各题型总分计算定时任务');
+        }
+        return 1;
+    }
+
+
+    protected function generateIndex($step, $page)
+    {
+        return $step * 1000000 + $page;
+    }
+
+    protected function getStepAndPage($index)
+    {
+        $step = intval($index / 1000000);
+        $page = $index % 1000000;
+
+        return array($step, $page);
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isIndexExist($table, $filedName, $indexName)
+    {
+        $sql = "show index from `{$table}` where column_name = '{$filedName}' and Key_name = '{$indexName}';";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function createIndex($table, $index, $column)
+    {
+        if (!$this->isIndexExist($table, $column, $index)) {
+            $this->getConnection()->exec("ALTER TABLE {$table} ADD INDEX {$index} ({$column})");
+        }
+    }
+
+    protected function isJobExist($code)
+    {
+        $sql = "select * from biz_scheduler_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function deleteCache()
+    {
+        $cachePath = $this->biz['cache_directory'];
+        $filesystem = new Filesystem();
+        $filesystem->remove($cachePath);
+
+        clearstatcache(true);
+
+        $this->logger('info', '删除缓存');
+
+        return 1;
+    }
+
+    private function makeUUID()
+    {
+        return sha1(uniqid(mt_rand(), true));
+    }
+
+    private function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+    protected function getUserDao()
+    {
+        return $this->createDao('User:UserDao');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Service\SchedulerService
+     */
+    protected function getSchedulerService()
+    {
+        return $this->createService('Scheduler:SchedulerService');
+    }
+
+    /**
+     * @return \Codeages\Biz\Framework\Scheduler\Dao\JobDao
+     */
+    protected function getJobDao()
+    {
+        return $this->createDao('Scheduler:JobDao');
+    }
+
+    /**
+     * @return \Biz\CloudPlatform\Service\AppService
+     */
+    protected function getAppService()
+    {
+        return $this->createService('CloudPlatform:AppService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+
+    protected function logger($level, $message)
+    {
+        $version = \AppBundle\System::VERSION;
+        $data = date('Y-m-d H:i:s') . " [{$level}] {$version} " . $message . PHP_EOL;
+        if (!file_exists($this->getLoggerFile())) {
+            touch($this->getLoggerFile());
+        }
+        file_put_contents($this->getLoggerFile(), $data, FILE_APPEND);
+    }
+
+    private function getLoggerFile()
+    {
+        return $this->biz['kernel.root_dir'] . '/../app/logs/upgrade.log';
+    }
+}
