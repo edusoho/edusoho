ff3ad096c519235b10def0b412c9d172f9dfdf7d
lizhenxiong <lizhenxiong@howzhi.com>
2019-10-15 17:11:41 +0800

    feat: #51951 补充授权登陆相关数据库表comment

--- migrations/20190822091235_create_table_oauth.php
+++ migrations/20190822091235_create_table_oauth.php
@@ -12,85 +12,82 @@ class CreateTableOauth extends Migration
         $container = $this->getContainer();
         $db = $container['db'];
 
-        $db->exec('
+        $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_access_token` (
-              `token` varchar(40) NOT NULL,
-              `client_id` varchar(50) DEFAULT NULL,
-              `user_id` varchar(100) DEFAULT NULL,
-              `expires` datetime NOT NULL,
-              `scope` varchar(50) DEFAULT NULL,
+              `token` varchar(40) NOT NULL COMMENT '授权TOKEN',
+              `client_id` varchar(50) DEFAULT NULL COMMENT '客户端ID',
+              `user_id` varchar(100) DEFAULT NULL COMMENT '用户ID',
+              `expires` datetime NOT NULL COMMENT '有效期',
+              `scope` varchar(50) DEFAULT NULL COMMENT '授权范围',
               PRIMARY KEY (`token`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        ');
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权令牌表';
+        ");
 
-        $db->exec('
+        $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_authorization_code` (
-              `code` varchar(40) NOT NULL,
-              `client_id` varchar(50) DEFAULT NULL,
-              `expires` datetime NOT NULL,
-              `user_id` varchar(100) DEFAULT NULL,
-              `redirect_uri` longtext NOT NULL,
-              `scope` varchar(255) DEFAULT NULL,
+              `code` varchar(40) NOT NULL COMMENT '授权码',
+              `client_id` varchar(50) DEFAULT NULL COMMENT '客户端ID',
+              `expires` datetime NOT NULL COMMENT '有效期',
+              `user_id` varchar(100) DEFAULT NULL COMMENT '用户ID',
+              `redirect_uri` longtext NOT NULL COMMENT '客户端授权登陆回调地址',
+              `scope` varchar(255) DEFAULT NULL COMMENT '授权范围',
               `id_token` varchar(255) DEFAULT NULL,
               PRIMARY KEY (`code`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        ');
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权码表';
+        ");
 
         $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_client` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `client_id` varchar(50) NOT NULL DEFAULT '',
-              `client_secret` varchar(40) NOT NULL DEFAULT '',
-              `redirect_uri` text NOT NULL,
-              `grant_types` text,
-              `scopes` text,
-              `createdUserId` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '创建用户ID',
-              `createdTime` int(10) unsigned NOT NULL DEFAULT 0  COMMENT '创建时间',
-              `updateTime` int(10) unsigned NOT NULL DEFAULT 0  COMMENT '最后更新时间',
+              `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+              `client_id` varchar(50) NOT NULL DEFAULT '' COMMENT '客户端ID',
+              `client_secret` varchar(40) NOT NULL DEFAULT '' COMMENT '客户端secret',
+              `redirect_uri` text NOT NULL COMMENT '客户端授权登陆回调地址',
+              `grant_types` text COMMENT '授权类型',
+              `scopes` text COMMENT '授权范围',
+              `createdUserId` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建用户ID',
+              `createdTime` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
+              `updateTime` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '最后更新时间',
               PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权客户端表';
         ");
 
-        $db->exec('
+        $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_client_public_key` (
               `client_id` varchar(50) NOT NULL,
               `public_key` longtext NOT NULL,
               PRIMARY KEY (`client_id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        ');
+        ");
 
-        $db->exec('
+        $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_refresh_token` (
-              `token` varchar(40) NOT NULL,
-              `client_id` varchar(50) DEFAULT NULL,
-              `user_id` varchar(100) DEFAULT NULL,
-              `expires` datetime NOT NULL,
-              `scope` varchar(255) DEFAULT NULL,
+              `token` varchar(40) NOT NULL COMMENT '授权TOKEN',
+              `client_id` varchar(50) DEFAULT NULL COMMENT '客户端ID',
+              `user_id` varchar(100) DEFAULT NULL COMMENT '用户ID',
+              `expires` datetime NOT NULL COMMENT '有效期',
+              `scope` varchar(255) DEFAULT NULL COMMENT '授权类型',
               PRIMARY KEY (`token`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
-        ');
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权刷新令牌表';
+        ");
 
-        $db->exec('
+        $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_scope` (
-              `scope` varchar(255) NOT NULL,
-              `description` varchar(255) NOT NULL,
+              `scope` varchar(255) NOT NULL COMMENT '授权范围',
+              `description` varchar(255) NOT NULL COMMENT '授权范围描述',
               PRIMARY KEY (`scope`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
-        ');
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权范围表';
+        ");
 
-        $db->exec('
+        $db->exec("
             CREATE TABLE IF NOT EXISTS `oauth_user` (
-              `username` varchar(255) NOT NULL,
-              `password` varchar(255) NOT NULL,
+              `username` varchar(255) NOT NULL COMMENT '用户名',
+              `password` varchar(255) NOT NULL COMMENT '密码',
               `salt` varchar(255) NOT NULL,
-              `roles` longtext,
-              `scopes` longtext,
+              `roles` longtext COMMENT '用户角色',
+              `scopes` longtext COMMENT '用户授权范围',
               PRIMARY KEY (`username`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        ');
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='授权用户';
+        ");
     }
 
     /**
