2387d2da10336d1c637b624d7868a85f527cd285
zhaolaisong <zhaolaisong@edusoho.com>
2019-10-28 20:16:02 +0800

    删除多余事件，增加测试token解释

--- tests/Unit/Distributor/Service/DistributorProductDealerServiceTest.php
+++ tests/Unit/Distributor/Service/DistributorProductDealerServiceTest.php
@@ -21,6 +21,17 @@ class DistributorProductDealerServiceTest extends BaseTestCase
 
         $this->biz['drp.plugin.logger'] = $logger;
 
+        /*
+         * 分销系统生成的token {内容类型}:{内容数据}:{网校ID}:{代理商ID}:{时间戳}:{随机数}:{签名}
+         *  $splitStr = explode(':', $token);
+            $data = array(
+                'redirect_type' => $splitStr[0],
+                'redirect_content' => $splitStr[1],
+                'merchant_id' => $splitStr[2],
+                'agency_id' => $splitStr[3],
+            );
+        *   利用云平台api：cloud_access_key cloud_secret_key加密数据
+        */
         $token = 'redirect:L2NvdXJzZS8x:1:1:1565751518:dasdaskjd:CtFJ-tHEOkPSj1yabW9nbXo9oKA=';
 
         $drpUserService = $this->mockBiz(
