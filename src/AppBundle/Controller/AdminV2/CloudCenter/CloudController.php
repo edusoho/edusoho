b7016d0ebf2553fc7fe050f229f16f09af334d37
zhangyao <zhangyao@howzhi.com>
2019-10-29 13:52:37 +0800

    feat: #54292 应用中心-基础云-云服务

--- src/AppBundle/Controller/AdminV2/CloudCenter/CloudController.php
+++ src/AppBundle/Controller/AdminV2/CloudCenter/CloudController.php
@@ -9,6 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CloudController extends BaseController
 {
+    public function buyAction(Request $request, $type)
+    {
+        $params = array('type' => $type);
+
+        return $this->redirectUrl('edu_cloud_buy', $params);
+    }
+
     public function accessWechatAction(Request $request)
     {
         return $this->redirectUrl('product_marketing_detail', array('type' => 'microprogram'));
@@ -17,6 +24,68 @@ class CloudController extends BaseController
     public function accessAction(Request $request)
     {
         return $this->redirectUrl('edu_cloud');
+    }
+
+    public function smsAccountAction(Request $request)
+    {
+        return $this->redirectUrl('service_sms_count');
+    }
+
+    public function smsDetailAction(Request $request)
+    {
+        return $this->redirectUrl('service_sms_list_detail');
+    }
+
+    public function smsStatisticsAction(Request $request)
+    {
+        return $this->redirectUrl('service_sms_count');
+    }
+
+    public function smsSettingAction(Request $request)
+    {
+        return $this->redirectUrl('service_sms_setting');
+    }
+
+    public function cloudShowAction(Request $request, $type)
+    {
+        return $this->redirectUrl('edu_cloud_show', array('type' => $type));
+    }
+
+    public function videoAction(Request $request)
+    {
+        $params = array('type' => 'video');
+
+        return $this->redirectUrl('edu_cloud_show', $params);
+    }
+
+    public function videoRenewAction(Request $request)
+    {
+        return $this->redirectUrl('edu_cloud_video_renew');
+    }
+
+    public function videoDetailAction()
+    {
+        return $this->redirectUrl('service_storage_chart');
+    }
+
+    public function videoUpgradeAction(Request $request)
+    {
+        return $this->redirectUrl('edu_cloud_video_upgrade');
+    }
+
+    public function liveMoreAction(Request $request)
+    {
+        return $this->redirectUrl('service_live_count');
+    }
+
+    public function liveUpgradeAction(Request $request)
+    {
+        return $this->redirectUrl('edu_cloud_live_upgrade');
+    }
+
+    public function liveRenewAction(Request $request)
+    {
+        return $this->redirectUrl('edu_cloud_live_renew');
     }
 
     public function rechargeAction(Request $request)
@@ -39,6 +108,49 @@ class CloudController extends BaseController
         $url = 'service_'.$type.'_overview';
 
         return $this->redirectUrl($url);
+    }
+
+    public function smsSignAction(Request $request)
+    {
+        return $this->redirectUrl('service_sms_sign');
+    }
+
+    public function emailBuyAction(Request $request, $type)
+    {
+        $params = array('type' => $type);
+
+        return $this->redirectUrl('edu_cloud_buy_custom', $params);
+    }
+
+    public function emailListAction(Request $request)
+    {
+        return $this->redirectUrl('service_email_list');
+    }
+
+    public function liveAction(Request $request)
+    {
+        $params = array('type' => 'live');
+
+        return $this->redirectUrl('edu_cloud_show', $params);
+    }
+
+    public function tlpAction(Request $request)
+    {
+        $params = array('type' => 'tlp');
+
+        return $this->redirectUrl('edu_cloud_show', $params);
+    }
+
+    public function searchAction(Request $request)
+    {
+        $params = array('type' => 'search');
+
+        return $this->redirectUrl('edu_cloud_show', $params);
+    }
+
+    public function searchDetailAction(Request $request)
+    {
+        return $this->redirectUrl('service_search_overview');
     }
 
     protected function redirectUrl($routingName, $params = array())
