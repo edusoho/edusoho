5e7b88da70c0045fad18010a2a993ca5ff2926e4
LianJion <1995lian1002@gmail.com>
2019-11-06 21:16:16 +0800

    fix: #54319 自测发现的问题

--- app/Resources/static-src/app/js/admin-v2/default/index.js
+++ app/Resources/static-src/app/js/admin-v2/default/index.js
@@ -6,21 +6,35 @@ $('.js-show-toggle').on('click', (event) => {
   $target.find('i').toggleClass('es-icon-keyboardarrowup es-icon-keyboardarrowdown');
 });
 
+const $modal = $('#functionModal');
 const $html = $('html');
-$('#functionModal').on('shown.bs.modal', (e) =>  {
+$modal.on('shown.bs.modal', (e) =>  {
   $html.css('overflow', 'hidden');
 }).on('hidden.bs.modal', (e) => {
   $html.css('overflow', 'scroll');
-})
+});
 
 
 $('.js-entrance-list').on('click', '.js-function-choose', (event) => {
   const $target = $(event.currentTarget);
   $target.toggleClass('active');
-})
+});
+
+let currentItem = [];
 
 $('.js-save-btn').on('click', (event) => {
+  const $target = $(event.currentTarget);
   if ($('.js-function-choose.active').length > 7) {
-    cd.message({type: 'danger', message: '最多设置7个快捷入口位'})
+    cd.message({type: 'danger', message: '最多设置7个快捷入口位'});
+    return;
   }
-})
+  const $quickItem = $('.js-function-body').find('.js-function-choose');
+  $quickItem.each(item => {
+    const code = $(item).data('link');
+    const flag = $(item).hasClass('active');
+    if (flag && !currentItem.includes(code)) {
+      currentItem.push(code);
+    }
+  });
+  $modal.modal('hide');
+});
