d41c1a77ae76ce8da510c5d2cf2f0f1c51112ec3
wangcaijuan <wangcaijuan@edusoho.com>
2019-11-12 10:24:29 +0800

    feat: #54944 气球鱼sdk

---
+++ vendor/codeages/qiqiuyun-php-sdk/src/Constants/PlatformNewsBlockTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QiQiuYun\SDK\Constants;
+
+final class PlatformNewsBlockTypes
+{
+    //  ES商学院课程推荐
+    const ADVICE_BLOCK = 1;
+
+    //  ES后台插件推荐
+    const PLUGIN_BLOCK = 2;
+
+    //  公众号二维码
+    const QR_CODE_BLOCK = 3;
+
+    //  站长公告
+    const ANNOUNCEMENT_BLOCK = 4;
+}
