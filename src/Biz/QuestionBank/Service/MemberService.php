e6889b1734f2819918657edd1cce994732372216
shenchupeng <shenchupeng@howzhi.com>
2019-11-14 16:29:23 +0800

    feat: #55021 fix

--- src/Biz/QuestionBank/Service/MemberService.php
+++ src/Biz/QuestionBank/Service/MemberService.php
@@ -4,5 +4,11 @@ namespace Biz\QuestionBank\Service;
 
 interface MemberService
 {
+    public function findMembersByBankId($bankId);
+
     public function createMember($fields);
+
+    public function batchDeleteByBankId($bankId);
+
+    public function batchCreateMembers($bankId, $userIds);
 }
