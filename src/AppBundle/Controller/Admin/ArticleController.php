3f9756f8427b09affee1e19933399cec6f261eeb
shenchupeng <shenchupeng@howzhi.com>
2019-11-14 16:32:14 +0800

    feat: #55021 fix

--- src/AppBundle/Controller/Admin/ArticleController.php
+++ src/AppBundle/Controller/Admin/ArticleController.php
@@ -103,7 +103,7 @@ class ArticleController extends BaseController
 
             $this->getUploadFileService()->createUseFiles($attachment['fileIds'], $article['id'], $attachment['targetType'], $attachment['type']);
 
-            //return $this->redirect($this->generateUrl('admin_article'));
+            return $this->redirect($this->generateUrl('admin_article'));
         }
 
         return $this->render('admin/article/article-modal.html.twig', array(
