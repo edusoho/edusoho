f88a675083a406eea86d77e4fd7ac5f40cf52515
chelingfeng <chelingfeng@edusoho.com>
2019-11-15 15:13:48 +0800

    feat:直播ticket接口迁移

---
+++ src/ApiBundle/Api/Resource/Task/TaskLiveReplay.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ApiBundle\Api\Resource\Task;
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+
+class TaskLiveReplay extends AbstractResource
+{
+    public function add(ApiRequest $request, $taskId)
+    {
+    }
+}
