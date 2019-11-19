64672f6740d178b511c6c02d64a216134cd85ddd
LianJion <1995lian1002@gmail.com>
2019-11-19 19:56:27 +0800

    fix: #55232 对接问卷

--- app/Resources/static-src/app/js/admin-v2/default/switch-old-version/index.js
+++ app/Resources/static-src/app/js/admin-v2/default/switch-old-version/index.js
@@ -1,13 +1,45 @@
-let $switchBtn = $('#switch-old-version-btn');
-$switchBtn.on('click', function (e) {
-    if (!confirm(Translator.trans('admin.switch_old_version.confirm_message'))) {
-        return;
-    }
+const $switchBtn = $('.js-switch-version-btn');
+$switchBtn.on('click', () => {
+  cd.confirm({
+    title: Translator.trans('admin.switch_old_version.title'),
+    content: Translator.trans('admin.switch_old_version.confirm_message'),
+    okText: Translator.trans('site.confirm'),
+    cancelText: Translator.trans('site.cancel'),
+    className: '',
+  }).on('ok', () => {
     $switchBtn.button('loading');
-    $.post($switchBtn.data('url'), function (res) {
-        if (res.status == 'success' && res.url) {
-            window.location.href = res.url;
-        }
+    $.post($switchBtn.data('url'), (res) => {
+      if (res.status == 'success' && res.url) {
+        window.location.href = res.url;
+      }
     });
-
+  }).on('cancel', () => {});
 });
+
+
+const iframe = document.getElementById('iframe');
+iframe.onload = () => {
+  iframe.contentWindow.postMessage(JSON.stringify({
+    actions: 'init'
+  }), '*');
+};
+
+const onMessage = function (event) {
+  try {
+    let handleData = event.data;
+    if (typeof event.data !== 'object') {
+      handleData = JSON.parse(event.data);
+    }
+    if (handleData.actions === 'reload') {
+      $switchBtn.prop('disabled', false);
+    }
+  } catch (e) {
+    console.log('catch');
+  }
+};
+
+if (window.addEventListener) {
+  window.addEventListener('message', onMessage, false);
+} else {
+  window.attachEvent('onmessage', onMessage);
+}
