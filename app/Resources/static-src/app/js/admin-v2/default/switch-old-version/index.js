8e80b32da765f506e96f4a89e14fd54d2cdc4f5c
LianJion <1995lian1002@gmail.com>
2019-11-19 20:26:01 +0800

    fix: #55232 修改交互

--- app/Resources/static-src/app/js/admin-v2/default/switch-old-version/index.js
+++ app/Resources/static-src/app/js/admin-v2/default/switch-old-version/index.js
@@ -1,23 +1,6 @@
 const $switchBtn = $('.js-switch-version-btn');
-$switchBtn.on('click', () => {
-  cd.confirm({
-    title: Translator.trans('admin.switch_old_version.title'),
-    content: Translator.trans('admin.switch_old_version.confirm_message'),
-    okText: Translator.trans('site.confirm'),
-    cancelText: Translator.trans('site.cancel'),
-    className: '',
-  }).on('ok', () => {
-    $switchBtn.button('loading');
-    $.post($switchBtn.data('url'), (res) => {
-      if (res.status == 'success' && res.url) {
-        window.location.href = res.url;
-      }
-    });
-  }).on('cancel', () => {});
-});
-
-
 const iframe = document.getElementById('iframe');
+
 iframe.onload = () => {
   iframe.contentWindow.postMessage(JSON.stringify({
     actions: 'init'
@@ -31,7 +14,11 @@ const onMessage = function (event) {
       handleData = JSON.parse(event.data);
     }
     if (handleData.actions === 'reload') {
-      $switchBtn.prop('disabled', false);
+      $.post($switchBtn.data('url'), (res) => {
+        if (res.status == 'success' && res.url) {
+          window.location.href = res.url;
+        }
+      });
     }
   } catch (e) {
     console.log('catch');
