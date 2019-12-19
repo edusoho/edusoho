89178595e8b295a0e46e443fcf005b0aa6f1b32a
shenchupeng <shenchupeng@howzhi.com>
2019-12-19 15:01:06 +0800

    feat: #56486 中台题库创建和编辑

--- src/AppBundle/Controller/QuestionBank/ManageController.php
+++ src/AppBundle/Controller/QuestionBank/ManageController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller\QuestionBank;
 use AppBundle\Common\ArrayToolkit;
 use AppBundle\Common\Paginator;
 use AppBundle\Controller\BaseController;
+use Biz\QuestionBank\Service\CategoryService;
+use Biz\QuestionBank\Service\MemberService;
 use Biz\QuestionBank\Service\QuestionBankService;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -43,8 +45,69 @@ class ManageController extends BaseController
         ));
     }
 
-    public function manageAction()
+    public function createAction(Request $request)
     {
+        if ('POST' == $request->getMethod()) {
+            $data = $request->request->all();
+            $data['members'] = $this->getCurrentUser()->getId();
+            $questionBank = $this->getQuestionBankService()->createQuestionBank($data);
+
+            return $this->createJsonResponse(array(
+                'goto' => $this->generateUrl('question_bank_manage_question_category', array('id' => $questionBank['id']))
+            ));
+        }
+
+        $categoryTree = $this->getCategoryService()->getCategoryTree();
+
+        return $this->render('question-bank/manage/create-modal.html.twig', array(
+            'categoryTree' => $categoryTree,
+        ));
+    }
+
+    public function manageAction(Request $request, $id)
+    {
+        if (!$this->getQuestionBankService()->canManageBank($id)) {
+            return $this->createMessageResponse('error', '您不是该题库管理者，不能查看此页面！');
+        }
+
+        if ('POST' == $request->getMethod()) {
+            $members = $request->request->get('members', '');
+            $questionBank = $this->getQuestionBankService()->updateQuestionBankWithMembers($id, $request->request->all(), $members);
+
+            return $this->createJsonResponse(array(
+                'goto' => $this->generateUrl('question_bank_manage_question_category', array('id' => $questionBank['id']))
+            ));
+        }
+
+        $questionBank = $this->getQuestionBankService()->getQuestionBank($id);
+        $members = $this->getMemberService()->findMembersByBankId($id);
+        $users = $this->getUserService()->findUsersByIds(ArrayToolkit::column($members, 'userId'));
+        $bankMembers = array();
+        foreach ($users as $user) {
+            $bankMembers[] = array('id' => $user['id'], 'name' => $user['nickname']);
+        }
+        $categoryTree = $this->getCategoryService()->getCategoryTree();
+
+        return $this->render('question-bank/manage/info.html.twig', array(
+            'questionBank' => $questionBank,
+            'categoryTree' => $categoryTree,
+            'bankMembers' => json_encode($bankMembers),
+        ));
+    }
+
+    public function memberMatchAction(Request $request)
+    {
+        $queryField = $request->query->get('q');
+
+        $users = $this->getUserService()->searchUsers(
+            array('nickname' => $queryField, 'roles' => 'ROLE_TEACHER'),
+            array('createdTime' => 'DESC'),
+            0,
+            10,
+            array('id', 'nickname')
+        );
+
+        return $this->createJsonResponse($users);
     }
 
     /**
@@ -53,5 +116,21 @@ class ManageController extends BaseController
     protected function getQuestionBankService()
     {
         return $this->createService('QuestionBank:QuestionBankService');
+    }
+
+    /**
+     * @return CategoryService
+     */
+    protected function getCategoryService()
+    {
+        return $this->createService('QuestionBank:CategoryService');
+    }
+
+    /**
+     * @return MemberService
+     */
+    protected function getMemberService()
+    {
+        return $this->createService('QuestionBank:MemberService');
     }
 }
