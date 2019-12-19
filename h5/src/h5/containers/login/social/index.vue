9d23158809f248265b8e6966f9388ed365d1ec37
chenjinhong <chenjinhong@edusoho.com>
2019-12-19 16:00:37 +0800

    feat: #56417 解决封禁用户后，微信环境内一直在登录问题

--- h5/src/h5/containers/login/social/index.vue
+++ h5/src/h5/containers/login/social/index.vue
@@ -1,84 +1,92 @@
 <template>
-  <div/>
+  <div />
 </template>
 
 <script>
-import activityMixin from '@/mixins/activity'
-import redirectMixin from '@/mixins/saveRedirect'
-import * as types from '@/store/mutation-types'
-import Api from '@/api'
-import { Toast } from 'vant'
-import { mapMutations, mapState } from 'vuex'
+import activityMixin from "@/mixins/activity";
+import redirectMixin from "@/mixins/saveRedirect";
+import * as types from "@/store/mutation-types";
+import Api from "@/api";
+import { Toast } from "vant";
+import { mapMutations, mapState } from "vuex";
 
 export default {
-  name: 'SocialWx',
+  name: "SocialWx",
   mixins: [activityMixin, redirectMixin],
   async created() {
     if (this.$store.state.token) {
       // 判断非首次登陆后的跳转
-      const firstDirect = localStorage.getItem('first_direct')
+      const firstDirect = localStorage.getItem("first_direct");
       if (firstDirect == 1) {
         this.$router.replace({
-          path: this.$route.query.redirect || '/'
-        })
-        return
+          path: this.$route.query.redirect || "/"
+        });
+        return;
       }
 
       // 判断首次登陆后的跳转
       Toast.loading({
-        message: '请稍后'
-      })
-      this.afterLogin()
-      localStorage.setItem('first_direct', 1)
-      return
+        message: "请稍后"
+      });
+      this.afterLogin();
+      localStorage.setItem("first_direct", 1);
+      return;
     }
     // 获取微信绑定状态
-    const socialBinded_wx = localStorage.getItem('socialBinded_wx')
-      ? JSON.parse(localStorage.getItem('socialBinded_wx'))
-      : this.socialBinded['wx']
+    const socialBinded_wx = localStorage.getItem("socialBinded_wx")
+      ? JSON.parse(localStorage.getItem("socialBinded_wx"))
+      : this.socialBinded["wx"];
 
-    this.setSocialStatus({ key: 'wx', status: socialBinded_wx })
+    this.setSocialStatus({ key: "wx", status: socialBinded_wx });
 
-    let code = location.search.match(/\?code.*&/g)
+    let code = location.search.match(/\?code.*&/g);
 
     if (!code) {
-      localStorage.setItem('first_direct', 0)
-      this.wxLogin()
-      return
+      localStorage.setItem("first_direct", 0);
+      this.wxLogin();
+      return;
     }
     Toast.loading({
-      message: '正在登录'
-    })
-    code = code[0].slice(6, -1)
+      message: "正在登录"
+    });
+    code = code[0].slice(6, -1);
     await Api.login({
       params: {
         code: code,
-        type: 'weixinmob'
+        type: "weixinmob"
       }
-    }).then(res => {
-      this.userLogin(res)
-      Toast.clear()
-      Toast.success({
-        duration: 2000,
-        message: '登录成功'
-      })
-      let routerDepth = this.socialBinded.wx ? -2 : -7
-      if (this.$route.query.callbackType) {
-        routerDepth = routerDepth + 1
-      }
-      localStorage.setItem('socialBinded_wx', true)
-      this.$router.go(routerDepth)
-    }).catch(err => {
-      // 更新微信绑定状态
-      const socialBinded_wx = false
-      localStorage.setItem('socialBinded_wx', socialBinded_wx)
-
-      window.location.href = '/login/bind/weixinmob?os=h5&_target_path=' +
-        encodeURIComponent(location.pathname + location.hash)
     })
+      .then(res => {
+        this.userLogin(res);
+        Toast.clear();
+        Toast.success({
+          duration: 2000,
+          message: "登录成功"
+        });
+        let routerDepth = this.socialBinded.wx ? -2 : -7;
+        if (this.$route.query.callbackType) {
+          routerDepth = routerDepth + 1;
+        }
+        localStorage.setItem("socialBinded_wx", true);
+        this.$router.go(routerDepth);
+      })
+      .catch(err => {
+        if (err.code === 4030115) {
+          Toast.fail({duration: 2000,message: err.message })
+          this.redirectLogin()
+          return;
+        }
+        // 更新微信绑定状态
+        const socialBinded_wx = false;
+        localStorage.setItem("socialBinded_wx", socialBinded_wx);
+
+        window.location.href =
+          "/login/bind/weixinmob?os=h5&_target_path=" +
+          encodeURIComponent(location.pathname + location.hash);
+      });
   },
   computed: {
-    ...mapState(['socialBinded'])
+    ...mapState(["socialBinded"])
   },
   methods: {
     ...mapMutations({
@@ -87,15 +95,28 @@ export default {
     }),
     wxLogin() {
       Toast.loading({
-        message: '请稍后'
-      })
-      const redirectUrl = encodeURIComponent(location.origin + location.pathname + location.hash)
-      window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' +
+        message: "请稍后"
+      });
+      const redirectUrl = encodeURIComponent(
+        location.origin + location.pathname + location.hash
+      );
+      window.location.href =
+        "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +
         this.$route.query.weixinmob_key +
-        '&redirect_uri=' +
+        "&redirect_uri=" +
         redirectUrl +
-        '&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect'
+        "&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect";
+    },
+    redirectLogin() {
+      setTimeout(() => {
+        this.$router.replace({
+          name: "login" ,
+          query: {
+            'forbidWxLogin':true
+          }
+        });
+      }, 2000);
     }
   }
-}
+};
 </script>
