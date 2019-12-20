1d6c249286b60176867940b576a55c96d13f9e45
shenchupeng <shenchupeng@howzhi.com>
2019-12-20 15:15:40 +0800

    feat: #55879 增加索引定时任务

--- upgrade-scripts/upgrade-8.6.0.php
+++ upgrade-scripts/upgrade-8.6.0.php
@@ -76,6 +76,7 @@ class EduSohoUpgrade extends AbstractUpdater
             'migrateTestpapers',
             'migrateQuestionsAndExercises',
             'updateTestpaperActivity',
+            'addTableIndex',
         );
 
         $funcNames = array();
@@ -454,6 +455,52 @@ class EduSohoUpgrade extends AbstractUpdater
         }
 
         return $nextPage;
+    }
+
+    protected function addTableIndex()
+    {
+        if ($this->isJobExist('HandlingTimeConsumingUpdateStructuresJob')) {
+            return 1;
+        }
+
+        $currentTime = time();
+        $today = strtotime(date('Y-m-d', $currentTime) . '02:00:00');
+
+        if ($currentTime > $today) {
+            $time = strtotime(date('Y-m-d', strtotime('+1 day')) . '02:00:00');
+        }
+
+        $this->getConnection()->exec("INSERT INTO `biz_scheduler_job` (
+              `name`,
+              `expression`,
+              `class`,
+              `args`,
+              `priority`,
+              `pre_fire_time`,
+              `next_fire_time`,
+              `misfire_threshold`,
+              `misfire_policy`,
+              `enabled`,
+              `creator_id`,
+              `updated_time`,
+              `created_time`
+        ) VALUES (
+              'HandlingTimeConsumingUpdateStructuresJob',
+              '',
+              'Biz\\\\UpdateDatabaseStructure\\\\\Job\\\\HandlingTimeConsumingUpdateStructuresJob',
+              '',
+              '200',
+              '0',
+              '{$time}',
+              '300',
+              'executing',
+              '1',
+              '0',
+              '{$currentTime}',
+              '{$currentTime}'
+        )");
+        $this->logger('info', 'INSERT增加索引的定时任务HandlingTimeConsumingUpdateStructuresJob');
+        return 1;
     }
 
     protected function generateIndex($step, $page)
