a36fd56e7471317500d0c0d63222e62f83f6b7db
mixiaotian <mixiaotian@edusoho.com>
2019-12-20 12:11:50 +0800

    feat: #191220 更新版本号

--- src/AppBundle/System.php
+++ src/AppBundle/System.php
@@ -4,7 +4,7 @@ namespace AppBundle;
 
 class System
 {
-    const VERSION = '8.5.1';
+    const VERSION = '8.6.0';
     const RELEASE_NOTES = 'http://www.edusoho.com/intro/13';
 
     const OS_UNKNOWN = 1;
