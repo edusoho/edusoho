924f8d69fdf88441085699ff7fdcb61ff4795f5f
zhangyao <zhangyao@howzhi.com>
2020-01-14 20:34:35 +0800

    feat: #10000 带emf的图片导出

--- src/AppBundle/Component/Wrapper/ExportQuestionWrapper.php
+++ src/AppBundle/Component/Wrapper/ExportQuestionWrapper.php
@@ -119,6 +119,9 @@ class ExportQuestionWrapper extends Wrapper
         $result = preg_split('/(<img [^>]*?>)/', $text, -1, PREG_SPLIT_DELIM_CAPTURE);
         foreach ($result as $item) {
             if (preg_match('/<img .*src=[\'\"](.*?)[\'\"].*>/', $item, $matches)) {
+                if (strrpos($webDir.$matches[1], '.emf') !== false) {
+                    continue;
+                }
                 $items[] = array(
                     'element' => 'img',
                     'content' => $webDir.$matches[1],
