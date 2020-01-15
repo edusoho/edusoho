3b3c5b8b27efcd86657a99d28623c57939a6c8e8
LianJion <1995lian1002@gmail.com>
2020-01-15 17:33:22 +0800

    fix: #20191227 暂时存储文件

--- app/Resources/static-src/app/js/vue/index.js
+++ app/Resources/static-src/app/js/vue/index.js
@@ -1,33 +1,8 @@
-class Message {
-  constructor(options) {
-    this.$element = $(options.element);
-    this.validator();
-  }
+import Vue from 'vue/dist/vue.esm.js';
+const App = require('./App.vue');
+import app from './test.vue';
+Vue.config.productionTip = false;
 
-  validator() {
-    let $element = this.$element;
-    $element.validate({
-      rules: {
-        'message[receiver]': {
-          required: true,
-          es_remote: true,
-          chinese_alphanumeric: true
-        },
-        'message[content]': {
-          required: true,
-          maxlength: 500
-        }
-      },
-      ajax: true,
-      submitSuccess() {
-        cd.message({ type: 'success', message: Translator.trans('private_message.send_success') });
-        $element.closest('.modal').modal('hide');
-      },
-      submitError(response) {
-        cd.message({ type: 'danger', message: Translator.trans(response.responseJSON.error.message) });
-      }
-    });
-  }
-}
-
-export default Message;
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
