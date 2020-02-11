eb5dd1e505cd3b8923f5097ac9dfd6e05648306d
yanrui <yanrui@howzhi.com>
2020-02-11 18:29:18 +0800

    feat: #57861 页面

--- src/AppBundle/Controller/AdminV2/User/DestroyAccountController.php
+++ src/AppBundle/Controller/AdminV2/User/DestroyAccountController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller\AdminV2\User;
 
+use AppBundle\Common\Paginator;
 use AppBundle\Controller\AdminV2\BaseController;
+use Biz\DestroyAccount\Service\DestroyAccountRecordService;
 use Biz\User\Service\UserService;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -10,7 +12,33 @@ class DestroyAccountController extends BaseController
 {
     public function recordIndexAction(Request $request)
     {
+        $conditions = $request->query->all();
+
+        $paginator = new Paginator(
+            $request,
+            $this->getDestroyAccountRecordService()->countDestroyAccountRecords($conditions),
+            20
+        );
+
+        $records = $this->getDestroyAccountRecordService()->searchDestroyAccountRecords(
+            $conditions,
+            array(),
+            $paginator->getOffsetCount(),
+            $paginator->getPerPageCount()
+        );
+
         return $this->render('admin-v2/user/destroy-account/record-list.html.twig', array(
+            'records' => $records,
+            'paginator' => $paginator,
+        ));
+    }
+
+    public function recordDetailAction(Request $request, $id)
+    {
+        $record = $this->getDestroyAccountRecordService()->getDestroyAccountRecord($id);
+
+        return $this->render('admin-v2/user/destroy-account/record-detail.html.twig', array(
+            'record' => $record,
         ));
     }
 
@@ -26,5 +54,13 @@ class DestroyAccountController extends BaseController
     protected function getUserService()
     {
         return $this->createService('User:UserService');
+    }
+
+    /**
+     * @return DestroyAccountRecordService
+     */
+    protected function getDestroyAccountRecordService()
+    {
+        return $this->createService('DestroyAccount:DestroyAccountRecordService');
     }
 }
