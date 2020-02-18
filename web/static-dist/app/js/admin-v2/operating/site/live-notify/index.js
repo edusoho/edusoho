71e08cb71a4450fb00739978ec5a45a93aa1b21c
Diyen <mi_xtyan@qq.com>
2020-02-18 11:12:17 +0800

    feat: #200218 changelog、修改版本、编译

---
+++ web/static-dist/app/js/admin-v2/operating/site/live-notify/index.js
@@ -0,0 +1,1 @@
+webpackJsonp(["app/js/admin-v2/operating/site/live-notify/index"],{ee6dfd073b2d8d43a3e6:function(e,i,d){"use strict";var t=$("#live-notify-setting-form");t.validate({rules:{preTime:{required:!0,positive_integer:!0,min:1}}})}},["ee6dfd073b2d8d43a3e6"]);
