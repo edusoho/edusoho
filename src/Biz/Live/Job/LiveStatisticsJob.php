193e76a3819497258b6bf95258657778c9e1972b
wangjiechao <wangjiechao@edusoho.com>
2020-02-25 16:47:04 +0800

    feat:#58341 创建/修改直播设置时注册数据同步任务

---
+++ src/Biz/Live/Job/LiveStatisticsJob.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Biz\Live\Job;
+
+use Codeages\Biz\Framework\Scheduler\AbstractJob;
+use AppBundle\Common\ArrayToolkit;
+use Biz\Util\EdusohoLiveClient;
+use Biz\Activity\Service\LiveActivityService;
+
+class LiveStatisticsJob extends AbstractJob
+{
+    private $liveApi;
+
+    public function execute()
+    {
+        $this->getLiveStatisticsService()->createLiveCheckinStatistics($this->args['liveId']);
+    }
+
+    private function getLiveStatisticsService()
+    {
+        return $this->biz->service('User:NotificationService');
+    }
+}
