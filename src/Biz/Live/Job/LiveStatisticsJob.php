92cd18ba50e7eae1f9ddbb043643ad3cd180e8b0
wangcaijuan <wangcaijuan@edusoho.com>
2020-03-02 15:15:21 +0800

    feat: #58341 定时任务正常获取数据

--- src/Biz/Live/Job/LiveStatisticsJob.php
+++ src/Biz/Live/Job/LiveStatisticsJob.php
@@ -2,15 +2,26 @@
 
 namespace Biz\Live\Job;
 
+use Biz\Live\LiveStatisticsException;
 use Biz\Live\Service\LiveStatisticsService;
+use Biz\System\Service\LogService;
 use Codeages\Biz\Framework\Scheduler\AbstractJob;
 
 class LiveStatisticsJob extends AbstractJob
 {
     public function execute()
     {
-        $this->getLiveStatisticsService()->updateCheckinStatistics($this->args['liveId']);
-        $this->getLiveStatisticsService()->updateVisitorStatistics($this->args['liveId']);
+        try {
+            $this->getLiveStatisticsService()->updateCheckinStatistics($this->args['liveId']);
+        } catch (LiveStatisticsException $e) {
+            $this->getLogService()->warning('job', 'update_live_statistic_checkin', '获取直播点名信息失败:'.$e->getMessage());
+        }
+
+        try {
+            $this->getLiveStatisticsService()->updateVisitorStatistics($this->args['liveId']);
+        } catch (LiveStatisticsException $e) {
+            $this->getLogService()->warning('job', 'update_live_statistic_checkin', '获取直播访问记录失败:'.$e->getMessage());
+        }
     }
 
     /**
@@ -19,5 +30,13 @@ class LiveStatisticsJob extends AbstractJob
     private function getLiveStatisticsService()
     {
         return $this->biz->service('Live:LiveStatisticsService');
+    }
+
+    /**
+     * @return LogService
+     */
+    private function getLogService()
+    {
+        return $this->biz->service('System:LogService');
     }
 }
