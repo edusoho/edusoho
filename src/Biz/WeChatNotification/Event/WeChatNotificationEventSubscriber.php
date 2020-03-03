fe25d664d8002d733fc3d62ab3a11d198de60ce3
shenchupeng <shenchupeng@howzhi.com>
2020-03-03 11:44:34 +0800

    feat: #57339 上课提醒时间未设置保存

--- src/Biz/WeChatNotification/Event/WeChatNotificationEventSubscriber.php
+++ src/Biz/WeChatNotification/Event/WeChatNotificationEventSubscriber.php
@@ -392,7 +392,7 @@ class WeChatNotificationEventSubscriber extends EventSubscriber implements Event
 
         if ('courseRemind' == $key) {
             $templates['courseRemind']['sendTime'] = $fields['sendTime'];
-            $templates['courseRemind']['sendDays'] = $fields['sendDays'];
+            $templates['courseRemind']['sendDays'] = empty($fields['sendDays']) ? array() : $fields['sendDays'];
             $notificationJob = $this->getSchedulerService()->getJobByName('WeChatNotificationJob_CourseRemind');
             if ($notificationJob) {
                 $this->getSchedulerService()->deleteJob($notificationJob['id']);
