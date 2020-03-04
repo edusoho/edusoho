74bc68faf152786677f2cb177515aec9ce25c3ea
chenjinhong <chenjinhong@edusoho.com>
2020-03-04 16:14:10 +0800

    merge/master

--- h5/public/static/js/appJSSDK.js
+++ h5/public/static/js/appJSSDK.js
@@ -1,19 +1,4 @@
 (function () {
-<<<<<<< HEAD
-  "use strict"
-  function postNativeMessage(params) {
-    console.log(params);
-    params = params || {};
-    if (params === "undefind") return;
-    if (params.action === "undefind") return;
-    delete params.data.metas;
-    var data = {
-      action: params.action,
-      callback: "nativeCallback",
-      data: params.data,
-    }
-    var dataStr = JSON.stringify(data);
-=======
   function postNativeMessage(params) {
     console.log(params);
     params = params || {};
@@ -26,7 +11,6 @@
       data: params.data
     };
     const dataStr = JSON.stringify(data);
->>>>>>> master
     window.native && window.native.sendMessage(dataStr || null);
     window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.native && window.webkit.messageHandlers.native.postMessage(dataStr || null);
   }
