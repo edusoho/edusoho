e4b3e63b7a9e99912255a0cb3186420f6c971d53
chenjinhong <chenjinhong@edusoho.com>
2020-03-18 14:14:11 +0800

    修改.gitignore

--- h5/build/env.js
+++ h5/build/env.js
@@ -2,7 +2,6 @@ module.exports = {
   //url: "https://www.easy-mock.com/mock/5b1742522de86c43cc2dc73a/edusoho",
   //url: "http://zyc.st.edusoho.cn",
   //url: "http://wr.st.edusoho.cn",
-  //"https://www.iyamusic.com",
   url: "http://gdy.st.edusoho.cn",
   //url: "http://try6.edusoho.cn",
   //url: "http://devtest.edusoho.cn",
