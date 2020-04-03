1dbe5790ea72f46ae4fe327b8658b483d7e7a254
钟云昶 <zhongyunchang@zhongyungdeMBP2.lan>
2020-04-03 16:55:07 +0800

    fix: #200403 修复订单页点击跳转问题

--- h5/src/components/e-course/e-course.vue
+++ h5/src/components/e-course/e-course.vue
@@ -129,13 +129,27 @@ export default {
         console.log(e.target.tagName)
         return
       }
+
       if (isOrder) {
         location.href = this.order.targetUrl
         return
       }
-      this.$router.push({
-        path: (this.typeList === 'classroom_list') ? `/classroom/${id}` : `/course/${id}`
-      })
+
+      if (this.typeList === 'course') {
+        return
+      }
+
+      if (this.typeList === 'classroom_list') {
+        this.$router.push({
+          path: `/classroom/${id}`
+        })
+      }
+
+      if (this.typeList === 'course_list') {
+        this.$router.push({
+          path: `/course/${id}`
+        })
+      }
     }
   }
 }
