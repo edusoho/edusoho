324a9b3d69083b33c1ec1f1b27ef23165e433206
mixiaotian <mixiaotian@edusoho.com>
2020-04-23 16:42:19 +0800

    feat: #200423 修改移除成功文案

--- app/Resources/static-src/app/js/course-manage/students/index.js
+++ app/Resources/static-src/app/js/course-manage/students/index.js
@@ -25,10 +25,10 @@ class Students {
       }
       $.post($(evt.target).data('url'), function (data) {
         if (data.success) {
-          cd.message({ type: 'success', message: Translator.trans('site.delete_success_hint') });
+          cd.message({ type: 'success', message: Translator.trans('member.delete_success_hint') });
           location.reload();
         } else {
-          cd.message({ type: 'danger', message: Translator.trans('site.delete_fail_hint') + ':' + data.message });
+          cd.message({ type: 'danger', message: Translator.trans('member.delete_fail_hint') + ':' + data.message });
         }
       });
     });
@@ -81,10 +81,10 @@ class Students {
       }
       $.post($(this).data('url'), {studentIds: ids}, function (resp) {
         if (resp.success) {
-          cd.message({ type: 'success', message: Translator.trans('site.delete_success_hint') });
+          cd.message({ type: 'success', message: Translator.trans('member.delete_success_hint') });
           location.reload();
         } else {
-          cd.message({ type: 'danger', message: Translator.trans('site.delete_fail_hint') + ':' + resp.message });
+          cd.message({ type: 'danger', message: Translator.trans('member.delete_fail_hint') + ':' + resp.message });
         }
       });
     });
