0a5ec431dbf6d6c5affce665630da82ec0ce9fe6
zhongyunchang <zhongyunchang@zhongyungdeMBP2.lan>
2020-04-24 09:31:09 +0800

    feat: #60725 详细数据同步

---
+++ src/Biz/S2B2C/Event/CourseProductSubscriber.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Biz\S2B2C\Event;
+
+use Biz\S2B2C\Service\CourseProductService;
+use Codeages\Biz\Framework\Event\Event;
+use Codeages\PluginBundle\Event\EventSubscriber;
+use QiQiuYun\SDK\Service\S2B2CService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CourseProductSubscriber extends EventSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            //同步暂时关闭
+//            'course.create' => 'onCourseCreate',
+            'course.update' => 'onCourseUpdate',
+        );
+    }
+
+    public function onCourseUpdate(Event $event)
+    {
+        $course = $event->getSubject();
+        if ($this->isSupplierCourse($course)) {
+            $this->getS2B2CService()->changeProductSellingPrice($course['sourceCourseId'], 'course', $course['price']);
+        }
+    }
+
+    public function onCourseCreate(Event $event)
+    {
+        $course = $event->getSubject();
+        if ($this->isSupplierCourse($course)) {
+            $this->getCourseProductService()->syncCourse($course['id']);
+        }
+    }
+
+    protected function isSupplierCourse($course)
+    {
+        return !empty($course['sourceCourseId']);
+    }
+
+    /**
+     * @return CourseProductService
+     */
+    protected function getCourseProductService()
+    {
+        return $this->getBiz()->service('S2B2C:CourseProductService');
+    }
+
+    /**
+     * @return S2B2CService
+     */
+    protected function getS2B2CService()
+    {
+        return $this->getBiz()->offsetGet('qiQiuYunSdk.s2b2cService');
+    }
+}
--- src/Biz/S2B2C/Service/Impl/CourseProductServiceImpl.php
+++ src/Biz/S2B2C/Service/Impl/CourseProductServiceImpl.php
@@ -7,6 +7,7 @@ use Biz\Course\Service\CourseService;
 use Biz\Course\Service\CourseSetService;
 use Biz\S2B2C\Service\CourseProductService;
 use Biz\S2B2C\Service\S2B2CFacadeService;
+use Biz\System\Service\LogService;
 use Monolog\Logger;
 
 /**
@@ -48,6 +49,73 @@ class CourseProductServiceImpl extends BaseService implements CourseProductServi
         }
     }
 
+    public function syncCourse($courseId)
+    {
+        $course = $this->getCourseService()->getCourse($courseId);
+        $this->getLogger()->info("开始尝试同步课程(#{$courseId})");
+        if (!$this->validateCourseData($course)) {
+            return false;
+        }
+
+        try {
+            $this->beginTransaction();
+            $sourceCourse = $this->tryGetSupplierProductSyncData($course['s2b2cDistributeId']);
+
+            $this->biz['course_product_sync']->sync($sourceCourse, array('syncCourseId' => $courseId));
+//
+//            $syncCourse = $this->getCourseProductDao()->update($courseId, array('statusSyncTime' => time(), 'syncStatus' => self::SYNC_STATUS_FINISHED));
+//            $this->getCourseSetProductDao()->update($syncCourse['courseSetId'], array('sourceVersion' => $syncCourse['sourceVersion'], 'syncStatus' => self::SYNC_STATUS_FINISHED, 'minCoursePrice' => $syncCourse['price'], 'maxCoursePrice' => $syncCourse['price']));
+            $this->getLogger()->info("[syncCourseProduct] 同步课程 - {$course['courseSetTitle']}(courseSetId#{$course['courseSetId']}) 成功", array('courseId' => $course['id']));
+            $this->getLogService()->info('course', 'create', "内容市场选择商品(#{$courseId})《{$course['courseSetTitle']}》同步数据成功", array('userId' => $this->getCurrentUser()->getId()));
+            $this->commit();
+        } catch (\Exception $e) {
+            $this->rollback();
+            $this->getLogger()->error("[syncCourseProduct] 同步课程 - {$course['courseSetTitle']}(courseSetId#{$course['courseSetId']}) 失败", array('error' => $e));
+            $courseCounts = $this->getCourseProductDao()->count(array('courseSetId' => $course['courseSetId']));
+            if (1 == $courseCounts) {
+                $this->getCourseSetProductDao()->delete($course['courseSetId']);
+            }
+            $this->getCourseProductDao()->delete($courseId);
+            $this->getLogService()->error('course', 'create', "内容市场选择商品(#{$courseId})《{$course['courseSetTitle']}》同步数据失败", array('userId' => $this->getCurrentUser()->getId()));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function validateCourseData($course)
+    {
+        if (empty($course)) {
+            $this->getLogger()->error('课程不存在，拒绝操作');
+
+            return false;
+        }
+        if ('waiting' !== $course['syncStatus']) {
+            $this->getLogger()->info("课程 - {$course['courseSetTitle']}(courseSetId#{$course['courseSetId']}) 无需处理", array('syncStatus' => $course['syncStatus']));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function tryGetSupplierProductSyncData($distributeId)
+    {
+        $sourceCourse = $this->getS2B2CFacadeService()->getSupplierPlatformApi()->getSupplierProductSyncData($distributeId);
+
+        if (empty($sourceCourse['id'])) {
+            $this->getLogger()->info('[syncCourseProduct] 获取源平台课程数据失败', array('DATA' => $sourceCourse));
+            throw $this->createServiceException('获取新版本课程数据失败，请稍后重试', 5001730);
+        }
+        if ('published' != $sourceCourse['editStatus']) {
+            $this->getLogger()->info('[syncCourseProduct] 源平台课程正在编辑中，无法处理', array('DATA' => $sourceCourse));
+            throw $this->createServiceException('源平台课程正在编辑中，无法更新，请稍后再试', 5001731);
+        }
+
+        return $sourceCourse;
+    }
+
     /**
      * @return CourseSetService
      */
@@ -70,6 +138,14 @@ class CourseProductServiceImpl extends BaseService implements CourseProductServi
     protected function getS2B2CFacadeService()
     {
         return $this->createService('S2B2C:S2B2CFacadeService');
+    }
+
+    /**
+     * @return LogService
+     */
+    protected function getLogService()
+    {
+        return $this->createService('System:LogService');
     }
 
     /**
