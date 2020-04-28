066544aed98d532199034e119db1d7f0f4f7d623
王才娟 <wangcaijuan@edusoho.com>
2020-04-28 16:32:20 +0800

    feat: #60402 plumber init

--- bootstrap/bootstrap_plumber.php
+++ bootstrap/bootstrap_plumber.php
@@ -1,60 +1,52 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
 use Pimple\Psr11\Container as PsrContainer;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\HttpFoundation\Request;
 
-if (file_exists(__DIR__.'/../web/merchant.php')) {
-    include __DIR__.'/../web/merchant.php';
-}
+$loader = require __DIR__.'/../vendor/autoload.php';
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-$kernel = new AppKernel('prod', true);
-$kernel->setRequest(Request::createFromGlobals());
+$input = new ArgvInput();
+$env = $input->getParameterOption(['--env', '-e'], getenv('SYMFONY_ENV') ?: 'dev');
+
+$request = Request::createFromGlobals();
+
+$kernel = new AppKernel($env, true);
+$kernel->setRequest($request);
 $kernel->boot();
 
 $container = $kernel->getContainer();
-$plumberConfig = $container->getParameter('plumber');
-$queueConfig = $plumberConfig['queue'];
-$workerConfig = $plumberConfig['worker'];
-
-$options = array(
-    'app_name' => $plumberConfig['name'],
-    'queues' => array(
-        'init_queue' => array(
-            'type' => 'beanstalk',
-            'host' => $queueConfig['init_queue']['beanstalk_host'],
-            'port' => $queueConfig['init_queue']['beanstalk_port'],
-            'password' => $queueConfig['init_queue']['beanstalk_password'],
-        ),
-        'default_queue' => array(
-            'type' => 'beanstalk',
-            'host' => $queueConfig['default_queue']['beanstalk_host'],
-            'port' => $queueConfig['default_queue']['beanstalk_port'],
-            'password' => $queueConfig['default_queue']['beanstalk_password'],
-        ),
-    ),
-    'workers' => array(
-        array(
-            'class' => 'AppBundle\Worker\InitMerchantWorker',
-            'num' => $workerConfig['init_worker']['worker_num'],
-            'queue' => 'init_queue',
-            'topic' => $workerConfig['init_worker']['topic_name'],
-        ),
-        array(
-            'class' => 'AppBundle\Worker\CrontabJobWorker',
-            'num' => $workerConfig['crontab_job_worker']['worker_num'],
-            'queue' => 'default_queue',
-            'topic' => $workerConfig['crontab_job_worker']['topic_name'],
-        ),
-    ),
-    'log_path' => $container->getParameter('kernel.logs_dir').'/plumber.log',
-    'pid_path' => $container->getParameter('kernel.root_dir').'/data/plumber.pid',
-);
 
 $biz = $container->get('biz');
 $biz['biz'] = $biz;
-$psrContainer = new PsrContainer($biz);
 
-return array(
+$plumberQueueDatabases = $container->getParameter('plumber_queue_databases');
+
+$options = [
+    'app_name' => 's2b2c_m_plumber',
+    'queues' => [
+        'example_queue' => array_merge([
+            'type' => 'beanstalk',
+            'enable_queue' => true,
+            'host' => '127.0.0.1',
+            'port' => 11300,
+            'password' => '',
+        ], $plumberQueueDatabases['example_queue']),
+    ],
+    'workers' => [
+        'worker_1' => [
+            'topic' => 'merchant.example',
+            'num' => 1,
+            'class' => 'AppBundle\Worker\ExampleWorker',
+            'queue' => 'example_queue',
+        ],
+    ],
+    'log_path' => $container->getParameter('kernel.logs_dir').'/plumber.log',
+    'pid_path' => $container->getParameter('kernel.root_dir').'/data/plumber.pid',
+];
+
+return [
     'options' => $options,
-    'container' => $psrContainer,
-);
+    'container' => new PsrContainer($biz),
+];
--- src/AppBundle/Worker/ExampleWorker.php
+++ src/AppBundle/Worker/ExampleWorker.php
@@ -8,8 +8,6 @@ class ExampleWorker extends BaseWorker
 {
     public function doExecute(Job $job)
     {
-        file_put_contents('/var/www/edusoho/app/logs/example.log', "execute job #{$job->getId()}, body: {$job->getBody()}", FILE_APPEND);
-
         $this->logger->info("execute job #{$job->getId()}, body: {$job->getBody()}");
 
         return self::FINISH;
