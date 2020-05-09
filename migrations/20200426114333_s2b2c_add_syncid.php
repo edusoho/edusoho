48e125d9870312b8132df7f533dda6577f04bc92
zhongyunchang <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-05-09 09:35:00 +0800

    feat: #60725 格式化

--- migrations/20200426114333_s2b2c_add_syncid.php
+++ migrations/20200426114333_s2b2c_add_syncid.php
@@ -28,7 +28,6 @@ class S2b2cAddSyncid extends Migration
         $connection->exec("ALTER TABLE `question` ADD `syncId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '内容市场引用的源Id';");
         $connection->exec("ALTER TABLE `course_chapter` ADD `syncId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '内容市场引用的源Id';");
         $connection->exec("ALTER TABLE `file_used` ADD `syncId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '内容市场引用的源Id';");
-
     }
 
     /**
@@ -38,22 +37,22 @@ class S2b2cAddSyncid extends Migration
     {
         $biz = $this->getContainer();
         $connection = $biz['db'];
-        $connection->exec("ALTER TABLE `course_task` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_download` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_video` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_audio` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_doc` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_ppt` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_flash` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_text` DROP `syncId`");
-        $connection->exec("ALTER TABLE `activity_testpaper` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `activity_live` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `upload_files` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `course_material_v8` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `testpaper_item_v8` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `question` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `course_chapter` DROP `syncId`;");
-        $connection->exec("ALTER TABLE `file_used` DROP `syncId`;");
+        $connection->exec('ALTER TABLE `course_task` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_download` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_video` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_audio` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_doc` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_ppt` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_flash` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_text` DROP `syncId`');
+        $connection->exec('ALTER TABLE `activity_testpaper` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `activity_live` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `upload_files` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `course_material_v8` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `testpaper_item_v8` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `question` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `course_chapter` DROP `syncId`;');
+        $connection->exec('ALTER TABLE `file_used` DROP `syncId`;');
     }
 }
