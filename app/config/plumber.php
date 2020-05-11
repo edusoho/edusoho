2a16ef44f0183944b824b0a3ea707197df66a700
王才娟 <wangcaijuan@edusoho.com>
2020-05-11 15:41:54 +0800

    feat: #20200511 delete exampleworker

--- app/config/plumber.php
+++ app/config/plumber.php
@@ -3,23 +3,12 @@
 return [
     'app_name' => 's2b2c_m_plumber',
     'queues' => [
-        'example_queue' => [
-            'enable_queue' => true,
-            'queue_options' => 'redis_default',
-        ],
         'crontab_job_queue' => [
             'enable_queue' => true,
             'queue_options' => 'default',
         ],
     ],
     'workers' => [
-        'worker_1' => [
-            'topic' => 'merchant.example',
-            'num' => 1,
-            'class' => 'AppBundle\Worker\ExampleWorker',
-            'queue' => 'example_queue',
-            'hour_limit' => 'default_hour_limit',
-        ],
         'crontab_job_worker' => [
             'topic' => 'crontab_job_worker',
             'num' => 1,
