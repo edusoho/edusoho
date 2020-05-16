d15b758211eb12bfc1d3ed4ee0e1c971abcc16d9
王才娟 <wangcaijuan@edusoho.com>
2020-05-16 17:37:37 +0800

    feat: #200516 自动更新产品及worker日志优化

--- src/AppBundle/Command/PlumberSchedulerCommand.php
+++ src/AppBundle/Command/PlumberSchedulerCommand.php
@@ -21,6 +21,8 @@ class PlumberSchedulerCommand extends BaseCommand
     {
         $logger = $this->getBiz()->offsetGet('s2b2c.merchant.logger');
         $logger->info('Crontab:开始将定时任务放入plumber');
+        $output->writeln('<info>开始将定时任务放入plumber</info>');
+
         try {
             $queue = $this->getCrontabJobQueue();
 
@@ -30,14 +32,30 @@ class PlumberSchedulerCommand extends BaseCommand
 
             if (false === $queueJobId) {
                 $logger->info('Crontab:定时任务放入队列失败');
+                $output->writeln('<error>定时任务放入队列失败</error>');
             }
 
             $logger->info("Crontab:定时任务# {$jobId} 放入plumber成功");
+            $output->writeln("<info>定时任务# {$jobId} 放入plumber成功\"</info>");
         } catch (\Exception $e) {
             $logger->error('Crontab:定时任务放入队列出错 error:'.$e->getMessage().' TraceString:'.$e->getTraceAsString());
+            $output->writeln("<error>定时任务放入队列出错 error:{$e->getMessage()}, TraceString:{$e->getTraceAsString()}</error>");
         }
 
         return true;
+    }
+
+    protected function getConfig()
+    {
+        $configFilePath = $this->getContainer()->getParameter('kernel.root_dir').'/config/plumber.php';
+        if (!$this->getContainer()->hasParameter('plumber_queues_options') || !file_exists($configFilePath)) {
+            return [];
+        }
+
+        return [
+            include "{$configFilePath}",
+            $this->getContainer()->getParameter('plumber_queues_options'),
+        ];
     }
 
     protected function getCrontabJobQueue()
@@ -52,12 +70,19 @@ class PlumberSchedulerCommand extends BaseCommand
             'socket_timeout' => 20,
             'logger' => null,
         ];
-        $plumberQueueOptions = $this->getContainer()->getParameter('plumber_queues_options');
 
-        $plumberQueues = include $this->getContainer()->getParameter('kernel.root_dir').'/config/plumber.php';
+        list($plumberQueues, $plumberQueueOptions) = $this->getConfig();
+        if (empty($plumberQueues) || empty($plumberQueueOptions)) {
+            return ['queue' => null, 'message' => 'Config not exist in plumber.php or parameters.yml.'];
+        }
 
-        $optionKey = $plumberQueues['queues']['crontab_job_queue']['queue_options'];
-        $config = empty($plumberQueueOptions[$optionKey]) ? $defaultConfig : $plumberQueueOptions[$optionKey];
+        if (empty($plumberQueues['workers']) || empty($plumberQueues['workers']['crontab_job_worker'])) {
+            return ['queue' => null, 'message' => 'Workers config not exist.'];
+        }
+
+        $worker = $plumberQueues['workers']['crontab_job_worker'];
+
+        $config = empty($plumberQueueOptions[$worker['queue']]) ? $defaultConfig : $plumberQueueOptions[$worker['queue']];
 
         return QueueFactory::create($config['type'], $config);
     }
