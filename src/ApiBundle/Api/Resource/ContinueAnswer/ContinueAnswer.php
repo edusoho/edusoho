009955da4e698edbceb6f4aa4e708b65852f2c4e
chelingfeng <chelingfeng@edusoho.com>
2020-05-16 22:47:53 +0800

    fix: #61303 答题相关

---
+++ src/ApiBundle/Api/Resource/ContinueAnswer/ContinueAnswer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ApiBundle\Api\Resource\ContinueAnswer;
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+use ApiBundle\Api\Resource\Assessment\AssessmentFilter;
+use Biz\Common\CommonException;
+
+class ContinueAnswer extends AbstractResource
+{
+    public function add(ApiRequest $request)
+    {
+        $answerRecord = $this->getAnswerRecordService()->get($request->request->get('answer_record_id'));
+        if (empty($answerRecord) || $this->getCurrentUser()['id'] != $answerRecord['user_id']) {
+            throw CommonException::ERROR_PARAMETER();
+        }
+
+        $answerRecord = $this->getAnswerService()->continueAnswer($request->request->get('answer_record_id'));
+
+        $assessment = $this->getAssessmentService()->showAssessment($answerRecord['assessment_id']);
+        $assessmentFilter = new AssessmentFilter();
+        $assessmentFilter->filter($assessment);
+
+        return [
+            'assessment' => $assessment,
+            'assessment_response' => $this->getAnswerService()->getAssessmentResponseByAnswerRecordId($answerRecord['id']),
+            'answer_scene' => $this->getAnswerSceneService()->get($answerRecord['answer_scene_id']),
+            'answer_record' => $answerRecord,
+        ];
+    }
+
+    protected function getAnswerActivityService()
+    {
+        return $this->service('AnswerActivity:AnswerActivityService');
+    }
+
+    protected function getAnswerService()
+    {
+        return $this->service('ItemBank:Answer:AnswerService');
+    }
+
+    protected function getAnswerSceneService()
+    {
+        return $this->service('ItemBank:Answer:AnswerSceneService');
+    }
+
+    protected function getAssessmentService()
+    {
+        return $this->service('ItemBank:Assessment:AssessmentService');
+    }
+
+    protected function getAnswerRecordService()
+    {
+        return $this->service('ItemBank:Answer:AnswerRecordService');
+    }
+}
