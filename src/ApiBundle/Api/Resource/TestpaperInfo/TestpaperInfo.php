bd088bf144c42f7d6b1edb2330bca4de1c92b2a3
shenchupeng <shenchupeng@howzhi.com>
2020-05-19 17:55:08 +0800

    feat: #1 修改云监考接口

--- src/ApiBundle/Api/Resource/TestpaperInfo/TestpaperInfo.php
+++ src/ApiBundle/Api/Resource/TestpaperInfo/TestpaperInfo.php
@@ -83,7 +83,7 @@ class TestpaperInfo extends AbstractResource
         $task['activity'] = $activity;
 
         $results['testpaper']['limitedTime'] = empty($scene['limited_time']) ? '0' : $scene['limited_time'];
-        $results['task'] = $task;
+        $results['task'] = $this->filterFaceInspectionTask($task, $scene);
     }
 
     private function filterTestpaperItems($items)
@@ -96,6 +96,16 @@ class TestpaperInfo extends AbstractResource
 
         return $itemArray;
     }
+
+    private function filterFaceInspectionTask($task, $scene)
+    {
+        if (!empty($scene['enable_facein'])) {
+            $task['enable_facein'] = $scene['enable_facein'];
+        }
+
+        return $task;
+    }
+
 
     /**
      * @return TaskService
