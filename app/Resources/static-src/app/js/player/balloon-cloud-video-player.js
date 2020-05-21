1e666ef19e6c831ea1f60be2bf6d27bff4f992bc
yangfeiyue <yangfeiyue@yangfeiyuedeMacBook-Pro.local>
2020-05-21 17:19:20 +0800

    代码规范

--- app/Resources/static-src/app/js/player/balloon-cloud-video-player.js
+++ app/Resources/static-src/app/js/player/balloon-cloud-video-player.js
@@ -110,7 +110,6 @@ class BalloonCloudVideoPlayer extends Emitter {
     });
 
     player.on('timeupdate', function(e) {
-      console.log('balloon timechange');
       //    player.__events get all the event;
       self.emit('timechange', e);
     });
