e6d476dfe45ca9a074bd0666bf81f0afd39907bb
zhongyunchang <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-05-28 11:40:42 +0800

    feat: #55611 解决代码格式

--- tests/Unit/AppBundle/Component/Export/Course/OverviewTestpaperTaskDetailExporterTest.php
+++ tests/Unit/AppBundle/Component/Export/Course/OverviewTestpaperTaskDetailExporterTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Unit\Component\Export\Invite;
 
-use Biz\BaseTestCase;
 use AppBundle\Component\Export\Course\OverviewTestpaperTaskDetailExporter;
+use Biz\BaseTestCase;
 
 class OverviewTestpaperTaskDetailExporterTest extends BaseTestCase
 {
@@ -12,156 +12,156 @@ class OverviewTestpaperTaskDetailExporterTest extends BaseTestCase
         self::$appKernel->getContainer()->set('biz', $this->getBiz());
         $this->mockBiz(
             'User:UserService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'findUsersByIds',
-                    'returnValue' => array(
-                        1 => array(
+                    'returnValue' => [
+                        1 => [
                             'id' => 1,
                             'nickname' => 'lalala',
-                        ),
-                    ),
-                ),
-            )
+                        ],
+                    ],
+                ],
+            ]
         );
         $this->mockBiz(
             'Testpaper:TestpaperService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'findTestResultsByTestpaperIdAndUserIds',
-                    'returnValue' => array(
-                        1 => array(
+                    'returnValue' => [
+                        1 => [
                             'id' => '1',
                             'userId' => 1,
                             'usedTime' => 41,
                             'firstScore' => 1,
                             'maxScore' => 14,
-                        ),
-                    ),
-                ),
-                array(
+                        ],
+                    ],
+                ],
+                [
                     'functionName' => 'getTestpaperByIdAndType',
-                    'returnValue' => array(
+                    'returnValue' => [
                         'id' => 1,
-                    ),
-                ),
-            )
+                    ],
+                ],
+            ]
         );
 
-         $this->mockBiz(
+        $this->mockBiz(
             'ItemBank:Answer:AnswerReportService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'search',
-                    'returnValue' => array(
-                       array('id' => 1, 'answer_record_id' => 1, 'score' => 1, 'user_id' => 1),
-                       array('id' => 2, 'answer_record_id' => 2, 'score' => 41, 'user_id' => 1),
-                    ),
-                ),
-                array(
+                    'returnValue' => [
+                       ['id' => 1, 'answer_record_id' => 1, 'score' => 1, 'user_id' => 1],
+                       ['id' => 2, 'answer_record_id' => 2, 'score' => 41, 'user_id' => 1],
+                    ],
+                ],
+                [
                     'functionName' => 'count',
-                    'returnValue' => array(
+                    'returnValue' => [
                         'id' => 1,
-                    ),
-                ),
-            )
+                    ],
+                ],
+            ]
         );
 
         $this->mockBiz(
             'ItemBank:Answer:AnswerRecordService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'search',
-                    'returnValue' => array(
-                       array('id' => 1, 'user_id' => 1, 'used_time' => 60),
-                       array('id' => 2, 'user_id' => 1, 'used_time' => 60),
-                    ),
-                ),
-                array(
+                    'returnValue' => [
+                       ['id' => 1, 'user_id' => 1, 'used_time' => 60],
+                       ['id' => 2, 'user_id' => 1, 'used_time' => 60],
+                    ],
+                ],
+                [
                     'functionName' => 'count',
-                    'returnValue' => 1
-                ),
-            )
+                    'returnValue' => 1,
+                ],
+            ]
         );
 
         $this->mockBiz(
             'Task:TaskResultService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'searchTaskResults',
-                    'returnValue' => array(
-                        array(
+                    'returnValue' => [
+                        [
                             'id' => '1',
                             'userId' => 1,
                             'createdTime' => 12,
                             'finishedTime' => 1341,
-                        ),
-                    ),
-                ),
-            )
+                        ],
+                    ],
+                ],
+            ]
         );
-        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), array(
+        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), [
             'courseTaskId' => 1,
-        ));
+        ]);
 
         $result = $expoter->getContent(0, 100);
 
-        $this->assertArrayEquals(array(
+        $this->assertArrayEquals([
             'lalala',
             '1970-01-01 08:00:12',
             '1970-01-01 08:22:21',
             '1',
             '1',
             '41',
-        ), $result[0]);
+        ], $result[0]);
     }
 
     public function testBuildCondition()
     {
-        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), array(
+        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), [
             'courseTaskId' => 1,
-        ));
-        $result = $expoter->buildCondition(array(
+        ]);
+        $result = $expoter->buildCondition([
             'courseTaskId' => 1,
             'alal' => '1123',
             'titleLike' => '1123',
-        ));
+        ]);
 
-        $this->assertArrayEquals(array(
+        $this->assertArrayEquals([
             'courseTaskId' => 1,
-        ), $result);
+        ], $result);
     }
 
     public function testBuildParameter()
     {
-        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), array(
+        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), [
             'courseTaskId' => 1,
-        ));
-        $result = $expoter->buildParameter(array(
+        ]);
+        $result = $expoter->buildParameter([
             'courseTaskId' => 1,
-        ));
+        ]);
 
-        $this->assertArrayEquals(array(
+        $this->assertArrayEquals([
             'start' => 0,
             'fileName' => '',
             'courseTaskId' => 1,
-        ), $result);
+        ], $result);
     }
 
     public function testGetTitles()
     {
-        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), array(
+        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), [
             'courseTaskId' => 1,
-        ));
+        ]);
 
-        $title = array(
+        $title = [
             'task.learn_data_detail.nickname',
             'task.learn_data_detail.createdTime',
             'course.task.finish_time',
             'task.learn_data_detail.testpaper_firstUsedTime',
             'task.learn_data_detail.testpaper_firstScore',
             'task.learn_data_detail.testpaper_maxScore',
-        );
+        ];
 
         $this->assertArrayEquals($title, $expoter->getTitles());
     }
@@ -171,16 +171,16 @@ class OverviewTestpaperTaskDetailExporterTest extends BaseTestCase
         self::$appKernel->getContainer()->set('biz', $this->getBiz());
         $this->mockBiz(
             'Task:TaskResultService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'countTaskResults',
                     'returnValue' => 51,
-                ),
-            )
+                ],
+            ]
         );
-        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), array(
+        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), [
             'courseTaskId' => 1,
-        ));
+        ]);
 
         $this->assertEquals(51, $expoter->getCount());
     }
@@ -190,34 +190,34 @@ class OverviewTestpaperTaskDetailExporterTest extends BaseTestCase
         self::$appKernel->getContainer()->set('biz', $this->getBiz());
         $this->mockBiz(
             'Course:CourseService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'tryManageCourse',
                     'returnValue' => false,
-                ),
-            )
+                ],
+            ]
         );
-        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), array(
+        $expoter = new OverviewTestpaperTaskDetailExporter(self::$appKernel->getContainer(), [
             'courseTaskId' => 1,
-        ));
+        ]);
 
         $result = $expoter->canExport();
         $this->assertTrue($result);
 
         $biz = $this->getBiz();
         $user = $biz['user'];
-        $user->setPermissions(array());
+        $user->setPermissions([]);
         $result = $expoter->canExport();
         $this->assertNotTrue($result);
 
         $this->mockBiz(
             'Course:CourseService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'tryManageCourse',
                     'returnValue' => true,
-                ),
-            )
+                ],
+            ]
         );
         $result = $expoter->canExport();
         $this->assertTrue($result);
