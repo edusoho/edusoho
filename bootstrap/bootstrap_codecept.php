7de6eb61c08dd54b92ba0bde88ef3558decd56fc
zhongyunchang <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-05-28 19:09:33 +0800

    feat: #55611 解决代码格式

--- bootstrap/bootstrap_codecept.php
+++ bootstrap/bootstrap_codecept.php
@@ -4,3 +4,11 @@ if (class_exists('PHPUnit_TextUI_ResultPrinter') && !class_exists('PHPUnit\TextU
     class_alias('PHPUnit_TextUI_ResultPrinter', 'PHPUnit\TextUI\ResultPrinter');
     class_alias('PHPUnit_TextUI_TestRunner', 'PHPUnit\TextUI\TestRunner');
 }
+if (class_exists('PHPUnit_Runner_Filter_Factory') && !class_exists('PHPUnit\Runner\Filter\Factory')) {
+    class_alias('PHPUnit_Runner_Filter_Factory', 'PHPUnit\Runner\Filter\Factory');
+    class_alias('PHPUnit_Runner_Filter_Test', 'PHPUnit\Runner\Filter\NameFilterIterator');
+    class_alias('PHPUnit_Runner_Filter_Group_Include', 'PHPUnit\Runner\Filter\IncludeGroupFilterIterator');
+    class_alias('PHPUnit_Runner_Filter_Group_Exclude', 'PHPUnit\Runner\Filter\ExcludeGroupFilterIterator');
+    class_alias('PHPUnit_Runner_Version', 'PHPUnit\Runner\Version');
+}
+
