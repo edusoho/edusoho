18b9cc8316626a24d354b2340f7ea73d153257fc
王才娟 <wangcaijuan@edusoho.com>
2020-06-02 15:23:07 +0800

    feat: #62579 product service and dao with test unit

--- tests/Unit/Product/Service/ProductServiceTest.php
+++ tests/Unit/Product/Service/ProductServiceTest.php
@@ -3,28 +3,29 @@
 namespace Tests\Unit\Product\Service;
 
 use Biz\BaseTestCase;
+use Biz\Common\CommonException;
 use Biz\Product\Dao\ProductDao;
 use Biz\Product\Service\ProductService;
 
 class ProductServiceTest extends BaseTestCase
 {
-    public function testCreateProduct_ParamMissingException_WithoutTargetType()
+    public function testCreateProduct_whenMissingTargetType_thenThrowParamMissingException()
     {
-        $this->expectException('\Biz\Common\CommonException');
+        $this->expectException(CommonException::class);
         $this->expectExceptionMessage('exception.common_parameter_missing');
         $this->getProductService()->createProduct(['targetId' => 1, 'title' => 'testTitle']);
     }
 
-    public function testCreateProduct_ParamMissingException_WithoutTargetId()
+    public function testCreateProduct_whenMissingTargetId_thenThrowParamMissingException()
     {
-        $this->expectException('\Biz\Common\CommonException');
+        $this->expectException(CommonException::class);
         $this->expectExceptionMessage('exception.common_parameter_missing');
         $this->getProductService()->createProduct(['targetType' => 'testType', 'title' => 'testTitle']);
     }
 
-    public function testCreateProduct_ParamMissingException_WithoutTitle()
+    public function testCreateProduct_whenMissingTitle_thenThrowParamMissingException()
     {
-        $this->expectException('\Biz\Common\CommonException');
+        $this->expectException(CommonException::class);
         $this->expectExceptionMessage('exception.common_parameter_missing');
         $this->getProductService()->createProduct(['targetType' => 'testType', 'targetId' => 1]);
     }
@@ -81,7 +82,7 @@ class ProductServiceTest extends BaseTestCase
         $this->assertEmpty($after);
     }
 
-    public function testSearchProducts_differentConditions()
+    public function testSearchProducts_withDifferentConditions()
     {
         $product1 = $this->createProduct();
         $product2 = $this->createProduct('testType2');
@@ -105,7 +106,7 @@ class ProductServiceTest extends BaseTestCase
         $this->assertArrayEquals([$product4], $result3);
     }
 
-    public function testSearchProducts_orderBysAndLimits()
+    public function testSearchProducts_withDifferentOrderBysAndLimits()
     {
         $product1 = $this->createProduct();
         $product2 = $this->createProduct('testType2');
@@ -122,21 +123,30 @@ class ProductServiceTest extends BaseTestCase
         $this->assertArrayEquals([$product1, $product3], $result3);
     }
 
-    public function testSearchProducts_clumns()
+    public function testSearchProducts_withDifferentColumns()
     {
         $product1 = $this->createProduct();
         $product2 = $this->createProduct('testType2');
         $product3 = $this->createProduct('testType', 2);
         $product4 = $this->createProduct('testType', 1, 'testTitle2');
 
-        $expected = [
+        $expected1 = [
             ['targetType' => $product1['targetType'], 'targetId' => $product1['targetId']],
             ['targetType' => $product3['targetType'], 'targetId' => $product3['targetId']],
             ['targetType' => $product4['targetType'], 'targetId' => $product4['targetId']],
         ];
 
         $result1 = $this->getProductService()->searchProducts(['targetType' => 'testType'], ['id' => 'ASC'], 0, 10, ['targetId', 'targetType']);
-        $this->assertArrayEquals($expected, $result1);
+        $this->assertArrayEquals($expected1, $result1);
+
+        $expected2 = [
+            ['targetType' => $product1['targetType'], 'title' => $product1['title']],
+            ['targetType' => $product3['targetType'], 'title' => $product3['title']],
+            ['targetType' => $product4['targetType'], 'title' => $product4['title']],
+        ];
+
+        $result2 = $this->getProductService()->searchProducts(['targetType' => 'testType'], ['id' => 'ASC'], 0, 10, ['targetType', 'title']);
+        $this->assertArrayEquals($expected2, $result2);
     }
 
     public function testGetProductByTargetIdAndType()
