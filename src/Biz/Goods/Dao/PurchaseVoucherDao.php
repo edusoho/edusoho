b983085de4cf1f1c35e99212196e5a200c28b60d
zhongyunchang <zhongyunchang@zhongyungdeMBP2.lan>
2020-06-04 15:38:13 +0800

    feat: #62583 定义接口

--- src/Biz/Goods/Dao/PurchaseVoucherDao.php
+++ src/Biz/Goods/Dao/PurchaseVoucherDao.php
@@ -4,6 +4,9 @@ namespace Biz\Goods\Dao;
 
 use Codeages\Biz\Framework\Dao\AdvancedDaoInterface;
 
+/**
+ * Interface PurchaseVoucherDao
+ */
 interface PurchaseVoucherDao extends AdvancedDaoInterface
 {
     public function getBySpecsId($specsId);
--- src/Biz/Goods/Service/Impl/PurchaseServiceImpl.php
+++ src/Biz/Goods/Service/Impl/PurchaseServiceImpl.php
@@ -3,7 +3,35 @@
 namespace Biz\Goods\Service\Impl;
 
 use Biz\BaseService;
+use Biz\Goods\Dao\PurchaseVoucherDao;
 
 class PurchaseServiceImpl extends BaseService
 {
+    public function getVoucher($id)
+    {
+    }
+
+    public function createVoucher($voucher)
+    {
+    }
+
+    public function updateVoucher($id, $voucher)
+    {
+    }
+
+    public function deleteVoucher($id)
+    {
+    }
+
+    public function countVouchers($conditions)
+    {
+    }
+
+    /**
+     * @return PurchaseVoucherDao
+     */
+    protected function getPurchaseVoucherDao()
+    {
+        return $this->createDao('Goods:PurchaseVoucherDao');
+    }
 }
