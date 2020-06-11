e4b233623e042a9b40f18a9190b8e8824cf2fcd4
hongyongchun <hongyongchun@edusoho.com>
2020-06-11 19:04:33 +0800

    feat: #62792 商品默认信息

--- app/Resources/static-src/app/js/goods/show/product-detail.vue
+++ app/Resources/static-src/app/js/goods/show/product-detail.vue
@@ -1,6 +1,58 @@
 <template>
-  <div class="product-detail">
-      prodtct-detail
+  <div class="product-detail clearfix">
+    <div class="product-detail__left detail-left pull-left">
+      <img src="http://sce2a3b1c3d5nk-sb-qn.qiqiuyun.net/files/course/2020/03-19/102135fa5a9d376668.jpg?version=2" alt="">
+      <ul class="detail-left__text clearfix">
+        <li class="pull-left"><i class="es-icon es-icon-friends"></i>人加入学习</li>
+        <li class="pull-right">
+          <span class="detail-left__text-share"><i class="es-icon es-icon-share"></i>分享</span>
+          <span><i class="es-icon es-icon-favoriteoutline"></i>收藏</span>
+        </li>
+      </ul>
+    </div>
+    <div class="product-detail__right detail-right pull-right">
+      <p class="detail-right__title">这里是标题这里是标题这里是标题这里是标题这里这里是标题这里是标题这里是标题这里是标题这里这里是标题这里是标题这里是标题这里是标题这里这里是标题这里是标题这里是标题这里是标题这里</p>
+      <p class="detail-right__subtitle">这里是副标题这里是副标题这里是副标题这里是副标题这里是副标题这里副标题这里是副标题这里是副标题这里是副标题这里是副标题这里是副标题这里是副标题这里是副标题这里是副标题</p>
+      <!-- 价格 -->
+      <div class="detail-right__price">
+        <!-- 优惠活动 -->
+        <div class="detail-right__price__activities">该商品享受“某某某某某某某某某某某某”打折促销活动，24：00：00后结束，请尽快购买！</div>
+        <div class="detail-right__price__num">
+          优惠价
+          <span>880.00</span>
+          <s>价格: 2000元</s>
+        </div>
+      </div>
+      <!-- 教学计划 -->
+      <div class="detail-right__plan plan clearfix">
+        <div class="plan-title pull-left">教学计划</div>
+        <div class="plan-btns pull-right">
+          <span class="plan-btns__item">三月班</span>
+          <span class="plan-btns__item active">四月班级</span>
+          <span class="plan-btns__item">某某某教学计划</span>
+          <span class="plan-btns__item">某某教学计划</span>
+          <span class="plan-btns__item">三月班</span>
+          <span class="plan-btns__item active">四月班级</span>
+        </div>
+      </div>
+      <!-- 学习有效期 -->
+      <div class="detail-right__validity validity clearfix">
+        <span class="validity-title pull-left">学习有效期</span>
+        <span class="validity-content pull-left">长期有效</span>
+      </div>
+      <!-- 承诺服务 -->
+      <div class="detail-right__promise promise clearfix">
+        <div class="promise-title pull-left">承诺服务</div>
+        <div class="promise-content pull-left">
+          <span class="promise-content__item">疑</span>
+          <span class="promise-content__item">问</span>
+          <span class="promise-content__item">试</span>
+          <span class="promise-content__item">练</span>
+        </div>
+      </div>
+      <!-- 立即购买 -->
+      <div class="product-detail__btn">立即购买</div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +60,8 @@
   export default {
     data() {
       return {
-          
-      }  
+        
+      }
     },
     methods: {
       
