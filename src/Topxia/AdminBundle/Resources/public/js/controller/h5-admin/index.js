0573871cb312b00546a9a0007b77c31ef08e5b27
chelingfeng <chelingfeng@edusoho.com>
2020-06-12 18:21:11 +0800

    fix: #62438 默认选中tab

--- src/Topxia/AdminBundle/Resources/public/js/controller/h5-admin/index.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/h5-admin/index.js
@@ -7,6 +7,13 @@ define(function(require, exports, module) {
       $('#wap-setting-form').submit();
     });
 
+    $(".old-site-set").click(function(){
+      $('.setting-nav-tab a[href="#theme-page"]').tab('show');
+    });
+    
+    var href = window.location.href.split('#');
+    $('.setting-nav-tab a[href="#'+href[1]+'"]').tab('show');
+
     $('.js-site-edit').click(function () {
       $('#find-page').html('<iframe src="/h5/admin/index.html" frameborder="0" style="width: 100%;height: 700px;border: none;"></iframe>');
     });
