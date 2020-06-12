a01ea11cde1f2f91f8da8181ee78fadda305e5e1
hongyongchun <hongyongchun@edusoho.com>
2020-06-12 16:43:04 +0800

    feat: #62792 授课老师

---
+++ app/Resources/static-src/app/js/goods/show/info-right-teacher.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="info-right-teacher info-right-box">
+    <div class="title">授课老师</div>
+    <div class="teacher-info clearfix">
+      <img class="teacher-info__img pull-left" src="http://t5.edusoho.cn/files/default/2019/08-15/14354956e275898429.jpeg" alt="">
+      <div class="teacher-info__introduce introduce pull-left">
+        <p class="introduce__name">教师名称</p>
+        <p class="introduce__text">教师介绍教师介绍教师介绍教师介绍教师介绍教师介绍</p>
+      </div>
+    </div>
+  </div>
+</template>
--- app/Resources/static-src/app/js/goods/show/product-info.vue
+++ app/Resources/static-src/app/js/goods/show/product-info.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="product-info">
-    
+  <div class="product-info clearfix">
+    <div class="product-info__left pull-left">
+      left
+    </div>
+    <div class="product-info__right pull-right">
+      <!-- 授课老师 -->
+      <info-right-teacher></info-right-teacher>
+    </div>
   </div>
 </template>
 
 <script>
+  import infoRightTeacher from './info-right-teacher';
   export default {
     data() {
       return {
         
       }
+    },
+    components: {
+      infoRightTeacher
     },
     methods: {
       
