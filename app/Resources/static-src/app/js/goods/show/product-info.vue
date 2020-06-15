da4e6f17588c3e24258b3b737f8efa42d46ceb77
hongyongchun <hongyongchun@edusoho.com>
2020-06-15 15:45:27 +0800

    feat: #62792 吸顶效果

--- app/Resources/static-src/app/js/goods/show/product-info.vue
+++ app/Resources/static-src/app/js/goods/show/product-info.vue
@@ -1,7 +1,89 @@
 <template>
   <div class="product-info clearfix">
-    <div class="product-info__left pull-left">
-      left
+    <div class="product-info__left info-left pull-left">
+      <div v-if="isFixed" class="fixed">
+        <div class="cd-container clearfix" >
+          <ul class="info-left__nav pull-left">
+            <li :class="howActive == 1 ? 'active' : ''"><a href="#info-left-1">商品介绍</a></li>
+            <li :class="howActive == 2 ? 'active' : ''"><a href="#info-left-2">学习目录</a></li>
+            <li :class="howActive == 3 ? 'active' : ''"><a href="#info-left-3">学员评价</a></li>
+          </ul>
+          <div class="buy__btn pull-right">
+            <a href="javascript:;">立即购买</a>
+          </div>
+        </div>
+      </div>
+      <ul class="info-left__nav" ref="infoLeftNav">
+        <li :class="howActive == 1 ? 'active' : ''"><a href="#info-left-1">商品介绍</a></li>
+        <li :class="howActive == 2 ? 'active' : ''"><a href="#info-left-2">学习目录</a></li>
+        <li :class="howActive == 3 ? 'active' : ''"><a href="#info-left-3">学员评价</a></li>
+      </ul>
+      <div class="info-left__content">
+        <div v-if="isFixed" class="fixed-box"></div>
+        <h3 id="info-left-1" class="info-left__content__title">商品介绍</h3>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <h3 id="info-left-2" class="info-left__content__title">学习目录</h3>
+           <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+           <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <h3 id="info-left-3" class="info-left__content__title">学员评价</h3>
+           <p>text</p>
+        <p>text</p>
+        <p>text</p>
+           <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+        <p>text</p>
+      </div>
     </div>
     <div class="product-info__right pull-right">
       <!-- 授课老师 -->
@@ -21,7 +103,8 @@
   export default {
     data() {
       return {
-        
+        isFixed: false,
+        howActive: 1
       }
     },
     components: {
@@ -30,7 +113,18 @@
       infoRightLearn
     },
     methods: {
-      
+      handleScroll() {
+        let eleTop = this.$refs.infoLeftNav.offsetTop + this.$refs.infoLeftNav.offsetHeight;
+        let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if ( eleTop <= scrollTop && !this.isFixed ) this.isFixed = true; 
+        if ( eleTop > scrollTop && this.isFixed ) this.isFixed = false;
+      }
+    },
+    mounted() {
+      window.addEventListener("scroll", this.handleScroll);
+    },
+    destroyed() {
+      window.removeEventListener('scroll', this.handleScroll);
     }
   }
 </script>
