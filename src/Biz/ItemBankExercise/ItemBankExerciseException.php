3b6290aa7333964cc6487a0ec6bce0a06fac12db
chelingfeng <chelingfeng@edusoho.com>
2020-06-22 11:56:46 +0800

    fix: #1 修改异常码

--- src/Biz/ItemBankExercise/ItemBankExerciseException.php
+++ src/Biz/ItemBankExercise/ItemBankExerciseException.php
@@ -6,12 +6,14 @@ use AppBundle\Common\Exception\AbstractException;
 
 class ItemBankExerciseException extends AbstractException
 {
-    const FORBIDDEN_LEARN = 4039001;
+    const EXCEPTION_MODULE = 73;
 
-    const CANNOT_START_CHAPTER_ANSWER = 4039002;
+    const FORBIDDEN_LEARN = 4037301;
+
+    const CANNOT_START_CHAPTER_ANSWER = 4037302;
 
     public $messages = [
-        4039001 => 'exception.item_bank_exercise.forbidden_learn',
-        4039002 => 'exception.item_bank_exercise.cannot_start_chapter_answer',
+        4037301 => 'exception.item_bank_exercise.forbidden_learn',
+        4037302 => 'exception.item_bank_exercise.cannot_start_chapter_answer',
     ];
 }
