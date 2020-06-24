52252a7bc9d0bc3c3d04277b6178c846a82d6489
hongyongchun <hongyongchun@edusoho.com>
2020-06-24 12:00:17 +0800

    feat: #62825 教学计划

---
+++ h5/src/h5/containers/goods/components/detail-plan.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="detail-plan">
+    <!-- 学习计划 -->
+    <div class="detail-plan__plan clearfix" @click="showPopup">
+      <div class="pull-left plan-left">教学计划</div>
+      <div class="pull-left plan-right">
+        已选某某教学计划
+        <i class="iconfont icon-arrow-right plan-icon"></i>
+      </div>
+    </div>
+    <!-- 学习计划弹出框 -->
+    <van-popup v-model="show" round position="bottom" @close="onClose" class="detail-plan__popup plan-popup">
+      <div class="plan-popup__title">
+        <span></span>
+        选择教学计划
+      </div>
+    </van-popup>
+    <!-- 学习有效期 -->
+    <div class="detail-plan__plan clearfix">
+      <div class="pull-left plan-left">学习有效期</div>
+      <div class="pull-left plan-right">长期有效</div>
+    </div>
+    <!-- 承诺服务 -->
+    <div class="detail-plan__plan clearfix">
+      <div class="pull-left plan-left">承诺服务</div>
+      <div class="pull-left plan-right">
+        <span class="plan-promise">练</span>
+        <span class="plan-promise">试</span>
+        <span class="plan-promise">问</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false // 是否显示弹出层
+    };
+  },
+  methods: {
+    // 点击显示弹窗
+    showPopup() {
+      this.show = true;
+    },
+    // 关闭弹窗时触发
+    onClose() {
+      this.show = false;
+    }
+  }
+};
+</script>
--- h5/src/h5/containers/goods/detail.vue
+++ h5/src/h5/containers/goods/detail.vue
@@ -8,16 +8,20 @@
     <detail-discount />
     <!-- 商品名称、价格 -->
     <detail-info />
+    <!-- 教学计划、有效期、服务 -->
+    <detail-plan />
   </div>
 </template>
 
 <script>
 import DetailDiscount from './components/detail-discount';
 import DetailInfo from './components/detail-info';
+import DetailPlan from './components/detail-plan';
 export default {
   components: {
     DetailDiscount,
-    DetailInfo
+    DetailInfo,
+    DetailPlan
   }
 }
 </script>
