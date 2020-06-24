426201bcfe8d06e8345f2e61dc3bc0f3c4374fce
hongyongchun <hongyongchun@edusoho.com>
2020-06-24 09:44:27 +0800

    feat: #62825 标题和价格展示

--- h5/src/h5/containers/goods/components/detail-info.vue
+++ h5/src/h5/containers/goods/components/detail-info.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="detail-info">
-    <p class="detail-info__title text-overflow">这是一个商品的名称字数限制字数限制字数限制</p>
-    <div class="detail-info__price">price</div>
+    <p class="detail-info__title text-overflow">这是一个商品的名称字数限制字数限制字数限制限制限制</p>
+    <div class="detail-info__price">
+      <div class="clearfix">
+        <div class="pull-left">
+          优惠价
+          <span class="price">129.00</span>
+          元
+        </div>
+        <div class="pull-right study-num">22人在学</div>
+      </div>
+      <p class="original-price"><s>价格&nbsp;<span>399.00</span>元</s></p>
+    </div>
   </div>
 </template>
