71fa6d93a32ccd9246411b9ff14efdf21de86aec
钟云昶 <zhongyunchang@zhongyungdeMBP2.lan>
2020-06-28 09:08:16 +0800

    fix: #63650 修改升级脚本

---
+++ upgrade-scripts/upgrade-s2bsc.php
@@ -0,0 +1,338 @@
+<?php
+
+use Symfony\Component\Filesystem\Filesystem;
+use AppBundle\Common\ArrayToolkit;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function __construct($biz)
+    {
+        parent::__construct($biz);
+    }
+
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $result = $this->updateScheme($index);
+
+            $this->getConnection()->commit();
+
+            if (!empty($result)) {
+                return $result;
+            } else {
+                $this->logger('info', '执行升级脚本结束');
+            }
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            $this->logger('error', $e->getTraceAsString());
+            throw $e;
+        }
+
+        try {
+            $dir = realpath($this->biz['kernel.root_dir'].'/../web/install');
+            $filesystem = new Filesystem();
+
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+            $this->logger('error', $e->getTraceAsString());
+        }
+
+        $developerSetting = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+
+        $this->getSettingService()->set('developer', $developerSetting);
+        $this->getSettingService()->set('crontab_next_executed_time', time());
+    }
+
+    private function updateScheme($index)
+    {
+        $definedFuncNames = array(
+            'productConvert',
+            'uploadFileConvert',
+        );
+
+        $funcNames = array();
+        foreach ($definedFuncNames as $key => $funcName) {
+            $funcNames[$key + 1] = $funcName;
+        }
+
+        if (0 == $index) {
+            $this->logger('info', '开始执行升级脚本');
+            $this->deleteCache();
+
+            return array(
+                'index' => $this->generateIndex(1, 1),
+                'message' => '升级数据...',
+                'progress' => 0,
+            );
+        }
+
+        list($step, $page) = $this->getStepAndPage($index);
+        $method = $funcNames[$step];
+        $page = $this->$method($page);
+
+        if (1 == $page) {
+            ++$step;
+        }
+
+        if ($step <= count($funcNames)) {
+            return array(
+                'index' => $this->generateIndex($step, $page),
+                'message' => '升级数据...',
+                'progress' => 0,
+            );
+        }
+    }
+
+    protected function productConvert()
+    {
+        $this->getConnection()->exec("
+            INSERT INTO `s2b2c_product` 
+            (`supplierId`, `productType`, `remoteProductId`, `remoteResourceId`, `localResourceId`, `cooperationPrice`, `suggestionPrice`, `localVersion`, `changelog`, `createdTime`, `updatedTime`)
+            SELECT 
+            originPlatformId, 
+            'course', 
+            s2b2cDistributeId, 
+            sourceCourseId, 
+            id, 
+            cooperationPrice, 
+            suggestionPrice, 
+            sourceVersion, 
+            versionChangeLog, 
+            statusSyncTime, 
+            statusSyncTime 
+            FROM `course_v8` WHERE originPlatform = 'supplier';
+        ");
+
+        $this->getConnection()->exec("
+            INSERT INTO `s2b2c_product` (`supplierId`, `productType`, `remoteProductId`, `remoteResourceId`, `localResourceId`, `localVersion`, `changelog`, `createdTime`, `updatedTime`)
+            SELECT 
+            originPlatformId, 
+            'course_set', 
+            '0', 
+            sourceCourseSetId, 
+            id, 
+            sourceVersion, 
+            versionChangeLog, 
+            createdTime, 
+            updatedTime 
+            FROM `course_set_v8` WHERE originPlatform = 'supplier';
+        ");
+
+        return 1;
+    }
+
+    protected function uploadFileConvert()
+    {
+        $this->getConnection()->exec("
+            UPDATE `upload_files` SET storage='supplier' WHERE originPlatform='supplier';
+        ");
+
+        return 1;
+    }
+
+    public function addTableIndexJob()
+    {
+        if ($this->isJobExist('HandlingTimeConsumingUpdateStructuresJob')) {
+            return 1;
+        }
+
+        $currentTime = time();
+        $today = strtotime(date('Y-m-d', $currentTime) . '02:00:00');
+
+        if ($currentTime > $today) {
+            $time = strtotime(date('Y-m-d', strtotime('+1 day')) . '02:00:00');
+        }
+
+        $this->getConnection()->exec("INSERT INTO `biz_scheduler_job` (
+              `name`,
+              `expression`,
+              `class`,
+              `args`,
+              `priority`,
+              `pre_fire_time`,
+              `next_fire_time`,
+              `misfire_threshold`,
+              `misfire_policy`,
+              `enabled`,
+              `creator_id`,
+              `updated_time`,
+              `created_time`
+        ) VALUES (
+              'HandlingTimeConsumingUpdateStructuresJob',
+              '',
+              'Biz\\\\UpdateDatabaseStructure\\\\\Job\\\\HandlingTimeConsumingUpdateStructuresJob',
+              '',
+              '200',
+              '0',
+              '{$time}',
+              '300',
+              'executing',
+              '1',
+              '0',
+              '{$currentTime}',
+              '{$currentTime}'
+        )");
+        $this->logger('info', 'INSERT增加索引的定时任务HandlingTimeConsumingUpdateStructuresJob');
+        return 1;
+    }
+
+    public function createTableIndex()
+    {
+        if (!$this->isIndexExist('reward_point_account_flow', 'userId')) {
+            $this->createIndex('reward_point_account_flow', 'userId', 'userId');
+        }
+
+        if (!$this->isIndexExist('member_operation_record', 'operateType_targetType_reasonType')) {
+            $this->createIndex('member_operation_record', 'operateType_targetType_reasonType', 'operate_type,target_type,reason_type');
+        }
+
+        return 1;
+    }
+
+    protected function generateIndex($step, $page)
+    {
+        return $step * 1000000 + $page;
+    }
+
+    protected function getStepAndPage($index)
+    {
+        $step = intval($index / 1000000);
+        $page = $index % 1000000;
+
+        return array($step, $page);
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function isIndexExist($table, $indexName)
+    {
+        $sql = "show index from `{$table}` where key_name='{$indexName}';";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return empty($result) ? false : true;
+    }
+
+    protected function createIndex($table, $index, $column)
+    {
+        if (!$this->isIndexExist($table, $column, $index)) {
+            $this->getConnection()->exec("ALTER TABLE {$table} ADD INDEX {$index} ({$column})");
+        }
+    }
+
+    protected function isJobExist($code)
+    {
+        $sql = "select * from biz_scheduler_job where name='{$code}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return empty($result) ? false : true;
+    }
+
+    protected function deleteCache()
+    {
+        $cachePath = $this->biz['cache_directory'];
+        $filesystem = new Filesystem();
+        $filesystem->remove($cachePath);
+
+        clearstatcache(true);
+
+        $this->logger('info', '删除缓存');
+
+        return 1;
+    }
+
+    protected function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+
+    protected function logger($level, $message)
+    {
+        $version = \AppBundle\System::VERSION;
+        $data = date('Y-m-d H:i:s')." [{$level}] {$version} ".$message.PHP_EOL;
+        if (!file_exists($this->getLoggerFile())) {
+            touch($this->getLoggerFile());
+        }
+        file_put_contents($this->getLoggerFile(), $data, FILE_APPEND);
+    }
+
+    private function getLoggerFile()
+    {
+        return $this->biz['kernel.root_dir'].'/../app/logs/upgrade.log';
+    }
+
+    /**
+     * @return \Biz\DiscoveryColumn\Service\DiscoveryColumnService
+     */
+    protected function getDiscoveryColumnService()
+    {
+        return $this->createService('DiscoveryColumn:DiscoveryColumnService');
+    }
+
+    /**
+     * @return \Biz\Taxonomy\Service\CategoryService
+     */
+    protected function getCategoryService()
+    {
+        return $this->createService('Taxonomy:CategoryService');
+    }
+
+    /**
+     * @return \Biz\System\Service\H5SettingService
+     */
+    protected function getH5SettingService()
+    {
+        return $this->createService('System:H5SettingService');
+    }
+
+    /**
+     * @return \Biz\Course\Service\CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->createService('Course:CourseService');
+    }
+}
