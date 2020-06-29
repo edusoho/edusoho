3304659241cc5fb95db397d4fdd96483970cb2b6
chelingfeng <chelingfeng@edusoho.com>
2020-06-29 10:25:57 +0800

    feat: #1 app收藏相关接口

---
+++ src/ApiBundle/Api/Resource/Item/ItemFilter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ApiBundle\Api\Resource\Item;
+
+use ApiBundle\Api\Resource\Filter;
+
+class ItemFilter extends Filter
+{
+    protected $publicFields = [
+        'id',
+        'bank_id',
+        'type',
+        'material',
+        'category_id',
+        'difficulty',
+        'question_num',
+        'questions',
+    ];
+
+    protected function publicFields(&$item)
+    {
+        $questionFilter = new QuestionFilter();
+        !empty($item['material']) && $item['material'] = $this->convertAbsoluteUrl($item['material']);
+        !empty($item['analysis']) && $item['analysis'] = $this->convertAbsoluteUrl($item['analysis']);
+        $questionFilter->filters($item['questions']);
+    }
+}
