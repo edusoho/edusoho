e5162b106fdbc1c642f19c1d73755802cffd7535
hongyongchun <hongyongchun@edusoho.com>
2020-07-02 17:40:57 +0800

    feat: #62792 旧版微网校基本设置

---
+++ app/Resources/static-src/app/js/goods/min-screen-show/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="min-screen-show">
+    min-screen-show
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        componentsData: {}
+      }
+    },
+    components: {
+    },
+    methods: {
+      
+    },
+    created() {
+      axios.get('/api/goods/1', {
+        headers: { 'Accept': 'application/vnd.edusoho.v2+json'}
+      }).then((res) => {
+        this.componentsData = res.data;
+      });
+    }
+  }
+</script>
