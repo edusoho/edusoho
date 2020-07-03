dde5446bbab12ad16067904d731cecb3e55d9cee
shenchupeng <shenchupeng@howzhi.com>
2020-07-03 10:56:51 +0800

    feat: #62937 修改错误码

--- src/Biz/ItemBankExercise/ItemBankExerciseException.php
+++ src/Biz/ItemBankExercise/ItemBankExerciseException.php
@@ -38,26 +38,27 @@ class ItemBankExerciseException extends AbstractException
 
     const ASSESSMENT_EXCEED = 5007515;
 
-    const NOTFOUND_MODULE = 4047316;
+    const NOTFOUND_MODULE = 4047516;
 
-    const ASSESSMENT_EXERCISE_EXIST = 5007317;
+    const ASSESSMENT_EXERCISE_EXIST = 5007517;
 
     public $messages = [
-        4037301 => 'exception.item_bank_exercise.forbidden_learn',
-        5007302 => 'exception.item_bank_exercise.chapter_answer_is_doing',
-        4047303 => 'exception.item_bank_exercise.exercise.not_found',
-        4037304 => 'exception.item_bank_exercise.exercise.forbidden_manage_exercise',
-        4037305 => 'exception.item_bank_exercise.exercise.exercise_not_published',
-        5007306 => 'exception.item_bank_exercise.expirydays_required',
-        5007307 => 'exception.item_bank_exercise.expirydays_invalid',
-        5007308 => 'exception.item_bank_exercise.expiryenddate_required',
-        5007309 => 'exception.item_bank_exercise.expirystartdate_required',
-        5007310 => 'exception.item_bank_exercise.expirydate_end_later_than_start',
-        5007311 => 'exception.item_bank_exercise.expirymode_invalid',
-        5007312 => 'exception.item_bank_exercise.chapter_exercise_closed',
-        5007313 => 'exception.item_bank_exercise.assessment_answer_is_doing',
-        5007314 => 'exception.item_bank_exercise.module.exceeded_number',
-        4047315 => 'exception.item_bank_exercise.exercise.module_not_found',
-        5007316 => 'exception.item_bank_exercise.assessment_exercise_exist',
+        4037501 => 'exception.item_bank_exercise.forbidden_learn',
+        5007502 => 'exception.item_bank_exercise.chapter_answer_is_doing',
+        4047503 => 'exception.item_bank_exercise.exercise.not_found',
+        4037504 => 'exception.item_bank_exercise.exercise.forbidden_manage_exercise',
+        4037505 => 'exception.item_bank_exercise.exercise.exercise_not_published',
+        5007506 => 'exception.item_bank_exercise.expirydays_required',
+        5007507 => 'exception.item_bank_exercise.expirydays_invalid',
+        5007508 => 'exception.item_bank_exercise.expiryenddate_required',
+        5007509 => 'exception.item_bank_exercise.expirystartdate_required',
+        5007510 => 'exception.item_bank_exercise.expirydate_end_later_than_start',
+        5007511 => 'exception.item_bank_exercise.expirymode_invalid',
+        5007512 => 'exception.item_bank_exercise.chapter_exercise_closed',
+        5007513 => 'exception.item_bank_exercise.assessment_exercise_closed',
+        5007514 => 'exception.item_bank_exercise.assessment_answer_is_doing',
+        5007515 => 'exception.item_bank_exercise.module.exceeded_number',
+        4047516 => 'exception.item_bank_exercise.exercise.module_not_found',
+        5007517 => 'exception.item_bank_exercise.assessment_exercise_exist',
     ];
 }
