a89168f820e10dcf1cb9373c5671bf59debe8756
钟云昶 <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-07-06 09:50:55 +0800

    fix: #63850 修改数据结构

--- migrations/20200617180417_create_table_review.php
+++ migrations/20200617180417_create_table_review.php
@@ -20,7 +20,7 @@ class CreateTableReview extends Migration
                 `parentId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '回复id',
                 `meta` text COMMENT '评论元信息',
                 `createdTime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评价创建时间',
-                `updatedTime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评价更新时间',
+                `updatedTime` int(10) unsigned DEFAULT '0' COMMENT '评价更新时间',
                 PRIMARY KEY (`id`),
                 KEY `targetType_targetId` (targetType, targetId)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评价表';
