077144f4f04134aa7fa314aa381deed4d2a9274c
chelingfeng <chelingfeng@edusoho.com>
2020-07-08 15:49:01 +0800

    feat: #64030 评价相关接口

--- tests/Unit/ItemBankExercise/Event/ExerciseEventSubscriberTest.php
+++ tests/Unit/ItemBankExercise/Event/ExerciseEventSubscriberTest.php
@@ -22,4 +22,34 @@ class ExerciseEventSubscriberTest extends BaseTestCase
         $result = $subscriber->onQuestionBankUpdate($event);
         $this->assertNull($result);
     }
+
+    public function testOnReviewChanged()
+    {
+        $subscriber = new ExerciseEventSubscriber($this->biz);
+        $event = new Event(
+            [
+                'id' => 1,
+                'targetType' => 'item_bank_exercise',
+                'targetId' => 1,
+            ]
+        );
+
+        $result = $subscriber->onReviewChanged($event);
+        $this->assertNull($result);
+    }
+
+    public function testOnReviewChanged_whenTargetTypeInvalid_thenReturnFalse()
+    {
+        $subscriber = new ExerciseEventSubscriber($this->biz);
+        $event = new Event(
+            [
+                'id' => 1,
+                'targetType' => '222',
+                'targetId' => 1,
+            ]
+        );
+
+        $result = $subscriber->onReviewChanged($event);
+        $this->assertEquals($result, false);
+    }
 }
