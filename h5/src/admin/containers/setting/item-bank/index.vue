d8e4f78f0979b173007ae73101eb7b2b4189cda8
chenjinhong <chenjinhong@edusoho.com>
2020-07-09 17:29:46 +0800

    feat: #63938-完成题库后台发现页配置

---
+++ h5/src/admin/containers/setting/item-bank/index.vue
@@ -0,0 +1,426 @@
+<template>
+  <module-frame
+    :is-active="isActive"
+    :is-incomplete="isIncomplete"
+    container-class="setting-course"
+  >
+    <div slot="preview" class="find-page__part">
+      <e-course-list
+        :course-list="copyModuleData.data"
+        :feedback="false"
+        type-list="item_bank_exercise"
+        show-mode="admin"
+        uiStyle="new"
+        @fetchCourse="fetchItemBankList"
+      />
+    </div>
+    <div slot="setting">
+      <e-suggest v-if="moduleData.tips" :suggest="moduleData.tips"></e-suggest>
+      <header class="title">
+        公开课列表设置
+        <div class="text-12 color-gray mts">
+          注意：仅含直播公开课内容，不包含录播公开课
+        </div>
+      </header>
+      <div class="default-allocate__content clearfix">
+        <!-- 列表名称 -->
+        <setting-cell title="列表名称：" left-class="required-option">
+          <el-input
+            v-model="copyModuleData.data.title"
+            size="mini"
+            max-length="15"
+            placeholder="请输入列表名称"
+            clearable
+          />
+        </setting-cell>
+
+        <!-- 排列方式： -->
+        <setting-cell v-if="portal !== 'miniprogram'" title="排列方式：">
+          <el-select v-model="displayStyle" placeholder="排列方式" size="mini">
+            <el-option
+              v-for="item in layoutOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </setting-cell>
+        <!-- 课程来源 -->
+        <!-- <setting-cell :title="typeLabel + '来源：'">
+          <el-radio v-model="sourceType" label="condition"
+            >{{ typeLabel }}分类</el-radio
+          >
+          <el-radio v-model="sourceType" label="custom">自定义</el-radio>
+        </setting-cell> -->
+
+        <!-- 课程分类 -->
+        <setting-cell :title="typeLabel + '分类：'">
+          <el-cascader
+            v-show="sourceType === 'condition'"
+            :options="courseCategories"
+            :props="cascaderProps"
+            v-model="categoryTempId"
+            size="mini"
+            placeholder="请输入列表名称"
+            filterable
+            change-on-select
+          />
+          <div v-show="sourceType === 'custom'" class="required-option">
+            <el-button size="mini" @click="openModal"
+              >选择{{ typeLabel }}</el-button
+            >
+          </div>
+        </setting-cell>
+
+        <draggable
+          v-show="sourceType === 'custom' && copyModuleData.data.items.length"
+          v-model="copyModuleData.data.items"
+          class="default-draggable__list"
+        >
+          <div
+            v-for="(courseItem, index) in copyModuleData.data.items"
+            :key="index"
+            class="default-draggable__item"
+          >
+            <div class="default-draggable__title text-overflow">
+              {{ courseItem.displayedTitle || courseItem.title }}
+            </div>
+            <i
+              class="h5-icon h5-icon-cuowu1 default-draggable__icon-delete"
+              @click="deleteCourse(index)"
+            />
+          </div>
+        </draggable>
+
+        <!-- 排列顺序 -->
+        <setting-cell v-show="sourceType === 'condition'" title="排列顺序：">
+          <div class="section-right__item pull-left">
+            <el-select v-model="sort" placeholder="顺序" size="mini">
+              <el-option
+                v-for="item in sortOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </div>
+          <div class="section-right__item pull-right">
+            <el-select v-model="lastDays" placeholder="时间区间" size="mini">
+              <el-option
+                v-for="item in dateOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </div>
+        </setting-cell>
+
+        <!-- 显示个数 -->
+        <setting-cell v-show="sourceType === 'condition'" title="显示个数：">
+          <el-select v-model="limit" placeholder="请选择个数" size="mini">
+            <el-option
+              v-for="item in limitOptions"
+              :key="item"
+              :label="item"
+              :value="item"
+            />
+          </el-select>
+        </setting-cell>
+      </div>
+    </div>
+    <!-- <course-modal
+      slot="modal"
+      :visible="modalVisible"
+      :limit="limit"
+      :type="type"
+      :course-list="copyModuleData.data.items"
+      @visibleChange="modalVisibleHandler"
+      @updateCourses="getUpdatedCourses"
+    /> -->
+  </module-frame>
+</template>
+<script>
+import draggable from 'vuedraggable';
+import courseList from '&/components/e-course-list/e-course-list';
+// import courseModal from '../course/modal/course-modal';
+import moduleFrame from '../module-frame';
+import settingCell from '../module-frame/setting-cell';
+import { mapState, mapActions } from 'vuex';
+import treeDigger from 'admin/utils/tree-digger';
+import pathName2Portal from 'admin/config/api-portal-config';
+import suggest from '&/components/e-suggest/e-suggest.vue';
+
+export default {
+  components: {
+    'e-course-list': courseList,
+    draggable,
+    // courseModal,
+    moduleFrame,
+    settingCell,
+    'e-suggest': suggest,
+  },
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    moduleData: {
+      type: Object,
+      default: () => {},
+    },
+    incomplete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      modalVisible: false,
+      limitOptions: [1, 2, 3, 4, 5, 6, 7, 8],
+      type: this.moduleData.type,
+      layoutOptions: [
+        {
+          value: 'row',
+          label: '一行一列',
+        },
+        {
+          value: 'distichous',
+          label: '一行两列',
+        },
+      ],
+      sortOptions: [
+        {
+          value: '-studentNum',
+          label: '加入最多',
+        },
+        {
+          value: '-createdTime',
+          label: '最近创建',
+        },
+        {
+          value: '-rating',
+          label: '评分最高',
+        },
+        {
+          value: 'recommendedSeq',
+          label: `推荐题库`,
+        },
+      ],
+      cascaderProps: {
+        label: 'name',
+        value: 'id',
+      },
+      pathName: this.$route.name,
+      categoryTempId: this.moduleData.data.categoryIdArray || ['0'],
+      categoryDiggered: false,
+      dateOptions: [
+        {
+          value: '7',
+          label: '最近7天',
+        },
+        {
+          value: '30',
+          label: '最近30天',
+        },
+        {
+          value: '90',
+          label: '最近90天',
+        },
+        {
+          value: '0',
+          label: '历史所有',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(['courseCategories', 'classCategories']),
+    typeLabel() {
+      return '题库';
+    },
+    isActive: {
+      get() {
+        return this.active;
+      },
+      set() {},
+    },
+    uiStyle: {
+      get() {
+        if (
+          this.$route.name === 'miniprogramSetting' ||
+          this.$route.query.from === 'miniprogramSetting'
+        ) {
+          return 'old';
+        } else {
+          return 'new';
+        }
+      },
+    },
+    isIncomplete: {
+      get() {
+        return this.incomplete;
+      },
+      set() {},
+    },
+    copyModuleData: {
+      get() {
+        return this.moduleData;
+      },
+      set() {
+        console.log('changed copyModuleData');
+      },
+    },
+    showDateOptions() {
+      const isNewCreated =
+        this.moduleData.data.sort === this.sortOptions[1].value;
+      const isRecommend =
+        this.moduleData.data.sort === this.sortOptions[3].value;
+
+      if (isNewCreated || isRecommend) {
+        // 如果是 最新创建 或 推荐课程 时间区间为所有
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        this.moduleData.data.lastDays = '0';
+      }
+      return !isNewCreated && !isRecommend;
+    },
+    sourceType: {
+      get() {
+        return this.copyModuleData.data.sourceType;
+      },
+      set(value) {
+        this.copyModuleData.data.sourceType = value;
+      },
+    },
+    sort: {
+      get() {
+        return this.copyModuleData.data.sort;
+      },
+      set(value) {
+        this.copyModuleData.data.sort = value;
+      },
+    },
+    displayStyle: {
+      get() {
+        return this.copyModuleData.data.displayStyle;
+      },
+      set(value) {
+        this.copyModuleData.data.displayStyle = value;
+      },
+    },
+    lastDays: {
+      get() {
+        return this.copyModuleData.data.lastDays.toString();
+      },
+      set(value) {
+        this.copyModuleData.data.lastDays = value;
+      },
+    },
+    limit: {
+      get() {
+        return this.copyModuleData.data.limit;
+      },
+      set(value) {
+        this.copyModuleData.data.limit = value;
+      },
+    },
+    categoryId: {
+      get() {
+        return this.copyModuleData.data.categoryId;
+      },
+      set(value) {
+        this.copyModuleData.data.categoryId = value;
+      },
+    },
+    portal() {
+      return pathName2Portal[this.pathName];
+    },
+  },
+  watch: {
+    copyModuleData: {
+      handler(data) {
+        this.$emit('updateModule', data);
+      },
+      deep: true,
+    },
+    categoryTempId: {
+      handler(value) {
+        if (!value.length) {
+          return;
+        }
+        const endIndex = value.length - 1;
+        // 多级分类需要拿到最后等级的id
+        this.moduleData.data.categoryIdArray = value;
+        this.moduleData.data.categoryId = value[endIndex];
+      },
+    },
+    courseCategories: {
+      handler(tree) {
+        if (!tree || this.categoryDiggered) return;
+        let categoryExist = false;
+        treeDigger(tree, (children, id) => {
+          if (id) {
+            categoryExist = id == this.categoryTempId;
+          }
+          return children;
+        });
+        this.categoryDiggered = true;
+
+        if (categoryExist) return true;
+        // this.categoryTempId = ['0'];
+      },
+      immediate: true,
+    },
+    classCategories: {
+      handler(tree) {
+        if (!tree || this.categoryDiggered) return;
+
+        let categoryExist = false;
+        treeDigger(tree, (children, id) => {
+          if (id) {
+            categoryExist = id == this.categoryTempId;
+          }
+          return children;
+        });
+        this.categoryDiggered = true;
+
+        if (categoryExist) return;
+        this.categoryTempId = ['0'];
+      },
+      immediate: true,
+    },
+    sourceType(value) {
+      this.limit = value === 'condition' ? 4 : 8;
+    },
+  },
+  methods: {
+    ...mapActions(['getItemBankList']),
+    getUpdatedCourses(courses) {
+      this.copyModuleData.data.items = courses;
+    },
+    modalVisibleHandler(visible) {
+      this.modalVisible = visible;
+    },
+    openModal() {
+      this.modalVisible = true;
+    },
+    // 删除自定义课程
+    deleteCourse(index) {
+      this.copyModuleData.data.items.splice(index, 1);
+    },
+    fetchItemBankList({ params, index }) {
+      if (this.sourceType === 'custom') return;
+      this.getItemBankList(params)
+        .then(res => {
+          this.moduleData.data.items = res.data;
+        })
+        .catch(err => {
+          this.$message({
+            message: err.message,
+            type: 'error',
+          });
+        });
+    },
+  },
+};
+</script>
--- h5/src/admin/store/actions.js
+++ h5/src/admin/store/actions.js
@@ -7,152 +7,175 @@ export const updateLoading = ({ commit }, { isLoading }) => {
 };
 
 // 全局设置
-export const getGlobalSettings = ({ commit }, { type, key }) => Api.getSettings({
- query: { type },
-}).then(res => {
-  commit(types.GET_SETTINGS, {
-    key,
-    setting: res || {}
+export const getGlobalSettings = ({ commit }, { type, key }) =>
+  Api.getSettings({
+    query: { type },
+  }).then(res => {
+    commit(types.GET_SETTINGS, {
+      key,
+      setting: res || {},
+    });
+    return res;
   });
-  return res;
-});
 
 // 全局vip元素显示开关
-export const setVipLevels = ({ commit }) => Api.getVipLevels().then(levels => {
-  const levelsExist = levels;
-  commit(types.GET_SETTINGS, { key: 'vipLevels', setting: levelsExist });
-  return levelsExist;
-});
+export const setVipLevels = ({ commit }) =>
+  Api.getVipLevels().then(levels => {
+    const levelsExist = levels;
+    commit(types.GET_SETTINGS, { key: 'vipLevels', setting: levelsExist });
+    return levelsExist;
+  });
 
 // vip插件安装
-export const setVipSetupStatus = ({ commit }) => Api.vipPlugin().then(vipPlugin => {
-  const pluginInfo = vipPlugin || {};
-  const pluginStatus = Object.keys(vipPlugin).length > 1;
-  commit(types.GET_SETTINGS, { key: 'vipSetupStatus', setting: pluginStatus });
-  commit(types.GET_SETTINGS, { key: 'vipPlugin', setting: pluginInfo });
-  return vipPlugin;
-});
+export const setVipSetupStatus = ({ commit }) =>
+  Api.vipPlugin().then(vipPlugin => {
+    const pluginInfo = vipPlugin || {};
+    const pluginStatus = Object.keys(vipPlugin).length > 1;
+    commit(types.GET_SETTINGS, {
+      key: 'vipSetupStatus',
+      setting: pluginStatus,
+    });
+    commit(types.GET_SETTINGS, { key: 'vipPlugin', setting: pluginInfo });
+    return vipPlugin;
+  });
 
 // 课程分类
-export const getCourseCategories = ({ commit }) => Api.getCategories({
-  query: {
-    groupCode: 'course'
-  }
-}).then(res => {
-  res.unshift({ id: '0', children: undefined, name: '全部' });
-  const formatedRes = treeDigger(res, children => {
-    if (!children.length) {
-      children = undefined;
-    }
-    return children;
+export const getCourseCategories = ({ commit }) =>
+  Api.getCategories({
+    query: {
+      groupCode: 'course',
+    },
+  }).then(res => {
+    res.unshift({ id: '0', children: undefined, name: '全部' });
+    const formatedRes = treeDigger(res, children => {
+      if (!children.length) {
+        children = undefined;
+      }
+      return children;
+    });
+    commit(types.GET_COURSE_CATEGORIES, formatedRes);
+    return formatedRes;
   });
-  commit(types.GET_COURSE_CATEGORIES, formatedRes);
-  return formatedRes;
-});
 
 // 班级分类
-export const getClassCategories = ({ commit }) => Api.getCategories({
-  query: {
-    groupCode: 'classroom'
-  }
-}).then(res => {
-  res.unshift({ id: '0', children: undefined, name: '全部' });
-  const formatedRes = treeDigger(res, children => {
-    if (!children.length) {
-      children = undefined;
-    }
-    return children;
+export const getClassCategories = ({ commit }) =>
+  Api.getCategories({
+    query: {
+      groupCode: 'classroom',
+    },
+  }).then(res => {
+    res.unshift({ id: '0', children: undefined, name: '全部' });
+    const formatedRes = treeDigger(res, children => {
+      if (!children.length) {
+        children = undefined;
+      }
+      return children;
+    });
+    commit(types.GET_CLASS_CATEGORIES, formatedRes);
+    return formatedRes;
   });
-  commit(types.GET_CLASS_CATEGORIES, formatedRes);
-  return formatedRes;
-});
-
 
 // 获取后台配置（草稿／正式）
-export const getDraft = ({ commit }, { portal, type, mode }) => Api.getDraft({
-  query: {
-    portal,
-    type
-  },
-  params: {
-    mode
-  }
-});
+export const getDraft = ({ commit }, { portal, type, mode }) =>
+  Api.getDraft({
+    query: {
+      portal,
+      type,
+    },
+    params: {
+      mode,
+    },
+  });
 
 // 获取后台配置（草稿／正式）
-export const getTemplate = ({ commit }, { portal, template }) => Api.getTemplate({
-  query: {
-    portal,
-    template
-  }
-});
+export const getTemplate = ({ commit }, { portal, template }) =>
+  Api.getTemplate({
+    query: {
+      portal,
+      template,
+    },
+  });
 
 export const getCategoryType = ({ commit }, { type }) => {
   let current = type;
   if (type === 'openCourse') {
-    current = 'course'
+    current = 'course';
   }
   return Api.getCategoryType({
     query: {
-      type: current
-    }
-  })
+      type: current,
+    },
+  });
 };
 
 // 删除后台配置（草稿／正式）
-export const deleteDraft = ({ commit }, { portal, type, mode }) => Api.deleteDraft({
-  query: {
-    portal,
-    type
-  },
-  params: {
-    mode
-  }
-});
+export const deleteDraft = ({ commit }, { portal, type, mode }) =>
+  Api.deleteDraft({
+    query: {
+      portal,
+      type,
+    },
+    params: {
+      mode,
+    },
+  });
 
 // 保存后台配置（草稿／正式）
-export const saveDraft = ({ commit }, { portal, type, mode, data }) => Api.saveDraft({
-  params: {
-    type,
-    mode
-  },
-  query: { portal },
-  data
-});
+export const saveDraft = ({ commit }, { portal, type, mode, data }) =>
+  Api.saveDraft({
+    params: {
+      type,
+      mode,
+    },
+    query: { portal },
+    data,
+  });
 
 // 课程搜索列表数据
-export const getCourseList = ({ commit }, params) => Api.getCourseList({
-  params
-});
+export const getCourseList = ({ commit }, params) =>
+  Api.getCourseList({
+    params,
+  });
 
 // 班级搜索列表数据
-export const getClassList = ({ commit }, params) => Api.getClassList({
-  params
-});
+export const getClassList = ({ commit }, params) =>
+  Api.getClassList({
+    params,
+  });
 
 //公开课
-export const getOpenCourseList = ({ commit }, params) => Api.getOpenCourseList({
-  params
-});
+export const getOpenCourseList = ({ commit }, params) =>
+  Api.getOpenCourseList({
+    params,
+  });
+
+//题库课
+export const getItemBankList = ({ commit }, params) =>
+  Api.getItemBankList({
+    params,
+  });
 
 // 营销活动搜索列表数据
-export const getMarketingList = ({ commit }, params) => Api.getMarketingList({
-  params
-});
+export const getMarketingList = ({ commit }, params) =>
+  Api.getMarketingList({
+    params,
+  });
 
 // 优惠券搜索列表数据
-export const getCouponList = ({ commit }, params) => Api.getCouponList({
-  params
-});
+export const getCouponList = ({ commit }, params) =>
+  Api.getCouponList({
+    params,
+  });
 
 // 后台配置预览二维码
-export const getQrcode = ({ commit }, { route, preview, times, duration }) => Api.getQrcode({
-  query: {
-    route
-  },
-  params: {
-    preview,
-    times,
-    duration
-  }
-});
+export const getQrcode = ({ commit }, { route, preview, times, duration }) =>
+  Api.getQrcode({
+    query: {
+      route,
+    },
+    params: {
+      preview,
+      times,
+      duration,
+    },
+  });
