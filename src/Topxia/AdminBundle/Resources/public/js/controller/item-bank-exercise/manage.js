0e0dcd4557af9db0269fa16966cbff5c2ac9bc7a
shenchupeng <shenchupeng@howzhi.com>
2020-07-09 16:20:10 +0800

    feat: #62940 修改文案

--- src/Topxia/AdminBundle/Resources/public/js/controller/item-bank-exercise/manage.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/item-bank-exercise/manage.js
@@ -7,16 +7,26 @@ define(function(require, exports, module) {
     var $table = $('#exercise-table');
     $table.on('click', '.cancel-recommend-exercise', function () {
       $.post($(this).data('url'), function (html) {
-        Notify.success(Translator.trans('admin.course.cancel_recommend_success_hint'));
+        Notify.success(Translator.trans('admin.item_bank_exercise.exercise.cancel_recommend_success_hint'));
         window.location.reload();
       });
     });
 
     $table.on('click', '.publish-exercise', function() {
-      var studentNum = $(this).closest('tr').next().val();
-      if (!confirm(Translator.trans('admin.course.publish_hint'))) return false;
+      if (!confirm(Translator.trans('admin.item_bank_exercise.exercise.publish_hint'))) return false;
       $.post($(this).data('url'), function(response) {
-        Notify.success(Translator.trans('admin.course.publish_success_hint'));
+        Notify.success(Translator.trans('admin.item_bank_exercise.exercise.publish_success_hint'));
+        window.location.reload();
+      }).error(function(e) {
+        var res = e.responseJSON.error.message || Translator.trans('admin.course.unknow_error_hint');
+        Notify.danger(res);
+      });
+    });
+
+    $table.on('click', '.close-exercise', function() {
+      if (!confirm(Translator.trans('admin.item_bank_exercise.exercise.close_hint'))) return false;
+      $.post($(this).data('url'), function(response) {
+        Notify.success(Translator.trans('admin.item_bank_exercise.exercise.close_success_hint'));
         window.location.reload();
       }).error(function(e) {
         var res = e.responseJSON.error.message || Translator.trans('admin.course.unknow_error_hint');
@@ -26,7 +36,7 @@ define(function(require, exports, module) {
 
     $table.on('click', '.delete-exercise', function() {
       var $this = $(this);
-      if (!confirm(Translator.trans('admin.course.delete_hint')))
+      if (!confirm(Translator.trans('admin.item_bank_exercise.exercise.delete_hint')))
         return;
       $.post($this.data('url'), function(data) {
         Notify.success(Translator.trans('site.delete_success_hint'));
