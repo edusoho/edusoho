0f89809b83f392042977d80e01a8fad4f2091523
zhangsimin <zhangsimin@edusoho.com>
2020-07-13 20:14:52 -0700

    feat: #64122 exercise-show

--- tests/Unit/ItemBankExercise/Dao/AssessmentExerciseDaoTest.php
+++ tests/Unit/ItemBankExercise/Dao/AssessmentExerciseDaoTest.php
@@ -3,46 +3,8 @@
 namespace Tests\Unit\ItemBankExercise\Dao;
 
 use Biz\BaseTestCase;
-<<<<<<< HEAD
+
 use Biz\ItemBankExercise\Dao\AssessmentExerciseDao;
-
-class AssessmentExerciseDaoTest extends BaseTestCase
-{
-    public function testFindByExerciseIdAndModuleId()
-    {
-        $excepted = $this->createAssessmentExercise();
-        $res = $this->getItemBankAssessmentExerciseDao()->findByExerciseIdAndModuleId($excepted['exerciseId'], $excepted['moduleId']);
-
-        $this->assertEquals($excepted['exerciseId'], $res[0]['exerciseId']);
-        $this->assertEquals($excepted['moduleId'], $res[0]['moduleId']);
-        $this->assertEquals($excepted['assessmentId'], $res[0]['assessmentId']);
-    }
-
-    public function testIsAssessmentExercise()
-    {
-        $excepted = $this->createAssessmentExercise();
-        $res = $this->getItemBankAssessmentExerciseDao()->isAssessmentExercise($excepted['moduleId'],$excepted['assessmentId'], $excepted['exerciseId']);
-
-        $this->assertEquals($excepted['exerciseId'], $res['exerciseId']);
-        $this->assertEquals($excepted['moduleId'], $res['moduleId']);
-        $this->assertEquals($excepted['assessmentId'], $res['assessmentId']);
-    }
-
-    protected function createAssessmentExercise()
-    {
-        return $this->getItemBankAssessmentExerciseDao()->create(
-            [
-                'exerciseId' => 1,
-                'moduleId' => 1,
-                'assessmentId' => 1,
-            ]
-        );
-    }
-
-    /**
-     * @return AssessmentExerciseDao
-     */
-=======
 
 class AssessmentExerciseDaoTest extends BaseTestCase
 {
@@ -65,7 +27,29 @@ class AssessmentExerciseDaoTest extends BaseTestCase
         ]);
     }
 
->>>>>>> a6d69cf857e21cbdbdf3718ba5ca22c8759e27ee
+    public function testFindByExerciseIdAndModuleId()
+    {
+        $this->mockAssessmentExercise();
+        $res = $this->getItemBankAssessmentExerciseDao()->findByExerciseIdAndModuleId(1, 1);
+
+        $this->assertEquals(1, $res[0]['exerciseId']);
+        $this->assertEquals(1, $res[0]['moduleId']);
+        $this->assertEquals(1, $res[0]['assessmentId']);
+    }
+
+    public function testIsAssessmentExercise()
+    {
+        $this->mockAssessmentExercise();
+        $res = $this->getItemBankAssessmentExerciseDao()->isAssessmentExercise(1, 1, 1);
+
+        $this->assertEquals(1, $res['exerciseId']);
+        $this->assertEquals(1, $res['moduleId']);
+        $this->assertEquals(1, $res['assessmentId']);
+    }
+
+    /**
+     * @return AssessmentExerciseDao
+     */
     protected function getItemBankAssessmentExerciseDao()
     {
         return $this->biz->dao('ItemBankExercise:AssessmentExerciseDao');
