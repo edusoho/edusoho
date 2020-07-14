802af5dae3ed82cc76a2f9a139812c205880e266
shenchupeng <shenchupeng@howzhi.com>
2020-07-14 15:41:46 +0800

    feat: #62942 单词拼写错误修改

--- src/Biz/ItemBankExercise/Service/Impl/ExerciseMemberServiceImpl.php
+++ src/Biz/ItemBankExercise/Service/Impl/ExerciseMemberServiceImpl.php
@@ -95,7 +95,7 @@ class ExerciseMemberServiceImpl extends BaseService implements ExerciseMemberSer
         return empty($member) ? false : true;
     }
 
-    public function getByEerciseIdAndUserId($exerciseId, $userId)
+    public function getByExerciseIdAndUserId($exerciseId, $userId)
     {
         return $this->getExerciseMemberDao()->getByExerciseIdAndUserId($exerciseId, $userId);
     }
@@ -146,7 +146,7 @@ class ExerciseMemberServiceImpl extends BaseService implements ExerciseMemberSer
             return;
         }
 
-        $this->getExerciseMemberDao()->update($member['id'], ['locked' => 1]);
+        return $this->getExerciseMemberDao()->update($member['id'], ['locked' => 1]);
     }
 
     public function unlockStudent($exerciseId, $userId)
@@ -170,7 +170,7 @@ class ExerciseMemberServiceImpl extends BaseService implements ExerciseMemberSer
             return;
         }
 
-        $this->getExerciseMemberDao()->update($member['id'], ['locked' => 0]);
+        return $this->getExerciseMemberDao()->update($member['id'], ['locked' => 0]);
     }
 
     public function removeStudent($exerciseId, $userId, $reason = [])
@@ -181,7 +181,7 @@ class ExerciseMemberServiceImpl extends BaseService implements ExerciseMemberSer
             $this->createNewException(ItemBankExerciseException::NOTFOUND_EXERCISE());
         }
 
-        $member = $this->getByEerciseIdAndUserId($exerciseId, $userId);
+        $member = $this->getByExerciseIdAndUserId($exerciseId, $userId);
 
         if (empty($member) || ('student' != $member['role'])) {
             $this->createNewException(ItemBankExerciseException::NOTFOUND_MEMBER());
