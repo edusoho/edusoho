0b7761e710b3a15010a315343a75a21f9716866f
chenjinhong <chenjinhong@edusoho.com>
2020-07-15 17:43:20 +0800

    提交刷题目录

---
+++ h5/src/h5/containers/brush-exercise/directory/components/exercise.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
---
+++ h5/src/h5/containers/brush-exercise/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <directory />
+  </div>
+</template>
+
+<script>
+import directory from './directory';
+export default {
+  components: {
+    directory,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
