a446d8d5c024672bba73a9bc56fc8af55dd7d447
shenchupeng <shenchupeng@howzhi.com>
2020-07-15 14:36:00 +0800

    feat: #62944 题库列表

--- src/AppBundle/Controller/ItemBankExercise/ExploreController.php
+++ src/AppBundle/Controller/ItemBankExercise/ExploreController.php
@@ -14,7 +14,7 @@ class ExploreController extends BaseController
         $conditions = $request->query->all();
 
         list($conditions, $filter) = $this->getFilter($conditions);
-        $orderBy = $conditions['orderBy'];
+        $orderBy = empty($conditions['orderBy']) ? 'latest' : $conditions['orderBy'];
         unset($conditions['orderBy']);
         $conditions['status'] = 'published';
 
@@ -31,13 +31,61 @@ class ExploreController extends BaseController
             $paginator->getPerPageCount()
         );
 
+        if ('recommendedSeq' == $orderBy) {
+            $currentPage = $request->query->get('page') ? $request->query->get('page') : 1;
+            $exercises = $this->searchExerciseOrderByRecommend($conditions, $currentPage, $orderBy);
+        }
+
         return $this->render(
             'item-bank-exercise/explore/index.html.twig',
             [
                 'exercises' => $exercises,
                 'paginator' => $paginator,
+                'filter' => $filter,
             ]
         );
+    }
+
+    protected function searchExerciseOrderByRecommend($conditions, $currentPage, $orderBy)
+    {
+        $conditions['recommended'] = 1;
+        $recommendCount = $this->getExerciseService()->count($conditions);
+        $recommendPage = (int) ($recommendCount / 20);
+        $recommendLeft = $recommendCount % 20;
+
+        if ($currentPage <= $recommendPage) {
+            $exercises = $this->getExerciseService()->search(
+                $conditions,
+                $orderBy,
+                ($currentPage - 1) * 20,
+                20
+            );
+        } elseif (($recommendPage + 1) == $currentPage) {
+            $exercises = $this->getExerciseService()->search(
+                $conditions,
+                $orderBy,
+                ($currentPage - 1) * 20,
+                20
+            );
+            $conditions['recommended'] = 0;
+            $exercisesTemp = $this->getExerciseService()->search(
+                $conditions,
+                array('createdTime' => 'DESC'),
+                0,
+                20 - $recommendLeft
+            );
+            $exercises = array_merge($exercises, $exercisesTemp);
+        } else {
+            $conditions['recommended'] = 0;
+            $exercises = $this->getExerciseService()->search(
+                $conditions,
+                array('createdTime' => 'DESC'),
+                (20 - $recommendLeft) + ($currentPage - $recommendPage - 2) * 20,
+                20
+            );
+        }
+
+        return $exercises;
     }
 
     protected function getFilter($conditions)
