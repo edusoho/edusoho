1dd3cf369ca12fe76f339ce27a702cedaf64e12c
王才娟 <wangcaijuan@edusoho.com>
2020-07-16 17:12:59 +0800

    feat: #64386 猜你想学接口调整

--- src/ApiBundle/Api/Resource/Good/GoodFilter.php
+++ src/ApiBundle/Api/Resource/Good/GoodFilter.php
@@ -5,9 +5,15 @@ namespace ApiBundle\Api\Resource\Good;
 use ApiBundle\Api\Resource\Filter;
 use ApiBundle\Api\Resource\User\UserFilter;
 use ApiBundle\Api\Util\AssetHelper;
+use ApiBundle\Api\Util\Money;
 
 class GoodFilter extends Filter
 {
+    protected $simpleFields = [
+        'id', 'title', 'subtitle', 'summary', 'minPrice', 'maxPrice', 'images', 'ratingNum', 'rating', 'hitNum',
+        'hotSeq', 'showable', 'buyable', 'creator',
+    ];
+
     protected $publicFields = [
         'id', 'title', 'subtitle', 'product', 'extensions', 'specs', 'creator', 'status',
         'showable', 'buyable', 'summary', 'minPrice', 'maxPrice', 'images', 'orgId', 'orgCode', 'ratingNum', 'rating',
@@ -18,6 +24,17 @@ class GoodFilter extends Filter
         'course' => 'ApiBundle\Api\Resource\CourseSet\CourseSetFilter',
         'classroom' => 'ApiBundle\Api\Resource\Classroom\ClassroomFilter',
     ];
+
+    protected function simpleFields(&$data)
+    {
+        $userFilter = new UserFilter();
+        $userFilter->setMode(Filter::SIMPLE_MODE);
+        $userFilter->filter($data['creator']);
+
+        $data['summary'] = $this->convertAbsoluteUrl($data['summary']);
+        $this->transformImages($data['images']);
+        $this->transMinAndMaxPrice($data);
+    }
 
     protected function publicFields(&$data)
     {
@@ -38,6 +55,13 @@ class GoodFilter extends Filter
         $userFilter->filter($data['creator']);
 
         $this->transformImages($data['images']);
+        $this->transMinAndMaxPrice($data);
+    }
+
+    private function transMinAndMaxPrice(&$data)
+    {
+        $data['minPrice'] = Money::convert($data['minPrice']);
+        $data['maxPrice'] = Money::convert($data['maxPrice']);
     }
 
     private function transformImages(&$images)
