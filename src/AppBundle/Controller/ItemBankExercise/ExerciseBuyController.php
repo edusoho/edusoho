bce7fcce813f45e9feaf6506e29e597dd22575a7
shenchupeng <shenchupeng@howzhi.com>
2020-07-17 18:27:20 +0800

    feat: #62951 题库购买

--- src/AppBundle/Controller/ItemBankExercise/ExerciseBuyController.php
+++ src/AppBundle/Controller/ItemBankExercise/ExerciseBuyController.php
@@ -2,20 +2,9 @@
 
 namespace AppBundle\Controller\ItemBankExercise;
 
-use AppBundle\Common\ArrayToolkit;
-use AppBundle\Common\Paginator;
 use AppBundle\Controller\BuyFlowController;
-use Biz\Course\Service\MemberService;
-use Biz\ItemBankExercise\ItemBankExerciseException;
-use Biz\ItemBankExercise\Service\AssessmentExerciseService;
 use Biz\ItemBankExercise\Service\ExerciseMemberService;
-use Biz\ItemBankExercise\Service\ExerciseModuleService;
 use Biz\ItemBankExercise\Service\ExerciseService;
-use Biz\QuestionBank\Service\QuestionBankService;
-use Biz\Testpaper\TestpaperException;
-use Codeages\Biz\ItemBank\Assessment\Service\AssessmentService;
-use Codeages\Biz\ItemBank\Item\Service\ItemCategoryService;
-use Symfony\Component\HttpFoundation\Request;
 
 class ExerciseBuyController extends BuyFlowController
 {
@@ -23,7 +12,7 @@ class ExerciseBuyController extends BuyFlowController
 
     protected function getSuccessUrl($id)
     {
-        return $this->generateUrl('my_item_bank_exercise_show', array('id' => $id));
+        return $this->generateUrl('my_item_bank_exercise_show', ['id' => $id]);
     }
 
     protected function isJoined($id)
