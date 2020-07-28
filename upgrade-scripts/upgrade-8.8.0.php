269a6ab1f273ea0a79793fcddf1c6d144161b553
chelingfeng <chelingfeng@edusoho.com>
2020-07-28 16:15:11 +0800

    fix: 去掉索引脚本

--- upgrade-scripts/upgrade-8.8.0.php
+++ upgrade-scripts/upgrade-8.8.0.php
@@ -50,7 +50,6 @@ class EduSohoUpgrade extends AbstractUpdater
     private function updateScheme($index)
     {
         $definedFuncNames = array(
-            'addTableIndexJob',
             'addBizItemCategoryColumn',
             'updateBizItemCategory',
             'addBizItemBankColumn',
@@ -97,53 +96,6 @@ class EduSohoUpgrade extends AbstractUpdater
                 'progress' => 0,
             );
         }
-    }
-
-    public function addTableIndexJob()
-    {
-        if ($this->isJobExist('HandlingTimeConsumingUpdateStructuresJob')) {
-            return 1;
-        }
-
-        $currentTime = time();
-        $time = strtotime(date('Y-m-d', $currentTime) . '02:00:00');
-
-        if ($currentTime > $time) {
-            $time = strtotime(date('Y-m-d', strtotime('+1 day')) . '02:00:00');
-        }
-
-        $this->getConnection()->exec("INSERT INTO `biz_scheduler_job` (
-            `name`,
-            `expression`,
-            `class`,
-            `args`,
-            `priority`,
-            `pre_fire_time`,
-            `next_fire_time`,
-            `misfire_threshold`,
-            `misfire_policy`,
-            `enabled`,
-            `creator_id`,
-            `updated_time`,
-            `created_time`
-        ) VALUES (
-            'HandlingTimeConsumingUpdateStructuresJob',
-            '',
-            'Biz\\\\UpdateDatabaseStructure\\\\\Job\\\\HandlingTimeConsumingUpdateStructuresJob',
-            '',
-            '200',
-            '0',
-            '{$time}',
-            '300',
-            'executing',
-            '1',
-            '0',
-            '{$currentTime}',
-            '{$currentTime}'
-        )");
-        $this->logger('info', 'INSERT增加索引的定时任务HandlingTimeConsumingUpdateStructuresJob');
-
-        return 1;
     }
 
     public function addBizItemCategoryColumn()
