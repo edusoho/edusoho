f63bd75b12cc8ea3aaaa5d35e23e1f23a67fbcf6
zhangsimin <zhangsimin@edusoho.com>
2020-08-02 19:44:47 -0700

    feat: #64933 save-user-excel

---
+++ migrations/20200803103537_file_group_add_conlumn.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class FileGroupAddConlumn extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $db = $biz['db'];
+        $db->exec("INSERT INTO `file_group` (`id`, `name`, `code`, `public`) VALUES (NULL, '用户导入', 'user_import', '0');");
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
