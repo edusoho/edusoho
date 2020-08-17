6795cd7217ee1e797863c41b3faf1d665c6bb84e
zhongyunchang <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-08-17 09:21:28 +0800

    fix: #65321 修改会员免费学交互BUG

--- app/Resources/static-src/app/js/goods/show/components/buy-sku.vue
+++ app/Resources/static-src/app/js/goods/show/components/buy-sku.vue
@@ -8,7 +8,7 @@
         <span v-else-if="sku.buyable != 1" class="product-detail__unpublished">抱歉，该商品为限制商品，请联系客服</span>
         <span v-else>
             <span class="product-detail__disable_btn" v-if="(sku.vipLevelInfo && !sku.vipUser) || sku.vipLevelInfo && sku.vipUser && sku.vipLevelInfo.seq > sku.vipUser.level.seq" data-container=".product-detail__disable_btn" data-toggle="popover" data-placement="top" data-trigger="hover" data-html="true"
-			:data-content="!sku.vipUser ? `你还不是${sku.vipLevelInfo.name}，<a class='color-primary' href='/vip/upgrade' target='_blank'>升级会员</a>` : `你还不是${ sku.vipLevelInfo.name }，<a class='color-primary' href='/vip/buy' target='_blank'>购买会员</a>`">
+			:data-content="sku.vipUser ? `你还不是${sku.vipLevelInfo.name}，<a class='color-primary' href='/vip/upgrade' target='_blank'>升级会员</a>` : `你还不是${ sku.vipLevelInfo.name }，<a class='color-primary' href='/vip/buy' target='_blank'>购买会员</a>`">
                 <slot>会员免费学</slot>
             </span>
             <a v-if="!sku.isMember" :class="btnClass" href="javascript:;" @click="buySku">
