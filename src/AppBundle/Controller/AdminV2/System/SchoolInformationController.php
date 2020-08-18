76b3a8f12d15b30511bd4dae8240dda2867c6a10
yangshengsen <yangshengsen@edusoho.com>
2020-08-17 19:18:14 -0700

    feat: #60101 add-setting

--- src/AppBundle/Controller/AdminV2/System/SchoolInformationController.php
+++ src/AppBundle/Controller/AdminV2/System/SchoolInformationController.php
@@ -3,16 +3,17 @@
 namespace AppBundle\Controller\AdminV2\System;
 
 use AppBundle\Controller\AdminV2\BaseController;
-use Biz\Common\HTMLHelper;
 use Biz\System\Service\SettingService;
 use Symfony\Component\HttpFoundation\Request;
+use Biz\Common\HTMLHelper;
 
-class SchoolInformationController extends BaseController
+class
+SchoolInformationController extends BaseController
 {
     public function siteAction(Request $request)
     {
-        $site = $this->getSettingService()->get('site', []);
-        $default = [
+        $site = $this->getSettingService()->get('site', array());
+        $default = array(
             'name' => '',
             'slogan' => '',
             'url' => '',
@@ -27,12 +28,12 @@ class SchoolInformationController extends BaseController
             'closed_note' => '',
             'favicon' => '',
             'copyright' => '',
-        ];
+        );
         $site = array_merge($default, $site);
 
-        return $this->render('admin-v2/system/school-information.html.twig', [
+        return $this->render('admin-v2/system/school-information.html.twig', array(
             'site' => $site,
-        ]);
+        ));
     }
 
     public function saveSiteAction(Request $request)
@@ -45,23 +46,23 @@ class SchoolInformationController extends BaseController
         }
         $this->getSettingService()->set('site', $site);
 
-        return $this->createJsonResponse([
+        return $this->createJsonResponse(array(
             'message' => $this->trans('site.save.success'),
-        ]);
+        ));
     }
 
     public function licenseAction(Request $request)
     {
-        $license = $this->getSettingService()->get('license', []);
+        $license = $this->getSettingService()->get('license', array());
 
-        $default = [
+        $default = array(
             'license_name' => '',
             'license_picture' => '',
             'license_url' => '',
             'permits' => [
                 ['name' => '', 'record_number' => '', 'picture' => ''],
-            ],
-        ];
+            ]
+        );
         $license = array_merge($default, $license);
 
         return $this->render('admin-v2/system/certificates-setting.html.twig', [
@@ -73,7 +74,7 @@ class SchoolInformationController extends BaseController
     {
         $license = $request->request->all();
         foreach ($license['permits'] as $key => $permit) {
-            if ('' == $permit['name'] && '' == $permit['record_number'] && '' == $permit['picture'] && 0 != $key) {
+            if (empty($permit['name']) && empty($permit['record_number']) && empty($permit['picture']) && $key != 0 ) {
                 unset($license['permits'][$key]);
             }
         }
@@ -84,10 +85,11 @@ class SchoolInformationController extends BaseController
         }
         $this->getSettingService()->set('license', $license);
 
-        return $this->createJsonResponse([
+        return $this->createJsonResponse(array(
             'message' => $this->trans('site.save.success'),
-        ]);
+        ));
     }
+
 
     /**
      * @return SettingService
--- src/AppBundle/Controller/AdminV2/System/SettingController.php
+++ src/AppBundle/Controller/AdminV2/System/SettingController.php
@@ -2,10 +2,10 @@
 
 namespace AppBundle\Controller\AdminV2\System;
 
-use AppBundle\Common\Exception\FileToolkitException;
-use AppBundle\Common\FileToolkit;
 use AppBundle\Common\JsonToolkit;
 use AppBundle\Controller\AdminV2\BaseController;
+use AppBundle\Common\Exception\FileToolkitException;
+use AppBundle\Common\FileToolkit;
 use Biz\Content\Service\FileService;
 use Biz\System\Service\SettingService;
 use Biz\User\Service\AuthService;
@@ -15,25 +15,25 @@ class SettingController extends BaseController
 {
     public function securityAction(Request $request)
     {
-        $security = $this->getSettingService()->get('security', []);
-        $default = [
-            'safe_iframe_domains' => [],
-        ];
+        $security = $this->getSettingService()->get('security', array());
+        $default = array(
+            'safe_iframe_domains' => array(),
+        );
         $security = array_merge($default, $security);
 
         if ($request->isMethod('POST')) {
             $security = $request->request->all();
 
-            $security['safe_iframe_domains'] = trim(str_replace(["\r\n", "\n", "\r"], ' ', $security['safe_iframe_domains']));
+            $security['safe_iframe_domains'] = trim(str_replace(array("\r\n", "\n", "\r"), ' ', $security['safe_iframe_domains']));
             $security['safe_iframe_domains'] = array_filter(explode(' ', $security['safe_iframe_domains']));
 
             $this->getSettingService()->set('security', $security);
             $this->setFlashMessage('success', 'site.save.success');
         }
 
-        return $this->render('admin-v2/system/security/security.html.twig', [
+        return $this->render('admin-v2/system/security/security.html.twig', array(
             'security' => $security,
-        ]);
+        ));
     }
 
     public function ipBlacklistAction(Request $request)
@@ -43,16 +43,16 @@ class SettingController extends BaseController
         if ('POST' === $request->getMethod()) {
             $data = $request->request->all();
 
-            $purifiedBlackIps = trim(str_replace(["\r\n", "\n", "\r"], ' ', $data['blackListIps']));
+            $purifiedBlackIps = trim(str_replace(array("\r\n", "\n", "\r"), ' ', $data['blackListIps']));
             $purifiedWhiteIps = isset($data['whiteListIps']) ? $data['whiteListIps'] : null;
-            $purifiedWhiteIps = trim(str_replace(["\r\n", "\n", "\r"], ' ', $purifiedWhiteIps));
+            $purifiedWhiteIps = trim(str_replace(array("\r\n", "\n", "\r"), ' ', $purifiedWhiteIps));
 
             $logService = $this->getLogService();
 
             if (empty($purifiedBlackIps)) {
                 $settingService->delete('blacklist_ip');
 
-                $blackListIps['ips'] = [];
+                $blackListIps['ips'] = array();
             } else {
                 $blackListIps['ips'] = array_filter(explode(' ', $purifiedBlackIps));
                 $settingService->set('blacklist_ip', $blackListIps);
@@ -61,7 +61,7 @@ class SettingController extends BaseController
             if (empty($purifiedWhiteIps)) {
                 $settingService->delete('whitelist_ip');
 
-                $whiteListIps['ips'] = [];
+                $whiteListIps['ips'] = array();
             } else {
                 $whiteListIps['ips'] = array_filter(explode(' ', $purifiedWhiteIps));
                 $settingService->set('whitelist_ip', $whiteListIps);
@@ -70,107 +70,43 @@ class SettingController extends BaseController
             $this->setFlashMessage('success', 'site.save.success');
         }
 
-        $blackListIps = $settingService->get('blacklist_ip', []);
-        $whiteListIps = $settingService->get('whitelist_ip', []);
+        $blackListIps = $settingService->get('blacklist_ip', array());
+        $whiteListIps = $settingService->get('whitelist_ip', array());
 
         if (!empty($blackListIps)) {
             $default['ips'] = join("\n", $blackListIps['ips']);
             $blackListIps = array_merge($blackListIps, $default);
         } else {
-            $blackListIps = [];
+            $blackListIps = array();
         }
 
         if (!empty($whiteListIps)) {
             $default['ips'] = join("\n", $whiteListIps['ips']);
             $whiteListIps = array_merge($whiteListIps, $default);
         } else {
-            $whiteListIps = [];
+            $whiteListIps = array();
         }
 
-        return $this->render('admin-v2/system/security/ip-blacklist.html.twig', [
+        return $this->render('admin-v2/system/security/ip-blacklist.html.twig', array(
             'blackListIps' => $blackListIps,
             'whiteListIps' => $whiteListIps,
-        ]);
+        ));
     }
 
     public function postNumRulesAction(Request $request)
     {
         if ('POST' === $request->getMethod()) {
-            $setting = $request->request->get('setting', []);
+            $setting = $request->request->get('setting', array());
             $this->getSettingService()->set('post_num_rules', $setting);
             $this->setFlashMessage('success', 'site.save.success');
         }
 
-        $setting = $this->getSettingService()->get('post_num_rules', []);
+        $setting = $this->getSettingService()->get('post_num_rules', array());
         $setting = JsonToolkit::prettyPrint(json_encode($setting));
 
-        return $this->render('admin-v2/system/security/post-num-rules.html.twig', [
+        return $this->render('admin-v2/system/security/post-num-rules.html.twig', array(
             'setting' => $setting,
-        ]);
-    }
-
-    public function logoUploadAction(Request $request)
-    {
-        $fileId = $request->request->get('id');
-        $objectFile = $this->getFileService()->getFileObject($fileId);
-
-        if (!FileToolkit::isImageFile($objectFile)) {
-            $this->createNewException(FileToolkitException::NOT_IMAGE());
-        }
-
-        $file = $this->getFileService()->getFile($fileId);
-        $parsed = $this->getFileService()->parseFileUri($file['uri']);
-
-        $site = $this->getSettingService()->get('site', []);
-
-        $oldFileId = empty($site['logo_file_id']) ? null : $site['logo_file_id'];
-        $site['logo_file_id'] = $fileId;
-        $site['logo'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/".$parsed['path'];
-        $site['logo'] = ltrim($site['logo'], '/');
-
-        $this->getSettingService()->set('site', $site);
-
-        if ($oldFileId) {
-            $this->getFileService()->deleteFile($oldFileId);
-        }
-
-        $response = [
-            'path' => $site['logo'],
-            'url' => $this->container->get('assets.packages')->getUrl($site['logo']),
-        ];
-
-        return $this->createJsonResponse($response);
-    }
-
-    public function licensePictureUploadAction(Request $request)
-    {
-        $fileId = $request->request->get('id');
-        $objectFile = $this->getFileService()->getFileObject($fileId);
-
-        if (!FileToolkit::isImageFile($objectFile)) {
-            $this->createNewException(FileToolkitException::NOT_IMAGE());
-        }
-
-        $file = $this->getFileService()->getFile($fileId);
-        $parsed = $this->getFileService()->parseFileUri($file['uri']);
-
-        $license = $this->getSettingService()->get('license', []);
-
-        $oldFileId = empty($license['license_picture_file_id']) ? null : $license['license_picture_file_id'];
-        $license['license_picture_file_id'] = $fileId;
-        $license['license_picture'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/".$parsed['path'];
-        $license['license_picture'] = ltrim($license['license_picture'], '/');
-
-        if ($oldFileId) {
-            $this->getFileService()->deleteFile($oldFileId);
-        }
-
-        $response = [
-            'path' => $license['license_picture'],
-            'url' => $this->container->get('assets.packages')->getUrl($license['license_picture']),
-        ];
-
-        return $this->createJsonResponse($response);
+        ));
     }
 
     public function logoRemoveAction(Request $request)
@@ -188,6 +124,39 @@ class SettingController extends BaseController
         }
 
         return $this->createJsonResponse(true);
+    }
+
+    public function logoUploadAction(Request $request)
+    {
+        $fileId = $request->request->get('id');
+        $objectFile = $this->getFileService()->getFileObject($fileId);
+
+        if (!FileToolkit::isImageFile($objectFile)) {
+            $this->createNewException(FileToolkitException::NOT_IMAGE());
+        }
+
+        $file = $this->getFileService()->getFile($fileId);
+        $parsed = $this->getFileService()->parseFileUri($file['uri']);
+
+        $site = $this->getSettingService()->get('site', array());
+
+        $oldFileId = empty($site['logo_file_id']) ? null : $site['logo_file_id'];
+        $site['logo_file_id'] = $fileId;
+        $site['logo'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/" . $parsed['path'];
+        $site['logo'] = ltrim($site['logo'], '/');
+
+        $this->getSettingService()->set('site', $site);
+
+        if ($oldFileId) {
+            $this->getFileService()->deleteFile($oldFileId);
+        }
+
+        $response = array(
+            'path' => $site['logo'],
+            'url' => $this->container->get('assets.packages')->getUrl($site['logo']),
+        );
+
+        return $this->createJsonResponse($response);
     }
 
     public function licensePictureRemoveAction(Request $resquest)
@@ -219,11 +188,11 @@ class SettingController extends BaseController
         $file = $this->getFileService()->getFile($fileId);
         $parsed = $this->getFileService()->parseFileUri($file['uri']);
 
-        $permit = $this->getSettingService()->get('license', []);
+        $permit = $this->getSettingService()->get('license', array());
 
         $oldFileId = empty($permit['permit_picture_file_id']) ? null : $permit['permit_picture_file_id'];
         $permit['permit_picture_file_id'] = $fileId;
-        $permit['permit_picture'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/".$parsed['path'];
+        $permit['permit_picture'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/" . $parsed['path'];
         $permit['permit_picture'] = ltrim($permit['permit_picture'], '/');
 
         if ($oldFileId) {
@@ -267,11 +236,11 @@ class SettingController extends BaseController
         $file = $this->getFileService()->getFile($fileId);
         $parsed = $this->getFileService()->parseFileUri($file['uri']);
 
-        $site = $this->getSettingService()->get('site', []);
+        $site = $this->getSettingService()->get('site', array());
 
         $oldFileId = empty($site['favicon_file_id']) ? null : $site['favicon_file_id'];
         $site['favicon_file_id'] = $fileId;
-        $site['favicon'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/".$parsed['path'];
+        $site['favicon'] = "{$this->container->getParameter('topxia.upload.public_url_path')}/" . $parsed['path'];
         $site['favicon'] = ltrim($site['favicon'], '/');
 
         $this->getSettingService()->set('site', $site);
@@ -281,14 +250,14 @@ class SettingController extends BaseController
         }
 
         //浏览器图标覆盖默认图标
-        copy($this->getParameter('kernel.root_dir').'/../web/'.$site['favicon'], $this->getParameter('kernel.root_dir').'/../web/favicon.ico');
+        copy($this->getParameter('kernel.root_dir') . '/../web/' . $site['favicon'], $this->getParameter('kernel.root_dir') . '/../web/favicon.ico');
 
-        $this->getLogService()->info('system', 'update_settings', '更新浏览器图标', ['favicon' => $site['favicon']]);
+        $this->getLogService()->info('system', 'update_settings', '更新浏览器图标', array('favicon' => $site['favicon']));
 
-        $response = [
+        $response = array(
             'path' => $site['favicon'],
             'url' => $this->container->get('assets.packages')->getUrl($site['favicon']),
-        ];
+        );
 
         return $this->createJsonResponse($response);
     }
@@ -313,9 +282,9 @@ class SettingController extends BaseController
     public function adminSyncAction(Request $request)
     {
         $currentUser = $this->getUser();
-        $setting = $this->getSettingService()->get('user_partner', []);
+        $setting = $this->getSettingService()->get('user_partner', array());
 
-        if (empty($setting['mode']) || !in_array($setting['mode'], ['phpwind', 'discuz'])) {
+        if (empty($setting['mode']) || !in_array($setting['mode'], array('phpwind', 'discuz'))) {
             return $this->createMessageResponse('info', '未开启用户中心，不能同步管理员帐号！');
         }
 
@@ -349,10 +318,10 @@ class SettingController extends BaseController
         }
 
         response:
-        return $this->render('admin-v2/system/user-setting/admin-sync.html.twig', [
+        return $this->render('admin-v2/system/user-setting/admin-sync.html.twig', array(
             'mode' => $setting['mode'],
             'bind' => $bind,
-        ]);
+        ));
     }
 
     /**
