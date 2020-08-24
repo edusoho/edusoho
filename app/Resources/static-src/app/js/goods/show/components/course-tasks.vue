ec0fe04d5cdb7d734ec5dfcb411c626d1dcf83a4
zhongyunchang <zhongyunchang@zhongyunchangdeMacBook-Pro-2.local>
2020-08-24 10:17:56 +0800

    fix: #65403 课时免费学试看内容

--- app/Resources/static-src/app/js/goods/show/components/course-tasks.vue
+++ app/Resources/static-src/app/js/goods/show/components/course-tasks.vue
@@ -1,47 +1,51 @@
 <template>
     <div class="course-tasks-content">
-        <ul class="task-list goods-task-list">
-            <span v-for="item in courseItems">
-                <li class="task-item bg-gray-lighter js-task-chapter infinite-item" v-if="isChapter(item)">
-                    <i class="es-icon es-icon-menu left-menu"></i>
-                    <a href="javascript:" class="title gray-dark">{{ getChapterName(item) }}</a>
-                    <i class="right-menu es-icon es-icon-remove js-remove-icon"></i>
-                </li>
-                <li class="task-item color-gray bg-gray-lighter infinite-item" v-if="isUnit(item)">
-                    <span class="title">{{ getUnitName(item) }}</span>
-                </li>
-                <li class="task-item infinite-item {lessonContainerClass}" v-if="isLesson(item)">
-                    <span class="title">{{ getLessonName(item) }}</span>
-                    <span v-if="item.isItemDisplayedAsUnpublished">
-                      <span class="right-menu ">{{'course.catalogue.task_status.looking_forward'|trans}}</span>
-                    </span>
-                </li>
-                <li class="task-item task-content mouse-control infinite-item color-gray bg-gray-lighter" v-if="isTask(item)">
-                    <i v-if="item.isTaskLocked" class="{taskClass}" data-toggle="tooltip" data-trigger="hover"
-                       data-placement="top" title="'course.task.lock_tips'|trans">
-                    </i>
-                    <i v-if="item.isTaskLocked" class="hidden"></i>
-                    <span class="title" href="javascript:;" data-toggle="modal" style="margin-top:-6px">{{ getTaskName(item) }}</span>
-                    <span class="right-menu color-gray "></span>
-                    <span class="right-menu mouse-leave">
-                        <i :class="getMetaIcon(item)"></i>
-                    </span>
-<!--                    <span class="right-menu mouse-enter color-warning">{{'course.plan_task.locked.task_locked'|trans({'%taskName%':'{title}'})}}</span>-->
-                    <!--试看逻辑-->
-                </li>
-            </span>
-        </ul>
+        <div class="js-tasks-show goods-task-list"
+             :data-url="renderUrl"></div>
+<!--        <ul class="task-list goods-task-list">-->
+<!--            <span v-for="item in courseItems">-->
+<!--                <li class="task-item bg-gray-lighter js-task-chapter infinite-item" v-if="isChapter(item)">-->
+<!--                    <i class="es-icon es-icon-menu left-menu"></i>-->
+<!--                    <a href="javascript:" class="title gray-dark">{{ getChapterName(item) }}</a>-->
+<!--                    <i class="right-menu es-icon es-icon-remove js-remove-icon"></i>-->
+<!--                </li>-->
+<!--                <li class="task-item color-gray bg-gray-lighter infinite-item" v-if="isUnit(item)">-->
+<!--                    <span class="title">{{ getUnitName(item) }}</span>-->
+<!--                </li>-->
+<!--                <li class="task-item infinite-item {lessonContainerClass}" v-if="isLesson(item)">-->
+<!--                    <span class="title">{{ getLessonName(item) }}</span>-->
+<!--                    <span v-if="item.isItemDisplayedAsUnpublished">-->
+<!--                      <span class="right-menu ">{{'course.catalogue.task_status.looking_forward'|trans}}</span>-->
+<!--                    </span>-->
+<!--                </li>-->
+<!--                <li class="task-item task-content mouse-control infinite-item color-gray bg-gray-lighter" v-if="isTask(item)">-->
+<!--                    <i v-if="item.isTaskLocked" class="{taskClass}" data-toggle="tooltip" data-trigger="hover"-->
+<!--                       data-placement="top" title="'course.task.lock_tips'|trans">-->
+<!--                    </i>-->
+<!--                    <i v-if="item.isTaskLocked" class="hidden"></i>-->
+<!--                    <span class="title" href="javascript:;" data-toggle="modal" style="margin-top:-6px">{{ getTaskName(item) }}</span>-->
+<!--                    <span class="right-menu color-gray "></span>-->
+<!--                    <span class="right-menu mouse-leave">-->
+<!--                        <i :class="getMetaIcon(item)"></i>-->
+<!--                    </span>-->
+<!--&lt;!&ndash;                    <span class="right-menu mouse-enter color-warning">{{'course.plan_task.locked.task_locked'|trans({'%taskName%':'{title}'})}}</span>&ndash;&gt;-->
+<!--                    &lt;!&ndash;试看逻辑&ndash;&gt;-->
+<!--                </li>-->
+<!--            </span>-->
+<!--        </ul>-->
     </div>
 </template>
 
 <script>
     import axios from "axios";
+    import PagedCourseTask from 'app/js/courseset/show/paged-course-task-list';
 
     export default {
         data() {
             return {
                courseItems: [],
                hasOptional: false,
+               renderUrl: `/course/${this.sku.targetId}/task/list/render/default`,
             };
         },
         props: {
@@ -60,15 +64,20 @@
         },
         methods: {
             getTasksListInfo() {
-                axios.get(`/course/${this.sku.targetId}/tasks/list_data`, {
-                    headers: {'X-Requested-With': 'XMLHttpRequest'}
-                }).then((res) => {
-                    if (res.data.dataoptionalTaskCount > 0) {
-                        this.hasOptional = true;
-                    }
-                    this.courseItems = res.data.courseItems;
-                    console.log(this.courseItems);
-                });
+                // axios.get(`/course/${this.sku.targetId}/tasks/list_data`, {
+                //     headers: {'X-Requested-With': 'XMLHttpRequest'}
+                // }).then((res) => {
+                //     if (res.data.dataoptionalTaskCount > 0) {
+                //         this.hasOptional = true;
+                //     }
+                //     this.courseItems = res.data.courseItems;
+                //     console.log(this.courseItems);
+                // });
+                console.log(this.renderUrl);
+
+                this.renderUrl = `/course/${this.sku.targetId}/task/list/render/default`;
+                $('.js-tasks-show').data('url', this.renderUrl);
+                new PagedCourseTask();
             },
             isChapter(data) {
                 return 'chapter' === data.itemType;
