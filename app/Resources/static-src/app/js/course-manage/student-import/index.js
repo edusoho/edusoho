8bfccf342714aba660f07eca17cec81115b8a74f
ruei <wurui@edusoho.com>
2020-08-24 11:13:09 +0800

    refactor: 删除价格限制

--- app/Resources/static-src/app/js/course-manage/student-import/index.js
+++ app/Resources/static-src/app/js/course-manage/student-import/index.js
@@ -11,22 +11,6 @@ let rules = {
 
 let messages = {};
 
-if ($('#min-price').length) {
-  rules.price = Object.assign({
-    required: true,
-    min: parseFloat($('#min-price').data('price'))
-  }, rules.price);
-  messages.price = Object.assign({min: Translator.trans('course_manage.student_create.price_min_error_hint')}, messages.price);
-}
-
-if ($('#min-price').length) {
-  rules.price = Object.assign({
-    required: true,
-    min: parseFloat($('#min-price').data('price'))
-  }, rules.price);
-  messages.price = Object.assign({min: Translator.trans('course_manage.student_create.price_min_error_hint')}, messages.price);
-}
-
 new Importer({
   rules: rules,
   messages: messages
