926f2e7a994554ece903d8d2e421aa7414c88213
hongyongchun <hongyongchun@edusoho.com>
2020-08-26 20:06:52 +0800

    fix: #66077 商品名称显示全部

--- h5/src/h5/containers/goods/components/detail.vue
+++ h5/src/h5/containers/goods/components/detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="detail-info" v-if="goods.id">
-    <p class="detail-info__title text-overflow">{{ goods.title }}</p>
+    <p class="detail-info__title">{{ goods.title }}</p>
 
     <div class="detail-info__price">
       <div class="clearfix">
