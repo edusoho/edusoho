8a9cff2e5611c0a88e87c6b96b2a59f6774d03db
shenchupeng <shenchupeng@howzhi.com>
2020-08-28 17:33:55 +0800

    feat: #66144 去除提示

--- tests/Unit/Certificate/Strategy/ClassroomStrategyTest.php
+++ tests/Unit/Certificate/Strategy/ClassroomStrategyTest.php
@@ -6,7 +6,6 @@ use Biz\BaseTestCase;
 use Biz\Certificate\Dao\CertificateDao;
 use Biz\Certificate\Dao\TemplateDao;
 use Biz\Certificate\Strategy\Impl\ClassroomStrategy;
-use Biz\Certificate\Strategy\Impl\CourseStrategy;
 
 class ClassroomStrategyTest extends BaseTestCase
 {
