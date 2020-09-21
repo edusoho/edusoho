a472d300cc52550ce77e6f4b81f5fa2096ce60c7
zhangsimin <zhangsimin@edusoho.com>
2020-09-21 03:38:27 -0700

    feat: #67089 collect-list

--- src/Biz/InformationCollect/Service/EventService.php
+++ src/Biz/InformationCollect/Service/EventService.php
@@ -4,11 +4,9 @@ namespace Biz\InformationCollect\Service;
 
 interface EventService
 {
-<<<<<<< HEAD
     public function count($conditions);
 
     public function search($conditions, $orderBy, $start, $limit);
-=======
+
     public function getEventByActionAndLocation($action, array $location);
->>>>>>> ab44444462c5817572ced9bbd9c72e66f0f72aec
 }
