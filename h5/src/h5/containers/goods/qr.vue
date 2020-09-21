356e5a8f70ac98243c4d18b738059edb977bcfbf
hongyongchun <hongyongchun@edusoho.com>
2020-09-21 11:37:34 +0800

    feature: #200921 二维码页面重写

--- h5/src/h5/containers/goods/qr.vue
+++ h5/src/h5/containers/goods/qr.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="goods-qr">
-    <p class="goods-qr__want">
-      {{ this.$route.query.title }}
-    </p>
-    <div class="goods-qr__img">
-      <img :src="this.$route.query.imageUrl" alt="" />
+    <div class="goods-qr__header">
+      <div class="goods-qr__header-bg"></div>
     </div>
-    <p class="goods-qr__scan">
-      {{ this.$route.query.content }}
-    </p>
+    <div class="goods-qr__body">
+      <h3 class="goods-qr__body__title">{{ this.$route.query.title }}</h3>
+      <p class="goods-qr__body__subtitle">长按识别二维码联系我们</p>
+      <div class="goods-qr__body__img">
+        <img :src="this.$route.query.imageUrl" alt="" />
+        <span class="qr-border qr-border-tl"></span>
+        <span class="qr-border qr-border-tr"></span>
+        <span class="qr-border qr-border-bl"></span>
+        <span class="qr-border qr-border-br"></span>
+      </div>
+    </div>
   </div>
 </template>
