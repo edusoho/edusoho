6c10c07ffdd51e2e0663163fe428d1b541ab995a
shenchupeng <shenchupeng@howzhi.com>
2020-09-24 01:33:21 +0800

    feat: #200923 格式化

--- src/AppBundle/Command/FixTestpaperWithAiCommand.php
+++ src/AppBundle/Command/FixTestpaperWithAiCommand.php
@@ -2,12 +2,12 @@
 
 namespace AppBundle\Command;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Topxia\Service\Common\ServiceKernel;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 class FixTestpaperWithAiCommand extends BaseCommand
 {
@@ -30,27 +30,32 @@ class FixTestpaperWithAiCommand extends BaseCommand
         $user = $this->getUserService()->getUser($userId);
         if (empty($user)) {
             var_dump('用户不存在');
+
             return;
         }
 
         $record = $this->getAnswerRecordService()->get($recordId);
         if (empty($record)) {
             var_dump('答题记录不存在');
+
             return;
         }
 
         if ($user['id'] != $record['user_id']) {
             var_dump('--------用户答题记录不存在--------');
+
             return;
         }
 
-        if ($record['status'] != 'finished') {
+        if ('finished' != $record['status']) {
             var_dump('答题未完成');
+
             return;
         }
 
         if ($record['used_time'] > 0) {
             var_dump('已正确记录答题时间');
+
             return;
         }
 
@@ -58,7 +63,7 @@ class FixTestpaperWithAiCommand extends BaseCommand
             [
                 'user_id' => $userId,
                 'answer_scene_id' => $record['answer_scene_id'],
-                'assessment_id' => $record['assessment_id']
+                'assessment_id' => $record['assessment_id'],
             ],
             [],
             0,
@@ -67,24 +72,26 @@ class FixTestpaperWithAiCommand extends BaseCommand
 
         if (empty($report) || count($report) > 1) {
             var_dump('答题报告不存在或次数大于1');
+
             return;
         }
 
         $questionReports = $this->getAnswerQuestionReportService()->findByAnswerRecordId($recordId);
         if (empty($questionReports)) {
             var_dump('答题问题记录不存在');
+
             return;
         }
 
         $this->getLogger()->info('---------------- start ----------------');
-        $this->getLogger()->info('----------------'. $userId . ' and ' . $recordId .'----------------');
+        $this->getLogger()->info('----------------'.$userId.' and '.$recordId.'----------------');
         $this->getLogger()->info('---------------- record ----------------');
-        $this->getLogger()->info('record: ' . json_encode($record));
+        $this->getLogger()->info('record: '.json_encode($record));
         $this->getLogger()->info('---------------- report ----------------');
-        $this->getLogger()->info('report: ' . json_encode($report));
+        $this->getLogger()->info('report: '.json_encode($report));
         $this->getLogger()->info('---------------- question reports ----------------');
         foreach ($questionReports as $key => $questionReport) {
-            $this->getLogger()->info('question report '. $key . ' : ' . json_encode($questionReport));
+            $this->getLogger()->info('question report '.$key.' : '.json_encode($questionReport));
         }
 
         $this->getLogger()->info('---------------- end ----------------');
@@ -92,9 +99,9 @@ class FixTestpaperWithAiCommand extends BaseCommand
         try {
             $this->getBiz()['db']->beginTransaction();
 
-            $sql1 = 'DELETE FROM `biz_answer_record` WHERE id = ' . (int)$record['id'] . ' LIMIT 1';
-            $sql2 = 'DELETE FROM `biz_answer_report` WHERE answer_record_id = ' . (int)$record['id'] . ' LIMIT 1';
-            $sql3 = 'DELETE FROM `biz_answer_question_report` WHERE answer_record_id = ' . (int)$record['id'] . ' LIMIT ' . count($questionReports);
+            $sql1 = 'DELETE FROM `biz_answer_record` WHERE id = '.(int) $record['id'].' LIMIT 1';
+            $sql2 = 'DELETE FROM `biz_answer_report` WHERE answer_record_id = '.(int) $record['id'].' LIMIT 1';
+            $sql3 = 'DELETE FROM `biz_answer_question_report` WHERE answer_record_id = '.(int) $record['id'].' LIMIT '.count($questionReports);
 
             $this->getBiz()['db']->executeUpdate($sql1);
             $this->getBiz()['db']->executeUpdate($sql2);
@@ -102,6 +109,7 @@ class FixTestpaperWithAiCommand extends BaseCommand
 
             $this->getBiz()['db']->commit();
             var_dump('删除成功');
+
             return;
         } catch (\Exception $e) {
             var_dump('删除失败');
@@ -120,11 +128,10 @@ class FixTestpaperWithAiCommand extends BaseCommand
         $this->logger = new Logger($name);
 
         $biz = $this->getBiz();
-        $this->logger->pushHandler(new StreamHandler($biz['log_directory'] . '/testpaper-with-ai.log', Logger::DEBUG));
+        $this->logger->pushHandler(new StreamHandler($biz['log_directory'].'/testpaper-with-ai.log', Logger::DEBUG));
 
         return $this->logger;
     }
-
 
     protected function getUserService()
     {
