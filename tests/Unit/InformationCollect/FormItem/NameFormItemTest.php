65079d8917311d4485db4d272bd4fac7c0cbee89
chelingfeng <chelingfeng@edusoho.com>
2020-09-27 19:28:07 +0800

    fix: #1 姓名增加校验规则

--- tests/Unit/InformationCollect/FormItem/NameFormItemTest.php
+++ tests/Unit/InformationCollect/FormItem/NameFormItemTest.php
@@ -8,28 +8,30 @@ class NameFormItemTest extends BaseTestCase
 {
     public function testGetData()
     {
-        $data = json_decode('
-            {
-                "type": "input",
-                "title": "姓名",
-                "field": "name",
-                "value": "张三",
-                "validate": [
-                    {
-                        "required": true,
-                        "message": "姓名不能为空"
-                    },
-                    {
-                        "min": 2,
-                        "message": "最少输入2个字符"
-                    },
-                    {
-                        "max": 20,
-                        "message": "最多输入20个字符"
-                    }
-                ]
-            }
-        ', true);
+        $data = [
+            'type' => 'input',
+            'title' => '姓名',
+            'field' => 'name',
+            'value' => '张三',
+            'validate' => [
+                [
+                    'required' => true,
+                    'message' => '姓名不能为空',
+                ],
+                [
+                    'min' => 2,
+                    'message' => '最少输入2个字符',
+                ],
+                [
+                    'max' => 20,
+                    'message' => '最多输入20个字符',
+                ],
+                [
+                    'pattern' => "^[\u4E00-\u9FA5A-Za-z0-9 ]+$",
+                    'message' => '姓名格式错误',
+                ],
+            ],
+        ];
 
         $formItem = new \Biz\InformationCollect\FormItem\NameFormItem();
 
