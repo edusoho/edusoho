39dc8ff21d94f56f6392fbf1cce36c17f1c17524
zhongyunchang <zhongyunchang@yeah.net>
2020-09-27 14:56:21 +0800

    feat: #200923 修改单元测试

--- migrations/20200731151200_open_course_recommend_add_goods_column.php
+++ migrations/20200731151200_open_course_recommend_add_goods_column.php
@@ -10,7 +10,7 @@ class OpenCourseRecommendAddGoodsColumn extends Migration
     public function up()
     {
         $biz = $this->getContainer();
-        $biz['db']->exec("ALTER TABLE `open_course_recommend` ADD COLUMN `recommendGoodsId` int(10) NOT NULL COMMENT '推荐商品id' AFTER `recommendCourseId`;");
+        $biz['db']->exec("ALTER TABLE `open_course_recommend` ADD COLUMN `recommendGoodsId` int(10) NOT NULL DEFAULT '0' COMMENT '推荐商品id' AFTER `recommendCourseId`;");
     }
 
     /**
--- tests/Unit/OpenCourse/Dao/OpenCourseRecommendedDaoTest.php
+++ tests/Unit/OpenCourse/Dao/OpenCourseRecommendedDaoTest.php
@@ -4,42 +4,42 @@ namespace Tests\Unit\OpenCourse\Dao;
 
 use Tests\Unit\Base\BaseDaoTestCase;
 
-class RecommendedCourseDaoTest extends BaseDaoTestCase
+class OpenCourseRecommendedDaoTest extends BaseDaoTestCase
 {
     public function testSearch()
     {
-        $expected = array();
-        $expected[] = $this->mockDataObject(array('openCourseId' => 2));
-        $expected[] = $this->mockDataObject(array('recommendCourseId' => 2));
-        $expected[] = $this->mockDataObject(array('type' => 'b'));
-        $testCondition = array(
-            array(
-                'condition' => array(),
+        $expected = [];
+        $expected[] = $this->mockDataObject(['openCourseId' => 2]);
+        $expected[] = $this->mockDataObject(['recommendCourseId' => 2]);
+        $expected[] = $this->mockDataObject(['type' => 'b']);
+        $testCondition = [
+            [
+                'condition' => [],
                 'expectedResults' => $expected,
                 'expectedCount' => 3,
-                ),
-            array(
-                'condition' => array('openCourseId' => 2),
-                'expectedResults' => array($expected[0]),
+                ],
+            [
+                'condition' => ['openCourseId' => 2],
+                'expectedResults' => [$expected[0]],
                 'expectedCount' => 1,
-                ),
-            array(
-                'condition' => array('recommendCourseId' => 2),
-                'expectedResults' => array($expected[1]),
+                ],
+            [
+                'condition' => ['recommendCourseId' => 2],
+                'expectedResults' => [$expected[1]],
                 'expectedCount' => 1,
-                ),
-            array(
-                'condition' => array('type' => 'b'),
-                'expectedResults' => array($expected[2]),
+                ],
+            [
+                'condition' => ['type' => 'b'],
+                'expectedResults' => [$expected[2]],
                 'expectedCount' => 1,
-                ),
-            );
+                ],
+            ];
         $this->searchTestUtil($this->getDao(), $testCondition, $this->getCompareKeys());
     }
 
     public function testGetByCourseIdAndType()
     {
-        $expected = array();
+        $expected = [];
         $expected[0] = $this->mockDataObject();
         $res = $this->getDao()->getByCourseIdAndType(1, 1, 'a');
         $this->assertArrayEquals($expected[0], $res, $this->getCompareKeys());
@@ -47,7 +47,7 @@ class RecommendedCourseDaoTest extends BaseDaoTestCase
 
     public function testFindByOpenCourseId()
     {
-        $expected = array();
+        $expected = [];
         $expected[0] = $this->mockDataObject();
         $res = $this->getDao()->findByOpenCourseId(1);
         $this->assertEquals(1, count($res));
@@ -56,7 +56,7 @@ class RecommendedCourseDaoTest extends BaseDaoTestCase
 
     public function testDeleteByOpenCourseIdAndRecommendCourseId()
     {
-        $expected = array();
+        $expected = [];
         $expected[0] = $this->mockDataObject();
         $res = $this->getDao()->deleteByOpenCourseIdAndRecommendCourseId(1, 1);
         $this->assertEquals(1, $res);
@@ -64,7 +64,7 @@ class RecommendedCourseDaoTest extends BaseDaoTestCase
 
     public function testFindRandomRecommendCourses()
     {
-        $expected = array();
+        $expected = [];
         $expected[0] = $this->mockDataObject();
         $res = $this->getDao()->findRandomRecommendCourses(1, 2);
         $this->assertEquals(1, count($res));
@@ -73,10 +73,10 @@ class RecommendedCourseDaoTest extends BaseDaoTestCase
 
     protected function getDefaultMockFields()
     {
-        return array(
+        return [
             'openCourseId' => 1,
             'recommendCourseId' => 1,
             'type' => 'a',
-            );
+            ];
     }
 }
