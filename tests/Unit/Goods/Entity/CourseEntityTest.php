cceb101415fe79193736a6775eb519bbe2a8f70f
zhongyunchang <zhongyunchang@yeah.net>
2020-09-28 16:26:38 +0800

    feat: #200928 实体类CourseSet

--- tests/Unit/Goods/Entity/CourseEntityTest.php
+++ tests/Unit/Goods/Entity/CourseEntityTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Goods\Entity;
 use Biz\BaseTestCase;
 use Biz\Course\CourseSetException;
 use Biz\Course\Dao\CourseSetDao;
+use Biz\Course\Service\CourseService;
 use Biz\Course\Service\CourseSetService;
 use Biz\Goods\GoodsEntityFactory;
 use Biz\Goods\Service\GoodsService;
@@ -28,6 +29,50 @@ class CourseEntityTest extends BaseTestCase
 
         $this->getCourseSetDao()->delete($courseSet['id']);
         $this->getGoodsEntityFactory()->create('course')->getTarget($goods);
+    }
+
+    public function testHitTarget()
+    {
+        $courseSet = $this->getCourseSetService()->createCourseSet($this->mockCourseSet());
+        list($product, $goods) = $this->getProductAndGoods($courseSet);
+        $hitNum = $this->getGoodsEntityFactory()->create('course')->hitTarget($goods);
+        self::assertEquals(1, $hitNum);
+    }
+
+    public function testGetSpecsByTargetId()
+    {
+        $courseSet = $this->getCourseSetService()->createCourseSet($this->mockCourseSet());
+        $defaultCourse = $this->getCourseService()->getCourse($courseSet['defaultCourseId']);
+        $specs = $this->getGoodsEntityFactory()->create('course')->getSpecsByTargetId($defaultCourse['id']);
+        self::assertEquals($specs['targetId'], $defaultCourse['id']);
+    }
+
+    public function testGetTargets()
+    {
+        $courseSet = $this->getCourseSetService()->createCourseSet($this->mockCourseSet());
+        list($product, $goods) = $this->getProductAndGoods($courseSet);
+        $goodsLists = $this->getGoodsEntityFactory()->create('course')->fetchTargets([$goods]);
+        self::assertEquals($courseSet, reset($goodsLists)['courseSet']);
+    }
+
+    public function testGetTargets_whenEmpty()
+    {
+        $goodsLists = $this->getGoodsEntityFactory()->create('course')->fetchTargets([]);
+        self::assertEmpty($goodsLists);
+    }
+
+    public function testFetchSpecs()
+    {
+        $courseSet = $this->getCourseSetService()->createCourseSet($this->mockCourseSet());
+        $defaultCourse = $this->getCourseService()->getCourse($courseSet['defaultCourseId']);
+        $courses = $this->getGoodsEntityFactory()->create('course')->fetchSpecs([$defaultCourse]);
+        self::assertEquals($defaultCourse['id'], reset($courses)['spec']['targetId']);
+    }
+
+    public function testFetchSpecs_whenEmpty()
+    {
+        $courses = $this->getGoodsEntityFactory()->create('course')->fetchSpecs([]);
+        self::assertEmpty($courses);
     }
 
     private function getProductAndGoods($courseSet)
@@ -89,5 +134,13 @@ class CourseEntityTest extends BaseTestCase
     protected function getCourseSetDao()
     {
         return $this->createDao('Course:CourseSetDao');
+    }
+
+    /**
+     * @return CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->createService('Course:CourseService');
     }
 }
