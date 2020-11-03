68a29ab9e411a00bca96c293ad486b331b8d997e
zhangsimin <zhangsimin@edusoho.com>
2020-11-02 23:33:52 -0800

    fix: #68783 students reviews

--- src/AppBundle/Extensions/DataTag/LatestCourseReviewsDataTag.php
+++ src/AppBundle/Extensions/DataTag/LatestCourseReviewsDataTag.php
@@ -22,7 +22,7 @@ class LatestCourseReviewsDataTag extends CourseBaseDataTag implements DataTag
 
         $defaultConditions = [
             'parentId' => 0,
-            'targetType' => 'course',
+            'targetType' => 'goods',
         ];
 
         if (isset($conditions['courseId'])) {
