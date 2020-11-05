d83335c166ce8406e10b925cc8edf23f4ad560da
zsm0620 <2645839086@qq.com>
2020-11-05 14:21:45 +0800

    fix: #54912 classroom-title-xss-change

--- src/Biz/Classroom/Service/Impl/ClassroomServiceImpl.php
+++ src/Biz/Classroom/Service/Impl/ClassroomServiceImpl.php
@@ -258,6 +258,7 @@ class ClassroomServiceImpl extends BaseService implements ClassroomService
 
         $classroom = $this->fillOrgId($classroom);
         $userId = $this->getCurrentUser()->getId();
+        $classroom['title'] = $this->purifyHtml($classroom['title'], true);
         $classroom['creator'] = $userId;
         $classroom['teacherIds'] = [];
         $classroom['expiryMode'] = 'forever';
