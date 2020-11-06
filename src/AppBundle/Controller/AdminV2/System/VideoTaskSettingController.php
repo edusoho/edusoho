82309b1f80dc83586990d41a9bcc3ade54e4cede
shenchupeng <shenchupeng@howzhi.com>
2020-11-06 14:25:55 +0800

    feat: #68999 视频课时学习配置

--- src/AppBundle/Controller/AdminV2/System/VideoTaskSettingController.php
+++ src/AppBundle/Controller/AdminV2/System/VideoTaskSettingController.php
@@ -2,19 +2,22 @@
 
 namespace AppBundle\Controller\AdminV2\System;
 
-use AppBundle\Common\ArrayToolkit;
-use AppBundle\Component\OAuthClient\OAuthClientFactory;
 use AppBundle\Controller\AdminV2\BaseController;
 use Biz\System\Service\SettingService;
-use Biz\User\Service\UserFieldService;
-use Biz\User\UserFieldException;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class VideoTaskSettingController extends BaseController
 {
     public function videoPlaySettingAction(Request $request)
     {
 
+    }
+
+    /**
+     * @return SettingService
+     */
+    protected function getSettingService()
+    {
+        return $this->biz->service('System:SettingService');
     }
 }
