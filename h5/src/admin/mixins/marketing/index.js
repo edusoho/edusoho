870efe433d08e91048e83bd06d298f4b3302350f
hongyongchun <hongyongchun@edusoho.com>
2020-11-06 15:03:16 +0800

    fix: #68906 优化微营销创建活动地址

--- h5/src/admin/mixins/marketing/index.js
+++ h5/src/admin/mixins/marketing/index.js
@@ -1,8 +1,8 @@
+import { mapState } from 'vuex';
+
 // 微营销创建活动地址
 export default {
-  data() {
-    return {
-      createMarketingUrl: '/admin/login/marketing?target=activity_create',
-    };
+  computed: {
+    ...mapState(['createMarketingUrl']),
   },
 };
