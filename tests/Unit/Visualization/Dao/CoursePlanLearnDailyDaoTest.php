73bd2528befbd75cf2256d839fa8c7f546ea146b
shenchupeng <shenchupeng@howzhi.com>
2020-11-12 18:02:11 +0800

    feat: #69325 dao设计

--- tests/Unit/Visualization/Dao/CoursePlanLearnDailyDaoTest.php
+++ tests/Unit/Visualization/Dao/CoursePlanLearnDailyDaoTest.php
@@ -4,10 +4,6 @@ namespace Tests\Unit\Visualization\Dao;
 
 use Biz\BaseTestCase;
 use Biz\Visualization\Dao\CoursePlanLearnDailyDao;
-use Biz\Visualization\Dao\Impl\CoursePlanLearnDailyDaoImpl;
-use Biz\Visualization\Dao\Impl\UserLearnDailyDaoImpl;
-use Biz\Visualization\Dao\UserLearnDailyDao;
-use Biz\Visualization\Dao\UserStayDailyDao;
 
 class CoursePlanLearnDailyDaoTest extends BaseTestCase
 {
