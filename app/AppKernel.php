706d252084df1bb46989818c41515052201a42fd
xieyang <xieyang@howzhi.com>
2020-12-02 15:52:58 +0800

    fix: #20201202 修复测试环境的 notice 问题

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,7 +48,6 @@ class AppKernel extends Kernel implements PluginableHttpKernelInterface
                 $this->resetServices = false;
                 if ($this->debug) {
                     $this->startTime = microtime(true);
-                    error_reporting(3);
                 }
             }
 
@@ -56,6 +55,7 @@ class AppKernel extends Kernel implements PluginableHttpKernelInterface
         }
         if ($this->debug) {
             $this->startTime = microtime(true);
+            error_reporting(3);
         }
         if ($this->debug && !isset($_ENV['SHELL_VERBOSITY']) && !isset($_SERVER['SHELL_VERBOSITY'])) {
             putenv('SHELL_VERBOSITY=3');
