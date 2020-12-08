33d643639bd984051c5f957613f2121f29611457
zhongyunchang <zhongyunchang@yeah.net>
2020-12-08 17:28:29 +0800

    fix: #70537 修改升级脚本UK

--- tests/Unit/Activity/Listener/LiveActivityWatchListenerTest.php
+++ tests/Unit/Activity/Listener/LiveActivityWatchListenerTest.php
@@ -2,40 +2,15 @@
 
 namespace Tests\Unit\Activity\Listener;
 
-use Biz\BaseTestCase;
 use Biz\Activity\Listener\LiveActivityWatchListener;
+use Biz\BaseTestCase;
 
 class LiveActivityWatchListenerTest extends BaseTestCase
 {
     public function testHandle()
     {
         $listener = new LiveActivityWatchListener($this->getBiz());
-        $result = $listener->handle(array(), array('task' => array()));
+        $result = $listener->handle([], ['task' => []]);
         $this->assertNull($result);
-
-        $taskService = $this->mockBiz('Task:TaskService', array(
-            array(
-                'functionName' => 'getTimeSec',
-                'returnValue' => 60,
-            ),
-            array(
-                'functionName' => 'watchTask',
-                'returnValue' => array(),
-            ),
-        ));
-
-        $xpiService = $this->mockBiz('Xapi:XapiService', array(
-            array(
-                'functionName' => 'watchTask',
-                'returnValue' => array(),
-            ),
-        ));
-
-        $listener = new LiveActivityWatchListener($this->getBiz());
-        $result = $listener->handle(array(), array('task' => array('id' => 10), 'watchTime' => 10));
-
-        $taskService->shouldHaveReceived('getTimeSec')->times(1);
-        $taskService->shouldHaveReceived('watchTask')->times(1);
-        $xpiService->shouldHaveReceived('watchTask')->times(1);
     }
 }
