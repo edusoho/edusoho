a8c276468fd95a2714e171413c5a70755f0d25ad
zhongyunchang <zhongyunchang@yeah.net>
2020-12-18 09:59:17 +0800

    fix: #71086 兼容保存观看时长同时触发停留时长，兼容安卓端上报数据异常为0 的状况

--- src/Biz/Task/Event/ActivitySubscriber.php
+++ src/Biz/Task/Event/ActivitySubscriber.php
@@ -55,17 +55,7 @@ class ActivitySubscriber extends EventSubscriber implements EventSubscriberInter
         if (empty($task)) {
             return;
         }
-//        //如果有duration，不要按照lastTime计算出来的时间作为学习时长
-//        if ($event->hasArgument('duration')) {
-//            $this->getTaskService()->doTask($task['id'], $event->getArgument('duration'));
-//        } elseif ($time > 0) {
-//            $this->getTaskService()->doTask($task['id'], $time);
-//        }
         $this->updateLastLearnTime($task);
-//
-//        if ($this->getTaskService()->isFinished($task['id'])) {
-//            $this->getTaskService()->finishTaskResult($task['id']);
-//        }
     }
 
     /**
