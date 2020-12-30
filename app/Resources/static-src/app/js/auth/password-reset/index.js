eb7ff9077f18eefad75138442a57c1c34d3c89c2
yangshengsen <yangshengsen@edusoho.com>
2020-12-30 16:19:19 +0800

    fix: #56014 mail

--- app/Resources/static-src/app/js/auth/password-reset/index.js
+++ app/Resources/static-src/app/js/auth/password-reset/index.js
@@ -10,10 +10,11 @@ class Reset {
     this.event();
     this.dragHtml = $('.js-drag-box').html();
     $('.js-drag-box').remove();
-    if ($('#password-reset-form').css('display')){
+    if ($('#password-reset-form').css('display') == 'none'){
       $('#password-reset-by-mobile-form').prepend(this.dragHtml);
+    } else {
+      $('#password-reset-form').prepend(this.dragHtml);
     }
-    $('#password-reset-form').prepend(this.dragHtml);
     this.drag = new Drag($('#drag-btn'), $('.js-jigsaw'), {
       limitType: 'reset_password',
     });
