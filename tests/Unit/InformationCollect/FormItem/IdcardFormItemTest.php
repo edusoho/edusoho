73ddff3a7d0bacaa2a929a48f90f560281bb63ef
zhangsimin <zhangsimin@edusoho.com>
2020-12-29 22:58:13 -0800

    fix: #69543 fix

--- tests/Unit/InformationCollect/FormItem/IdcardFormItemTest.php
+++ tests/Unit/InformationCollect/FormItem/IdcardFormItemTest.php
@@ -9,25 +9,25 @@ class IdcardFormItemTest extends BaseTestCase
     public function testGetData()
     {
         $data = [
-                "type" => "input",
-                "title" => "身份证号",
-                "field" => "idcard",
-                "value" => "330621199406213333",
-                "group" => "base",
-                "required" => true,
-                "props" => [
-                    "placeholder" => "仅支持中国大陆身份证号"
+                'type' => 'input',
+                'title' => '身份证号',
+                'field' => 'idcard',
+                'value' => '330621199406213333',
+                'group' => 'base',
+                'required' => true,
+                'props' => [
+                    'placeholder' => '仅支持中国大陆身份证号',
                 ],
-                "validate" => [
+                'validate' => [
                     [
-                        "required" => true,
-                        "message" => "身份证号不能为空"
+                        'required' => true,
+                        'message' => '身份证号不能为空',
                     ],
                     [
-                        "pattern" => "^[1-9]\d{5}(19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$",
-                        "message" => "身份证号格式错误"
-                    ]
-                ]
+                        'pattern' => "^[1-9]\d{5}(19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$",
+                        'message' => '身份证号格式错误',
+                    ],
+                ],
             ];
 
         $formItem = new \Biz\InformationCollect\FormItem\IdcardFormItem();
