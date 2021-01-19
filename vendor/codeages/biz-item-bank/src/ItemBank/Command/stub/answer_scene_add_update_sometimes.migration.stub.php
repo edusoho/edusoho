af8708a64324fd7608e50f2ed2fe5f6bfc702a2f
zhongyunchang <zhongyunchang@yeah.net>
2021-01-19 15:29:39 +0800

    fix: #72388 优化vendor中的migrations 内容 以及优化字段

--- vendor/codeages/biz-item-bank/src/ItemBank/Command/stub/answer_scene_add_update_sometimes.migration.stub.php
+++ vendor/codeages/biz-item-bank/src/ItemBank/Command/stub/answer_scene_add_update_sometimes.migration.stub.php
@@ -10,7 +10,7 @@ class BizAnswerSceneAddUpdateSometimes extends Migration
     public function up()
     {
         $biz = $this->getContainer();
-        $biz['db']->exec("ALTER TABLE `biz_answer_scene` ADD COLUMN `last_review_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '考题最后提交时间' AFTER `updated_user_id`;");
+        $biz['db']->exec("ALTER TABLE `biz_answer_scene` ADD COLUMN `last_review_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '考题最后批阅沟通时间' AFTER `updated_user_id`;");
         $biz['db']->exec("ALTER TABLE `biz_answer_scene` ADD COLUMN `question_report_update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '场次题目分析最后生成时间' AFTER `updated_user_id`;");
         $biz['db']->exec("ALTER TABLE `biz_answer_scene` ADD COLUMN `question_report_job_name` varchar(256) DEFAULT '' COMMENT '场次分析JOB Name';");
     }
