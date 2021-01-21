2cf103eb06bbdc465bb35235728bbb1564f5f11d
zhongyunchang <zhongyunchang@yeah.net>
2021-01-21 10:03:26 +0800

    fix: #72388 js update 解决自动播放问题

--- app/Resources/static-src/app/js/task/task.js
+++ app/Resources/static-src/app/js/task/task.js
@@ -71,10 +71,10 @@ export default class TaskShow extends Emitter {
         $('input[name="task-result-status"]', $('#js-hidden-data')).val('finish');
       });
     }
-    if (nextTaskUrl && response.playerMsg && response.playerMsg.mode == 'sequence') {
-      let playerCurrentTime = response.currentTime||0;
-      let playerDuration = response.duration||0;
 
+    if (nextTaskUrl && response.playerMsg && response.playerMsg.mode == 'sequence') {
+      let playerCurrentTime = response.playerMsg.currentTime||0;
+      let playerDuration = response.playerMsg.duration||0;
       //player.ended 事件不一定是播放到最后一秒，所以必须判断是否播放轴到了最后
       if (playerCurrentTime !== 0 && playerDuration !== 0 && (playerDuration - playerCurrentTime < 2)) {
         window.location.href = nextTaskUrl;
