694c0fe2d23bfadc8f0d82da0f0177fbd2207702
yangshengsen <yangshengsen@edusoho.com>
2021-01-21 15:22:20 +0800

    feat: #72610 gongan

--- src/AppBundle/Controller/AdminV2/System/SettingController.php
+++ src/AppBundle/Controller/AdminV2/System/SettingController.php
@@ -249,15 +249,11 @@ class SettingController extends BaseController
     public function recordPictureUploadAction(Request $request)
     {
         $fileId = $request->request->get('id');
-
-        $site = $this->getSettingService()->get('site', []);
-        $site['recordPicture'] = $this->replaceFile('recordPicture', $fileId);
-
-        $this->getSettingService()->set('site', $site);
+        $path = $this->replaceFile('recordPicture', $fileId);
 
         $response = [
-            'path' => $site['recordPicture'],
-            'url' => $this->container->get('assets.packages')->getUrl($site['recordPicture']),
+            'path' => $path,
+            'url' => $this->container->get('assets.packages')->getUrl($path),
         ];
 
         return $this->createJsonResponse($response);
