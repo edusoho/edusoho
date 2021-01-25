b4ad666472ab0f045f8ebd2865852f64d97d505a
王才娟 <wangcaijuan@edusoho.com>
2021-01-25 17:22:23 +0800

    feat: #210125 phpunit

---
+++ tests/Unit/Visualization/Job/RefreshCoursePlanLearnDailyJobTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Visualization\Job;
+
+use Biz\BaseTestCase;
+use Biz\Visualization\Job\RefreshCoursePlanLearnDailyJob;
+use Codeages\Biz\Framework\Scheduler\Service\SchedulerService;
+
+class RefreshCoursePlanLearnDailyJobTest extends BaseTestCase
+{
+    public function testExecuteWithSettingByPlaying()
+    {
+        $mockedCacheService = $this->mockBiz('System:CacheService', [
+            ['functionName' => 'clear', 'withParams' => ['refresh_learn_daily']],
+        ]);
+        $mockedSettingService = $this->mockBiz('System:SettingService', [
+            ['functionName' => 'get', 'withParams' => ['videoEffectiveTimeStatistics'], 'returnValue' => ['statistical_dimension' => 'playing']],
+        ]);
+
+        $mockedJobDao = $this->mockBiz('Visualization:CoursePlanLearnDailyDao', [
+            ['functionName' => 'batchUpdate'],
+        ]);
+        $before = $this->getSchedulerService()->countJobs(['name' => 'refresh']);
+
+        $job = new RefreshCoursePlanLearnDailyJob([], $this->biz);
+        $job->execute();
+
+        $mockedCacheService->shouldHaveReceived('clear')->times(1);
+    }
+
+    /**
+     * @return SchedulerService
+     */
+    protected function getSchedulerService()
+    {
+        return $this->biz->service('Scheduler:SchedulerService');
+    }
+}
