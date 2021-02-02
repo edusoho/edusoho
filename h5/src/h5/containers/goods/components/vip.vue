0123784bf177284ff914c7d692257ff562f92cb5
hongyongchun <hongyongchun@edusoho.com>
2021-02-02 16:56:38 +0800

    feat: #72901 vip 入口文案优化

--- h5/src/h5/containers/goods/components/vip.vue
+++ h5/src/h5/containers/goods/components/vip.vue
@@ -2,9 +2,9 @@
   <div class="goods-vip" @click="clickGoToVip">
     <div class="goods-vip__left">
       <img class="vip-icon" :src="currentSku.vipLevelInfo.icon" alt="" />
-      {{ currentSku.vipLevelInfo.name }}免费学
+      加入{{ currentSku.vipLevelInfo.name }}，免费学习此门课程
     </div>
-    <i class="van-icon van-icon-arrow pull-right" />
+    <i class="van-icon van-icon-arrow" />
   </div>
 </template>
 
