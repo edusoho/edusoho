6889ac97518039554208965309729b503d273adc
hongyongchun <hongyongchun@edusoho.com>
2021-02-02 15:13:52 +0800

    feat: #72901 结构优化

--- h5/src/h5/containers/goods/components/detail.vue
+++ h5/src/h5/containers/goods/components/detail.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="detail-info" v-if="goods.id">
     <p class="detail-info__title" :class="isShare && 'detail-info__title--pr'">
-      <span class="certificate-icon" v-if="currentSku.hasCertificate">证</span
-      >{{ goods.title }}
+      <span class="certificate-icon" v-if="currentSku.hasCertificate">证</span>
+      {{ goods.title }}
       <i
+        v-if="isShare"
         class="iconfont icon-fenxiang goods-share"
         @click="onShare"
-        v-if="isShare"
       ></i>
     </p>
 
@@ -20,15 +20,17 @@
           <span
             v-if="currentSku.displayPriceObj.currency === 'RMB'"
             class="price"
-            >{{ currentSku.displayPriceObj.amount | formatPrice }}元
+          >
+            {{ currentSku.displayPriceObj.amount | formatPrice }}元
           </span>
           <span
             v-if="currentSku.displayPriceObj.currency === 'coin'"
             class="price"
-            >{{ currentSku.displayPriceObj.coinAmount | formatPrice
-            }}<span class="detail-right__price__unit">{{
-              currentSku.displayPriceObj.coinName
-            }}</span>
+          >
+            {{ currentSku.displayPriceObj.coinAmount | formatPrice }}
+            <span class="detail-right__price__unit">
+              {{ currentSku.displayPriceObj.coinName }}
+            </span>
           </span>
         </div>
         <div class="pull-right study-num">
@@ -37,6 +39,7 @@
         </div>
       </div>
     </div>
+
     <div
       v-if="!goods.discount || currentSku.displayPrice == 0"
       class="detail-info__price"
@@ -47,16 +50,18 @@
           <span
             v-if="currentSku.displayPriceObj.currency === 'RMB'"
             class="price"
-            >{{ currentSku.displayPriceObj.amount | formatPrice }}元
+          >
+            {{ currentSku.displayPriceObj.amount | formatPrice }}元
           </span>
           <span
             v-if="currentSku.displayPriceObj.currency === 'coin'"
             class="price"
-            >{{ currentSku.displayPriceObj.coinAmount | formatPrice
-            }}<span class="detail-right__price__unit">{{
-              currentSku.displayPriceObj.coinName
-            }}</span></span
           >
+            {{ currentSku.displayPriceObj.coinAmount | formatPrice }}
+            <span class="detail-right__price__unit">
+              {{ currentSku.displayPriceObj.coinName }}
+            </span>
+          </span>
         </div>
         <div
           v-if="goodsSetting.show_number_data === 'join'"
