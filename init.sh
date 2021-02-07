059f2069a9df78a20175456f45464866c1df0e52
andypau <andy.pau@qq.com>
2021-02-07 13:36:12 +0800

    update init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#git clean -fdx
+cp -n app/config/parameters.yml.dist app/config/parameters.yml
+rm -rf app/cache app/logs app/data web/files node_modules
+mkdir -p app/cache app/logs app/data web/files
+chmod 777 app/cache app/logs app/data web/files
+
+php bin/phpmig migrate
+php app/console system:init
+php app/console assets:install web --symlink --relative
+
+touch .webpack-watch.log
+yarn
