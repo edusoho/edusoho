2feb5cc19c7a6e32689dd3c41f93531aec433d57
andypau <andy.pau@qq.com>
2021-02-08 15:26:24 +0800

    add database init

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 #git clean -fdx
+mysql -uroot -e 'DROP DATABASE `edusoho`'                                                                      ─╯
+mysql -uroot -e 'CREATE DATABASE `edusoho` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci'
+
 cp -n app/config/parameters.yml.dist app/config/parameters.yml
 rm -rf app/cache app/logs app/data web/files node_modules
 mkdir -p app/cache app/logs app/data web/files
