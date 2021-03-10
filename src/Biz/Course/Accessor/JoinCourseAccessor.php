5ba6b4638185cb3d3a9b3780319e3d3d24566ca7
shenchupeng <shenchupeng@howzhi.com>
2021-03-10 17:49:46 +0800

    feat: #73675 fix

--- src/Biz/Course/Accessor/JoinCourseAccessor.php
+++ src/Biz/Course/Accessor/JoinCourseAccessor.php
@@ -15,26 +15,26 @@ class JoinCourseAccessor extends AccessorAdapter
 
         $courseSet = $this->getCourseSetService()->getCourseSet($course['courseSetId']);
         if ('draft' === $course['status'] || 'draft' == $courseSet['status']) {
-            return $this->buildResult('course.unpublished', array('courseId' => $course['id']));
+            return $this->buildResult('course.unpublished', ['courseId' => $course['id']]);
         }
 
         if ('closed' === $course['status'] || 'closed' == $courseSet['status']) {
-            return $this->buildResult('course.closed', array('courseId' => $course['id']));
+            return $this->buildResult('course.closed', ['courseId' => $course['id']]);
         }
 
         if (!$course['buyable']) {
-            return $this->buildResult('course.not_buyable', array('courseId' => $course['id']));
+            return $this->buildResult('course.not_buyable', ['courseId' => $course['id']]);
         }
 
         if ($this->isExpired($course)) {
-            return $this->buildResult('course.expired', array('courseId' => $course['id']));
+            return $this->buildResult('course.expired', ['courseId' => $course['id']]);
         }
         if ($course['buyExpiryTime'] && time() > $course['buyExpiryTime']) {
-            return $this->buildResult('course.buy_expired', array('courseId' => $course['id']));
+            return $this->buildResult('course.buy_expired', ['courseId' => $course['id']]);
         }
 
         if ($course['maxStudentNum'] && $course['maxStudentNum'] <= $course['studentNum']) {
-            return $this->buildResult('course.reach_max_student_num', array('courseId' => $course['id']));
+            return $this->buildResult('course.reach_max_student_num', ['courseId' => $course['id']]);
         }
 
         return null;
