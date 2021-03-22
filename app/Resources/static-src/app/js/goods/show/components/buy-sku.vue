6c1bd2a10bed093c8a10738d9fcb05e5c7b6e00e
shenchupeng <shenchupeng@howzhi.com>
2021-03-22 16:09:15 +0800

    feat: #73675 会员中心页跳转

--- app/Resources/static-src/app/js/goods/show/components/buy-sku.vue
+++ app/Resources/static-src/app/js/goods/show/components/buy-sku.vue
@@ -53,7 +53,7 @@
                 buyViewText : '',
                 needBuyVip : 0,
                 needBuyVipText: '',
-                needBuyVipUrl: "/vip",
+                needBuyVipUrl: "",
             }
         },
         props: {
@@ -153,8 +153,10 @@
                         this.buyViewText = sku.displayPrice == 0 ? Translator.trans('goods.show_page.free_join_btn') : Translator.trans('goods.show_page.buy_btn');
                         if (!sku.vipUser || (sku.vipUser && parseInt(sku.vipUser.deadline) * 1000 < new Date().getTime())){
                             this.needBuyVipText = Translator.trans('goods.show_page.vip_buy',{vipName:sku.vipLevelInfo.name});
+                            this.needBuyVipUrl = "/vip?levelId=" + sku.vipLevelInfo.id;
                         }else if (sku.vipUser){
                             this.needBuyVipText = Translator.trans('goods.show_page.vip_upgrade',{vipName:sku.vipLevelInfo.name});
+                            this.needBuyVipUrl = "/vip?levelId=" + sku.vipLevelInfo.id;
                         }
                     }
                 } else if (sku.buyable != 1) {  // 已发布，但是未开放购买
