70221f91c3aed43681a62f1363cca2808f6a8927
zhongyunchang <zhongyunchang@yeah.net>
2021-03-24 14:24:39 +0800

    feat: #74401 核心JS改动

---
+++ app/Resources/static-src/app/js/admin-v2/system/user-content-control/note/index.js
@@ -0,0 +1,15 @@
+let $saveBtn = $('.js-note-setting-save-btn');
+let validator = $('#user_content_control_note').validate({
+  rules: {},
+  ajax: true,
+  submitSuccess(data) {
+    cd.message({type: 'success', message: Translator.trans('site.save_success_hint')});
+    $saveBtn.button('reset');
+  }
+});
+
+$saveBtn.on('click', (event) => {
+  const $this = $(event.currentTarget);
+  $this.button('loading');
+  $('#user_content_control_note').submit();
+});
