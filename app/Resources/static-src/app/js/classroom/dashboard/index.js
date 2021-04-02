aad6942fe44293fd9079a2d9f3c972102eb4e390
sunruonan <sunruonan@edusoho.com>
2021-04-02 10:01:42 +0800

    feat: #73837 代码规范

--- app/Resources/static-src/app/js/classroom/dashboard/index.js
+++ app/Resources/static-src/app/js/classroom/dashboard/index.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(window).resize();
 });
-$(window).resize(function(){
-  isNavTabsScroll()
+$(window).resize(function () {
+  isNavTabsScroll();
 });
 
 function isNavTabsScroll () {
-  const $navTabs = $('.nav-tabs')
-  const width = Math.ceil($navTabs.width())
+  const $navTabs = $('.nav-tabs');
+  const width = Math.ceil($navTabs.width());
   if (width < $navTabs[0].scrollWidth) {
-    $navTabs.addClass('nav-tabs-scroll') 
+    $navTabs.addClass('nav-tabs-scroll') ;
     $('.nav-tabs-scroll').perfectScrollbar();
     $('.nav-tabs-scroll').perfectScrollbar('update');
   } else {
-    $navTabs.removeClass('nav-tabs-scroll')
+    $navTabs.removeClass('nav-tabs-scroll');
   }
 }
  
