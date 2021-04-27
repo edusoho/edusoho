9535d68ac33fa2d867e03b01ca026e4c9d4a74b2
hongyongchun <hongyongchun@edusoho.com>
2021-04-27 15:41:11 +0800

    feat: #75610 color

--- app/Resources/static-src/app/js/wechat-subscribe-new/index.js
+++ app/Resources/static-src/app/js/wechat-subscribe-new/index.js
@@ -5,11 +5,13 @@ let $jweixin = $('.js-wechat-data');
 
 wx.config($jweixin.data('config'));
 
+notify('success', '订阅成功-hongbusi');
+
 wx.ready(function() {
   var btn = document.getElementById('subscribe-btn');
   btn.addEventListener('success', function (e) {
-    console.log('success', e.detail);
-    notify('danger', '订阅成功');
+    console.log('Hongbusi-success', e.detail);
+    notify('success', '订阅成功');
   });   
   btn.addEventListener('error',function (e) {
     console.log('fail', e.detail);
