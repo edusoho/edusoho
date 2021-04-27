11c7a4a11fb95099e34a1472cee1c81afa71ac8e
hongyongchun <hongyongchun@edusoho.com>
2021-04-27 18:53:22 +0800

    feat: #75610 正则

--- app/Resources/static-src/app/js/wechat-subscribe-new/index.js
+++ app/Resources/static-src/app/js/wechat-subscribe-new/index.js
@@ -9,11 +9,9 @@ wx.ready(function() {
   var btn = document.getElementById('subscribe-btn');
   btn.addEventListener('success', function (e) {
     const subscribeDetails = e.detail.subscribeDetails;
-    if (subscribeDetails.indexof('accept') !== -1) {
+    if (subscribeDetails.test(/accept/)) {
       notify('success', '订阅成功indexof');
     }
-    console.log('Hongbusi-success', subscribeDetails);
-    console.log('Hongbusi-success', subscribeDetails.indexof('accept'));
   });   
   btn.addEventListener('error',function (e) {
     console.log('fail', e.detail);
