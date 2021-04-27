aef825d9e7eb9d2b5646da79857c54fd38664f1b
hongyongchun <hongyongchun@edusoho.com>
2021-04-27 15:18:37 +0800

    feat: #75610 wechatconfig

--- app/Resources/static-src/app/js/wechat-subscribe-new/index.js
+++ app/Resources/static-src/app/js/wechat-subscribe-new/index.js
@@ -1,4 +1,5 @@
 import wx from 'weixin-js-sdk';
+import notify from 'common/notify';
 
 let $jweixin = $('.js-wechat-data');
 
@@ -6,10 +7,11 @@ wx.config($jweixin.data('config'));
 
 wx.ready(function() {
   var btn = document.getElementById('subscribe-btn');
-  btn.addEventListener('success', function (e) {            
+  btn.addEventListener('success', function (e) {
     console.log('success', e.detail);
+    notify('danger', '订阅成功');
   });   
-  btn.addEventListener('error',function (e) {             
+  btn.addEventListener('error',function (e) {
     console.log('fail', e.detail);
   });
 });
