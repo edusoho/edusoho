16c8238fa3d364e775ec431d07e84c049fef982b
hongyongchun <hongyongchun@edusoho.com>
2021-04-28 20:42:35 +0800

    feat: #75610 判断是不是微信

--- app/Resources/static-src/app/js/wechat-subscribe-new/index.js
+++ app/Resources/static-src/app/js/wechat-subscribe-new/index.js
@@ -3,12 +3,17 @@ import notify from 'common/notify';
 
 let $jweixin = $('.js-wechat-data');
 
+// 判断是不是微信环境
+const wx = navigator.userAgent.toLowerCase();
+if (wx.match(/MicroMessenger/i) == 'micromessenger') return;
+
 wx.config($jweixin.data('config'));
 
 const reg = /accept/;
 
 wx.ready(function() {
   var btn = document.getElementById('subscribe-btn');
+  $('.js-wechat-subscribe').removeClass('hidden');
   btn.addEventListener('success', function (e) {
     const subscribeDetails = e.detail.subscribeDetails;
     console.log(subscribeDetails);
