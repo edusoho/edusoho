cb60352e5d0e41c44259e55bd45824eaa0d85f30
zhongyunchang <zhongyunchang@yeah.net>
2021-04-28 20:29:43 +0800

    fix: #75176 上报回执

--- src/Biz/AuditCenter/ReportSources/GroupThread.php
+++ src/Biz/AuditCenter/ReportSources/GroupThread.php
@@ -2,6 +2,7 @@
 
 namespace Biz\AuditCenter\ReportSources;
 
+use Biz\Group\Dao\ThreadDao;
 use Biz\Group\Service\ThreadService;
 
 class GroupThread extends AbstractSource
@@ -17,11 +18,33 @@ class GroupThread extends AbstractSource
         ];
     }
 
+    public function handleSource($audit)
+    {
+        $thread = $this->getThreadService()->getThread($audit['targetId']);
+        if (empty($thread)) {
+            return;
+        }
+
+        $fields = $this->getAuditFields($audit);
+
+        if (!empty($fields)) {
+            $this->getGroupThreadDao()->update($thread['id'], $fields);
+        }
+    }
+
     /**
      * @return ThreadService
      */
     protected function getThreadService()
     {
         return $this->biz->service('Group:ThreadService');
+    }
+
+    /**
+     * @return ThreadDao
+     */
+    protected function getGroupThreadDao()
+    {
+        return $this->biz->dao('Group:ThreadDao');
     }
 }
