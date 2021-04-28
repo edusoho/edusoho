c2c27c00667c2d713235b7146f80174c91d12a11
zhongyunchang <zhongyunchang@yeah.net>
2021-04-28 11:38:53 +0800

    fix: #75176 个人中心违规处理

--- migrations/20210421090740_audit_relative_tables_alter.php
+++ migrations/20210421090740_audit_relative_tables_alter.php
@@ -10,6 +10,14 @@ class AuditRelativeTablesAlter extends Migration
     public function up()
     {
         $biz = $this->getContainer();
+        $biz['db']->exec("ALTER TABLE `review` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `meta`;");
+        $biz['db']->exec("ALTER TABLE `thread` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `status`;");
+        $biz['db']->exec("ALTER TABLE `thread_post` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `targetId`;");
+        $biz['db']->exec("ALTER TABLE `course_thread` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `type`;");
+        $biz['db']->exec("ALTER TABLE `course_thread_post` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `userId`;");
+        $biz['db']->exec("ALTER TABLE `groups_thread` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `status`;");
+        $biz['db']->exec("ALTER TABLE `groups_thread_post` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `userId`;");
+        $biz['db']->exec("ALTER TABLE `course_note` ADD COLUMN `auditStatus` varchar(32) NOT NULL DEFAULT 'none_checked' COMMENT '外部审核状态:none_checked、pass、illegal' AFTER `userId`;");
     }
 
     /**
@@ -18,6 +26,13 @@ class AuditRelativeTablesAlter extends Migration
     public function down()
     {
         $biz = $this->getContainer();
-        $biz['db']->exec('');
+        $biz['db']->exec('ALTER TABLE `review` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `thread` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `thread_post` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `course_thread` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `course_thread_post` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `groups_thread` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `groups_thread_post` DROP COLUMN `auditStatus`;');
+        $biz['db']->exec('ALTER TABLE `course_note` DROP COLUMN `auditStatus`;');
     }
 }
