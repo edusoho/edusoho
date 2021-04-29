44a5079fd9125d276265fe9d15eccf484f1c3133
hongyongchun <hongyongchun@edusoho.com>
2021-04-29 15:42:59 +0800

    feat: #75610 动态获取数据

--- app/Resources/static-src/app/js/wechat-subscribe-new/index.js
+++ app/Resources/static-src/app/js/wechat-subscribe-new/index.js
@@ -11,8 +11,6 @@ if (isWechatBrowser()) {
       'Accept':'application/vnd.edusoho.v2+json'
     }
   }).success(function (res) {
-    console.log($('.js-wechat-template').attr('template'));
-
     if (!res.enable || !$('.js-wechat-template').attr('template')) return;    
     initWechatConfig();
   });
@@ -24,47 +22,6 @@ function isWechatBrowser() {
   return browser.match(/MicroMessenger/i) == 'micromessenger';
 }
 
-function initWechatSubscribe() {
-  $.ajax({
-    url: '/api/template',
-    type: 'GET',
-    headers:{
-      'Accept':'application/vnd.edusoho.v2+json'
-    },
-    beforeSend(request) {
-      request.setRequestHeader('X-CSRF-Token', $('meta[name=csrf-token]').attr('content'));
-    },
-  }).success(function (res) {
-    if (!res) return;
-
-    const template = 
-    `
-    <i class="es-icon es-icon-share"></i>
-    <br>
-    <wx-open-subscribe template="${res}" id="subscribe-btn">
-      <template slot="style">
-        <style>
-          .subscribe-btn {
-            color: #919191;
-            font-size: 12px;
-          }
-        </style>
-      </template>
-      <template>
-        <span class="subscribe-btn">
-          订阅
-        </span>
-      </template>
-    </wx-open-subscribe>
-    `;
-    $('.js-wechat-subscribe').html(template);
-    // $('.js-wechat-template').attr('template', res);
-    // console.log($('.js-wechat-template').attr('template'));
-    initWechatConfig();
-  });
-}
-
-
 function initWechatConfig() {
   wx.config($jweixin.data('config'));
 
@@ -73,11 +30,8 @@ function initWechatConfig() {
   wx.ready(function() {
     $('.js-wechat-subscribe').removeClass('hidden');
     var btn = document.getElementById('subscribe-btn');
-    console.log('hongbusi', btn);
-    console.log($('#subscribe-btn'));
     btn.addEventListener('success', function (e) {
       const subscribeDetails = e.detail.subscribeDetails;
-      console.log(subscribeDetails);
       if (reg.test(subscribeDetails)) {
         notify('success', '订阅成功');
       }
