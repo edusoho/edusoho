2a48f15e870a6de81e2d3102253e60f352b975cb
shenchupeng <shenchupeng@howzhi.com>
2021-04-30 11:35:00 +0800

    feat: #210429 发送记录

--- src/Biz/Notification/Service/Impl/NotificationServiceImpl.php
+++ src/Biz/Notification/Service/Impl/NotificationServiceImpl.php
@@ -31,11 +31,11 @@ class NotificationServiceImpl extends BaseService implements NotificationService
 
     public function createBatch($batch)
     {
-        if (!ArrayToolkit::requireds($batch, ['eventId', 'sn', 'strategyId', 'source'])) {
+        if (!ArrayToolkit::requireds($batch, ['eventId', 'sn', 'strategyId', 'source', 'smsEventId'])) {
             $this->createNewException(CommonException::ERROR_PARAMETER_MISSING());
         }
 
-        $batch = ArrayToolkit::parts($batch, ['eventId', 'sn', 'extra', 'strategyId', 'source']);
+        $batch = ArrayToolkit::parts($batch, ['eventId', 'sn', 'extra', 'strategyId', 'source', 'smsEventId']);
 
         return $this->getNotificationBatchDao()->create($batch);
     }
@@ -163,7 +163,7 @@ class NotificationServiceImpl extends BaseService implements NotificationService
             'sn' => '',
             'status' => 'finished',
             'source' => $source,
-            'smsEventId' => $event['id']
+            'smsEventId' => $event['id'],
         ];
 
         return $this->createBatch($batch);
