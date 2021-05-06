1d43fa24f8fdea7706e55bb5ce683430b31825f7
shenchupeng <shenchupeng@howzhi.com>
2021-05-06 11:10:44 +0800

    feat: #76056 升级脚本

--- upgrade-scripts/upgrade-21.2.5.php
+++ upgrade-scripts/upgrade-21.2.5.php
@@ -186,20 +186,22 @@ class EduSohoUpgrade extends AbstractUpdater
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
         }
 
-        $this->getConnection()->exec("
-            CREATE TABLE IF NOT EXISTS `wechat_subscribe_record` (
-              `id` int unsigned NOT NULL AUTO_INCREMENT,
-              `toId` varchar(64) NOT NULL DEFAULT '' COMMENT '用户openId',
-              `templateCode` varchar(64) NOT NULL DEFAULT '' COMMENT '模板code',
-              `templateType` varchar(32) NOT NULL DEFAULT '' COMMENT '模板类型（一次性、长期）',
-              `isSend` tinyint NOT NULL DEFAULT 0 COMMENT '是否已发送',
-              `createdTime` int unsigned NOT NULL COMMENT '创建时间',
-              `updatedTime` int unsigned NOT NULL COMMENT '更新时间',
-              PRIMARY KEY (`id`),
-              key `toId` (`toId`),
-              key `templateCode` (`templateCode`)
-            ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='微信订阅记录表';
-        ");
+        if (!$this->isTableExist('wechat_subscribe_record')) {
+            $this->getConnection()->exec("
+                CREATE TABLE IF NOT EXISTS `wechat_subscribe_record` (
+                  `id` int unsigned NOT NULL AUTO_INCREMENT,
+                  `toId` varchar(64) NOT NULL DEFAULT '' COMMENT '用户openId',
+                  `templateCode` varchar(64) NOT NULL DEFAULT '' COMMENT '模板code',
+                  `templateType` varchar(32) NOT NULL DEFAULT '' COMMENT '模板类型（一次性、长期）',
+                  `isSend` tinyint NOT NULL DEFAULT 0 COMMENT '是否已发送',
+                  `createdTime` int unsigned NOT NULL COMMENT '创建时间',
+                  `updatedTime` int unsigned NOT NULL COMMENT '更新时间',
+                  PRIMARY KEY (`id`),
+                  key `toId` (`toId`),
+                  key `templateCode` (`templateCode`)
+                ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='微信订阅记录表';
+            ");
+        }
 
         return 1;
     }
