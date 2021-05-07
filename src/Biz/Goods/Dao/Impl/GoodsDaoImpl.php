2cae7c15806f38d517198f6c0a8aa30971f72304
shaoyang <zhoushaoyang@edusoho.com>
2021-05-07 18:01:30 +0800

    fix: #75479 公开课推荐课程bug

--- src/Biz/Goods/Dao/Impl/GoodsDaoImpl.php
+++ src/Biz/Goods/Dao/Impl/GoodsDaoImpl.php
@@ -38,7 +38,7 @@ class GoodsDaoImpl extends AdvancedDaoImpl implements GoodsDao
                 'recommendWeight < :recommendWeight_GT',
                 'recommendedTime > :recommendedTime_Gt',
             ],
-            'orderbys' => ['id', 'hotSeq', 'publishedTime', 'createdTime','recommendWeight'],
+            'orderbys' => ['id', 'hotSeq', 'publishedTime', 'createdTime', 'recommendWeight'],
         ];
     }
 
