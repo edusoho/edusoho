b9ebc2c9d5197060247c1f4c3cc5285edc185d6e
zsm0620 <2645839086@qq.com>
2021-05-19 18:35:44 +0800

    feat: #76487 更新课时状态

--- src/ApiBundle/Api/Resource/MultiClass/MultiClassLesson.php
+++ src/ApiBundle/Api/Resource/MultiClass/MultiClassLesson.php
@@ -6,6 +6,10 @@ namespace ApiBundle\Api\Resource\MultiClass;
 
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Common\CommonException;
+use Biz\Course\Service\LessonService;
+use Biz\MultiClass\MultiClassException;
+use Biz\MultiClass\Service\MultiClassService;
 use Biz\Task\Service\TaskService;
 use Biz\Task\TaskException;
 
@@ -24,11 +28,47 @@ class MultiClassLesson extends AbstractResource
         return ['success' => true];
     }
 
+    public function update(ApiRequest $request, $multiClassId, $lessonId)
+    {
+        $multiClass = $this->getMultiClassService()->getMultiClass($multiClassId);
+        if (!$multiClass) {
+            throw MultiClassException::MULTI_CLASS_NOT_EXIST();
+        }
+
+        $type = $request->request->get('type', '');
+        if (!$type || !in_array($type, ['publish', 'unpublish'])){
+            throw CommonException::ERROR_PARAMETER();
+        }
+
+        if ('publish' === $type){
+            return $this->getCourseLessonService()->publishLesson($multiClass['courseId'], $lessonId);
+        }else{
+            return $this->getCourseLessonService()->unpublishLesson($multiClass['courseId'], $lessonId);
+        }
+
+    }
+
     /**
      * @return TaskService
      */
     protected function getTaskService()
     {
         return $this->service('Task:TaskService');
+    }
+
+    /**
+     * @return MultiClassService
+     */
+    protected function getMultiClassService()
+    {
+        return $this->service('MultiClass:MultiClassService');
+    }
+
+    /**
+     * @return LessonService
+     */
+    protected function getCourseLessonService()
+    {
+        return $this->service('Course:LessonService');
     }
 }
