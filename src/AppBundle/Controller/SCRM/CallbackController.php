1063713fca7c2e03ece4c20ed053729ebd9a141a
zhongyunchang <zhongyunchang@yeah.net>
2021-05-19 08:55:44 +0800

    feat: #76635 修改新增模糊搜索

---
+++ src/AppBundle/Controller/SCRM/CallbackController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Controller\SCRM;
+
+use AppBundle\Common\ArrayToolkit;
+use AppBundle\Common\Exception\InvalidArgumentException;
+use AppBundle\Controller\BaseController;
+use Biz\Goods\Service\GoodsService;
+use ESCloud\SDK\Service\ScrmService;
+use Symfony\Component\HttpFoundation\Request;
+
+class CallbackController extends BaseController
+{
+    public function goodsAction(Request $request)
+    {
+        $query = $request->query->all();
+        $this->filterQuery($query);
+
+        $userInfo = $this->getScrmSdk()->getUserByToken($query['user_token']);
+        if (!empty($userInfo)) {
+            $this->getUserService()->register([
+                '',
+                '',
+            ]);
+        }
+    }
+
+    private function filterQuery($query)
+    {
+        if (ArrayToolkit::requireds($query, [
+            'user_token',
+            'receipt_token',
+        ])) {
+            throw new InvalidArgumentException('参数不正确！');
+        }
+    }
+
+    /**
+     * @return GoodsService
+     */
+    protected function getGoodsService()
+    {
+        return $this->createService('Goods:GoodsService');
+    }
+
+    /**
+     * @return ScrmService
+     */
+    protected function getScrmSdk()
+    {
+        $biz = $this->getBiz();
+
+        return $biz['ESCloudSdk.scrm'];
+    }
+}
