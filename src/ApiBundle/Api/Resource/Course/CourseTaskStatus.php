af3404a2712da7ac0f96ec571136bbbb2a1cb518
zsm0620 <2645839086@qq.com>
2021-05-21 14:21:27 +0800

    feat: #76487 更改

--- src/ApiBundle/Api/Resource/Course/CourseTaskStatus.php
+++ src/ApiBundle/Api/Resource/Course/CourseTaskStatus.php
@@ -10,15 +10,23 @@ use Biz\Common\CommonException;
 use Biz\Course\CourseException;
 use Biz\Course\Service\CourseService;
 use Biz\Course\Service\LessonService;
+use Biz\Task\Service\TaskService;
+use Biz\Task\TaskException;
 
 class CourseTaskStatus extends AbstractResource
 {
-    public function update(ApiRequest $request, $courseId, $lessonId)
+    public function update(ApiRequest $request, $courseId, $taskId)
     {
         $course = $this->getCourseService()->getCourse($courseId);
         if (!$course) {
             throw CourseException::NOTFOUND_COURSE();
         }
+
+        $task = $this->getTaskService()->getTask($taskId);
+        if (!$task) {
+            throw TaskException::NOTFOUND_TASK();
+        }
+        $lessonId = $task['categoryId'];
 
         $type = $request->request->get('type', '');
         if (!$type || !in_array($type, ['publish', 'unpublish'])){
@@ -47,5 +55,13 @@ class CourseTaskStatus extends AbstractResource
     protected function getCourseLessonService()
     {
         return $this->service('Course:LessonService');
+    }
+
+    /**
+     * @return TaskService
+     */
+    private function getTaskService()
+    {
+        return $this->service('Task:TaskService');
     }
 }
