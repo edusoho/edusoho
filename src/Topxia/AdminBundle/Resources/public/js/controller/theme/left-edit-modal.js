2188c56ee6102b53ba38310b5b4fcf7ea02eca2d
zsm0620 <2645839086@qq.com>
2021-05-21 18:32:29 +0800

    feat: #76819 资讯栏目选择优化

--- src/Topxia/AdminBundle/Resources/public/js/controller/theme/left-edit-modal.js
+++ src/Topxia/AdminBundle/Resources/public/js/controller/theme/left-edit-modal.js
@@ -9,6 +9,21 @@ define(function(require, exports, module) {
         var $form = $($(this).data('form'));
 
         var config = $form.serializeJSON();
+        if (config.categoryIds && config.categoryIds.length > 1){
+          var arr = [];
+          var brr = [];
+          for(var i = 0; i < config.categoryIds.length; i++) {
+            var item = config.categoryIds[i].categoryId;
+            if (arr.indexOf(item) == -1 && String(item) != '0'){
+              arr.push(item);
+              brr.push({
+                categoryId: item
+              });
+            }
+          }
+          config.categoryIds = brr;
+        }
+
         // 多选项为空时，置空
         var checkboxSetting = {};
         var $checkbox = $form.find("input[type='checkbox']");
@@ -16,7 +31,6 @@ define(function(require, exports, module) {
           checkboxSetting[$(this).attr('name')] = '';
         });
         config = $.extend(checkboxSetting, config);
-        console.log(config);
         $themeEditContent.trigger('save_part_config', config);
         $("#modal").modal('hide');
       });
@@ -58,18 +72,26 @@ define(function(require, exports, module) {
       }
 
       $('#addCategory').on('click', function (event) {
-        var selectCount = $('#categories').children('select').length;
+        var selectCount = $('.articleCategory').length;
         if (selectCount >= 4){
           $('.categoriesTip').removeClass('hidden');
           return;
         }
         var categoryChoices = JSON.parse($('#categoryChoices').val());
-        var html = '<select class="form-control width-input-large pull-left" name="categoryIds[][categoryId]" style="margin-top: 12px">\n <option value=""></option>';
+        var html = "<div class='articleCategory clearfix'>\n ";
+        html += '<select class="form-control width-input-large pull-left" name="categoryIds[][categoryId]" style="margin-bottom: 12px">\n <option value="0">请选择所属栏目</option>';
         for (var categoryId in categoryChoices) {
           html += '<option value="' + categoryId + '">' + categoryChoices[categoryId] + '</option>';
         }
-        html += '</select>';
+        html += '</select>\n <span class="delete-item-btn"><i class="cd-icon cd-icon-close"></i></span>\n </div>';
         $('#categories').append(html);
       });
+
+      $('body').on('click', '.delete-item-btn', function (event) {
+        $('.categoriesTip').addClass('hidden');
+        if ($('.articleCategory').length > 1){
+          $(event.currentTarget).parent('.articleCategory').remove();
+        }
+      })
     };
 });
