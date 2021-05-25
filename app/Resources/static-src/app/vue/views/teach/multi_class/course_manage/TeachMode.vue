fd2fdebf1453ce863be57e512b65a5160cc37b59
hongyongchun <hongyongchun@edusoho.com>
2021-05-25 17:18:07 +0800

    feat: #210525 教学模式完善

--- app/Resources/static-src/app/vue/views/teach/multi_class/course_manage/TeachMode.vue
+++ app/Resources/static-src/app/vue/views/teach/multi_class/course_manage/TeachMode.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     {{ record.tasks.type | teachType }}
-    <a-divider type="vertical" />
-    <a-checkable-tag>已结束</a-checkable-tag>
-    <a-checkable-tag style="color: #43bc60;">直播中</a-checkable-tag>
-    <a-checkable-tag style="color: #fb8d4d;">未开始</a-checkable-tag>
-    <br>
-    <a-tag color="green" style="margin-top: 4px;">有回放</a-tag>
+
+    <template v-if="progressStatus">
+      <a-divider type="vertical" />
+      <a-checkable-tag v-if="progressStatus">已结束</a-checkable-tag>
+      <a-checkable-tag v-if="progressStatus" style="color: #43bc60;">直播中</a-checkable-tag>
+      <a-checkable-tag v-if="progressStatus" style="color: #fb8d4d;">未开始</a-checkable-tag>
+    </template>
+
+    <template v-if="replayStatus">
+      <br>
+      <a-tag color="green" style="margin-top: 4px;">有回放</a-tag>
+    </template>
   </div>
 </template>
 
@@ -22,6 +28,12 @@ const type = {
 export default {
   name: 'TeachMode',
 
+  filters: {
+    teachType(value) {
+      return type[value];
+    }
+  },
+
   props: {
     record: {
       type: Object,
@@ -32,10 +44,22 @@ export default {
     }
   },
 
-  filters: {
-    teachType(value) {
-      return type[value];
+  computed: {
+    replayStatus() {
+      const { tasks: { type, activity: { ext } }} = this.record;
+      if (type == 'live' && ext.replayStatus == 'videoGenerate') {
+        return true;
+      }
+      return false;
+    },
+
+    progressStatus() {
+      const { tasks: { type, activity: { ext } }} = this.record;
+      if (type == 'live') {
+        return ext.progressStatus;
+      }
+      return '';
     }
-  },
+  }
 }
 </script>
