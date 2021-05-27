57912a8d0cb6e2004e0ee96dccc39972e3303e2b
秦瑞 <qinrui@edusoho.com>
2021-05-26 23:38:27 -0700

    feat: #76836 教师推荐接口 review fix

--- src/ApiBundle/Api/Resource/Teacher/TeacherSeq.php
+++ src/ApiBundle/Api/Resource/Teacher/TeacherSeq.php
@@ -2,16 +2,29 @@
 
 namespace ApiBundle\Api\Resource\Teacher;
 
+use ApiBundle\Api\Annotation\Access;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\MultiClass\MultiClassException;
 use Biz\User\Service\UserService;
 use Biz\User\UserException;
 
 class TeacherSeq extends AbstractResource
 {
+    /**
+     * @param $id
+     * @param $type
+     *
+     * @return bool[]
+     * @Access(roles="ROLE_ADMIN,ROLE_SUPER_ADMIN")
+     */
     public function update(ApiRequest $request, $id, $type)
     {
         $teacher = $this->getUserService()->getUser($id);
+
+        if (!in_array($type, ['promoted', 'cancel'])) {
+            throw MultiClassException::MULTI_CLASS_DATA_FIELDS_MISSING();
+        }
 
         if (empty($teacher) || !in_array('ROLE_TEACHER', $teacher['roles'])) {
             throw UserException::NOTFOUND_USER();
