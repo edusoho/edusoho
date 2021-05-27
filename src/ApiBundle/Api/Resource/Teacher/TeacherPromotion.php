76f7ad5b66c44a7dd22582bef07c26e67d67410c
秦瑞 <qinrui@edusoho.com>
2021-05-27 02:43:08 -0700

    feat: #76836 教师推荐接口 review fix

--- src/ApiBundle/Api/Resource/Teacher/TeacherPromotion.php
+++ src/ApiBundle/Api/Resource/Teacher/TeacherPromotion.php
@@ -5,37 +5,46 @@ namespace ApiBundle\Api\Resource\Teacher;
 use ApiBundle\Api\Annotation\Access;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
-use Biz\MultiClass\MultiClassException;
 use Biz\User\Service\UserService;
 use Biz\User\UserException;
 
-class TeacherSeq extends AbstractResource
+class TeacherPromotion extends AbstractResource
 {
     /**
      * @param $id
-     * @param $type
      *
      * @return bool[]
      * @Access(roles="ROLE_ADMIN,ROLE_SUPER_ADMIN")
      */
-    public function update(ApiRequest $request, $id, $type)
+    public function add(ApiRequest $request, $id)
     {
         $teacher = $this->getUserService()->getUser($id);
-
-        if (!in_array($type, ['promoted', 'cancel'])) {
-            throw MultiClassException::MULTI_CLASS_DATA_FIELDS_MISSING();
-        }
 
         if (empty($teacher) || !in_array('ROLE_TEACHER', $teacher['roles'])) {
             throw UserException::NOTFOUND_USER();
         }
 
-        if ('promoted' == $type) {
-            $number = $request->request->get('number', 0);
-            $this->getUserService()->promoteUser($id, $number);
-        } elseif ('cancel' == $type) {
-            $this->getUserService()->cancelPromoteUser($id);
+        $number = $request->request->get('number', 0);
+        $this->getUserService()->promoteUser($id, $number);
+
+        return ['success' => true];
+    }
+
+    /**
+     * @param $id
+     *
+     * @return bool[]
+     * @Access(roles="ROLE_ADMIN,ROLE_SUPER_ADMIN")
+     */
+    public function remove(ApiRequest $request, $id)
+    {
+        $teacher = $this->getUserService()->getUser($id);
+
+        if (empty($teacher) || !in_array('ROLE_TEACHER', $teacher['roles'])) {
+            throw UserException::NOTFOUND_USER();
         }
+
+        $this->getUserService()->cancelPromoteUser($id);
 
         return ['success' => true];
     }
