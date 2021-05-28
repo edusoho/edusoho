c46a663e6ab7e8b14bd7130a81e44ce3ed865e7e
hongyongchun <hongyongchun@edusoho.com>
2021-05-28 18:24:05 +0800

    feat: #76525 表单提交

--- app/Resources/static-src/app/vue/views/teach/multi_class/create/Schedule.vue
+++ app/Resources/static-src/app/vue/views/teach/multi_class/create/Schedule.vue
@@ -55,7 +55,7 @@ export default {
   data() {
     return {
       lessonDirectory: [],
-      createLiveVisible: true,
+      createLiveVisible: false,
       addChapterOrUnitVisible: false,
       addType: ''
     }
