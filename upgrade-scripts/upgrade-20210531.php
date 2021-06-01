5d1ce94ba885676ba4bf33d0527cf8d379ba86bf
shenchupeng <shenchupeng@howzhi.com>
2021-06-01 14:16:10 +0800

    feat: #76838 大班课升级脚本

--- upgrade-scripts/upgrade-20210531.php
+++ upgrade-scripts/upgrade-20210531.php
@@ -54,6 +54,7 @@ class EduSohoUpgrade extends AbstractUpdater
             'addMultiClassTables',
             'addDefaultProduct',
             'updateRolePermission',
+            'initAssistantPermission',
         );
 
         $funcNames = array();
@@ -164,6 +165,35 @@ class EduSohoUpgrade extends AbstractUpdater
         $this->getConnection()->exec("
             UPDATE role SET data_v2 = '[\"admin_v2\",\"admin_v2_teach\",\"admin_v2_course_group\",\"admin_v2_multi_class\",\"admin_v2_multi_class_manage\",\"admin_v2_course_show\",\"admin_v2_course_manage\",\"admin_v2_course_content_manage\",\"admin_v2_course_add\",\"admin_v2_course_guest_member_preview\",\"admin_v2_course_set_close\",\"admin_v2_course_set_clone\",\"admin_v2_course_set_publish\",\"admin_v2_course_set_delete\",\"admin_v2_course_set_remove\",\"admin_v2_course_set_data\",\"web\",\"course_manage\",\"course_manage_info\",\"course_manage_base\",\"course_manage_detail\",\"course_manage_picture\",\"course_manage_lesson\",\"live_course_manage_replay\",\"course_manage_files\",\"course_manage_setting\",\"course_manage_price\",\"course_manage_teachers\",\"course_manage_students\",\"course_manage_student_create\",\"course_manage_questions\",\"course_manage_question\",\"course_manage_testpaper\",\"course_manange_operate\",\"course_manage_data\",\"course_manage_order\",\"classroom_manage\",\"classroom_manage_settings\",\"classroom_manage_set_info\",\"classroom_manage_set_price\",\"classroom_manage_set_picture\",\"classroom_manage_service\",\"classroom_manage_headteacher\",\"classroom_manage_teachers\",\"classroom_manage_assistants\",\"classroom_manage_content\",\"classroom_manage_courses\",\"classroom_manage_students\",\"classroom_manage_testpaper\"]' WHERE code = 'ROLE_TEACHER';
         ");
+
+        return 1;
+    }
+
+    public function initAssistantPermission()
+    {
+        $setting = $this->getSettingService()->get('assistant_permission', []);
+        if (!empty($setting)) {
+            return 1;
+        }
+
+        $permissions = [
+            'multi_class_manage',
+            'course_manage',
+            'course_lesson_manage',
+            'course_member_manage',
+            'course_member_create',
+            'course_member_deadline_edit',
+            'course_member_import',
+            'course_live_manage',
+            'course_homework_review',
+            'course_exam_review',
+            'course_statistics_view',
+            'course_announcement_manage',
+            'course_replay_manage',
+            'course_question_marker_manage',
+            'course_order_manage',
+        ];
+        $this->getSettingService()->set('assistant_permission', $permissions);
 
         return 1;
     }
