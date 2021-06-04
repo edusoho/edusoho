af49a1e7c4259c7b84c7d7d2b2b3b7aa17d84db3
词庸 <13466916775@163.com>
2021-06-04 18:22:08 +0800

    fix: #77501 班课列表中多个助教时没有全部展示

--- app/Resources/static-src/app/vue/views/teach/multi_class/index.vue
+++ app/Resources/static-src/app/vue/views/teach/multi_class/index.vue
@@ -25,9 +25,7 @@
           :href="`/course/${course.id}`" target="_blank">
           {{ course.title || course.courseSetTitle }}
         </a>
-        <template slot="assistant" slot-scope="assistant">
-          {{ assistant ? assistant.join('、') : '' }}
-        </template>
+        <assistant slot="assistant" slot-scope="assistant" :assistant="assistant" />
         <a slot="studentNum" slot-scope="text, record"
           href="javascript:;"
           @click="$router.push({ name: 'MultiClassStudentManage', params: { id: record.id } })">
@@ -69,6 +67,7 @@
 <script>
 import AsideLayout from 'app/vue/views/layouts/aside.vue';
 import { MultiClass } from 'common/vue/service/index.js';
+import Assistant from './course_manage/Assistant.vue';
 
 const columns = [
   {
@@ -144,7 +143,8 @@ export default {
   name: 'MultiClassList',
 
   components: {
-    AsideLayout
+    AsideLayout,
+    Assistant
   },
 
   data () {
