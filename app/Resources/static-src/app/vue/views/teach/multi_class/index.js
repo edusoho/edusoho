54408a1e88b7a25ac5e087c977b449fa5c975b1d
hongyongchun <hongyongchun@edusoho.com>
2021-06-08 15:52:57 +0800

    fix: #77433 暂无数据优化

--- app/Resources/static-src/app/vue/views/teach/multi_class/index.js
+++ app/Resources/static-src/app/vue/views/teach/multi_class/index.js
@@ -1,8 +1,6 @@
 import Vue from 'common/vue';
 import Router from 'vue-router';
 import routes from 'app/vue/router/teach/multi_class/index.js';
-import zhCN from '@codeages/design-vue/lib/locale-provider/zh_CN';
-import { ConfigProvider } from '@codeages/design-vue';
 import AntConfigProvider from 'app/vue/views/components/AntConfigProvider.vue';
 
 const router = new Router({
@@ -16,11 +14,7 @@ new Vue({
   el: '#app',
   router,
   components: {
-    ConfigProvider,
     AntConfigProvider
-  },
-  data: {
-    locale: zhCN
   },
   template: `<ant-config-provider />`
 });
