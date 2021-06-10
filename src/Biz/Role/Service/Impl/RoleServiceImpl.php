76d4e056c78b38279c91088e6250486421cfe7dd
shenchupeng <shenchupeng@howzhi.com>
2021-06-10 19:42:00 +0800

    feat: #210610 修改教师权限

--- src/Biz/Role/Service/Impl/RoleServiceImpl.php
+++ src/Biz/Role/Service/Impl/RoleServiceImpl.php
@@ -357,7 +357,7 @@ class RoleServiceImpl extends BaseService implements RoleService
     private function getDefaultTeacherAdminRole()
     {
         return [
-            "admin_v2","admin_v2_teach","admin_v2_course_group","admin_v2_multi_class","admin_v2_multi_class_manage","admin_v2_course_show","admin_v2_course_manage","admin_v2_course_content_manage","admin_v2_course_add","admin_v2_course_guest_member_preview","admin_v2_course_set_close","admin_v2_course_set_clone","admin_v2_course_set_publish","admin_v2_course_set_delete","admin_v2_course_set_remove","admin_v2_course_set_data"
+            'admin_v2', 'admin_v2_teach', 'admin_v2_course_group', 'admin_v2_multi_class', 'admin_v2_multi_class_manage', 'admin_v2_course_show', 'admin_v2_course_manage', 'admin_v2_course_content_manage', 'admin_v2_course_add', 'admin_v2_course_guest_member_preview', 'admin_v2_course_set_close', 'admin_v2_course_set_clone', 'admin_v2_course_set_publish', 'admin_v2_course_set_delete', 'admin_v2_course_set_remove', 'admin_v2_course_set_data',
         ];
     }
 
