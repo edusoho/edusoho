cc358126c99a1b5ea6782b8ef299cc6c2c1df400
hongyongchun <hongyongchun@edusoho.com>
2021-06-15 15:37:54 +0800

    feat: #77864 引入 ant 样式文件

--- app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/index.vue
+++ app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/index.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>我的错题本</div>
+  <div>
+    <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="Tab 1">
+        Content of Tab Pane 1
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2" force-render>
+        Content of Tab Pane 2
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">
+        Content of Tab Pane 3
+      </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'WrongQuestionBook',
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {
+    callback(key) {
+      console.log(key);
+    }
+  }
+};
+</script>
