b5e8a0f3bcd481d0545c82b242df4b157c8f8c56
hongyongchun <hongyongchun@edusoho.com>
2021-06-15 17:40:56 +0800

    feat: #77864 列表完善

---
+++ app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/ListItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="list-item clearfix">
+    <div class="item-cover-image pull-left">
+      <img src="/files/course/2020/08-12/093700c063c8447618.jpg" alt="">
+    </div>
+    <div class="item-info pull-left">
+      <h3 class="text-overflow">标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题</h3>
+      <p class="number">20 题</p>
+    </div>
+    <a-button type="primary" class="check-wrong-question pull-right">
+      查看错题
+    </a-button>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.list-item {
+  position: relative;
+  padding: 8px 24px 8px 8px;
+  margin-top: 16px;
+  border: 1px solid #eee;
+
+  .item-cover-image {
+    margin-right: 24px;
+    width: 227px;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .item-info {
+    width: 45%;
+
+    .number {
+      margin: 86px 0 0;
+      color: #ff4040;
+    }
+  }
+
+  .check-wrong-question {
+    margin-top: 45px;
+  }
+}
+
+@media (max-width: 767px) {
+  .list-item {
+    padding: 8px;
+
+    .item-cover-image {
+      width: 100%;
+    }
+
+    .item-info {
+      margin-top: 16px;
+      width: 100%;
+
+      .number {
+        margin: 8px 0 0;
+      }
+    }
+
+    .check-wrong-question {
+      margin-top: 16px;
+      width: 100%;
+    }
+  }
+}
+</style>
--- app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/course.vue
+++ app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/course.vue
@@ -1,3 +1,49 @@
 <template>
-  <div>course</div>
+  <div>
+    <a-input-search
+      allowClear
+      placeholder="请输入课程名称"
+      style="width: 224px; margin-top: 16px;"
+      @search="onSearch"
+    />
+
+    <template>
+      <list-item />
+    </template>
+
+    <a-pagination
+      class="text-center"
+      style="margin-top: 16px;"
+      :hide-on-single-page="true"
+      v-model="pagination.current"
+      :total="pagination.total"
+      @change="onChange"
+    />
+  </div>
 </template>
+
+<script>
+import ListItem from './ListItem.vue';
+
+export default {
+  components: {
+    ListItem
+  },
+
+  data() {
+    return {
+      pagination: {}
+    }
+  },
+
+  methods: {
+    onSearch(value) {
+      console.log(value);
+    },
+
+    onChange(current) {
+      console.log(current);
+    }
+  }
+}
+</script>
