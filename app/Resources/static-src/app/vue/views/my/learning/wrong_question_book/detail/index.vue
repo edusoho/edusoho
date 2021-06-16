f6f1cc0e7497b85b3ed7ab09e0c00ac1b2fefe1c
hongyongchun <hongyongchun@edusoho.com>
2021-06-16 16:03:05 +0800

    feat: #77864 detail

--- app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/detail/index.vue
+++ app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/detail/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>fsafsd</div>
+  <div class="wrong-question-detail">
+    查看错题
+  </div>
 </template>
+
+<style lang="less" scoped>
+.wrong-question-detail {
+  padding: 24px 16px;
+  background-color: #fff;
+}
+</style>
