c4f0298fcfe35e419eb66c6e0781c846ac962417
hongyongchun <hongyongchun@edusoho.com>
2021-06-18 14:53:58 +0800

    feat: #77864 解析,来源

---
+++ app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/question/Analysis.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="clearfix analysis">
+    <div class="pull-left analysis-label">解析：</div>
+    <div class="pull-left analysis-content">正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C正确答案是C</div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.analysis {
+  padding: 16px 26px 16px 0;
+  background: rgba(70, 195, 123, 0.05);
+  border-radius: 4px;
+
+  .analysis-label {
+    text-indent: 2em;
+  }
+
+  .analysis-content {
+    width: calc(100% - 72px);
+  }
+}
+</style>
