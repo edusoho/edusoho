fcc3d5fabe3847964e2a2844773e02d7f364bd01
bigabiga_bellorchid <zhongyunchang@yeah.net>
2021-06-21 11:37:38 +0800

    feat: #75785 完善Pool 扩展

---
+++ src/Biz/WrongBook/Dao/Impl/WrongQuestionBookExerciseDaoImpl.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Biz\WrongBook\Dao\Impl;
+
+use Biz\WrongBook\Dao\WrongQuestionBookExerciseDao;
+use Codeages\Biz\Framework\Dao\AdvancedDaoImpl;
+
+class WrongQuestionBookExerciseDaoImpl extends AdvancedDaoImpl implements WrongQuestionBookExerciseDao
+{
+    protected $table = 'biz_wrong_book_exercise';
+
+    public function declares()
+    {
+        return [
+            'timestamps' => ['created_time', 'updated_time'],
+            'serializes' => [],
+            'orderbys' => ['id'],
+            'conditions' => [
+                'id = :id',
+            ],
+        ];
+    }
+}
