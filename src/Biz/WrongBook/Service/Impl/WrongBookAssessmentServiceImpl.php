48d67e0ae4846b856df1fe6289b1b01200319bb4
bigabiga_bellorchid <zhongyunchang@yeah.net>
2021-06-21 18:35:12 +0800

    feat: #78078 刷题组卷业务继承重写

---
+++ src/Biz/WrongBook/Service/Impl/WrongBookAssessmentServiceImpl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Biz\WrongBook\Service\Impl;
+
+use Biz\BaseService;
+use Biz\WrongBook\Service\WrongBookAssessmentService;
+
+class WrongBookAssessmentServiceImpl extends BaseService implements WrongBookAssessmentService
+{
+}
---
+++ src/Biz/WrongBook/Service/Impl/WrongBookPractiseServiceImpl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Biz\WrongBook\Service\Impl;
+
+use Biz\BaseService;
+use Biz\WrongBook\Service\WrongBookPractiseService;
+
+class WrongBookPractiseServiceImpl extends BaseService implements WrongBookPractiseService
+{
+}
