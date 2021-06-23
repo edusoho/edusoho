a790e76b7b5382ee00b340b0dc74032263779911
zsm0620 <2645839086@qq.com>
2021-06-23 11:55:43 +0800

    feat: #78124 班课助教列表

---
+++ src/ApiBundle/Api/Resource/MultiClass/MultiClassAssistant.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace ApiBundle\Api\Resource\MultiClass;
+
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+use ApiBundle\Api\Resource\Filter;
+use ApiBundle\Api\Resource\User\UserFilter;
+use AppBundle\Common\ArrayToolkit;
+use Biz\Course\Service\MemberService;
+use Biz\MultiClass\MultiClassException;
+use Biz\MultiClass\Service\MultiClassService;
+use Biz\User\Service\UserService;
+use ApiBundle\Api\Annotation\Access;
+
+class MultiClassAssistant extends AbstractResource
+{
+    /**
+     * @param ApiRequest $request
+     * @param $multiClassId
+     * @return mixed
+     * @Access(roles="ROLE_ADMIN,ROLE_SUPER_ADMIN")
+     */
+    public function search(ApiRequest $request, $multiClassId)
+    {
+        $multiClass = $this->getMultiClassService()->getMultiClass($multiClassId);
+        if (empty($multiClass)) {
+            throw MultiClassException::MULTI_CLASS_NOT_EXIST();
+        }
+
+        $assistants = $this->getCourseMemberService()->findMultiClassMemberByMultiClassIdAndRole($multiClassId, 'assistant');
+        $userIds = ArrayToolkit::column($assistants, 'userId');
+        $conditions = [
+            'userIds' => $userIds,
+            'nickname' => $request->query->get('nickname', '')
+        ];
+
+        $users = $this->getUserService()->searchUsers($conditions, ['createdTime' => 'DESC'], 0, count($userIds));
+        $userFilter = new UserFilter();
+        $userFilter->setMode(Filter::SIMPLE_MODE);
+        $userFilter->filters($users);
+
+        return $users;
+    }
+
+    /**
+     * @return MultiClassService
+     */
+    protected function getMultiClassService()
+    {
+        return $this->service('MultiClass:MultiClassService');
+    }
+
+    /**
+     * @return MemberService
+     */
+    protected function getCourseMemberService()
+    {
+        return $this->service('Course:MemberService');
+    }
+
+    /**
+     * @return UserService
+     */
+    protected function getUserService()
+    {
+        return $this->service('User:UserService');
+    }
+}
