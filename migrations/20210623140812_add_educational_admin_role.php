21deb3a06c38cbb4e4d80b47ed4d6767e1d68b01
zsm0620 <2645839086@qq.com>
2021-06-23 15:36:21 +0800

    feat: #78114 后台添加教务模块

--- migrations/20210623140812_add_educational_admin_role.php
+++ migrations/20210623140812_add_educational_admin_role.php
@@ -9,7 +9,11 @@ class AddEducationalAdminRole extends Migration
      */
     public function up()
     {
-
+        $currentTime = time();
+        $biz = $this->getContainer();
+        $biz['db']->exec("
+            INSERT INTO `role` (`name`, `code`, `data_v2`, `createdTime`, `createdUserId`, `updatedTime`) VALUES ('教务', 'ROLE_EDUCATIONAL_ADMIN', '[\"admin_v2\",\"admin_v2_education\",\"admin_v2_education_manage\",\"admin_v2_multi_class_product\",\"admin_v2_multi_class_product_manage\",\"admin_v2_multi_class\",\"admin_v2_multi_class_manage\",\"admin_v2_teacher\",\"admin_v2_teacher_manage\",\"admin_v2_assistant\",\"admin_v2_assistant_manage\"]', '{$currentTime}', 1, '{$currentTime}');
+        ");
     }
 
     /**
@@ -17,6 +21,9 @@ class AddEducationalAdminRole extends Migration
      */
     public function down()
     {
-
+        $biz = $this->getContainer();
+        $biz['db']->exec("
+            DELETE FROM `role` where `code` = 'ROLE_EDUCATIONAL_ADMIN';
+        ");
     }
 }
