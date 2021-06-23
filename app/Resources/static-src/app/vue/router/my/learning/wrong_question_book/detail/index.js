08da1d559cc1329cd1a2253c70800a43c9268608
hongyongchun <hongyongchun@edusoho.com>
2021-06-23 11:35:22 +0800

    feat: #77865 路由优化

--- app/Resources/static-src/app/vue/router/my/learning/wrong_question_book/detail/index.js
+++ app/Resources/static-src/app/vue/router/my/learning/wrong_question_book/detail/index.js
@@ -1,6 +1,6 @@
 export default [
   {
-    path: '/',
+    path: '/target_type/:target_type/target_id/:id',
     name: 'WrongQuestionBookDetail',
     component: () => import(/* webpackChunkName: "app/vue/dist/WrongQuestionBookDetail" */ 'app/vue/views/my/learning/wrong_question_book/detail/index.vue')
   }
