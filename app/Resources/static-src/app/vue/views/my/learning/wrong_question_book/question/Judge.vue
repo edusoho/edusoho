60faa228832fd7f1152ca74cda9059babfc6e634
hongyongchun <hongyongchun@edusoho.com>
2021-06-24 10:52:36 +0800

    feat: #77865 判断题

--- app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/question/Judge.vue
+++ app/Resources/static-src/app/vue/views/my/learning/wrong_question_book/question/Judge.vue
@@ -1,5 +1,84 @@
 <template>
-  <div>
-    TrueFalse
+  <div class="question-layout">
+    <order :question="question" />
+
+    <div class="mb16" v-html="question.stem" />
+
+    <div class="prevent-click">
+      <a-radio-group :default-value="report.response[0]">
+        <a-radio
+          :class="['choose-answer', getAnswerClass(item.radio.val)]"
+          v-for="(item, index) in question.response_points"
+          :key="index"
+          :value="item.radio.val"
+        >
+          <div class="choose-answer-content">
+            <span class="choose-answer-text" v-html="item.radio.text" />
+          </div>
+        </a-radio>
+      </a-radio-group>
+    </div>
+
+     <a-divider style="margin: 16px 0;" />
+
+    <div class="clearfix result mb16">
+      <div class="pull-left result-label">正确答案：</div>
+      <div class="pull-left result-content">{{ rightAnswer }}</div>
+    </div>
+
+    <analysis :analysis="question.analysis" />
+
+    <slot name="situation" />
   </div>
 </template>
+
+<script>
+import _ from 'lodash';
+import Order from './components/Order.vue';
+import Analysis from './components/Analysis.vue';
+
+export default {
+  name: 'Judge',
+
+  props: {
+    question: {
+      type: Object,
+      required: true
+    },
+
+    report: {
+      type: Object,
+      required: true
+    }
+  },
+
+  components: {
+    Order,
+    Analysis
+  },
+
+  computed: {
+    rightAnswer() {
+      const { answer } = this.question;
+      return `${_.join(answer, '、')}`;
+    }
+  },
+
+  methods: {
+    getAnswerClass(value) {
+      const { answer } = this.question; // 正确答案
+      const { response } = this.report; // 用户选择的答案
+
+      if (_.includes(_.difference(answer, response), value)) return 'right-answer'; // 用户未选的正确答案
+
+      if (_.includes(_.difference(response, answer), value)) return 'choose-answer--wrong'; // 用户选择的错误答案
+
+      if (_.includes(_.intersection(answer, response), value)) return 'choose-answer--right'; // 用户选中的正确答案
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import './common.less';
+</style>
