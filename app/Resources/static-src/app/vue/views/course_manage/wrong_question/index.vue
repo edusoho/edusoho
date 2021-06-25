8316c9bff468c93f65b46c769df3fbc478bbc517
hongyongchun <hongyongchun@edusoho.com>
2021-06-25 14:54:58 +0800

    feat: #78291 错题列表完善

--- app/Resources/static-src/app/vue/views/course_manage/wrong_question/index.vue
+++ app/Resources/static-src/app/vue/views/course_manage/wrong_question/index.vue
@@ -1,5 +1,181 @@
 <template>
   <div>
-    课程管理学员错题
+    <a-form-model
+      ref="form"
+      :model="form"
+      layout="inline"
+    >
+      <a-form-model-item>
+        <a-select
+          v-model="form.source"
+          style="width: 120px;"
+          placeholder="题目来源"
+        >
+          <a-select-option value="zuoye">
+            作业任务
+          </a-select-option>
+          <a-select-option value="zuoye">
+            作业任务
+          </a-select-option>
+          <a-select-option value="kaoshi">
+            考试任务
+          </a-select-option>
+          <a-select-option value="lianxi">
+            练习任务
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+
+      <a-form-model-item>
+        <a-select
+          v-model="form.taskName"
+          style="width: 120px;"
+          placeholder="任务名称"
+        >
+          <a-select-option value="zuoye">
+            作业任务
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+
+      <a-form-model-item>
+        <a-select
+          v-model="form.sort"
+          style="width: 120px;"
+          placeholder="答错人次"
+        >
+          <a-select-option value="DES">
+            由高至低
+          </a-select-option>
+          <a-select-option value="ASC">
+            由低至高
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+
+      <a-form-model-item>
+        <a-button type="primary" @click="onSearch">搜索</a-button>
+      </a-form-model-item>
+    </a-form-model>
+
+    <a-table
+      class="mt24"
+      :columns="columns"
+      :row-key="record => record.order"
+      :data-source="data"
+      :pagination="pagination"
+      :loading="loading"
+      bordered
+      @change="handleTableChange"
+    >
+      <template slot="actions" slot-scope="actions, record">
+        <a-button type="link" @click="handleClickViewDetails(record)">查看详情</a-button>
+      </template>
+    </a-table>
   </div>
 </template>
+
+<script>
+const columns = [
+  {
+    title: '',
+    dataIndex: 'order',
+    width: '5%'
+  },
+  {
+    title: '题目',
+    dataIndex: 'stem',
+    width: '40%'
+  },
+  {
+    title: '任务名称',
+    dataIndex: 'taskName',
+    width: '15%'
+  },
+  {
+    title: '来源',
+    dataIndex: 'source',
+    width: '15%'
+  },
+  {
+    title: '答错人次',
+    dataIndex: 'email',
+    width: '15%'
+  },
+  {
+    title: 'actions',
+    width: '10%',
+    scopedSlots: { customRender: 'actions' }
+  },
+];
+
+export default {
+  name: 'CourseManageWrongQuestion',
+
+  data() {
+    return {
+      form: {
+        source: undefined,
+        taskName: undefined,
+        sort: undefined
+      },
+      columns,
+      data: [{
+        order: 1,
+        stem: '这是一个题目',
+        taskName: '这是任务名称',
+        source: '这是来源',
+        email: '这是答错人次',
+        actions: '查看详情'
+      }],
+      pagination: {
+        hideOnSinglePage: true
+      },
+      loading: false
+    }
+  },
+
+  methods: {
+    onSearch() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          // do
+        }
+      })
+    },
+
+    handleTableChange(pagination) {
+      const pager = { ...this.pagination };
+      pager.current = pagination.current;
+      this.pagination = pager;
+      // this.fetch({
+      //   results: pagination.pageSize,
+      //   page: pagination.current,
+      //   sortField: sorter.field,
+      //   sortOrder: sorter.order,
+      //   ...filters,
+      // });
+    },
+
+    fetch(params = {}) {
+      this.loading = true;
+      // queryData({
+      //   results: 10,
+      //   ...params,
+      // }).then(({ data }) => {
+      //   const pagination = { ...this.pagination };
+      //   // Read total count from server
+      //   // pagination.total = data.totalCount;
+      //   pagination.total = 200;
+      //   this.loading = false;
+      //   this.data = data.results;
+      //   this.pagination = pagination;
+      // });
+    },
+
+    handleClickViewDetails() {
+
+    }
+  }
+}
+</script>
