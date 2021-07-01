e059e9e119ae4c867b281ea7b7dc8aa54b5154eb
Hongbusi <q951686662@qq.com>
2021-07-01 17:46:52 +0800

    feat: #77860 来源

--- h5/src/h5/containers/wrong-question-book/components/Question.vue
+++ h5/src/h5/containers/wrong-question-book/components/Question.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <head-top />
+
+    <situation />
   </div>
 </template>
 
 <script>
 import HeadTop from './QuestionElement/HeadTop.vue';
+import Situation from './QuestionElement/Situation.vue';
 
 export default {
   components: {
     HeadTop,
+    Situation,
   },
 };
 </script>
---
+++ h5/src/h5/containers/wrong-question-book/components/QuestionElement/Situation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="situation">
+    <div class="situation-top">来源：课程名称-作业课时任务</div>
+    <div class="situation-bottom">
+      <span>2021-04-15 20:20:00</span>
+      <span>做错频次：<span class="frequency">3</span>次</span>
+    </div>
+  </div>
+</template>
