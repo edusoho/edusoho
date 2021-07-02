75dbcc6229b7fd1c0ecf119b0741c63f755ba3a1
hongyongchun <hongyongchun@edusoho.com>
2021-07-02 10:53:28 +0800

    feat: #78291 结构优化

--- app/Resources/static-src/app/vue/views/course_manage/wrong_question/index.vue
+++ app/Resources/static-src/app/vue/views/course_manage/wrong_question/index.vue
@@ -1,62 +1,6 @@
 <template>
   <div>
-    <a-form-model
-      ref="form"
-      :model="form"
-      layout="inline"
-    >
-      <a-form-model-item>
-        <a-select
-          v-model="form.source"
-          style="width: 120px;"
-          placeholder="题目来源"
-        >
-          <a-select-option value="zuoye">
-            作业任务
-          </a-select-option>
-          <a-select-option value="zuoye">
-            作业任务
-          </a-select-option>
-          <a-select-option value="kaoshi">
-            考试任务
-          </a-select-option>
-          <a-select-option value="lianxi">
-            练习任务
-          </a-select-option>
-        </a-select>
-      </a-form-model-item>
-
-      <a-form-model-item>
-        <a-select
-          v-model="form.taskName"
-          style="width: 120px;"
-          placeholder="任务名称"
-        >
-          <a-select-option value="zuoye">
-            作业任务
-          </a-select-option>
-        </a-select>
-      </a-form-model-item>
-
-      <a-form-model-item>
-        <a-select
-          v-model="form.sort"
-          style="width: 120px;"
-          placeholder="答错人次"
-        >
-          <a-select-option value="DES">
-            由高至低
-          </a-select-option>
-          <a-select-option value="ASC">
-            由低至高
-          </a-select-option>
-        </a-select>
-      </a-form-model-item>
-
-      <a-form-model-item>
-        <a-button type="primary" @click="onSearch">搜索</a-button>
-      </a-form-model-item>
-    </a-form-model>
+    <search />
 
     <student-wrong-question-table
       class="mt24"
@@ -72,15 +16,17 @@
 
 <script>
 import { WrongBookStudentWrongQuestion } from 'common/vue/service/index.js';
-import StudentWrongQuestionTable from 'app/vue/views/components/WrongQuestion/StudentWrongQuestionTable.vue';
+import Search from './Search.vue';
 import ViewDetailModal from './ViewDetailModal.vue';
+import StudentWrongQuestionTable from 'app/vue/views/components/WrongQuestion/StudentWrongQuestionTable.vue';
 
 export default {
   name: 'CourseManageWrongQuestion',
 
   components: {
-    StudentWrongQuestionTable,
-    ViewDetailModal
+    Search,
+    ViewDetailModal,
+    StudentWrongQuestionTable
   },
 
   data() {
