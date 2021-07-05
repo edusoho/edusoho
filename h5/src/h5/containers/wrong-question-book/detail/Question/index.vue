1ccefee3a917322120ba8a3a518a9989a82c29a0
Hongbusi <q951686662@qq.com>
2021-07-05 14:41:56 +0800

    优化

--- h5/src/h5/containers/wrong-question-book/detail/Question/index.vue
+++ h5/src/h5/containers/wrong-question-book/detail/Question/index.vue
@@ -20,28 +20,28 @@
           :question="question"
         />
       </div>
-
-      <div class="answer-result">
-        你的回答：未作答
-      </div>
-
-      <div class="right-answer">
-        正确答案：这里是答案
-      </div>
-
-      <div class="question-analysis">
-        <div class="question-analysis__label">题目解析：</div>
-        <div class="question-analysis__result">这里是材料题的解析</div>
-      </div>
-
-      <div class="question-situation">
-        <div class="situation-top">来源：课程名称-作业课时任务</div>
-        <div class="situation-bottom">
-          <span>2021-04-15 20:20:00</span>
-          <span>做错频次：<span class="frequency">3</span>次</span>
-        </div>
-      </div>
     </div>
+
+    <!-- <div class="question-answer-result">
+      你的回答：未作答
+    </div>
+
+    <div class="question-right-answer">
+      正确答案：这里是答案
+    </div>
+
+    <div class="question-analysis">
+      <div class="question-analysis__label">题目解析：</div>
+      <div class="question-analysis__result">这里是材料题的解析</div>
+    </div>
+
+    <div class="question-situation">
+      <div class="situation-top">来源：课程名称-作业课时任务</div>
+      <div class="situation-bottom">
+        <span>2021-04-15 20:20:00</span>
+        <span>做错频次：<span class="frequency">3</span>次</span>
+      </div>
+    </div> -->
   </div>
 </template>
 
