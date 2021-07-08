cf3fb79d24d20f98da0df1c10b7521a69c315e2c
bigabiga_bellorchid <zhongyunchang@yeah.net>
2021-07-08 16:30:55 +0800

    feat: #78163 sonar

--- src/ApiBundle/Api/Resource/WrongBook/WrongBookSourceManageCondition.php
+++ src/ApiBundle/Api/Resource/WrongBook/WrongBookSourceManageCondition.php
@@ -4,10 +4,21 @@ namespace ApiBundle\Api\Resource\WrongBook;
 
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\WrongBook\Service\WrongQuestionService;
 
 class WrongBookSourceManageCondition extends AbstractResource
 {
     public function search(ApiRequest $request, $type, $targetId)
     {
+        $conditions = $request->query->all();
+        $this->getWrongQuestionService()->
+    }
+
+    /**
+     * @return WrongQuestionService
+     */
+    protected function getWrongQuestionService()
+    {
+        return $this->biz->service('WrongBook:WrongQuestionService');
     }
 }
