d0610b3c70774fd7badb7600fcdb533c025fb607
zhaoshanshan <zhaoshanshan@edusoho.com>
2021-07-08 19:02:22 +0800

    feat: #20210708 结果页添加数据

--- h5/src/h5/containers/wrong-question-book/result/index.vue
+++ h5/src/h5/containers/wrong-question-book/result/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrong-question-result">
+    <e-loading v-if="isLoading" />
     <div class="result-data">
       <div class="result-data__item">
         正确率
         <div class="result-data__bottom data-number-green">
-          <span class="data-number">10</span>%
+          <span class="data-number">{{ rightRate }}</span
+          >%
         </div>
       </div>
       <div class="result-data__item">
         做题用时
         <div class="result-data__bottom data-number-gray">
-          <span class="data-number">3</span>分钟
+          <span class="data-number">{{ usedTime }}</span>
+          分钟
         </div>
       </div>
     </div>
@@ -32,8 +35,15 @@
 
     <div class="result-panel">
       <ul class="result-list">
-        <li :class="['result-list__item testpaper-number', `circle-green`]">
-          2
+        <li
+          v-for="(item, index) in reports.item_reports"
+          :key="index"
+          :class="[
+            'result-list__item testpaper-number',
+            `circle-${color[item.question_reports[0].status]}`,
+          ]"
+        >
+          {{ index + 1 }}
         </li>
       </ul>
     </div>
@@ -50,19 +60,72 @@
 </template>
 
 <script>
+import Api from '@/api';
 export default {
   name: 'WrongQuestionResult',
-  data() {},
+  data() {
+    return {
+      isLoading: true,
+      assessment: {},
+      answerScene: {},
+      answerReport: {},
+      answerRecord: {},
+      reports: {},
+      color: {
+        right: 'green',
+        wrong: 'orange',
+        no_answer: 'gray',
+      },
+    };
+  },
+  computed: {
+    usedTime() {
+      const timeInterval = parseInt(this.answerRecord.used_time) || 0;
+      return timeInterval <= 60 ? 1 : Math.round(timeInterval / 60);
+    },
+    rightRate() {
+      return parseInt(
+        (this.reports.right_question_num / this.reports.question_count) * 100,
+      );
+    },
+  },
+
   beforeRouteEnter(to, from, next) {
     document.getElementById('app').style.background = '#f6f6f6';
     next();
   },
+
   beforeRouteLeave(to, from, next) {
     document.getElementById('app').style.background = '';
     next();
   },
 
+  created() {
+    this.fetchAnswerRecord();
+  },
+
   methods: {
+    fetchAnswerRecord() {
+      const query = {
+        answerRecordId: Number(this.$route.query.recordId),
+      };
+      Api.answerRecord({
+        query,
+      })
+        .then(res => {
+          this.assessment = res.assessment;
+          this.answerScene = res.answer_scene;
+          this.answerReport = res.answer_report;
+          this.answerRecord = res.answer_record;
+          this.reports = res.answer_report.section_reports[0];
+          this.isLoading = false;
+        })
+        .catch(err => {
+          this.isLoading = false;
+          this.$toast(err.message);
+        });
+    },
+
     viewAnalysis() {
       this.$router.push({
         name: 'WrongExercisesAnalysis',
