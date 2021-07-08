13517f417af563a4e0fd6fc2036aca42ba42ecb6
shenchupeng <shenchupeng@howzhi.com>
2021-07-08 13:53:06 +0800

    feat: #78583 直播课时排序

--- src/AppBundle/Extensions/DataTag/MultiClassDataTag.php
+++ src/AppBundle/Extensions/DataTag/MultiClassDataTag.php
@@ -6,16 +6,6 @@ use Biz\MultiClass\Service\MultiClassService;
 
 class MultiClassDataTag extends BaseDataTag implements DataTag
 {
-    /**
-     * 获取导航列表.
-     *
-     * 可传入的参数：
-     *   type     必需 导航类型   ['top':'顶部导航'|'foot':'底部导航']
-     *
-     * @param array $arguments 参数
-     *
-     * @return array 导航列表
-     */
     public function getData(array $arguments)
     {
         return $this->getMultiClassService()->getMultiClassByCourseId($arguments['courseId']);
