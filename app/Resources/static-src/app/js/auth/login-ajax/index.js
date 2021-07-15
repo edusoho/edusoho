014c3f8579a0117bee9d76b233a746d3a07d9b33
shenchupeng <shenchupeng@howzhi.com>
2021-07-15 10:02:41 +0800

    feat: #78486 验证码登录

--- app/Resources/static-src/app/js/auth/login-ajax/index.js
+++ app/Resources/static-src/app/js/auth/login-ajax/index.js
@@ -55,7 +55,6 @@ if ($('.js-sts-login-link').length) {
 
 const $loginModal = $('#login-modal');
 $('#sms-login').click((event) => {
-  // $loginModal.modal('hide');
   $.get($(event.currentTarget).data('url'), function (html) {
     $loginModal.html(html);
   });
