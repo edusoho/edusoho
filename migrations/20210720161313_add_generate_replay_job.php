8e56fb460a60ec5f33a63f7b8ec2f3d7b0d97864
shenchupeng <shenchupeng@howzhi.com>
2021-07-20 17:59:10 +0800

    feat: #79258 生成回放定时任务

---
+++ migrations/20210720161313_add_generate_replay_job.php
@@ -0,0 +1,57 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddGenerateReplayJob extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $biz = $this->getContainer();
+        $connection = $biz['db'];
+
+        $currentTime = time();
+        $connection->exec("
+            INSERT INTO `biz_scheduler_job` (
+                `name`,
+                `pool`,
+                `expression`,
+                `class`,
+                `args`,
+                `priority`,
+                `next_fire_time`,
+                `misfire_threshold`,
+                `misfire_policy`,
+                `enabled`,
+                `creator_id`,
+                `updated_time`,
+                `created_time`
+            ) VALUES
+            (
+                'GenerateReplayJob',
+                'default',
+                '0 3 * * *',
+                'Biz\\\\Course\\\\Job\\\\GenerateReplayJob',
+                '',
+                '100',
+                '{$currentTime}',
+                '0',
+                'executing',
+                '1',
+                '0',
+                '{$currentTime}',
+                '{$currentTime}'
+            );
+        ");
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
