94a716eeb01a8928e055399f4426db07bdea994a
bigabiga_bellorchid <zhongyunchang@yeah.net>
2021-07-21 11:28:01 +0800

    fix: #79440 增加课程笔记接口

--- src/ApiBundle/Api/Resource/Course/CourseNote.php
+++ src/ApiBundle/Api/Resource/Course/CourseNote.php
@@ -4,13 +4,26 @@ namespace ApiBundle\Api\Resource\Course;
 
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Course\CourseException;
 use Biz\Course\Service\CourseNoteService;
 use Biz\Course\Service\CourseService;
+use Biz\Course\Service\MemberService;
 
 class CourseNote extends AbstractResource
 {
+    public function get(ApiRequest $request, $courseId, $noteId)
+    {
+        list($course, $member) = $this->getCourseService()->tryTakeCourse($courseId);
+        if ('published' !== $course['status']) {
+            throw CourseException::UNPUBLISHED_COURSE();
+        }
+
+        return $this->getCourseNoteService()->getNote($noteId);
+    }
+
     public function search(ApiRequest $request, $courseId)
     {
+        list($course, $member) = $this->getCourseService()->tryTakeCourse($courseId);
         $conditions = $request->query->all();
         $conditions['courseId'] = $courseId;
         $this->getCourseService()->getCourse($courseId);
@@ -36,5 +49,13 @@ class CourseNote extends AbstractResource
     protected function getCourseNoteService()
     {
         return $this->getBiz()->service('Course:CourseNoteService');
+    }
+
+    /**
+     * @return MemberService
+     */
+    protected function getCourseMemberService()
+    {
+        return $this->getBiz()->service('Course:MemberService');
     }
 }
