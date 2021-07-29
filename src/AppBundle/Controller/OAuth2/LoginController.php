a18ee464aba13902e8e02905fdaede26247efe4c
zsm0620 <2645839086@qq.com>
2021-07-29 19:16:23 +0800

    fix: #210729 验证码失效

--- src/AppBundle/Controller/OAuth2/LoginController.php
+++ src/AppBundle/Controller/OAuth2/LoginController.php
@@ -162,9 +162,7 @@ class LoginController extends LoginBindController
             }
 
             $bindMobile = $request->request->get('originalMobileAccount', '');
-            if ($bindMobile && OAuthUser::MOBILE_TYPE != $oauthUser->accountType){
-                $oauthUser->captchaEnabled = false;
-            }
+            $oauthUser->captchaEnabled = $bindMobile ? false : $oauthUser->captchaEnabled;
             $this->registerAttemptCheck($request);
 
             if ($request->request->get('originalEmailAccount', '') && $request->request->get('originalAccountPassword', '')) {
