23b91cfadf1ee4176b982df4e934f79eab189560
zhangsimin <zhangsimin@edusoho.com>
2021-07-30 01:47:59 -0700

    fix: #210730 test-unit

--- tests/Unit/AppBundle/Component/RateLimit/RegisterSmsRateLimiterTest.php
+++ tests/Unit/AppBundle/Component/RateLimit/RegisterSmsRateLimiterTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Unit\AppBundle\Component\RateLimit;
 
-use Biz\BaseTestCase;
 use AppBundle\Component\RateLimit\RegisterSmsRateLimiter;
+use Biz\BaseTestCase;
 use Biz\Common\BizCaptcha;
 
 class RegisterSmsRateLimiterTest extends BaseTestCase
@@ -12,42 +12,42 @@ class RegisterSmsRateLimiterTest extends BaseTestCase
     {
         $limiter = new RegisterSmsRateLimiter($this->biz);
         $request = $this->mockRequest(
-            array(
-                'request' => array(
+            [
+                'request' => [
                     'mobile' => '13967340627',
                     'dragCaptchaToken' => 'kuozhi',
                     'phrase' => 'password',
                     'unique' => 'true',
-                ),
+                ],
                 'getClientIp' => '128.2.2.1',
-            )
+            ]
         );
 
         $captcha = $this->mockBiz(
             'biz_drag_captcha',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'check',
-                    'withParams' => array('kuozhi'),
+                    'withParams' => ['kuozhi'],
                     'returnValue' => BizCaptcha::STATUS_SUCCESS,
-                ),
-            )
+                ],
+            ]
         );
 
         $userService = $this->mockBiz(
             'User:UserService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'getSmsRegisterCaptchaStatus',
-                    'withParams' => array('128.2.2.1'),
+                    'withParams' => ['128.2.2.1'],
                     'returnValue' => 'captchaRequired',
-                ),
-                array(
+                ],
+                [
                     'functionName' => 'isMobileUnique',
                     'withParams' => '',
                     'returnValue' => true,
-                ),
-            )
+                ],
+            ]
         );
 
         $this->biz['biz_drag_captcha'] = $captcha;
@@ -64,42 +64,42 @@ class RegisterSmsRateLimiterTest extends BaseTestCase
     {
         $limiter = new RegisterSmsRateLimiter($this->biz);
         $request = $this->mockRequest(
-            array(
-                'request' => array(
+            [
+                'request' => [
                     'captchaToken' => 'kuozhi',
                     'phrase' => 'password',
                     'mobile' => '13967340627',
                     'unique' => 'true',
-                ),
+                ],
                 'getClientIp' => '128.2.2.1',
-            )
+            ]
         );
 
         $captcha = $this->mockBiz(
             'biz_captcha',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'check',
-                    'withParams' => array('kuozhi', 'password'),
+                    'withParams' => ['kuozhi', 'password'],
                     'returnValue' => BizCaptcha::STATUS_SUCCESS,
-                ),
-            )
+                ],
+            ]
         );
 
         $userService = $this->mockBiz(
             'User:UserService',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'getSmsRegisterCaptchaStatus',
-                    'withParams' => array('128.2.2.1'),
+                    'withParams' => ['128.2.2.1'],
                     'returnValue' => 'captchaIgnore',
-                ),
-                array(
+                ],
+                [
                     'functionName' => 'isMobileUnique',
                     'withParams' => '',
                     'returnValue' => true,
-                ),
-            )
+                ],
+            ]
         );
 
         $this->biz['biz_captcha'] = $captcha;
