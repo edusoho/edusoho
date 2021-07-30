f035789de1d2f926adc8ad4c5bad56cd0c6feb70
zsm0620 <2645839086@qq.com>
2021-07-30 16:26:39 +0800

    fix: #210730 fix

--- tests/Unit/AppBundle/Component/RateLimit/RegisterSmsRateLimiterTest.php
+++ tests/Unit/AppBundle/Component/RateLimit/RegisterSmsRateLimiterTest.php
@@ -17,6 +17,7 @@ class RegisterSmsRateLimiterTest extends BaseTestCase
                     'mobile' => '13967340627',
                     'dragCaptchaToken' => 'kuozhi',
                     'phrase' => 'password',
+                    'unique' => 'true',
                 ),
                 'getClientIp' => '128.2.2.1',
             )
