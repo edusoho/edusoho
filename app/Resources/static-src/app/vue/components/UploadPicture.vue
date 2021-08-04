282566d34d34752a7a42d1b3c9f5e78c076dd267
hongyongchun <hongyongchun@edusoho.com>
2021-08-04 19:47:49 +0800

    feat: #80015 上传图片

---
+++ app/Resources/static-src/app/vue/components/UploadPicture.vue
@@ -0,0 +1,200 @@
+<template>
+  <div>
+    <a-upload
+      ref="upload"
+      accept="image/*"
+      :file-list="[]"
+      list-type="picture-card"
+      :customRequest="() => {}"
+      @change="uploadPicture"
+    >
+      <img v-if="pictureUrl" :src="pictureUrl" style="width: 100%;" />
+      <div v-else>
+        <a-icon :type="loading ? 'loading' : 'plus'" />
+        <div class="ant-upload-text">
+          上传照片
+        </div>
+      </div>
+    </a-upload>
+    <p class="mb0" style="font-size: 14px; line-height: 20px;">{{ tip }}</p>
+
+    <a-modal
+      :title="title"
+      :key="cropperKey"
+      :visible="visible"
+      :mask-closable="false"
+      @cancel="handleCancel"
+    >
+      <vue-cropper
+        ref="cropper"
+        :view-mode="3"
+        :aspect-ratio="aspectRatio"
+        :src="imgUrl"
+      />
+      <template slot="footer">
+        <a-button @click="handleReselect">重新选择</a-button>
+        <a-button type="primary" @click="handleSaveCropper" :loading="uploadLoading">保存图片</a-button>
+      </template>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+import { UploadToken, File } from 'common/vue/service/index.js';
+
+export default {
+  name: 'CropPicture',
+
+  components: {
+    VueCropper
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: '图片裁剪',
+    },
+
+    tip: {
+      type: String,
+      default: '',
+    },
+
+    aspectRatio: {
+      type: Number
+    }
+  },
+
+  data() {
+    return {
+      loading: false,
+      pictureName: '',
+      pictureUrl: '', // 裁剪后图片
+      imgUrl: '', // 原图片
+      visible: false,
+      uploadLoading: false,
+      uploadToken: {},
+      cropperKey: 0,
+      imgs: {}
+    }
+  },
+
+  methods: {
+    async getUploadToken() {
+      this.uploadToken = await UploadToken.get('default');
+    },
+
+    uploadPicture(info) {
+      this.cropperKey++;
+
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        this.imgUrl = event.target.result;
+        this.visible = true;
+      };
+
+      this.pictureName = info.file.originFileObj.name;
+      reader.readAsDataURL(info.file.originFileObj);
+    },
+
+    handleCancel() {
+      this.visible = false;
+    },
+
+    handleReselect() {
+      const $inputs = this.$refs.upload.$el.getElementsByTagName('input');
+
+      this.visible = false;
+
+      if ($inputs.length > 0) {
+        $inputs[0].click();
+      }
+    },
+
+    async handleSaveCropper() {
+      this.loading = true;
+
+      if (!this.uploadToken.expiry || (new Date() >= new Date(this.uploadToken.expiry))) {
+        await this.getUploadToken();
+      }
+
+      const cropper = this.$refs.cropper;
+
+      cropper.getCroppedCanvas().toBlob(async blob => {
+        const { x, y, width, height } = cropper.getData();
+        const { naturalWidth, naturalHeight } = cropper.getImageData();
+
+        const cropperData = {
+          x: _.ceil(_.max([0, x])),
+          y: _.ceil(_.max([0, y])),
+          width: _.ceil(width),
+          height: _.ceil(height)
+        };
+
+        const cropResult = {
+          x: cropperData.x,
+          y: cropperData.y,
+          x2: _.add(cropperData.x, cropperData.width),
+          y2: _.add(cropperData.y, cropperData.height),
+          w: cropperData.width, // 裁剪后宽度
+          h: cropperData.height, // 裁剪后高度
+          'imgs[large][0]': 480,
+          'imgs[large][1]': 270,
+          'imgs[middle][0]': 304,
+          'imgs[middle][1]': 171,
+          'imgs[small][0]': 96,
+          'imgs[small][1]': 54,
+          post: false,
+          width: naturalWidth, // 原图片宽度
+          height: naturalHeight, // 原图片高度
+          group: 'course',
+          post: false
+        };
+        const formData = new FormData();
+
+        formData.append('file', blob, this.pictureName);
+        formData.append('token', this.uploadToken.token);
+
+        this.uploadLoading = true;
+
+        try {
+          const { url } = await File.uploadFile(formData);
+
+          this.pictureUrl = url;
+
+          const formData1 = new FormData();
+          for(const key in cropResult) {
+            formData1.append(key, cropResult[key]);
+          }
+
+          this.imgs = await File.imgCrop(formData1);
+        } finally {
+          this.loading = false;
+          this.visible = false;
+          this.uploadLoading = false;
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "~common/variable.less";
+
+.ant-upload-select-picture-card {
+  i {
+    font-size: 32px;
+    color: @gray;
+  }
+}
+
+.ant-upload-select-picture-card .ant-upload-text {
+  margin-top: 8px;
+  color: @gray-dark;
+}
+</style>
--- app/Resources/static-src/app/vue/views/components/Teacher/EditorCertificate.vue
+++ app/Resources/static-src/app/vue/views/components/Teacher/EditorCertificate.vue
@@ -8,45 +8,23 @@
       :wrapper-col="wrapperCol"
     >
       <a-form-model-item label="照片" prop="region">
-        <a-upload
-          ref="upload"
-          accept="image/*"
-          :file-list="[]"
-          list-type="picture-card"
-          :customRequest="() => {}"
-          @change="uploadTeacherPicture"
-        >
-          <img v-if="teacherPictureUrl" :src="teacherPictureUrl" />
-          <div v-else>
-            <a-icon :type="loading ? 'loading' : 'plus'" />
-            <div class="ant-upload-text">
-              上传照片
-            </div>
-          </div>
-        </a-upload>
+        <upload-picture
+          :aspect-ratio="1 / 1"
+          tip="请上传jpg, gif, png格式的图片，建议图片尺寸为 270×270px，建议图片大小不超过2MB"
+        />
       </a-form-model-item>
     </a-form-model>
-
-    <cropper-modal
-      :visible="cropperModalVisible"
-      :img-url="imgUrl"
-      :img-name="teacherPictureName"
-      :aspect-ratio="1 / 1"
-      @cancal="handleCancel"
-      @reselect="handleReselect"
-      @save="handlesave"
-    />
   </div>
 </template>
 
 <script>
-import CropperModal from 'app/vue/views/components/CropperModal.vue';
+import UploadPicture from 'app/vue/components/UploadPicture.vue';
 
 export default {
   name: 'EditorTeacherCertificate',
 
   components: {
-    CropperModal
+    UploadPicture
   },
 
   data() {
@@ -64,12 +42,7 @@ export default {
       },
       rules: {
 
-      },
-      loading: false,
-      teacherPictureName: '',
-      teacherPictureUrl: '',
-      imgUrl: '',
-      cropperModalVisible: false
+      }
     }
   },
 
@@ -87,53 +60,7 @@ export default {
 
     resetForm() {
       this.$refs.ruleForm.resetFields();
-    },
-
-    uploadTeacherPicture(info) {
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        this.imgUrl = event.target.result;
-        this.cropperModalVisible = true;
-      };
-
-      this.teacherPictureName = info.file.originFileObj.name;
-      reader.readAsDataURL(info.file.originFileObj);
-    },
-
-    handleCancel() {
-      this.cropperModalVisible = false;
-    },
-
-    handleReselect() {
-      const $inputs = this.$refs.upload.$el.getElementsByTagName('input');
-
-      this.cropperModalVisible = false;
-
-      if ($inputs.length > 0) {
-        $inputs[0].click()
-      }
-    },
-
-    handlesave(obj) {
-      console.log(obj);
     }
   }
 }
 </script>
-
-<style lang="less" scoped>
-@import "~common/variable.less";
-
-.ant-upload-select-picture-card {
-  i {
-    font-size: 32px;
-    color: @gray;
-  }
-}
-
-.ant-upload-select-picture-card .ant-upload-text {
-  margin-top: 8px;
-  color: @gray-dark;
-}
-</style>
