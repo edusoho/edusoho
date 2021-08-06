256ea8dd4c625746108f056588599538a34285ed
shenchupeng <shenchupeng@howzhi.com>
2021-08-06 15:28:24 +0800

    feat: #80214 直播回调

---
+++ src/AppBundle/Controller/Callback/LiveController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller\Callback;
+
+use AppBundle\Common\JWTAuth;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class LiveController extends BaseController
+{
+    public function handle(Request $request)
+    {
+        $this->validToken($request);
+        $event = $request->request->get('event');
+        return new JsonResponse(['success' => true]);
+    }
+
+    protected function validToken($request)
+    {
+        $token = $request->headers->get('Authorization');
+        $result = $this->getJWTAuth()->auth($token);
+        if (!$result) {
+            throw new BadRequestHttpException('Token Error');
+        }
+    }
+
+    protected function getJWTAuth()
+    {
+        $setting = $this->setting('storage', []);
+        $secretKey = !empty($setting['cloud_secret_key']) ? $setting['cloud_secret_key'] : '';
+
+        return new JWTAuth($secretKey);
+    }
+}
