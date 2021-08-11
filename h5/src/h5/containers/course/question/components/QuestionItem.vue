ea630480905a929487e7d79c9adafce304fe0ed6
Hongbusi <q951686662@qq.com>
2021-08-11 17:14:34 +0800

    feat: #80058 问答完善

---
+++ h5/src/h5/containers/course/question/components/QuestionItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="question-item">
+    <h3 class="question-title text-overflow">Photoshop基础入门班级课程阿建设大街爱Photoshop基础入门班级课程阿建设大街爱好</h3>
+    <p class="question-desc">Photoshop基础入门班级Photoshop基础入门班级Photoshop基础入门班级Photoshop基础入门班级Photoshop基础入门班级…Photoshop基础入门班级Photoshop基础入门班级Photoshop基础入门班级Photoshop基础入门班级Photoshop基础入门班级…</p>
+    <div class="question-details">
+      <div class="question-details__left">
+        <img class="user-avatar" src="">
+        <span>曲敬良发起 03/21 20:25</span>
+      </div>
+      <div class="question-details__right">1.4k</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionItem'
+}
+</script>
+
+<style lang="scss" scoped>
+.question-item {
+  padding: vw(16);
+  border-bottom: 1px solid #f5f5f5;
+
+  .question-title {
+    margin: 0;
+    font-size: vw(16);
+    font-weight: 500;
+    color: #333;
+    line-height: vw(24);
+  }
+
+  .question-desc {
+    margin-top: vw(8);
+    font-size: vw(12);
+    color: #666;
+    line-height: vw(16);
+    @include text-overflow(2);
+  }
+
+  .question-details {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: vw(8);
+    font-size: vw(14);
+    line-height: vw(20);
+
+    &__left {
+      display: flex;
+      align-items: center;
+      color: #666;
+
+      .user-avatar {
+        margin-right: vw(4);
+        width: vw(24);
+        height: vw(24);
+        border-radius: 50%;
+      }
+    }
+
+    &__right {
+      color: #999;
+    }
+  }
+}
+</style>
--- h5/src/h5/containers/course/question/index.vue
+++ h5/src/h5/containers/course/question/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>问答</div>
+  <div class="question">
+    <question-item v-for="question in 10" :key="question" />
+
+    <div class="question-btn">
+      <van-button type="primary" block>发起问答</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import QuestionItem from './components/QuestionItem.vue';
+
 export default {
-  name: 'Question'
+  name: 'Question',
+
+  components: {
+    QuestionItem
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.question {
+  padding-bottom: vw(80);
+
+  &-btn {
+    position: fixed;
+    bottom: vw(16);
+    left: 50%;
+    transform: translateX(-50%);
+    width: vw(340);
+
+    .van-button {
+      box-shadow: 0px 2px 6px 0px rgba(64, 143, 251, 0.5);
+      border-radius: 8px;
+      font-size: vw(16);
+    }
+  }
+}
+</style>
