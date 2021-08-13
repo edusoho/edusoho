a94bb911e71c7b316cb8854c001ddd84837139a8
Hongbusi <q951686662@qq.com>
2021-08-13 16:00:31 +0800

    feat: #80058 tabs 优化

--- h5/src/h5/router/index.js
+++ h5/src/h5/router/index.js
@@ -940,26 +940,6 @@ const routes = [
         /* webpackChunkName: "wrong-question-book" */ '@/containers/wrong-question-book/wrong-exercises/analysis.vue'
       ),
   },
-
-  // -------------------------------
-  {
-    path: '/course/discussion/detail',
-    name: 'DiscussionDetail',
-    meta: {
-      title: '查看详情',
-      keepAlive: true
-    },
-    component: () => import(/* webpackChunkName: "course-discussion" */ '@/containers/course/discussion/detail.vue')
-  },
-  {
-    path: '/course/discussion/create',
-    name: 'CreateDiscussion',
-    meta: {
-      title: '创建话题',
-      keepAlive: true
-    },
-    component: () => import(/* webpackChunkName: "course-discussion" */ '@/containers/course/discussion/create.vue')
-  }
 ];
 
 // 页面刷新，store数据会被清掉，需对token、user重新赋值
