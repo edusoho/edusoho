cd8a00ef147f3e417236c45502abb92bb1464f0a
Hongbusi <q951686662@qq.com>
2021-08-13 18:17:35 +0800

    feat: #80058 话题详情优化

--- h5/src/h5/containers/course/discussion/detail.vue
+++ h5/src/h5/containers/course/discussion/detail.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <van-button type="primary" @click="handleClickGoToList">返回</van-button>
+  <div class="discussion-detail">
+    <h3>f </h3>
+
+    <div class="reply">
+      <van-field
+        ref="replyInput"
+        v-model="value"
+        placeholder="回复..."
+        @keyup.enter="handleClickEnter"
+      />
+    </div>
   </div>
 </template>
 
@@ -8,10 +17,38 @@
 export default {
   name: 'DiscussionDetail',
 
+  data() {
+    return {
+      value: ''
+    }
+  },
+
   methods: {
     handleClickGoToList() {
       this.$emit('change-current-component', { component: 'List' });
+    },
+
+    handleClickEnter() {
+      // alert(this.value)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.discussion-detail {
+
+  .reply {
+    position: fixed;
+    bottom: vw(8);
+    left: 50%;
+    transform: translateX(-50%);
+    width: vw(340);
+
+    .van-cell {
+      background: #f5f5f5;
+      border-radius: 24px;
+    }
+  }
+}
+</style>
