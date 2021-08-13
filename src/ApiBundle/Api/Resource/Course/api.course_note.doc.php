b095b075772c7fcabd1cbbd04f5d3c63e2c267a9
bigabiga_bellorchid <zhongyunchang@yeah.net>
2021-08-13 15:46:35 +0800

    fix: #52258 add api

--- src/ApiBundle/Api/Resource/Course/api.course_note.doc.php
+++ src/ApiBundle/Api/Resource/Course/api.course_note.doc.php
@@ -2,7 +2,7 @@
 
 /**
  * @OA\Get(
- *     path="/api/course/{courseId}/notes/{noteId}",
+ *     path="/api/courses/{courseId}/notes/{noteId}",
  *     tags={"course"},
  *     summary="课程单个笔记获取接口",
  *     @OA\Response(
@@ -16,7 +16,7 @@
  * )
  *
  * @OA\Get(
- *     path="/api/course/{courseId}/notes",
+ *     path="/api/courses/{courseId}/notes",
  *     tags={"course"},
  *     summary="课程多个笔记获取接口",
  *     @OA\Response(
@@ -47,15 +47,14 @@
  *      schema="course.note",
  *      title="course.note",
  *      description="课程笔记",
- *      @OA\Property(property="id",title="id",description="ID",type="integer"),
- *      @OA\Property(property="userId",title="userId",description="userId",type="integer"),
- *      @OA\Property(property="taskId",title="taskId",description="taskId",type="integer"),
+ *      @OA\Property(property="id",title="id",description="ID",type="number"),
+ *      @OA\Property(property="userId",title="userId",description="userId",type="number"),
+ *      @OA\Property(property="taskId",title="taskId",description="taskId",type="number"),
  *      @OA\Property(property="content",title="content",description="content",type="string"),
- *      @OA\Property(property="length",title="length",description="length",type="integer"),
- *      @OA\Property(property="likeNum",title="likeNum",description="likeNum",type="integer"),
- *      @OA\Property(property="createdTime",title="createdTime",description="创建时间",type="datetime"),
- *      @OA\Property(property="updatedTime",title="updatedTime",description="更新时间",type="datetime"),
+ *      @OA\Property(property="length",title="length",description="length",type="number"),
+ *      @OA\Property(property="likeNum",title="likeNum",description="likeNum",type="number"),
+ *      @OA\Property(property="createdTime",title="createdTime",description="创建时间",type="date-time"),
+ *      @OA\Property(property="updatedTime",title="updatedTime",description="更新时间",type="date-time"),
  *      @OA\Property(property="user",title="id",ref="#/components/schemas/user.simple")
  * )
- *
  */
