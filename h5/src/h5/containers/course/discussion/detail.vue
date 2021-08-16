ac0f91722092ce13c5c6df92c814d79f8c4df229
Hongbusi <q951686662@qq.com>
2021-08-16 16:38:11 +0800

    feat: #80058 话题问答完善

--- h5/src/h5/containers/course/discussion/detail.vue
+++ h5/src/h5/containers/course/discussion/detail.vue
@@ -11,6 +11,29 @@
       <span class="detail-header__btn">回复</span>
     </div>
 
+    <div class="discussion-body">
+      <div class="discussion-body__info">
+        <img class="avatar" src="http://try6.edusoho.cn/files/user/2021/04-22/1528579ec515309441.png">
+        <div class="info-right">
+          <span class="info-nickname">曲敬良发起</span>
+          <span class="info-time">20:25</span>
+        </div>
+      </div>
+      <div class="discussion-body__title">Photoshop基础入门班级课程阿建设大街</div>
+      <div class="discussion-body__content">
+        Photoshop基础入门班级Photoshop基础入门班级Photoshop础入门班级Photoshop基础入门班级Photoshop基础入门班级大开杀戒打卡机卡萨丁就爱看山东矿机奥斯卡了低级趣味坡地区欧派我看到强迫我看懂情况的迫切看我的皮卡丘颇为读卡器坡屋顶看破情况的迫切我看到我都快亲我阿斯达是阿
+      </div>
+    </div>
+
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="fetchCourseThreadPost"
+    >
+      <reply-item v-for="item in replyList" :key="item.id" :item="item" />
+    </van-list>
+
     <div class="reply">
       <van-field
         ref="replyInput"
@@ -25,9 +48,14 @@
 <script>
 import Api from '@/api';
 import _ from 'lodash';
+import ReplyItem from './components/ReplyItem.vue'
 
 export default {
   name: 'DiscussionDetail',
+
+  components: {
+    ReplyItem
+  },
 
   props: {
     id: {
@@ -39,7 +67,10 @@ export default {
   data() {
     return {
       content: '',
-      courseId: this.$route.params.id
+      courseId: this.$route.params.id,
+      loading: false,
+      finished: false,
+      replyList: []
     }
   },
 
@@ -80,7 +111,9 @@ export default {
           content
         }
       });
-      console.log(result);
+
+      this.replyList.unshift(result);
+      this.content = '';
     }
   }
 }
@@ -88,6 +121,7 @@ export default {
 
 <style lang="scss" scoped>
 .discussion-detail {
+  padding-bottom: vw(60);
 
   .detail-header {
     display: flex;
@@ -108,6 +142,56 @@ export default {
     }
   }
 
+
+  .discussion-body {
+    padding: vw(2) vw(16) vw(16);
+    border-bottom: vw(8) solid #f5f5f5;
+
+    &__info {
+      display: flex;
+
+      .avatar {
+        margin-right: vw(8);
+        width: vw(42);
+        height: vw(42);
+        border-radius: 50%;
+      }
+
+      .info-right {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+
+        .info-nickname {
+          font-size: vw(14);
+          color: #666;
+          line-height: vw(20);
+        }
+
+        .info-time {
+          font-size: vw(12);
+          color: #999;
+          line-height: vw(16);
+        }
+      }
+    }
+
+    &__title {
+      margin-top: vw(8);
+      font-size: vw(16);
+      font-weight: 500;
+      color: #333;
+      line-height: vw(24);
+    }
+
+    &__content {
+      margin-top: vw(8);
+      font-size: vw(12);
+      color: #666;
+      line-height: vw(16);
+    }
+  }
+
   .reply {
     position: fixed;
     bottom: vw(8);
@@ -119,6 +203,10 @@ export default {
       background: #f5f5f5;
       border-radius: 24px;
     }
+  }
+
+  .van-list {
+    margin-top: 0;
   }
 }
 </style>
