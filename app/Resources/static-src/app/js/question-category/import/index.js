2c648e5d9c58c5d4a99c63ad50929c58bca09e8c
秦瑞 <qinrui@edusoho.com>
2021-08-23 03:10:36 -0700

    feat: #77602 上传题目到对应分类

--- app/Resources/static-src/app/js/question-category/import/index.js
+++ app/Resources/static-src/app/js/question-category/import/index.js
@@ -1,9 +1,3 @@
-
-$('.js-category-set').click((element) => {
-  let categoryId = $('#select_category').val();
-  $('#category_Id').val(categoryId);
-});
-
 $('.js-item-create').click((element) => {
   let categoryId = $('#select_category').val();
   let importUrl = $(element.currentTarget).data('url')
