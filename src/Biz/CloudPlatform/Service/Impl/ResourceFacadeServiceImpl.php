4f936f2e46a3596420a2c38ac7f9392594be3cb6
cdd <chendongdong@howzhi.com>
2021-08-24 18:51:24 +0800

    fix: #20210727 修复使用本地存储时, 视频不能播放问题

--- src/Biz/CloudPlatform/Service/Impl/ResourceFacadeServiceImpl.php
+++ src/Biz/CloudPlatform/Service/Impl/ResourceFacadeServiceImpl.php
@@ -53,11 +53,7 @@ class ResourceFacadeServiceImpl extends BaseFacade implements ResourceFacadeServ
             $payload['encrypt'] = 3;
         }
 
-        $context['token'] = '';
-        if ('cloud' == $file['storage']) {
-            $context['token'] = $this->makePlayToken($file, 600, $payload);
-        }
-
+        $context['token'] = 'cloud' == $file['storage'] ? $this->makePlayToken($file, 600, $payload) : '';
         $context['resNo'] = $file['globalId'];
 
         //转码状态
