fead9b1dfe791b2fa1e2d561f3f396c907e4f40c
zsm0620 <2645839086@qq.com>
2021-09-01 15:37:24 +0800

    feat: #81248 migration

--- src/Biz/OperationStatistic/Service/Impl/OperationCountStatisticServiceImpl.php
+++ src/Biz/OperationStatistic/Service/Impl/OperationCountStatisticServiceImpl.php
@@ -5,9 +5,17 @@ namespace Biz\OperationStatistic\Service\Impl;
 
 
 use Biz\BaseService;
+use Biz\OperationStatistic\Dao\OperationCountStatisticDao;
 use Biz\OperationStatistic\Service\OperationCountStatisticService;
 
 class OperationCountStatisticServiceImpl extends BaseService implements OperationCountStatisticService
 {
 
+    /**
+     * @return OperationCountStatisticDao
+     */
+    protected function getOperationCountStatisticDao()
+    {
+        return $this->createDao('OperationStatistic:OperationCountStatisticDao');
+    }
 }
