137ff68cce851718e58171c108f0ae6cf6910a8b
zhangsimin <zhangsimin@edusoho.com>
2021-08-31 19:55:04 -0700

    feat: #81248 commit1

---
+++ migrations/20210901105433_add_operation_related_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddOperationRelatedTable extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
