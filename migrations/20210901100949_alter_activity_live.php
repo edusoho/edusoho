11eed93cd9c06564bda37c037c57bad8af693d1b
秦瑞 <qinrui@edusoho.com>
2021-09-07 02:10:42 -0700

    feat: #2100902 migrations

--- migrations/20210901100949_alter_activity_live.php
+++ migrations/20210901100949_alter_activity_live.php
@@ -12,8 +12,8 @@ class AlterActivityLive extends Migration
         $biz = $this->getContainer();
         $biz['db']->exec("
             ALTER TABLE `activity_live` MODIFY COLUMN `replayStatus` enum('ungenerated','generating','generated','videoGenerated','failure') NOT NULL DEFAULT 'ungenerated' COMMENT '回放状态' AFTER `liveProvider`;
-            ALTER TABLE `activity_live` ADD COLUMN `liveSatrtTime` int(10) NOT NULL DEFAULT 0 COMMENT '直播开始时间' AFTER `progressStatus`;
-            ALTER TABLE `activity_live` ADD COLUMN `liveEndTime` int(10) NOT NULL DEFAULT 0 COMMENT '直播开始时间' AFTER `liveSatrtTime`;
+            ALTER TABLE `activity_live` ADD COLUMN `liveStartTime` int(10) NOT NULL DEFAULT 0 COMMENT '直播开始时间' AFTER `progressStatus`;
+            ALTER TABLE `activity_live` ADD COLUMN `liveEndTime` int(10) NOT NULL DEFAULT 0 COMMENT '直播开始时间' AFTER `liveStartTime`;
             ALTER TABLE `activity_live` ADD COLUMN `replayTagId` int(10) NOT NULL DEFAULT 0 COMMENT '回放标签ID' AFTER `liveEndTime`;
             ALTER TABLE `activity_live` ADD COLUMN `replayPublic` tinyint(4) NOT NULL DEFAULT 0 COMMENT '回放是否共享' AFTER `replayTagId`;
             ALTER TABLE `activity_live` ADD COLUMN `anchorId` int(10) NOT NULL DEFAULT 0 COMMENT '主讲人Id' AFTER `replayPublic`;
