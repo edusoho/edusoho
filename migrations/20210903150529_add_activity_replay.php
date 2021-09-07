38d1ca0f27e6a2e41bba749dad1421cedce4fc40
秦瑞 <qinrui@edusoho.com>
2021-09-06 22:40:14 -0700

    feat: #2100902 migrations

--- migrations/20210903150529_add_activity_replay.php
+++ migrations/20210903150529_add_activity_replay.php
@@ -13,27 +13,27 @@ class AddActivityReplay extends Migration
         $biz['db']->exec("
             CREATE TABLE IF NOT EXISTS `activity_replay`  (
                   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-                  `finishType` varchar(32) NOT NULL DEFAULT 'end' COMMENT '完成类型',
-                  `finishDetail` varchar(32) NOT NULL DEFAULT '' COMMENT '完成条件',
-                  `originLessonId` int(10) NOT NULL DEFAULT 0 COMMENT '引用课时ID',
-                  `createdTime` int(10) NOT NULL COMMENT '创建时间',
-                  `updatedTime` int(10) NOT NULL COMMENT '最后更新时间',
+                  `finish_type` varchar(32) NOT NULL DEFAULT 'end' COMMENT '完成类型',
+                  `finish_detail` varchar(32) NOT NULL DEFAULT '' COMMENT '完成条件',
+                  `origin_lessonId` int(10) NOT NULL DEFAULT 0 COMMENT '引用课时ID',
+                  `created_time` int(10) NOT NULL COMMENT '创建时间',
+                  `updated_time` int(10) NOT NULL COMMENT '最后更新时间',
                   PRIMARY KEY (`id`)
                   )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT = '直播回放活动';
 
              CREATE TABLE IF NOT EXISTS `classroom_live_group`  (
                   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-                  `classroomId` int(10) NOT NULL COMMENT '班级ID',
-                  `liveCode` varchar(64) NOT NULL DEFAULT '' COMMENT '直播分组ID',
-                  `liveId` int(10) NOT NULL DEFAULT 0 COMMENT '直播ID',
-                  `createdTime` int(10) NOT NULL COMMENT '创建时间',
+                  `classroom_id` int(10) NOT NULL COMMENT '班级ID',
+                  `live_code` varchar(64) NOT NULL DEFAULT '' COMMENT '直播分组ID',
+                  `live_id` int(10) NOT NULL DEFAULT 0 COMMENT '直播ID',
+                  `created_time` int(10) NOT NULL COMMENT '创建时间',
                   PRIMARY KEY (`id`)
                   )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT = '班级直播分组';
 
              CREATE TABLE IF NOT EXISTS `lesson_marker`  (
                   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-                  `lessonId` int(10) NOT NULL DEFAULT end COMMENT '教学活动ID',
-                  `markerId` int(10) NOT NULL DEFAULT 0 COMMENT '驻点ID',
+                  `lesson_id` int(10) NOT NULL DEFAULT end COMMENT '教学活动ID',
+                  `marker_id` int(10) NOT NULL DEFAULT 0 COMMENT '驻点ID',
                   PRIMARY KEY (`id`)
                   )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT = '课时弹题';
             ");
