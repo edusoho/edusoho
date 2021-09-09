f8400eeba5a36c1ee54e853197b0bdd9423185f3
zhongyunchang <zhongyunchang@yeah.net>
2021-09-09 09:14:53 +0800

    fix: #52258 修改自研直播回放功能

--- src/ApiBundle/Api/Resource/Course/CourseLiveReplay.php
+++ src/ApiBundle/Api/Resource/Course/CourseLiveReplay.php
@@ -20,6 +20,8 @@ class CourseLiveReplay extends AbstractResource
 
         return [
             'url' => $replay['url'],
+            'token' => $replay['token'],
+            'roomId' => $replay['roomId'],
             'type' => 'selfLive', //目前只有自研直播，多供应商直播支持后，可以扩展类型
         ];
     }
