19858d53c83bec121efbc2614e2d236e01b54247
cdd <chendongdong@howzhi.com>
2021-10-18 14:51:55 +0800

    fix: #211018 直播回放资源列表接口和修改接口

---
+++ src/ApiBundle/Api/Resource/LiveReplay/LiveReplay.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace ApiBundle\Api\Resource\LiveReplay;
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+use AppBundle\Common\ArrayToolkit;
+use Biz\Activity\Service\ActivityService;
+use Biz\Activity\Service\LiveActivityService;
+use Biz\Course\Service\CourseSetService;
+use Biz\User\Service\UserService;
+
+class LiveReplay extends AbstractResource
+{
+    public function update(ApiRequest $request, $id)
+    {
+        $fields = $this->filterBaseFields($request->request->all());
+    }
+
+    protected function filterBaseFields($fields)
+    {
+    }
+
+    public function search(ApiRequest $request)
+    {
+        $conditions = $request->query->all();
+        $conditions = $this->filterReplayCondition($conditions);
+        list($offset, $limit) = $this->getOffsetAndLimit($request);
+        $activities = $this->getActivityService()->search($conditions, ['createdTime' => 'DESC'], $offset, $limit);
+        $liveActivities = $this->getActivityService()->findActivities(ArrayToolkit::column($activities, 'id'), true);
+        $liveActivities = $this->handleActivityReplay($liveActivities);
+
+        return $liveActivities;
+    }
+
+    protected function handleActivityReplay($liveActivities)
+    {
+        $activitiesList = [];
+        foreach ($liveActivities as $activity) {
+            if (isset($activity['ext'])) {
+                $user = $this->getUserService()->getUser($activity['ext']['anchorId']);
+                $liveTime = $activity['ext']['liveEndTime'] - $activity['ext']['liveStartTime'];
+                $activitiesList[] = [
+                    'id' => $activity['id'],
+                    'title' => $activity['title'],
+                    'liveStartTime' => empty($activity['ext']['liveStartTime']) ? '-' : date('Y-m-d H:i:s', $activity['ext']['liveStartTime']),
+                    'liveTime' => empty($liveTime) ? '-' : round($liveTime / 60, 1),
+                    'liveSecond' => $liveTime,
+                    'anchor' => empty($user['nickname']) ? '-' : $user['nickname'],
+                ];
+            }
+        }
+
+        return $activitiesList;
+    }
+
+    protected function filterReplayCondition($conditions)
+    {
+        $currentUser = $this->getCurrentUser();
+
+        if ($currentUser->isAdmin()) {
+            $liveActivity = $this->getLiveActivityService()->findLiveActivitiesByReplayStatus();
+        } else {
+            $liveActivity = $this->getLiveActivityService()->findLiveActivitiesByIsPublic();
+        }
+
+        $activityPublic = [];
+        $liveActivitiesIds = ArrayToolkit::column($liveActivity, 'id');
+        if (!empty($liveActivitiesIds)) {
+            $activityPublic = $this->getActivityService()->findActivitiesByMediaIdsAndMediaType(ArrayToolkit::column($liveActivity, 'id'), 'live');
+        }
+
+        $activityCreator = $this->getActivityService()->findActivitiesByCourseSetIdAndType($conditions['courseSetId'], 'live');
+        $activityIds = ArrayToolkit::column(array_merge($activityPublic, $activityCreator), 'id');
+
+        if (isset($conditions['categoryId']) && !empty($conditions['categoryId'])) {
+            $courseSet = $this->getCourseSetService()->findCourseSetsByCategoryIdAndCreator($conditions['categoryId'], $currentUser->getId());
+            $activityCategory = $this->getActivityService()->findActivitiesByCourseSetIdsAndType(ArrayToolkit::column($courseSet, 'id'), 'live');
+            $activityCategoryIds = ArrayToolkit::column($activityCategory, 'id');
+            $activityIds = array_intersect($activityIds, $activityCategoryIds);
+        }
+
+        if (isset($conditions['tagId']) && !empty($conditions['tagId'])) {
+            $liveActivity = $this->getLiveActivityService()->findLiveActivitiesByReplayTagId($conditions['tagId']);
+            $activityTagIds = ArrayToolkit::column($liveActivity, 'id');
+            $activityIds = array_intersect($activityIds, $activityTagIds);
+        }
+
+        if (isset($conditions['keyword']) && !empty($conditions['keyword'])) {
+            $activityLikeTitle = $this->getActivityService()->findActivitiesLiveByLikeTitle($conditions['keyword']);
+            $activityLikeTitleIds = ArrayToolkit::column($activityLikeTitle, 'id');
+            $activityIds = array_intersect($activityIds, $activityLikeTitleIds);
+        }
+
+        unset($conditions['tagId']);
+        unset($conditions['keyword']);
+        unset($conditions['categoryId']);
+        $conditions['ids'] = empty($activityIds) ? [-1] : $activityIds;
+        $conditions['mediaType'] = 'live';
+
+        return $conditions;
+    }
+
+    /**
+     * @return ActivityService
+     */
+    protected function getActivityService()
+    {
+        return $this->service('Activity:ActivityService');
+    }
+
+    /**
+     * @return LiveActivityService
+     */
+    protected function getLiveActivityService()
+    {
+        return $this->service('Activity:LiveActivityService');
+    }
+
+    /**
+     * @return UserService
+     */
+    protected function getUserService()
+    {
+        return $this->service('User:UserService');
+    }
+
+    /**
+     * @return CourseSetService
+     */
+    protected function getCourseSetService()
+    {
+        return $this->service('Course:CourseSetService');
+    }
+}
