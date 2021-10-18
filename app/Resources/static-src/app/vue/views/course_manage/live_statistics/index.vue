e5af528af44d9d1931ea3d893246637c14bc2c18
hongyongchun <hongyongchun@edusoho.com>
2021-10-18 11:05:39 +0800

    feat: #81277 课程直播统计页面

---
+++ app/Resources/static-src/app/vue/views/course_manage/live_statistics/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="course-manage">
+    <a-input-search placeholder="input search text" style="width: 200px" @search="onSearch" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseManageLiveStatistics'
+}
+</script>
