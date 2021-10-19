d9436a8d99af9713fefe797430375402c90f462a
hongyongchun <hongyongchun@edusoho.com>
2021-10-19 15:51:51 +0800

    feat: #81277 直播数据详情页面 - 学习时长统计

--- app/Resources/static-src/app/vue/views/course_manage/live_statistics/details/index.vue
+++ app/Resources/static-src/app/vue/views/course_manage/live_statistics/details/index.vue
@@ -2,7 +2,7 @@
   <layout>
     <template #title>
       <div class="clearfix">
-        <div class="pull-left">xxx / xxx</div>
+        <div class="pull-left">xxx / {{ liveData.task.title }}</div>
         <div class="pull-right tips">数据来源于直播间，自动在直播结束一天同步</div>
       </div>
     </template>
