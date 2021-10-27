0a4ce047bdb834f9c9643382bb47cf79911353c8
hongyongchun <hongyongchun@edusoho.com>
2021-10-27 16:58:15 +0800

    feat: #81271 搜索条件处理

--- app/Resources/static-src/app/vue/views/teach/cloud_resources/live_playback/index.vue
+++ app/Resources/static-src/app/vue/views/teach/cloud_resources/live_playback/index.vue
@@ -6,7 +6,7 @@
       </a-form-model-item>
 
       <a-form-model-item>
-        <a-select v-model="searchForm.classification" placeholder="课程分类" style="width: 200px;">
+        <a-select v-model="searchForm.courseCategoryId" placeholder="课程分类" style="width: 200px;">
           <a-select-option value="shanghai">
             Zone one
           </a-select-option>
@@ -17,7 +17,7 @@
       </a-form-model-item>
 
       <a-form-model-item>
-        <a-select v-model="searchForm.tag" placeholder="回放标签" style="width: 200px;">
+        <a-select v-model="searchForm.replayTagId" placeholder="回放标签" style="width: 200px;">
           <a-select-option value="shanghai">
             Zone one
           </a-select-option>
@@ -41,6 +41,7 @@
     <a-table
       class="mt16"
       :columns="columns"
+      :row-key="record => record.id"
       :data-source="data"
       :pagination="pagination"
       :loading="loading"
@@ -77,6 +78,9 @@
 </template>
 
 <script>
+import _ from 'lodash';
+import { LiveReplay } from 'common/vue/service';
+
 const columns = [
   {
     dataIndex: 'title',
@@ -107,18 +111,11 @@ export default {
     return {
       searchForm: {
         time: undefined,
-        tag: undefined,
-        classification: undefined,
+        replayTagId: undefined,
+        courseCategoryId: undefined,
         keyword: ''
       },
-      data: [
-        {
-          title: 'title',
-          anchor: 'anchor',
-          liveTime: '11111111111111',
-          liveStartTime: '11111111111'
-        }
-      ],
+      data: [],
       columns,
       pagination: {
         hideOnSinglePage: true,
@@ -130,13 +127,44 @@ export default {
     }
   },
 
+  mounted() {
+    this.fetchLiveReplay();
+  },
+
   methods: {
     handleClickSearch() {
-      console.log(this.searchForm);
+      const query = _.pickBy(this.searchForm, _.identity);
+
+      if (!_.size(query)) return;
+
+      const { time } = query;
+      if (time) {
+        query.startTime = moment(time[0]).valueOf();
+        query.endTime = moment(time[1]).valueOf();
+        delete query.time;
+      }
+
+      console.log(query);
     },
 
-    handleTableChange() {
+    handleTableChange(pagination) {
+      this.pagination.current = pagination.current;
+      this.fetchLiveReplay();
+    },
 
+    async fetchLiveReplay() {
+      this.loading = true;
+      const { current, pageSize } = this.pagination;
+      const params = {
+        params: {
+          offset: (current - 1) * pageSize,
+          limit: pageSize
+        }
+      }
+      const { data, paging } = await LiveReplay.get(params);
+      this.loading = false;
+      this.pagination.total = paging.total;
+      this.data = data;
     }
   }
 }
