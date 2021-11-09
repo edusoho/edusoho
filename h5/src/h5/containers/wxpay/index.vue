cfa7d3650047adbb871b7ec2105baebbeafd720f
Hongbusi <q951686662@qq.com>
2021-11-09 10:58:42 +0800

    feat: #211109 test

--- h5/src/h5/containers/wxpay/index.vue
+++ h5/src/h5/containers/wxpay/index.vue
@@ -71,9 +71,12 @@ export default {
         },
       })
         .then(data => {
-          alert('test')
+
           // 微信支付优化：使用优惠卷抵扣后 0 元，不再调用微信支付。
           const { isPaid, payUrl } = data;
+
+          alert('test', isPaid, payUrl)
+
           if (isPaid) {
             window.location.href = payUrl;
           }
