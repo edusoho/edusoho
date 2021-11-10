c4f3ca9ed0062992af0ddcc331dbd4177de331f9
zhangyao <zhangyao@howzhi.com>
2021-11-10 17:52:44 +0800

    feat: #10000 bugfix

--- src/AppBundle/Controller/Callback/LiveController.php
+++ src/AppBundle/Controller/Callback/LiveController.php
@@ -9,13 +9,21 @@ use Biz\Live\Service\LiveService;
 use Firebase\JWT\JWT;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class LiveController extends BaseController
 {
     public function handleAction(Request $request)
     {
-        $this->validToken($request);
+        if ('GET' == $request->getMethod()) {
+            return $this->createJsonResponse(['success' => false, 'message' => 'Only allow post requests']);
+        }
+        $authorization = $request->headers->get('authorization', '');
+        $key = trim(str_replace('Secret', '', $authorization));
+        $storage = $this->setting('storage', []);
+        $data = JWT::decode($key, $storage['cloud_secret_key'], ['HS256']);
+        if (empty($data)) {
+            return $this->createJsonResponse(['success' => false, 'message' => '请求参数错误']);
+        }
         $this->handleEvent($request);
 
         return new JsonResponse(['success' => true]);
@@ -23,65 +31,28 @@ class LiveController extends BaseController
 
     public function handleEvent(Request $request)
     {
-        $event = $request->request->get('event');
-        $liveId = $request->request->get('id');
-
-        $method = '';
-        switch ($event) {
-            case 'room.started':
-                $method = 'startEvent';
-                break;
-            case 'room.finished':
-                $method = 'closeEvent';
-                break;
-            case 'replay.finished':
-                $method = 'replayEvent';
-                break;
-            default:
-                break;
+        try {
+            $eventData = $request->request->all();
+            $status = isset($eventData['event']) ? $eventData['event'] : 'null';
+            $this->getLogService()->info('live_callback', 'live_callback', '直播状态回调'.json_encode($eventData));
+            switch ($status) {
+                case 'room.started':
+                    $this->getLiveActivityService()->startLive($eventData['id'], $eventData['startTime']);
+                    break;
+                case 'room.finished':
+                    $confirmStatus = $this->getLiveService()->confirmLiveStatus($eventData['id']);
+                    if (isset($confirmStatus[$eventData['id']]['status']) && 'finished' === $confirmStatus['status']) {
+                        $this->getLiveActivityService()->closeLive($eventData['id'], $eventData['endTime']);
+                    }
+                    break;
+                case 'replay.finished':
+                    $this->getLiveReplayService()->handleReplayGenerateEvent($eventData['id'], $eventData['replayDatas']);
+                    break;
+                default:
+                    break;
+            }
+        } catch (\Exception $e) {
         }
-
-        if ($method) {
-            $this->$method($request, $liveId);
-        }
-    }
-
-    protected function startEvent(Request $request, $liveId)
-    {
-        $startTime = $request->query->get('startTime', time());
-        $this->getLiveActivityService()->startLive($liveId, $startTime);
-    }
-
-    protected function closeEvent(Request $request, $liveId)
-    {
-        $confirmStatus = $this->getLiveService()->confirmLiveStatus($liveId);
-        if (isset($confirmStatus[$liveId]['status']) && 'finished' === $confirmStatus['status']) {
-            $closeTime = $request->query->get('liveEndTime', time());
-            $this->getLiveActivityService()->closeLive($liveId, $closeTime);
-        }
-    }
-
-    protected function replayEvent(Request $request, $liveId)
-    {
-        $this->getLiveReplayService()->handleReplayGenerateEvent($request->request->get('replayDatas'));
-    }
-
-    protected function validToken($request)
-    {
-        $token = $request->headers->get('Authorization');
-        $token = explode(' ', $token);
-        $payload = JWT::decode($token[1], $this->getKey(), ['HS256']);
-        if (!$payload) {
-            throw new BadRequestHttpException('Token Error');
-        }
-    }
-
-    protected function getKey()
-    {
-        $setting = $this->setting('storage', []);
-        $secretKey = !empty($setting['cloud_secret_key']) ? $setting['cloud_secret_key'] : '';
-
-        return $secretKey;
     }
 
     /**
