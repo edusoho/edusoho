1019ecbe40dd35d548990a2454a2f61f161b9088
Hongbusi <q951686662@qq.com>
2021-11-17 17:57:55 +0800

    feat: #211117 layout

--- app/Resources/static-src/app/vue/views/operating/app_decoration/index.vue
+++ app/Resources/static-src/app/vue/views/operating/app_decoration/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="app-decoration">
-
+    <div class="app-page">
+      ff
+    </div>
   </div>
 </template>
+
+<style lang="less" scoped>
+.app-decoration {
+  margin: 30px 416px 0 110px;
+  text-align: center;
+  background: #f5f5f5;
+
+  .app-page {
+
+  }
+}
+</style>
