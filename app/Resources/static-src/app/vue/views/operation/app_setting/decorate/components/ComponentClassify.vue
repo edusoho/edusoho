c5a1305a387f7979f53a0f6300c049785bc8bff2
Hongbusi <q951686662@qq.com>
2021-11-19 16:08:52 +0800

    feat: #211119 装修页面完善

---
+++ app/Resources/static-src/app/vue/views/operation/app_setting/decorate/components/ComponentClassify.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="component-classify-item">
+    <div :class="{ active: isActive }">
+      <svg-icon :icon="classify.icon" />
+      <div class="classify-title">{{ classify.title }}</div>
+    </div>
+    <template v-if="classify.lists">
+      <div v-show="isActive" class="add-component-lists">
+        <div class="clearfix">
+          <div
+            v-for="component in classify.lists"
+            :key="component.name"
+            class="add-list-item pull-left"
+          >
+            <svg-icon :icon="component.icon" />
+            <div class="component-title">{{ component.title }}</div>
+          </div>
+        </div>
+        <div class="arrow-content" :style="{ top: (64 * index + 44) + 'px' }"></div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classify: {
+      type: Object,
+      required: true
+    },
+
+    index: {
+      type: Number,
+      required: true
+    },
+
+    currentClassify: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    isActive() {
+      return this.classify.key === this.currentClassify;
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.component-classify-item {
+  margin-bottom: 40px;
+  text-align: center;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.35);
+
+  .svg-icon {
+    font-size: 24px;
+    display: block;
+    margin: 8px auto;
+  }
+
+  .classify-title {
+    margin-top: 4px;
+    line-height: 16px;
+    height: 16px;
+    font-size: 12px;
+  }
+
+  .active {
+    .svg-icon {
+      color: #fff;
+    }
+
+    .classify-title {
+      color: #fff;
+    }
+  }
+
+  .add-component-lists {
+    position: absolute;
+    top: 0;
+    left: 80px;
+    width: 190px;
+    height: 100%;
+    background: #fff;
+    box-shadow: 4px 0px 8px 0px rgba(0, 0, 0, 0.05);
+    z-index: 10;
+    padding: 11px;
+    color: #333;
+
+    .add-list-item {
+      position: relative;
+      width: 78px;
+      height: 72px;
+      text-align: center;
+      border-radius: 2px;
+      cursor: pointer;
+      margin-left: 10px;
+      transition: all 0.3s ease;
+
+      .svg-icon {
+        font-size: 24px;
+        display: block;
+        margin: 8px auto;
+      }
+
+      .component-title {
+        color: #4a4a4a;
+        line-height: 16px;
+        height: 16px;
+        font-size: 12px;
+      }
+
+      &.limit-add {
+        opacity: 0.3;
+        cursor: not-allowed;
+      }
+
+      &:nth-of-type(even) {
+        margin-left: 0;
+      }
+
+      &:hover {
+        background: #31a1ff;
+        color: #fff;
+      }
+    }
+
+    .arrow-content {
+      position: absolute;
+      width: 8px;
+      height: 8px;
+      left: 0;
+      margin-left: -4px;
+      transform: rotate(45deg);
+      background-color: #fff;
+    }
+  }
+}
+</style>
--- app/Resources/static-src/app/vue/views/operation/app_setting/decorate/index.vue
+++ app/Resources/static-src/app/vue/views/operation/app_setting/decorate/index.vue
@@ -1,5 +1,116 @@
 <template>
-  <div class="app-decoration">
-    editor
+  <div class="decorate-container">
+    <the-header />
+
+    <div class="decorate-main clearfix">
+      <aside class="left-choose-container pull-left">
+        <div class="component-add-container">
+          <component-classify
+            v-for="(classify, index) in Classifys"
+            :key="classify.key"
+            :classify="classify"
+            :index="index"
+            :current-classify="currentClassify"
+            @click.native="handleChangeClassify(classify.key)"
+          />
+        </div>
+      </aside>
+
+      <section class="center-preview-container pull-left"></section>
+
+      <aside class="right-edit-container pull-left"></aside>
+    </div>
   </div>
 </template>
+
+<script>
+const Classifys = [
+  {
+    title: '基础组件',
+    key: 'basic',
+    icon: 'icon-check-circle',
+    lists: [
+      { title: '轮播图', icon: 'icon-check-circle', name: 'Swiper' },
+      { title: '课程列表', icon: 'icon-check-circle', name: 'CouseList' },
+      { title: '班级列表', icon: 'icon-check-circle', name: 'ClassroomList' },
+      { title: '图片公告', icon: 'icon-check-circle', name: 'Ad' },
+      { title: '图文导航', icon: 'icon-check-circle', name: 'Nav' },
+      { title: '公开课列表', icon: 'icon-check-circle', name: 'OpenCourseList' },
+      { title: '题库列表', icon: 'icon-check-circle', name: 'QuestionBankList' }
+    ]
+  },
+  {
+    title: '营销组件',
+    key: 'marketing',
+    icon: 'icon-check-circle',
+    lists: [
+      { title: '优惠卷', icon: 'icon-check-circle', name: 'Coupon' },
+      { title: '会员专区', icon: 'icon-check-circle', name: 'Vip' }
+    ]
+  }
+];
+
+import TheHeader from './components/TheHeader.vue';
+import ComponentClassify from './components/ComponentClassify.vue';
+
+export default {
+  components: {
+    TheHeader,
+    ComponentClassify
+  },
+
+  data() {
+    return {
+      Classifys,
+      currentClassify: 'basic'
+    }
+  },
+
+  methods: {
+    handleChangeClassify(val) {
+      this.currentClassify = val;
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.decorate-container {
+  width: 100%;
+  height: 100%;
+
+  .decorate-main {
+    overflow-y: hidden;
+    position: relative;
+    width: 100%;
+    height: calc(100% - 52px);
+
+    .left-choose-container {
+      width: 80px;
+      height: 100%;
+      background: #243042;
+
+      .component-add-container {
+        position: relative;
+        padding-top: 20px;
+        height: 100%;
+        user-select: none;
+      }
+    }
+
+    .center-preview-container {
+      overflow-y: auto;
+      padding: 40px 0 20px 190px;
+      width: calc(100% - 384px - 80px);
+    }
+
+    .right-edit-container {
+      overflow-y: auto;
+      width: 384px;
+      height: 100%;
+      background-color: #fff;
+    }
+  }
+}
+
+</style>
