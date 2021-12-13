5773ef0ec18adfec75d4d0fe98e26c239141c60b
Hongbusi <q951686662@qq.com>
2021-12-13 15:43:27 +0800

    feat: #211213 update

--- app/Resources/static-src/app/vue/views/operation/app_setting/decorate/components/PosterEdit/index.vue
+++ app/Resources/static-src/app/vue/views/operation/app_setting/decorate/components/PosterEdit/index.vue
@@ -19,21 +19,31 @@
         </div>
       </div>
 
-      <div class="design-editor__item">
-        <span>选择链接：</span>
-        <a-radio-group>
-          <a-radio value="1">
-            开启
-          </a-radio>
-          <a-radio value="0">
-            关闭
-          </a-radio>
-        </a-radio-group>
+      <div class="design-editor__item clearfix">
+        <span class="pull-left">选择链接：</span>
+        <div
+          v-show="selectdLink"
+          class="pull-left text-overflow selectd-link"
+        >
+          {{ selectdLink }}
+          <a-icon @click="handleModity" type="close-circle" />
+        </div>
+        <a-dropdown class="pull-left">
+          <a class="ant-dropdown-link" @click="(e) => e.preventDefault()">
+            {{ selectText }}<a-icon type="down" />
+          </a>
+          <a-menu slot="overlay" @click="handleSelectLink">
+            <a-menu-item key="course">选择课程</a-menu-item>
+            <a-menu-item key="classroom">选择班级</a-menu-item>
+            <a-menu-item key="vip">选择会员</a-menu-item>
+            <a-menu-item key="custom">自定义链接</a-menu-item>
+          </a-menu>
+        </a-dropdown>
       </div>
 
       <div class="design-editor__item">
         <span>自适应手机屏幕：</span>
-        <a-radio-group>
+        <a-radio-group :default-value="moduleData.responsive" @change="handleChangeResponsive">
           <a-radio value="1">
             开启
           </a-radio>
@@ -43,6 +53,10 @@
         </a-radio-group>
       </div>
     </div>
+
+    <custom-link-modal ref="customLink" @update-link="handleUpdateLink" />
+    <course-link-modal ref="courseLink" @update-link="handleUpdateLink" />
+    <classroom-link-modal ref="classroomLink" @update-link="handleUpdateLink" />
   </edit-layout>
 </template>
 
@@ -50,6 +64,9 @@
 import _ from 'lodash';
 import EditLayout from '../EditLayout.vue';
 import { UploadToken, File } from 'common/vue/service';
+import CustomLinkModal from '../CustomLinkModal.vue';
+import CourseLinkModal from '../CourseLinkModal.vue';
+import ClassroomLinkModal from '../ClassroomLinkModal.vue';
 
 export default {
   name: 'PosterEdit',
@@ -62,12 +79,35 @@ export default {
   },
 
   components: {
-    EditLayout
+    EditLayout,
+    CustomLinkModal,
+    CourseLinkModal,
+    ClassroomLinkModal
   },
 
   data() {
     return {
       uploadToken: {}
+    }
+  },
+
+  computed: {
+    selectdLink() {
+      const { target, type, url } = this.moduleData.link;
+      if (!type && url) return url;
+
+      if (type === 'vip') return '会员专区';
+
+      if (_.includes(['classroom', 'course'], type)) {
+        const { title, displayedTitle } = target;
+        return title || displayedTitle;
+      }
+
+      return '';
+    },
+
+    selectText() {
+      return this.selectdLink ? '修改' : '选择链接';
     }
   },
 
@@ -90,17 +130,61 @@ export default {
 
       try {
         const data = await File.uploadFile(formData);
-        this.$emit('update-edit', {
-          type: 'poster',
-          key: 'image',
-          value: data
-        });
+        this.update({ key: 'image', value: data });
       } catch(error) {
         const { status } = error.response;
         if (status == 413) {
           Vue.prototype.$message.error('文件过大，请上传小于 2M 的文件！');
         }
       }
+    },
+
+    handleChangeResponsive(e) {
+      this.update({ key: 'responsive', value: e.target.value });
+    },
+
+    handleSelectLink({ key }) {
+      if (key === 'vip') {
+        const params = {
+          target: null,
+          type: 'vip',
+          url: ''
+        };
+        this.update({ key: 'link', value: params });
+        return;
+      }
+
+      if (key === 'custom') {
+        this.$refs.customLink.showModal();
+        return;
+      }
+
+      if (key === 'course') {
+        this.$refs.courseLink.showModal();
+        return;
+      }
+
+      if (key === 'classroom') {
+        this.$refs.classroomLink.showModal();
+        return;
+      }
+    },
+
+    handleModity() {
+      const params = {
+        target: null,
+        type: '',
+        url: ''
+      };
+      this.update({ key: 'link', value: params });
+    },
+
+    handleUpdateLink(params) {
+      this.update({ key: 'link', value: params });
+    },
+
+    update({ key, value }) {
+      this.$emit('update-edit', { type: 'poster', key, value });
     }
   }
 }
@@ -148,6 +232,26 @@ export default {
     display: none;
     color: #fff;
     background: rgba(0, 0, 0, 0.5);
+  }
+}
+
+.selectd-link {
+  position: relative;
+  padding-right: 30px;
+  max-width: 160px;
+
+  i {
+    position: absolute;
+    right: 12px;
+    top: 3px;
+    display: none;
+    color: #31A1FF;
+  }
+
+  &:hover {
+    i {
+      display: block;
+    }
   }
 }
 </style>
