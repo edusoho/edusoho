239cfd59e1f7a93495a9810decc8f1b4a1194d97
Hongbusi <q951686662@qq.com>
2021-12-13 18:19:41 +0800

    feat: #211213 优化

--- app/Resources/static-src/app/vue/views/operation/app_setting/components/GraphicNavigation/index.vue
+++ app/Resources/static-src/app/vue/views/operation/app_setting/components/GraphicNavigation/index.vue
@@ -5,21 +5,52 @@
     :is-last="isLast"
     @event-actions="handleClickAction"
   >
-    graphic_navigation
+    <div class="gn-list clearfix">
+      <item
+        v-for="(item, index) in moduleData"
+        :key="index"
+        class="pull-left"
+        :style="{ width: itemWidth }"
+        :item="item"
+      />
+    </div>
   </layout>
 </template>
 
 <script>
 import _ from 'lodash';
 import moduleMixin from '../moduleMixin';
+import Item from './Item.vue';
 
 export default {
   name: 'GraphicNavigation',
 
-  mixins: [moduleMixin]
+  mixins: [moduleMixin],
+
+  components: {
+    Item
+  },
+
+  computed: {
+    itemWidth() {
+      const length =  _.clamp(_.size(this.moduleData), 1, 4);
+      return `${100 / length}%`;
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.gn-list {
+  margin-right: 16px;
+  margin-left: 16px;
+  background: linear-gradient(
+    to left,
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(255, 255, 255, 0.8) 25%,
+    rgba(255, 255, 255, 1) 50%,
+    rgba(255, 255, 255, 0.8) 75%,
+    rgba(255, 255, 255, 0.1) 100%
+  );
+}
 </style>
--- app/Resources/static-src/app/vue/views/operation/app_setting/decorate/index.vue
+++ app/Resources/static-src/app/vue/views/operation/app_setting/decorate/index.vue
@@ -291,7 +291,7 @@ export default {
         width: 375px;
         min-height: 90%;
         box-shadow: 0px 1px 3px 1px rgba(0, 0, 0, 0.05);
-        background-color: #fff;
+        background-color: #f5f5f5;
       }
     }
 
