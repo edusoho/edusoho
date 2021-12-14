a334bc75bf28ad67533a849b605925ec52767011
mixiaotian <mixiaotian@edusoho.com>
2021-12-14 15:20:33 +0800

    feat: #83812 公开课播放B站视频

--- app/Resources/static-src/app/js/open-course/header/index.js
+++ app/Resources/static-src/app/js/open-course/header/index.js
@@ -1,9 +1,7 @@
 import OpenCoursePlayer from './open-course-player';
-import swfobject from 'es-swfobject';
-import { isMobileDevice } from 'common/utils';
 
 if ($('#firstLesson').length > 0) {
-  let openCoursePlayer = new OpenCoursePlayer({
+  new OpenCoursePlayer({
     url: $('#firstLesson').data('url'),
     element: '.open-course-views',
   });
