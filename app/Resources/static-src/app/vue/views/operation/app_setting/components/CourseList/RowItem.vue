ec61708c0c69928021f2e895e1f1ec7a79459ab8
Hongbusi <q951686662@qq.com>
2021-12-15 16:11:30 +0800

    feat: #211215 course item

---
+++ app/Resources/static-src/app/vue/views/operation/app_setting/components/CourseList/RowItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row-item clearfix">
+    <div class="row-item__img pull-left"></div>
+    <div class="row-item__info pull-left">
+      <div class="row-item__title text-overflow">课程名称</div>
+      <div class="row-item__price">课程价格</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseListItem'
+}
+</script>
+
+<style lang="less" scoped>
+.row-item {
+  overflow: hidden;
+  margin-top: 16px;
+  width: 100%;
+  height: 90px;
+  border-radius: 8px;
+  background: #fff;
+  box-shadow: 0px 2px 16px 0px rgba(0, 0, 0, 0.05);
+
+  &__img {
+    overflow: hidden;
+    width: 160px;
+    height: 100%;
+    border-radius: 8px;
+    background-color: red;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  &__info {
+    position: relative;
+    padding: 8px;
+    width: calc(100% - 160px);
+    height: 100%;
+  }
+
+  &__title {
+    font-size: 14px;
+    font-weight: 500;
+    color: #333;
+    line-height: 20px;
+  }
+
+  &__price {
+    position: absolute;
+    left: 8px;
+    bottom: 8px;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 16px;
+  }
+}
+</style>
--- app/Resources/static-src/app/vue/views/operation/app_setting/components/CourseList/index.vue
+++ app/Resources/static-src/app/vue/views/operation/app_setting/components/CourseList/index.vue
@@ -5,18 +5,75 @@
     :is-last="isLast"
     @event-actions="handleClickAction"
   >
-    course-list
+    <div class="course-list">
+      <div class="clearfix">
+        <div class="course-list__title pull-left text-overflow">推荐课程</div>
+        <div class="course-list__more pull-right">查看更多<a-icon type="right" /></div>
+      </div>
+      <div class="">
+        <!-- <column-item />
+        <column-item />
+        <column-item />
+        <column-item /> -->
+        <row-item />
+        <row-item />
+        <row-item />
+        <row-item />
+      </div>
+    </div>
   </layout>
 </template>
 
 <script>
 import _ from 'lodash';
 import moduleMixin from '../moduleMixin';
+import ColumnItem from './ColumnItem.vue';
+import RowItem from './RowItem.vue';
 
 export default {
   name: 'CourseList',
 
-  mixins: [moduleMixin]
+  mixins: [moduleMixin],
+
+  components: {
+    ColumnItem,
+    RowItem
+  }
 }
 </script>
+
+<style lang="less" scoped>
+.course-list {
+  padding-right: 16px;
+  padding-left: 16px;
+
+  .course-list__title {
+    position: relative;
+    padding-left: 10px;
+    max-width: 60%;
+    font-size: 16px;
+    font-weight: 500;
+    color: #333;
+    line-height: 24px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 6px;
+      left: 0;
+      width: 4px;
+      height: 12px;
+      background: #03c777;
+      border-radius: 1px;
+    }
+  }
+
+  &__more {
+    margin-top: 4px;
+    font-size: 12px;
+    color: #999;
+    line-height: 16px;
+  }
+}
+</style>
 
