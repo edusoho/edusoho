34c0283bef69e599fb54b5f55a8f5dfb14f625fc
Hongbusi <q951686662@qq.com>
2021-12-15 16:38:46 +0800

    feat: #211215 course item

--- app/Resources/static-src/app/vue/views/operation/app_setting/decorate/components/CourseListEdit/index.vue
+++ app/Resources/static-src/app/vue/views/operation/app_setting/decorate/components/CourseListEdit/index.vue
@@ -3,7 +3,52 @@
     <template #title>课程列表设置</template>
 
     <div class="design-editor">
-      course-list-edit
+      <div class="design-editor__item">
+        <span class="design-editor__required">列表名称：</span>
+        <a-input
+          placeholder="请输入列表名称"
+          style="width: 200px;"
+          size="small"
+          allow-clear
+        />
+      </div>
+
+      <div class="design-editor__item">
+        <span>排列方式：</span>
+        <a-select style="width: 200px;" size="small">
+          <a-select-option key="1">一行一列</a-select-option>
+          <a-select-option key="2">一行两列</a-select-option>
+        </a-select>
+      </div>
+
+      <div class="design-editor__item">
+        <span>课程来源：</span>
+        <a-radio-group>
+          <a-radio value="show">
+            课程分类
+          </a-radio>
+          <a-radio value="unshow">
+            自定义
+          </a-radio>
+        </a-radio-group>
+      </div>
+
+      <div class="design-editor__item">
+        <span>排列顺序：</span>
+        <a-select style="width: 200px;" size="small">
+          <a-select-option key="1">加入最多</a-select-option>
+          <a-select-option key="2">最近创建</a-select-option>
+          <a-select-option key="3">评分最高</a-select-option>
+          <a-select-option key="4">推荐课程</a-select-option>
+        </a-select>
+      </div>
+
+      <div class="design-editor__item">
+        <span>显示个数：</span>
+        <a-select style="width: 200px;" size="small">
+          <a-select-option v-for="item in 8" :key="item">{{ item  }}</a-select-option>
+        </a-select>
+      </div>
     </div>
   </edit-layout>
 </template>
