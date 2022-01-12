05a31733f44dd83a4519907aa9a5af77ef83a81a
zhangyao <zhangyao@howzhi.com>
2022-01-12 18:51:23 +0800

    feat: #100000 bugfix

--- src/AppBundle/Controller/AnswerEngine/AnswerEngineController.php
+++ src/AppBundle/Controller/AnswerEngine/AnswerEngineController.php
@@ -8,6 +8,7 @@ use Codeages\Biz\ItemBank\Answer\Service\AnswerSceneService;
 use Codeages\Biz\ItemBank\Answer\Service\AnswerService;
 use Codeages\Biz\ItemBank\Assessment\Service\AssessmentService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AnswerEngineController extends BaseController
 {
@@ -61,6 +62,7 @@ class AnswerEngineController extends BaseController
             'answerRecordId' => $answerRecordId,
             'role' => $role,
             'activity' => $activity,
+            'goBackUrl' => $this->generateUrl('course_manage_testpaper_result_list', ['id' => $activity['fromCourseId'], 'testpaperId'=>$answerRecord['assessment_id'], 'activityId'=>$activity['id']], UrlGeneratorInterface::ABSOLUTE_URL)
         ]);
     }
 
