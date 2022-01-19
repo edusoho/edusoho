cecf28fce8a4cedda885177e36864d71da7d99aa
Hongbusi <q951686662@qq.com>
2022-01-19 14:33:23 +0800

    feat: #84721 init

---
+++ app/Resources/static-src/app/vue/views/layouts/AdminRightContent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="aside-container">
+    <div class="aside-header">
+      <div class="aside-header__title">
+        <slot name="title"></slot>
+      </div>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.aside-container {
+  margin-top: -10px;
+  padding: 16px;
+
+  .aside-header {
+    &__title {
+      position: relative;
+      padding-left: 22px;
+      font-size: 16px;
+      color: #333;
+      letter-spacing: 0;
+      line-height: 26px;
+
+      &::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 8px;
+        transform: translateY(-50%);
+        width: 2px;
+        height: 14px;
+        background: #1890ff;
+        border-radius: 1px;
+      }
+    }
+  }
+}
+</style>
---
+++ app/Resources/static-src/app/vue/views/system/purchase_agreement/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <admin-right-content>
+    <template #title>购买协议设置</template>
+    zheshineir
+  </admin-right-content>
+</template>
+
+<script>
+import AdminRightContent from 'app/vue/views/layouts/AdminRightContent.vue';
+
+export default {
+  name: 'PurchaseAgreementSettings',
+
+  components: {
+    AdminRightContent
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
