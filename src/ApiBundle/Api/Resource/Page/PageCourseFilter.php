e6a412ca10816e8d61c439cb438ea5f503b5db88
zhangyao <zhangyao@howzhi.com>
2022-01-20 17:42:27 +0800

    feat: #10000 drainage

--- src/ApiBundle/Api/Resource/Page/PageCourseFilter.php
+++ src/ApiBundle/Api/Resource/Page/PageCourseFilter.php
@@ -19,7 +19,7 @@ class PageCourseFilter extends Filter
         'audiences', 'goals', 'isDefault', 'maxStudentNum', 'status', 'isFree', 'price', 'originPrice', 'teachers',
         'creator', 'services', 'courseSet', 'courseItems', 'courses', 'member', 'courseType', 'progress', 'buyable',
         'reviews', 'myReview', 'enableFinish', 'hasCertificate', 'goodsId', 'specsId', 'spec', 'hitNum', 'classroom',
-        'assistant',
+        'assistant', 'drainage',
     ];
 
     protected function publicFields(&$data)
