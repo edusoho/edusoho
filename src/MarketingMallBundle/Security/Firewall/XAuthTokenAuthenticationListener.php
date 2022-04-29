7245745ed3cb31d88911fc3cb04d629e998a223c
zhangyao <zhangyao@howzhi.com>
2022-04-29 15:07:19 +0800

    feat: #1000 login

--- src/MarketingMallBundle/Security/Firewall/XAuthTokenAuthenticationListener.php
+++ src/MarketingMallBundle/Security/Firewall/XAuthTokenAuthenticationListener.php
@@ -15,26 +15,26 @@ class XAuthTokenAuthenticationListener extends BaseListener
     public function handle(Request $request)
     {
         if (null != $tokenInHeader = $request->headers->get(self::MALL_TOKEN_HEADER)) {
-//            $mallSettings = $this->getSettingService()->get('marketing_mall', []);
-//            $storages = $this->getSettingService()->get('storages', []);
-//            try {
-//                if (empty($mallSettings['secret_key'])) {
-//                    $result = JWT::decode($tokenInHeader, $storages['cloud_secret_key'], array('HS256'));
-//                    $access_key = $storages['cloud_access_key'];
-//                }else{
-//                    $result = JWT::decode($tokenInHeader, $mallSettings['secret_key'], array('HS256'));
-//                    $access_key = $mallSettings['access_key'];
-//                }
-//            }catch (\RuntimeException $e){
-//                throw new NotFoundException('token 鉴权失败！');
-//            }
-//
-//            if($result->access_key != $access_key){
-//                throw new NotFoundException('token 鉴权失败！');
-//            }
-//            if (empty($result->user_id)) {
-//                throw new NotFoundException('user_id 不存在！');
-//            }
+            $mallSettings = $this->getSettingService()->get('marketing_mall', []);
+            $storages = $this->getSettingService()->get('storages', []);
+            try {
+                if (empty($mallSettings['secret_key'])) {
+                    $result = JWT::decode($tokenInHeader, $storages['cloud_secret_key'], ['HS256']);
+                    $access_key = $storages['cloud_access_key'];
+                } else {
+                    $result = JWT::decode($tokenInHeader, $mallSettings['secret_key'], ['HS256']);
+                    $access_key = $mallSettings['access_key'];
+                }
+            } catch (\RuntimeException $e) {
+                throw new NotFoundException('token 鉴权失败！');
+            }
+
+            if ($result->access_key != $access_key) {
+                throw new NotFoundException('token 鉴权失败！');
+            }
+            if (empty($result->user_id)) {
+                throw new NotFoundException('user_id 不存在！');
+            }
             $token = $this->createTokenFromRequest($request, 2);
             $this->getTokenStorage()->setToken($token);
         } else {
