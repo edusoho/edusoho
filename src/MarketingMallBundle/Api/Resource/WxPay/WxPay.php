70af2ff0ac971390766728b545b0de3faaebcd92
赵世界 <zhaoshijie@edusoho.com>
2022-06-21 15:45:28 +0800

    feat: #87600 增加商城鉴权注解

--- src/MarketingMallBundle/Api/Resource/WxPay/WxPay.php
+++ src/MarketingMallBundle/Api/Resource/WxPay/WxPay.php
@@ -2,12 +2,16 @@
 
 namespace MarketingMallBundle\Api\Resource\WxPay;
 
+use ApiBundle\Api\Annotation\AuthClass;
 use ApiBundle\Api\ApiRequest;
 use Biz\System\Service\SettingService;
 use MarketingMallBundle\Api\Resource\BaseResource;
 
 class WxPay extends BaseResource
 {
+    /**
+     * @AuthClass(className="MarketingMallBundle\\Security\\Firewall\\MallAuthTokenAuthenticationListener")
+     */
     public function get(ApiRequest $apiRequest)
     {
         return $this->getSettingService()->get('payment', []);
