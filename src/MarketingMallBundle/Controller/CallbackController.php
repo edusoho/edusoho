8ca40c14fd0581fa1160f3a0000ed12e04dea8d0
赵世界 <zhaoshijie@edusoho.com>
2022-06-22 17:33:17 +0800

    feat: #87596 不进行学员判定

--- src/MarketingMallBundle/Controller/CallbackController.php
+++ src/MarketingMallBundle/Controller/CallbackController.php
@@ -6,9 +6,6 @@ use AppBundle\Common\ArrayToolkit;
 use AppBundle\Common\DeviceToolkit;
 use AppBundle\Common\Exception\InvalidArgumentException;
 use AppBundle\Controller\BaseController;
-use Biz\Classroom\Service\ClassroomService;
-use Biz\Course\Service\MemberService;
-use Biz\ItemBankExercise\Service\ExerciseMemberService;
 use Biz\System\Service\SettingService;
 use Firebase\JWT\JWT;
 use Http\Discovery\Exception\NotFoundException;
@@ -72,22 +69,12 @@ class CallbackController extends BaseController
 
     protected function getUrlWithTarget($query)
     {
-        $route = 'homepage';
-        if ('course' === $query['targetType']) {
-            $route = 'my_course_show';
-            $isMember = $this->getCourseMemberService()->getCourseMember($query['targetId'], $query['userId']);
-        }
-        if ('classroom' === $query['targetType']) {
-            $route = 'classroom_courses';
-            $isMember = $this->getClassroomService()->getClassroomMember($query['targetId'], $query['userId']);
-        }
-        if ('item_bank_exercise' === $query['targetType']) {
-            $route = 'my_item_bank_exercise_show';
-            $isMember = $this->getItemBankExerciseMemberService()->getExerciseMember($query['targetId'], $query['userId']);
-        }
-        if (empty($isMember)) {
-            throw new NotFoundException('is not member');
-        }
+        $routes = [
+            'course' => 'my_course_show',
+            'classroom' => 'classroom_courses',
+            'item_bank_exercise' => 'my_item_bank_exercise_show',
+        ];
+        $route = $routes[$query['targetType']] ?? 'homepage';
         $param = ['id' => $query['targetId']];
         if (2 == $this->setting('wap.version') && DeviceToolkit::isMobileClient()) {
             $token = $this->getUserService()->makeToken('mobile_login', $query['userId'], time() + 3600 * 24 * 30, []);
@@ -103,29 +90,5 @@ class CallbackController extends BaseController
     protected function getSettingService()
     {
         return $this->getBiz()->service('System:SettingService');
-    }
-
-    /**
-     * @return MemberService
-     */
-    protected function getCourseMemberService()
-    {
-        return $this->getBiz()->service('Course:MemberService');
-    }
-
-    /**
-     * @return ClassroomService
-     */
-    protected function getClassroomService()
-    {
-        return $this->getBiz()->service('Classroom:ClassroomService');
-    }
-
-    /**
-     * @return ExerciseMemberService
-     */
-    protected function getItemBankExerciseMemberService()
-    {
-        return $this->getBiz()->service('ItemBankExercise:ExerciseMemberService');
     }
 }
