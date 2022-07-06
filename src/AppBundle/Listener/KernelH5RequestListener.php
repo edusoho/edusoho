1c57ecaa80b887b7e12d1bd78f93a4e92238033b
赵世界 <zhaoshijie@edusoho.com>
2022-07-06 18:18:09 +0800

    fix: #87996 修复班级题库目标跳转错误问题

--- src/AppBundle/Listener/KernelH5RequestListener.php
+++ src/AppBundle/Listener/KernelH5RequestListener.php
@@ -66,11 +66,11 @@ class KernelH5RequestListener
     {
         $params = $request->query->all();
         $query = http_build_query($params);
-        if (in_array($route['_route'], ['my_course_show', 'course_show', 'classroom_show', 'item_bank_exercise_show'])) {
+        if (in_array($route['_route'], ['my_course_show', 'course_show'])) {
             $pathInfo = $this->container->get('router')->generate('course_show', ['id' => $route['id']], UrlGeneratorInterface::ABSOLUTE_PATH);
-            if (isset($params['loginToken'])) {
-                $pathInfo = $pathInfo.'/loginToken/'.$params['loginToken'];
-            }
+        }
+        if (isset($params['loginToken']) && in_array($route['_route'], ['my_course_show', 'course_show', 'classroom_show', 'item_bank_exercise_show'])) {
+            $pathInfo .= '/loginToken/' . $params['loginToken'];
         }
 
         if (in_array($route['_route'], ['classroom_reviews', 'classroom_introductions'])) {
