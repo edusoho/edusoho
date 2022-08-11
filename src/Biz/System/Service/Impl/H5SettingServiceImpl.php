307849d5205685c74e660c3438644b900256bcd0
zhulei <zhulei@edusoho.com>
2022-08-11 11:51:09 +0800

    feat: #88520 添加announcementFilter

--- src/Biz/System/Service/Impl/H5SettingServiceImpl.php
+++ src/Biz/System/Service/Impl/H5SettingServiceImpl.php
@@ -368,6 +368,10 @@ class H5SettingServiceImpl extends BaseService implements H5SettingService
         return $discoverySetting;
     }
 
+    public function announcementFilter($discoverySetting, $portal, $usage = 'show'){
+        return $discoverySetting;
+    }
+
     public function vipFilter($discoverySetting, $portal, $usage = 'show')
     {
         if ($this->isPluginInstalled('Vip')) {
