df5c4d1c3bcaabf06c574036e331398cb4a6b4e8
Johnson <ziyuorg@gmail.com>
2022-08-15 10:57:24 +0800

    feat: #88023 加入唯一索引

--- migrations/20220812153612_user_token_add_refresh_time.php
+++ migrations/20220812153612_user_token_add_refresh_time.php
@@ -13,6 +13,7 @@ class UserTokenAddRefreshTime extends Migration
         $biz['db']->exec("
             ALTER  TABLE `user_token` ADD COLUMN `refresh_token` varchar(255) COMMENT '刷新token' AFTER `remainedTimes`;
             ALTER  TABLE `user_token` ADD COLUMN `refresh_expire_time` BIGINT COMMENT '刷新token过期时间' AFTER `refresh_token`;
+            ALTER  TABLE `user_token` ADD UNIQUE (`refresh_token`);
         ");
     }
 
