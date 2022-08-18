589d7528c10e3e5cc66360ca0b1e5103e3480336
Johnson <ziyuorg@gmail.com>
2022-08-18 10:05:02 +0800

    fix: #99359 删除多余代码

--- src/Biz/OrderFacade/Product/Product.php
+++ src/Biz/OrderFacade/Product/Product.php
@@ -226,7 +226,6 @@ abstract class Product extends BizAware implements OrderStatusCallback
             'refund_id' => $orderRefund['id'],
             'reason_type' => 'refund',
         ];
-        file_put_contents('/tmp/record1.txt', var_export($record, true), FILE_APPEND);
         $this->getMemberOperationService()->updateRefundInfoByOrderId($orderRefund['order_id'], $record);
     }
 
