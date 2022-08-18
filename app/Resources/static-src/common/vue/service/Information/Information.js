95bc1c0f071326068bf07811e50cec792429c33e
吴博 <wubo@howzhi.com>
2022-08-18 15:23:42 +0800

    feat: #88523 APP装修新增资讯

--- app/Resources/static-src/common/vue/service/Information/Information.js
+++ app/Resources/static-src/common/vue/service/Information/Information.js
@@ -1,6 +1,6 @@
 import { apiClient } from 'common/vue/service/api-client.js';
 
-const baseUrl = '/api/latest_real_time_info';
+const baseUrl = '/api/latest_information';
 
 export const Information = {
   async search(params) {
