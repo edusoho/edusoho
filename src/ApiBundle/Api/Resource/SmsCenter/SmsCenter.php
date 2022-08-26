328534eacf7cac614d51b6016e4dbe42c6b78fd2
Johnson <ziyuorg@gmail.com>
2022-08-26 09:48:32 +0800

    feat: #90521 完成手机端部分接口拦截

--- src/ApiBundle/Api/Resource/SmsCenter/SmsCenter.php
+++ src/ApiBundle/Api/Resource/SmsCenter/SmsCenter.php
@@ -23,6 +23,14 @@ class SmsCenter extends AbstractResource
      */
     public function add(ApiRequest $request)
     {
+        if (!$request->getHttpRequest()->isXmlHttpRequest()) {
+            $mobile = $this->getSettingService()->get('mobile',array());
+            $wap = $this->getSettingService()->get('wap',array());
+            if ($mobile['enabled'] == 0 && $wap['template'] != 'sail'){
+                return null;
+            }
+        }
+
         if ($this->getBehaviorVerificationService()->behaviorVerification($request->getHttpRequest())){
             return new JsonResponse(['ACK' => 'ok', "allowance" => 0]);
         }
