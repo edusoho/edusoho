b9e2c009beac9ac8088df8fc0e3a4ccecf9bd4bd
吴博 <wubo@howzhi.com>
2022-08-29 16:58:05 +0800

    feat: #220829 底部导航图标更换

--- h5/src/h5/containers/home.vue
+++ h5/src/h5/containers/home.vue
@@ -5,8 +5,8 @@
       :class="{ 'van-tabbar--iphonex': firstVisit }"
       @change="onChange"
     >
-      <van-tabbar-item v-for="item in items" :key="item.type">
-        <span>{{ $t(item.type) }}</span>
+      <van-tabbar-item v-for="(item, index) in items" :key="item.type">
+        <span :style="{ color: active === index ? '#3DCD7F' : '' }">{{ $t(item.type) }}</span>
         <template slot="icon" slot-scope="props">
           <img :src="props.active ? item.active : item.normal" />
         </template>
