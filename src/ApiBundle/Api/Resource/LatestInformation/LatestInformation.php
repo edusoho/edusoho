ba78e2a0beb5153fa61aaf8fa2626f3393570fce
Johnson <ziyuorg@gmail.com>
2022-08-31 10:42:22 +0800

    fix: #88521 修改单独获取资讯图片没有路径

--- src/ApiBundle/Api/Resource/LatestInformation/LatestInformation.php
+++ src/ApiBundle/Api/Resource/LatestInformation/LatestInformation.php
@@ -5,13 +5,21 @@ namespace ApiBundle\Api\Resource\LatestInformation;
 
 
 use ApiBundle\Api\Resource\AbstractResource;
+use ApiBundle\Api\Util\AssetHelper;
 use Biz\Article\Service\ArticleService;
 
 class LatestInformation extends AbstractResource
 {
     public function search()
     {
-        return $this ->getArticleService() -> searchArticles(['status' => 'published'], ['sticky' => 'DESC' ,'publishedTime' => 'DESC'], 0, 3);
+        $information = $this ->getArticleService() -> searchArticles(['status' => 'published'], ['sticky' => 'DESC' ,'publishedTime' => 'DESC'], 0, 3);
+        foreach ($information as &$info) {
+            $info['body'] = AssetHelper::transformImages($info['body']);
+            $info['thumb'] = AssetHelper::transformImagesAddUrl($info['thumb'], '');
+            $info['originalThumb'] = AssetHelper::transformImagesAddUrl($info['originalThumb'], '');
+            $info['picture'] = AssetHelper::transformImagesAddUrl($info['picture'], 'picture');
+        }
+        return $information;
     }
 
     /**
