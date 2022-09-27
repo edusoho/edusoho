d63721010e995fb111738fcbe50b760c1b329219
zhaolishuang <zhaolishuang@edusoho.com>
2022-09-27 10:43:29 +0800

    feat: #91289 staffManage

--- src/AppBundle/Controller/AdminV2/User/UserManageController.php
+++ src/AppBundle/Controller/AdminV2/User/UserManageController.php
@@ -170,6 +170,7 @@ class UserManageController extends UserCommonController
 
     protected function kickUserLogout($userId)
     {
+
         $tokens = $this->getTokenService()->findTokensByUserIdAndType($userId, 'mobile_login');
         if (!empty($tokens)) {
             foreach ($tokens as $token) {
