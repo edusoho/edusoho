a28e2d7dc6118dc9cabd9e7de22b3e48ff166b89
zhaolishuang <zhaolishuang@edusoho.com>
2022-11-03 16:41:42 +0800

    fix: #92220 exam-answer

--- migrations/20221103140831_add_biz_answer_report_index.php
+++ migrations/20221103140831_add_biz_answer_report_index.php
@@ -4,14 +4,12 @@ use Phpmig\Migration\Migration;
 
 class AddBizAnswerReportIndex extends Migration
 {
-    protected $biz = null;
     /**
      * Do the migration
      */
     public function up()
     {
-        $this->biz = $this->getContainer();
-        $connection = $this->biz['db'];
+        $connection = $this->getContainer()['db'];
 
         if (!$this->isIndexExist('biz_answer_record', 'assessment_id', 'assessment_id')) {
             $connection->exec('ALTER TABLE `biz_answer_record` ADD INDEX assessment_id ( `assessment_id`);');
@@ -32,23 +30,25 @@ class AddBizAnswerReportIndex extends Migration
      */
     public function down()
     {
+        $biz = $this->getContainer();
         if ($this->isIndexExist('biz_answer_record', 'assessment_id', 'assessment_id')) {
-            $this->biz['db']->exec('ALTER TABLE `biz_answer_record` DROP INDEX `assessment_id`;');
+            $biz['db']->exec('ALTER TABLE `biz_answer_record` DROP INDEX `assessment_id`;');
         }
 
         if ($this->isIndexExist('biz_answer_report', 'assessment_id', 'assessment_id')) {
-            $this->biz['db']->exec('ALTER TABLE `biz_answer_report` DROP INDEX `assessment_id`;');
+            $biz['db']->exec('ALTER TABLE `biz_answer_report` DROP INDEX `assessment_id`;');
         }
 
         if ($this->isIndexExist('biz_answer_question_report', 'assessment_id', 'assessment_id')) {
-            $this->biz['db']->exec('ALTER TABLE `biz_answer_question_report` DROP INDEX `assessment_id`;');
+            $biz['db']->exec('ALTER TABLE `biz_answer_question_report` DROP INDEX `assessment_id`;');
         }
     }
 
     protected function isIndexExist($table, $filedName, $indexName)
     {
         $sql = "show index from `{$table}` where column_name = '{$filedName}' and Key_name = '{$indexName}';";
-        $result = $this->biz['db']->fetchAssoc($sql);
-        return empty($result) ? false : true;
+        $result = $this->getContainer()['db']->fetchAssoc($sql);
+
+        return !empty($result);
     }
 }
