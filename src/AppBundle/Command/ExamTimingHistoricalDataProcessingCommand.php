04aefa9d45e0604d585091f24cc705c700ac2064
zhaolishuang <zhaolishuang@edusoho.com>
2022-11-29 15:50:45 +0800

    feat: #93039 计时考试历史数据处理

--- src/AppBundle/Command/ExamTimingHistoricalDataProcessingCommand.php
+++ src/AppBundle/Command/ExamTimingHistoricalDataProcessingCommand.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command;
 
+use Codeages\Biz\ItemBank\Answer\Service\AnswerRecordService;
 use Codeages\Biz\ItemBank\Answer\Service\AnswerSceneService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,10 +24,15 @@ class ExamTimingHistoricalDataProcessingCommand extends BaseCommand
      * 考试时长等于0
      */
     protected function LimitedTimeAmountZero(OutputInterface $output) {
-        $answerScenes = $this->getAnswerSceneService()->search(['limited_time' => 0], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name']);
+        $answerScenes = $this->getAnswerSceneService()->search(['limited_time' => 0], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name', 'limited_time']);
         foreach ($answerScenes as $answerScene) {
-            $this->getAnswerSceneService()->update($answerScene['id'], ['exam_mode'=> 1, 'enable_facein'=> 0, 'name' => $answerScene['name']]);
+            $answerScene = $this->getAnswerSceneService()->update($answerScene['id'], ['exam_mode'=> 1, 'enable_facein'=> 0, 'name' => $answerScene['name']]);
+            $answerRecords = $this->getAnswerRecordService()->search(['answer_scene_id' => $answerScene['id']], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'limited_time']);
+            foreach ($answerRecords as $answerRecord) {
+                $this->getAnswerRecordService()->update($answerRecord['id'], ['exam_mode'=> $answerScene['exam_mode'], 'limited_time' => $answerScene['limited_time']]);
+            }
         }
+
         $output->writeln("<info>执行成功</info>");
     }
 
@@ -34,9 +40,13 @@ class ExamTimingHistoricalDataProcessingCommand extends BaseCommand
      * 考试时长大于0
      */
     protected function LimitedTimeGreaterThanZero(OutputInterface $output) {
-        $answerScenes = $this->getAnswerSceneService()->search(['limited_times' => 0], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name']);
+        $answerScenes = $this->getAnswerSceneService()->search(['limited_times' => 0], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name', 'limited_time']);
         foreach ($answerScenes as $answerScene) {
-            $this->getAnswerSceneService()->update($answerScene['id'], ['exam_mode'=> 0, 'name' => $answerScene['name']]);
+            $answerScene = $this->getAnswerSceneService()->update($answerScene['id'], ['exam_mode'=> 0, 'name' => $answerScene['name']]);
+            $answerRecords = $this->getAnswerRecordService()->search(['answer_scene_id' => $answerScene['id']], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'limited_time']);
+            foreach ($answerRecords as $answerRecord) {
+                $this->getAnswerRecordService()->update($answerRecord['id'], ['exam_mode'=> $answerScene['exam_mode'], 'limited_time' => $answerScene['limited_time']]);
+            }
         }
         $output->writeln("<info>执行成功</info>");
     }
@@ -49,4 +59,11 @@ class ExamTimingHistoricalDataProcessingCommand extends BaseCommand
         return $this->getBiz()->service('ItemBank:Answer:AnswerSceneService');
     }
 
+    /**
+     * @return AnswerRecordService
+     */
+    protected function getAnswerRecordService()
+    {
+        return $this->createService('ItemBank:Answer:AnswerRecordService');
+    }
 }
