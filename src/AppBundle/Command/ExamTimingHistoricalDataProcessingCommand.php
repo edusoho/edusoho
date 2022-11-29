a5bb6e4d419c88d32c2e264320f5b697c82e9d22
zhaolishuang <zhaolishuang@edusoho.com>
2022-11-29 19:41:29 +0800

    feat: #93039 计时考试历史数据处理

--- src/AppBundle/Command/ExamTimingHistoricalDataProcessingCommand.php
+++ src/AppBundle/Command/ExamTimingHistoricalDataProcessingCommand.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command;
 
+use Biz\Activity\Service\ActivityService;
 use Codeages\Biz\ItemBank\Answer\Service\AnswerRecordService;
 use Codeages\Biz\ItemBank\Answer\Service\AnswerSceneService;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,14 +25,23 @@ class ExamTimingHistoricalDataProcessingCommand extends BaseCommand
      * 考试时长等于0
      */
     protected function LimitedTimeAmountZero(OutputInterface $output) {
-        $answerScenes = $this->getAnswerSceneService()->search(['limited_time' => 0], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name', 'limited_time']);
+        $answerRecordData= [];
+        $activitys = $this->getActivityService()->search(['mediaType'=>'testpaper'], [], 0, PHP_INT_MAX, ['id', 'mediaId']);
+        $activityTestpapers = $this->getTestpaperActivityService()->findActivitiesByIds(array_column($activitys,'mediaId'));
+
+        $answerScenes = $this->getAnswerSceneService()->search(['limited_time' => 0, 'ids'=>array_column($activityTestpapers, 'answerSceneId')], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name', 'limited_time']);
         foreach ($answerScenes as $answerScene) {
             $answerScene = $this->getAnswerSceneService()->update($answerScene['id'], ['exam_mode'=> 1, 'enable_facein'=> 0, 'name' => $answerScene['name']]);
             $answerRecords = $this->getAnswerRecordService()->search(['answer_scene_id' => $answerScene['id']], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'limited_time']);
             foreach ($answerRecords as $answerRecord) {
-                $this->getAnswerRecordService()->update($answerRecord['id'], ['exam_mode'=> $answerScene['exam_mode'], 'limited_time' => $answerScene['limited_time']]);
+                $answerRecordData[$answerRecord['id']] = [
+                    'exam_mode' => $answerScene['exam_mode'],
+                    'limited_time' => $answerScene['limited_time'],
+                ];
             }
         }
+
+        $this->getAnswerRecordDao()->batchUpdate(array_keys($answerRecordData),array_values($answerRecordData));
 
         $output->writeln("<info>执行成功</info>");
     }
@@ -40,14 +50,24 @@ class ExamTimingHistoricalDataProcessingCommand extends BaseCommand
      * 考试时长大于0
      */
     protected function LimitedTimeGreaterThanZero(OutputInterface $output) {
-        $answerScenes = $this->getAnswerSceneService()->search(['limited_times' => 0], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name', 'limited_time']);
+        $answerRecordData= [];
+        $activitys = $this->getActivityService()->search(['mediaType'=>'testpaper'], [], 0, PHP_INT_MAX, ['id', 'mediaId']);
+        $activityTestpapers = $this->getTestpaperActivityService()->findActivitiesByIds(array_column($activitys,'mediaId'));
+
+        $answerScenes = $this->getAnswerSceneService()->search(['limited_times' => 0, 'ids'=>array_column($activityTestpapers, 'answerSceneId')], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'enable_facein', 'name', 'limited_time']);
         foreach ($answerScenes as $answerScene) {
             $answerScene = $this->getAnswerSceneService()->update($answerScene['id'], ['exam_mode'=> 0, 'name' => $answerScene['name']]);
             $answerRecords = $this->getAnswerRecordService()->search(['answer_scene_id' => $answerScene['id']], [], 0, PHP_INT_MAX, ['id', 'exam_mode', 'limited_time']);
             foreach ($answerRecords as $answerRecord) {
-                $this->getAnswerRecordService()->update($answerRecord['id'], ['exam_mode'=> $answerScene['exam_mode'], 'limited_time' => $answerScene['limited_time']]);
+                $answerRecordData[$answerRecord['id']] = [
+                    'exam_mode' => $answerScene['exam_mode'],
+                    'limited_time' => $answerScene['limited_time'],
+                ];
             }
         }
+
+        $this->getAnswerRecordDao()->batchUpdate(array_keys($answerRecordData),array_values($answerRecordData));
+
         $output->writeln("<info>执行成功</info>");
     }
 
@@ -66,4 +86,24 @@ class ExamTimingHistoricalDataProcessingCommand extends BaseCommand
     {
         return $this->createService('ItemBank:Answer:AnswerRecordService');
     }
+
+
+    protected function getTestpaperActivityService()
+    {
+        return $this->createService('Activity:TestpaperActivityService');
+    }
+
+    /**
+     * @return ActivityService
+     */
+    protected function getActivityService()
+    {
+        return $this->getBiz()->service('Activity:ActivityService');
+    }
+
+    protected function getAnswerRecordDao()
+    {
+        return $this->getBiz()->dao('ItemBank:Answer:AnswerRecordDao');
+    }
 }
+
