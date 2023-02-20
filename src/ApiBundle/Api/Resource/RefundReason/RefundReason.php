65aeaaa5b345e4c718d2cace19c00c6de8e90778
zhaolishuang <zhaolishuang@edusoho.com>
2023-02-20 14:31:23 +0800

    feat: #94484 新增APP退出原因接口

---
+++ src/ApiBundle/Api/Resource/RefundReason/RefundReason.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace ApiBundle\Api\Resource\RefundReason;
+
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+
+class RefundReason extends AbstractResource
+{
+    public function get(ApiRequest $request)
+    {
+        $action = $request->query->get('action');
+        $refundReason = $this->container->get('codeages_plugin.dict_twig_extension')->getDict('refund_reason');
+        return [
+            'action' => $action,
+            'refundReason' => $refundReason
+        ];
+    }
+}
