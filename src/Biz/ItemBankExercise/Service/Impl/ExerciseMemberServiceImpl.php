d0d6beb902a4827ff9af5797e632f2cab6e6c5e9
zhaolishuang <zhaolishuang@edusoho.com>
2023-02-20 18:01:57 +0800

    feat: #94488 新增APP题库退出接口

--- src/Biz/ItemBankExercise/Service/Impl/ExerciseMemberServiceImpl.php
+++ src/Biz/ItemBankExercise/Service/Impl/ExerciseMemberServiceImpl.php
@@ -174,7 +174,7 @@ class ExerciseMemberServiceImpl extends BaseService implements ExerciseMemberSer
                 $this->getLogService()->info(
                     'item_bank_exercise',
                     'exit_item_bank_exercise',
-                    "学员{$user['nickname']}(#{$user['id']})退出了班级《{$exercise['title']}》(#{$exercise['id']})"
+                    "学员{$user['nickname']}(#{$user['id']})退出了教学计划《{$exercise['title']}》(#{$exercise['id']})"
                 );
             } else {
                 $this->getLogService()->info(
