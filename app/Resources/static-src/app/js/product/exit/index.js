3a4bb397f873d17b740f18113bdbecc169a72fdc
吴博 <wubo@howzhi.com>
2023-03-02 18:07:16 +0800

    feat: #230302 题库优化V1.0

--- app/Resources/static-src/app/js/product/exit/index.js
+++ app/Resources/static-src/app/js/product/exit/index.js
@@ -2,28 +2,43 @@ let $form = $('#refund-form');
 let $modal = $form.parents('.modal');
 let $reasonNote = $form.find('#reasonNote');
 let $warnning = $form.find('.warnning');
+const $submitBtn = $('button[type="submit"]')
 
 $form.find('[name="reason[type]"]').on('change', function () {
   let $this = $(this),
-    $selected = $this.find('option:selected');
+  $selected = $this.find('option:selected');
+
   if ($selected.val() == 'other') {
     $reasonNote.val('').removeClass('hide');
+    $submitBtn.attr('disabled', true)
   } else {
     $reasonNote.addClass('hide').val($selected.text());
+    if ($selected.val() !== 'reason') {
+      $submitBtn.removeAttr('disabled')
+    } else {
+      $submitBtn.attr('disabled', true)
+    }
   }
   $warnning.text('');
-}).change();
+})
 
 $reasonNote.on('change', function () {
   let $this = $(this);
-  if ($this.val().length > 120) {
-    $warnning.text(Translator.trans('order.refund.reason_limit_hint'));
-  } else if ($this.val().length == 0) {
+
+  if ($this.val().length == 0) {
     $warnning.text(Translator.trans('order.refund.reason_required_hint'));
   } else {
     $warnning.text('');
   }
-}).change();
+
+  if ($form.find('[name="reason[type]"] option:selected').val() === 'other') {
+    if ($this.val().length == 0) {
+      $submitBtn.attr('disabled', true)
+    } else {
+      $submitBtn.removeAttr('disabled')
+    }
+  }
+})
 
 $form.on('submit', function () {
   if ($form.find('#reasonType').val() == 'reason') {
