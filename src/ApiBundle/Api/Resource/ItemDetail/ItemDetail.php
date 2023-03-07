6fd7aef0247a21aa3327963705ca9e7fe04c3d88
zhaolishuang <zhaolishuang@edusoho.com>
2023-03-07 14:42:58 +0800

    feat: #94490 h5附件查看

--- src/ApiBundle/Api/Resource/ItemDetail/ItemDetail.php
+++ src/ApiBundle/Api/Resource/ItemDetail/ItemDetail.php
@@ -2,53 +2,35 @@
 
 namespace ApiBundle\Api\Resource\ItemDetail;
 
-use ApiBundle\Api\Annotation\ApiConf;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
-use Biz\Classroom\Service\ClassroomService;
 use Biz\CloudFile\Service\CloudFileService;
 use Biz\CloudPlatform\Service\ResourceFacadeService;
-use Biz\Course\MemberException;
-use Biz\Course\Service\MemberService;
 use Biz\ItemBankExercise\ItemBankExerciseMemberException;
 use Biz\ItemBankExercise\Service\ExerciseMemberService;
+use Biz\QuestionBank\Service\QuestionBankService;
+use Codeages\Biz\ItemBank\Item\Dao\QuestionDao;
 use Codeages\Biz\ItemBank\Item\Service\AttachmentService;
+use Codeages\Biz\ItemBank\Item\Service\ItemService;
 
 class ItemDetail extends AbstractResource
 {
-    /**
-     * @ApiConf(isRequiredAuth=false)
-     */
     public function search(ApiRequest $request)
     {
         $req = $request->query->all();
         $user = $this->getCurrentUser();
 
-        switch ($req['type']) {
-            case 'course':
-                if (!$this->getCourseMemberService()->isCourseMember($req['id'], $user['id'])) {
-                    throw MemberException::NOTFOUND_MEMBER();
-                }
-                break;
-            case 'classroom':
-                if (!$this->getClassroomService()->getClassroomMember($req['id'], $user['id'])) {
-                    throw MemberException::NOTFOUND_MEMBER();
-                }
-                break;
-            case 'exercise':
-                if (!$this->getExerciseMemberService()->isExerciseMember($req['id'], $user['id'])) {
-                    throw ItemBankExerciseMemberException::NOTFOUND_MEMBER();
-                }
-                break;
-            default:
-                throw MemberException::NOTFOUND_MEMBER();
-                break;
-        }
-
         $attachment = $this->getAttachmentService()->getAttachmentByGlobalId($req['globalId']);
         if (empty($attachment)) {
             return ['result' => false, 'msg' => '文件不存在', 'data' => []];
         }
+
+//        $question = $this->getQuestionDao()->get($attachment['target_id']);
+//        $item = $this->getItemService()->getItem($question['item_id']);
+//        $itemBank = $this->getItemBankService()->getItemBank($item['bank_id']);
+//        $questionBank = $this->getQuestionBankService()->getQuestionBankByItemBankId($itemBank['id']);
+//        if (!$this->getQuestionBankService()->canManageBank($questionBank['id']) && !$this->getExerciseMemberService()->isExerciseMember($req['id'], $user['id'])) {
+//        }
 
         $file = $this->getCloudFileService()->getByGlobalId($req['globalId']);
         if (empty($file)) {
@@ -100,19 +82,27 @@ class ItemDetail extends AbstractResource
     }
 
     /**
-     * @return MemberService
+     * @return QuestionDao
      */
-    protected function getCourseMemberService()
+    protected function getQuestionDao()
     {
-        return $this->service('Course:MemberService');
+        return $this->biz->dao('ItemBank:Item:QuestionDao');
     }
 
     /**
-     * @return ClassroomService
+     * @return ItemService
      */
-    private function getClassroomService()
+    protected function getItemService()
     {
-        return $this->service('Classroom:ClassroomService');
+        return $this->service('ItemBank:Item:ItemService');
+    }
+
+    /**
+     * @return \Codeages\Biz\ItemBank\ItemBank\Service\ItemBankService
+     */
+    protected function getItemBankService()
+    {
+        return $this->service('ItemBank:ItemBank:ItemBankService');
     }
 
     /**
@@ -120,6 +110,14 @@ class ItemDetail extends AbstractResource
      */
     protected function getExerciseMemberService()
     {
-        return $this->service('ItemBankExercise:ExerciseMemberService');
+        return $this->biz->service('ItemBankExercise:ExerciseMemberService');
+    }
+
+    /**
+     * @return QuestionBankService
+     */
+    protected function getQuestionBankService()
+    {
+        return $this->service('QuestionBank:QuestionBankService');
     }
 }
