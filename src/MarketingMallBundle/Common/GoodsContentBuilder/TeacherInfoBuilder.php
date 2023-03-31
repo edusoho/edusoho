00ee008e58491b16da79d6cd0f4d09a5857195fb
zhaolishuang <zhaolishuang@edusoho.com>
2023-03-31 15:53:19 +0800

    feat: #20230331 删除无用代码

--- src/MarketingMallBundle/Common/GoodsContentBuilder/TeacherInfoBuilder.php
+++ src/MarketingMallBundle/Common/GoodsContentBuilder/TeacherInfoBuilder.php
@@ -10,7 +10,6 @@ class TeacherInfoBuilder extends AbstractBuilder
     public function build($id)
     {
         $user = $this->getUserService()->getUserAndProfile($id);
-        file_put_contents('/tmp/1.txt', json_encode($user));
 
         return [
             'userId' => $id,
