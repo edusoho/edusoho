0e32a553fb42f6491e90a79ad4faafea63b6ee87
陈景森 <chenjingsen@edusoho.com>
2023-04-27 19:02:49 +0800

    fix: #230427 增加支付配置检查接口

--- src/ApiBundle/Api/Resource/UnifiedPayment/UnifiedPayment.php
+++ src/ApiBundle/Api/Resource/UnifiedPayment/UnifiedPayment.php
@@ -17,25 +17,37 @@ class UnifiedPayment extends AbstractResource
     /**
      * @ApiConf(isRequiredAuth=false)
      */
+    public function search(ApiRequest $request)
+    {
+        $params = $request->query->all();
+        $payload = $this->parsePayload($params);
+
+        $trade = $this->getUnifiedPaymentService()->getTradeByTradeSn($payload['tradeSn']);
+        if (empty($trade)) {
+            throw new NotFoundHttpException(sprintf('订单#%s未找到', $payload['tradeSn']));
+        }
+        if (!$this->getUnifiedPaymentService()->isEnabledPlatform($trade['platform'])) {
+            return ['success' => false, 'message' => '支付方式未配置，请联系机构处理。'];
+        }
+        if ('paid' === $trade['status']) {
+            return ['success' => false, 'message' => '已支付'];
+        }
+
+        return [
+            'orderSn' => $trade['orderSn'],
+            'amount' => $trade['amount'],
+            'siteName' => SettingToolkit::getSetting('site.name'),
+            'redirectUrl' => $trade['redirectUrl'],
+        ];
+    }
+
+    /**
+     * @ApiConf(isRequiredAuth=false)
+     */
     public function add(ApiRequest $request)
     {
         $params = $request->request->all();
-        if (empty($params['token'])) {
-            throw CommonException::ERROR_PARAMETER_MISSING();
-        }
-        $storage = $this->getSettingService()->get('storage', []);
-        if (empty($storage['cloud_secret_key'])) {
-            throw new InvalidArgumentException('请配置授权码');
-        }
-
-        $payload = (array) JWT::decode($params['token'], $storage['cloud_secret_key'], ['HS256']);
-        if (empty($payload)) {
-            throw new InvalidArgumentException('令牌内容错误');
-        }
-
-        if (empty($payload['tradeSn'])) {
-            throw new InvalidArgumentException('令牌内容订单信息缺失：'.json_encode($payload));
-        }
+        $payload = $this->parsePayload($params);
 
         $trade = $this->getUnifiedPaymentService()->getTradeByTradeSn($payload['tradeSn']);
         if (empty($trade)) {
@@ -56,6 +68,28 @@ class UnifiedPayment extends AbstractResource
             'siteName' => SettingToolkit::getSetting('site.name'),
             'redirectUrl' => $trade['redirectUrl'],
         ];
+    }
+
+    public function parsePayload(array $params): array
+    {
+        if (empty($params['token'])) {
+            throw CommonException::ERROR_PARAMETER_MISSING();
+        }
+        $storage = $this->getSettingService()->get('storage', []);
+        if (empty($storage['cloud_secret_key'])) {
+            throw new InvalidArgumentException('请配置授权码');
+        }
+
+        $payload = (array) JWT::decode($params['token'], $storage['cloud_secret_key'], ['HS256']);
+        if (empty($payload)) {
+            throw new InvalidArgumentException('令牌内容错误');
+        }
+
+        if (empty($payload['tradeSn'])) {
+            throw new InvalidArgumentException('令牌内容订单信息缺失：'.json_encode($payload));
+        }
+
+        return $payload;
     }
 
     /**
