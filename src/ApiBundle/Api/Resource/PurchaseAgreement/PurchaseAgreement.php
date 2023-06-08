2a47737a7b1bcc6d9eda5200cecae0c5c3c170a5
zhaolishuang <zhaolishuang@edusoho.com>
2023-06-08 18:37:48 +0800

    fix: #96925 用户购买协议设置功能增加内容过滤

--- src/ApiBundle/Api/Resource/PurchaseAgreement/PurchaseAgreement.php
+++ src/ApiBundle/Api/Resource/PurchaseAgreement/PurchaseAgreement.php
@@ -2,34 +2,35 @@
 
 namespace ApiBundle\Api\Resource\PurchaseAgreement;
 
+use ApiBundle\Api\Annotation\Access;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use AppBundle\Common\ArrayToolkit;
-use Biz\User\UserException;
 
 class PurchaseAgreement extends AbstractResource
 {
     public function search(ApiRequest $request)
     {
-        $user = $this->getCurrentUser();
-        if (!$user->isSuperAdmin() && !$user->isAdmin()) {
-            throw UserException::PERMISSION_DENIED();
-        }
-
         return $this->getSettingService()->get('course_purchase_agreement', ['enabled' => 0, 'title' => '', 'content' => '', 'type' => 'tick']);
     }
 
+    /**
+     * @return array
+     * @Access(roles="ROLE_ADMIN,ROLE_SUPER_ADMIN")
+     */
     public function add(ApiRequest $request)
     {
-        $user = $this->getCurrentUser();
-        if (!$user->isSuperAdmin() && !$user->isAdmin()) {
-            throw UserException::PERMISSION_DENIED();
-        }
-
         $params = $request->request->all();
-
+        $params['content'] = $this->purifyHtml($params['content']);
         $this->getSettingService()->set('course_purchase_agreement', ArrayToolkit::parts($params, ['enabled', 'title', 'content', 'type']));
 
         return true;
+    }
+
+    protected function purifyHtml($html, $trusted = false)
+    {
+        $htmlHelper = $this->biz['html_helper'];
+
+        return $htmlHelper->purify($html, $trusted);
     }
 }
