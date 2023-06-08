0b594d5c3481c40dd9e6627782912e41e76ff36a
zhaolishuang <zhaolishuang@edusoho.com>
2023-06-08 15:07:52 +0800

    fix: #96925 增加用户购买协议设置接口鉴权

--- src/ApiBundle/Api/Resource/PurchaseAgreement/PurchaseAgreement.php
+++ src/ApiBundle/Api/Resource/PurchaseAgreement/PurchaseAgreement.php
@@ -5,17 +5,27 @@ namespace ApiBundle\Api\Resource\PurchaseAgreement;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use AppBundle\Common\ArrayToolkit;
-use Biz\System\Service\SettingService;
+use Biz\User\UserException;
 
 class PurchaseAgreement extends AbstractResource
 {
     public function search(ApiRequest $request)
     {
+        $user = $this->getCurrentUser();
+        if (!$user->isSuperAdmin() && !$user->isAdmin()) {
+            throw UserException::PERMISSION_DENIED();
+        }
+
         return $this->getSettingService()->get('course_purchase_agreement', ['enabled' => 0, 'title' => '', 'content' => '', 'type' => 'tick']);
     }
 
     public function add(ApiRequest $request)
     {
+        $user = $this->getCurrentUser();
+        if (!$user->isSuperAdmin() && !$user->isAdmin()) {
+            throw UserException::PERMISSION_DENIED();
+        }
+
         $params = $request->request->all();
 
         $this->getSettingService()->set('course_purchase_agreement', ArrayToolkit::parts($params, ['enabled', 'title', 'content', 'type']));
