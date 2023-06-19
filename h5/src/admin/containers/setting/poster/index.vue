cc923f90018ce9e2e31541c9afb76acefed057fe
zhanghuapeng <zhanghuapeng@edusoho.com>
2023-06-19 17:38:18 +0800

    feat: #97150 修改字符长度的样式和解决无法显示子任务

--- h5/src/admin/containers/setting/poster/index.vue
+++ h5/src/admin/containers/setting/poster/index.vue
@@ -111,7 +111,7 @@
         >
           <el-input 
             size="mini"
-            v-model="linkUrl" 
+            v-model="copyModuleData.link.url" 
             placeholder="例如 http://www.eduosho.com" 
             clearable>
         </el-input>
@@ -233,7 +233,7 @@ export default {
           return this.type === 'course_list' ? data.displayedTitle : data.title;
         }
         if (this.copyModuleData.link.target) {
-          return this.copyModuleData.link.target.title;
+          return this.copyModuleData.link.target.displayedTitle;
         }
       },
       set() {},
@@ -263,6 +263,7 @@ export default {
     },
   },
   created() {
+    console.log(this.copyModuleData.link.target.displayedTitle)
     if (this.pathName !== 'miniprogramSetting') {
       this.type = this.moduleData.data.link.type;
       if (this.moduleData.data.link.type === 'url') {
--- h5/src/h5/containers/goods/components/lesson-directory.vue
+++ h5/src/h5/containers/goods/components/lesson-directory.vue
@@ -30,7 +30,7 @@
                   lessonactive:
                     currentTask == lessonItem.tasks[lessonItem.index].id,
                 }"
-                class="text-overflow ks"
+                class="ks py-8" style="display: flex;"
               >
                 <span
                   v-if="isTry(lessonItem.tasks[lessonItem.index])"
@@ -46,6 +46,7 @@
                   :class="iconfont(lessonItem.tasks[lessonItem.index])"
                   class="iconfont"
                 />
+                <div>
                 {{
                   Number(lessonItem.tasks[lessonItem.index].isOptional)
                     ? ''
@@ -57,6 +58,7 @@
                         lessonItem.title
                       }`
                 }}
+                </div>
               </span>
             </div>
 
@@ -141,17 +143,18 @@
           <div class="litem" v-if="showTask(taskItem, taskIndex)">
             <div
               :class="{ lessonactive: currentTask == Number(taskItem.id) }"
-              class="litem-r text-overflow"
+              class="litem-r" style="display: flex;"
             >
               <span v-if="isTry(taskItem)" class="tryLes">{{ $t('goods.preview') }}</span>
               <span v-if="isFree(taskItem)" class="freeAdmission">{{ $t('goods.free') }}</span>
               <i :class="iconfont(taskItem)" class="iconfont" />
+              <div>
               {{ Number(taskItem.isOptional) ? '' : $t('goods.lesson')
-              }}{{
-                Number(taskItem.isOptional)
-                  ? taskItem.title
-                  : `${taskItem.number}:${taskItem.title}`
               }}
+              {{
+                Number(taskItem.isOptional) ? taskItem.title : taskItem.number + ':' + taskItem.title
+              }}
+              </div>
             </div>
             <div v-if="showTask(taskItem, taskIndex)" class="litem-l clearfix">
               <span :class="[liveClass(taskItem), 'text-overflow']">{{
@@ -229,6 +232,7 @@ export default {
     },
   },
   mounted() {
+    console.log(this.lesson)
     if (Object.keys(this.$route.query).length) {
       const { sourceType, taskId } = this.$route.query;
       this.setSourceType({
