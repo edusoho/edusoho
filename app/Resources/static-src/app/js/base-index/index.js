80689172ef477069364b1e182bc5ed74e386738b
zhanghuapeng <zhanghuapeng@edusoho.com>
2023-06-25 16:44:28 +0800

    feat: #230625 修改代码

--- app/Resources/static-src/app/js/base-index/index.js
+++ app/Resources/static-src/app/js/base-index/index.js
@@ -2,5 +2,5 @@ window.addEventListener('load', (event) => {
   $('[data-toggle="tooltip"]').tooltip({
   html: true,
   });
-  $(".cd-notification")[0].css('overflow', 'none');
+  $(".cd-notification")[0].css({'overflow': 'none'});
 });
