076d6d6dedf8f7fd0cf90c7aabdea341726e097b
zhanghuapeng <zhanghuapeng@edusoho.com>
2023-06-25 16:40:36 +0800

    feat: #230625 直播测试7.0

--- app/Resources/static-src/app/js/base-index/index.js
+++ app/Resources/static-src/app/js/base-index/index.js
@@ -2,5 +2,5 @@ window.addEventListener('load', (event) => {
   $('[data-toggle="tooltip"]').tooltip({
   html: true,
   });
-  document.getElementsByClassName("cd-notification")[0].css('overflow', 'none')
+  $(".cd-notification")[0].css('overflow', 'none');
 });
