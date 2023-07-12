f350e41c82be79ee17fdb0506ea2f6e65c09eee5
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2023-07-12 19:23:44 +0800

    fix: #230712 修复考试有效期无限或指定开始时间无法考试的问题

--- web/activities/testpaper/static-src/js/show/index.js
+++ web/activities/testpaper/static-src/js/show/index.js
@@ -1,16 +1,15 @@
 import notify from 'common/notify';
 
 $('.js-start-exam').on('click', event => {
-	const endTime = $('input[name="endTime"]').val() * 1000
+  const endTime = $('input[name="endTime"]').val() * 1000;
 
-	if (endTime <= Date.now()) {
-		event.preventDefault();
+  if (endTime <= Date.now() && endTime != 0) {
+    event.preventDefault();
 
-		notify('danger', Translator.trans('validate.endTime.validity'));
+    notify('danger', Translator.trans('validate.endTime.validity'));
 
-		setTimeout(() => {
-			window,location.reload();
-		}, 2000)
-	}
-
-})
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+  }
+});
