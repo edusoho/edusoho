1ecb063168855731b82ea7d1018898cf64f139e3
Johnson <ziyuorg@gmail.com>
2023-08-09 21:56:14 +0800

    fix: #98096 修复同名事件无法推送更新

--- src/MarketingMallBundle/Biz/AuthSetting/Event/AuthSettingEventSubscriber.php
+++ src/MarketingMallBundle/Biz/AuthSetting/Event/AuthSettingEventSubscriber.php
@@ -12,31 +12,22 @@ class AuthSettingEventSubscriber extends EventSubscriber
     public static function getSubscribedEvents()
     {
         return [
-            'user.auth.setting.update' => 'onUserProtocolUpdate',
-            'user.auth.setting.update' => 'onPasswordLevelUpdate',
+            'user.auth.setting.update' => 'onUserAuthSettingUpdate',
         ];
     }
 
-    public function onUserProtocolUpdate(Event $event)
+    public function onUserAuthSettingUpdate(Event $event)
     {
         if (!$this->getMallService()->isInit()) {
             return;
         }
         $auth = $event->getSubject();
-
         $this->getMallClient()->notifyUserProtocolUpdate([
             'userTerms' => $auth['user_terms'] ?? 'closed',
             'userTermsBody' => $auth['user_terms_body'] ?? '',
             'privacyPolicy' => $auth['privacy_policy'] ?? 'closed',
             'privacyPolicyBody' => $auth['privacy_policy_body'] ?? '',
         ]);
-    }
-
-    public function onPasswordLevelUpdate(Event $event) {
-        if (!$this->getMallService()->isInit()) {
-            return;
-        }
-        $auth = $event->getSubject();
 
         $this->getMallClient()->notifyPasswordLevelUpdate([
             'passwordLevel' => $auth['password_level'] ?? 'low',
