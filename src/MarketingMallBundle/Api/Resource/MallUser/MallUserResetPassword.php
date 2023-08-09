e93708541b344c28629eafb1f30b1e80dc1c3723
Johnson <ziyuorg@gmail.com>
2023-08-09 12:03:55 +0800

    feat: #98052 商城修改密码新增权限认证

--- src/MarketingMallBundle/Api/Resource/MallUser/MallUserResetPassword.php
+++ src/MarketingMallBundle/Api/Resource/MallUser/MallUserResetPassword.php
@@ -2,12 +2,16 @@
 
 namespace MarketingMallBundle\Api\Resource\MallUser;
 
+use ApiBundle\Api\Annotation\AuthClass;
 use ApiBundle\Api\ApiRequest;
 use Biz\User\Service\UserService;
-use Biz\User\UserException;
 use MarketingMallBundle\Api\Resource\BaseResource;
 
 class MallUserResetPassword extends BaseResource {
+
+    /**
+     * @AuthClass(ClassName="MarketingMallBundle\Security\Firewall\MallAuthTokenAuthenticationListener")
+     */
     public function add(ApiRequest $request, $id) {
         $password = $request->request->all();
         $this->getUserService()->initPassword($id, $password['password']);
