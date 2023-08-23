527d829b09d54b0c026962632c8fd277f2aa4c09
zhaolishuang <zhaolishuang@edusoho.com>
2023-08-23 12:52:51 +0800

    feat: #99048 修改错题本信息接口返回值增加过滤

--- src/ApiBundle/Api/Resource/WrongBook/WrongBookFilter.php
+++ src/ApiBundle/Api/Resource/WrongBook/WrongBookFilter.php
@@ -3,19 +3,24 @@
 namespace ApiBundle\Api\Resource\WrongBook;
 
 use ApiBundle\Api\Resource\Filter;
-use ApiBundle\Api\Util\AssetHelper;
-use ApiBundle\Api\Util\TagUtil;
 
 class WrongBookFilter extends Filter
 {
-    protected $publicFields = array(
+    protected $publicFields = [
         'user_id',
         'sum_wrong_num',
         'target_type',
-    );
+        'wrongNumCount',
+    ];
 
     protected function publicFields(&$data)
     {
-        $data['sum_wrong_num']=(int)$data['sum_wrong_num'];
+        if (isset($data['sum_wrong_num'])) {
+            $data['sum_wrong_num'] = (int) $data['sum_wrong_num'];
+        }
+
+        if (isset($data['wrongNumCount'])) {
+            $data['wrongNumCount'] = (int) $data['wrongNumCount'];
+        }
     }
 }
