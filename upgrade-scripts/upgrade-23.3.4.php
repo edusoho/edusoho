cf8461a2ca61bf7e836139bc3d7a79ec15462785
paul <13554292890@163.com>
2023-09-06 16:08:49 +0800

    feat: #99528 初始化云短信参数

---
+++ upgrade-scripts/upgrade-23.3.4.php
@@ -0,0 +1,84 @@
+<?php
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function __construct($biz)
+    {
+        parent::__construct($biz);
+    }
+
+    public function update($index = 0)
+    {
+        try {
+            $dir = realpath($this->biz['kernel.root_dir'] . '/../web/install');
+            $filesystem = new Filesystem();
+            if (!empty($dir)) {
+                $filesystem->remove($dir);
+            }
+        } catch (\Exception $e) {
+            $this->logger('error', $e->getTraceAsString());
+        }
+
+        $smsSetting = $this->getSettingService()->get('cloud_sms');
+        if ($smsSetting && !isset($smsSetting['sms_shipping_notify'])){
+            $smsSetting['sms_shipping_notify'] = 'on';
+            $this->getSettingService()->set('cloud_sms',$smsSetting);
+        }
+    }
+
+
+    protected function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+
+    protected function logger($level, $message)
+    {
+        $version = \AppBundle\System::VERSION;
+        $data = date('Y-m-d H:i:s') . " [{$level}] {$version} " . $message . PHP_EOL;
+        if (!file_exists($this->getLoggerFile())) {
+            touch($this->getLoggerFile());
+        }
+        file_put_contents($this->getLoggerFile(), $data, FILE_APPEND);
+    }
+
+    private function getLoggerFile()
+    {
+        return $this->biz['kernel.root_dir'] . '/../app/logs/upgrade.log';
+    }
+
+    protected function getAppLogDao()
+    {
+        return $this->createDao('CloudPlatform:CloudAppLogDao');
+    }
+}
