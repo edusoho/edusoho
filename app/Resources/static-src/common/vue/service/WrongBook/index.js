f55507e142fe96c5719dbabf1ad253315e63e296
wgs <wanggengsheng@edusoho.com>
2023-09-14 10:52:37 +0800

    feat: #98969 课程练习说明-章节练习说明-错题练习说明-章节分类拖拽

--- app/Resources/static-src/common/vue/service/WrongBook/index.js
+++ app/Resources/static-src/common/vue/service/WrongBook/index.js
@@ -1,5 +1,6 @@
 export * from './WrongBookQuestionShow';
 export * from './WrongBookCondition';
+export * from './WrongBookpool';
 export * from './WrongBookStudentWrongQuestion';
 export * from './WrongBookWrongQuestionDetail';
 export * from './WrongBookSourceManageCondition';
