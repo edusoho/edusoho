fbb9dabd6a00223ba1751e31722d6b99c98c0a97
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2023-09-26 15:20:29 +0800

    fix: #230926 修复单元测试

--- tests/Unit/AppBundle/Component/OAuthClient/QqOAuthClientTest.php
+++ tests/Unit/AppBundle/Component/OAuthClient/QqOAuthClientTest.php
@@ -2,16 +2,16 @@
 
 namespace Tests\Unit\AppBundle\Component\OAuthClient;
 
-use Biz\BaseTestCase;
-use AppBundle\Component\OAuthClient\QqOAuthClient;
 use AppBundle\Common\ReflectionUtils;
+use AppBundle\Component\OAuthClient\QqOAuthClient;
+use Biz\BaseTestCase;
 
 class QqOAuthClientTest extends BaseTestCase
 {
     public function testGetAuthorizeUrl()
     {
-        $client = new QqOAuthClient(array('key' => 'auth_key'));
-        $result = $client->getAuthorizeUrl('www.edusoho.com');
+        $client = new QqOAuthClient(['key' => 'auth_key']);
+        $result = $client->getAuthorizeUrl('www.edusoho.com', 'credential');
         $this->assertEquals(
             'https://graph.qq.com/oauth2.0/authorize?client_id=auth_key&response_type=code&redirect_uri=www.edusoho.com&status=pro',
             $result
@@ -20,57 +20,57 @@ class QqOAuthClientTest extends BaseTestCase
 
     public function testGetAccessToken()
     {
-        $client = new QqOAuthClient(array('key' => 'auth_key', 'secret' => 'auth_secret'));
+        $client = new QqOAuthClient(['key' => 'auth_key', 'secret' => 'auth_secret']);
 
         $request = $this->mockBiz(
             'request',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'getRequest',
-                    'withParams' => array(
+                    'withParams' => [
                         'https://graph.qq.com/oauth2.0/token',
-                        array(
+                        [
                             'grant_type' => 'authorization_code',
                             'client_id' => 'auth_key',
                             'redirect_uri' => 'http://www.edusoho.com',
                             'client_secret' => 'auth_secret',
                             'code' => 'code',
-                        ),
-                    ),
+                        ],
+                    ],
                     'returnValue' => 'expires_in=1231&access_token=get_access_token',
                     'times' => 1,
-                ),
-                array(
+                ],
+                [
                     'functionName' => 'getRequest',
-                    'withParams' => array(
+                    'withParams' => [
                         'https://graph.qq.com/oauth2.0/me',
-                        array(
+                        [
                             'access_token' => 'get_access_token',
-                        ),
-                    ),
-                    'returnValue' => 'callback=('.json_encode(array('openid' => 'get_openid')).')',
+                        ],
+                    ],
+                    'returnValue' => 'callback=('.json_encode(['openid' => 'get_openid']).')',
                     'times' => 1,
-                ),
-                array(
+                ],
+                [
                     'functionName' => 'getRequest',
-                    'withParams' => array(
+                    'withParams' => [
                         'https://graph.qq.com/user/get_user_info',
-                        array(
+                        [
                             'oauth_consumer_key' => 'auth_key',
                             'openid' => 'get_openid',
                             'format' => 'json',
                             'access_token' => 'get_access_token',
-                        ),
-                    ),
-                    'returnValue' => json_encode(array(
+                        ],
+                    ],
+                    'returnValue' => json_encode([
                         'id' => 'get_id',
                         'nickname' => 'get_nickname',
                         'figureurl_qq_2' => 'get_figureurl_qq_2',
                         'gender' => 'get_gender',
-                    )),
+                    ]),
                     'times' => 1,
-                ),
-            )
+                ],
+            ]
         );
 
         ReflectionUtils::setProperty($client, 'request', $request);
@@ -79,12 +79,12 @@ class QqOAuthClientTest extends BaseTestCase
         $request->shouldHaveReceived('getRequest')->times(3);
 
         $this->assertArrayEquals(
-            array(
+            [
                 'userId' => 'get_openid',
                 'expiredTime' => '1231',
                 'access_token' => 'get_access_token',
                 'token' => 'get_access_token',
-            ),
+            ],
             $result
         );
     }
--- tests/Unit/AppBundle/Component/OAuthClient/WeixinWebOAuthClientTest.php
+++ tests/Unit/AppBundle/Component/OAuthClient/WeixinWebOAuthClientTest.php
@@ -2,66 +2,66 @@
 
 namespace Tests\Unit\AppBundle\Component\OAuthClient;
 
-use Biz\BaseTestCase;
-use AppBundle\Component\OAuthClient\WeixinwebOAuthClient;
 use AppBundle\Common\ReflectionUtils;
+use AppBundle\Component\OAuthClient\WeixinwebOAuthClient;
+use Biz\BaseTestCase;
 
 class WeixinWebOAuthClientTest extends BaseTestCase
 {
     public function testGetAuthorizeUrl()
     {
-        $client = new WeixinwebOAuthClient(array('key' => 'auth_key'));
-        $result = $client->getAuthorizeUrl('www.edusoho.com');
+        $client = new WeixinwebOAuthClient(['key' => 'auth_key']);
+        $result = $client->getAuthorizeUrl('www.edusoho.com', 'state');
         $this->assertEquals(
-            'https://open.weixin.qq.com/connect/qrconnect?appid=auth_key&response_type=code&redirect_uri=www.edusoho.com&scope=snsapi_login',
+            'https://open.weixin.qq.com/connect/qrconnect?appid=auth_key&response_type=code&redirect_uri=www.edusoho.com&scope=snsapi_login&state=state',
             $result
         );
     }
 
     public function testGetAccessToken()
     {
-        $client = new WeixinwebOAuthClient(array('key' => 'auth_key', 'secret' => 'auth_secret'));
+        $client = new WeixinwebOAuthClient(['key' => 'auth_key', 'secret' => 'auth_secret']);
 
         $request = $this->mockBiz(
             'request',
-            array(
-                array(
+            [
+                [
                     'functionName' => 'getRequest',
-                    'withParams' => array(
+                    'withParams' => [
                         'https://api.weixin.qq.com/sns/oauth2/access_token',
-                        array(
+                        [
                             'appid' => 'auth_key',
                             'secret' => 'auth_secret',
                             'code' => 'code',
                             'grant_type' => 'authorization_code',
-                        ),
-                    ),
-                    'returnValue' => json_encode(array(
+                        ],
+                    ],
+                    'returnValue' => json_encode([
                         'expires_in' => 1231,
                         'access_token' => 'get_access_token',
                         'openid' => 'get_openid',
-                    )),
+                    ]),
                     'times' => 1,
-                ),
-                array(
+                ],
+                [
                     'functionName' => 'getRequest',
-                    'withParams' => array(
+                    'withParams' => [
                         'https://api.weixin.qq.com/sns/userinfo',
-                        array(
+                        [
                             'openid' => 'get_openid',
                             'access_token' => 'get_access_token',
-                        ),
-                    ),
-                    'returnValue' => json_encode(array(
+                        ],
+                    ],
+                    'returnValue' => json_encode([
                         'openid' => 'get_openid',
                         'unionid' => 'get_unionid',
                         'nickname' => 'get_nickname',
                         'headimgurl' => 'get_headimgurl.png',
                         'sex' => 1,
-                    )),
+                    ]),
                     'times' => 1,
-                ),
-            )
+                ],
+            ]
         );
 
         ReflectionUtils::setProperty($client, 'request', $request);
@@ -70,13 +70,13 @@ class WeixinWebOAuthClientTest extends BaseTestCase
         $request->shouldHaveReceived('getRequest')->times(2);
 
         $this->assertArrayEquals(
-            array(
+            [
                 'userId' => 'get_unionid',
                 'expiredTime' => '1231',
                 'access_token' => 'get_access_token',
                 'token' => 'get_access_token',
                 'openid' => 'get_openid',
-            ),
+            ],
             $result
         );
     }
