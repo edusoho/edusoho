1c63830c1ccda6628d1e01dc7dfdaa866db98be2
zhanghuapeng <zhanghuapeng@edusoho.com>
2023-10-26 15:12:22 +0800

    feat: #230726 课程页面显示

---
+++ app/Resources/static-src/app/js/tab/Tab.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="ibs-vue">
+    <a-tabs default-active-key="1" @change="callback">
+      <a-tab-pane key="1" tab="Tab 1">
+        Content of Tab Pane 1
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2" force-render>
+        Content of Tab Pane 2
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">
+        Content of Tab Pane 3
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+<script>
+import { Tabs } from 'ant-design-vue';
+export default {
+  data(){
+    return {
+      a: 1
+    }
+  },
+  methods: {
+    callback(key) {
+      console.log(key);
+    },
+  },
+}
+</script>
