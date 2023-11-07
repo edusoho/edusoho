144d6d80bebc2f80eb729c9c8f72a6c8d6e897b2
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2023-11-07 14:03:38 +0800

    feat: #231107 材料题没有子题错误提示

--- src/Biz/Question/Adapter/MaterialAdapterTrait.php
+++ src/Biz/Question/Adapter/MaterialAdapterTrait.php
@@ -3,6 +3,8 @@
 namespace Biz\Question\Adapter;
 
 use Codeages\Biz\ItemBank\Item\Type\MaterialItem;
+use ExamParser\Constants\QuestionElement;
+use ExamParser\Constants\QuestionErrors;
 
 trait MaterialAdapterTrait
 {
@@ -11,6 +13,9 @@ trait MaterialAdapterTrait
         $adaptQuestion = $this->adaptQuestion($question);
         $adaptQuestion['type'] = MaterialItem::TYPE;
         $adaptQuestion['subQuestions'] = empty($question['children']) ? [] : $this->adaptQuestions($question['children']);
+        if (empty($adaptQuestion['subQuestions'])) {
+            $adaptQuestion['errors'][QuestionElement::SUB_QUESTIONS] = $this->adaptError(QuestionElement::SUB_QUESTIONS, QuestionErrors::NO_SUB_QUESTIONS);
+        }
         $errors = $this->adaptMaterialErrors($adaptQuestion['subQuestions']);
         if ($errors) {
             $adaptQuestion['errors'] = empty($adaptQuestion['errors']) ? $errors : array_merge($adaptQuestion['errors'], $errors);
