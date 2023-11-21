fe0101a64767f9b1225c99b5f2aabfe66e8d4b42
Summer <summer@SummerdeMacBook-Air.local>
2023-11-21 15:39:35 +0800

    feat: #231121 修改组件引入方式

--- app/Resources/static-src/app/js/question-bank/duplicative-questions/index.js
+++ app/Resources/static-src/app/js/question-bank/duplicative-questions/index.js
@@ -1,13 +1,7 @@
 import DuplicativeQuestions from './index.vue';
 import qs from 'qs';
-import { Button, Message, Modal } from '@codeages/design-vue';
-  
-Vue.use(Button)
-Vue.use(Modal)
 
 Vue.prototype.$qs = qs;
-Vue.prototype.$confirm = Modal.confirm;
-Vue.prototype.$message = Message;
 
 
 Vue.filter('trans', function (value, params) {
@@ -19,29 +13,3 @@ new Vue({
   el: '#app',
   render: createElement => createElement(DuplicativeQuestions)
 })
-
-
-// const wechatIntro = () => {
-//     introJs().setOptions({
-//         steps: [{
-//           element: '.duplicate-question',
-//           intro: Translator.trans('upgrade.cloud.capabilities.to.experience'),
-//           position: 'bottom',
-//         },
-//         {
-//             element: '.question-head',
-//             intro: Translator.trans('upgrade.cloud.capabilities.to.experience'),
-//             position: 'bottom',
-//           }],
-//         doneLabel: '我知道了（2/2）',
-//         nextLabel: '下一步（1/2）',
-//         showBullets: false,
-//         showStepNumbers: false,
-//         exitOnEsc: false,
-//         exitOnOverlayClick: false,
-//         tooltipClass: '',
-//       }).start()
-//   }
-
-  
-//     wechatIntro();
--- app/Resources/static-src/libs/vue.js
+++ app/Resources/static-src/libs/vue.js
@@ -1,4 +1,13 @@
 import Vue from 'vue/dist/vue.esm.js';
+import { Button, Message, Modal } from '@codeages/design-vue';
+
+Vue.use(Button)
+Vue.use(Modal)
+
+Vue.prototype.$confirm = Modal.confirm;
+Vue.prototype.$message = Message;
+
+
 window.Vue = Vue;
 
 export default window.Vue;
