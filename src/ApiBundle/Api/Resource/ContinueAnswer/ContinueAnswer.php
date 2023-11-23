06a7b84041eba2c2bea33438d2bccea682441240
Johnson <ziyuorg@gmail.com>
2023-11-23 19:41:29 +0800

    fix: #101208 修复继续答题未正常抛出异常

--- src/ApiBundle/Api/Resource/ContinueAnswer/ContinueAnswer.php
+++ src/ApiBundle/Api/Resource/ContinueAnswer/ContinueAnswer.php
@@ -9,6 +9,10 @@ use ApiBundle\Api\Resource\Assessment\AssessmentFilter;
 use ApiBundle\Api\Resource\Assessment\AssessmentResponseFilter;
 use Biz\Activity\Service\ActivityService;
 use Biz\Common\CommonException;
+use Biz\Course\CourseException;
+use Biz\Course\Service\CourseService;
+use Biz\ItemBankExercise\Service\ExerciseService;
+use Biz\Testpaper\ExerciseException;
 use Codeages\Biz\ItemBank\Answer\Constant\ExerciseMode;
 use Codeages\Biz\ItemBank\Answer\Service\AnswerRandomSeqService;
 use Codeages\Biz\ItemBank\Answer\Service\AnswerReviewedQuestionService;
@@ -19,6 +23,19 @@ class ContinueAnswer extends AbstractResource
 {
     public function add(ApiRequest $request)
     {
+        $assessmentResponse = $request->request->all();
+        if (!empty($assessmentResponse['courseId'])) {
+            $course = $this->getCourseService()->getCourse($assessmentResponse['courseId']);
+            if ('0' == $course['canLearn']) {
+                throw CourseException::CLOSED_COURSE();
+            }
+        }
+        if (!empty($assessmentResponse['exerciseId'])) {
+            $exercise = $this->getExerciseService()->get($assessmentResponse['exerciseId']);
+            if ('closed' == $exercise['status']) {
+                throw ExerciseException::CLOSED_EXERCISE();
+            }
+        }
         $answerRecord = $this->getAnswerRecordService()->get($request->request->get('answer_record_id'));
         if (empty($answerRecord) || $this->getCurrentUser()['id'] != $answerRecord['user_id']) {
             throw CommonException::ERROR_PARAMETER();
@@ -126,5 +143,21 @@ class ContinueAnswer extends AbstractResource
     protected function getAnswerReviewedQuestionService()
     {
         return $this->service('ItemBank:Answer:AnswerReviewedQuestionService');
+    }
+
+    /**
+     * @return CourseService
+     */
+    protected function getCourseService()
+    {
+        return $this->service('Course:CourseService');
+    }
+
+    /**
+     * @return ExerciseService
+     */
+    protected function getExerciseService()
+    {
+        return $this->service('ItemBankExercise:ExerciseService');
     }
 }
