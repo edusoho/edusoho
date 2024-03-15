2921afbdb89b7df775a5845a051d1f6103a7530a
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2024-03-15 19:23:32 +0800

    fix: #240315 修复系统升级每次请求都至少10s的问题

--- src/Biz/QiQiuYun/Service/Impl/QiQiuYunSdkProxyServiceImpl.php
+++ src/Biz/QiQiuYun/Service/Impl/QiQiuYunSdkProxyServiceImpl.php
@@ -3,10 +3,11 @@
 namespace Biz\QiQiuYun\Service\Impl;
 
 use Biz\BaseService;
+use Biz\QiQiuYun\Service\QiQiuYunSdkProxyService;
 use QiQiuYun\SDK\Service\ESopService;
 use QiQiuYun\SDK\Service\NotificationService;
 
-class QiQiuYunSdkProxyServiceImpl extends BaseService
+class QiQiuYunSdkProxyServiceImpl extends BaseService implements QiQiuYunSdkProxyService
 {
     public function pushEventTracking($action, $data = null)
     {
