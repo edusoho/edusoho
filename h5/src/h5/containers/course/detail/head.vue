6b6f701558fb4a85f60bd615b654ed04d41d8be3
李治云 <lizhiyun@edusoho.com>
2024-04-11 17:43:26 +0800

    feat:# 240411h5跳转app的修改

--- h5/src/h5/containers/course/detail/head.vue
+++ h5/src/h5/containers/course/detail/head.vue
@@ -442,7 +442,7 @@ export default {
       }
 
       if (this.courseSettings.only_learning_on_APP==0) {
-        const { goodsId, courseId } = this.course.details;
+        const { goodsId, id } = this.course.details;
 
         window.location.href = `kuozhi://${window.location.host}?courseId=${courseId}&goodsId=${goodsId}`; 
 
