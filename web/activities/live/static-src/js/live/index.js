cbe4628059f7bb11d0f4015d2475287439bfd1c1
吴博 <wubo@howzhi.com>
2024-04-25 19:31:41 +0800

    feat: #240425 学习页面的仅限APP学习

--- web/activities/live/static-src/js/live/index.js
+++ web/activities/live/static-src/js/live/index.js
@@ -1,8 +1,35 @@
 import ActivityEmitter from 'app/js/activity/activity-emitter';
+import 'codeages-design';
+
 export default class LiveShow {
   constructor() {
     this.interval = 1;
     this.emitter = new ActivityEmitter();
+
+    this.init()
+  }
+
+  async init() {
+    const { only_learning_on_APP } = await $.ajax({
+      url: '/api/settings/course',
+      headers: {
+        Accept: 'application/vnd.edusoho.v2+json'
+      }
+    })
+
+    if (only_learning_on_APP === 1) {
+      cd.confirm({
+        title: Translator.trans('activity.video.only_can_app_title'),
+        content: Translator.trans('activity.video.only_can_app_desc'),
+        okText: Translator.trans('site.confirm'),
+        cancelText: false
+      }).on('ok', () => {
+        $('.js-back-link', window.parent.document)[0].click()
+      });
+
+      return;
+    }
+
     this.startEvent();
     this.finishByReplay();
     this.countdownEvent();
