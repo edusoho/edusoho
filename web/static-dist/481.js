cfcdc768d50a9116de13e536798bb68ec6acba06
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2024-05-14 22:45:59 +0800

    feat: #240514 compile

---
+++ web/static-dist/481.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[481,325],{799:function(e,t){$(".js-org-tree-select").select2({treeview:!0,dropdownAutoWidth:!0,treeviewInitState:"collapsed",placeholderOption:"first"})}}]);
