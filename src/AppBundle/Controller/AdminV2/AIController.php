772fffad37aece3e928b1178845506c82625db89
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2024-06-03 20:49:41 +0800

    feat: #105170 ai问卷数据上报

--- src/AppBundle/Controller/AdminV2/AIController.php
+++ src/AppBundle/Controller/AdminV2/AIController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\AdminV2;
 
+use Biz\CloudData\Service\CloudDataService;
 use Symfony\Component\HttpFoundation\Request;
 
 class AIController extends BaseController
@@ -13,6 +14,24 @@ class AIController extends BaseController
 
     public function likeAction(Request $request)
     {
+        $user = $this->getUserService()->getUser($this->getUser()->getId());
+        $userprofile = $this->getUserService()->getUserProfile($user['id']);
+        $body = [
+            'feature' => $request->request->get('feature'),
+            'nickname' => $user['nickname'],
+            'mobile' => $user['verifiedMobile'] ?: $userprofile['mobile'],
+            'email' => $user['email'],
+        ];
+        $this->getCloudDataService()->push('ai.like_feature', $body, time(), 'important');
+
         return $this->createJsonResponse(['ok' => true]);
+    }
+
+    /**
+     * @return CloudDataService
+     */
+    private function getCloudDataService()
+    {
+        return $this->createService('CloudData:CloudDataService');
     }
 }
