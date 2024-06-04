bdeb6d42c7024f6a9903ca21e4d98124b100546d
Johnson <ziyuorg@gmail.com>
2024-06-04 19:50:34 +0800

    feat: #105273 api新增获取系统角色

---
+++ src/ApiBundle/Api/Resource/Role/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ApiBundle\Api\Resource\role;
+
+use ApiBundle\Api\ApiRequest;
+use ApiBundle\Api\Resource\AbstractResource;
+use Biz\Role\Service\RoleService;
+
+class Role extends AbstractResource
+{
+    public function search(ApiRequest $request)
+    {
+        $conditions = $request->query->all();
+
+        return $this->getRoleService()->searchRoles($conditions, [], 0, PHP_INT_MAX);
+    }
+
+    /**
+     * @return RoleService
+     */
+    protected function getRoleService()
+    {
+        return $this->service('Role:RoleService');
+    }
+}
