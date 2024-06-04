feed21bfccad86aff5d9b8c2a8adb33a486b1999
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2024-06-04 16:53:31 +0800

    feat: #240604 ai功能试用申请

--- src/AppBundle/Controller/AdminV2/AIController.php
+++ src/AppBundle/Controller/AdminV2/AIController.php
@@ -12,16 +12,35 @@ class AIController extends BaseController
         return $this->render('admin-v2/ai/survey-modal.html.twig');
     }
 
-    public function likeAction(Request $request)
+    public function applyAction(Request $request)
     {
         $user = $this->getUserService()->getUser($this->getUser()->getId());
         $userprofile = $this->getUserService()->getUserProfile($user['id']);
-        $body = [
-            'feature' => $request->request->get('feature'),
-            'nickname' => $user['nickname'],
+
+        return $this->render('admin-v2/ai/apply-modal.html.twig', [
+            'name' => $userprofile['truename'],
             'mobile' => $user['verifiedMobile'] ?: $userprofile['mobile'],
+            'feature' => $request->query->get('feature'),
+        ]);
+    }
+
+    public function likeFeatureAction(Request $request)
+    {
+        $user = $this->getUserService()->getUser($this->getUser()->getId());
+        $userprofile = $this->getUserService()->getUserProfile($user['id']);
+        $fields = $request->request->all();
+        $body = [
+            'feature' => $fields['feature'],
+            'nickname' => $user['nickname'],
+            'bindMobile' => $user['verifiedMobile'] ?: $userprofile['mobile'],
             'email' => $user['email'],
         ];
+        if (isset($fields['name'])) {
+            $body['name'] = $fields['name'];
+        }
+        if (isset($fields['mobile'])) {
+            $body['mobile'] = $fields['mobile'];
+        }
         $this->getCloudDataService()->push('ai.like_feature', $body, time(), 'important');
 
         return $this->createJsonResponse(['ok' => true]);
