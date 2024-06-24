54db16233458bcc7eeb0398876aba8c95cf87b2e
Johnson <ziyuorg@gmail.com>
2024-06-24 14:05:22 +0800

    feat: #105273 新增鉴权

--- src/ApiBundle/Api/Resource/Role/Role.php
+++ src/ApiBundle/Api/Resource/Role/Role.php
@@ -5,11 +5,17 @@ namespace ApiBundle\Api\Resource\role;
 use ApiBundle\Api\ApiRequest;
 use ApiBundle\Api\Resource\AbstractResource;
 use Biz\Role\Service\RoleService;
+use Biz\User\UserException;
 
 class Role extends AbstractResource
 {
     public function search(ApiRequest $request)
     {
+        $currentUser = $this->getCurrentUser();
+        if (!$currentUser->isSuperAdmin()) {
+            throw UserException::PERMISSION_DENIED();
+        }
+
         $conditions = $request->query->all();
 
         return $this->getRoleService()->searchRoles($conditions, [], 0, PHP_INT_MAX);
