334eb5f91e6d4acfda1484b22b22890f23ae885b
mixiaotian <kuozhi@kuozhideMacBook-Pro.local>
2024-07-04 11:11:13 +0800

    feat: #104826 创建随机卷-难度调节

--- app/Resources/static-src/common/vue/index.js
+++ app/Resources/static-src/common/vue/index.js
@@ -2,59 +2,62 @@ import Vue from 'vue/dist/vue.esm.js';
 import 'moment';
 import './icons/iconfont.js';
 import SvgIcon from './icons/SvgIcon.vue';
-import MobileIce from './components/MobileIce.vue'
+import MobileIce from './components/MobileIce.vue';
 // import './mock';
 import VueLazyload from 'vue-lazyload';
 
-import { Menu, Button, Table, Select, Form, AutoComplete, Upload,
+import {
+  Menu, Button, Table, Select, Form, AutoComplete, Upload,
   FormModel, DatePicker, Input, Modal, Col, Row, Radio, Switch, Icon, Checkbox,
   Pagination, Spin, Popconfirm, Dropdown, Space, Descriptions, Tag, Tooltip,
   Divider, Message, Notification, Tabs, Tree, TimePicker, InputNumber, Breadcrumb,
-  Empty, PageHeader, TreeSelect, Popover, Avatar, Card, Cascader, Alert
+  Empty, PageHeader, TreeSelect, Popover, Avatar, Card, Cascader, Alert, Drawer, Slider
 } from '@codeages/design-vue';
 import Clipboard from 'v-clipboard';
 
 if (!window.Vue) {
-  Vue.use(Menu)
-  Vue.use(Button)
-  Vue.use(Table)
-  Vue.use(Select)
-  Vue.use(Form)
-  Vue.use(FormModel)
-  Vue.use(DatePicker)
-  Vue.use(Input)
-  Vue.use(Modal)
-  Vue.use(Col)
-  Vue.use(Row)
-  Vue.use(Radio)
-  Vue.use(Switch)
-  Vue.use(AutoComplete)
-  Vue.use(Upload)
-  Vue.use(Icon)
-  Vue.use(Pagination)
-  Vue.use(Spin)
-  Vue.use(Popconfirm)
-  Vue.use(Dropdown)
-  Vue.use(Checkbox)
-  Vue.use(Space)
-  Vue.use(Descriptions)
-  Vue.use(Tag)
-  Vue.use(Tooltip)
-  Vue.use(Divider)
-  Vue.use(Tree)
-  Vue.use(Tabs)
-  Vue.use(TimePicker)
-  Vue.use(InputNumber)
-  Vue.use(Breadcrumb)
-  Vue.use(Empty)
-  Vue.use(PageHeader)
-  Vue.use(TreeSelect)
-  Vue.use(Popover)
-  Vue.use(Avatar)
-  Vue.use(Card)
-  Vue.use(Clipboard)
-  Vue.use(Cascader)
-  Vue.use(Alert)
+  Vue.use(Menu);
+  Vue.use(Button);
+  Vue.use(Table);
+  Vue.use(Select);
+  Vue.use(Form);
+  Vue.use(FormModel);
+  Vue.use(DatePicker);
+  Vue.use(Input);
+  Vue.use(Modal);
+  Vue.use(Col);
+  Vue.use(Row);
+  Vue.use(Radio);
+  Vue.use(Switch);
+  Vue.use(AutoComplete);
+  Vue.use(Upload);
+  Vue.use(Icon);
+  Vue.use(Pagination);
+  Vue.use(Spin);
+  Vue.use(Popconfirm);
+  Vue.use(Dropdown);
+  Vue.use(Checkbox);
+  Vue.use(Space);
+  Vue.use(Descriptions);
+  Vue.use(Tag);
+  Vue.use(Tooltip);
+  Vue.use(Divider);
+  Vue.use(Tree);
+  Vue.use(Tabs);
+  Vue.use(TimePicker);
+  Vue.use(InputNumber);
+  Vue.use(Breadcrumb);
+  Vue.use(Empty);
+  Vue.use(PageHeader);
+  Vue.use(TreeSelect);
+  Vue.use(Popover);
+  Vue.use(Avatar);
+  Vue.use(Card);
+  Vue.use(Clipboard);
+  Vue.use(Cascader);
+  Vue.use(Alert);
+  Vue.use(Drawer);
+  Vue.use(Slider);
 
   Vue.prototype.$message = Message;
   Vue.prototype.$notification = Notification;
@@ -66,7 +69,7 @@ if (!window.Vue) {
   Vue.prototype.$destroyAll = Modal.destroyAll;
 
   Message.config({
-    top: `100px`
+    top: '100px'
   });
 
   Vue.component('svg-icon', SvgIcon);
@@ -82,22 +85,22 @@ if (!window.Vue) {
 
   Vue.filter('formatHtml', (value = '') => {
     return value && value.replace(/<(?:.|\n)*?>/gm, '')
-    .replace(/(&rdquo;)/g, '\"')
-    .replace(/&ldquo;/g, '\"')
-    .replace(/&mdash;/g, '-')
-    .replace(/&nbsp;/g, '')
-    .replace(/&amp;/g, '&')
-    .replace(/&gt;/g, '>')
-    .replace(/&lt;/g, '<')
-    .replace(/<[\w\s"':=\/]*/, '');
+      .replace(/(&rdquo;)/g, '\"')
+      .replace(/&ldquo;/g, '\"')
+      .replace(/&mdash;/g, '-')
+      .replace(/&nbsp;/g, '')
+      .replace(/&amp;/g, '&')
+      .replace(/&gt;/g, '>')
+      .replace(/&lt;/g, '<')
+      .replace(/<[\w\s"':=\/]*/, '');
   });
 
-  Vue.prototype.$dateFormat = function(value, format = 'YYYY-MM-DD') {
+  Vue.prototype.$dateFormat = function (value, format = 'YYYY-MM-DD') {
     if (value == 0) {
       return '';
     }
-    return moment(value * 1000).format(format)
-  }
+    return moment(value * 1000).format(format);
+  };
 }
 
 window.Vue = window.Vue || Vue;
