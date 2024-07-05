53605ffec7f78124a8d5d195ffb5d8a03cf8b119
xuzhiyuan <269648145@qq.com>
2024-07-05 16:30:46 +0800

    feat: #104819 创建随机卷-按题型分类抽题-题型设置-悬停编辑效果

--- app/Resources/static-src/app/js/question-bank/testpaper/create/QuestionTypeCategoryDisplay.vue
+++ app/Resources/static-src/app/js/question-bank/testpaper/create/QuestionTypeCategoryDisplay.vue
@@ -2,33 +2,214 @@
 
 export default {
   name: 'QuestionTypeCategoryDisplay',
-  components: {}
-}
+  components: {},
+  props: {
+    questionDisplayTypes: undefined,
+  },
+  data() {
+    return {
+      categories: [
+        {
+          level: '一级分类',
+          name: '一建《机电》分章练习',
+          questionTypes: [
+            {
+              type: 'single_choice',
+              name: '单选题',
+              num: 1,
+            }, {
+              type: 'choice',
+              name: '多选题',
+              num: 0,
+            },
+            {
+              type: 'essay',
+              name: '问答题',
+              num: 0,
+            },
+            {
+              type: 'uncertain_choice',
+              name: '不定项',
+              num: 0,
+            },
+            {
+              type: 'determine',
+              name: '判断题',
+              num: 0,
+            },
+            {
+              type: 'fill',
+              name: '填空题',
+              num: 0,
+            },
+            {
+              type: 'material',
+              name: '材料题',
+              num: 0,
+            },
+          ]
+        },
+        {
+          level: '二级分类',
+          name: '1H410000机电工程技术',
+          questionTypes: [
+            {
+              type: 'single_choice',
+              name: '单选题',
+              num: 1,
+            }, {
+              type: 'choice',
+              name: '多选题',
+              num: 0,
+            },
+            {
+              type: 'essay',
+              name: '问答题',
+              num: 0,
+            },
+            {
+              type: 'uncertain_choice',
+              name: '不定项',
+              num: 0,
+            },
+            {
+              type: 'determine',
+              name: '判断题',
+              num: 0,
+            },
+            {
+              type: 'fill',
+              name: '填空题',
+              num: 0,
+            },
+            {
+              type: 'material',
+              name: '材料题',
+              num: 0,
+            },
+          ]
+        },
+        {
+          level: '三级分类',
+          name: '1H410000机电工程常用材料及工程设备材料',
+          questionTypes: [
+            {
+              type: 'single_choice',
+              name: '单选题',
+              num: 1,
+            }, {
+              type: 'choice',
+              name: '多选题',
+              num: 0,
+            },
+            {
+              type: 'essay',
+              name: '问答题',
+              num: 0,
+            },
+            {
+              type: 'uncertain_choice',
+              name: '不定项',
+              num: 0,
+            },
+            {
+              type: 'determine',
+              name: '判断题',
+              num: 0,
+            },
+            {
+              type: 'fill',
+              name: '填空题',
+              num: 0,
+            },
+            {
+              type: 'material',
+              name: '材料题',
+              num: 0,
+            },
+          ]
+        },
+        {
+          level: '三级分类',
+          name: '1H410000机电工程常用材料及工程设备材料',
+          questionTypes: [
+            {
+              type: 'single_choice',
+              name: '单选题',
+              num: 1,
+            }, {
+              type: 'choice',
+              name: '多选题',
+              num: 0,
+            },
+            {
+              type: 'essay',
+              name: '问答题',
+              num: 0,
+            },
+            {
+              type: 'uncertain_choice',
+              name: '不定项',
+              num: 0,
+            },
+            {
+              type: 'determine',
+              name: '判断题',
+              num: 0,
+            },
+            {
+              type: 'fill',
+              name: '填空题',
+              num: 0,
+            },
+            {
+              type: 'material',
+              name: '材料题',
+              num: 0,
+            },
+          ]
+        },
+      ],
+      editMaskVisible: false,
+    };
+  },
+  methods: {
+    showEditMask() {
+      this.editMaskVisible = true;
+    },
+    hideEditMask() {
+      this.editMaskVisible = false;
+    }
+  },
+  created() {
+  }
+};
 
 </script>
 <template>
-  <div class="question-type-display">
-    <div class="question-type-display-header">
-      <div class="question-type-display-header-top">题型设置</div>
-      <div class="question-type-display-header-normal">题目数量</div>
-      <div class="question-type-display-header-normal">每题分值</div>
-      <div class="question-type-display-header-bottom">
-        <span class="question-type-display-header-bottom-title">合计</span>
-        <span class="question-type-display-header-bottom-description">（题数/总分）</span>
+  <div class="question-type-category-display" @mouseover="showEditMask" @mouseleave="hideEditMask">
+    <div class="question-type-category-display-header">
+      <div class="question-type-category-display-header-top">分类</div>
+      <div v-if="categories && categories.length > 0" class="question-type-category-display-header-normal" v-for="category in categories">
+        <a-tag>{{ category.level }}</a-tag>
+        <span class="category-name">{{ category.name }}</span>
+      </div>
+      <div class="question-type-category-display-header-bottom">
+        <span class="question-type-category-display-header-bottom-title">合计</span>
+        <span class="question-type-category-display-header-bottom-description">（题数/总分）</span>
       </div>
     </div>
-    <div v-for="type in questionDisplayTypes">
-      <div class="question-type-display-header-top">{{ type.name }}</div>
-      <div class="question-type-display-cell-number">
-                <span class="question-type-display-cell-number-edit">
-                  <input type="number" value="0"/>
-                </span>
-        <span class="question-type-display-cell-number-total">/{{ questionCounts[type.type].total }}</span>
+    <div v-if="questionDisplayTypes && questionDisplayTypes.length > 0" v-for="type in questionDisplayTypes" class="question-type-category-display-header-type">
+      <div class="question-type-category-display-header-top">
+        <div class="question-type-category-display-header-top-content">{{ type.name }}</div>
       </div>
-      <div class="question-type-display-cell-score">
-        <input type="number" value="2"/>
+      <div v-for="category in categories" class="question-type-category-display-cell-number">
+        <span class="question-type-category-display-cell-number-total">{{ category.questionTypes.find(questionType => questionType.type === type.type).num }}</span>
       </div>
-      <div class="question-type-display-cell-sum">0 / 0.0</div>
+      <div class="question-type-category-display-cell-sum">0 / 0.0</div>
+    </div>
+    <div v-show="editMaskVisible" class="edit-mask-container">
+      <a-button>编辑</a-button>
     </div>
   </div>
 </template>
