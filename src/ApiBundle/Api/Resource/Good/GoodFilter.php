0d146966fdbf13c9517ec082fba2926943694cfd
Johnson <ziyuorg@gmail.com>
2024-08-08 17:46:45 +0800

    feat: #106643 判断时候需要签署合约新增合同编号

--- src/ApiBundle/Api/Resource/Good/GoodFilter.php
+++ src/ApiBundle/Api/Resource/Good/GoodFilter.php
@@ -15,7 +15,7 @@ class GoodFilter extends Filter
         'minDisplayPrice', 'maxDisplayPrice', 'minPriceObj', 'maxPriceObj',
         'minDisplayPriceObj', 'maxDisplayPriceObj', 'images', 'ratingNum', 'rating', 'hitNum',
         'hotSeq', 'showable', 'buyable', 'creator', 'discount', 'canManage', 'discountId', 'isMember',
-        'peopleShowNum', 'manageUrl', 'needSignContract',
+        'peopleShowNum', 'manageUrl', 'contractId',
     ];
 
     protected $publicFields = [
@@ -23,7 +23,7 @@ class GoodFilter extends Filter
         'showable', 'buyable', 'summary', 'minPrice', 'maxPrice', 'minDisplayPrice', 'maxDisplayPrice', 'minPriceObj',
         'maxPriceObj', 'minDisplayPriceObj', 'maxDisplayPriceObj', 'images', 'orgId', 'orgCode', 'ratingNum', 'rating',
         'hitNum', 'hotSeq', 'recommendWeight', 'recommendedTime', 'createdTime', 'updatedTime', 'isFavorite', 'discount',
-        'discountId', 'canManage', 'isMember', 'peopleShowNum', 'manageUrl', 'needSignContract',
+        'discountId', 'canManage', 'isMember', 'peopleShowNum', 'manageUrl', 'contractId',
     ];
 
     protected $filterMap = [
