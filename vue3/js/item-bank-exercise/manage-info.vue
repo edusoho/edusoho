66e4e7f6009839970d01438ae4796940dc55b205
Johnson <ziyuorg@gmail.com>
2024-08-20 02:29:00 +0800

    feat: #106870 修复数据为空

---
+++ vue3/js/item-bank-exercise/manage-info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a-button type="primary">Ant Design Button</a-button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue';
+
+export default {
+  name: 'ManageInfo',
+  components: {
+    'a-button': Button,
+  },
+};
+</script>
+
+<style scoped>
+/* 你的组件样式 */
+</style>
