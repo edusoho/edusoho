580f22b6d25fd8cb0c7245feaf5335e60a85f45e
吴博 <wubo@howzhi.com>
2024-08-27 14:00:16 +0800

    feat: #240827 我的合同详情页面优化;

--- h5/src/h5/containers/my/contract/detail.vue
+++ h5/src/h5/containers/my/contract/detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-html="contractDetail" class="p-16"></div>
+  <div v-html="contractDetail" class="my-contract-detail"></div>
 </template>
 
 <script>
@@ -21,3 +21,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.my-contract-detail {
+  position: fixed;
+  top: 46px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 16px;
+  overflow-y: auto;
+}
+</style>
