f8f47e44c0e5a730a7301dc8a44e909c0bbf7554
Johnson <ziyuorg@gmail.com>
2024-09-05 15:44:56 +0800

    feat: #107290 新增字体文件

--- src/ApiBundle/Api/Resource/DownloadContract/DownloadContract.php
+++ src/ApiBundle/Api/Resource/DownloadContract/DownloadContract.php
@@ -8,6 +8,7 @@ use ApiBundle\Api\Resource\Contract\ContractDisplayTrait;
 use ApiBundle\Api\Util\AssetHelper;
 use Biz\User\UserException;
 use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
@@ -28,9 +29,14 @@ class DownloadContract extends AbstractResource
         }
         $signSnapshot['contract']['content'] = $this->replaceContentVariable($signSnapshot['contract']['content'], $signedContract['goodsKey'], $signSnapshot['contractCode'], $signSnapshot['sign']);
         $html = $this->getHtmlByRecord($signSnapshot['contract']['content'], $signSnapshot);
+//        $html = '
+//	<body style="font-family:simsun">中文字体</body>
+//';
+        $options = new Options();
+        $options->setDefaultFont('simsun');
         // 创建一个 DomPDF 实例
-        $pdf = new DomPDF();
-        $pdf->loadHtml($html);
+        $pdf = new DomPDF($options);
+        $pdf->loadHtml($html, 'UTF-8');
         $pdf->render();
         // 获取生成的 PDF 输出
         $output = $pdf->output();
@@ -39,7 +45,7 @@ class DownloadContract extends AbstractResource
         // 设置 PDF 文件名
         $disposition = $response->headers->makeDisposition(
             ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            $signSnapshot['contract']['name'].'.pdf'
+            'abc.pdf'
         );
         // 设置响应头
         $response->headers->set('Content-Type', 'application/pdf');
