79e7d6c795528d92c5d7ed0aba35037343458191
Johnson <ziyuorg@gmail.com>
2024-09-06 14:04:10 +0800

    feat: #107290 修改下载逻辑

--- src/ApiBundle/Api/Resource/DownloadContract/DownloadContract.php
+++ src/ApiBundle/Api/Resource/DownloadContract/DownloadContract.php
@@ -7,12 +7,9 @@ use ApiBundle\Api\Resource\AbstractResource;
 use ApiBundle\Api\Resource\Contract\ContractDisplayTrait;
 use ApiBundle\Api\Util\AssetHelper;
 use Biz\User\UserException;
-use Dompdf\Dompdf;
-use Dompdf\Options;
 use Mpdf\Mpdf;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-
 
 class DownloadContract extends AbstractResource
 {
@@ -30,47 +27,21 @@ class DownloadContract extends AbstractResource
         }
         $signSnapshot['contract']['content'] = $this->replaceContentVariable($signSnapshot['contract']['content'], $signedContract['goodsKey'], $signSnapshot['contractCode'], $signSnapshot['sign']);
         $html = $this->getHtmlByRecord($signSnapshot['contract']['content'], $signSnapshot);
-
-//        $options = new Options();
-//        $options->set('isHtml5ParserEnabled', true);
-//        $options->setDefaultFont('simsun');
-//        // 创建一个 DomPDF 实例
-//        $pdf = new DomPDF($options);
-//        $pdf->loadHtml($html, 'UTF-8');
-//        $pdf->setPaper('A4', 'landscape');
-//        $pdf->render();
-//        // 获取生成的 PDF 输出
-//        $output = $pdf->output();
-//        // 创建响应对象
-//        $response = new response($output);
-//        // 设置 PDF 文件名
-//        $disposition = $response->headers->makeDisposition(
-//            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-//            'abc.pdf'
-//        );
-        // 创建一个 mPDF 实例
         $mpdf = new Mpdf([
-//            'format' => 'A4-L', // A4 纸张，横向 (landscape)
-//            'default_font' => 'simsun', // 设置默认字体为 simsun
             'utf-8' => true,
             'autoScriptToLang' => true,
-            'autoLangToFont'   => true,
+            'autoLangToFont' => true,
             'useSubstitutions' => true,
         ]);
-
-// 加载 HTML 内容
         $mpdf->WriteHTML($html);
-
-// 获取生成的 PDF 输出
+        // 获取生成的 PDF 输出
         $output = $mpdf->Output('', 'S'); // 'S' 表示将 PDF 输出为字符串
-
-// 创建响应对象
+        // 创建响应对象
         $response = new Response($output);
-
-// 设置 PDF 文件名
         $disposition = $response->headers->makeDisposition(
             ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            'abc.pdf'
+            rawurlencode($this->getFileName($signSnapshot)),
+            'utf-8'
         );
         // 设置响应头
         $response->headers->set('Content-Type', 'application/pdf');
@@ -80,5 +51,10 @@ class DownloadContract extends AbstractResource
         $response->headers->set('Content-Length', strlen($output));
 
         return $response;
+    }
+
+    protected function getFileName($signSnapshot)
+    {
+        return $signSnapshot['contractCode'].'_'.$signSnapshot['sign']['truename'].'_'.$signSnapshot['contract']['name'].'.pdf';
     }
 }
