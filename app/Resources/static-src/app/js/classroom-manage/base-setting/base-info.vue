00cd144a9a9ca5dcff59ef58ab958a8bcfe0fa85
张杰 <zhangjie@MacBook-Pro-2.local>
2024-09-23 11:31:46 +0800

    fix: #107369 支持富文本添加iframe

--- app/Resources/static-src/app/js/classroom-manage/base-setting/base-info.vue
+++ app/Resources/static-src/app/js/classroom-manage/base-setting/base-info.vue
@@ -88,7 +88,11 @@
                     import('app/js/upload-image/index.js');
                     aboutEditor = CKEDITOR.replace('about', {
                         allowedContent: true,
-                        toolbar: 'Detail',
+                        toolbar: [
+                          {items: ['FontSize', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
+                          {items: ['Bold', 'Italic', 'Underline', 'TextColor', '-', 'RemoveFormat', 'PasteText', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', 'uploadpictures', 'CodeSnippet', 'Iframe', '-', 'Source', 'kityformula', '-', 'Maximize']}
+                        ],
+                        extraPlugins: 'questionblank,smiley,table,font,kityformula,codesnippet,uploadpictures,shortUrl,image2,colorbutton,colordialog,justify,find,filebrowser,pasteimage,katex,iframe',
                         fileSingleSizeLimit: app.fileSingleSizeLimit,
                         filebrowserImageUploadUrl: $('#about').data('imageUploadUrl'),
                         filebrowserFlashUploadUrl: $('#about').data('flashUploadUrl')
