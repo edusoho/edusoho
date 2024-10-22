a73420db62352a34ff0ea17fb280ecb02702ad87
张杰 <zhangjie@MacBook-Pro-2.local>
2024-10-22 20:49:47 +0800

    feat: #20241022 暂存

--- vue3/js/my/learning/item-bank/ItemBankList.vue
+++ vue3/js/my/learning/item-bank/ItemBankList.vue
@@ -17,6 +17,27 @@ async function fetchMyBindItemBank(params) {
   bindItemBankList.value = data;
 }
 
+function getTableTotal(total) {
+  return `共 ${total} 项`
+}
+
+async function handleTableChange(paging) {
+  pagination.current = paging.current === 0 ? 1 : paging.current;
+  pagination.total = paging.total;
+  pagination.pageSize = paging.pageSize;
+  const params = {
+    limit: paging.pageSize,
+    offset: (paging.current - 1) * paging.pageSize,
+  };
+  await fetchMyBindItemBank(params);
+}
+
+async function handlePaginationChange(page, pageSize) {
+  pagination.current = page;
+  pagination.pageSize = pageSize;
+  await handleTableChange(pagination);
+}
+
 onBeforeMount(async() => {
   const params = {
     limit: pagination.pageSize,
@@ -29,15 +50,40 @@ onBeforeMount(async() => {
 
 <template>
   <AntConfigProvider>
-    <div class="flex flex-col w-full border border-[#E4ECF3] border-solid bg-white">
-      <div class="flex flex-col px-24 pt-24 space-y-20">
+    <div class="flex flex-col w-full border border-[#E5E6EB] border-solid bg-white rounded-4">
+      <div class="flex flex-col px-24 pt-24 space-y-20 mb-20">
         <div class="text-18 text-[#1E2226] font-medium">我的题库</div>
-        <div v-for="item in bindItemBankList" class="border border-[#E5E6EB] border-solid px-24 py-16">
-
+        <div v-for="item in bindItemBankList" class="flex justify-between border border-[#E5E6EB] border-solid rounded-6 px-24 py-16">
+          <div class="flex space-x-16">
+            <div class="relative">
+              <img :src="item.itemBankExercise.cover.middle" class="h-90 rounded-6" draggable="false" alt="">
+              <div class="text-12 text-white font-medium px-8 py-2 bg-[#00C261] rounded-tl-5 rounded-br-5 leading-20 absolute top-0 left-0">已发布</div>
+            </div>
+            <div class="flex flex-col justify-between">
+              <div class="flex flex-col">
+                <div class="text-16 font-medium text-[#37393D] mb-8">{{ item.itemBankExercise.title }}</div>
+                <div class="flex">
+                  <div class="text-14 text-[#919399] font-normal mr-20">答题率：<span class="text-[#37393D]">{{ `${item.completionRate}%` }}</span></div>
+                  <div class="text-14 text-[#919399] font-normal">掌握率：<span class="text-[#37393D]">{{ `${item.masteryRate}%` }}</span></div>
+                </div>
+              </div>
+              <div>33333</div>
+            </div>
+          </div>
+          <div class="flex items-center">
+            <a-button type="primary">去学习</a-button>
+          </div>
         </div>
       </div>
-      <div>
-
+      <div class="flex flex-row-reverse px-40 pt-24 pb-32 border border-[#E5E6EB] border-solid border-x-0 border-b-0">
+        <a-pagination
+          :show-total="total => getTableTotal(total)"
+          v-model="pagination.current"
+          :total="pagination.total"
+          @change="handlePaginationChange"
+          show-less-items
+          :disabled="pagination.total === 0"
+        />
       </div>
     </div>
   </AntConfigProvider>
--- vue3/js/my/learning/item-bank/index.js
+++ vue3/js/my/learning/item-bank/index.js
@@ -1,7 +1,7 @@
 import {createApp} from 'vue';
 import 'vue3/main.less';
 import { createStyleTag, setCurrentPrimaryColor } from 'vue3/js/common';
-import { Tooltip } from 'ant-design-vue';
+import { Button, Tooltip, Pagination } from 'ant-design-vue';
 import ItemBankList from './ItemBankList.vue';
 
 const app = createApp(ItemBankList);
@@ -9,6 +9,8 @@ const app = createApp(ItemBankList);
 setCurrentPrimaryColor(app);
 
 app.use(Tooltip);
+app.use(Button);
+app.use(Pagination);
 
 if (process.env.NODE_ENV === 'production') {
   createStyleTag(`/static-dist/vue3/js/my-contract/index.css?${window.app.version}`);
