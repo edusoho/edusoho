34a8cf3bb7b60b61bfd02ba3186bd8b26c56ee8e
张杰 <zhangjie@MacBook-Pro-2.local>
2024-10-23 15:11:48 +0800

    feat: #20241023 订单确认页面添加赠送题库

---
+++ vue3/js/order/ItemBankListItem.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  item: {required: true},
+})
+
+function toItemBankExercisePage(exerciseId) {
+  window.location.href = `/item_bank_exercise/${exerciseId}`
+}
+</script>
+
+<template>
+  <div class="flex justify-between p-12 bg-[#FAFAFA] rounded-6">
+    <div class="flex items-center">
+      <img class="w-18 mr-8" src="../../img/item-bank/item-bank-gift-icon.jpg" alt="">
+      <div class="text-14 text-[#5E6166] font-normal max-w-400 truncate cursor-pointer hover:text-[#46C37B]" @click="toItemBankExercisePage(item.itemBankExercise.id)">{{ item.itemBankExercise.title }}</div>
+    </div>
+    <div>
+      <div v-if="item.itemBankExercise.price === '0.00'" class="text-16 text-[#FF7E56] font-semibold">免费</div>
+      <div v-else class="flex items-center space-x-8">
+        <div class="text-12 text-[#FF7E56] font-semibold">¥<span class="text-16 ml-2">0.</span>00</div>
+        <div class="text-12 text-[#FFA58A] font-normal line-through mt-4">{{ `¥${item.itemBankExercise.price}` }}</div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ vue3/js/order/index.js
@@ -0,0 +1,20 @@
+import {createApp} from 'vue';
+import 'vue3/main.less';
+import { createStyleTag, setCurrentPrimaryColor } from 'vue3/js/common';
+import {Modal, Button} from 'ant-design-vue';
+import ItemBankGift from './ItemBankGift.vue';
+
+const app = createApp(ItemBankGift, {
+  exerciseBind: $('#item-bank-gift').data('exerciseBind'),
+});
+
+setCurrentPrimaryColor(app);
+
+app.use(Modal);
+app.use(Button);
+
+if (process.env.NODE_ENV === 'production') {
+  createStyleTag(`/static-dist/vue3/js/my-contract/index.css?${window.app.version}`);
+}
+
+app.mount('#item-bank-gift')
