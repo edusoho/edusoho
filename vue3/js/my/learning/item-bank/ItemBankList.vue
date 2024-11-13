5c35e4474b2fa2949c1849e65bb66f9c8ddde67a
张杰 <zhangjie@MacBook-Pro-2.local>
2024-11-13 15:40:10 +0800

    feat: #20241113 compile

--- vue3/js/my/learning/item-bank/ItemBankList.vue
+++ vue3/js/my/learning/item-bank/ItemBankList.vue
@@ -73,7 +73,8 @@ onBeforeMount(async() => {
               <a-tooltip placement="top">
                 <template #title>{{ item.bindTitle }}</template>
                 <div v-if="item.bindTitle" class="flex text-14 font-normal">
-                  <div class="text-[#919399] max-w-320 truncate">{{ item.bindTitle }}<span class="text-[#5E6166] ml-8">赠送的题库</span></div>
+                  <div class="text-[#919399] max-w-320 w-fit truncate">{{ item.bindTitle }}</div>
+                  <div class="text-[#5E6166] ml-8">赠送的题库</div>
                 </div>
               </a-tooltip>
             </div>
