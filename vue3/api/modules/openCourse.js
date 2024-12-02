34ec5c12ea8c2025b9d19b8ab36730e125426ef0
张杰 <zhangjie@MacBook-Pro-2.local>
2024-12-02 17:39:05 +0800

    feat: #20241202 完成公开课发布删除编辑

--- vue3/api/modules/openCourse.js
+++ vue3/api/modules/openCourse.js
@@ -4,7 +4,19 @@ export default {
   async createLesson(courseId, params) {
     return apiClient.post(`/open_course/${courseId}/lesson`, params);
   },
-  async findLesson(courseId) {
+  async findLessons(courseId) {
     return apiClient.get(`/open_course/${courseId}/lesson`);
+  },
+  async deleteLesson(courseId, lessonId) {
+    return apiClient.delete(`/open_course/${courseId}/lesson/${lessonId}`);
+  },
+  async publishLesson(courseId, lessonId) {
+    return apiClient.post(`/open_course/${courseId}/lesson/${lessonId}/publish`);
+  },
+  async unpublishLesson(courseId, lessonId) {
+    return apiClient.delete(`/open_course/${courseId}/lesson/${lessonId}/publish`);
+  },
+  async getLesson(courseId, lessonId) {
+    return apiClient.get(`/open_course/${courseId}/lesson/${lessonId}`);
   },
 }
