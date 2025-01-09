abc377949b0873e604993c7f86bc2147407622ce
张杰 <zhangjie@MacBook-Pro-2.local>
2025-01-09 20:43:22 +0800

    feat: #202502109 compile

--- h5/src/components/e-class/e-class.vue
+++ h5/src/components/e-class/e-class.vue
@@ -6,7 +6,7 @@
       <div v-if="Number(isVip)" class="absolute left-0 -bottom-2 px-8 py-2 text-white text-12 leading-20 font-medium bg-black bg-opacity-80 rounded-bl-6 rounded-tr-12 z-10">会员免费</div>
       <div v-if="course.videoMaxLevel === '2k' && !Number(isVip)" class="absolute left-0 -bottom-2 px-8 py-2 text-white text-12 leading-20 font-medium bg-black bg-opacity-80 rounded-bl-6 rounded-tr-12 z-10">2K 优享</div>
       <div v-if="course.videoMaxLevel === '4k' && !Number(isVip)" class="absolute left-0 -bottom-2 px-8 py-2 text-[#492F0B] text-12 leading-20 font-medium bg-gradient-to-l from-[#F7D27B] to-[#FCEABE] rounded-bl-6 rounded-tr-12">4K 臻享</div>
-      <div v-if="courseType === 'live'" class="absolute bottom-0 w-full px-6 py-4 text-white font-medium text-12 leading-12 bg-black bg-opacity-40 text-right flex items-center flex-row-reverse">直播<span class="inline-block w-4 h-4 bg-[#60F1A3] mr-4 text-right" style="border-radius: 9999px;"></span></div>
+      <div v-if="courseType === 'live'" class="absolute -bottom-2 w-full px-6 py-4 text-white font-medium text-12 leading-12 bg-black bg-opacity-40 rounded-bl-6 text-right flex items-center flex-row-reverse">直播<span class="inline-block w-4 h-4 bg-[#60F1A3] mr-4 text-right" style="border-radius: 9999px;"></span></div>
     </div>
     <div class="flex flex-col justify-between flex-1 p-12 relative" style="width: calc(100% - 170px);border-radius: 0 6px 6px 0;">
       <img v-if="isShowErrImg" class="err-img" :src="errImgUrl" />
