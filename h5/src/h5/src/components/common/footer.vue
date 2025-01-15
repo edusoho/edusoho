9e920efb0fba256ad0a92ca7907cefd84db3d162
张杰 <zhangjie@MacBook-Pro-2.local>
2025-01-15 17:24:39 +0800

    feat: #20250115 暂存

--- h5/src/h5/src/components/common/footer.vue
+++ h5/src/h5/src/components/common/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-10 bottom-0 left-0 text-[#333] leading-32 drop-shadow-lg flex w-full bg-white" style="box-shadow: 0 -2px 4px 0 rgba(0, 0, 0, 0.1);">
+  <div class="fixed z-10 bottom-0 left-0 text-[#333] leading-32 flex w-full bg-white" style="box-shadow: 0 -2px 4px 0 rgba(0, 0, 0, 0.1);">
     <div
       v-for="(item, index) in footerItem"
       :key="index"
