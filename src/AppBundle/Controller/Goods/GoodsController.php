3a87ca5b06d23dc23ec321ad07d6bfc4cedc29aa
张杰 <zhangjie@MacBook-Pro-2.local>
2025-02-12 10:30:33 +0800

    fix: #108509 修复h5没有购买按钮

--- src/AppBundle/Controller/Goods/GoodsController.php
+++ src/AppBundle/Controller/Goods/GoodsController.php
@@ -18,6 +18,9 @@ class GoodsController extends BaseController
         $targetId = $request->query->get('targetId', 0);
         $goodsApiRequest = new ApiRequest("/api/goods/{$id}", 'GET', ['preview' => $preview, 'targetId' => $targetId]);
         $goods = $this->container->get('api_resource_kernel')->handleApiRequest($goodsApiRequest);
+        if (1 != $preview && 'unpublished' === $goods['status']) {
+            return $this->redirectToRoute('goods_show', ['id' => $id, 'preview' => 1, 'targetId' => $targetId]);
+        }
         $goodsComponentsApiRequest = new ApiRequest("/api/goods/{$id}/components", 'GET');
         $goodsComponents = $this->container->get('api_resource_kernel')->handleApiRequest($goodsComponentsApiRequest);
         $goods['showPlan'] = 1 == count($goods['specs']) || empty($goods['specs'][0]['title']) ? 0 : 1;
