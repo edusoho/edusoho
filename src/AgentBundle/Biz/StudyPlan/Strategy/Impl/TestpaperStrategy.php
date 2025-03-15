3930686c4efb7bb2c183e9302466e223576b6d2a
ziyu <ziyuorg@gmail.com>
2025-03-15 23:26:46 +0800

    feat: #110376 修复考试类型时间试卷错误

--- src/AgentBundle/Biz/StudyPlan/Strategy/Impl/TestpaperStrategy.php
+++ src/AgentBundle/Biz/StudyPlan/Strategy/Impl/TestpaperStrategy.php
@@ -11,6 +11,13 @@ class TestpaperStrategy implements TimeCalculationStrategy
 {
     public function calculateTime(array $activity): int
     {
-        return $activity['ext']['limitedTime'];
+        $testPaperTime = 0;
+        if ($activity['ext']['limitedTime'] != 0) {
+            $testPaperTime = (int)$activity['ext']['limitedTime'];
+        }
+        if ($activity['length'] != 0) {
+            $testPaperTime = $activity['length'];
+        }
+        return $testPaperTime;
     }
 }
