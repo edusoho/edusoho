79f44c095c7518e0f0b03f487b7945791b26cb29
mixiaotian <mi_xtyan@qq.com>
2025-03-17 14:03:55 +0800

    feat: #250316 25.1.5升级脚本

---
+++ upgrade-scripts/upgrade-25.1.5.php
@@ -0,0 +1,198 @@
+<?php
+
+class EduSohoUpgrade extends AbstractUpdater
+{
+    public function __construct($biz)
+    {
+        parent::__construct($biz);
+    }
+
+    public function update($index = 0)
+    {
+        $this->getConnection()->beginTransaction();
+        try {
+            $result = $this->updateScheme((int)$index);
+            $this->getConnection()->commit();
+            if (!empty($result)) {
+                return $result;
+            } else {
+                $this->logger('info', '执行升级脚本结束');
+            }
+        } catch (\Exception $e) {
+            $this->getConnection()->rollback();
+            $this->logger('error', $e->getTraceAsString());
+            throw $e;
+        }
+
+        $developerSetting = $this->getSettingService()->get('developer', array());
+        $developerSetting['debug'] = 0;
+        $this->getSettingService()->set('developer', $developerSetting);
+        $this->getSettingService()->set('crontab_next_executed_time', time());
+    }
+
+    private function updateScheme($index)
+    {
+        $definedFuncNames = [
+            'updateUserSettings',
+            'alterTableUserAddField',
+        ];
+        $funcNames = array();
+        foreach ($definedFuncNames as $key => $funcName) {
+            $funcNames[$key + 1] = $funcName;
+        }
+        if (0 == $index) {
+            $this->logger('info', '开始执行升级脚本');
+
+            return array(
+                'index' => $this->generateIndex(1, 1),
+                'message' => '升级数据...',
+                'progress' => 0,
+            );
+        }
+
+        list($step, $page) = $this->getStepAndPage($index);
+        $method = $funcNames[$step];
+
+        $page = $this->$method($page);
+        if (1 == $page) {
+            ++$step;
+        }
+        if ($step <= count($funcNames)) {
+            return array(
+                'index' => $this->generateIndex($step, $page),
+                'message' => '升级数据...',
+                'progress' => 0,
+            );
+        }
+    }
+
+    protected function updateUserSettings()
+    {
+        $auth = $this->getSettingService()->get('auth', []);
+        $auth['email_enabled'] = 'opened';
+        $auth['password_level'] = 'high';
+        $auth['register_protective'] = 'high';
+        $this->getSettingService()->set('auth', $auth);
+        $this->logger('info', '修改auth设置成功');
+
+        $loginConnect = $this->getSettingService()->get('login_bind', []);
+        $loginConnect['temporary_lock_enabled'] = 1;
+        $loginConnect['temporary_lock_allowed_times'] = empty($loginConnect['temporary_lock_allowed_times']) ? 5 : $loginConnect['temporary_lock_allowed_times'];
+        $loginConnect['ip_temporary_lock_allowed_times'] = empty($loginConnect['ip_temporary_lock_allowed_times']) ? 20 : $loginConnect['ip_temporary_lock_allowed_times'];
+        $loginConnect['temporary_lock_minutes'] = empty($loginConnect['temporary_lock_minutes']) ? 20 : $loginConnect['temporary_lock_minutes'];
+        $this->getSettingService()->set('login_bind', $loginConnect);
+        $this->logger('info', '修改login_bind设置成功');
+
+        return 1;
+    }
+
+    protected function alterTableUserAddField()
+    {
+        if (!$this->isFieldExist('user', 'passwordUpgraded')) {
+            $this->getConnection()->exec("ALTER TABLE `user` ADD COLUMN `passwordUpgraded` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已升级密码';");
+        }
+        $this->logger('info', '`user`表新增字段`passwordUpgraded`成功');
+
+        return 1;
+    }
+
+    protected function generateIndex($step, $page)
+    {
+        return $step * 1000000 + $page;
+    }
+
+    protected function getStepAndPage($index)
+    {
+        $step = intval($index / 1000000);
+        $page = $index % 1000000;
+
+        return array($step, $page);
+    }
+
+    protected function isTableExist($table)
+    {
+        $sql = "SHOW TABLES LIKE '{$table}'";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return !empty($result);
+    }
+
+    protected function isFieldExist($table, $filedName)
+    {
+        $sql = "DESCRIBE `{$table}` `{$filedName}`;";
+        $result = $this->getConnection()->fetchAssoc($sql);
+
+        return !empty($result);
+    }
+
+    protected function isIndexExist($table, $indexName)
+    {
+        $sql = "show index from `{$table}` where key_name='{$indexName}';";
+        $result = $this->getConnection()->fetchAssoc($sql);
+        return !empty($result);
+    }
+
+    protected function getSchedulerService()
+    {
+        return $this->createService('Scheduler:SchedulerService');
+    }
+
+    protected function getSettingService()
+    {
+        return $this->createService('System:SettingService');
+    }
+
+    /**
+     * @return \Biz\User\Service\UserService
+     */
+    protected function getUserService()
+    {
+        return $this->createService('User:UserService');
+    }
+}
+
+abstract class AbstractUpdater
+{
+    protected $biz;
+
+    public function __construct($biz)
+    {
+        $this->biz = $biz;
+    }
+
+    public function getConnection()
+    {
+        return $this->biz['db'];
+    }
+
+    protected function createService($name)
+    {
+        return $this->biz->service($name);
+    }
+
+    protected function createDao($name)
+    {
+        return $this->biz->dao($name);
+    }
+
+    abstract public function update();
+
+    protected function logger($level, $message)
+    {
+        $version = \AppBundle\System::VERSION;
+        $data = date('Y-m-d H:i:s') . " [{$level}] {$version} " . $message . PHP_EOL;
+        if (!file_exists($this->getLoggerFile())) {
+            touch($this->getLoggerFile());
+        }
+        file_put_contents($this->getLoggerFile(), $data, FILE_APPEND);
+    }
+
+    private function getLoggerFile()
+    {
+        return $this->biz['kernel.root_dir'] . '/../app/logs/upgrade.log';
+    }
+
+    protected function getAppLogDao()
+    {
+        return $this->createDao('CloudPlatform:CloudAppLogDao');
+    }
+}
