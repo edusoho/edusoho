387ca656f3b149afc9769f860e01fbc4e02eeeb4
mixiaotian <mi_xtyan@qq.com>
2025-03-22 18:46:11 +0800

    feat: #250322 表格之前加空行

--- src/AgentBundle/Workflow/PlanGenerate.php
+++ src/AgentBundle/Workflow/PlanGenerate.php
@@ -45,6 +45,7 @@ class PlanGenerate extends AbstractWorkflow
 2、学习时间：{$this->makeStudyBoundaryDate($plan['startDate'])} 至 {$this->makeStudyBoundaryDate($plan['endDate'])} 内，每{$this->makeChineseWeekDays($plan['weekDays'])}，共计{$studyDateCount}个学习日
 3、每次至少学习：{$everytimeLearnHour}小时
 我会在每个学习日提醒你完成学习，期待你的参与！
+
 {$this->makeTable($plan['courseId'], $studyDates, $tasks)}
 点击「学习内容」链接直达
 MARKDOWN;
