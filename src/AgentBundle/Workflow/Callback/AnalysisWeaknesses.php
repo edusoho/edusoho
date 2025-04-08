cc1cf9a92d7b42756bbebe8511d0f46c1ba6834e
mixiaotian <mi_xtyan@qq.com>
2025-04-08 20:05:49 +0800

    feat: #250408 修改push的markdown

--- src/AgentBundle/Workflow/Callback/AnalysisWeaknesses.php
+++ src/AgentBundle/Workflow/Callback/AnalysisWeaknesses.php
@@ -14,8 +14,8 @@ class AnalysisWeaknesses extends AbstractWorkflow
             'contentType' => 'text',
             'content' => $this->makeMarkdown($inputs),
             'push' => [
-                'title' => '',
-                'content' => '',
+                'title' => '推荐学习',
+                'content' => '刚才的答题结果✍️已帮你分析出薄弱知识点，快来看看自己哪方面知识需要加强👉 ',
             ],
         ]);
     }
@@ -31,7 +31,7 @@ class AnalysisWeaknesses extends AbstractWorkflow
         if (empty($inputs['documents'])) {
             return $markdown;
         }
-        $markdown .= "\n推荐以下学习知识点的相关课程任务：";
+        $markdown .= "\n推荐以下学习知识点的相关课程任务：  \n";
         $tasks = $this->getTaskService()->findTasksByActivityIds(array_column($inputs['documents'], 'extId'));
         foreach ($inputs['documents'] as $key => $document) {
             $seq = $key + 1;
