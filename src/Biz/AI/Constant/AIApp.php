0b301b6b31aa06c92cb3c5f20d5a58738eb77cf4
mixiaotian <mi_xtyan@qq.com>
2025-04-13 18:17:16 +0800

    feat: #250413 AI题目解析调用新接口

--- src/Biz/AI/Constant/AIApp.php
+++ src/Biz/AI/Constant/AIApp.php
@@ -4,19 +4,19 @@ namespace Biz\AI\Constant;
 
 final class AIApp
 {
-    const CHOICE_QUESTION_GENERATE_ANALYSIS = 'QChoiceGenAnalysis';
+    const CHOICE_QUESTION_GENERATE_ANALYSIS = 'edusoho.QChoiceGenAnalysis';
 
-    const DETERMINE_QUESTION_GENERATE_ANALYSIS = 'QTrueOrFalseGenAnalysis';
+    const DETERMINE_QUESTION_GENERATE_ANALYSIS = 'edusoho.QTrueOrFalseGenAnalysis';
 
-    const FILL_QUESTION_GENERATE_ANALYSIS = 'QBlankGenAnalysis';
+    const FILL_QUESTION_GENERATE_ANALYSIS = 'edusoho.QBlankGenAnalysis';
 
-    const ESSAY_QUESTION_GENERATE_ANALYSIS = 'QEssayGenAnalysis';
+    const ESSAY_QUESTION_GENERATE_ANALYSIS = 'edusoho.QEssayGenAnalysis';
 
-    const MATERIAL_CHOICE_QUESTION_GENERATE_ANALYSIS = 'QMChoiceGenAnalysis';
+    const MATERIAL_CHOICE_QUESTION_GENERATE_ANALYSIS = 'edusoho.QMChoiceGenAnalysis';
 
-    const MATERIAL_DETERMINE_QUESTION_GENERATE_ANALYSIS = 'QMTrueOrFalseGeAnalysis';
+    const MATERIAL_DETERMINE_QUESTION_GENERATE_ANALYSIS = 'edusoho.QMTrueOrFalseGeAnalysis';
 
-    const MATERIAL_FILL_QUESTION_GENERATE_ANALYSIS = 'QMBlankGenAnalysis';
+    const MATERIAL_FILL_QUESTION_GENERATE_ANALYSIS = 'edusoho.QMBlankGenAnalysis';
 
-    const MATERIAL_ESSAY_QUESTION_GENERATE_ANALYSIS = 'QMEssayGenAnalysis';
+    const MATERIAL_ESSAY_QUESTION_GENERATE_ANALYSIS = 'edusoho.QMEssayGenAnalysis';
 }
