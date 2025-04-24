952d51da845d75188026ed1edfb39acfdd0efa82
Johnson <ziyuorg@gmail.com>
2025-04-24 13:46:05 +0800

    feat: #111011 新增班级数据概览导出二次验证

--- app/Resources/static-src/app/js/classroom-manage/statistics/index.js
+++ app/Resources/static-src/app/js/classroom-manage/statistics/index.js
@@ -1,6 +1,7 @@
 import ClassroomStudentTrend from './student-trend';
 import StudentDetail from './student-detail';
 import TaskDetail from './task-detail';
+import ExportSecondaryVerification from '../../secondary-verification/export-secondary-verification';
 
 class ClassroomStatistics{
   constructor() {
@@ -11,6 +12,16 @@ class ClassroomStatistics{
     new ClassroomStudentTrend();
     new StudentDetail($('#student-detail-chart'));
     new TaskDetail($('#task-data-chart'));
+    new ExportSecondaryVerification({
+      buttonSelector: '.classroom-student-chart-js-export-btn',
+      formSelector: '#overview-student-detail',
+      requestUrlBase: '/secondary/verification?exportFileName=overviewStudentDetail&targetFormId=' + $('#overview-student-detail').find('[name="courseId"]').val()
+    });
+    new ExportSecondaryVerification({
+      buttonSelector: '.classroom-course-chart-js-export-btn',
+      formSelector: '#overview-task-list',
+      requestUrlBase: '/secondary/verification?exportFileName=overviewTaskList&targetFormId=' + $('#overview-task-list').find('[name="courseId"]').val()
+    });
   }
 }
 
