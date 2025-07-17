d64dccbfbe429b5eae436dc66f9801428219a770
张杰 <zhangjie@MacBook-Pro-2.local>
2025-07-17 16:58:13 +0800

    feat: #112337 PC-注册、登录、找回密码、修改密码时，密码不符合要求时给出格式提示

--- app/Resources/static-src/app/js/auth/password-reset-update/index.js
+++ app/Resources/static-src/app/js/auth/password-reset-update/index.js
@@ -11,3 +11,27 @@ let validator = $form.validate({
     }
   }
 });
+
+$('.js-password-open-eye').on('click', function () {
+  $('#form_password').attr('type', 'password');
+  $('.js-password-open-eye').hide();
+  $('.js-password-close-eye').show();
+})
+
+$('.js-password-close-eye').on('click', function () {
+  $('#form_password').attr('type', 'text');
+  $('.js-password-close-eye').hide();
+  $('.js-password-open-eye').show();
+})
+
+$('.js-confirm-password-open-eye').on('click', function () {
+  $('#form_confirmPassword').attr('type', 'password');
+  $('.js-confirm-password-open-eye').hide();
+  $('.js-confirm-password-close-eye').show();
+})
+
+$('.js-confirm-password-close-eye').on('click', function () {
+  $('#form_confirmPassword').attr('type', 'text');
+  $('.js-confirm-password-close-eye').hide();
+  $('.js-confirm-password-open-eye').show();
+})
