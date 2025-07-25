6db63e8e963999ebb06e89d31a595ecb505b5092
张杰 <zhangjie@MacBook-Pro-2.local>
2025-07-25 17:13:55 +0800

    feat: #20250725 暂存

--- h5/src/h5/utils/rule-message.js
+++ h5/src/h5/utils/rule-message.js
@@ -2,4 +2,4 @@ export const EMAIL_MESSAGE = '请输入正确的邮箱';
 export const PHONE_MESSAGE = '请输入正确的手机号';
 export const USER_MESSAGE = '名称已存在';
 export const PASSWORD_MESSAGE = '密码有误';
-export const PASSWORD_REGISTER = '请设置8-32位包含字母大小写、数字、符号四种字符组合成的密码';
+export const PASSWORD_REGISTER = '请输入8-32位包含字母大小写、数字、符号四种字符组合成的密码';
