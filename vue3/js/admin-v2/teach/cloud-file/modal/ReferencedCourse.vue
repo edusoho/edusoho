160b75bb42840c3285805887ad8ea533801e5e21
张杰 <zhangjie@MacBook-Pro-2.local>
2025-08-22 11:13:21 +0800

    feat: #20250822 重构代码

---
+++ vue3/js/admin-v2/teach/cloud-file/modal/ReferencedCourse.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue';
+
+const modalVisible = ref(false);
+
+emitter.on('open-category-modal', (params) => {
+  if (params.ids.length > 0) {
+    ids.value = params.ids;
+    modalVisible.value = true;
+  }
+});
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
