9c9eb7f54a76015fbd402cd8313e7c1fb274b64a
ZhangJie <zhangjie@edusoho.com>
2025-09-30 00:09:25 +0800

    修复t函数兼容性问题

--- app/Resources/static-src/common/vue/locale/index.js
+++ app/Resources/static-src/common/vue/locale/index.js
@@ -26,8 +26,10 @@ export const t = function(path, options) {
   for (let i = 0, j = array.length; i < j; i++) {
     const property = array[i];
     value = current[property];
-    if (i === j - 1) return format(value, options);
-    if (!value) return '';
+    if (i === j - 1) {
+      return value != null ? format(value, options) : path;
+    }
+    if (!value) return path;
     current = value;
   }
   return '';
